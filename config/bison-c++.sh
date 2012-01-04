2dd24cfcb860034f21f940f907bcb193b2f5b9c4
Frank Celler <frank@arangodb.com>
2012-01-04 15:23:06 +0100

    adjustment for different build dir

--- config/bison-c++.sh
+++ config/bison-c++.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
 BISON="$1"
-SRC="$2"
-DST="$2"
-FILE="$3"
+OUTPUT="$2"
+INPUT="$3"
 
-if test "x$BISON" = x -o "x$SRC" = x -o "x$DST" = x -o "x$FILE" = x;  then
-  echo "usage: $0 <bison> <directory> <file-prefix>"
-  exit 1
-fi
-
-if test ! -d "$SRC";  then
-  echo "$0: expecting '$SRC' to be a directory"
-  exit 1
-fi
-
-if test ! -d "$DST";  then
-  echo "$0: expecting '$DST' to be a directory"
+if test "x$BISON" = x -o "x$OUTPUT" = x -o "x$INPUT" = x;  then
+  echo "usage: $0 <bison> <output> <input>"
   exit 1
 fi
 
@@ -24,33 +13,34 @@ fi
 ## bison
 #############################################################################
 
-${BISON} -l -d -ra -S lalr1.cc -o "${DST}/${FILE}.cpp" "${SRC}/${FILE}.yy"
+${BISON} -l -d -ra -S lalr1.cc -o ${OUTPUT} ${INPUT}
 
 #############################################################################
 ## sanity checks
 #############################################################################
 
-test -f ${SRC}/${FILE}.hpp || exit 1
-test -f ${SRC}/${FILE}.cpp || exit 1
-test -f ${SRC}/position.hh || exit 1
+PREFIX=`echo ${OUTPUT} | sed -e 's:\.cpp$::'`
+
+test -f ${PREFIX}.hpp || exit 1
+test -f ${PREFIX}.cpp || exit 1
 
 #############################################################################
 ## rename file
 #############################################################################
 
-mv ${SRC}/${FILE}.hpp ${SRC}/${FILE}.h || exit 1
+mv ${PREFIX}.hpp ${PREFIX}.h || exit 1
 
 #############################################################################
 ## fix header file name in source, fix defines
 #############################################################################
 
-sed -e 's:\.hpp:.h:' < ${SRC}/${FILE}.cpp \
+sed -e 's:\.hpp:.h:' < ${OUTPUT} \
   | sed -e 's:# if YYENABLE_NLS:# if defined(YYENABLE_NLS) \&\& YYENABLE_NLS:' \
-  > ${SRC}/${FILE}.cpp.tmp
+  > ${OUTPUT}.tmp
 
 # give some information
-diff -u ${SRC}/${FILE}.cpp ${SRC}/${FILE}.cpp.tmp
+diff -u ${OUTPUT} ${OUTPUT}.tmp
 
 # and move the files to the final destination
-mv ${SRC}/${FILE}.cpp.tmp ${SRC}/${FILE}.cpp || exit 1
+mv ${OUTPUT}.tmp ${OUTPUT} || exit 1
 
--- config/flex-c++.sh
+++ config/flex-c++.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
 FLEX="$1"
-DIR="$2"
-FILE="$3"
+OUTPUT="$2"
+INPUT="$3"
 
-if test "x$FLEX" = "x" -o "x$DIR" = "x" -o "x$FILE" = "x" -o ! -d "$DIR";  then
-  echo "usage: $0 <flex> <directory> <file-prefix>"
-  exit 1
-fi
-
-if test ! -d "$DIR";  then
-  echo "$0: expecting '$DIR' to be a directory"
+if test "x$FLEX" = "x" -o "x$OUTPUT" = "x" -o "x$INPUT" = "x";  then
+  echo "usage: $0 <flex> <output> <input>"
   exit 1
 fi
 
@@ -18,24 +13,24 @@ fi
 ## flex
 #############################################################################
 
-${FLEX} -L -o${DIR}/${FILE}.cpp ${DIR}/${FILE}.ll
+${FLEX} -L -o${OUTPUT} ${INPUT}
 
 #############################################################################
 ## sanity checks
 #############################################################################
 
-test -f ${DIR}/${FILE}.cpp || exit 1
+test -f ${OUTPUT} || exit 1
 
 #############################################################################
 ## fix casts
 #############################################################################
 
-cat ${DIR}/${FILE}.cpp \
+cat ${OUTPUT} \
   | sed -e 's:(yy_n_chars), (size_t) num_to_read );:(yy_n_chars), (int) num_to_read );:' \
-  > ${DIR}/${FILE}.cpp.tmp
+  > ${OUTPUT}.tmp
 
 # give some information
-diff -u ${DIR}/${FILE}.cpp ${DIR}/${FILE}.cpp.tmp
+diff -u ${OUTPUT} ${OUTPUT}.tmp
 
 # and move the files to the final destination
-mv ${DIR}/${FILE}.cpp.tmp ${DIR}/${FILE}.cpp || exit 1
+mv ${OUTPUT}.tmp ${OUTPUT} || exit 1
