c54f60fb471bfdf768cfc1c5353123a160beb650
Frank Celler <frank@arangodb.com>
2012-01-13 15:43:01 +0100

    adjusted paths

---
+++ RestServer/install.h
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @brief installation guide
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2004-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Dr. Frank Celler
+/// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @page CompilingTOC
+///
+/// <ol>
+///  <li>@ref CompilingPrerequisites "Prerequisites"</li>
+/// </ol>
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @page Compiling Compiling the AvocadoDB
+///
+/// <hr>
+/// @copydoc CompilingTOC
+/// <hr>
+///
+/// @anchor CompilingPrerequisites
+/// @section CompilingPrerequisites
+///
+/// In order to compile the AvocadoDB, you will need
+///
+/// - a recent version of GCC / G++
+/// - a recent version of FLEX
+/// - a recent version of BISON
+///
+/// - the libev in version 3 or 4
+/// - Google's V8 engine
+/// - the readline library
+/// - the ncurses library
+/// - the boost header files
+////////////////////////////////////////////////////////////////////////////////
+
+// Local Variables:
+// mode: outline-minor
+// outline-regexp: "^\\(/// @brief\\|/// {@inheritDoc}\\|/// @addtogroup\\|// --SECTION--\\|/// @\\}\\)"
+// End:
