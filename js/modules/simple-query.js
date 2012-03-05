865a402e3e4e266b159f01830787dc659646b4ab
Frank Celler <frank@arangodb.com>
2012-03-05 10:06:41 +0100

    better error handling

--- js/modules/simple-query.js
+++ js/modules/simple-query.js
@@ -1,11 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief Avocado Query Language
+/// @brief Avocado Simple Query Language
 ///
 /// @file
 ///
 /// DISCLAIMER
 ///
-/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+/// Copyright 2012 triagens GmbH, Cologne, Germany
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
