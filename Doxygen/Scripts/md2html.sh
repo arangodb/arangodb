2f0aeda80018ff813d5ec56d737b2e0533305ebe
Frank Celler <frank@arangodb.com>
2012-03-11 21:17:15 +0100

    doc

--- Doxygen/Scripts/md2html.sh
+++ Doxygen/Scripts/md2html.sh
@@ -13,4 +13,6 @@ OUTPUT="`dirname $INPUT`/`basename $INPUT .md`.html"
 perl "$MARKDOWN" "$INPUT" \
     | sed -r -e "s/href=\"([^\"#]+)([\"#])/href=\"\1\.html\2/g" \
     | sed -e "s/href=\"wiki\//href=\"/g" \
+    | sed -e "s/<var>/<it>/g" \
+    | sed -e "s/<\/var>/<\/it>/g" \
     | sed -e "s/#wiki-/#/g" > $OUTPUT
