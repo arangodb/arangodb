d87e698d61cad60b5b3e4f0b7625a7e8dcf0beee
Frank Celler <frank@arangodb.com>
2012-04-11 16:47:42 +0200

    more doc for shell interface

--- VocBase/vocbase.c
+++ VocBase/vocbase.c
@@ -1403,6 +1403,7 @@ int TRI_DropCollectionVocBase (TRI_vocbase_t* vocbase, TRI_vocbase_col_t* collec
   // .............................................................................
 
   else if (collection->_status == TRI_VOC_COL_STATUS_NEW_BORN) {
+    collection->_status = TRI_VOC_COL_STATUS_DELETED;
     TRI_WRITE_UNLOCK_STATUS_VOCBASE_COL(collection);
     return TRI_ERROR_NO_ERROR;
   }
