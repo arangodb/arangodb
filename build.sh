2ed9558cdc83abbb827f3a8e8d5cc1c35acc500b
Frank Celler <frank@arangodb.com>
2012-04-12 15:02:29 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -6,10 +6,9 @@ echo "########################################################"
 
 . config/detect_distro.sh
 
-ARCH=""
-OPTIONS=""
+OPTIONS="--disable-dependency-tracking --disable-debug --disable-relative"
 PREFIX="--prefix=/usr --sysconfdir=/etc"
-RESULTS="avocado"
+RESULTS="avocado avocsh"
 
 export CPPFLAGS=""
 export LDFLAGS=""
@@ -26,25 +25,25 @@ case $TRI_OS_LONG in
 
   Linux-openSUSE-11.4*)
     echo "Using configuration for openSuSE 11.4"
-    STATIC=""
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
   Linux-openSUSE-11*)
     echo "Using configuration for openSuSE 11"
-    STATIC="--enable-static-libev"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
   Linux-Debian-6*)
     echo "Using configuration for Debian"
-    STATIC="--enable-static-libev --enable-static-boost --enable-static-mysql"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
   Linux-Debian*)
     echo "Using configuration for Debian"
-    STATIC="--enable-static-libev --enable-static-boost --enable-static-mysql"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
@@ -55,37 +54,34 @@ case $TRI_OS_LONG in
 
   Linux-Ubuntu-11.10*)
     echo "Using configuration for Ubuntu"
-    STATIC="--enable-static-libev --enable-static-boost --enable-static-mysql"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
   Linux-Ubuntu-*)
     echo "Using configuration for Ubuntu"
-    STATIC="--enable-static-libev --enable-static-boost --enable-static-mysql"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     LDD_INFO="yes"
     ;;
 
   Darwin*)
     echo "Using configuration for DARWIN"
-    ARCH="32 64"
     CPPFLAGS='-isystem /usr/include -isystem /opt/local/include -Wno-deprecated-declarations'
     LDFLAGS='-L/usr/lib -L/opt/local/lib' # need to use OpenSSL from system
-    STATIC="--enable-static-boost --enable-static-mysql"
+    OPTIONS="$(OPTIONS) --enable-all-in-one"
     ;;
 
   *)
     echo "Using default configuration"
-    OPTIONS="--enable-error-on-warning"
+    OPTIONS="$(OPTIONS) --enable-error-on-warning"
     ;;
 
 esac
 
 echo
 echo "########################################################"
-echo "ARCH: $ARCH"
 echo "CPPFLAGS: $CPPFLAGS"
 echo "LDFLAGS: $LDFLAGS"
-echo "STATIC: $STATIC"
 echo "OPTIONS: $OPTIONS"
 echo "########################################################"
 echo
@@ -97,76 +93,23 @@ echo
 
 make setup || exit 1
 
-TARGETS=""
+echo
+echo "########################################################"
+echo "configure:"
+echo "   ./configure $PREFIX $OPTIONS"
+echo "########################################################"
+echo
 
-if test "x$ARCH" = "x";  then
+./configure $PREFIX $OPTIONS || exit 1
 
-  echo
-  echo "########################################################"
-  echo "configure:"
-  echo "   ./configure $PREFIX $OPTIONS"
-  echo "########################################################"
-  echo
+echo
+echo "########################################################"
+echo "compile:"
+echo "    make $TARGETS"
+echo "########################################################"
+echo
 
-  ./configure $PREFIX $OPTIONS || exit 1
-
-  echo
-  echo "########################################################"
-  echo "compile:"
-  echo "    make $TARGETS"
-  echo "########################################################"
-  echo
-
-  make -j $MAKEJ $TARGETS || exit 1
-
-else
-  for arch in $ARCH;  do
-    CONF_FLAGS=""
-
-    if test "x$arch" = x32;  then
-      CONF_FLAGS="--enable-32bit"
-    fi
-
-    echo
-    echo "########################################################"
-    echo "configure architekture $arch"
-    echo "   rm -rf ARCH.$arch ; mkdir ARCH.$arch ; cd ARCH.$arch"
-    echo "   ../configure $PREFIX $OPTIONS $CONF_FLAGS\""
-    echo "########################################################"
-    echo
-
-    rm -rf ARCH.$arch
-    mkdir ARCH.$arch
-
-    cd ARCH.$arch 
-
-    ../configure \
-        $PREFIX \
-        $OPTIONS \
-        $CONF_FLAGS || exit 1
-
-    echo
-    echo "########################################################"
-    echo "compile: "
-    echo "    make -j $MAKEJ $TARGETS "
-    echo "########################################################"
-    echo
-
-    make -j $MAKEJ $TARGETS || exit 1
-
-    cd ..
-  done
-
-  for result in $RESULTS;  do
-    lipo=""
-
-    for arch in $ARCH;  do
-      lipo="$lipo ARCH.$arch/$result"
-    done
-
-    lipo $lipo -create -output $result || exit 1
-  done
-fi
+make -j $MAKEJ || exit 1
 
 for result in $RESULTS;  do
   echo
