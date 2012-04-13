c2f9fcfb2c3c56266616941b0642d525462abf5d
Frank Celler <frank@arangodb.com>
2012-04-13 18:51:10 +0200

    added build file for coverage

---
+++ coverage.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+echo "########################################################"
+echo "#                     build.sh                         #"
+echo "########################################################"
+
+. config/detect_distro.sh
+
+OPTIONS="--disable-dependency-tracking --disable-relative --enable-gcov"
+PREFIX="--prefix=/usr --sysconfdir=/etc"
+RESULTS="avocado avocsh"
+
+export CPPFLAGS=""
+export LDFLAGS=""
+export MAKEJ=2
+export LDD_INFO="no"
+
+echo
+echo "########################################################"
+echo "Building on $TRI_OS_LONG"
+echo "########################################################"
+echo
+
+case $TRI_OS_LONG in
+
+  Linux-openSUSE-11.4*)
+    echo "Using configuration for openSuSE 11.4"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-openSUSE-11*)
+    echo "Using configuration for openSuSE 11"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    VALGRIND_TEST="yes"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-Debian-6*)
+    echo "Using configuration for Debian"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-Debian*)
+    echo "Using configuration for Debian"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-CentOS-*)
+    echo "Using configuration for Centos"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-Ubuntu-11.10*)
+    echo "Using configuration for Ubuntu"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    LDD_INFO="yes"
+    ;;
+
+  Linux-Ubuntu-*)
+    echo "Using configuration for Ubuntu"
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    LDD_INFO="yes"
+    ;;
+
+  Darwin*)
+    echo "Using configuration for DARWIN"
+    CPPFLAGS='-isystem /usr/include -isystem /opt/local/include -Wno-deprecated-declarations'
+    LDFLAGS='-L/usr/lib -L/opt/local/lib' # need to use OpenSSL from system
+    OPTIONS="$OPTIONS --enable-all-in-one"
+    ;;
+
+  *)
+    echo "Using default configuration"
+    OPTIONS="$OPTIONS --enable-error-on-warning"
+    ;;
+
+esac
+
+echo
+echo "########################################################"
+echo "CPPFLAGS: $CPPFLAGS"
+echo "LDFLAGS: $LDFLAGS"
+echo "OPTIONS: $OPTIONS"
+echo "########################################################"
+echo
+echo "########################################################"
+echo "init system:"
+echo "   make setup"
+echo "########################################################"
+echo
+
+make setup || exit 1
+
+echo
+echo "########################################################"
+echo "configure:"
+echo "   ./configure $PREFIX $OPTIONS"
+echo "########################################################"
+echo
+
+./configure $PREFIX $OPTIONS || exit 1
+
+echo
+echo "########################################################"
+echo "compile:"
+echo "    make $TARGETS"
+echo "########################################################"
+echo
+
+make -j $MAKEJ || exit 1
+
+echo
+
+echo
+echo "########################################################"
+echo "unittests:"
+echo "    make unittests"
+echo "########################################################"
+echo
+
+make unittests FORCE=1 || exit 1
