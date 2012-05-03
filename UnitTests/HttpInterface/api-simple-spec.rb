0939ae81d04fee638d6a29f09f7e96677aea6390
Frank Celler <f.celler@triagens.de>
2012-05-03 14:42:43 +0200

    added within

--- UnitTests/HttpInterface/api-simple-spec.rb
+++ UnitTests/HttpInterface/api-simple-spec.rb
@@ -100,7 +100,7 @@ describe AvocadoDB do
     end
 
 ################################################################################
-## geo query
+## geo near query
 ################################################################################
 
     context "geo query:" do
@@ -166,6 +166,86 @@ describe AvocadoDB do
 	cmd = api + "/near"
 	body = "{ \"collection\" : \"#{@cid}\", \"latitude\" : 0, \"longitude\" : 0, \"limit\" : 5, \"distance\" : \"distance\" }"
 	doc = AvocadoDB.log_put("#{prefix}-near-distance", cmd, :body => body)
+
+	doc.code.should eq(201)
+	doc.headers['content-type'].should eq("application/json")
+	doc.parsed_response['error'].should eq(false)
+	doc.parsed_response['code'].should eq(201)
+	doc.parsed_response['hasMore'].should eq(false)
+	doc.parsed_response['result'].length.should eq(5)
+	doc.parsed_response['count'].should eq(5)
+
+	doc.parsed_response['result'].map{|i| i['distance'].floor.to_f}.should eq([0,1111949,1111949,1111949,1111949])
+      end
+    end
+
+################################################################################
+## geo within query
+################################################################################
+
+    context "geo query:" do
+      before do
+	@cn = "UnitTestsCollectionGeo"
+	AvocadoDB.drop_collection(@cn)
+	@cid = AvocadoDB.create_collection(@cn, false)
+
+	cmd = api + "?collection=#{@cid}"
+	body = "{ \"type\" : \"geo\", \"fields\" : [ \"a\" ] }"
+        #doc = AvocadoDB.post(cmd, :body => body)
+
+	(0..10).each{|i|
+	  lat = 10 * (i - 5)
+
+	  (0..10).each{|j|
+	    lon = 10 * (j - 5)
+	    
+	    AvocadoDB.post("/document?collection=#{@cid}", :body => "{ \"loc\" : [ #{lat}, #{lon} ] }")
+	  }
+	}
+      end
+
+      after do
+	AvocadoDB.drop_collection(@cn)
+      end
+
+      it "returns an error for within without index" do
+	cmd = api + "/within"
+	body = "{ \"collection\" : \"#{@cid}\", \"latitude\" : 0, \"longitude\" : 0, \"skip\" : 1, \"limit\" : 5 }"
+	doc = AvocadoDB.log_put("#{prefix}-within-missing", cmd, :body => body)
+
+	doc.code.should eq(400)
+	doc.headers['content-type'].should eq("application/json")
+	doc.parsed_response['error'].should eq(true)
+	doc.parsed_response['code'].should eq(400)
+	doc.parsed_response['errorNum'].should eq(1512)
+      end
+
+      it "returns documents within a radius" do
+	cmd = "/_api/index?collection=#{@cid}"
+	body = "{ \"type\" : \"geo\", \"fields\" : [ \"loc\" ] }"
+        doc = AvocadoDB.post(cmd, :body => body)
+
+	cmd = api + "/within"
+	body = "{ \"collection\" : \"#{@cid}\", \"latitude\" : 0, \"longitude\" : 0, \"skip\" : 1, \"radius\" : 1111950 }"
+	doc = AvocadoDB.log_put("#{prefix}-within", cmd, :body => body)
+
+	doc.code.should eq(201)
+	doc.headers['content-type'].should eq("application/json")
+	doc.parsed_response['error'].should eq(false)
+	doc.parsed_response['code'].should eq(201)
+	doc.parsed_response['hasMore'].should eq(false)
+	doc.parsed_response['result'].length.should eq(4)
+	doc.parsed_response['count'].should eq(4)
+      end
+
+      it "returns documents and distance within a radius" do
+	cmd = "/_api/index?collection=#{@cid}"
+	body = "{ \"type\" : \"geo\", \"fields\" : [ \"loc\" ] }"
+        doc = AvocadoDB.post(cmd, :body => body)
+
+	cmd = api + "/within"
+	body = "{ \"collection\" : \"#{@cid}\", \"latitude\" : 0, \"longitude\" : 0, \"distance\" : \"distance\", \"radius\" : 1111950 }"
+	doc = AvocadoDB.log_put("#{prefix}-within-distance", cmd, :body => body)
 
 	doc.code.should eq(201)
 	doc.headers['content-type'].should eq("application/json")
