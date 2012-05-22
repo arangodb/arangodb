0b9c6f7de1988367f3061656e8a4a77d4c82da19
Heiko Kernbach <heiko@arangodb.com>
2012-05-22 12:26:12 +0200

    added _api/import

--- RestServer/ArangoServer.cpp
+++ RestServer/ArangoServer.cpp
@@ -701,6 +701,8 @@ int ArangoServer::startupServer () {
                                    RestHandlerCreator<RestActionHandler>::createData< pair< TRI_vocbase_t*, set<string>* >* >,
                                    (void*) &handlerDataAdmin);
 
+    adminFactory->addPrefixHandler(RestVocbaseBaseHandler::DOCUMENT_IMPORT_PATH, RestHandlerCreator<RestImportHandler>::createData<TRI_vocbase_t*>, _vocbase);
+
     _adminHttpServer = _applicationHttpServer->buildServer(new ArangoHttpServer(scheduler, dispatcher), adminFactory, adminPorts);
   }
 
