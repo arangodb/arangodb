262e046d0ee21c27f91fd34e8ebb9b7fca406331
moonglum <moonglum@moonbeamlabs.com>
2012-05-23 16:21:34 +0200

    Added the Random 500 Test and fixed a bug

--- js/common/tests/shell-graph-algorithms.js
+++ js/common/tests/shell-graph-algorithms.js
@@ -1,15 +1,15 @@
 /*jslint indent: 2,
          nomen: true,
-         maxlen: 80 */
+         maxlen: 80,
+         sloppy: true */
 /*global require,
     db,
     assertEqual, assertTrue,
     print,
     PRINT_OBJECT,
     console,
-    AvocadoCollection, AvocadoEdgesCollection */
-(function () {
-  "use strict";
+    AvocadoCollection, AvocadoEdgesCollection,
+    processCsvFile */
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief test the graph class
@@ -38,7 +38,8 @@
 /// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
 ////////////////////////////////////////////////////////////////////////////////
 
-  var jsunity = require("jsunity");
+var jsunity = require("jsunity"),
+  Helper = require("test-helper").Helper;
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                collection methods
@@ -48,127 +49,174 @@
 /// @brief test suite: Dijkstra
 ////////////////////////////////////////////////////////////////////////////////
 
-  function dijkstraSuite() {
-    var Graph = require("graph").Graph,
-      graph_name = "UnitTestsCollectionGraph",
-      vertex = "UnitTestsCollectionVertex",
-      edge = "UnitTestsCollectionEdge",
-      graph = null;
+function dijkstraSuite() {
+  var Graph = require("graph").Graph,
+    graph_name = "UnitTestsCollectionGraph",
+    vertex = "UnitTestsCollectionVertex",
+    edge = "UnitTestsCollectionEdge",
+    graph = null;
 
-    return {
+  return {
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief set up
 ////////////////////////////////////////////////////////////////////////////////
 
-      setUp : function () {
+    setUp : function () {
+      try {
         try {
-          try {
-            // Drop the graph if it exsits
-            graph = new Graph(graph_name);
-            print("FOUND: ");
-            PRINT_OBJECT(graph);
-            graph.drop();
-          } catch (err1) {
-          }
-
-          graph = new Graph(graph_name, vertex, edge);
-        } catch (err2) {
-          console.error("[FAILED] setup failed:" + err2);
+          // Drop the graph if it exsits
+          graph = new Graph(graph_name);
+          print("FOUND: ");
+          PRINT_OBJECT(graph);
+          graph.drop();
+        } catch (err1) {
         }
-      },
+
+        graph = new Graph(graph_name, vertex, edge);
+      } catch (err2) {
+        console.error("[FAILED] setup failed:" + err2);
+      }
+    },
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief tear down
 ////////////////////////////////////////////////////////////////////////////////
 
-      tearDown : function () {
-        try {
-          if (graph !== null) {
-            graph.drop();
-          }
-        } catch (err) {
-          console.error("[FAILED] tear-down failed:" + err);
+    tearDown : function () {
+      try {
+        if (graph !== null) {
+          graph.drop();
         }
-      },
+      } catch (err) {
+        console.error("[FAILED] tear-down failed:" + err);
+      }
+    },
 
-// Helper
+    testPushToNeighbors : function () {
+      var v1 = graph.addVertex(1),
+        v2 = graph.addVertex(2),
+        v3 = graph.addVertex(3),
+        test_array = [];
 
-      testPushToNeighbors : function () {
-        var v1 = graph.addVertex(1),
-          v2 = graph.addVertex(2),
-          v3 = graph.addVertex(3),
-          test_array = [];
+      graph.addEdge(v1, v2);
+      graph.addEdge(v1, v3);
 
-        graph.addEdge(v1, v2);
-        graph.addEdge(v1, v3);
+      v1._pushNeigborsToArray(test_array);
 
-        v1._pushNeigborsToArray(test_array);
+      assertEqual(test_array[0], 2);
+      assertEqual(test_array[1], 3);
+    },
 
-        assertEqual(test_array[0], 2);
-        assertEqual(test_array[1], 3);
-      },
+    testShortestDistanceFor : function () {
+      var v1 = graph.addVertex(1),
+        v2 = graph.addVertex(2),
+        v3 = graph.addVertex(3),
+        todo_list = [v1.getId(), v2.getId()], // [ID]
+        distances = {}, // {ID => [Node]}
+        node_id_found; // Node
 
-      testShortestDistanceFor : function () {
-        var v1 = graph.addVertex(1),
-          v2 = graph.addVertex(2),
-          v3 = graph.addVertex(3),
-          todo_list = [v1.getId(), v2.getId()], // [ID]
-          distances = {}, // {ID => [Node]}
-          node_id_found; // Node
+      distances[v1.getId()] = [v1, v2, v3];
+      distances[v2.getId()] = [v2, v3];
+      distances[v3.getId()] = [v3];
 
-        distances[v1.getId()] = [v1, v2, v3];
-        distances[v2.getId()] = [v2, v3];
-        distances[v3.getId()] = [v3];
-
-        node_id_found = v1._getShortestDistanceFor(todo_list, distances);
-        assertEqual(node_id_found, v2.getId());
-      },
+      node_id_found = v1._getShortestDistanceFor(todo_list, distances);
+      assertEqual(node_id_found, v2.getId());
+    },
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief get a short, distinct path
 ////////////////////////////////////////////////////////////////////////////////
 
-      testGetAShortDistinctPath : function () {
-        var v1, v2, e1;
+    testGetAShortDistinctPath : function () {
+      var v1, v2, e1;
 
-        v1 = graph.addVertex(1);
-        v2 = graph.addVertex(2);
+      v1 = graph.addVertex(1);
+      v2 = graph.addVertex(2);
 
-        e1 = graph.addEdge(v1, v2);
+      e1 = graph.addEdge(v1, v2);
 
-        assertEqual(v1.pathTo(v2).length, 2);
-        assertEqual(v1.pathTo(v2)[0].getId(), v1.getId());
-        assertEqual(v1.pathTo(v2)[1].getId(), v2.getId());
-      },
+      assertEqual(v1.pathTo(v2).length, 2);
+      assertEqual(v1.pathTo(v2)[0].getId(), v1.getId());
+      assertEqual(v1.pathTo(v2)[1].getId(), v2.getId());
+    },
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief get a longer, distinct path
 ////////////////////////////////////////////////////////////////////////////////
 
-      testGetALongerDistinctPath : function () {
-        var v1, v2, v3, e1, e2;
+    testGetALongerDistinctPath : function () {
+      var v1, v2, v3, e1, e2;
 
-        v1 = graph.addVertex(1);
-        v2 = graph.addVertex(2);
-        v3 = graph.addVertex(3);
+      v1 = graph.addVertex(1);
+      v2 = graph.addVertex(2);
+      v3 = graph.addVertex(3);
 
-        e1 = graph.addEdge(v1, v2);
-        e2 = graph.addEdge(v2, v3);
+      e1 = graph.addEdge(v1, v2);
+      e2 = graph.addEdge(v2, v3);
 
-        assertEqual(v1.pathTo(v3).length, 3);
-        assertEqual(v1.pathTo(v3)[0].getId(), v1.getId());
-        assertEqual(v1.pathTo(v3)[1].getId(), v2.getId());
-        assertEqual(v1.pathTo(v3)[2].getId(), v3.getId());
-      }
-    };
-  }
+      assertEqual(v1.pathTo(v3).length, 3);
+      assertEqual(v1.pathTo(v3)[0].getId(), v1.getId());
+      assertEqual(v1.pathTo(v3)[1].getId(), v2.getId());
+      assertEqual(v1.pathTo(v3)[2].getId(), v3.getId());
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief compare with Neo4j on a random network of size 500
+////////////////////////////////////////////////////////////////////////////////
+
+    testComparisonWithNeo4j500 : function () {
+      var base_path = "js/common/test-data/random500/",
+        v1,
+        v2,
+        e,
+        path,
+        results = [],
+        counter;
+
+      Helper.process(base_path + "generated_edges.csv", function (row) {
+        v1 = graph.getOrAddVertex(row[1]);
+        v2 = graph.getOrAddVertex(row[2]);
+        e = graph.addEdge(v1, v2);
+      });
+
+      Helper.process(base_path + "generated_testcases.csv", function (row) {
+        v1 = graph.getVertex(row[0]);
+        v2 = graph.getVertex(row[1]);
+        if (v1 !== null && v2 !== null) {
+          path = v1.pathTo(v2);
+          results.push(path);
+        }
+      });
+
+      v1 = "";
+      v2 = "";
+      counter = 0;
+      Helper.process(base_path + "neo4j-dijkstra.csv", function (row) {
+        var result;
+
+        if (!(v1 === row[0] && v2 === row[row.length - 1])) {
+          v1 = row[0];
+          v2 = row[row.length - 1];
+          result = results[counter];
+
+          assertEqual(v1, result[0].getId());
+          assertEqual(v2, result[result.length - 1].getId());
+
+          if (result.length !== row.length) {
+            console.log("Neo4j: Path from " + v1 + " to " + v2 + " has length " + result.length + " (should be " + row.length + ")");
+          }
+          counter += 1;
+        }
+        //assertEqual(raw_row, results[index]);
+      });
+    }
+  };
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief executes the test suites
 ////////////////////////////////////////////////////////////////////////////////
 
-  jsunity.run(dijkstraSuite);
-  jsunity.done();
-
-}());
+jsunity.run(dijkstraSuite);
+return jsunity.done();
