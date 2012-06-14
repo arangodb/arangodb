7b4fd66b2268daa260470c121432ea46ad9cb2c8
hkernbach <h.kernbach@triagens.de>
2012-06-14 11:19:50 +0200

    result not used

--- html/admin/js/test.js
+++ html/admin/js/test.js
@@ -6,18 +6,62 @@ jsUnity.log = console;
 
 function tableTestSuite () {
 
-  function testDerHans () {
-    jsUnity.assertions.assertEqual("string: peng", value2html("peng"));
-    jsUnity.assertions.assertEqual("string: 25", value2html("25"));
-    jsUnity.assertions.assertEqual("number: 25", value2html(25));
+////////////////////////////////////////////////////////////////////////////////
+/// @brief value2Html function
+////////////////////////////////////////////////////////////////////////////////
+
+  function test_value2Html () {
+    jsUnity.assertions.assertEqual("<a class=sh_string>peng</a>", value2html("peng"));
+    jsUnity.assertions.assertEqual("<a class=sh_string>25</a>", value2html("25"));
+    jsUnity.assertions.assertEqual("<a class=sh_number>25</a>", value2html(25));
     jsUnity.assertions.assertFalse(false);
     jsUnity.assertions.assertTrue(!!true);
   }
-/*
-  function testFleisch () {
-    //jsUnity.assertions.assertEqual("string: peng sexmann", value2html("peng"));  
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief is_int function 
+////////////////////////////////////////////////////////////////////////////////
+
+  function test_is_int () {
+    jsUnity.assertions.assertEqual(true, is_int(25));
+    jsUnity.assertions.assertEqual(false, is_int(25.5));
   }
-*/
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief bytesToSize function
+////////////////////////////////////////////////////////////////////////////////
+
+  function test_bytesToSize() {
+    jsUnity.assertions.assertEqual("1 B", bytesToSize(1));
+    jsUnity.assertions.assertEqual("1 KB", bytesToSize(1024));
+    jsUnity.assertions.assertEqual("1 MB", bytesToSize(1048576));
+    jsUnity.assertions.assertEqual("1 GB", bytesToSize(1073741824));
+    jsUnity.assertions.assertEqual("1 TB", bytesToSize(1099511627776));
+  }
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief RealTypeOf function
+////////////////////////////////////////////////////////////////////////////////
+
+  function test_RealTypeOf() {
+    jsUnity.assertions.assertEqual("string", RealTypeOf("hehe"));
+    jsUnity.assertions.assertEqual("number", RealTypeOf(123));
+    jsUnity.assertions.assertEqual("string", RealTypeOf("123"));
+    jsUnity.assertions.assertEqual("array", RealTypeOf([1, 2, 3]));
+    jsUnity.assertions.assertEqual("object", RealTypeOf({"hehe":123}));
+  }
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief getTypedValue
+////////////////////////////////////////////////////////////////////////////////
+
+  function test_getTypedValue() {
+    jsUnity.assertions.assertEqual(true, getTypedValue("true"));
+    jsUnity.assertions.assertEqual(false, getTypedValue("false"));
+    jsUnity.assertions.assertEqual(123, getTypedValue("123"));
+    jsUnity.assertions.assertEqual("hehe", getTypedValue("hehe"));
+    jsUnity.assertions.assertEqual(null, getTypedValue("null"));
+  }
 
 }
 
