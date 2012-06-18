13915a9c859b32c60d8066d4abb39abda36c63bd
jsteemann <j.steemann@triagens.de>
2012-06-18 11:12:42 +0200

    added getopt options parsing, added pipelining

--- PerfTests/zclient.cpp
+++ PerfTests/zclient.cpp
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
+#include <getopt.h>
 
 #include <pthread.h>
 
@@ -18,8 +20,99 @@ using namespace std;
 
 void* context = 0;
 size_t loop = 10000;
+size_t concurrency = 1;
+size_t pipelined = 1;
 char const* connection = "tcp://localhost:5555";
+char const* path = "/_api/version";
 
+
+
+void* ThreadStarter (void* data) {
+  int res;
+  size_t n, p;
+  void* requester;
+
+  requester = zmq_socket(context, ZMQ_REQ);
+  res = zmq_connect(requester, connection);
+
+  if (res != 0) {
+    printf("ERROR zmq_connect: %d\n", errno);
+    exit(EXIT_FAILURE);
+  }
+  
+  printf("connected\n");
+
+  for (n = 0; n < loop; n += pipelined) {
+    zmq_msg_t request;
+    zmq_msg_t reply;
+
+    PB_ArangoMessage messages;
+    PB_ArangoBatchMessage* batch;
+    PB_ArangoBlobRequest* blob;
+
+    for (p = 0; p < pipelined; p++) {
+      batch = messages.add_messages();
+
+      batch->set_type(PB_BLOB_REQUEST);
+      blob = batch->mutable_request();
+
+      blob->set_requesttype(PB_REQUEST_TYPE_GET);
+      blob->set_url(path);
+      blob->set_contenttype(PB_NO_CONTENT);
+      blob->set_contentlength(0);
+      blob->set_content("");
+    }
+   
+    string data;
+    messages.SerializeToString(&data);
+
+    // send
+    zmq_msg_init_size(&request, data.size());
+    memcpy(zmq_msg_data(&request), data.c_str(), data.size());
+
+    res = zmq_send(requester, &request, 0);
+
+    if (res != 0) {
+      printf("ERROR zmq_send: %d %d %s\n", (int) res, (int) errno, strerror(errno));
+    }
+
+    zmq_msg_close(&request);
+
+    // receive
+    zmq_msg_init(&reply);
+
+    res = zmq_recv(requester, &reply, 0);
+
+    if (res != 0) {
+      printf("ERROR zmq_recv: %d %d %s\n", (int) res, (int) errno, strerror(errno));
+    }
+
+    zmq_msg_close(&reply);
+  }
+
+  zmq_close(requester);
+
+  return 0;
+}
+
+
+/* Return 1 if the difference is negative, otherwise 0.  */
+int timeval_subtract(struct timeval* result, struct timeval* t2, struct timeval* t1) {
+  long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+  result->tv_sec = diff / 1000000;
+  result->tv_usec = diff % 1000000;
+
+  return (diff<0);
+}
+
+
+
+int main (int argc, char* argv[]) {
+  struct timeval start, end, result;
+  float rps, runtime;
+  size_t i;
+  int opt;
+  pthread_t* threads;
 
 // Aufruf:
 //
@@ -31,121 +124,70 @@ char const* connection = "tcp://localhost:5555";
 // -c <concurrency>     use <concurrency> parallel threads
 // -n <count>           send a total of <count> requests
 
-
-
-void* ThreadStarter (void* data) {
-  int res;
-  size_t n;
-  void *requester;
-
-  requester = zmq_socket(context, ZMQ_REQ);
-  res = zmq_connect(requester, connection);
-
-  if (res != 0) {
-    printf("ERROR zmq_connect: %d\n", errno);
-  }
-  
-  printf("connected\n");
-
-  for (n = 0;  n < loop;  n++) {
-    zmq_msg_t request;
-    zmq_msg_t reply;
-
-    PB_ArangoMessage messages;
-    PB_ArangoBatchMessage* batch;
-    PB_ArangoBlobRequest* blob;
-
-    batch = messages.add_messages();
-
-    batch->set_type(PB_BLOB_REQUEST);
-    blob = batch->mutable_request();
-
-    blob->set_requesttype(PB_REQUEST_TYPE_GET);
-    blob->set_url("/_api/version");
-    blob->set_contenttype(PB_NO_CONTENT);
-    blob->set_contentlength(0);
-    blob->set_content("");
-
-    string data;
-    messages.SerializeToString(&data);
-
-    // send
-    zmq_msg_init_size (&request, data.size());
-    memcpy (zmq_msg_data(&request), data.c_str(), data.size());
-
-    res = zmq_send (requester, &request, 0);
-
-    if (res != 0) {
-      printf("ERROR zmq_send: %d %d %s\n", (int) res, (int) errno, strerror(errno));
+  while ((opt = getopt(argc, argv, "C:u:h:P:c:n:")) != -1) {
+    switch (opt) {
+      case 'C':
+        connection = optarg;
+        break;
+      case 'u':
+        path = optarg;
+        break;
+      case 'h':
+        // TODO
+        break;
+      case 'P':
+        pipelined = atoi(optarg);
+        if (pipelined < 1) {
+          fprintf(stderr, "pipeline argument must be at least 1\n");
+          exit(EXIT_FAILURE);
+        }
+        break;
+      case 'c':
+        concurrency = atoi(optarg);
+        if (concurrency < 1) {
+          fprintf(stderr, "concurrency must be at least 1\n");
+          exit(EXIT_FAILURE);
+        }
+        break;
+      case 'n':
+        loop = atoi(optarg);
+        break;
+      default: /* '?' */
+        fprintf(stderr, "Usage: %s [-C connection] [-u path] [-h key value] [-P pipeline] [-c concurrency] [-n count]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-
-    zmq_msg_close (&request);
-
-    // receive
-    zmq_msg_init (&reply);
-
-    res = zmq_recv (requester, &reply, 0);
-
-    if (res != 0) {
-      printf("ERROR zmq_recv: %d %d %s\n", (int) res, (int) errno, strerror(errno));
-    }
-
-    zmq_msg_close (&reply);
   }
 
-  zmq_close (requester);
-  return 0;
-}
-
-
-
-
-int main (int argc, char* argv[])
-{
-  int conc = 1;
-  int t1;
-  int t2;
-  int rt;
-  int i;
-  pthread_t* threads;
-  
-  if (1 < argc) {
-    loop = atoi(argv[1]);
-  }
-
-  if (2 < argc) {
-    conc = atoi(argv[2]);
-  }
-
-  if (3 < argc) {
-    connection = argv[3];
-  }
-
-  printf("Looping %d times pro thread, total %d\n", (int) loop, (int)(loop * conc));
-  printf("Concurrency %d\n", conc);
+  printf("Looping %d times per thread, total %d, pipelined %d\n", (int) loop, (int) (loop * concurrency), (int) pipelined);
+  printf("Concurrency %d\n", (int) concurrency);
   printf("Connection %s\n", connection);
 
   context = zmq_init(16);
 
   // Socket to talk to server
-  printf ("Connecting to hello world server…\n");
+  printf("Connecting to server…\n");
 
-  t1 = time(0);
-  threads = (pthread_t*) malloc(sizeof(pthread_t) * conc);
+  threads = (pthread_t*) malloc(sizeof(pthread_t) * concurrency);
 
-  for (i = 0;  i < conc;  ++i) {
+  for (i = 0;  i < concurrency;  ++i) {
     pthread_create(&threads[i], 0, &ThreadStarter, 0);
   }
 
-  for (i = 0;  i < conc;  ++i) {
+  gettimeofday(&start, 0);
+
+  for (i = 0;  i < concurrency;  ++i) {
     pthread_join(threads[i], 0);
   }
+  
+  gettimeofday(&end, 0);
+  timeval_subtract(&result, &end, &start);
+  runtime = (float) result.tv_sec + (float) ((float) result.tv_usec / 1000000.0);
 
-  t2 = time(0);
-  rt = t2 - t1;
+  rps = (float) loop * (float) concurrency / runtime;
+  printf("runtime %f sec, req/sec %f, req/sec/thread %f\n", runtime, rps, rps / (float) concurrency);
 
-  printf("runtime %d, req/sec %f\n", rt, 1.0 * loop * conc / rt);
+  zmq_term(context);
+  free(threads);
 
-  zmq_term (context);
   return 0;
 }
