a31d5e8f274ce6a9e8d0c80efc59bca4245440db
Lucas Dohmen <lucas@arangodb.com>
2012-06-20 15:14:48 +0200

    Monkeypatches have their own module now.

--- js/common/modules/graph.js
+++ js/common/modules/graph.js
@@ -6,7 +6,7 @@
 /*global require, WeakDictionary, exports */
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief JavaScript actions functions
+/// @brief Graph interface to the database
 ///
 /// @file
 ///
@@ -40,6 +40,8 @@ var internal = require("internal"),
   findOrCreateCollectionByName,
   findOrCreateEdgeCollectionByName;
 
+require("monkeypatches");
+
 // -----------------------------------------------------------------------------
 // --SECTION--                                                   private methods
 // -----------------------------------------------------------------------------
@@ -48,79 +50,6 @@ var internal = require("internal"),
 /// @addtogroup ArangoGraph
 /// @{
 ////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief remove last occurrence of element from an array
-////////////////////////////////////////////////////////////////////////////////
-
-Object.defineProperty(Array.prototype, "removeLastOccurrenceOf", {
-  value: function (element) {
-    var index = this.lastIndexOf(element);
-    return this.splice(index, 1);
-  }
-});
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief return the union with another array
-////////////////////////////////////////////////////////////////////////////////
-
-Object.defineProperty(Array.prototype, "unite", {
-  value: function (other_array) {
-    return other_array.concat(this.filter(function (element) {
-      return (other_array.indexOf(element) === -1);
-    }));
-  }
-});
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief return the intersection with another array
-////////////////////////////////////////////////////////////////////////////////
-
-Object.defineProperty(Array.prototype, "intersect", {
-  value: function (other_array) {
-    return this.filter(function (element) {
-      return (other_array.indexOf(element) > -1);
-    });
-  }
-});
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief shallow copy properties
-////////////////////////////////////////////////////////////////////////////////
-
-Object.defineProperty(Object.prototype, "shallowCopy", {
-  get: function() {
-    var shallow = {},
-      key;
-
-    for (key in this) {
-      if (this.hasOwnProperty(key) && key[0] !== '_' && key[0] !== '$') {
-        shallow[key] = this[key];
-      }
-    }
-
-    return shallow;
-  }
-});
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief property keys
-////////////////////////////////////////////////////////////////////////////////
-
-Object.defineProperty(Object.prototype, "propertyKeys", {
-  get: function() {
-    var keys = [],
-      key;
-
-    for (key in this) {
-      if (this.hasOwnProperty(key) && key[0] !== '_' && key[0] !== '$') {
-        keys.push(key);
-      }
-    }
-
-    return keys;
-  }
-});
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief find or create a collection by name
