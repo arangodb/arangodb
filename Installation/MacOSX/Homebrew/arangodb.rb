3b3e881c850bc7967e14b81469fa9a31e012c5e6
Frank Celler <frank@arangodb.com>
2012-06-29 15:44:12 +0200

    added homebrew file

---
+++ Installation/MacOSX/Homebrew/arangodb.rb
@@ -0,0 +1,88 @@
+require 'formula'
+
+class Arangodb < Formula
+  homepage 'http://www.arangodb.org/'
+  url "https://github.com/triAGENS/ArangoDB/zipball/v1.0.alpha2"
+  sha1 'd6e90221dcc02252ae9bb66085a797d495be5bc4'
+
+  head "https://github.com/triAGENS/ArangoDB.git"
+
+  depends_on 'libev'
+  depends_on 'v8'
+
+  devel do
+    version "devel"
+    url "https://github.com/triAGENS/ArangoDB/zipball/devel"
+    sha1 ''
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-relative",
+                          "--disable-all-in-one",
+                          "--enable-mruby",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--datadir=#{share}",
+                          "--localstatedir=#{var}"
+
+    system "make install"
+
+    (var+'arango').mkpath
+    
+    plist_path.write startup_plist
+    plist_path.chmod 0644    
+  end
+
+  def caveats; <<-EOS.undent
+    Please note that this is a very early version if ArangoDB. There will be
+    bugs and the ArangoDB team would really appreciate it if you report them:
+
+      https://github.com/triAGENS/ArangoDB/issues
+
+    If this is your first install, automatically load on login with:
+        mkdir -p ~/Library/LaunchAgents
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    If this is an upgrade and you already have the #{plist_path.basename} loaded:
+        launchctl unload -w ~/Library/LaunchAgents/#{plist_path.basename}
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    To start the ArangoDB server manually, run:
+        /usr/local/sbin/arangod
+
+    To start the ArangoDB shell, run:
+        arangosh
+    EOS
+  end
+  
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>KeepAlive</key>
+    <true/>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{HOMEBREW_PREFIX}/sbin/arangod</string>
+      <string>-c</string>
+      <string>#{etc}/arango.conf</string>
+    </array>
+    
+    <key>RunAtLoad</key>
+    <true/>
+    
+	<key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+    
+  </dict>
+</plist>
+    EOPLIST
+  end  
+end
