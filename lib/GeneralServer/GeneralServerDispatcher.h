82ca4f24cee12e891533feafd2c5ac446f44c976
Jan Steemann <jan@arangodb.com>
2012-07-03 13:29:25 +0200

    fixed compile errors

--- lib/GeneralServer/GeneralServerDispatcher.h
+++ lib/GeneralServer/GeneralServerDispatcher.h
@@ -319,7 +319,7 @@ namespace triagens {
                 return false;
               }
               else {
-                Job* ajob = handler->createJob(_dispatcher, this);
+                Job* ajob = handler->createJob(this);
                 ServerJob* job = dynamic_cast<ServerJob*>(ajob);
 
                 if (job == 0) {
