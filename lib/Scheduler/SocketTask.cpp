460f698fae08383e5d950d358b9eff9df8d033e6
jsteemann <j.steemann@triagens.de>
2012-08-06 17:54:48 +0200

    committed this in the wrong branch. reverting it

--- lib/Scheduler/SocketTask.cpp
+++ lib/Scheduler/SocketTask.cpp
@@ -53,12 +53,10 @@ using namespace triagens::rest;
 
 SocketTask::SocketTask (socket_t fd)
   : Task("SocketTask"),
-    keepAliveWatcher(0),
     readWatcher(0),
     writeWatcher(0),
     watcher(0),
     commSocket(fd),
-    _keepAliveTimeout(300.0),
     _writeBuffer(0),
 #ifdef TRI_ENABLE_FIGURES
     _writeBufferStatistics(0),
@@ -215,12 +213,6 @@ bool SocketTask::handleWrite (bool& closed, bool noWrite) {
 
     if (closed) {
       return false;
-    }
-
-    // rearm timer for keep-alive timeout
-    if (_keepAliveTimeout > 0.0) {
-      // TODO: do we need some lock before we modify the scheduler?
-      scheduler->rearmTimer(keepAliveWatcher, _keepAliveTimeout);
     }
   }
 
@@ -390,11 +382,6 @@ void SocketTask::setup (Scheduler* scheduler, EventLoop loop) {
   readWatcher = scheduler->installSocketEvent(loop, EVENT_SOCKET_READ, this, commSocket);
   writeWatcher = scheduler->installSocketEvent(loop, EVENT_SOCKET_WRITE, this, commSocket);
 
-  // install timer for keep-alive timeout with some high default value
-  keepAliveWatcher = scheduler->installTimerEvent(loop, this, 60.0);
-  // and stop it immediately so it's not actively at the start
-  scheduler->clearTimer(keepAliveWatcher);
-
   tid = Thread::currentThreadId();
 }
 
@@ -405,9 +392,6 @@ void SocketTask::setup (Scheduler* scheduler, EventLoop loop) {
 void SocketTask::cleanup () {
   scheduler->uninstallEvent(watcher);
   watcher = 0;
-  
-  scheduler->uninstallEvent(keepAliveWatcher);
-  keepAliveWatcher = 0;
 
   scheduler->uninstallEvent(readWatcher);
   readWatcher = 0;
@@ -424,23 +408,7 @@ bool SocketTask::handleEvent (EventToken token, EventType revents) {
   bool result = true;
   bool closed = false;
 
-  if (token == keepAliveWatcher && (revents & EVENT_TIMER)) {
-    // got a keep-alive timeout
-    LOGGER_TRACE << "got keep-alive timeout signal, closing connection";
-
-    // TODO: do we need some lock before we modify the scheduler?
-    scheduler->clearTimer(token);
-
-    // this will close the connection and destroy the task
-    handleTimeout();
-    return false;
-  }
-
   if (token == readWatcher && (revents & EVENT_SOCKET_READ)) {
-    if (keepAliveWatcher != 0) {
-      // disable timer for keep-alive timeout
-      scheduler->clearTimer(keepAliveWatcher);
-    }
     result = handleRead(closed);
   }
 
