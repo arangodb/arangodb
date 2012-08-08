01a61bd3830f362f929fe9984a0fcfe3e06ec821
jsteemann <j.steemann@triagens.de>
2012-08-08 14:51:00 +0200

    reduced sleep intervals

--- js/common/tests/shell-cap-constraint.js
+++ js/common/tests/shell-cap-constraint.js
@@ -69,15 +69,15 @@ function CapConstraintSuite() {
     collection.unload();
 
     while (collection.status() != internal.ArangoCollection.STATUS_UNLOADED) {
-      console.log("waiting for collection '%s' to unload", cn);
-      internal.wait(5);
+      console.log("waiting for collection '%s' to unload.");
+      internal.wait(1);
     }
 
     collection.drop();
 
     while (collection.status() != internal.ArangoCollection.STATUS_DELETED) {
       console.log("waiting for collection '%s' to drop", cn);
-      internal.wait(5);
+      internal.wait(1);
     }
   },
 
