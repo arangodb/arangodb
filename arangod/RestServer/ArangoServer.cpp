affd513151d0d4118dde9006d9b0898f0cc5473a
jsteemann <j.steemann@triagens.de>
2012-09-12 21:56:24 +0200

    issue #195

--- arangod/RestServer/ArangoServer.cpp
+++ arangod/RestServer/ArangoServer.cpp
@@ -740,8 +740,8 @@ int ArangoServer::executeConsole (OperationMode::server_operation_mode_e mode) {
           sysTestFiles->Set((uint32_t) i, v8::String::New(_unitTests[i].c_str()));
         }
 
-        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()));
-        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False());
+        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()), v8::ReadOnly);
+        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False(), v8::ReadOnly);
         context->_context->Global()->Set(v8::String::New("SYS_UNIT_TESTS"), sysTestFiles);
         context->_context->Global()->Set(v8::String::New("SYS_UNIT_TESTS_RESULT"), v8::True());
 
@@ -775,8 +775,8 @@ int ArangoServer::executeConsole (OperationMode::server_operation_mode_e mode) {
           sysTestFiles->Set((uint32_t) i, v8::String::New(_jslint[i].c_str()));
         }
 
-        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()));
-        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False());
+        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()), v8::ReadOnly);
+        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False(), v8::ReadOnly);
         context->_context->Global()->Set(v8::String::New("SYS_UNIT_TESTS"), sysTestFiles);
         context->_context->Global()->Set(v8::String::New("SYS_UNIT_TESTS_RESULT"), v8::True());
 
@@ -801,8 +801,8 @@ int ArangoServer::executeConsole (OperationMode::server_operation_mode_e mode) {
       case OperationMode::MODE_SCRIPT: {
         v8::TryCatch tryCatch;
 
-        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()));
-        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False());
+        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()), v8::ReadOnly);
+        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False(), v8::ReadOnly);
 
         for (size_t i = 0;  i < _scriptFile.size();  ++i) {
           bool r = TRI_LoadJavaScriptFile(context->_context, _scriptFile[i].c_str());
@@ -859,8 +859,8 @@ int ArangoServer::executeConsole (OperationMode::server_operation_mode_e mode) {
       // .............................................................................
 
       case OperationMode::MODE_CONSOLE: {
-        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()));
-        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False());
+        context->_context->Global()->Set(v8::String::New("DATABASEPATH"), v8::String::New(_databasePath.c_str()), v8::ReadOnly);
+        context->_context->Global()->Set(v8::String::New("VALGRIND"), _runningOnValgrind ? v8::True() : v8::False(), v8::ReadOnly);
         V8LineEditor* console = new V8LineEditor(context->_context, ".arango");
 
         console->open(true);
