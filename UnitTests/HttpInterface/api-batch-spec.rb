9bd9e5403b068c9d81856fd1bdea172bbb8dcac0
jsteemann <j.steemann@triagens.de>
2012-09-26 18:11:05 +0200

    some more meaningful tests

--- UnitTests/HttpInterface/api-batch-spec.rb
+++ UnitTests/HttpInterface/api-batch-spec.rb
@@ -53,23 +53,134 @@ describe ArangoDB do
     end
 
 ################################################################################
+## checking invalid posts
+################################################################################
+  
+    context "checking wrong/missing content-type/boundary:" do
+      it "checks missing content-type" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-ct-none", cmd, :body => "xx" )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks invalid content-type (xxx)" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-ct-wrong-1", cmd, :body => "xx", :headers => { "Content-Type" => "xxx/xxx" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks invalid content-type (json)" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-ct-wrong-2", cmd, :body => "xx", :headers => { "Content-Type" => "application/json" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks valid content-type with missing boundary" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-boundary-wrong-1", cmd, :body => "xx", :headers => { "Content-Type" => "multipart/form-data" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks valid content-type with unexpected boundary" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-boundary-wrong-2", cmd, :body => "xx", :headers => { "Content-Type" => "multipart/form-data; peng" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks valid content-type with broken boundary" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-boundary-wrong-3", cmd, :body => "xx", :headers => { "Content-Type" => "multipart/form-data; boundary=" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+      
+      it "checks valid content-type with too short boundary" do
+        cmd = "/_api/batch"
+
+        doc = ArangoDB.log_post("#{prefix}-post-boundary-wrong-4", cmd, :body => "xx", :headers => { "Content-Type" => "multipart/form-data; boundary=a" } )
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+    end
+
+################################################################################
 ## checking simple batches
 ################################################################################
   
     context "checking simple requests:" do
+      
+      it "checks an empty operation multipart message" do
+        cmd = "/_api/batch"
 
-      it "checks whether POST is allowed on /_api/batch" do
+        multipart = ArangoMultipartBody.new()
+
+        doc = ArangoDB.log_post("#{prefix}-post-version-empty", cmd, :body => multipart.to_s, :format => :json, :headers => { "Content-Type" => "multipart/form-data; boundary=" + multipart.getBoundary })
+
+        doc.code.should eq(400)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(400)
+      end
+
+      it "checks a multipart message with a single operation" do
         cmd = "/_api/batch"
 
         multipart = ArangoMultipartBody.new()
 
         multipart.addPart("GET", "/_api/version", { }, "")
-#        multipart.addPart("GET", "/_api/version", { }, "")
-        doc = ArangoDB.log_post("#{prefix}-post-version1", cmd, :body => multipart.to_s, :format => :plain, :headers => { "Content-Type" => "multipart/form-data; boundary=" + multipart.getBoundary })
+        doc = ArangoDB.log_post("#{prefix}-post-version-one", cmd, :body => multipart.to_s, :format => :plain, :headers => { "Content-Type" => "multipart/form-data; boundary=" + multipart.getBoundary })
 
         doc.code.should eq(200)
-#        doc.parsed_response['error'].should eq(false) 
-#        doc.parsed_response['code'].should eq(200)
+
+        parts = multipart.getParts(multipart.getBoundary, doc.response.body)
+
+        parts.each do|part|
+          part[:status].should eq(200)
+        end
+      end
+      
+      it "checks a multipart message with a multiple operations" do
+        cmd = "/_api/batch"
+
+        multipart = ArangoMultipartBody.new()
+
+        multipart.addPart("GET", "/_api/version", { }, "")
+        multipart.addPart("GET", "/_api/version", { }, "")
+        multipart.addPart("GET", "/_api/version", { }, "")
+        multipart.addPart("GET", "/_api/version", { }, "")
+        doc = ArangoDB.log_post("#{prefix}-post-version-mult", cmd, :body => multipart.to_s, :format => :plain, :headers => { "Content-Type" => "multipart/form-data; boundary=" + multipart.getBoundary })
+
+        doc.code.should eq(200)
+
+        parts = multipart.getParts(multipart.getBoundary, doc.response.body)
+
+        parts.each do|part|
+          part[:status].should eq(200)
+        end
       end
     
     end
