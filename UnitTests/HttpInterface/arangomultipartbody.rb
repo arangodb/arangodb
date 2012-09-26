3904a5ae8d2f7046f41846cf62078e0a89673e8e
Jan Steemann <jan@arangodb.com>
2012-09-26 18:11:05 +0200

    some more meaningful tests

--- UnitTests/HttpInterface/arangomultipartbody.rb
+++ UnitTests/HttpInterface/arangomultipartbody.rb
@@ -37,6 +37,86 @@ class ArangoMultipartBody
   end
 
 ################################################################################
+## split the response body into its individual parts
+################################################################################
+
+  def getParts (boundary, body)
+    parts = [ ]
+    valid = false
+
+    while 1
+      position = body.index("--" + boundary)
+
+      break if position != 0 or position == nil
+
+      follow = body.slice(position + boundary.length + 2, 2)
+
+      break if follow != "\r\n" and follow != "--"
+
+      if follow == "--"
+        valid = true
+        break
+      end if
+
+      # strip boundary start from body
+      body = body.slice(position + boundary.length + 4, body.length)
+        
+      # look for boundary end
+      final = body.index("--" + boundary)
+
+      break if final == nil
+
+      # extract the part
+      partTotal = body.slice(0, final)
+
+      # look for envelope bounds
+      position = partTotal.index("\r\n\r\n");
+      break if position == nil
+
+      # strip envelope
+      partTotal = partTotal.slice(position + 4, partTotal.length)
+
+      # look for actual header & body
+      partHeader, partBody = "", ""
+      position = partTotal.index("\r\n\r\n");
+
+      break if position == nil
+      
+      partHeader = partTotal.slice(0, position)
+      partBody = partTotal.slice(position + 4, partTotal.length)
+
+      partHeaders = { }
+      status = 500
+      lineNumber = 0
+
+      # parse headers and status code
+
+      partHeader.each_line("\r\n") do |line|
+        if lineNumber == 0
+          position = line.index("HTTP/1.1 ")
+          break if position == nil
+          status = line.slice(9, 3).to_i
+        else
+          key, void, value = line.partition(":")
+          partHeaders[key.strip] = value.strip
+        end
+        lineNumber = lineNumber + 1
+      end
+
+      part = { :headers => partHeaders, :body => partBody, :status => status }
+      parts.push(part)
+
+      body = body.slice(final, body.length)
+    end
+
+    if not valid
+      raise "invalid multipart response received"
+    end
+
+    parts
+  end
+
+################################################################################
 ## get the string representation of a multipart message body
 ################################################################################
 
