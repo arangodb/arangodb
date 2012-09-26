ee6b36b1ad4674313d8c91a3cc46e36084e921ca
Jan Steemann <jan@arangodb.com>
2012-09-26 16:22:46 +0200

    torso of test cases for batch requests

--- UnitTests/HttpInterface/api-batch-spec.rb
+++ UnitTests/HttpInterface/api-batch-spec.rb
@@ -2,6 +2,7 @@
 
 require 'rspec'
 require './arangodb.rb'
+require './arangomultipartbody.rb'
 
 describe ArangoDB do
   prefix = "api-batch"
@@ -9,42 +10,68 @@ describe ArangoDB do
   context "dealing with HTTP batch requests:" do
 
 ################################################################################
-## checking HTTP HEAD responses
+## checking disallowed methods
 ################################################################################
+  
+    context "using disallowed methods:" do
 
-    it "checks whether GET is allowed on /_api/batch" do
-      cmd = "/_api/batch"
-      doc = ArangoDB.log_get("#{prefix}-get", cmd)
+      it "checks whether GET is allowed on /_api/batch" do
+        cmd = "/_api/batch"
+        doc = ArangoDB.log_get("#{prefix}-get", cmd)
 
-      doc.code.should eq(405)
-      doc.parsed_response['error'].should eq(true) 
-      doc.parsed_response['code'].should eq(405)
-    end
+        doc.code.should eq(405)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(405)
+      end
     
-    it "checks whether HEAD is allowed on /_api/batch" do
-      cmd = "/_api/batch"
-      doc = ArangoDB.log_head("#{prefix}-get", cmd)
+      it "checks whether HEAD is allowed on /_api/batch" do
+        cmd = "/_api/batch"
+        doc = ArangoDB.log_head("#{prefix}-get", cmd)
 
-      doc.code.should eq(405)
-      doc.response.body.should be_nil
-    end
+        doc.code.should eq(405)
+        doc.response.body.should be_nil
+      end
     
-    it "checks whether DELETE is allowed on /_api/batch" do
-      cmd = "/_api/batch"
-      doc = ArangoDB.log_delete("#{prefix}-delete", cmd)
+      it "checks whether DELETE is allowed on /_api/batch" do
+        cmd = "/_api/batch"
+        doc = ArangoDB.log_delete("#{prefix}-delete", cmd)
 
-      doc.code.should eq(405)
-      doc.parsed_response['error'].should eq(true) 
-      doc.parsed_response['code'].should eq(405)
-    end
+        doc.code.should eq(405)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(405)
+      end
     
-    it "checks whether PATCH is allowed on /_api/batch" do
-      cmd = "/_api/batch"
-      doc = ArangoDB.log_patch("#{prefix}-patch", cmd)
+      it "checks whether PATCH is allowed on /_api/batch" do
+        cmd = "/_api/batch"
+        doc = ArangoDB.log_patch("#{prefix}-patch", cmd)
 
-      doc.code.should eq(405)
-      doc.parsed_response['error'].should eq(true) 
-      doc.parsed_response['code'].should eq(405)
+        doc.code.should eq(405)
+        doc.parsed_response['error'].should eq(true) 
+        doc.parsed_response['code'].should eq(405)
+      end
+
+    end
+
+################################################################################
+## checking simple batches
+################################################################################
+  
+    context "checking simple requests:" do
+
+      it "checks whether POST is allowed on /_api/batch" do
+        cmd = "/_api/batch"
+
+        multipart = ArangoMultipartBody.new()
+
+        multipart.addPart("GET", "/_api/version", { }, "")
+#        multipart.addPart("GET", "/_api/version", { }, "")
+        doc = ArangoDB.log_post("#{prefix}-post-version1", cmd, :body => multipart.to_s, :format => :plain, :headers => { "Content-Type" => "multipart/form-data; boundary=" + multipart.getBoundary })
+
+        doc.code.should eq(200)
+#        doc.parsed_response['error'].should eq(false) 
+#        doc.parsed_response['code'].should eq(200)
+      end
+    
     end
       
   end
