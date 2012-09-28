10bee4aee83d0c11d00fd578030c35ebc61b4573
jsteemann <j.steemann@triagens.de>
2012-09-28 14:29:32 +0200

    fixed compile warnings when compiled with --disable-logger

--- arangod/RestHandler/RestDocumentHandler.cpp
+++ arangod/RestHandler/RestDocumentHandler.cpp
@@ -122,7 +122,10 @@ HttpHandler::status_e RestDocumentHandler::execute () {
   }
 
   _timing << *task;
+#ifdef TRI_ENABLE_LOGGER
+  // if ifdef is not used, the compiler will complain
   LOGGER_REQUEST_IN_START_I(_timing);
+#endif
 
   // execute one of the CRUD methods
   bool res = false;
