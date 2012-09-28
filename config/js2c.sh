8726b833d44712cfb8e9b7764c90ee3bfd60641a
Oreste Panaia <o.panaia@triagens.de>
2012-09-28 21:52:28 +0800

    removed long static standard c++ string from header file

--- config/js2c.sh
+++ config/js2c.sh
@@ -3,4 +3,4 @@ NAME=`echo $1 | sed -e 's:^\(.*/\)*js/\(.*\)\.js$:\2:' | tr "/-" "__"`
 
 cat $1 \
   | sed -e 's:\(["\]\):\\\1:g' \
-  | awk 'BEGIN {print "static string JS_'$NAME' = " } { print "  \"" $0 "\\n\"" } END { print ";"}'
+  | awk 'BEGIN {print "static string JS_'$NAME' = string(\"\") " } { print " + \"" $0 "\\n\"" } END { print ";"}'
