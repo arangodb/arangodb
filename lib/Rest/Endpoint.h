c23d0fa6ee25587537ee0b1bdf8a3fd907b4d899
oreste <oreste@triagens-gmbh.de>
2012-09-28 18:53:17 +0800

    Don't ask

--- lib/Rest/Endpoint.h
+++ lib/Rest/Endpoint.h
@@ -32,6 +32,7 @@
 #include <Basics/StringUtils.h>
 
 
+
 #ifdef TRI_HAVE_LINUX_SOCKETS
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -44,8 +45,6 @@
 
 
 #ifdef TRI_HAVE_WINSOCK2_H
-#include <winsock2.h>
-#include <ws2tcpip.h>
 #endif
 
 
