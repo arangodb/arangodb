8d6c2e38bd4875b1f7d2774443e55103227cf9d3
Jan Steemann <jan@arangodb.com>
2012-10-22 10:17:57 +0200

    issue #251: added --javascript.v8-options command line argument

--- arangod/V8Server/ApplicationV8.h
+++ arangod/V8Server/ApplicationV8.h
@@ -386,6 +386,39 @@ namespace triagens {
         double _gcFrequency;
 
 ////////////////////////////////////////////////////////////////////////////////
+/// @brief optional arguments to pass to v8
+///
+/// @CMDOPT{\-\-javascript.v8-options @CA{options}}
+///
+/// Optional arguments to pass to the V8 Javascript engine. The V8 engine will
+/// run with default settings unless explicit options are specified using this
+/// option. The options passed will be forwarded to the V8 engine which will
+/// parse them on its own. Passing invalid options may result in an error being
+/// printed on stderr and the option being ignored.
+///
+/// Options need to be passed in one string, with V8 option names being prefixed
+/// with double dashes. Multiple options need to be separated by whitespace.
+/// To get a list of all available V8 options, you can use
+/// the value @LIT{"--help"} as follows:
+/// @code
+/// --javascript.v8-options "--help"
+/// @endcode
+///
+/// Another example of specific V8 options being set at startup:
+/// @code
+/// --javascript.v8-options "--harmony --log"
+/// @endcode
+///
+/// Names and features or usable options depend on the version of V8 being used,
+/// and might change in the future if a different version of V8 is being used
+/// in ArangoDB. Not all options offered by V8 might be sensible to use in the
+/// context of ArangoDB. Use the specific options only if you are sure that
+/// they are not harmful for the regular database operation.
+////////////////////////////////////////////////////////////////////////////////
+
+        string _v8Options;
+
+////////////////////////////////////////////////////////////////////////////////
 /// @brief V8 startup loader
 ////////////////////////////////////////////////////////////////////////////////
 
