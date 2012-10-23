2fc1f0329417579d306f678cf8704bb8ae5abdcc
jsteemann <j.steemann@triagens.de>
2012-10-23 18:51:22 +0200

    initialise variable

--- js/server/modules/org/arangodb/actions.js
+++ js/server/modules/org/arangodb/actions.js
@@ -843,7 +843,7 @@ function reloadRouting () {
   // .............................................................................
 
   RoutingCache = {};
-
+  RoutingCache.flat = {};
   RoutingCache.routes = {};
   RoutingCache.middleware = {};
 
@@ -1008,7 +1008,7 @@ function firstRouting (type, parts) {
     url = "/" + parts.join("/");
   }
 
-  if (! RoutingCache.flat.hasOwnProperty(type)) {
+  if (! RoutingCache.flat || ! RoutingCache.flat.hasOwnProperty(type)) {
     return {
       parts: parts,
       position: -1,
