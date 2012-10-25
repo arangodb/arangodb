f60f71718bcb4616d3548e72c67eada130c41153
Jan Steemann <jan@arangodb.com>
2012-10-25 11:00:53 +0200

    issue #262

--- js/common/bootstrap/modules.js
+++ js/common/bootstrap/modules.js
@@ -56,6 +56,12 @@
 ModuleCache = {};
 
 ////////////////////////////////////////////////////////////////////////////////
+/// @brief file exists cache
+////////////////////////////////////////////////////////////////////////////////
+
+ExistsCache = {};
+
+////////////////////////////////////////////////////////////////////////////////
 /// @brief module constructor
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -353,6 +359,8 @@ ModuleCache["/internal"] = new Module("/internal");
     var mc;
     var n;
 
+    var existsCache = ExistsCache;
+
     // try to load the file
     var paths = internal.MODULES_PATH;
 
@@ -367,6 +375,7 @@ ModuleCache["/internal"] = new Module("/internal");
       }
 
       if (fs.exists(n)) {
+        existsCache[path] = true;
         return { path : n, content : internal.read(n) };
       }
     }
@@ -379,6 +388,7 @@ ModuleCache["/internal"] = new Module("/internal");
 
       if (n !== null) {
         if (n.hasOwnProperty('content')) {
+          existsCache[path] = true;
           return { path : "_collection/" + path, content : n.content };
         }
         else {
@@ -387,6 +397,7 @@ ModuleCache["/internal"] = new Module("/internal");
       }
     }
 
+    existsCache[path] = false;
     throw "cannot find a file named '"
         + path
         + "' using the module path(s) '" 
