c751611ee87bb8a1a034ef82f1b3dca2ce096bb2
Jan Steemann <jan@arangodb.com>
2012-11-24 17:12:52 +0100

    fixed logic error

--- arangod/VocBase/vocbase.c
+++ arangod/VocBase/vocbase.c
@@ -1786,8 +1786,7 @@ int TRI_RenameCollectionVocBase (TRI_vocbase_t* vocbase, TRI_vocbase_col_t* coll
   // collection is loaded
   // .............................................................................
 
-  // TODO: FIXME: this if condition is always true because the sub-parts are mutually exclusive
-  else if (collection->_status != TRI_VOC_COL_STATUS_LOADED || collection->_status != TRI_VOC_COL_STATUS_UNLOADING) {
+  else if (collection->_status != TRI_VOC_COL_STATUS_LOADED && collection->_status != TRI_VOC_COL_STATUS_UNLOADING) {
     res = TRI_RenameCollection(&collection->_collection->base, newName);
 
     if (res != TRI_ERROR_NO_ERROR) {
