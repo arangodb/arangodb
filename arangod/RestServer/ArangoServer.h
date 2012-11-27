bfe68608d86496ac0e46a17d2a6d78275c953cc0
a-brandt <a.brandt@triagens.de>
2012-11-27 13:46:45 +0100

    added ibcu and --default-language documentation

--- arangod/RestServer/ArangoServer.h
+++ arangod/RestServer/ArangoServer.h
@@ -398,11 +398,16 @@ namespace triagens {
         vector<string> _scriptParameters;
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief server default language
+/// @brief server default language for sorting strings
 ///
-/// @CMDOPT{\--default-language @CA{script-parameter}}
+/// @CMDOPT{\-\-default-language @CA{default-language}}
 ///
-/// Parameter to script.
+/// The default language ist used for sorting and comparing strings. 
+/// The language value is a two-letter language code (ISO-639) or it is 
+/// composed by a two-letter language code with and a two letter country code 
+/// (ISO-3166). Valid languages are "de", "en", "en_US" or "en_UK".
+///
+/// The default default-language is set to be the system locale on that platform.
 ////////////////////////////////////////////////////////////////////////////////
 
         string _defaultLanguage;
