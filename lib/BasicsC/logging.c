1d98ab44fc178b6b9af98c86337fd27483a3ed5b
Jan Steemann <jan@arangodb.com>
2012-11-27 12:23:26 +0100

    issue #295

--- lib/BasicsC/logging.c
+++ lib/BasicsC/logging.c
@@ -1432,6 +1432,7 @@ TRI_log_appender_t* TRI_CreateLogAppenderFile (char const* filename) {
 
 typedef struct log_appender_syslog_s {
   TRI_log_appender_t base;
+  TRI_mutex_t _mutex;
 }
 log_appender_syslog_t;
 
@@ -1462,6 +1463,7 @@ static void LogAppenderSyslog_Log (TRI_log_appender_t* appender,
                                    char const* msg,
                                    size_t length) {
   int priority;
+  log_appender_syslog_t* self;
 
   switch (severity) {
     case TRI_LOG_SEVERITY_EXCEPTION: priority = LOG_CRIT;  break;
@@ -1481,8 +1483,12 @@ static void LogAppenderSyslog_Log (TRI_log_appender_t* appender,
       case TRI_LOG_LEVEL_TRACE: priority = LOG_DEBUG; break;
     }
   }
+  
+  self = (log_appender_syslog_t*) appender;
 
+  TRI_LockMutex(&self->_mutex);
   syslog(priority, "%s", msg);
+  TRI_UnlockMutex(&self->_mutex);
 }
 
 #endif
@@ -1518,9 +1524,14 @@ static void LogAppenderSyslog_Close (TRI_log_appender_t* appender) {
   }
 
   TRI_UnlockSpin(&AppendersLock);
-
+  
   self = (log_appender_syslog_t*) appender;
+  TRI_LockMutex(&self->_mutex);
   closelog();
+  TRI_UnlockMutex(&self->_mutex);
+
+  TRI_DestroyMutex(&self->_mutex);
+
   TRI_Free(TRI_CORE_MEM_ZONE, self);
 }
 
@@ -1549,9 +1560,12 @@ TRI_log_appender_t* TRI_CreateLogAppenderSyslog (char const* name, char const* f
   log_appender_syslog_t* appender;
   int value;
 
+  assert(facility);
+  assert(*facility != '\0');
+
   // no logging
   if (name == NULL || *name == '\0') {
-    name = "[voc]";
+    name = "[arangod]";
   }
 
   // allocate space
@@ -1561,6 +1575,8 @@ TRI_log_appender_t* TRI_CreateLogAppenderSyslog (char const* name, char const* f
   appender->base.log = LogAppenderSyslog_Log;
   appender->base.reopen = LogAppenderSyslog_Reopen;
   appender->base.close = LogAppenderSyslog_Close;
+
+  TRI_InitMutex(&appender->_mutex);
 
   // find facility
   value = LOG_LOCAL0;
@@ -1582,7 +1598,9 @@ TRI_log_appender_t* TRI_CreateLogAppenderSyslog (char const* name, char const* f
   }
 
   // and open logging
+  TRI_LockMutex(&appender->_mutex);
   openlog(name, LOG_CONS | LOG_PID, value);
+  TRI_UnlockMutex(&appender->_mutex);
 
   // and store it
   TRI_LockSpin(&AppendersLock);
