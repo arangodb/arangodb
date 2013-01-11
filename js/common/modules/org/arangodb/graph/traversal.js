1c8eafa9de3345f4cb6e7cf6d92f115e6a1ad49d
jsteemann <j.steemann@triagens.de>
2013-01-11 13:59:03 +0100

    cleaned up traversal code, initial unittest

---
+++ js/common/modules/org/arangodb/graph/traversal.js
@@ -0,0 +1,502 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Traversal "classes"
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2011-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+/// @author Copyright 2011-2012, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+var internal = require("internal");
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                      constructors and destructors
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @addtogroup ArangoTraverser
+/// @{
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief traversal constructor
+////////////////////////////////////////////////////////////////////////////////
+
+function ArangoTraverser (edgeCollection, properties) {
+  // check edgeCollection
+  if (edgeCollection == undefined || edgeCollection == null) {
+    throw "invalid edgeCollection";
+  }
+
+  this._edgeCollection = edgeCollection;
+  
+  if (typeof properties !== "object") {
+    throw "invalid properties";
+  }
+
+  this._order = properties.visitOrder || ArangoTraverser.PRE_ORDER;
+  this._itemOrder = properties.itemOrder || ArangoTraverser.FORWARD;
+
+  // check the visitation strategy    
+  if (properties.strategy !== ArangoTraverser.BREADTH_FIRST &&
+      properties.strategy !== ArangoTraverser.DEPTH_FIRST) {
+    throw "invalid strategy";
+  }
+  this._strategy = properties.strategy;
+
+  // initialise uniqueness check attributes
+  this._uniqueness = { 
+    vertices: properties.uniqueness.vertices || ArangoTraverser.UNIQUE_NONE,
+    edges:    properties.uniqueness.edges    || ArangoTraverser.UNIQUE_NONE
+  };
+
+  // callbacks
+  // visitor
+  this._visitor = properties.visitor;
+
+  // filter
+  this._filter  = properties.filter || function () {
+    return {
+      visit:  true,
+      expand: true
+    };
+  };
+
+  this._expander = properties.expander || OutboundExpander;
+
+  if (typeof this._visitor !== "function") {
+    throw "invalid visitor";
+  }
+
+  if (typeof this._filter !== "function") {
+    throw "invalid filter";
+  }
+
+  if (typeof this._expander !== "function") {
+    throw "invalid expander";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                    public methods
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @addtogroup ArangoTraverser
+/// @{
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief execute the traversal
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.prototype.traverse = function (startVertex, context) { 
+  // check the start vertex
+  if (startVertex == undefined || startVertex == null) {
+    throw "invalid startVertex specified for traversal";
+  }
+
+  // set user defined context
+  this._context = context;
+
+  // run the traversal
+  var strategy;
+  if (this._strategy === ArangoTraverser.BREADTH_FIRST) {
+    strategy = BreadthFirstSearch;
+  }
+  else {
+    strategy = DepthFirstSearch;
+  }
+
+  strategy().run(this, startVertex);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                 private functions
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @addtogroup ArangoTraverser
+/// @{
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief apply the uniqueness checks
+////////////////////////////////////////////////////////////////////////////////
+
+function CheckUniqueness (uniqueness, visited, vertex, edge) {
+  if (uniqueness.vertices !== ArangoTraverser.UNIQUE_NONE) {
+    if (visited.vertices[vertex._id] === true) {
+      return false;
+    }
+
+    visited.vertices[vertex._id] = true;
+  }
+
+  if (edge != null && uniqueness.edges !== ArangoTraverser.UNIQUE_NONE) {
+    if (visited.edges[edge._id] === true) {
+      return false;
+    }
+
+    visited.edges[edge._id] = true;
+  }
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief check if we must process items in reverse order
+////////////////////////////////////////////////////////////////////////////////
+
+function CheckReverse (traverser) {
+  if (traverser._order === ArangoTraverser.POST_ORDER) {
+    // post order
+    if (traverser._itemOrder === ArangoTraverser.FORWARD) {
+      return true;
+    }
+  }
+  else if (traverser._order === ArangoTraverser.PRE_ORDER) {
+    // pre order
+    if (traverser._itemOrder === ArangoTraverser.BACKWARD && traverser._strategy === ArangoTraverser.BREADTH_FIRST) {
+      return true;
+    }
+    if (traverser._itemOrder === ArangoTraverser.FORWARD && traverser._strategy === ArangoTraverser.DEPTH_FIRST) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief implementation details for breadth-first strategy
+////////////////////////////////////////////////////////////////////////////////
+
+function BreadthFirstSearch () {
+  return {
+
+    createPath: function (items, idx) {
+      var path = { edges: [ ], vertices: [ ] };
+      var pathItem = items[idx];
+      
+      while (true) {
+        if (pathItem.edge != null) {
+          path.edges.unshift(pathItem.edge);
+        }
+        path.vertices.unshift(pathItem.vertex);
+        idx = pathItem.parentIndex;
+        if (idx < 0) {
+          break;
+        }
+        pathItem = items[idx];
+      }
+
+      return path;
+    },
+
+    run: function (traverser, startVertex) {
+      var toVisit = [ { edge: null, vertex: startVertex, parentIndex: -1 } ];
+      var visited = { edges: { }, vertices: { } };
+
+      var index = 0;
+      var step = 1;
+      var reverse = CheckReverse(traverser);
+
+      while ((step == 1 && index < toVisit.length) ||
+             (step == -1 && index >= 0)) {
+        var current = toVisit[index];
+        var vertex  = current.vertex;
+        var edge    = current.edge;
+
+        if (current.visit == null) {
+          current.visit = false;
+          
+          // first apply uniqueness check
+          if (! CheckUniqueness(traverser._uniqueness, visited, vertex, edge)) {
+            if (index < toVisit.length - 1) {
+              index += step;
+            }
+            else {
+              step = -1;
+            }
+            continue;
+          }
+
+          var path = this.createPath(toVisit, index);
+
+          var filterResult = traverser._filter(traverser, vertex, path);
+          if (traverser._order === ArangoTraverser.PRE_ORDER && filterResult.visit) {
+            // preorder
+            traverser._visitor(traverser, vertex, path);
+          }
+          else {
+            // postorder
+            current.visit = filterResult.visit || false;
+          }
+
+          if (filterResult.expand) {
+            var connected = traverser._expander(traverser, vertex, path);
+            if (connected.length > 0) {
+              reverse && connected.reverse();
+              for (var i = 0; i < connected.length; ++i) {
+                connected[i].parentIndex = index;
+                toVisit.push(connected[i]);
+              }
+            }
+            if (traverser._order === ArangoTraverser.POST_ORDER) {
+              if (index < toVisit.length - 1) {
+                index += step;
+              }
+              else {
+                step = -1;
+              }
+            }
+          }
+        }
+        else {
+          if (traverser._order === ArangoTraverser.POST_ORDER && current.visit) {
+            var path = this.createPath(toVisit, index);
+            traverser._visitor(traverser, vertex, path);
+          }
+          index += step;
+          
+        }
+
+      }
+    }
+
+  };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief implementation details for depth-first strategy
+////////////////////////////////////////////////////////////////////////////////
+
+function DepthFirstSearch () {
+  return {
+
+    run: function (traverser, startVertex) {
+      var toVisit = [ { edge: null, vertex: startVertex, visit: null } ];
+      var path    = { edges: [ ], vertices: [ ] };
+      var visited = { edges: { }, vertices: { } };
+      var reverse = CheckReverse(traverser);
+
+      while (toVisit.length > 0) {
+        // peek at the top of the stack
+        var current = toVisit[toVisit.length - 1];
+        var vertex  = current.vertex;
+        var edge    = current.edge;
+
+        // check if we visit the element for the first time
+        if (current.visit == null) {
+          current.visit = false;
+
+          // first apply uniqueness check
+          if (! CheckUniqueness(traverser._uniqueness, visited, vertex, edge)) {
+            // skip element if not unique
+            toVisit.pop();
+            continue;
+          }
+
+          // push the current element onto the path stack
+          if (edge != null) {
+            path.edges.push(edge);
+          }
+          path.vertices.push(vertex);
+
+          
+          var filterResult = traverser._filter(traverser, vertex, path);
+          if (traverser._order === ArangoTraverser.PRE_ORDER && filterResult.visit) {
+            // preorder visit
+            traverser._visitor(traverser, vertex, path);
+          }
+          else {
+            // postorder. mark the element visitation flag because we'll got to check it later
+            current.visit = filterResult.visit || false;
+          }
+
+          // expand the element's children?
+          if (filterResult.expand) {
+            var connected = traverser._expander(traverser, vertex, path);
+            if (connected.length > 0) {
+              reverse && connected.reverse();
+              for (var i = 0; i < connected.length; ++i) {
+                connected[i].visit = null;
+                toVisit.push(connected[i]);
+              }
+            }
+          }
+        }
+        else {
+          // we have already seen this element
+          if (traverser._order === ArangoTraverser.POST_ORDER && current.visit) {
+            // postorder visitation
+            traverser._visitor(traverser, vertex, path);
+          }
+
+          // pop the element from the stack
+          toVisit.pop();
+          if (path.edges.length > 0) {
+            path.edges.pop();
+          }
+          path.vertices.pop();
+        }
+
+      }
+    }
+  
+  };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief default outbound expander function
+////////////////////////////////////////////////////////////////////////////////
+
+function OutboundExpander (traverser, vertex, path) {
+  var connections = [ ];
+
+  this._edgeCollection.outEdges(vertex._id).forEach(function (edge) {
+    var vertex = internal.db._document(edge._to);
+    connections.push({ edge: edge, vertex: vertex });    
+  });
+
+  return connections;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief default inbound expander function 
+////////////////////////////////////////////////////////////////////////////////
+
+function InboundExpander (traverser, vertex, path) {
+  var connections = [ ];
+
+  this._edgeCollection.inEdges(vertex._id).forEach(function (edge) {
+    var vertex = internal.db._document(edge._from);
+    connections.push({ edge: edge, vertex: vertex });    
+  });
+
+  return connections;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                  public constants
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @addtogroup ArangoTraverser
+/// @{
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief every element can be revisited
+////////////////////////////////////////////////////////////////////////////////
+  
+ArangoTraverser.UNIQUE_NONE          = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief element can only be revisited if not already in current path
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.UNIQUE_PATH          = 1;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief element can only be revisited if not already visited
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.UNIQUE_GLOBAL        = 2;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief visitation strategy breadth first
+////////////////////////////////////////////////////////////////////////////////
+  
+ArangoTraverser.BREADTH_FIRST        = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief visitation strategy depth first
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.DEPTH_FIRST          = 1;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief pre-order traversal
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.PRE_ORDER            = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief post-order traversal
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.POST_ORDER           = 1;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief forward item processing order
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.FORWARD              = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief backward item processing order
+////////////////////////////////////////////////////////////////////////////////
+
+ArangoTraverser.BACKWARD             = 1;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                    MODULE EXPORTS
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @addtogroup ArangoTraverser
+/// @{
+////////////////////////////////////////////////////////////////////////////////
+
+exports.Traverser        = ArangoTraverser;
+exports.OutboundExpander = OutboundExpander;
+exports.InboundExpander  = InboundExpander;
+  
+////////////////////////////////////////////////////////////////////////////////
+/// @}
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                       END-OF-FILE
+// -----------------------------------------------------------------------------
+
+// Local Variables:
+// mode: outline-minor
+// outline-regexp: "^\\(/// @brief\\|/// @addtogroup\\|// --SECTION--\\|/// @page\\|/// @\\}\\)"
+// End:
---
+++ js/common/tests/shell-graph-traversal.js
@@ -0,0 +1,212 @@
+/*jslint indent: 2,
+         nomen: true,
+         maxlen: 80,
+         sloppy: true */
+/*global require,
+    db,
+    assertEqual, assertTrue */
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test the graph traversal class
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+/// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+var jsunity = require("jsunity");
+var console = require("console");
+var internal = require("internal");
+var traversal = require("org/arangodb/graph/traversal");
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                            graph traversal module
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test: Graph Traversal
+////////////////////////////////////////////////////////////////////////////////
+
+function GraphTraversalSuite() {
+  var vertices;
+  var edges;
+
+  var stringifyPath = function (path) {
+    var result = "[";
+    for (var i = 0; i < path.edges.length; ++i) {
+      if (i > 0) {
+        result += ", ";
+      }
+      result += path.edges[i]._key;
+    }
+
+    result += " ]";
+    return result;
+  }
+
+  var visitor = function (traverser, vertex, path) {
+    var context = traverser._context;
+
+    context.visited.push(vertex._id);
+    context.paths.push(stringifyPath(path));
+  };
+
+  var expander = function (traverser, vertex, path) {
+    var result = [ ];
+    
+    var edgesList = edges[vertex._id];
+    if (edgesList != undefined) {
+      for (i = 0; i < edgesList.length; ++i) {
+        result.push({ edge: edgesList[i], vertex: vertices[edgesList[i]._to] });
+      }
+    }
+    return result;
+
+  }
+
+  return {
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief set up
+////////////////////////////////////////////////////////////////////////////////
+
+    setUp : function () {
+      vertices = { };
+      edges    = { };
+  
+      [ "World", "Nothing",
+        "Europe", "Asia", "America", "Australia", "Blackhole",
+        "DE", "FR", "GB", "CN", "JP", "TW", "US", "MX", "AU", 
+        "London", "Paris", "Lyon", "Cologne", "Dusseldorf", "Beijing", "Shanghai", "Tokyo", "Kyoto", "Taipeh", "Perth", "Sydney" 
+      ].forEach(function (item) {
+        var key = item;
+        var vertex = {
+          _id : "vertices/" + key,
+          _key : key,
+          name : item
+        };
+        vertices[vertex._id] = vertex;
+      });
+
+      var connect = function (from, to) {
+        var key = from + "x" + to;
+        var edge = {
+          _id : "edges/" + key,
+          _key : key, 
+          _from : "vertices/" + from,
+          _to : "vertices/" + to,
+          what : from + "->" + to
+        };
+
+        if (edges[edge._from] == undefined) {
+          edges[edge._from] = [ ];
+        }
+        edges[edge._from].push(edge);
+      };
+
+      connect("World", "Europe");
+      connect("World", "Asia");
+      connect("World", "America");
+      connect("World", "Australia");
+      connect("Europe", "DE"); 
+      connect("Europe", "FR"); 
+      connect("Europe", "GB"); 
+      connect("Asia", "CN"); 
+      connect("Asia", "JP"); 
+      connect("Asia", "TW"); 
+      connect("America", "US"); 
+      connect("America", "MX"); 
+      connect("Australia", "AU"); 
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief tear down
+////////////////////////////////////////////////////////////////////////////////
+
+    tearDown : function () {
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test depth-first pre-order forward visitation
+////////////////////////////////////////////////////////////////////////////////
+
+    testDepthFirstPreOrderForward : function () {
+      var properties = { 
+        strategy: traversal.Traverser.DEPTH_FIRST,
+        visitOrder: traversal.Traverser.PRE_ORDER,
+        itemOrder: traversal.Traverser.FORWARD,
+        uniqueness: {
+          vertices: traversal.Traverser.UNIQUE_NONE,
+          edges: traversal.Traverser.UNIQUE_NONE
+        }, 
+        visitor: visitor,
+        // filter: filter,
+        expander: expander
+      };
+      
+      var traverser = new traversal.Traverser("edges", properties);
+      var context = {
+        visited: [ ],
+        paths: [ ]
+      }; 
+  
+      traverser.traverse(vertices["vertices/World"], context);
+
+      var expectedVisits = [
+        "vertices/World",
+        "vertices/Europe",
+        "vertices/DE",
+        "vertices/FR",
+        "vertices/GB",
+        "vertices/Asia",
+        "vertices/CN",
+        "vertices/JP",
+        "vertices/TW",
+        "vertices/America",
+        "vertices/US",
+        "vertices/MX",
+        "vertices/Australia",
+        "vertices/AU"
+      ];
+
+      assertEqual(expectedVisits, context.visited);
+    },
+
+  };
+}
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                              main
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief executes the test suites
+////////////////////////////////////////////////////////////////////////////////
+
+jsunity.run(GraphTraversalSuite);
+
+return jsunity.done();
+
+// Local Variables:
+// mode: outline-minor
+// outline-regexp: "^\\(/// @brief\\|/// @addtogroup\\|// --SECTION--\\|/// @page\\|/// @}\\)"
+// End:
