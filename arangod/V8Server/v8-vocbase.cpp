acd170122734b79baf656438fcfd405056717931
jsteemann <j.steemann@triagens.de>
2013-01-21 09:19:03 +0100

    added documentation for collection.revision()

--- arangod/V8Server/v8-vocbase.cpp
+++ arangod/V8Server/v8-vocbase.cpp
@@ -5000,7 +5000,20 @@ static v8::Handle<v8::Value> JS_StatusVocbaseCol (v8::Arguments const& argv) {
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief returns the revision id of a collection
-/// the revision id is updated when the document data in a collection changes
+///
+/// @FUN{@FA{collection}.revision()}
+///
+/// Returns the revision id of the collection
+///
+/// The revision id is updated when the document data is modified, either by
+/// inserting, deleting, updating or replacing documents in it.
+///
+/// The revision id of a collection can be used by clients to check whether 
+/// data in a collection has changed or if it is still unmodified since a
+/// previous fetch of the revision id.
+///
+/// The revision id returned is a string value. Clients should treat this value
+/// as an opaque string, and only use it for equality/non-equality comparisons.
 ////////////////////////////////////////////////////////////////////////////////
 
 static v8::Handle<v8::Value> JS_RevisionVocbaseCol (v8::Arguments const& argv) {
