4b433a511a001ed7b61dcbfef9bb68495b1c9445
Jan Steemann <jan@arangodb.com>
2013-01-23 11:28:33 +0100

    added followEdges property for AQL TRAVERSE() function

--- html/admin/js/modules/org/arangodb/graph/traversal.js
+++ html/admin/js/modules/org/arangodb/graph/traversal.js
@@ -479,7 +479,9 @@ function OutboundExpander (config, vertex, path) {
   outEdges.forEach(function (edge) {
     try {
       var v = config.datasource.getVertex(edge._to);
-      connections.push({ edge: edge, vertex: v });    
+      if (! config.expandFilter || config.expandFilter(config, v, edge, path)) {
+        connections.push({ edge: edge, vertex: v });    
+      }
     }
     catch (e) {
       // continue even in the face of non-existing documents
@@ -504,7 +506,9 @@ function InboundExpander (config, vertex, path) {
   inEdges.forEach(function (edge) {
     try {
       var v = config.datasource.getVertex(edge._from);
-      connections.push({ edge: edge, vertex: v });    
+      if (! config.expandFilter || config.expandFilter(config, v, edge, path)) {
+        connections.push({ edge: edge, vertex: v });    
+      }
     }
     catch (e) {
       // continue even in the face of non-existing documents
@@ -529,7 +533,9 @@ function AnyExpander (config, vertex, path) {
   edges.forEach(function (edge) {
     try {
       var v = config.datasource.getVertex(edge._from === vertex._id ? edge._to : edge._from);
-      connections.push({ edge: edge, vertex: v });    
+      if (! config.expandFilter || config.expandFilter(config, v, edge, path)) {
+        connections.push({ edge: edge, vertex: v });    
+      }
     }
     catch (e) {
       // continue even in the face of non-existing documents
