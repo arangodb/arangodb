a4eb737dbeb583f3f942cebda787317e1d4e89a6
Jan Steemann <jan@arangodb.com>
2013-02-06 11:46:59 +0100

    fixed some jslint warnings

--- html/admin/js/modules/org/arangodb/graph-common.js
+++ html/admin/js/modules/org/arangodb/graph-common.js
@@ -83,20 +83,20 @@ GraphArray.prototype = new Array();
 /// @brief map
 ////////////////////////////////////////////////////////////////////////////////
 
-GraphArray.prototype.map = function (fun) {
+GraphArray.prototype.map = function (fun, thisp) {
   var len = this.length;
   var i;
 
-  if (typeof fun != "function") {
+  if (typeof fun !== "function") {
     throw new TypeError();
   }
   
   var res = new GraphArray(len);
-  var thisp = arguments[1];
 
   for (i = 0;  i < len;  i++) {
-    if (i in this)
+    if (i in this) {
       res[i] = fun.call(thisp, this[i], i, this);
+    }
   }
 
   return res;
