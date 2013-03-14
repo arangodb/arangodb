714eaa9f5f7f412a6b28820f2e8482ee2e29d487
Frank Celler <frank@arangodb.com>
2013-03-14 17:08:18 +0100

    removed unused variable

--- arangod/VocBase/document-collection.c
+++ arangod/VocBase/document-collection.c
@@ -1464,13 +1464,13 @@ static bool OpenIterator (TRI_df_marker_t const* marker, void* data, TRI_datafil
     // it is an update, but only if found has a smaller revision identifier
     else if (found->_rid < d->_rid || (found->_rid == d->_rid && found->_fid <= datafile->_fid)) {
       TRI_doc_mptr_t* newHeader;
-      TRI_doc_mptr_t oldData;
+      //TRI_doc_mptr_t oldData;
 
       // delete old entries
       DeleteSecondaryIndexes(collection, found);
 
       // save the old data
-      oldData = *found;
+      // oldData = *found;
 
       // TODO: this will be identical for non-transactional collections only
       newHeader = CONST_CAST(found);
