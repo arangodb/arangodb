79e975cba10f21a9d051fae7c24e561e0e71f63b
hkernbach <h.kernbach@triagens.de>
2013-03-14 13:55:00 +0100

    removes new row entry if no content is given

--- frontend/js/views/documentView.js
+++ frontend/js/views/documentView.js
@@ -3,6 +3,7 @@ var documentView = Backbone.View.extend({
   table: '#documentTableID',
   colid: 0,
   docid: 0,
+  currentKey: 0,
 
   init: function () {
     this.initTable();
@@ -17,7 +18,27 @@ var documentView = Backbone.View.extend({
     "click #documentTableID tr" : "clicked",
     "click #editSecondRow"      : "editSecond",
     "click .cancelButton"       : "checkIfEmpty",
-    "keyup .sorting_1"          : "listenKey"
+    "keydown .sorting_1"        : "listenKey",
+    "keydown"                   : "listenGlobalKey",
+    "focusout textarea"         : "checkFocus"
+  },
+
+  checkFocus: function(e) {
+    //check if new row has to be saved
+    var self = this;
+    var data = $(this.table).dataTable().fnGetData();
+    $.each(data, function(key, val) {
+      if (val[0] === self.currentKey) {
+        $(self.table).dataTable().fnDeleteRow( key );
+      }
+    });
+
+  },
+
+  listenGlobalKey: function(e) {
+    if (e.keyCode === 27) {
+      this.checkFocus();
+    }
   },
 
   listenKey: function(e) {
@@ -156,9 +177,11 @@ var documentView = Backbone.View.extend({
 
   addLine: function () {
     var randomKey = arangoHelper.getRandomToken();
+    var self = this;
+    self.currentKey = "zkey"+randomKey;
     $(this.table).dataTable().fnAddData(
       [
-        "zkey"+randomKey,
+        self.currentKey,
         '<i class="icon-edit" id="editFirstRow"></i>',
         this.value2html("editme"),
         JSON.stringify("editme"),
@@ -169,7 +192,7 @@ var documentView = Backbone.View.extend({
     this.makeEditable();
     var tableContent = $('table').find('td');
     $.each(tableContent, function(key, val) {
-      if ($(val).text() === "zkey"+randomKey) {
+      if ($(val).text() === self.currentKey) {
         $(val).click();
         $('.jediTextarea textarea').val("");
         return;
@@ -268,9 +291,9 @@ var documentView = Backbone.View.extend({
     $('.writeable', documentEditTable.fnGetNodes()).editable(function(value, settings) {
       var aPos = documentEditTable.fnGetPosition(this);
       if (aPos[1] == 0) {
-        documentEditTable.fnUpdate(self.escaped(value), aPos[0], aPos[1]);
-        self.updateLocalDocumentStorage();
-        return value;
+          documentEditTable.fnUpdate(self.escaped(value), aPos[0], aPos[1]);
+          self.updateLocalDocumentStorage();
+          return value;
       }
       if (aPos[1] == 2) {
         var oldContent = JSON.parse(documentEditTable.fnGetData(aPos[0], aPos[1] + 1));
@@ -292,6 +315,11 @@ var documentView = Backbone.View.extend({
         var aPos = documentEditTable.fnGetPosition(this);
         var value = documentEditTable.fnGetData(aPos[0], aPos[1]);
         if (aPos[1] == 0) {
+        //check if this row was newly created
+        if (value === self.currentKey) {
+          console.log("ist alt"); 
+          return value;
+        }
           return value;
         }
         if (aPos[1] == 2) {
