e97f6c53c54be7869a6cc6c6db862c3be962ada3
Hacki <michael.hackstein@rwth-aachen.de>
2013-03-20 14:31:02 +0100

    Start of event dispatcher implementation

--- html/admin/js/graphViewer/jasmine_test/specEvents/eventDispatcherSpec.js
+++ html/admin/js/graphViewer/jasmine_test/specEvents/eventDispatcherSpec.js
@@ -2,8 +2,8 @@
 /*global beforeEach, afterEach */
 /*global describe, it, expect */
 /*global runs, spyOn, waitsFor */
-/*global window, eb, loadFixtures, document, jQuery */
-/*global EventLibrary*/
+/*global window, document, $, d3 */
+/*global EventDispatcher, NodeShaper, EdgeShaper*/
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief Graph functionality
@@ -36,454 +36,138 @@
 (function () {
   "use strict";
 
-  describe('Event Library', function () {
+  describe('Event Dispatcher', function () {
 
-    var eventLib;
+    var dispatcher,
+    nodeShaper,
+    edgeShaper,
+    svg;
     
     beforeEach(function() {
-      eventLib = new EventLibrary();
+      svg = document.createElement("svg");
+      document.body.appendChild(svg);
+      
+      nodeShaper = new NodeShaper(d3.select("svg"));
+      edgeShaper = new EdgeShaper(d3.select("svg"));
+      dispatcher = new EventDispatcher(nodeShaper, edgeShaper);
+    });
+    
+    afterEach(function() {
+      document.body.removeChild(svg);
+    });
+    
+    describe('set up process', function() {
+      it('should throw an error if nodeShaper is not given', function() {
+        expect(
+          function() {
+            var t = new EventDispatcher();
+          }
+        ).toThrow("NodeShaper has to be given.");
+      });
+      
+      it('should throw an error if edgeShaper is not given', function() {
+        expect(
+          function() {
+            var t = new EventDispatcher(nodeShaper);
+          }
+        ).toThrow("EdgeShaper has to be given.");
+      });
+      
+    });
+    
+    describe('checking objects to bind events to', function() {
+      
+      
+      it('should be able to bind to any DOM-element', function() {
+        var called = false,
+        callback = function() {
+          called = true;
+        };
+        
+        runs(function() {
+          var target = $("svg");
+          dispatcher.bind(target, "click", callback);
+          target.click();
+        });
+        
+        waitsFor(function() {
+          return called;
+        }, 1000, "The click event should have been triggered.");
+        
+        runs(function() {
+          // Just display that everything had worked
+          expect(true).toBeTruthy();
+        });
+        
+      });
+      
+      it('should be able to bind to all nodes', function() {
+        var called;
+        
+        
+        runs(function() {
+          var nodes = [{_id: 1}, {_id:2}],
+          callback = function() {
+            called++;
+          };
+          called = 0;
+          nodeShaper.drawNodes(nodes);
+          dispatcher.bind("nodes", "click", callback);
+          $("#1").click();
+          $("#2").click();
+        });
+        
+        waitsFor(function() {
+          return called === 2;
+        }, 1000, "The two click events should have been triggered.");
+        
+        runs(function() {
+          // Just display that everything had worked
+          expect(true).toBeTruthy();
+        });
+        
+      });
+      
+      it('should be able to bind to all edges', function() {
+        var called;
+        
+        
+        runs(function() {
+          var n1 = {_id: 1},
+          n2 = {_id: 2},
+          n3 = {_id: 3},
+          edges = [
+            {source: n1, target: n2},
+            {source: n2, target: n3}
+          ],
+          callback = function() {
+            called++;
+          };
+          called = 0;
+          edgeShaper.drawEdges(edges);
+          dispatcher.bind("edges", "click", callback);
+          $("#1-2").click();
+          $("#2-3").click();
+        });
+        
+        waitsFor(function() {
+          return called === 2;
+        }, 1000, "The two click events should have been triggered.");
+        
+        runs(function() {
+          // Just display that everything had worked
+          expect(true).toBeTruthy();
+        });
+        
+      });
+      
+    });
+    
+    describe('checking triggers for events', function() {
+      
     });
 
-    describe('Expand', function() {
-      
-      var loaded,
-      reshaped,
-      nodes,
-      edges,
-      loadedNodes,
-      reshapedNodes,
-      started,
-      loadNodeCallback = function(node) {
-        loaded++;
-        loadedNodes.push(node);
-      },
-      reshapeNodeCallback = function(node) {
-        reshaped++;
-        reshapedNodes.push(node);
-      },
-      startCallback = function() {
-        started++;
-      },
-      testee;
-      
-      beforeEach(function() {
-        loaded = 0;
-        reshaped = 0;
-        started = 0;
-      
-        nodes = [];
-        edges = [];
-        loadedNodes = [];
-        reshapedNodes = [];
-      });
-      
-      it('should expand a collapsed node', function() {
-        var node = {
-          _id: 0,
-          _outboundCounter: 0,
-          _inboundCounter: 0
-        };
-        nodes.push(node);
-        testee = eventLib.Expand(
-          edges,
-          nodes,
-          startCallback,
-          loadNodeCallback,
-          reshapeNodeCallback
-        );
-        testee(node);
-        
-        expect(node._expanded).toBeTruthy();
-        expect(started).toEqual(1);
-        expect(reshaped).toEqual(1);
-        expect(loaded).toEqual(1);
-        expect(reshapedNodes.length).toEqual(1);
-        expect(reshapedNodes[0]).toEqual(node);
-        expect(loadedNodes.length).toEqual(1);
-        expect(loadedNodes[0]).toEqual(node._id);
-        
-      });
-      
-      it('should collapse an expanded node', function() {
-        var node = {
-          id: 0,
-          _expanded: true,
-          _outboundCounter: 0,
-          _inboundCounter: 0
-        };
-        nodes.push(node);
-        testee = eventLib.Expand(
-          edges,
-          nodes,
-          startCallback,
-          loadNodeCallback,
-          reshapeNodeCallback
-        );
-        testee(node);
-        
-        expect(node._expanded).toBeFalsy();
-        expect(started).toEqual(1);
-        expect(reshaped).toEqual(1);
-        expect(loaded).toEqual(0);
-        expect(reshapedNodes.length).toEqual(1);
-        expect(reshapedNodes[0]).toEqual(node);
-      });
-      
-      it('should collapse a tree', function() {
-        var root = {
-          id: 0,
-          _expanded: true,
-          _outboundCounter: 2,
-          _inboundCounter: 0
-        },
-        c1 = {
-          id: 1,
-          _outboundCounter: 0,
-          _inboundCounter: 1
-        },
-        c2 = {
-          id: 2,
-          _outboundCounter: 0,
-          _inboundCounter: 1
-        };
-        
-        nodes.push(root);
-        nodes.push(c1);
-        nodes.push(c2);
-        edges.push({source: root, target: c1});
-        edges.push({source: root, target: c2});
-        
-        testee = eventLib.Expand(
-          edges,
-          nodes,
-          startCallback,
-          loadNodeCallback,
-          reshapeNodeCallback
-        );
-        testee(root);
-        
-        expect(root._expanded).toBeFalsy();
-        expect(started).toEqual(1);
-        expect(reshaped).toEqual(1);
-        expect(loaded).toEqual(0);
-        expect(nodes.length).toEqual(1);
-        expect(edges.length).toEqual(0);
-        expect(reshapedNodes.length).toEqual(1);
-        expect(reshapedNodes[0]).toEqual(root);
-      });
-      
-      describe('setup process', function() {
-                
-        it('should throw an error if edges are not given', function() {          
-          expect(
-            function() {
-              eventLib.Expand();
-            }
-          ).toThrow("Edges have to be defined");
-        });
-        
-        it('should throw an error if nodes are not given', function() {
-          expect(
-            function() {
-              eventLib.Expand([]);
-            }
-          ).toThrow("Nodes have to be defined");
-        });
-        
-        it('should throw an error if start callback is not given', function() {
-          expect(
-            function() {
-              eventLib.Expand([], []);
-            }
-          ).toThrow("A callback to the Start-method has to be defined");
-        });
-        
-        it('should throw an error if load node callback is not given', function() {
-          expect(
-            function() {
-              eventLib.Expand([], [], function(){});
-            }
-          ).toThrow("A callback to load a node has to be defined");
-        });
-        
-        it('should throw an error if reshape node callback is not given', function() {
-          expect(
-            function() {
-              eventLib.Expand([], [], function(){}, function(){});
-            }
-          ).toThrow("A callback to reshape a node has to be defined");
-        });
-        
-      });
-      
-    });
-    
-    describe('Insert Node', function() {
-      
-      it('should create an event to add a node', function() {
-        var adapterDummy = {},
-        nodeShaperDummy = {},
-        nodes = [],
-        created = null,
-        reshaped = null,
-        called = false,
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.createNode = function(nodeToCreate, callback) {
-          created = nodeToCreate;
-          nodes.push(created);
-          callback(created);
-        };
-        
-        nodeShaperDummy.drawNodes = function(nodes) {
-          reshaped = nodes;
-        };
-        
-        runs(function() {
-          testee = eventLib.InsertNode(nodes, adapterDummy, nodeShaperDummy);
-          testee(callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(created).toBeDefined();
-          expect(created._inboundCounter).toEqual(0);
-          expect(created._outboundCounter).toEqual(0);
-          expect(reshaped[0]).toEqual(created);
-        });
-        
-      });
-      
-    });
-    
-    describe('Patch Node', function() {
-      
-      it('should create an event to patch a node', function() {
-        var adapterDummy = {},
-        nodeShaperDummy = {},
-        patched = {id: "1"},
-        data = {hello: "world"},
-        nodes = [patched],
-        reshaped = null,
-        called = false,
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.patchNode = function(nodeToPatch, patchData, callback) {
-          patched = nodeToPatch;
-          jQuery.extend(patched, patchData);
-          callback();
-        };
-        
-        nodeShaperDummy.drawNodes = function(nodes) {
-          reshaped = nodes;
-        };
-        
-        runs(function() {
-          testee = eventLib.PatchNode(nodes, adapterDummy, nodeShaperDummy);
-          testee(patched, data, callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(patched).toBeDefined();
-          expect(reshaped[0]).toEqual(patched);
-          expect(patched.id).toEqual("1");
-          expect(patched.hello).toEqual("world");
-        });
-      });
-      
-    });
-    
-    describe('Delete Node', function() {
-      
-      it('should create an event to delete a node', function() {
-        var adapterDummy = {},
-        nodeShaperDummy = {},
-        toDel = {id: "2"},
-        nodes = [toDel],
-        deleted = null,
-        reshaped = null,
-        called = false,
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.deleteNode = function(nodeToDelete, callback) {
-          deleted = nodeToDelete;
-          nodes.pop();
-          callback();
-        };
-        
-        nodeShaperDummy.drawNodes = function(nodes) {
-          reshaped = nodes;
-        };
-        
-        runs(function() {
-          testee = eventLib.DeleteNode(nodes, adapterDummy, nodeShaperDummy);
-          testee(toDel, callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(deleted).toEqual(toDel);
-        });
-        
-      });
-    
-    });
-    
-    
-    describe('Insert Edge', function() {
-      
-      it('should create an event to add an edge', function() {
-        var adapterDummy = {},
-        edgeShaperDummy = {},
-        edges = [],
-        reshaped = null,
-        called = false,
-        created = null,
-        source = {_id: 1},
-        target = {_id: 2},
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.createEdge = function(edgeToCreate, callback) {
-          created = edgeToCreate;
-          edges.push(created);
-          callback(created);
-        };
-        
-        edgeShaperDummy.drawEdges = function(edges) {
-          reshaped = edges;
-        };
-        
-        runs(function() {
-          testee = eventLib.InsertEdge(edges, adapterDummy, edgeShaperDummy);
-          testee(source, target, callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(created).toBeDefined();
-          expect(reshaped[0]).toEqual(created);
-          expect(created.source).toEqual(source);
-          expect(created.target).toEqual(target);
-        });
-        
-      });
-      
-    });
-    
-    describe('Patch Edge', function() {
-      
-      it('should create an event to patch an edge', function() {
-        var adapterDummy = {},
-        edgeShaperDummy = {},
-        source = {_id: 1},
-        target = {_id: 2},
-        patched = {source: source, target: target},
-        data = {hello: "world"},
-        edges = [patched],
-        reshaped = null,
-        called = false,
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.patchEdge = function(edgeToPatch, patchData, callback) {
-          patched = edgeToPatch;
-          jQuery.extend(patched, patchData);
-          callback();
-        };
-        
-        edgeShaperDummy.drawEdges = function(edges) {
-          reshaped = edges;
-        };
-        
-        runs(function() {
-          testee = eventLib.PatchEdge(edges, adapterDummy, edgeShaperDummy);
-          testee(patched, data, callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(patched).toBeDefined();
-          expect(reshaped[0]).toEqual(patched);
-          expect(patched.source).toEqual(source);
-          expect(patched.target).toEqual(target);
-          expect(patched.hello).toEqual("world");
-        });
-        
-      });
-      
-    });
-    
-    describe('Delete Edge', function() {
-      
-      it('should create an event to delete an edge', function() {
-        var adapterDummy = {},
-        edgeShaperDummy = {},
-        source = {_id: 1},
-        target = {_id: 2},
-        toDel = {source: source, target: target},
-        edges = [toDel],
-        deleted = null,
-        reshaped = null,
-        called = false,
-        callbackCheck = function() {
-          called = true;
-        },
-        testee;
-        
-        adapterDummy.deleteEdge = function(edgeToDelete, callback) {
-          deleted = edgeToDelete;
-          edges.pop();
-          callback();
-        };
-        
-        edgeShaperDummy.drawEdges = function(edges) {
-          reshaped = edges;
-        };
-        
-        runs(function() {
-          testee = eventLib.DeleteEdge(edges, adapterDummy, edgeShaperDummy);
-          testee(toDel, callbackCheck);
-        });
-        
-        waitsFor(function() {
-          return called;
-        });
-        
-        runs(function() {
-          expect(deleted).toEqual(toDel);
-        });
-        
-      });
+    describe('checking different events', function() {
       
     });
   });
