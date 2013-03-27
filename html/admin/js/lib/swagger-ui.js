b5bfe7056c8b9cae5d626618f4115d5169855d4b
Hacki <michael.hackstein@rwth-aachen.de>
2013-03-27 18:58:37 +0100

    Frontend: Removed the ugly bar below the swagger documentation

--- html/admin/js/lib/swagger-ui.js
+++ html/admin/js/lib/swagger-ui.js
@@ -381,8 +381,11 @@ function program1(depth0,data) {
     + "\n        ";
   return buffer;
   }
-
-  buffer += "\n<div class='container' id='resources_container'>\n    <ul id='resources'>\n    </ul>\n\n    <div class=\"footer\">\n        <br>\n        <br>\n        <h4 style=\"color: #999\">[ <span style=\"font-variant: small-caps\">base url</span>: ";
+  
+  buffer += "\n<div class='container' id='resources_container'>\n    <ul id='resources'>\n    </ul>";
+  return buffer;
+  /* Old Version from swagger.
+  buffer += "\n\n    <div class=\"footer\">\n        <br>\n        <br>\n        <h4 style=\"color: #999\">[ <span style=\"font-variant: small-caps\">base url</span>: ";
   if (stack1 = helpers.basePath) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.basePath; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -391,6 +394,7 @@ function program1(depth0,data) {
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "]</h4>\n    </div>\n</div>\n";
   return buffer;
+  */
   });
 })();
 
