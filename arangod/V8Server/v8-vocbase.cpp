f81c5d3970060be5ea450bdf8f42fcfdca2c8451
Jan Steemann <jan@arangodb.com>
2013-03-28 17:02:15 +0100

    updated documentation about key generators

--- arangod/V8Server/v8-vocbase.cpp
+++ arangod/V8Server/v8-vocbase.cpp
@@ -5628,6 +5628,14 @@ static v8::Handle<v8::Value> JS_CompletionsVocbase (v8::Arguments const& argv) {
 /// With properties:
 ///
 /// @verbinclude shell_create-collection-properties
+///
+/// With a key generator:
+///
+/// @verbinclude shell_create-collection-keygen
+///
+/// With a special key option:
+///
+/// @verbinclude shell_create-collection-keyoptions
 ////////////////////////////////////////////////////////////////////////////////
 
 static v8::Handle<v8::Value> JS_CreateVocbase (v8::Arguments const& argv) {
