d9d1080f4b957746643ee5c2d46434bc72e28c8c
Hacki <michael.hackstein@rwth-aachen.de>
2013-04-02 09:23:44 +0200

    GraphViewer: Moved EdgeShaper interface to the same as NodeShaper now

--- html/admin/js/graphViewer/graph/edgeShaper.js
+++ html/admin/js/graphViewer/graph/edgeShaper.js
@@ -31,6 +31,9 @@
 * flags example format:
 * {
 *   label: "key" \\ function(node)
+*   actions: {
+*     click: function(edge)
+*   }
 * }
 *
 *
@@ -43,6 +46,185 @@ function EdgeShaper(parent, flags, idfunc) {
     idFunction = function(d) {
       return d.source._id + "-" + d.target._id;
     },
+    noop = function (edge) {
+    
+    },
+    events = {
+      click: noop,
+      dblclick: noop,
+      mousedown: noop,
+      mouseup: noop,
+      mousemove: noop
+    },
+    addUpdate = noop,
+    addShape = noop,
+    reloadShape = noop,
+    addLabel = noop,
+    reloadLabel = noop,
+    
+    addEvents = function (edges) {
+      _.each(events, function (func, type) {
+        if (type === "update") {
+          addUpdate = func;
+        } else {
+          edges.on(type, func);
+        }
+        
+      });
+    },
+    
+    bindEvent = function (type, func) {
+      if (type === "update") {
+        addUpdate = func;
+      } else if (events[type] === undefined) {
+        throw "Sorry Unknown Event " + type + " cannot be bound.";
+      }
+      events[type] = func;
+    },
+    
+    shapeEdges = function (nodes) {
+      if (nodes !== undefined) {
+        var data, handle;
+        data = self.parent
+          .selectAll(".link")
+          .data(nodes, idFunction);
+        handle = data
+          .enter()
+          .append("line")
+          .attr("class", "link") // link is CSS class that might be edited
+          .attr("id", idFunction);
+        addShape(handle);
+        addLabel(handle);
+        addEvents(handle);
+        data.exit().remove();
+        return handle;
+      }
+    },
+    
+    reshapeEdges = function () {
+      var handle;
+      handle = self.parent
+        .selectAll(".link");
+      $(".link").empty();
+      addShape(handle);
+      addLabel(handle);
+      addEvents(handle);
+    },
+    
+    reshapeEdge = function (edge) {
+      var handle = self.parent
+        .selectAll(".link")
+        .filter(function (e) {
+          return e._id === edge._id;
+        });
+      $("#" + edge._id).empty();
+      addShape(handle);
+      addLabel(handle);
+      addEvents(handle);
+    },
+    
+    updateEdges = function () {
+      var edges = self.parent.selectAll(".link")
+        // Set source x coordinate for edge.
+        .attr("x1", function(d) {
+            return d.source.x; 
+        })
+        // Set source y coordinate for edge.
+        .attr("y1", function(d) {
+            return d.source.y; 
+        })
+        // Set target x coordinate for edge.
+        .attr("x2", function(d) {
+            return d.target.x; 
+        })
+        // Set target y coordinate for edge.
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+        edges.attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")"; 
+        });
+      addUpdate(edges);
+    },
+    
+    parseShapeFlag = function (shape) {
+      switch (shape.type) {
+        default:
+          throw "Sorry given Shape not known!";
+      }
+    },
+    
+    parseLabelFlag = function (label) {
+      if (_.isFunction(label)) {
+        addLabel = function (edge) {
+          edge.append("text") // Append a label for the edge
+            .attr("text-anchor", "middle") // Define text-anchor
+            .text(label);
+        };
+      } else {
+        addLabel = function (edge) {
+          edge.append("text") // Append a label for the edge
+            .attr("text-anchor", "middle") // Define text-anchor
+            .text(function(d) { 
+              return d[label] !== undefined ? d[label] : ""; // Which value should be used as label
+            });
+        };
+      }
+    },
+    
+    parseActionFlag = function (actions) {
+      _.each(actions, function(func, type) {
+        bindEvent(type, func);
+      });
+    },
+    
+    parseConfig = function(config) {
+      if (config.shape !== undefined) {
+        parseShapeFlag(config.shape);
+      }
+      if (config.label !== undefined) {
+        parseLabelFlag(config.label);
+      }
+      if (config.actions !== undefined) {
+        parseActionFlag(config.actions);
+      }
+    };
+    
+  self.parent = parent;  
+   
+  if (flags !== undefined) {
+    parseConfig(flags);
+  } 
+
+  if (_.isFunction(idfunc)) {
+    idFunction = idfunc;
+  }
+  
+  /////////////////////////////////////////////////////////
+  /// Public functions
+  /////////////////////////////////////////////////////////
+  
+  self.changeTo = function(config) {
+    parseConfig(config);
+    reshapeEdges();
+  };
+  
+  self.drawEdges = function (edges) {
+    return shapeEdges(edges);
+  };
+  
+  self.updateEdges = function () {
+    updateEdges();
+  };
+  
+  self.reshapeEdge = function(edge) {
+    reshapeEdge(edge);
+  };
+  
+  self.reshapeEdges = function() {
+    reshapeEdges();
+  };  
+/*    OLD Backup
     additionalShaping = function(edge) {
       return;
     },
@@ -62,7 +244,7 @@ function EdgeShaper(parent, flags, idfunc) {
     },
     parseLabelFlag;
     
-  self.parent = parent;
+  
   
   if (flags !== undefined) {
 
@@ -146,7 +328,7 @@ function EdgeShaper(parent, flags, idfunc) {
     self.drawEdges();
   };
   
-  
+*/  
 }
 
 
