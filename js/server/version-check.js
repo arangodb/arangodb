f94a0ee96ae12455e8bae2586484667802192952
Jan Steemann <jan@arangodb.com>
2013-04-10 08:49:41 +0200

    create _ids collection

--- js/server/version-check.js
+++ js/server/version-check.js
@@ -371,6 +371,24 @@
         return true;
     });
 
+    // set up the collection _ids
+    addTask("setupIds", "setup _ids collection", function () {
+      return createSystemCollection("_ids", { waitForSync : false });
+    });
+    
+    // create a cap constraint for _ids
+    addTask("ensureIdsCap", "ensureCapConstraint for _ids collection", function () {
+      var ids = getCollection("_ids");
+
+      if (! ids) {
+        return false;
+      }
+
+      ids.ensureCapConstraint(50);
+
+      return true;
+    });
+
     // loop through all tasks and execute them
     console.log("Found " + allTasks.length + " defined task(s), "
                 + activeTasks.length + " task(s) to run");
