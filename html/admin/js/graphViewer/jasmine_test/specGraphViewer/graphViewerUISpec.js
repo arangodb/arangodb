22145f889ba3ab6d0c33ebe9723db5552ff63832
Hacki <michael.hackstein@rwth-aachen.de>
2013-04-10 15:40:06 +0200

    GraphViewer: Finished tests for GraphViewer UI

--- html/admin/js/graphViewer/jasmine_test/specGraphViewer/graphViewerUISpec.js
+++ html/admin/js/graphViewer/jasmine_test/specGraphViewer/graphViewerUISpec.js
@@ -1,7 +1,7 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, white: true  plusplus: true */
 /*global beforeEach, afterEach */
 /*global describe, it, expect, jasmine*/
-/*global runs, waitsFor, spyOn */
+/*global runs, waitsFor, spyOn, waits */
 /*global window, eb, loadFixtures, document */
 /*global $, _, d3*/
 /*global helper, mocks*/
@@ -41,15 +41,16 @@
   describe('Graph Viewer UI', function () {
     
     var div,
-    ui;
+    ui,
+    adapterConfig;
     
     beforeEach(function() {
       div = document.createElement("div");
       div.id = "contentDiv";
+      adapterConfig = {type: "json", path: "../test_data/"};
       document.body.appendChild(div);
-      ui = new GraphViewerUI(div);
+      ui = new GraphViewerUI(div, adapterConfig);
       this.addMatchers({
-        
         toBeADropdownMenu: function() {
           var div = this.actual,
             btn = div.children[0],
@@ -83,7 +84,8 @@
             msg = "first elements data-target to be a link";
             return false;
           }
-          if (btn.children[0].tagName.toLowerCase() !== "b" && btn.children[0].className !== "caret") {
+          if (btn.children[0].tagName.toLowerCase() !== "b"
+            && btn.children[0].className !== "caret") {
             msg = "first element to contain a caret";
             return false;
           }
@@ -126,6 +128,14 @@
       ).toThrow("The parent element needs an unique id.");
     });
     
+    it('should throw an error if the adapter config is not given', function() {
+      expect(
+        function() {
+          var t = new GraphViewerUI(div);
+        }
+      ).toThrow("An adapter configuration has to be given");
+    });
+    
     it('should append a svg to the given parent', function() {
       expect($("#contentDiv svg").length).toEqual(1);
     });
@@ -150,6 +160,19 @@
       
       it('should append the menubar', function() {
         expect($("#contentDiv #menubar").length).toEqual(1);
+      });
+      
+      it('should contain a field to load a node', function() {
+        expect($("#contentDiv #menubar #nodeid").length).toEqual(1);
+        expect($("#contentDiv #menubar #loadnode").length).toEqual(1);
+        var field = $("#contentDiv #menubar #nodeid")[0],
+          btn = $("#contentDiv #menubar #loadnode")[0];
+        expect(field.tagName.toLowerCase()).toEqual("input");
+        expect(field.className).toEqual("searchInput");
+        expect(btn.tagName.toLowerCase()).toEqual("img");
+        expect(btn.width).toEqual(16);
+        expect(btn.height).toEqual(16);
+        expect(btn.src).toEqual("img/enter_icon");
       });
       
       it('should contain a menu for the node shapes', function() {
@@ -186,145 +209,107 @@
       
     });
     
-  });
-  
-  describe('set up with jsonAdapter, forceLayout, click Expand and default shapers', function() {
     
-    var viewer;
+    describe('set up with jsonAdapter and click Expand rest default', function() {
     
-    beforeEach(function() {
-      var aconf = {type: "json", path: "../test_data/"},
-      lconf = {type: "force"},
-      evconf = {
-        expand: {
-          target: "nodes",
-          type: "click",
-          callback: function(){}
-        }
-      },
-      config = {
-        layouter: lconf,
-        events: evconf
+      var waittime = 100,
+    
+      clickOnNode = function(id) {
+        helper.simulateMouseEvent("click", id);
       };
-      viewer = new GraphViewer(svg, 10, 10,aconf,  config);
-      
-      this.addMatchers({
-        toBeDisplayed: function() {
-          var nodes = this.actual,
-          nonDisplayed = [];
-          this.message = function(){
-            var msg = "Nodes: [";
-            _.each(nonDisplayed, function(n) {
-              msg += n + " ";
-            });
-            msg += "] are not displayed.";
-            return msg;
-          };
-          _.each(nodes, function(n) {
-            if ($("svg #" + n)[0] === undefined) {
-              nonDisplayed.push(n);
-            }
-          });
-          return nonDisplayed.length === 0;
-        },
-        toNotBeDisplayed: function() {
-          var nodes = this.actual,
-          displayed = [];
-          this.message = function() {
-            var msg = "Nodes: [";
-            _.each(displayed, function(n) {
-              msg += n + " ";
-            });
-            msg += "] are still displayed.";
-            return msg;
-          };
-          _.each(nodes, function(n) {
-            if ($("svg #" + n)[0] !== undefined) {
-              displayed.push(n);
-            }
-          });
-          return displayed.length === 0;
-        },
-        toBeConnectedTo: function(target) {
-          var source = this.actual;
-          this.message = function() {
-            return source + " -> " + target + " edge, does not exist";
-          };
-          return $("svg #" + source + "-" + target)[0] !== undefined;
-        },
-        toNotBeConnectedTo: function(target) {
-          var source = this.actual;
-          this.message = function() {
-            return source + " -> " + target + " edge, does still exist";
-          };
-          return $("svg #" + source + "-" + target)[0] === undefined;
-        }
-      });
-      
-    });
     
-
-    
-    describe('when a graph has been loaded', function() {
       beforeEach(function() {
-      
-        runs (function() {
-          viewer.loadGraph(0);
-        });
-    
-        waits(waittime);
-      });
-      
-      /*
-      it('should be able to rebind the events', function() {
-        var called = false;
-        
-        runs(function() {
-          var newEventConfig = {custom: [
-            {
-              target: "nodes",
-              type: "click",
-              func: function() {
-                called = true;
+
+        this.addMatchers({
+          toBeDisplayed: function() {
+            var nodes = this.actual,
+            nonDisplayed = [];
+            this.message = function(){
+              var msg = "Nodes: [";
+              _.each(nonDisplayed, function(n) {
+                msg += n + " ";
+              });
+              msg += "] are not displayed.";
+              return msg;
+            };
+            _.each(nodes, function(n) {
+              if ($("svg #" + n)[0] === undefined) {
+                nonDisplayed.push(n);
               }
-            }
-          ]};
-          viewer.rebind(newEventConfig);
-          clickOnNode(1);
+            });
+            return nonDisplayed.length === 0;
+          },
+          toNotBeDisplayed: function() {
+            var nodes = this.actual,
+            displayed = [];
+            this.message = function() {
+              var msg = "Nodes: [";
+              _.each(displayed, function(n) {
+                msg += n + " ";
+              });
+              msg += "] are still displayed.";
+              return msg;
+            };
+            _.each(nodes, function(n) {
+              if ($("svg #" + n)[0] !== undefined) {
+                displayed.push(n);
+              }
+            });
+            return displayed.length === 0;
+          },
+          toBeConnectedTo: function(target) {
+            var source = this.actual;
+            this.message = function() {
+              return source + " -> " + target + " edge, does not exist";
+            };
+            return $("svg #" + source + "-" + target)[0] !== undefined;
+          },
+          toNotBeConnectedTo: function(target) {
+            var source = this.actual;
+            this.message = function() {
+              return source + " -> " + target + " edge, does still exist";
+            };
+            return $("svg #" + source + "-" + target)[0] === undefined;
+          }
+
         });
-        
-        waitsFor(function() {
-          return called;
-        }, 1000, "The click event should have been triggered.");
-        
-        runs(function() {
-          expect(called).toBeTruthy();
+      
+        runs (function() {
+          $("#contentDiv #menubar #nodeid").attr("value", "0");
+          helper.simulateMouseEvent("click", "loadnode");
+          helper.simulateMouseEvent("click", "control_expand");
         });
-        
+
+        waits(waittime);
+      
       });
-      */
-      it("should be able to expand a node", function() {
     
+      it('should have loaded the graph', function() {
+        expect([0, 1, 2, 3, 4]).toBeDisplayed();
+      });
+    
+      it("should be able to expand a node", function() {
+  
         runs (function() {
           clickOnNode(1);
         });
-    
+  
         waits(waittime);
-        
+      
         runs (function() {
           expect([0, 1, 2, 3, 4, 5, 6, 7]).toBeDisplayed();
         });
 
       });
-      
-      it("should be able to collapse the root", function() {
     
+      it("should be able to collapse the root", function() {
+  
         runs (function() {
           clickOnNode(0);
         });
-    
+  
         waits(waittime);
-    
+  
         runs (function() {
           // Load 1 Nodes: Root
           expect([0]).toBeDisplayed();
@@ -332,23 +317,24 @@
         });
 
       });
-      
+    
       it("should be able to load a different graph", function() {
         runs (function() {
-          viewer.loadGraph(42);
+          $("#contentDiv #menubar #nodeid").attr("value", "42");
+          helper.simulateMouseEvent("click", "loadnode");
         });
-    
+  
         waits(waittime);
-    
+  
         runs (function() {
           expect([42, 43, 44, 45]).toBeDisplayed();
           expect([0, 1, 2, 3, 4]).toNotBeDisplayed();
         });
-      
+    
       });
-      
+    
       describe("when a user rapidly expand nodes", function() {
-        
+      
         beforeEach(function() {
           runs (function() {
             clickOnNode(1);
@@ -356,17 +342,17 @@
             clickOnNode(3);
             clickOnNode(4);
           });
-    
+  
           waits(waittime);
-          
+        
           it("the graph should still be correct", function() {
             expect([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12]).toBeDisplayed();
           });
         });
       });
-      
+    
       describe("when a user rapidly expand and collapses nodes", function() {
-      
+    
         beforeEach(function() {
           runs (function() {
             clickOnNode(1);
@@ -374,7 +360,7 @@
             clickOnNode(3);
             clickOnNode(4);
           });
-        
+      
           // Wait a gentle second for all nodes to expand properly
           waits(waittime);
 
@@ -382,76 +368,76 @@
             clickOnNode(1);
             clickOnNode(4);
           });
-        
+      
           waits(waittime);
         });
-        
+      
         it("the graph should still be correct", function() {
           expect([0, 1, 2, 3, 4, 8, 9]).toBeDisplayed();
           expect([5, 6, 7, 12]).toNotBeDisplayed();
         });
       });
-      
+    
       describe("when an undirected circle has been loaded", function() {
-        
+      
         beforeEach(function() {
 
           runs (function() {
             clickOnNode(2);
             clickOnNode(3);
           });
-          
-          waits(waittime);
-          
-        });
         
+          waits(waittime);
+        
+        });
+      
         it("the basis should be correct", function() {
           expect([0, 1, 2, 3, 4, 8, 9]).toBeDisplayed();
         });
-        
+      
         it("should be able to collapse one node "
          + "without removing the double referenced one", function() {
-      
+    
           runs (function() {
             clickOnNode(2);
           });
-      
+    
           waits(waittime);
-      
+    
           runs (function() {
             expect([2, 3, 8]).toBeDisplayed();
             expect(2).toNotBeConnectedTo(8);
             expect(3).toBeConnectedTo(8);
           });
         });
-        
-        
+      
+      
         it("should be able to collapse the other node "
          + "without removing the double referenced one", function() {
-      
+    
           runs (function() {
             clickOnNode(3);
           });
-      
+    
           waits(waittime);
-      
+    
           runs (function() {
             expect([2, 3, 8]).toBeDisplayed();
             expect(3).toNotBeConnectedTo(8);
             expect(2).toBeConnectedTo(8);
           });
         });
-        
+      
         it("should be able to collapse the both nodes "
          + "and remove the double referenced one", function() {
-      
+    
           runs (function() {
             clickOnNode(3);
             clickOnNode(2);
           });
-      
+    
           waits(waittime);
-      
+    
           runs (function() {
             expect([2, 3]).toBeDisplayed();
             expect([8]).toNotBeDisplayed();
@@ -459,11 +445,11 @@
             expect(2).toNotBeConnectedTo(8);
           });
         });
-        
-      });
       
+      });
+    
       describe("when a complex graph has been loaded", function() {
-        
+      
         beforeEach(function() {
           runs(function() {
             clickOnNode(1);
@@ -473,39 +459,39 @@
           });
           waits(waittime);
         });
-        
+      
         it("should be able to collapse a node "
         + "referencing a node connected to a subgraph", function() {
-          
+        
           runs(function() {         
             clickOnNode(1);
           });
-          
+        
           waits(waittime);
-          
+        
           runs(function() {
-            
+          
             expect([0, 1, 2, 3, 4, 5, 8, 9, 12]).toBeDisplayed();
             expect([6, 7, 10, 11]).toNotBeDisplayed();
-            
+          
             expect(0).toBeConnectedTo(1);
             expect(0).toBeConnectedTo(2);
             expect(0).toBeConnectedTo(3);
             expect(0).toBeConnectedTo(4); 
             expect(1).toNotBeConnectedTo(5);
-            
+          
             expect(2).toBeConnectedTo(8);
-            
+          
             expect(3).toBeConnectedTo(8);
             expect(3).toBeConnectedTo(9);
-            
+          
             expect(4).toBeConnectedTo(5);
             expect(4).toBeConnectedTo(12);
           });
         });
       });
-      
     });
+    
   });
-  
+
 }());
