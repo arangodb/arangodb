cc9b9407afdeeeb699519cc169a019a8df179500
Michael Hackstein <michael@arangodb.com>
2013-04-11 14:50:21 +0200

    GraphViewer: Added GraphViewer to Admin Interface, however Menu is still hidden. Also started with a spec to test if the generated UI conforms to Admin Interface css

--- html/admin/js/views/graphView.js
+++ html/admin/js/views/graphView.js
@@ -1,132 +1,42 @@
-var graphView = Backbone.View.extend({
+window.graphView = Backbone.View.extend({
   el: '#content',
-  svgWidth: 898,
-  svgHeight: 350,
-  scaleFactor: 1,
-  NODERADIUS: 12, // Constant Node Radius
-  LINKDIST: 100, // Constant Link Distance
-  handles: {},
   
   template: new EJS({url: 'js/templates/graphView.ejs'}),
   
   initialize: function () {
     var self = this;
-    self.force = d3.layout.force();
-    self.force.size([self.svgWidth, self.svgHeight]);
-    self.force.charge(-300);
+
   },
 
   events: {
-    'click #submitQueryIcon'   : 'submitQuery',
-    'click #submitQueryButton' : 'submitQuery',
-    'click .clearicon': 'clearOutput'
+    "click #createViewer" : "createViewer"
   },
 
-  clearOutput: function() {
-    $('#graphOutput').empty();
+  createViewer: function() {
+    var host,
+    ecol,
+    ncol,
+    aaconfig;
+    
+    host = $("#host")[0].value;
+    ecol = $("#edgeCollection")[0].value;
+    ncol = $("#nodeCollection")[0].value;
+           
+    aaconfig = {
+      type: "arango",
+      nodeCollection: ncol,
+      edgeCollection: ecol,
+      host: host
+    };
+
+    $("#creationDialog").remove();
+    ui = new GraphViewerUI(document.getElementById("content"), aaconfig);
   },
 
-  submitQuery: function() {
-    // TODO!
-    var self = this;
-    //self.renderGraph("js/graph/testdata/friends.json");
-    self.renderGraph("js/graph/testdata/big.json");
-    /*
-    var editor = ace.edit("aqlEditor");
-    var data = {query: editor.getValue()};
-
-    var editor2 = ace.edit("queryOutput");
-
-    $.ajax({
-      type: "POST",
-      url: "/_api/cursor",
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      processData: false,
-      success: function(data) {
-        editor2.setValue(self.FormatJSON(data.result));
-      },
-      error: function(data) {
-        try {
-          var temp = JSON.parse(data.responseText);
-          editor2.setValue('[' + temp.errorNum + '] ' + temp.errorMessage);
-        }
-        catch (e) {
-          editor2.setValue('ERROR');
-        }
-      }
-    });
-    */
-  },
-
-  renderGraph: function(jsonURL) {
-    var self = this;
-    if (self.force) {
-      self.force.stop();
-    }
-    $('#graphSVG').empty();
-    d3.json(jsonURL, function(error, graph) {
-      var s = (typeof(self.scaleFactor) != "undefined" ? self.scaleFactor : 1);
-      var r = s * self.NODERADIUS;
-      var nodes = graph.nodes;
-      var links = graph.links;
-      var svg = d3.select("svg");
-      
-      window.graph.startForcebasedLayout(self.force, nodes, links, self.LINKDIST * s, self.forceTick.bind(self));
-      var edge = window.graph.drawEdgeWithSimpleArrowHead(svg, links, self.NODERADIUS, s);
-      var edgeLabel = window.graph.appendLabelToEdges(svg, links, "label");
-      var node = window.graph.drawNodes(svg, nodes);
-      window.graph.makeNodesCircular(node, r);
-      window.graph.makeNodesDragable(node, self.force);
-      window.graph.makeNodesClickable(node, self.nodeClicked);
-      window.graph.appendLabelToNodes(node, "name");
-      
-      self.handles.edge = edge;
-      self.handles.node = node;
-      self.handles.edgeLabel = edgeLabel;
-      self.handles.nodes = nodes;
-    });
-  },
-
-  forceTick: function() {
-    var self = this;
-    var changedDistance = window.graph.tick.nodePosition(self.handles.node, self.handles.nodes, self.x_scale);
-    window.graph.tick.edgePosition(self.handles.edge);
-    window.graph.tick.edgeLabelPosition(self.handles.edgeLabel, "label");
-    if (changedDistance < 0.5) {
-      self.force.stop();
-    }
-  },
-
-  nodeClicked: function(node) {
-    console.log(node);
-  },
 
   render: function() {
     $(this.el).html(this.template.text);
-    var aqlEditor = ace.edit("aqlEditor");
-    aqlEditor.getSession().setMode("ace/mode/javascript");
-    aqlEditor.setTheme("ace/theme/merbivore_soft");
-    aqlEditor.resize();
     
-    $('#graphOutput').resizable({
-      handles: "s",
-      ghost: true,
-      stop: function () {
-        
-      }
-    });
-    $('#aqlEditor').resizable({
-      handles: "n, s",
-      ghost: true,
-      helper: "resizable-helper",
-      stop: function () {
-        var aqlEditor = ace.edit("aqlEditor");
-        aqlEditor.resize();
-      }
-    });
-
-    $('#aqlEditor .ace_text-input').focus();
     return this;
   }
 
