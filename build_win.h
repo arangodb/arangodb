09b2c16e8a44e93288f4d55317213afc24894d1e
Oreste Panaia <o.panaia@triagens.de>
2013-04-12 23:26:22 +0800

    windows build changes

--- build_win.h
+++ build_win.h
@@ -1,13 +1,13 @@
 #ifdef _WIN64
   #ifdef _DEBUG
-    #define TRIAGENS_VERSION "1.2.2 [WIN64-DEBUG]"
+    #define TRIAGENS_VERSION "1.2.3 [WIN64-DEBUG]"
   #else  
-    #define TRIAGENS_VERSION "1.2.2 [WIN64-RELEASE]"
+    #define TRIAGENS_VERSION "1.2.3 [WIN64-RELEASE]"
   #endif  
 #else  
   #ifdef _DEBUG
-    #define TRIAGENS_VERSION "1.2.2 [WIN32-DEBUG]"
+    #define TRIAGENS_VERSION "1.2.3 [WIN32-DEBUG]"
   #else  
-    #define TRIAGENS_VERSION "1.2.2 [WIN32-RELEASE"
+    #define TRIAGENS_VERSION "1.2.3 [WIN32-RELEASE"
   #endif  
 #endif  
