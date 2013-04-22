33239e8c3c1f74002fad53b6731d32222093b6fb
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-04-22 14:24:52 +0200

    backticks replaced by <em></em> in swagger description text

--- Documentation/Scripts/generateSwagger.py
+++ Documentation/Scripts/generateSwagger.py
@@ -65,6 +65,12 @@ def parameters(line):
     line , c , r = line.rpartition('}')
     return line
 
+def BackTicks(txt, wordboundary = ['<em>','</em>']):
+    # `word` -> <b>word</b>
+    r = rc(r"""([\(\s'/">]|^|.)\`(.*?)\`([<\s\.\),:;'"?!/-]|$)""", MS)
+    subpattern = '\\1' + wordboundary[0] + '\\2' + wordboundary[1] + '\\3'
+    return r.sub(subpattern, txt)
+
 def FA(txt, wordboundary = ['<b>','</b>']):
     # @FA{word} -> <b>word</b>
     r = rc(r"""([\(\s'/">]|^|.)@FA\{(.*?)\}([<\s\.\),:;'"?!/-]|$)""", MS)
@@ -84,6 +90,7 @@ def LIT(txt, wordboundary = ['<b>','</b>']):
     return r.sub(subpattern, txt)
 
 def Typography(txt):
+    txt = BackTicks(txt)
     txt = FN(txt)
     txt = LIT(txt)
     txt = FA(txt)
