d56c0617d2b9b57cca841e50feff6701f94b58f3
Jan Steemann <jan@arangodb.com>
2013-05-13 09:26:29 +0200

    suppress (probably invalid) message

--- arangod/V8Server/ApplicationV8.cpp
+++ arangod/V8Server/ApplicationV8.cpp
@@ -594,11 +594,7 @@ bool ApplicationV8::prepare () {
   }
   
   // check whether package-path exist
-  if (! _packagePath.empty() && ! FileUtils::isDirectory(_packagePath.c_str())) {
-    LOGGER_ERROR("specified package-path '" << _packagePath << "' does not exist.");
-    // TODO: decide if we want to abort server start here
-  }
-  else if (_packagePath.empty()) {
+  if (_packagePath.empty()) {
     LOGGER_ERROR("--javascript.package-path option was not specified. this may cause follow-up errors.");
     // TODO: decide if we want to abort server start here
   }
