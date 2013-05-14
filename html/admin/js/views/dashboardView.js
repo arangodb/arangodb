d1d5a8201c62e342a705fe9bac301d9fe4e75c72
jsteemann <j.steemann@triagens.de>
2013-05-14 02:36:01 +0200

    removed hard-coded groups from statistics web interface
    now all statistics are dynamic
    removed redundant code in statistics web interface

--- html/admin/js/views/dashboardView.js
+++ html/admin/js/views/dashboardView.js
@@ -1,40 +1,42 @@
 var dashboardView = Backbone.View.extend({
   el: '#content',
-  updateInterval: 1000, // constant
-  updateFrequency: 5,
+  updateInterval: 1000, // 1 second, constant
+  updateFrequency: 5, // the actual update rate (5 s)
   updateCounter: 0,
-  arraySize: 100,
-  clients: [],
-  systems: [],
-  convertedData: {},
-  oldSum: {},
+  arraySize: 99, // how many values will we keep per figure?
+  seriesData: {},
+  charts: {},
+  units: [],
 
   initialize: function () {
     var self = this;
+    
+    this.initUnits();
 
     this.collection.fetch({
       success: function() {
-        self.renderCharts("system");
-        self.renderCharts("client");
+        self.calculateSeries();
+        self.renderCharts();
 
         window.setInterval(function() {
-              self.updateCounter++;
-              if (self.updateCounter < self.updateFrequency) {
-                return false;
-              }
+          self.updateCounter++;
 
-              self.updateCounter = 0;
+          if (self.updateCounter < self.updateFrequency) {
+            return false;
+          }
 
-              self.collection.fetch({
-                success: function() {
-                  self.updateCharts("system");
-                  self.updateCharts("client");
-                },
-                error: function() {
-                  // need to flush previous values
-                  self.oldSum = { };
-                }
-              });
+          self.updateCounter = 0;
+          self.collection.fetch({
+            success: function() {
+              self.calculateSeries();
+              self.renderCharts();
+            },
+            error: function() {
+              // need to flush previous values
+              self.calculateSeries(true);
+              self.renderCharts();
+            }
+          });
 
         }, self.updateInterval);
       }
@@ -52,341 +54,223 @@ var dashboardView = Backbone.View.extend({
     var self = this;
     $(this.el).html(this.template.text);
 
-    $.each(this.options.description.models[0].attributes.groups, function(key, val) {
+    $.each(this.options.description.models[0].attributes.groups, function () {
       $('.thumbnails').append(
-        '<ul class="statGroups" id="'+this.group+'">' +
-        '<h4 class="statsHeader">'+this.name+'</h4>' +
+        '<ul class="statGroups" id="' + this.group + '">' +
+        '<h4 class="statsHeader">' + this.name + '</h4>' +
         '</ul>');
+      $('#menuGroups').append(
+        '<li class="nav-header">' + this.name + '</li>' + 
+        '<li class="divider" id="' + this.group + 'Divider"></li>'
+      );
     });
-    $.each(this.options.description.models[0].attributes.figures, function(key, val) {
-      if (this.group === 'system') {
-        self.renderSystem(this.identifier, this.name, this.description, this.type, this.units);
-        self.systems.push(this.identifier);
-      }
-      else if (this.group === 'client') {
-        self.renderClient(this.identifier, this.name, this.description, this.type, this.units);
-        self.clients.push(this.identifier);
-      }
+
+    $.each(this.options.description.models[0].attributes.figures, function () {
+      self.renderFigure(this);
     });
+
     if (this.collection.models[0] === undefined) {
       this.collection.fetch({
         success: function() {
-          self.renderCharts("system");
-          self.renderCharts("client");
+          self.calculateSeries();
+          self.renderCharts();
+        },
+        error: function() {
+          self.calculateSeries();
+          self.renderCharts(flush);
         }
       });
     }
     else {
-      self.renderCharts("system");
-      self.renderCharts("client");
+      self.calculateSeries();
+      self.renderCharts();
     }
 
     return this;
   },
-  checkInterval: function (a) {
-    var self = this;
-    self.updateFrequency = a.target.value;
 
+  checkInterval: function (a) {
+    this.updateFrequency = a.target.value;
   },
+
   checkEnabled: function (a) {
     var myId = a.target.id;
     var position = myId.search('Checkbox');
     var preparedId = myId.substring(0, position);
     var toCheck = $(a.target).is(':checked');
     if (toCheck === false) {
-      $("#"+preparedId).hide();
+      $("#" + preparedId).hide();
     }
     else if (toCheck === true) {
-      $("#"+preparedId).show();
+      $("#" + preparedId).show();
     }
   },
 
-  renderCharts: function (type) {
+  initUnits : function () {
+    this.units = [ ];
+    var i;
+    var scale = 0.0001;
+
+    for (i = 0; i < 12; ++i) {
+      this.units.push(scale * 1);
+      this.units.push(scale * 2);
+      this.units.push(scale * 2.5);
+      this.units.push(scale * 4);
+      this.units.push(scale * 5);
+      scale *= 10;
+    }
+  },
+
+  getMaxValue : function (identifier) {
+    var max = this.seriesData[identifier].max || 1;
+
+    var i = 0, n = this.units.length;
+    while (i < n) {
+      var unit = this.units[i++];
+      if (max > unit) {
+        continue;
+      }
+      if (max == unit) {
+        break;
+      }
+      return unit;
+    }
+
+    return max;
+  },
+
+  renderCharts: function () {
     var self = this;
-    var dataType;
-    if (type === "system") {
-      dataType = self.systems;
-    }
-    else if (type === "client") {
-      dataType = self.clients;
-    }
 
-    this.generateClientData(type);
-    var label;
+    $.each(self.options.description.models[0].attributes.figures, function () {
+      var figure = this;
+      var identifier = figure.identifier;
+      var chart;
 
-    $.each(dataType, function(key, client) {
-      var client = client;
-      nv.addGraph(function() {
-        var chart = nv.models.lineChart();
-
-        chart.xAxis
-        .axisLabel('')
-
-        $.each(self.options.description.models[0].attributes.figures, function(k,v) {
-          if (v.identifier === client) {
-            if (v.units === 'bytes') {
-              label = 'megabytes';
-            }
-            else {
-              label = v.units;
-            }
-          }
-        });
-
-        chart.yAxis
-        .axisLabel(label)
-
-
-        // restrict to y-range 0...
-        var max = 0;
-        self.convertedData[client].map(function (value) { 
-          if (value.y > max) {
-            max = value.y;
-          }
-        });
-        chart.yDomain([0, max]);
-
-        d3.select("#"+client+"Chart svg")
-        .datum(self.generateD3Input(self.convertedData[client], client, "#8AA051"))
-        .transition().duration(500)
-        .call(chart)
-
-        nv.utils.windowResize(function() {
-          d3.select('#'+client+'Chart svg').call(chart);
-        });
-
-        d3.selectAll('.nv-x text').text(function(d){
+      if (self.charts[identifier] === undefined) {      
+        chart = self.charts[identifier] = nv.models.lineChart();
+        chart.xAxis.axisLabel('').tickFormat(function (d) {
           if (isNaN(d)) {
-            return d;
+            return '';
           }
-          else {
-            function pad (value) {
-              return (value < 10 ? "0" + String(value) : String(value));
-            }
-
-            var date = new Date(d * 1000);
- 
-            return pad(date.getHours()) + ":" + pad(date.getMinutes()) + ":" + pad(date.getSeconds());
-          }
-        });
-
-        return chart;
-      });
-
-    });
-  },
-
-  updateCharts: function (type) {
-    var self = this;
-    var dataType;
-    if (type === "system") {
-      dataType = self.systems;
-    }
-    else if (type === "client") {
-      dataType = self.clients;
-    }
-
-    this.generateClientData(type);
-
-    var label;
-
-    $.each(dataType, function(key, client) {
-      var client = client;
-      var chart = nv.models.lineChart();
-
-      chart.xAxis
-      //.axisLabel("Time (ms)")
-      .axisLabel("");
-
-      $.each(self.options.description.models[0].attributes.figures, function(k,v) {
-        if (v.identifier === client) {
-            if (v.units === 'bytes') {
-              label = 'megabytes';
-            }
-            else {
-              label = v.units;
-            }
-        }
-      });
-
-      chart.yAxis
-      .axisLabel(label);
-
-      // restrict to y-range 0...
-      var max = 0;
-      self.convertedData[client].map(function (value) {
-        if (value.y > max) {
-          max = value.y;
-        }
-      });
-      chart.yDomain([0, max]);
-
-      d3.select("#"+client+"Chart svg")
-      .datum(self.generateD3Input(self.convertedData[client], client, "#8AA051"))
-      .transition().duration(500)
-      .call(chart)
-
-      d3.selectAll('.nv-x text').text(function(d){
-        if (isNaN(d)) {
-          return d;
-        }
-        else {
+           
           function pad (value) {
             return (value < 10 ? "0" + String(value) : String(value));
           }
 
-          var date = new Date(d * 1000);
+          var date = new Date(d / 10); 
+  
           return pad(date.getHours()) + ":" + pad(date.getMinutes()) + ":" + pad(date.getSeconds());
-        }
-      });
-
-    });
-
-
-  },
-
-  generateClientData: function (type) {
-    var self = this;
-
-    var dataType;
-    if (type === "system") {
-      dataType = this.collection.models[0].attributes.system;
-    }
-    else if (type === "client") {
-      dataType = this.collection.models[0].attributes.client;
-    }
-
-    var x = []; //Time
-    var y = []; //Value
-    var tempName;
-    var tempArray = [];
-    $.each(dataType, function(key, val) {
-      tempName = key;
-      if (self.convertedData[tempName] === undefined) {
-        self.convertedData[tempName] = [];
-      }
-
-      if (self.convertedData[tempName].length === 0) {
-        var oldValue;
-        if (val.sum === undefined) {
-          oldValue = val;
-        }
-        else {
-          oldValue = val.sum;
-        }
-        if (oldValue < 0) {
-          oldValue = 0;
-        }
-
-        self.oldSum[tempName] = oldValue;
-        tempArray = self.convertedData[tempName];
-        var timeStamp = Math.round(+new Date()/1000);
-        tempArray.push({x:timeStamp, y:0});
-      }
-      if (self.convertedData[tempName].length !== 0)  {
-        tempArray = self.convertedData[tempName];
-
-        var timeStamp = Math.round(+new Date()/1000);
-
-        var myType;
-        $.each(self.options.description.models[0].attributes.figures, function(k,v) {
-          if (v.identifier === tempName) {
-            myType = v.type;
-            if (v.units === 'bytes') {
-              if (val.sum === undefined) {
-                val = val / 1024 / 1024;
-                val = Math.round(val * 100) / 100;
-                if (val < 0) {
-                  val = 0;
-                }
-              }
-              else {
-                val.sum = val.sum / 1024 / 1024;
-                val.sum = Math.round(val.sum * 100) / 100;
-
-                if (val.sum < 0) {
-                  val.sum = 0;
-                }
-              }
-            }
-          }
         });
 
-        if (myType === "current") {
-          tempArray.push({x:timeStamp, y:val});
-          return;
-        }
-        else {
-          var calcValue = (val.sum === undefined ? val : val.sum);
-          if (calcValue < 0) {
-            calcValue = 0;
-          }
+        var label = figure.units;
 
-          var calculatedSum = calcValue - self.oldSum[tempName];
-          //Round value
-          calculatedSum = Math.round(calculatedSum * 100) / 100;
-          if (calculatedSum < 0) {
-            calculatedSum = 0;
-          }
-          tempArray.push({x:timeStamp, y:calculatedSum});
-          self.oldSum[tempName] = calcValue;
-        }
+        if (figure.units === 'bytes') {
+          label = "megabytes";
+        } 
+        chart.yAxis.axisLabel(label);
+          
+        nv.addGraph (function () {
+          nv.utils.windowResize(function () {
+            d3.select('#' + identifier + 'Chart svg').call(chart);
+          });
+
+          return chart;
+        });
       }
+      else {
+        chart = self.charts[identifier];
+      }
+      
+      chart.yDomain([ 0, self.getMaxValue(identifier) ]);
+
+      d3.select("#" + identifier + "Chart svg")
+        .call(chart)
+        .datum([ { values: self.seriesData[identifier].values, key: identifier, color: "#8AA051" } ])
+        .transition().duration(500);
     });
   },
 
-  generateD3Input: function (values, key, color) {
-    if (values === undefined) {
-      return;
-    }
+  calculateSeries: function (flush) {
+    var self = this;
+    var timeStamp = Math.round(new Date() * 10);
 
-    return [
-      {
-        values: values,
-        key: key,
-        color: color
+    $.each(self.options.description.models[0].attributes.figures, function () {
+      var figure = this;
+      var identifier = figure.identifier;
+
+      if (self.seriesData[identifier] === undefined) {
+        self.seriesData[identifier] = { max: 0, values: [ ] };
       }
-    ]
+
+      while (self.seriesData[identifier].values.length > self.arraySize) {
+        self.seriesData[identifier].values.shift();
+      }
+
+      if (flush) {
+        self.seriesData[identifier].values.push({ x: timeStamp, y: undefined, value: undefined });
+        return;
+      }
+      
+      var responseValue = self.collection.models[0].attributes[figure.group][identifier];
+
+      if (responseValue !== undefined && responseValue !== null) {
+        if (responseValue.sum !== undefined) {
+          responseValue = responseValue.sum;
+        }
+      }
+
+      function sanitize (value, figure) {
+        if (value < 0) {
+          value = 0;
+        }
+        else {
+          if (figure.units === 'bytes') {
+            value /= (1024 * 1024);
+          }
+          value = Math.round(value * 100) / 100;
+        }
+
+        return value;
+      }
+
+      var newValue = 0;
+
+      if (figure.type === 'current') {
+        newValue = responseValue;
+      }
+      else {
+        var n = self.seriesData[identifier].values.length;
+
+        if (responseValue !== undefined && n > 0) {
+          var previous = self.seriesData[identifier].values[n - 1];
+          if (previous.value !== undefined) {
+            newValue = responseValue - previous.value;
+          }
+        }
+      }
+
+      newValue = sanitize(newValue, figure);
+      if (newValue > self.seriesData[identifier].max) {
+        self.seriesData[identifier].max = newValue;
+      }
+
+      self.seriesData[identifier].values.push({ x: timeStamp, y: newValue, value: responseValue });
+    });
   },
 
-  renderClient: function (identifier, name, desc, type, units) {
-    $('#client').append(
-      '<li class="statClient" id="'+identifier+'">' +
-      '<div class="boxHeader"><h6 class="dashboardH6">' + name + '</h6></div>' +
-      '<div class="statChart" id="'+identifier+'Chart"><svg class="svgClass"/></div>' +
+  renderFigure: function (figure) {
+    $('#' + figure.group).append(
+      '<li class="statClient" id="' + figure.identifier + '">' +
+      '<div class="boxHeader"><h6 class="dashboardH6">' + figure.name + '</h6></div>' +
+      '<div class="statChart" id="' + figure.identifier + 'Chart"><svg class="svgClass"/></div>' +
       '</li>'
     );
-    $('#lastDashDivider').before(
-      '<li><a><label class="checkbox">'+
-      '<input type="checkbox" id='+identifier+'Checkbox checked>'+name+'</label></a></li>'
-    );
-  },
-  renderSystem: function (identifier, name, desc, type, units) {
-    $('#system').append(
-      '<li class="statClient" id="'+identifier+'">' +
-      '<div class="boxHeader"><h6 class="dashboardH6">' + name + '</h6></div>' +
-      '<div class="statChart" id="'+identifier+'Chart"><svg class="svgClass"/></div>' +
-      '</li>'
-    );
-    $('#firstDashDivider').before(
-      '<li><a><label class="checkbox">'+
-      '<input type="checkbox" id='+identifier+'Checkbox checked>'+name+'</label></a></li>'
-    );
-  },
-  /*
-renderSystem: function (identifier, name, desc, type, units) {
-$('#system').append(
-'<div class="statSystem" id="'+identifier+'">' +
-'<table><tr>' +
-'<th>'+ name +'</th>' +
-'<th class="updateValue">'+ 'counting...' +'</th>' +
-'</tr></table>' +
-'</div>'
-);
-},
-*/
-updateClient: function (identifier, count, counts) {
 
-}
+    $('#' + figure.group + 'Divider').before(
+      '<li><a><label class="checkbox">'+
+      '<input type="checkbox" id=' + figure.identifier + 'Checkbox checked>' + figure.name + '</label></a></li>'
+    );
+  }
 
 });
