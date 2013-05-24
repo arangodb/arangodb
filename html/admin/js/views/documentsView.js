56ea6d46d6d1b85e469b6d58c7596330c443a814
Hacki <michael.hackstein@rwth-aachen.de>
2013-05-24 11:04:43 +0200

    Web-Interface: Fixed #541

--- html/admin/js/views/documentsView.js
+++ html/admin/js/views/documentsView.js
@@ -26,7 +26,16 @@ var documentsView = Backbone.View.extend({
     "click #documents_next"      : "nextDocuments",
     "click #confirmDeleteBtn"    : "confirmDelete",
     "keyup .modal-body"          : "listenKey",
-    "click .key"                 : "nop"
+    "click .key"                 : "nop",
+    "keyup"                      : "returnPressedHandler"
+  },
+
+  returnPressedHandler: function(event) {
+    if (event.keyCode === 13) {
+      if (!!$("#confirmDeleteBtn").attr("disabled") === false) {
+        this.confirmDelete();
+      }
+    } 
   },
 
   nop: function(event) {
@@ -125,11 +134,12 @@ var documentsView = Backbone.View.extend({
     var thiselement = a.currentTarget.parentElement;
     this.idelement = $(thiselement).prev().prev();
     this.alreadyClicked = true;
-
+    $("#confirmDeleteBtn").attr("disabled", false);
     $('#docDeleteModal').modal('show');
 
   },
   confirmDelete: function () {
+    $("#confirmDeleteBtn").attr("disabled", true);	
     this.reallyDelete();
   },
   reallyDelete: function () {
@@ -148,7 +158,7 @@ var documentsView = Backbone.View.extend({
         deleted = true;
       }
       else if (result === false) {
-        arangoHelper.arangoError('Document error');
+        arangoHelper.arangoError('Could not delete document');
       }
     }
     else if (this.type === 'edge') {
