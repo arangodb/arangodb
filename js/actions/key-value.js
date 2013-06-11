c83ae99c8f2f690903a41c7bf156dea75a7a1d2e
Jan Steemann <jan@arangodb.com>
2013-06-11 11:58:41 +0200

    added undefined variable

--- js/actions/key-value.js
+++ js/actions/key-value.js
@@ -39,6 +39,7 @@ var actions = require("org/arangodb/actions");
 var simple = require("org/arangodb/simple-query");
 var db = require("org/arangodb").db;
 var internal = require("internal");
+var arangodb = require("org/arangodb");
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                 private functions
