11fc096873599ed08317a7a093bc0521e1b8fdd8
Thomas Richter <t.richter@triagens.de>
2013-06-11 15:10:15 +0200

    admin interface swagger api added

---
+++ html/admin/js/views/apiView.js
@@ -0,0 +1,17 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true */
+/*global require, exports, Backbone, EJS, $*/
+
+var apiView = Backbone.View.extend({
+  el: '#content',
+  init: function () {
+  },
+
+  template: new EJS({url: 'js/templates/apiView.ejs'}),
+
+  render: function() {
+    $(this.el).html(this.template.text);
+    $.gritter.removeAll();
+    return this;
+  }
+
+});
