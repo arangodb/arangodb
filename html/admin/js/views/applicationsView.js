31fca729f03bd63db0da1bb0c96d405862e94c54
Michael Hackstein <michael@arangodb.com>
2013-06-26 16:19:12 +0200

    WebInterface: Apps can now be filtered by active/inactive/devel, also develapps now tell you that they are

--- html/admin/js/views/applicationsView.js
+++ html/admin/js/views/applicationsView.js
@@ -7,7 +7,34 @@ var ApplicationsView = Backbone.View.extend({
   template: new EJS({url: 'js/templates/applicationsView.ejs'}),
   
   events: {
-    "click .toggle-icon": "toggleView"
+    "click .toggle-icon": "toggleView",
+    "click #checkDevel": "toggleDevel",
+    "click #checkActive": "toggleActive",
+    "click #checkInactive": "toggleInactive"
+  },
+  
+  toggleDevel: function() {
+    var self = this;
+    this._showDevel = !this._showDevel;
+    _.each(this._installedSubViews, function(v) {
+      v.toggle("devel", self._showDevel)
+    });
+  },
+  
+  toggleActive: function() {
+    var self = this;
+    this._showActive = !this._showActive;
+    _.each(this._installedSubViews, function(v) {
+      v.toggle("active", self._showActive)
+    });
+  },
+  
+  toggleInactive: function() {
+    var self = this;
+    this._showInactive = !this._showInactive;
+    _.each(this._installedSubViews, function(v) {
+      v.toggle("inactive", self._showInactive)
+    });
   },
   
   toggleView: function(event) {
@@ -52,6 +79,9 @@ var ApplicationsView = Backbone.View.extend({
   initialize: function() {    
     this._installedSubViews = {};
     this._availableSubViews = {};
+    this._showDevel = true;
+    this._showActive = true;
+    this._showInactive = true;
     this.reload();
   },
   
@@ -65,6 +95,9 @@ var ApplicationsView = Backbone.View.extend({
       $("#availableList").append(v.render());
     });
     this.delegateEvents();
+    $('#checkActive').attr('checked', this._showActive);
+    $('#checkInactive').attr('checked', this._showInactive);
+    $('#checkDevel').attr('checked', this._showDevel);
     return this;
   }
 });
