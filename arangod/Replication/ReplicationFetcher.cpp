8b8cd1fc74499d563b0ad4b4c2e800951608d52a
Jan Steemann <jan@arangodb.com>
2013-07-03 17:47:37 +0200

    stubs

--- arangod/Replication/ReplicationFetcher.cpp
+++ arangod/Replication/ReplicationFetcher.cpp
@@ -27,10 +27,17 @@
 
 #include "ReplicationFetcher.h"
 
+#include "Rest/HttpRequest.h"
+#include "Rest/SslInterface.h"
+#include "SimpleHttpClient/GeneralClientConnection.h"
+#include "SimpleHttpClient/SimpleHttpClient.h"
+#include "SimpleHttpClient/SimpleHttpResult.h"
+
 using namespace std;
 //using namespace triagens::basics;
-//using namespace triagens::rest;
+using namespace triagens::rest;
 using namespace triagens::arango;
+using namespace triagens::httpclient;
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                      constructors and destructors
@@ -45,7 +52,21 @@ using namespace triagens::arango;
 /// @brief constructor
 ////////////////////////////////////////////////////////////////////////////////
 
-ReplicationFetcher::ReplicationFetcher () {
+ReplicationFetcher::ReplicationFetcher (const string& masterEndpoint,
+                                        double timeout) :
+  _masterEndpoint(masterEndpoint),
+  _timeout(timeout),
+  _endpoint(Endpoint::clientFactory(masterEndpoint)),
+  _connection(0),
+  _client(0) {
+ 
+  if (_endpoint != 0) { 
+    _connection = GeneralClientConnection::factory(_endpoint, _timeout, _timeout, 3);
+
+    if (_connection != 0) {
+      _client = new SimpleHttpClient(_connection, _timeout, false);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,6 +74,18 @@ ReplicationFetcher::ReplicationFetcher () {
 ////////////////////////////////////////////////////////////////////////////////
 
 ReplicationFetcher::~ReplicationFetcher () {
+  // shutdown everything properly
+  if (_client != 0) {
+    delete _client;
+  }
+
+  if (_connection != 0) {
+    delete _connection;
+  }
+
+  if (_endpoint != 0) {
+    delete _endpoint;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,6 +100,85 @@ ReplicationFetcher::~ReplicationFetcher () {
 /// @addtogroup ArangoDB
 /// @{
 ////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief disconnect
+////////////////////////////////////////////////////////////////////////////////
+  
+void ReplicationFetcher::disconnect () {
+  if (_connection != 0) {
+    _connection->disconnect();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief connect
+////////////////////////////////////////////////////////////////////////////////
+    
+int ReplicationFetcher::connect () {
+  if (_connection == 0) {
+    return TRI_ERROR_INTERNAL;
+  }
+
+  if (! _connection->connect()) {
+    // TODO: fix error code
+    return TRI_ERROR_INTERNAL;
+  }
+
+  return TRI_ERROR_NO_ERROR;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief get master status
+////////////////////////////////////////////////////////////////////////////////
+
+int ReplicationFetcher::getMasterStatus () {
+  if (_client == 0) {
+    return TRI_ERROR_INTERNAL;
+  }
+
+  map<string, string> headers;
+
+  // send GET request
+  SimpleHttpResult* response = _client->request(HttpRequest::HTTP_REQUEST_GET, 
+                                                "/_api/replication/state",
+                                                0, 
+                                                0,  
+                                                headers); 
+
+  if (response == 0) {
+    // TODO: fix error code
+    return TRI_ERROR_OUT_OF_MEMORY;
+  }
+
+  string returnMessage;
+  int    returnCode;
+
+  if (! response->isComplete()) {
+    returnMessage = _client->getErrorMessage();
+    returnCode    = 500;
+  }
+  else {
+    returnMessage = response->getHttpReturnMessage();
+    returnCode    = response->getHttpReturnCode();
+  }
+
+  std::cout << "GOT STATUS CODE " << returnCode << ", MESSAGE: " << returnMessage << std::endl;
+  std::cout << response->getBody().str() << std::endl;
+  
+  delete response;
+
+  return TRI_ERROR_NO_ERROR;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief run method
+////////////////////////////////////////////////////////////////////////////////
+
+int ReplicationFetcher::run () {
+  // TODO: make this a loop
+  return getMasterStatus();
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @}
