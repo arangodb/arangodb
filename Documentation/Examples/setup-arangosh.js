c9a60ae9a37cf2683e578c09b074749401992048
jsteemann <j.steemann@triagens.de>
2013-07-05 16:40:34 +0200

    re-added file that got deleted accidently

---
+++ Documentation/Examples/setup-arangosh.js
@@ -0,0 +1,19 @@
+db._drop("demo");
+db._create("demo");
+db.demo.save({
+  "_key" : "schlonz",
+  "firstName" : "Hugo",
+  "lastName" : "Schlonz",
+  "address" : {
+   "street" : "Strasse 1",
+    "city" : "Hier"
+  },
+  "hobbies" : [
+    "swimming",
+    "biking",
+    "programming"
+  ]
+});
+
+db._drop("animals");
+db._create("animals"); 
