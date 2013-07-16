9ac28c0c8b4b57027cec1fb709dc0c76fa325cae
mchacki <m.hackstein@triagens.de>
2013-07-16 18:37:41 +0200

    GraphViewer: Fixed the last bug of the CommunityNode, old functionality is now assured. Next: Let it render itself and its interior nodes

--- html/admin/js/graphViewer/jasmine_test/specAdapter/abstractAdapterSpec.js
+++ html/admin/js/graphViewer/jasmine_test/specAdapter/abstractAdapterSpec.js
@@ -1139,6 +1139,7 @@
           expect(e.source).toEqual(n4);
           expect(e.target).toEqual(comm);
           expect(e._target).toEqual(n2);
+          expect(comm.dissolve().edges.inbound).toEqual([e3, e]);
         });
         
         it('should be possible to insert an edge starting'
@@ -1151,6 +1152,7 @@
           expect(e.source).toEqual(comm);
           expect(e._source).toEqual(n2);
           expect(e.target).toEqual(n4);
+          expect(comm.dissolve().edges.outbound).toEqual([e2, e]);
         });
         
       });
