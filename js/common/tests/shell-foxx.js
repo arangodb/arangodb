b3dd4d7b59882c1e326fb31fb56b6ed8e424f067
Lucas Dohmen <lucas@arangodb.com>
2013-08-05 13:36:26 +0200

    Foxx: Model tests pulled out

--- js/common/tests/shell-foxx.js
+++ js/common/tests/shell-foxx.js
@@ -763,85 +763,6 @@ function FormatMiddlewareSpec () {
   };
 }
 
-function ModelSpec () {
-  var FoxxModel, TestModel, instance;
-
-  return {
-    setUp: function () {
-      FoxxModel = require('org/arangodb/foxx').Model;
-    },
-
-    testWithInitialData: function () {
-      instance = new FoxxModel({
-        a: 1
-      });
-
-      assertEqual(instance.get("a"), 1);
-      instance.set("a", 2);
-      assertEqual(instance.get("a"), 2);
-    },
-
-    testWithoutInitialData: function () {
-      instance = new FoxxModel();
-
-      assertEqual(instance.get("a"), undefined);
-      instance.set("a", 1);
-      assertEqual(instance.get("a"), 1);
-    },
-
-    testAddingAMethodWithExtend: function () {
-      TestModel = FoxxModel.extend({
-        getA: function() {
-          return this.get("a");
-        }
-      });
-
-      instance = new TestModel({
-        a: 5
-      });
-
-      assertEqual(instance.getA(), 5);
-    },
-
-    testOverwritingAMethodWithExtend: function () {
-      TestModel = FoxxModel.extend({
-        get: function() {
-          return 1;
-        }
-      });
-
-      instance = new TestModel({
-        a: 5
-      });
-
-      assertEqual(instance.get(), 1);
-    },
-
-    testHas: function () {
-      instance = new FoxxModel({
-        a: 1,
-        b: null
-      });
-
-      assertTrue(instance.has("a"));
-      assertFalse(instance.has("b"));
-      assertFalse(instance.has("c"));
-    },
-
-    testSerialization: function () {
-      var raw = {
-        a: 1,
-        b: 2
-      };
-
-      instance = new FoxxModel(raw);
-
-      assertEqual(instance.forDB(), raw);
-      assertEqual(instance.forClient(), raw);
-    }
-  };
-}
-
 jsunity.run(CreateFoxxApplicationSpec);
 jsunity.run(SetRoutesFoxxApplicationSpec);
 jsunity.run(DocumentationAndConstraintsSpec);
@@ -850,6 +771,5 @@ jsunity.run(BaseMiddlewareWithoutTemplateSpec);
 jsunity.run(BaseMiddlewareWithTemplateSpec);
 jsunity.run(ViewHelperSpec);
 jsunity.run(FormatMiddlewareSpec);
-jsunity.run(ModelSpec);
 
 return jsunity.done();
