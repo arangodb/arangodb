17b3df39843067f2dfee04ebc9f564507e1f386e
Heiko Kernbach <heiko@arangodb.com>
2013-08-14 19:25:59 +0200

    docs css finished

--- html/admin/js/views/documentsView.js
+++ html/admin/js/views/documentsView.js
@@ -563,10 +563,10 @@ var documentsView = Backbone.View.extend({
     target.pagination(options);
     $('#documentsToolbarF').prepend(
       '<ul class="prePagi"><li><a id="documents_first">'+
-      '<i class="icon icon-step-backward"></i></a></li></ul>');
+      '<span class="glyphicon glyphicon-step-backward"></span></a></li></ul>');
       $('#documentsToolbarF').append(
         '<ul class="lasPagi"><li><a id="documents_last">'+
-        '<i class="icon icon-step-forward"></i></a></li></ul>');
+        '<span class="glyphicon glyphicon-step-forward"></span></a></li></ul>');
         var total = $('#totalDocuments');
         if (total.length > 0) {
           total.html("Total: " + this.documentsCount + " documents");
