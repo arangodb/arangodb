e8998f6026366e0b72852ebffb427f2caf2f6b57
hkernbach <h.kernbach@triagens.de>
2013-08-14 19:25:59 +0200

    docs css finished

--- html/admin/js/lib/bootstrap-pagination.js
+++ html/admin/js/lib/bootstrap-pagination.js
@@ -9,8 +9,8 @@
   function stopPropagation(event) { event.stopPropagation(); }
   $.fn.pagination = function (config) {
     var options = $.extend({
-      prev: '<i class="icon icon-backward"></i>',
-      next: '<i class="icon icon-forward"></i>',
+      prev: '<span class="glyphicon glyphicon-backward"></span>',
+      next: '<span class="glyphicon glyphicon-forward"></span>',
       left: 3,
       right: 3,
       page: 1,
