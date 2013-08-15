fae8c6c89782028a901c2ce4fb093781258b956b
hkernbach <h.kernbach@triagens.de>
2013-08-15 18:28:43 +0200

    css

--- html/admin/js/views/dashboardView.js
+++ html/admin/js/views/dashboardView.js
@@ -86,7 +86,7 @@ var dashboardView = Backbone.View.extend({
   template: new EJS({url: 'js/templates/dashboardView.ejs'}),
 
   toggleEvent: function () {
-    $('#dashboardDropdown').slideToggle(70);
+    $('#dashboardDropdownOut').slideToggle(70);
   },
 
   countCollections: function() {
