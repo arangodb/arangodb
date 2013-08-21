390712377833c70da2a04f7e33778758c7db677a
hkernbach <h.kernbach@triagens.de>
2013-08-21 15:56:13 +0200

    jslint

--- html/admin/js/views/footerView.js
+++ html/admin/js/views/footerView.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true */
-/*global require, exports, Backbone, EJS, $*/
+/*global require, exports, Backbone, EJS, $, arangoHelper, window*/
 
 var footerView = Backbone.View.extend({
   el: '.footer',
