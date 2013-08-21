544afe129b5af34d09a4b7bab100d6cecfbcde45
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-08-21 12:31:26 +0200

    comment uncomment button

--- html/admin/src/mode-aql.js
+++ html/admin/src/mode-aql.js
@@ -47,7 +47,7 @@ oop.inherits(Mode, TextMode);
     this.toggleCommentLines = function(state, doc, startRow, endRow) {
         var outdent = true;
         var outentedRows = [];
-        var re = /^(\s*)--/;
+        var re = /^(\s*)\/\//;
 
         for (var i=startRow; i<= endRow; i++) {
             if (!re.test(doc.getLine(i))) {
@@ -69,7 +69,7 @@ oop.inherits(Mode, TextMode);
             }
         }
         else {
-            doc.indentRows(startRow, endRow, "--");
+            doc.indentRows(startRow, endRow, "//");
         }
     };
 
