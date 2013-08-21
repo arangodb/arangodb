d2e1e0fa49e09563ecea5442fc493450dee55ade
Heiko Kernbach <heiko@arangodb.com>
2013-08-21 15:47:25 +0200

    added server-status function

--- html/admin/js/views/footerView.js
+++ html/admin/js/views/footerView.js
@@ -3,9 +3,25 @@
 
 var footerView = Backbone.View.extend({
   el: '.footer',
+  system: {},
+  isOffline: false,
+
   initialize: function () {
+    //also server online check
+    this.updateVersion();
+  },
+
+  template: new EJS({url: 'js/templates/footerView.ejs'}),
+
+  updateVersion: function () {
     var self = this;
-    self.system = { };
+    window.setInterval(function(){
+      self.getVersion();
+    }, 10000);
+  },
+
+  getVersion: function () {
+    var self = this;
     $.ajax({
       type: "GET",
       cache: false,
@@ -14,21 +30,36 @@ var footerView = Backbone.View.extend({
       processData: false,
       async: false,
       success: function(data) {
+        if (self.isOffline === true) {
+          self.isOffline = false;
+          arangoHelper.removeNotifications();
+          window.setTimeout(function(){
+            arangoHelper.arangoNotification("Server connected");
+          }, 1000);
+        }
+
         self.system.name = data.server;
         self.system.version = data.version;
+        self.renderVersion();
+      },
+      error: function (data) {
+        self.isOffline = true;
+        arangoHelper.arangoError("Server is offline");
       }
     });
   },
 
-  template: new EJS({url: 'js/templates/footerView.ejs'}),
-
-  render: function() {
-    $(this.el).html(this.template.text);
-
-    // only fill in version if we have a version number...
+  renderVersion: function () {
     if (this.system.hasOwnProperty('name')) {
       $('.footer-right p').html(this.system.name + ' : ' + this.system.version);
     }
+  },
+
+  render: function () {
+    $(this.el).html(this.template.text);
+    this.getVersion();
+
+    // only fill in version if we have a version number...
     return this;
   }
 
