8277ac4b02ad79671688bd12626e27edc3cec3d3
Jan Steemann <jan@arangodb.com>
2013-08-29 15:25:21 +0200

    fixed version number in Windows build

--- build_win.h
+++ build_win.h
@@ -1,14 +1,14 @@
 #ifdef _WIN64
-  #define WINDOWS_ARRANGO_VERSION_NUMBER 1.3.0  
+  #define WINDOWS_ARRANGO_VERSION_NUMBER 1.3.3  
   #ifdef _DEBUG
-    #define TRIAGENS_VERSION "1.3.0 [WIN64-DEBUG ALPHA 2]"
+    #define TRIAGENS_VERSION "1.3.3 [WIN64-DEBUG]"
   #else  
-    #define TRIAGENS_VERSION "1.3.0 [WIN64-RELEASE ALPHA 2]"
+    #define TRIAGENS_VERSION "1.3.3 [WIN64-RELEASE]"
   #endif  
 #else  
   #ifdef _DEBUG
-    #define TRIAGENS_VERSION "1.3.0 [WIN32-DEBUG ALPHA 2]"
+    #define TRIAGENS_VERSION "1.3.3 [WIN32-DEBUG]"
   #else  
-    #define TRIAGENS_VERSION "1.3.0 [WIN32-RELEASE ALPHA 2]"
+    #define TRIAGENS_VERSION "1.3.3 [WIN32-RELEASE]"
   #endif  
 #endif  
