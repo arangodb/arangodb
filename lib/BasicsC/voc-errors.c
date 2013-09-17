4aee2df2f374bd5e9b6b00d5eb14d878bdea4fc2
Jan Steemann <jan@arangodb.com>
2013-09-17 15:56:24 +0200

    force usage of --upgrade

--- lib/BasicsC/voc-errors.c
+++ lib/BasicsC/voc-errors.c
@@ -53,9 +53,10 @@ void TRI_InitialiseErrorMessages (void) {
   REG_ERROR(ERROR_ARANGO_FILESYSTEM_FULL, "filesystem full");
   REG_ERROR(ERROR_ARANGO_NO_JOURNAL, "no journal");
   REG_ERROR(ERROR_ARANGO_DATAFILE_ALREADY_EXISTS, "cannot create/rename datafile because it already exists");
-  REG_ERROR(ERROR_ARANGO_DATABASE_LOCKED, "database is locked");
+  REG_ERROR(ERROR_ARANGO_DATADIR_LOCKED, "database directory is locked");
   REG_ERROR(ERROR_ARANGO_COLLECTION_DIRECTORY_ALREADY_EXISTS, "cannot create/rename collection because directory already exists");
   REG_ERROR(ERROR_ARANGO_MSYNC_FAILED, "msync failed");
+  REG_ERROR(ERROR_ARANGO_DATADIR_UNLOCKABLE, "cannot lock database directory");
   REG_ERROR(ERROR_ARANGO_CONFLICT, "conflict");
   REG_ERROR(ERROR_ARANGO_DATADIR_INVALID, "invalid database directory");
   REG_ERROR(ERROR_ARANGO_DOCUMENT_NOT_FOUND, "document not found");
@@ -86,8 +87,6 @@ void TRI_InitialiseErrorMessages (void) {
   REG_ERROR(ERROR_ARANGO_DATABASE_NOT_FOUND, "database not found");
   REG_ERROR(ERROR_ARANGO_DATABASE_NAME_INVALID, "database name invalid");
   REG_ERROR(ERROR_ARANGO_USE_SYSTEM_DATABASE, "operation only allowed in system database");
-  REG_ERROR(ERROR_ARANGO_DATADIR_LOCKED, "database directory is locked");
-  REG_ERROR(ERROR_ARANGO_DATADIR_UNLOCKABLE, "cannot lock database directory");
   REG_ERROR(ERROR_ARANGO_DATAFILE_FULL, "datafile full");
   REG_ERROR(ERROR_REPLICATION_NO_RESPONSE, "no response");
   REG_ERROR(ERROR_REPLICATION_INVALID_RESPONSE, "invalid response");
