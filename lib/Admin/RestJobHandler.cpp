09cea315134fda54977eaab77e0da21edf5c3fec
jsteemann <j.steemann@triagens.de>
2013-09-26 18:45:20 +0200

    async job management

--- lib/Admin/RestJobHandler.cpp
+++ lib/Admin/RestJobHandler.cpp
@@ -28,6 +28,8 @@
 #include "RestJobHandler.h"
 
 #include "Basics/StringUtils.h"
+#include "BasicsC/conversions.h"
+#include "BasicsC/tri-strings.h"
 #include "HttpServer/AsyncJobManager.h"
 #include "Rest/HttpRequest.h"
 #include "Rest/HttpResponse.h"
@@ -110,7 +112,7 @@ HttpHandler::status_e RestJobHandler::execute () {
   if (type == HttpRequest::HTTP_REQUEST_GET) {
     getJob();
   }
-  if (type == HttpRequest::HTTP_REQUEST_DELETE) {
+  else if (type == HttpRequest::HTTP_REQUEST_DELETE) {
     deleteJob();
   }
   else {
@@ -134,17 +136,82 @@ HttpHandler::status_e RestJobHandler::execute () {
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief returns a job result by id
+/// @brief get handler
 ////////////////////////////////////////////////////////////////////////////////
 
 void RestJobHandler::getJob () {
   const vector<string> suffix = _request->suffix();
-
-  if (suffix.size() != 1) {
+    
+  if (suffix.size() > 1) {
     generateError(HttpResponse::BAD, TRI_ERROR_HTTP_BAD_PARAMETER);
     return;
   }
+
+  if (suffix.size() == 0) {
+    return getJobList();
+  }
+
+  return getJobSingle();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief returns the list of jobs (by type)
+////////////////////////////////////////////////////////////////////////////////
+
+void RestJobHandler::getJobList () {
+  size_t maxCount = 100;
+
+  // extract "count" parameter
+  bool found;
+  char const* value = _request->value("count", found);
+
+  if (found) {
+    maxCount = StringUtils::uint64(value);
+  }
+
+  // extract "type" parameter
+  char const* type = _request->value("type", found);
+
+  if (! found) {
+    generateError(HttpResponse::BAD, TRI_ERROR_HTTP_BAD_PARAMETER);
+    return;
+  }
+
+  vector<AsyncJobResult::IdType> ids;
+  if (TRI_EqualString(type, "done")) {
+    ids = _jobManager->done(maxCount);
+  }
+  else if (TRI_EqualString(type, "pending")) {
+    ids = _jobManager->pending(maxCount);
+  }
+  else {
+    generateError(HttpResponse::BAD, TRI_ERROR_HTTP_BAD_PARAMETER);
+    return;
+  }
+
+  TRI_json_t* json = TRI_CreateList2Json(TRI_CORE_MEM_ZONE, ids.size());
+
+  if (json != 0) {
+    for (size_t i = 0, n = ids.size(); i < n; ++i) {
+      char* idString = TRI_StringUInt64(ids[i]);
+
+      TRI_PushBack3ListJson(TRI_CORE_MEM_ZONE, json, TRI_CreateStringJson(TRI_CORE_MEM_ZONE, idString));
+    }
   
+    generateResult(json);
+    TRI_FreeJson(TRI_CORE_MEM_ZONE, json);
+  }
+  else {
+    generateError(HttpResponse::SERVER_ERROR, TRI_ERROR_OUT_OF_MEMORY);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief returns information about a single job (by job id)
+////////////////////////////////////////////////////////////////////////////////
+
+void RestJobHandler::getJobSingle () {
+  const vector<string> suffix = _request->suffix();
   const string& value = suffix[0];
   uint64_t jobId = StringUtils::uint64(value);
 
@@ -199,10 +266,13 @@ void RestJobHandler::deleteJob () {
 
   if (json != 0) {
     TRI_Insert3ArrayJson(TRI_CORE_MEM_ZONE, json, "result", TRI_CreateBooleanJson(TRI_CORE_MEM_ZONE, true));
+  
+    generateResult(json);
+    TRI_FreeJson(TRI_CORE_MEM_ZONE, json);
   }
-
-  generateResult(json);
-  TRI_FreeJson(TRI_CORE_MEM_ZONE, json);
+  else {
+    generateError(HttpResponse::SERVER_ERROR, TRI_ERROR_OUT_OF_MEMORY);
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
