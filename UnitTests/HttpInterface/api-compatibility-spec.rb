1a7bbb70585812e4c79592df43f44eea0253b0f8
Frank Celler <frank@arangodb.com>
2013-10-29 15:24:38 +0100

    new version 1.5

--- UnitTests/HttpInterface/api-compatibility-spec.rb
+++ UnitTests/HttpInterface/api-compatibility-spec.rb
@@ -17,7 +17,7 @@ describe ArangoDB do
       doc.code.should eq(200)
       compatibility = doc.parsed_response['compatibility']
       compatibility.should be_kind_of(Integer)
-      compatibility.should eq(10400)
+      compatibility.should eq(10500)
     end
     
     it "tests the compatibility value when a broken header is set" do
@@ -29,7 +29,7 @@ describe ArangoDB do
         doc.code.should eq(200)
         compatibility = doc.parsed_response['compatibility']
         compatibility.should be_kind_of(Integer)
-        compatibility.should eq(10400)
+        compatibility.should eq(10500)
       end
     end
     
@@ -59,6 +59,19 @@ describe ArangoDB do
       end
     end
     
+    it "tests the compatibility value when a valid header is set" do
+      versions = [ "1.5.0", "1.5.1", "1.5.2", "1.5.0-devel", "1.5.0-beta2", "   1.5", "1.5  ", " 1.5.0", "  1.5.0  ", "10500", "10501", "10599" ]
+
+      versions.each do|value|
+        doc = ArangoDB.get("/_admin/echo", :headers => { "x-arango-version" => value })
+
+        doc.code.should eq(200)
+        compatibility = doc.parsed_response['compatibility']
+        compatibility.should be_kind_of(Integer)
+        compatibility.should eq(10500)
+      end
+    end
+
     it "tests the compatibility value when a too low version is set" do
       versions = [ "0.0", "0.1", "0.2", "0.9", "1.0", "1.1", "1.2" ]
 
