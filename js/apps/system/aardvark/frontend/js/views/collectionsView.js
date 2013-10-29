a85dce82ee226e2f8ff4ec10235618f875640012
Jan Steemann <jan@arangodb.com>
2013-10-29 01:34:20 +0100

    issue #634: Web interface bug: Escape does not always propagate

--- js/apps/system/aardvark/frontend/js/views/collectionsView.js
+++ js/apps/system/aardvark/frontend/js/views/collectionsView.js
@@ -41,17 +41,8 @@ var collectionsView = Backbone.View.extend({
     }, this);
 
     //append info icon for loaded collections
-    /*
     $('.loaded').parent().prev().append(
-      '<i class="icon-info-sign show-info-view" alt="Show collection properties"'+
-      'title="Show collection properties"></i>'
-    );
-    $('.unloaded').parent().prev().append(
-      '<i class="icon-info-sign disabled-info-view" alt="disabled"'+
-      'title="disabled"></i>'
-    );*/
-    $('.loaded').parent().prev().append(
-      '<span class="glyphicon glyphicon-info-sign spanInfo ICON" alt="Collection properties"</span>'
+      '<span class="glyphicon glyphicon-info-sign spanInfo ICON" title="Show collection properties"</span>'
     );
     $('.unloaded').parent().prev().append(
       '<span class="glyphicon glyphicon-info-sign spanDisabled ICON" alt="disabled"</span>'
@@ -63,7 +54,7 @@ var collectionsView = Backbone.View.extend({
     $('#searchInput').val('');
     $('#searchInput').val(val);
 
-    arangoHelper.fixTooltips(".glyphicon, .arangoicon", "top");
+    arangoHelper.fixTooltips(".glyphicon, .arangoicon", "left");
 
     return this;
   },
