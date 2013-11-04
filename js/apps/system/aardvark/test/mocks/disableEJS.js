60026f40f27a2dd3ae269f9375edf45f47e4efef
Michael Hackstein <michael@arangodb.com>
2013-11-04 16:31:20 +0100

    Removed debug console.log in tests

--- js/apps/system/aardvark/test/mocks/disableEJS.js
+++ js/apps/system/aardvark/test/mocks/disableEJS.js
@@ -3,11 +3,6 @@
     var prefix = "base/frontend/js/templates/";
     var exports = {};
     exports.createTemplate = function(path) {
-      _.each(window.__karma__.files, function(v,k) {
-        if (k.indexOf(path, k.length - path.length) !== -1) {
-          console.log(v);
-        }
-      });
       var param = {
         url: prefix + path
       };
