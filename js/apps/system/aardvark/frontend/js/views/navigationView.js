35ba2136c0c98ec586489d92f372ddf2f8dc224e
mchacki <m.hackstein@triagens.de>
2013-11-05 16:21:17 +0100

    Added selection box in Admin interface to switch between databases. The user will only see databases where he has access to

--- js/apps/system/aardvark/frontend/js/views/navigationView.js
+++ js/apps/system/aardvark/frontend/js/views/navigationView.js
@@ -5,14 +5,24 @@ var navigationView = Backbone.View.extend({
   el: '.header',
   initialize: function () {
     var self = this;
+    this.dbSelectionView = new window.DBSelectionView({
+      collection: arangoDatabase
+    });
   },
 
   template: templateEngine.createTemplate("navigationView.ejs"),
 
   render: function() {
-    $(this.el).html(this.template.text);
+    $(this.el).html(this.template.render({}));
     this.handleSelectNavigation();
+    this.dbSelectionView.render($("#selectDB2"));
     return this;
+  },
+
+  handleResize: function (margin) {
+    $('.arango-logo').css('margin-left', margin - 17);
+    $("#selectDB2").css('margin-left', margin - 17);
+    $('.nav-collapse').css('margin-right', margin - 10);
   },
 
   handleSelectNavigation: function () {
