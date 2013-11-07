ad3a9510fff6674c3bad00297b96d134f8f9ac8c
mchacki <m.hackstein@triagens.de>
2013-11-07 17:09:50 +0100

    JSLint fixes

--- js/apps/system/aardvark/frontend/js/views/dbSelectionView.js
+++ js/apps/system/aardvark/frontend/js/views/dbSelectionView.js
@@ -1,38 +1,38 @@
-window.DBSelectionView = Backbone.View.extend({
-  template: templateEngine.createTemplate("dbSelectionView.ejs"),
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true */
+/*global templateEngine, window, Backbone, $*/
+(function() {
+  "use strict";
+  window.DBSelectionView = Backbone.View.extend({
+    template: templateEngine.createTemplate("dbSelectionView.ejs"),
 
-  events: {
-    "change #dbSelectionList": "changeDatabase"
-  },
+    events: {
+      "change #dbSelectionList": "changeDatabase"
+    },
 
-  initialize: function() {
-                var self = this;
-    this.collection.fetch({
-      success: function(){
-        self.render(self.$el);
-       } 
-    });
-    this.current = this.collection.getCurrentDatabase();
-  },
+    initialize: function() {
+                  var self = this;
+      this.collection.fetch({
+        success: function(){
+          self.render(self.$el);
+         } 
+      });
+      this.current = this.collection.getCurrentDatabase();
+    },
 
-  changeDatabase: function(e) {
-    var changeTo = $("#dbSelectionList > option:selected").attr("id");
-    var url = this.collection.createDatabaseURL(changeTo);
-    location.replace(url);
-    /*
-    var dbname = $(e.currentTarget).text();
-    var route =  '/_db/' + encodeURIComponent(dbname) + '/_admin/aardvark/index.html#databases';
-    window.location = "http://"+window.location.host + route;
-    */
-  },
+    changeDatabase: function(e) {
+      var changeTo = $("#dbSelectionList > option:selected").attr("id");
+      var url = this.collection.createDatabaseURL(changeTo);
+      window.location.replace(url);
+    },
 
-  render: function(el) {
-    this.$el = el;
-    el.html(this.template.render({
-      list: this.collection,
-      current: this.current
-    }));
-    this.delegateEvents();
-    return el;
-  }
-});
+    render: function(el) {
+      this.$el = el;
+      el.html(this.template.render({
+        list: this.collection,
+        current: this.current
+      }));
+      this.delegateEvents();
+      return el;
+    }
+  });
+}());
