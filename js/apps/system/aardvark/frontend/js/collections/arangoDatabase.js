90c7f75abba6e2bbf9dd650ea9fb988badaad520
jsteemann <j.steemann@triagens.de>
2013-11-07 18:49:54 +0100

    added missing web interface files

---
+++ js/apps/system/aardvark/frontend/js/collections/arangoDatabase.js
@@ -0,0 +1,94 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true */
+/*global window, Backbone, $, window, _*/
+
+(function() {
+  window.ArangoDatabase = Backbone.Collection.extend({
+    model: window.Database,
+
+    comparator: "name",
+
+    sync: function(method, model, options) {
+      'use strict';
+      if (method === "read") {
+        options.url = model.url() + "user";
+      }
+      return Backbone.sync(method, model, options);
+    },
+
+    url: function() {
+      return '/_api/database/';
+    },
+
+    parse: function(response) {
+      return _.map(response.result, function(v) {
+        return {name:v};
+      });
+    },
+
+    initialize: function() {
+      var self = this; 
+      this.fetch().done(function() {
+        self.sort();  
+      });
+    },
+
+    getDatabases: function() {
+      var self = this; 
+      this.fetch().done(function() {
+        self.sort();  
+      });
+      return this.models;
+    },
+
+    createDatabaseURL: function(name, protocol, port) {
+      var loc = window.location;
+      var hash = window.location.hash;
+      if (protocol) {
+        if (protocol === "SSL" || protocol === "https:") {
+          protocol = "https:";
+        } else {
+          protocol = "http:"; 
+        }
+      } else {
+        protocol = loc.protocol;
+      }
+      port = port || loc.port;
+
+      var url = protocol
+        + "//"
+        + window.location.hostname
+        + ":"
+        + port
+        + "/_db/"
+        + encodeURIComponent(name)
+        + "/_admin/aardvark/index.html";
+      if (hash) {
+        url += hash;
+      }
+      return url;
+    },
+
+    getCurrentDatabase: function() {
+      var returnVal;
+      $.ajax({
+        type: "GET",
+        cache: false,
+        url: "/_api/database/current",
+        contentType: "application/json",
+        processData: false,
+        async: false,
+        success: function(data) {
+          if (data.code === 200) {
+            returnVal = data.result.name;
+            return;
+          }
+          returnVal = data;
+        },
+        error: function(data) {
+          returnVal = data;
+        }
+      });
+      return returnVal;
+    }
+  });
+}());
---
+++ js/apps/system/aardvark/frontend/js/views/databaseView.js
@@ -0,0 +1,164 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true */
+/*global window, document, Backbone, EJS, SwaggerUi, hljs, $, arangoHelper */
+
+window.databaseView = Backbone.View.extend({
+  el: '#content',
+
+  template: new EJS({url: 'js/templates/databaseView.ejs'}),
+
+  currentDB: "",
+
+  events: {
+    "click #createDatabase"       : "createDatabase",
+    "click #submitCreateDatabase" : "submitCreateDatabase",
+    "click #selectDatabase"       : "updateDatabase",
+    "click #databaseTable .glyphicon-minus-sign" : "removeDatabase",
+    "click #submitDeleteDatabase" : "submitRemoveDatabase",
+    "click .databaseInactive a" : "changeDatabase"
+  },
+
+  initialize: function() {
+    var self = this;
+    this.collection.fetch({async:false});
+  },
+
+  render: function(){
+    this.currentDatabase();
+    $(this.el).html(this.template.render({}));
+    this.renderTable();
+    this.selectCurrentDatabase();
+    return this;
+  },
+
+  renderTable: function () {
+    this.collection.map(function(dbs) {
+      $("#databaseTable tbody").append(
+        '<tr><td><a>' + dbs.get("name") + '</a></td>' +
+        '<td><span class="glyphicon glyphicon-minus-sign"' + 
+        'data-original-title="Delete database"></span></td></tr>'
+      );
+    });
+  },
+
+  selectedDatabase: function () {
+    return $('#selectDatabases').val();
+  },
+
+  handleError: function(status, text, dbname) {
+    if (status === 409) {
+      arangoHelper.arangoError("Database " + dbname + " already exists.");
+      return;
+    }
+    if (status === 400) {
+      arangoHelper.arangoError("Invalid Parameters");
+      return;
+    }
+    if (status === 403) {
+      arangoHelper.arangoError("Insufficent rights. Execute this from _system database");
+      return;
+    }
+  },
+
+  validateDatabaseInfo: function (db, user, pw) {
+    if (user === "") {
+      arangoHelper.arangoError("You have to define an owner for the new database");
+      return false;
+    }
+    if (db === "") {
+      arangoHelper.arangoError("You have to define a name for the new database"); 
+      return false;
+    }
+    if (db.indexOf("_") === 0) {
+      arangoHelper.arangoError("Databasename should not start with _");
+      return false;
+    }
+    if (!db.match(/^[a-zA-Z][a-zA-Z0-9_\-]*$/)) {
+      arangoHelper.arangoError("Databasename may only contain numbers, letters, _ and -");
+      return false;
+    }
+    return true;
+  },
+
+  submitCreateDatabase: function() {
+    var self = this;
+    var name  = $('#newDatabaseName').val();
+    var userName = $('#newUser').val();
+    var userPassword = $('#newPassword').val();
+    if (!this.validateDatabaseInfo(name, userName, userPassword)) {
+      return;
+    }
+    var options = {
+      name: name,
+      users: [
+        {
+          username: userName,
+          passwd: userPassword,
+          active: true
+        }
+      ]
+    };
+    this.collection.create(options, {
+      wait:true,
+      error: function(data, err) {
+        self.handleError(err.status, err.statusText, name);
+      },
+      success: function(data) {
+        arangoHelper.arangoNotification("Database " + name + " created.");
+        self.hideModal();
+        self.updateDatabases();
+      }
+    });
+  },
+
+  hideModal: function() {
+    $('#createDatabaseModal').modal('hide');
+  },
+
+  showModal: function() {
+    $('#createDatabaseModal').modal('show');
+  },
+
+  createDatabase: function() {
+    this.showModal();
+  },
+
+  submitRemoveDatabase: function(e) {
+    var toDelete = this.collection.where({name: this.dbToDelete});
+    toDelete[0].destroy({wait: true, url:"/_api/database/"+this.dbToDelete});
+    arangoHelper.arangoNotification("Database " + this.dbToDelete + " deleted.");
+    this.dbToDelete = '';
+    $('#deleteDatabaseModal').modal('hide');
+    this.updateDatabases();
+  },
+
+  removeDatabase: function(e) {
+    this.dbToDelete = $(e.currentTarget).parent().parent().children().first().text();
+    $('#deleteDatabaseModal').modal('show');
+  },
+
+  currentDatabase: function() {
+    this.currentDB = this.collection.getCurrentDatabase();
+  },
+
+  selectCurrentDatabase: function() {
+    $('#databaseTableBody tr').addClass('databaseInactive');
+    var tr = $('#databaseTableBody td:contains('+this.currentDB+')').parent();
+    $(tr).removeClass('databaseInactive').addClass('databaseActive');
+  },
+
+  changeDatabase: function(e) {
+    var dbname = $(e.currentTarget).text();
+    var route =  '/_db/' + encodeURIComponent(dbname) + '/_admin/aardvark/index.html#databases';
+    window.location = "http://"+window.location.host + route;
+  },
+
+  updateDatabases: function() {
+    var self = this;
+    this.collection.fetch({
+      success: function() {
+        self.render();
+      }
+    });
+  }
+
+});
