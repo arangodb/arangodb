eab4480bf77d4adcd6440d2cbebba2dfe2e95aef
mchacki <m.hackstein@triagens.de>
2013-11-12 16:14:57 +0100

    Fixed jslint warning for sync, (we are nt syncing but JSLint matches sync in parameternames

--- js/apps/system/aardvark/frontend/js/views/collectionView.js
+++ js/apps/system/aardvark/frontend/js/views/collectionView.js
@@ -1,4 +1,4 @@
-/*jslint indent: 2, nomen: true, maxlen: 100, vars: true, white: true, plusplus: true */
+/*jslint indent: 2, nomen: true, maxlen: 100, vars: true, white: true, plusplus: true, stupid: true */
 /*global require, window, exports, Backbone, $, arangoHelper, templateEngine */
 (function() {
   "use strict";
