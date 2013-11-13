9f340c10d5952ab10fa763d70db716e591e9dc36
Jan Steemann <jan@arangodb.com>
2013-11-13 23:18:43 +0100

    update database selector after adding/dropping databases

--- js/apps/system/aardvark/frontend/js/views/databaseView.js
+++ js/apps/system/aardvark/frontend/js/views/databaseView.js
@@ -157,6 +157,14 @@ window.databaseView = Backbone.View.extend({
     this.collection.fetch({
       success: function() {
         self.render();
+        var select = $("#dbSelectionList");
+        select.empty();
+        self.collection.toJSON().forEach(function(item) {
+          select.append($("<option></option>")
+            .attr("value", item.name)
+            .text(item.name)
+            .attr("selected", item.name === this.currentDB));
+        });
       }
     });
   }
