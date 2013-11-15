e37eab270aad3b60b8381ce73e455761d44d2250
mchacki <m.hackstein@triagens.de>
2013-11-16 00:43:37 +0100

    DB Selection view is now managed by the router and not managed by navigationView any more

--- js/apps/system/aardvark/test/specs/views/dbSelectionViewSpec.js
+++ js/apps/system/aardvark/test/specs/views/dbSelectionViewSpec.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, white: true  plusplus: true, browser: true*/
-/*global describe, beforeEach, afterEach, it, spyOn, expect*/
+/*global describe, beforeEach, afterEach, it, spyOn, expect, runs, waitsFor*/
 /*global _, $, DBSelectionView*/
 
 (function() {
@@ -10,24 +10,27 @@
     var view,
       list,
       dbCollection,
+      fetched,
       current,
       div;
 
     beforeEach(function() {
       dbCollection = new window.ArangoDatabase();
       list = ["_system", "second", "first"];
-      current = "first";
+      current = new window.CurrentDatabase({
+        name: "first",
+        isSystem: false
+      });
       _.each(list, function(n) {
         dbCollection.add({name: n});
       });
-      spyOn(dbCollection, "fetch");
-      spyOn(dbCollection, "getCurrentDatabase").andCallFake(
-        function() {
-          return current;
-        }
-      );
+      fetched = false;
+      spyOn(dbCollection, "fetch").andCallFake(function(opt) {
+        fetched = true;
+        opt.success();
+      });
       div = document.createElement("div");
-      div.id = "selectDB";
+      div.id = "dbSelect";
       document.body.appendChild(div);
     });
 
@@ -36,50 +39,114 @@
     });
 
     it("should display all databases ordered", function() {
-      view = new DBSelectionView({collection: dbCollection});
-      view.render($("#selectDB"));
-      var select = $(div).children()[0],
-        childs;
-      expect(div.childElementCount).toEqual(1);
-      childs = $(select).children();
-      expect(childs.length).toEqual(3);
-      expect(childs[0].id).toEqual(list[0]);
-      expect(childs[1].id).toEqual(list[2]);
-      expect(childs[2].id).toEqual(list[1]);
+
+      runs(function() {
+        view = new DBSelectionView(
+          {
+            collection: dbCollection,
+            current: current
+          }
+        );
+      });
+
+      waitsFor(function() {
+        return fetched;
+      }, 1000);
+
+      runs(function() {
+        var select = $(div).children()[0],
+          childs;
+        expect(div.childElementCount).toEqual(1);
+        childs = $(select).children();
+        expect(childs.length).toEqual(3);
+        expect(childs[0].id).toEqual(list[0]);
+        expect(childs[1].id).toEqual(list[2]);
+        expect(childs[2].id).toEqual(list[1]);
+      });
     });
 
     it("should select the current db", function() {
-      view = new DBSelectionView({collection: dbCollection});
-      view.render($("#selectDB"));
-      expect($(div).find(":selected").attr("id")).toEqual(current);
+      runs(function() {
+        view = new DBSelectionView(
+          {
+            collection: dbCollection,
+            current: current
+          }
+        );
+      });
+
+      waitsFor(function() {
+        return fetched;
+      }, 1000);
+
+      runs(function() {
+        expect($(div).find(":selected").attr("id")).toEqual(current.get("name"));
+      });
     });
 
     it("should trigger fetch on collection", function() {
-      view = new DBSelectionView({collection: dbCollection});
-      expect(dbCollection.fetch).toHaveBeenCalled();
+      runs(function() {
+        view = new DBSelectionView(
+          {
+            collection: dbCollection,
+            current: current
+          }
+        );
+      });
+
+      waitsFor(function() {
+        return fetched;
+      }, 1000);
+
+      runs(function() {
+        expect(dbCollection.fetch).toHaveBeenCalled();
+      });
     });
 
     it("should trigger a database switch on click", function() {
-      view = new DBSelectionView({collection: dbCollection});
-      view.render($("#selectDB"));
-      spyOn(dbCollection, "createDatabaseURL").andReturn("switchURL");
-      spyOn(location, "replace");
-      $("#dbSelectionList").val("second").trigger("change");
-      expect(dbCollection.createDatabaseURL).toHaveBeenCalledWith("second");
-      expect(location.replace).toHaveBeenCalledWith("switchURL");
+      runs(function() {
+        view = new DBSelectionView(
+          {
+            collection: dbCollection,
+            current: current
+          }
+        );
+      });
+      
+      waitsFor(function() {
+        return fetched;
+      }, 1000);
+
+      runs(function() {
+        spyOn(dbCollection, "createDatabaseURL").andReturn("switchURL");
+        spyOn(location, "replace");
+        $("#dbSelectionList").val("second").trigger("change");
+        expect(dbCollection.createDatabaseURL).toHaveBeenCalledWith("second");
+        expect(location.replace).toHaveBeenCalledWith("switchURL");
+      });
     });
 
     it("should not render if the list has only one element", function() {
-      var oneCollection = new window.ArangoDatabase();
-      oneCollection.add({name: current});
-      spyOn(oneCollection, "getCurrentDatabase").andCallFake(
-        function() {
-          return current;
-        }
-      );
-      view = new DBSelectionView({collection: oneCollection});
-      view.render($("#selectDB"));
-      expect($(div).text().trim()).toEqual("");
+      runs(function() {
+        var oneCollection = new window.ArangoDatabase();
+        oneCollection.add({name: current});
+        spyOn(oneCollection, "fetch").andCallFake(function(opts) {
+          fetched = true;
+          opts.success();
+        });
+        view = new DBSelectionView({
+          collection: oneCollection,
+          current: current
+        });
+      });
+
+      waitsFor(function() {
+        return fetched;
+      }, 1000);
+
+      runs(function() {
+        expect($(div).text().trim()).toEqual("");
+      });
     });
   });
 }());
