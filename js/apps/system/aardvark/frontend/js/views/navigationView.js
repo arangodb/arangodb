71a8db7a0c177ff78f9a555513b31802aed1d1fe
Michael Hackstein <michael@arangodb.com>
2013-11-16 17:37:44 +0100

    JSLint cleanup

--- js/apps/system/aardvark/frontend/js/views/navigationView.js
+++ js/apps/system/aardvark/frontend/js/views/navigationView.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, vars: true, white: true, plusplus: true */
-/*global require, exports, Backbone, templateEngine, $, window, arangoDatabase*/
+/*global Backbone, templateEngine, $, window*/
 (function() {
   "use strict";
   window.NavigationView = Backbone.View.extend({
