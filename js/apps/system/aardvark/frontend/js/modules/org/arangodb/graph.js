97b829b9c8ce9a992f50960f7da8b4db46bbbbec
jsteemann <j.steemann@triagens.de>
2013-11-25 10:22:36 +0100

    issue #671: make other graph methods work client-side, too

--- js/apps/system/aardvark/frontend/js/modules/org/arangodb/graph.js
+++ js/apps/system/aardvark/frontend/js/modules/org/arangodb/graph.js
@@ -217,6 +217,14 @@ Graph.prototype.initialize = function (name, vertices, edges) {
   this._properties = results.graph;
   this._vertices = arangodb.db._collection(this._properties.vertices);
   this._edges = arangodb.db._collection(this._properties.edges);
+  
+  // and dictionary for vertices and edges
+  this._verticesCache = {};
+  this._edgesCache = {};
+  
+  // and store the cashes
+  this.predecessors = {};
+  this.distances = {};
 
   return this;
 };
@@ -304,6 +312,7 @@ Graph.prototype.getVertices = function () {
     wrapper = function(object) {
       return new Vertex(graph, object);
     };
+  return new Iterator(wrapper, cursor, "[vertex iterator]");
 };
 
 ////////////////////////////////////////////////////////////////////////////////
