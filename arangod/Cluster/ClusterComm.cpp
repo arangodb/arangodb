e4cebe053642ca8373b8b99d173f53f61e98c908
Max Neunhoeffer <max@arangodb.com>
2013-12-11 16:48:43 +0100

    Very first incomplete draft of ClusterComm.
    Note that this is not yet in the Makefiles, but it at least compiles
    if one puts it there.

---
+++ arangod/Cluster/ClusterComm.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Library for intra-cluster communications
+///
+/// @file ClusterComm.cpp
+///
+/// DISCLAIMER
+///
+/// Copyright 2010-2013 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Max Neunhoeffer
+/// @author Copyright 2013, triagens GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+#include <arangod/Cluster/ClusterComm.h>
+
+using namespace triagens::arango;
+
+// -----------------------------------------------------------------------------
+// --SECTION--
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief
+////////////////////////////////////////////////////////////////////////////////
+
+// Local Variables:
+// mode: outline-minor
+// outline-regexp: "^\\(/// @brief\\|/// {@inheritDoc}\\|/// @addtogroup\\|// --SECTION--\\|/// @\\}\\)"
+// End:
+
+
