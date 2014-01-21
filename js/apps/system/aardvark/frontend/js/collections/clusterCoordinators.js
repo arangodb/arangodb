d78e3ed92bdb465bfa3c8242ab1a58d1434a55c2
Michael Hackstein <michael@arangodb.com>
2014-01-21 16:48:33 +0100

    Added tests for cluster views, of necessary information is passed through each view

--- js/apps/system/aardvark/frontend/js/collections/clusterCoordinators.js
+++ js/apps/system/aardvark/frontend/js/collections/clusterCoordinators.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, white: true, plusplus: true */
-/*global window, Backbone */
+/*global window, Backbone, console */
 (function() {
   "use strict";
   window.ClusterCoordinators = Backbone.Collection.extend({
