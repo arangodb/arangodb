370eaccd00dbc677cdca89d3abcd11050faeb066
jsteemann <j.steemann@triagens.de>
2014-01-27 12:15:34 +0100

    updated documentation

--- arangod/RestHandler/RestDocumentHandler.cpp
+++ arangod/RestHandler/RestDocumentHandler.cpp
@@ -744,6 +744,12 @@ bool RestDocumentHandler::readAllDocuments () {
 /// 
 /// @RESTHEADERPARAMETERS
 ///
+/// @RESTHEADERPARAM{If-None-Match,string,optional}
+/// If the "If-None-Match" header is given, then it must contain exactly one
+/// etag. If the current document revision is different to the specified etag,
+/// an `HTTP 200` response is returned. If the current document revision is 
+/// identical to the specified etag, then an `HTTP 304` is returned.
+///
 /// @RESTHEADERPARAM{If-Match,string,optional}
 /// You can conditionally fetch a document based on a target revision id by
 /// using the `if-match` HTTP header.
@@ -758,12 +764,12 @@ bool RestDocumentHandler::readAllDocuments () {
 /// @RESTRETURNCODE{200}
 /// is returned if the document was found
 ///
-/// @RESTRETURNCODE{404}
-/// is returned if the document or collection was not found
-///
 /// @RESTRETURNCODE{304}
 /// is returned if the "If-None-Match" header is given and the document has
 /// same version
+///
+/// @RESTRETURNCODE{404}
+/// is returned if the document or collection was not found
 ///
 /// @RESTRETURNCODE{412}
 /// is returned if a "If-Match" header or `rev` is given and the found
