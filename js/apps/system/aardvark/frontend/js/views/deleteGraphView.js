54346784ca3f642f4dde9984b221770c9482c151
Michael Hackstein <michael@arangodb.com>
2014-01-30 16:56:31 +0100

    Added confirm dialog to graph deletion

---
+++ js/apps/system/aardvark/frontend/js/views/deleteGraphView.js
@@ -0,0 +1,59 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, vars: true, white: true, plusplus: true*/
+/*global Backbone, $, _, window, templateEngine, arangoHelper*/
+
+(function() {
+  "use strict";
+
+  window.DeleteGraphView = Backbone.View.extend({
+
+    el: '#modalPlaceholder',
+
+    template: templateEngine.createTemplate("deleteGraphView.ejs"),
+
+    events: {
+      "click #cancel": "hide",
+      "hidden": "hidden",
+      "click #confirmDelete": "deleteGraph"
+    },
+
+    deleteGraph: function() {
+      var self = this;
+      this.collection.get(this.name).destroy({
+        success: function() {
+          self.hide();
+        },
+        error: function() {
+          var response = JSON.parse(err.responseText),
+            msg = response.errorMessage;
+          arangoHelper.arangoError(msg);
+          self.hide();
+        }
+      });
+    },
+
+    hide: function() {
+      $('#delete-graph').modal('hide');
+    },
+
+    hidden: function () {
+      this.undelegateEvents();
+      window.App.navigate("graphManagement", {trigger: true});
+    },
+
+    render: function(name) {
+      if (!name) {
+        window.App.navigate("graphManagement", {trigger: true});
+      }
+      this.name = name;
+      $(this.el).html(this.template.render({
+        name: name
+      }));
+      this.delegateEvents();
+      $('#delete-graph').modal('show');
+      return this;
+    }
+
+  });
+
+}());
+
