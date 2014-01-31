47d77aa6ef33c76ae557fa77881f445d33f2f5d0
mchacki <m.hackstein@triagens.de>
2014-01-31 21:41:40 +0100

    Made unexpanded vertices semi-transparent, i am not yet happy with this

--- js/apps/system/aardvark/frontend/js/graphViewer/ui/modalDialogHelper.js
+++ js/apps/system/aardvark/frontend/js/graphViewer/ui/modalDialogHelper.js
@@ -451,7 +451,6 @@ var modalDialogHelper = modalDialogHelper || {};
   
   modalDialogHelper.createModalChangeDialog = function(title, idprefix, objects, callback) {
     var table =  modalDialogHelper.modalDivTemplate(title, "Change", idprefix, callback);
-    console.log(objects);
     _.each(objects, function(o) {
       insertModalRow(table, idprefix, o);
     });
