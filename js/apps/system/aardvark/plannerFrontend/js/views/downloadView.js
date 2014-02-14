4f3fac44559f598488e1cbcdb289acc2c9e1ddbc
mchacki <m.hackstein@triagens.de>
2014-02-14 09:37:12 +0100

    Added download for the startup configuration  and implemented the start of the cluster

---
+++ js/apps/system/aardvark/plannerFrontend/js/views/downloadView.js
@@ -0,0 +1,30 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true, newcap: true */
+/*global window, $, Backbone, plannerTemplateEngine, alert */
+
+(function() {
+  "use strict";
+  
+  window.DownloadView = Backbone.View.extend({
+    el: "#content",
+    template: plannerTemplateEngine.createTemplate("download.ejs"),
+
+    events: {
+      "click #startDownload": "download"
+    },
+
+    download: function() {
+      window.open(this.content, "clusterConfiguration.json");
+    },
+
+    render: function(content) {
+      this.content = "data:application/octet-stream," + encodeURIComponent(JSON.stringify(content, 2));
+      var toShow = _.findWhere(content.runInfo.runInfo, {isStartServers: true});
+      $(this.el).html(this.template.render({
+        endpoints: toShow.endpoints,
+        roles: toShow.roles
+      }));
+    }
+  });
+
+}());
+
