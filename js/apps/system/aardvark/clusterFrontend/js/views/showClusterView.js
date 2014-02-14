631ff08dcc96d98716fe5d95e9dfac3a1eb13e57
scottashton <f.bartels@triagens.de>
2014-02-14 16:33:33 +0100

    clusterFrontend

--- js/apps/system/aardvark/clusterFrontend/js/views/showClusterView.js
+++ js/apps/system/aardvark/clusterFrontend/js/views/showClusterView.js
@@ -8,11 +8,35 @@
     el: "#content",
     template: clusterTemplateEngine.createTemplate("showCluster.ejs"),
 
-    events: {
-      "click #startPlan": "startPlan"
+    initialize: function() {
+        this.dbservers = new window.ClusterServers();
+        this.dbservers.fetch({
+            async : false
+        });
+        this.coordinators = new window.ClusterCoordinators();
+        this.coordinators.fetch({
+            async : false
+        });
+        this.data = [];
+        if (this.statisticsDescription === undefined) {
+            this.statisticsDescription = new window.StatisticsDescription();
+            this.statisticsDescription.fetch({
+                async:false
+            });
+        }
+        var self = this;
+        this.dbservers.forEach(function (server) {
+            var statCollect  = new window.StatisticsCollection();
+            statCollect.url = server.get("address").replace("tcp", "http") + "/_admin/statistics";
+            self.data.push(statCollect);
+            statCollect.fetch();
+        });
+
     },
 
     render: function() {
+      console.log(this.data);
+      console.log(this.coordinators.getOverview());
       $(this.el).html(this.template.render({}));
     }
   });
