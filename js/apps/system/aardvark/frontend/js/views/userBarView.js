79ceeed1d53756aafeb882c69fe37a26044f102d
gschwab <g.schwab@triagens.de>
2014-02-17 08:57:17 +0100

    added user and statistic view (fakey and inactive)

--- js/apps/system/aardvark/frontend/js/views/userBarView.js
+++ js/apps/system/aardvark/frontend/js/views/userBarView.js
@@ -1,21 +1,70 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, vars: true, white: true, plusplus: true */
 /*global Backbone, templateEngine, $, window*/
 (function () {
-    "use strict";
+  "use strict";
 
-    window.UserBarView = Backbone.View.extend({
-        el: '#statisticBar',
+  window.UserBarView = Backbone.View.extend({
 
-        events: {
+    events: {
+      "change #userBarSelect": "navigateBySelect",
+      "click .tab": "navigateByTab",
+      "mouseenter .dropdown": "showDropdown",
+      "mouseleave .dropdown": "hideDropdown"
+    },
 
-        },
+    template: templateEngine.createTemplate("userBarView.ejs"),
 
-        template: templateEngine.createTemplate("userBarView.ejs"),
+    navigateBySelect: function () {
+      var navigateTo = $("#arangoCollectionSelect").find("option:selected").val();
+      window.App.navigate(navigateTo, {trigger: true});
+    },
 
-        render: function() {
-            $(this.el).html(this.template.render(this.template.text));
-            return this;
+    navigateByTab: function (e) {
+      var tab = e.target || e.srcElement;
+      tab = $(tab).closest("a");
+      var navigateTo = tab.attr("id");
+      if (navigateTo === "user") {
+        $("#user_dropdown").slideToggle(200);
+        e.preventDefault();
+        return;
+      }
+      window.App.navigate(navigateTo, {trigger: true});
+      e.preventDefault();
+    },
 
-        }
-    });
+    showDropdown: function (e) {
+      var tab = e.target || e.srcElement;
+      var navigateTo = tab.id;
+      if (navigateTo === "user") {
+        $("#user_dropdown").show(200);
+        return;
+      }
+    },
+
+    hideDropdown: function (e) {
+      var tab = e.target || e.srcElement;
+      var navigateTo = tab.id;
+      if (navigateTo === "") {
+        tab = $(tab).closest(".user-dropdown-menu");
+        navigateTo = tab.attr("id");
+      }
+      if (navigateTo === "user" || navigateTo === "user_dropdown" || navigateTo === "userimage" ) {
+        $("#user_dropdown").hide();
+        return;
+      }
+    },
+
+
+
+    render: function (el) {
+      this.$el = el;
+      this.$el.html(this.template.render({
+        img : "https://s.gravatar.com/avatar/9c53a795affc3c3c03801ffae90e2e11?s=80",
+        prename : "Floyd",
+        lastname : "Pepper"
+      }));
+      this.delegateEvents();
+      return this.$el;
+    }
+  });
 }());
