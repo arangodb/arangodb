04cd14b91f28de44fb3645be6aab195734b47fb6
Max Neunhoeffer <max@arangodb.com>
2014-02-18 10:15:13 +0100

    Update convenience scripts.

--- scripts/kill_cluster.js
+++ scripts/kill_cluster.js
@@ -1,9 +1,16 @@
-var Planner = require("org/arangodb/cluster").Planner;
-var Kickstarter = require("org/arangodb/cluster").Kickstarter;
-var p = new Planner({});
-var k = new Kickstarter(p.getPlan());
-var fs = require("fs");
-k.runInfo = JSON.parse(fs.read("runInfo.json"));
-k.shutdown();
-k.cleanup();
-
+function main (argv) {
+  var runInfoName = "runInfo.json";
+  var fs = require("fs");
+  var print = require("internal").print;
+  if (argv.length > 1) {
+    runInfoName = argv[1];
+    print("Using runInfo from:",runInfoName);
+  }
+  var Planner = require("org/arangodb/cluster").Planner;
+  var Kickstarter = require("org/arangodb/cluster").Kickstarter;
+  var runInfo = JSON.parse(fs.read(runInfoName));
+  var k = new Kickstarter(runInfo.plan);
+  k.runInfo = runInfo.runInfo;
+  k.shutdown();
+  k.cleanup();
+}
