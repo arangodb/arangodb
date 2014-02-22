2c2fc69c0acb169f97b2794dd9aa8c204fb92992
scottashton <f.bartels@triagens.de>
2014-02-22 10:51:19 +0100

    dashboard refactoring

--- js/apps/system/aardvark/frontend/js/views/dashboardView.js
+++ js/apps/system/aardvark/frontend/js/views/dashboardView.js
@@ -54,14 +54,31 @@
       this.description = this.options.description.models[0];
       this.startUpdating();
       this.graphs = {};
+     },
 
 
 
+    getChartsForFigure : function (figure) {
+        this.series[figure.group][figure.identifier] = {};
+        var result = {};
+        if (this.chartTypeExceptions[figure.type] &&
+            this.chartTypeExceptions[figure.type][figure.identifier]) {
+            result[this.chartTypeExceptions[figure.type][figure.identifier]] =
+                this.getChartStructure(this.chartTypeExceptions[figure.type][figure.identifier]);
+            if (figure.type === "distribution") {
+                result[figure.type] = this.getChartStructure(figure.type);
+            }
+        } else {
+            result[figure.type] = this.getChartStructure(figure.type);
+        }
+        this.series[figure.group][figure.identifier] =  result;
+    },
 
-
-
-
-
+    getChartStructure: function (type) {
+        return {
+            data: type === "distribution" ? undefined  : [],
+            options : {}
+        };
     },
 
     prepareSeries: function () {
@@ -71,75 +88,62 @@
             self.series[group.group] = {};
         });
         self.description.get("figures").forEach(function(figure) {
-            self.series[figure.group][figure.identifier] = {};
-            if (self.chartTypeExceptions[figure.type] &&
-                self.chartTypeExceptions[figure.type][figure.identifier]) {
-                self.series[figure.group][figure.identifier][
-                    self.chartTypeExceptions[figure.type][figure.identifier]
-                    ] = [];
-
-                if (figure.type === "distribution") {
-                    self.series[figure.group][figure.identifier]["distribution"] = undefined;
-                }
-            } else {
-                 self.series[figure.group][figure.identifier][figure.type] = [];
-            }
-
-
-
+            self.getChartsForFigure(figure);
+        });
+        Object.keys(self.combinedCharts).forEach(function (cc) {
+            var part = cc.split("_");
+            var fig = {identifier : part[1], group : part[0], type : "current"};
+            self.getChartsForFigure(fig);
 
         });
     },
 
 
+    processSingleStatistic: function (entry) {
+        var self = this;
+        var time = entry.time * 1000;
+        self.description.get("groups").forEach(function(g) {
+            Object.keys(entry[g.group]).forEach(function(figure) {
+                var valueLists = self.series[g.group][figure];
+                Object.keys(valueLists).forEach(function (valueList) {
+                    var val = entry[g.group][figure];
+                    if (valueList === "lineChartDiffBased") {
+                        if (!self.LastValues[figure]) {
+                            self.LastValues[figure] = val;
+                        }
+                        valueLists[valueList]["data"].push([new Date(time), (val - self.LastValues[figure]) / val]);
+                        self.LastValues[figure] = val;
+                    } else if (valueList === "distribution") {
+                        valueLists[valueList]["data"] = val;
+                    } else if (valueList === "accumulated") {
+
+                    } else if (valueList === "current") {
+                        valueLists[valueList]["data"].push([new Date(time), val]);
+                    } else if (valueList === "currentDistribution")  {
+                        valueLists[valueList]["data"].push([
+                            new Date(time),
+                            val.count === 0 ? 0 : val.sum / val.count
+                        ]);
+                    }
+                });
+
+            });
+        });
+        Object.keys(self.combinedCharts).forEach(function (cc) {
+            var part = cc.split("_");
+            var val = [new Date(time)];
+            self.combinedCharts[cc].sort().forEach(function(attrib) {
+                val.push(entry[part[0]][attrib]);
+            })
+            self.series[part[0]][part[1]]["current"]["data"].push(val);
+        })
+    },
+
     calculateSeries: function () {
         var self = this;
         self.LastValues = {};
         self.history.forEach(function(entry) {
-            var time = entry.time * 1000;
-            //iteration über Gruppen
-            self.description.get("groups").forEach(function(g) {
-                //iteration über figures
-                Object.keys(entry[g.group]).forEach(function(figure) {
-                   //iteration über valuelisten:
-                   var valueLists = self.series[g.group][figure];
-                    Object.keys(valueLists).forEach(function (valueList) {
-                        var val = entry[g.group][figure];
-                        if (valueList === "lineChartDiffBased") {
-                            if (!self.LastValues[figure]) {
-                                self.LastValues[figure] = val;
-                            }
-                            valueLists[valueList].push([new Date(time), (val - self.LastValues[figure]) / val]);
-                            self.LastValues[figure] = val;
-                        } else if (valueList === "distribution") {
-                            valueLists[valueList] = val;
-                        } else if (valueList === "accumulated") {
-
-                        } else if (valueList === "current") {
-                            valueLists[valueList].push([new Date(time), val]);
-                        } else if (valueList === "currentDistribution")  {
-                            valueLists[valueList].push([
-                                new Date(time),
-                                val.count === 0 ? 0 : val.sum / val.count
-                            ]);
-                        }
-                    });
-
-                });
-            });
-            Object.keys(self.combinedCharts).forEach(function (cc) {
-                var part = cc.split("_");
-                if (!self.series[part[0]][part[1]]) {
-                    self.series[part[0]][part[1]] = {
-                        current:  []
-                    };
-                }
-                var val = [new Date(time)];
-                self.combinedCharts[cc].sort().forEach(function(attrib) {
-                    val.push(entry[part[0]][attrib]);
-                })
-                self.series[part[0]][part[1]]["current"].push(val);
-            })
+            self.processSingleStatistic(entry);
         });
 
         Object.keys(self.series).forEach(function(group) {
@@ -151,7 +155,7 @@
                     } else if (valueList === "accumulated") {
                         delete self.series[group][figure][valueList];
                     } else if (valueList === "current") {
-
+                        self.graphs[figure] = undefined;
                     } else if (valueList === "distribution") {
 
                     } else if (valueList === "currentDistribution")  {
@@ -172,76 +176,96 @@
     },
 
 
-    createLineChart: function() {
-        if (!this.graphs["request"]) {
-            console.log(this.series["http"]["requests"]["current"]);
-            this.graphs["request"] = new Dygraph(
-                document.getElementById("dashboardHttpGroup"),
-                this.series["http"]["requests"]["current"],
-                {   title: 'Average request time in milliseconds',
-                    //yLabelWidth: "15",
-                    //labelsDivStyles: { 'backgroundColor': 'transparent','textAlign': 'right' },
-                    //hideOverlayOnMouseOut: true,
-                    //labelsSeparateLines: true,
-                    //legend: "always",
-                    //labelsDivWidth: 150,
-                    //labelsShowZeroValues: false,
-                    //highlightSeriesBackgroundAlpha: 0.5,
-                    //drawPoints: true,
-                    //width: 480,
-                    //height: 320,
-                    labels: ["dateTime", "requestsGet","requestsHead",
-                        "requestsPost","requestsPut",
-                        "requestsPatch","requestsDelete",
-                        "requestsOptions", "requestsOther"],
-                    //visibility:getVisibility() ,
-                    //valueRange: [self.min -0.1 * self.min, self.max + 0.1 * self.max],
-                    //stackedGraph: false,
-                    //axes: {
-                    //  y: {
-                    //        valueFormatter: function(y) {
-                    //          return y.toPrecision(2);
-                    //                          },
-                    //      axisLabelFormatter: function(y) {
-                    //          return y.toPrecision(2);
-                    //      }
-                    //  },
-                    //      x: {
-                    //      valueFormatter: function(d) {
-                    //              if (d === -1) {return "";}
-                    //              var date = new Date(d);
-                    //              return Dygraph.zeropad(date.getHours()) + ":"
-                    //                  + Dygraph.zeropad(date.getMinutes()) + ":"
-                    //                  + Dygraph.zeropad(date.getSeconds());
-                    //                          }
-                    //  }
-                    //                  },
-                    //highlightCircleSize: 2,
-                    //strokeWidth: 1,
-                    //strokeBorderWidth: null,
-                   highlightSeriesOpts: {
-                        strokeWidth: 3,
-                        strokeBorderWidth: 1,
-                        highlightCircleSize: 5
-                      }
-                    });
-                var onclick = function(ev) {
-                    if (this.graphs["request"].isSeriesLocked()) {
-                        this.graphs["request"].clearSelection();
-                    } else {
-                        this.graphs["request"].setSelection(this.graphs["request"].getSelection(), this.graphs["request"].getHighlightSeries(), true);
-                    }
-                };
-                this.graphs["request"].updateOptions({clickCallback: onclick}, true);
-                this.graphs["request"].setSelection(false, 'ClusterAverage', true);
-             } else {
+    updateSeries : function(data) {
 
-            this.graph.updateOptions( {
-                'file': this.series["http"]["requests"]["current"]
-            } );
-        }
-        }
-    ,
+    },
+
+
+    createLineCharts: function() {
+        var self = this;
+        Object.keys(self.series).forEach(function(group) {
+            Object.keys(self.series[group]).forEach(function(figure) {
+                Object.keys(self.series[group][figure]).forEach(function(valueList) {
+                    if (valueList === "current") {
+                        if (!self.graphs[figure]) {
+                            self.renderHttpGroup(figure);
+                            var label = ["dateTime"];
+                            if (self.combinedCharts[group + "_"  +figure]) {
+                                label = label.concat(self.combinedCharts[group + "_"  +figure].sort());
+                            } else {
+                                label.push(figure);
+                            }
+                            var stacked = false;
+                            if (self.stackedCharts.indexOf(group + "_" + figure) != -1) {
+                                stacked = true;
+                            }
+                            self.graphs[figure] = new Dygraph(
+                                document.getElementById(figure),
+                                self.series[group][figure]["current"],
+                                {   title: group + figure,
+                                    //yLabelWidth: "15",
+                                    //labelsDivStyles: { 'backgroundColor': 'transparent','textAlign': 'right' },
+                                    //hideOverlayOnMouseOut: true,
+                                    //labelsSeparateLines: true,
+                                    //legend: "always",
+                                    //labelsDivWidth: 150,
+                                    //labelsShowZeroValues: false,
+                                    //highlightSeriesBackgroundAlpha: 0.5,
+                                    //drawPoints: true,
+                                    //width: 480,
+                                    //height: 320,
+                                    labels: label,
+                                    //visibility:getVisibility() ,
+                                    //valueRange: [self.min -0.1 * self.min, self.max + 0.1 * self.max],
+                                    stackedGraph: false,
+                                    //axes: {
+                                    //  y: {
+                                    //        valueFormatter: function(y) {
+                                    //          return y.toPrecision(2);
+                                    //                          },
+                                    //      axisLabelFormatter: function(y) {
+                                    //          return y.toPrecision(2);
+                                    //      }
+                                    //  },
+                                    //      x: {
+                                    //      valueFormatter: function(d) {
+                                    //              if (d === -1) {return "";}
+                                    //              var date = new Date(d);
+                                    //              return Dygraph.zeropad(date.getHours()) + ":"
+                                    //                  + Dygraph.zeropad(date.getMinutes()) + ":"
+                                    //                  + Dygraph.zeropad(date.getSeconds());
+                                    //                          }
+                                    //  }
+                                    //                  },
+                                    //highlightCircleSize: 2,
+                                    //strokeWidth: 1,
+                                    //strokeBorderWidth: null,
+                                   highlightSeriesOpts: {
+                                        strokeWidth: 3,
+                                        strokeBorderWidth: 1,
+                                        highlightCircleSize: 5
+                                      }
+                                    });
+                                var onclick = function(ev) {
+                                    if (self.graphs[figure].isSeriesLocked()) {
+                                        self.graphs[figure].clearSelection();
+                                    } else {
+                                        self.graphs[figure].setSelection(self.graphs[figure].getSelection(), self.graphs[figure].getHighlightSeries(), true);
+                                    }
+                                };
+                                self.graphs[figure].updateOptions({clickCallback: onclick}, true);
+                                self.graphs[figure].setSelection(false, 'ClusterAverage', true);
+                             } else {
+
+                                self.graphs[figure].updateOptions( {
+                                'file': self.series[group][figure]["current"]
+                                 } );
+                        }
+                    }
+                });
+            });
+        });
+    },
 
     renderFigures: function () {
 
@@ -273,7 +297,7 @@
         this.timer = window.setInterval(function() {
                 self.collection.fetch({
                 success: function() {
-                    self.history.push({
+                    self.updateSeries({
                         time : new Date().getTime() / 1000,
 
                         client: self.collection.first().get("client"),
@@ -286,7 +310,7 @@
                     self.calculateSeries();
                     self.renderFigures();
                     self.renderPieCharts();
-                    self.createLineChart();
+                    self.createLineCharts();
                 },
                  error: function() {
 
@@ -301,8 +325,9 @@
 
     httpTemplate: templateEngine.createTemplate("dashboardHttpGroup.ejs"),
 
-    renderHttpGroup: function() {
-      $('.contentDiv').append(this.httpTemplate.render());
+    renderHttpGroup: function(id) {
+        console.log(id, "render");
+      $('.contentDiv').append(this.httpTemplate.render({id : id}));
     },
 
     render: function() {
@@ -312,10 +337,9 @@
       this.calculateSeries();
       this.renderFigures();
       this.renderPieCharts();
-      this.createLineChart();
+      this.createLineCharts();
 
-      this.renderHttpGroup();
-      console.log(this.series);
+
     }
 
 
