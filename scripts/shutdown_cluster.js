623ff40dbce415713260ddc9fc0f14b874e8b688
Max Neunhoeffer <max@arangodb.com>
2014-02-24 15:59:24 +0100

    Add a shutdown_cluster script.

---
+++ scripts/shutdown_cluster.js
@@ -0,0 +1,14 @@
+function main (argv) {
+  var runInfoName = "runInfo.json";
+  var fs = require("fs");
+  var print = require("internal").print;
+  if (argv.length > 1) {
+    runInfoName = argv[1];
+    print("Using runInfo from:",runInfoName);
+  }
+  var Kickstarter = require("org/arangodb/cluster").Kickstarter;
+  var runInfo = JSON.parse(fs.read(runInfoName));
+  var k = new Kickstarter(runInfo.plan);
+  k.runInfo = runInfo.runInfo;
+  k.shutdown();
+}
