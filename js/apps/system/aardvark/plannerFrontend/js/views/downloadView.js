f99d1c4c121a2fee3249d2f1983e2ba51733a195
mchacki <m.hackstein@triagens.de>
2014-02-25 18:27:46 +0100

    Added a link to the cluster overview and replaced tcp with http and ssl with https for clickable links

--- js/apps/system/aardvark/plannerFrontend/js/views/downloadView.js
+++ js/apps/system/aardvark/plannerFrontend/js/views/downloadView.js
@@ -20,7 +20,9 @@
       this.content = "data:application/octet-stream," + encodeURIComponent(JSON.stringify(content, 2));
       var toShow = _.findWhere(content.runInfo.runInfo, {isStartServers: true});
       $(this.el).html(this.template.render({
-        endpoints: toShow.endpoints,
+        endpoints: _.map(toShow.endpoints, function(e) {
+          return e.replace("tcp://","http://").replace("ssl://", "https://");
+        }),
         roles: toShow.roles
       }));
     }
