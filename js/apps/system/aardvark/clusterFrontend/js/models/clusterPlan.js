5102857ea468f41265ab724529ea2af18d9232fe
gschwab <g.schwab@triagens.de>
2014-02-28 18:26:24 +0100

    fixed checkbox problem with reading value

--- js/apps/system/aardvark/clusterFrontend/js/models/clusterPlan.js
+++ js/apps/system/aardvark/clusterFrontend/js/models/clusterPlan.js
@@ -19,19 +19,21 @@
       var i,j,r;
       r = this.get("runInfo");
       j = r.length-1;
-      while (j > 0 && r[j].isStartServers === undefined) {
-        j--;
-      }
-      var l = r[j];
-      if (l.endpoints) {
-        for (i = 0; i < l.endpoints.length;i++) {
-          if (l.roles[i] === "Coordinator") {
-            this._coord = l.endpoints[i]
-              .replace("tcp://","http://")
-              .replace("ssl://", "https://");
-            return this._coord;
+      while (j > 0) {
+        if(r[j].isStartServers) {
+          var l = r[j];
+          if (l.endpoints) {
+            for (i = 0; i < l.endpoints.length;i++) {
+              if (l.roles[i] === "Coordinator") {
+                this._coord = l.endpoints[i]
+                  .replace("tcp://","http://")
+                  .replace("ssl://", "https://");
+                return this._coord;
+              }
+            }
           }
         }
+        j--;
       }
     },
 
