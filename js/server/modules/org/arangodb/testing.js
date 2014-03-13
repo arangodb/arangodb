51ab7bbb9b9bacbd5a5cbe38c7cfa59612021dff
Max Neunhoeffer <max@arangodb.com>
2014-03-13 13:38:19 +0100

    Skip dump and import tests on the cluster because they cannot work.

--- js/server/modules/org/arangodb/testing.js
+++ js/server/modules/org/arangodb/testing.js
@@ -638,6 +638,11 @@ var impTodo = [
 ];
 
 testFuncs.importing = function (options) {
+  if (options.cluster) {
+    print("Skipped because of cluster.");
+    return {"ok":true, "skipped":0};
+  }
+    
   var instanceInfo = startInstance("tcp",options);
 
   var result = {};
@@ -730,6 +735,10 @@ testFuncs.foxx_manager = function (options) {
 };
 
 testFuncs.dump = function (options) {
+  if (options.cluster) {
+    print("Skipped because of cluster.");
+    return {"ok":true, "skipped":0};
+  }
   print("dump tests...");
   var instanceInfo = startInstance("tcp",options);
   var results = {};
