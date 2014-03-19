bcceb5cb5ab3ec55013c7109dd72a042656cae5f
Michael Hackstein <michael@arangodb.com>
2014-03-19 15:30:35 +0100

    Fixed pagination, buttons, a table in documents view and scss-lint warning

--- js/apps/system/aardvark/frontend/js/lib/bootstrap-pagination.js
+++ js/apps/system/aardvark/frontend/js/lib/bootstrap-pagination.js
@@ -38,7 +38,7 @@
       return li;
     }
     function newUL() {
-      var ul = $('<ul class="arangoPagination"/>'), i;
+      var ul = $('<ul class="arango-pagination"/>'), i;
       ul.append(newLI(options.prev, options.page - 1));
       for (i = begin; i <= end; i += 1) { ul.append(newLI(i, i)); }
       return ul.append(newLI(options.next, options.page + 1));
