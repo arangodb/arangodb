0bfe77b2ea12afeb00f5909eb47bf14a215af52f
jsteemann <j.steemann@triagens.de>
2014-03-25 10:50:38 +0100

    fixed memleak

--- arangod/V8Server/v8-query.cpp
+++ arangod/V8Server/v8-query.cpp
@@ -268,26 +268,23 @@ static int SetupExampleObject (v8::Handle<v8::Object> const& example,
 
     if (*keyStr != 0) {
       pids[i] = shaper->lookupAttributePathByName(shaper, *keyStr);
-      values[i] = TRI_ShapedJsonV8Object(val, shaper, false, false);
 
-      if (pids[i] == 0 || values[i] == 0) {
+      if (pids[i] == 0) {
         // no attribute path found. this means the result will be empty 
         CleanupExampleObject(shaper->_memoryZone, i, pids, values);
         return TRI_RESULT_ELEMENT_NOT_FOUND;
       }
+      
+      values[i] = TRI_ShapedJsonV8Object(val, shaper, false, false);
+
+      if (values[i] == 0) {
+        CleanupExampleObject(shaper->_memoryZone, i, pids, values);
+        return TRI_RESULT_ELEMENT_NOT_FOUND;
+      }
     }
-
-    if (*keyStr == 0 || pids[i] == 0 || values[i] == 0) {
-      CleanupExampleObject(shaper->_memoryZone, i, pids, values);
-
-      if (*keyStr == 0) {
-        *err = TRI_CreateErrorObject(TRI_ERROR_BAD_PARAMETER,
-                                     "cannot convert attribute path to UTF8");
-      }
-      else {
-        *err = TRI_CreateErrorObject(TRI_ERROR_BAD_PARAMETER,
-                                     "cannot convert value to JSON");
-      }
+    else {
+      *err = TRI_CreateErrorObject(TRI_ERROR_BAD_PARAMETER,
+                                   "cannot convert attribute path to UTF8");
       return TRI_ERROR_BAD_PARAMETER;
     }
   }
@@ -605,8 +602,6 @@ static TRI_index_operator_t* SetupConditionsBitarrayHelper (TRI_index_t* idx,
 
   v8::Handle<v8::Value> value;
   TRI_index_operator_type_e operatorType;
-  TRI_index_operator_t* indexOperator = 0;
-
 
   // ........................................................................
   // Check the various operator conditions
@@ -736,6 +731,8 @@ static TRI_index_operator_t* SetupConditionsBitarrayHelper (TRI_index_t* idx,
     return 0;
   }
 
+  TRI_index_operator_t* indexOperator = 0;
+
   // ........................................................................
   // Since we have a valid condition, act upon it
   // may require recursion
@@ -850,11 +847,6 @@ static TRI_index_operator_t* SetupConditionsBitarrayHelper (TRI_index_t* idx,
       }
       indexOperator = TRI_CreateIndexOperator(operatorType, NULL, NULL, parameters, shaper, NULL, parameters->_value._objects._length, NULL);
       break;
-    }
-
-
-    default: {
-      return 0;
     }
 
   } // end of switch (operatorType)
