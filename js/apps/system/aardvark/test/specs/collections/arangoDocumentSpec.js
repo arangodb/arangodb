e8904d5d937568382de291f2c1879ddc25709954
scottashton <f.bartels@triagens.de>
2014-03-27 12:15:13 +0100

    fixed jslint

--- js/apps/system/aardvark/test/specs/collections/arangoDocumentSpec.js
+++ js/apps/system/aardvark/test/specs/collections/arangoDocumentSpec.js
@@ -1,5 +1,6 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, white: true  plusplus: true, browser: true*/
-/*global describe, beforeEach, afterEach, it, spyOn, expect*/
+/*global XMLHttpRequestException, describe, beforeEach, afterEach, it, spyOn,
+ arangoHelper, expect*/
 /*global $*/
 
 (function () {
@@ -15,8 +16,7 @@
         });
 
         it("should deleteEdge and succeed", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("DELETE");
@@ -25,13 +25,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success();
             });
-            var result = col.deleteEdge(colid, docid);
+            result = col.deleteEdge(colid, docid);
             expect(result).toEqual(true);
         });
 
         it("should deleteEdge and fail", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("DELETE");
@@ -40,22 +39,20 @@
                 expect(opt.async).toEqual(false);
                 opt.error();
             });
-            var result = col.deleteEdge(colid, docid);
+            result = col.deleteEdge(colid, docid);
             expect(result).toEqual(false);
 
         });
         it("should deleteEdge and throw exception", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andThrow(XMLHttpRequestException);
-            var result = col.deleteEdge(colid, docid);
+            result = col.deleteEdge(colid, docid);
             expect(result).toEqual(false);
 
         });
 
         it("should deleteDocument and succeed", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("DELETE");
@@ -64,13 +61,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success();
             });
-            var result = col.deleteDocument(colid, docid);
+            result = col.deleteDocument(colid, docid);
             expect(result).toEqual(true);
         });
 
         it("should deleteDocument and fail", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("DELETE");
@@ -79,25 +75,23 @@
                 expect(opt.async).toEqual(false);
                 opt.error();
             });
-            var result = col.deleteDocument(colid, docid);
+            result = col.deleteDocument(colid, docid);
             expect(result).toEqual(false);
 
         });
         it("should deleteDocument and throw exception", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andThrow(XMLHttpRequestException);
-            var result = col.deleteDocument(colid, docid);
+            result = col.deleteDocument(colid, docid);
             expect(result).toEqual(false);
 
         });
 
         it("should createTypeEdge and succeed", function () {
-            var collectionID = 1;
-            var from = 2;
-            var to = 3;
+            var collectionID = 1, from = 2, to = 3, result;
             spyOn($, "ajax").andCallFake(function (opt) {
-                expect(opt.url).toEqual("/_api/edge?collection=" + collectionID + "&from=" + from + "&to=" + to);
+                expect(opt.url).toEqual(
+                    "/_api/edge?collection=" + collectionID + "&from=" + from + "&to=" + to);
                 expect(opt.type).toEqual("POST");
                 expect(opt.contentType).toEqual("application/json");
                 expect(opt.cache).toEqual(false);
@@ -105,16 +99,15 @@
                 expect(opt.async).toEqual(false);
                 opt.success({_id: 100});
             });
-            var result = col.createTypeEdge(collectionID, from, to);
+            result = col.createTypeEdge(collectionID, from, to);
             expect(result).toEqual(100);
         });
 
         it("should createTypeEdge and fail", function () {
-            var collectionID = 1;
-            var from = 2;
-            var to = 3;
+            var collectionID = 1, from = 2, to = 3, result;
             spyOn($, "ajax").andCallFake(function (opt) {
-                expect(opt.url).toEqual("/_api/edge?collection=" + collectionID + "&from=" + from + "&to=" + to);
+                expect(opt.url).toEqual(
+                    "/_api/edge?collection=" + collectionID + "&from=" + from + "&to=" + to);
                 expect(opt.type).toEqual("POST");
                 expect(opt.contentType).toEqual("application/json");
                 expect(opt.cache).toEqual(false);
@@ -122,13 +115,13 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.createTypeEdge(collectionID, from, to);
+            result = col.createTypeEdge(collectionID, from, to);
             expect(result).toEqual(false);
 
         });
 
         it("should createTypeDocument and succeed", function () {
-            var collectionID = 1;
+            var collectionID = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document?collection=" + collectionID);
                 expect(opt.type).toEqual("POST");
@@ -138,12 +131,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success({_id: 100});
             });
-            var result = col.createTypeDocument(collectionID);
+            result = col.createTypeDocument(collectionID);
             expect(result).toEqual(100);
         });
 
         it("should createTypeDocument and fail", function () {
-            var collectionID = 1;
+            var collectionID = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document?collection=" + collectionID);
                 expect(opt.type).toEqual("POST");
@@ -153,13 +146,13 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.createTypeDocument(collectionID);
+            result = col.createTypeDocument(collectionID);
             expect(result).toEqual(false);
 
         });
 
         it("should addDocument and succeed", function () {
-            var collectionID = 1;
+            var collectionID = 1, result;
             spyOn(col, "createTypeDocument");
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document?collection=" + collectionID);
@@ -170,13 +163,13 @@
                 expect(opt.async).toEqual(false);
                 opt.success({_id: 100});
             });
-            var result = col.addDocument(collectionID);
+            result = col.addDocument(collectionID);
             expect(col.createTypeDocument).toHaveBeenCalled();
         });
 
 
         it("should getCollectionInfo and succeed", function () {
-            var identifier = 1;
+            var identifier = 1, result;
             spyOn(arangoHelper, "getRandomToken").andReturn("2");
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/collection/" + identifier + "?" + "2");
@@ -187,12 +180,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success({_id: 100});
             });
-            var result = col.getCollectionInfo(identifier);
+            result = col.getCollectionInfo(identifier);
             expect(result).toEqual({_id: 100});
         });
 
         it("should getCollectionInfo and fail", function () {
-            var identifier = 1;
+            var identifier = 1, result;
             spyOn(arangoHelper, "getRandomToken").andReturn("2");
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/collection/" + identifier + "?" + "2");
@@ -203,14 +196,13 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.getCollectionInfo(identifier);
+            result = col.getCollectionInfo(identifier);
             expect(result).toEqual({});
 
         });
 
         it("should getEdge and succeed", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("GET");
@@ -221,14 +213,13 @@
                 opt.success("bla");
             });
             spyOn(window.arangoDocumentStore, "add");
-            var result = col.getEdge(colid, docid);
+            result = col.getEdge(colid, docid);
             expect(result).toEqual(true);
             expect(window.arangoDocumentStore.add).toHaveBeenCalledWith("bla");
         });
 
         it("should getEdge and fail", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("GET");
@@ -238,14 +229,13 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.getEdge(colid, docid);
+            result = col.getEdge(colid, docid);
             expect(result).toEqual(false);
 
         });
 
         it("should getDocument and succeed", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("GET");
@@ -256,14 +246,13 @@
                 opt.success("bla");
             });
             spyOn(window.arangoDocumentStore, "add");
-            var result = col.getDocument(colid, docid);
+            result = col.getDocument(colid, docid);
             expect(result).toEqual(true);
             expect(window.arangoDocumentStore.add).toHaveBeenCalledWith("bla");
         });
 
         it("should getDocument and fail", function () {
-            var docid = 1;
-            var colid = 1;
+            var docid = 1, colid = 1, result;
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("GET");
@@ -273,16 +262,14 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.getDocument(colid, docid);
+            result = col.getDocument(colid, docid);
             expect(result).toEqual(false);
 
         });
 
 
         it("should saveEdge and succeed", function () {
-            var docid = 1;
-            var colid = 1;
-            var model = {name: "heinz"};
+            var docid = 1, colid = 1, result, model = {name: "heinz"};
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("PUT");
@@ -293,14 +280,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success("bla");
             });
-            var result = col.saveEdge(colid, docid, model);
+            result = col.saveEdge(colid, docid, model);
             expect(result).toEqual(true);
         });
 
         it("should saveEdge and fail", function () {
-            var docid = 1;
-            var colid = 1;
-            var model = {name: "heinz"};
+            var docid = 1, colid = 1, result, model = {name: "heinz"};
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/edge/" + colid + "/" + docid);
                 expect(opt.type).toEqual("PUT");
@@ -311,15 +296,13 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.saveEdge(colid, docid, model);
+            result = col.saveEdge(colid, docid, model);
             expect(result).toEqual(false);
 
         });
 
         it("should saveDocument and succeed", function () {
-            var docid = 1;
-            var colid = 1;
-            var model = {name: "heinz"};
+            var docid = 1, colid = 1, result, model = {name: "heinz"};
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("PUT");
@@ -330,14 +313,12 @@
                 expect(opt.async).toEqual(false);
                 opt.success("bla");
             });
-            var result = col.saveDocument(colid, docid, model);
+            result = col.saveDocument(colid, docid, model);
             expect(result).toEqual(true);
         });
 
         it("should saveDocument and fail", function () {
-            var docid = 1;
-            var colid = 1;
-            var model = {name: "heinz"};
+            var docid = 1, colid = 1, result, model = {name: "heinz"};
             spyOn($, "ajax").andCallFake(function (opt) {
                 expect(opt.url).toEqual("/_api/document/" + colid + "/" + docid);
                 expect(opt.type).toEqual("PUT");
@@ -348,7 +329,7 @@
                 expect(opt.async).toEqual(false);
                 opt.error({});
             });
-            var result = col.saveDocument(colid, docid, model);
+            result = col.saveDocument(colid, docid, model);
             expect(result).toEqual(false);
 
         });
