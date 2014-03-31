86dd3cc94bbe81c0f4857f71879ad4e177091d2d
scottashton <f.bartels@triagens.de>
2014-03-31 13:05:36 +0200

    finished router tests and reformated test files

--- js/apps/system/aardvark/test/karma/karma.conf.js
+++ js/apps/system/aardvark/test/karma/karma.conf.js
@@ -4,66 +4,66 @@
 filesJSON = require("./files.json");
 filesJSON.files.push('test/specJSLint/jsLintSpec.js');
 
-module.exports = function(karma) {
+module.exports = function (karma) {
 
-  karma.set({
+    karma.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../../',
-
-
-    // frameworks to use
-    //frameworks: ['jasmine', 'junit-reporter'],
-    frameworks: ['jasmine'],
+        // base path, that will be used to resolve files and exclude
+        basePath: '../../',
 
 
-    // list of files / patterns to load in the browser
-    files: filesJSON.files,
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['dots'],
-
-    // web server port
-    port: 9876,
+        // frameworks to use
+        //frameworks: ['jasmine', 'junit-reporter'],
+        frameworks: ['jasmine'],
 
 
-    // cli runner port
-    runnerPort: 9100,
+        // list of files / patterns to load in the browser
+        files: filesJSON.files,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: false,
+        // list of files to exclude
+        exclude: [
+        ],
 
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['dots'],
 
-    // level of logging
-    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
-    logLevel: karma.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ["PhantomJS"],
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // web server port
+        port: 9876,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true
-  });
+        // cli runner port
+        runnerPort: 9100,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: false,
+
+
+        // level of logging
+        // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+        logLevel: karma.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ["PhantomJS"],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
