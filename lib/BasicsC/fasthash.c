e5b4bd8718e0afe8550042f5b7cea12e7eddd52d
Jan Steemann <jan@arangodb.com>
2014-04-01 10:50:08 +0200

    fix for VS

--- lib/BasicsC/fasthash.c
+++ lib/BasicsC/fasthash.c
@@ -27,10 +27,10 @@
 
 // Compression function for Merkle-Damgard construction.
 // This function is generated using the framework provided.
-#define mix(h) ({					\
+#define mix(h) 					\
 			(h) ^= (h) >> 23;		\
 			(h) *= 0x2127599bf4325c37ULL;	\
-			(h) ^= (h) >> 47; })
+			(h) ^= (h) >> 47; 
 
 uint64_t fasthash64(const void *buf, size_t len, uint64_t seed)
 {
