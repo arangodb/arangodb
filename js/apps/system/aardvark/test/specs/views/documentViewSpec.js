b14ce4cedaaa6559fbf2e257899f92d510063e54
hkernbach <h.kernbach@triagens.de>
2014-04-07 14:39:34 +0200

    documentViewSpec

--- js/apps/system/aardvark/test/specs/views/documentViewSpec.js
+++ js/apps/system/aardvark/test/specs/views/documentViewSpec.js
@@ -18,7 +18,43 @@
       view = new window.DocumentView();
     });
 
-    it("should offer a list view", function() {
+    it("assert the basics", function () {
+      expect(view.colid).toEqual(0);
+      expect(view.colid).toEqual(0);
+      expect(view.editor).toEqual(0);
+      expect(view.events).toEqual({
+        "click #saveDocumentButton" : "saveDocument"
+      });
+    });
+
+    it("check document view type check", function() {
+      var model = new window.arangoDocument({
+        _key: "123",
+        _id: "v/123",
+        _rev: "123",
+        name: "alice"
+      });
+
+      var arangoDocStoreDummy = {
+        getEdge: function () {
+          return true;
+        },
+        getDocument: function () {
+          return true;
+        },
+        first: function () {
+          return model;
+        }
+      };
+
+      //spyOn(window, "arangoDocumentStore").andReturn(arangoDocStoreDummy);
+      //spyOn(window.arangoDocumentStore, "getEdge").andReturn(true);
+
+      view.typeCheck('edge').toEqual(true);
+      view.typeCheck('document').toEqual(true);
+      view.typeCheck('xyz').toEqual(false);
+      view.typeCheck(false).toEqual(false);
+      view.typeCheck(123).toEqual(false);
     });
 
     it("should offer a source view", function() {
