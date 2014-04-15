03c98cb76e26771622e9cae9eea277776305d230
mchacki <m.hackstein@triagens.de>
2014-04-15 08:45:27 +0200

    Fixed karma tests for missing logsView (old)

--- js/apps/system/aardvark/test/specs/models/arangoCollectionModelSpec.js
+++ js/apps/system/aardvark/test/specs/models/arangoCollectionModelSpec.js
@@ -5,19 +5,18 @@
 (function() {
   "use strict";
 
-  var myCollection;
-
-  beforeEach(function() {
-    myCollection = new window.arangoCollectionModel();
-  });
-
   describe("Arango Collection Model", function() {
+
+    var myCollection;
+
+    beforeEach(function() {
+      myCollection = new window.arangoCollectionModel();
+    });
+
     it("verifies urlRoot", function() {
       expect(myCollection.urlRoot).toEqual('/_api/collection');
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies defaults", function() {
       expect(myCollection.get('id')).toEqual('');
       expect(myCollection.get('name')).toEqual('');
@@ -26,20 +25,18 @@
       expect(myCollection.get('isSystem')).toBeFalsy();
       expect(myCollection.get('picture')).toEqual('');
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies set defaults", function() {
       var name = 'blub',
         status = 'active',
-        type   = 'myType',
-        myCollection = new window.arangoCollectionModel(
-          {
-            name    : name,
-            status  : status,
-            type    : type
-          }
-        );
+        type   = 'myType';
+      myCollection = new window.arangoCollectionModel(
+        {
+          name    : name,
+          status  : status,
+          type    : type
+        }
+      );
       expect(myCollection.get('id')).toEqual('');
       expect(myCollection.get('name')).toEqual(name);
       expect(myCollection.get('status')).toEqual(status);
@@ -47,52 +44,44 @@
       expect(myCollection.isSystem).toBeFalsy();
       expect(myCollection.get('picture')).toEqual('');
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies getProperties", function() {
       var id = "4711";
       spyOn($, "ajax").andCallFake(function(opt) {
         expect(opt.url).toEqual("/_api/collection/" + id + "/properties");
         expect(opt.type).toEqual("GET");
       });
-
+      myCollection.set('id', id);
       myCollection.getProperties();
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies getFigures", function() {
       var id = "4711";
       spyOn($, "ajax").andCallFake(function(opt) {
         expect(opt.url).toEqual("/_api/collection/" + id + "/figures");
         expect(opt.type).toEqual("GET");
       });
-
+      myCollection.set('id', id);
       myCollection.getFigures();
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies getRevision", function() {
       var id = "4711";
       spyOn($, "ajax").andCallFake(function(opt) {
         expect(opt.url).toEqual("/_api/collection/" + id + "/revision");
         expect(opt.type).toEqual("GET");
       });
-
+      myCollection.set('id', id);
       myCollection.getRevision();
     });
-  });
 
-  describe("Arango Database Model", function() {
     it("verifies getIndex", function() {
       var id = "4711";
       spyOn($, "ajax").andCallFake(function(opt) {
         expect(opt.url).toEqual("/_api/index/?collection=" + id);
         expect(opt.type).toEqual("GET");
       });
-
+      myCollection.set('id', id);
       myCollection.getIndex();
     });
   });
