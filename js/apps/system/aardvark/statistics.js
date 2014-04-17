43cb8d405a2e6f9e6f8da86bf951a4c1b2c8dfec
scottashton <f.bartels@triagens.de>
2014-04-17 09:53:10 +0200

    adjusted statistics request for clusterFrontend

--- js/apps/system/aardvark/statistics.js
+++ js/apps/system/aardvark/statistics.js
@@ -824,7 +824,8 @@ function computeStatisticsSeries (start, attrs) {
 controller.get("full", function (req, res) {
   var start = req.params("start");
   var filter = req.params("filter");
-  var server = req.params("server");
+  var serverEndpoint = decodeURIComponent(req.params("serverEndpoint"));
+  var isDbServer = req.params("isDbServer");
   var attrs = null;
 
   if (start !== null && start !== undefined) {
