c8c79de48097f517d3d6c90f21c4430d106c9244
= <e.lombeyda@triagens.de>
2014-05-02 15:00:11 +0200

    Renaming installer helper

--- installer-generator.sh
+++ installer-generator.sh
@@ -4,7 +4,8 @@ NSIS_PATH="/cygdrive/c/Program Files (x86)/NSIS"
 
 bits=$1
 INSTALLERNAME=`grep CPACK_PACKAGE_FILE_NAME Build$bits/CPackConfig.cmake | grep -o '".*"' | awk -F\" '{print $2}'`
-cat Installation/Windows/Templates/arango-packer-template.nsi | sed -e "s/@BITS@/$bits/g" | sed -e  "s/@INSTALLERNAME@/${INSTALLERNAME}/g" > Build$bits/arango-packer-$bits.nsi
+cp Build$bits/$INSTALLERNAME.exe Build$bits/$INSTALLERNAME-internal.exe 
+cat Installation/Windows/Templates/arango-packer-template.nsi | sed -e "s/@BITS@/$bits/g" | sed -e  "s/@INSTALLERNAME@/${INSTALLERNAME}/g" > Build$bits/$INSTALLERNAME.nsi
 
-"$NSIS_PATH"/bin/makensis.exe   Build$bits/arango-packer-$bits.nsi
+"$NSIS_PATH"/bin/makensis.exe   Build$bits/$INSTALLERNAME.nsi
 
