8935048c91b33441d14c277735b30d8f36281e02
Jan Steemann <jan@arangodb.com>
2014-05-14 12:32:31 +0200

    fixed too eager datafile deletion

--- arangod/VocBase/barrier.c
+++ arangod/VocBase/barrier.c
@@ -74,6 +74,11 @@ static void LinkBarrierElement (TRI_barrier_t* element,
     container->_end = element;
   }
 
+  if (element->_type == TRI_BARRIER_ELEMENT) {
+    // increase counter for barrier elements
+    ++container->_numBarrierElements;
+  }
+
   TRI_UnlockSpin(&container->_lock);
 }
 
@@ -101,7 +106,9 @@ void TRI_InitBarrierList (TRI_barrier_list_t* container,
   TRI_InitSpin(&container->_lock);
 
   container->_begin = NULL;
-  container->_end = NULL;
+  container->_end   = NULL;
+
+  container->_numBarrierElements = 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -146,11 +153,17 @@ void TRI_DestroyBarrierList (TRI_barrier_list_t* container) {
 
 bool TRI_ContainsBarrierList (TRI_barrier_list_t* container,  
                               TRI_barrier_type_e type) {
-  TRI_barrier_t* ptr;
-
   TRI_LockSpin(&container->_lock);
+  
+  if (type == TRI_BARRIER_ELEMENT) {
+    // shortcut
+    bool hasBarriers = (container->_numBarrierElements > 0);
+    TRI_UnlockSpin(&container->_lock);
 
-  ptr = container->_begin;
+    return hasBarriers;
+  }
+
+  TRI_barrier_t* ptr = container->_begin;
 
   while (ptr != NULL) {
     if (ptr->_type == type) {
@@ -375,6 +388,11 @@ void TRI_FreeBarrier (TRI_barrier_t* element) {
   }
   else {
     element->_next->_prev = element->_prev;
+  }
+  
+  if (element->_type == TRI_BARRIER_ELEMENT) {
+    // decrease counter for barrier elements
+    --container->_numBarrierElements;
   }
 
   TRI_UnlockSpin(&container->_lock);
