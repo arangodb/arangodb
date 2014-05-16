16b2f45aca51e5930e776a2e8f1094c11881f06d
hkernbach <h.kernbach@triagens.de>
2014-05-16 12:18:19 +0200

    added shellViewSpec, invertet order of logs showing up @ frontend

--- js/apps/system/aardvark/frontend/js/collections/arangoLogs.js
+++ js/apps/system/aardvark/frontend/js/collections/arangoLogs.js
@@ -20,6 +20,7 @@
         });
       });
       this.totalAmount = response.totalAmount;
+      console.log("parseamount: "+this.totalAmount);
       return myResponse;
     },
 
@@ -33,8 +34,23 @@
     model: window.newArangoLog,
 
     url: function() {
-      var type, rtnStr, offset;
+      var type, rtnStr, offset, size;
       offset = this.page * this.pagesize;
+      console.log("page: "+this.page);
+      console.log("amount: " + this.totalAmount);
+      var inverseOffset = this.totalAmount % this.pagesize - (this.pagesize * this.page);
+      if (inverseOffset < 0) {
+        inverseOffset = 0;
+        size = (this.totalAmount % this.pagesize);
+      }
+      else {
+        size = this.pagesize;
+      }
+
+      if (this.totalAmount === 0) {
+        size = 1;
+      }
+      console.log("offset: " + inverseOffset);
 
       if (this.upto) {
         type = 'upto';
@@ -42,7 +58,7 @@
       else  {
         type = 'level';
       }
-      rtnStr = '/_admin/log?'+type+'='+this.loglevel+'&size='+this.pagesize+'&offset='+offset;
+      rtnStr = '/_admin/log?'+type+'='+this.loglevel+'&size='+size+'&offset='+inverseOffset;
       return rtnStr;
     }
 
