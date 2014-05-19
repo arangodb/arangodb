be12176deab619e3b60ad965020320bc519be635
Esteban Lombeyda <e.lombeyda@triagens.de>
2014-05-19 11:30:31 +0200

    cleaning

--- lib/V8/V8LineEditor.cpp
+++ lib/V8/V8LineEditor.cpp
@@ -28,23 +28,9 @@
 #include "V8LineEditor.h"
 #include "Utilities/ShellImplFactory.h"
 
-#ifdef TRI_HAVE_LINENOISE
-extern "C" {
-#include <linenoise.h>
-}
-#else
-#include <readline/readline.h>
-#include <readline/history.h>
-#endif
-
 #include "BasicsC/tri-strings.h"
 #include "V8/v8-utils.h"
 
-#ifndef TRI_HAVE_LINENOISE
-#if RL_READLINE_VERSION >= 0x0500
-#define completion_matches rl_completion_matches
-#endif
-#endif
 
 #include "Basics/StringUtils.h"
 
@@ -121,62 +107,62 @@ bool V8Completer::isComplete(std::string const& source, size_t lineno, size_t co
     }
     else if (state == NORMAL_1) {
       switch (*ptr) {
-      case '/':
-        state = SINGLE_COMMENT;
-        ++ptr;
-        break;
+        case '/':
+          state = SINGLE_COMMENT;
+          ++ptr;
+          break;
 
-      case '*':
-        state = MULTI_COMMENT;
-        ++ptr;
-        break;
+        case '*':
+          state = MULTI_COMMENT;
+          ++ptr;
+          break;
 
-      default:
-        state = NORMAL; // try again, do not change ptr
-        break;
+        default:
+          state = NORMAL; // try again, do not change ptr
+          break;
       }
     }
     else {
       switch (*ptr) {
-      case '"':
-        state = DOUBLE_QUOTE;
-        break;
+        case '"':
+          state = DOUBLE_QUOTE;
+          break;
 
-      case '\'':
-        state = SINGLE_QUOTE;
-        break;
+        case '\'':
+          state = SINGLE_QUOTE;
+          break;
 
-      case '/':
-        state = NORMAL_1;
-        break;
+        case '/':
+          state = NORMAL_1;
+          break;
 
-      case '(':
-        ++openParen;
-        break;
+        case '(':
+          ++openParen;
+          break;
 
-      case ')':
-        --openParen;
-        break;
+        case ')':
+          --openParen;
+          break;
 
-      case '[':
-        ++openBrackets;
-        break;
+        case '[':
+          ++openBrackets;
+          break;
 
-      case ']':
-        --openBrackets;
-        break;
+        case ']':
+          --openBrackets;
+          break;
 
-      case '{':
-        ++openBraces;
-        break;
+        case '{':
+          ++openBraces;
+          break;
 
-      case '}':
-        --openBraces;
-        break;
+        case '}':
+          --openBraces;
+          break;
 
-      case '\\':
-        ++ptr;
-        break;
+        case '\\':
+          ++ptr;
+          break;
       }
 
       ++ptr;
@@ -191,269 +177,96 @@ bool V8Completer::isComplete(std::string const& source, size_t lineno, size_t co
 ////////////////////////////////////////////////////////////////////////////////
 
 void V8Completer::getAlternatives(char const * text, vector<string> & result) {
-    // locate global object or sub-object
-    v8::Handle<v8::Object> current = v8::Context::GetCurrent()->Global();
-    string path;
-    char* prefix;
+  // locate global object or sub-object
+  v8::Handle<v8::Object> current = v8::Context::GetCurrent()->Global();
+  string path;
+  char* prefix;
 
-    if (*text != '\0') {
-      TRI_vector_string_t splitted = TRI_SplitString(text, '.');
+  if (*text != '\0') {
+    TRI_vector_string_t splitted = TRI_SplitString(text, '.');
 
-      if (1 < splitted._length) {
-        for (size_t i = 0;  i < splitted._length - 1;  ++i) {
-          v8::Handle<v8::String> name = v8::String::New(splitted._buffer[i]);
+    if (1 < splitted._length) {
+      for (size_t i = 0;  i < splitted._length - 1;  ++i) {
+        v8::Handle<v8::String> name = v8::String::New(splitted._buffer[i]);
 
-          if (! current->Has(name)) {
-            TRI_DestroyVectorString(&splitted);
-            return;
-          }
-
-          v8::Handle<v8::Value> val = current->Get(name);
-
-          if (! val->IsObject()) {
-            TRI_DestroyVectorString(&splitted);
-            return;
-          }
-
-          current = val->ToObject();
-          path = path + splitted._buffer[i] + ".";
+        if (! current->Has(name)) {
+          TRI_DestroyVectorString(&splitted);
+          return;
         }
 
-        prefix = TRI_DuplicateString(splitted._buffer[splitted._length - 1]);
-      }
-      else {
-        prefix = TRI_DuplicateString(text);
+        v8::Handle<v8::Value> val = current->Get(name);
+
+        if (! val->IsObject()) {
+          TRI_DestroyVectorString(&splitted);
+          return;
+        }
+
+        current = val->ToObject();
+        path = path + splitted._buffer[i] + ".";
       }
 
-      TRI_DestroyVectorString(&splitted);
+      prefix = TRI_DuplicateString(splitted._buffer[splitted._length - 1]);
     }
     else {
       prefix = TRI_DuplicateString(text);
     }
 
-    v8::HandleScope scope;
-
-    // compute all possible completions
-    v8::Handle<v8::Array> properties;
-    v8::Handle<v8::String> cpl = v8::String::New("_COMPLETIONS");
-
-    if (current->HasOwnProperty(cpl)) {
-      v8::Handle<v8::Value> funcVal = current->Get(cpl);
-
-      if (funcVal->IsFunction()) {
-        v8::Handle<v8::Function> func = v8::Handle<v8::Function>::Cast(funcVal);
-        v8::Handle<v8::Value> args;
-        v8::Handle<v8::Value> cpls = func->Call(current, 0, &args);
-
-        if (cpls->IsArray()) {
-          properties = v8::Handle<v8::Array>::Cast(cpls);
-        }
-      }
-    }
-    else {
-      properties = current->GetPropertyNames();
-    }
-
-    // locate
-    if (! properties.IsEmpty()) {
-      const uint32_t n = properties->Length();
-
-      for (uint32_t i = 0;  i < n;  ++i) {
-        v8::Handle<v8::Value> v = properties->Get(i);
-
-        TRI_Utf8ValueNFC str(TRI_UNKNOWN_MEM_ZONE, v);
-        char const* s = *str;
-
-        if (s != 0 && *s) {
-          string suffix = (current->Get(v)->IsFunction()) ? "()" : "";
-          string name = path + s + suffix;
-
-          if (*prefix == '\0' || TRI_IsPrefixString(s, prefix)) {
-            result.push_back(name);
-          }
-        }
-      }
-    }
-
-    TRI_FreeString(TRI_CORE_MEM_ZONE, prefix);
+    TRI_DestroyVectorString(&splitted);
   }
+  else {
+    prefix = TRI_DuplicateString(text);
+  }
+
+  v8::HandleScope scope;
+
+  // compute all possible completions
+  v8::Handle<v8::Array> properties;
+  v8::Handle<v8::String> cpl = v8::String::New("_COMPLETIONS");
+
+  if (current->HasOwnProperty(cpl)) {
+    v8::Handle<v8::Value> funcVal = current->Get(cpl);
+
+    if (funcVal->IsFunction()) {
+      v8::Handle<v8::Function> func = v8::Handle<v8::Function>::Cast(funcVal);
+      v8::Handle<v8::Value> args;
+      v8::Handle<v8::Value> cpls = func->Call(current, 0, &args);
+
+      if (cpls->IsArray()) {
+        properties = v8::Handle<v8::Array>::Cast(cpls);
+      }
+    }
+  }
+  else {
+    properties = current->GetPropertyNames();
+  }
+
+  // locate
+  if (! properties.IsEmpty()) {
+    const uint32_t n = properties->Length();
+
+    for (uint32_t i = 0;  i < n;  ++i) {
+      v8::Handle<v8::Value> v = properties->Get(i);
+
+      TRI_Utf8ValueNFC str(TRI_UNKNOWN_MEM_ZONE, v);
+      char const* s = *str;
+
+      if (s != 0 && *s) {
+        string suffix = (current->Get(v)->IsFunction()) ? "()" : "";
+        string name = path + s + suffix;
+
+        if (*prefix == '\0' || TRI_IsPrefixString(s, prefix)) {
+          result.push_back(name);
+        }
+      }
+    }
+  }
+
+  TRI_FreeString(TRI_CORE_MEM_ZONE, prefix);
+}
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                class V8LineEditor
 // -----------------------------------------------------------------------------
-
-// -----------------------------------------------------------------------------
-// --SECTION--                                                 private variables
-// -----------------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief word break characters
-////////////////////////////////////////////////////////////////////////////////
-
-#ifndef TRI_HAVE_LINENOISE
-
-static char WordBreakCharacters[] = {
-    ' ', '\t', '\n', '"', '\\', '\'', '`', '@',
-    '<', '>', '=', ';', '|', '&', '{', '}', '(', ')',
-    '\0'
-};
-
-#endif
-// -----------------------------------------------------------------------------
-// --SECTION--                                                 private functions
-// -----------------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief completion generator
-////////////////////////////////////////////////////////////////////////////////
-
-#ifndef TRI_HAVE_LINENOISE
-
-static char* CompletionGenerator (char const* text, int state) {
-  static size_t currentIndex;
-  static vector<string> result;
-
-  // compute the possible completion
-  if (state == 0) {
-    if (! v8::Context::InContext()) {
-      return 0;
-    }
-
-    // locate global object or sub-object
-    v8::Handle<v8::Object> current = v8::Context::GetCurrent()->Global();
-    string path;
-    char* prefix;
-
-    if (*text != '\0') {
-      TRI_vector_string_t splitted = TRI_SplitString(text, '.');
-
-      if (1 < splitted._length) {
-        for (size_t i = 0;  i < splitted._length - 1;  ++i) {
-          v8::Handle<v8::String> name = v8::String::New(splitted._buffer[i]);
-
-          if (! current->Has(name)) {
-            TRI_DestroyVectorString(&splitted);
-            return 0;
-          }
-
-          v8::Handle<v8::Value> val = current->Get(name);
-
-          if (! val->IsObject()) {
-            TRI_DestroyVectorString(&splitted);
-            return 0;
-          }
-
-          current = val->ToObject();
-          path = path + splitted._buffer[i] + ".";
-        }
-
-        prefix = TRI_DuplicateString(splitted._buffer[splitted._length - 1]);
-      }
-      else {
-        prefix = TRI_DuplicateString(text);
-      }
-
-      TRI_DestroyVectorString(&splitted);
-    }
-    else {
-      prefix = TRI_DuplicateString(text);
-    }
-
-    v8::HandleScope scope;
-
-    // compute all possible completions
-    v8::Handle<v8::Array> properties;
-    v8::Handle<v8::String> cpl = v8::String::New("_COMPLETIONS");
-
-    if (current->HasOwnProperty(cpl)) {
-      v8::Handle<v8::Value> funcVal = current->Get(cpl);
-
-      if (funcVal->IsFunction()) {
-        v8::Handle<v8::Function> func = v8::Handle<v8::Function>::Cast(funcVal);
-        v8::Handle<v8::Value> args;
-        v8::Handle<v8::Value> cpls = func->Call(current, 0, &args);
-
-        if (cpls->IsArray()) {
-          properties = v8::Handle<v8::Array>::Cast(cpls);
-        }
-      }
-    }
-    else {
-      properties = current->GetPropertyNames();
-    }
-
-    // locate
-    if (! properties.IsEmpty()) {
-      const uint32_t n = properties->Length();
-
-      for (uint32_t i = 0;  i < n;  ++i) {
-        v8::Handle<v8::Value> v = properties->Get(i);
-
-        TRI_Utf8ValueNFC str(TRI_UNKNOWN_MEM_ZONE, v);
-        char const* s = *str;
-
-        if (s != 0 && *s) {
-          string suffix = (current->Get(v)->IsFunction()) ? "()" : "";
-          string name = path + s + suffix;
-
-          if (*prefix == '\0' || TRI_IsPrefixString(s, prefix)) {
-            result.push_back(name);
-          }
-        }
-      }
-    }
-
-    currentIndex = 0;
-
-    TRI_FreeString(TRI_CORE_MEM_ZONE, prefix);
-  }
-
-  if (currentIndex < result.size()) {
-    return TRI_SystemDuplicateString(result[currentIndex++].c_str());
-  }
-  else {
-    result.clear();
-    return 0;
-  }
-}
-
-#endif
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief linenoise completion generator
-////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief attempted completion
-////////////////////////////////////////////////////////////////////////////////
-
-#ifdef TRI_HAVE_LINENOISE
-
-#else
-
-static char** AttemptedCompletion (char const* text, int start, int end) {
-  char** result;
-
-  result = completion_matches(text, CompletionGenerator);
-  rl_attempted_completion_over = true;
-
-  if (result != 0 && result[0] != 0 && result[1] == 0) {
-    size_t n = strlen(result[0]);
-
-    if (result[0][n - 1] == ')') {
-      result[0][n - 1] = '\0';
-    }
-  }
-
-#if RL_READLINE_VERSION >= 0x0500
-  // issue #289
-  rl_completion_suppress_append = 1;
-#endif
-
-  return result;
-}
-
-#endif
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                      constructors and destructors
@@ -472,21 +285,17 @@ V8LineEditor::V8LineEditor (v8::Handle<v8::Context> context, std::string const& 
 ////////////////////////////////////////////////////////////////////////////////
 
 V8LineEditor::~V8LineEditor () {
+  // nothing
 }
-
-void V8LineEditor::initializeShell() {
-  ShellImplFactory factory;
-  _shellImpl = factory.buildShell(_history, &_completer);
-}
-
-// -----------------------------------------------------------------------------
-// --SECTION--                                                    public methods
-// -----------------------------------------------------------------------------
-
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                 protected methods
 // -----------------------------------------------------------------------------
+//
+void V8LineEditor::initializeShell() {
+  ShellImplFactory factory;
+  _shellImpl = factory.buildShell(_history, &_completer);
+}
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                       END-OF-FILE
