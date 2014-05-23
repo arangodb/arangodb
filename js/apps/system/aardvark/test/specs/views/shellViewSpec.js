7476f9b461630aa8a6dd7c715885d0c81f11ca40
hkernbach <h.kernbach@triagens.de>
2014-05-23 16:20:22 +0200

    added jasmine spec for shell view

--- js/apps/system/aardvark/test/specs/views/shellViewSpec.js
+++ js/apps/system/aardvark/test/specs/views/shellViewSpec.js
@@ -2,13 +2,13 @@
 /*global describe, beforeEach, afterEach, it, spyOn, expect*/
 /*global arangoHelper*/
 
-/*
+
 (function() {
   "use strict";
 
   describe("The shell view", function() {
 
-    var view, div;
+    var view, div, jQueryDummy;
 
     beforeEach(function() {
       div = document.createElement("div");
@@ -17,11 +17,8 @@
 
       view = new window.shellView({
       });
+      spyOn(view, "resize");
 
-      window.height = function() {
-        return 400;
-      }
-      spyOn(window, "height");
       view.render();
     });
 
@@ -33,19 +30,61 @@
       expect(view.resizing).toEqual(false);
     });
 
-    /*
-    it("shortcut Ctrl+Z", function () {
-      spyOn(view, "resize").andCallFake({
+    it("should render a js command", function () {
+
+    });
+
+    it("should execute a js command", function () {
+      view.executeJs("1336+1");
+      var toExpect = jQuery('.jssuccess').last().children().text();
+      expect(toExpect).toContain('1337');
+      expect(toExpect).toContain('==>');
+    });
+
+    it("should use an undefined variable", function () {
+      view.executeJs("hallo");
+      var toExpect = jQuery('.jserror').last().children().text();
+      expect(toExpect).toContain('ReferenceError');
+      expect(toExpect).toContain(toExpect);
+    });
+
+    it("should execute a false js command", function () {
+      view.executeJs("1asdfkoasd234g3,.o!afdsg");
+      var toExpect = jQuery('.jserror').last().children().text();
+      expect(toExpect).toContain('SyntaxError');
+      expect(toExpect).toContain('Parse error');
+      expect(toExpect).toContain(toExpect);
+    });
+
+    it("should execute a special js command: help", function () {
+      view.executeJs("help");
+      var toExpect = jQuery('.jssuccess').last().children().text();
+      expect(toExpect).toContain('help');
+    });
+
+    it("should execute an incomplete command", function () {
+      view.executeJs("2 + ");
+      var toExpect = jQuery('.jssuccess').last().children().text();
+      expect(toExpect).toContain('...');
+    });
+
+    it("should check js shell hotkeys functionality", function () {
+      var counter = 0;
+      _.each(jqconsole.shortcuts, function(k,v) {
+        counter++;
+        _.each(k, function(x,y) {
+          x();
+        });
       });
-      var press = jQuery.Event("keypress");
-      press.ctrlKey = true;
-      press.which = 90;
-      //spyOn(window, "jqconsole");
-      spyOn(jqconsole, "AbortPrompt");
-      $("#replShell").focus();
-      $("#replShell").trigger(press);
-    });*/
+      expect(counter).toBe(4);
+    });
 
+    it("should execute a command using enter-keypress", function () {
+      var e = jQuery.Event("keydown");
+      e.which = 13; // # Some key code value
+      e.keyCode = 13
+      $(".jqconsole").trigger(e);
+    });
 
- // });
-//}()); 
+  });
+}());
