6bd29d650a13debd300053630ee200a96d2701a1
mchacki <m.hackstein@triagens.de>
2014-05-28 10:18:58 +0200

    Added unreachable view

---
+++ js/apps/system/aardvark/clusterFrontend/js/views/clusterUnreachableView.js
@@ -0,0 +1,35 @@
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true, newcap: true */
+/*global window, $, Backbone, templateEngine, plannerTemplateEngine, alert */
+
+(function() {
+  "use strict";
+  
+  window.ClusterUnreachableView = Backbone.View.extend({
+
+    el: "#content",
+
+    template: templateEngine.createTemplate("clusterUnreachable.ejs"),
+
+    events: {
+    },
+
+    initialize: function() {
+      this.coordinators = new window.ClusterCoordinators([], {
+      });
+    },
+
+
+    retryConnection: function() {
+      this.coordinators.checkConnection(function() {
+        window.App.navigate("showCluster", {trigger: true});
+      });
+      
+    },
+
+    render: function() {
+      $(this.el).html(this.template.render({}));
+      window.setTimeout(this.retryConnection.bind(this), 10000);
+    }
+
+  });
+}());
