70d1665285951ae9d3c27c84d20572e095a6338d
mchacki <m.hackstein@triagens.de>
2014-05-28 16:10:30 +0200

    Fixed cluster dashboard on server failure

--- js/apps/system/aardvark/clusterFrontend/js/views/clusterUnreachableView.js
+++ js/apps/system/aardvark/clusterFrontend/js/views/clusterUnreachableView.js
@@ -1,5 +1,5 @@
 /*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true, newcap: true */
-/*global window, $, Backbone, templateEngine, plannerTemplateEngine, alert */
+/*global window, $, Backbone, templateEngine, plannerTemplateEngine, alert, _ */
 
 (function() {
   "use strict";
@@ -9,8 +9,10 @@
     el: "#content",
 
     template: templateEngine.createTemplate("clusterUnreachable.ejs"),
+    modal: templateEngine.createTemplate("waitModal.ejs"),
 
     events: {
+      "click #clusterShutdown": "shutdown"
     },
 
     initialize: function() {
@@ -25,9 +27,31 @@
       });
     },
 
+    shutdown: function() {
+      window.clearTimeout(this.timer);
+      window.App.shutdownView.clusterShutdown();
+    },
+
     render: function() {
-      $(this.el).html(this.template.render({}));
-      window.setTimeout(this.retryConnection.bind(this), 10000);
+      var plan = window.App.clusterPlan;
+      var list = [];
+      if (plan && plan.has("runInfo")) {
+        var startServerInfos = _.where(plan.get("runInfo"), {isStartServers: true});
+        _.each(
+          _.filter(startServerInfos, function(s) {
+            return _.contains(s.roles, "Coordinator");
+          }), function(s) {
+            var name = s.endpoints[0].split("://")[1];
+            name = name.split(":")[0];
+            list.push(name);
+          }
+        );
+      }
+      $(this.el).html(this.template.render({
+        coordinators: list
+      }));
+      $(this.el).append(this.modal.render({}));
+      this.timer = window.setTimeout(this.retryConnection.bind(this), 10000);
     }
 
   });
