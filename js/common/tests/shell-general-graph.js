61db5210282380ebff7cee9e99bc5ee45e06cdf8
gschwab <g.schwab@triagens.de>
2014-06-10 13:30:07 +0200

    - added function to remove a single edge definition from a graph\n- some docu

--- js/common/tests/shell-general-graph.js
+++ js/common/tests/shell-general-graph.js
@@ -413,10 +413,49 @@ function GeneralGraphCreationSuite() {
       // This should remove edges
       assertEqual(g[rn].count(), 1);
       graph._drop(gn, true);
+    },
+
+
+    test_deleteEdgeDefinitionFromExistingGraph: function() {
+      var gN1 = "UnitTestEdgeDefDeleteGraph1",
+        gN2 = "UnitTestEdgeDefDeleteGraph2",
+        ec1 = "UnitTestEdgeDefDeleteEdgeCol1",
+        ec2 = "UnitTestEdgeDefDeleteEdgeCol2",
+        ec3 = "UnitTestEdgeDefDeleteEdgeCol3",
+        vc1 = "UnitTestEdgeDefDeleteVertexCol1",
+        vc2 = "UnitTestEdgeDefDeleteVertexCol2",
+        vc3 = "UnitTestEdgeDefDeleteVertexCol3",
+        vc4 = "UnitTestEdgeDefDeleteVertexCol4",
+        vc5 = "UnitTestEdgeDefDeleteVertexCol5";
+      try {
+        graph._drop(gN1);
+        graph._drop(gN2);
+      } catch(ignore) {
+      }
+
+      var dr1 = graph._directedRelationDefinition(ec1, [vc1], [vc1, vc2]),
+        dr2 = graph._directedRelationDefinition(ec2, [vc3], [vc4, vc5]),
+        dr3 = graph._directedRelationDefinition(ec3, [vc4], [vc5]),
+        g1 = graph._create(gN1, [dr1, dr2, dr3]),
+        g2 = graph._create(gN2, [dr3]);
+
+      g1._deleteEdgeDefinition(ec1, false);
+      assertEqual([dr2, dr3], g1.__edgeDefinitions);
+
+      g1._deleteEdgeDefinition(ec2, true);
+      assertEqual([dr3], g1.__edgeDefinitions);
+      assertTrue(db._collection(vc3) === null);
+      assertFalse(db._collection(vc4) === null);
+      assertFalse(db._collection(vc5) === null);
+
+      try {
+        graph._drop(gN1);
+        graph._drop(gN2);
+      } catch(ignore) {
+      }
+
     }
-
   };
-
 }
 
 // -----------------------------------------------------------------------------
@@ -2221,11 +2260,15 @@ function GeneralGraphCommonNeighborsSuite() {
 /// @brief executes the test suites
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
 jsunity.run(GeneralGraphCommonNeighborsSuite);
 jsunity.run(GeneralGraphAQLQueriesSuite);
 jsunity.run(EdgesAndVerticesSuite);
+*/
 jsunity.run(GeneralGraphCreationSuite);
+/*
 jsunity.run(ChainedFluentAQLResultsSuite);
+*/
 
 return jsunity.done();
 
