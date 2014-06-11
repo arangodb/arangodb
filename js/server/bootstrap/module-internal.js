2ae271e14d1c4f9612b3d3ef37ea613eea9273a0
jsteemann <j.steemann@triagens.de>
2014-06-11 16:00:27 +0200

    jslint

--- js/server/bootstrap/module-internal.js
+++ js/server/bootstrap/module-internal.js
@@ -1,7 +1,7 @@
 /*jslint indent: 2, nomen: true, maxlen: 120, sloppy: true, vars: true, white: true, plusplus: true, nonpropdel: true */
 /*global require, db, ArangoCollection, ArangoDatabase, ArangoCursor, module,
          ShapedJson, RELOAD_AUTH, SYS_DEFINE_ACTION, SYS_EXECUTE_GLOBAL_CONTEXT_FUNCTION,
-         AHUACATL_RUN, AHUACATL_PARSE, AHUACATL_EXPLAIN */
+         AHUACATL_RUN, AHUACATL_PARSE, AHUACATL_EXPLAIN, WAL_FLUSH */
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief module "internal"
