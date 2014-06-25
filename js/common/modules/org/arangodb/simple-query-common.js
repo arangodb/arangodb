c572e387fd33b4da539803bc5f9342e71ddb6041
Thomas Schmidts <Thomas.Schmidts@me.com>
2014-06-25 17:27:12 +0200

    More changes in the documentation code

--- js/common/modules/org/arangodb/simple-query-common.js
+++ js/common/modules/org/arangodb/simple-query-common.js
@@ -313,27 +313,31 @@ SimpleQuery.prototype.clone = function () {
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief executes a query
+/// @startDocuBlock executeQuery
+/// `query.execute(batchSize)`
 ///
-/// @FUN{@FA{query}.execute(@FA{batchSize})}
+/// Executes a simple query. If the optional batchSize value is specified,
+/// the server will return at most batchSize values in one roundtrip.
+/// The batchSize cannot be adjusted after the query is first executed.
 ///
-/// Executes a simple query. If the optional @FA{batchSize} value is specified,
-/// the server will return at most @FN{batchSize} values in one roundtrip.
-/// The @FA{batchSize} cannot be adjusted after the query is first executed.
-///
-/// Note that there is no need to explicitly call the execute method if another
+/// **Note**: There is no need to explicitly call the execute method if another
 /// means of fetching the query results is chosen. The following two approaches
 /// lead to the same result:
-/// @code
+/// 
+/// @EXAMPLE_ARANGOSH_OUTPUT{executeQuery}
 /// result = db.users.all().toArray();
 /// q = db.users.all(); q.execute(); result = [ ]; while (q.hasNext()) { result.push(q.next()); }
-/// @endcode
+/// @END_EXAMPLE_ARANGOSH_OUTPUT
 ///
-/// The following two alternatives both use a @FA{batchSize} and return the same
+/// The following two alternatives both use a batchSize and return the same
 /// result:
-/// @code
+///
+/// @EXAMPLE_ARANGOSH_OUTPUT{executeQueryBatchSize}
 /// q = db.users.all(); q.setBatchSize(20); q.execute(); while (q.hasNext()) { print(q.next()); }
 /// q = db.users.all(); q.execute(20); while (q.hasNext()) { print(q.next()); }
-/// @endcode
+/// @END_EXAMPLE_ARANGOSH_OUTPUT
+///
+/// @endDocuBlock
 ////////////////////////////////////////////////////////////////////////////////
 
 SimpleQuery.prototype.execute = function () {
