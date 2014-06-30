52ad8fc70229647d4dc656c423338116fc9a10b6
Thomas Schmidts <thomas@arangodb.com>
2014-06-30 09:29:02 +0200

    Some changes in the documentation code for gitbook

--- js/server/modules/org/arangodb/actions.js
+++ js/server/modules/org/arangodb/actions.js
@@ -1462,7 +1462,7 @@ function badParameter (req, res, name) {
 /// `actions.resultOk(req, res, code, result, headers)`
 ///
 /// The function defines a response. *code* is the status code to
-/// return. @*result* is the result object, which will be returned as JSON
+/// return. *result* is the result object, which will be returned as JSON
 /// object in the body. *headers* is an array of headers to returned.
 /// The function adds the attribute *error* with value *false*
 /// and *code* with value *code* to the *result*.
