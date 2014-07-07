5905abfbdbb6ceae6646e2406ce0db0efa39cbbe
jsteemann <j.steemann@triagens.de>
2014-07-08 00:31:17 +0200

    small fixes

--- arangod/RestServer/ArangoServer.h
+++ arangod/RestServer/ArangoServer.h
@@ -270,7 +270,7 @@ namespace triagens {
 /// `--server.disable-authentication`
 ///
 /// Setting value to true will turn off authentication on the server side
-///	so all clients can execute any action without authorization and privilege
+/// so all clients can execute any action without authorization and privilege
 /// checks.
 ///
 /// The default value is *false*.
