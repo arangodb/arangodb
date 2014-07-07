0703809e00decfde8451f39388d15442d5d34dda
Frank Celler <frank@arangodb.com>
2014-07-07 18:46:02 +0200

    added isIP

--- lib/V8/v8-utils.cpp
+++ lib/V8/v8-utils.cpp
@@ -3007,6 +3007,30 @@ static v8::Handle<v8::Value> JS_SleepAndRequeue (const v8::Arguments& args) {
   return scope.Close(self);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// @brief isIP
+////////////////////////////////////////////////////////////////////////////////
+
+static v8::Handle<v8::Value> JS_IsIP (const v8::Arguments& args) {
+  v8::HandleScope scope;
+
+  if (args.Length() != 1) {
+    TRI_V8_EXCEPTION_USAGE(scope, "base64Decode(<value>)");
+  }
+
+  TRI_Utf8ValueNFC address(TRI_UNKNOWN_MEM_ZONE, args[0]);
+
+  if (TRI_InetPton4(*address, NULL) == TRI_ERROR_NO_ERROR) {
+    return scope.Close(v8::Number::New(4));
+  }
+  else if (TRI_InetPton6(*address, NULL) == 0) {
+    return scope.Close(v8::Number::New(6));
+  }
+  else {
+    return scope.Close(v8::Number::New(0));
+  }
+}
+
 // -----------------------------------------------------------------------------
 // --SECTION--                                                  public functions
 // -----------------------------------------------------------------------------
@@ -3482,6 +3506,7 @@ void TRI_InitV8Utils (v8::Handle<v8::Context> context,
   TRI_AddGlobalFunctionVocbase(context, "SYS_GETLINE", JS_Getline);
   TRI_AddGlobalFunctionVocbase(context, "SYS_HMAC", JS_HMAC);
   TRI_AddGlobalFunctionVocbase(context, "SYS_HTTP_STATISTICS", JS_HttpStatistics);
+  TRI_AddGlobalFunctionVocbase(context, "SYS_IS_IP", JS_IsIP);
   TRI_AddGlobalFunctionVocbase(context, "SYS_KILL_EXTERNAL", JS_KillExternal);
   TRI_AddGlobalFunctionVocbase(context, "SYS_LOAD", JS_Load);
   TRI_AddGlobalFunctionVocbase(context, "SYS_LOG", JS_Log);
@@ -3495,9 +3520,9 @@ void TRI_InitV8Utils (v8::Handle<v8::Context> context,
   TRI_AddGlobalFunctionVocbase(context, "SYS_READ64", JS_Read64);
   TRI_AddGlobalFunctionVocbase(context, "SYS_SAVE", JS_Save);
   TRI_AddGlobalFunctionVocbase(context, "SYS_SERVER_STATISTICS", JS_ServerStatistics);
-  TRI_AddGlobalFunctionVocbase(context, "SYS_SHA256", JS_Sha256);
-  TRI_AddGlobalFunctionVocbase(context, "SYS_SHA224", JS_Sha224);
   TRI_AddGlobalFunctionVocbase(context, "SYS_SHA1", JS_Sha1);
+  TRI_AddGlobalFunctionVocbase(context, "SYS_SHA224", JS_Sha224);
+  TRI_AddGlobalFunctionVocbase(context, "SYS_SHA256", JS_Sha256);
   TRI_AddGlobalFunctionVocbase(context, "SYS_SLEEP", JS_Sleep);
   TRI_AddGlobalFunctionVocbase(context, "SYS_SPRINTF", JS_SPrintF);
   TRI_AddGlobalFunctionVocbase(context, "SYS_STATUS_EXTERNAL", JS_StatusExternal);
