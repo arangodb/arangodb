b48a5f0304c8a67fac6ccf739d5957f6b6f80f61
gschwab <g.schwab@triagens.de>
2014-07-16 09:31:08 +0200

    fixed jslint

--- js/apps/system/cerberus/cerberus.js
+++ js/apps/system/cerberus/cerberus.js
@@ -1,39 +1,43 @@
-(function() {
-  "use strict";
+/*global require, applicationContext */
+(function () {
+    "use strict";
 
-  var Foxx = require("org/arangodb/foxx"),
-    users = require("org/arangodb/users"),
-    controller = new Foxx.Controller(applicationContext),
-    url = require("url");
+    var Foxx = require("org/arangodb/foxx"),
+        users = require("org/arangodb/users"),
+        controller = new Foxx.Controller(applicationContext),
+        url = require("url"),
+        token,
+        path,
+        password;
 
-  controller.get("/initpwd/:token", function(req, res) {
-    var token = req.params("token");
-    var username = users.userByToken(token);
-    var path = url.parse(req.url).pathname.split("/");
-    path = path.slice(0, path.length - 2).join("/");
+    controller.get("/initpwd/:token", function (req, res) {
+        token = req.params("token");
+        var username = users.userByToken(token);
+        path = url.parse(req.url).pathname.split("/");
+        path = path.slice(0, path.length - 2).join("/");
 
-    if (username) {
-      res.set("Location", path + "/changePassword.html" + "?n=" + username + "&t=" + token);
-    } else {
-      res.set("Location", path + "/invalid.html");
-    }
-    
-    res.status(303);
-  });
+        if (username) {
+            res.set("Location", path + "/changePassword.html" + "?n=" + username + "&t=" + token);
+        } else {
+            res.set("Location", path + "/invalid.html");
+        }
 
-  controller.post("/checkpwd", function(req, res) {
-    var params = req.rawBody().split("&");
-    var password = params[0].split("=")[1];
-    var token = params[2].split("=")[1];
-    var path = url.parse(req.url).pathname.split("/");
-    path = path.slice(0, path.length - 1).join("/");
-    
-    if (users.changePassword(token, password)) {
-      res.set("Location", path + "/confirmed.html");
-    } else {
-      res.set("Location", path + "/invalid.html");
-    }
-    
-    res.status(303);
-  });
+        res.status(303);
+    });
+
+    controller.post("/checkpwd", function (req, res) {
+        var params = req.rawBody().split("&");
+        password = params[0].split("=")[1];
+        token = params[2].split("=")[1];
+        path = url.parse(req.url).pathname.split("/");
+        path = path.slice(0, path.length - 1).join("/");
+
+        if (users.changePassword(token, password)) {
+            res.set("Location", path + "/confirmed.html");
+        } else {
+            res.set("Location", path + "/invalid.html");
+        }
+
+        res.status(303);
+    });
 }());
