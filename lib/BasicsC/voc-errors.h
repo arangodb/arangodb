059b83b1d24b799d801450c12366eb44293b5580
Thomas Schmidts <thomas@arangodb.com>
2014-07-23 17:06:32 +0200

    Comments in .md will be deleted now

--- lib/BasicsC/voc-errors.h
+++ lib/BasicsC/voc-errors.h
@@ -8,7 +8,7 @@ extern "C" {
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @page ArangoErrors Error codes and meanings
-///
+/// @startDocuBlock errorCodes
 /// The following errors might be raised when running ArangoDB:
 ///
 /// - 0: @LIT{no error}
@@ -586,6 +586,7 @@ extern "C" {
 ///   Will be returned if the element was not found in the structure.
 /// - 20000: @LIT{newest version of app already installed}
 ///   newest version of app already installed
+/// @endDocuBlock
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
