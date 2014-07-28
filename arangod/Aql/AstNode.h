9168993b84d4ed8dd2a4c0b79aa329a77626559a
jsteemann <j.steemann@triagens.de>
2014-07-28 09:03:59 +0200

    added AstNode.cpp

--- arangod/Aql/AstNode.h
+++ arangod/Aql/AstNode.h
@@ -138,19 +138,13 @@ namespace triagens {
 /// @brief create a node
 ////////////////////////////////////////////////////////////////////////////////
 
-      AstNode (AstNodeType type) 
-        : type(type) {
-  
-        TRI_InitVectorPointer(&members, TRI_UNKNOWN_MEM_ZONE);
-      }
+      AstNode (AstNodeType); 
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief destroy the node
 ////////////////////////////////////////////////////////////////////////////////
 
-      ~AstNode () {
-        TRI_DestroyVectorPointer(&members);
-      }
+      ~AstNode ();
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                    public methods
@@ -162,81 +156,8 @@ namespace triagens {
 /// @brief convert the node into JSON
 ////////////////////////////////////////////////////////////////////////////////
 
-        void toJson (TRI_json_t* json,
-                     TRI_memory_zone_t* zone) {
-          TRI_ASSERT(TRI_IsListJson(json));
-
-          TRI_json_t* node = TRI_CreateArrayJson(zone);
-
-          if (node == nullptr) {
-            THROW_ARANGO_EXCEPTION(TRI_ERROR_OUT_OF_MEMORY);
-          }
-
-          // dump node type
-          TRI_Insert3ArrayJson(zone, node, "type", TRI_CreateStringCopyJson(zone, typeString().c_str()));
-
-          if (type == NODE_TYPE_VARIABLE ||
-              type == NODE_TYPE_REFERENCE ||
-              type == NODE_TYPE_COLLECTION ||
-              type == NODE_TYPE_PARAMETER ||
-              type == NODE_TYPE_ATTRIBUTE_ACCESS ||
-              type == NODE_TYPE_FCALL ||
-              type == NODE_TYPE_FCALL_USER) {
-            // dump "name" of node
-            TRI_Insert3ArrayJson(zone, node, "name", TRI_CreateStringCopyJson(zone, getStringValue()));
-          }
-
-          if (type == NODE_TYPE_VALUE) {
-            // dump value of "value" node
-            switch (value.type) {
-              case VALUE_TYPE_NULL:
-                TRI_Insert3ArrayJson(zone, node, "value", TRI_CreateStringCopyJson(zone, "null"));
-                break;
-              case VALUE_TYPE_BOOL:
-                TRI_Insert3ArrayJson(zone, node, "value", TRI_CreateStringCopyJson(zone, value.value._bool ? "true" : "false"));
-                break;
-              case VALUE_TYPE_INT:
-                TRI_Insert3ArrayJson(zone, node, "value", TRI_CreateNumberJson(zone, static_cast<double>(value.value._int)));
-                break;
-              case VALUE_TYPE_DOUBLE:
-                TRI_Insert3ArrayJson(zone, node, "value", TRI_CreateNumberJson(zone, value.value._int));
-                break;
-              case VALUE_TYPE_STRING:
-                TRI_Insert3ArrayJson(zone, node, "value", TRI_CreateStringCopyJson(zone, value.value._string));
-                break;
-              default: {
-              }
-            }
-          }
-         
-          // dump sub-nodes 
-          size_t const n = members._length;
-
-          if (n > 0) {
-            TRI_json_t* subNodes = TRI_CreateListJson(zone);
-
-            if (subNodes == nullptr) {
-              TRI_FreeJson(zone, node);
-              THROW_ARANGO_EXCEPTION(TRI_ERROR_OUT_OF_MEMORY);
-            }
-
-            try {
-              for (size_t i = 0; i < n; ++i) {
-                auto member = getMember(i);
-                member->toJson(subNodes, zone);
-              }
-            }
-            catch (...) {
-              TRI_FreeJson(zone, subNodes);
-              TRI_FreeJson(zone, node);
-              THROW_ARANGO_EXCEPTION(TRI_ERROR_OUT_OF_MEMORY);
-            }
-            
-            TRI_Insert3ArrayJson(zone, node, "subNodes", subNodes);
-          }
-
-          TRI_PushBack3ListJson(zone, json, node);
-        }
+        void toJson (TRI_json_t*,
+                     TRI_memory_zone_t*);
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief add a member to the node
@@ -258,7 +179,7 @@ namespace triagens {
 /// @brief add a member to the node
 ////////////////////////////////////////////////////////////////////////////////
 
-        void addMember (AstNode const* node) {
+        inline void addMember (AstNode const* node) {
           addMember(const_cast<AstNode*>(node));
         }
 
@@ -266,7 +187,7 @@ namespace triagens {
 /// @brief return a member of the node
 ////////////////////////////////////////////////////////////////////////////////
 
-        AstNode* getMember (size_t i) {
+        inline AstNode* getMember (size_t i) {
           if (i >= members._length) {
             THROW_ARANGO_EXCEPTION(TRI_ERROR_INTERNAL);
           }
@@ -277,7 +198,7 @@ namespace triagens {
 /// @brief set the node's value type
 ////////////////////////////////////////////////////////////////////////////////
 
-        void setValueType (AstNodeValueType type) {
+        inline void setValueType (AstNodeValueType type) {
           value.type = type;
         }
 
@@ -285,7 +206,7 @@ namespace triagens {
 /// @brief return the bool value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        int64_t getBoolValue () const {
+        inline int64_t getBoolValue () const {
           return value.value._bool;
         }
 
@@ -293,7 +214,7 @@ namespace triagens {
 /// @brief set the bool value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        void setBoolValue (bool v) {
+        inline void setBoolValue (bool v) {
           value.value._bool = v;
         }
 
@@ -301,7 +222,7 @@ namespace triagens {
 /// @brief return the int value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        int64_t getIntValue () const {
+        inline int64_t getIntValue () const {
           return value.value._int;
         }
 
@@ -309,7 +230,7 @@ namespace triagens {
 /// @brief set the int value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        void setIntValue (int64_t v) {
+        inline void setIntValue (int64_t v) {
           value.value._int = v;
         }
 
@@ -317,7 +238,7 @@ namespace triagens {
 /// @brief return the double value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        double getDoubleValue () const {
+        inline double getDoubleValue () const {
           return value.value._double;
         }
 
@@ -325,7 +246,7 @@ namespace triagens {
 /// @brief set the string value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        void setDoubleValue (double v) {
+        inline void setDoubleValue (double v) {
           value.value._double = v;
         }
 
@@ -333,7 +254,7 @@ namespace triagens {
 /// @brief return the string value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        char const* getStringValue () const {
+        inline char const* getStringValue () const {
           return value.value._string;
         }
 
@@ -341,7 +262,7 @@ namespace triagens {
 /// @brief set the string value of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        void setStringValue (char const* v) {
+        inline void setStringValue (char const* v) {
           value.value._string = v;
         }
 
@@ -349,111 +270,7 @@ namespace triagens {
 /// @brief return the type name of a node
 ////////////////////////////////////////////////////////////////////////////////
 
-        std::string typeString () const {
-          switch (type) {
-            case NODE_TYPE_ROOT:
-              return "root";
-            case NODE_TYPE_FOR:
-              return "for";
-            case NODE_TYPE_LET:
-              return "let";
-            case NODE_TYPE_FILTER:
-              return "filter";
-            case NODE_TYPE_RETURN:
-              return "return";
-            case NODE_TYPE_REMOVE:
-              return "remove";
-            case NODE_TYPE_INSERT:
-              return "insert";
-            case NODE_TYPE_UPDATE:
-              return "update";
-            case NODE_TYPE_REPLACE:
-              return "replace";
-            case NODE_TYPE_COLLECT:
-              return "collect";
-            case NODE_TYPE_SORT:
-              return "sort";
-            case NODE_TYPE_SORT_ELEMENT:
-              return "sort element";
-            case NODE_TYPE_LIMIT:
-              return "limit";
-            case NODE_TYPE_VARIABLE:
-              return "variable";
-            case NODE_TYPE_ASSIGN:
-              return "assign";
-            case NODE_TYPE_OPERATOR_UNARY_PLUS:
-              return "unary plus";
-            case NODE_TYPE_OPERATOR_UNARY_MINUS:
-              return "unary minus";
-            case NODE_TYPE_OPERATOR_UNARY_NOT:
-              return "unary not";
-            case NODE_TYPE_OPERATOR_BINARY_AND:
-              return "logical and";
-            case NODE_TYPE_OPERATOR_BINARY_OR:
-              return "logical or";
-            case NODE_TYPE_OPERATOR_BINARY_PLUS:
-              return "plus";
-            case NODE_TYPE_OPERATOR_BINARY_MINUS:
-              return "minus";
-            case NODE_TYPE_OPERATOR_BINARY_TIMES:
-              return "times";
-            case NODE_TYPE_OPERATOR_BINARY_DIV:
-              return "division";
-            case NODE_TYPE_OPERATOR_BINARY_MOD:
-              return "modulus";
-            case NODE_TYPE_OPERATOR_BINARY_EQ:
-              return "compare ==";
-            case NODE_TYPE_OPERATOR_BINARY_NE:
-              return "compare !=";
-            case NODE_TYPE_OPERATOR_BINARY_LT:
-              return "compare <";
-            case NODE_TYPE_OPERATOR_BINARY_LE:
-              return "compare <=";
-            case NODE_TYPE_OPERATOR_BINARY_GT:
-              return "compare >";
-            case NODE_TYPE_OPERATOR_BINARY_GE:
-              return "compare >=";
-            case NODE_TYPE_OPERATOR_BINARY_IN:
-              return "compare in";
-            case NODE_TYPE_OPERATOR_TERNARY:
-              return "ternary";
-            case NODE_TYPE_SUBQUERY:
-              return "subquery";
-            case NODE_TYPE_ATTRIBUTE_ACCESS:
-              return "attribute access";
-            case NODE_TYPE_BOUND_ATTRIBUTE_ACCESS:
-              return "bound attribute access";
-            case NODE_TYPE_INDEXED_ACCESS:
-              return "indexed access";
-            case NODE_TYPE_EXPAND:
-              return "expand";
-            case NODE_TYPE_VALUE:
-              return "value";
-            case NODE_TYPE_LIST:
-              return "list";
-            case NODE_TYPE_ARRAY:
-              return "array";
-            case NODE_TYPE_ARRAY_ELEMENT:
-              return "array element";
-            case NODE_TYPE_COLLECTION:
-              return "collection";
-            case NODE_TYPE_REFERENCE:
-              return "reference";
-            case NODE_TYPE_ATTRIBUTE:
-              return "attribute";
-            case NODE_TYPE_PARAMETER:
-              return "parameter";
-            case NODE_TYPE_FCALL:
-              return "function call";
-            case NODE_TYPE_FCALL_USER:
-              return "user function call";
-            default: {
-            }
-          }
-
-          TRI_ASSERT(false);
-          return "";
-        }
+        std::string typeString () const;
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                  public variables
