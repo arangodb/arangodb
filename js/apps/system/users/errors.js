a98509318844739d470dff53c2a046f2366091fa
Alan Plum <me@pluma.io>
2014-08-21 16:39:50 +0200

    Prettier stacks for users app errors.

--- js/apps/system/users/errors.js
+++ js/apps/system/users/errors.js
@@ -5,11 +5,17 @@
 
   function UserNotFound(uid) {
     this.message = 'User with user id ' + uid + ' not found.';
+    var err = new Error(this.message);
+    err.name = this.constructor.name;
+    this.stack = err.stack;
   }
   UserNotFound.prototype = new Error();
 
   function UsernameNotAvailable(username) {
     this.message = 'The username ' + username + ' is not available or already taken.';
+    var err = new Error(this.message);
+    err.name = this.constructor.name;
+    this.stack = err.stack;
   }
   UsernameNotAvailable.prototype = new Error();
 
