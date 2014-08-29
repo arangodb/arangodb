6b05228d2c955b968e92401dc5581dbb255fe8ed
james <james@arangodb.org>
2014-08-29 14:22:46 +0200

    "proper" estimateCost method for IndexRangeNode.

--- arangod/Aql/Optimizer.cpp
+++ arangod/Aql/Optimizer.cpp
@@ -316,7 +316,7 @@ void Optimizer::setupRules () {
   registerRule("use-index-range", useIndexRange, 710);
 
   // try to find sort blocks which are superseeded by indexes
-  registerRule("use-index-for-sort", useIndexForSort, 720);
+  //registerRule("use-index-for-sort", useIndexForSort, 720);
 
   //////////////////////////////////////////////////////////////////////////////
   /// END OF OPTIMISATIONS
