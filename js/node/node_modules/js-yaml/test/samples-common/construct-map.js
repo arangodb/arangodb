b3338217cccdf2664701ed0e858717c10e2094b9
Wilfried Goesgens <w.goesgens@arangodb.org>
2014-09-08 09:48:05 +0200

    Add YAML Parser/Dumper

---
+++ js/node/node_modules/js-yaml/test/samples-common/construct-map.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  'Block style': {
+    'Clark': 'Evans',
+    'Brian': 'Ingerson',
+    'Oren': 'Ben-Kiki'
+  },
+  'Flow style': {
+    'Clark': 'Evans',
+    'Brian': 'Ingerson',
+    'Oren': 'Ben-Kiki'
+  },
+  'foo,bar': 'baz'
+};
---
+++ js/node/node_modules/js-yaml/test/samples-common/emitting-unacceptable-unicode-character-bug.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = '\udd00';
