dac9c604bb61054f04462ea0634d66bc8db9fafd
Wilfried Goesgens <w.goesgens@arangodb.org>
2014-09-22 15:54:02 +0200

    try another place to add our performance results in the xml.

--- js/common/modules/reportgenerator.js
+++ js/common/modules/reportgenerator.js
@@ -21,25 +21,37 @@ var generatePerfReport = function (reportName, testdata, longdesc) {
     if (testdata.hasOwnProperty(testname)) {
       for (var testrunner in testdata[testname]) {
         if (testdata[testname].hasOwnProperty(testrunner)) {
-          x = x + '\t<test name="/' + testname + '/' + testrunner + '" executed="yes">\n';
-          x = x + '\t\t<description><![CDATA[This is the description of the test ' + testname + ' executed by the testrunner ' + testrunner + ']]></description>\n';
-          x = x + '\t\t<targets>\n\t\t\t<target threaded="false">AQL</target>\n\t\t</targets>\n';
-
           var oneResultSet = testdata[testname][testrunner][0].calced;
+
+
+
+
+
           var j = 0;
           for (i = 0; i < measurements.length; i++) {
             if (oneResultSet.hasOwnProperty([measurements[i]])) {
-              var s = oneResultSet[measurements[0]].sum;
-              var t = oneResultSet[measurements[0]].avg;
+              var s = oneResultSet[measurements[i]].sum;
+              var t = oneResultSet[measurements[i]].avg;
+              x = x + '\t<test name="/' + testname + '/' + testrunner + '/' + measurements[i] + '" executed="yes">\n';
+              x = x + '\t\t<description><![CDATA[This is the description of the test ' + testname + ' executed by the testrunner ' + testrunner + ' run component ' + measurements[i] + ']]></description>\n';
+              x = x + '\t\t<targets>\n\t\t\t<target threaded="false">AQL</target>\n\t\t</targets>\n';
+              
               x += '\t\t<commandline rank="' + j + '" time="20100128-195406.590832" duration="' + s + '">Sum ' + measurements[i] + '</commandline>\n';
               j += 1;
 
               x += '\t\t<commandline rank="' + j + '" time="20100128-195406.590832" duration="' + t + '">Average ' + measurements[i] + '</commandline>\n';
               j += 1;
+              
+              x = x + '\t\t<result>\n';
+              x += '\t\t\t<success passed="yes" state="100" hasTimedOut="false" />\n';
+              x += '\t\t\t<compiletime unit="s" mesure="1.0" isRelevant="false" />\n';
+              x += '\t\t\t<performance unit="GFLOPs" mesure="1.0" isRelevant="false" />\n';
+              x += '\t\t\t<executiontime unit="s" mesure="'+ oneResultSet[measurements[0]].avg + '" isRelevant="false" />\n';
+
+              x += '\t\t</result>\n';
+              x = x + '\t</test>\n';
             }
           }
-          x = x + '\t\t<result>\n\t\t\t<success passed="yes" state="100" hasTimedOut="false" />\n\t\t</result>\n';
-          x = x + '\t</test>\n';
         }
       }
     }
