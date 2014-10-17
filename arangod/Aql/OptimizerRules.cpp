32f42160053bca0aeaa02e42f765072f1395b0fc
James <james@arangodb.com>
2014-10-17 10:22:26 +0100

    updating distribute-in-cluster opt rule

--- arangod/Aql/OptimizerRules.cpp
+++ arangod/Aql/OptimizerRules.cpp
@@ -1688,7 +1688,6 @@ int triagens::aql::scatterInCluster (Optimizer* opt,
         
       // re-link with the remote node
       node->addDependency(remoteNode);
-      
 
       // insert another remote node
       remoteNode = new RemoteNode(plan, plan->nextId(), vocbase, collection, "", "", "");
@@ -1742,21 +1741,33 @@ int triagens::aql::distributeInCluster (Optimizer* opt,
     auto const nodeType = node->getType();
     
     if (nodeType != ExecutionNode::INSERT  &&
-        nodeType != ExecutionNode::UPDATE  &&
-        nodeType != ExecutionNode::REPLACE &&
         nodeType != ExecutionNode::REMOVE) {
       opt->addPlan(plan, rule->level, wasModified);
       return TRI_ERROR_NO_ERROR;
     }
+
+    // TODO check if the collection of the node is sharded or not
+    
+    Collection const* collection = static_cast<ModificationNode*>(node)->collection();
+    
+    if (nodeType == ExecutionNode::REMOVE) {
+      // check if collection shard keys are only _key
+      std::vector<std::string> shardIds = collection->shardIds();
+      std::cout << "here!\n" 
+      if (shardIds.size() != 1 || shardIds[0] != "_key") {
+        opt->addPlan(plan, rule->level, wasModified);
+        return TRI_ERROR_NO_ERROR;
+      }
+    }
+
     auto deps = node->getDependencies();
     TRI_ASSERT(deps.size() == 1);
 
     // unlink the node
     plan->unlinkNode(node, true);
 
-    // extract database and collection from plan node
+    // extract database from plan node
     TRI_vocbase_t* vocbase = static_cast<ModificationNode*>(node)->vocbase();
-    Collection const* collection = static_cast<ModificationNode*>(node)->collection();
 
     // insert a distribute node
     TRI_ASSERT(node->getVariablesUsedHere().size() == 1);
@@ -1782,6 +1793,7 @@ int triagens::aql::distributeInCluster (Optimizer* opt,
   opt->addPlan(plan, rule->level, wasModified);
   return TRI_ERROR_NO_ERROR;
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief move filters up into the cluster distribution part of the plan
 /// this rule modifies the plan in place
