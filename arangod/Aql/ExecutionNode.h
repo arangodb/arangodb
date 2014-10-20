6eb207333651bd39481e6bb39db3d329a172fd0d
Max Neunhoeffer <max@arangodb.com>
2014-10-20 09:14:01 +0200

    Rename VarOverview into RegisterPlan, because that is what it is.

--- arangod/Aql/ExecutionNode.h
+++ arangod/Aql/ExecutionNode.h
@@ -513,7 +513,7 @@ namespace triagens {
           }
         };
 
-        struct VarOverview : public WalkerWorker<ExecutionNode> {
+        struct RegisterPlan : public WalkerWorker<ExecutionNode> {
           // The following are collected for global usage in the ExecutionBlock,
           // although they are stored here in the node:
 
@@ -539,11 +539,11 @@ namespace triagens {
         private:
 
           // This is used to tell all nodes and share a pointer to ourselves
-          shared_ptr<VarOverview>* me;
+          shared_ptr<RegisterPlan>* me;
 
         public:
 
-          VarOverview ()
+          RegisterPlan ()
             : depth(0), totalNrRegs(0), me(nullptr) {
             nrRegsHere.push_back(0);
             nrRegs.push_back(0);
@@ -551,13 +551,13 @@ namespace triagens {
           
           void clear ();
 
-          void setSharedPtr (shared_ptr<VarOverview>* shared) {
+          void setSharedPtr (shared_ptr<RegisterPlan>* shared) {
             me = shared;
           }
 
           // Copy constructor used for a subquery:
-          VarOverview (VarOverview const& v, unsigned int newdepth);
-          ~VarOverview () {};
+          RegisterPlan (RegisterPlan const& v, unsigned int newdepth);
+          ~RegisterPlan () {};
 
           virtual bool enterSubquery (ExecutionNode*,
                                       ExecutionNode*) {
@@ -566,7 +566,7 @@ namespace triagens {
 
           virtual void after (ExecutionNode *eb);
 
-          VarOverview* clone(ExecutionPlan* otherPlan, ExecutionPlan* plan);
+          RegisterPlan* clone(ExecutionPlan* otherPlan, ExecutionPlan* plan);
 
         };
 
@@ -577,12 +577,12 @@ namespace triagens {
         void planRegisters (ExecutionNode* super = nullptr);
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief get varOverview
+/// @brief get RegisterPlan
 ////////////////////////////////////////////////////////////////////////////////
 
-        VarOverview const* getVarOverview () const {
-          TRI_ASSERT(_varOverview.get() != nullptr);
-          return _varOverview.get();
+        RegisterPlan const* getRegisterPlan () const {
+          TRI_ASSERT(_registerPlan.get() != nullptr);
+          return _registerPlan.get();
         }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -706,7 +706,7 @@ namespace triagens {
 /// @brief info about variables, filled in by planRegisters
 ////////////////////////////////////////////////////////////////////////////////
 
-        std::shared_ptr<VarOverview> _varOverview;
+        std::shared_ptr<RegisterPlan> _registerPlan;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief depth of the current frame, will be filled in by planRegisters
