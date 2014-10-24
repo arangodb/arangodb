a5a57ccbb7bc54d5ffa1959b0f1a246ad0a7ca9d
Jan Steemann <jan@arangodb.com>
2014-10-25 01:30:27 +0200

    updated after_failure script

--- Installation/travisCI/after_failure.sh
+++ Installation/travisCI/after_failure.sh
@@ -6,15 +6,4 @@ if [[ -f core ]]; then
   echo "thread apply all bt full" | gdb -c core bin/arangod
 fi
 
-echo
-echo "$0: compiling ArangoDB"
-
-make -j2 || exit 1
-
-echo
-echo "$0: testing ArangoDB"
-
-make jslint unittests-shell-server unittests-shell-server-ahuacatl unittests-http-server SKIP_RANGES=1 || exit 1
-
-echo
 echo "$0: done"
