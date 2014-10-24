79543efbdf418b6aeb9e9a63a27c8cdbd3007839
Jan Steemann <jan@arangodb.com>
2014-10-24 23:43:58 +0200

    issue #1063: Docs: measuring unit of --wal.logfile-size

--- arangod/Wal/LogfileManager.h
+++ arangod/Wal/LogfileManager.h
@@ -817,8 +817,8 @@ namespace triagens {
 /// @startDocuBlock WalLogfileSize
 /// `--wal.logfile-size`
 ///
-/// Specifies the filesize for each write-ahead logfile. The logfile size 
-/// should be chosen so that each logfile can store a considerable amount of
+/// Specifies the filesize (in bytes) for each write-ahead logfile. The logfile 
+/// size should be chosen so that each logfile can store a considerable amount of
 /// documents. The bigger the logfile size is chosen, the longer it will take
 /// to fill up a single logfile, which also influences the delay until the data
 /// in a logfile will be garbage-collected and written to collection journals
