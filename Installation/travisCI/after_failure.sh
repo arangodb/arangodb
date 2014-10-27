ac110e98141d59e1f78930a7556b41104ed57546
Jan Steemann <jan@arangodb.com>
2014-10-27 09:39:33 +0100

    use sudo

--- Installation/travisCI/after_failure.sh
+++ Installation/travisCI/after_failure.sh
@@ -3,7 +3,7 @@
 echo "$0: checking for core file"
 if [[ -f core ]]; then
   echo "$0: found a core file"
-  echo "thread apply all bt full" | gdb -c core bin/arangod
+  sudo echo "thread apply all bt full" | gdb -c core bin/arangod
 fi
 
 echo "$0: done"
