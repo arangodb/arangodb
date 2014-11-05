76af14e25513f398e14e8773fd718879416af7f0
mchacki <m.hackstein@triagens.de>
2014-11-05 10:16:03 +0000

    Fixed tests for statistics bar

--- js/apps/system/aardvark/frontend/js/views/statisticBarView.js
+++ js/apps/system/aardvark/frontend/js/views/statisticBarView.js
@@ -17,38 +17,41 @@
       this.currentDB = this.options.currentDB;
     },
 
+    replaceSVG: function($img) {
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+
+      $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(imgID === undefined) {
+          $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(imgClass === undefined) {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+      }, 'xml');
+    },
+
     render: function () {
+      var self = this;
       $(this.el).html(this.template.render({
         isSystem: this.currentDB.get("isSystem")
       }));
 
-      $('img.svg').each(function(){
-        var $img = $(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        $.get(imgURL, function(data) {
-          // Get the SVG tag, ignore the rest
-          var $svg = $(data).find('svg');
-
-          // Add replaced image's ID to the new SVG
-          if(imgID === undefined) {
-            $svg = $svg.attr('id', imgID);
-          }
-          // Add replaced image's classes to the new SVG
-          if(imgClass === undefined) {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-          }
-
-          // Remove any invalid XML tags as per http://validator.w3.org
-          $svg = $svg.removeAttr('xmlns:a');
-
-          // Replace image with new SVG
-          $img.replaceWith($svg);
-
-        }, 'xml');
-
+      $('img.svg').each(function() {
+        self.replaceSVG($(this));
       });
       return this;
     },
