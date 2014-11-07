6279c0aea93e9dbea87e924c402f87a30eb384e5
Heiko Kernbach <h.kernbach@triagens.de>
2014-11-07 16:05:47 +0100

    added remove prog view button

--- js/apps/system/aardvark/frontend/js/views/progressView.js
+++ js/apps/system/aardvark/frontend/js/views/progressView.js
@@ -19,11 +19,12 @@
     action: function(){},
 
     events: {
-      "click .progress-action button": "performAction"
+      "click .progress-action button": "performAction",
     },
 
     performAction: function() {
-      this.action();
+      //this.action();
+      window.progressView.hide();
     },
 
     initialize: function() {
@@ -43,9 +44,10 @@
     show: function(msg, action, button) {
       $(this.el).html(this.template.render({}));
       $(".progress-text").text(msg);
-      $(".progress-action").html(button);
-
-      this.action = action;
+      $(".progress-action").html('<button class="button-danger">Cancel</button>');
+      this.action = this.hide();
+      //$(".progress-action").html(button);
+      //this.action = action;
 
       $(this.el).show();
       //$(this.el2).html('<i class="fa fa-spinner fa-spin"></i>');
