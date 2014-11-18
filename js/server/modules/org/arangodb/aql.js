f1f13a428427f38a7917905c99d96fd9fa787675
Jan Steemann <jan@arangodb.com>
2014-11-18 11:26:42 +0100

    added several AQL functions

--- js/server/modules/org/arangodb/aql.js
+++ js/server/modules/org/arangodb/aql.js
@@ -544,8 +544,7 @@ function FCALL_USER (name, parameters) {
 
   if (UserFunctions[prefix].hasOwnProperty(name)) {
     try {
-      var result = UserFunctions[prefix][name].func.apply(null, parameters);
-      return FIX_VALUE(result);
+      return FIX_VALUE(UserFunctions[prefix][name].func.apply(null, parameters));
     }
     catch (err) {
       WARN(name, INTERNAL.errors.ERROR_QUERY_FUNCTION_RUNTIME_ERROR, AQL_TO_STRING(err));
@@ -554,6 +553,76 @@ function FCALL_USER (name, parameters) {
   }
 
   THROW(null, INTERNAL.errors.ERROR_QUERY_FUNCTION_NOT_FOUND, NORMALIZE_FNAME(name));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief dynamically call a function
+////////////////////////////////////////////////////////////////////////////////
+
+function FCALL_DYNAMIC (func, applyDirect, values, name, args) {
+  var toCall;
+
+  name = AQL_TO_STRING(name).toUpperCase();
+  if (name.indexOf('::') > 0) {
+    // user-defined function
+    var prefix = DB_PREFIX();
+    if (! UserFunctions.hasOwnProperty(prefix)) {
+      reloadUserFunctions();
+    }
+
+    if (! UserFunctions.hasOwnProperty(prefix) ||
+        ! UserFunctions[prefix].hasOwnProperty(name)) {
+      THROW(func, INTERNAL.errors.ERROR_QUERY_FUNCTION_NOT_FOUND, NORMALIZE_FNAME(name));
+    }
+
+    toCall = UserFunctions[prefix][name].func;
+  }
+  else {
+    // built-in function
+    if (name === "CALL" || name === "APPLY") {
+      THROW(func, INTERNAL.errors.ERROR_QUERY_DISALLOWED_DYNAMIC_CALL, NORMALIZE_FNAME(name));
+    }
+
+    if (! exports.hasOwnProperty("AQL_" + name)) {
+      THROW(func, INTERNAL.errors.ERROR_QUERY_FUNCTION_NOT_FOUND, NORMALIZE_FNAME(name));
+    }
+
+    toCall = exports["AQL_" + name];
+  }
+
+  if (applyDirect) {
+    try {
+      return FIX_VALUE(toCall.apply(null, args));
+    }
+    catch (err) {
+      WARN(name, INTERNAL.errors.ERROR_QUERY_FUNCTION_RUNTIME_ERROR, AQL_TO_STRING(err));
+      return null;
+    }
+  }
+
+  var type = TYPEWEIGHT(values), result, i;
+
+  if (type === TYPEWEIGHT_DOCUMENT) {
+    result = { };
+    for (i in values) {
+      if (values.hasOwnProperty(i)) {
+        args[0] = values[i];
+        result[i] = FIX_VALUE(toCall.apply(null, args));
+      }
+    }
+    return result;
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    result = [ ];
+    for (i = 0; i < values.length; ++i) {
+      args[0] = values[i];
+      result[i] = FIX_VALUE(toCall.apply(null, args));
+    }
+    return result;
+  }
+    
+  WARN(func, INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -2524,6 +2593,323 @@ function AQL_UNION_DISTINCT () {
   });
 
   return result;
+}
+/*
+////////////////////////////////////////////////////////////////////////////////
+/// @brief call a function for each element in the input list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_CALL (values, name) {
+  "use strict";
+
+  var args = [ null ], i;
+  for (i = 2; i < arguments.length; ++i) {
+    args.push(arguments[i]);
+  }
+
+  return FCALL_DYNAMIC("CALL", false, values, name, args);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief call a function for each element in the input list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_APPLY (values, name, parameters) {
+  "use strict";
+
+  var args = [ null ], i;
+  if (Array.isArray(parameters)) {
+    args = args.concat(parameters);
+  }
+
+  return FCALL_DYNAMIC("APPLY", false, values, name, args);
+}
+*/
+////////////////////////////////////////////////////////////////////////////////
+/// @brief call a function for each element in the input list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_CALL (name) {
+  "use strict";
+
+  var args = [ ], i;
+  for (i = 1; i < arguments.length; ++i) {
+    args.push(arguments[i]);
+  }
+
+  return FCALL_DYNAMIC("CALL", true, null, name, args);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief call a function for each element in the input list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_APPLY (name, parameters) {
+  "use strict";
+
+  var args = [ ], i;
+  if (Array.isArray(parameters)) {
+    args = args.concat(parameters);
+  }
+
+  return FCALL_DYNAMIC("APPLY", true, null, name, args);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief removes elements from a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_REMOVE_ALL (list, values) {
+  "use strict";
+
+  var type = TYPEWEIGHT(values);
+  if (type === TYPEWEIGHT_NULL) {
+    return list;
+  }
+  else if (type !== TYPEWEIGHT_LIST) {
+    WARN("REMOVE_ALL", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+    return null;
+  }
+
+  type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return [ ];
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    var copy = [ ], i;
+    for (i = 0; i < list.length; ++i) {
+      if (RELATIONAL_IN(list[i], values)) {
+        continue;
+      }
+      copy.push(CLONE(list[i]));
+    }
+    return copy;
+  }
+
+  WARN("REMOVE_ALL", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief removes an element from a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_REMOVE_VALUE (list, value, limit) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return [ ];
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    if (TYPEWEIGHT(limit) === TYPEWEIGHT_NULL) {
+      limit = -1;
+    }
+
+    var copy = [ ], i;
+    for (i = 0; i < list.length; ++i) {
+      if (limit === -1 && RELATIONAL_CMP(list[i], value) === 0) {
+        continue;
+      }
+      else if (limit > 0 && RELATIONAL_CMP(list[i], value) === 0) {
+        --limit;
+        continue;
+      }
+      copy.push(CLONE(list[i]));
+    }
+    return copy;
+  }
+
+  WARN("REMOVE_VALUE", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief removes an element from a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_REMOVE_NTH (list, position) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return [ ];
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    position = AQL_TO_NUMBER(position);
+    if (position >= list.length || position < - list.length) {
+      return list;
+    }
+    if (position === 0) {
+      return list.slice(1);
+    }
+    else if (position === - list.length) {
+      return list.slice(position + 1);
+    }
+    else if (position === list.length - 1) {
+      return list.slice(0, position);
+    }
+    else if (position < 0) {
+      return list.slice(0, list.length + position).concat(list.slice(list.length + position + 1));
+    }
+
+    return list.slice(0, position).concat(list.slice(position + 1));
+  }
+
+  WARN("REMOVE_NTH", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief adds an element to a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_PUSH (list, value, unique) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return [ value ];
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    if (AQL_TO_BOOL(unique)) {
+      if (RELATIONAL_IN(value, list)) {
+        return list;
+      } 
+    }
+
+    var copy = CLONE(list);
+    copy.push(value);
+    return copy;
+  }
+
+  WARN("PUSH", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief adds elements to a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_APPEND (list, values, unique) {
+  "use strict";
+
+  var type = TYPEWEIGHT(values);
+  if (type === TYPEWEIGHT_NULL) {
+    return list;
+  }
+  else if (type !== TYPEWEIGHT_LIST) {
+    WARN("APPEND", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+    return null;
+  }
+
+  if (values.length === 0) {
+    return list;
+  }
+
+  unique = AQL_TO_BOOL(unique);
+  if (values.length > 1 && unique) {
+    // make values unique themselves
+    values = AQL_UNIQUE(values);
+  }
+
+  type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return values;
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    var copy = CLONE(list);
+    if (unique) {
+      var i;
+      for (i = 0; i < values.length; ++i) {
+        if (RELATIONAL_IN(values[i], list)) {
+          continue;
+        }
+        copy.push(values[i]);
+      }
+      return copy;
+    }
+    return copy.concat(values);
+  }
+
+  WARN("APPEND", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief pops an element from a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_POP (list) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return null;
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    if (list.length === 0) {
+      return [ ];
+    }
+    var copy = CLONE(list);
+    copy.pop();
+
+    return copy;
+  }
+
+  WARN("POP", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief insert an element into a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_UNSHIFT (list, value, unique) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return [ value ];
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    if (unique) {
+      if (RELATIONAL_IN(value, list)) {
+        return list;
+      } 
+    }
+
+    var copy = CLONE(list);
+    copy.unshift(value);
+    return copy;
+  }
+
+  WARN("UNSHIFT", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief pops an element from a list
+////////////////////////////////////////////////////////////////////////////////
+
+function AQL_SHIFT (list) {
+  "use strict";
+
+  var type = TYPEWEIGHT(list);
+  if (type === TYPEWEIGHT_NULL) {
+    return null;
+  }
+  else if (type === TYPEWEIGHT_LIST) {
+    if (list.length === 0) {
+      return [ ];
+    }
+    var copy = CLONE(list);
+    copy.shift();
+
+    return copy;
+  }
+
+  WARN("SHIFT", INTERNAL.errors.ERROR_QUERY_FUNCTION_ARGUMENT_TYPE_MISMATCH);
+  return null;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -7139,6 +7525,16 @@ exports.AQL_RANGE = AQL_RANGE;
 exports.AQL_UNIQUE = AQL_UNIQUE;
 exports.AQL_UNION = AQL_UNION;
 exports.AQL_UNION_DISTINCT = AQL_UNION_DISTINCT;
+exports.AQL_CALL = AQL_CALL;
+exports.AQL_APPLY = AQL_APPLY;
+exports.AQL_REMOVE_VALUE = AQL_REMOVE_VALUE; 
+exports.AQL_REMOVE_ALL = AQL_REMOVE_ALL; 
+exports.AQL_REMOVE_NTH = AQL_REMOVE_NTH; 
+exports.AQL_PUSH = AQL_PUSH;
+exports.AQL_APPEND = AQL_APPEND;
+exports.AQL_POP = AQL_POP;
+exports.AQL_SHIFT = AQL_SHIFT;
+exports.AQL_UNSHIFT = AQL_UNSHIFT;
 exports.AQL_SLICE = AQL_SLICE;
 exports.AQL_MINUS = AQL_MINUS;
 exports.AQL_INTERSECTION = AQL_INTERSECTION;
