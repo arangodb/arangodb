e50be091c3d4c5dc04b101f93f4141433a79af94
Jan Steemann <jan@arangodb.com>
2014-12-09 09:47:28 +0100

    fixed potential memleak

--- arangod/Aql/ExecutionBlock.cpp
+++ arangod/Aql/ExecutionBlock.cpp
@@ -876,6 +876,10 @@ IndexRangeBlock::~IndexRangeBlock () {
   if (_freeCondition && _condition != nullptr) {
     delete _condition;
   }
+    
+  if (_skiplistIterator != nullptr) {
+    TRI_FreeSkiplistIterator(_skiplistIterator);
+  }
 }
 
 int IndexRangeBlock::initialize () {
@@ -1695,6 +1699,7 @@ void IndexRangeBlock::readSkiplistIndex (size_t atMost) {
   }
   catch (...) {
     TRI_FreeSkiplistIterator(_skiplistIterator);
+    _skiplistIterator = nullptr;
     throw;
   }
   LEAVE_BLOCK;
