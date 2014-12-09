da5c25b4fe53df322a55921fa26588ef7628d0b6
mchacki <m.hackstein@triagens.de>
2014-12-09 03:26:08 -0500

    Added first version of query plan viewer. Needs to be beautified. A node-only cost estimation is would be good

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/queryView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/queryView.js
@@ -649,16 +649,25 @@
         return json;
       },
 
-      drawTree: function(treeData) {
+      drawTree: function() {
+        var treeHeight = 0;
+        if (!this.treeData) {
+          return;
+        }
+        var treeData = this.treeData;
         // outputEditor.setValue(JSON.stringify(treeData, undefined, 2));
 
+        console.log($("svg#explainOutput").parent().width());
+
         var margin = {top: 20, right: 20, bottom: 20, left: 20},
-            width = 960 - margin.right - margin.left,
+            width = $("svg#explainOutput").parent().width() - margin.right - margin.left,
             height = 500 - margin.top - margin.bottom;
 
         var i = 0;
 
         var tree = d3.layout.tree().size([width, height]);
+
+        d3.select("svg#explainOutput g").remove();
 
         var svg = d3.select("svg#explainOutput")
           .attr("width", width + margin.right + margin.left)
@@ -676,7 +685,7 @@
           .projection(function(d) { return [d.x, d.y]; });
 
         // Normalize for fixed-depth.
-        nodes.forEach(function(d) { d.y = d.depth * 180 + margin.top; });
+        nodes.forEach(function(d) { d.y = d.depth * 60 + margin.top; });
 
         // Declare the nodes¦
         var node = svg.selectAll("g.node")
@@ -684,8 +693,15 @@
             if (!d.id) {
               d.id = ++i;
             }
+            if (treeHeight < d.y) {
+              treeHeight = d.y;
+            }
             return d.id;
           });
+
+          treeHeight += 60;
+          console.log("height:", treeHeight);
+          $(".query-output").height(treeHeight);
 
         // Enter the nodes.
         var nodeEnter = node.enter().append("g")
@@ -726,6 +742,10 @@
 
       },
 
+      resize: function() {
+        this.drawTree();
+      },
+
       showExplainPlan: function(plan) {
         $("svg#explainOutput").html();
         var nodes = plan.nodes;
@@ -733,7 +753,8 @@
           // Handle root element differently
           var treeData = this.preparePlanNodeEntry(nodes[0]);
           this.followQueryPath(treeData, nodes);
-          this.drawTree([treeData]);
+          this.treeData = [treeData];
+          this.drawTree();
         }
       },
 
@@ -751,11 +772,11 @@
           contentType: "application/json",
           processData: false,
           success: function (data) {
-            self.showExplainWarnings(data.warnings);
-            self.showExplainPlan(data.plan);
             if (typeof callback === "function") {
               callback();
             }
+            self.showExplainWarnings(data.warnings);
+            self.showExplainPlan(data.plan);
           },
           error: function (errObj) {
             var res = errObj.responseJSON;
