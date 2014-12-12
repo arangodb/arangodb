b84accc6527cac00c44442310113c64d876084b2
Lucas Dohmen <lucas@dohmen.io>
2014-12-12 21:38:25 +0100

    Design for applicationDetailView
    * Open and Delete App Buttons Fake implementation
    * Breadcrumb for applicationDetailView

--- js/apps/system/aardvark/frontend/js/views/applicationDetailView.js
+++ js/apps/system/aardvark/frontend/js/views/applicationDetailView.js
@@ -8,11 +8,20 @@ window.ApplicationDetailView = Backbone.View.extend({
   template: templateEngine.createTemplate('applicationDetailView.ejs'),
 
   events: {
+    'click .open': 'openApp',
+    'click .delete': 'deleteApp'
   },
 
   render: function() {
-    console.log(this.model);
     $(this.el).html(this.template.render(this.model));
     return $(this.el);
+  },
+
+  openApp: function() {
+    alert('Open!');
+  },
+
+  deleteApp: function() {
+    alert('Delete!');
   }
 });
