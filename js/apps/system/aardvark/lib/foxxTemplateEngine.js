f10ff4c1dcdcb8bd192c3852658a596889c6cdcc
Lucas Dohmen <lucas@dohmen.io>
2014-12-13 22:40:46 +0100

    First steps towards generating a Foxx app

--- js/apps/system/aardvark/lib/foxxTemplateEngine.js
+++ js/apps/system/aardvark/lib/foxxTemplateEngine.js
@@ -4,8 +4,28 @@
   var fs = require("fs");
   var _ = require("underscore");
 
-  var Engine = function(path) {
-    this._path = path;
+  var Engine = function(opts) {
+    this.applicationContext = opts.applicationContext;
+    this.path = opts.path;
+    this.name = opts.name;
+    this.collectionNames = opts.collectionNames;
+    this.authenticated = opts.authenticated;
+    this.author = opts.author;
+    this.description = opts.description;
+    this.license = opts.license;
+    this._path = this.applicationContext.foxxFilename("templates");
+    this.folder = fs.join(this.path, this.name);
+  };
+
+  Engine.prototype.write = function() {
+    fs.makeDirectory(this.folder);
+
+    fs.write(fs.join(this.folder, "manifest.json"), this.buildManifest({
+      name: this.name,
+      description: this.description,
+      author: this.author,
+      license: this.license
+    }));
   };
 
   Engine.prototype.buildManifest = function(info) {
