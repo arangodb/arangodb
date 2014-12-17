9e3474d335dc8bbb31b734c2234a51c53a4ddaf7
Frank Celler <frank@arangodb.com>
2014-12-17 09:31:55 +0100

    raised version

--- UnitTests/HttpInterface/api-compatibility-spec.rb
+++ UnitTests/HttpInterface/api-compatibility-spec.rb
@@ -16,7 +16,7 @@ describe ArangoDB do
       doc.code.should eq(200)
       compatibility = doc.parsed_response['compatibility']
       compatibility.should be_kind_of(Integer)
-      compatibility.should eq(20300)
+      compatibility.should eq(20400)
     end
     
     it "tests the compatibility value when a broken header is set" do
@@ -28,7 +28,7 @@ describe ArangoDB do
         doc.code.should eq(200)
         compatibility = doc.parsed_response['compatibility']
         compatibility.should be_kind_of(Integer)
-        compatibility.should eq(20300)
+        compatibility.should eq(20400)
       end
     end
     
