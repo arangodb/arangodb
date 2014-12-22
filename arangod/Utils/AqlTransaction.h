1782039138a3a5c5c1f7b43af27eba91fba2e419
Max Neunhoeffer <max@arangodb.com>
2014-12-22 16:21:45 +0100

    Fix cluster-wide locking for AQL.

--- arangod/Utils/AqlTransaction.h
+++ arangod/Utils/AqlTransaction.h
@@ -190,13 +190,12 @@ namespace triagens {
 
         int lockCollections () {
           auto trx = getInternals();
-          size_t i = trx->_collections._length;
 
-          while (i-- > 0) {
+          for (size_t i = 0; i < trx->_collections._length; i++) { 
             TRI_transaction_collection_t* trxCollection 
               = static_cast<TRI_transaction_collection_t*>
                            (TRI_AtVectorPointer(&trx->_collections, i));
-            int res = TRI_UnlockCollectionTransaction(trxCollection, 
+            int res = TRI_LockCollectionTransaction(trxCollection, 
                                          trxCollection->_accessType, 0);
             if (res != TRI_ERROR_NO_ERROR) {
               return res;
