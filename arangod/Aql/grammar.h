ad554d01e8c67f5e46e2eba5faad78d8d964fe94
Frank Celler <frank@arangodb.com>
2014-12-26 01:15:29 +0100

    added generated files

---
+++ arangod/Aql/grammar.h
@@ -0,0 +1,144 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_AQL_ARANGOD_AQL_GRAMMAR_HPP_INCLUDED
+# define YY_AQL_ARANGOD_AQL_GRAMMAR_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int Aqldebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    T_END = 0,
+    T_FOR = 258,
+    T_LET = 259,
+    T_FILTER = 260,
+    T_RETURN = 261,
+    T_COLLECT = 262,
+    T_SORT = 263,
+    T_LIMIT = 264,
+    T_ASC = 265,
+    T_DESC = 266,
+    T_IN = 267,
+    T_WITH = 268,
+    T_INTO = 269,
+    T_REMOVE = 270,
+    T_INSERT = 271,
+    T_UPDATE = 272,
+    T_REPLACE = 273,
+    T_NULL = 274,
+    T_TRUE = 275,
+    T_FALSE = 276,
+    T_STRING = 277,
+    T_QUOTED_STRING = 278,
+    T_INTEGER = 279,
+    T_DOUBLE = 280,
+    T_PARAMETER = 281,
+    T_ASSIGN = 282,
+    T_NOT = 283,
+    T_AND = 284,
+    T_OR = 285,
+    T_EQ = 286,
+    T_NE = 287,
+    T_LT = 288,
+    T_GT = 289,
+    T_LE = 290,
+    T_GE = 291,
+    T_PLUS = 292,
+    T_MINUS = 293,
+    T_TIMES = 294,
+    T_DIV = 295,
+    T_MOD = 296,
+    T_EXPAND = 297,
+    T_QUESTION = 298,
+    T_COLON = 299,
+    T_SCOPE = 300,
+    T_RANGE = 301,
+    T_COMMA = 302,
+    T_OPEN = 303,
+    T_CLOSE = 304,
+    T_DOC_OPEN = 305,
+    T_DOC_CLOSE = 306,
+    T_LIST_OPEN = 307,
+    T_LIST_CLOSE = 308,
+    UMINUS = 309,
+    UPLUS = 310,
+    FUNCCALL = 311,
+    REFERENCE = 312,
+    INDEXED = 313
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 22 "arangod/Aql/grammar.y" /* yacc.c:1915  */
+
+  triagens::aql::AstNode*  node;
+  char*                    strval;
+  bool                     boolval;
+  int64_t                  intval;
+
+#line 121 "arangod/Aql/grammar.hpp" /* yacc.c:1915  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+/* Location type.  */
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+};
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+
+
+int Aqlparse (triagens::aql::Parser* parser);
+
+#endif /* !YY_AQL_ARANGOD_AQL_GRAMMAR_HPP_INCLUDED  */
