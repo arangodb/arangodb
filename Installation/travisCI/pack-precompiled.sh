08e9f0cf77b0e271265ad2e8f315e7f957df0761
Frank Celler <frank@arangodb.com>
2015-01-07 19:58:19 +0100

    added compression

--- Installation/travisCI/pack-precompiled.sh
+++ Installation/travisCI/pack-precompiled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar cvf travis-precompiled.tar \
+tar cvzf precompiled-libraries.tar.gz \
   .v8-build-64 \
   3rdParty/V8-3.29.59/include \
   3rdParty/V8-3.29.59/out/x64.release/obj.target/tools/gyp \
