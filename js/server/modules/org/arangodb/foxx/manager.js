663af450a2f2bfa3d1fff953bf04078905c702e3
Frank Celler <f.celler@triagens.de>
2015-01-19 10:12:37 +0100

    actions and mounts

--- js/server/modules/org/arangodb/foxx/manager.js
+++ js/server/modules/org/arangodb/foxx/manager.js
@@ -1674,6 +1674,151 @@ var scanFoxx = function(mount, options) {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+/// @brief computes the routes of an app
+////////////////////////////////////////////////////////////////////////////////
+
+function routingAalApp (app) {
+  "use strict";
+
+  var i;
+
+  var prefix = app.collectionPrefix; // TODO
+  var defaultDocument = app._manifest.defaultDocument; // TODO by default "index.html"
+
+  // setup the routes
+  var routes = {
+    urlPrefix: mount,
+    routes: [],
+    middleware: [],
+    context: {},
+    models: {},
+
+    foxx: true,
+
+    appContext: {
+      app: app,
+      module: app._module // TODO
+    }
+  };
+
+  var p = mount;
+  var devel = app._isDevelopment;
+
+  if ((p + defaultDocument) !== p) {
+    // only add redirection if src and target are not the same
+    routes.routes.push({
+      "url" : { match: "/" },
+      "action" : {
+        "do" : "org/arangodb/actions/redirectRequest",
+        "options" : {
+          "permanently" : ! devel,
+          "destination" : defaultDocument,
+          "relative" : true
+        }
+      }
+    });
+  }
+
+  // mount all exports
+  if (app._manifest.hasOwnProperty("exports")) {
+    var exps = app._manifest.exports;
+    var result, context;
+
+    for (i in exps) {
+      if (exps.hasOwnProperty(i)) {
+        file = exps[i];
+        result = {};
+
+        // TODO ?
+        context = { exports: result };
+
+        // TODO 
+        appContext = _.extend({}, appContextTempl);
+        appContext.prefix = "/";
+
+        app.loadAppScript(appContext, file, { context: context });
+
+        app._exports[i] = result;
+      }
+    }
+  }
+
+  // mount all controllers
+  var controllers = app._manifest.controllers;
+
+  try {
+    for (i in controllers) {
+      if (controllers.hasOwnProperty(i)) {
+        file = controllers[i];
+
+        // TODO ????
+        // set up a context for the application start function
+        appContext = _.extend({}, appContextTempl);
+        appContext.prefix = arangodb.normalizeURL("/" + i); // app mount
+        appContext.routingInfo = {};
+        appContext.foxxes = [];
+
+        app.loadAppScript(appContext, file, { transform: transformScript(file) });
+
+        // .............................................................................
+        // routingInfo
+        // .............................................................................
+
+        var foxxes = appContext.foxxes;
+        var u;
+
+        for (u = 0;  u < foxxes.length;  ++u) {
+          var foxx = foxxes[u];
+          var ri = foxx.routingInfo;
+          var rm = [ "routes", "middleware" ];
+
+          var route;
+          var j;
+          var k;
+
+          _.extend(routes.models, foxx.models);
+
+          p = ri.urlPrefix;
+
+          for (k = 0;  k < rm.length;  ++k) {
+            var key = rm[k];
+
+            if (ri.hasOwnProperty(key)) {
+              var rt = ri[key];
+
+              for (j = 0;  j < rt.length;  ++j) {
+                route = rt[j];
+
+                if (route.hasOwnProperty("url")) {
+                  route.url.match = arangodb.normalizeURL(p + "/" + route.url.match);
+                }
+
+                route.context = i;
+
+                routes[key].push(route);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // install all files and assets
+    installAssets(app, routes);
+
+    // and return all routes
+    return routes;
+  }
+  catch (err) {
+    console.errorLines(
+      "Cannot compute Foxx application routes: %s", String(err.stack || err));
+    throw err;
+  }
+
+  return null;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 /// @brief Internal install function. Check install.
 /// Does not check parameters and throws errors.
 ////////////////////////////////////////////////////////////////////////////////
