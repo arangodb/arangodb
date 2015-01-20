b66796e69f9cb6925a64b0b3e8e6ae5e73766170
Willi Goesgens <willi@arangodb.com>
2015-01-20 16:48:11 +0100

    Add sysconfdir on the winxx implementation too, so we can have etc/relative installations.

--- lib/Basics/files.cpp
+++ lib/Basics/files.cpp
@@ -2182,7 +2182,11 @@ char* TRI_LocateConfigDirectory () {
   v = TRI_LocateInstallDirectory();
 
   if (v != NULL) {
+#if defined(_SYSCONFDIR_)
+    TRI_AppendString(&v, _SYSCONFDIR_);
+#else
     TRI_AppendString(&v, "etc\\arangodb\\");
+#fi
   }
 
   return v;
