cc1a52d159b2b53fd9e7afffbb16e8ef0fd383e6
Michael Hackstein <michael@arangodb.com>
2015-01-23 15:03:32 +0100

    Enabled concatenation and uglification of javascript code. If minified version is used, Foxx-Documentation is not working. Variable resolution not possible.

--- js/apps/system/aardvark/frontend/js/graphViewer/graph/expandedClass.js
+++
@@ -1,4 +0,0 @@
-window.graph.expandedClass = function(node) {
-  node.selectAll("circle") // Display nodes as circles
-    .attr("class", function(d) { return d._expanded ? "expanded" : "none"});
-}
