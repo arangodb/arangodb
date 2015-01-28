0243c4af10f9fa2af8b0b051eddee1e10a653809
Willi Goesgens <willi@arangodb.com>
2015-01-28 17:23:31 +0100

    Windows portability: socket timeouts are handled differently.

--- lib/Basics/socket-utils.h
+++ lib/Basics/socket-utils.h
@@ -200,6 +200,44 @@ static inline int TRI_setsockopt (TRI_socket_t s, int level, int optname,
 }
 #endif
 
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief setsockopt abstraction for different OSes
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef _WIN32
+static inline bool TRI_setsockopttimeout (TRI_socket_t s, double timeout) {
+  DWORD to = (DWORD) timeout * 1000;
+
+  if (TRI_setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&to, sizeof(to)) != 0) {
+    return false;
+  }
+
+  if (TRI_setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, (const char*)&to, sizeof(to)) != 0) {
+    return false;
+  }
+  return true;
+}
+#else
+static inline bool TRI_setsockopttimeout (TRI_socket_t s, double timeout) {
+  struct timeval tv;
+
+  // shut up Valgrind
+  memset(&tv, 0, sizeof(tv));
+  tv.tv_sec = (long) timeout;
+  tv.tv_usec = (long) ((timeout - (double) tv.tv_sec) * 1000000.0);
+ 
+  if (TRI_setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) != 0) {
+    return false;
+  }
+
+  if (TRI_setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) != 0) {
+    return false;
+  }
+  return true;
+}
+#endif
+
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief checks whether or not a socket is valid
 ////////////////////////////////////////////////////////////////////////////////
