1fa562ed89922c1a74fae8067f9a0bcaf9154061
Frank Celler <frank@arangodb.com>
2015-02-08 13:19:55 +0100

    use precompiled libs

--- Installation/travisCI/pack-precompiled.sh
+++ Installation/travisCI/pack-precompiled.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+V8_VERSION=3.29.59
+
 tar cvzf precompiled-libraries.tar.gz \
   .v8-build-64 \
-  3rdParty/V8-3.29.59/include \
-  3rdParty/V8-3.29.59/out/x64.release/obj.target/tools/gyp \
-  3rdParty/V8-3.29.59/third_party/icu/source/common \
-  3rdParty/V8-3.29.59/third_party/icu/source/i18n \
-  3rdParty/V8-3.29.59/third_party/icu/source/io \
-  3rdParty/V8-3.29.59/out/x64.release/obj.target/third_party/icu
+  3rdParty/V8-${V8_VERSION}/include \
+  3rdParty/V8-${V8_VERSION}/out/x64.release/obj.target/tools/gyp \
+  3rdParty/V8-${V8_VERSION}/third_party/icu/source/common \
+  3rdParty/V8-${V8_VERSION}/third_party/icu/source/i18n \
+  3rdParty/V8-${V8_VERSION}/third_party/icu/source/io \
+  3rdParty/V8-${V8_VERSION}/out/x64.release/obj.target/third_party/icu
