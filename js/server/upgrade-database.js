f84f4ed7981ea6b2612a60fbed9c0ee908b0276c
mchacki <m.hackstein@triagens.de>
2015-02-18 14:39:04 +0100

    Fixed upgrade script. Now moves and mounts dev apps correctly

--- js/server/upgrade-database.js
+++ js/server/upgrade-database.js
@@ -1461,18 +1461,20 @@ function updateGlobals() {
 
         // 2. If development mode, Zip all development APPs and create a map name => zipFile
         
-        var devPath = module.basePaths().devAppPath;
+        var devPath = module.devAppPath();
         var mapDevAppZip = {};
         var i;
         if (devPath !== "") {
           appsToZip = fs.list(devPath);
+          logger.warn("Dev Path: " + JSON.stringify(devPath));
+          logger.warn("Apps To Zip: " + JSON.stringify(appsToZip));
           for (i = 0; i < appsToZip.length; ++i) {
             path = fs.join(devPath, appsToZip[i]);
             if (fs.exists(path) && fs.isDirectory(path)) {
               try {
                 mapDevAppZip[appsToZip[i]] = fmUtils.zipDirectory(path);
               } catch (e) {
-                logger.errorLines("Tried to move app " + appsToZip[i] + " but it was not found at app-path" + path +
+                logger.errorLines("Tried to move app " + appsToZip[i] + " but it was not found at dev-app-path" + path +
                   " : " + (e.stack || String(e)));
               }
             }
