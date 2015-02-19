02854af11ba9d2dd7b53670f627bbbe94e4b451d
mchacki <m.hackstein@triagens.de>
2015-02-19 14:53:24 +0100

    Fixed a caching problem in the foxx manager

--- js/client/tests/shell-fm.js
+++ js/client/tests/shell-fm.js
@@ -63,6 +63,7 @@ function FoxxManagerSuite () {
 
     setUp: function () {
       db._useDatabase("_system");
+      fm.update();
     },
 
 ////////////////////////////////////////////////////////////////////////////////
