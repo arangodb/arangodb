db514396b14e40716635973711d984cafd0d88a7
Jan Steemann <jan@arangodb.com>
2015-02-20 15:02:57 +0100

    fix collection name generation so collection names in development mode work, too

--- js/server/modules/org/arangodb/foxx/manager.js
+++ js/server/modules/org/arangodb/foxx/manager.js
@@ -201,7 +201,8 @@ function extendContext (context, app, root) {
   }
 
   context.collectionName = function (name) {
-    var replaced = cname + name.replace(/[^a-zA-Z0-9]/g, '_').replace(/(^_+|_+$)/g, '').substr(0, 64);
+    var replaced = cname.replace(/[:\.]+/g, '_') +  
+                   name.replace(/[^a-zA-Z0-9]/g, '_').replace(/(^_+|_+$)/g, '').substr(0, 64);
 
     if (replaced.length === 0) {
       throw new Error("Cannot derive collection name from '" + name + "'");
