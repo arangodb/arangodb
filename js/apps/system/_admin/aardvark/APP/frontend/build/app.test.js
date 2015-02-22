023108cf9da3597f75a141c9be24312e68104e3d
mchacki <m.hackstein@triagens.de>
2015-02-22 21:23:03 +0100

    Added generated files in WebInterface

--- js/apps/system/_admin/aardvark/APP/frontend/build/app.test.js
+++ js/apps/system/_admin/aardvark/APP/frontend/build/app.test.js
@@ -12264,15 +12264,19 @@ window.ArangoUsers = Backbone.Collection.extend({
     },
 
     installFoxxFromStore: function(e) {
-      var mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
-      var toInstall = $(e.currentTarget).attr("appId");
-      var version = $(e.currentTarget).attr("appVersion");
-      this.collection.installFromStore({name: toInstall, version: version}, mount, this.installCallback.bind(this));
+      if (window.modalView.modalTestAll()) {
+        var mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
+        var toInstall = $(e.currentTarget).attr("appId");
+        var version = $(e.currentTarget).attr("appVersion");
+        this.collection.installFromStore({name: toInstall, version: version}, mount, this.installCallback.bind(this));
+      }
     },
 
     installFoxxFromZip: function(files, data) {
-      var mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
-      this.collection.installFromZip(data.filename, mount, this.installCallback.bind(this));
+      if (window.modalView.modalTestAll()) {
+        var mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
+        this.collection.installFromZip(data.filename, mount, this.installCallback.bind(this));
+      }
     },
 
     installCallback: function(result) {
@@ -12292,29 +12296,31 @@ window.ArangoUsers = Backbone.Collection.extend({
     },
 
     installFoxxFromGithub: function() {
-      var url, version, mount;
+      if (window.modalView.modalTestAll()) {
+        var url, version, mount;
 
-      //fetch needed information, need client side verification
-      //remove name handling on server side because not needed
-      mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
-      url = window.arangoHelper.escapeHtml($('#repository').val());
-      version = window.arangoHelper.escapeHtml($('#tag').val());
+        //fetch needed information, need client side verification
+        //remove name handling on server side because not needed
+        mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
+        url = window.arangoHelper.escapeHtml($('#repository').val());
+        version = window.arangoHelper.escapeHtml($('#tag').val());
 
-      if (version === '') {
-        version = "master";
+        if (version === '') {
+          version = "master";
+        }
+        var info = {
+          url: window.arangoHelper.escapeHtml($('#repository').val()),
+          version: window.arangoHelper.escapeHtml($('#tag').val())
+        };
+
+        try {
+          Joi.assert(url, Joi.string().regex(/^[a-zA-Z0-9_\-]+\/[a-zA-Z0-9_\-]+$/));
+        } catch (e) {
+          return;
+        }
+        //send server req through collection
+        this.collection.installFromGithub(info, mount, this.installCallback.bind(this));
       }
-      var info = {
-        url: window.arangoHelper.escapeHtml($('#repository').val()),
-        version: window.arangoHelper.escapeHtml($('#tag').val())
-      };
-
-      try {
-        Joi.assert(url, Joi.string().regex(/^[a-zA-Z0-9_\-]+\/[a-zA-Z0-9_\-]+$/));
-      } catch (e) {
-        return;
-      }
-      //send server req through collection
-      this.collection.installFromGithub(info, mount, this.installCallback.bind(this));
     },
 
     /* potential error handling
@@ -12329,23 +12335,22 @@ window.ArangoUsers = Backbone.Collection.extend({
     }
     */
 
-
-    /// NEW CODE
-
     generateNewFoxxApp: function() {
-      var mount;
-      mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
-      var info = {
-        name: window.arangoHelper.escapeHtml($("#new-app-name").val()),
-        collectionNames: _.map($('#new-app-collections').select2("data"), function(d) {
-          return window.arangoHelper.escapeHtml(d.text);
-        }),
-//        authenticated: window.arangoHelper.escapeHtml($("#new-app-name").val()),
-        author: window.arangoHelper.escapeHtml($("#new-app-author").val()),
-        license: window.arangoHelper.escapeHtml($("#new-app-license").val()),
-        description: window.arangoHelper.escapeHtml($("#new-app-description").val())
-      };
-      this.collection.generate(info, mount, this.installCallback.bind(this));
+      if (window.modalView.modalTestAll()) {
+        var mount;
+        mount = window.arangoHelper.escapeHtml($('#new-app-mount').val());
+        var info = {
+          name: window.arangoHelper.escapeHtml($("#new-app-name").val()),
+          collectionNames: _.map($('#new-app-collections').select2("data"), function(d) {
+            return window.arangoHelper.escapeHtml(d.text);
+          }),
+  //        authenticated: window.arangoHelper.escapeHtml($("#new-app-name").val()),
+          author: window.arangoHelper.escapeHtml($("#new-app-author").val()),
+          license: window.arangoHelper.escapeHtml($("#new-app-license").val()),
+          description: window.arangoHelper.escapeHtml($("#new-app-description").val())
+        };
+        this.collection.generate(info, mount, this.installCallback.bind(this));
+      }
     },
 
     addAppAction: function() {
@@ -17416,7 +17421,8 @@ window.ArangoUsers = Backbone.Collection.extend({
       var tests = _.map(this._validators, function(v) {
         return v();
       });
-      if (_.any(tests)) {
+      var invalid = _.any(tests);
+      if (invalid) {
         $('.modal-footer .button-success')
           .prop('disabled', true)
           .addClass('disabled');
@@ -17425,6 +17431,7 @@ window.ArangoUsers = Backbone.Collection.extend({
           .prop('disabled', false)
           .removeClass('disabled');
       }
+      return !invalid;
     },
 
     clearValidators: function() {
@@ -17998,8 +18005,13 @@ window.ArangoUsers = Backbone.Collection.extend({
     },
 
     getCachedQuery: function() {
-      var query =  JSON.parse(localStorage.getItem("cachedQuery"));
-      return query;
+      if(typeof(Storage) !== "undefined") {
+        var cache = localStorage.getItem("cachedQuery");
+        if (cache !== undefined) {
+          var query = JSON.parse(cache);
+          return query;
+        }
+      }
     },
 
     setCachedQuery: function(query) {
@@ -18193,10 +18205,6 @@ window.ArangoUsers = Backbone.Collection.extend({
         }
 
         if (! isUpdate) {
-          //this.customQueries.push({
-          // name: saveName,
-          // value: content
-          //});
           this.collection.add({
             name: saveName,
             value: content
