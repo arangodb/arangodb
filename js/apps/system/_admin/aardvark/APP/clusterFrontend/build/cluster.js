7590adf00fbe959798904562c7b58f4bc0c101a9
Heiko Kernbach <h.kernbach@triagens.de>
2015-03-06 15:42:36 +0100

    fixed frontend bug

--- js/apps/system/_admin/aardvark/APP/clusterFrontend/build/cluster.js
+++ js/apps/system/_admin/aardvark/APP/clusterFrontend/build/cluster.js
@@ -3478,38 +3478,6 @@ window.StatisticsDescriptionCollection = Backbone.Collection.extend({
 
 }());
 
-/*jshint browser: true */
-/*jshint unused: false */
-/*global Backbone, templateEngine, $, window, arangoHelper*/
-(function () {
-  "use strict";
-  window.NavigationView = Backbone.View.extend({
-    el: '#navigationBar',
-
-    events: {
-    },
-
-    initialize: function () {
-    },
-
-    template: templateEngine.createTemplate("clusterNavigationView.ejs"),
-
-    render: function () {
-      $(this.el).html(this.template.render());
-      console.log(123);
-    },
-
-    selectMenuItem: function (menuItem) {
-      $('.navlist li').removeClass('active');
-      if (menuItem) {
-        $('.' + menuItem).addClass('active');
-      }
-    }
-
-
-  });
-}());
-
 /*global Backbone, $, _, window, templateEngine */
 (function() {
 
