dedbf7baa3ef05e18dc8e0d96709d9c9f0ee43bc
Jan Steemann <jan@arangodb.com>
2015-03-12 11:40:46 +0100

    added documentation, fixed potential UTF-8 truncation

--- js/actions/api-query.js
+++ js/actions/api-query.js
@@ -43,6 +43,18 @@ var ArangoError = arangodb.ArangoError;
 ///
 /// @RESTHEADER{GET /_api/query/current, Returns the currently running AQL queries}
 ///
+/// Returns an array containing the AQL queries currently running in the selected
+/// database. Each query is a JSON object with the following attributes:
+///
+/// - *id*: the query's id
+///
+/// - *query*: the query string (potentially truncated)
+///
+/// - *started*: the date and time when the query was started
+///
+/// - *runTime*: the query's run time up to the point the list of queries was
+///   queried
+///
 /// @RESTRETURNCODES
 ///
 /// @RESTRETURNCODE{200}
@@ -60,6 +72,24 @@ var ArangoError = arangodb.ArangoError;
 ///
 /// @RESTHEADER{GET /_api/query/slow, Returns the list of slow AQL queries}
 ///
+/// Returns an array containing the last AQL queries that exceeded the slow 
+/// query threshold in the selected database. 
+/// The maximum amount of queries in the list can be controlled by setting
+/// the query tracking property `maxSlowQueries`. The threshold for treating
+/// a query as *slow* can be adjusted by setting the query tracking property
+/// `slowQueryThreshold`.
+///
+/// Each query is a JSON object with the following attributes:
+///
+/// - *id*: the query's id
+///
+/// - *query*: the query string (potentially truncated)
+///
+/// - *started*: the date and time when the query was started
+///
+/// - *runTime*: the query's run time up to the point the list of queries was
+///   queried
+///
 /// @RESTRETURNCODES
 ///
 /// @RESTRETURNCODE{200}
@@ -76,6 +106,31 @@ var ArangoError = arangodb.ArangoError;
 /// @brief returns the configuration for the AQL query tracking
 ///
 /// @RESTHEADER{GET /_api/query/properties, Returns the properties for the AQL query tracking}
+///
+/// Returns the current query tracking configuration. The configuration is a
+/// JSON object with the following properties:
+/// 
+/// - *enabled*: if set to *true*, then queries will be tracked. If set to 
+///   *false*, neither queries nor slow queries will be tracked.
+///
+/// - *trackSlowQueries*: if set to *true*, then slow queries will be tracked
+///   in the list of slow queries if their runtime exceeds the value set in 
+///   *slowQueryThreshold*. In order for slow queries to be tracked, the *enabled*
+///   property must also be set to *true*.
+/// 
+/// - *maxSlowQueries*: the maximum number of slow queries to keep in the list
+///   of slow queries. If the list of slow queries is full, the oldest entry in
+///   it will be discarded when additional slow queries occur.
+///
+/// - *slowQueryThreshold*: the threshold value for treating a query as slow. A
+///   query with a runtime greater or equal to this threshold value will be
+///   put into the list of slow queries when slow query tracking is enabled.
+///   The value for *slowQueryThreshold* is specified in seconds.
+///
+/// - *maxQueryStringLength*: the maximum query string length to keep in the
+///   list of queries. Query strings can have arbitrary lengths, and this property
+///   can be used to save memory in case very long query strings are used. The
+///   value is specified in bytes.
 ///
 /// @RESTRETURNCODES
 ///
@@ -151,9 +206,41 @@ function delete_api_query (req, res) {
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @startDocuBlock PutApiQueryProperties
-/// @brief chages the configuration for the AQL query tracking
+/// @brief changes the configuration for the AQL query tracking
 ///
 /// @RESTHEADER{PUT /_api/query/properties, Changes the properties for the AQL query tracking}
+///
+/// @RESTBODYPARAM{properties,json,required}
+/// The properties for query tracking in the current database. 
+///
+/// The properties need to be passed in the attribute *properties* in the body
+/// of the HTTP request. *properties* needs to be a JSON object with the following
+/// properties:
+/// 
+/// - *enabled*: if set to *true*, then queries will be tracked. If set to 
+///   *false*, neither queries nor slow queries will be tracked.
+///
+/// - *trackSlowQueries*: if set to *true*, then slow queries will be tracked
+///   in the list of slow queries if their runtime exceeds the value set in 
+///   *slowQueryThreshold*. In order for slow queries to be tracked, the *enabled*
+///   property must also be set to *true*.
+/// 
+/// - *maxSlowQueries*: the maximum number of slow queries to keep in the list
+///   of slow queries. If the list of slow queries is full, the oldest entry in
+///   it will be discarded when additional slow queries occur.
+///
+/// - *slowQueryThreshold*: the threshold value for treating a query as slow. A
+///   query with a runtime greater or equal to this threshold value will be
+///   put into the list of slow queries when slow query tracking is enabled.
+///   The value for *slowQueryThreshold* is specified in seconds.
+///
+/// - *maxQueryStringLength*: the maximum query string length to keep in the
+///   list of queries. Query strings can have arbitrary lengths, and this property
+///   can be used to save memory in case very long query strings are used. The
+///   value is specified in bytes.
+///
+/// After the properties have been changed, the current set of properties will
+/// be returned in the HTTP response.
 ///
 /// @RESTRETURNCODES
 ///
