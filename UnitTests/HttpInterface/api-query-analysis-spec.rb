1e6cb3098344e76f3dda4770038cc4a7e1f684f6
mchacki <m.hackstein@triagens.de>
2015-03-13 13:43:59 +0100

    Made the api-query-anaylsis test more relyable

--- UnitTests/HttpInterface/api-query-analysis-spec.rb
+++ UnitTests/HttpInterface/api-query-analysis-spec.rb
@@ -12,8 +12,12 @@ describe ArangoDB do
     @current = "#{@api}/current"
     @slow = "#{@api}/slow"
     @properties = "#{@api}/properties"
-    @queryBody = JSON.dump({query: "FOR x IN 1..5 LET y = SLEEP(1) RETURN x"})
-    @longQuery = JSON.dump({query: "FOR x IN 1..1 LET y = SLEEP(5) LET a = y LET b = a LET c = b LET d = c LET e = d LET f = e  RETURN x"})
+    @query = "FOR x IN 1..5 LET y = SLEEP(1) RETURN x"
+    @queryBody = JSON.dump({query: @query})
+    @fastQuery = "FOR x IN 1..1 LET y = SLEEP(0.2) RETURN x"
+    @fastQueryBody = JSON.dump({query: @fastQuery})
+    @longQuery = "FOR x IN 1..1 LET y = SLEEP(0.2) LET a = y LET b = a LET c = b LET d = c LET e = d LET f = e  RETURN x"
+    @longQueryBody = JSON.dump({query: @longQuery})
     @queryEndpoint ="/_api/cursor"
     @queryPrefix = "api-cursor"
   end
@@ -22,6 +26,24 @@ describe ArangoDB do
     count.times do
       ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => @queryBody, :headers => {"X-Arango-Async" => async })
     end
+  end
+
+  def send_fast_queries (count = 1, async = "true") 
+    count.times do
+      ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => @fastQueryBody, :headers => {"X-Arango-Async" => async })
+    end
+  end
+
+
+  def contains_query (body, testq = @query)
+      found = false
+      res = JSON.parse body
+      res.each do |q|
+        if q["query"] === testq
+          found = true
+        end
+      end
+      return found
   end
 
   it "should activate Tracking" do
@@ -43,8 +65,12 @@ describe ArangoDB do
       # Let the queries finish
       doc = ArangoDB.log_get("#{@prefix}-current", @current)
       res = JSON.parse doc.body
-      if (res.length > 0)
-        sleep 6
+      count = 0
+      while res.length > 0 && count < 10 do
+        sleep 1
+        doc = ArangoDB.log_get("#{@prefix}-current", @current)
+        res = JSON.parse doc.body
+        count += 1
       end
     end
 
@@ -52,36 +78,36 @@ describe ArangoDB do
       send_queries
       doc = ArangoDB.log_get("#{@prefix}-current", @current)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(1)
+      found = contains_query doc.body, @query
+      found.should eq(true)
     end
 
     it "should track slow queries by threshold" do
-      send_queries 1,"false"
+      send_fast_queries 1,"false"
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(0)
+      found = contains_query doc.body, @fastQuery
+      found.should eq(false)
 
-      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 2}))
+      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 0.1}))
 
-      send_queries 1,"false"
+      send_fast_queries 1,"false"
       doc = ArangoDB.log_get("#{@prefix}-current", @current)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(0)
+      found = contains_query doc.body, @fastQuery
+      found.should eq(false)
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(1)
+      found = contains_query doc.body, @fastQuery
+      found.should eq(true)
     end
 
     it "should track at most n slow queries" do 
       max = 3
-      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 2, maxSlowQueries: max}))
-      send_queries 6
+      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 0.1, maxSlowQueries: max}))
+      send_fast_queries 6
 
-      sleep 12
+      sleep 2
 
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
@@ -92,16 +118,16 @@ describe ArangoDB do
     it "should not track slow queries if turned off" do
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(0)
+      found = contains_query doc.body, @fastQuery
+      found.should eq(false)
 
-      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 2, trackSlowQueries: false}))
-      send_queries 1, "false"
+      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 0.1, trackSlowQueries: false}))
+      send_fast_queries 1, "false"
 
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
-      res = JSON.parse doc.body
-      res.length.should eq(0)
+      found = contains_query doc.body, @fastQuery
+      found.should eq(false)
     end
 
     it "should truncate the query string to at least 64 bit" do
@@ -112,40 +138,50 @@ describe ArangoDB do
     end
 
     it "should truncate the query string" do
-      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 2, maxQueryStringLength: 64}))
-      ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => @longQuery)
+      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 0.1, maxQueryStringLength: 64}))
+      ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => @longQueryBody)
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
       res = JSON.parse doc.body
       res.length.should eq(1)
       # This string is exactly 64 bit long
-      shortend = "FOR x IN 1..1 LET y = SLEEP(5) LET a = y LET b = a LET c = b LET"
-      res[0]["query"].should eq(shortend + "...")
+      shortend = "FOR x IN 1..1 LET y = SLEEP(0.2) LET a = y LET b = a LET c = b L"
+      found = contains_query doc.body, shortend + "..."
+      found.should eq(true)
     end
 
     it "should properly truncate UTF8 symbols" do
-      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 2, maxQueryStringLength: 64}))
-      ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => JSON.dump({query: 'FOR x IN 1..1 LET y = SLEEP(5) LET a = y LET b = a LET c= "ööööööö" RETURN c'}))
+      ArangoDB.log_put("#{@prefix}-properties", @properties, :body => JSON.dump({slowQueryThreshold: 0.1, maxQueryStringLength: 64}))
+      ArangoDB.log_post(@queryPrefix, @queryEndpoint, :body => JSON.dump({query: 'FOR x IN 1..1 LET y = SLEEP(0.2) LET a= y LET b= a LET c= "ööööööö" RETURN c'}))
       doc = ArangoDB.log_get("#{@prefix}-slow", @slow)
       doc.code.should eq(200)
       res = JSON.parse doc.body
       res.length.should eq(1)
       # This string is exactly 64 bit long
-      shortend = "FOR x IN 1..1 LET y = SLEEP(5) LET a = y LET b = a LET c= \"öö"
-      res[0]["query"].should eq(shortend + "...")
+      shortend = "FOR x IN 1..1 LET y = SLEEP(0.2) LET a= y LET b= a LET c= \"öö"
+      found = contains_query doc.body, shortend + "..."
+      found.should eq(true)
     end
 
     it "should be able to kill a running query" do
       send_queries
       doc = ArangoDB.log_get("#{@prefix}-current", @current)
+      found = false
       res = JSON.parse doc.body
-      res.length.should eq(1)
-      doc = ArangoDB.log_delete(@prefix, "#{@api}/" + res[0]["id"])
+      id = ""
+      res.each do |q|
+        if q["query"] === @query
+          found = true
+          id = q["id"]
+        end
+      end
+      found.should eq(true)
+      doc = ArangoDB.log_delete(@prefix, "#{@api}/" + id)
       doc.code.should eq(200)
       sleep 1
       doc = ArangoDB.log_get("#{@prefix}-current", @current)
-      res = JSON.parse doc.body
-      res.length.should eq(0)
+      found = contains_query doc.body, @query
+      found.should eq(false)
     end
 
   end
