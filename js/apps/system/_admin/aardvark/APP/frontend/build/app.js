64f01d38ecddad8757af36e8da6661de953110c8
Heiko Kernbach <h.kernbach@triagens.de>
2015-03-16 11:18:51 +0100

    fixed docs save bug in front-end

--- js/apps/system/_admin/aardvark/APP/frontend/build/app.js
+++ js/apps/system/_admin/aardvark/APP/frontend/build/app.js
@@ -103618,8 +103618,7 @@ window.ArangoUsers = Backbone.Collection.extend({
       "click #confirmDeleteDocument" : "deleteDocument",
       "click #document-from" : "navigateToDocument",
       "click #document-to" : "navigateToDocument",
-      "dblclick #documentEditor tr" : "addProperty",
-      "focusout .ace_editor": "parseInvalidJson"
+      "dblclick #documentEditor tr" : "addProperty"
     },
 
     editor: 0,
@@ -103799,12 +103798,10 @@ window.ArangoUsers = Backbone.Collection.extend({
     },
 
     saveDocument: function () {
-      var model, result, fixedJSON;
+      var model, result;
 
       try {
-        model = this.editor.getText();
-        fixedJSON = model.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
-        model = JSON.parse(fixedJSON);
+        model = this.editor.get();
       }
       catch (e) {
         this.errorConfirmation();
@@ -103878,19 +103875,6 @@ window.ArangoUsers = Backbone.Collection.extend({
     escaped: function (value) {
       return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
       .replace(/"/g, "&quot;").replace(/'/g, "&#39;");
-    },
-
-    parseInvalidJson: function() {
-      var model, fixedJSON;
-
-      try {
-        model = this.editor.getText();
-        fixedJSON = model.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
-        model = JSON.parse(fixedJSON);
-        this.editor.set(model);
-      }
-      catch (e) {
-      }
     }
 
   });
