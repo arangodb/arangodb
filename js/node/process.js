108fccdbf79d35e02e85a8159c977b8498f7459e
Willi Goesgens <willi@arangodb.com>
2015-03-20 11:55:32 +0100

    Add environment variable to V8 wrapping.

--- js/node/process.js
+++ js/node/process.js
@@ -1,4 +1,6 @@
-exports.env = {};
+/*jshint esnext: true, strict: false, unused: false, -W051: true */
+/*global require */
+exports.env = require("internal").env; 
 exports.stdout = {
   isTTY: false
 };
