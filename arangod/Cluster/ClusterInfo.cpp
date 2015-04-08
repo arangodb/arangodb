05f4e5e13e457c573aa2b46e25849eff3297df81
Max Neunhoeffer <max@arangodb.com>
2015-04-08 12:22:11 +0200

    Fix number of tries in getCollection.

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -684,7 +684,7 @@ shared_ptr<CollectionInfo> ClusterInfo::getCollection
     ++tries;
   }
 
-  while (++tries <= 2) {
+  while (true) {   // left by break
     {
       READ_LOCKER(_lock);
       // look up database by id
@@ -698,6 +698,9 @@ shared_ptr<CollectionInfo> ClusterInfo::getCollection
           return (*it2).second;
         }
       }
+    }
+    if (++tries >= 2) {
+      break;
     }
 
     // must load collections outside the lock
