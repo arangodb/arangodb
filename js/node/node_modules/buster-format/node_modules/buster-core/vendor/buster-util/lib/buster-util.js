ace8ada55c717ad4e326bd266af8bd56c57e4d4c
Alan Plum <me@pluma.io>
2015-04-10 00:20:44 +0200

    Removed unused modules.

--- js/node/node_modules/buster-format/node_modules/buster-core/vendor/buster-util/lib/buster-util.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    testCase: require("./buster-util/test-case"),
-    testDir: require("./buster-util/runner")
-};
--- js/node/node_modules/decimal/tests/vendor/require.js
+++
@@ -1,5 +0,0 @@
-function require(src) {
-    var s = document.createElement('script');
-    s.src = src;
-    document.body.appendChild(s);
-}
