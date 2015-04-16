e25072a2ce6a0c8c4102a1fe270556701d99c5f4
Jan Steemann <jsteemann@mail.ru>
2015-04-16 12:29:00 +0200

    adjusted default value for `--server.backlog-size` from 10 to 64

--- lib/HttpServer/ApplicationEndpointServer.cpp
+++ lib/HttpServer/ApplicationEndpointServer.cpp
@@ -103,7 +103,7 @@ ApplicationEndpointServer::ApplicationEndpointServer (ApplicationServer* applica
     _keepAliveTimeout(300.0),
     _defaultApiCompatibility(0),
     _allowMethodOverride(false),
-    _backlogSize(10),
+    _backlogSize(64),
     _httpsKeyfile(),
     _cafile(),
     _sslProtocol(TLS_V1),
@@ -112,6 +112,11 @@ ApplicationEndpointServer::ApplicationEndpointServer (ApplicationServer* applica
     _sslCipherList(""),
     _sslContext(nullptr),
     _rctx() {
+
+  // if our default value is too high, we'll use half of the max value provided by the system
+  if (_backlogSize > SOMAXCONN) {
+    _backlogSize = SOMAXCONN / 2;
+  }
 
   _defaultApiCompatibility = Version::getNumericServerVersion();
 }
