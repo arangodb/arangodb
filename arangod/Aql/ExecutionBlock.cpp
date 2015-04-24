745d80d9994e610c33e346efc2231eb8efd8bfdf
Jan Steemann <jan@arangodb.com>
2015-04-24 12:48:50 +0200

    fixed compile warning with g++4.9.2

--- arangod/Aql/ExecutionBlock.cpp
+++ arangod/Aql/ExecutionBlock.cpp
@@ -1080,7 +1080,6 @@ void IndexRangeBlock::buildExpressions () {
   // The following are needed to evaluate expressions with local data from
   // the current incoming item:
   AqlItemBlock* cur = _buffer.front();
-  vector<TRI_document_collection_t const*>& docColls(cur->getDocumentCollections());
         
   IndexOrCondition* newCondition = nullptr;
 
@@ -2692,8 +2691,6 @@ void CalculationBlock::fillBlockWithReference (AqlItemBlock* result) {
 ////////////////////////////////////////////////////////////////////////////////
 
 void CalculationBlock::executeExpression (AqlItemBlock* result) {
-  std::vector<TRI_document_collection_t const*>& docColls(result->getDocumentCollections());
-
   result->setDocumentCollection(_outReg, nullptr);
 
   bool const hasCondition = (static_cast<CalculationNode const*>(_exeNode)->_conditionVariable != nullptr);
