90a11c9019487d430a59fb0896ca87924a7e7702
Jan Steemann <jan@arangodb.com>
2015-05-13 21:23:55 +0200

    added Documentation for `--log.performance`

--- lib/ApplicationServer/ApplicationServer.h
+++ lib/ApplicationServer/ApplicationServer.h
@@ -754,6 +754,24 @@ namespace triagens {
         std::string _logContentFilter;
 
 ////////////////////////////////////////////////////////////////////////////////
+/// @brief performance logging
+/// @startDocuBlock logPerformance
+/// `--log.performance`
+///
+/// If this option is set, performance-related info messages will be logged via
+/// the regular logging mechanisms. These will consist of mostly timing and 
+/// debugging information for performance-critical operations.
+/// 
+/// Currently performance-related operations are logged as INFO messages.
+/// Messages starting with prefix `[action]` indicate that an instrumented
+/// operation was started (note that its end won't be logged). Messages with
+/// prefix `[timer]` will contain timing information for operations. Note that
+/// no timing information will be logged for operations taking less time than
+/// 1 second. This is to ensure that sub-second operations do not pollute logs.
+/// @endDocuBlock
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 /// @brief random number generator to use
 /// @startDocuBlock randomGenerator
 /// `--random.generator arg`
