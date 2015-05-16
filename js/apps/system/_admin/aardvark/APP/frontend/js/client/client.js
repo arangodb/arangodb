b782d0ebef710b6ff0d70fb3ee100e6ec34f2f98
Frank Celler <frank@arangodb.com>
2015-05-16 22:56:01 +0200

    release version 2.6.0-alpha1

--- js/apps/system/_admin/aardvark/APP/frontend/js/client/client.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/client/client.js
@@ -141,8 +141,7 @@ global.tutorial = require("org/arangodb/tutorial");
 /// @brief prints help
 ////////////////////////////////////////////////////////////////////////////////
 
-window.initHelp = function() {
-  "use strict";
+(function() {
   var internal = require("internal");
 
   if (internal.db) {
@@ -193,7 +192,7 @@ window.initHelp = function() {
     delete global.IS_UNIT_TESTS;
     delete global.IS_JS_LINT;
   } catch (e) {}
-};
+}());
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                       END-OF-FILE
