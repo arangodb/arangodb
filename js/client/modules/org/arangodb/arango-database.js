df57e47c33accfa5851c504b47703b90b273713b
Max Neunhoeffer <max@arangodb.com>
2015-05-18 01:39:54 -0700

    Fix configurability of indexBuckets.

--- js/client/modules/org/arangodb/arango-database.js
+++ js/client/modules/org/arangodb/arango-database.js
@@ -314,7 +314,7 @@ ArangoDatabase.prototype._create = function (name, properties, type) {
   if (properties !== undefined) {
     [ "waitForSync", "journalSize", "isSystem", "isVolatile",
       "doCompact", "keyOptions", "shardKeys", "numberOfShards",
-      "distributeShardsLike" ].forEach(function(p) {
+      "distributeShardsLike", "indexBuckets" ].forEach(function(p) {
       if (properties.hasOwnProperty(p)) {
         body[p] = properties[p];
       }
