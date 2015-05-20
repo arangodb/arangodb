83970b31cc93ba95196ce9d86826f4e6a2e5ff38
Alan Plum <me@pluma.io>
2015-05-20 16:27:18 +0200

    Restored setup script.

---
+++ js/apps/system/_system/sessions/APP/setup.js
@@ -0,0 +1,8 @@
+/*global applicationContext */
+'use strict';
+var db = require('org/arangodb').db;
+var collectionName = '_sessions';
+
+if (db._collection(collectionName) === null) {
+  db._create(collectionName, {isSystem: true});
+}
