c772d550a35bddbf6d9ea1ea0fb84d1f8087253a
Alan Plum <me@pluma.io>
2015-05-20 15:10:23 +0200

    De-IIFE users and simple-auth.

--- js/apps/system/_system/simple-auth/APP/auth.js
+++ js/apps/system/_system/simple-auth/APP/auth.js
@@ -1,28 +1,26 @@
 /*global applicationContext */
-(function () {
-  'use strict';
-  var crypto = require('org/arangodb/crypto'),
-    cfg = applicationContext.configuration;
+'use strict';
+var crypto = require('org/arangodb/crypto'),
+  cfg = applicationContext.configuration;
 
-  function verifyPassword(authData, password) {
-    if (!authData) {
-      authData = {};
-    }
-    var hashMethod = authData.method || cfg.hashMethod,
-      salt = authData.salt || '',
-      storedHash = authData.hash || '',
-      generatedHash = crypto[hashMethod](salt + password);
-    // non-lazy comparison to avoid timing attacks
-    return crypto.constantEquals(storedHash, generatedHash);
+function verifyPassword(authData, password) {
+  if (!authData) {
+    authData = {};
   }
+  var hashMethod = authData.method || cfg.hashMethod,
+    salt = authData.salt || '',
+    storedHash = authData.hash || '',
+    generatedHash = crypto[hashMethod](salt + password);
+  // non-lazy comparison to avoid timing attacks
+  return crypto.constantEquals(storedHash, generatedHash);
+}
 
-  function hashPassword(password) {
-    var hashMethod = cfg.hashMethod,
-      salt = crypto.genRandomAlphaNumbers(cfg.saltLength),
-      hash = crypto[hashMethod](salt + password);
-    return {method: hashMethod, salt: salt, hash: hash};
-  }
+function hashPassword(password) {
+  var hashMethod = cfg.hashMethod,
+    salt = crypto.genRandomAlphaNumbers(cfg.saltLength),
+    hash = crypto[hashMethod](salt + password);
+  return {method: hashMethod, salt: salt, hash: hash};
+}
 
-  exports.verifyPassword = verifyPassword;
-  exports.hashPassword = hashPassword;
-}());
+exports.verifyPassword = verifyPassword;
+exports.hashPassword = hashPassword;
