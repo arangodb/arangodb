91ee0bf0601ab7a49f286d9ad6e7fc8b47a9efaf
Jan Steemann <jan@arangodb.com>
2015-06-03 15:44:10 +0200

    updated documentation for graph functions

--- js/server/modules/org/arangodb/aql.js
+++ js/server/modules/org/arangodb/aql.js
@@ -6684,26 +6684,33 @@ function AQL_NEIGHBORS (vertexCollection,
 ///
 /// * *graphName*          : The name of the graph as a string.
 /// * *vertexExample*      : An example for the desired
-/// vertices (see [example](#short_explanation_of_the_example_parameter)).
+///                          vertices (see [example](#short_explanation_of_the_example_parameter)).
 /// * *options*            : An object containing the following options:
 ///   * *direction*                        : The direction
-///      of the edges. Possible values are *outbound*, *inbound* and *any* (default).
+///     of the edges. Possible values are *outbound*, *inbound* and *any* (default).
 ///   * *edgeExamples*                     : A filter example for the edges to
-///      the neighbors (see [example](#short_explanation_of_the_example_parameter)).
+///     the neighbors (see [example](#short_explanation_of_the_example_parameter)).
 ///   * *neighborExamples*                 : An example for the desired neighbors
-///      (see [example](#short_explanation_of_the_example_parameter)).
+///     (see [example](#short_explanation_of_the_example_parameter)).
 ///   * *edgeCollectionRestriction*        : One or multiple edge
 ///   collection names. Only edges from these collections will be considered for the path.
 ///   * *vertexCollectionRestriction* : One or multiple vertex
-///   collection names. Only vertices from these collections will be contained in the
+///     collection names. Only vertices from these collections will be contained in the
 ///   result. This does not effect vertices on the path.
 ///   * *minDepth*                         : Defines the minimal
-///      depth a path to a neighbor must have to be returned (default is 1).
+///     depth a path to a neighbor must have to be returned (default is 1).
 ///   * *maxDepth*                         : Defines the maximal
-///      depth a path to a neighbor must have to be returned (default is 1).
+///     depth a path to a neighbor must have to be returned (default is 1).
 ///   * *maxIterations*: the maximum number of iterations that the traversal is
-///      allowed to perform. It is sensible to set this number so unbounded traversals
-/// will terminate at some point.
+///     allowed to perform. It is sensible to set this number so unbounded traversals
+///     will terminate at some point.
+///   * *includeData* is a boolean value to define if the returned documents should be extracted 
+///     instead of returning their ids only. The default is *false*.
+///
+/// Note: in ArangoDB versions prior to 2.6 *NEIGHBORS* returned the array of neighbor vertices with 
+/// all attributes and not just the vertex ids. To return to the same behavior, set the *includeData*
+/// option to *true* in 2.6 and above.
+///
 /// @EXAMPLES
 ///
 /// A route planner example, all neighbors of locations with a distance of either
