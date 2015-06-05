665fa43f297060d92272730491e55f73c117a1f9
Jan Steemann <jsteemann@mail.ru>
2015-06-05 09:21:54 +0200

    fixed link

--- js/client/modules/org/arangodb/foxx/manager.js
+++ js/client/modules/org/arangodb/foxx/manager.js
@@ -194,7 +194,7 @@ var help = function () {
   arangodb.print();
   arangodb.print("Use foxx-manager --help to show a list of global options\n");
   arangodb.print("There is also an online manual available at:");
-  arangodb.print("https://docs.arangodb.com/FoxxManager/README.html");
+  arangodb.print("https://docs.arangodb.com/Foxx/Install/index.html");
 
   // additional newline
   arangodb.print();
