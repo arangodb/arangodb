c4403671ae492c016941e46393d333eae1a59cc7
Michael Hackstein <michael@arangodb.com>
2015-06-15 13:57:58 +0200

    Fixed a bug in AQL Neighbors. The edge examples were not propagated properly. Also added the first error test in example matcher

--- js/server/tests/aql-graph.js
+++ js/server/tests/aql-graph.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, sub: true, maxlen: 500 */
-/*global assertEqual, assertTrue */
+/*global assertEqual, assertTrue, fail */
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief tests for query language, graph functions
@@ -30,7 +30,8 @@
 
 var jsunity = require("jsunity");
 var db = require("org/arangodb").db;
-var errors = require("internal").errors;
+var internal = require("internal");
+var errors = internal.errors;
 var helper = require("org/arangodb/aql-helper");
 var getQueryResults = helper.getQueryResults;
 var getRawQueryResults = helper.getRawQueryResults;
@@ -1654,6 +1655,80 @@ function ahuacatlQueryTraversalTreeTestSuite () {
 }  
 
 ////////////////////////////////////////////////////////////////////////////////
+/// @brief test suite for Neighbors with intentional failures
+////////////////////////////////////////////////////////////////////////////////
+
+function ahuacatlQueryNeighborsErrorsSuite () {
+  var vn = "UnitTestsTraversalVertices";
+  var en = "UnitTestsTraversalEdges";
+  var vertexCollection;
+  var edgeCollection;
+
+  return {
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief set up
+////////////////////////////////////////////////////////////////////////////////
+
+    setUp : function () {
+      db._drop(vn);
+      db._drop(en);
+      internal.debugClearFailAt();
+
+      vertexCollection = db._create(vn);
+      edgeCollection = db._createEdgeCollection(en);
+
+      [ "A", "B", "C", "D" ].forEach(function (item) {
+        vertexCollection.save({ _key: item, name: item });
+      });
+
+      [ [ "A", "B" ], [ "B", "C" ], [ "A", "D" ], [ "D", "C" ], [ "C", "A" ] ].forEach(function (item) {
+        var l = item[0];
+        var r = item[1];
+        edgeCollection.save(vn + "/" + l, vn + "/" + r, { _key: l + r, what : l + "->" + r });
+      });
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief tear down
+////////////////////////////////////////////////////////////////////////////////
+
+    tearDown : function () {
+      db._drop(vn);
+      db._drop(en);
+      internal.debugClearFailAt();
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief checks error handling in NEIGHBORS()
+////////////////////////////////////////////////////////////////////////////////
+
+    testNeighborsErrors : function () {
+      var v1 = vn + "/A";
+      var v2 = vn + "/B";
+
+      var queryStart = "FOR n IN NEIGHBORS(" + vn + " , " + en + ", '";
+      var queryEnd = "', 'outbound', [{_id: '" + en + "/AB'}]) SORT n RETURN n";
+
+      var actual = getQueryResults(queryStart + v1 + queryEnd);
+      // Positive Check
+      assertEqual(actual, [ v2 ]);
+
+      internal.debugSetFailAt("ExampleNoContextVocbase");
+
+      // Negative Check
+      try {
+        actual = getQueryResults(queryStart + v1 + queryEnd);
+        fail();
+      } catch (e) {
+        assertEqual(e.errorNum, errors.ERROR_ARANGO_DATABASE_NOT_FOUND.code);
+      }
+    }
+
+  };
+}
+
+////////////////////////////////////////////////////////////////////////////////
 /// @brief executes the test suite
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -1663,6 +1738,9 @@ jsunity.run(ahuacatlQueryShortestPathTestSuite);
 jsunity.run(ahuacatlQueryTraversalFilterTestSuite);
 jsunity.run(ahuacatlQueryTraversalTestSuite);
 jsunity.run(ahuacatlQueryTraversalTreeTestSuite);
+if (internal.debugCanUseFailAt()) {
+  jsunity.run(ahuacatlQueryNeighborsErrorsSuite);
+}
 
 return jsunity.done();
 
