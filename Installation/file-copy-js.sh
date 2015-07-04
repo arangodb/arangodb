feef4da9ace4a870f4dcf4f25989eb65363a46d2
Frank Celler <frank@arangodb.com>
2015-07-04 21:50:49 +0200

    fixed packaging of JS files

---
+++ Installation/file-copy-js.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+SRCDIR=$1
+DSTDIR=$2
+
+${SRCDIR}/Installation/file-list-js.sh ${SRCDIR} | while read a; do
+  if test -f "$DSTDIR/$a";  then
+    echo "$DSTDIR/$a: already exists, giving up"
+    exit 1
+  fi
+
+  FROM="${SRCDIR}/$a"
+  TO="${DSTDIR}/$a"
+  DIR=`dirname "${TO}"`
+  
+  test -d "${DIR}" || mkdir -p "${DIR}"
+  cp -n "${FROM}" "${TO}"
+done
