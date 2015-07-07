11d52b25b3149e3051d82b22212caaed11060e0b
Alan Plum <me@pluma.io>
2015-07-07 12:25:31 +0200

    Cleaned up users app. Added missing error names.

--- js/apps/system/_system/sessions/APP/errors.js
+++ js/apps/system/_system/sessions/APP/errors.js
@@ -1,19 +1,34 @@
 'use strict';
 function SessionNotFound(sid) {
   this.message = 'Session with session id ' + sid + ' not found.';
-  this.name = this.constructor.name;
+  this.name = this.name;
   Error.captureStackTrace(this, SessionNotFound);
 }
 SessionNotFound.prototype = new Error();
 SessionNotFound.prototype.constructor = SessionNotFound;
+Object.defineProperty(SessionNotFound.prototype, 'name', {
+  enumerable: true,
+  configurable: true,
+  get: function () {
+    return this.constructor.name;
+  }
+});
 
 function SessionExpired(sid) {
   this.message = 'Session with session id ' + sid + ' has expired.';
-  this.name = this.constructor.name;
+  this.name = this.name;
   Error.captureStackTrace(this, SessionExpired);
 }
 SessionExpired.prototype = Object.create(SessionNotFound.prototype);
 SessionExpired.prototype.constructor = SessionExpired;
+SessionExpired.prototype.name = SessionExpired.name;
+Object.defineProperty(SessionExpired.prototype, 'name', {
+  enumerable: true,
+  configurable: true,
+  get: function () {
+    return this.constructor.name;
+  }
+});
 
 exports.SessionNotFound = SessionNotFound;
 exports.SessionExpired = SessionExpired;
--- js/apps/system/_system/users/APP/errors.js
+++ js/apps/system/_system/users/APP/errors.js
@@ -1,21 +1,35 @@
-(function () {
-  'use strict';
-  function UserNotFound(uid) {
-    this.message = 'User with user id ' + uid + ' not found.';
-    var err = new Error(this.message);
-    err.name = this.constructor.name;
-    this.stack = err.stack;
+'use strict';
+function UserNotFound(uid) {
+  this.message = 'User with user id ' + uid + ' not found.';
+  var err = new Error(this.message);
+  err.name = this.name;
+  this.stack = err.stack;
+}
+UserNotFound.prototype = new Error();
+UserNotFound.prototype.constructor = UserNotFound;
+Object.defineProperty(UserNotFound.prototype, 'name', {
+  enumerable: true,
+  configurable: true,
+  get: function () {
+    return this.constructor.name;
   }
-  UserNotFound.prototype = new Error();
+});
 
-  function UsernameNotAvailable(username) {
-    this.message = 'The username ' + username + ' is not available or already taken.';
-    var err = new Error(this.message);
-    err.name = this.constructor.name;
-    this.stack = err.stack;
+function UsernameNotAvailable(username) {
+  this.message = 'The username ' + username + ' is not available or already taken.';
+  var err = new Error(this.message);
+  err.name = this.name;
+  this.stack = err.stack;
+}
+UsernameNotAvailable.prototype = new Error();
+UsernameNotAvailable.prototype.constructor = UsernameNotAvailable;
+Object.defineProperty(UsernameNotAvailable.prototype, 'name', {
+  enumerable: true,
+  configurable: true,
+  get: function () {
+    return this.constructor.name;
   }
-  UsernameNotAvailable.prototype = new Error();
+});
 
-  exports.UserNotFound = UserNotFound;
-  exports.UsernameNotAvailable = UsernameNotAvailable;
-}());
+exports.UserNotFound = UserNotFound;
+exports.UsernameNotAvailable = UsernameNotAvailable;
