cba613214980e3d76f9de8d98987183d5719dbc3
Michael Hackstein <michael@arangodb.com>
2015-08-05 17:05:06 +0200

    Implemented a new AttributeNameParser this is able to identify [*] expansion and shall be used for array indexes. Also includes tests.

---
+++ UnitTests/Basics/AttributeNameParserTest.cpp
@@ -0,0 +1,221 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test suite for PathEnumerator class
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2004-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+/// @author Copyright 2015, ArangoDB GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+#include <boost/test/unit_test.hpp>
+
+#include "Basics/AttributeNameParser.h"
+#include "Basics/Exceptions.h"
+
+using namespace triagens;
+using namespace triagens::basics;
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                 setup / tear-down
+// -----------------------------------------------------------------------------
+
+struct AttributeNameParserSetup {
+  AttributeNameParserSetup () {
+    BOOST_TEST_MESSAGE("setup AttributeNameParser");
+  }
+
+  ~AttributeNameParserSetup () {
+    BOOST_TEST_MESSAGE("tear-down AttributeNameParser");
+  }
+};
+
+
+// -----------------------------------------------------------------------------
+// --SECTION--                                                        test suite
+// -----------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief setup
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_SUITE (AttributeNameParserTest, AttributeNameParserSetup)
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_simpleString
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_simpleString) {
+  std::string input = "test";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 1);
+  BOOST_CHECK_EQUAL(result[0].name, input);
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, false);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_subAttribute
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_subAttribute) {
+  std::string input = "foo.bar";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 2);
+  BOOST_CHECK_EQUAL(result[0].name, "foo");
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, false);
+  BOOST_CHECK_EQUAL(result[1].name, "bar");
+  BOOST_CHECK_EQUAL(result[1].shouldExpand, false);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_subsubAttribute
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_subsubAttribute) {
+  std::string input = "foo.bar.baz";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 3);
+  BOOST_CHECK_EQUAL(result[0].name, "foo");
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, false);
+  BOOST_CHECK_EQUAL(result[1].name, "bar");
+  BOOST_CHECK_EQUAL(result[1].shouldExpand, false);
+  BOOST_CHECK_EQUAL(result[2].name, "baz");
+  BOOST_CHECK_EQUAL(result[2].shouldExpand, false);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_expandAttribute
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_expandAttribute) {
+  std::string input = "foo[*]";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 1);
+  BOOST_CHECK_EQUAL(result[0].name, "foo");
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, true);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_expandSubAttribute
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_expandSubAttribute) {
+  std::string input = "foo.bar[*]";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 2);
+  BOOST_CHECK_EQUAL(result[0].name, "foo");
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, false);
+  BOOST_CHECK_EQUAL(result[1].name, "bar");
+  BOOST_CHECK_EQUAL(result[1].shouldExpand, true);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_expandedSubAttribute
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_expandedSubAttribute) {
+  std::string input = "foo[*].bar";
+  std::vector<AttributeName const> result;
+
+  TRI_ParseAttributeString(input, result);
+  
+  BOOST_CHECK_EQUAL(result.size(), 2);
+  BOOST_CHECK_EQUAL(result[0].name, "foo");
+  BOOST_CHECK_EQUAL(result[0].shouldExpand, true);
+  BOOST_CHECK_EQUAL(result[1].name, "bar");
+  BOOST_CHECK_EQUAL(result[1].shouldExpand, false);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_invalidAttributeAfterExpand
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_invalidAttributeAfterExpand) {
+  std::string input = "foo[*]bar";
+  std::vector<AttributeName const> result;
+
+  try {
+    TRI_ParseAttributeString(input, result);
+    BOOST_FAIL("Expected the function to throw");
+  } catch (Exception& e) {
+    BOOST_CHECK_EQUAL(e.code(), TRI_ERROR_ARANGO_PARSER_FAILED);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_nonClosing[
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_nonClosingBracket) {
+  std::string input = "foo[*bar";
+  std::vector<AttributeName const> result;
+
+  try {
+    TRI_ParseAttributeString(input, result);
+    BOOST_FAIL("Expected the function to throw");
+  } catch (Exception& e) {
+    BOOST_CHECK_EQUAL(e.code(), TRI_ERROR_ARANGO_PARSER_FAILED);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test_nonClosing[2
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE (test_nonClosingBracket2) {
+  std::string input = "foo[ * ].baz";
+  std::vector<AttributeName const> result;
+
+  try {
+    TRI_ParseAttributeString(input, result);
+    BOOST_FAIL("Expected the function to throw");
+  } catch (Exception& e) {
+    BOOST_CHECK_EQUAL(e.code(), TRI_ERROR_ARANGO_PARSER_FAILED);
+  }
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief generate tests
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// Local Variables:
+// mode: outline-minor
+// outline-regexp: "^\\(/// @brief\\|/// {@inheritDoc}\\|/// @addtogroup\\|// --SECTION--\\|/// @\\}\\)"
+// End:
---
+++ lib/Basics/AttributeNameParser.cpp
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Parser for Attibute Names. Tokenizes the attribute by dots and handles [*] expansion.
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2014-2015 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+/// @author Copyright 2014-2015, ArangoDB GmbH, Cologne, Germany
+/// @author Copyright 2012-2013, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+#include "AttributeNameParser.h"
+#include "Exceptions.h"
+
+void triagens::basics::TRI_ParseAttributeString (
+    std::string const& input,
+    std::vector<AttributeName const>& result
+  ) {
+  size_t parsedUntil = 0;
+  size_t length = input.length();
+  for (size_t pos = 0; pos < length; ++pos) {
+    auto token = input.at(pos);
+    if (token == '.') {
+      result.emplace_back(input.substr(parsedUntil, pos - parsedUntil), false);
+      ++pos; // Drop the .
+      parsedUntil = pos; 
+    }
+    else if (token == '[') {
+      // We only allow attr[*] and attr[*].attr2 as valid patterns
+      if (   length - pos < 3
+          || input.at(pos + 1) != '*'
+          || input.at(pos + 2) != ']'
+          || (length - pos > 3 && input.at(pos + 3) != '.')) {
+        THROW_ARANGO_EXCEPTION(TRI_ERROR_ARANGO_PARSER_FAILED);
+      }
+      result.emplace_back(input.substr(parsedUntil, pos - parsedUntil), true);
+      pos += 4;
+      parsedUntil = pos; 
+    }
+  }
+  if (parsedUntil < length) {
+    result.emplace_back(input.substr(parsedUntil), false);
+  }
+}
