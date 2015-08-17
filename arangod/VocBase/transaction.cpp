2d2933cc2e38d89bb2b9b3c1f412ba90ec2a620e
Jan Steemann <jan@arangodb.com>
2015-08-17 18:49:55 +0200

    deref pointer later

--- arangod/VocBase/transaction.cpp
+++ arangod/VocBase/transaction.cpp
@@ -153,11 +153,12 @@ static void FreeOperations (TRI_transaction_t* trx) {
 
   for (size_t i = 0; i < n; ++i) {
     TRI_transaction_collection_t* trxCollection = static_cast<TRI_transaction_collection_t*>(TRI_AtVectorPointer(&trx->_collections, i));
-    TRI_document_collection_t* document = trxCollection->_collection->_collection;
 
     if (trxCollection->_operations == nullptr) {
       continue;
     }
+    
+    TRI_document_collection_t* document = trxCollection->_collection->_collection;
 
     if (mustRollback) {
       // revert all operations
