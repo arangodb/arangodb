0cc23d34a1697818b1569d080135a3998d0aea69
Jan Steemann <jsteemann@mail.ru>
2015-08-26 10:21:36 +0200

    added indexFigures

--- arangod/Indexes/EdgeIndex.cpp
+++ arangod/Indexes/EdgeIndex.cpp
@@ -317,14 +317,16 @@ EdgeIndex::EdgeIndex (TRI_idx_iid_t iid,
                       TRI_document_collection_t* collection) 
   : Index(iid, collection, std::vector<std::string>({ TRI_VOC_ATTRIBUTE_FROM, TRI_VOC_ATTRIBUTE_TO })),
     _edgesFrom(nullptr),
-    _edgesTo(nullptr) {
+    _edgesTo(nullptr),
+    _numBuckets(1) {
   
   TRI_ASSERT(iid != 0);
 
   uint32_t indexBuckets = 1;
+
   if (collection != nullptr) {
     // document is a nullptr in the coordinator case
-    indexBuckets = collection->_info._indexBuckets;
+    _numBuckets = static_cast<size_t>(collection->_info._indexBuckets);
   }
 
   auto context = [this] () -> std::string {
@@ -336,7 +338,7 @@ EdgeIndex::EdgeIndex (TRI_idx_iid_t iid,
                                        IsEqualKeyEdgeFrom,
                                        IsEqualElementEdge,
                                        IsEqualElementEdgeFromByKey,
-                                       indexBuckets, 
+                                       _numBuckets, 
                                        64,
                                        context);
 
@@ -345,7 +347,7 @@ EdgeIndex::EdgeIndex (TRI_idx_iid_t iid,
                                      IsEqualKeyEdgeTo,
                                      IsEqualElementEdge,
                                      IsEqualElementEdgeToByKey,
-                                     indexBuckets,
+                                     _numBuckets,
                                      64,
                                      context);
 }
@@ -395,6 +397,8 @@ triagens::basics::Json EdgeIndex::toJson (TRI_memory_zone_t* zone,
       
 triagens::basics::Json EdgeIndex::toJsonFigures (TRI_memory_zone_t* zone) const {
   triagens::basics::Json json(triagens::basics::Json::Object);
+  
+  json("buckets", triagens::basics::Json(static_cast<double>(_numBuckets)));
 
   return json;
 }
