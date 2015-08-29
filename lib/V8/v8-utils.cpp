a9d63a0612a0d84f333bfd47f9d974e5894ad2f7
Jan Steemann <jan@arangodb.com>
2015-08-29 02:46:01 +0200

    do not advertise support for delate encoding in `org/arangodb/request` by default

--- lib/V8/v8-utils.cpp
+++ lib/V8/v8-utils.cpp
@@ -809,6 +809,8 @@ static void JS_Download (const v8::FunctionCallbackInfo<v8::Value>& args) {
     }
 
     SimpleHttpClient client(connection.get(), timeout, false);
+    client.setSupportDeflate(false);
+    client.setExposeArangoDB(false);
 
     v8::Handle<v8::Object> result = v8::Object::New(isolate);
 
