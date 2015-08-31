29767a2aa8c75a9dc6009709d06222fdc0ba8709
Michael Hackstein <michael@arangodb.com>
2015-08-31 11:07:53 +0200

    C++ified index-operator. Removed the free function now everything is handled properly via destructors

--- arangod/IndexOperators/index-operator.cpp
+++ arangod/IndexOperators/index-operator.cpp
@@ -38,6 +38,24 @@
 // --SECTION--                                      constructors and destructors
 // -----------------------------------------------------------------------------
 
+TRI_relation_index_operator_t::~TRI_relation_index_operator_t () {
+  if (_parameters != nullptr) {
+    TRI_FreeJson(TRI_UNKNOWN_MEM_ZONE, _parameters);
+  }
+  if (_fields != nullptr) {
+    // _fields contains _numFields shapedJson objects
+    for (size_t i = 0; i < _numFields; ++i) {
+      // destroy each individual shapedJson object
+      TRI_shaped_json_t* shaped = _fields + i;
+      TRI_DestroyShapedJson(_shaper->memoryZone(), shaped);
+    }
+    // free the memory pointer
+    TRI_Free(TRI_UNKNOWN_MEM_ZONE, _fields);
+  }
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief create a new index operator of the specified type
 ////////////////////////////////////////////////////////////////////////////////
@@ -48,26 +66,15 @@ TRI_index_operator_t* TRI_CreateIndexOperator (TRI_index_operator_type_e operato
                                                TRI_json_t* parameters,
                                                VocShaper* shaper,
                                                size_t numFields) {
-
-  TRI_index_operator_t* newOperator;
-
   switch (operatorType) {
     case TRI_AND_INDEX_OPERATOR:
     case TRI_NOT_INDEX_OPERATOR:
     case TRI_OR_INDEX_OPERATOR: {
-      TRI_logical_index_operator_t* newLogicalOperator = (TRI_logical_index_operator_t*) TRI_Allocate(TRI_UNKNOWN_MEM_ZONE, sizeof(TRI_logical_index_operator_t), false);
-
-      if (newLogicalOperator == nullptr) {
+      std::unique_ptr<TRI_logical_index_operator_t> newLogicalOperator(operatorType, shaper, leftOperand, rightOperand);
+      if (! newLogicalOperator) { // out of memory
         return nullptr;
       }
-
-      newLogicalOperator->base._type    = operatorType;
-      newLogicalOperator->base._shaper  = shaper;
-      newLogicalOperator->_left         = leftOperand;
-      newLogicalOperator->_right        = rightOperand;
-
-      newOperator = &(newLogicalOperator->base);
-      break;
+      return newLogicalOperator.release();
     }
 
     case TRI_EQ_INDEX_OPERATOR:
@@ -76,167 +83,22 @@ TRI_index_operator_t* TRI_CreateIndexOperator (TRI_index_operator_type_e operato
     case TRI_NE_INDEX_OPERATOR:
     case TRI_LE_INDEX_OPERATOR:
     case TRI_LT_INDEX_OPERATOR: {
-      TRI_relation_index_operator_t* newRelationOperator = (TRI_relation_index_operator_t*) TRI_Allocate(TRI_UNKNOWN_MEM_ZONE, sizeof(TRI_relation_index_operator_t), false);
-
-      if (newRelationOperator == nullptr) {
+      std::unique_ptr<TRI_relation_index_operator_t> newRelationOperator(operatorType, shaper, parameters, nullptr, numFields);
+      if (! newRelationOperator) { // out of memory
         return nullptr;
       }
+      return newRelationOperator.release();
 
-      newRelationOperator->base._type   = operatorType;
-      newRelationOperator->base._shaper = shaper;
-      newRelationOperator->_parameters  = parameters;
-      newRelationOperator->_fields      = nullptr;
-      newRelationOperator->_numFields   = numFields;
-
+      /* Old version. returned base!
       newOperator = &(newRelationOperator->base);
       break;
+      */
     }
 
     default: {
-      newOperator = nullptr;
-      break;
+      return nullptr;
     }
   } // end of switch statement
-
-  return newOperator;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief Destroys and frees any memory associated with an index operator
-////////////////////////////////////////////////////////////////////////////////
-
-static void ClearIndexOperator(TRI_index_operator_t* indexOperator) {
-  if (indexOperator == nullptr) {
-    return;
-  }
-
-  switch (indexOperator->_type) {
-    case TRI_AND_INDEX_OPERATOR:
-    case TRI_NOT_INDEX_OPERATOR:
-    case TRI_OR_INDEX_OPERATOR: {
-      TRI_logical_index_operator_t* logicalOperator = (TRI_logical_index_operator_t*) indexOperator;
-      ClearIndexOperator(logicalOperator->_left);
-      ClearIndexOperator(logicalOperator->_right);
-
-      TRI_Free(TRI_UNKNOWN_MEM_ZONE, logicalOperator);
-      break;
-    }
-
-    case TRI_EQ_INDEX_OPERATOR:
-    case TRI_GE_INDEX_OPERATOR:
-    case TRI_GT_INDEX_OPERATOR:
-    case TRI_NE_INDEX_OPERATOR:
-    case TRI_LE_INDEX_OPERATOR:
-    case TRI_LT_INDEX_OPERATOR: {
-      TRI_relation_index_operator_t* relationOperator = (TRI_relation_index_operator_t*) indexOperator;
-
-      if (relationOperator->_parameters != nullptr) {
-        TRI_FreeJson(TRI_UNKNOWN_MEM_ZONE, relationOperator->_parameters);
-      }
-
-      if (relationOperator->_fields != nullptr) {
-        // relationOperator->_fields contains _numFields shapedJson objects
-        for (size_t i = 0; i < relationOperator->_numFields; ++i) {
-          // destroy each individual shapedJson object
-          TRI_shaped_json_t* shaped = relationOperator->_fields + i;
-          TRI_DestroyShapedJson(relationOperator->base._shaper->memoryZone(), shaped);
-        }
-        // free the memory pointer
-        TRI_Free(TRI_UNKNOWN_MEM_ZONE, relationOperator->_fields);
-      }
-      TRI_Free(TRI_UNKNOWN_MEM_ZONE, relationOperator);
-      break;
-    }
-
-  } // end of switch statement
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief copy a skiplist operator recursively (deep copy)
-////////////////////////////////////////////////////////////////////////////////
-
-TRI_index_operator_t* TRI_CopyIndexOperator (TRI_index_operator_t* indexOperator) {
-  TRI_index_operator_t* newOperator = nullptr;
-
-  if (indexOperator == nullptr) {
-    return nullptr;
-  }
-
-  switch (indexOperator->_type) {
-    case TRI_AND_INDEX_OPERATOR:
-    case TRI_NOT_INDEX_OPERATOR:
-    case TRI_OR_INDEX_OPERATOR: {
-      TRI_logical_index_operator_t* oldLogicalOperator = (TRI_logical_index_operator_t*) indexOperator;
-      TRI_logical_index_operator_t* newLogicalOperator = (TRI_logical_index_operator_t*) TRI_Allocate(TRI_UNKNOWN_MEM_ZONE, sizeof(TRI_logical_index_operator_t), false);
-
-      if (newLogicalOperator == nullptr) { // out of memory ?
-        break;
-      }
-
-      newLogicalOperator->base._type   = indexOperator->_type;
-      newLogicalOperator->base._shaper = indexOperator->_shaper;
-      newLogicalOperator->_left         = TRI_CopyIndexOperator(oldLogicalOperator->_left);
-      newLogicalOperator->_right        = TRI_CopyIndexOperator(oldLogicalOperator->_right);
-      newOperator = &(newLogicalOperator->base);
-
-      break;
-
-    }
-
-    case TRI_EQ_INDEX_OPERATOR:
-    case TRI_GE_INDEX_OPERATOR:
-    case TRI_GT_INDEX_OPERATOR:
-    case TRI_NE_INDEX_OPERATOR:
-    case TRI_LE_INDEX_OPERATOR:
-    case TRI_LT_INDEX_OPERATOR: {
-      TRI_relation_index_operator_t* oldRelationOperator = (TRI_relation_index_operator_t*) indexOperator;
-      TRI_relation_index_operator_t* newRelationOperator = (TRI_relation_index_operator_t*) TRI_Allocate(TRI_UNKNOWN_MEM_ZONE, sizeof(TRI_relation_index_operator_t), false);
-
-      if (newRelationOperator == nullptr) { // out of memory?
-        break;
-      }
-
-      newRelationOperator->base._type   = indexOperator->_type;
-      newRelationOperator->base._shaper = indexOperator->_shaper;
-
-      if (oldRelationOperator->_parameters != nullptr) {
-        newRelationOperator->_parameters = TRI_CopyJson(TRI_UNKNOWN_MEM_ZONE, oldRelationOperator->_parameters);
-      }
-      else {
-        newRelationOperator->_parameters = nullptr;
-      }
-
-      if (oldRelationOperator->_fields != nullptr) {
-        newRelationOperator->_fields = TRI_CopyShapedJson(oldRelationOperator->base._shaper, oldRelationOperator->_fields);
-      }
-      else {
-        newRelationOperator->_fields = nullptr;
-      }
-
-      if (newRelationOperator->_fields != nullptr) {
-        newRelationOperator->_numFields = oldRelationOperator->_numFields;
-      }
-      else {
-        newRelationOperator->_numFields = 0;
-      }
-
-      newOperator = &(newRelationOperator->base);
-
-      break;
-    }
-  }
-
-  return newOperator;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief free a skiplist operator recursively
-////////////////////////////////////////////////////////////////////////////////
-
-void TRI_FreeIndexOperator (TRI_index_operator_t* indexOperator) {
-  ClearIndexOperator(indexOperator);
 }
 
 // -----------------------------------------------------------------------------
--- arangod/IndexOperators/index-operator.h
+++ arangod/IndexOperators/index-operator.h
@@ -99,9 +99,19 @@ TRI_index_operator_type_e;
 /// @brief operands and operators
 ////////////////////////////////////////////////////////////////////////////////
 
-struct TRI_index_operator_t {
-  TRI_index_operator_type_e _type;
-  VocShaper* _shaper;
+class TRI_index_operator_t {
+  public: 
+    TRI_index_operator_type_e const _type;
+    VocShaper const* _shaper;
+
+    TRI_index_operator_t (
+      TRI_index_operator_type_e const type,
+      VocShaper const* shaper
+    ) : _type(type),
+        _shaper(shaper)
+    {};
+
+    ~TRI_index_operator_t () {};
 };
 
 
@@ -110,9 +120,25 @@ struct TRI_index_operator_t {
 // It is a binary or unary operator
 //................................................................................
 
-struct TRI_logical_index_operator_t : public TRI_index_operator_t {
-  TRI_index_operator_t* _left;  // could be a relation or another logical operator
-  TRI_index_operator_t* _right; // could be a relation or another logical operator or null
+class TRI_logical_index_operator_t : public TRI_index_operator_t {
+  public:
+    TRI_index_operator_t* _left;  // could be a relation or another logical operator
+    TRI_index_operator_t* _right; // could be a relation or another logical operator or null
+
+    TRI_logical_index_operator_t (
+      TRI_index_operator_type_e type,
+      VocShaper* shaper,
+      TRI_index_operator_t* left,
+      TRI_index_operator_t* right
+    ) : TRI_index_operator_t(type, shaper),
+        _left(left),
+        _right(right)
+    {};
+
+    ~TRI_logical_index_operator_t () {
+      delete _left;
+      delete _right;
+    };
 };
 
 
@@ -120,11 +146,27 @@ struct TRI_logical_index_operator_t : public TRI_index_operator_t {
 // Storage for relation operator, e.g. <, <=, >, >=, ==, in
 //................................................................................
 
-struct TRI_relation_index_operator_t : public TRI_index_operator_t {
-  TRI_json_t* _parameters;    // parameters with which this relation was called with
-  TRI_shaped_json_t* _fields; // actual data from the parameters converted from
-                              // a json array to a shaped json array
-  size_t _numFields;          // number of fields in the array above
+class TRI_relation_index_operator_t : public TRI_index_operator_t {
+  public:
+    TRI_json_t* _parameters;    // parameters with which this relation was called with
+    TRI_shaped_json_t* _fields; // actual data from the parameters converted from
+                                      // a json array to a shaped json array
+    size_t _numFields;          // number of fields in the array above
+
+    TRI_relation_index_operator_t (
+      TRI_index_operator_type_e const type,
+      VocShaper const* shaper,
+      TRI_json_t* parameters,
+      TRI_shaped_json_t* fields,
+      size_t numFields
+    ) : TRI_index_operator_t(type, shaper),
+        _parameters(parameters),
+        _fields(fields),
+        _numFields(numFields)
+    {};
+
+    ~TRI_relation_index_operator_t ();
+
 };
 
 
@@ -141,15 +183,6 @@ TRI_index_operator_t* TRI_CreateIndexOperator (TRI_index_operator_type_e,
                                                TRI_json_t*,
                                                VocShaper*,
                                                size_t);
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief free an index operator recursively
-///
-/// note that this will also free all the bound json parameters
-////////////////////////////////////////////////////////////////////////////////
-
-void TRI_FreeIndexOperator (TRI_index_operator_t*);
-
 #endif
 
 // -----------------------------------------------------------------------------
