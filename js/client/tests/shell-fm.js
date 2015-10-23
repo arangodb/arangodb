94a7f2f2a79c64e1cdd6652cf76c8639d74e833b
Jan Steemann <jan@arangodb.com>
2015-10-23 14:49:58 +0200

    fixed wrong test assertions

--- js/client/tests/shell-fm.js
+++ js/client/tests/shell-fm.js
@@ -154,7 +154,7 @@ function FoxxManagerSuite () {
 
         for (version in entry.versions) {
           if (entry.versions.hasOwnProperty(version)) {
-            assertMatch(/^\d+(\.\d+)*$/, version);
+            assertMatch(/^\d+(\.\d+)*([a-zA-Z-]+)?$/, version);
             assertTrue(entry.versions[version].hasOwnProperty("type"));
             assertTrue(entry.versions[version].hasOwnProperty("location"));
           }
@@ -177,7 +177,7 @@ function FoxxManagerSuite () {
       // validate the results structure, simply take the first match
       var entry = result.pop();
 
-      assertEqual("jsteemann", entry.author);
+      assertEqual("Jan Steemann", entry.author);
       assertEqual("itzpapalotl", entry.name);
     },
 
