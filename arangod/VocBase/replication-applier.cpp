c690e91d778812f1b5df49a9a5508e0736ea6989
Jan Steemann <jan@arangodb.com>
2015-11-17 12:14:08 +0100

    improved error messages

--- arangod/VocBase/replication-applier.cpp
+++ arangod/VocBase/replication-applier.cpp
@@ -1157,13 +1157,14 @@ int TRI_replication_applier_t::start (TRI_voc_tick_t initialTick,
   syncer.release();
 
   if (useTick) {
-    LOG_INFO("started replication applier for database '%s' from tick %llu", 
+    LOG_INFO("started replication applier for database '%s', endpoint '%s' from tick %llu", 
              _databaseName.c_str(),
+             _configuration._endpoint,
              (unsigned long long) initialTick);
   }
   else {
-    LOG_INFO("re-started replication applier for database '%s'",
-              _databaseName.c_str());
+    LOG_INFO("re-started replication applier for database '%s', endpoint '%s'",
+              _databaseName.c_str(), _configuration._endpoint);
   }
 
   return TRI_ERROR_NO_ERROR;
