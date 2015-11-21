7e55bb6cf3af5c7e3871c336ab2032ad73155c66
Frank Celler <frank@arangodb.com>
2015-11-21 16:53:19 +0100

    fixed quotes

--- Installation/travisCI/build.sh
+++ Installation/travisCI/build.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo
-echo '$0: loading precompiled libraries'
+echo "$0: loading precompiled libraries"
 
 wget -q --progress=dot --show-prgress \
   -O - --progress=dot --show-prgress \
   "https://www.arangodb.com/support-files/travisCI/precompiled-libraries-4.3.61.tar.gz" | tar xzf -
 
 echo
-echo '$0: setup make-system'
+echo "$0: setup make-system"
 
 make setup || exit 1
 
