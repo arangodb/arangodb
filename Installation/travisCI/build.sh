1cc69d22dfcb37eba283ae6c9d506da3b679e2ec
Frank Celler <frank@arangodb.com>
2015-11-21 15:12:32 +0100

    show progress

--- Installation/travisCI/build.sh
+++ Installation/travisCI/build.sh
@@ -4,7 +4,9 @@ set -e
 echo
 echo '$0: loading precompiled libraries'
 
-wget -q -O - "https://www.arangodb.com/support-files/travisCI/precompiled-libraries-4.3.61.tar.gz" | tar xzf -
+wget -q --progress=dot --show-prgress \
+  -O - --progress=dot --show-prgress \
+  "https://www.arangodb.com/support-files/travisCI/precompiled-libraries-4.3.61.tar.gz" | tar xzf -
 
 echo
 echo '$0: setup make-system'
