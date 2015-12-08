ea085918c4222f7849262f69b13f8ab796527d02
Alan Plum <me@pluma.io>
2015-12-08 14:12:04 +0100

    Add jwt support back to Foxx sessions

--- js/server/modules/org/arangodb/foxx/sessions.js
+++ js/server/modules/org/arangodb/foxx/sessions.js
@@ -30,6 +30,7 @@
 const joi = require('joi');
 const deprecated = require('org/arangodb/deprecated');
 const Foxx = require('org/arangodb/foxx');
+const crypto = require('org/arangodb/crypto');
 const paramSchema = joi.string().optional().description('Foxx session ID');
 
 // -----------------------------------------------------------------------------
@@ -65,6 +66,9 @@ function decorateController(auth, controller) {
       sid = req.headers[cfg.header.toLowerCase()];
     }
     if (sid) {
+      if (cfg.jwt) {
+        sid = crypto.jwtDecode(cfg.jwt.secret, sid, !cfg.jwt.verify);
+      }
       try {
         req.session = sessions.get(sid);
       } catch (e) {
@@ -81,6 +85,9 @@ function decorateController(auth, controller) {
   controller.after('/*', function (req, res) {
     if (req.session) {
       var sid = req.session.forClient();
+      if (cfg.jwt) {
+        sid = crypto.jwtEncode(cfg.jwt.secret, sid, cfg.jwt.algorithm);
+      }
       if (cfg.cookie) {
         res.cookie(cfg.cookie.name, sid, {
           ttl: req.session.getTTL() / 1000,
@@ -207,6 +214,39 @@ class Sessions {
         opts.param = 'FOXXSID';
       } else if (typeof opts.param !== 'string') {
         throw new Error('Param name must be true, a string or empty.');
+      }
+    }
+    if (opts.jwt) {
+      deprecated('3.0',
+        'The Foxx session option "jwt" is deprecated.'
+        + ' Please use the session-jwt app instead.'
+      );
+      if (opts.jwt === true) {
+        opts.jwt = {};
+      } else if (typeof opts.jwt === 'string') {
+        opts.jwt = {secret: opts.jwt};
+      } else if (typeof opts.jwt !== 'object') {
+        throw new Error('Expected JWT settings to be an object, boolean or string.');
+      }
+      if (opts.jwt.verify !== false) {
+        opts.jwt.verify = true;
+      }
+      if (!opts.jwt.secret) {
+        opts.jwt.secret = '';
+        if (!opts.jwt.algorithm) {
+          opts.jwt.algorithm = 'none';
+        } else if (opts.jwt.algorithm !== 'none') {
+          throw new Error('Must provide a JWT secret to use any algorithm other than "none".');
+        }
+      } else {
+        if (typeof opts.jwt.secret !== 'string') {
+          throw new Error('Header JWT secret must be a string or empty.');
+        }
+        if (!opts.jwt.algorithm) {
+          opts.jwt.algorithm = 'HS256';
+        } else {
+          opts.jwt.algorithm = crypto.jwtCanonicalAlgorithmName(opts.jwt.algorithm);
+        }
       }
     }
     if (opts.autoCreateSession !== false) {
