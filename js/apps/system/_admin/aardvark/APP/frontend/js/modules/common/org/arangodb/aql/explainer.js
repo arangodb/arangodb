ac276e7e8781f6118152a0d50389dbca284c8ff7
Wilfried Goesgens <willi@arangodb.com>
2015-12-16 11:16:39 +0100

    Implement de/serializing of filters in traversalnodes

--- js/apps/system/_admin/aardvark/APP/frontend/js/modules/common/org/arangodb/aql/explainer.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/modules/common/org/arangodb/aql/explainer.js
@@ -579,7 +579,7 @@ function processQuery (query, explain) {
           }
           rc += "[" + value(indexNo) + "] -> ";
           rc += buildExpression(item.varAccess);
-          rc += " " + item.comparisonType + " ";
+          rc += " " + item.comparisonTypeStr + " ";
           rc += buildExpression(item.compareTo);
         }
       }
