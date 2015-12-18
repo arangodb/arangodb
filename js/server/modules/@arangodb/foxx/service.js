bc0b5d40c45883d7b7803e3bfdc97d6345a43cb7
Alan Plum <me@pluma.io>
2015-12-18 16:31:00 +0100

    Remove context._prefix

--- js/server/modules/@arangodb/foxx/service.js
+++ js/server/modules/@arangodb/foxx/service.js
@@ -51,7 +51,7 @@ class FoxxContext {
   }
 
   fileName(filename) {
-    return fs.safeJoin(this._prefix, filename);
+    return fs.safeJoin(this.basePath, filename);
   }
 
   file(filename, encoding) {
@@ -59,7 +59,7 @@ class FoxxContext {
   }
 
   path(name) {
-    return path.join(this._prefix, name);
+    return path.join(this.basePath, name);
   }
 
   collectionName(name) {
@@ -75,10 +75,6 @@ class FoxxContext {
 
   collection(name) {
     return internal.db._collection(this.collectionName(name));
-  }
-
-  get _prefix() {
-    return this.basePath;
   }
 
   get baseUrl() {
