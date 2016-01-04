a15c18572f22a6c9165918b49627e305e548da56
Jan Steemann <jan@arangodb.com>
2016-01-04 19:33:18 +0100

    fixed test, added more tests

--- js/server/modules/@arangodb/foxx/repository.js
+++ js/server/modules/@arangodb/foxx/repository.js
@@ -536,8 +536,8 @@ _.extend(Repository.prototype, {
 /// ```
 /// @endDocuBlock
 ////////////////////////////////////////////////////////////////////////////////
-  updateByExample(example, data) {
-    return this.collection.updateByExample(example, data);
+  updateByExample(example, data, options) {
+    return this.collection.updateByExample(example, data, options);
   },
 
 ////////////////////////////////////////////////////////////////////////////////
