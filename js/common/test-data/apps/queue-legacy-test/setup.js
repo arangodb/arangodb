e6149f1fff562d0b58b136e32f792d8712890f6f
Alan Plum <me@pluma.io>
2016-01-06 19:20:41 +0100

    rm unused queue-legacy-test fixture

--- js/common/test-data/apps/queue-legacy-test/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var db = require('@arangodb').db;
-var collectionName = 'test__queue_test_data';
-if (db._collection(collectionName)) {
-  require('console').warn('Collection already exists:', collectionName);
-  db._collection(collectionName).truncate();
-} else {
-  db._createDocumentCollection(collectionName);
-}
