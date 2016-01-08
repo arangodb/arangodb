f90a2abc0b4349675894bd37d8edfdb9db1d6d30
Alan Plum <me@pluma.io>
2016-01-08 14:54:47 +0100

    Fix legacy mode applicationContext injection

--- js/server/modules/@arangodb/foxx/service.js
+++ js/server/modules/@arangodb/foxx/service.js
@@ -233,7 +233,6 @@ class FoxxService {
       console.debug(
         `Running ${data.mount} in 2.x compatibility mode (requested version ${range} pre-dates 3.0.0)`
       );
-      this.main[$_MODULE_CONTEXT].applicationContext = this.main.context;
       this.main.context.foxxFilename = this.main.context.fileName;
     }
   }
@@ -443,6 +442,10 @@ class FoxxService {
       Object.keys(options.context).forEach(function (key) {
         module[$_MODULE_CONTEXT][key] = options.context[key];
       });
+    }
+
+    if (this.legacy) {
+      module[$_MODULE_CONTEXT].applicationContext = module.context;
     }
 
     try {
