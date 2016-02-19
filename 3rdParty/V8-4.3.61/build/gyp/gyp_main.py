b8f01f93bdebc9da77538ccbbfa9baeb19d863bb
Wilfried Goesgens <willi@arangodb.com>
2016-02-19 15:30:15 +0100

    Detect and set environments.

--- 3rdParty/V8-4.3.61/build/gyp/gyp_main.py
+++ 3rdParty/V8-4.3.61/build/gyp/gyp_main.py
@@ -5,14 +5,16 @@
 # found in the LICENSE file.
 
 import sys
+import os.path
 
 # TODO(mark): sys.path manipulation is some temporary testing stuff.
 try:
   import gyp
+  sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+  os.environ['PYTHON_EXECUTABLE'] = sys.executable
 except ImportError, e:
-  import os.path
   sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), 'pylib'))
   import gyp
-
+  
 if __name__ == '__main__':
   sys.exit(gyp.script_main())
