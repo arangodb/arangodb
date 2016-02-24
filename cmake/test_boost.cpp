22d0f3049bea31445b8da14636acbd4aaa7bb09a
Frank Celler <frank@arangodb.com>
2016-02-24 21:04:42 +0100

    added boost link test

---
+++ cmake/test_boost.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+struct TestSetup {
+  TestSetup() {
+    BOOST_TEST_MESSAGE("test message");
+  }
+};
+
+BOOST_FIXTURE_TEST_SUITE(Test, TestSetup)
+
+BOOST_AUTO_TEST_CASE (test_Test1) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
