3833d9e462330eabb50c6d937ce379210573226c
Wilfried Goesgens <willi@arangodb.com>
2016-02-24 16:08:43 +0100

    Swap sequence for visual studio.

--- 3rdParty/libev/libev-4.22/ev.c
+++ 3rdParty/libev/libev-4.22/ev.c
@@ -1946,7 +1946,7 @@ array_nextsize (int elem, int cur, int cnt)
   return ncur;
 }
 
-static void * noinline ecb_cold
+static noinline void * ecb_cold
 array_realloc (int elem, void *base, int *cur, int cnt)
 {
   *cur = array_nextsize (elem, *cur, cnt);
