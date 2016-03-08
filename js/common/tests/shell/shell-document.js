7c4d4ab36cdca32574d772474a75310960a31543
Max Neunhoeffer <max@arangodb.com>
2016-03-08 18:46:15 +0100

    Adjust test to (new) behaviour.

--- js/common/tests/shell/shell-document.js
+++ js/common/tests/shell/shell-document.js
@@ -689,7 +689,14 @@ function CollectionDocumentSuite () {
       // check with revision
       var d3 = collection.replace("2", { });
       // d2 is gone now...
-      assertFalse(collection.exists(d2));
+      try {
+        collection.exists(d2);
+        fail();
+      }
+      catch (err0) {
+        assertEqual(ERRORS.ERROR_ARANGO_CONFLICT.code, err0.errorNum);
+      }
+
       // d3 is still there
       assertTrue(collection.exists(d3));
 
@@ -1524,13 +1531,24 @@ function DatabaseDocumentSuite () {
       // check with revision
       var d3 = collection.replace("2", { });
       // d2 is gone now...
-      assertFalse(db._exists(d2));
+      try {
+        db._exists(d2);
+      }
+      catch (err0) {
+        assertEqual(ERRORS.ERROR_ARANGO_CONFLICT.code, err0.errorNum);
+      }
+
       // d3 is still there
       assertTrue(db._exists(d3));
 
 
       // non existing collection
-      assertFalse(db._exists("UnitTestsNonExistingCollection/1"));
+      try {
+        db._exists("UnitTestsNonExistingCollection/1");
+      }
+      catch (err1) {
+        assertEqual(ERRORS.ERROR_ARANGO_COLLECTION_NOT_FOUND.code, err1.errorNum);
+      }
 
       // invalid key pattern
       try {
