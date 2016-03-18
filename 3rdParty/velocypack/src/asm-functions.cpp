a2413a8ab3fa9e996096fbd1fa94fbd3ceb46694
jsteemann <jan@arangodb.com>
2016-03-18 17:41:13 +0100

    updated vpack library

--- 3rdParty/velocypack/src/asm-functions.cpp
+++ 3rdParty/velocypack/src/asm-functions.cpp
@@ -30,6 +30,8 @@
 #include "velocypack/velocypack-common.h"
 #include "asm-functions.h"
 
+static bool DisableSSE42 = false;
+
 size_t JSONStringCopyC(uint8_t* dst, uint8_t const* src, size_t limit) {
   return JSONStringCopyInline(dst, src, limit);
 }
@@ -101,7 +103,7 @@ static size_t JSONStringCopySSE42(uint8_t* dst, uint8_t const* src,
 }
 
 static size_t DoInitCopy(uint8_t* dst, uint8_t const* src, size_t limit) {
-  if (HasSSE42()) {
+  if (!DisableSSE42 && HasSSE42()) {
     JSONStringCopy = JSONStringCopySSE42;
   } else {
     JSONStringCopy = JSONStringCopyC;
@@ -150,7 +152,7 @@ static size_t JSONStringCopyCheckUtf8SSE42(uint8_t* dst, uint8_t const* src,
 
 static size_t DoInitCopyCheckUtf8(uint8_t* dst, uint8_t const* src,
                                   size_t limit) {
-  if (HasSSE42()) {
+  if (!DisableSSE42 && HasSSE42()) {
     JSONStringCopyCheckUtf8 = JSONStringCopyCheckUtf8SSE42;
   } else {
     JSONStringCopyCheckUtf8 = JSONStringCopyCheckUtf8C;
@@ -191,7 +193,7 @@ static size_t JSONSkipWhiteSpaceSSE42(uint8_t const* ptr, size_t limit) {
 }
 
 static size_t DoInitSkip(uint8_t const* ptr, size_t limit) {
-  if (HasSSE42()) {
+  if (!DisableSSE42 && HasSSE42()) {
     JSONSkipWhiteSpace = JSONSkipWhiteSpaceSSE42;
   } else {
     JSONSkipWhiteSpace = JSONSkipWhiteSpaceC;
@@ -660,6 +662,10 @@ int main(int argc, char* argv[]) {
   delete[] src;
   delete[] dst;
   return 0;
+}
+
+void DisableAssemblerFunctions() {
+  DisableSSE42 = true;
 }
 
 #endif
