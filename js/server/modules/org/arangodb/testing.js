92b85c62e7c34a7bc696d4c9ceff5438990a99ab
Frank Celler <frank@arangodb.com>
2016-03-19 23:20:14 +0100

    fixed chmod

--- js/server/modules/org/arangodb/testing.js
+++ js/server/modules/org/arangodb/testing.js
@@ -1745,7 +1745,7 @@ testFuncs.arangosh = function(options) {
              "#!" + ARANGOSH_BIN + " --javascript.execute \n" +
              "print('hello world');\n");
 
-    executeExternal("sh", ["-c", "chmod", "a+x", shebangFile]);
+    executeExternal("sh", ["-c", "chmod a+x " + shebangFile]);
 
     const startTime3 = time();
     rc = executeExternalAndWait("sh", ["-c", shebangFile]);
