ecf2deadb841dec9ad6d336df6deebd8a5b81ea1
Jan Steemann <jan@arangodb.com>
2016-03-24 19:35:37 +0100

    jslint

--- js/client/modules/@arangodb/testing.js
+++ js/client/modules/@arangodb/testing.js
@@ -1597,14 +1597,6 @@ function startInstanceCluster(instanceInfo, protocol, options,
   return true;
 }
 
-function wait2(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
-}
-
 function startInstanceAgency(instanceInfo, protocol, options,
   addArgs, testname, appDir, tmpDataDir) {
   const N = options.agencySize;
@@ -1651,8 +1643,8 @@ function startInstanceAgency(instanceInfo, protocol, options,
         l.push("--agency.endpoint");
         l.push(endpoints[j]);
       }
-      l.push("--agency.notify")
-      l.push("true")
+      l.push("--agency.notify");
+      l.push("true");
       
       args["flatCommands"] = l;
     }
