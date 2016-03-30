dff6210e4bf4e7048996b68f01988109158f9d55
Alan Plum <me@pluma.io>
2016-03-30 15:02:04 +0200

    Add testcase for #1742

---
+++ js/server/tests/shell-foxx-manager-spec.js
@@ -0,0 +1,47 @@
+/*global describe, beforeEach, afterEach, it, expect*/
+'use strict';
+
+var _ = require("underscore");
+var FoxxManager = require("org/arangodb/foxx/manager");
+var arangodb = require("org/arangodb");
+var fs = require("fs");
+var internal = require('internal');
+var basePath = fs.makeAbsolute(fs.join(internal.startupPath, "common", "test-data", "apps"));
+
+describe("Foxx Manager", function () {
+  describe("when manifest changes", function () {
+    var mount;
+
+    beforeEach(function () {
+      mount = "/unittest/clobbered-manifest";
+      FoxxManager.install(fs.join(basePath, "minimal-working-manifest"), mount);
+    });
+
+    afterEach(function () {
+      FoxxManager.uninstall(mount, {force: true});
+    });
+
+    it("doesn't mutilate mounted apps", function () {
+      var deps1 = {hello: {name: 'world', required: true, version: '*'}};
+      var deps2 = {clobbered: {name: 'completely', required: true, version: '*'}};
+
+      var app = FoxxManager.lookupApp(mount);
+      expect(app.manifest.dependencies).toEqual({});
+      var filename = app.main.context.applicationContext.fileName('manifest.json');
+      var rawJson = fs.readFileSync(filename, 'utf-8');
+      var json = JSON.parse(rawJson);
+
+      json.dependencies = deps1;
+      fs.writeFileSync(filename, JSON.stringify(json));
+      FoxxManager.scanFoxx(mount, {replace: true});
+      app = FoxxManager.lookupApp(mount);
+      expect(app.manifest.dependencies).toEqual(deps1);
+
+      json.dependencies = deps2;
+      fs.writeFileSync(filename, JSON.stringify(json));
+      FoxxManager.scanFoxx(mount, {replace: true});
+      app = FoxxManager.lookupApp(mount);
+      expect(app.manifest.dependencies).toEqual(deps2);
+    });
+  });
+});
