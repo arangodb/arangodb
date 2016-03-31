1ccb98510932bf02a0ba791514af3bfbada21606
Wilfried Goesgens <willi@arangodb.com>
2016-03-31 10:55:17 +0200

    Forward patch missing changes to the V8 build for windows/solaris/arch linux

--- 3rdParty/V8/V8-4.9.391/third_party/icu/source/common/uposixdefs.h
+++ 3rdParty/V8/V8-4.9.391/third_party/icu/source/common/uposixdefs.h
@@ -42,7 +42,9 @@
      * and define _XOPEN_SOURCE to different values depending on __STDC_VERSION__.
      * In C++ source code (e.g., putil.cpp), __STDC_VERSION__ is not defined at all.
      */
+#if !defined(_SCO_DS)
 #   define _XOPEN_SOURCE 600
+#endif
 #endif
 
 /*
@@ -52,8 +54,10 @@
  *
  * z/OS needs this definition for timeval and to get usleep.
  */
+#if !defined(__sun) && !defined(_SCO_DS)
 #if !defined(_XOPEN_SOURCE_EXTENDED)
 #   define _XOPEN_SOURCE_EXTENDED 1
+#endif
 #endif
 
 /*
