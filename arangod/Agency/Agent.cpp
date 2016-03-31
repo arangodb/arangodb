fcc159764c53b7305b63266dbc3c4f7973e57d40
Kaveh Vahedipour <kaveh@arangodb.com>
2016-03-31 09:19:05 +0000

    reading persistent log failes on linux

--- arangod/Agency/Agent.cpp
+++ arangod/Agency/Agent.cpp
@@ -237,13 +237,13 @@ append_entries_t Agent::sendAppendEntriesRPC (id_t slave_id) {
 
 bool Agent::load () {
   LOG_TOPIC(INFO, Logger::AGENCY) << "Loading persistent state.";
-  if (!_state.loadCollections()) {
+  /*  if (!_state.loadCollections()) {
     LOG(FATAL) << "Failed to load persistent state on statup.";
   }
 
   LOG_TOPIC(INFO, Logger::AGENCY) << "Reassembling spearhead and read stores.";
   _read_db.apply(_state.slices());
-  _spearhead.apply(_state.slices(_last_commit_index+1));
+  _spearhead.apply(_state.slices(_last_commit_index+1));*/
 
   LOG_TOPIC(INFO, Logger::AGENCY) << "Starting spearhead worker.";
   _spearhead.start();
