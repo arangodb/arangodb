d8b993f0fb583bed7fb512723e0badc8735046b1
Jan Steemann <jan@arangodb.com>
2016-04-04 10:13:13 +0200

    fixed some issues with tests

--- lib/Endpoint/Endpoint.h
+++ lib/Endpoint/Endpoint.h
@@ -32,6 +32,8 @@
 #include <WS2tcpip.h>
 #endif
 
+#include <ostream>
+
 namespace arangodb {
 
 class Endpoint {
@@ -94,5 +96,10 @@ class Endpoint {
 };
 
 }
+
+std::ostream& operator<<(std::ostream&, arangodb::Endpoint::TransportType);
+std::ostream& operator<<(std::ostream&, arangodb::Endpoint::EndpointType);
+std::ostream& operator<<(std::ostream&, arangodb::Endpoint::EncryptionType);
+std::ostream& operator<<(std::ostream&, arangodb::Endpoint::DomainType);
 
 #endif
