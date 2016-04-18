777848472db9c16a91207e1af453ba1c9004df15
Alan Plum <me@pluma.io>
2016-04-18 12:47:19 +0200

    standalone.html -> index.html

--- js/apps/system/_admin/aardvark/APP/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const db = require('@arangodb').db;
-const collections = ['_sessions', '_users'];
-
-collections.forEach(name => {
-  if (!db._collection(name)) {
-    db._createDocumentCollection(name, {isSystem: true});
-  }
-});
