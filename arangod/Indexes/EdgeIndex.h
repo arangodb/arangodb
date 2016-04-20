ae80b86599f946c337eca09cb43c0ce51f27b90d
Michael Hackstein <michael@arangodb.com>
2016-04-20 15:13:42 +0200

    Added a function to request babies from an IndexIterator (next n elements). Used this function in OperationCursor to get a list of mptrs. The only Index supporting it right now is EdgeIndex. It is only used in OutboundNeighbors at this point.

--- arangod/Indexes/EdgeIndex.h
+++ arangod/Indexes/EdgeIndex.h
@@ -41,6 +41,8 @@ class EdgeIndexIterator final : public IndexIterator {
 
   TRI_doc_mptr_t* next() override;
 
+  void nextBabies(std::vector<TRI_doc_mptr_t*>&, size_t) override;
+
   void reset() override;
 
   EdgeIndexIterator(arangodb::Transaction* trx,
