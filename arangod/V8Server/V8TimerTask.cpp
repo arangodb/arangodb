32766eb984ddc2665b44ed9bcac7ce7339d9e3d3
jsteemann <jan@arangodb.com>
2016-04-22 11:15:01 +0200

    fixed V8 context handling

--- arangod/V8Server/V8TimerTask.cpp
+++ arangod/V8Server/V8TimerTask.cpp
@@ -83,7 +83,11 @@ bool V8TimerTask::handleTimeout() {
       new V8Job(_vocbase, "(function (params) { " + _command + " } )(params);",
                 _parameters, _allowUseDatabase));
 
-  DispatcherFeature::DISPATCHER->addJob(job);
+  int res = DispatcherFeature::DISPATCHER->addJob(job);
+
+  if (res != TRI_ERROR_NO_ERROR) {
+    LOG(WARN) << "could not add task " << _command << " to queue";
+  }
 
   // note: this will destroy the task (i.e. ourselves!!)
   SchedulerFeature::SCHEDULER->destroyTask(this);
