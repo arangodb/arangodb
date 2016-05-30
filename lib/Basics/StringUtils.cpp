db00e467720ac2c10a76085d2f86327c3bf31b4b
jsteemann <jan@arangodb.com>
2016-05-30 22:11:13 +0200

    added optimized StringUtils::uint64_trusted()

--- lib/Basics/StringUtils.cpp
+++ lib/Basics/StringUtils.cpp
@@ -1586,10 +1586,39 @@ uint64_t uint64_check(std::string const& str) {
   int64_t value = std::stoull(str, &n, 10);
 
   if (n < str.size()) {
-    throw std::invalid_argument("cannot convert '" + str + "' to int64");
+    throw std::invalid_argument("cannot convert '" + str + "' to uint64");
   }
 
   return value;
+}
+
+uint64_t uint64_trusted(char const* value, size_t length) {
+  uint64_t result = 0;
+    
+  switch (length) { 
+    case 20:    result += (value[length - 20] - '0') * 10000000000000000000ULL;
+    case 19:    result += (value[length - 19] - '0') * 1000000000000000000ULL;
+    case 18:    result += (value[length - 18] - '0') * 100000000000000000ULL;
+    case 17:    result += (value[length - 17] - '0') * 10000000000000000ULL;
+    case 16:    result += (value[length - 16] - '0') * 1000000000000000ULL;
+    case 15:    result += (value[length - 15] - '0') * 100000000000000ULL;
+    case 14:    result += (value[length - 14] - '0') * 10000000000000ULL;
+    case 13:    result += (value[length - 13] - '0') * 1000000000000ULL;
+    case 12:    result += (value[length - 12] - '0') * 100000000000ULL;
+    case 11:    result += (value[length - 11] - '0') * 10000000000ULL;
+    case 10:    result += (value[length - 10] - '0') * 1000000000ULL;
+    case  9:    result += (value[length -  9] - '0') * 100000000ULL;
+    case  8:    result += (value[length -  8] - '0') * 10000000ULL;
+    case  7:    result += (value[length -  7] - '0') * 1000000ULL;
+    case  6:    result += (value[length -  6] - '0') * 100000ULL;
+    case  5:    result += (value[length -  5] - '0') * 10000ULL;
+    case  4:    result += (value[length -  4] - '0') * 1000ULL;
+    case  3:    result += (value[length -  3] - '0') * 100ULL;
+    case  2:    result += (value[length -  2] - '0') * 10ULL;
+    case  1:    result += (value[length -  1] - '0');
+  }
+
+  return result;
 }
 
 int32_t int32(std::string const& str) {
