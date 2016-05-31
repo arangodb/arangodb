b6e15313c31d8a21ee2eed169e66241429e08c99
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-05-31 16:45:23 +0200

    Moving Job classes out of Supervision

---
+++ arangod/Agency/FailedServer.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_CONSENSUS_FAILED_SERVER_H
+#define ARANGOD_CONSENSUS_FAILED_SERVER_H 1
+
+#include "Job.h"
+#include "Supervision.h"
+
+namespace arangodb {
+namespace consensus {
+
+struct FailedServer : public Job {
+
+  FailedServer(Node const& snapshot, Agent* agent, std::string const& jobId,
+               std::string const& creator, std::string const& agencyPrefix,
+               std::string const& failed);
+  
+  virtual ~FailedServer ();
+
+  virtual bool start() const override;
+  virtual bool create () const override;
+  virtual unsigned status () const override;
+  
+  std::string const& _failed;
+  
+};
+
+
+}}
+
+#endif
