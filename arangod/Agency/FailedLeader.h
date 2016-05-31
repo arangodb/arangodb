7b440f94dc5ba7bfec7545ce6a95c9178d8fc353
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-05-31 16:28:54 +0200

    Moving Job classes out of Supervision

---
+++ arangod/Agency/FailedLeader.h
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_CONSENSUS_FAILED_LEADER_H
+#define ARANGOD_CONSENSUS_FAILED_LEADER_H 1
+
+#include "Job.h"
+#include "Supervision.h"
+
+namespace arangodb {
+namespace consensus {
+
+struct FailedLeader : public Job {
+  
+  FailedLeader(Node const& snapshot, Agent* agent, std::string const& jobId,
+               std::string const& creator, std::string const& agencyPrefix,
+               std::string const& database = std::string(),
+               std::string const& collection = std::string(),
+               std::string const& shard = std::string(),
+               std::string const& from = std::string(),
+               std::string const& to = std::string());
+
+  virtual ~FailedLeader();
+
+  virtual bool create () const override;
+  virtual bool start() const override; 
+  virtual unsigned status () const override;
+    
+  std::string const& _database;
+  std::string const& _collection;
+  std::string const& _shard;
+  std::string const& _from;
+  std::string const& _to;
+  
+};
+
+}} //namespaces
+
+#endif 
+
+
