d844ff790ffb4d0ebc425a44979525dda13f81f5
hkernbach <heiko@arangodb.com>
2016-06-13 20:40:14 +0200

    shard view

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/shardsView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/shardsView.js
@@ -12,10 +12,14 @@
     knownServers: [],
 
     events: {
+      "click #shardsContent .pure-table-row" : "moveShard",
+      "click #rebalanceShards"               : "rebalanceShards"
     },
 
-    initialize: function () {
+    initialize: function (options) {
       var self = this;
+
+      self.dbServers = options.dbServers;
       clearInterval(this.intervalFunction);
 
       if (window.App.isCluster) {
@@ -24,7 +28,7 @@
         //start polling with interval
         this.intervalFunction = window.setInterval(function() {
           if (window.location.hash === '#shards') {
-            self.render();
+            self.render(false);
           }
         }, this.interval);
       }
@@ -52,6 +56,117 @@
       if (navi !== false) {
         arangoHelper.buildNodesSubNav('Shards');
       }
+    },
+
+    moveShard: function(e) {
+      var dbName = window.App.currentDB.get("name");
+      var collectionName = $(e.currentTarget).attr("collection");
+      var shardName = $(e.currentTarget).attr("shard");
+      var fromServer = $(e.currentTarget).attr("leader");
+
+      var buttons = [],
+        tableContent = [];
+
+      var array = [];
+      this.dbServers[0].each(function(db) {
+        if (db.get("name") !== fromServer) {
+          array.push({
+            value: db.get("name"),
+            label: db.get("name")
+          });
+        }
+      });
+      array = array.reverse();
+
+      tableContent.push(
+        window.modalView.createSelectEntry(
+          "toDBServer",
+          "Destination",
+          undefined,
+          //this.users !== null ? this.users.whoAmI() : 'root',
+          "Please select the target databse server. The selected database " + 
+          "server will be the new leader of the shard.",
+            array
+        )
+      );
+
+      buttons.push(
+        window.modalView.createSuccessButton(
+          "Move",
+          this.confirmMoveShards.bind(this, dbName, collectionName, shardName, fromServer)
+        )
+      );
+
+      window.modalView.show(
+        "modalTable.ejs",
+        "Move shard: " + shardName,
+        buttons,
+        tableContent
+      );
+
+    },
+
+    confirmMoveShards: function(dbName, collectionName, shardName, fromServer) {
+      var self = this;
+      var toServer = $('#toDBServer').val();
+
+      var data = {
+        database: dbName,
+        collection: collectionName,
+        shard: shardName,
+        fromServer: fromServer,
+        toServer: toServer
+      };
+
+      $.ajax({
+        type: "POST",
+        cache: false,
+        url: arangoHelper.databaseUrl("/_admin/cluster/moveShard"),
+        contentType: "application/json",
+        processData: false,
+        data: JSON.stringify(data),
+        async: true,
+        success: function(data) {
+          if (data === true) {
+            window.setTimeout(function() {
+              self.render(false);
+            }, 1500);
+            arangoHelper.arangoNotification("Shard " + shardName + " will be moved to " + toServer + ".");
+          }
+        },
+        error: function() {
+          arangoHelper.arangoNotification("Shard " + shardName + " could not be moved to " + toServer + ".");
+        }
+      });
+
+      window.modalView.hide();
+    },
+
+    rebalanceShards: function() {
+      var self = this;
+
+      $.ajax({
+        type: "POST",
+        cache: false,
+        url: arangoHelper.databaseUrl("/_admin/cluster/rebalanceShards"),
+        contentType: "application/json",
+        processData: false,
+        data: JSON.stringify({}),
+        async: true,
+        success: function(data) {
+          if (data === true) {
+            window.setTimeout(function() {
+              self.render(false);
+            }, 1500);
+            arangoHelper.arangoNotification("Started rebalance process.");
+          }
+        },
+        error: function() {
+          arangoHelper.arangoNotification("Could not start rebalance process.");
+        }
+      });
+
+      window.modalView.hide();
     },
 
     continueRender: function(collections) {
