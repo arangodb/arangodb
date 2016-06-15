6d8d7614988b6f2cdfe16174aa690cb83a010395
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-06-15 11:30:57 +0200

    agency threads reflect cluster size

--- scripts/startStandAloneAgency.sh
+++ scripts/startStandAloneAgency.sh
@@ -19,21 +19,27 @@ if [[ $(( $NRAGENTS % 2 )) == 0 ]]; then
     exit 1
 fi
 
+MINP=1.0
+MAXP=5.0
+SFRE=2.5
+COMP=1000
+BASE=4001
+
 rm -rf agency
 mkdir agency
 echo -n "Starting agency ... "
 if [ $NRAGENTS -gt 1 ]; then
    for aid in `seq 0 $(( $NRAGENTS - 2 ))`; do
-       port=$(( 4001 + $aid ))
+       port=$(( $BASE + $aid ))
        build/bin/arangod \
            -c none \
            --agency.id $aid \
            --agency.size $NRAGENTS \
            --agency.supervision true \
-           --agency.supervision-frequency 1 \
-           --agency.wait-for-sync true \
-           --agency.election-timeout-min 5.0 \
-           --agency.election-timeout-max 10.0 \
+           --agency.supervision-frequency $SFRE \
+           --agency.wait-for-sync false \
+           --agency.election-timeout-min $MINP \
+           --agency.election-timeout-max $MAXP \
            --database.directory agency/data$port \
            --javascript.app-path ./js/apps \
            --javascript.startup-directory ./js \
@@ -42,13 +48,13 @@ if [ $NRAGENTS -gt 1 ]; then
            --server.authentication false \
            --server.endpoint tcp://127.0.0.1:$port \
            --server.statistics false \
-           --agency.compaction-step-size 1000 \
+           --agency.compaction-step-size $COMP \
            --log.force-direct true \
            > agency/$port.stdout 2>&1 &
    done
 fi
 for aid in `seq 0 $(( $NRAGENTS - 1 ))`; do
-    endpoints="$endpoints --agency.endpoint tcp://localhost:$(( 4001 + $aid ))"          
+    endpoints="$endpoints --agency.endpoint tcp://localhost:$(( $BASE + $aid ))"          
 done
 build/bin/arangod \
     -c none \
@@ -57,23 +63,23 @@ build/bin/arangod \
     --agency.notify true \
     --agency.size $NRAGENTS \
     --agency.supervision true \
-    --agency.supervision-frequency 1 \
-    --agency.wait-for-sync true \
-    --agency.election-timeout-min 5.0 \
-    --agency.election-timeout-max 10.0 \
-    --database.directory agency/data$(( 4001 + $aid )) \
+    --agency.supervision-frequency $SFRE \
+    --agency.wait-for-sync false \
+    --agency.election-timeout-min $MINP \
+    --agency.election-timeout-max $MAXP \
+    --database.directory agency/data$(( $BASE + $aid )) \
     --javascript.app-path ./js/apps \
     --javascript.startup-directory ./js \
     --javascript.v8-contexts 1 \
-    --log.file agency/$(( 4001 + $aid )).log \
+    --log.file agency/$(( $BASE + $aid )).log \
     --server.authentication false \
-    --server.endpoint tcp://127.0.0.1:$(( 4001 + $aid )) \
+    --server.endpoint tcp://127.0.0.1:$(( $BASE + $aid )) \
     --server.statistics false \
-    --agency.compaction-step-size 1000 \
+    --agency.compaction-step-size $COMP \
     --log.force-direct true \
-    > agency/$(( 4001 + $aid )).stdout 2>&1 &
+    > agency/$(( $BASE + $aid )).stdout 2>&1 &
 
 echo " done."
-echo "Your agents are ready at port 4001 onward"
+echo "Your agents are ready at port $BASE onward"
 
 
