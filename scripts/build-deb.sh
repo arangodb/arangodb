98af68aadf4f10086fbbf9745ab56f750e56e592
Max Neunhoeffer <max@arangodb.com>
2016-06-20 00:41:18 -0700

    Add an important step to debian build script.

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -6,5 +6,6 @@ mkdir -p build-debian
 cd build-debian
 CFLAGS="-g -static-libgcc -O3 -fomit-frame-pointer" CXXFLAGS="-g -static-libgcc -static-libstdc++ -O3 -fomit-frame-pointer" V8_CFLAGS="-static-libgcc" V8_CXXFLAGS="-static-libgcc -static-libstdc++" V8_LDFLAGS="-static-libgcc -static-libstdc++" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_SKIP_RPATH=On -DUSE_OPTIMIZE_FOR_ARCHITECTURE=Off -DETCDIR=/etc  -DCMAKE_INSTALL_PREFIX=/usr -DVARDIR=/var -DUSE_JEMALLOC=On ..
 make -j12
+make prepare_debian
 cpack -G DEB --verbose
 cd ..
