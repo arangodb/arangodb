696da5882ce7b64962a0390ea999e38daf6e9f93
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-06-30 09:31:04 +0000

    recvAppendEntries will enforce leaderID

--- arangod/Agency/Constituent.cpp
+++ arangod/Agency/Constituent.cpp
@@ -50,6 +50,9 @@ using namespace arangodb::rest;
 using namespace arangodb::velocypack;
 using namespace arangodb;
 
+static const id_t NO_LEADER =
+  (std::numeric_limits<arangodb::consensus::id_t>::max)();
+
 /// Raft role names for display purposes 
 const std::vector<std::string> roleStr ({"Follower", "Candidate", "Leader"});
 
@@ -78,11 +81,11 @@ Constituent::Constituent()
     _queryRegistry(nullptr),
     _term(0),
     _cast(false),
-    _leaderID((std::numeric_limits<arangodb::consensus::id_t>::max)()),
+    _leaderID(NO_LEADER),
     _id(0),
     _role(FOLLOWER),
     _agent(nullptr),
-    _votedFor((std::numeric_limits<arangodb::consensus::id_t>::max)()),
+    _votedFor(NO_LEADER),
     _notifier(nullptr) {}
 
 
@@ -321,7 +324,6 @@ bool Constituent::vote(term_t term, arangodb::consensus::id_t id,
     MUTEX_LOCKER(guard, _castLock);
     t = _term;
     lid = _leaderID;
-    cast = _cast;
     _cast = true;
     if (appendEntries && t <= term) {
       _leaderID = id;
@@ -357,10 +359,9 @@ void Constituent::callElection() {
 
   votes.at(_id) = true;  // vote for myself
   _cast = true;
-  if (_role == CANDIDATE) {
-    _votedFor = _id;
-    this->term(_term + 1);  // raise my term
-  }
+  _votedFor = _id;
+  _leaderID = NO_LEADER;
+  this->term(_term + 1);  // raise my term
 
   std::string body;
   std::vector<OperationID> operationIDs(config().endpoints.size());
