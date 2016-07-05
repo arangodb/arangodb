b788babcc8a19d76118e46342cdc1b49cdd37c6b
jsteemann <jan@arangodb.com>
2016-07-05 13:53:22 +0200

    added arangovpack binary

---
+++ arangosh/VPack/VPackFeature.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2016 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGODB_VPACK_VPACK_FEATURE_H
+#define ARANGODB_VPACK_VPACK_FEATURE_H 1
+
+#include "ApplicationFeatures/ApplicationFeature.h"
+
+#include "Basics/VelocyPackHelper.h"
+
+namespace arangodb {
+
+class VPackFeature final
+    : public application_features::ApplicationFeature {
+ public:
+  VPackFeature(application_features::ApplicationServer* server,
+               int* result);
+
+ public:
+  void collectOptions(std::shared_ptr<options::ProgramOptions>) override;
+  void start() override;
+
+ private:
+  int* _result;
+  std::string _inputFile;
+  std::string _outputFile;
+  bool _prettyPrint;
+  bool _hexInput;
+  bool _printNonJson;
+};
+}
+
+#endif
---
+++ arangosh/VPack/arangovpack.cpp
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Basics/Common.h"
+
+#include "ApplicationFeatures/ConfigFeature.h"
+#include "ApplicationFeatures/ShutdownFeature.h"
+#include "ApplicationFeatures/VersionFeature.h"
+#include "Basics/ArangoGlobalContext.h"
+#include "Logger/LoggerFeature.h"
+#include "ProgramOptions/ProgramOptions.h"
+#include "Random/RandomFeature.h"
+#include "VPack/VPackFeature.h"
+
+using namespace arangodb;
+using namespace arangodb::application_features;
+
+int main(int argc, char* argv[]) {
+  ArangoGlobalContext context(argc, argv);
+  context.installHup();
+
+  std::shared_ptr<options::ProgramOptions> options(new options::ProgramOptions(
+      argv[0], "Usage: arangovpack [<options>]", "For more information use:"));
+
+  ApplicationServer server(options);
+
+  int ret;
+
+  server.addFeature(new ConfigFeature(&server, "arangovpack"));
+  server.addFeature(new LoggerFeature(&server, false));
+  server.addFeature(new RandomFeature(&server));
+  server.addFeature(new ShutdownFeature(&server, {"VPack"}));
+  server.addFeature(new VPackFeature(&server, &ret));
+  server.addFeature(new VersionFeature(&server));
+
+  try {
+    server.run(argc, argv);
+  } catch (std::exception const& ex) {
+    LOG(ERR) << "arangovpack terminated because of an unhandled exception: "
+             << ex.what();
+    ret = EXIT_FAILURE;
+  } catch (...) {
+    LOG(ERR) << "arangovpack terminated because of an unhandled exception of "
+                "unknown type";
+    ret = EXIT_FAILURE;
+  }
+
+  return context.exit(ret);
+}
