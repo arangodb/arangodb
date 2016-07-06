eee2d127276c8c7f567b8a6cb7257931db90f715
Alan Plum <me@pluma.io>
2016-07-06 14:00:21 +0200

    Move "executeScript" from FM into Service

--- js/server/modules/@arangodb/foxx/manager.js
+++ js/server/modules/@arangodb/foxx/manager.js
@@ -307,7 +307,7 @@ function checkMountedSystemService(dbname) {
     mount = usedSystemMountPoints[i];
     delete serviceCache[dbname][mount];
     _scanFoxx(mount, {replace: true});
-    executeScript('setup', lookupService(mount));
+    lookupService(mount).executeScript('setup');
   }
 }
 
@@ -543,22 +543,6 @@ function computeServicePath(mount) {
   var root = computeRootServicePath(mount);
   var mountPath = transformMountToPath(mount);
   return joinPath(root, mountPath);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief executes a service script
-////////////////////////////////////////////////////////////////////////////////
-
-function executeScript(scriptName, service, argv) {
-  var scripts = service.manifest.scripts;
-  // Only run setup/teardown scripts if they exist
-  if (scripts[scriptName] || (scriptName !== 'setup' && scriptName !== 'teardown')) {
-    return service.run(scripts[scriptName], {
-      foxxContext: {
-        argv: argv ? (Array.isArray(argv) ? argv : [argv]) : []
-      }
-    });
-  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1101,7 +1085,7 @@ function _install(serviceInfo, mount, options, runSetup) {
   try {
     service = _scanFoxx(mount, options);
     if (runSetup) {
-      executeScript('setup', lookupService(mount));
+      lookupService(mount).executeScript('setup');
     }
     if (!service.needsConfiguration()) {
       // Validate Routing & Exports
@@ -1239,7 +1223,7 @@ function _uninstall(mount, options) {
   }
   if (options.teardown !== false && options.teardown !== 'false') {
     try {
-      executeScript('teardown', service);
+      service.executeScript('teardown');
     } catch (e) {
       if (!options.force) {
         throw e;
@@ -1465,9 +1449,9 @@ function initializeFoxx(options) {
   var mounts = syncWithFolder(options);
   refillCaches(dbname);
   checkMountedSystemService(dbname);
-  mounts.forEach(function (mount) {
-    executeScript('setup', lookupService(mount));
-  });
+  for (const mount of mounts) {
+    lookupService(mount).executeScript('setup');
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
