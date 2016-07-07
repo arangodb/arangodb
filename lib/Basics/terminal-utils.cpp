ac567074f38cc646b0897448373acccca0de8cd6
Jan Steemann <jan@arangodb.com>
2016-07-07 11:38:43 +0200

    fixed ifdef

--- lib/Basics/terminal-utils.cpp
+++ lib/Basics/terminal-utils.cpp
@@ -30,9 +30,7 @@
 #if !defined(TRI_HAVE_SYS_IOCTL_H) && !defined(TRI_WIN32_CONSOLE)
 
 int TRI_ColumnsWidth(void) {
-  char* e;
-
-  e = getenv("COLUMNS");
+  char* e = getenv("COLUMNS");
 
   if (e != 0) {
     int c = (int)TRI_Int32String(e);
@@ -65,7 +63,7 @@ void TRI_SetStdinVisibility(bool visible) {
   }
   (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 #else
-#ifdef WIN32
+#ifdef _WIN32
   HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); 
   DWORD mode;
   GetConsoleMode(hStdin, &mode);
