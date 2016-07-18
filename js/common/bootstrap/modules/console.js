dc51d9dea5017155efa3896d145f8878912a7058
Jan Steemann <jan@arangodb.com>
2016-07-18 10:55:52 +0200

    issue #1944: nicer error logging in console.log()

--- js/common/bootstrap/modules/console.js
+++ js/common/bootstrap/modules/console.js
@@ -98,6 +98,8 @@ global.DEFINE_MODULE('console', (function () {
       if (typeof arg === 'object') {
         if (arg === null) {
           arg = 'null';
+        } else if (arg instanceof Error) {
+          arg = String(arg);
         } else if (arg instanceof Date || arg instanceof RegExp) {
           arg = String(arg);
         } else if (Object.prototype.isPrototypeOf(arg) || Array.isArray(arg)) {
