03bf10d255d166605d27e291653ce7fe93d528cf
Jan Steemann <jan@arangodb.com>
2016-07-21 12:10:09 +0200

    don't copy unneeded registers

--- arangod/Aql/IndexBlock.cpp
+++ arangod/Aql/IndexBlock.cpp
@@ -452,7 +452,7 @@ AqlItemBlock* IndexBlock::getSome(size_t atLeast, size_t atMost) {
     return nullptr;
   }
 
-  std::unique_ptr<AqlItemBlock> res(nullptr);
+  std::unique_ptr<AqlItemBlock> res;
 
   do {
     // repeatedly try to get more stuff from upstream
@@ -506,11 +506,11 @@ AqlItemBlock* IndexBlock::getSome(size_t atLeast, size_t atMost) {
     size_t toSend = (std::min)(atMost, available);
 
     if (toSend > 0) {
+      // automatically freed should we throw
       res.reset(new AqlItemBlock(
           toSend,
           getPlanNode()->getRegisterPlan()->nrRegs[getPlanNode()->getDepth()]));
 
-      // automatically freed should we throw
       TRI_ASSERT(curRegs <= res->getNrRegs());
 
       // only copy 1st row of registers inherited from previous frame(s)
@@ -532,7 +532,7 @@ AqlItemBlock* IndexBlock::getSome(size_t atLeast, size_t atMost) {
         // but can just take cur->getNrRegs() as registerId:
         auto doc = _documents[_posInDocs++];
         TRI_ASSERT(!doc.isExternal());
-        // doc points directly in the data files
+        // doc points directly into the data files
         res->setValue(j, static_cast<arangodb::aql::RegisterId>(curRegs), 
                       AqlValue(doc.begin()));
         // No harm done, if the setValue throws!
