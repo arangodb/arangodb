b74437716b0665ff8d9a0687ba78e25832a1a3ac
hkernbach <heiko@arangodb.com>
2016-07-22 20:36:54 +0200

    fixed issue: #1881

--- js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionActive.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionActive.js
@@ -1,18 +1,30 @@
 /* jshint browser: true */
 /* jshint unused: false */
-/* global window, Backbone, $, frontendConfig */
+/* global window, Backbone, arangoHelper, $, frontendConfig */
 (function () {
   'use strict';
   window.QueryManagementActive = Backbone.Collection.extend({
     model: window.queryManagementModel,
 
     url: function () {
-      return frontendConfig.basePath + '/_api/query/current';
+      var url = frontendConfig.basePath + '/_api/query/current';
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/current');
+      }
+
+      return url;
     },
 
     killRunningQuery: function (id, callback) {
+      var url = frontendConfig.basePath + '/_api/query/' + encodeURIComponent(id);
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/' + encodeURIComponent(id));
+      }
+
       $.ajax({
-        url: frontendConfig.basePath + '/_api/query/' + encodeURIComponent(id),
+        url: url,
         type: 'DELETE',
         success: function (result) {
           callback();
