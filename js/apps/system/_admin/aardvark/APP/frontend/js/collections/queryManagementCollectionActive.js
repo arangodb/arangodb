fbf25a9299b7e122ea1a8ae7a0cd14defb2b3463
hkernbach <heiko@arangodb.com>
2016-07-22 20:36:54 +0200

    cherry picked fix for issue #1881

--- js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionActive.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionActive.js
@@ -1,22 +1,32 @@
-/*jshint browser: true */
-/*jshint unused: false */
-/*global window, Backbone, $, frontendConfig */
-(function() {
-  "use strict";
+/* jshint browser: true */
+/* jshint unused: false */
+/* global window, Backbone, arangoHelper, $, frontendConfig */
+(function () {
+  'use strict';
   window.QueryManagementActive = Backbone.Collection.extend({
-
     model: window.queryManagementModel,
 
-    url: function() {
-      return frontendConfig.basePath + '/_api/query/current';
+    url: function () {
+      var url = frontendConfig.basePath + '/_api/query/current';
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/current');
+      }
+
+      return url;
     },
 
-    killRunningQuery: function(id, callback) {
-      var self = this;
+    killRunningQuery: function (id, callback) {
+      var url = frontendConfig.basePath + '/_api/query/' + encodeURIComponent(id);
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/' + encodeURIComponent(id));
+      }
+
       $.ajax({
-        url: frontendConfig.basePath + '/_api/query/'+encodeURIComponent(id),
+        url: url,
         type: 'DELETE',
-        success: function(result) {
+        success: function (result) {
           callback();
         }
       });
--- js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionSlow.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/collections/queryManagementCollectionSlow.js
@@ -1,21 +1,36 @@
-/*jshint browser: true */
-/*jshint unused: false */
-/*global window, Backbone, $ */
-(function() {
-  "use strict";
+/* jshint browser: true */
+/* jshint unused: false */
+/* global window, Backbone, arangoHelper, frontendConfig, $ */
+(function () {
+  'use strict';
   window.QueryManagementSlow = Backbone.Collection.extend({
     model: window.queryManagementModel,
-    url: "/_api/query/slow",
 
-    deleteSlowQueryHistory: function(callback) {
-      var self = this;
+    url: function () {
+      var url = frontendConfig.basePath + '/_api/query/slow';
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/slow');
+      }
+
+      return url;
+    },
+
+    deleteSlowQueryHistory: function (callback) {
+      var url = frontendConfig.basePath + '/_api/query/slow';
+
+      if (window.frontendConfig.db !== '_system') {
+        url = arangoHelper.databaseUrl('/_api/query/slow');
+      }
+
       $.ajax({
-        url: self.url,
+        url: url,
         type: 'DELETE',
-        success: function(result) {
+        success: function (result) {
           callback();
         }
       });
     }
+
   });
 }());
