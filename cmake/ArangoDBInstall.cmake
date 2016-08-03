59c57a207f671cec25495c136a27e768d7ba670d
Wilfried Goesgens <willi@arangodb.com>
2016-08-03 15:38:26 +0200

    Add current state of RPM build

--- cmake/ArangoDBInstall.cmake
+++ cmake/ArangoDBInstall.cmake
@@ -104,4 +104,5 @@ install(
 #get_cmake_property(_variableNames VARIABLES)
 #foreach (_variableName ${_variableNames})
 #    message(STATUS "${_variableName}=${${_variableName}}")
-##--------------------------------------------------------------------------------
+#endforeach()
+#--------------------------------------------------------------------------------
