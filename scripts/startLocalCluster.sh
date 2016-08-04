b847447b7c47543a1572160403de4647b6b3d36e
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-08-04 15:48:06 +0200

    fixed compaction bug

--- scripts/startLocalCluster.sh
+++ scripts/startLocalCluster.sh
@@ -48,11 +48,11 @@ SECONDARIES="$5"
 
 MINP=0.5
 MAXP=2.5
-COMP=100
+COMP=1000
 BASE=4001
 NATH=$(( $NRDBSERVERS + $NRCOORDINATORS + $NRAGENTS ))
 
-#rm -rf cluster
+rm -rf cluster
 mkdir -p cluster
 echo Starting agency ... 
 if [ $NRAGENTS -gt 1 ]; then
