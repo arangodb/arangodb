0601224cba22663c8b46a35d024d82881e6d159c
Jan Steemann <jan@arangodb.com>
2016-08-25 12:41:23 +0200

    fixed issue #2012

--- arangod/Wal/LogfileManager.h
+++ arangod/Wal/LogfileManager.h
@@ -116,6 +116,7 @@ class LogfileManager final : public application_features::ApplicationFeature {
   void validateOptions(std::shared_ptr<options::ProgramOptions>) override final;
   void prepare() override final;
   void start() override final;
+  void beginShutdown() override final;
   void unprepare() override final;
 
  public:
@@ -186,13 +187,13 @@ class LogfileManager final : public application_features::ApplicationFeature {
   inline void maxThrottleWait(uint64_t value) { _maxThrottleWait = value; }
 
   // whether or not write-throttling is currently enabled
-  inline bool isThrottled() { return (_writeThrottled != 0); }
+  inline bool isThrottled() { return _writeThrottled; }
 
   // activate write-throttling
-  void activateWriteThrottling() { _writeThrottled = 1; }
+  void activateWriteThrottling() { _writeThrottled = true; }
 
   // deactivate write-throttling
-  void deactivateWriteThrottling() { _writeThrottled = 0; }
+  void deactivateWriteThrottling() { _writeThrottled = false; }
 
   // allow or disallow writes to the WAL
   inline void allowWrites(bool value) { _allowWrites = value; }
@@ -537,7 +538,7 @@ class LogfileManager final : public application_features::ApplicationFeature {
   Mutex _idLock;
 
   // whether or not write-throttling is currently enabled
-  int _writeThrottled;
+  bool _writeThrottled;
 
   // whether or not we have been shut down already
   volatile sig_atomic_t _shutdown;
