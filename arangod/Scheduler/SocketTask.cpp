4a911768ddf1bb4469a4875d09ee272463389eed
jsteemann <jan@arangodb.com>
2016-09-01 10:46:45 +0200

    fix some issues in shutdown

--- arangod/Scheduler/SocketTask.cpp
+++ arangod/Scheduler/SocketTask.cpp
@@ -64,6 +64,8 @@ SocketTask::SocketTask(TRI_socket_t socket, double keepAliveTimeout)
 ////////////////////////////////////////////////////////////////////////////////
 
 SocketTask::~SocketTask() {
+  cleanup();
+
   if (TRI_isvalidsocket(_commSocket)) {
     TRI_CLOSE_SOCKET(_commSocket);
     TRI_invalidatesocket(&_commSocket);
@@ -320,9 +322,11 @@ bool SocketTask::setup(Scheduler* scheduler, EventLoop loop) {
 
   _tid = Thread::currentThreadId();
 
+  TRI_ASSERT(_writeWatcher == nullptr);
   _writeWatcher = _scheduler->installSocketEvent(loop, EVENT_SOCKET_WRITE, this,
                                                  _commSocket);
 
+  TRI_ASSERT(_readWatcher == nullptr);
   _readWatcher = _scheduler->installSocketEvent(loop, EVENT_SOCKET_READ, this,
                                                 _commSocket);
   return true;
