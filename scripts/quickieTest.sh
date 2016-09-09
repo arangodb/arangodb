8b6f0e1f51a445682ea35fc51f98c55e393c191e
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-09-09 15:03:12 +0200

    agency supervision turned off for quickietest

--- scripts/quickieTest.sh
+++ scripts/quickieTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 scripts/unittest shell_server --test js/common/tests/shell/shell-quickie.js
-scripts/unittest shell_server --test js/common/tests/shell/shell-quickie.js --cluster true
+scripts/unittest shell_server --test js/common/tests/shell/shell-quickie.js --cluster true --agencySupervision false
 scripts/unittest shell_client --test js/common/tests/shell/shell-quickie.js
 scripts/unittest shell_client --test js/common/tests/shell/shell-quickie.js --cluster true --agencySize 1
