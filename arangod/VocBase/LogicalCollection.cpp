5f2eca61ab1919e000796351b1c8d93df698e11c
Max Neunhoeffer <max@arangodb.com>
2016-09-28 16:21:09 +0200

    Remove isSmart attribute in community edition for Agency.

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -1034,7 +1034,6 @@ void LogicalCollection::toVelocyPackInObject(VPackBuilder& result) const {
   result.add("indexBuckets", VPackValue(_indexBuckets));
   result.add("replicationFactor", VPackValue(_replicationFactor));
   result.add("numberOfShards", VPackValue(_numberOfShards));
-  result.add("isSmart", VPackValue(_isSmart));
   if (!_distributeShardsLike.empty()) {
     result.add("distributeShardsLike", VPackValue(_distributeShardsLike));
   }
