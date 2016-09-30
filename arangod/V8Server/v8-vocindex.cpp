1ab7b4368c38facac244a78a659d79448c8cd10d
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-09-30 14:12:13 +0200

    distributeShardsLike now stores CID

--- arangod/V8Server/v8-vocindex.cpp
+++ arangod/V8Server/v8-vocindex.cpp
@@ -666,9 +666,12 @@ LogicalCollection* CreateCollectionCoordinator(LogicalCollection* parameters) {
         }
       } catch (...) {
       }
+      parameters->distributeShardsLike(otherCidString);
     }
   }
 
+  
+  
   // If the list dbServers is still empty, it will be filled in
   // distributeShards below.
 
