931d1920675775848eb9d1d967e7840390e7ea9e
Kaveh Vahedipour <kaveh@vahedipour.de>
2016-10-06 09:14:32 +0200

    if inception fails, agency fatal errors

--- arangod/Agency/Inception.cpp
+++ arangod/Agency/Inception.cpp
@@ -322,11 +322,6 @@ void Inception::run() {
   }
 
   // 4. If still incomplete bail out :(
-  if (!config.poolComplete()) {
-    LOG_TOPIC(FATAL, Logger::AGENCY)
-      << "Failed to build environment for RAFT algorithm. Bailing out!";
-    FATAL_ERROR_EXIT();
-  }
 
   _agent->ready(true);
 
