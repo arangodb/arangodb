445068836126fed9607c31ffad1ea73777b13973
Jan Steemann <jan@arangodb.com>
2016-10-07 09:13:32 +0200

    issue #2091: decrease connect timeout to 5 seconds on startup

--- js/common/modules/org/arangodb-common.js
+++ js/common/modules/org/arangodb-common.js
@@ -538,7 +538,7 @@ exports.checkAvailableVersions = function (version) {
   try {
     var u = "https://www.arangodb.com/repositories/versions.php?version=" + version +
             "&os=" + internal.platform;
-    var d = internal.download(u, "", {timeout: 300});
+    var d = internal.download(u, "", {timeout: 5});
     var v = JSON.parse(d.body);
 
     if (v.hasOwnProperty("bugfix")) {
