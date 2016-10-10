arangodb/arangodb

commit 4e8fc9b665f2d1b059f214b40d7f067cec6bcf35

Update Version.cpp
remove duble-space

lib/Rest/Version.cpp
@@ -301,9 +301,9 @@ std::string Version::getCompiler() {
 #if defined(__clang__)
   return "clang [" + (std::string)__VERSION__ + "]";
 #elif defined(__GNUC__) || defined(__GNUG__)
-  return "gcc  [" + (std::string)__VERSION__ + "]";
+  return "gcc [" + (std::string)__VERSION__ + "]";
 #elif defined(_MSC_VER)
-  return "msvc  [" + (std::string)_MSC_VER + "]";
+  return "msvc [" + (std::string)_MSC_VER + "]";
 #endif
   return "unknown";
 }

commit 7d2123cf6886a86069bcbc1415ffa5c1aa25208c

fix compilation on windows broken by 4e8fc9b665f2d1b059f214b40d7f067cec6bcf35

lib/Rest/Version.cpp
@@ -299,11 +299,11 @@ std::string Version::getICUVersion() {
 
 std::string Version::getCompiler() {
 #if defined(__clang__)
-  return "clang [" + (std::string)__VERSION__ + "]";
+  return "clang [" + std::string(__VERSION__) + "]";
 #elif defined(__GNUC__) || defined(__GNUG__)
-  return "gcc [" + (std::string)__VERSION__ + "]";
+  return "gcc [" + std::string(__VERSION__) + "]";
 #elif defined(_MSC_VER)
-  return "msvc [" + (std::string)_MSC_VER + "]";
+  return "msvc [" + std::string(_MSC_VER) + "]";
 #endif
   return "unknown";
 }