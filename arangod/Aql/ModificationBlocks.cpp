82b2d23345a2191cb4558412653d9abb00ba3556
Max Neunhoeffer <max@arangodb.com>
2016-10-13 20:05:56 +0200

    Fix UpdateBlock for smart edges.

--- arangod/Aql/ModificationBlocks.cpp
+++ arangod/Aql/ModificationBlocks.cpp
@@ -576,14 +576,16 @@ AqlItemBlock* UpdateBlock::work(std::vector<AqlItemBlock*>& blocks) {
 
     size_t const n = res->size();
     bool isMultiple = (n > 1);
+    object.clear();
     if (isMultiple) {
-      object.clear();
       object.openArray();
     }
       
     std::string key;
 
     // loop over the complete block
+    std::vector<bool> wasTaken;
+    wasTaken.reserve(n);
     for (size_t i = 0; i < n; ++i) {
       inheritRegisters(res, result.get(), i, dstRow + i);
 
@@ -609,26 +611,33 @@ AqlItemBlock* UpdateBlock::work(std::vector<AqlItemBlock*>& blocks) {
       }
 
       if (errorCode == TRI_ERROR_NO_ERROR) {
-        if (hasKeyVariable) {
-          keyBuilder.clear();
-          keyBuilder.openObject();
-          keyBuilder.add(StaticStrings::KeyString, VPackValue(key));
-          keyBuilder.close();
+        if (!ep->_options.consultAqlWriteFilter ||
+            !_collection->getCollection()->skipForAqlWrite(a.slice(), key)) {
+          wasTaken.push_back(true);
+          if (hasKeyVariable) {
+            keyBuilder.clear();
+            keyBuilder.openObject();
+            keyBuilder.add(StaticStrings::KeyString, VPackValue(key));
+            keyBuilder.close();
 
-          VPackBuilder tmp = VPackCollection::merge(
-              a.slice(), keyBuilder.slice(), false, false);
-          if (isMultiple) {
-            object.add(tmp.slice());
-          } else {
-            object = tmp;
+            VPackBuilder tmp = VPackCollection::merge(
+                a.slice(), keyBuilder.slice(), false, false);
+            if (isMultiple) {
+              object.add(tmp.slice());
+            } else {
+              object = tmp;
+            }
           }
-        }
-        else {
-          // use original slice for updating
-          object.add(a.slice());
+          else {
+            // use original slice for updating
+            object.add(a.slice());
+          }
+        } else {
+          wasTaken.push_back(false);
         }
       } else {
         handleResult(errorCode, ep->_options.ignoreErrors, &errorMessage);
+        wasTaken.push_back(false);
       }
     }
 
@@ -638,7 +647,9 @@ AqlItemBlock* UpdateBlock::work(std::vector<AqlItemBlock*>& blocks) {
 
     VPackSlice toUpdate = object.slice();
 
-    if (toUpdate.isNone()) {
+    if (toUpdate.isNone() ||
+        (toUpdate.isArray() && toUpdate.length() == 0)) {
+      dstRow += n;
       continue;
     }
 
@@ -670,22 +681,28 @@ AqlItemBlock* UpdateBlock::work(std::vector<AqlItemBlock*>& blocks) {
       if (producesOutput) {
         VPackSlice resultList = opRes.slice();
         TRI_ASSERT(resultList.isArray());
-        for (auto const& elm : VPackArrayIterator(resultList)) {
-          bool wasError = arangodb::basics::VelocyPackHelper::getBooleanValue(elm, "error", false);
-          if (!wasError) {
-            if (ep->_outVariableOld != nullptr) {
-              // store $OLD
-              result->setValue(dstRow, _outRegOld, AqlValue(elm.get("old")));
+        auto iter = VPackArrayIterator(resultList);
+        for (size_t i = 0; i < n; ++i) {
+          if (wasTaken[i]) {
+            TRI_ASSERT(iter.valid());
+            auto elm = iter.value();
+            bool wasError = arangodb::basics::VelocyPackHelper::getBooleanValue(elm, "error", false);
+            if (!wasError) {
+              if (ep->_outVariableOld != nullptr) {
+                // store $OLD
+                result->setValue(dstRow, _outRegOld, AqlValue(elm.get("old")));
+              }
+              if (ep->_outVariableNew != nullptr) {
+                // store $NEW
+                result->setValue(dstRow, _outRegNew, AqlValue(elm.get("new")));
+              }
             }
-            if (ep->_outVariableNew != nullptr) {
-              // store $NEW
-              result->setValue(dstRow, _outRegNew, AqlValue(elm.get("new")));
-            }
+            ++iter;
           }
           dstRow++;
         }
       } else {
-        dstRow += static_cast<size_t>(toUpdate.length());
+        dstRow += n;
       }
       handleBabyResult(opRes.countErrorCodes, static_cast<size_t>(toUpdate.length()),
                        ep->_options.ignoreErrors);
