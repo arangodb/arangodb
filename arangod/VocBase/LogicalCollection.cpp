aca4bc1a2b424c86f8e4e20a68c02c8fbfb61aef
Jan Steemann <jan@arangodb.com>
2016-11-08 11:25:57 +0100

    version detection fixes

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -410,10 +410,7 @@ LogicalCollection::LogicalCollection(TRI_vocbase_t* vocbase,
                          "with the --database.auto-upgrade option.");
 
     THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_FAILED, errorMsg);
-  } else if (info.hasKey("indexes") && _version == VERSION_30) {
-    // already a 3.1 collection. upgrade the _version data
-    setVersion(VERSION_31);
-  }
+  } 
 
   if (_isVolatile && _waitForSync) {
     // Illegal collection configuration
