2ed6d8cc3e3700324c3eea45cad3d55ae285e798
Alan Plum <me@pluma.io>
2016-11-18 11:46:53 +0100

    Discard unknown Foxx configs in production
    Fixes #2162

--- js/server/modules/@arangodb/foxx/service.js
+++ js/server/modules/@arangodb/foxx/service.js
@@ -96,7 +96,7 @@ module.exports =
       if (warnings) {
         console.warnLines(`Stored configuration for service "${data.mount}" has errors:\n  ${
           Object.keys(warnings).map((key) => warnings[key]).join('\n  ')
-        }`);
+        }\nValues for unknown options will be discarded if you save the configuration in production mode using the web interface.`);
       }
 
       this.thumbnail = null;
