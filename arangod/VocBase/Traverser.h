b5d367f0ece298205912be7192d7cfba6989f7e6
Michael Hackstein <michael@arangodb.com>
2016-11-22 17:16:57 +0100

    Allow the SmartGraph parts from the enterprise version to modifiy the traverser.

--- arangod/VocBase/Traverser.h
+++ arangod/VocBase/Traverser.h
@@ -165,6 +165,10 @@ class Traverser {
   friend class BreadthFirstEnumerator;
   friend class DepthFirstEnumerator;
   friend class NeighborsEnumerator;
+#ifdef USE_ENTERPRISE
+  friend class SmartDepthFirstPathEnumerator;
+  friend class SmartBreadthFirstPathEnumerator;
+#endif
 
  protected:
 
