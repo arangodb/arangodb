14b134d0b8849546020a20c4586c6e797c1d7ab9
Frank Celler <frank@arangodb.com>
2016-11-23 15:46:01 +0100

    filer internal function

--- lib/SimpleHttpClient/Communicator.cpp
+++ lib/SimpleHttpClient/Communicator.cpp
@@ -53,7 +53,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-int dumb_socketpair(SOCKET socks[2], int make_overlapped) {
+static int dumb_socketpair(SOCKET socks[2], int make_overlapped) {
   union {
     struct sockaddr_in inaddr;
     struct sockaddr addr;
