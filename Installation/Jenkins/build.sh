e26546690ddca7214f1b95a561b83602be68056f
Wilfried Goesgens <willi@arangodb.com>
2016-12-02 10:47:59 +0100

    fix snap package port

--- Installation/Jenkins/build.sh
+++ Installation/Jenkins/build.sh
@@ -296,7 +296,7 @@ while [ $# -gt 0 ];  do
             ;;
 
         --snap)
-            CONFIGURE_OPTIONS="${CONFIGURE_OPTIONS} -DUSE_SNAPCRAFT=ON -DSNAP_PORT=8533"
+            CONFIGURE_OPTIONS="${CONFIGURE_OPTIONS} -DUSE_SNAPCRAFT=ON -DSNAP_PORT=8529"
             shift
             ;;
 
