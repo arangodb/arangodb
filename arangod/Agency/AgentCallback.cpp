ba29e21a0cd2a14a9efb1cff226e50d0e6fdf618
Max Neunhoeffer <max@arangodb.com>
2016-12-23 14:09:01 +0100

    Improve a debug message.

--- arangod/Agency/AgentCallback.cpp
+++ arangod/Agency/AgentCallback.cpp
@@ -42,15 +42,19 @@ bool AgentCallback::operator()(arangodb::ClusterCommResult* res) {
     if (_agent) {
       _agent->reportIn(_slaveID, _last);
     }
-    LOG_TOPIC(DEBUG, Logger::AGENCY) << "comm_status(" << res->status
-                                     << "), last(" << _last << "), follower("
-                                     << _slaveID << "), time("
-                                     << TRI_microtime() - _startTime << ")";
+    LOG_TOPIC(DEBUG, Logger::AGENCY) 
+      << "Got good callback from AppendEntriesRPC: "
+      << "comm_status(" << res->status
+      << "), last(" << _last << "), follower("
+      << _slaveID << "), time("
+      << TRI_microtime() - _startTime << ")";
   } else {
-    LOG_TOPIC(DEBUG, Logger::AGENCY) << "comm_status(" << res->status
-                                     << "), last(" << _last << "), follower("
-                                     << _slaveID << "), time("
-                                     << TRI_microtime() - _startTime << ")";
+    LOG_TOPIC(DEBUG, Logger::AGENCY) 
+      << "Got callback from AppendEntriesRPC: "
+      << "comm_status(" << res->status
+      << "), last(" << _last << "), follower("
+      << _slaveID << "), time("
+      << TRI_microtime() - _startTime << ")";
   }
   return true;
 }
