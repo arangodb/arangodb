b447051677f726ea134396dde0e434fd36b60745
Andreas Streichardt <andreas@arangodb.com>
2017-01-06 17:24:39 +0100

    remove console.logs

--- js/server/modules/@arangodb/cluster.js
+++ js/server/modules/@arangodb/cluster.js
@@ -316,7 +316,6 @@ function createLocalDatabases (plannedDatabases, currentDatabases, writeLocked) 
 
   // check which databases need to be created locally
   for (name in plannedDatabases) {
-    console.log("NAME", name, plannedDatabases[name]);
     if (plannedDatabases.hasOwnProperty(name)) {
       var payload = plannedDatabases[name];
       payload.error = false;
@@ -374,7 +373,6 @@ function dropLocalDatabases (plannedDatabases, writeLocked) {
 
   var localDatabases = getLocalDatabases();
   var name;
-  console.log(localDatabases);
 
   // check which databases need to be deleted locally
   for (name in localDatabases) {
