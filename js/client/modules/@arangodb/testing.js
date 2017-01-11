7b023bd66f66b4d5510268a0e20f30420f953f04
Kaveh Vahedipour <kaveh@arangodb.com>
2017-01-11 11:50:18 +0100

    move shard jobs fail early if the contracting goes south

--- js/client/modules/@arangodb/testing.js
+++ js/client/modules/@arangodb/testing.js
@@ -1368,7 +1368,7 @@ function startInstanceCluster (instanceInfo, protocol, options,
 
       ++count;
 
-      if (count % 60 === 0) {
+      if (count % 120 === 0) {
         if (!checkArangoAlive(arangod, options)) {
           throw new Error('startup failed! bailing out!');
         }
