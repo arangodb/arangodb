e82c073de3a28dc1b79fa1f68215bec2cda21911
Manuel B <baslr@users.noreply.github.com>
2017-01-12 13:40:51 +0100

    fix more
    added _printPos to fix the more cmd. thanks @jsteemann

--- js/client/modules/@arangodb/arango-query-cursor.js
+++ js/client/modules/@arangodb/arango-query-cursor.js
@@ -69,6 +69,7 @@ ArangoQueryCursor.prototype.toString = function () {
   const currentPos = this._pos;
   var isCaptureModeActive = internal.isCaptureMode();
   var rows = [], i = 0;
+  this._pos = this._printPos || currentPos;
   while (++i <= 10 && this.hasNext()) {
     rows.push(this.next());
   }
@@ -117,6 +118,7 @@ ArangoQueryCursor.prototype.toString = function () {
     internal.print(result);
     result = '';
   }
+  this._printPos = this._pos;
   this._pos = currentPos;
   return result;
 };
