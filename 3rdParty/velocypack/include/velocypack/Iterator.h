83f872bcaa2288005a197be073c3e737f3193522
jsteemann <jan@arangodb.com>
2017-01-13 09:10:20 +0100

    removed wrong noexcept declarations

--- 3rdParty/velocypack/include/velocypack/Iterator.h
+++ 3rdParty/velocypack/include/velocypack/Iterator.h
@@ -119,7 +119,7 @@ class ArrayIterator {
     return operator*();
   }
 
-  inline void next() noexcept {
+  inline void next() {
     operator++();
   }
 
@@ -287,7 +287,7 @@ class ObjectIterator {
     return _slice.getNthValue(_position);
   }
 
-  inline void next() noexcept {
+  inline void next() {
     operator++();
   }
 
