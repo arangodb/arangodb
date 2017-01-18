2c1624a1b5458a894fdaa8c5947bab1f71da10c5
Wilfried Goesgens <willi@arangodb.com>
2017-01-18 17:10:52 +0100

    flush build directory if we reset the git copy

--- Installation/Jenkins/build.sh
+++ Installation/Jenkins/build.sh
@@ -522,7 +522,9 @@ echo "  MAKE: ${MAKE_CMD_PREFIX} ${MAKE} ${MAKE_PARAMS}"
 if test ${CLEAN_IT} -eq 1; then
     echo "found fundamental changes, rebuilding from scratch!"
     git clean -f -d -x
-    test -d ${BUILD_DIR} || rm -rf ${BUILD_DIR}
+    if test -d ${BUILD_DIR}; then
+        rm -rf ${BUILD_DIR}
+    fi
 fi
 
 SRC=`pwd`
