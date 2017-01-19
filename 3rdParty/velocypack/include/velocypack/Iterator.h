4dab3847eaf2fd1a93fd7f259a5ada7f303e215f
jsteemann <jan@arangodb.com>
2017-01-19 13:04:54 +0100

    potential fixes for Iterator

--- 3rdParty/velocypack/include/velocypack/Iterator.h
+++ 3rdParty/velocypack/include/velocypack/Iterator.h
@@ -51,19 +51,13 @@ class ArrayIterator {
     reset();   
   }
 
-  ArrayIterator(ArrayIterator const& other)
+  ArrayIterator(ArrayIterator const& other) noexcept
       : _slice(other._slice),
         _size(other._size),
         _position(other._position),
         _current(other._current) {}
 
-  ArrayIterator& operator=(ArrayIterator const& other) {
-    _slice = other._slice;
-    _size = other._size;
-    _position = other._position;
-    _current = other._current;
-    return *this;
-  }
+  ArrayIterator& operator=(ArrayIterator const& other) = delete;
 
   // prefix ++
   ArrayIterator& operator++() {
@@ -94,18 +88,26 @@ class ArrayIterator {
     return _slice.at(_position);
   }
 
-  ArrayIterator begin() { return ArrayIterator(_slice); }
+  ArrayIterator begin() { 
+    auto it = ArrayIterator(*this);
+    it._position = 0;
+    return it;
+  }
 
-  ArrayIterator begin() const { return ArrayIterator(_slice); }
+  ArrayIterator begin() const { 
+    auto it = ArrayIterator(*this);
+    it._position = 0;
+    return it;
+  }
 
   ArrayIterator end() {
-    auto it = ArrayIterator(_slice);
+    auto it = ArrayIterator(*this);
     it._position = it._size;
     return it;
   }
 
   ArrayIterator end() const {
-    auto it = ArrayIterator(_slice);
+    auto it = ArrayIterator(*this);
     it._position = it._size;
     return it;
   }
@@ -165,7 +167,7 @@ class ArrayIterator {
 
  private:
   Slice _slice;
-  ValueLength _size;
+  ValueLength const _size;
   ValueLength _position;
   uint8_t const* _current;
 };
@@ -197,21 +199,14 @@ class ObjectIterator {
     }
   }
 
-  ObjectIterator(ObjectIterator const& other)
+  ObjectIterator(ObjectIterator const& other) noexcept
       : _slice(other._slice),
         _size(other._size),
         _position(other._position),
         _current(other._current),
         _allowRandomIteration(other._allowRandomIteration) {}
 
-  ObjectIterator& operator=(ObjectIterator const& other) {
-    _slice = other._slice;
-    _size = other._size;
-    _position = other._position;
-    _current = other._current;
-    _allowRandomIteration = other._allowRandomIteration;
-    return *this;
-  }
+  ObjectIterator& operator=(ObjectIterator const& other) = delete;
 
   // prefix ++
   ObjectIterator& operator++() {
@@ -246,9 +241,17 @@ class ObjectIterator {
     return ObjectPair(_slice.getNthKey(_position, true), _slice.getNthValue(_position));
   }
 
-  ObjectIterator begin() { return ObjectIterator(*this); }
+  ObjectIterator begin() { 
+    auto it = ObjectIterator(*this);
+    it._position = 0;
+    return it;
+  }
 
-  ObjectIterator begin() const { return ObjectIterator(*this); }
+  ObjectIterator begin() const { 
+    auto it = ObjectIterator(*this);
+    it._position = 0;
+    return it;
+  }
 
   ObjectIterator end() {
     auto it = ObjectIterator(*this);
@@ -300,10 +303,10 @@ class ObjectIterator {
 
  private:
   Slice _slice;
-  ValueLength _size;
+  ValueLength const _size;
   ValueLength _position;
   uint8_t const* _current;
-  bool _allowRandomIteration;
+  bool const _allowRandomIteration;
 };
 
 }  // namespace arangodb::velocypack
