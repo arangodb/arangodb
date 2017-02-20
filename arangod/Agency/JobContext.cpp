bacaed29d4c7eefe07c86f658721d45e265009b9
Kaveh Vahedipour <kaveh@arangodb.com>
2017-02-20 17:30:11 +0100

    JobContext introduced to Supervision

---
+++ arangod/Agency/JobContext.cpp
@@ -0,0 +1,104 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#include "JobContext.h"
+
+#include "Agency/AddFollower.h"
+#include "Agency/CleanOutServer.h"
+#include "Agency/FailedFollower.h"
+#include "Agency/FailedLeader.h"
+#include "Agency/FailedServer.h"
+#include "Agency/MoveShard.h"
+#include "Agency/RemoveServer.h"
+#include "Agency/UnassumedLeadership.h"
+
+using namespace arangodb::consensus;
+
+JobContext::JobContext (
+  std::string const& type, Node const& snapshot, Agent* agent,
+  std::string const& id, std::string const& creator, std::string const& prefix)
+  : _job(nullptr) {
+
+  if (type == "failedLeader") {
+    _job = std::move(
+      std::unique_ptr<FailedLeader>(
+        new FailedLeader(snapshot, agent, id, creator, prefix)));
+  } else if (type == "failedFollower") {
+    _job = std::move(
+      std::unique_ptr<FailedFollower>(
+        new FailedFollower(snapshot, agent, id, creator, prefix)));
+  } else if (type == "failedServer") {
+    _job = std::move(
+      std::unique_ptr<FailedServer>(
+        new FailedServer(snapshot, agent, id, creator, prefix)));
+  } else if (type == "cleanOutServer") {
+    _job = std::move(
+      std::unique_ptr<CleanOutServer>(
+        new CleanOutServer(snapshot, agent, id, creator, prefix)));
+  } else if (type == "removeServer") {
+    _job = std::move(
+      std::unique_ptr<RemoveServer>(
+        new RemoveServer(snapshot, agent, id, creator, prefix)));
+  } else if (type == "moveShard") {
+    _job = std::move(
+      std::unique_ptr<MoveShard>(
+        new MoveShard(snapshot, agent, id, creator, prefix)));
+  } else if (type == "addFollower") {
+    _job = std::move(
+      std::unique_ptr<AddFollower>(
+        new AddFollower(snapshot, agent, id, creator, prefix)));
+  } else if (type == "unassumedLeadership") {
+    _job = std::move(
+      std::unique_ptr<UnassumedLeadership>(
+        new UnassumedLeadership( snapshot, agent, id, creator, prefix)));
+  } else {
+    LOG_TOPIC(ERR, Logger::AGENCY) <<
+      "Failed to run supervision job " << type << " with id " << id;
+  }
+
+}
+
+void JobContext::create() {
+  if (_job != nullptr) {
+    _job->create();
+  } 
+}
+
+void JobContext::start() {
+  if (_job != nullptr) {
+    _job->start();
+  }
+}
+
+void JobContext::run() {
+  if (_job != nullptr) {
+    _job->run();
+  }
+}
+
+void JobContext::abort() {
+  if (_job != nullptr) {
+    _job->abort();
+  }
+}
+
---
+++ arangod/Agency/JobContext.h
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_CONSENSUS_JOB_CONTEXT_H
+#define ARANGOD_CONSENSUS_JOB_CONTEXT_H 1
+
+#include "Job.h"
+
+#include <velocypack/Iterator.h>
+#include <velocypack/Slice.h>
+#include <velocypack/velocypack-aliases.h>
+
+#include <string>
+
+namespace arangodb {
+namespace consensus {
+
+
+class JobContext {
+
+public:
+
+  /// @brief Contextualize arbitrary Job
+  JobContext(std::string const& name, Node const& snapshot, Agent* agent,
+             std::string const& jobId, std::string const& creator,
+             std::string const& agencyPrefix);
+
+  /// @brief Create job
+  void create();
+
+  /// @brief Start job
+  void start();
+
+  /// @brief Run job
+  void run();
+  
+  /// @brief Abort job
+  void abort();
+
+private:
+  
+  /// @brief Actual job context
+  std::unique_ptr<Job> _job;
+
+};
+
+}}
+
+#endif
