5d9f12333143d22b8efa59d7a1d1557d8b47fbb9
Wilfried Goesgens <willi@arangodb.com>
2017-02-21 22:06:47 +0100

    move tgz package into its own file for variable scoping

--- cmake/packages/packages.cmake
+++ cmake/packages/packages.cmake
@@ -38,16 +38,7 @@ elseif (MSVC)
   include(packages/nsis)
 endif ()
 
-################################################################################
-## generic tarball
-################################################################################
-set(CPACK_PACKAGE_FILE_NAME "${ARANGODB_BASIC_PACKAGE_FILE_NAME}")
-set(CPACK_PACKAGE_TGZ "${CMAKE_BINARY_DIR}/${ARANGODB_BASIC_PACKAGE_FILE_NAME}.tar.gz")
-add_custom_target(TGZ_package
-  COMMENT "create TGZ-package"
-  COMMAND ${CMAKE_CPACK_COMMAND} -G TGZ  -C ${CMAKE_BUILD_TYPE}
-  )
-
+include(packages/tar)
 
 ################################################################################
 ## SNAPCRAFT PACKAGE
