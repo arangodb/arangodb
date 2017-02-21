2d27e79f767494de04186d4cd0fa66075bca7a11
Wilfried Goesgens <willi@arangodb.com>
2017-02-22 00:00:42 +0100

    structure when to define and use the cpack package filename so its consistent

--- cmake/packages/tar.cmake
+++ cmake/packages/tar.cmake
@@ -2,9 +2,18 @@
 ################################################################################
 ## generic tarball
 ################################################################################
-set(CPACK_PACKAGE_FILE_NAME "${ARANGODB_BASIC_PACKAGE_FILE_NAME}")
-set(CPACK_PACKAGE_TGZ "${CMAKE_BINARY_DIR}/${ARANGODB_BASIC_PACKAGE_FILE_NAME}.tar.gz")
+set(CPACK_PACKAGE_TGZ "${CMAKE_BINARY_DIR}/${CPACK_PACKAGE_FILE_NAME}.tar.gz")
 add_custom_target(TGZ_package
   COMMENT "create TGZ-package"
-  COMMAND ${CMAKE_CPACK_COMMAND} -G TGZ  -C ${CMAKE_BUILD_TYPE}
+  COMMAND ${CMAKE_CPACK_COMMAND} -G TGZ -C ${CMAKE_BUILD_TYPE}
   )
+
+add_custom_target(remove_tgz_packages
+  COMMAND ${CMAKE_COMMAND} -E remove_directory _CPack_Packages
+  COMMENT Removing server packaging build directory
+  COMMAND ${CMAKE_COMMAND} -E remove ${CPACK_PACKAGE_TGZ}
+  COMMENT Removing local tgz packages
+  )
+
+list(APPEND CLEAN_PACKAGES_LIST remove_tgz_packages)
+
