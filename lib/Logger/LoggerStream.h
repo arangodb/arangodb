43a32b04c456ce6d769f80a48b3ec9388cb34e4c
jsteemann <jan@arangodb.com>
2017-03-06 13:25:19 +0100

    fix compile errors in new clangs

--- lib/Logger/LoggerStream.h
+++ lib/Logger/LoggerStream.h
@@ -77,11 +77,11 @@ class LoggerStream {
   }
 
   template <typename T>
-  LoggerStream& operator<<(T const& obj) {
-    _out << obj;
-    return *this;
+  friend LoggerStream& operator<<(LoggerStream& out, T const& obj) {
+    out << obj;
+    return out;
   }
-  
+
   template <typename T1, typename T2>
   LoggerStream& operator<<(std::pair<T1, T2> const& obj) {
     _out << '(' << obj.first << ", " << obj.second << ')';
