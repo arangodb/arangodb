ac912c57d9284e0410dd8fd75df117bed99d9dcd
Wilfried Goesgens <willi@arangodb.com>
2017-03-17 15:13:26 +0100

    cleanup

--- cmake/ArangoDBInstall.cmake
+++ cmake/ArangoDBInstall.cmake
@@ -136,7 +136,6 @@ install(
 set(IS_SYSTEMD_INSTALL 0)
 set(SYSTEMD_UNIT_DIR "")
 if (UNIX)
-  
   if (${USE_ENTERPRISE})
     set(SERVICE_NAME "arangodb3e")
   else ()
@@ -170,7 +169,6 @@ if (UNIX)
       DESTINATION ${SYSTEMD_UNIT_DIR}/
       RENAME ${SERVICE_NAME})
     
-    endif()
   endif()
 endif()
 ################################################################################
