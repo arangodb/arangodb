3a0f31dcf638bc407ce332586949fdec25ad9a1e
Simon Grätzer <simon@graetzer.org>
2017-04-07 12:31:00 +0200

    Fixing more recovery tests

--- js/server/tests/recovery/create-collections.js
+++ js/server/tests/recovery/create-collections.js
@@ -112,9 +112,11 @@ function recoverySuite () {
       prop = c.properties();
       assertTrue(prop.waitForSync);
       assertEqual(2, c.type());
-      assertEqual(8 * 1024 * 1024, prop.journalSize);
-      assertFalse(prop.doCompact);
-      assertFalse(prop.isVolatile);
+      if (db._engine().name !== "rocksdb") {
+        assertEqual(8 * 1024 * 1024, prop.journalSize);
+        assertFalse(prop.doCompact);
+        assertFalse(prop.isVolatile);
+      }
       assertFalse(prop.isSystem);
       idx = c.getIndexes();
       assertEqual(3, idx.length);
@@ -124,9 +126,11 @@ function recoverySuite () {
       prop = c.properties();
       assertFalse(prop.waitForSync);
       assertEqual(2, c.type());
-      assertEqual(16 * 1024 * 1024, prop.journalSize);
-      assertTrue(prop.doCompact);
-      assertTrue(prop.isVolatile);
+      if (db._engine().name !== "rocksdb") {
+        assertEqual(16 * 1024 * 1024, prop.journalSize);
+        assertTrue(prop.doCompact);
+        assertTrue(prop.isVolatile);
+      }
       assertFalse(prop.isSystem);
       idx = c.getIndexes();
       assertEqual(2, idx.length);
@@ -138,9 +142,11 @@ function recoverySuite () {
       prop = c.properties();
       assertFalse(prop.waitForSync);
       assertEqual(3, c.type());
-      assertEqual(32 * 1024 * 1024, prop.journalSize);
-      assertTrue(prop.doCompact);
-      assertFalse(prop.isVolatile);
+      if (db._engine().name !== "rocksdb") {
+        assertEqual(32 * 1024 * 1024, prop.journalSize);
+        assertTrue(prop.doCompact);
+        assertFalse(prop.isVolatile);
+      }
       assertFalse(prop.isSystem);
       idx = c.getIndexes();
       assertEqual(3, idx.length);
