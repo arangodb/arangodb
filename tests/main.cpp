f60a60a4b99bf8f73fdac60cd5d062024d5031e6
Andreas Streichardt <andreas@arangodb.com>
2017-04-12 14:48:11 +0200

    manually initialize logger

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,14 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+#include "Logger/Logger.h"
+
+int main( int argc, char* argv[] )
+{
+  // global setup...
+  arangodb::Logger::initialize(false);
+  int result = Catch::Session().run( argc, argv );
+  arangodb::Logger::shutdown();
+  // global clean-up...
+
+  return ( result < 0xff ? result : 0xff );
+}
