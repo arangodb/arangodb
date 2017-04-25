2b4038953e6afea43e816b95a0cf4857d4991835
Andreas Streichardt <andreas@arangodb.com>
2017-04-25 15:19:42 +0200

    Remove unused CXX checks (were not in use and cmake has built in support for these)

--- cmake/CheckCXX11Features/cxx11-test-atomic_uint_fast16_t.cpp
+++
@@ -1,6 +0,0 @@
-#include <atomic>
-
-int main () {
-  std::atomic_uint_fast16_t a;
-  return 0;
-}
--- cmake/CheckCXX11Features/cxx11-test-sizeof_member_fail.cpp
+++
@@ -1,9 +0,0 @@
-struct foo {
-	int baz;
-	double bar;
-};
-
-int main(void)
-{
-	return (sizeof(foo::bar) == 4) ? 0 : 1;
-}
