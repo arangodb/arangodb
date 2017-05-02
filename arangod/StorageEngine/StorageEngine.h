64ec34548160876a2805beaee4089a37f45dd55b
jsteemann <jan@arangodb.com>
2017-05-02 12:22:13 +0200

    add dependencies for TransactionManager

--- arangod/StorageEngine/StorageEngine.h
+++ arangod/StorageEngine/StorageEngine.h
@@ -78,6 +78,7 @@ class StorageEngine : public application_features::ApplicationFeature {
     startsAfter("EngineSelector");
     startsAfter("FileDescriptors");
     startsAfter("Temp");
+    startsAfter("TransactionManager");
   }
 
   virtual void start() {}
