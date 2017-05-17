9b8a641e7702c9a3c5604f1b4b449fe8b88ef727
Alan Plum <me@pluma.io>
2017-05-17 16:51:28 +0200

    loadServiceFromDisk was renamed

--- js/server/tests/shell/shell-foxx-manager-spec.js
+++ js/server/tests/shell/shell-foxx-manager-spec.js
@@ -32,13 +32,13 @@ describe('Foxx Manager', function () {
 
       json.dependencies = deps1;
       fs.writeFileSync(filename, JSON.stringify(json));
-      FoxxManager.loadServiceFromDisk(mount);
+      FoxxManager.reloadInstalledService(mount);
       service = FoxxManager.lookupService(mount);
       expect(service.manifest.dependencies).to.eql(deps1);
 
       json.dependencies = deps2;
       fs.writeFileSync(filename, JSON.stringify(json));
-      FoxxManager.loadServiceFromDisk(mount);
+      FoxxManager.reloadInstalledService(mount);
       service = FoxxManager.lookupService(mount);
       expect(service.manifest.dependencies).to.eql(deps2);
     });
