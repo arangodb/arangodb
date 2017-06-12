b1599598678ca78bbe9c7f4c81e616ca0614c203
Frank Celler <frank@arangodb.com>
2017-06-12 12:03:04 +0200

    Jenkins pipeline for feature branches

---
+++ Installation/Pipeline/build_community_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+concurrency=$1
+
+(
+    mkdir -p build-jenkins
+    cd build-jenkins
+
+    CXXFLAGS=-fno-omit-frame-pointer cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On ..
+
+    make -j$concurrency
+)
+
+# copy binaries to preserve them
+rm -rf build
+mkdir build
+
+cp -a build-jenkins/bin   build
+cp -a build-jenkins/etc   build
+cp -a build-jenkins/tests build
