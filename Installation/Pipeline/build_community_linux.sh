182376de3ac8a6ebffcfdcfedadf9c89b75a842f
Frank Celler <frank@arangodb.com>
2017-06-16 18:20:23 +0200

    correct log path

--- Installation/Pipeline/build_community_linux.sh
+++ Installation/Pipeline/build_community_linux.sh
@@ -7,10 +7,14 @@ echo "CONCURRENY: $concurrency"
 echo "HOST: `hostname`"
 echo "PWD: `pwd`"
 
+rm -rf log-output/$type.log
+mkdir -p log-output
+
 (
     mkdir -p build-jenkins
     cd build-jenkins
 
+    echo "Configuring..."
     CXXFLAGS=-fno-omit-frame-pointer \
         cmake \
             -DCMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -18,12 +22,10 @@ echo "PWD: `pwd`"
             -DUSE_CATCH_TESTS=On \
             -DUSE_FAILURE_TESTS=On \
             -DDEBUG_SYNC_REPLICATION=On \
-            ..
+            .. > ../log-output/$type.log 2>&1
 
-    rm -rf log-output/$type
-    mkdir -p log-output
-
-    make -j$concurrency > log-output/$type.log 2>&1
+    echo "Building..."
+    make -j$concurrency >> ../log-output/$type.log 2>&1
 )
 
 # copy binaries to preserve them
