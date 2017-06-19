957137d5f7a67709d03b9cd85cac5c6a83563159
jsteemann <jan@arangodb.com>
2017-06-19 17:38:19 +0200

    removed dead code

--- arangod/Replication/InitialSyncer.h
+++ arangod/Replication/InitialSyncer.h
@@ -55,7 +55,7 @@ int syncChunkRocksDB(InitialSyncer& syncer, SingleCollectionTransaction* trx,
                      std::string const& keysId, uint64_t chunkId,
                      std::string const& lowString,
                      std::string const& highString,
-                     std::vector<std::pair<std::string, uint64_t>> markers,
+                     std::vector<std::pair<std::string, uint64_t>> const& markers,
                      std::string& errorMsg);
 namespace httpclient {
 class SimpleHttpResult;
@@ -78,7 +78,7 @@ class InitialSyncer : public Syncer {
                      std::string const& keysId, uint64_t chunkId,
                      std::string const& lowString,
                      std::string const& highString,
-                     std::vector<std::pair<std::string, uint64_t>> markers,
+                     std::vector<std::pair<std::string, uint64_t>> const& markers,
                      std::string& errorMsg);
 
  private:
@@ -220,33 +220,6 @@ class InitialSyncer : public Syncer {
 
   int handleCollectionSync(arangodb::LogicalCollection*, std::string const&,
                            std::string const&, TRI_voc_tick_t, std::string&);
-
-  //////////////////////////////////////////////////////////////////////////////
-  /// @brief incrementally fetch data from a collection
-  //////////////////////////////////////////////////////////////////////////////
-
-  int handleSyncKeysRocksDB(arangodb::LogicalCollection* col,
-                            std::string const& keysId, std::string const& cid,
-                            std::string const& collectionName,
-                            TRI_voc_tick_t maxTick, std::string& errorMsg);
-  //////////////////////////////////////////////////////////////////////////////
-  /// @brief incrementally fetch chunk data from a collection
-  //////////////////////////////////////////////////////////////////////////////
-
-  int syncChunkRocksDB(SingleCollectionTransaction* trx,
-                       std::string const& keysId, uint64_t chunkId,
-                       std::string const& lowKey, std::string const& highKey,
-                       std::vector<std::pair<std::string, uint64_t>> markers,
-                       std::string& errorMsg);
-
-  //////////////////////////////////////////////////////////////////////////////
-  /// @brief incrementally fetch data from a collection
-  //////////////////////////////////////////////////////////////////////////////
-
-  int handleSyncKeysMMFiles(arangodb::LogicalCollection* col,
-                            std::string const& keysId, std::string const& cid,
-                            std::string const& collectionName,
-                            TRI_voc_tick_t maxTick, std::string& errorMsg);
 
   //////////////////////////////////////////////////////////////////////////////
   /// @brief changes the properties of a collection, based on the VelocyPack
--- arangod/RocksDBEngine/RocksDBIncrementalSync.cpp
+++ arangod/RocksDBEngine/RocksDBIncrementalSync.cpp
@@ -44,7 +44,7 @@ int syncChunkRocksDB(InitialSyncer& syncer, SingleCollectionTransaction* trx,
                      std::string const& keysId, uint64_t chunkId,
                      std::string const& lowString,
                      std::string const& highString,
-                     std::vector<std::pair<std::string, uint64_t>> markers,
+                     std::vector<std::pair<std::string, uint64_t>> const& markers,
                      std::string& errorMsg) {
   std::string const baseUrl = syncer.BaseUrl + "/keys";
   TRI_voc_tick_t const chunkSize = 5000;
