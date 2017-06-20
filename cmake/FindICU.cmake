561323ebfff2b6f43eb9b7475a51ffebd19f3f18
Vasiliy <vasiliy@arangodb.com>
2017-06-20 12:46:37 +0300

    add missed cmake file

---
+++ cmake/FindICU.cmake
@@ -0,0 +1,12 @@
+# -*- mode: CMAKE; -*-
+
+# - Find ICU (Overloaded version of FindICU from iResearch to use ICU from V8)
+#  ICU_FOUND, whether ICU has been found
+
+set(ICU_FOUND TRUE) # ICU build from source in 3rdParty directory
+
+message("ICU_INCLUDE_DIR: " ${ICU_INCLUDE_DIR})
+message("ICU_LIBRARY_DIR: " ${ICU_LIBRARY_DIR})
+message("ICU_SHARED_LIBS: " ${ICU_SHARED_LIBS})
+message("ICU_STATIC_LIBS: " ${ICU_STATIC_LIBS})
+message("ICU_SHARED_LIB_RESOURCES: " ${ICU_SHARED_LIB_RESOURCES})
