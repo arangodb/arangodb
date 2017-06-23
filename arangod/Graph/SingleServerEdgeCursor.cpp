a0669522c6a2e946dbb674e0d8021c99a76852e8
Jan Christoph Uhde <Jan@UhdeJC.com>
2017-06-23 13:41:18 +0200

    fix #277 make use of nextExtra

--- arangod/Graph/SingleServerEdgeCursor.cpp
+++ arangod/Graph/SingleServerEdgeCursor.cpp
@@ -69,88 +69,104 @@ SingleServerEdgeCursor::~SingleServerEdgeCursor() {
   }
 }
 
-bool SingleServerEdgeCursor::next(
-    std::function<void(std::unique_ptr<EdgeDocumentToken>&&, VPackSlice, size_t)> callback) {
-  if (_currentCursor == _cursors.size()) {
-    return false;
-  }
-  if (_cachePos < _cache.size()) {
-    LogicalCollection* collection =
-        _cursors[_currentCursor][_currentSubCursor]->collection();
-    auto etkn = std::make_unique<SingleServerEdgeDocumentToken>(collection->cid(), _cache[_cachePos++]);
-
-    if (collection->readDocument(_trx, etkn->token(), *_mmdr)) {
-      VPackSlice edgeDocument(_mmdr->vpack());
-      _opts->cache()->increaseCounter();
-      if (_internalCursorMapping != nullptr) {
-        TRI_ASSERT(_currentCursor < _internalCursorMapping->size());
-        callback(std::move(etkn), edgeDocument,
-                 _internalCursorMapping->at(_currentCursor));
-      } else {
-        callback(std::move(etkn), edgeDocument, _currentCursor);
-      }
-    }
-
-    return true;
-  }
-  // We need to refill the cache.
-  _cachePos = 0;
-  auto cursorSet = _cursors[_currentCursor];
-  while (cursorSet.empty()) {
-    // Fast Forward to the next non-empty cursor set
-    _currentCursor++;
-    _currentSubCursor = 0;
-    if (_currentCursor == _cursors.size()) {
-      return false;
-    }
-    cursorSet = _cursors[_currentCursor];
-  }
-  auto cursor = cursorSet[_currentSubCursor];
-  // NOTE: We cannot clear the cache,
-  // because the cursor expect's it to be filled.
-  do {
-    if (!cursor->hasMore()) {
-      // This one is exhausted, next
-      ++_currentSubCursor;
-      while (_currentSubCursor == cursorSet.size()) {
-        ++_currentCursor;
-        _currentSubCursor = 0;
-        if (_currentCursor == _cursors.size()) {
-          // We are done, all cursors exhausted.
-          return false;
-        }
-        cursorSet = _cursors[_currentCursor];
-      }
-      cursor = cursorSet[_currentSubCursor];
-      // If we switch the cursor. We have to clear the cache.
-      _cache.clear();
-    } else {
-      _cache.clear();
-      auto cb = [&](DocumentIdentifierToken const& token) {
-        if (token._data != 0) {
-          // Document not found
-          _cache.emplace_back(token);
-        }
-      };
-      bool tmp = cursor->next(cb, 1000);
-      TRI_ASSERT(tmp == cursor->hasMore());
-    }
-  } while (_cache.empty());
-
-  TRI_ASSERT(_cachePos < _cache.size());
-  LogicalCollection* collection = cursor->collection();
+void SingleServerEdgeCursor::getDocAndRunCallback(OperationCursor* cursor, Callback callback) {
+  auto collection = cursor->collection();
   auto etkn = std::make_unique<SingleServerEdgeDocumentToken>(collection->cid(), _cache[_cachePos++]);
   if (collection->readDocument(_trx, etkn->token(), *_mmdr)) {
     VPackSlice edgeDocument(_mmdr->vpack());
     _opts->cache()->increaseCounter();
     if (_internalCursorMapping != nullptr) {
       TRI_ASSERT(_currentCursor < _internalCursorMapping->size());
-      callback(std::move(etkn), edgeDocument,
-               _internalCursorMapping->at(_currentCursor));
+      callback(std::move(etkn), edgeDocument, _internalCursorMapping->at(_currentCursor));
     } else {
       callback(std::move(etkn), edgeDocument, _currentCursor);
     }
   }
+}
+
+bool SingleServerEdgeCursor::advanceCursor(OperationCursor*& cursor, std::vector<OperationCursor*>& cursorSet) {
+  ++_currentSubCursor;
+  if (_currentSubCursor >= cursorSet.size()) {
+    ++_currentCursor;
+    _currentSubCursor = 0;
+    if (_currentCursor == _cursors.size()) {
+      // We are done, all cursors exhausted.
+      return false;
+    }
+    cursorSet = _cursors[_currentCursor];
+  }
+
+  cursor = cursorSet[_currentSubCursor];
+  // If we switch the cursor. We have to clear the cache.
+  _cache.clear();
+  return true;
+}
+
+bool SingleServerEdgeCursor::next(std::function<void(std::unique_ptr<EdgeDocumentToken>&&, VPackSlice, size_t)> callback) {
+  // fills callback with next EdgeDocumentToken and Slice that contains the ohter side of the edge
+  // (we are standing on a node and want to iterate all connected edges
+
+  if (_currentCursor == _cursors.size()) {
+    return false;
+  }
+
+  // There is still something in the cache
+  if (_cachePos < _cache.size()) {
+    //get the collection
+    auto cur = _cursors[_currentCursor][_currentSubCursor];
+    getDocAndRunCallback(cur, callback);
+    return true;
+  }
+
+  // We need to refill the cache.
+  _cachePos = 0;
+  auto cursorSet = _cursors[_currentCursor];
+
+  // get current cursor
+  auto cursor = cursorSet[_currentSubCursor];
+
+  // NOTE: We cannot clear the cache,
+  // because the cursor expect's it to be filled.
+  do {
+    if (cursorSet.empty() || !cursor->hasMore()) {
+      if (!advanceCursor(cursor, cursorSet)) {
+        return false;
+      }
+    } else {
+      if(cursor->hasExtra()){
+        bool operationSuccessfull = false;
+        auto extraCB = [&](DocumentIdentifierToken const& token, VPackSlice edge){
+          if (token._data != 0) {
+            operationSuccessfull = true;
+            auto etkn = std::make_unique<SingleServerEdgeDocumentToken>(cursor->collection()->cid(), token);
+            if (_internalCursorMapping != nullptr) {
+              TRI_ASSERT(_currentCursor < _internalCursorMapping->size());
+              callback(std::move(etkn), edge, _internalCursorMapping->at(_currentCursor));
+            } else {
+              callback(std::move(etkn), edge, _currentCursor);
+            }
+          }
+        };
+        cursor->nextWithExtra(extraCB,1);
+        if (operationSuccessfull) {
+          return true;
+        }
+      } else {
+        _cache.clear();
+        auto cb = [&](DocumentIdentifierToken const& token) {
+          if (token._data != 0) {
+            // Document not found
+            _cache.emplace_back(token);
+          }
+        };
+        bool tmp = cursor->next(cb, 1000);
+        TRI_ASSERT(tmp == cursor->hasMore());
+      }
+    }
+  } while (_cache.empty());
+  TRI_ASSERT(!_cache.empty());
+  TRI_ASSERT(_cachePos < _cache.size());
+  getDocAndRunCallback(cursor, callback);
   return true;
 }
 
@@ -170,7 +186,6 @@ void SingleServerEdgeCursor::readAll(
       auto cid = collection->cid();
       if (cursor->hasExtra()) {
         auto cb = [&](DocumentIdentifierToken const& token, VPackSlice edge) {
-          _opts->cache()->increaseCounter();
           callback(std::make_unique<SingleServerEdgeDocumentToken>(cid, token), edge, cursorId);
         };
         cursor->allWithExtra(cb);
