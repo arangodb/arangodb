a2abe4fc39cfa95e76a1e5f776a9c740bcf3ceed
Frank Celler <frank@arangodb.com>
2017-06-25 12:16:05 +0200

    mac

--- Installation/Pipeline/test_cluster_enterprise_mmfiles_mac.sh
+++ Installation/Pipeline/test_cluster_enterprise_mmfiles_mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./Installation/Pipeline/include/test_MODE_EDITION_ENGINE_linux.inc "$1" cluster enterprise mmfiles
+. ./Installation/Pipeline/include/test_MODE_EDITION_ENGINE_OS.inc "$1" cluster enterprise mmfiles mac
