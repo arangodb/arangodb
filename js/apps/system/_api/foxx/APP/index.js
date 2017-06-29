bf9c018f7b9799c963e019c684c42d0aeb4ab191
Alan Plum <me@pluma.io>
2017-06-29 15:12:58 +0200

    Add idiomatic flag to Foxx tests (#2693)

--- js/apps/system/_api/foxx/APP/index.js
+++ js/apps/system/_api/foxx/APP/index.js
@@ -327,18 +327,19 @@ scriptsRouter.post('/:name', (req, res) => {
 
 instanceRouter.post('/tests', (req, res) => {
   const service = req.service;
+  const idiomatic = req.queryParams.idiomatic;
   const reporter = req.queryParams.reporter || null;
   const result = FoxxManager.runTests(service.mount, {reporter});
-  if (reporter === 'stream' && req.accepts(LDJSON, 'json') === LDJSON) {
+  if (reporter === 'stream' && (idiomatic || req.accepts(LDJSON, 'json') === LDJSON)) {
     res.type(LDJSON);
     for (const row of result) {
       res.write(JSON.stringify(row) + '\r\n');
     }
-  } else if (reporter === 'xunit' && req.accepts('xml', 'json') === 'xml') {
+  } else if (reporter === 'xunit' && (idiomatic || req.accepts('xml', 'json') === 'xml')) {
     res.type('xml');
     res.write('<?xml version="1.0" encoding="utf-8"?>\n');
     res.write(jsonml2xml(result) + '\n');
-  } else if (reporter === 'tap' && req.accepts('text', 'json') === 'text') {
+  } else if (reporter === 'tap' && (idiomatic || req.accepts('text', 'json') === 'text')) {
     res.type('text');
     for (const row of result) {
       res.write(row + '\n');
@@ -348,6 +349,7 @@ instanceRouter.post('/tests', (req, res) => {
   }
 })
 .queryParam('reporter', joi.only(...reporters).optional())
+.queryParam('idiomatic', schemas.flag.default(false))
 .response(200, ['json', LDJSON, 'xml', 'text']);
 
 instanceRouter.post('/download', (req, res) => {
