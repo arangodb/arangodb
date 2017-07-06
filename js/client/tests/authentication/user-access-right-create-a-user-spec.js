1f642045ae35d6fc56068c1b430165aec02086dc
mchacki <michael@arangodb.com>
2017-07-06 14:41:51 +0200

    Added tests for access-rights on users api

---
+++ js/client/tests/authentication/user-access-right-create-a-user-spec.js
@@ -0,0 +1,136 @@
+/* jshint globalstrict:true, strict:true, maxlen: 5000 */
+/* global describe, before, after, it, require, print */
+
+// //////////////////////////////////////////////////////////////////////////////
+// / @brief tests for user access rights
+// /
+// / @file
+// /
+// / DISCLAIMER
+// /
+// / Copyright 2017 ArangoDB GmbH, Cologne, Germany
+// /
+// / Licensed under the Apache License, Version 2.0 (the "License");
+// / you may not use this file except in compliance with the License.
+// / You may obtain a copy of the License at
+// /
+// /     http://www.apache.org/licenses/LICENSE-2.0
+// /
+// / Unless required by applicable law or agreed to in writing, software
+// / distributed under the License is distributed on an "AS IS" BASIS,
+// / WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// / See the License for the specific language governing permissions and
+// / limitations under the License.
+// /
+// / Copyright holder is ArangoDB GmbH, Cologne, Germany
+// /
+// / @author Michael Hackstein
+// / @author Copyright 2017, ArangoDB GmbH, Cologne, Germany
+// //////////////////////////////////////////////////////////////////////////////
+
+'use strict';
+
+const expect = require('chai').expect;
+const users = require('@arangodb/users');
+const helper = require('@arangodb/user-helper');
+const namePrefix = helper.namePrefix;
+const dbName = helper.dbName;
+const colName = helper.colName;
+const rightLevels = helper.rightLevels;
+const testDBName = `${namePrefix}DBNew`;
+
+const userSet = helper.userSet;
+const systemLevel = helper.systemLevel;
+const dbLevel = helper.dbLevel;
+const colLevel = helper.colLevel;
+const activeUsers = helper.activeUsers;
+const inactiveUsers = helper.inactiveUsers;
+const testUser = `${namePrefix}TestUser`;
+
+const arango = require('internal').arango;
+const db = require('internal').db;
+for (let l of rightLevels) {
+  systemLevel[l] = new Set();
+  dbLevel[l] = new Set();
+  colLevel[l] = new Set();
+}
+
+const switchUser = (user) => {
+  arango.reconnect(arango.getEndpoint(), '_system', user, '');
+};
+
+describe('User Rights Management', () => {
+
+  before(() => {
+    helper.removeAllUsers();
+    helper.generateAllUsers();
+  });
+
+  after(helper.removeAllUsers);
+
+  it('should test rights for', () => {
+
+    for (let name of userSet) {
+
+      describe(`user ${name}`, () => {
+
+         before(() => {
+           switchUser(name);
+         });
+
+         describe('administrate on server level', () => {
+
+           const rootTestUser = (switchBack = true) => {
+             switchUser(root);
+             try {
+               const u = users.document(testUser);
+               if (switchBack) {
+                 switchUser(name);
+               }
+               return u !== undefined;
+             } catch (e) {
+               if (switchBack) {
+                 switchUser(name);
+               }
+               return false;
+             }
+           };
+
+           const rootDropUser = () => {
+             if (rootTestUser(false)) {
+               users.remove(testUser);
+             }
+             switchUser(name);
+           };
+
+           before(() => {
+             db._useDatabase('_system');
+             rootDropUser();
+           });
+
+           after(() => {
+             rootDropUser();
+           });
+
+           it('create a user', () => {
+             if (activeUsers.has(name) && systemLevel['rw'].has(name)) {
+               // User needs rw on _system
+               users.save('UnitTestTestUser', '', true);
+               expect(rootTestUser()).to.equal(true, `User creation reported success, but User was not found afterwards.`);
+             } else {
+               try {
+                 users.save('UnitTestTestUser', '', true);
+               } catch (e) {
+                 print(e);
+               }
+               expect(rootTestUser()).to.equal(false, `${name} was able to create a user with insufficent rights`);
+             }
+           });
+
+         });
+       });
+
+    }
+  });
+});
+
