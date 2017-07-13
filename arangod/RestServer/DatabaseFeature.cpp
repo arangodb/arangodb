18355121136579672b14581d0c2fec92c1767054
Frank Celler <frank@arangodb.com>
2017-07-13 19:27:32 +0200

    fixed dependencies (#2796)

--- arangod/RestServer/DatabaseFeature.cpp
+++ arangod/RestServer/DatabaseFeature.cpp
@@ -237,6 +237,7 @@ DatabaseFeature::DatabaseFeature(ApplicationServer* server)
   startsAfter("DatabasePath");
   startsAfter("EngineSelector");
   startsAfter("InitDatabase");
+  startsAfter("MMFilesEngine"); // TODO: remove from here! Currently, we need the _vocbase in systemDatabase()
   startsAfter("MMFilesPersistentIndex"); // TODO: remove from here!
   startsAfter("RocksDBEngine");
   startsAfter("Scheduler");
