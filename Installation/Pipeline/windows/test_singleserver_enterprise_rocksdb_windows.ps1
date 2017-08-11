5b97d6c17de90a58ada61c195ebf0cd79dd73989
Andreas Streichardt <andreas@arangodb.com>
2017-08-11 14:18:38 +0200

    Allow catch execution

--- Installation/Pipeline/windows/test_singleserver_enterprise_rocksdb_windows.ps1
+++ Installation/Pipeline/windows/test_singleserver_enterprise_rocksdb_windows.ps1
@@ -5,6 +5,7 @@ $ErrorActionPreference="Stop"
 . Installation\Pipeline\include\test_MODE_EDITION_ENGINE_windows.ps1
 
 Copy-Item -force .\build\bin\RelWithDebInfo\* .\build\bin\
+Copy-Item -force .\build\tests\RelWithDebInfo\* .\build\tests\
 
 $result = RunTests -port $port -engine rocksdb -edition enterprise -mode singleserver
 
