e3561ed41e025cb7b107a7bea2e3e7e67a4ec9a5
Andreas Streichardt <andreas@arangodb.com>
2017-08-15 12:23:54 +0200

    ICH RASTE AUS

--- Installation/Pipeline/include/test_MODE_EDITION_ENGINE_windows.ps1
+++ Installation/Pipeline/include/test_MODE_EDITION_ENGINE_windows.ps1
@@ -127,12 +127,9 @@ function createTests {
       name=$name
       script={
         param($name, $myport, $maxPort, $test, $cluster, $engine, $testArgs, $log)
-        $ErrorActionPreference="Stop"
-        Start-Transcript -Path $log
-        .\build\bin\arangosh.exe --log.level warning --javascript.execute UnitTests\unittest.js $test -- --cluster $cluster --storageEngine $engine --minPort $myport --maxPort $maxPort --skipNondeterministic true --skipTimeCritical true  --configDir etc/jenkins --skipLogAnalysis true $testargs *>&1
-        $result = $?
-        Stop-Transcript
-        if ($result -eq $false) {
+        $ErrorActionPreference="SilentlyContinue"
+        .\build\bin\arangosh.exe --log.level warning --javascript.execute UnitTests\unittest.js $test -- --cluster $cluster --storageEngine $engine --minPort $myport --maxPort $maxPort --skipNondeterministic true --skipTimeCritical true  --configDir etc/jenkins --skipLogAnalysis true $testargs *>&1 | Tee-Object -FilePath $log
+        if ($? -eq $false) {
           throw "arangosh returned a non zero exit code!"
         }
       }
