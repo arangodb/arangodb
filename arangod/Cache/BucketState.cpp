35f84836ce184af998009263b5c5d125f52772a5
Dan Larkin <dan@arangodb.com>
2017-08-17 17:20:20 +0200

    Small tweaks to BucketState locks.

--- arangod/Cache/BucketState.cpp
+++ arangod/Cache/BucketState.cpp
@@ -28,6 +28,7 @@
 #include <stdint.h>
 #include <atomic>
 
+using namespace std;
 using namespace arangodb::cache;
 
 BucketState::BucketState() : _state(0) {}
@@ -51,12 +52,13 @@ bool BucketState::lock(int64_t maxTries, BucketState::CallbackType cb) {
   int64_t attempt = 0;
   while (maxTries < 0 || attempt < maxTries) {
     // expect unlocked, but need to preserve migrating status
-    uint32_t current = _state.load();
+    uint32_t current = _state.load(memory_order_relaxed);
     uint32_t expected = current & (~static_cast<uint32_t>(Flag::locked));
     if (current == expected) {
+      uint32_t desired = expected | static_cast<uint32_t>(Flag::locked);
       bool success = _state.compare_exchange_strong(
-          expected,
-          (expected | static_cast<uint32_t>(Flag::locked)));  // try to lock
+          expected, desired,
+          memory_order_acquire, memory_order_relaxed);  // try to lock
       if (success) {
         cb();
         return true;
@@ -72,7 +74,7 @@ bool BucketState::lock(int64_t maxTries, BucketState::CallbackType cb) {
 
 void BucketState::unlock() {
   TRI_ASSERT(isLocked());
-  _state &= ~static_cast<uint32_t>(Flag::locked);
+  _state.fetch_and(~static_cast<uint32_t>(Flag::locked), memory_order_release);
 }
 
 bool BucketState::isSet(BucketState::Flag flag) const {
