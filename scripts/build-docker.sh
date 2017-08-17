9fab44cb320c111238b6f19acdc55ec23dc00063
Kaveh Vahedipour <kaveh@arangodb.com>
2017-08-17 14:21:20 +0200

    Feature/build docker to build using stretch container (#3062)
    * build docker to build using stretch container
    
    * open and close methods without specifying vocbase

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -12,8 +12,9 @@ VERSION=$(cat ${ROOTDIR}/VERSION)
 export DEBVERSION=${VERSION}-1
 DOCKERTAG=${VERSION}-local
 DEBIMAGE_NAME="arangodb3-${DEBVERSION}_amd64"
-BUILDDEB_ARGS=""
-BUILDDEB_DOCKER_ARGS="--gcc6"
+BUILDDEB_ARGS="--gcc6"
+BUILDDEB_DOCKER_ARGS=""
+
 DOCKERFILENAME=Dockerfile$(echo ${VERSION} | cut -d '.' -f 1,2 --output-delimiter=).local
 for i in $@; do
     if test "$i" == "--enterprise"; then
