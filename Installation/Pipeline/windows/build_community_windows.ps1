7585cd040b3e51a2a8debbccb12c8f6db5714fad
Andreas Streichardt <andreas@arangodb.com>
2017-08-25 10:52:43 +0200

    hihihihihihihiihihihi

--- Installation/Pipeline/windows/build_community_windows.ps1
+++ Installation/Pipeline/windows/build_community_windows.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference="Stop"
 $env:_MSPDBSRV_ENDPOINT_="community-${env:BUILD_TAG}"
+echo "HASS ${env:_MSPDBSRV_ENDPOINT_}"
 $buildOptions = "-DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSKIP_PACKAGING=On"
 Remove-Item -Force -Recurse log-output -ErrorAction SilentlyContinue
 New-Item -Force -ItemType Directory log-output -ErrorAction SilentlyContinue
