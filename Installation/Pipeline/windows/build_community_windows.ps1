c89fba4c116af4d01a162ec60cf28fb2bb7a61ef
Andreas Streichardt <andreas@arangodb.com>
2017-08-25 10:48:52 +0200

    Fix build?

--- Installation/Pipeline/windows/build_community_windows.ps1
+++ Installation/Pipeline/windows/build_community_windows.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference="Stop"
-echo "TAG: ${env:BUILD_TAG}"
-$env:_MSPDBSRV_ENDPOINT_=$env:BUILD_TAG
+$env:_MSPDBSRV_ENDPOINT_="community-${env:BUILD_TAG}"
 $buildOptions = "-DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSKIP_PACKAGING=On"
 Remove-Item -Force -Recurse log-output -ErrorAction SilentlyContinue
 New-Item -Force -ItemType Directory log-output -ErrorAction SilentlyContinue
