02990540d91cd744ca07ace454eedb4fae00db4f
Dan Larkin <danielhlarkin@users.noreply.github.com>
2017-08-28 08:54:23 -0400

    Report proper error for arangoimp create collection failure. (#3127)

--- arangosh/Import/ImportHelper.cpp
+++ arangosh/Import/ImportHelper.cpp
@@ -715,9 +715,21 @@ bool ImportHelper::checkCreateCollection() {
     return true;
   }
 
-  LOG_TOPIC(ERR, arangodb::Logger::FIXME)
-      << "unable to create collection '" << _collectionName
-      << "', server returned status code: " << static_cast<int>(code);
+  std::shared_ptr<velocypack::Builder> bodyBuilder(result->getBodyVelocyPack());
+  velocypack::Slice error = bodyBuilder->slice();
+  if (!error.isNone()) {
+    auto errorNum = error.get("errorNum").getUInt();
+    auto errorMsg = error.get("errorMessage").copyString();
+    LOG_TOPIC(ERR, arangodb::Logger::FIXME)
+        << "unable to create collection '" << _collectionName
+        << "', server returned status code: " << static_cast<int>(code)
+        << "; error [" << errorNum << "] " << errorMsg;
+  } else {
+    LOG_TOPIC(ERR, arangodb::Logger::FIXME)
+        << "unable to create collection '" << _collectionName
+        << "', server returned status code: " << static_cast<int>(code)
+        << "; server returned message: " << result->getBody();
+  }
   _hasError = true;
   return false;
 }
