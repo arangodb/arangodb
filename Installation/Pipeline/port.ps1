20f3a081725879347af856589a1b67d1a7c53357
Andreas Streichardt <andreas@arangodb.com>
2017-08-29 11:12:36 +0200

    FUNKTIONIERE!!!

--- Installation/Pipeline/port.ps1
+++ Installation/Pipeline/port.ps1
@@ -1,8 +1,4 @@
-echo "HMM"
-
 New-Item -ItemType Directory -Force -Path C:\ports | Out-Null
-
-echo "HUU"
 
 $timeLimit = (Get-Date).AddMinutes(-480)
 Get-ChildItem C:\ports | ? { $_.LastWriteTime -lt $timeLimit } | Remove-Item -ErrorAction SilentlyContinue | Out-Null
@@ -11,11 +7,9 @@ $port = 10000
 $portIncrement = 2000
 
 $port = $port - $portIncrement
-
 do {
     $port = $port + $portIncrement
     $portFile = "C:\ports\$port"
 }
-until (New-Item -ItemType File -Path $portFile -ErrorAction SilentlyContinue)
+until (New-Item -ItemType File -Path $portFile -Force -ErrorAction SilentlyContinue | Out-Null)
 Write-Output $port
-exit 0
