3eac29e50671f771f305189d71cea87bbeb98525
Wilfried Goesgens <willi@arangodb.com>
2017-09-06 16:36:33 +0200

    make the different values influencing the compaction run configurable

--- arangod/MMFiles/MMFilesCompactorThread.cpp
+++ arangod/MMFiles/MMFilesCompactorThread.cpp
@@ -21,6 +21,7 @@
 /// @author Dr. Frank Celler
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "MMFilesCompactionFeature.h"
 #include "MMFilesCompactorThread.h"
 #include "Basics/ConditionLocker.h"
 #include "Basics/ReadLocker.h"
@@ -662,7 +663,7 @@ bool MMFilesCompactorThread::compactCollection(LogicalCollection* collection, bo
   }
   
   std::vector<CompactionInfo> toCompact;
-  toCompact.reserve(maxFiles());
+  toCompact.reserve(MMFilesCompactionFeature::COMPACTOR->maxFiles());
 
   // now we have datafiles that we can process 
   size_t const n = datafiles.size();
@@ -674,12 +675,12 @@ bool MMFilesCompactorThread::compactCollection(LogicalCollection* collection, bo
   uint64_t const numDocuments = getNumberOfDocuments(collection);
 
   // get maximum size of result file
-  uint64_t maxSize = maxSizeFactor() * static_cast<MMFilesCollection*>(collection->getPhysical())->journalSize();
+  uint64_t maxSize = MMFilesCompactionFeature::COMPACTOR->maxSizeFactor() * static_cast<MMFilesCollection*>(collection->getPhysical())->journalSize();
   if (maxSize < 8 * 1024 * 1024) {
     maxSize = 8 * 1024 * 1024;
   }
-  if (maxSize >= maxResultFilesize()) {
-    maxSize = maxResultFilesize();
+  if (maxSize >= MMFilesCompactionFeature::COMPACTOR->maxResultFilesize()) {
+    maxSize = MMFilesCompactionFeature::COMPACTOR->maxResultFilesize();
   }
 
   if (start >= n || numDocuments == 0) {
@@ -711,7 +712,9 @@ bool MMFilesCompactorThread::compactCollection(LogicalCollection* collection, bo
       break;
     }
 
-    if (!doCompact && df->maximalSize() < smallDatafileSize() && i < n - 1) {
+    if (!doCompact &&
+        df->maximalSize() < MMFilesCompactionFeature::COMPACTOR->smallDatafileSize() &&
+        (i < n - 1)) {
       // very small datafile and not the last one. let's compact it so it's
       // merged with others
       doCompact = true;
@@ -728,18 +731,18 @@ bool MMFilesCompactorThread::compactCollection(LogicalCollection* collection, bo
       // compact first datafile(s) if they contain only deletions
       doCompact = true;
       reason = ReasonOnlyDeletions;
-    } else if (dfi.sizeDead >= deadSizeThreshold()) {
+    } else if (dfi.sizeDead >= MMFilesCompactionFeature::COMPACTOR->deadSizeThreshold()) {
       // the size of dead objects is above some threshold
       doCompact = true;
       reason = ReasonDeadSize;
     } else if (dfi.sizeDead > 0 &&
                (((double)dfi.sizeDead /
-                     ((double)dfi.sizeDead + (double)dfi.sizeAlive) >= deadShare()) ||
-                ((double)dfi.sizeDead / (double)df->maximalSize() >= deadShare()))) {
+                     ((double)dfi.sizeDead + (double)dfi.sizeAlive) >= MMFilesCompactionFeature::COMPACTOR->deadShare()) ||
+                ((double)dfi.sizeDead / (double)df->maximalSize() >= MMFilesCompactionFeature::COMPACTOR->deadShare()))) {
       // the size of dead objects is above some share
       doCompact = true;
       reason = ReasonDeadSizeShare;
-    } else if (dfi.numberDead >= deadNumberThreshold()) {
+    } else if (dfi.numberDead >= MMFilesCompactionFeature::COMPACTOR->deadNumberThreshold()) {
       // the number of dead objects is above some threshold
       doCompact = true;
       reason = ReasonDeadCount;
@@ -813,8 +816,8 @@ bool MMFilesCompactorThread::compactCollection(LogicalCollection* collection, bo
       break;
     }
 
-    if (totalSize >= smallDatafileSize() &&
-        toCompact.size() >= maxFiles()) {
+    if (totalSize >= MMFilesCompactionFeature::COMPACTOR->smallDatafileSize() &&
+        toCompact.size() >= MMFilesCompactionFeature::COMPACTOR->maxFiles()) {
       // found enough files to compact
       break;
     }
@@ -859,7 +862,6 @@ void MMFilesCompactorThread::run() {
   MMFilesEngine* engine = static_cast<MMFilesEngine*>(EngineSelectorFeature::ENGINE);
   std::vector<arangodb::LogicalCollection*> collections;
   int numCompacted = 0;
-
   while (true) {
     // keep initial _state value as vocbase->_state might change during
     // compaction loop
@@ -904,7 +906,7 @@ void MMFilesCompactorThread::run() {
 
               try {
                 double const now = TRI_microtime();
-                if (physical->lastCompactionStamp() + compactionCollectionInterval() <= now) {
+                if (physical->lastCompactionStamp() + MMFilesCompactionFeature::COMPACTOR->compactionCollectionInterval() <= now) {
                   auto ce = arangodb::MMFilesCollection::toMMFilesCollection(
                                 collection)
                                 ->ditches()
@@ -963,7 +965,7 @@ void MMFilesCompactorThread::run() {
       } else if (state != TRI_vocbase_t::State::SHUTDOWN_COMPACTOR && _vocbase->state() == TRI_vocbase_t::State::NORMAL) {
         // only sleep while server is still running
         CONDITION_LOCKER(locker, _condition);
-        _condition.wait(compactionSleepTime());
+        _condition.wait(MMFilesCompactionFeature::COMPACTOR->compactionSleepTime());
       }
     
       if (state == TRI_vocbase_t::State::SHUTDOWN_COMPACTOR || isStopping()) {
