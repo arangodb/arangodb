91994f634db2df829a1bc5e7878304fcd6a83090
Frank Celler <frank@arangodb.com>
2017-09-18 14:55:36 +0200

    fixed missing brace

--- lib/Random/RandomGenerator.cpp
+++ lib/Random/RandomGenerator.cpp
@@ -313,7 +313,7 @@ class RandomDeviceCombined : public RandomDevice {
       if (r == 0) {
         LOG_TOPIC(FATAL, arangodb::Logger::FIXME) << "read on random device failed: nothing read";
         FATAL_ERROR_EXIT();
-      } else if (r < 0) 
+      } else if (r < 0) {
         if (errno == EWOULDBLOCK || errno == EAGAIN) {
           LOG_TOPIC(INFO, arangodb::Logger::FIXME) << "not enough entropy (got " << (sizeof(buffer) - n)
                     << "), switching to pseudo-random";
