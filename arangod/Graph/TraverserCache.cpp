59bb1fbc946b6eb1c1960f2be7d180c8218c0c73
jsteemann <jan@arangodb.com>
2017-09-21 12:23:31 +0200

    oops

--- arangod/Graph/TraverserCache.cpp
+++ arangod/Graph/TraverserCache.cpp
@@ -66,7 +66,7 @@ VPackSlice TraverserCache::lookupToken(EdgeDocumentToken const& idToken) {
     LOG_TOPIC(ERR, arangodb::Logger::GRAPHS) << "Could not extract indexed edge document, return 'null' instead. "
       << "This is most likely a caching issue. Try: 'db."
       << col->name() <<".unload(); db." << col->name() << ".load()' in arangosh to fix this.";
-    TRI_ASSERT(false); for maintainer mode
+    TRI_ASSERT(false); // for maintainer mode
     return basics::VelocyPackHelper::NullValue();
   }
 
