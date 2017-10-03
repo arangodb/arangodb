744a74b064b5d384e842d981e91116efec67adec
Frank Celler <frank@arangodb.com>
2017-10-04 00:05:47 +0200

    updated pipeline to devel

--- Installation/Pipeline/build_OS_EDITION_MAINTAINER.sh
+++ Installation/Pipeline/build_OS_EDITION_MAINTAINER.sh
@@ -3,7 +3,6 @@ concurrency=$1
 os=$2
 edition=$3
 maintainer=$4
-logdir=$5
 
 ENTERPRISE=""
 type="build"
@@ -48,36 +47,51 @@ fi
 
 mkdir -p build
 
-if [ -z "$logdir" ]; then
-  logdir=log-output
-  rm -rf $logdir
-  mkdir -p $logdir
+if [ ! -f build/location ]; then
+    if [ "$os" == mac ]; then
+        (ls -l  && echo "$os-$edition-$maintainer") | md5 | awk '{print $1}' > build/location
+    else
+        (ls -l  && echo "$os-$edition-$maintainer") | md5sum | awk '{print $1}' > build/location
+    fi
 fi
 
-touch $logdir/build.log
+GENPATH="/tmp/`cat build/location`"
 
-echo "CONCURRENY: $concurrency" | tee -a $logdir/build.log
-echo "HOST: `hostname`" | tee -a $logdir/build.log
-echo "PWD: `pwd`" | tee -a $logdir/build.log
+rm -f $GENPATH
+ln -s `pwd` $GENPATH
+cd $GENPATH
 
-(
-    set -eo pipefail
-    cd build
+if test -z "$CMAKE_BUILD_TYPE"; then
+    CMAKE_BUILD_TYPE=RelWithDebInfo
+fi
 
+echo "CONCURRENY: $concurrency"
+echo "HOST: `hostname`"
+echo "PWD: `pwd`"
+echo "BUILD_TYPE: $CMAKE_BUILD_TYPE"
+
+function configureBuild {
     echo "`date +%T` configuring..."
     CXXFLAGS=-fno-omit-frame-pointer \
         cmake \
-            -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
             -DCMAKE_INSTALL_PREFIX=/ \
             -DUSE_CATCH_TESTS=On \
             -DDEBUG_SYNC_REPLICATION=On \
             $ENTERPRISE \
             $MAINTAINER \
             $PACKAGING \
-            ..  2>&1 | tee -a ../$logdir/build.log
+            ..
+}
+
+(
+    set -eo pipefail
+    cd build
+
+    configureBuild
 
     echo "`date +%T` building..."
-    make -j $concurrency -l $load 2>&1 | tee -a ../$logdir/build.log
+    make -j $concurrency -l $load 2>&1
 ) || exit 1
 
 echo "`date +%T` done..."
