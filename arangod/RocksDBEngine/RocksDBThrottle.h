01730fdb36079fdb4db91c92caef0c4b69733a26
matthewvon <matthewv@arangodb.com>
2017-10-06 09:08:50 -0400

    correct comments in file header

--- arangod/RocksDBEngine/RocksDBThrottle.h
+++ arangod/RocksDBEngine/RocksDBThrottle.h
@@ -17,9 +17,21 @@
 ///
 /// Copyright holder is ArangoDB GmbH, Cologne, Germany
 ///
-/// @author Daniel H. Larkin
-/// @author Jan Christoph Uhde
+/// @author Matthew Von-Maszewski
 ////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// based upon leveldb/util/throttle.cc
+// Copyright (c) 2011-2017 Basho Technologies, Inc. All Rights Reserved.
+//
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+////////////////////////////////////////////////////////////////////////////////
+
 
 #ifndef ARANGO_ROCKSDB_ROCKSDB_THROTTLE_H
 #define ARANGO_ROCKSDB_ROCKSDB_THROTTLE_H 1
