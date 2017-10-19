8c639f9718bfdb22f54ab294931d47b4806b157b
Simon Grätzer <simon@graetzer.org>
2017-10-19 13:40:58 +0200

    Using superuser rights for dashboard now

--- arangod/GeneralServer/GeneralCommTask.cpp
+++ arangod/GeneralServer/GeneralCommTask.cpp
@@ -416,10 +416,14 @@ rest::ResponseCode GeneralCommTask::canAccessPath(GeneralRequest* request) const
     if (result != rest::ResponseCode::OK) {
       if (path == "/" ||
           StringUtils::isPrefix(path, Open) ||
-          StringUtils::isPrefix(path, AdminAardvark) ||
-          StringUtils::isPrefix(path, ApiUser + username + '/')) {
+          StringUtils::isPrefix(path, AdminAardvark)) {
         // mop: these paths are always callable...they will be able to check
         // req.user when it could be validated
+        result = rest::ResponseCode::OK;
+        vc->upgradeSuperuser();
+      } else if (StringUtils::isPrefix(path, ApiUser + username + '/')) {
+        // simon: unauthorized users should be able to call
+        // `/_api/users/<name>` to check their passwords
         result = rest::ResponseCode::OK;
         vc->upgradeReadOnly();
       }
