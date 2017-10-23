cce430eb9add6e5ab817ef3c285e8f023176f9d3
Heiko <hkernbach@users.noreply.github.com>
2017-10-23 14:42:41 +0200

    V3.2.5 documentation (#3487)
    * make sure we always have properly defined whether we found systemd or… (#3379)
    
    * make sure we always have properly defined whether we found systemd or not.
    
    * fix source file location - we don't pre-process this.
    
    * minify
    
    * added hs to docu
    
    * info arangorestore

---
+++ Documentation/Books/Cookbook/styles/hs.js
@@ -0,0 +1,25 @@
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+(function (id, src, attrs) {
+  if (document.getElementById(id)) {
+    try { console.warn('duplicate hubspot script with id: "' + id + '" included on page'); }
+    finally { return; }
+  }
+  var js = document.createElement('script');
+  js.src = src;
+  js.type = 'text/javascript';
+  js.id = id;
+  for (var name in attrs) { if(attrs.hasOwnProperty(name)) { js.setAttribute(name, attrs[name]); } }
+  var e = document.getElementsByTagName('script')[0];
+  e.parentNode.insertBefore(js, e);
+})('hubspot-messages-loader', 'https://js.usemessages.com/messageswidgetshell.js', {"data-loader":"hs-scriptloader","data-hsjs-portal":2482448,"data-hsjs-env":"prod"});
+
+(function (id, src) {
+  if (document.getElementById(id)) { return; }
+  var js = document.createElement('script');
+  js.src = src;
+  js.type = 'text/javascript';
+  js.id = id;
+  var e = document.getElementsByTagName('script')[0];
+  e.parentNode.insertBefore(js, e);
+})('hs-analytics', '//js.hs-analytics.net/analytics/1508760300000/2482448.js');
