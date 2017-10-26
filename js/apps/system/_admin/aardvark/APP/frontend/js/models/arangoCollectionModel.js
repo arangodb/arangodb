bf528485382b09db336d46cef0e6ef4449b3a5ee
Heiko <hkernbach@users.noreply.github.com>
2017-10-26 09:56:25 +0200

    replicationFactor of a collection is now changeable in a cluster environment (#3502)

--- js/apps/system/_admin/aardvark/APP/frontend/js/models/arangoCollectionModel.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/models/arangoCollectionModel.js
@@ -220,7 +220,7 @@
       });
     },
 
-    changeCollection: function (wfs, journalSize, indexBuckets, callback) {
+    changeCollection: function (wfs, journalSize, indexBuckets, replicationFactor, callback) {
       var result = false;
       if (wfs === 'true') {
         wfs = true;
@@ -232,6 +232,10 @@
         journalSize: parseInt(journalSize, 10),
         indexBuckets: parseInt(indexBuckets, 10)
       };
+
+      if (replicationFactor) {
+        data.replicationFactor = parseInt(replicationFactor, 10);
+      }
 
       $.ajax({
         cache: false,
