9e8132eecdc013751f0c16a720697fb83d9390df
jsteemann <jan@arangodb.com>
2017-11-08 18:17:03 +0100

    fix crash

--- arangod/Aql/AstNode.cpp
+++ arangod/Aql/AstNode.cpp
@@ -1278,7 +1278,7 @@ double AstNode::getDoubleValue() const {
 
 /// @brief whether or not the node value is trueish
 bool AstNode::isTrue() const {
-  if (type == NODE_TYPE_ATTRIBUTE_ACCESS) {
+  if (type == NODE_TYPE_ATTRIBUTE_ACCESS && isConstant()) {
     AstNode const* resolved = Ast::resolveConstAttributeAccess(this);
     return resolved->isTrue();
   }
@@ -1320,7 +1320,7 @@ bool AstNode::isTrue() const {
 
 /// @brief whether or not the node value is falsey
 bool AstNode::isFalse() const {
-  if (type == NODE_TYPE_ATTRIBUTE_ACCESS) {
+  if (type == NODE_TYPE_ATTRIBUTE_ACCESS && isConstant()) {
     AstNode const* resolved = Ast::resolveConstAttributeAccess(this);
     return resolved->isFalse();
   }
