2a21d1adae18a7c8db5b63586315fe2ca97bdb14
Daniel Larkin-York <dan@arangodb.com>
2017-11-20 09:32:13 -0500

    Added prefix-matching for suite names and correct verison list.

--- js/client/modules/@arangodb/testsuites/upgrade-data.js
+++ js/client/modules/@arangodb/testsuites/upgrade-data.js
@@ -159,8 +159,7 @@ exports.setup = function (testFns, defaultFns, opts, fnDocs, optionsDoc) {
   const functionsDocumentation = {};
   const engines = ['mmfiles', 'rocksdb'];
   const versions = [
-    '3.2.7',
-    'devel'
+    '3.2.1', '3.2.2', '3.2.3', '3.2.4', '3.2.5', '3.2.6', '3.2.7'
   ];
   for (let i = 0; i < engines.length; i++) {
     const engine = engines[i];
