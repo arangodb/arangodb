8b3358e018ce6db5206faaedf3e7ceff35be1025
Wilfried Goesgens <willi@arangodb.com>
2017-11-29 13:57:51 +0100

    deploy versions through shell script from central places

--- Documentation/Books/Cookbook/styles/header.js
+++ Documentation/Books/Cookbook/styles/header.js
@@ -93,7 +93,7 @@ function appendHeader() {
   //render header
   rerenderNavbar();
   function addGoogleSrc() {
-    var cx = '010085642145132923492:ol7d_ppmq48';
+    var cx = '@GSEARCH_ID@';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
