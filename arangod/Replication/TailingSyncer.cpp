08f9326eedf65f3d29ce87a3471002a16fc9943a
Jan <jsteemann@users.noreply.github.com>
2017-12-07 10:36:13 +0100

    make sure we do not dereference a nullptr (#3906)

--- arangod/Replication/TailingSyncer.cpp
+++ arangod/Replication/TailingSyncer.cpp
@@ -568,6 +568,8 @@ Result TailingSyncer::renameCollection(VPackSlice const& slice) {
     if (col == nullptr) {
       return Result(TRI_ERROR_ARANGO_COLLECTION_NOT_FOUND, "unknown old collection name");
     }
+  } else {
+    return Result(TRI_ERROR_ARANGO_COLLECTION_NOT_FOUND, "unable to identify collection");
   }
   if (col->isSystem()) {
     LOG_TOPIC(WARN, Logger::REPLICATION) << "Renaming system collection " << col->name();
