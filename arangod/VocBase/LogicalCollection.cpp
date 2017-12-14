a93773c3d00a9c6968cacbae8c28ba8e976fb7f5
Jan <jsteemann@users.noreply.github.com>
2017-12-14 10:05:34 +0100

    forgotten fix for globallyUniqueId (#4032)

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -1388,9 +1388,11 @@ std::string LogicalCollection::generateGloballyUniqueId() const {
   result.reserve(64);
   if (ServerState::isCoordinator(role)) {
     TRI_ASSERT(_planId != 0);
+    result.append("c");
     result.append(std::to_string(_planId));
   } else if (ServerState::isDBServer(role)) {
     TRI_ASSERT(_planId != 0);
+    result.append("c");
     // we add the shard name to the collection. If we ever
     // replicate shards, we can identify them cluster-wide
     result.append(std::to_string(_planId));
