cbbb7c316bb0c090bae1d8313daaddf5fd5b8bd7
Simran Brucherseifer <simran@arangodb.com>
2017-12-19 16:44:18 +0100

    Remove MarkdownPP completely

--- Documentation/Scripts/codeBlockReader.py
+++ Documentation/Scripts/codeBlockReader.py
@@ -3,10 +3,19 @@ import sys
 import re
 import inspect
 
-validExtensions = (".cpp", ".h", ".js", ".mdpp")
+validExtensions = (".cpp", ".h", ".js", ".md")
 # specify the paths in which docublocks are searched. note that js/apps/* must not be included because it contains js/apps/system/
 # and that path also contains copies of some files present in js/ anyway.
-searchPaths = ["arangod/", "lib/", "js/actions", "js/client", "js/apps/system/_system/cerberus", "js/apps/system/_api/gharial", "js/common", "js/server", "Documentation/Books/Users/"]
+
+searchMDPPPaths = [
+  "Users",
+  "Cookbook"
+]
+searchPaths = [
+  "Documentation/Books/Users/",
+  "Documentation/Books/Cookbook/",
+  "Documentation/DocuBlocks/"
+]
 fullSuccess = True
 
 def file_content(filepath):
@@ -23,8 +32,12 @@ def file_content(filepath):
 
   for line in enumerate(filelines):
     if "@startDocuBlock" in line[1]:
-      # in the mdpp's we have non-terminated startDocuBlocks, else its an error:
-      if _start != None and not 'mdpp' in filepath:
+      # in the unprocessed md files we have non-terminated startDocuBlocks, else it is an error:
+      if ((_start != None) and
+          (not searchMDPPPaths[0] in filepath) and 
+          (not searchMDPPPaths[1] in filepath) and 
+          (not searchMDPPPaths[2] in filepath) and 
+          (not searchMDPPPaths[3] in filepath)):
         print "next startDocuBlock found without endDocuBlock inbetween in file %s [%s]" %(filepath, line)
         raise
       _start = line[0]
