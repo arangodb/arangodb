a052248f4bb8a0fb3e345a30b63e808640648f66
jsteemann <jan@arangodb.com>
2018-01-15 17:39:49 +0100

    added more tests

--- UnitTests/HttpInterface/api-replication-rocksdb-spec.rb
+++ UnitTests/HttpInterface/api-replication-rocksdb-spec.rb
@@ -398,7 +398,7 @@ describe ArangoDB do
         sleep 5
 
         cmd = api + "/logger-follow?from=" + fromTick
-        doc = ArangoDB.log_get("#{prefix}-follow-create-collection", cmd, :body => "", :format => :plain)
+        doc = ArangoDB.log_get("#{prefix}-follow-collection", cmd, :body => "", :format => :plain)
         doc.code.should eq(200)
 
         doc.headers["x-arango-replication-lastincluded"].should match(/^\d+$/)
