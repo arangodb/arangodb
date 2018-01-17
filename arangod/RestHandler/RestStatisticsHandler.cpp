48d7eba081a65f659691ea397ce2a54b2cf135c4
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-01-17 13:19:46 +0100

    create new class and add it to CMake

---
+++ arangod/RestHandler/RestStatisticsHandler.cpp
@@ -0,0 +1,51 @@
+
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2018 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Christoph Uhde
+////////////////////////////////////////////////////////////////////////////////
+
+#include "RestStatisticsHandler.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "Rest/HttpRequest.h"
+
+#include <velocypack/Builder.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::basics;
+using namespace arangodb::rest;
+
+RestStatisticsHandler::RestStatisticsHandler(GeneralRequest* request, GeneralResponse* response)
+  : RestVocbaseBaseHandler(request, response) {}
+
+RestStatus RestStatisticsHandler::execute() {
+  bool parseSuccess = true;
+  std::shared_ptr<VPackBuilder> parsedBody =
+      parseVelocyPackBody(parseSuccess);
+
+  if (parseSuccess) {
+    VPackBuilder result;
+    generateResult(rest::ResponseCode::OK, parsedBody->slice());
+  }
+
+  return RestStatus::DONE;
+}
---
+++ arangod/RestHandler/RestStatisticsHandler.h
@@ -0,0 +1,42 @@
+
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2018 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Christoph Uhde
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_REST_HANDLER_REST_STATISTICS_HANDLER_H
+#define ARANGOD_REST_HANDLER_REST_STATISTICS_HANDLER_H 1
+
+#include "RestHandler/RestVocbaseBaseHandler.h"
+
+namespace arangodb {
+class RestStatisticsHandler : public arangodb::RestVocbaseBaseHandler {
+ public:
+  RestStatisticsHandler(GeneralRequest*, GeneralResponse*);
+
+ public:
+  char const* name() const override final { return "RestStatisticsHandler"; }
+  bool isDirect() const override { return true; }
+  RestStatus execute() override;
+};
+}
+
+#endif
