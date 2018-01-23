e3f136f11d888d984f1f544b147e9a4af36a9547
Jan <jsteemann@users.noreply.github.com>
2018-01-23 17:44:31 +0100

    potential fix for issue #4272 (#4310)

--- lib/V8/v8-utils.cpp
+++ lib/V8/v8-utils.cpp
@@ -2681,7 +2681,7 @@ static void JS_Write(v8::FunctionCallbackInfo<v8::Value> const& args) {
                                      "invalid <content> buffer value");
     }
 
-    std::ofstream file;
+    std::fstream file;
 
     file.open(*name, std::ios::out | std::ios::binary);
 
@@ -2689,6 +2689,7 @@ static void JS_Write(v8::FunctionCallbackInfo<v8::Value> const& args) {
       file.write(data, size);
       if (flush) {
         file.flush();
+        file.sync();
       }
       file.close();
       TRI_V8_RETURN_TRUE();
@@ -2700,7 +2701,7 @@ static void JS_Write(v8::FunctionCallbackInfo<v8::Value> const& args) {
       TRI_V8_THROW_TYPE_ERROR("<content> must be a string");
     }
 
-    std::ofstream file;
+    std::fstream file;
 
     file.open(*name, std::ios::out | std::ios::binary);
 
@@ -2708,6 +2709,7 @@ static void JS_Write(v8::FunctionCallbackInfo<v8::Value> const& args) {
       file << *content;
       if (flush) {
         file.flush();
+        file.sync();
       }
       file.close();
       TRI_V8_RETURN_TRUE();
