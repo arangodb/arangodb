144ec947bfa5bba273a90179dc701d993c21b354
Alan Plum <me@pluma.io>
2018-01-30 15:31:20 +0100

    Fix store support in foxx-manager legacy API

--- js/actions/_admin/foxx/app.js
+++ js/actions/_admin/foxx/app.js
@@ -103,7 +103,7 @@ function resolveAppInfo (appInfo) {
   if (fs.exists(appInfo)) {
     return fs.readFileSync(appInfo);
   }
-  return appInfo;
+  return {source: appInfo};
 }
 
 // //////////////////////////////////////////////////////////////////////////////
