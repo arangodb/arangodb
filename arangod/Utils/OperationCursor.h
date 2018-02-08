f7545cf538ec541e9849b09b25c602ca2a5773c2
jsteemann <jan@arangodb.com>
2018-02-08 10:23:11 +0100

    simplify implementation as suggested by @graetzer

--- arangod/Utils/OperationCursor.h
+++ arangod/Utils/OperationCursor.h
@@ -118,17 +118,11 @@ struct OperationCursor {
     while (nextDocument(callback, batchSize)) {}
   }
   
-  /// @brief convenience function to retrieve a single result
-  void oneDocument(IndexIterator::DocumentCallback const& callback) {
-    nextDocument(callback, 1);
-  }
-
-/// @brief Skip the next toSkip many elements.
-///        skipped will be increased by the amount of skipped elements afterwards
-///        Check hasMore()==true before using this
-///        NOTE: This will throw on OUT_OF_MEMORY
+  /// @brief Skip the next toSkip many elements.
+  ///        skipped will be increased by the amount of skipped elements afterwards
+  ///        Check hasMore()==true before using this
+  ///        NOTE: This will throw on OUT_OF_MEMORY
   int skip(uint64_t, uint64_t&);
-
 };
 
 }
