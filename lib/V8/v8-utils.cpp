bca73c16f16010ccccb215a50126acd4e3801fa3
jsteemann <jan@arangodb.com>
2018-02-12 10:38:54 +0100

    it's dangerous to read from TryCatch if it has not been properly populated before

--- lib/V8/v8-utils.cpp
+++ lib/V8/v8-utils.cpp
@@ -416,10 +416,12 @@ static void JS_Parse(v8::FunctionCallbackInfo<v8::Value> const& args) {
       v8::Local<v8::Object> exceptionObj =
           tryCatch.Exception().As<v8::Object>();
       v8::Handle<v8::Message> message = tryCatch.Message();
-      exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
-                        v8::Number::New(isolate, message->GetLineNumber()));
-      exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
-                        v8::Number::New(isolate, message->GetStartColumn()));
+      if (!message.IsEmpty()) {
+        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
+                          v8::Number::New(isolate, message->GetLineNumber()));
+        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
+                          v8::Number::New(isolate, message->GetStartColumn()));
+      }
       exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "fileName"), filename->ToString());
       tryCatch.ReThrow();
       return;
@@ -487,10 +489,12 @@ static void JS_ParseFile(v8::FunctionCallbackInfo<v8::Value> const& args) {
       v8::Local<v8::Object> exceptionObj =
           tryCatch.Exception().As<v8::Object>();
       v8::Handle<v8::Message> message = tryCatch.Message();
-      exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
-                        v8::Number::New(isolate, message->GetLineNumber()));
-      exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
-                        v8::Number::New(isolate, message->GetStartColumn()));
+      if (!message.IsEmpty()) {
+        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
+                          v8::Number::New(isolate, message->GetLineNumber()));
+        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
+                          v8::Number::New(isolate, message->GetStartColumn()));
+      }
       exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "fileName"), args[0]);
       tryCatch.ReThrow();
       return;
@@ -1044,10 +1048,12 @@ static void JS_Execute(v8::FunctionCallbackInfo<v8::Value> const& args) {
         v8::Local<v8::Object> exceptionObj =
             tryCatch.Exception().As<v8::Object>();
         v8::Handle<v8::Message> message = tryCatch.Message();
-        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
-                          v8::Number::New(isolate, message->GetLineNumber()));
-        exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
-                          v8::Number::New(isolate, message->GetStartColumn()));
+        if (!message.IsEmpty()) {
+          exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "lineNumber"),
+                            v8::Number::New(isolate, message->GetLineNumber()));
+          exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "columnNumber"),
+                            v8::Number::New(isolate, message->GetStartColumn()));
+        }
         exceptionObj->Set(TRI_V8_ASCII_STRING(isolate, "fileName"),
                           filename->ToString());
         tryCatch.ReThrow();
