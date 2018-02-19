7f3f29ea63d900266e035752f1bf6791c9d609ca
Tobias Gödderz <tobias@arangodb.com>
2018-02-19 08:54:40 +0100

    Unified status code during shutdown to 503

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -564,7 +564,7 @@ void RestReplicationHandler::handleTrampolineCoordinator() {
   auto cc = ClusterComm::instance();
   if (cc == nullptr) {
     // nullptr happens only during controlled shutdown
-    generateError(rest::ResponseCode::BAD, TRI_ERROR_SHUTTING_DOWN,
+    generateError(rest::ResponseCode::SERVICE_UNAVAILABLE, TRI_ERROR_SHUTTING_DOWN,
                   "shutting down server");
     return;
   }
@@ -2200,7 +2200,7 @@ void RestReplicationHandler::handleCommandHoldReadLockCollection() {
   }
 
   if (stopping) {
-    generateError(rest::ResponseCode::SERVER_ERROR, TRI_ERROR_SHUTTING_DOWN);
+    generateError(rest::ResponseCode::SERVICE_UNAVAILABLE, TRI_ERROR_SHUTTING_DOWN);
     return;
   }
 
