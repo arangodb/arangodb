fa0f10017029ea2e61bbdc9c285abe1b070deb78
jsteemann <jan@arangodb.com>
2018-02-21 11:34:08 +0100

    properly convert the JS object to VPack for transactions
    previously this did not work when the "action" attribute was a JavaScript function

--- lib/V8/v8-vpack.cpp
+++ lib/V8/v8-vpack.cpp
@@ -283,8 +283,11 @@ static inline void AddValue(BuilderContext& context,
 template <bool performAllChecks, bool inObject>
 static int V8ToVPack(BuilderContext& context,
                      v8::Handle<v8::Value> const parameter,
-                     arangodb::StringRef const& attributeName) {
-  if (parameter->IsNull() || parameter->IsUndefined()) {
+                     arangodb::StringRef const& attributeName,
+                     bool convertFunctionsToNull) {
+  if (parameter->IsNull() || 
+      parameter->IsUndefined() || 
+      (convertFunctionsToNull && parameter->IsFunction())) {
     AddValue<VPackValue, inObject>(context, attributeName,
                                    VPackValue(VPackValueType::Null));
     return TRI_ERROR_NO_ERROR;
@@ -347,7 +350,7 @@ static int V8ToVPack(BuilderContext& context,
       }
 
       int res = V8ToVPack<performAllChecks, false>(context, value,
-                                                   arangodb::StringRef());
+                                                   arangodb::StringRef(), convertFunctionsToNull);
 
       --context.level;
 
@@ -417,7 +420,7 @@ static int V8ToVPack(BuilderContext& context,
 
           if (!converted.IsEmpty()) {
             // return whatever toJSON returned
-            return V8ToVPack<performAllChecks, inObject>(context, converted, attributeName);
+            return V8ToVPack<performAllChecks, inObject>(context, converted, attributeName, convertFunctionsToNull);
           }
         }
 
@@ -452,7 +455,7 @@ static int V8ToVPack(BuilderContext& context,
       }
 
       int res = V8ToVPack<performAllChecks, true>(
-          context, value, arangodb::StringRef(*str, str.length()));
+          context, value, arangodb::StringRef(*str, str.length()), convertFunctionsToNull);
 
       --context.level;
 
@@ -475,13 +478,14 @@ static int V8ToVPack(BuilderContext& context,
 ////////////////////////////////////////////////////////////////////////////////
 
 int TRI_V8ToVPack(v8::Isolate* isolate, VPackBuilder& builder,
-                  v8::Handle<v8::Value> const value, bool keepTopLevelOpen) {
+                  v8::Handle<v8::Value> const value, bool keepTopLevelOpen,
+                  bool convertFunctionsToNull) {
   v8::HandleScope scope(isolate);
   BuilderContext context(isolate, builder, keepTopLevelOpen);
   TRI_GET_GLOBALS();
   TRI_GET_GLOBAL_STRING(ToJsonKey);
   context.toJsonKey = ToJsonKey;
-  return V8ToVPack<true, false>(context, value, arangodb::StringRef());
+  return V8ToVPack<true, false>(context, value, arangodb::StringRef(), convertFunctionsToNull);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -495,5 +499,5 @@ int TRI_V8ToVPackSimple(v8::Isolate* isolate,
                         v8::Handle<v8::Value> const value) {
   // a HandleScope must have been created by the caller already
   BuilderContext context(isolate, builder, false);
-  return V8ToVPack<false, false>(context, value, arangodb::StringRef());
+  return V8ToVPack<false, false>(context, value, arangodb::StringRef(), false);
 }
