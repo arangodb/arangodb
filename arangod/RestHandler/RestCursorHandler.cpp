e533877bb1fe004bea482e2cc2d006eab3413ef1
jsteemann <jan@arangodb.com>
2018-03-09 09:47:18 +0100

    prevent crash on invalid json

--- arangod/RestHandler/RestCursorHandler.cpp
+++ arangod/RestHandler/RestCursorHandler.cpp
@@ -47,7 +47,8 @@ RestCursorHandler::RestCursorHandler(
       _queryLock(),
       _query(nullptr),
       _hasStarted(false),
-      _queryKilled(false) {}
+      _queryKilled(false),
+      _isValidForFinalize(false) {}
 
 RestStatus RestCursorHandler::execute() {
   // extract the sub-request type
@@ -423,6 +424,11 @@ void RestCursorHandler::createCursor() {
       // error message generated in parseVelocyPackBody
       return;
     }
+    
+    // tell RestCursorHandler::finalizeExecute that the request
+    // could be parsed successfully and that it may look at it
+    _isValidForFinalize = true;
+
     VPackSlice body = parsedBody.get()->slice();
 
     processQuery(body);
