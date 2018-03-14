263fe1f6e9469867cdf6ef678e0e61ddacde0730
Kaveh Vahedipour <kaveh@arangodb.com>
2018-03-14 12:03:40 +0100

    3.3: fixed the missed changes to plan after agency callback is registred f… (#4777)
    * fixed the missed changes to plan after agency callback is registred for create collection
    * fixed agency inquire

--- arangod/Agency/AgencyComm.cpp
+++ arangod/Agency/AgencyComm.cpp
@@ -1496,17 +1496,13 @@ AgencyCommResult AgencyComm::sendWithFailover(
       result = send(
           connection.get(), method, conTimeout, url, b.toJson());
 
-      // Inquire returns a body like write or if the write is still ongoing
-      // We check, if the operation is still ongoing then body is {"ongoing:true"}
+      // Inquire returns a body like write, if the transactions are not known,
+      // the list of results is empty.
       // _statusCode can be 200 or 412
       if (result.successful() || result._statusCode == 412) {
         std::shared_ptr<VPackBuilder> resultBody
           = VPackParser::fromJson(result._body);
         VPackSlice outer = resultBody->slice();
-        // If the operation is still ongoing, simply ask again later:
-        if (outer.isObject() && outer.hasKey("ongoing")) {
-          continue;
-        }
 
         // If we get an answer, and it contains a "results" key,
         // we release the connection and break out of the loop letting the
