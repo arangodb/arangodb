48d85506d929d438da8856efa5383a1433b4d0c3
Andrey Abramov <andrey@arangodb.com>
2018-03-21 20:14:59 +0300

    more places to change error codes

--- lib/Basics/voc-errors.h
+++ lib/Basics/voc-errors.h
@@ -344,7 +344,7 @@ constexpr int TRI_ERROR_ARANGO_DATADIR_INVALID                                  
 /// Will be raised when a document with a given identifier or handle is unknown.
 constexpr int TRI_ERROR_ARANGO_DOCUMENT_NOT_FOUND                               = 1202;
 
-/// 1203: ERROR_ARANGO_COLLECTION_NOT_FOUND
+/// 1203: ERROR_ARANGO_DATA_SOURCE_NOT_FOUND
 /// "collection not found"
 /// Will be raised when a collection with the given identifier or name is
 /// unknown.
