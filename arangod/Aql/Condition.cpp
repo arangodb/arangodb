1c426435f870167db0954381350a4fd679a45de5
jsteemann <jan@arangodb.com>
2018-03-21 16:42:01 +0100

    remove useless code

--- arangod/Aql/Condition.cpp
+++ arangod/Aql/Condition.cpp
@@ -1176,9 +1176,6 @@ bool Condition::CanRemove(ExecutionPlan const* plan, ConditionPart const& me,
             temp.clear();
             TRI_AttributeNamesToString(result.second, temp);
             if (temp == me.attributeName) {
-            //if (temp == me.attributeName ||
-            //    lhs->toString() == std::string("$") + std::to_string(result.first->id) + "." + me.attributeName ||
-            //    lhs->toString() + "[*]" == std::string("$") + std::to_string(result.first->id) + "." + me.attributeName) {
               if (rhs->isConstant()) {
                 ConditionPart indexCondition(result.first, result.second, operand,
                                             ATTRIBUTE_LEFT, nullptr);
@@ -1204,9 +1201,6 @@ bool Condition::CanRemove(ExecutionPlan const* plan, ConditionPart const& me,
             temp.clear();
             TRI_AttributeNamesToString(result.second, temp);
             if (temp == me.attributeName) {
-            //if (temp == me.attributeName ||
-            //    rhs->toString() == std::string("$") + std::to_string(result.first->id) + "." + me.attributeName ||
-            //    rhs->toString() + "[*]" == std::string("$") + std::to_string(result.first->id) + "." + me.attributeName) {
               if (lhs->isConstant()) {
                 ConditionPart indexCondition(result.first, result.second, operand,
                                             ATTRIBUTE_RIGHT, nullptr);
