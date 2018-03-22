d5e5bfcf42760c455accda2f8f52a078753dff02
Tobias Gödderz <tobias@arangodb.com>
2018-03-22 14:19:52 +0100

    Cleanup of test repair-distribute-shards-like-spec

--- js/server/tests/resilience/repair-distribute-shards-like-spec.js
+++ js/server/tests/resilience/repair-distribute-shards-like-spec.js
@@ -52,9 +52,6 @@ const waitForPlanEqualCurrent = function (collection) {
     const Plan = shardDist[collection.name()].Plan;
     const Current = shardDist[collection.name()].Current;
 
-    if (!_.isObject(Plan)) {
-      internal.print(Plan);
-    }
     if (_.isObject(Plan) && _.isEqual(Plan, Current)) {
       return true;
     }
@@ -144,26 +141,11 @@ const expectCollectionPlanToEqualProto = function (collection, protoCollection) 
     let followers = shardDistPlan[shard].followers.slice().sort();
     let protoFollowers = protoShardDistPlan[protoShard].followers.slice().sort();
 
-    internal.print("comparing ", followers);
-    internal.print("with      ", protoFollowers);
     expect(followers).to.deep.equal(protoFollowers);
   }
 };
 
 
-// TODO instead of a smart graph create two collections, one with distributeShardsLike:
-// - Create collection A
-// - Create collection B with distributeShardsLike=A
-// - Wait for both to be replicated
-// - Use an agency transaction to rename distributeShardsLike (with a comment
-//   that this will break your database in production)
-// - Use MoveShard Operations (and wait for them) to break the
-//   distributeShardsLike assumptions. Use a DBServer order/permutation
-//   depending on the servers available in the proto-shard and their order
-//   to make this deterministic.
-// - Use an agency transaction to restore distributeShardsLike (with a comment
-//   that this will break your database in production)
-// - Execute repairs
 describe('Collections with distributeShardsLike', function () {
   afterEach(function() {
     internal.db._drop(colName);
@@ -188,6 +170,17 @@ describe('Collections with distributeShardsLike', function () {
     expect(response).to.have.property("message", "Nothing to do.");
   });
 
+
+// - Create collection A
+// - Create collection B with distributeShardsLike=A
+// - Wait for both to be replicated
+// - Use an agency transaction to rename distributeShardsLike
+// - Use MoveShard Operations (and wait for them) to break the
+//   distributeShardsLike assumptions. Use a DBServer order/permutation
+//   depending on the servers available in the proto-shard and their order
+//   to make this deterministic.
+// - Use an agency transaction to restore distributeShardsLike
+// - Execute repairs
   it('if broken, should be repaired', function() {
     const replicationFactor = dbServerCount - 1;
     const protoCollection = internal.db._createDocumentCollection(protoColName,
@@ -198,17 +191,9 @@ describe('Collections with distributeShardsLike', function () {
     expect(waitForPlanEqualCurrent(protoCollection)).to.be.true;
     expect(waitForPlanEqualCurrent(collection)).to.be.true;
 
-    const printDsl = () => {
-      const query = `arango/Plan/Collections/${internal.db._name()}/${collection._id}/distributeShardsLike`;
-      const result = global.ArangoAgency.read([[query]]);
-
-      internal.print("=== DSL: ", result[0].arango.Plan.Collections[internal.db._name()][collection._id]);
-    };
-
-printDsl();
-internal.print(`=== REMOVE DSL`);
+    // IMPORTANT NOTE: Never do this in a real environment. Changing
+    // distributeShardsLike will break your cluster!
     global.ArangoAgency.remove(`Plan/Collections/${internal.db._name()}/${collection._id}/distributeShardsLike`);
-printDsl();
 
     global.ArangoClusterInfo.flush();
     const protoShardDist = internal.getCollectionShardDistribution(protoColName)[protoColName].Plan;
@@ -224,14 +209,6 @@ printDsl();
         (nameToId, server) => {
           nameToId[server.serverName] = server.serverId;
           return nameToId;
-        },
-        {}
-      );
-    const dbServerNameById =
-      dbServers.reduce(
-        (idToName, server) => {
-          idToName[server.serverId] = server.serverName;
-          return idToName;
         },
         {}
       );
@@ -269,93 +246,36 @@ printDsl();
     const leaderDbServer = dbServerIdByName[shardInfo.leader];
     const followerDbServer = dbServerIdByName[shardInfo.followers[0]];
 
-    const printPlan = () => {
-      global.ArangoClusterInfo.flush();
-      const shardDist = internal.getCollectionShardDistribution(collection._id);
-      const Plan = shardDist[collection.name()].Plan;
-      internal.print(`=== PLAN: "${shard}":`, Plan[shard]);
-    };
-    const printCurrent = () => {
-      global.ArangoClusterInfo.flush();
-      const shardDist = internal.getCollectionShardDistribution(collection._id);
-      const Current = shardDist[collection.name()].Current;
-      internal.print(`=== CURR: "${shard}":`, Current[shard]);
-    };
-
-    internal.print({
-      protoShardInfo: protoShardInfo,
-      shard: shard,
-      shardInfo: shardInfo,
-      freeDbServers: freeDbServers,
-      dbServerIdByName: dbServerIdByName,
-    });
-printPlan(); printCurrent();
-    internal.print("Wait for plan to equal current...");
     expect(waitForPlanEqualCurrent(collection)).to.be.true;
-printPlan(); printCurrent();
-internal.print(`=== FIRST MOVE`);
-internal.print(`Moving ${dbServerNameById[leaderDbServer]} to ${dbServerNameById[freeDbServer]}`
-  + ` (on ${collection._dbName}.${collection._id}.${shard})`);
     let jobId = postMoveShardJob(leaderDbServer, freeDbServer, true);
-internal.print(`jobId = ${jobId}`);
-printPlan(); printCurrent();
-internal.print("Wait for agency job...");
     let result = waitForAgencyJob(jobId);
     expect(result).to.equal(true);
-printPlan(); printCurrent();
-    internal.print("Wait for plan to equal current...");
     expect(waitForPlanEqualCurrent(collection)).to.be.true;
-printPlan(); printCurrent();
 
-internal.print(`=== SECOND MOVE`);
-internal.print(`Moving ${dbServerNameById[followerDbServer]} to ${dbServerNameById[leaderDbServer]}`
-  + ` (on ${collection._dbName}.${collection._id}.${shard})`);
     jobId = postMoveShardJob(followerDbServer, leaderDbServer, false);
-internal.print(`jobId = ${jobId}`);
-    printPlan(); printCurrent();
-    internal.print("Wait for agency job...");
     result = waitForAgencyJob(jobId);
-printPlan(); printCurrent();
-
-    const target = global.ArangoAgency.read([['arango/Target']])[0]['arango']["Target"];
-    internal.print(`=== arango/Target: `, target);
 
     expect(result).to.equal(true);
-    internal.print("Wait for plan to equal current...");
     expect(waitForPlanEqualCurrent(collection)).to.be.true;
-printPlan(); printCurrent(); printDsl();
 
-internal.print(`=== RESTORE DSL`);
+    // IMPORTANT NOTE: Never do this in a real environment. Changing
+    // distributeShardsLike will break your cluster!
     global.ArangoAgency.set(
       `Plan/Collections/${internal.db._name()}/${collection._id}/distributeShardsLike`,
       protoCollection._id
     );
-printPlan(); printCurrent(); printDsl();
 
-    internal.print("Wait plan to equal current...");
     expect(waitForPlanEqualCurrent(collection)).to.be.true;
-printPlan(); printCurrent(); printDsl();
 
-    // TODO dump plan and see if
-    // a) the collection is broken as intended and
-    // b) distributeShardsLike is set correctly, so the repair job should find
-    //    something to do
-
-internal.print(`=== STARTING REPAIRS`);
     const d = request.post(coordinator.url + '/_admin/repair/distributeShardsLike');
-internal.print(`=== FINISHED REPAIRS`);
 
     let response = JSON.parse(d.body);
 
-internal.print(`=== [DEBUG] response = `, response);
-
     expect(response).to.have.property("error", false);
     expect(response).to.have.property("code", 200);
-    // TODO make sure 5 is correct, I haven't thought it through yet
     expect(response).to.have.property("message", "Executing 5 operations");
 
     global.ArangoClusterInfo.flush();
-    printDsl();
 
     // Note: properties() returns the name of the collection in distributeShardsLike
     // instead of the id!
@@ -364,225 +284,8 @@ internal.print(`=== [DEBUG] response = `, response);
     ).to.equal(protoCollection.name());
 
     expectCollectionPlanToEqualProto(collection, protoCollection);
+
+    // TODO Should the exact operations be checked? Or do we leave that to the unit tests?
   });
 
 });
-
-/* example response:
-
-{
-  "error": false,
-  "code": 200,
-  "message": "Executing 3 operations",
-  "Operations": [
-    "BeginRepairsOperation\n{ database: _system\n, collection: _local_E (11020054)\n, protoCollection: V (11020039)\n, collectionReplicationFactor: 3\n, protoReplicationFactor: 2\n, renameDistributeShardsLike: 1\n}",
-    "MoveShardOperation\n{ database: _system\n, collection: _local_E (11020054)\n, shard: s11020057\n, from: PRMR-287ae22f-92ba-4f10-a641-9e760f4f0e88\n, to: PRMR-8e8bf7e1-a792-4a54-b64f-95559009855a\n, isLeader: 0\n}",
-    "FinishRepairsOperation\n{ database: _system\n, collection: _local_E (11020054)\n, protoCollection: V (11020039)\n, replicationFactor: 2\n}"
-  ]
-}
-
- */
-
-/* example shard distribution:
-
-{
-  "RepairDistributeShardsLikeTestCollection": {
-    "Plan": {
-      "s5010126": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010125": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010124": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      },
-      "s5010122": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010121": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010113": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010123": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010115": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010112": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010111": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010120": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010116": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010114": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      },
-      "s5010117": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010118": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010119": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      }
-    },
-    "Current": {
-      "s5010126": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010125": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010124": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      },
-      "s5010122": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010121": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010113": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010123": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010115": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010112": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010111": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010120": {
-        "leader": "DBServer0005",
-        "followers": [
-          "DBServer0004"
-        ]
-      },
-      "s5010116": {
-        "leader": "DBServer0004",
-        "followers": [
-          "DBServer0003"
-        ]
-      },
-      "s5010114": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      },
-      "s5010117": {
-        "leader": "DBServer0003",
-        "followers": [
-          "DBServer0002"
-        ]
-      },
-      "s5010118": {
-        "leader": "DBServer0002",
-        "followers": [
-          "DBServer0001"
-        ]
-      },
-      "s5010119": {
-        "leader": "DBServer0001",
-        "followers": [
-          "DBServer0005"
-        ]
-      }
-    }
-  }
-}
- */
