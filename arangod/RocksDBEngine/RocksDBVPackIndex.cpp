a3073bec2a1b7337b933d181c31e0f42a2ab9c80
jsteemann <jan@arangodb.com>
2018-03-24 22:56:41 +0100

    adjust index selection slightly

--- arangod/RocksDBEngine/RocksDBVPackIndex.cpp
+++ arangod/RocksDBEngine/RocksDBVPackIndex.cpp
@@ -1112,8 +1112,6 @@ bool RocksDBVPackIndex::supportsFilterCondition(
       double estimate = this->selectivityEstimate(&ignore);
       if (estimate > 0.0) {
         estimatedItems = static_cast<size_t>(1.0 / estimate);
-        // expect less results
-        itemsInIndex = estimatedItems;
       }
     }
     if (itemsInIndex == 0) {
