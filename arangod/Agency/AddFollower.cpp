69f0352c6c2dafba97d4c88dfdd08ebf4094b465
Simon Grätzer <simon@graetzer.org>
2018-04-06 14:36:15 +0200

    fixing formatting

--- arangod/Agency/AddFollower.cpp
+++ arangod/Agency/AddFollower.cpp
@@ -33,14 +33,14 @@ AddFollower::AddFollower(Node const& snapshot, AgentInterface* agent,
                          std::string const& database,
                          std::string const& collection,
                          std::string const& shard)
-: Job(NOTFOUND, snapshot, agent, jobId, creator),
-_database(database),
-_collection(collection),
-_shard(shard) {}
+    : Job(NOTFOUND, snapshot, agent, jobId, creator),
+      _database(database),
+      _collection(collection),
+      _shard(shard) {}
 
 AddFollower::AddFollower(Node const& snapshot, AgentInterface* agent,
                          JOB_STATUS status, std::string const& jobId)
-: Job(status, snapshot, agent, jobId) {
+    : Job(status, snapshot, agent, jobId) {
   // Get job details from agency:
   try {
     std::string path = pos[status] + _jobId + "/";
@@ -65,23 +65,23 @@ void AddFollower::run() {
 
 bool AddFollower::create(std::shared_ptr<VPackBuilder> envelope) {
   LOG_TOPIC(INFO, Logger::SUPERVISION) << "Todo: AddFollower(s) "
-  << " to shard " << _shard << " in collection " << _collection;
-  
+    << " to shard " << _shard << " in collection " << _collection;
+
   bool selfCreate = (envelope == nullptr); // Do we create ourselves?
-  
+
   if (selfCreate) {
     _jb = std::make_shared<Builder>();
   } else {
     _jb = envelope;
   }
-  
+
   std::string now(timepointToString(std::chrono::system_clock::now()));
-  
+
   if (selfCreate) {
     _jb->openArray();
     _jb->openObject();
   }
-  
+
   _jb->add(VPackValue(toDoPrefix + _jobId));
   { VPackObjectBuilder guard(_jb.get());
     _jb->add("creator", VPackValue(_creator));
@@ -92,24 +92,24 @@ bool AddFollower::create(std::shared_ptr<VPackBuilder> envelope) {
     _jb->add("jobId", VPackValue(_jobId));
     _jb->add("timeCreated", VPackValue(now));
   }
-  
+
   _status = TODO;
-  
+
   if (!selfCreate) {
     return true;
   }
-  
+
   _jb->close();  // transaction object
   _jb->close();  // close array
-  
+
   write_ret_t res = singleWriteTransaction(_agent, *_jb);
-  
+
   if (res.accepted && res.indices.size() == 1 && res.indices[0]) {
     return true;
   }
-  
+
   _status = NOTFOUND;
-  
+
   LOG_TOPIC(INFO, Logger::SUPERVISION) << "Failed to insert job " + _jobId;
   return false;
 }
@@ -117,7 +117,7 @@ bool AddFollower::create(std::shared_ptr<VPackBuilder> envelope) {
 bool AddFollower::start() {
   // If anything throws here, the run() method catches it and finishes
   // the job.
-  
+
   // Are we distributeShardsLiking other shard? Then fail miserably.
   if (!_snapshot.has(planColPrefix + _database + "/" + _collection)) {
     finish("", "", true, "collection has been dropped in the meantime");
@@ -132,12 +132,12 @@ bool AddFollower::start() {
   
   // Look at Plan:
   std::string planPath =
-  planColPrefix + _database + "/" + _collection + "/shards/" + _shard;
-  
+      planColPrefix + _database + "/" + _collection + "/shards/" + _shard;
+
   Slice planned = _snapshot.get(planPath).slice();
-  
+
   TRI_ASSERT(planned.isArray());
-  
+
   // First check that we still have too few followers for the current
   // `replicationFactor`:
   size_t desiredReplFactor = collection.get("replicationFactor").getUInt();
@@ -146,14 +146,14 @@ bool AddFollower::start() {
     finish("", "", true, "job no longer necessary, have enough replicas");
     return true;
   }
-  
+
   // Check that the shard is not locked:
   if (_snapshot.has(blockedShardsPrefix + _shard)) {
     LOG_TOPIC(DEBUG, Logger::SUPERVISION) << "shard " << _shard
-    << " is currently locked, not starting AddFollower job " << _jobId;
+      << " is currently locked, not starting AddFollower job " << _jobId;
     return false;
   }
-  
+
   // Now find some new servers to add:
   auto available = Job::availableServers(_snapshot);
   // Remove those already in Plan:
@@ -170,15 +170,15 @@ bool AddFollower::start() {
       ++it;
     }
   }
-  
+
   // Check that we have enough:
   if (available.size() < desiredReplFactor - actualReplFactor) {
     LOG_TOPIC(DEBUG, Logger::SUPERVISION) << "shard " << _shard
-    << " does not have enough candidates to add followers, waiting, jobId="
-    << _jobId;
+      << " does not have enough candidates to add followers, waiting, jobId=" 
+      << _jobId;
     return false;
   }
-  
+
   // Randomly choose enough servers:
   std::vector<std::string> chosen;
   for (size_t i = 0; i < desiredReplFactor - actualReplFactor; ++i) {
@@ -190,14 +190,14 @@ bool AddFollower::start() {
     }
     available.pop_back();
   }
-  
+
   // Now we can act, simply add all in chosen to all plans for all shards:
   std::vector<Job::shard_t> shardsLikeMe
-  = clones(_snapshot, _database, _collection, _shard);
-  
+      = clones(_snapshot, _database, _collection, _shard);
+
   // Copy todo to finished:
   Builder todo, trx;
-  
+
   // Get todo entry
   { VPackArrayBuilder guard(&todo);
     // When create() was done with the current snapshot, then the job object
@@ -210,7 +210,7 @@ bool AddFollower::start() {
         // Just in case, this is never going to happen, since we will only
         // call the start() method if the job is already in ToDo.
         LOG_TOPIC(INFO, Logger::SUPERVISION)
-        << "Failed to get key " + toDoPrefix + _jobId + " from agency snapshot";
+          << "Failed to get key " + toDoPrefix + _jobId + " from agency snapshot";
         return false;
       }
     } else {
@@ -220,7 +220,7 @@ bool AddFollower::start() {
         // Just in case, this is never going to happen, since when _jb is
         // set, then the current job is stored under ToDo.
         LOG_TOPIC(WARN, Logger::SUPERVISION) << e.what() << ": "
-        << __FILE__ << ":" << __LINE__;
+          << __FILE__ << ":" << __LINE__;
         return false;
       }
     }
@@ -228,26 +228,26 @@ bool AddFollower::start() {
   
   // Enter pending, remove todo, block toserver
   { VPackArrayBuilder listOfTransactions(&trx);
-    
+
     { VPackObjectBuilder objectForMutation(&trx);
-      
+
       addPutJobIntoSomewhere(trx, "Finished", todo.slice()[0]);
       addRemoveJobFromSomewhere(trx, "ToDo", _jobId);
-      
+
       // --- Plan changes
       doForAllShards(_snapshot, _database, shardsLikeMe,
-                     [&trx, &chosen](Slice plan, Slice current, std::string& planPath) {
-                       trx.add(VPackValue(planPath));
-                       { VPackArrayBuilder serverList(&trx);
-                         for (auto const& srv : VPackArrayIterator(plan)) {
-                           trx.add(srv);
-                         }
-                         for (auto const& srv : chosen) {
-                           trx.add(VPackValue(srv));
-                         }
-                       }
-                     });
-      
+        [&trx, &chosen](Slice plan, Slice current, std::string& planPath) {
+          trx.add(VPackValue(planPath));
+          { VPackArrayBuilder serverList(&trx);
+            for (auto const& srv : VPackArrayIterator(plan)) {
+              trx.add(srv);
+            }
+            for (auto const& srv : chosen) {
+              trx.add(VPackValue(srv));
+            }
+          }
+        });
+
       addIncreasePlanVersion(trx);
     }  // mutation part of transaction done
     // Preconditions
@@ -263,15 +263,15 @@ bool AddFollower::start() {
   
   // Transact to agency
   write_ret_t res = singleWriteTransaction(_agent, trx);
-  
+
   if (res.accepted && res.indices.size() == 1 && res.indices[0]) {
     _status = FINISHED;
     LOG_TOPIC(INFO, Logger::SUPERVISION)
-    << "Finished: Addfollower(s) to shard " << _shard << " in collection "
-    << _collection;
+      << "Finished: Addfollower(s) to shard " << _shard << " in collection "
+      << _collection;
     return true;
   }
-  
+
   LOG_TOPIC(INFO, Logger::SUPERVISION) << "Start precondition failed for AddFollower " + _jobId;
   return false;
 }
@@ -280,29 +280,28 @@ JOB_STATUS AddFollower::status() {
   if (_status != PENDING) {
     return _status;
   }
-  
+
   TRI_ASSERT(false);   // PENDING is not an option for this job, since it
-  // travels directly from ToDo to Finished or Failed
+                       // travels directly from ToDo to Finished or Failed
   return _status;
 }
 
 arangodb::Result AddFollower::abort() {
-  
+
   // We can assume that the job is in ToDo or not there:
   if (_status == NOTFOUND || _status == FINISHED || _status == FAILED) {
     return Result(TRI_ERROR_SUPERVISION_GENERAL_FAILURE,
                   "Failed aborting addFollower job beyond pending stage");
   }
-  
-  Result result;
+
+  Result result;  
   // Can now only be TODO or PENDING
   if (_status == TODO) {
     finish("", "", false, "job aborted");
     return result;
   }
-  
+
   TRI_ASSERT(false);  // cannot happen, since job moves directly to FINISHED
   return result;
   
 }
-
