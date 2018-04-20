a57cf066989627fd4992e0a58030ee853921993e
Simran Brucherseifer <simran@arangodb.com>
2018-04-20 12:50:39 +0200

    Spelling in Aardvark

--- js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
@@ -1134,7 +1134,7 @@
       tableContent.push(
         window.modalView.createTextEntry(
           'new-app-mount',
-          'Mountpoint',
+          'Mount point',
           mountPoint,
           'The path the app will be mounted. Is not allowed to start with _',
           'mountpoint',
