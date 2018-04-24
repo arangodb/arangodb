6de6d95ee49e6b49dbe5436b8f1bd02030161652
mpoeter <manuel.poeter@leanfive.com>
2018-04-24 15:42:50 +0200

    Use read/write specific unlock function in *Locker and *Unlocker.

--- lib/Basics/WriteUnlocker.h
+++ lib/Basics/WriteUnlocker.h
@@ -77,14 +77,14 @@ class WriteUnlocker {
 
   WriteUnlocker(ReadWriteLocker* readWriteLock, char const* file, int line)
       : _readWriteLock(readWriteLock), _file(file), _line(line) {
-    _readWriteLock->unlock();
+    _readWriteLock->unlockWrite();
   }
 
 #else
 
   explicit WriteUnlocker(ReadWriteLock* readWriteLock)
       : _readWriteLock(readWriteLock) {
-      _readWriteLock->unlock();
+      _readWriteLock->unlockWrite();
     }
 
 #endif
