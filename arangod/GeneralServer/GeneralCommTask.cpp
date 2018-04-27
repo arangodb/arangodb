45fd380bd6c1e7c8be4289762dc8b701e5ea810c
Simon Grätzer <simon@graetzer.org>
2018-04-27 18:08:00 +0200

    adding some debugging stuff

--- arangod/GeneralServer/GeneralCommTask.cpp
+++ arangod/GeneralServer/GeneralCommTask.cpp
@@ -252,6 +252,8 @@ bool GeneralCommTask::handleRequestSync(std::shared_ptr<RestHandler> handler) {
     isDirect = false;
     isPrio = true;
   }
+#warning TODO remove later
+  isDirect = false;
 
   if (isDirect) {
     handleRequestDirectly(basics::ConditionalLocking::DoNotLock,
