9f1d7fb5aa8f5eb75fe1ce607fbd0ddda16b779c
mchacki <michael@arangodb.com>
2018-05-07 11:07:13 +0200

    Moved creation of the Agent into the prepare phase of the feature. THereby it is guaranteed that agents at least exists before the GeneralServer is activating endpoints

--- arangod/Agency/AgencyFeature.cpp
+++ arangod/Agency/AgencyFeature.cpp
@@ -228,13 +228,11 @@ void AgencyFeature::validateOptions(std::shared_ptr<ProgramOptions> options) {
 }
 
 void AgencyFeature::prepare() {
-}
-
-void AgencyFeature::start() {
   if (!isEnabled()) {
     return;
   }
 
+  // Available after validateOptions of ClusterFeature
   // Find the agency prefix:
   auto feature = ApplicationServer::getFeature<ClusterFeature>("Cluster");
   if (!feature->agencyPrefix().empty()) {
@@ -242,13 +240,13 @@ void AgencyFeature::start() {
       std::string("/") + feature->agencyPrefix());
     arangodb::consensus::Job::agencyPrefix = feature->agencyPrefix();;
   }
-  
-  // TODO: Port this to new options handling
+
   std::string endpoint;
 
   if (_agencyMyAddress.empty()) {
     std::string port = "8529";
 
+    // Available after prepare of EndpointFeature
     EndpointFeature* endpointFeature =
         ApplicationServer::getFeature<EndpointFeature>("Endpoint");
     auto endpoints = endpointFeature->httpEndpoints();
@@ -280,9 +278,15 @@ void AgencyFeature::start() {
         _waitForSync, _supervisionFrequency, _compactionStepSize,
         _compactionKeepSize, _supervisionGracePeriod, _cmdLineTimings,
         _maxAppendSize)));
-  
+
   AGENT = _agent.get();
-  
+}
+
+void AgencyFeature::start() {
+  if (!isEnabled()) {
+    return;
+  }
+ 
   LOG_TOPIC(DEBUG, Logger::AGENCY) << "Starting agency personality";
   _agent->start();
   
