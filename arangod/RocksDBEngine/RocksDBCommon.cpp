5e4d13332438ae3c61bb84ca1595102f6ae3d5d8
Simon Grätzer <simon@graetzer.org>
2018-05-12 01:27:58 +0200

    Slightly increase batch size

--- arangod/RocksDBEngine/RocksDBCommon.cpp
+++ arangod/RocksDBEngine/RocksDBCommon.cpp
@@ -198,7 +198,7 @@ Result removeLargeRange(rocksdb::TransactionDB* db,
       ++total;
       ++counter;
       batch.Delete(cf, it->key());
-      if (counter >= 200) {
+      if (counter >= 256) {
         LOG_TOPIC(DEBUG, Logger::FIXME) << "intermediate delete write";
         // Persist deletes all 1000 documents
         rocksdb::Status status = bDB->Write(rocksdb::WriteOptions(), &batch);
