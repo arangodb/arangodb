bd659c65bb621d10c1b2cc58c3a0fa17a5e5b6ec
Wilfried Goesgens <willi@arangodb.com>
2018-05-15 18:01:33 +0200

    backport a7b1bcb055bac21a8713793a23850e646de23dcf (#5357)

--- arangod/VocBase/AuthUserEntry.cpp
+++ arangod/VocBase/AuthUserEntry.cpp
@@ -316,43 +316,44 @@ VPackBuilder AuthUserEntry::toVPackBuilder() const {
   TRI_ASSERT(!_username.empty());
 
   VPackBuilder builder;
-  VPackObjectBuilder o(&builder, true);
-  if (!_key.empty()) {
-    builder.add(StaticStrings::KeyString, VPackValue(_key));
-  }
-  builder.add("user", VPackValue(_username));
-  AddSource(builder, _source);
-
-  {  // authData sub-object
-    VPackObjectBuilder o2(&builder, "authData", true);
-    builder.add("active", VPackValue(_active));
-    if (_source == AuthSource::COLLECTION) {
-      VPackObjectBuilder o3(&builder, "simple", true);
-      builder.add("hash", VPackValue(_passwordHash));
-      builder.add("salt", VPackValue(_passwordSalt));
-      builder.add("method", VPackValue(_passwordMethod));
+  {
+    VPackObjectBuilder o(&builder, true);
+    if (!_key.empty()) {
+      builder.add(StaticStrings::KeyString, VPackValue(_key));
     }
-  }
-  {  // databases sub-object
-    VPackObjectBuilder o2(&builder, "databases", true);
-    for (auto const& dbCtxPair : _dbAccess) {
-      VPackObjectBuilder o3(&builder, dbCtxPair.first, true);
-      {  // permissions
-        VPackObjectBuilder o4(&builder, "permissions", true);
-        AuthLevel lvl = dbCtxPair.second._databaseAuthLevel;
-        AddAuthLevel(builder, lvl);
+    builder.add("user", VPackValue(_username));
+    AddSource(builder, _source);
+
+    {  // authData sub-object
+      VPackObjectBuilder o2(&builder, "authData", true);
+      builder.add("active", VPackValue(_active));
+      if (_source == AuthSource::COLLECTION) {
+        VPackObjectBuilder o3(&builder, "simple", true);
+        builder.add("hash", VPackValue(_passwordHash));
+        builder.add("salt", VPackValue(_passwordSalt));
+        builder.add("method", VPackValue(_passwordMethod));
       }
-      {  // collections
-        VPackObjectBuilder o4(&builder, "collections", true);
-        for (auto const& colAccessPair : dbCtxPair.second._collectionAccess) {
-          VPackObjectBuilder o4(&builder, colAccessPair.first, true);
-          VPackObjectBuilder o5(&builder, "permissions", true);
-          AddAuthLevel(builder, colAccessPair.second);
+    }
+    {  // databases sub-object
+      VPackObjectBuilder o2(&builder, "databases", true);
+      for (auto const& dbCtxPair : _dbAccess) {
+        VPackObjectBuilder o3(&builder, dbCtxPair.first, true);
+        {  // permissions
+          VPackObjectBuilder o4(&builder, "permissions", true);
+          AuthLevel lvl = dbCtxPair.second._databaseAuthLevel;
+          AddAuthLevel(builder, lvl);
+        }
+        {  // collections
+          VPackObjectBuilder o4(&builder, "collections", true);
+          for (auto const& colAccessPair : dbCtxPair.second._collectionAccess) {
+            VPackObjectBuilder o4(&builder, colAccessPair.first, true);
+            VPackObjectBuilder o5(&builder, "permissions", true);
+            AddAuthLevel(builder, colAccessPair.second);
+          }
         }
       }
     }
   }
-
   return builder;
 }
 
