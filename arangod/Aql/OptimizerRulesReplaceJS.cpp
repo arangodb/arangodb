9b2601f44ce81482594bdcb006ca44608fa67f64
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-06-06 11:36:56 +0200

    WIP - try fail read ... repeat -> until success (REPLACE COMMIT LATER)

--- arangod/Aql/OptimizerRulesReplaceJS.cpp
+++ arangod/Aql/OptimizerRulesReplaceJS.cpp
@@ -69,6 +69,7 @@ namespace {
     FullText
   };
 
+  //NEAR(coll, 0 /*lat*/, 0 /*lon*/[, 10 /*limit*/])
   struct nearParams{
     std::string collection;
     double latitude;
@@ -90,12 +91,34 @@ namespace {
     }
   };
 
+  //FULLTEXT(collection, "attribute", "search", 100 /*limit*/[, "distance name"])
+  struct fulltextParams{
+    std::string collection;
+    std::string attribute;
+    std::string search;
+    AstNode* limit;
+
+    fulltextParams(AstNode const* node){
+      TRI_ASSERT(node->type == AstNodeType::NODE_TYPE_FCALL);
+      AstNode* arr = node->getMember(0);
+      TRI_ASSERT(arr->type == AstNodeType::NODE_TYPE_ARRAY);
+      collection = arr->getMember(0)->getString();
+      attribute = arr->getMember(1)->getString();
+      search = arr->getMember(2)->getString();
+      if(arr->numMembers() > 3){
+        limit = arr->getMember(3);
+      } else {
+        limit = nullptr;
+      }
+    }
+  };
+
   using NodeInfo = std::tuple<CalculationNode*, AstNode*, JSFunctionType>;
-  constexpr int c_cn = 0;
-  constexpr int c_an = 1; //node to function call (near/within/fulltext)
+  constexpr int c_calcNode = 0;
+  constexpr int c_astNode = 1; //node to function call (near/within/fulltext)
   constexpr int c_type = 2;
 
-  AstNode* getAst(CalculationNode* c){
+  AstNode* getAstNode(CalculationNode* c){
     return c->expression()->nodeForModification();
   }
 
@@ -116,35 +139,37 @@ namespace {
     SmallVector<ExecutionNode*> nodes{a};
     plan->findNodesOfType(nodes, ExecutionNode::CALCULATION, true);
 
-
     for(auto const& n : nodes){
       auto* c = static_cast<CalculationNode*>(n);
-      auto* cAst = getAst(c);
-      if(cAst->callsFunction()){
-        auto* fun = getFunction(cAst);
+      auto visitor = [&rv,&c](AstNode* a){
+        auto* fun = getFunction(a);
         if(fun){
           LOG_DEVEL << "loop " << fun->name;
           if (fun->name == std::string("NEAR")){
-            rv.emplace_back(c, cAst, JSFunctionType::Near);
+            rv.emplace_back(c, a, JSFunctionType::Near);
           }
           if (fun->name == std::string("WITHIN")){
-            rv.emplace_back(c, cAst, JSFunctionType::Within);
+            rv.emplace_back(c, a, JSFunctionType::Within);
           }
           if (fun->name == std::string("FULLTEXT")){
-            rv.emplace_back(c, cAst, JSFunctionType::FullText);
+            rv.emplace_back(c, a, JSFunctionType::FullText);
           }
         }
-      }
+        return a;
+      };
+      Ast::traverseAndModify(getAstNode(c),visitor);
+      // cat not be read only because we want a non-const
+      // pointer in the tuple
     }
     return rv;
   }
 
-  bool replaceNear(NodeInfo info){
-    auto* cAst = std::get<c_an>(info);
-    auto* fun = getFunction(cAst);
+  bool replaceNear(NodeInfo info, ExecutionPlan* plan){
+    auto* astNode = std::get<c_astNode>(info);
+    auto* fun = getFunction(astNode);
 
     LOG_DEVEL << fun->name;
-    nearParams params(cAst);
+    nearParams params(astNode);
 
     // get logical
     // find out if index is available
@@ -155,14 +180,13 @@ namespace {
     // delete function call form calculation node
 
     LOG_DEVEL << "collection: " << params.collection;
-    LOG_DEVEL << "achtung der willi";
 
     return true;
   }
 
-  bool replaceWithin(NodeInfo info){
+  bool replaceWithin(NodeInfo info, ExecutionPlan* plan){
     LOG_DEVEL << "replaceNear";
-    auto* cAst = std::get<c_an>(info);
+    auto* cAst = std::get<c_astNode>(info);
     auto* fun = getFunction(cAst);
 
     LOG_DEVEL << fun->name;
@@ -170,12 +194,71 @@ namespace {
     return true;
   }
 
-  bool replaceFulltext(NodeInfo info){
+  bool replaceFulltext(NodeInfo info, ExecutionPlan* plan){
     LOG_DEVEL << "replaceNear";
-    auto* cAst = std::get<c_an>(info);
+    auto* cAst = std::get<c_astNode>(info);
     auto* fun = getFunction(cAst);
 
     LOG_DEVEL << fun->name;
+    // store params as ast nodes?
+    fulltextParams params(cAst);
+    LOG_DEVEL << params.collection;
+    LOG_DEVEL << params.attribute;
+    LOG_DEVEL << params.search;
+
+    auto ast = plan->getAst();
+    CalculationNode* calcNode = std::get<c_calcNode>(info);
+
+    //// create subquery plan for fulltext
+
+    AstNode* subRootNode = ast->createNodeSubquery();
+
+
+    // create node for index [and limit]
+    AstNode* dummy = nullptr;
+
+    AstNode* indexAstNode = ast->createNodeLimit(dummy/*offset*/,params.limit/*count*/);
+    AstNode* resultExpression = indexAstNode;
+
+    if(params.limit){
+      AstNode* limitAstNode = ast->createNodeLimit(dummy,dummy);
+      indexAstNode->addMember(limitAstNode);
+      resultExpression = limitAstNode;
+    }
+
+    // Result expression must contain the calculations result
+    // provided by fulltext index and optional limit
+    // is limit correct?
+
+
+    AstNode* subReturnNode = ast->createNodeReturn(resultExpression);
+    subRootNode->addMember(subReturnNode);
+
+    //// create real plan from ast
+    ExecutionNode* subquery = plan->fromNode(subRootNode);
+    Variable* outVariable = plan->getAst()->variables()->createTemporaryVariable();
+    SubqueryNode* subqueryNode = plan->registerSubquery(
+        new SubqueryNode(plan, plan->nextId(), subquery, outVariable)
+    );
+    // outvariable let outvar = SUBQUERY(.....)
+
+    plan->insertDependency(calcNode, subqueryNode); // why do we need this
+                                                    // node is registed and
+                                                    // the outvariable will
+                                                    // be put in the original
+                                                    // CN what should already
+                                                    // define the dependency
+
+    // FIXME - expression gets only replaced if function ast node is
+    // the first in calculation node's expression
+    if(std::get<c_astNode>(info) == getAstNode(std::get<c_calcNode>(info))) {
+      auto* replacement = ast->createNodeReference(outVariable);
+      std::get<c_calcNode>(info)->expression()->replaceNode(replacement);
+    } else {
+      // IMPLEMENT
+      // what are examples of this case? fulltext eg is not meant to be used in a filter.
+      // query: ""
+    }
 
     return true;
   }
@@ -188,17 +271,17 @@ void arangodb::aql::replaceJSFunctions(Optimizer* opt
 
   bool modified = false;
 
-  auto nodes = getCandidates(plan.get());
-  for(auto& node : nodes){
-    switch (std::get<c_type>(node)) {
+  auto candidates = getCandidates(plan.get());
+  for(auto& candidate : candidates){
+    switch (std::get<c_type>(candidate)) {
       case JSFunctionType::FullText:
-        modified = replaceFulltext(node);
+        modified = replaceFulltext(candidate, plan.get());
         break;
       case JSFunctionType::Near:
-        modified = replaceNear(node);
+        modified = replaceNear(candidate, plan.get());
         break;
       case JSFunctionType::Within:
-        modified = replaceWithin(node);
+        modified = replaceWithin(candidate, plan.get());
         break;
     }
   }
