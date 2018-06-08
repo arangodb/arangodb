98a1fcce6dd975cc62b0f55d7405fe7bc0169076
hkernbach <heiko@arangodb.com>
2018-06-08 10:27:46 +0200

    applied requested review changes

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -1946,6 +1946,7 @@ void RestReplicationHandler::handleCommandApplierGetStateAll() {
   if (_request->databaseName() != StaticStrings::SystemDatabase) {
     generateError(rest::ResponseCode::FORBIDDEN, TRI_ERROR_FORBIDDEN,
                   "global inventory can only be fetched from within _system database");
+    return;
   }
   DatabaseFeature* databaseFeature = application_features::ApplicationServer::getFeature<DatabaseFeature>("Database");
 
@@ -1954,11 +1955,17 @@ void RestReplicationHandler::handleCommandApplierGetStateAll() {
   for (auto& name : databaseFeature->getDatabaseNames()) {
     builder.add(name, VPackValue(VPackValueType::Object));
     TRI_vocbase_t* vocbase = databaseFeature->lookupDatabase(name);
+
+    if (vocbase == nullptr) {
+      return;
+    }
+
     ReplicationApplier* applier = vocbase->replicationApplier();
 
     if (applier == nullptr) {
       return;
     }
+
     applier->toVelocyPack(builder);
     builder.close();
   }
