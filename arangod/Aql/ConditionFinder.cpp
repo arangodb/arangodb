f81e42af367835752e43579a280791a74fdd38cd
Jan Christoph Uhde <ObiWahn@users.noreply.github.com>
2018-06-12 13:48:31 +0200

    Add optimizer rule that replaces js function calls (NEAR/WITHIN/FULLTEXT) with pure AQL (#5529)

--- arangod/Aql/ConditionFinder.cpp
+++ arangod/Aql/ConditionFinder.cpp
@@ -148,8 +148,7 @@ bool ConditionFinder::before(ExecutionNode* en) {
         opts.ascending = !descending;
         std::unique_ptr<ExecutionNode> newNode(new IndexNode(
             _plan, _plan->nextId(), node->collection(),
-            node->outVariable(), usedIndexes, condition.get(), opts));
-        condition.release();
+            node->outVariable(), usedIndexes, std::move(condition), opts));
         TRI_IF_FAILURE("ConditionFinder::insertIndexNode") {
           THROW_ARANGO_EXCEPTION(TRI_ERROR_DEBUG);
         }
--- arangod/Aql/ExecutionNode.cpp
+++ arangod/Aql/ExecutionNode.cpp
@@ -1330,7 +1330,7 @@ void EnumerateCollectionNode::toVelocyPackHelper(VPackBuilder& builder, unsigned
 
   // add outvariable and projection
   DocumentProducingNode::toVelocyPack(builder);
-  
+
   // add collection information
   CollectionAccessingNode::toVelocyPack(builder);
 
@@ -1544,7 +1544,7 @@ void CalculationNode::toVelocyPackHelper(VPackBuilder& nodes, unsigned flags) co
   }
 
   nodes.add("expressionType", VPackValue(_expression->typeString()));
-  
+
   if ((flags & SERIALIZE_FUNCTIONS) &&
       _expression->node() != nullptr) {
     auto root = _expression->node();
@@ -1562,7 +1562,7 @@ void CalculationNode::toVelocyPackHelper(VPackBuilder& nodes, unsigned flags) co
             nodes.add("name", VPackValue(func->name));
             nodes.add("isDeterministic", VPackValue(func->isDeterministic));
             nodes.add("canRunOnDBServer", VPackValue(func->canRunOnDBServer));
-            nodes.add("usesV8", VPackValue(func->implementation == nullptr || (func->condition && !func->condition())));
+            nodes.add("usesV8", VPackValue(false));
             nodes.close();
           }
         } else if (node->type == NODE_TYPE_FCALL_USER) {
@@ -1684,7 +1684,7 @@ bool SubqueryNode::mayAccessCollections() {
     // document, then we count this as a "yes"
     return true;
   }
-  
+
   TRI_ASSERT(_subquery != nullptr);
 
   // if the subquery contains any of these nodes, it may access data from
@@ -1706,10 +1706,10 @@ bool SubqueryNode::mayAccessCollections() {
 
   SmallVector<ExecutionNode*>::allocator_type::arena_type a;
   SmallVector<ExecutionNode*> nodes{a};
-     
+
   NodeFinder<std::vector<ExecutionNode::NodeType>> finder(types, nodes, true);
   _subquery->walk(finder);
-  
+
   if (!nodes.empty()) {
     return true;
   }
