a67c4617b329884b14d20de58a69c67e474c5adf
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-06-13 10:28:50 +0200

    delete old code

--- arangod/Aql/OptimizerRulesCluster.cpp
+++ arangod/Aql/OptimizerRulesCluster.cpp
@@ -64,7 +64,6 @@ using namespace arangodb;
 using namespace arangodb::aql;
 using EN = arangodb::aql::ExecutionNode;
 
-////////////////////////////////////////////////////////////////////////////////
 ExecutionNode* hasSingleParent(ExecutionNode* in){
   auto parents = in->getParents();
   if(parents.size() == 1){
@@ -91,7 +90,6 @@ ExecutionNode* hasSingleParent(ExecutionNode* in, std::vector<EN::NodeType> type
         return parent;
       }
     }
-
   }
   return nullptr;
 }
@@ -146,119 +144,10 @@ bool hasBinaryCompare(ExecutionNode* node){
   return result;
 }
 
-struct DetectSingleDocumentOperations final : public WalkerWorker<ExecutionNode> {
-  ExecutionPlan* _plan;
-  IndexNode* _indexNode;
-  UpdateNode* _updateNode;
-  ReplaceNode* _replaceNode;
-  RemoveNode* _removeNode;
-
- public:
-  explicit DetectSingleDocumentOperations(ExecutionPlan* plan)
-      : _plan(plan),
-        _indexNode(nullptr),
-        _updateNode(nullptr),
-        _replaceNode(nullptr),
-        _removeNode(nullptr)
-  {}
-
-  bool enterSubquery(ExecutionNode*, ExecutionNode*) override final {
-    return false;
-  }
-
-  bool before(ExecutionNode* en) override final {
-    switch (en->getType()) {
-      case EN::TRAVERSAL:
-      case EN::SHORTEST_PATH:
-      case EN::ENUMERATE_LIST:
-#ifdef USE_IRESEARCH
-      case EN::ENUMERATE_IRESEARCH_VIEW:
-#endif
-      case EN::SUBQUERY:
-      case EN::FILTER:
-      case EN::ENUMERATE_COLLECTION:
-      case EN::CALCULATION:
-      case EN::COLLECT:
-      case EN::INSERT:
-      case EN::UPSERT:
-      case EN::NORESULTS:
-      case EN::SCATTER:
-      case EN::DISTRIBUTE:
-      case EN::GATHER:
-      case EN::REMOTE:
-      case EN::SORT:
-      case EN::LIMIT:  // LIMIT is criterion to stop
-        _indexNode = nullptr;
-        return true;   // abort.
-
-      case EN::UPDATE:
-        _updateNode = ExecutionNode::castTo<UpdateNode*>(en);
-        return false;
-      case EN::REPLACE:
-        _replaceNode = ExecutionNode::castTo<ReplaceNode*>(en);
-        return false;
-      case EN::REMOVE:
-        _removeNode =  ExecutionNode::castTo<RemoveNode*>(en);
-        return false;
-      case EN::SINGLETON:
-      case EN::RETURN:
-        return false;
-      case EN::INDEX:
-        if (_indexNode == nullptr) {
-          _indexNode = ExecutionNode::castTo<IndexNode*>(en);
-          return !hasBinaryCompare(en);
-        }
-        else {
-          _indexNode = nullptr;
-          return true;
-        }
-
-      default: {
-        // should not reach this point
-        _indexNode = nullptr;
-        TRI_ASSERT(false);
-      }
-    }
-    return true;
-  }
-};
-
 void arangodb::aql::substituteClusterSingleDocumentOperations(Optimizer* opt,
                                                               std::unique_ptr<ExecutionPlan> plan,
                                                               OptimizerRule const* rule) {
   bool modified = false;
-  if(false) {
-    SmallVector<ExecutionNode*>::allocator_type::arena_type a;
-    SmallVector<ExecutionNode*> nodes{a};
-    plan->findNodesOfType(nodes, EN::FILTER, true);
-
-    ExecutionNode* firstNode = nodes[0];
-
-    DetectSingleDocumentOperations finder(plan.get());
-    firstNode->walk(finder);
-
-    if (finder._indexNode != nullptr) {
-      modified = true;
-      auto roNode = new SingleRemoteOperationNode(finder._indexNode,
-                                                  finder._updateNode,
-                                                  finder._replaceNode,
-                                                  finder._removeNode);
-      plan->registerNode(roNode);
-      plan->replaceNode(finder._indexNode, roNode);
-
-      if (finder._updateNode != nullptr) {
-        plan->unlinkNode(finder._updateNode);
-      }
-      if (finder._replaceNode != nullptr) {
-        plan->unlinkNode(finder._replaceNode);
-      }
-      if (finder._removeNode != nullptr) {
-        plan->unlinkNode(finder._removeNode);
-      }
-
-    }
-  }
-
   SmallVector<ExecutionNode*>::allocator_type::arena_type a;
   SmallVector<ExecutionNode*> nodes{a};
   plan->findNodesOfType(nodes, EN::INDEX, true);
@@ -266,15 +155,13 @@ void arangodb::aql::substituteClusterSingleDocumentOperations(Optimizer* opt,
   for(auto* node : nodes){
     Index* index = hasSingleIndexHandle(node, Index::TRI_IDX_TYPE_PRIMARY_INDEX);
     if (index){
-      IndexNode* indexNode = static_cast<IndexNode*>(node);
       LOG_DEVEL << "has compare";
       if(!hasBinaryCompare(node)){
         // do nothing if index does not work on a single document
         break;
       }
-      //check fields and ranges ==
-      auto* parentModification = hasSingleParent(indexNode,{EN::INSERT, EN::REMOVE, EN::UPDATE, EN::UPSERT, EN::REPLACE});
-      auto* parentSelect = hasSingleParent(indexNode,EN::RETURN);
+      auto* parentModification = hasSingleParent(node,{EN::INSERT, EN::REMOVE, EN::UPDATE, EN::UPSERT, EN::REPLACE});
+      auto* parentSelect = hasSingleParent(node,EN::RETURN);
 
       if (parentModification){
         LOG_DEVEL << "optimize modification node";
@@ -294,7 +181,5 @@ void arangodb::aql::substituteClusterSingleDocumentOperations(Optimizer* opt,
     }
   }
 
-
   opt->addPlan(std::move(plan), rule, modified);
 }
-////////////////////////////////////////////////////////////////////////////////
