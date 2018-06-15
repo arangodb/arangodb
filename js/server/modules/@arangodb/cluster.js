a264d902276fcf78b144df1524dc2ddd8f67a117
Max Neunhöffer <max@arangodb.com>
2018-06-15 16:28:23 +0200

    Add an API to query for status of moveShard and cleanOutServer jobs. (#5594)
    This is so far intentionally undocumented, since we want to collect
    experience with it first.

--- js/server/modules/@arangodb/cluster.js
+++ js/server/modules/@arangodb/cluster.js
@@ -2149,6 +2149,22 @@ function endpoints() {
   }
 }
 
+function queryAgencyJob(id) {
+  let job = null;
+  let states = ["Finished", "Pending", "Failed", "ToDo"];
+  for (let s of states) {
+    try {
+      job = global.ArangoAgency.get('Target/' + s + '/' + id);
+      job = job.arango.Target[s];
+      if (Object.keys(job).length !== 0 && job.hasOwnProperty(id)) {
+        return {error: false, id, status: s, job: job[id]};
+      }
+    } catch (err) {
+    }
+  }
+  return {error: true, errorMsg: "Did not find job.", id, job: null};
+}
+
 exports.coordinatorId = coordinatorId;
 exports.handlePlanChange = handlePlanChange;
 exports.isCluster = isCluster;
@@ -2167,6 +2183,7 @@ exports.supervisionState = supervisionState;
 exports.waitForSyncRepl = waitForSyncRepl;
 exports.endpoints = endpoints;
 exports.fetchKey = fetchKey;
+exports.queryAgencyJob = queryAgencyJob;
 
 exports.executePlanForDatabases = executePlanForDatabases;
 exports.executePlanForCollections = executePlanForCollections;
