a247159b55bd39393cd805d8cdc1e3ae6b147e0f
jsteemann <jan@arangodb.com>
2018-06-26 20:05:23 +0200

    honor "restrictType" and "restrictCollections" when syncing

--- arangod/Replication/DatabaseInitialSyncer.h
+++ arangod/Replication/DatabaseInitialSyncer.h
@@ -124,8 +124,13 @@ class DatabaseInitialSyncer final : public InitialSyncer {
   /// in rocksdb for a constant view of the data
   double batchUpdateTime() const { return _batchUpdateTime; }
 
+  /// @brief fetch the server's inventory, public method
+  Result inventory(arangodb::velocypack::Builder& builder);
+  
  private:
-
+  /// @brief fetch the server's inventory
+  Result fetchInventory(arangodb::velocypack::Builder& builder);
+  
   /// @brief set a progress message
   void setProgress(std::string const& msg) override;
 
@@ -161,10 +166,7 @@ class DatabaseInitialSyncer final : public InitialSyncer {
   Result handleCollection(arangodb::velocypack::Slice const&,
                           arangodb::velocypack::Slice const&, bool incremental,
                           sync_phase_e);
-
-  /// @brief fetch the server's inventory
-  Result fetchInventory(arangodb::velocypack::Builder& builder);
-
+  
   /// @brief handle the inventory response of the master
   Result handleLeaderCollections(arangodb::velocypack::Slice const&, bool);
 
