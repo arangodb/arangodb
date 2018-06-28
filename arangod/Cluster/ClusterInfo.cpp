c871621e159ebab1e8bcfc790be732f31fa1eab0
jsteemann <jan@arangodb.com>
2018-06-28 12:08:39 +0200

    don't hang in shutdown while creating a collection or an index

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -1660,6 +1660,10 @@ int ClusterInfo::createCollectionCoordinator(std::string const& databaseName,
         events::CreateCollection(name, TRI_ERROR_CLUSTER_TIMEOUT);
         return setErrormsg(TRI_ERROR_CLUSTER_TIMEOUT, errorMsg);
       }
+      
+      if (application_features::ApplicationServer::isStopping()) {
+        return setErrormsg(TRI_ERROR_SHUTTING_DOWN, errorMsg);
+      }
 
       agencyCallback->executeByCallbackOrTimeout(interval);
     }
@@ -2557,6 +2561,10 @@ int ClusterInfo::ensureIndexCoordinatorWithoutRollback(
 
       if (TRI_microtime() > endTime) {
         return setErrormsg(TRI_ERROR_CLUSTER_TIMEOUT, errorMsg);
+      }
+
+      if (application_features::ApplicationServer::isStopping()) {
+        return setErrormsg(TRI_ERROR_SHUTTING_DOWN, errorMsg);
       }
 
       agencyCallback->executeByCallbackOrTimeout(interval);
