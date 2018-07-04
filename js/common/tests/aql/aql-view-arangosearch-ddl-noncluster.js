6390642aadf9a5c31765276bca262969232be36e
Vadim Kondratyev <vadim@arangodb.com>
2018-07-04 19:54:15 +0300

    Fix conflict.

--- js/common/tests/aql/aql-view-arangosearch-ddl-noncluster.js
+++ js/common/tests/aql/aql-view-arangosearch-ddl-noncluster.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertUndefined, assertEqual, assertTrue, assertFalse*/
+/*global assertUndefined, assertEqual, assertNotEqual, assertTrue, assertFalse*/
 
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
