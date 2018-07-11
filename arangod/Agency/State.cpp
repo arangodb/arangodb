73cf95166cc5da3fed0d7191fd3881589303f544
Kaveh Vahedipour <kaveh@arangodb.com>
2018-07-11 11:28:18 +0200

    backport devel fix for lastConfirmed bug fix (#5743)

--- arangod/Agency/State.cpp
+++ arangod/Agency/State.cpp
@@ -61,6 +61,7 @@ using namespace arangodb::rest;
 State::State()
     : _agent(nullptr),
       _vocbase(nullptr),
+      _ready(false),
       _collectionsChecked(false),
       _collectionsLoaded(false),
       _nextCompactionAfter(0),
@@ -238,6 +239,11 @@ index_t State::logFollower(query_t const& transactions) {
   VPackSlice slices = transactions->slice();
   size_t nqs = slices.length();
 
+  while(!_ready && !_agent->isStopping()) {
+    LOG_TOPIC(DEBUG, Logger::AGENCY) << "Waiting for state to get ready ...";
+    std::this_thread::sleep_for(std::chrono::duration<double>(0.1));
+  }
+
   MUTEX_LOCKER(logLock, _logLock);
 
   // Check whether we have got a snapshot in the first position:
@@ -337,7 +343,14 @@ size_t State::removeConflicts(query_t const& transactions,  bool gotSnapshot) {
   LOG_TOPIC(TRACE, Logger::AGENCY) << "removeConflicts " << slices.toJson();
   try {
 
-    // _log is never empty, but for now, we leave this Vorsichtsmassnahme:
+    // If we've got a snapshot anything we might have is obsolete, note that
+    // this happens if and only if we decided at the call site that we actually
+    // use the snapshot and we have erased our _log there (see
+    // storeLogFromSnapshot which was called above)!
+    if (_log.empty()) {
+      TRI_ASSERT(gotSnapshot);
+      return 1;
+    }
     index_t lastIndex = _log.back().index;
 
     while (ndups < slices.length()) {
@@ -348,11 +361,11 @@ size_t State::removeConflicts(query_t const& transactions,  bool gotSnapshot) {
           << idx << " > " << lastIndex << " break.";
         break;
       }
-      term_t trm = slice.get("term").getUInt();
       if (idx < _cur) { // already compacted, treat as equal
         ++ndups;
         continue;
       }
+      term_t trm = slice.get("term").getUInt();
       size_t pos = idx - _cur;  // position in _log
       TRI_ASSERT(pos < _log.size());
       if (idx == _log.at(pos).index && trm != _log.at(pos).term) {
@@ -468,7 +481,7 @@ log_t State::atNoLock(index_t index) const {
     std::string excMessage = 
       std::string(
         "Access beyond the end of the log deque: (last, requested): (") +
-      std::to_string(_cur+_log.size()) + ", " + std::to_string(index);
+      std::to_string(_cur+_log.size()) + ", " + std::to_string(index) + ")";
     LOG_TOPIC(DEBUG, Logger::AGENCY) << excMessage;
     throw std::out_of_range(excMessage);
   }
@@ -485,6 +498,11 @@ int State::checkLog(index_t index, term_t term) const {
 
   MUTEX_LOCKER(mutexLocker, _logLock); // Cannot be read lock (Compaction)
 
+  // If index above highest entry
+  if (_log.size() > 0 && index > _log.back().index) {
+    return -1;
+  }
+  
   // Catch exceptions and avoid overflow:
   if (index < _cur || index - _cur > _log.size()) {
     return 0;
@@ -571,6 +589,7 @@ log_t State::lastLog() const {
   return _log.back();  
 }
   
+  
 /// Configure with agent
 bool State::configure(Agent* agent) {
   _agent = agent;
@@ -623,6 +642,11 @@ bool State::createCollection(std::string const& name) {
   return true;
 }
 
+
+// Are we ready for action?
+bool State::ready() const { return _ready; }
+
+
 /// Load collections
 bool State::loadCollections(TRI_vocbase_t* vocbase,
                             QueryRegistry* queryRegistry, bool waitForSync) {
@@ -646,6 +670,7 @@ bool State::loadCollections(TRI_vocbase_t* vocbase,
         log_t(index_t(0), term_t(0), buf, std::string()));
       persist(0, 0, value, std::string());
     }
+    _ready = true;
     return true;
   }
 
@@ -1291,6 +1316,13 @@ index_t State::lastIndex() const {
   MUTEX_LOCKER(mutexLocker, _logLock);
   TRI_ASSERT(!_log.empty());
   return _log.back().index;
+}
+
+// Index of last log entry
+index_t State::firstIndex() const {
+  MUTEX_LOCKER(mutexLocker, _logLock);
+  TRI_ASSERT(!_log.empty());
+  return _cur;
 }
 
 /// @brief this method is intended for manual recovery only. It only looks
