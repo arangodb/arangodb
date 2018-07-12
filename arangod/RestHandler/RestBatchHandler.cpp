6446ad8e749fd3458ce4f8696a2e1894a1f52c74
Wilfried Goesgens <willi@arangodb.com>
2018-07-12 16:55:06 +0200

    work on https://github.com/arangodb/arangodb/issues/5827 to achieve more RFC compliance when parsing http headers

--- arangod/RestHandler/RestBatchHandler.cpp
+++ arangod/RestHandler/RestBatchHandler.cpp
@@ -355,14 +355,20 @@ bool RestBatchHandler::getBoundaryHeader(std::string* result) {
     return false;
   }
 
-  std::string boundary = "--" + parts[1].substr(boundaryLength);
+  std::string boundary = parts[1].substr(boundaryLength);
 
-  if (boundary.size() < 5) {
+  if ((boundary[0]  == '"') && (boundary[boundary.length() -1] == '"')) {
+    StringUtils::trimInPlace(boundary, "\"");
+  } else if ((boundary[0] == '\'') && (boundary[boundary.length() -1] == '\'')) {
+    StringUtils::trimInPlace(boundary, "'");
+  }
+
+  if (boundary.size() < 3) {
     // 3 bytes is min length for boundary (without "--")
     return false;
   }
 
-  *result = boundary;
+  *result = "--" + boundary;
   return true;
 }
 
