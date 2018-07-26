e1e951b0ba0be90d9312cf4e65546615e06749ac
jsteemann <jan@arangodb.com>
2018-07-26 11:13:09 +0200

    fix failing test

--- UnitTests/HttpInterface/api-miscellaneous-spec.rb
+++ UnitTests/HttpInterface/api-miscellaneous-spec.rb
@@ -10,23 +10,24 @@ describe ArangoDB do
     it "checks attributes" do
       cmd = "/_admin/status"
       doc = ArangoDB.log_get("#{prefix}-status-attributes", cmd)
-        doc["server"].should eq("arango")
-        doc.should have_key("mode") # to be removed
-        doc.should have_key("operationMode")
-        doc["mode"].should eq(doc["operationMode"])
-        doc.should have_key("version")
-        doc.should have_key("pid")
-        doc.should have_key("license")
-        doc.should have_key("host")
-        doc.should have_key("hostname")
+      doc["server"].should eq("arango")
+      doc.should have_key("mode") # to be removed
+      doc.should have_key("operationMode")
+      doc["mode"].should eq(doc["operationMode"])
+      doc.should have_key("version")
+      doc.should have_key("pid")
+      doc.should have_key("license")
+      doc.should have_key("host")
+      # "hostname" is optional in the response
+      # doc.should have_key("hostname")
 
 
-        info = doc["serverInfo"]
-        info.should have_key("maintenance")
-        info.should have_key("role")
-        info.should have_key("writeOpsEnabled") # to be removed
-        info.should have_key("readOnly")
-        info["writeOpsEnabled"].should eq(!info["readOnly"])
+      info = doc["serverInfo"]
+      info.should have_key("maintenance")
+      info.should have_key("role")
+      info.should have_key("writeOpsEnabled") # to be removed
+      info.should have_key("readOnly")
+      info["writeOpsEnabled"].should eq(!info["readOnly"])
     end
   end
 end
