b8650a6b970831adcd2218680b48c24031341f9c
Kaveh Vahedipour <kaveh@arangodb.com>
2018-08-01 10:15:45 +0200

    missing files

---
+++ arangod/Cluster/DropIndex.cpp
@@ -0,0 +1,116 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+/// @author Matthew Von-Maszewski
+////////////////////////////////////////////////////////////////////////////////
+
+#include "DropIndex.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "Basics/VelocyPackHelper.h"
+#include "Cluster/ClusterFeature.h"
+#include "VocBase/Methods/Collections.h"
+#include "VocBase/Methods/Indexes.h"
+#include "VocBase/Methods/Databases.h"
+
+using namespace arangodb::application_features;
+using namespace arangodb::maintenance;
+using namespace arangodb::methods;
+
+DropIndex::DropIndex(
+  MaintenanceFeature& feature, ActionDescription const& d) :
+  ActionBase(feature, d) {
+
+  if (!d.has(COLLECTION)) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "DropIndex: collection must be specified";
+    _state = FAILED;
+  }
+
+  if (!d.has(DATABASE)) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "DropIndex: database must be specified";
+    _state = FAILED;
+  }
+  
+  if (!d.has(INDEX)) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "DropIndex: index id must be stecified";
+    _state = FAILED;
+  }
+  
+  TRI_ASSERT(d.has(INDEX));
+}
+
+DropIndex::~DropIndex() {};
+
+bool DropIndex::first() {
+  
+  auto const& database = _description.get(DATABASE);
+  auto const& collection = _description.get(COLLECTION);
+  auto const& id = _description.get(INDEX);
+
+  VPackBuilder index;
+  index.add(VPackValue(_description.get(INDEX)));
+
+  auto vocbase = Databases::lookup(database);
+  if (vocbase == nullptr) {
+    std::string errorMsg("DropIndex: Failed to lookup database ");
+    errorMsg += database;
+    _result.reset(TRI_ERROR_ARANGO_DATABASE_NOT_FOUND, errorMsg);
+    return false;
+  }
+
+  auto col = vocbase->lookupCollection(collection);
+  if (col == nullptr) {
+    std::string errorMsg("EnsureIndex: Failed to lookup local collection ");
+    errorMsg += collection + " in database " + database;
+    _result.reset(TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, errorMsg);
+    setState(FAILED);
+    return false;
+  }
+
+  Result found = methods::Collections::lookup(
+    vocbase, collection, [&](LogicalCollection& coll) {
+      LOG_TOPIC(DEBUG, Logger::MAINTENANCE)
+        << "Dropping local index " + collection + "/" + id;
+      _result = Indexes::drop(&coll, index.slice());
+    });
+
+  if (found.fail()) {
+    std::string errorMsg("DropIndex: Failed to lookup local collection ");
+    errorMsg += collection + "in database " + database;
+    _result.reset(TRI_ERROR_ARANGO_DATABASE_NOT_FOUND, errorMsg);
+  }
+  
+  return false;
+  
+}
+
+arangodb::Result DropIndex::kill(Signal const& signal) {
+  return actionError(
+    TRI_ERROR_ACTION_OPERATION_UNABORTABLE, "Cannot kill DropIndex action");
+}
+
+arangodb::Result DropIndex::progress(double& progress) {
+  progress = 0.5;
+  return arangodb::Result(TRI_ERROR_NO_ERROR);
+}
---
+++ arangod/Cluster/ResignShardLeadership.cpp
@@ -0,0 +1,144 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2018 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+/// @author Matthew Von-Maszewski
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ResignShardLeadership.h"
+#include "MaintenanceFeature.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "Basics/VelocyPackHelper.h"
+#include "Cluster/ClusterFeature.h"
+#include "Cluster/FollowerInfo.h"
+#include "Transaction/StandaloneContext.h"
+#include "Transaction/Methods.h"
+#include "VocBase/LogicalCollection.h"
+#include "VocBase/Methods/Collections.h"
+#include "VocBase/Methods/Databases.h"
+
+#include <velocypack/Compare.h>
+#include <velocypack/Iterator.h>
+#include <velocypack/Slice.h>
+#include <velocypack/velocypack-aliases.h>
+
+
+using namespace arangodb::application_features;
+using namespace arangodb::maintenance;
+using namespace arangodb::methods;
+
+ResignShardLeadership::ResignShardLeadership(
+  MaintenanceFeature& feature, ActionDescription const& desc)
+  : ActionBase(feature, desc) {
+
+  if (!desc.has(DATABASE)) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "ResignShardLeadership: database must be specified";
+    _state = FAILED;
+  }
+  
+  if (!desc.has(SHARD)) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "ResignShardLeadership: shard must be stecified";
+    _state = FAILED;
+  }
+  
+  TRI_ASSERT(desc.has(INDEX));
+}
+
+ResignShardLeadership::~ResignShardLeadership() {};
+
+
+
+bool ResignShardLeadership::first() {
+
+  auto const& database = _description.get(DATABASE);
+  auto const& collection = _description.get(SHARD);
+
+  LOG_TOPIC(DEBUG, Logger::MAINTENANCE)
+    << "trying to withdraw as leader of shard '%s/%s'";
+  
+  auto vocbase = Databases::lookup(database);
+  if (vocbase == nullptr) {
+    std::string errorMsg("ResignShardLeadership: Failed to lookup database ");
+    errorMsg += database;
+    _result.reset(TRI_ERROR_ARANGO_DATABASE_NOT_FOUND, errorMsg);
+    setState(FAILED);
+    return false;
+  }
+
+  auto col = vocbase->lookupCollection(collection);
+  if (col == nullptr) {
+    std::string errorMsg("EnsureIndex: Failed to lookup local collection ");
+    errorMsg += collection + " in database " + database;
+    _result.reset(TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, errorMsg);
+    setState(FAILED);
+    return false;
+  }
+
+  // This starts a write transaction, just to wait for any ongoing
+  // write transaction on this shard to terminate. We will then later
+  // report to Current about this resignation. If a new write operation
+  // starts in the meantime (which is unlikely, since no coordinator that
+  // has seen the _ will start a new one), it is doomed, and we ignore the
+  // problem, since similar problems can arise in failover scenarios anyway.
+
+  try {
+    // we know the shard exists locally!
+
+    col->followers()->setTheLeader("LEADER_NOT_YET_KNOWN");  // resign
+    // Note that it is likely that we will be a follower for this shard
+    // with another leader in due course. However, we do not know the
+    // name of the new leader yet. This setting will make us a follower
+    // for now but we will not accept any replication operation from any
+    // leader, until we have negotiated a deal with it. Then the actual
+    // name of the leader will be set.
+
+    // Get write transaction on collection
+    auto ctx = std::make_shared<transaction::StandaloneContext>(*vocbase);
+    transaction::Methods trx(ctx, {}, {collection}, {}, transaction::Options());
+    
+    Result res = trx.begin();
+    
+    if (!res.ok()) {
+      THROW_ARANGO_EXCEPTION(res);
+     }
+  } catch (std::exception const& e) {
+    LOG_TOPIC(ERR, Logger::MAINTENANCE)
+      << "exception thrown when resigning:" << e.what();
+  }
+
+  setState(COMPLETE);
+  return false;
+  
+}
+
+arangodb::Result ResignShardLeadership::kill(Signal const& signal) {
+  return actionError(
+    TRI_ERROR_ACTION_OPERATION_UNABORTABLE, "Cannot kill ResignShardLeadership action");
+}
+
+arangodb::Result ResignShardLeadership::progress(double& progress) {
+  progress = 0.5;
+  return arangodb::Result(TRI_ERROR_NO_ERROR);
+}
+
+
