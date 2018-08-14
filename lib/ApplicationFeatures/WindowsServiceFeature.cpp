ad40130e8e87b7dd56df741a87366d5a23efa58d
Vasiliy <vasiliy@arangodb.com>
2018-08-14 18:20:09 +0300

    address MSVC build failure

--- lib/ApplicationFeatures/WindowsServiceFeature.cpp
+++ lib/ApplicationFeatures/WindowsServiceFeature.cpp
@@ -497,7 +497,7 @@ void WINAPI ServiceCtrl(DWORD dwCtrlCode) {
 
 WindowsServiceFeature::WindowsServiceFeature(application_features::ApplicationServer& server)
   : ApplicationFeature(server, "WindowsService"),
-    _server(server){
+    _server(&server) {
   _progress = 2;
   setOptional(true);
   requiresElevatedPrivileges(true);
