54e15b58f5b50ac6562520cdb0b055242c70e1b8
Kaveh Vahedipour <kaveh@arangodb.com>
2018-08-17 16:54:10 +0200

    2 500000us waits in 1 1s wait

--- arangod/Cluster/HeartbeatThread.cpp
+++ arangod/Cluster/HeartbeatThread.cpp
@@ -1006,8 +1006,7 @@ void HeartbeatThread::dispatchedJobResult(DBServerAgencySyncResult result) {
   if (doSleep) {
     // Sleep a little longer, since this might be due to some synchronization
     // of shards going on in the background
-    std::this_thread::sleep_for(std::chrono::microseconds(500000));
-
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   CONDITION_LOCKER(guard, _condition);
   _wasNotified = true;
