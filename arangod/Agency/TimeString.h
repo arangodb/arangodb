94730f5c44e5ae33ae9e822f16aad03d04042a48
Max Neunhoeffer <max@arangodb.com>
2018-08-20 23:02:45 +0200

    Add missing copyright notice.

--- arangod/Agency/TimeString.h
+++ arangod/Agency/TimeString.h
@@ -1,3 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2018 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_CONSENSUS_TIMESTRING_H
+#define ARANGOD_CONSENSUS_TIMESTRING_H 1
+
 #include <chrono>
 
 inline std::string timepointToString(std::chrono::system_clock::time_point const& t) {
@@ -32,3 +57,5 @@ inline std::string timepointToString(std::chrono::system_clock::duration const& 
   }
   return std::chrono::time_point<std::chrono::system_clock>();
 }
+
+#endif
