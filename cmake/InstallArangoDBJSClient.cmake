53978b6b93b21f84f4c0a7927698aab04b9da9c9
Frank Celler <392005+fceller@users.noreply.github.com>
2018-08-27 23:25:21 +0200

    Feature/semantic versioning (#6265)

--- cmake/InstallArangoDBJSClient.cmake
+++ cmake/InstallArangoDBJSClient.cmake
@@ -7,7 +7,7 @@ install(
     ${ARANGODB_SOURCE_DIR}/js/common
     ${ARANGODB_SOURCE_DIR}/js/client 
   DESTINATION
-    ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/js
+    ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/${ARANGODB_JS_VERSION}
   FILES_MATCHING
     PATTERN "*.js"
   REGEX
@@ -23,7 +23,7 @@ if (USE_ENTERPRISE)
     DIRECTORY
       ${ARANGODB_SOURCE_DIR}/enterprise/js/common
       ${ARANGODB_SOURCE_DIR}/enterprise/js/client 
-    DESTINATION    ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/js
+    DESTINATION    ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/${ARANGODB_JS_VERSION}
     FILES_MATCHING PATTERN "*.js"
     REGEX          "^.*/common/test-data$"                 EXCLUDE
     REGEX          "^.*/common/tests$"                     EXCLUDE
@@ -34,7 +34,7 @@ endif ()
 # For the node modules we need all files:
 install(
   DIRECTORY ${ARANGODB_SOURCE_DIR}/js/node
-  DESTINATION ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/js
+  DESTINATION ${CMAKE_INSTALL_DATAROOTDIR_ARANGO}/${ARANGODB_JS_VERSION}
   REGEX "^.*/eslint"                                       EXCLUDE
   REGEX "^.*/.npmignore"                                   EXCLUDE
   REGEX "^.*/.bin"                                         EXCLUDE
