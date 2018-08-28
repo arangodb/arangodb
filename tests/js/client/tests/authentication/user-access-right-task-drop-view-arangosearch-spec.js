a23f9db3586514f8b9ec67f55253bf9ab659a8ae
Vadim Kondratyev <vadim@arangodb.com>
2018-08-28 18:03:54 +0200

    Fix typo

--- tests/js/client/tests/authentication/user-access-right-task-drop-view-arangosearch-spec.js
+++ tests/js/client/tests/authentication/user-access-right-task-drop-view-arangosearch-spec.js
@@ -44,7 +44,7 @@ const rightLevels = helper.rightLevels;
 const testViewName = `${namePrefix}ViewNew`;
 const testViewType = "arangosearch";
 const testCol1Name = `${namePrefix}Col1New`;
-const testCol2Name = `${namePrefix}Col1New`;
+const testCol2Name = `${namePrefix}Col2New`;
 const keySpaceId = 'task_drop_view_keyspace';
 
 const userSet = helper.userSet;
@@ -180,18 +180,18 @@ function hasIResearch (db) {
             };
 
             const rootGrantCollection = (colName, user, explicitRight = '') => {
-                if (rootTestCollection(colName, false)) {
-                  if (explicitRight !== '' && rightLevels.includes(explicitRight))
-                  {
-                    if (helper.isLdapEnabledExternal()) {
-                      users.grantCollection(':role:' + user, dbName, colName, explicitRight);
-                    } else {
-                      users.grantCollection(user, dbName, colName, explicitRight);
-                    }
+              if (rootTestCollection(colName, false)) {
+                if (explicitRight !== '' && rightLevels.includes(explicitRight))
+                {
+                  if (helper.isLdapEnabledExternal()) {
+                    users.grantCollection(':role:' + user, dbName, colName, explicitRight);
+                  } else {
+                    users.grantCollection(user, dbName, colName, explicitRight);
                   }
                 }
-                helper.switchUser(user, dbName);
-              };
+              }
+              helper.switchUser(user, dbName);
+            };
 
             const rootTestView = (viewName = testViewName, switchBack = true) => {
               helper.switchUser('root', dbName);
@@ -200,7 +200,7 @@ function hasIResearch (db) {
               if (switchBack) {
                   helper.switchUser(name, dbName);
               }
-              return view != null;
+              return view !== null;
             };
 
             const rootCreateView = (viewName = testViewName, properties = null) => {
@@ -249,7 +249,7 @@ function hasIResearch (db) {
                   command: `(function (params) {
                     try {
                       const db = require('@arangodb').db;
-                      db._view('${testViewName}').drop();
+                      db._dropView('${testViewName}');
                       global.KEY_SET('${keySpaceId}', '${name}_status', true);
                     } catch (e) {
                       global.KEY_SET('${keySpaceId}', '${name}_status', false);
@@ -405,7 +405,7 @@ function hasIResearch (db) {
                   } else {
                     tasks.register(task);
                     wait(keySpaceId, name);
-                    expect(getKey(keySpaceId, `${name}_status`)).to.equal(false, `${name} could update the view with insufficient rights`);
+                    expect(getKey(keySpaceId, `${name}_status`)).to.equal(false, `${name} could drop the view with insufficient rights`);
                     expect(rootTestView(testViewName)).to.equal(true, `${name} was able to drop a view with insufficent rights`);
                   }
                 } else {
@@ -416,7 +416,7 @@ function hasIResearch (db) {
                     checkError(e);
                     return;
                   } finally {
-                    expect(getKey(keySpaceId, `${name}_status`)).to.equal(false, `${name} could update the view with insufficient rights`);
+                    expect(getKey(keySpaceId, `${name}_status`)).to.equal(false, `${name} could drop the view with insufficient rights`);
                   }
                   expect(false).to.equal(true, `${name} managed to register a task with insufficient rights`);
                 }
