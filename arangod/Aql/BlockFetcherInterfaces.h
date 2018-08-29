be0078d3b49ae3d1fbfffba0e9bb7954960da75a
Michael Hackstein <michael@arangodb.com>
2018-08-29 23:54:34 +0200

    Added some Doxygen comments to the aql-by-line function stubs.

--- arangod/Aql/BlockFetcherInterfaces.h
+++ arangod/Aql/BlockFetcherInterfaces.h
@@ -33,10 +33,34 @@ namespace aql {
 
 class AqlItemRow;
 
+/**
+ * @brief Interface for all AqlExecutors that do only need one
+ *        row at a time in order to make progress.
+ *        The guarantee is the following:
+ *        If fetchRow returns a row the pointer to
+ *        this row stays valid until the next call
+ *        of fetchRow.
+ */
 class SingleRowFetcher {
  public:
   SingleRowFetcher() = default;
   virtual ~SingleRowFetcher() = default;
+
+  /**
+   * @brief Fetch one new AqlItemRow from upstream.
+   *        **Guarantee**: the pointer returned is valid only
+   *        until the next call to fetchRow.
+   *
+   * @return A pair with the following properties:
+   *         ExecutionState:
+   *           WAITING => IO going on, immediatly return to caller.
+   *           DONE => No more to expect from Upstream, if you are done with this row return DONE to caller.
+   *           HASMORE => There is potentially more from above, call again if you need more input.
+   *         AqlItemRow:
+   *           If WAITING => Do not use this Row, it is a nullptr.
+   *           If HASMORE => The Row is guaranteed to not be a nullptr.
+   *           If DONE => Row can be a nullptr (nothing received) or valid.
+   */
   virtual std::pair<ExecutionState, AqlItemRow const*> fetchRow() = 0;
 };
 
--- arangod/Aql/ExecutionBlockImpl.h
+++ arangod/Aql/ExecutionBlockImpl.h
@@ -38,26 +38,126 @@ class AqlItemRow;
 class ExecutionNode;
 class ExecutionEngine;
 
+/**
+ * @brief This is the implementation class of AqlExecutionBlocks.
+ *        It is responsible to create AqlItemRows for subsequent
+ *        Blocks, also it will fetch new AqlItemRows from preceeding
+ *        Blocks whenever it is necessary.
+ *        For performance reasons this will all be done in batches
+ *        of 1000 Rows each.
+ *
+ * @tparam Executor A class that needs to have the following signature:
+ *         Executor {
+ *           Executor(xxxFetcher&)
+ *           std::pair<ExecutionState, std::unique_ptr<AqlItemRow>> produceRow(); 
+ *         }
+ *         The Executor is the implementation of one AQLNode.
+ *         It is only allowed to produce one outputRow at a time, but can fetch
+ *         as many rows from above as it likes. It can only follow the
+ *         xxxFetcher interface to get AqlItemRows from Upstream.
+ */
 template<class Executor>
 class ExecutionBlockImpl : public ExecutionBlock, public SingleRowFetcher {
  public:
 
+   /**
+    * @brief Construct a new ExecutionBlock
+    *        This API is subject to change, we want to make it as independent of
+    *        AQL / Query interna as possible.
+    *
+    * @param engine The AqlExecutionEngine holding the query and everything required
+    *               for the execution.
+    * @param node The Node used to create this ExecutionBlock
+    */
   ExecutionBlockImpl(ExecutionEngine* engine, ExecutionNode const* node);
   ~ExecutionBlockImpl();
 
+  /**
+   * @brief Produce atMost many output rows, or less.
+   *        May return waiting if I/O has to be performed
+   *        so we can release this thread.
+   *        Is required to return DONE if it is guaranteed
+   *        that this block does not produce more rows,
+   *        Returns HASMORE if the DONE guarantee cannot be given.
+   *        HASMORE does not give strict guarantee, it maybe that
+   *        HASMORE is returned but no more rows can be produced.
+   *
+   * @param atMost Upper bound of AqlItemRows to be returned.
+   *               Target is to get as close to this upper bound
+   *               as possible.
+   *
+   * @return A pair with the following properties:
+   *         ExecutionState:
+   *           WAITING => IO going on, immediatly return to caller.
+   *           DONE => No more to expect from Upstream, if you are done with this row return DONE to caller.
+   *           HASMORE => There is potentially more from above, call again if you need more input.
+   *         AqlItemBlock:
+   *           A matrix of result rows.
+   *           Guaranteed to be non nullptr in HASMORE cas, maybe a nullptr in DONE.
+   *           Is a nullptr in WAITING
+   */
   std::pair<ExecutionState, std::unique_ptr<AqlItemBlock>> getSome(
       size_t atMost) override;
 
+  /**
+   * @brief Like get some, but lines are skipped and not returned.
+   *        This can use optimizations to not actually create the data.
+   *
+   * @param atMost Upper bound of AqlItemRows to be skipped.
+   *               Target is to get as close to this upper bound
+   *               as possible.
+   *
+   * @return A pair with the following properties:
+   *         ExecutionState:
+   *           WAITING => IO going on, immediatly return to caller.
+   *           DONE => No more to expect from Upstream, if you are done with this row return DONE to caller.
+   *           HASMORE => There is potentially more from above, call again if you need more input.
+   *         size_t:
+   *           Number of rows effectively skipped.
+   *           On WAITING this is always 0.
+   *           On any other state this is between 0 and atMost.
+   */
   std::pair<ExecutionState, size_t> skipSome(size_t atMost) override;
 
+  /**
+   * @brief Fetch one new AqlItemRow from upstream.
+   *        **Guarantee**: the pointer returned is valid only
+   *        until the next call to fetchRow.
+   *
+   * @return A pair with the following properties:
+   *         ExecutionState:
+   *           WAITING => IO going on, immediatly return to caller.
+   *           DONE => No more to expect from Upstream, if you are done with this row return DONE to caller.
+   *           HASMORE => There is potentially more from above, call again if you need more input.
+   *         AqlItemRow:
+   *           If WAITING => Do not use this Row, it is a nullptr.
+   *           If HASMORE => The Row is guaranteed to not be a nullptr.
+   *           If DONE => Row can be a nullptr (nothing received) or valid.
+   */
   std::pair<ExecutionState, AqlItemRow const*> fetchRow() override;
 
  private:
 
+  /**
+   * @brief Internal helper function that fetches the next block
+   *        of AqlItemRows from upstream.
+   *        Will be called by the public fetchXXX() functions.
+   *        **Guarantee:** Fetches a batch of 1000 rows from
+   *        upstream, if successful this batch (AqlItemBlock)
+   *        is retained by this class only until the next call
+   *        to fetchBlock.
+   *
+   * @return The upstream state, can be DONE, WAITING or HASMORE.
+   */
   ExecutionState fetchBlock();
 
  private:
 
+  /**
+   * @brief This is the working party of this implementation
+   *        the template class needs to implement the logic
+   *        to produce a single row from the upstream information.
+   */
   Executor& _executor;
 };
 
