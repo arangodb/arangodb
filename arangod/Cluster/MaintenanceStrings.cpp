67e0ddca1b314d47df2f72979a30be2a83d611db
Kaveh Vahedipour <kaveh@arangodb.com>
2018-09-03 16:16:25 +0200

    more strings unified

---
+++ arangod/Cluster/MaintenanceStrings.cpp
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2018 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Kaveh Vahedipour
+////////////////////////////////////////////////////////////////////////////////
+
+#include "MaintenanceStrings.h"
+
+std::string const arangodb::maintenance::COLLECTION = "collection";
+std::string const arangodb::maintenance::DATABASE = "database";
+std::string const arangodb::maintenance::DB = "/_db/";
+std::string const arangodb::maintenance::EDGE = "edge";
+std::string const arangodb::maintenance::FIELDS = "fields";
+std::string const arangodb::maintenance::FOLLOWER_ID = "followerId";
+std::string const arangodb::maintenance::GLOB_UID = "globallyUniqueId";
+std::string const arangodb::maintenance::ID = "id";
+std::string const arangodb::maintenance::INDEX = "index";
+std::string const arangodb::maintenance::INDEXES = "indexes";
+std::string const arangodb::maintenance::KEY = "key";
+std::string const arangodb::maintenance::LOCAL_LEADER = "localLeader";
+std::string const arangodb::maintenance::NAME = "name";
+std::string const arangodb::maintenance::OBJECT_ID = "objectId";
+std::string const arangodb::maintenance::OP = "op";
+std::string const arangodb::maintenance::PRIMARY = "primary";
+std::string const arangodb::maintenance::SELECTIVITY_ESTIMATE = "selectivityEstimate";
+std::string const arangodb::maintenance::SERVER_ID = "serverId";
+std::string const arangodb::maintenance::SERVERS = "servers";
+std::string const arangodb::maintenance::SHARD = "shard";
+std::string const arangodb::maintenance::SHARDS = "shards";
+std::string const arangodb::maintenance::THE_LEADER = "theLeader";
+std::string const arangodb::maintenance::UNDERSCORE = "_";
