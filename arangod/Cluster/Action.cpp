263a47da5ed43648bb2eecaf939cffd0da3f9ff5
Kaveh Vahedipour <kaveh@vahedipour.de>
2018-09-08 23:19:00 +0200

    this works with osx / windows

--- arangod/Cluster/Action.cpp
+++ arangod/Cluster/Action.cpp
@@ -41,30 +41,6 @@
 using namespace arangodb;
 using namespace arangodb::maintenance;
 
-std::unordered_map<
-  std::string,
-  std::function<std::unique_ptr<ActionBase>(
-                  MaintenanceFeature&,ActionDescription const&)>> factories {
-  {CREATE_COLLECTION, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new CreateCollection(f,a)); }},
-  {CREATE_DATABASE, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new CreateDatabase(f,a)); }},
-  {DROP_COLLECTION, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new DropCollection(f,a)); }},
-  {DROP_DATABASE, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new DropDatabase(f,a)); }},
-  {DROP_INDEX, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new DropIndex(f,a)); }},    
-  {ENSURE_INDEX, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new EnsureIndex(f,a)); }},
-  {RESIGN_SHARD_LEADERSHIP, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new ResignShardLeadership(f,a)); }},
-  {SYNCHRONIZE_SHARD, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new SynchronizeShard(f,a)); }},    
-  {UPDATE_COLLECTION, [](MaintenanceFeature& f,ActionDescription const& a) {
-      return std::unique_ptr<ActionBase>(new UpdateCollection(f,a)); }},
-};
-
 Action::Action(
   MaintenanceFeature& feature,
   ActionDescription const& description) : _action(nullptr) {
@@ -95,10 +71,56 @@ Action::~Action() {}
 void Action::create(
   MaintenanceFeature& feature, ActionDescription const& description) {
 
+  using factories_t = std::unordered_map<
+    std::string, std::function<std::unique_ptr<ActionBase>(
+    MaintenanceFeature&,ActionDescription const&)>>;
+
+  static factories_t const factories = factories_t {
+
+    {CREATE_COLLECTION,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new CreateCollection(f,a)); }},
+
+    {CREATE_DATABASE,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new CreateDatabase(f,a)); }},
+
+    {DROP_COLLECTION,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new DropCollection(f,a)); }},
+
+    {DROP_DATABASE,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new DropDatabase(f,a)); }},
+
+    {DROP_INDEX,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new DropIndex(f,a)); }},    
+
+    {ENSURE_INDEX,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new EnsureIndex(f,a)); }},
+
+    {RESIGN_SHARD_LEADERSHIP,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new ResignShardLeadership(f,a)); }},
+
+    {SYNCHRONIZE_SHARD,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new SynchronizeShard(f,a)); }},    
+
+    {UPDATE_COLLECTION,
+     [](MaintenanceFeature& f,ActionDescription const& a) {
+        return std::unique_ptr<ActionBase>(new UpdateCollection(f,a)); }},
+  
+  };
+
   auto factory = factories.find(description.name());
+
   _action = (factory != factories.end()) ?
     factory->second(feature, description) :
     std::unique_ptr<ActionBase>(new NonAction(feature, description));
+    
   
 }
 
