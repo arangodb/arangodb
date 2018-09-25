f271cc22a78fae6f11d31248aaeccb9d56b4e1d3
Jan <jsteemann@users.noreply.github.com>
2018-09-25 09:38:08 +0200

    next attempt... (#6596)

--- arangod/Replication/DatabaseInitialSyncer.cpp
+++ arangod/Replication/DatabaseInitialSyncer.cpp
@@ -1269,8 +1269,8 @@ Result DatabaseInitialSyncer::handleCollection(VPackSlice const& parameters,
     std::string const& masterColl =
         !masterUuid.empty() ? masterUuid : itoa(masterCid);
     auto res = incremental && getSize(*col) > 0
-             ? fetchCollectionSync(col, masterColl, _config.master.lastLogTick)
-             : fetchCollectionDump(col, masterColl, _config.master.lastLogTick)
+             ? fetchCollectionSync(col, masterColl, _config.master.lastUncommittedLogTick)
+             : fetchCollectionDump(col, masterColl, _config.master.lastUncommittedLogTick)
              ;
 
     if (!res.ok()) {
