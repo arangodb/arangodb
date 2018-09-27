fae8a7dd8deb27d8158e37e24bd8a43596c27493
Wilfried Goesgens <willi@arangodb.com>
2018-09-27 14:34:01 +0200

    utf8 is actualy 65001 to windows

--- arangosh/Shell/ClientFeature.cpp
+++ arangosh/Shell/ClientFeature.cpp
@@ -65,7 +65,7 @@ ClientFeature::ClientFeature(
       _warn(false),
       _warnConnect(true),
       _haveServerPassword(false),
-      _codePage(65002) { // default to UTF8
+      _codePage(65001) { // default to UTF8
   setOptional(true);
   requiresElevatedPrivileges(false);
   startsAfter("GreetingsPhase");
