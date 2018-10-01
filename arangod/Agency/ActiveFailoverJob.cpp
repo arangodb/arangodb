2a90b3fa6cbd6aefd630cd9c5f75ba14d250f7f0
jsteemann <jan@arangodb.com>
2018-10-02 01:20:33 +0200

    fix compile issues

--- arangod/Agency/ActiveFailoverJob.cpp
+++ arangod/Agency/ActiveFailoverJob.cpp
@@ -274,7 +274,7 @@ std::string ActiveFailoverJob::findBestFollower() {
       if (!resp.isArray() || resp.length() == 0) {
         return "";
       }
-      VPackSlice obj = resp.at(0).get({ Job::agencyPrefix, "AsyncReplication"});
+      VPackSlice obj = resp.at(0).get<std::string>({ Job::agencyPrefix, std::string("AsyncReplication") });
       for (VPackObjectIterator::ObjectPair pair : VPackObjectIterator(obj)) {
         std::string srvUUID = pair.key.copyString();
         bool isAvailable = std::find(healthy.begin(), healthy.end(), srvUUID) != healthy.end();
