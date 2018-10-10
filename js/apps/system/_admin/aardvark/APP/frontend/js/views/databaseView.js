d1ad714330abccddbd3a9b18c1e59eb3f37437f4
hkernbach <heiko@arangodb.com>
2018-10-11 00:52:06 +0200

    fixed a caching bug in IE11

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/databaseView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/databaseView.js
@@ -80,6 +80,7 @@
           self.currentDB = db;
 
           self.collection.fetch({
+            cache: false,
             success: function () {
               // sorting
               self.collection.sort();
