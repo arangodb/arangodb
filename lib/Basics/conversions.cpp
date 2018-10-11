9e60c34455ec49d510d7694c7d157e570b7b36f9
jsteemann <jan@arangodb.com>
2018-10-11 21:10:06 +0200

    remove invalid assert

--- lib/Basics/conversions.cpp
+++ lib/Basics/conversions.cpp
@@ -58,11 +58,9 @@ double TRI_DoubleString(char const* str) {
 
   if (*endptr != '\0') {
     TRI_set_errno(TRI_ERROR_ILLEGAL_NUMBER);
-    TRI_ASSERT(false);
   } else if (errno == ERANGE &&
              (result == HUGE_VAL || result == -HUGE_VAL || result == 0)) {
     TRI_set_errno(TRI_ERROR_NUMERIC_OVERFLOW);
-    TRI_ASSERT(false);
   }
 
   return result;
