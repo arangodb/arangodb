d524ba616b6b3e73562420fbcc416daa02539ae5
Kaveh Vahedipour <kaveh@arangodb.com>
2018-10-12 17:03:08 +0200

    fixed hyperventing agent (#6776)
    * fixed hyperventing agent

--- arangod/Agency/Agent.cpp
+++ arangod/Agency/Agent.cpp
@@ -1774,17 +1774,19 @@ query_t Agent::gossip(query_t const& in, bool isCallback, size_t version) {
   }
     
   std::string err;
+  config_t::upsert_t u = config_t::UNCHANGED;
     
   /// Pool incomplete or the other guy is in my pool: I'll gossip.
   if (!_config.poolComplete() || _config.matchPeer(id, endpoint)) {
-      
-    if (!_config.upsertPool(pslice, id)) {
+
+    config_t::upsert_t upsert = _config.upsertPool(pslice, id);
+    if (u == config_t::WRONG) {
       LOG_TOPIC(FATAL, Logger::AGENCY) << "Discrepancy in agent pool!";
       FATAL_ERROR_EXIT();      /// disagreement over pool membership are fatal!
     }
-    auto pool = _config.pool();
       
     // Wrapped in envelope in RestAgencyPrivHandler
+    auto pool = _config.pool();
     out->add(VPackValue("pool"));
     { VPackObjectBuilder bb(out.get());
       for (auto const& i : pool) {
@@ -1859,7 +1861,7 @@ query_t Agent::gossip(query_t const& in, bool isCallback, size_t version) {
   }
     
   // let gossip loop know that it has new data
-  if ( _inception != nullptr && isCallback) {
+  if (_inception != nullptr && upsert == config_t::CHANGED) {
     _inception->signalConditionVar();
   }
     
