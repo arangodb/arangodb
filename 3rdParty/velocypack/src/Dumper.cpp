54fc927ea2058995a15b479ea29c24b4e08004c7
Simon Grätzer <simon@graetzer.org>
2018-10-16 16:51:22 +0200

    revert to old iteration order

--- 3rdParty/velocypack/src/Dumper.cpp
+++ 3rdParty/velocypack/src/Dumper.cpp
@@ -376,7 +376,7 @@ void Dumper::dumpValue(Slice const* slice, Slice const* base) {
     }
 
     case ValueType::Object: {
-      ObjectIterator it(*slice, true);
+      ObjectIterator it(*slice);
       _sink->push_back('{');
       if (options->prettyPrint) {
         _sink->push_back('\n');
