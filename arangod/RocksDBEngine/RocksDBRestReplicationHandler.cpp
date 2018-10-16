947baf9688ee6d2c46569ad720406397ac00c585
Simon <simon@graetzer.org>
2018-10-16 13:57:06 +0200

    Fix an intermediate commit error (#6909)

--- arangod/RocksDBEngine/RocksDBRestReplicationHandler.cpp
+++ arangod/RocksDBEngine/RocksDBRestReplicationHandler.cpp
@@ -186,10 +186,8 @@ void RocksDBRestReplicationHandler::handleCommandBarrier() {
   if (type == rest::RequestType::POST) {
     VPackBuilder b;
     b.add(VPackValue(VPackValueType::Object));
-    // always return a non-0 barrier id
-    // it will be ignored by the client anyway for the RocksDB engine
-    std::string const idString = std::to_string(TRI_NewTickServer());
-    b.add("id", VPackValue(idString));
+    // always return a 0 barrier ID so clients will ignore it
+    b.add("id", VPackValue("0"));
     b.close();
     generateResult(rest::ResponseCode::OK, b.slice());
   } else if (type == rest::RequestType::PUT ||
