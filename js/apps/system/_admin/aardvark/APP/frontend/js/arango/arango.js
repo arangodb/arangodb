5d20a520fd02b6f769a52e5f256f4144f588d532
hkernbach <heiko@arangodb.com>
2018-10-19 13:32:34 +0200

    fixes validation of allowed or not allowed foxx service mount paths

--- js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
@@ -1234,12 +1234,12 @@
           validateInput: function () {
             return [
               {
-                rule: Joi.string().regex(/^((APP[^/]+|(?!APP)[a-zA-Z0-9_\-%]+))+$/i),
+                rule: Joi.string().regex(/^(\/(APP[^\/]+|(?!APP)[a-zA-Z0-9_\-\/]+))+$/i),
                 msg: 'May not contain /APP'
               },
               {
-                rule: Joi.string().regex(/^([a-zA-Z0-9_\-%]+)+$/),
-                msg: 'Can only contain [a-zA-Z0-9_-%]'
+                rule: Joi.string().regex(/^([a-zA-Z0-9_\-\/]+)+$/),
+                msg: 'Can only contain [a-zA-Z0-9_-/]'
               },
               {
                 rule: Joi.string().regex(/([^_]|_open\/)/),
