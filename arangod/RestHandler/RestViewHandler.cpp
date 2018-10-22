76beacae6e36ca041d39a1da8f349b23823cc453
Andrey Abramov <andrey@arangodb.com>
2018-10-23 01:56:15 +0300

    fix compilation

--- arangod/RestHandler/RestViewHandler.cpp
+++ arangod/RestHandler/RestViewHandler.cpp
@@ -303,7 +303,7 @@ void RestViewHandler::modifyView(bool partialUpdate) {
       }
 
       auto newNameStr = newName.copyString();
-      auto res = _vocbase.renameView(view->id(), newNameStr);
+      auto res = _vocbase.renameView(view, newNameStr);
 
       if (res.ok()) {
         getView(newNameStr, false);
