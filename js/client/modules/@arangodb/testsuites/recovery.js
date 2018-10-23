b17f6c605811b46bc148bc831b94515aece3e382
Wilfried Goesgens <willi@arangodb.com>
2018-10-23 13:33:41 +0200

    Fix windows process handling for recovery and catch tests
    - don't spawn procdump if we know the process is going to be crashed and
      we don't care
    - keep the propper InstanceInfo so procdump crash information is preserved.

--- js/client/modules/@arangodb/testsuites/recovery.js
+++ js/client/modules/@arangodb/testsuites/recovery.js
@@ -151,13 +151,18 @@ function recovery (options) {
     let test = recoveryTests[i];
     let filtered = {};
     let localOptions = _.cloneDeep(options);
+    let disableMonitor = localOptions.disableMonitor;
 
     if (tu.filterTestcaseByOptions(test, localOptions, filtered)) {
-      let instanceInfo = {};
+      let instanceInfo = {
+        rootDir: pu.UNITTESTS_DIR
+      };
       count += 1;
 
       print(BLUE + "running setup of " + test + RESET);
+      localOptions.disableMonitor = true;
       runArangodRecovery(instanceInfo, localOptions, test, true, count);
+      localOptions.disableMonitor = disableMonitor;
 
       print(BLUE + "running recovery of " + test + RESET);
       results[test] = runArangodRecovery(instanceInfo, localOptions, test, false, count);
