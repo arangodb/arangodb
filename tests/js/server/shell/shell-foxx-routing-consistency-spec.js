a95d1173966d627d632f620a48233c40017ba8b9
Wilfried Goesgens <willi@arangodb.com>
2018-10-25 15:39:53 +0200

    if we run in maintainer mode, be more patient (#7050)

--- tests/js/server/shell/shell-foxx-routing-consistency-spec.js
+++ tests/js/server/shell/shell-foxx-routing-consistency-spec.js
@@ -12,6 +12,14 @@ const SERVICE_PATH = fs.makeAbsolute(fs.join(
 ));
 const MOUNT = '/consistencytest';
 
+let options = {
+  json: true
+};
+
+if (require('internal').db._version(true)['maintainer-mode'] === 'true') {
+  options.timeout = 300;
+}
+
 describe('Foxx routing consistency', () => {
   beforeEach(() => {
     FoxxManager.install(SERVICE_PATH, MOUNT);
@@ -24,7 +32,7 @@ describe('Foxx routing consistency', () => {
 
   it('serves service when everything is fine', () => {
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(200);
     expect(res.json).to.eql({hello: 'world'});
   });
@@ -32,7 +40,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_SERVICE_FILES_MISSING when bundle is missing', () => {
     fs.remove(FoxxService.bundlePath(MOUNT));
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_SERVICE_FILES_MISSING.code);
   });
@@ -40,7 +48,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_SERVICE_FILES_MISSING when service folder is missing', () => {
     fs.removeDirectoryRecursive(FoxxService.basePath(MOUNT), true);
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_SERVICE_FILES_MISSING.code);
   });
@@ -48,7 +56,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_SERVICE_FILES_OUTDATED when checksum is wrong', () => {
     fs.writeFileSync(FoxxService.bundlePath(MOUNT), 'keyboardcat');
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_SERVICE_FILES_OUTDATED.code);
   });
@@ -56,7 +64,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_MALFORMED_MANIFEST_FILE when manifest is mangled', () => {
     fs.writeFileSync(fs.join(FoxxService.basePath(MOUNT), 'manifest.json'), 'keyboardcat');
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_MALFORMED_MANIFEST_FILE.code);
   });
@@ -64,7 +72,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_SERVICE_MANIFEST_NOT_FOUND when manifest is deleted', () => {
     fs.remove(fs.join(FoxxService.basePath(MOUNT), 'manifest.json'));
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_SERVICE_MANIFEST_NOT_FOUND.code);
   });
@@ -72,7 +80,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_MODULE_SYNTAX_ERROR when entry file is mangled', () => {
     fs.writeFileSync(fs.join(FoxxService.basePath(MOUNT), 'index.js'), 'const keyboardcat;');
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_MODULE_SYNTAX_ERROR.code);
   });
@@ -80,7 +88,7 @@ describe('Foxx routing consistency', () => {
   it('serves ERROR_MODULE_NOT_FOUND when entry file is mangled', () => {
     fs.remove(fs.join(FoxxService.basePath(MOUNT), 'index.js'));
     FoxxManager.initializeFoxx();
-    const res = request.get(MOUNT, {json: true});
+    const res = request.get(MOUNT, options);
     expect(res.statusCode).to.equal(503);
     expect(res.json).to.have.property('errorNum', errors.ERROR_MODULE_NOT_FOUND.code);
   });
