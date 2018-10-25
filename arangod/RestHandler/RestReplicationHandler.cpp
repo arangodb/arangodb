3f8e75dbc27c1f8e7514a714451eaca1309ef347
Max Neunhoeffer <max@arangodb.com>
2018-10-25 16:36:46 +0200

    Get rid of numbers in exception log messages.

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -1416,17 +1416,17 @@ Result RestReplicationHandler::processRestoreDataBatch(
     }
   } catch (arangodb::basics::Exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not delete documents for restore exception (1): "
+      << "Could not delete documents for restore exception: "
       << ex.what();
     return Result(ex.code(), ex.what());
   } catch (std::exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not delete documents for restore exception (2): "
+      << "Could not delete documents for restore exception: "
       << ex.what();
     return Result(TRI_ERROR_INTERNAL, ex.what());
   } catch (...) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not delete documents for restore exception (3).";
+      << "Could not delete documents for restore exception.";
     return Result(TRI_ERROR_INTERNAL);
   }
 
@@ -1514,17 +1514,17 @@ Result RestReplicationHandler::processRestoreDataBatch(
     }
   } catch (arangodb::basics::Exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not insert documents for restore exception (1): "
+      << "Could not insert documents for restore exception: "
       << ex.what();
     return Result(ex.code(), ex.what());
   } catch (std::exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not insert documents for restore exception (2): "
+      << "Could not insert documents for restore exception: "
       << ex.what();
     return Result(TRI_ERROR_INTERNAL, ex.what());
   } catch (...) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not insert documents for restore exception (3).";
+      << "Could not insert documents for restore exception.";
     return Result(TRI_ERROR_INTERNAL);
   }
 
@@ -1582,17 +1582,17 @@ Result RestReplicationHandler::processRestoreDataBatch(
     }
   } catch (arangodb::basics::Exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not replace documents for restore exception (1): "
+      << "Could not replace documents for restore exception: "
       << ex.what();
     return Result(ex.code(), ex.what());
   } catch (std::exception const& ex) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not replace documents for restore exception (2): "
+      << "Could not replace documents for restore exception: "
       << ex.what();
     return Result(TRI_ERROR_INTERNAL, ex.what());
   } catch (...) {
     LOG_TOPIC(WARN, Logger::CLUSTER)
-      << "Could not replace documents for restore exception (3).";
+      << "Could not replace documents for restore exception.";
     return Result(TRI_ERROR_INTERNAL);
   }
 
