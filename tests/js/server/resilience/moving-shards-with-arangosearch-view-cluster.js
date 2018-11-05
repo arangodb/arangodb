e63b84fc561c3c4f2b7d67f6908b4395333a9465
Tobias Gödderz <tobias@arangodb.com>
2018-11-05 10:43:06 +0100

    Fix teardown, improved assertion & error message

--- tests/js/server/resilience/moving-shards-with-arangosearch-view-cluster.js
+++ tests/js/server/resilience/moving-shards-with-arangosearch-view-cluster.js
@@ -530,9 +530,10 @@ function MovingShardsWithViewSuite (options) {
       // actual servers that have the corresponding view index
       const serversPerShard = getViewServersPerShard();
       for(const [shard, leader] of Object.entries(leadersPerShard)) {
-        assertTrue(serversPerShard[shard].has(leader),
+        assertTrue(serversPerShard.hasOwnProperty(shard)
+          && serversPerShard[shard].has(leader),
           `Expected shard ${shard} to be available on ${leader}, but it's not. `
-          + `${{leadersPerShard, serversPerShard}}`);
+          + `${JSON.stringify({ leadersPerShard, serversPerShard })}`);
       }
     });
   }
@@ -762,10 +763,8 @@ function MovingShardsWithViewSuite (options) {
 ////////////////////////////////////////////////////////////////////////////////
 
     tearDown : function () {
-      for (var i = 0; i < c.length; ++i) {
-        v[i].drop();
-        c[i].drop();
-      }
+      v.forEach(v => v.drop());
+      c.forEach(c => c.drop());
       c = [];
       v = [];
       resetCleanedOutServers();
