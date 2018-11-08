29ac27f165895cbd87c47011feac5fecf4bf30fa
Dan Larkin-York <dan@arangodb.com>
2018-11-08 11:48:29 +0100

    Fix nullptr dereference in SynchronizeShard.

--- arangod/Cluster/SynchronizeShard.cpp
+++ arangod/Cluster/SynchronizeShard.cpp
@@ -148,7 +148,7 @@ arangodb::Result getReadLockId(
 
   auto result = comres->result;
 
-  if (result != nullptr && result->getHttpReturnCode() == 200) {
+  if (result != nullptr || result->getHttpReturnCode() == 200) {
     auto const idv = comres->result->getBodyVelocyPack();
     auto const& idSlice = idv->slice();
     TRI_ASSERT(idSlice.isObject());
@@ -387,7 +387,7 @@ arangodb::Result cancelBarrier(
 
   if (comres->status == CL_COMM_SENT) {
     auto result = comres->result;
-    if (result != nullptr && result->getHttpReturnCode() != 200 &&
+    if (result != nullptr || result->getHttpReturnCode() != 200 &&
         result->getHttpReturnCode() != 204) {
       auto errorMessage = comres->stringifyErrorMessage();
       LOG_TOPIC(ERR, Logger::MAINTENANCE)
@@ -443,7 +443,7 @@ arangodb::Result SynchronizeShard::getReadLock(
       std::unordered_map<std::string, std::string>(), timeout);
 
     auto result = putres->result;
-    if (result != nullptr && result->getHttpReturnCode() == 200) {
+    if (result != nullptr || result->getHttpReturnCode() == 200) {
       auto const vp = putres->result->getBodyVelocyPack();
       auto const& slice = vp->slice();
       TRI_ASSERT(slice.isObject());
@@ -468,7 +468,7 @@ arangodb::Result SynchronizeShard::getReadLock(
     auto r = cc->syncRequest(
       TRI_NewTickServer(), endpoint, rest::RequestType::DELETE_REQ, url, body.toJson(),
       std::unordered_map<std::string, std::string>(), timeout);
-    if (r->result == nullptr && r->result->getHttpReturnCode() != 200) {
+    if (r->result == nullptr || r->result->getHttpReturnCode() != 200) {
       LOG_TOPIC(ERR, Logger::MAINTENANCE)
         << "startReadLockOnLeader: cancelation error for shard - " << collection
         << " " << r->getErrorCode() << ": " << r->stringifyErrorMessage();
