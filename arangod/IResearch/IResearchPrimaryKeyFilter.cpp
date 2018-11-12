bc73a1b98be88674a1956cdd03a39fa946788c13
Andrey Abramov <andrey@arangodb.com>
2018-11-12 19:32:21 +0300

    refactor arangosearch pks

--- arangod/IResearch/IResearchPrimaryKeyFilter.cpp
+++ arangod/IResearch/IResearchPrimaryKeyFilter.cpp
@@ -52,8 +52,8 @@ irs::doc_iterator::ptr PrimaryKeyFilter::execute(
 size_t PrimaryKeyFilter::hash() const noexcept {
   size_t seed = 0;
   irs::hash_combine(seed, filter::hash());
-  irs::hash_combine(seed, _pk.cid());
-  irs::hash_combine(seed, _pk.rid());
+  irs::hash_combine(seed, _pk.first);
+  irs::hash_combine(seed, _pk.second);
   return seed;
 }
 
@@ -63,7 +63,9 @@ irs::filter::prepared::ptr PrimaryKeyFilter::prepare(
     irs::boost::boost_t /*boost*/,
     irs::attribute_view const& /*ctx*/
 ) const {
-  auto const pkRef = static_cast<irs::bytes_ref>(_pk);
+  irs::bytes_ref const pkRef(
+    reinterpret_cast<irs::byte_type const*>(&_pk), sizeof(_pk)
+  );
 
   for (auto& segment : index) {
     auto* pkField = segment.field(arangodb::iresearch::DocumentPrimaryKey::PK());
@@ -95,8 +97,8 @@ bool PrimaryKeyFilter::equals(filter const& rhs) const noexcept {
   auto const& trhs = static_cast<PrimaryKeyFilter const&>(rhs);
 
   return filter::equals(rhs)
-    && _pk.cid() == trhs._pk.cid()
-    && _pk.rid() == trhs._pk.rid();
+    && _pk.first == trhs._pk.first
+    && _pk.second == trhs._pk.second;
 }
 
 DEFINE_FILTER_TYPE(PrimaryKeyFilter);
