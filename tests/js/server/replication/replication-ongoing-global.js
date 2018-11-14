89eb1c54999e9094bb601a86b2bb76d05092628e
Simon Grätzer <simon@graetzer.org>
2018-11-14 22:12:53 +0100

    make test more reliable

--- tests/js/server/replication/replication-ongoing-global.js
+++ tests/js/server/replication/replication-ongoing-global.js
@@ -494,8 +494,13 @@ function BaseTestConfig () {
         },
 
         function (state) {
-          assertEqual(db._collection(cn).count(), 0);
-          assertEqual(db._collection(cn).all().toArray().length, 0);
+          const c = db._collection(cn);
+          let x = 10;
+          while (c.count() > 0 && x-- > 0) {
+            internal.sleep(1);
+          }
+          assertEqual(c.count(), 0);
+          assertEqual(c.all().toArray().length, 0);
         }
       );
     },
