393f5ea67b8743d89a2a03095f306f2d73aefbb9
Lars Maier <lars@arangodb.com>
2018-11-16 11:57:27 +0100

    As follower in an active failover answer a request with not a leader, even if the database does not exist. (#7338)

--- arangod/GeneralServer/GeneralCommTask.cpp
+++ arangod/GeneralServer/GeneralCommTask.cpp
@@ -150,11 +150,26 @@ bool resolveRequestContext(GeneralRequest& req) {
 GeneralCommTask::RequestFlow GeneralCommTask::prepareExecution(
     GeneralRequest& req) {
   if (!::resolveRequestContext(req)) {
-    addErrorResponse(rest::ResponseCode::NOT_FOUND, req.contentTypeResponse(),
-                     req.messageId(), TRI_ERROR_ARANGO_DATABASE_NOT_FOUND,
-                     TRI_errno_string(TRI_ERROR_ARANGO_DATABASE_NOT_FOUND));
 
-    return RequestFlow::Abort;
+    // BUG FIX - do not answer with DATABASE_NOT_FOUND, when follower in active failover
+    auto mode = ServerState::serverMode();
+    switch (mode) {
+      case ServerState::Mode::REDIRECT:
+        addErrorResponse(rest::ResponseCode::SERVICE_UNAVAILABLE, req.contentTypeResponse(),
+                     req.messageId(), TRI_ERROR_CLUSTER_NOT_LEADER,
+                     TRI_errno_string(TRI_ERROR_CLUSTER_NOT_LEADER));
+        return RequestFlow::Abort;
+      case ServerState::Mode::TRYAGAIN:
+        addErrorResponse(rest::ResponseCode::SERVICE_UNAVAILABLE, req.contentTypeResponse(),
+                     req.messageId(), TRI_ERROR_CLUSTER_LEADERSHIP_CHALLENGE_ONGOING,
+                     TRI_errno_string(TRI_ERROR_CLUSTER_LEADERSHIP_CHALLENGE_ONGOING));
+        return RequestFlow::Abort;
+      default:
+        addErrorResponse(rest::ResponseCode::NOT_FOUND, req.contentTypeResponse(),
+               req.messageId(), TRI_ERROR_ARANGO_DATABASE_NOT_FOUND,
+               TRI_errno_string(TRI_ERROR_ARANGO_DATABASE_NOT_FOUND));
+        return RequestFlow::Abort;
+    }
   }
   TRI_ASSERT(req.requestContext() != nullptr);
 
