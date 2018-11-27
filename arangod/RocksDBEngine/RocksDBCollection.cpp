87f6152ea64cfa3ec9833a5aaed7789c497f8994
Simon <simon@graetzer.org>
2018-11-27 20:22:18 +0100

    Micro-Optimize AQL CXX calls (#7486)
    (cherry picked from commit 8ddb9a063b8d54c7efe022418e3012ed18d7bc79)

--- arangod/RocksDBEngine/RocksDBCollection.cpp
+++ arangod/RocksDBEngine/RocksDBCollection.cpp
@@ -818,7 +818,7 @@ Result RocksDBCollection::insert(
     // up to the SavePoint. this can be super-expensive for bigger transactions.
     // to keep things simple, we are not checking for unique constraint violations
     // in secondary indexes here, but defer it to the regular index insertion check
-    VPackSlice keySlice = newSlice.get(StaticStrings::KeyString);
+    VPackSlice keySlice = transaction::helpers::extractKeyFromDocument(newSlice);
     if (keySlice.isString()) {
       LocalDocumentId const documentId = primaryIndex()->lookupKey(trx, StringRef(keySlice));
       if (documentId.isSet()) {
