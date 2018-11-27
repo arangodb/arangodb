54c3688609424eb164a04dbe33d16f3605bb1992
Dan Larkin-York <dan@arangodb.com>
2018-11-27 15:46:20 -0500

    Persist and check default language selection.

--- arangod/RestServer/LanguageCheckFeature.h
+++ arangod/RestServer/LanguageCheckFeature.h
@@ -17,37 +17,28 @@
 ///
 /// Copyright holder is ArangoDB GmbH, Cologne, Germany
 ///
-/// @author Dr. Frank Celler
+/// @author Daniel Larkin-York
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef ARANGODB_APPLICATION_FEATURES_LANGUAGE_FEATURE_H
-#define ARANGODB_APPLICATION_FEATURES_LANGUAGE_FEATURE_H 1
+#ifndef ARANGODB_REST_SERVER_LANGUAGE_CHECK_FEATURE_H
+#define ARANGODB_REST_SERVER_LANGUAGE_CHECK_FEATURE_H 1
 
 #include <unicode/locid.h>
 #include "ApplicationFeatures/ApplicationFeature.h"
 
 namespace arangodb {
 
-class LanguageFeature final : public application_features::ApplicationFeature {
+class LanguageCheckFeature final
+    : public application_features::ApplicationFeature {
  public:
-  explicit LanguageFeature(application_features::ApplicationServer& server);
-  ~LanguageFeature();
+  explicit LanguageCheckFeature(
+      application_features::ApplicationServer& server);
+  ~LanguageCheckFeature();
 
-  void collectOptions(std::shared_ptr<options::ProgramOptions>) override final;
-  void prepare() override final;
+  static std::string const& name() noexcept;
   void start() override final;
-  static void* prepareIcu(std::string const& binaryPath, std::string const& binaryExecutionPath, std::string& path, std::string const& binaryName);
-  static LanguageFeature* instance();
-  Locale& getLocale() { return _locale; }
-  std::string const& getDefaultLanguage() const { return _language; }
-
-private:
-  Locale _locale;
-  std::string _language;
-  char const* _binaryPath;
-  void* _icuDataPtr;
 };
 
-}
+}  // namespace arangodb
 
 #endif
