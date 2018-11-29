d939ddc8c20d14a4b5d142f7f2c091d2596111f9
Frank Celler <frank@arangodb.com>
2018-11-29 14:32:38 +0100

    signature fixed

--- arangod/RestHandler/RestTestHandler.cpp
+++ arangod/RestHandler/RestTestHandler.cpp
@@ -134,7 +134,7 @@ RestStatus RestTestHandler::execute() {
 
   bool ok = SchedulerFeature::SCHEDULER->queue(
     priority(res.get()),
-    [this, self, duration]() {
+    [this, self, duration](bool) {
       auto stop = clock::now() + duration;
 
       uint64_t count = 0;
