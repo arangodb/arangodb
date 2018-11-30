de96a89ba78e9f888de166b9e7f9895e08e81d91
Andrey Abramov <gnusmas@kvolitek.ru>
2018-12-01 01:19:34 +0300

    treat all iresearch scores as float_t for 3.4 (#7573)
    * treat all iresearch scores as float_t for 3.4
    
    * cleanup
    
    * attempt to fix tests
    
    * another attempt to fix tests

--- arangod/Aql/SortBlock.cpp
+++ arangod/Aql/SortBlock.cpp
@@ -49,11 +49,11 @@ class OurLessThan {
       auto const& lhs = _buffer[a.first]->getValueReference(a.second, reg.reg);
       auto const& rhs = _buffer[b.first]->getValueReference(b.second, reg.reg);
 
-#ifdef USE_IRESEARCH
+#if 0 // #ifdef USE_IRESEARCH
       TRI_ASSERT(reg.comparator);
       int const cmp = (*reg.comparator)(reg.scorer.get(), _trx, lhs, rhs);
 #else
-      int const cmp = AqlValue::Compare(_trx, lhs, rhs, true);
+    int const cmp = AqlValue::Compare(_trx, lhs, rhs, true);
 #endif
 
       if (cmp < 0) {
