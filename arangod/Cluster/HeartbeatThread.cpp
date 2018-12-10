ceeae07ffe8292079e51675aa5efc2f2cd8d5143
Tobias Gödderz <github@tobias.goedderz.info>
2018-12-10 12:41:50 +0100

    Reload Foxx routes during startup (#7533)

--- arangod/Cluster/HeartbeatThread.cpp
+++ arangod/Cluster/HeartbeatThread.cpp
@@ -678,7 +678,7 @@ void HeartbeatThread::runSingleServer() {
 
         // ensure everyone has server access
         ServerState::instance()->setFoxxmaster(_myId);
-        auto prv = ServerState::instance()->setServerMode(ServerState::Mode::DEFAULT);
+        auto prv = ServerState::setServerMode(ServerState::Mode::DEFAULT);
         if (prv == ServerState::Mode::REDIRECT) {
           LOG_TOPIC(INFO, Logger::HEARTBEAT) << "Successful leadership takeover: "
                                              << "All your base are belong to us";
