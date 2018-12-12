031df715e0147a20cdb51dd718307b2b34bca0bb
Michael Hackstein <michael@arangodb.com>
2018-12-12 18:26:56 +0100

    Fix a rare deadlock situation in Replication sync phase

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -2872,7 +2872,9 @@ bool RestReplicationHandler::isTombstoned(aql::QueryId id) const {
 
 void RestReplicationHandler::registerTombstone(aql::QueryId id) const {
   std::string key = IdToTombstoneKey(_vocbase, id);
-  WRITE_LOCKER(writeLocker, RestReplicationHandler::_tombLock);
-  RestReplicationHandler::_tombstones.emplace(key, std::chrono::steady_clock::now() + RestReplicationHandler::_tombstoneTimeout); 
+  {
+    WRITE_LOCKER(writeLocker, RestReplicationHandler::_tombLock);
+    RestReplicationHandler::_tombstones.emplace(key, std::chrono::steady_clock::now() + RestReplicationHandler::_tombstoneTimeout); 
+  }
   timeoutTombstones();
 }
