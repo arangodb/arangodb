13e712e72e34ba3a7cc4299497defbd9d169b031
Frank Celler <frank@arangodb.com>
2018-12-26 01:10:50 +0100

    reformat preps

--- utils/reformat.sh
+++ utils/reformat.sh
@@ -3,5 +3,5 @@
 find arangod arangosh lib enterprise \
      -name Zip -prune -o \
      -type f "(" -name "*.cpp" -o -name "*.h" ")" \
-     "!" "(" -name "tokens.*" -o -name "v8-json.*" -o -name "voc-errors.*" -o -name "grammar.*" -o -name "xxhash.*" ")" | \
+     "!" "(" -name "tokens.*" -o -name "v8-json.*" -o -name "voc-errors.*" -o -name "grammar.*" -o -name "xxhash.*" -o -name "exitcodes.*" ")" | \
      xargs clang-format -i -verbose
