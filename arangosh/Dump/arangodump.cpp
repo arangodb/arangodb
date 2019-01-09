873d762f4ff9c111f435b9d7ad575f744a78275f
Frank Celler <frank@arangodb.com>
2019-01-10 00:14:44 +0100

    moved xify to enterprise

--- arangosh/Dump/arangodump.cpp
+++ arangosh/Dump/arangodump.cpp
@@ -35,6 +35,7 @@
 #include "Dump/DumpFeature.h"
 #include "Logger/Logger.h"
 #include "Logger/LoggerFeature.h"
+#include "Maskings/AttributeMasking.h"
 #include "ProgramOptions/ProgramOptions.h"
 #include "Random/RandomFeature.h"
 #include "Shell/ClientFeature.h"
@@ -42,6 +43,7 @@
 
 #ifdef USE_ENTERPRISE
 #include "Enterprise/Encryption/EncryptionFeature.h"
+#include "Enterprise/Maskings/AttributeMaskingEE.h"
 #endif
 
 using namespace arangodb;
@@ -52,6 +54,12 @@ int main(int argc, char* argv[]) {
   return ClientFeature::runMain(argc, argv, [&](int argc, char* argv[]) -> int {
     ArangoGlobalContext context(argc, argv, BIN_DIRECTORY);
     context.installHup();
+
+    maskings::InstallMaskings();
+
+#ifdef USE_ENTERPRISE
+    maskings::InstallMaskingsEE();
+#endif
 
     std::shared_ptr<options::ProgramOptions> options(
         new options::ProgramOptions(argv[0], "Usage: arangodump [<options>]",
