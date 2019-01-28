65dc15bc610646d7d96a467884cb22d25e275a01
Jan <jsteemann@users.noreply.github.com>
2019-01-28 08:55:22 +0100

    Bug fix/misc issues (#8013)
    * added missing return statements
    
    * only spend up to 10 seconds for initially fetching the list of collections in arangosh
    
    fetching the list of collections is a blocking operation, and the default timeout for this is very high.
    If the server is blocked by whatever reason, then the shell is unusable until the collections list request returns.
    To avoid this, the initial request is limited to 10 seconds, so the shell can be used afterwards.
    
    * if an index cannot be used for sorting, its sort
    
    cost was previously returned as 0. this will in fact favor
    indexes that can be used for filtering but not for sorting
    over indexes that can be used for both.
    
    this change is to report the sort cost for indexes that
    cannot be used for sorting to n * log(n), where n is the
    number of documents that optimizer expects to come out of the
    index after filtering

--- arangod/Transaction/Methods.cpp
+++ arangod/Transaction/Methods.cpp
@@ -642,7 +642,11 @@ std::pair<bool, bool> transaction::Methods::findIndexHandleForAndNode(
     if (!sortCondition->isEmpty()) {
       // only take into account the costs for sorting if there is actually
       // something to sort
-      totalCost += sortCost;
+      if (supportsSort) {
+        totalCost += sortCost;
+      } else {
+        totalCost += estimatedItems * std::log2(static_cast<double>(estimatedItems));
+      }
     }
 
     LOG_TOPIC(TRACE, Logger::FIXME)
