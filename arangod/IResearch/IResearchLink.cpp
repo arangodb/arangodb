29c43b8b035fe9e2d90dea3e1a20984d7549063d
Vasiliy <vasiliy@arangodb.com>
2019-02-01 18:20:15 +0300

    address scenario where link is dropped twice

--- arangod/IResearch/IResearchLink.cpp
+++ arangod/IResearch/IResearchLink.cpp
@@ -627,14 +627,21 @@ arangodb::Result IResearchLink::drop() {
       logicalView.get() // args
     );
 
+    // may occur if the link was already unlinked from the view via another instance
+    // this behaviour was seen user-access-right-drop-view-arangosearch-spec.js
+    // where the collection drop was called through REST,
+    // the link was dropped as a result of the collection drop call
+    // then the view was dropped vvia a separate REST call
+    // then the vocbase was destroyed calling
+    // collection close()-> link unload() -> link drop() due to collection marked as dropped
+    // thus returning an error here will cause ~TRI_vocbase_t() on RocksDB to
+    // receive an exception which is not handled in the destructor
     if (!view) {
-      return arangodb::Result( // result
-        TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, // code
-        std::string("unable to find view '") + _viewGuid + "' while dropping arangosearch link '" + std::to_string(_id) + "'"
-      );
+      LOG_TOPIC(WARN, arangodb::iresearch::TOPIC)
+        << "unable to find arangosearch view '" << _viewGuid << "' while dropping arangosearch link '" << _id << "'";
+    } else {
+      view->unlink(_collection.id()); // unlink before reset() to release lock in view (if any)
     }
-
-    view->unlink(_collection.id()); // unlink before reset() to release lock in view (if any)
   }
 
   _flushCallback = IResearchFeature::WalFlushCallback(); // reset together with '_asyncSelf'
