da5304658271ddb6f0cf3f7140694a120c01e737
Wilfried Goesgens <willi@arangodb.com>
2019-02-15 16:03:06 +0100

    fix require statement

--- js/server/modules/@arangodb/aql-helper.js
+++ js/server/modules/@arangodb/aql-helper.js
@@ -33,7 +33,7 @@
 // / @brief normalize a single row result
 // //////////////////////////////////////////////////////////////////////////////
 
-let isEqual = require("test-helper").isEqual;
+let isEqual = require("@arangodb/test-helper").isEqual;
 
 exports.isEqual = isEqual;
 
