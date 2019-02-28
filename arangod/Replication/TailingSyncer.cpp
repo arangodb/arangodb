1de0d3682a03acb927dba603624a5187f9752992
jsteemann <jan@arangodb.com>
2019-02-28 10:35:29 +0100

    abort ongoing transactions in all cases

--- arangod/Replication/TailingSyncer.cpp
+++ arangod/Replication/TailingSyncer.cpp
@@ -1275,6 +1275,8 @@ retry:
                   "caught unknown exception during initial replication");
       }
     }
+      
+    abortOngoingTransactions();
 
     _applier->stop(res);
     return res;
