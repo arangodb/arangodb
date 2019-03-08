9bb59642f5d310c1ce15c0fedb771eb96c4f8ee3
Kaveh Vahedipour <kaveh@arangodb.com>
2019-03-08 10:36:09 +0100

    [3.3] fix agency compaction index mismatch insertion bug (#8331)
    * fix agency bug, where compaction index mismatch could lead to fatal exit on restart

--- arangod/Agency/State.cpp
+++ arangod/Agency/State.cpp
@@ -1237,7 +1237,24 @@ bool State::persistCompactionSnapshot(index_t cind, arangodb::consensus::term_t 
       THROW_ARANGO_EXCEPTION(res);
     }
 
-    auto result = trx.insert("compact", store.slice(), _options);
+    OperationResult result;
+    try {
+      auto result = trx.insert("compact", store.slice(), _options);
+      if (!result.ok()) {
+        if (result.is(TRI_ERROR_ARANGO_UNIQUE_CONSTRAINT_VIOLATED)) {
+          result = trx.replace("compact", store.slice(), _options);
+          if (!result.ok()) {
+            LOG_TOPIC(FATAL, Logger::AGENCY)
+              << "Failed to persist compated state: " << result.errorMessage();
+          }
+        } else {
+          LOG_TOPIC(FATAL, Logger::AGENCY)
+            << "Failed to persist compated state: " << result.errorMessage();
+        }
+      } 
+    } catch (std::exception const& e) {
+      LOG_TOPIC(FATAL, Logger::AGENCY) << "Failed to persist compated state: " << e.what();
+    }
     res = trx.finish(result.result);
 
     if (res.ok()) {
