febc4e5f455a4ea29c84615cb9209fc249b444c6
hkernbach <heiko.kernbach@gmail.com>
2019-03-08 15:39:48 +0100

    foxx arangoUser request

--- js/client/tests/authentication/foxx-arango-auth-spec.js
+++ js/client/tests/authentication/foxx-arango-auth-spec.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect;
 const FoxxManager = require('org/arangodb/foxx/manager');
 const fs = require('fs');
 const internal = require('internal');
-const basePath = fs.makeAbsolute(fs.join(internal.startupPath, 'common', 'test-data', 'apps', 'arango-auth'));
+const basePath = fs.makeAbsolute(fs.join(internal.pathForTesting('common'), 'test-data', 'apps', 'arango-auth'));
 const url = arango.getEndpoint().replace(/\+vpp/, '').replace(/^tcp:/, 'http:').replace(/^ssl:/, 'https:');
 
 describe('Foxx arangoUser', function () {
@@ -40,4 +40,148 @@ describe('Foxx arangoUser', function () {
     expect(result.code).to.equal(200);
     expect(result.body).to.eql(JSON.stringify({user: 'root'}));
   });
+
+  it('should not set the arangoUser object if not authenticated correctly - used invalid password', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('root:invalidpassword').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - used invalid username', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('iamnotavaliduser:').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - used invalid username and password', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('iamnotavaliduser:noriamavalidpassword').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - empty username and empty password', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer(':').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - empty password', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('iamnotavaliduser:').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - empty user', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer(':somerandompass').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - empty string', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str boolean true', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('true').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str boolean false', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('false').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str array', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('[]').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str obj', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer('{}').toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str array', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer([]).toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
+  it('should not set the arangoUser object if not authenticated correctly - str obj', function () {
+    const opts = { headers: {
+      authorization: (
+        'Basic ' + new Buffer({}).toString('base64')
+      )
+    }};
+    const result = internal.download(url + mount, '', opts);
+    expect(result.code).to.equal(200);
+    expect(result.body).to.eql(JSON.stringify({user: null}));
+  });
+
 });
