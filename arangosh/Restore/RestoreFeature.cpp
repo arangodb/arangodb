7cb8fd326a5d28eeb30b53b85733b11754dbbbd3
jsteemann <jan@arangodb.com>
2019-03-13 13:04:49 +0100

    fix warning message

--- arangosh/Restore/RestoreFeature.cpp
+++ arangosh/Restore/RestoreFeature.cpp
@@ -1348,7 +1348,7 @@ void RestoreFeature::start() {
   }
   
   if (role == "DBSERVER" || role == "PRIMARY") {
-    LOG_TOPIC(WARN, arangodb::Logger::DUMP) << "operations in the cluster should be carried via a coordinator, but we are connected to a database server node. this is an unsupported operation!";
+    LOG_TOPIC(WARN, arangodb::Logger::RESTORE) << "You connected to a DBServer node, but operations in a cluster should be carried out via a Coordinator. This is an unsupported operation!";
   }
 
   std::tie(result, _options.indexesFirst) =
