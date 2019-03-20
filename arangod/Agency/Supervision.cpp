fbb9c3d75da2d98e5e5af298c93c94f308b8f2e2
Kaveh Vahedipour <kaveh@arangodb.com>
2019-03-20 13:16:00 +0100

    [3.4] leader check needs to sit inside waitfor loop (#8444)
    * leader check needs to sit inside waitfor loop
    * Do not wait in Supervision for commits of new writes.
    * CHANGELOG.

--- arangod/Agency/Supervision.cpp
+++ arangod/Agency/Supervision.cpp
@@ -852,25 +852,27 @@ void Supervision::run() {
         }
       }
 
-      // If anything was rafted, we need to
-      index_t leaderIndex = _agent->index();
-      
-      if (leaderIndex != 0) {
-        while (!this->isStopping() && _agent->leading()) { // No point in progressing, if indexes cannot be advanced
-
-          auto result = _agent->waitFor(leaderIndex);
-          if (result == Agent::raft_commit_t::TIMEOUT) { // Oh snap
-            // Note that we can get UNKNOWN if we have lost leadership or
-            // if we are shutting down. In both cases we just leave the loop.
-            LOG_TOPIC(WARN, Logger::SUPERVISION) << "Waiting for commits to be done ... ";
-            continue; 
-          } else {                                       // Good we can continue
-            break;
+      // If anything was rafted, we need to wait until it is replicated,
+      // otherwise it is not "committed" in the Raft sense. However, let's
+      // only wait for our changes not for new ones coming in during the wait.
+      if (_agent->leading()) {
+        index_t leaderIndex = _agent->index();
+        if (leaderIndex != 0) {
+          while (!this->isStopping() && _agent->leading()) {
+            auto result = _agent->waitFor(leaderIndex);
+            if (result == Agent::raft_commit_t::TIMEOUT) { // Oh snap
+              // Note that we can get UNKNOWN if we have lost leadership or
+              // if we are shutting down. In both cases we just leave the loop.
+              LOG_TOPIC(WARN, Logger::SUPERVISION)
+                << "Waiting for commits to be done ... ";
+              continue;
+            } else {                       // Good we can continue
+              break;
+            }
           }
-
         }
       }
-      
+
       auto lapTime = std::chrono::duration_cast<std::chrono::microseconds>(
         std::chrono::steady_clock::now() - lapStart).count();
       
