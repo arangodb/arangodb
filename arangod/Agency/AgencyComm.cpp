8142f58279b9f501a213513bdcd938e69fabeedd
Kaveh Vahedipour <kaveh@arangodb.com>
2019-03-22 21:18:58 +0100

    cannot depend on ServerFeature

--- arangod/Agency/AgencyComm.cpp
+++ arangod/Agency/AgencyComm.cpp
@@ -1181,11 +1181,7 @@ AgencyCommResult AgencyComm::sendTransactionWithFailover(AgencyTransaction const
 bool AgencyComm::ensureStructureInitialized() {
   LOG_TOPIC(TRACE, Logger::AGENCYCOMM) << "checking if agency is initialized";
 
-  auto serverFeature =
-    application_features::ApplicationServer::getFeature<ServerFeature>("Server");
-
-
-  while (!serverFeature->isStopping() && shouldInitializeStructure()) {
+  while (shouldInitializeStructure()) {
 
     LOG_TOPIC(TRACE, Logger::AGENCYCOMM)
       << "Agency is fresh. Needs initial structure.";
@@ -1797,7 +1793,7 @@ bool AgencyComm::shouldInitializeStructure() {
   size_t nFail = 0;
 
   while (!application_features::ApplicationServer::isStopping()) {
-    
+
     auto result = getValues("Plan");
 
     if (!result.successful()) { // Not 200 - 299
@@ -1820,6 +1816,7 @@ bool AgencyComm::shouldInitializeStructure() {
         } else {
           LOG_TOPIC(DEBUG, Logger::AGENCYCOMM)
             << "agency initialisation under way or done";
+          return false;
         }
       } else {
         // Should never get here
