6f347f6430cde9c2972f7266c24b1478e05e85e0
Simon Grätzer <simon@graetzer.org>
2019-03-22 13:58:56 +0100

    do not move so often

--- arangod/RocksDBEngine/RocksDBCollection.cpp
+++ arangod/RocksDBEngine/RocksDBCollection.cpp
@@ -616,7 +616,7 @@ Result RocksDBCollection::truncate(transaction::Methods* trx, OperationOptions& 
 
     ++found;
     TRI_ASSERT(_objectId == RocksDBKey::objectId(iter->key()));
-    VPackSlice document = VPackSlice(iter->value().data());
+    VPackSlice document(iter->value().data());
     TRI_ASSERT(document.isObject());
     
     // tmp may contain a pointer into rocksdb::WriteBuffer::_rep. This is
