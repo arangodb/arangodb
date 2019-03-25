59d194092cc9d9418911b26c925a8269416f0d26
Vasiliy <vasiliy@arangodb.com>
2019-03-25 14:09:51 +0300

    comment out the rest of the tom-be removd functionality

--- tests/IResearch/IResearchAnalyzerFeature-test.cpp
+++ tests/IResearch/IResearchAnalyzerFeature-test.cpp
@@ -1908,7 +1908,7 @@ SECTION("test_start") {
     });
     CHECK((expected.empty()));
   }
-*/
+
   // FIXME TODO remove test since there is no more ensure()
   // test feature start load configuration (inRecovery, with configuration collection, uninitialized analyzers)
   {
