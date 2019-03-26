b919609aeeb119f40ee4f92c5fe3a45fc6ea84a8
Wilfried Goesgens <willi@arangodb.com>
2019-03-26 18:03:13 +0100

    add a way to specify argument vectors the arangodb way

--- js/common/bootstrap/modules/internal.js
+++ js/common/bootstrap/modules/internal.js
@@ -741,20 +741,27 @@ global.DEFINE_MODULE('internal', (function () {
           if (longOptsEqual) {
             vec.push('--' + key + '=' + structure[key]);
           } else {
-            vec.push('--' + key);
-            if (structure[key] !== false) {
-              if (structure[key] !== true) {
-                if (structure[key] !== null) {
-                  // The null case is for the case one wants to add an option
-                  // with an equals sign all in the key, which is necessary if
-                  // one wants to specify an option multiple times.
-                  vec.push(structure[key]);
+            if (Array.isArray(structure[key])) {
+              structure[key].forEach(function (i, j) {
+                vec.push('--' + key);
+                vec.push(i);
+              });
+            } else {
+              vec.push('--' + key);
+              if (structure[key] !== false) {
+                if (structure[key] !== true) {
+                  if (structure[key] !== null) {
+                    // The null case is for the case one wants to add an option
+                    // with an equals sign all in the key, which is necessary if
+                    // one wants to specify an option multiple times.
+                    vec.push(structure[key]);
+                  }
+                } else {
+                  vec.push('true');
                 }
               } else {
-                vec.push('true');
+                vec.push('false');
               }
-            } else {
-              vec.push('false');
             }
           }
         }
