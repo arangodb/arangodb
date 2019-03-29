d38b231f434ea96e5118a29620000053c3c91bb5
KVS85 <vadim@arangodb.com>
2019-03-30 01:33:45 +0300

    Backport testing improvement from devel 29.03.2019 (#8629)

--- js/client/modules/@arangodb/test-utils.js
+++ js/client/modules/@arangodb/test-utils.js
@@ -176,9 +176,20 @@ function performTests (options, testList, testname, runFn, serverOptions, startS
 
       let collectionsBefore = [];
       if (!serverDead) {
-        db._collections().forEach(collection => {
-          collectionsBefore.push(collection._name);
-        });
+        try {
+          db._collections().forEach(collection => {
+            collectionsBefore.push(collection._name);
+          });
+        }
+        catch (x) {
+          results[te] = {
+            status: false,
+            message: 'failed to fetch the currently available collections: ' + x.message + '. Original test status: ' + JSON.stringify(results[te])
+          };
+          continueTesting = false;
+          serverDead = true;
+          first = false;
+        }
       }
       while (first || options.loopEternal) {
         if (!continueTesting) {
@@ -591,7 +602,12 @@ function runThere (options, instanceInfo, file) {
     } else {
       if ((reply.code === 500) &&
           reply.hasOwnProperty('message') &&
-          (reply.message === 'Request timeout reached')) {
+          (
+            (reply.message.search('Request timeout reached') >= 0 ) ||
+            (reply.message.search('timeout during read') >= 0 ) ||
+            (reply.message.search('Connection closed by remote') >= 0 )
+          )) {
+        print(RED + Date() + " request timeout reached, aborting test execution" + RESET);
         return {
           status: false,
           message: reply.message,
