a0ee98853f01f48d2edd75515d7f941b5fadc8af
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2019-04-02 09:05:47 +0100

    Add k-shortest-paths finder

---
+++ arangod/Graph/ConstantWeightKShortestPathsFinder.cpp
@@ -0,0 +1,330 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ConstantWeightKShortestPathsFinder.h"
+
+#include "Aql/AqlValue.h"
+#include "Cluster/ServerState.h"
+#include "Graph/EdgeCursor.h"
+#include "Graph/EdgeDocumentToken.h"
+#include "Graph/ShortestPathOptions.h"
+#include "Graph/ShortestPathResult.h"
+#include "Graph/TraverserCache.h"
+#include "Transaction/Helpers.h"
+#include "Utils/OperationCursor.h"
+#include "VocBase/LogicalCollection.h"
+
+#include <velocypack/Iterator.h>
+#include <velocypack/Slice.h>
+#include <velocypack/StringRef.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::graph;
+
+//
+ConstantWeightKShortestPathsFinder::ConstantWeightKShortestPathsFinder(ShortestPathOptions& options)
+    : ShortestPathFinder(options), _pathAvailable(false) {}
+
+ConstantWeightKShortestPathsFinder::~ConstantWeightKShortestPathsFinder() {}
+
+// Sets up k-shortest-paths traversal from start to end
+// Returns number of currently known paths
+bool ConstantWeightKShortestPathsFinder::startKShortestPathsTraversal(
+    arangodb::velocypack::Slice const& start, arangodb::velocypack::Slice const& end) {
+  // TODO: ?
+  if (start == end) {
+    return true;
+  }
+
+  _start = arangodb::velocypack::StringRef(start);
+  _end = arangodb::velocypack::StringRef(end);
+  _pathAvailable = true;
+  _shortestPaths.clear();
+
+  TRI_IF_FAILURE("TraversalOOMInitialize") {
+    THROW_ARANGO_EXCEPTION(TRI_ERROR_DEBUG);
+  }
+
+  return true;
+}
+
+bool ConstantWeightKShortestPathsFinder::computeShortestPath(
+    const VertexRef& start, const VertexRef& end, const std::vector<VertexRef>& forbiddenVertices,
+    const std::vector<Edge>& forbiddenEdges, Path& result) {
+  bool found = false;
+  Ball left(start, FORWARD);
+  Ball right(end, BACKWARD);
+  VertexRef join;
+
+  result.clear();
+
+  while (!left._frontier.empty() && !right._frontier.empty() && !found) {
+    //    callback();
+
+    // Choose the smaller frontier to expand.
+    if (left._frontier.size() < right._frontier.size()) {
+      found = advanceFrontier(left, right, forbiddenVertices, forbiddenEdges, join);
+    } else {
+      found = advanceFrontier(right, left, forbiddenVertices, forbiddenEdges, join);
+    }
+  }
+  if (found) {
+    reconstructPath(left, right, join, result);
+  }
+  return found;
+}
+
+void ConstantWeightKShortestPathsFinder::computeNeighbourhoodOfVertex(
+    VertexRef vertex, Direction direction, std::vector<VertexRef>& neighbours,
+    std::vector<Edge>& edges) {
+  std::unique_ptr<EdgeCursor> edgeCursor;
+
+  switch (direction) {
+    case BACKWARD:
+      edgeCursor.reset(_options.nextReverseCursor(vertex));
+      break;
+    case FORWARD:
+      edgeCursor.reset(_options.nextCursor(vertex));
+      break;
+    default:
+      TRI_ASSERT(true);
+  }
+
+  auto callback = [&](EdgeDocumentToken&& eid, VPackSlice edge, size_t cursorIdx) -> void {
+    if (edge.isString()) {
+      if (edge.compareString(vertex.data(), vertex.length()) != 0) {
+        VertexRef id = _options.cache()->persistString(VertexRef(edge));
+        edges.emplace_back(std::move(eid));
+        neighbours.emplace_back(id);
+      }
+    } else {
+      VertexRef other(transaction::helpers::extractFromFromDocument(edge));
+      if (other == vertex) {
+        other = VertexRef(transaction::helpers::extractToFromDocument(edge));
+      }
+      if (other != vertex) {
+        VertexRef id = _options.cache()->persistString(other);
+        edges.emplace_back(std::move(eid));
+        neighbours.emplace_back(id);
+      }
+    }
+  };
+  edgeCursor->readAll(callback);
+}
+
+bool ConstantWeightKShortestPathsFinder::advanceFrontier(
+    Ball& source, const Ball& target, const std::vector<VertexRef>& forbiddenVertices,
+    const std::vector<Edge>& forbiddenEdges, VertexRef& join) {
+  std::vector<VertexRef> neighbours;
+  std::vector<graph::EdgeDocumentToken> edges;
+  Frontier newFrontier;
+
+  // TODO: This is slow and has to be converted to hash tables.
+  auto isEdgeForbidden = [forbiddenEdges](const Edge& e) -> bool {
+    for (auto const& f : forbiddenEdges) {
+      if (f.equals(e)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  auto isVertexForbidden = [forbiddenVertices](const VertexRef& v) -> bool {
+    for (auto const& w : forbiddenVertices) {
+      if (w.equals(v)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (auto& v : source._frontier) {
+    neighbours.clear();
+    edges.clear();
+
+    computeNeighbourhoodOfVertex(v, source._direction, neighbours, edges);
+    TRI_ASSERT(edges.size() == neighbours.size());
+    size_t const neighboursSize = neighbours.size();
+    for (size_t i = 0; i < neighboursSize; ++i) {
+      // FIXME: isEdgeForbidden is slow
+      if (!isEdgeForbidden(edges[i]) && !isVertexForbidden(neighbours[i])) {
+        auto const& n = neighbours[i];
+
+        // NOTE: _edges[i] stays intact after move
+        // and is reset to a nullptr. So if we crash
+        // here no mem-leaks. or undefined behavior
+        // Just make sure _edges is not used after
+        auto inserted =
+            source._vertices.emplace(n, FoundVertex(v, std::move(edges[i])));
+
+        // vertex was new
+        if (inserted.second) {
+          auto found = target._vertices.find(n);
+          if (found != target._vertices.end()) {
+            join = n;
+            return true;
+          }
+          newFrontier.emplace_back(n);
+        }
+      }
+    }
+  }
+  source._frontier = newFrontier;
+  return false;
+}
+
+void ConstantWeightKShortestPathsFinder::reconstructPath(const Ball& left, const Ball& right,
+                                                         const VertexRef& join,
+                                                         Path& result) {
+  result.clear();
+
+  result._vertices.emplace_back(join);
+  auto it = left._vertices.find(join);
+  VertexRef next;
+  while (it != left._vertices.end() && !it->second._startOrEnd) {
+    next = it->second._pred;
+    result._vertices.push_front(next);
+    result._edges.push_front(it->second._edge);
+    it = left._vertices.find(next);
+  }
+  it = right._vertices.find(join);
+  while (it != right._vertices.end() && !it->second._startOrEnd) {
+    next = it->second._pred;
+    result._vertices.emplace_back(next);
+    result._edges.emplace_back(it->second._edge);
+    it = right._vertices.find(next);
+  }
+
+  TRI_IF_FAILURE("TraversalOOMPath") {
+    THROW_ARANGO_EXCEPTION(TRI_ERROR_DEBUG);
+  }
+
+  //  _options.fetchVerticesCoordinator(result._vertices);
+}
+
+bool ConstantWeightKShortestPathsFinder::getNextPath(arangodb::graph::ShortestPathResult& path) {
+  bool available = false;
+  Path kShortestPath, tmpPath, candidate;
+  std::vector<VertexRef> forbiddenVertices;
+  std::vector<Edge> forbiddenEdges;
+  std::vector<Path> candidates;
+
+  if (_shortestPaths.empty()) {
+    available = computeShortestPath(_start, _end, {}, {}, kShortestPath);
+  } else {
+    auto& lastShortestPath = _shortestPaths.back();
+
+    for (size_t i = 0; i < lastShortestPath.length() - 1; ++i) {
+      auto& spur = lastShortestPath._vertices.at(i);
+
+      forbiddenVertices.clear();
+      forbiddenEdges.clear();
+
+      // Must not use vertices on the prefix
+      for (size_t j = 0; j < i; ++j) {
+        forbiddenVertices.emplace_back(lastShortestPath._vertices[j]);
+      }
+
+      for (auto const& p : _shortestPaths) {
+        bool eq = true;
+        for (size_t e = 0; e < i; ++e) {
+          if (!p._edges.at(e).equals(lastShortestPath._edges.at(e))) {
+            eq = false;
+            break;
+          }
+        }
+        if (eq && (i < p._edges.size())) {
+          forbiddenEdges.emplace_back(p._edges.at(i));
+        }
+      }
+
+      if (computeShortestPath(spur, _end, forbiddenVertices, forbiddenEdges, tmpPath)) {
+        candidate.clear();
+        candidate.append(lastShortestPath, 0, i);
+        candidate.append(tmpPath, 0, tmpPath.length() - 1);
+        candidates.emplace_back(candidate);
+      }
+    }
+
+    if (!candidates.empty()) {
+      std::sort(candidates.begin(), candidates.end(), [](const Path& p1, const Path& p2) {
+        return p1._vertices.size() < p2._vertices.size();
+      });
+
+      auto const& p = candidates.front();
+      kShortestPath.clear();
+      kShortestPath.append(p, 0, p.length() - 1);
+      available = true;
+    }
+  }
+
+  if (available) {
+    _shortestPaths.emplace_back(kShortestPath);
+
+    path.clear();
+    path._vertices = kShortestPath._vertices;
+    // WHUPS. Fix.
+    for (auto& e : kShortestPath._edges) {
+      path._edges.emplace_back(std::move(e));
+    }
+
+    _options.fetchVerticesCoordinator(path._vertices);
+
+    TRI_IF_FAILURE("TraversalOOMPath") {
+      THROW_ARANGO_EXCEPTION(TRI_ERROR_DEBUG);
+    }
+  }
+  _pathAvailable = available;
+  return available;
+}
+
+bool ConstantWeightKShortestPathsFinder::getNextPathAql(arangodb::velocypack::Builder& result) {
+  ShortestPathResult path;
+
+  if (getNextPath(path)) {
+    result.clear();
+    result.openObject();
+
+    result.add(VPackValue("edges"));
+    result.openArray();
+    for (auto const& it : path._edges) {
+      // TRI_ASSERT(it != nullptr);
+      _options.cache()->insertEdgeIntoResult(it, result);
+    }
+    result.close();  // Array
+
+    result.add(VPackValue("vertices"));
+    result.openArray();
+    for (auto const& it : path._vertices) {
+      _options.cache()->insertVertexIntoResult(it, result);
+    }
+    result.close();  // Array
+
+    result.close();  // Object
+    TRI_ASSERT(result.isClosed());
+    auto retresult = arangodb::aql::AqlValue(result.slice());
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ arangod/Graph/ConstantWeightKShortestPathsFinder.h
@@ -0,0 +1,202 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2019 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+// TODO: callback
+
+#ifndef ARANGODB_GRAPH_CONSTANT_WEIGHT_K_SHORTEST_PATHS_FINDER_H
+#define ARANGODB_GRAPH_CONSTANT_WEIGHT_K_SHORTEST_PATHS_FINDER_H 1
+
+#include "Aql/AqlValue.h"
+#include "Basics/VelocyPackHelper.h"
+#include "Graph/EdgeDocumentToken.h"
+#include "Graph/ShortestPathFinder.h"
+
+#include <velocypack/StringRef.h>
+
+namespace arangodb {
+
+namespace velocypack {
+class Slice;
+}
+
+namespace graph {
+
+struct ShortestPathOptions;
+
+// Class to find k shortest paths (with constant weight).
+// Given two vertices <start> and <end>, forms to balls around
+// <start> and <end> and expands them until they intersect in a
+// vertex
+//
+// Inherit from ShortestPathfinder to get destroyEngines and not copy it
+// again.
+// TODO: Traverser.h has destroyEngines as well (the code for the two functions
+//       is identical), refactor?
+class ConstantWeightKShortestPathsFinder : public ShortestPathFinder {
+ private:
+  // Mainly for readability
+  typedef arangodb::velocypack::StringRef VertexRef;
+  typedef arangodb::graph::EdgeDocumentToken Edge;
+  enum Direction { FORWARD, BACKWARD };
+
+  struct Path {
+    std::deque<VertexRef> _vertices;
+    std::deque<Edge> _edges;
+
+    void clear() {
+      _vertices.clear();
+      _edges.clear();
+    };
+    size_t length() const { return _vertices.size(); };
+    void append(const Path& p, size_t a, size_t b) {
+      if (a == b) {
+        _vertices.emplace_back(p._vertices.at(a));
+      } else if (this->length() == 0) {
+        for (size_t i = a; i < b; ++i) {
+          _vertices.emplace_back(p._vertices.at(i));
+          _edges.emplace_back(p._edges.at(i));
+        }
+        _vertices.emplace_back(p._vertices.at(b));
+      } else {
+        if (this->_vertices.back().equals(p._vertices.front())) {
+          _edges.emplace_back(p._edges.at(a));
+
+          for (size_t i = a + 1; i < b; ++i) {
+            _vertices.emplace_back(p._vertices.at(i));
+            _edges.emplace_back(p._edges.at(i));
+          }
+          _vertices.emplace_back(p._vertices.at(b));
+        } else {
+        }
+      }
+    }
+    void print(const std::string& pre) const {
+      LOG_DEVEL << pre << " vertices " << _vertices.size();
+      LOG_DEVEL << pre << " edges    " << _edges.size();
+      for (auto& v : _vertices) {
+        LOG_DEVEL << pre << "  v " << v.toString();
+      }
+    }
+  };
+
+  struct FoundVertex {
+    bool _startOrEnd;
+    VertexRef const _pred;
+    Edge _edge;
+
+    FoundVertex(VertexRef const& pred, Edge&& edge)
+        : _startOrEnd(false), _pred(pred), _edge(std::move(edge)){};
+    FoundVertex(bool startOrEnd)  // _npaths is 1 for start/end vertices
+        : _startOrEnd(startOrEnd){};
+  };
+  typedef std::deque<VertexRef> Frontier;
+  typedef std::deque<VertexRef> Trace;
+
+  // Contains the vertices that were found while searching
+  // for a shortest path between start and end together with
+  // the number of paths leading to that vertex and information
+  // how to trace paths from the vertex from start/to end.
+  typedef std::unordered_map<VertexRef, FoundVertex> FoundVertices;
+
+  struct Ball {
+    VertexRef _centre;
+    Direction _direction;
+    FoundVertices _vertices;
+    Frontier _frontier;
+    Trace _trace;
+
+    Ball(void){};
+    Ball(const VertexRef& centre, Direction direction)
+        : _centre(centre),
+          _direction(direction),
+          _vertices({{centre, FoundVertex(true)}}),
+          _frontier({centre}){};
+
+    void clear() {
+      _vertices.clear();
+      _frontier.clear();
+      _trace.clear();
+    };
+    void setCentre(const VertexRef& centre) {
+      _centre = centre;
+      _frontier.emplace_back(centre);
+      _vertices.emplace(centre, FoundVertex(true));
+    }
+    void setDirection(Direction direction) { _direction = direction; }
+    void setup(const VertexRef& centre, Direction direction) {
+      clear();
+      setCentre(centre);
+      setDirection(direction);
+    }
+  };
+
+ public:
+  explicit ConstantWeightKShortestPathsFinder(ShortestPathOptions& options);
+  ~ConstantWeightKShortestPathsFinder();
+
+  // This is here because we inherit from ShortestPathFinder (to get the destroyEngines function)
+  // TODO: Remove
+  bool shortestPath(arangodb::velocypack::Slice const& start,
+                    arangodb::velocypack::Slice const& target,
+                    arangodb::graph::ShortestPathResult& result,
+                    std::function<void()> const& callback) {
+    TRI_ASSERT(true);
+  };
+
+  //
+  bool startKShortestPathsTraversal(arangodb::velocypack::Slice const& start,
+                                    arangodb::velocypack::Slice const& end);
+
+  // get the next available path as AQL value.
+  bool getNextPathAql(arangodb::velocypack::Builder& builder);
+  // get the next available path as a ShortestPathResult
+  bool getNextPath(arangodb::graph::ShortestPathResult& path);
+  bool isPathAvailable(void) { return _pathAvailable; };
+
+ private:
+  bool computeShortestPath(const VertexRef& start, const VertexRef& end,
+                           const std::vector<VertexRef>& forbiddenVertices,
+                           const std::vector<Edge>& forbiddenEdges, Path& result);
+  void reconstructPath(const Ball& left, const Ball& right,
+                       const VertexRef& join, Path& result);
+
+  void computeNeighbourhoodOfVertex(VertexRef vertex, Direction direction,
+                                    std::vector<VertexRef>& neighbours,
+                                    std::vector<Edge>& edges);
+
+  // returns the number of paths found
+  bool advanceFrontier(Ball& source, const Ball& target,
+                       const std::vector<VertexRef>& forbiddenVertices,
+                       const std::vector<Edge>& forbiddenEdges, VertexRef& join);
+
+ private:
+  bool _pathAvailable;
+
+  VertexRef _start;
+  VertexRef _end;
+  std::vector<Path> _shortestPaths;
+};
+
+}  // namespace graph
+}  // namespace arangodb
+#endif
