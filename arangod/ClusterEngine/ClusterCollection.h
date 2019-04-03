d2978fe4aee27bd863e89527fa4fa3f65404d8ff
jsteemann <jan@arangodb.com>
2019-04-03 18:21:57 +0200

    remove/fix parameters in crud methods

--- arangod/ClusterEngine/ClusterCollection.h
+++ arangod/ClusterEngine/ClusterCollection.h
@@ -146,25 +146,23 @@ class ClusterCollection final : public PhysicalCollection {
                 arangodb::ManagedDocumentResult& result, OperationOptions& options,
                 TRI_voc_tick_t& resultMarkerTick, bool lock,
                 TRI_voc_tick_t& revisionId, KeyLockInfo* /*keyLockInfo*/,
-                std::function<Result(void)> callbackDuringLock) override;
+                std::function<Result(void)> const& callbackDuringLock) override;
 
   Result update(arangodb::transaction::Methods* trx, arangodb::velocypack::Slice const newSlice,
                 ManagedDocumentResult& result, OperationOptions& options,
                 TRI_voc_tick_t& resultMarkerTick, bool lock, TRI_voc_rid_t& prevRev,
-                ManagedDocumentResult& previous,
-                std::function<Result(void)> callbackDuringLock) override;
+                ManagedDocumentResult& previous) override;
 
   Result replace(transaction::Methods* trx, arangodb::velocypack::Slice const newSlice,
                  ManagedDocumentResult& result, OperationOptions& options,
                  TRI_voc_tick_t& resultMarkerTick, bool lock,
-                 TRI_voc_rid_t& prevRev, ManagedDocumentResult& previous,
-                 std::function<Result(void)> callbackDuringLock) override;
+                 TRI_voc_rid_t& prevRev, ManagedDocumentResult& previous) override;
 
   Result remove(transaction::Methods& trx, velocypack::Slice slice,
                 ManagedDocumentResult& previous, OperationOptions& options,
                 TRI_voc_tick_t& resultMarkerTick, bool lock, TRI_voc_rid_t& prevRev,
                 TRI_voc_rid_t& revisionId, KeyLockInfo* keyLockInfo,
-                std::function<Result(void)> callbackDuringLock) override;
+                std::function<Result(void)> const& callbackDuringLock) override;
 
  protected:
   /// @brief Inject figures that are specific to StorageEngine
