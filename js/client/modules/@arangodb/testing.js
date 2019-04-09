afcc62a751d836d4e0a88ed08fa9b00d64a3e81e
Wilfried Goesgens <willi@arangodb.com>
2019-04-09 10:26:04 +0200

    Reduce test leftovers on disk
    - don't dump agency by default on all errors
    - don't write junit xml reports by default

--- js/client/modules/@arangodb/testing.js
+++ js/client/modules/@arangodb/testing.js
@@ -83,6 +83,7 @@ let optionsDocumentation = [
   '   - `configDir`: the directory containing the config files, defaults to',
   '                  etc/testing',
   '   - `writeXmlReport`:  Write junit xml report files',
+  '   - `dumpAgencyOnError`: if we should create an agency dump if an error occurs',
   '   - `prefix`:    prefix for the tests in the xml reports',
   '',
   '   - `disableMonitor`: if set to true on windows, procdump will not be attached.',
@@ -119,6 +120,7 @@ let optionsDocumentation = [
 ];
 
 const optionsDefaults = {
+  'dumpAgencyOnError': false,
   'agencySize': 3,
   'agencyWaitForSync': false,
   'agencySupervision': true,
@@ -172,7 +174,7 @@ const optionsDefaults = {
   'valgrindHosts': false,
   'verbose': false,
   'walFlushTimeout': 30000,
-  'writeXmlReport': true,
+  'writeXmlReport': false,
   'testFailureText': 'testfailures.txt',
   'testCase': undefined,
   'disableMonitor': false
