d2287b1b23589ba344ef2ad3f7a5a8f6b1b3a001
Michael Hackstein <michael@arangodb.com>
2019-04-11 17:33:26 +0200

    Bug fix/collect prefetch (#8611)
    * Allow Collect Executors to prefetch rows
    
    * Alphabthical odering of includes
    
    * API change to prefetch the number of rows in use.

--- tests/Aql/TestEmptyExecutorHelper.h
+++ tests/Aql/TestEmptyExecutorHelper.h
@@ -89,7 +89,12 @@ class TestEmptyExecutorHelper {
    */
   std::pair<ExecutionState, Stats> produceRow(OutputAqlItemRow& output);
 
-  inline size_t numberOfRowsInFlight() const { return 0; }
+  inline std::pair<ExecutionState, size_t> expectedNumberOfRows(size_t) const {
+    TRI_ASSERT(false);
+    THROW_ARANGO_EXCEPTION_MESSAGE(
+        TRI_ERROR_INTERNAL,
+        "Logic_error, prefetching number fo rows not supported");
+  }
 
  public:
   Infos& _infos;
