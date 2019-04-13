740e3c85fb5693c0e2a3e17f944331c93cfab28f
jsteemann <jan@arangodb.com>
2019-04-13 23:01:54 +0200

    accidentially committed generated files

--- arangod/Aql/grammar.h
+++ arangod/Aql/grammar.h
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.3.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -29,6 +30,9 @@
 
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
+
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
 
 #ifndef YY_AQL_AQL_GRAMMAR_HPP_INCLUDED
 # define YY_AQL_AQL_GRAMMAR_HPP_INCLUDED
@@ -126,7 +130,7 @@ extern int Aqldebug;
 
 union YYSTYPE
 {
-#line 35 "Aql/grammar.y" /* yacc.c:1909  */
+#line 35 "Aql/grammar.y" /* yacc.c:1921  */
 
   arangodb::aql::AstNode*  node;
   struct {
@@ -136,7 +140,7 @@ union YYSTYPE
   bool                     boolval;
   int64_t                  intval;
 
-#line 140 "Aql/grammar.hpp" /* yacc.c:1909  */
+#line 144 "Aql/grammar.hpp" /* yacc.c:1921  */
 };
 
 typedef union YYSTYPE YYSTYPE;
