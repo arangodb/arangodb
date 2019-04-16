d734e64743773b7ce15565b702661ec8fcdb0345
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-04-16 09:48:38 +0200

    add more foxx tests

--- tests/js/common/test-data/apps/server-security/index.js
+++ tests/js/common/test-data/apps/server-security/index.js
@@ -10,3 +10,54 @@ router.get('/test', function (req, res) {
 router.get('/pid', function (req, res) {
   res.json(internal.getPid());
 });
+
+router.get('/passwd', function (req, res) {
+  res.json(require('fs').read('/etc/passwd'));
+});
+
+router.get('/dl-heise', function (req, res) {
+  res.json(internal.download("https://heise.de:443"));
+});
+
+router.get('/test-port', function (req, res) {
+  res.json(internal.testPort("tcp://localhost:111"));
+});
+
+router.get('/get-tmp-path', function (req, res) {
+  res.json(require('fs').getTempPath());
+});
+
+router.get('/get-tmp-file', function (req, res) {
+  res.json(require('fs').getTempFile());
+});
+
+router.get('/write-tmp-file', function (req, res) {
+  const fs = require('fs');
+  let file = fs.getTempFile();
+  fs.write(file,"ULF");
+  res.json(true);
+});
+
+router.get('/process-statistics', function (req, res) {
+  res.json(internal.processStatistics());
+});
+
+router.get('/execute-external', function (req, res) {
+  res.json(internal.executeExternal('/bin/ls'));
+});
+
+router.get('/environment-variables-get-path', function (req, res) {
+  let env = require('process').env;
+  res.json(env['PATH']);
+});
+
+router.get('/environment-variables-set-path', function (req, res) {
+  let env = require('process').env;
+  env['PATH'] = 'der reiher';
+  res.json(true);
+});
+
+router.get('/startup-options-log-file', function (req, res) {
+  res.json(internal.options()['log.file']);
+});
+
--- tests/js/server/permissions/test-foxx-service-access.js
+++ tests/js/server/permissions/test-foxx-service-access.js
@@ -11,7 +11,7 @@ if (getOptions === true) {
   //users.grantDatabase("test_ro", "_system", "ro");
 
   return {
-    'server.harden': 'false',
+    'server.harden': 'true',
     'server.authentication': 'true',
     'javascript.harden' : 'true',
     'javascript.files-black-list': [
@@ -20,6 +20,8 @@ if (getOptions === true) {
     'javascript.endpoints-white-list' : [
       'ssl://arangodb.com:443'
     ],
+    'javascript.environment-variables-black-list': 'PATH',
+    'javascript.startup-options-black-list': 'point|log',
   };
 }
 
@@ -40,14 +42,6 @@ const ArangoCollection = arangodb.ArangoCollection;
 const basePath = fs.makeAbsolute(fs.join(require('internal').pathForTesting('common'), 'test-data', 'apps'));
 
 const print = internal.print;
-
-
-
-
-
-
-
-
 
 function testSuite() {
   const foxxApp = fs.join(basePath, 'server-security');
@@ -88,6 +82,8 @@ function testSuite() {
       }
     },
 
+    // routes are defined in:
+    // js/common/test-data/apps/server-security/index.js
 
     testServiceInstalled : function() {
       const checksum = db._query(aql`
@@ -100,20 +96,119 @@ function testSuite() {
     },
 
     testTest : function() {
-          const url = endpoint + mount + "/test";
-          const res = download(url);
-          assertTrue(res.body === "true");
+      const url = endpoint + mount + "/test";
+      const res = download(url);
+      assertTrue(res.body === "true");
     },
 
     testPid : function() {
-          const url = endpoint + mount + "/pid";
-          const res = download(url);
-          assertTrue(res.code === 403);
-          assertTrue(res.message === "Forbidden");
+      const url = endpoint + mount + "/pid";
+      const res = download(url);
+      assertEqual(403, res.code);
+      assertEqual("Forbidden", res.message);
     },
+
+    testPasswd : function() {
+      const url = endpoint + mount + "/passwd";
+      const res = download(url);
+      assertEqual(403, res.code);
+      assertEqual("Forbidden", res.message);
+    },
+
+    testDlHeise : function() {
+      const url = endpoint + mount + "/dl-heise";
+      const res = download(url);
+      assertEqual(403, res.code);
+      assertEqual("Forbidden", res.message);
+    },
+
+    testTestPort : function() {
+      const url = endpoint + mount + "/test-port";
+      const res = download(url);
+      assertEqual(403, res.code);
+      assertEqual("Forbidden", res.message);
+    },
+
+    testGetTmpPath : function() {
+      const url = endpoint + mount + "/get-tmp-path";
+      const res = download(url);
+      assertEqual(200, res.code);
+      let body = JSON.parse(res.body)
+      assertTrue(body.startsWith("/tmp"));
+    },
+
+    testGetTmpFile : function() {
+      const url = endpoint + mount + "/get-tmp-file";
+      const res = download(url);
+      assertEqual(200, res.code);
+      let body = JSON.parse(res.body)
+      assertTrue(body.startsWith("/tmp"));
+    },
+
+    testWriteTmpFile : function() {
+      const url = endpoint + mount + "/write-tmp-file";
+      const res = download(url);
+      assertEqual(200, res.code);
+    },
+
+    testProcessStatistics : function() {
+      const url = endpoint + mount + "/process-statistics";
+      const res = download(url);
+      assertEqual(403, res.code);
+    },
+
+    testExecuteExternal : function() {
+      const url = endpoint + mount + "/execute-external";
+      const res = download(url);
+      assertEqual(403, res.code);
+    },
+
+    testPath : function() {
+      { // read
+        const url = endpoint + mount + "/environment-variables-get-path";
+        const res = download(url);
+        assertEqual(204, res.code)
+        assertEqual("undefined", res.body)
+      }
+      { // modify
+        const url = endpoint + mount + "/environment-variables-set-path";
+        const res = download(url);
+        assertEqual(200, res.code)
+        assertEqual("true", res.body)
+      }
+      { // read
+        const url = endpoint + mount + "/environment-variables-get-path";
+        const res = download(url);
+        assertEqual(204, res.code)
+        assertEqual("undefined", res.body)
+      }
+    },
+
+    testStartupOptions : function() {
+      const url = endpoint + mount + "/startup-options-log-file";
+      const res = download(url);
+      assertEqual(204, res.code)
+      assertEqual("undefined", res.body)
+    },
+
+    //testTemplate : function() {
+    //  return
+    //  const url = endpoint + mount + "/bla";
+    //  const res = download(url);
+    //  print("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUULLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLFF")
+    //  assertEqual(200,200)
+    //  assertEqual(403,403)
+    //  print(res)
+    //  if(res.code >= 200 && res.code < 300) {
+    //    let body = JSON.parse(res.body)
+    //    print(body)
+    //  }
+    //},
+
 
   };
 }
+
 
 jsunity.run(testSuite);
 return jsunity.done();
