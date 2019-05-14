0ce99bd09d50a9628f707cddfeaa847429057f2a
Lars Maier <lars@arangodb.com>
2019-05-14 15:38:56 +0200

    Fixed lost precondition when removing server. (#8988)

--- js/actions/api-cluster.js
+++ js/actions/api-cluster.js
@@ -84,11 +84,9 @@ actions.defineHttp({
     let msg = "";
     let used = [];
     while (++count <= 60) {
-      let preconditions = {};
-      preconditions['/arango/Supervision/Health/' + serverId + '/Status'] = {'old': 'FAILED'};
       // need to make sure it is not responsible for anything
       used = [];
-      preconditions = reducePlanServers(function (data, agencyKey, servers) {
+      let preconditions = reducePlanServers(function (data, agencyKey, servers) {
         data[agencyKey] = {'old': servers};
         if (servers.indexOf(serverId) !== -1) {
           used.push(agencyKey);
@@ -103,6 +101,7 @@ actions.defineHttp({
         return data;
       }, preconditions);
 
+      preconditions['/arango/Supervision/Health/' + serverId + '/Status'] = {'old': 'FAILED'};
       preconditions["/arango/Supervision/DBServers/" + serverId]
         = { "oldEmpty": true };
 
@@ -111,6 +110,7 @@ actions.defineHttp({
         operations['/arango/Plan/Coordinators/' + serverId] = {'op': 'delete'};
         operations['/arango/Plan/DBServers/' + serverId] = {'op': 'delete'};
         operations['/arango/Current/ServersRegistered/' + serverId] = {'op': 'delete'};
+        operations['/arango/Current/DBServers/' + serverId] = {'op': 'delete'};
         operations['/arango/Supervision/Health/' + serverId] = {'op': 'delete'};
         operations['/arango/Target/MapUniqueToShortID/' + serverId] = {'op': 'delete'};
 
