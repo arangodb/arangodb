6b262bfdbf29d07e298c45f6177dfc66ef5f13ea
Andrey Abramov <gnusmas@kvolitek.ru>
2019-05-16 00:44:05 +0300

    Update arangod/IResearch/IResearchAnalyzerFeature.cpp
    Co-Authored-By: Jan <jsteemann@users.noreply.github.com>

--- arangod/IResearch/IResearchAnalyzerFeature.cpp
+++ arangod/IResearch/IResearchAnalyzerFeature.cpp
@@ -1234,7 +1234,7 @@ IResearchAnalyzerFeature::AnalyzerPool::ptr IResearchAnalyzerFeature::get( // fi
                 << name << "'";
 
             // this should never happen, treat as an assertion failure
-            THROW_ARANGO_EXCEPTION(TRI_ERROR_INTERNAL);
+            THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_INTERNAL, "failed to create arangosearch static analyzer instance");
           }
 
           analyzers.emplace(irs::make_hashed_ref(name, std::hash<irs::string_ref>()), pool);
