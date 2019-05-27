f5528985fe9bc0a60e238d06b6cc683949ca86c0
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-05-27 10:02:07 +0200

    add tests and lint

--- tests/js/client/shell/shell-analyzer-rest-api.js
+++ tests/js/client/shell/shell-analyzer-rest-api.js
@@ -8,9 +8,9 @@ const error = internal.errors;
 const print = internal.print;
 
 function testSuite() {
-  let endpoint = arango.getEndpoint();
-  let db = require("@arangodb").db;
-  const name = "vimoji"
+  const endpoint = arango.getEndpoint();
+  const db = require("@arangodb").db;
+  const name = "vimoji";
 
   return {
     setUp: function() {},
@@ -88,6 +88,18 @@ function testSuite() {
       assertEqual(result.code, 201);
     },
 
+    testAnalyzerCreateTextMissingName : function() {
+      let body = JSON.stringify({
+        type : "text",
+        properties : { locale: "en.UTF-8", ignored_words: [ ] },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertTrue(result.error);
+      assertEqual(result.code, 400);
+      assertEqual(result.errorNum, error.ERROR_HTTP_BAD_PARAMETER.code);
+    },
+
     testAnalyzerCreateTextMissingProperties1 : function() {
       let body = JSON.stringify({
         type : "text",
@@ -114,11 +126,24 @@ function testSuite() {
       assertEqual(result.errorNum, error.ERROR_HTTP_BAD_PARAMETER.code);
     },
 
+    testAnalyzerCreateTextInvalidProperties1 : function() {
+      let body = JSON.stringify({
+        type : "text",
+        name : name,
+        properties : { locale: "en.UTF-8" , ignored_words: { invalid : "property" } },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertTrue(result.error);
+      assertEqual(result.code, 400);
+      assertEqual(result.errorNum, error.ERROR_HTTP_BAD_PARAMETER.code);
+    },
+
     testAnalyzerCreateDelimited : function() {
       let body = JSON.stringify({
         type : "delimited",
         name : name,
-        properties : { delimiter : "❤" } ,
+        properties : { delimiter : "❤" } , // .hsv - heart separated value list :)
       });
 
       let result = arango.POST_RAW("/_api/analyzer", body);
@@ -126,18 +151,18 @@ function testSuite() {
       assertEqual(result.code, 201);
     },
 
-    // //should work - fix or update docs
-    // testAnalyzerCreateDelimited : function() {
-    //   let body = JSON.stringify({
-    //     type : "delimited",
-    //     name : name,
-    //     properties : "❤" ,
-    //   });
-
-    //   let result = arango.POST_RAW("/_api/analyzer", body);
-    //   assertFalse(result.error);
-    //   assertEqual(result.code, 201);
-    // },
+//    // should work - fix or update docs
+//    testAnalyzerCreateDelimited : function() {
+//      let body = JSON.stringify({
+//        type : "delimited",
+//        name : name,
+//        properties : "❤" , // TODO/FIXME -- doc says a string as argument is possible - this is not true
+//      });
+//
+//      let result = arango.POST_RAW("/_api/analyzer", body);
+//      assertFalse(result.error);
+//      assertEqual(result.code, 201);
+//    },
 
     testAnalyzerCreateNgram : function() {
       let body = JSON.stringify({
@@ -151,19 +176,22 @@ function testSuite() {
       assertEqual(result.code, 201);
     },
 
-    // //should work
-    // testAnalyzerDifferentDB : function() {
-    //   let rv = internal.db._createDatabase("ulf");
-    //   let body = JSON.stringify({
-    //     type : "identity",
-    //     name : name,
-    //   });
-
-    //   let result = arango.POST_RAW("_db/ulf/_api/analyzer", body);
-    //   assertFalse(result.error);
-    //   assertEqual(result.code, 201);
-    //   internal.db._dropDatabase("ulf");
-    // },
+//    // should work
+//    testAnalyzerDifferentDB : function() {
+//      let rv = db._createDatabase("ulf");
+//      let body = JSON.stringify({
+//        type : "identity",
+//        name : name,
+//      });
+//
+//      let result = arango.POST_RAW("_db/ulf/_api/analyzer", body);
+//      print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"); // TODO - REMOVE
+//      print(internal.db._databases());        // TODO - REMOVE
+//      print(result);                          // TODO - REMOVE
+//      assertFalse(result.error);
+//      assertEqual(result.code, 201);
+//      db._dropDatabase("ulf");
+//    },
 
     testAnalyzerCreateMany : function() {
       // should be last test as we do no clean-up
@@ -181,11 +209,15 @@ function testSuite() {
       }
 
       let res = arango.GET("/_api/analyzer");
-      assertEqual(res.result.length, num + 1) // vimoji(0..9) + identity
+      assertEqual(res.result.length, num + 1); // vimoji(0..9) + identity
+
+      for (var j = 0; j < num; j++) {
+        arango.DELETE("/_api/analyzer/" + name + j);
+      }
     },
 
     testAnalyzerRemoveWhileUsed : function() {
-      //create
+      // -- create analyzer
       let body = JSON.stringify({
         type : "text",
         name : name,
@@ -196,19 +228,68 @@ function testSuite() {
       assertFalse(result.error);
       assertEqual(result.code, 201);
 
+      // -- put analyzer to use
       let query = `LET X=SLEEP(10) RETURN TOKENS('a quick brown fox jumps over the lazy dog', '_system::${name}')`;
       body = JSON.stringify({
         query : query,
       });
 
-      // create an async request
+      // create an async request, that uses analyzer for 10 seconds
       let headers = {"x-arango-async" : "store"};
       result = arango.POST_RAW("/_api/cursor", body, headers );
-      assertEqual(result.code, 202); //accepted
+      assertEqual(result.code, 202); //accepted - now running for 10
+
+      // -- test behaviour
+      // delete without force - must fail as analyzer should be in use
       result = arango.DELETE("/_api/analyzer/" + name);
-      assertTrue(result.error); //shold be used
+      assertTrue(result.error);
+      assertEqual(result.errorNum, error.ERROR_ARANGO_CONFLICT.code);
+
+      // delete with force - must succeed
+      result = arango.DELETE("/_api/analyzer/" + name + "?force=true");
+      assertFalse(result.error);
+    },
+
+    testAnalyzerLinks : function() {
+      let body = JSON.stringify({
+        name : "text_en",
+        type : "text",
+        properties : { locale: "en.UTF-8", ignored_words: [ ] },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertFalse(result.error);
+
+      let col = db._create("ulfColTestLinks");
+      let view = db._createView("ulfViewTestLinks", "arangosearch", {});
+      var properties = {
+        links : {
+          "ulfColTestLinks" : {
+            includeAllFields : true,
+            storeValues : "id",
+            fields : {
+              text : { analyzers : ["text_en"] }
+            }
+          }
+        }
+      };
+      view.properties(properties);
+
+      result = arango.DELETE("/_api/analyzer/text_en");
+      print(result);
+
+      assertTrue(result.error);
+      assertEqual(result.code, 409); // can not delete -- referencded by link
+      assertEqual(result.errorNum, error.ERROR_ARANGO_CONFLICT.code);
+
+      // delete with force - must succeed
+      result = arango.DELETE("/_api/analyzer/text_en?force=true");
+      assertFalse(result.error);
+
+      db._drop("ulfColTestLinks");
     }
 
+    // TODO permissions - lets test them in the other test
 
   }; // return
 } // end of suite
