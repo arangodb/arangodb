a9a2f2069ebbbd57ff1c3d169654bae300dc5965
Kaveh Vahedipour <kaveh@arangodb.com>
2019-05-27 12:13:04 +0200

    weird compile error

--- lib/Rest/CommonDefines.h
+++ lib/Rest/CommonDefines.h
@@ -78,7 +78,7 @@ inline const char* requestToString(RequestType requestType) {
 }
 
 inline std::ostream& operator<<(std::ostream& ostream, RequestType requestType) {
-  return ostream << requestToString(requestType);
+  return ostream << std::string(requestToString(requestType));
 }
 
 enum class ContentType {
@@ -253,7 +253,7 @@ inline const char* responseToString(ResponseCode responseCode) {
 }
 
 inline std::ostream& operator<<(std::ostream& ostream, ResponseCode responseCode) {
-  return ostream << responseToString(responseCode);
+  return ostream << std::string(responseToString(responseCode));
 }
 }  // namespace rest
 }  // namespace arangodb
