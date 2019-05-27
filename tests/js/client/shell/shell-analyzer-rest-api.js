242542276bd5a94c8a8406cd295243e443e713b3
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-05-27 08:35:08 +0200

    add more tests

--- tests/js/client/shell/shell-analyzer-rest-api.js
+++ tests/js/client/shell/shell-analyzer-rest-api.js
@@ -7,8 +7,6 @@ const internal = require('internal');
 const error = internal.errors;
 const print = internal.print;
 
-//arango.reconnect(endpoint, db._name(), "test_rw", "testi");
-
 function testSuite() {
   let endpoint = arango.getEndpoint();
   let db = require("@arangodb").db;
@@ -36,7 +34,7 @@ function testSuite() {
     },
 
 
-    testAnalyzerCreate : function() {
+    testAnalyzerCreateIdentity : function() {
       let body = JSON.stringify({
         type : "identity",
         name : name,
@@ -48,7 +46,7 @@ function testSuite() {
       assertEqual(result.code, 201);
     },
 
-    testAnalyzerCreateRemove : function() {
+    testAnalyzerCreateRemoveIdentiy : function() {
       let body = JSON.stringify({
         type : "identity",
         name : name,
@@ -65,7 +63,7 @@ function testSuite() {
       assertEqual(result.code, 404); // already deleted
     },
 
-    testAnalyzerCreateDouble : function() {
+    testAnalyzerCreateDoubleIdentity : function() {
       let body = JSON.stringify({
         type : "identity",
         name : name,
@@ -86,11 +84,86 @@ function testSuite() {
       });
 
       let result = arango.POST_RAW("/_api/analyzer", body);
-      print(result)
-
       assertFalse(result.error);
       assertEqual(result.code, 201);
     },
+
+    testAnalyzerCreateTextMissingProperties1 : function() {
+      let body = JSON.stringify({
+        type : "text",
+        name : name,
+        //properties : { locale: "en.UTF-8", ignored_words: [ ] },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertTrue(result.error);
+      assertEqual(result.code, 400);
+      assertEqual(result.errorNum, error.ERROR_HTTP_BAD_PARAMETER.code);
+    },
+
+    testAnalyzerCreateTextMissingProperties2 : function() {
+      let body = JSON.stringify({
+        type : "text",
+        name : name,
+        properties : { locale: "en.UTF-8" /*, ignored_words: [ ]*/ },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertTrue(result.error);
+      assertEqual(result.code, 400);
+      assertEqual(result.errorNum, error.ERROR_HTTP_BAD_PARAMETER.code);
+    },
+
+    testAnalyzerCreateDelimited : function() {
+      let body = JSON.stringify({
+        type : "delimited",
+        name : name,
+        properties : { delimiter : "❤" } ,
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertFalse(result.error);
+      assertEqual(result.code, 201);
+    },
+
+    // //should work - fix or update docs
+    // testAnalyzerCreateDelimited : function() {
+    //   let body = JSON.stringify({
+    //     type : "delimited",
+    //     name : name,
+    //     properties : "❤" ,
+    //   });
+
+    //   let result = arango.POST_RAW("/_api/analyzer", body);
+    //   assertFalse(result.error);
+    //   assertEqual(result.code, 201);
+    // },
+
+    testAnalyzerCreateNgram : function() {
+      let body = JSON.stringify({
+        type : "ngram",
+        name : name,
+        properties : { min : 3, max : 4, preserveOriginal : true } ,
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertFalse(result.error);
+      assertEqual(result.code, 201);
+    },
+
+    // //should work
+    // testAnalyzerDifferentDB : function() {
+    //   let rv = internal.db._createDatabase("ulf");
+    //   let body = JSON.stringify({
+    //     type : "identity",
+    //     name : name,
+    //   });
+
+    //   let result = arango.POST_RAW("_db/ulf/_api/analyzer", body);
+    //   assertFalse(result.error);
+    //   assertEqual(result.code, 201);
+    //   internal.db._dropDatabase("ulf");
+    // },
 
     testAnalyzerCreateMany : function() {
       // should be last test as we do no clean-up
@@ -103,15 +176,40 @@ function testSuite() {
         });
 
         let result = arango.POST_RAW("/_api/analyzer", body);
-        print(result)
-
         assertFalse(result.error);
         assertEqual(result.code, 201);
       }
 
       let res = arango.GET("/_api/analyzer");
       assertEqual(res.result.length, num + 1) // vimoji(0..9) + identity
+    },
+
+    testAnalyzerRemoveWhileUsed : function() {
+      //create
+      let body = JSON.stringify({
+        type : "text",
+        name : name,
+        properties : { locale: "en.UTF-8", ignored_words: [ ] },
+      });
+
+      let result = arango.POST_RAW("/_api/analyzer", body);
+      assertFalse(result.error);
+      assertEqual(result.code, 201);
+
+      let query = `LET X=SLEEP(10) RETURN TOKENS('a quick brown fox jumps over the lazy dog', '_system::${name}')`;
+      body = JSON.stringify({
+        query : query,
+      });
+
+      // create an async request
+      let headers = {"x-arango-async" : "store"};
+      result = arango.POST_RAW("/_api/cursor", body, headers );
+      assertEqual(result.code, 202); //accepted
+      result = arango.DELETE("/_api/analyzer/" + name);
+      assertTrue(result.error); //shold be used
     }
+
+
   }; // return
 } // end of suite
 jsunity.run(testSuite);
