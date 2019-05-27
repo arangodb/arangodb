47a2de302061761c9329460a9e0826f9af91ba10
Simon Grätzer <simon@graetzer.org>
2019-05-27 16:36:04 +0200

    port Pregel segmented buffers

--- arangod/Pregel/Worker.cpp
+++ arangod/Pregel/Worker.cpp
@@ -316,36 +316,37 @@ void Worker<V, E, M>::_startProcessing() {
   Scheduler* scheduler = SchedulerFeature::SCHEDULER;
 
   size_t total = _graphStore->localVertexCount();
-  size_t delta = total / _config.parallelism();
-  size_t start = 0, end = delta;
-  if (delta < 100 || total < 100) {
-    _runningThreads = 1;
-    end = total;
+  size_t numSegments = _graphStore->numberVertexSegments();
+
+  if (total > 100000) {
+    _runningThreads = std::min<size_t>(_config.parallelism(), numSegments);
   } else {
-    _runningThreads = total / delta;  // rounds-up unsigned integers
+    _runningThreads = 1;
   }
-  size_t i = 0;
-  do {
-    scheduler->queue(RequestLane::INTERNAL_LOW, [this, start, end, i] {
+  TRI_ASSERT(_runningThreads >= 1);
+  TRI_ASSERT(_runningThreads <= _config.parallelism());
+  size_t numT = _runningThreads;
+  
+  for (size_t i = 0; i < numT; i++) {
+    scheduler->queue(RequestLane::INTERNAL_LOW, [this, i, numT, numSegments] {
       if (_state != WorkerState::COMPUTING) {
         LOG_TOPIC("f0e3d", WARN, Logger::PREGEL) << "Execution aborted prematurely.";
         return;
       }
-      auto vertices = _graphStore->vertexIterator(start, end);
+      size_t startI = i * (numSegments / numT);
+      size_t endI = (i+1) * (numSegments / numT);
+      TRI_ASSERT(endI <= numSegments);
+      
+      auto vertices = _graphStore->vertexIterator(startI, endI);
       // should work like a join operation
       if (_processVertices(i, vertices) && _state == WorkerState::COMPUTING) {
         _finishedProcessing();  // last thread turns the lights out
       }
     });
-    start = end;
-    end = end + delta;
-    if (total < end + delta) {  // swallow the rest
-      end = total;
-    }
-    i++;
-  } while (start != total);
+  }
+  
   // TRI_ASSERT(_runningThreads == i);
-  LOG_TOPIC("425c3", DEBUG, Logger::PREGEL) << "Using " << i << " Threads";
+  LOG_TOPIC("425c3", DEBUG, Logger::PREGEL) << "Using " << numT << " Threads";
 }
 
 template <typename V, typename E, typename M>
@@ -360,7 +361,7 @@ void Worker<V, E, M>::_initializeVertexContext(VertexContext<V, E, M>* ctx) {
 // internally called in a WORKER THREAD!!
 template <typename V, typename E, typename M>
 bool Worker<V, E, M>::_processVertices(size_t threadId,
-                                       RangeIterator<VertexEntry>& vertexIterator) {
+                                       RangeIterator<Vertex<V,E>>& vertexIterator) {
   double start = TRI_microtime();
 
   // thread local caches
@@ -388,7 +389,8 @@ bool Worker<V, E, M>::_processVertices(size_t threadId,
   }
 
   size_t activeCount = 0;
-  for (VertexEntry* vertexEntry : vertexIterator) {
+  for (; vertexIterator.hasMore(); ++vertexIterator) {
+    Vertex<V,E>* vertexEntry = *vertexIterator;
     MessageIterator<M> messages =
         _readCache->getMessages(vertexEntry->shard(), vertexEntry->key());
 
@@ -471,13 +473,14 @@ void Worker<V, E, M>::_finishedProcessing() {
     if (_config.lazyLoading()) {  // TODO how to improve this?
       // hack to determine newly added vertices
       size_t currentAVCount = _graphStore->localVertexCount();
-      auto currentVertices = _graphStore->vertexIterator();
-      for (VertexEntry* vertexEntry : currentVertices) {
+      auto it = _graphStore->vertexIterator();
+      for (; it.hasMore(); ++it) {
+        Vertex<V,E>* vertexEntry = *it;
         // reduces the containedMessageCount
         _readCache->erase(vertexEntry->shard(), vertexEntry->key());
       }
 
-      _readCache->forEach([this](PregelShard shard, PregelKey const& key, M const&) {
+      _readCache->forEach([this](PregelShard shard, VPackStringRef const& key, M const&) {
         _graphStore->loadDocument(&_config, shard, key);
       });
 
@@ -629,7 +632,8 @@ void Worker<V, E, M>::aqlResult(VPackBuilder& b, bool withId) const {
 
   b.openArray(/*unindexed*/true);
   auto it = _graphStore->vertexIterator();
-  for (VertexEntry const* vertexEntry : it) {
+  for (; it.hasMore(); ++it) {
+    Vertex<V,E> const* vertexEntry = *it;
     
     TRI_ASSERT(vertexEntry->shard() < _config.globalShardIDs().size());
     ShardID const& shardId = _config.globalShardIDs()[vertexEntry->shard()];
@@ -642,7 +646,7 @@ void Worker<V, E, M>::aqlResult(VPackBuilder& b, bool withId) const {
         tmp.clear();
         tmp.append(cname);
         tmp.push_back('/');
-        tmp.append(vertexEntry->key());
+        tmp.append(vertexEntry->key().data(), vertexEntry->key().size());
         b.add(StaticStrings::IdString, VPackValue(tmp));
       }
     }
@@ -651,9 +655,9 @@ void Worker<V, E, M>::aqlResult(VPackBuilder& b, bool withId) const {
                                                    vertexEntry->key().size(),
                                                    VPackValueType::String));
     
-    V* data = _graphStore->mutableVertexData(vertexEntry);
+    V const& data = vertexEntry->data();
     // bool store =
-    _graphStore->graphFormat()->buildVertexDocument(b, data, sizeof(V));
+    _graphStore->graphFormat()->buildVertexDocument(b, &data, sizeof(V));
     b.close();
   }
   b.close();
@@ -718,7 +722,8 @@ void Worker<V, E, M>::compensateStep(VPackSlice const& data) {
     vCompensate->_writeAggregators = _workerAggregators.get();
 
     size_t i = 0;
-    for (VertexEntry* vertexEntry : vertexIterator) {
+    for (; vertexIterator.hasMore(); ++vertexIterator) {
+      Vertex<V,E>* vertexEntry = *vertexIterator;
       vCompensate->_vertexEntry = vertexEntry;
       vCompensate->compensate(i > _preRecoveryTotal);
       i++;
--- arangod/Pregel/Worker.h
+++ arangod/Pregel/Worker.h
@@ -72,7 +72,6 @@ class OutCache;
 
 template <typename T>
 class RangeIterator;
-class VertexEntry;
 
 template <typename V, typename E, typename M>
 class VertexContext;
@@ -139,7 +138,7 @@ class Worker : public IWorker {
   void _initializeMessageCaches();
   void _initializeVertexContext(VertexContext<V, E, M>* ctx);
   void _startProcessing();
-  bool _processVertices(size_t threadId, RangeIterator<VertexEntry>& vertexIterator);
+  bool _processVertices(size_t threadId, RangeIterator<Vertex<V,E>>& vertexIterator);
   void _finishedProcessing();
   void _continueAsync();
   void _callConductor(std::string const& path, VPackBuilder const& message);
