e23d4151602bc9cc355fb4c355ce932c0d1ce543
Tobias Gödderz <tobias@arangodb.com>
2019-05-31 14:10:08 +0200

    Set ReturnExecutor::Properties::inputSizeRestrictsOutputSize to true

--- arangod/Aql/ReturnExecutor.h
+++ arangod/Aql/ReturnExecutor.h
@@ -79,8 +79,7 @@ class ReturnExecutor {
     // register 0. So it does not pass through, but copy one column into a new
     // block with only this column.
     static const bool allowsBlockPassthrough = false;
-    /* This could be set to true after some investigation/fixes */
-    static const bool inputSizeRestrictsOutputSize = false;
+    static const bool inputSizeRestrictsOutputSize = true;
   };
   using Fetcher = SingleRowFetcher<Properties::allowsBlockPassthrough>;
   using Infos = ReturnExecutorInfos;
@@ -124,11 +123,8 @@ class ReturnExecutor {
     return {state, stats};
   }
 
-  inline std::pair<ExecutionState, size_t> expectedNumberOfRows(size_t) const {
-    TRI_ASSERT(false);
-    THROW_ARANGO_EXCEPTION_MESSAGE(
-        TRI_ERROR_INTERNAL,
-        "Logic_error, prefetching number of rows not supported");
+  inline std::pair<ExecutionState, size_t> expectedNumberOfRows(size_t atMost) const {
+    return _fetcher.preFetchNumberOfRows(atMost);
   }
 
  private:
