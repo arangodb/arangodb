53ac3c03225a98eb6e337f0f3fb88bee7c4bc890
Andrey Abramov <andrey@arangodb.com>
2019-06-03 14:37:14 +0300

    backport fix from IResarch upstream

--- 3rdParty/iresearch/core/search/score_doc_iterators.cpp
+++ 3rdParty/iresearch/core/search/score_doc_iterators.cpp
@@ -35,6 +35,16 @@ basic_doc_iterator_base::basic_doc_iterator_base(const order::prepared& ord)
 }
 
 void basic_doc_iterator_base::prepare_score(order::prepared::scorers&& scorers) {
+  struct scorer_ref {
+    explicit scorer_ref(const std::pair<sort::scorer::ptr, size_t>& bucket) NOEXCEPT
+      : scorer(bucket.first.get()), offset(bucket.second) {
+      assert(scorer);
+    }
+
+    irs::sort::scorer* scorer;
+    size_t offset;
+  }; // scorer_ref
+
   scorers_ = std::move(scorers);
 
   switch (scorers_.size()) {
@@ -43,22 +53,35 @@ void basic_doc_iterator_base::prepare_score(order::prepared::scorers&& scorers) 
       doc_iterator_base::prepare_score([](byte_type*){ });
     } break;
     case 1: {
-      auto* scorer = scorers_[0].first.get();
-      assert(scorer);
-      doc_iterator_base::prepare_score([scorer](byte_type* score) {
-        scorer->score(score);
-      });
+      const scorer_ref ref(scorers_[0]);
+
+      if (ref.offset) {
+        doc_iterator_base::prepare_score([ref](byte_type* score) {
+          ref.scorer->score(score + ref.offset);
+        });
+      } else {
+        auto* scorer = ref.scorer;
+
+        doc_iterator_base::prepare_score([scorer](byte_type* score) {
+          scorer->score(score);
+        });
+      }
     } break;
     case 2: {
-      auto* first = scorers_[0].first.get();
-      assert(first);
-      auto* second = scorers_[1].first.get();
-      assert(second);
-      const auto second_offset = scorers_[1].second;
-      doc_iterator_base::prepare_score([first, second, second_offset](byte_type* score) {
-        first->score(score);
-        second->score(score + second_offset);
-      });
+      const scorer_ref first(scorers_[0]);
+      const scorer_ref second(scorers_[1]);
+
+      if (first.offset) {
+        doc_iterator_base::prepare_score([first, second](byte_type* score) {
+          first.scorer->score(score + first.offset);
+          second.scorer->score(score + second.offset);
+        });
+      } else {
+        doc_iterator_base::prepare_score([first, second](byte_type* score) {
+          first.scorer->score(score);
+          second.scorer->score(score + second.offset);
+        });
+      }
     } break;
     default: {
       doc_iterator_base::prepare_score([this](byte_type* score) {
