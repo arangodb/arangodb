1ac5e6a0a03866666371141a0facb44d00b75b50
Michael Hackstein <michael@arangodb.com>
2019-06-04 10:42:19 +0200

    Forward port of 3.4 bug-fix

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -2047,9 +2047,20 @@ Result ClusterInfo::createCollectionsCoordinator(std::string const& databaseName
     TRI_ASSERT(agencyCallbacks.size() == infos.size());
     for (size_t i = 0; i < infos.size(); ++i) {
       if (infos[i].state == ClusterCollectionCreationInfo::INIT) {
-        // This one has not responded, wait for it.
-        CONDITION_LOCKER(locker, agencyCallbacks[i]->_cv);
-        agencyCallbacks[i]->executeByCallbackOrTimeout(interval);
+        {
+          // This one has not responded, wait for it.
+          CONDITION_LOCKER(locker, agencyCallbacks[i]->_cv);
+          bool wokenUp = agencyCallbacks[i]->executeByCallbackOrTimeout(interval);
+          if (wokenUp) {
+            // We got woken up by waittime, not by  callback.
+            // Let us check if we skipped other callbacks as well
+            for (; i < infos.size(); ++i) {
+              if (infos[i].state == ClusterCollectionCreationInfo::INIT) {
+                agencyCallbacks[i]->refetchAndUpdate(true, false);
+              }
+            }
+          }
+        }
         break;
       }
     }
