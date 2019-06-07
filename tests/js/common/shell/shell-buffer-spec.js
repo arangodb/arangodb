6c84f16e2469a855998857664eecdf0a28ef0c15
Alan Plum <me@pluma.io>
2019-06-07 15:27:50 +0200

    Add tests for Buffer#new

--- tests/js/common/shell/shell-buffer-spec.js
+++ tests/js/common/shell/shell-buffer-spec.js
@@ -3,6 +3,54 @@
 const { expect } = require('chai');
 
 describe('Buffer', function () {
+  describe('new', function () {
+    it('should accept numbers', () => {
+      const buf = new Buffer(32);
+      buf.fill(0);
+      expect(buf.length).to.equal(32);
+    });
+    it('should accept a string', () => {
+      const value = "hello world";
+      const buf = new Buffer(value);
+      expect(buf.toString()).to.equal(value);
+    });
+    it('should accept a buffer and return a copy', () => {
+      const value = "hello world";
+      const buf0 = new Buffer(value);
+      const buf = new Buffer(buf0);
+      expect(buf.toString()).to.equal(value);
+      buf0.fill(0);
+      expect(buf.toString()).to.equal(value);
+    });
+    it('should accept a buffer with offset/length and return a slice', () => {
+      const value = "hello world";
+      const buf0 = new Buffer(value);
+      const buf = new Buffer(buf0, 3, 1);
+      expect(buf.length).to.equal(3);
+      expect(buf.toString()).to.equal(value.slice(1, 4));
+      buf.fill(0);
+      expect(buf0[0]).to.equal("hello world".charCodeAt(0));
+      expect(buf0[1]).to.equal(0);
+      expect(buf0[2]).to.equal(0);
+      expect(buf0[3]).to.equal(0);
+      expect(buf0[4]).to.equal("hello world".charCodeAt(4));
+    });
+    it('should accept an ArrayBuffer', () => {
+      const values = [4, 8, 15, 16, 23, 42];
+      const ab = new ArrayBuffer(values.length);
+      const dv = new DataView(ab);
+      for (let i = 0; i < values.length; i++) {
+        dv.setUint8(i, values[i]);
+      }
+      const buf = new Buffer(ab);
+      expect([...buf]).to.eql(values);
+    });
+    it('should accept an array of byte values', () => {
+      const values = [4, 8, 15, 16, 23, 42];
+      const buf = new Buffer(values);
+      expect([...buf]).to.eql(values);
+    });
+  });
   describe('from', function () {
     it('should not accept numbers', () => {
       try {
@@ -18,10 +66,12 @@ describe('Buffer', function () {
       const buf = Buffer.from(value);
       expect(buf.toString()).to.equal(value);
     });
-    it('should accept a buffer', () => {
+    it('should accept a buffer and return a copy', () => {
       const value = "hello world";
-      const buf0 = new Buffer(value);
+      const buf0 = Buffer.from(value);
       const buf = Buffer.from(buf0);
+      expect(buf.toString()).to.equal(value);
+      buf0.fill(0);
       expect(buf.toString()).to.equal(value);
     });
     it('should accept an ArrayBuffer', () => {
