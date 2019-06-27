083ae18fc2e00bfa722f0812e387cecedbdaeed3
Tobias Gödderz <tobias@arangodb.com>
2019-06-27 11:19:57 +0200

    Avoid nullptr deref while logging

--- arangod/RestServer/FlushFeature.cpp
+++ arangod/RestServer/FlushFeature.cpp
@@ -737,7 +737,11 @@ arangodb::Result FlushFeature::releaseUnusedTicks() {
       }
     }
 
-    LOG_TOPIC("hunde", INFO, Logger::FIXME) << "Minimium subscription " << minTick << " came from " << minSubscr->_databaseId;
+    LOG_TOPIC("hunde", INFO, Logger::FIXME) << "Minimium subscription " << minTick << " came from " << (
+      minSubscr == nullptr
+      ? std::string{"engine->currentTick()"}
+      : std::string{"db#"} + std::to_string(minSubscr->_databaseId)
+    );
   }
 
   engine->waitForSyncTick(minTick);
