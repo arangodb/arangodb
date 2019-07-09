dcb70e22d3f7362229a94d5b7e91aa2699853c3f
Michael Hackstein <michael@arangodb.com>
2019-07-09 16:23:57 +0200

    Fixed JSLINT

--- tests/js/common/shell/shell-collection.js
+++ tests/js/common/shell/shell-collection.js
@@ -158,7 +158,7 @@ function CollectionSuiteErrorHandling () {
         assertTrue(false);
       }
     }
-  }
+  };
 }
 
 ////////////////////////////////////////////////////////////////////////////////
