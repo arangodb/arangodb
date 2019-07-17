ef3a4a200eb933c81c7d1f3ddf64a6ef0b8a071a
Max Neunhoeffer <max@arangodb.com>
2019-07-17 10:51:50 +0200

    Sort out getConnectedClient calls.

--- arangosh/Restore/RestoreFeature.cpp
+++ arangosh/Restore/RestoreFeature.cpp
@@ -1465,7 +1465,7 @@ void RestoreFeature::start() {
       _directory = std::make_unique<ManagedDirectory>(basics::FileUtils::buildFilename(_options.inputPath, db), false, false);
 
       result = _clientManager.getConnectedClient(httpClient, _options.force,
-                                                 false, !_options.createDatabase);
+                                                 false, !_options.createDatabase, false);
       if (result.is(TRI_ERROR_SIMPLE_CLIENT_COULD_NOT_CONNECT)) {
         LOG_TOPIC("3e715", FATAL, Logger::RESTORE)
             << "cannot create server connection, giving up!";
@@ -1489,7 +1489,7 @@ void RestoreFeature::start() {
           client->setDatabaseName(db);
 
           // re-check connection and version
-          result = _clientManager.getConnectedClient(httpClient, _options.force, false, true);
+          result = _clientManager.getConnectedClient(httpClient, _options.force, false, true, false);
         } else {
           LOG_TOPIC("be594", WARN, Logger::RESTORE) << "Database '" << db << "' does not exist on target endpoint. In order to create this database along with the restore, please use the --create-database option";
         }
