1c408dc2fb93a5d7c94236aafdc6c1a53624bbe0
Wilfried Goesgens <willi@arangodb.com>
2019-07-18 15:41:11 +0200

    connect the mocha testresults if we run it in an extra arangosh instance (#9485)
    * connect the mocha testresults if we run it in an extra arangosh instance
    
    * fix status handling, lint

--- js/common/modules/@arangodb/testrunner.js
+++ js/common/modules/@arangodb/testrunner.js
@@ -7,28 +7,36 @@ var runTest = require('jsunity').runTest,
   _ = require('lodash'),
   internal = require('internal');
 
+const deflRes = {
+  failed: 0,
+  status: true,
+  duration: 0,
+  total: 0,
+  totalSetUp: 0,
+  totalTearDown: 0
+};
+const statusKeys = [
+  'failed',
+  'status',
+  'duration',
+  'total',
+  'totalSetUp',
+  'totalTearDown'
+];
+
 // //////////////////////////////////////////////////////////////////////////////
   // / @brief runs all jsunity tests
   // //////////////////////////////////////////////////////////////////////////////
 
-function runJSUnityTests (tests) {
-  let env = require('internal').env;
-  let instanceinfo;
-  if (!env.hasOwnProperty('INSTANCEINFO')) {
-    throw new Error('env.INSTANCEINFO was not set by caller!');
-  }
-  instanceinfo = JSON.parse(env.INSTANCEINFO);
-  if (!instanceinfo) {
-    throw new Error('env.INSTANCEINFO was not set by caller!');
-  }
-  var result = true;
-  var allResults = [];
+function runJSUnityTests (tests, instanceinfo) {
+  let env = internal.env;
+  var allResults = _.clone(deflRes);
   var failed = [];
   var res;
 
   // find out whether we're on server or client...
   var runenvironment = 'arangod';
-  if (typeof (require('internal').arango) === 'object') {
+  if (typeof (internal.arango) === 'object') {
     runenvironment = 'arangosh';
   }
 
@@ -38,17 +46,32 @@ function runJSUnityTests (tests) {
   }
 
   _.each(tests, function (file) {
-    if (result) {
+    if (allResults.status) {
       print('\n' + Date() + ' ' + runenvironment + ": Running JSUnity test from file '" + file + "'");
     } else {
       print('\n' + Date() + ' ' + runenvironment +
-        ": Skipping JSUnity test from file '" + file + "' due to previous errors");
+            ": Skipping JSUnity test from file '" + file + "' due to previous errors");
+      if (!allResults.hasOwnProperty('ALLTESTS')) {
+        allResults['ALLTESTS'] = {
+          status: false,
+          skipped: true,
+          message: 'test failed, skipping others: '
+        };
+      }
+      allResults.ALLTESTS.message += file + ', ';
+      return;
     }
 
     try {
       res = runTest(file, true, unitTestFilter);
-      allResults.push(res);
-      result = result && res.status;
+      allResults[file] = res;
+      allResults.duration += res.duration;
+      allResults.total +=1;
+      allResults.totalSetUp += res.totalSetup;
+      allResults.totalTearDown += res.totalTearDown;
+
+      allResults.status = allResults.status && res.status;
+
       if (!res.status) {
         failed.push(file);
       }
@@ -63,17 +86,16 @@ function runJSUnityTests (tests) {
         );
         err = err.cause;
       }
-      result = false;
+      allResults.status = false;
     }
 
     internal.wait(0); // force GC
   });
-  fs.write(fs.join(instanceinfo.rootDir, 'testresult.json'), JSON.stringify(allResults));
 
   if (failed.length > 1) {
     print('The following ' + failed.length + ' test files produced errors: ', failed.join(', '));
   }
-  return result;
+  return allResults;
 }
 
 // //////////////////////////////////////////////////////////////////////////////
@@ -81,7 +103,7 @@ function runJSUnityTests (tests) {
 // //////////////////////////////////////////////////////////////////////////////
 
 function runMochaTests (testFiles) {
-  var result = true;
+  var result = _.clone(deflRes);
 
   if (testFiles.length > 0) {
     var unitTestFilter = internal.unitTestFilter();
@@ -90,7 +112,7 @@ function runMochaTests (testFiles) {
     }
 
     print('\nRunning Mocha Tests: ' + testFiles.join(', '));
-    result = require('@arangodb/mocha-runner')(testFiles, undefined, unitTestFilter);
+    result = require('@arangodb/mocha-runner')(testFiles, true, unitTestFilter);
   }
 
   return result;
@@ -101,7 +123,16 @@ function runMochaTests (testFiles) {
 // //////////////////////////////////////////////////////////////////////////////
 
 function runCommandLineTests () {
-  var result = true,
+  let instanceinfo;
+  let env = internal.env;
+  if (!env.hasOwnProperty('INSTANCEINFO')) {
+    throw new Error('env.INSTANCEINFO was not set by caller!');
+  }
+  instanceinfo = JSON.parse(env.INSTANCEINFO);
+  if (!instanceinfo) {
+    throw new Error('env.INSTANCEINFO was not set by caller!');
+  }
+  var result,
     unitTests = internal.unitTests(),
     isSpecRegEx = /.+-spec.*\.js/,
     isSpec = function (unitTest) {
@@ -110,12 +141,35 @@ function runCommandLineTests () {
     jsUnity = _.reject(unitTests, isSpec),
     mocha = _.filter(unitTests, isSpec);
 
-  result = (
-    runJSUnityTests(jsUnity)
-    && runMochaTests(mocha)
-  );
+  let resultJ = runJSUnityTests(jsUnity, instanceinfo);
+  let resultM = runMochaTests(mocha);
 
-  internal.setUnitTestsResult(result);
+  result = {
+    failed: resultJ.failed + resultM.failed,
+    total: resultJ.total + resultM.total,
+    duration: resultJ.duration + resultM.duration,
+    status: resultJ.status & resultM.status
+  };
+
+  let addResults = function(which) {
+    for (let key in which) {
+      if (which.hasOwnProperty(key)) {
+        if (! statusKeys.includes(key)) {
+          if (result.hasOwnProperty(key)) {
+            print('Duplicate test in "' + key + '" - \n"' + JSON.stringify(which) + "\n" + JSON.stringify(result));
+            throw new Error();
+          }
+        }
+        result[key] = which[key];
+      }
+    }
+  };
+
+  addResults(resultJ);
+  addResults(resultM);
+  fs.write(fs.join(instanceinfo.rootDir, 'testresult.json'), JSON.stringify(result));
+
+  internal.setUnitTestsResult(result.status);
 }
 
 exports.runCommandLineTests = runCommandLineTests;
