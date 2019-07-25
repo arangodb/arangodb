a9fa4a7e399dde25c06f5bdafd622f44342a714d
Wilfried Goesgens <willi@arangodb.com>
2019-07-25 14:47:34 +0200

    cleanup

--- lib/ApplicationFeatures/V8PlatformFeature.cpp
+++ lib/ApplicationFeatures/V8PlatformFeature.cpp
@@ -107,7 +107,7 @@ static void gcEpilogueCallback(v8::Isolate* isolate, v8::GCType type,
         << " interrupting V8 execution ("
         << "heap size limit " << heapSizeLimit
         << ", used " << usedHeadSize
-        << ") while " << whereFreed;
+        << ") during " << whereFreed;
 
     isolate->TerminateExecution();
     V8PlatformFeature::setOutOfMemory(isolate);
