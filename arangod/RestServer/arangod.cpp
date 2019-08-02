5bca5c1920d7a7beab5fb506484875f5cbe3ef35
Jan <jsteemann@users.noreply.github.com>
2019-08-02 16:26:01 +0200

    fix yet another undefined behavior (#9630)

--- arangod/RestServer/arangod.cpp
+++ arangod/RestServer/arangod.cpp
@@ -373,7 +373,7 @@ int main(int argc, char* argv[]) {
   delete arangodb::restartAction;
   if (res != 0) {
     std::cerr << "FATAL: RestartAction returned non-zero exit status: "
-      << res << ", giving up.";
+      << res << ", giving up." << std::endl;
     return res;
   }
   // It is not clear if we want to do the following under Linux and OSX,
@@ -382,7 +382,10 @@ int main(int argc, char* argv[]) {
   // to do this because the solution below is not possible. In these
   // cases, we need outside help to get the process restarted.
 #if defined(__linux__) || defined(__APPLE__)
-  chdir(workdir.c_str());
+  res = chdir(workdir.c_str());
+  if (res != 0) {
+    std::cerr << "WARNING: could not change into directory '" << workdir << "'" << std::endl;
+  }
   execv(argv[0], argv);
 #endif
 }
