bc479ff27cd02510ccb47f733e2147d7dc84939d
hkernbach <heiko.kernbach@gmail.com>
2019-08-07 17:13:31 +0200

    revert static string

--- tests/IResearch/common.cpp
+++ tests/IResearch/common.cpp
@@ -258,7 +258,7 @@ namespace arangodb {
 
 namespace tests {
 
-std::string const AnalyzerCollectionName(StaticStrings::AnalyzersCollection);
+std::string const AnalyzerCollectionName("_analyzers");
 
 std::string testResourceDir;
 
