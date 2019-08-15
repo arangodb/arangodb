12224aeb1e627f35a104a67dcc3e49884cc9db8d
hkernbach <heiko.kernbach@gmail.com>
2019-08-15 12:08:32 +0200

    fixed ui behaviour when replacing a foxx app

--- js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
@@ -1264,13 +1264,7 @@
       } else {
         $('#new-app-mount').attr('disabled', 'true');
         $('#new-app-replace').attr('checked', false);
-        $('#new-app-replace').on('click', function () {
-          if ($('#new-app-replace').prop('checked')) {
-            $('#new-app-teardown').attr('disabled', true);
-          } else {
-            $('#new-app-teardown').attr('disabled', false);
-          }
-        });
+        $('#new-app-teardown').attr('disabled', true);
       }
     }
   };
