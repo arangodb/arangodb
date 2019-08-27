9618b74423e006902c760e7dc143e243a61c0d0b
hkernbach <heiko.kernbach@gmail.com>
2019-08-27 21:20:25 +0200

    iterate through all vertexCollections of a graph to find a vertex to display

--- js/apps/system/_admin/aardvark/APP/aardvark.js
+++ js/apps/system/_admin/aardvark/APP/aardvark.js
@@ -562,14 +562,9 @@ authRouter.get('/graph/:name', function (req, res) {
   if (!verticesCollections || verticesCollections.length === 0) {
     res.throw('bad request', 'no vertex collections found for graph');
   }
-  var vertexName;
-  try {
-    vertexName = verticesCollections[Math.floor(Math.random() * verticesCollections.length)].name();
-  } catch (err) {
-    res.throw('bad request', 'vertex collection of graph not found');
-  }
 
   var vertexCollections = [];
+
   _.each(graph._vertexCollections(), function (vertex) {
     vertexCollections.push({
       name: vertex.name(),
@@ -577,7 +572,6 @@ authRouter.get('/graph/:name', function (req, res) {
     });
   });
 
-  var startVertex;
   var config;
 
   try {
@@ -586,25 +580,40 @@ authRouter.get('/graph/:name', function (req, res) {
     res.throw('bad request', e.message, {cause: e});
   }
 
-  var getPseudoRandomStartVertex = function (collName) {
-    let maxDoc = db[collName].count();
-    if (maxDoc === 0) {
-      return null;
-    }
-    if (maxDoc > 1000) {
-      maxDoc = 1000;
-    }
-    let randDoc = Math.floor(Math.random() * maxDoc);
+  var getPseudoRandomStartVertex = function () {
+    for (var i = 0; i < graph._vertexCollections().length; i++) {
+      var vertexCollection = graph._vertexCollections()[i];
+      let maxDoc = db[vertexCollection.name()].count();
 
-    return db._query(
-      'FOR vertex IN @@vertexCollection LIMIT @skipN, 1 RETURN vertex',
-      {
-        '@vertexCollection': collName,
-        'skipN': randDoc
+      if (maxDoc === 0) {
+        continue;
       }
-    ).toArray()[0];
+
+      if (maxDoc > 1000) {
+        maxDoc = 1000;
+      }
+
+      let randDoc = Math.floor(Math.random() * maxDoc);
+
+      let potentialVertex = db._query(
+        'FOR vertex IN @@vertexCollection LIMIT @skipN, 1 RETURN vertex',
+        {
+          '@vertexCollection': vertexCollection.name(),
+          'skipN': randDoc
+        }
+      ).toArray()[0];
+
+      if (potentialVertex) {
+        return potentialVertex;
+      }
+    }
+
+    return null;
   };
+
   var multipleIds;
+  var startVertex; // will be "randomly" choosen if no start vertex is specified
+
   if (config.nodeStart) {
     if (config.nodeStart.indexOf(' ') > -1) {
       multipleIds = config.nodeStart.split(' ');
@@ -615,11 +624,11 @@ authRouter.get('/graph/:name', function (req, res) {
         res.throw('bad request', e.message, {cause: e});
       }
       if (!startVertex) {
-        startVertex = getPseudoRandomStartVertex(vertexName);
+        startVertex = getPseudoRandomStartVertex();
       }
     }
   } else {
-    startVertex = getPseudoRandomStartVertex(vertexName);
+    startVertex = getPseudoRandomStartVertex();
   }
 
   var limit = 0;
@@ -633,7 +642,7 @@ authRouter.get('/graph/:name', function (req, res) {
   if (startVertex === null) {
     toReturn = {
       empty: true,
-      msg: 'Your graph is empty',
+      msg: 'Your graph is empty. We did not find a document in any available vertex collection.',
       settings: {
         vertexCollections: vertexCollections
       }
