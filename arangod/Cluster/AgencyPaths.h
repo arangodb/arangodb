fd377e312997495b07cd8496b6c2dceae1f4b983
Tobias Gödderz <tobias@arangodb.com>
2019-09-04 17:36:39 +0200

    Added /arango/Supervision

--- arangod/Cluster/AgencyPaths.h
+++ arangod/Cluster/AgencyPaths.h
@@ -57,6 +57,25 @@ namespace paths {
  *   // stream.str() == "/arango/Plan/Databases/_system"
  *
  * If you add anything, make sure to add tests in tests/Cluster/AgencyPathsTest.cpp.
+ *
+ * An example for a static component looks like this:
+ * class SomeOuterClass {
+ * ...
+ *   class YourComponent : public PathComponent<YourComponent, SomeOuterClass> {
+ *    public:
+ *     constexpr char const* component() const noexcept { return "YourComponent"; }
+ *
+ *     using BaseType::PathComponent;
+ *
+ *     // Add possible inner classes here
+ *   };
+ *
+ *   std::shared_ptr<YourComponent const> yourComponent() const {
+ *     return YourComponent::make_shared(shared_from_this());
+ *   }
+ * ...
+ * }
+ *
  */
 
 class Root;
@@ -162,6 +181,255 @@ class Root : public std::enable_shared_from_this<Root>, public Path {
 
     std::shared_ptr<Current const> current() const {
       return Current::make_shared(shared_from_this());
+    }
+
+    class Supervision : public PathComponent<Supervision, Arango> {
+     public:
+      constexpr char const* component() const noexcept { return "Supervision"; }
+
+      using BaseType::PathComponent;
+
+      class State : public PathComponent<State, Supervision> {
+       public:
+        constexpr char const* component() const noexcept { return "State"; }
+
+        using BaseType::PathComponent;
+
+        class Timestamp : public PathComponent<Timestamp, State> {
+         public:
+          constexpr char const* component() const noexcept {
+            return "Timestamp";
+          }
+
+          using BaseType::PathComponent;
+        };
+
+        std::shared_ptr<Timestamp const> timestamp() const {
+          return Timestamp::make_shared(shared_from_this());
+        }
+
+        class Mode : public PathComponent<Mode, State> {
+         public:
+          constexpr char const* component() const noexcept { return "Mode"; }
+
+          using BaseType::PathComponent;
+        };
+
+        std::shared_ptr<Mode const> mode() const {
+          return Mode::make_shared(shared_from_this());
+        }
+      };
+
+      std::shared_ptr<State const> state() const {
+        return State::make_shared(shared_from_this());
+      }
+
+      class Shards : public PathComponent<Shards, Supervision> {
+       public:
+        constexpr char const* component() const noexcept { return "Shards"; }
+
+        using BaseType::PathComponent;
+      };
+
+      std::shared_ptr<Shards const> shards() const {
+        return Shards::make_shared(shared_from_this());
+      }
+
+      class DbServers : public PathComponent<DbServers, Supervision> {
+       public:
+        constexpr char const* component() const noexcept { return "DBServers"; }
+
+        using BaseType::PathComponent;
+      };
+
+      std::shared_ptr<DbServers const> dbServers() const {
+        return DbServers::make_shared(shared_from_this());
+      }
+
+      class Health : public PathComponent<Health, Supervision> {
+       public:
+        constexpr char const* component() const noexcept { return "Health"; }
+
+        using BaseType::PathComponent;
+
+        class DbServer : public PathComponent<DbServer, Health> {
+         public:
+          char const* component() const noexcept { return _name.c_str(); }
+
+         private:
+          ServerID const _name;
+
+          // Only the parent type P may instantiate a component, so make this private
+          // and P a friend. MSVC ignores the friend declaration, though.
+#if defined(_WIN32) || defined(_WIN64)
+         public:
+#else
+         protected:
+          friend ParentType;
+#endif
+          DbServer(std::shared_ptr<ParentType const> parent, ServerID name) noexcept
+              : BaseType(std::move(parent)), _name(std::move(name)) {
+            // An empty id would break the path creation, and we would get a
+            // path to arango/Plan/Databases/. This could be all sorts of bad.
+            // This would best be prevented by DatabaseID being a real type,
+            // that disallows construction with an empty name.
+            TRI_ASSERT(!_name.empty());
+          }
+
+          // shared ptr constructor
+          static std::shared_ptr<DbServer const> make_shared(std::shared_ptr<ParentType const> parent,
+                                                             ServerID name) {
+            struct ConstructibleDbServer : public DbServer {
+             public:
+              ConstructibleDbServer(std::shared_ptr<ParentType const> parent, ServerID name) noexcept
+                  : DbServer(std::move(parent), std::move(name)) {}
+            };
+            return std::make_shared<ConstructibleDbServer const>(std::move(parent),
+                                                                 std::move(name));
+          }
+
+         public:
+          class SyncTime : public PathComponent<SyncTime, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "SyncTime";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<SyncTime const> syncTime() const {
+            return SyncTime::make_shared(shared_from_this());
+          }
+
+          class Timestamp : public PathComponent<Timestamp, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "Timestamp";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Timestamp const> timestamp() const {
+            return Timestamp::make_shared(shared_from_this());
+          }
+
+          class SyncStatus : public PathComponent<SyncStatus, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "SyncStatus";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<SyncStatus const> syncStatus() const {
+            return SyncStatus::make_shared(shared_from_this());
+          }
+
+          class LastAckedTime : public PathComponent<LastAckedTime, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "LastAckedTime";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<LastAckedTime const> lastAckedTime() const {
+            return LastAckedTime::make_shared(shared_from_this());
+          }
+
+          class Host : public PathComponent<Host, DbServer> {
+           public:
+            constexpr char const* component() const noexcept { return "Host"; }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Host const> host() const {
+            return Host::make_shared(shared_from_this());
+          }
+
+          class Engine : public PathComponent<Engine, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "Engine";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Engine const> engine() const {
+            return Engine::make_shared(shared_from_this());
+          }
+
+          class Version : public PathComponent<Version, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "Version";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Version const> version() const {
+            return Version::make_shared(shared_from_this());
+          }
+
+          class Status : public PathComponent<Status, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "Status";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Status const> status() const {
+            return Status::make_shared(shared_from_this());
+          }
+
+          class ShortName : public PathComponent<ShortName, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "ShortName";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<ShortName const> shortName() const {
+            return ShortName::make_shared(shared_from_this());
+          }
+
+          class Endpoint : public PathComponent<Endpoint, DbServer> {
+           public:
+            constexpr char const* component() const noexcept {
+              return "Endpoint";
+            }
+
+            using BaseType::PathComponent;
+          };
+
+          std::shared_ptr<Endpoint const> endpoint() const {
+            return Endpoint::make_shared(shared_from_this());
+          }
+        };
+
+        std::shared_ptr<DbServer const> dbServer(ServerID server) const {
+          return DbServer::make_shared(shared_from_this(), std::move(server));
+        }
+      };
+
+      std::shared_ptr<Health const> health() const {
+        return Health::make_shared(shared_from_this());
+      }
+    };
+
+    std::shared_ptr<Supervision const> supervision() const {
+      return Supervision::make_shared(shared_from_this());
     }
   };
 
--- tests/Cluster/AgencyPathsTest.cpp
+++ tests/Cluster/AgencyPathsTest.cpp
@@ -35,30 +35,79 @@ using namespace arangodb::cluster::paths;
 
 // We don't want any class in the hierarchy to be publicly constructible.
 // However, we can only check for specific constructors.
-// TODO complete the list
 #define CONSTRUCTIBLE_MESSAGE "This class should not be publicly constructible!"
+// clang-format off
+
+// First, default constructors
 static_assert(!std::is_default_constructible<Root>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_default_constructible<Root::Arango>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_default_constructible<Root::Arango::Current>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Current::ServersRegistered>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_default_constructible<Root::Arango::Plan>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_default_constructible<Root::Arango::Plan::Databases>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_default_constructible<Root::Arango::Plan::Databases::Database>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::State>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::State::Timestamp>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::State::Mode>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Shards>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::DbServers>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::SyncTime>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Timestamp>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::SyncStatus>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::LastAckedTime>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Host>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Engine>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Version>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Status>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::ShortName>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_default_constructible<Root::Arango::Supervision::Health::DbServer::Endpoint>::value, CONSTRUCTIBLE_MESSAGE);
 
 // Exclude these on windows, because the constructor is made public there.
 #ifndef _WIN32
+
+// Second, constructors with parent
 static_assert(!std::is_constructible<Root::Arango, Root>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_constructible<Root::Arango::Current, Root::Arango>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Current::ServersRegistered, Root::Arango::Current>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_constructible<Root::Arango::Plan, Root::Arango>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_constructible<Root::Arango::Plan::Databases, Root::Arango::Plan>::value, CONSTRUCTIBLE_MESSAGE);
 static_assert(!std::is_constructible<Root::Arango::Plan::Databases::Database, Root::Arango::Plan::Databases>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision, Root::Arango>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::State, Root::Arango::Supervision>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::State::Timestamp, Root::Arango::Supervision::State>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::State::Mode, Root::Arango::Supervision::State>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Shards, Root::Arango::Supervision>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::DbServers, Root::Arango::Supervision>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health, Root::Arango::Supervision>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer, Root::Arango::Supervision::Health>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::SyncTime, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Timestamp, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::SyncStatus, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::LastAckedTime, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Host, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Engine, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Version, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Status, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::ShortName, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer::Endpoint, Root::Arango::Supervision::Health::DbServer>::value, CONSTRUCTIBLE_MESSAGE);
+
+
+// Third, constructors with parent and an additional string where applicable
 static_assert(!std::is_constructible<Root::Arango::Plan::Databases::Database, Root::Arango::Plan::Databases, DatabaseID>::value, CONSTRUCTIBLE_MESSAGE);
+static_assert(!std::is_constructible<Root::Arango::Supervision::Health::DbServer, Root::Arango::Supervision::Health, ServerID>::value, CONSTRUCTIBLE_MESSAGE);
+
 #endif
+// clang-format on
 #undef CONSTRUCTIBLE_MESSAGE
 
 class AgencyPathsTest : public ::testing::Test {
  protected:
   // Vector of {expected, actual} pairs.
-  std::vector<std::pair<std::vector<std::string> const, std::shared_ptr<Path const> const>> const ioPairs {
+  std::vector<std::pair<std::vector<std::string> const, std::shared_ptr<Path const> const>> const ioPairs{
+      // clang-format off
       {{"arango"}, root()->arango()},
       {{"arango", "Plan"}, root()->arango()->plan()},
       {{"arango", "Plan", "Databases"}, root()->arango()->plan()->databases()},
@@ -66,6 +115,25 @@ class AgencyPathsTest : public ::testing::Test {
       {{"arango", "Plan", "Databases", "someCol"}, root()->arango()->plan()->databases()->database(DatabaseID{"someCol"})},
       {{"arango", "Current"}, root()->arango()->current()},
       {{"arango", "Current", "ServersRegistered"}, root()->arango()->current()->serversRegistered()},
+      {{"arango", "Supervision"}, root()->arango()->supervision()},
+      {{"arango", "Supervision", "State"}, root()->arango()->supervision()->state()},
+      {{"arango", "Supervision", "State", "Timestamp"}, root()->arango()->supervision()->state()->timestamp()},
+      {{"arango", "Supervision", "State", "Mode"}, root()->arango()->supervision()->state()->mode()},
+      {{"arango", "Supervision", "Shards"}, root()->arango()->supervision()->shards()},
+      {{"arango", "Supervision", "DBServers"}, root()->arango()->supervision()->dbServers()},
+      {{"arango", "Supervision", "Health"}, root()->arango()->supervision()->health()},
+      {{"arango", "Supervision", "Health", "CRDN-1234"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")},
+      {{"arango", "Supervision", "Health", "PRMR-5678", "SyncTime"}, root()->arango()->supervision()->health()->dbServer("PRMR-5678")->syncTime()},
+      {{"arango", "Supervision", "Health", "PRMR-5678", "Timestamp"}, root()->arango()->supervision()->health()->dbServer("PRMR-5678")->timestamp()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "SyncStatus"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->syncStatus()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "LastAckedTime"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->lastAckedTime()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "Host"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->host()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "Engine"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->engine()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "Version"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->version()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "Status"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->status()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "ShortName"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->shortName()},
+      {{"arango", "Supervision", "Health", "CRDN-1234", "Endpoint"}, root()->arango()->supervision()->health()->dbServer("CRDN-1234")->endpoint()},
+      // clang-format on
   };
 };
 
