960a4b78998c8f292394dbc93ebbd7f950f55673
Markus Pfeiffer <markus@arangodb.com>
2019-09-11 13:24:40 +0100

    Cleanup SubqueryEndNode

--- arangod/Aql/SubqueryEndExecutionNode.h
+++ arangod/Aql/SubqueryEndExecutionNode.h
@@ -37,74 +37,35 @@ class SubqueryEndNode : public ExecutionNode {
  public:
   SubqueryEndNode(ExecutionPlan*, arangodb::velocypack::Slice const& base);
 
-  SubqueryEndNode(ExecutionPlan* plan, size_t id, ExecutionNode* subquery, Variable const* outVariable)
-      : ExecutionNode(plan, id), _subquery(subquery), _outVariable(outVariable) {
-    TRI_ASSERT(_subquery != nullptr);
+  SubqueryEndNode(ExecutionPlan* plan, size_t id, Variable const* outVariable)
+      : ExecutionNode(plan, id), _outVariable(outVariable) {
     TRI_ASSERT(_outVariable != nullptr);
   }
 
-  /// @brief return the type of the node
+  CostEstimate estimateCost() const override final;
+
   NodeType getType() const override final { return SUBQUERY_END; }
 
-  /// @brief invalidate the cost estimate for the node and its dependencies
-  void invalidateCost() override;
-
-  /// @brief return the out variable
   Variable const* outVariable() const { return _outVariable; }
 
-  /// @brief export to VelocyPack
   void toVelocyPackHelper(arangodb::velocypack::Builder&, unsigned flags) const override final;
 
-  /// @brief creates corresponding ExecutionBlock
   std::unique_ptr<ExecutionBlock> createBlock(
       ExecutionEngine& engine,
       std::unordered_map<ExecutionNode*, ExecutionBlock*> const&) const override;
 
-  /// @brief clone ExecutionNode recursively
   ExecutionNode* clone(ExecutionPlan* plan, bool withDependencies,
                        bool withProperties) const override final;
 
-  /// @brief whether or not the subquery is a data-modification operation
-  bool isModificationSubquery() const;
 
-  /// @brief getter for subquery
-  ExecutionNode* getSubquery() const { return _subquery; }
-
-  /// @brief setter for subquery
-  void setSubquery(ExecutionNode* subquery, bool forceOverwrite) {
-    TRI_ASSERT(subquery != nullptr);
-    TRI_ASSERT((forceOverwrite && _subquery != nullptr) ||
-               (!forceOverwrite && _subquery == nullptr));
-    _subquery = subquery;
-  }
-
-  /// @brief estimateCost
-  CostEstimate estimateCost() const override final;
-
-  /// @brief getVariablesUsedHere, modifying the set in-place
-  void getVariablesUsedHere(arangodb::HashSet<Variable const*>& vars) const override final;
-
-  /// @brief getVariablesSetHere
   std::vector<Variable const*> getVariablesSetHere() const override final {
     return std::vector<Variable const*>{_outVariable};
   }
 
-  /// @brief replace the out variable, so we can adjust the name.
   void replaceOutVariable(Variable const* var);
 
-  bool isDeterministic() override final;
-
-  bool isConst();
-  bool mayAccessCollections();
-
  private:
-  /// @brief we need to have an expression and where to write the result
-  ExecutionNode* _subquery;
-
-  /// @brief variable to write to
   Variable const* _outVariable;
-
-  size_t depth;
 };
 
 } // namespace aql
