13add2af10a9c9af90806098fea1c53f4f122ba0
Markus Pfeiffer <markus@arangodb.com>
2019-09-12 13:52:41 +0100

    Update ExecutionNode tests

--- tests/Aql/ExecutionNodeTest.cpp
+++ tests/Aql/ExecutionNodeTest.cpp
@@ -23,18 +23,96 @@
 
 #include "gtest/gtest.h"
 
+#include "Aql/Ast.h"
+#include "Aql/Query.h"
+#include "Aql/ExecutionPlan.h"
 #include "Aql/SubqueryStartExecutionNode.h"
 #include "Aql/SubqueryEndExecutionNode.h"
 
+#include "Logger/LogMacros.h"
 
 #include "velocypack/Builder.h"
 #include "velocypack/velocypack-aliases.h"
 
+#include "../Mocks/Servers.h"
+
+using namespace arangodb::aql;
+
+namespace arangodb {
+namespace tests {
+namespace aql {
+
 class ExecutionNodeTest : public ::testing::Test {
-  ExecutionNodeTest() {};
+protected:
+  mocks::MockAqlServer server;
+  std::unique_ptr<arangodb::aql::Query> fakedQuery;
+  Ast ast;
+  ExecutionPlan plan;
+
+public:
+ ExecutionNodeTest()
+     : fakedQuery(server.createFakeQuery()),
+       ast(fakedQuery.get()),
+       plan(&ast)
+           {};
 };
 
-TEST_F(SubqueryStartExecutionNodeTest, to_velocypack) {
-  ASSERT_TRUE(true);
+TEST_F(ExecutionNodeTest, start_node_velocypack_roundtrip) {
+  VPackBuilder builder;
+
+  SubqueryStartNode *node, *nodeFromVPack;
+
+  node = new SubqueryStartNode(&plan, 0);
+
+  builder.openArray();
+  node->toVelocyPackHelper(builder, ExecutionNode::SERIALIZE_DETAILS);
+  builder.close();
+
+  nodeFromVPack = new SubqueryStartNode(&plan, builder.slice()[0]);
+
+  ASSERT_TRUE(node->isEqualTo(nodeFromVPack));
 }
 
+TEST_F(ExecutionNodeTest, start_node_not_equal_different_id) {
+  SubqueryStartNode *node1, *node2;
+
+  node1 = new SubqueryStartNode(&plan, 0);
+  node1 = new SubqueryStartNode(&plan, 1);
+
+  ASSERT_FALSE(node1->isEqualTo(node2));
+}
+
+TEST_F(ExecutionNodeTest, end_node_velocypack_roundtrip) {
+  VPackBuilder builder;
+
+  Variable *outvar = new Variable("name", 1);
+
+  SubqueryEndNode *node, *nodeFromVPack;
+
+  node = new SubqueryEndNode(&plan, 0, outvar);
+
+  builder.openArray();
+  node->toVelocyPackHelper(builder, ExecutionNode::SERIALIZE_DETAILS);
+  builder.close();
+
+  nodeFromVPack = new SubqueryEndNode(&plan, builder.slice()[0]);
+
+  ASSERT_TRUE(node->isEqualTo(nodeFromVPack));
+}
+
+TEST_F(ExecutionNodeTest, end_node_not_equal_different_id) {
+  SubqueryEndNode *node1, *node2;
+
+  Variable *outvar = new Variable("name", 1);
+
+  node1 = new SubqueryEndNode(&plan, 0, outvar);
+  node1 = new SubqueryEndNode(&plan, 1, outvar);
+
+  ASSERT_FALSE(node1->isEqualTo(node2));
+}
+
+
+
+} // namespace aql
+} // namespace tests
+} // namespace arangodb
