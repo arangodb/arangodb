b470ef54b796dad2712e56c70034802e946b31f5
Frank Celler <frank@arangodb.com>
2019-09-13 14:00:20 +0200

    WIP

---
+++ arangod/Auth/UseQueuesPrivilege.h
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Frank Celler
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Auth/Privilege.h"
+
+namespace arangodb {
+namespace auth {
+  class UseQueuesPrivilege : public Privilege {
+   public:
+    explicit UseQueuesPrivilege(std::string const& username) 
+      : _username(username) {}
+
+    std::string const& username() const { return _username; }
+
+   private:
+    std::string const _username;
+  };
+}  // namespace auth
+}  // namespace arangodb
---
+++ arangod/Auth/UseTasksPrivilege.h
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Frank Celler
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Auth/Privilege.h"
+
+namespace arangodb {
+namespace auth {
+  class UseTasksPrivilege : public Privilege {
+   public:
+    explicit UseTasksPrivilege(std::string const& username, std::string const& runAs) 
+      : _username(username), _runAs(runAs) {}
+
+    std::string const& username() const { return _username; }
+    std::string const& runAs() const { return _runAs; }
+
+   private:
+    std::string const _username;
+    std::string const _runAs;
+  };
+}  // namespace auth
+}  // namespace arangodb
