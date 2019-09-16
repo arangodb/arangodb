3f444e53f249111b9a6d2395184cf133da0fe8c6
KVS85 <vadim@arangodb.com>
2019-09-17 01:37:07 +0200

    Fix Windows client package

--- arangosh/install.cmake
+++ arangosh/install.cmake
@@ -4,6 +4,9 @@
 
 set(STRIP_DIR "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_X}/cstrip")
 add_custom_target(strip_install_client ALL)
+if (${USE_ENTERPRISE})
+strip_install_bin_and_config(arangobackup  ${STRIP_DIR} ${CMAKE_INSTALL_BINDIR} strip_install_client)
+endif ()
 strip_install_bin_and_config(arangobench   ${STRIP_DIR} ${CMAKE_INSTALL_BINDIR} strip_install_client)
 strip_install_bin_and_config(arangodump    ${STRIP_DIR} ${CMAKE_INSTALL_BINDIR} strip_install_client)
 strip_install_bin_and_config(arangoimport  ${STRIP_DIR} ${CMAKE_INSTALL_BINDIR} strip_install_client)
