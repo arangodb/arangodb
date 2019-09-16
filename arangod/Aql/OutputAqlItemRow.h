4f23c1ac810a81093ac317cd3b2df26e911a261d
Michael Hackstein <michael@arangodb.com>
2019-09-16 10:35:57 +0200

    it actually helps to save fies before commiting them...

--- arangod/Aql/OutputAqlItemRow.h
+++ arangod/Aql/OutputAqlItemRow.h
@@ -240,17 +240,9 @@ class OutputAqlItemRow {
 #endif
     _baseIndex = index;
   }
-<<<<<<< HEAD
-  // Use this function with caution! We need it for the SortedCollectExecutor
-  // and CountCollectExecutor.
-  void setAllowSourceRowUninitialized() { _allowSourceRowUninitialized = true; }
-=======
   // Use this function with caution! We need it for the SortedCollectExecutor,
   // CountCollectExecutor, and the ConstrainedSortExecutor.
-  void setAllowSourceRowUninitialized() {
-    _allowSourceRowUninitialized = true;
-  }
->>>>>>> 7ce085cefe9579b20727be3c860045315b36e817
+  void setAllowSourceRowUninitialized() { _allowSourceRowUninitialized = true; }
 
   // This function can be used to restore the row's invariant.
   // After setting this value numRowsWritten() rather returns
