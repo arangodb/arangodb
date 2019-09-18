d51b1a9077a7c28f4e957a252643a50eec4f0abe
Dan Larkin-York <dan@arangodb.com>
2019-09-18 17:29:16 -0400

    Fix typo.

--- arangod/Agency/Store.cpp
+++ arangod/Agency/Store.cpp
@@ -24,7 +24,7 @@
 #include "Store.h"
 
 #include "Agency/Agent.h"
-#include "ApplicationFeatures/ApplicaitonServer.h"
+#include "ApplicationFeatures/ApplicationServer.h"
 #include "Basics/ConditionLocker.h"
 #include "Basics/ReadLocker.h"
 #include "Basics/StringUtils.h"
