298df8e994e16e1b765f29a5d93c80288dac9355
Markus Pfeiffer <markus@arangodb.com>
2019-09-18 14:31:48 +0100

    Add SubqueryEndExecutor

---
+++ arangod/Aql/SubqueryEndExecutor.cpp
@@ -0,0 +1,138 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ExecutionBlock.h"
+#include "SubqueryEndExecutor.h"
+
+#include "Aql/OutputAqlItemRow.h"
+#include "Aql/SingleRowFetcher.h"
+
+using namespace arangodb;
+using namespace arangodb::aql;
+
+SubqueryEndExecutorInfos::SubqueryEndExecutorInfos(
+    std::shared_ptr<std::unordered_set<RegisterId>> readableInputRegisters,
+    std::shared_ptr<std::unordered_set<RegisterId>> writeableOutputRegisters,
+    RegisterId nrInputRegisters, RegisterId nrOutputRegisters,
+    std::unordered_set<RegisterId> const& registersToClear,
+    std::unordered_set<RegisterId>&& registersToKeep,
+    transaction::Methods* trxPtr, RegisterId outReg)
+    : ExecutorInfos(readableInputRegisters, writeableOutputRegisters, nrInputRegisters,
+                    nrOutputRegisters, registersToClear, std::move(registersToKeep)),
+      _trxPtr(trxPtr), _outReg(outReg) {}
+
+SubqueryEndExecutorInfos::SubqueryEndExecutorInfos(SubqueryEndExecutorInfos&& other) = default;
+
+SubqueryEndExecutorInfos::~SubqueryEndExecutorInfos() = default;
+
+SubqueryEndExecutor::SubqueryEndExecutor(Fetcher& fetcher, SubqueryEndExecutorInfos& infos)
+    : _fetcher(fetcher),
+      _infos(infos),
+      _outputPending(false) {
+  resetAccumulator();
+}
+
+SubqueryEndExecutor::~SubqueryEndExecutor() = default;
+
+std::pair<ExecutionState, NoStats> SubqueryEndExecutor::produceRows(OutputAqlItemRow& output) {
+  ExecutionState state;
+  NoStats stats;
+
+  InputAqlItemRow inputRow = InputAqlItemRow{CreateInvalidInputRowHint()};
+  ShadowAqlItemRow shadowRow = ShadowAqlItemRow{CreateInvalidShadowRowHint{}};
+
+  while (true) {
+    std::tie(state, inputRow) = _fetcher.fetchRow();
+
+    if (state == ExecutionState::WAITING) {
+      TRI_ASSERT(!inputRow.isInitialized());
+      return {state, std::move(stats)};
+    }
+
+    // We got a row, put it into the accumulator
+    if (inputRow.isInitialized()) {
+      // Above is a RETURN which writes to register 0
+      TRI_ASSERT(_accumulator.isOpenArray());
+      AqlValue value = inputRow.getValue(0);
+      value.toVelocyPack(_infos.getTrxPtr(), _accumulator, false);
+      _outputPending = true;
+    } else {
+      // Did not get a row, we had better be done now, because we caught WAITING above.
+      TRI_ASSERT(state == ExecutionState::DONE);
+
+      // do this only if pending outputs
+      // submit result to output
+      if(_outputPending) {
+        // Now we expect a shadow row which contains the input
+        // to our subquery, i.e. isRelevant().
+        std::tie(state, shadowRow) = _fetcher.fetchShadowRow();
+        if (state == ExecutionState::WAITING) {
+          TRI_ASSERT(!shadowRow.isInitialized());
+          return {ExecutionState::WAITING, std::move(stats)};
+        }
+        TRI_ASSERT(state == ExecutionState::DONE || state == ExecutionState::HASMORE);
+        TRI_ASSERT(shadowRow.isInitialized() == true);
+        TRI_ASSERT(shadowRow.isRelevant() == true);
+
+        // Here we have all data *and* the relevant shadow row,
+        // so we can now submit
+        _accumulator.close();
+
+        TRI_ASSERT(_accumulator.isClosed());
+
+        AqlValue resultDocVec{_accumulator.slice()};
+        AqlValueGuard guard{resultDocVec, true};
+
+        // Responsibility is handed over
+        output.moveValueInto(_infos.getOutputRegister(), shadowRow, guard);
+        TRI_ASSERT(output.produced());
+
+        resetAccumulator();
+        TRI_ASSERT(_outputPending == false);
+      }
+
+      // we have to consume and forward all immediately following shadow rows,
+      // and they have to have isRelevant == false
+      do {
+        std::tie(state, shadowRow) = _fetcher.fetchShadowRow();
+        if (state == ExecutionState::WAITING) {
+          return {ExecutionState::WAITING, std::move(stats)};
+        }
+        if (shadowRow.isInitialized()) {
+          output.decreaseShadowRowDepth(shadowRow);
+        }
+      } while(state != ExecutionState::DONE);
+
+      // we are done now
+      return {ExecutionState::DONE, std::move(stats)};
+    }
+  }
+
+  return {ExecutionState::DONE, NoStats{}};
+}
+
+void SubqueryEndExecutor::resetAccumulator() {
+  _accumulator.clear();
+  _accumulator.openArray();
+  _outputPending = false;
+}
---
+++ arangod/Aql/SubqueryEndExecutor.h
@@ -0,0 +1,99 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_AQL_SUBQUERY_END_EXECUTOR_H
+#define ARANGOD_AQL_SUBQUERY_END_EXECUTOR_H
+
+#include "Aql/ExecutionState.h"
+#include "Aql/ExecutorInfos.h"
+#include "Aql/InputAqlItemRow.h"
+#include "Aql/SingleRowFetcher.h"
+#include "Aql/Stats.h"
+
+namespace arangodb {
+namespace aql {
+
+class NoStats;
+class OutputAqlItemRow;
+template <bool>
+class SingleRowFetcher;
+
+class SubqueryEndExecutorInfos : public ExecutorInfos {
+ public:
+  SubqueryEndExecutorInfos(std::shared_ptr<std::unordered_set<RegisterId>> readableInputRegisters,
+                           std::shared_ptr<std::unordered_set<RegisterId>> writeableOutputRegisters,
+                           RegisterId nrInputRegisters, RegisterId nrOutputRegisters,
+                           std::unordered_set<RegisterId> const& registersToClear,
+                           std::unordered_set<RegisterId>&& registersToKeep,
+                           transaction::Methods* trxPtr, RegisterId outReg);
+
+  SubqueryEndExecutorInfos() = delete;
+  SubqueryEndExecutorInfos(SubqueryEndExecutorInfos&&);
+  SubqueryEndExecutorInfos(SubqueryEndExecutorInfos const&) = delete;
+  ~SubqueryEndExecutorInfos();
+
+  transaction::Methods* getTrxPtr() const noexcept { return _trxPtr; }
+  inline RegisterId getOutputRegister() const { return _outReg; }
+
+ private:
+  transaction::Methods* _trxPtr;
+  RegisterId const _outReg;
+};
+
+class SubqueryEndExecutor {
+ public:
+  struct Properties {
+    static const bool preservesOrder = true;
+    static const bool allowsBlockPassthrough = false;
+    static const bool inputSizeRestrictsOutputSize = false;
+  };
+
+  using Fetcher = SingleRowFetcher<Properties::allowsBlockPassthrough>;
+  using Infos = SubqueryEndExecutorInfos;
+  using Stats = NoStats;
+
+  SubqueryEndExecutor(Fetcher& fetcher, SubqueryEndExecutorInfos& infos);
+  ~SubqueryEndExecutor();
+
+  std::pair<ExecutionState, Stats> produceRows(OutputAqlItemRow& output);
+
+  inline std::tuple<ExecutionState, Stats, SharedAqlItemBlockPtr> fetchBlockForPassthrough(size_t atMost) {
+    auto rv = _fetcher.fetchBlockForPassthrough(atMost);
+    return {rv.first, {}, std::move(rv.second)};
+  }
+
+ private:
+  void resetAccumulator();
+
+ private:
+  Fetcher& _fetcher;
+  SubqueryEndExecutorInfos& _infos;
+
+  // Accumulator for rows between shadow rows
+  VPackBuilder _accumulator;
+  bool _outputPending;
+};
+}  // namespace aql
+}  // namespace arangodb
+
+#endif
