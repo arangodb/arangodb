da9d6cd880993db55f7f27e996d1e02b34cda8fc
jsteemann <jan@arangodb.com>
2019-10-01 13:09:36 +0200

    attempt to improve replication UI in edge cases

--- js/apps/system/_admin/aardvark/APP/aardvark.js
+++ js/apps/system/_admin/aardvark/APP/aardvark.js
@@ -447,9 +447,13 @@ authRouter.get('/replication/mode', function (req, res) {
   let mode = 0;
   let role = null;
   // active failover
-  if (endpoints.statusCode === 200 && endpoints.json.endpoints.length) {
+  if (endpoints.statusCode === 200) {
     mode = 3;
     role = 'leader';
+  } else if (endpoints.statusCode === 503) {
+    // 503 will be returned in case there is a leadership challenge ongoing
+    mode = 3;
+    // role unknown
   } else {
     // check if global applier (ga) is running
     // if that is true, this node is replicating from another arangodb instance
