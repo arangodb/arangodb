7fd3d56dcb160025962e9218369fe23e1303b3ed
KVS85 <vadim@arangodb.com>
2019-10-02 20:45:33 +0300

    Fix condition

--- lib/Basics/FileUtils.cpp
+++ lib/Basics/FileUtils.cpp
@@ -116,7 +116,7 @@ std::string buildFilename(char const* path, char const* name) {
 
   if (!result.empty()) {
     result = removeTrailingSeparator(result);
-    if (result.length() == 1 && result[0] != TRI_DIR_SEPARATOR_CHAR) {
+    if (result.length() != 1 || result[0] != TRI_DIR_SEPARATOR_CHAR) {
       result += TRI_DIR_SEPARATOR_CHAR;
     }
   }
@@ -138,7 +138,7 @@ std::string buildFilename(std::string const& path, std::string const& name) {
 
   if (!result.empty()) {
     result = removeTrailingSeparator(result);
-    if (result.length() == 1 && result[0] != TRI_DIR_SEPARATOR_CHAR) {
+    if (result.length() != 1 || result[0] != TRI_DIR_SEPARATOR_CHAR) {
       result += TRI_DIR_SEPARATOR_CHAR;
     }
   }
