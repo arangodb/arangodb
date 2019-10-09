87570a8892cc2c80a6b6a4238f89bf41a2e89b56
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-09 07:39:41 +0200

    make version selectable and add more search location

--- utils/clang-format-everything.sh
+++ utils/clang-format-everything.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
+set -u
 
 ## locate executable
 clang_format="${ARANGODB_CLANG_FORMAT:-""}"
-
-for candidate in "clang-format-arangodb" "clang-format-6" "clang-format"; do
-    path=$(type -p candidate)
-    if [[ -n $path ]]; then
-        clang_format="$path"
-        break;
-    fi
-done
+clang_format_version="${ARANGODB_CLANG_FORMAT_VERSION:-"6.0.1"}"
 
 # fallback if nothing is found
 if [[ -z $clang_format ]]; then
+    for candidate in "$HOME/.local/bin/clang-format" "clang-format-arangodb" "clang-format-6" "clang-format"; do
+        echo "checking candidate $candidate"
+        path="$(type -p ${candidate})"
+        if [[ -n $path ]]; then
+            clang_format="$path"
+            echo "selecting this candidate"
+            break;
+        fi
+    done
+else
+    echo "clang-format provided by environment"
+fi
+
+# fallback if nothing is found
+if [[ -z $clang_format ]]; then
+    echo "using fallback"
     clang_format="clang-format"
 fi
 
 ## check version
 echo "checking version of $clang_format"
 version_string=$(${clang_format} --version)
-re=".*version 6.0.1.*"
+re=".*version ${clang_format_version}.*"
 if ! [[ $version_string =~ $re ]]; then
     echo "your version: '$version_string' does not match version 6.0.1"
     exit 1
