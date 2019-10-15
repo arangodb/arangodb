fd456560431afb69b27539152ed9953ce2f7d5cb
Dan Larkin-York <dan@arangodb.com>
2019-10-15 16:37:25 -0400

    Initial partial implementation of Merkle trees.

---
+++ lib/Containers/MerkleTree.h
@@ -0,0 +1,333 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Dan Larkin-York
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGODB_CONTAINERS_MERKLE_TREE_H
+#define ARANGODB_CONTAINERS_MERKLE_TREE_H 1
+
+#include <atomic>
+#include <cmath>
+#include <cstddef>
+#include <functional>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+#include "Basics/debugging.h"
+
+#include "Logger/LogMacros.h"
+
+namespace arangodb {
+namespace containers {
+
+template <std::size_t const BranchingBits = 3,  // 8 children per internal node,
+          std::size_t const LockStripes = 64  // number number of buckets that can be accessed simultaneously
+          >
+class MerkleTree {
+ protected:
+  static constexpr std::size_t CacheLineSize = 64;
+  static constexpr std::size_t BranchingFactor = static_cast<std::size_t>(1) << BranchingBits;
+
+  struct Node {
+    /*std::atomic<*/ std::size_t /*>*/ hash;
+    /*std::atomic<*/ std::size_t /*>*/ count;
+
+    // Node(std::size_t h, std::size_t c) : hash(h), count(c) {}
+  };
+  static_assert(sizeof(Node) > CacheLineSize / 8,
+                "Node size assumptions invalid.");
+  static_assert(sizeof(Node) <= CacheLineSize,
+                "Node size assumptions invalid.");
+  static constexpr std::size_t NodeSize =
+      sizeof(Node) > (CacheLineSize / 2)
+          ? (CacheLineSize*((sizeof(Node) + (CacheLineSize - 1)) / CacheLineSize))
+          : (sizeof(Node) > (CacheLineSize / 4) ? (CacheLineSize / 2)
+                                                : (CacheLineSize / 4));
+
+  struct Meta {
+    std::size_t rangeMin;
+    std::size_t rangeMax;
+    std::size_t maxDepth;
+  };
+  static constexpr std::size_t MetaSize =
+      (CacheLineSize * ((sizeof(Meta) + (CacheLineSize - 1)) / CacheLineSize));
+
+  static constexpr std::size_t nodeCountAtDepth(std::size_t maxDepth) {
+    return static_cast<std::size_t>(1) << (BranchingBits * maxDepth);
+  }
+
+  static constexpr std::size_t nodeCountUpToDepth(std::size_t maxDepth) {
+    return ((static_cast<std::size_t>(1) << (BranchingBits * (maxDepth + 1))) - 1) /
+           (BranchingFactor - 1);
+  }
+
+  static constexpr bool isPowerOf2(std::size_t n) {
+    return n > 0 && (n & (n - 1)) == 0;
+  }
+
+  static constexpr std::size_t log2ceil(std::size_t n) {
+    if (n > (std::numeric_limits<std::size_t>::max() / 2)) {
+      return 8 * sizeof(std::size_t) - 1;
+    }
+    std::size_t i = 1;
+    for (; (static_cast<std::size_t>(1) << i) < n; ++i) {
+    }
+    return i;
+  }
+
+  static constexpr std::size_t minimumFactorFor(std::size_t current, std::size_t target) {
+    if (target < current) {
+      throw std::invalid_argument("Was expecting target >= current.");
+    }
+
+    if (!isPowerOf2(current)) {
+      throw std::invalid_argument("Was expecting current to be power of 2.");
+    }
+
+    // special fast case
+    if (target == current) {
+      return 2;
+    }
+
+    std::size_t rawFactor = target / current;
+    std::size_t correction = (rawFactor * current == target) ? 0 : 1;
+    std::size_t correctedFactor = static_cast<std::size_t>(1)
+                                  << log2ceil(rawFactor + correction);  // force power of 2
+    TRI_ASSERT(isPowerOf2(correctedFactor));
+    return correctedFactor;
+  }
+
+ public:
+  /**
+   * @brief Chooses the default range width for a tree of a given depth.
+   *
+   * Most applications should use either this value or some power-of-two
+   * mulitple of this value. The default is chosen so that each leaf bucket
+   * initially covers a range of 64 keys.
+   *
+   * @param maxDepth The same depth value passed to the constructor
+   */
+  static std::size_t defaultRange(std::size_t maxDepth) {
+    return nodeCountAtDepth(maxDepth) * 64;
+  }
+
+  /**
+   * @brief Construct a Merkle tree of a given depth with a given minimum key
+   *
+   * @param maxDepth The depth of the tree. This determines how much memory The
+   *                 tree will consume, and how fine-grained the hash is.
+   * @param rangeMin The minimum key that can be stored in the tree over its
+   *                 lifetime. An attempt to insert a smaller key will result
+   *                 in an exception.
+   * @param rangeMax Must be an offset from rangeMin of a multiple of the number
+   *                 of leaf nodes. If 0, it will be  chosen using the
+   *                 defaultRange method. This is just an initial value to
+   *                 prevent immediate resizing; if a key larger than rangeMax
+   *                 is inserted into the tree, it will be dynamically resized
+   *                 so that a larger rangeMax is chosen, and adjacent nodes
+   *                 merged as necessary.
+   */
+  MerkleTree(std::size_t maxDepth, std::size_t rangeMin, std::size_t rangeMax = 0)
+      : _buffer(new uint8_t[MetaSize + (NodeSize * nodeCountUpToDepth(maxDepth))]) {
+    if (rangeMax == 0) {
+      rangeMax = rangeMin + defaultRange(maxDepth);
+    }
+    TRI_ASSERT(((rangeMax - rangeMin) / nodeCountAtDepth(maxDepth)) * nodeCountAtDepth(maxDepth) ==
+               (rangeMax - rangeMin));
+    new (&meta()) Meta{rangeMin, rangeMax, maxDepth};
+
+    std::size_t const last = nodeCountUpToDepth(maxDepth);
+    for (std::size_t i = 0; i < last; ++i) {
+      new (&node(i)) Node{0, 0};
+    }
+  }
+
+  ~MerkleTree() {
+    std::unique_lock<std::shared_timed_mutex> guard(_bufferLock);
+
+    std::size_t const last = nodeCountUpToDepth(meta().maxDepth);
+    for (std::size_t i = 0; i < last; ++i) {
+      node(i).~Node();
+    }
+
+    meta().~Meta();
+  }
+
+  /**
+   * @brief Insert a value into the tree. May trigger a resize.
+   *
+   * @param key   The key for the item. If it is less than the minimum specified
+   *              at construction, then it will trigger an exception. If it is
+   *              greater than the current max, it will trigger a (potentially
+   *              expensive) growth operation to ensure the key can be inserted.
+   * @param value The hashed value associated with the key
+   */
+  void insert(std::size_t key, std::size_t value) {
+    std::shared_lock<std::shared_timed_mutex> guard(_bufferLock);
+    if (key < meta().rangeMin) {
+      throw std::out_of_range("Key out of current range.");
+    }
+
+    if (key >= meta().rangeMax) {
+      // unlock so we can get exclusive access to grow the range
+      guard.unlock();
+      grow(key);
+      guard.lock();
+    }
+
+    modify(key, value, /* isInsert */ true);
+  }
+
+  /**
+   * @brief Remove a value from the tree.
+   *
+   * @param key   The key for the item. If it is outside the current tree range,
+   *              then it will trigger an exception.
+   * @param value The hashed value associated with the key
+   */
+  void remove(std::size_t key, std::size_t value) {
+    std::shared_lock<std::shared_timed_mutex> guard(_bufferLock);
+    if (key < meta().rangeMin || key >= meta().rangeMax) {
+      throw std::out_of_range("Key out of current range.");
+    }
+
+    modify(key, value, /* isInsert */ false);
+  }
+
+  /**
+   * @brief Returns the number of hashed keys contained in the tree
+   */
+  std::size_t count() const {
+    std::shared_lock<std::shared_timed_mutex> guard(_bufferLock);
+    std::unique_lock<std::mutex> lock(this->lock(0));
+    return node(0).count;
+  }
+
+  /**
+   * @brief Returns the current range of the tree
+   */
+  std::pair<std::size_t, std::size_t> range() const {
+    std::shared_lock<std::shared_timed_mutex> guard(_bufferLock);
+    return {meta().rangeMin, meta().rangeMax};
+  }
+
+  /**
+   * @brief Returns the maximum depth of the tree
+   */
+  std::size_t maxDepth() const {
+    std::shared_lock<std::shared_timed_mutex> guard(_bufferLock);
+    return meta().maxDepth;
+  }
+
+ protected:
+  Meta& meta() const {
+    // not thread-safe, lock buffer from outside
+    return *reinterpret_cast<Meta*>(_buffer.get());
+  }
+
+  Node& node(std::size_t index) const {
+    // not thread-safe, lock buffer from outside
+    TRI_ASSERT(index < nodeCountUpToDepth(meta().maxDepth));
+    uint8_t* ptr = _buffer.get() + MetaSize + (NodeSize * index);
+    return *reinterpret_cast<Node*>(ptr);
+  }
+
+  std::mutex& lock(std::size_t index) const {
+    return _nodeLocks[_hasher(index) % LockStripes];
+  }
+
+  std::size_t index(std::size_t key, std::size_t depth) {
+    // not thread-safe, lock buffer from outside
+    TRI_ASSERT(depth <= meta().maxDepth);
+    TRI_ASSERT(key >= meta().rangeMin);
+    TRI_ASSERT(key < meta().rangeMax);
+
+    // special fast case
+    if (depth == 0) {
+      return 0;
+    }
+
+    std::size_t offset = key - meta().rangeMin;
+    std::size_t chunkSizeAtDepth =
+        (meta().rangeMax - meta().rangeMin) / (1 << (BranchingBits * depth));
+    std::size_t chunk = offset / chunkSizeAtDepth;
+
+    return chunk + nodeCountUpToDepth(depth - 1);
+  }
+
+  void modify(std::size_t key, std::size_t value, bool isInsert) {
+    // not thread-safe, lock buffer from outside
+    for (std::size_t depth = 0; depth <= meta().maxDepth; ++depth) {
+      std::size_t index = this->index(key, depth);
+      Node& node = this->node(index);
+      std::unique_lock<std::mutex> lock(this->lock(index));
+      if (isInsert) {
+        ++node.count;
+      } else {
+        if (node.count == 0) {
+          throw std::invalid_argument(
+              "Tried to remove key that is not present.");
+        }
+        --node.count;
+      }
+      node.hash ^= value;
+    }
+  }
+
+  void grow(std::size_t key) {
+    std::unique_lock<std::shared_timed_mutex> guard(_bufferLock);
+    // no need to lock nodes as we have an exclusive lock on the buffer
+
+    std::size_t rangeMin = meta().rangeMin;
+    std::size_t rangeMax = meta().rangeMax;
+
+    std::size_t factor = minimumFactorFor(rangeMax - rangeMin, key - rangeMin);
+
+    for (std::size_t depth = 1; depth <= meta().maxDepth; ++depth) {
+      // iterate over all nodes and left-combine, (skipping the first, identity)
+      for (std::size_t index = 1; index < nodeCountAtDepth(depth); ++index) {
+        Node& src = this->node(index);
+        Node& dst = this->node(index / factor);
+        dst.count += src.count;
+        dst.hash &= src.hash;
+        src = Node{0, 0};
+      }
+    }
+
+    meta().rangeMax = rangeMin + ((rangeMax - rangeMin) * factor);
+  }
+
+ private:
+  std::unique_ptr<uint8_t[]> _buffer;
+  // TODO use std::shared_mutex after migration to cpp17
+  mutable std::shared_timed_mutex _bufferLock;
+  std::hash<std::size_t> _hasher;
+  mutable std::mutex _nodeLocks[LockStripes];
+};
+
+}  // namespace containers
+}  // namespace arangodb
+
+#endif
---
+++ tests/Containers/MerkleTreeTest.cpp
@@ -0,0 +1,322 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Dan Larkin-York
+////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include "Containers/MerkleTree.h"
+
+class InternalMerkleTreeTest : public ::arangodb::containers::MerkleTree<>,
+                               public ::testing::Test {
+ public:
+  InternalMerkleTreeTest() : MerkleTree<>(2, 0, 64) {}
+};
+
+TEST_F(InternalMerkleTreeTest, test_log2ceil) {
+  ASSERT_EQ(log2ceil(0), 1);
+  ASSERT_EQ(log2ceil(1), 1);
+  ASSERT_EQ(log2ceil(2), 1);
+  ASSERT_EQ(log2ceil(3), 2);
+  ASSERT_EQ(log2ceil(4), 2);
+  ASSERT_EQ(log2ceil(5), 3);
+  ASSERT_EQ(log2ceil(8), 3);
+  ASSERT_EQ(log2ceil(9), 4);
+  ASSERT_EQ(log2ceil(16), 4);
+  ASSERT_EQ(log2ceil(17), 5);
+  // ...
+  ASSERT_EQ(log2ceil((std::numeric_limits<std::size_t>::max() / 2) + 1),
+            8 * sizeof(std::size_t) - 1);
+}
+
+TEST_F(InternalMerkleTreeTest, test_minimumFactorFor) {
+  // test error on target <= current
+  ASSERT_THROW(minimumFactorFor(2, 1), std::invalid_argument);
+
+  // test error on current not power of 2
+  ASSERT_THROW(minimumFactorFor(3, 4), std::invalid_argument);
+
+  // test valid inputs
+  ASSERT_EQ(minimumFactorFor(1, 2), 2);
+  ASSERT_EQ(minimumFactorFor(1, 4), 4);
+  ASSERT_EQ(minimumFactorFor(1, 12), 16);
+  ASSERT_EQ(minimumFactorFor(1, 16), 16);
+  ASSERT_EQ(minimumFactorFor(2, 4), 2);
+  ASSERT_EQ(minimumFactorFor(2, 5), 4);
+  ASSERT_EQ(minimumFactorFor(2, 8), 4);
+  ASSERT_EQ(minimumFactorFor(2, 17), 16);
+  ASSERT_EQ(minimumFactorFor(2, 31), 16);
+  ASSERT_EQ(minimumFactorFor(2, 32), 16);
+  ASSERT_EQ(minimumFactorFor(65536, 90000), 2);
+  ASSERT_EQ(minimumFactorFor(8192, 2147483600), 262144);
+}
+
+TEST_F(InternalMerkleTreeTest, test_nodeCountAtDepth) {
+  ASSERT_EQ(nodeCountAtDepth(0), 1);
+  ASSERT_EQ(nodeCountAtDepth(1), 8);
+  ASSERT_EQ(nodeCountAtDepth(2), 64);
+  ASSERT_EQ(nodeCountAtDepth(3), 512);
+  // ...
+  ASSERT_EQ(nodeCountAtDepth(10), 1073741824);
+}
+
+TEST_F(InternalMerkleTreeTest, test_nodeCountUpToDepth) {
+  ASSERT_EQ(nodeCountUpToDepth(0), 1);
+  ASSERT_EQ(nodeCountUpToDepth(1), 9);
+  ASSERT_EQ(nodeCountUpToDepth(2), 73);
+  ASSERT_EQ(nodeCountUpToDepth(3), 585);
+  // ...
+  ASSERT_EQ(nodeCountUpToDepth(10), 1227133513);
+}
+
+TEST_F(InternalMerkleTreeTest, test_index) {
+  std::pair<std::size_t, std::size_t> range = this->range();
+  ASSERT_EQ(range.first, 0);
+  ASSERT_EQ(range.second, 64);
+
+  // make sure depth 0 always gets us 0
+  ASSERT_EQ(index(0, 0), 0);
+  ASSERT_EQ(index(63, 0), 0);
+
+  // check boundaries at level 1
+  for (std::size_t chunk = 0; chunk < 8; ++chunk) {
+    std::size_t left = chunk * 8;
+    std::size_t right = ((chunk + 1) * 8) - 1;
+    ASSERT_EQ(index(left, 1), chunk + 1);
+    ASSERT_EQ(index(right, 1), chunk + 1);
+  }
+
+  // check boundaries at level 2
+  for (std::size_t chunk = 0; chunk < 64; ++chunk) {
+    std::size_t left = chunk;  // only one value per chunk
+    ASSERT_EQ(index(left, 2), chunk + 9);
+  }
+}
+
+TEST_F(InternalMerkleTreeTest, test_modify) {
+  ASSERT_EQ(nodeCountAtDepth(2), 64);
+  std::pair<std::size_t, std::size_t> range = this->range();
+  ASSERT_EQ(range.first, 0);
+  ASSERT_EQ(range.second, 64);
+
+  ASSERT_EQ(count(), 0);
+  // check that an attempt to remove will fail if it's empty
+  ASSERT_THROW(modify(0, 1, false), std::invalid_argument);
+  ASSERT_EQ(count(), 0);
+
+  // insert a single value
+  modify(0, 1, true);
+  ASSERT_EQ(count(), 1);
+
+  // build set of indexes that should be touched
+  std::set<std::size_t> indices0;
+  for (std::size_t depth = 0; depth <= meta().maxDepth; ++depth) {
+    indices0.emplace(this->index(0, depth));
+  }
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    bool inSet0 = indices0.find(index) != indices0.end();
+    std::size_t expectedCount = inSet0 ? 1 : 0;
+    std::size_t expectedHash = inSet0 ? 1 : 0;
+
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, expectedCount);
+    ASSERT_EQ(node.hash, expectedHash);
+  }
+
+  // insert another value, minimal overlap
+  modify(63, 64, true);
+  ASSERT_EQ(count(), 2);
+
+  // build set of indexes that should be touched
+  std::set<std::size_t> indices63;
+  for (std::size_t depth = 0; depth <= meta().maxDepth; ++depth) {
+    indices63.emplace(this->index(63, depth));
+  }
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    bool inSet0 = indices0.find(index) != indices0.end();
+    std::size_t expectedCount = inSet0 ? 1 : 0;
+    std::size_t expectedHash = inSet0 ? 1 : 0;
+    if (indices63.find(index) != indices63.end()) {
+      ++expectedCount;
+      expectedHash ^= 64;
+    }
+
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, expectedCount);
+    ASSERT_EQ(node.hash, expectedHash);
+  }
+
+  // insert another value, more overlap
+  modify(1, 2, true);
+  ASSERT_EQ(count(), 3);
+
+  // build set of indexes that should be touched
+  std::set<std::size_t> indices1;
+  for (std::size_t depth = 0; depth <= meta().maxDepth; ++depth) {
+    indices1.emplace(this->index(1, depth));
+  }
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    bool inSet0 = indices0.find(index) != indices0.end();
+    std::size_t expectedCount = inSet0 ? 1 : 0;
+    std::size_t expectedHash = inSet0 ? 1 : 0;
+    if (indices1.find(index) != indices1.end()) {
+      ++expectedCount;
+      expectedHash ^= 2;
+    }
+    if (indices63.find(index) != indices63.end()) {
+      ++expectedCount;
+      expectedHash ^= 64;
+    }
+
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, expectedCount);
+    ASSERT_EQ(node.hash, expectedHash);
+  }
+
+  // remove a value, minimal overlap
+  modify(63, 64, false);
+  ASSERT_EQ(count(), 2);
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    bool inSet0 = indices0.find(index) != indices0.end();
+    std::size_t expectedCount = inSet0 ? 1 : 0;
+    std::size_t expectedHash = inSet0 ? 1 : 0;
+    if (indices1.find(index) != indices1.end()) {
+      ++expectedCount;
+      expectedHash ^= 2;
+    }
+
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, expectedCount);
+    ASSERT_EQ(node.hash, expectedHash);
+  }
+
+  // remove a value, maximal overlap
+  modify(1, 2, false);
+  ASSERT_EQ(count(), 1);
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    bool inSet0 = indices0.find(index) != indices0.end();
+    std::size_t expectedCount = inSet0 ? 1 : 0;
+    std::size_t expectedHash = inSet0 ? 1 : 0;
+
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, expectedCount);
+    ASSERT_EQ(node.hash, expectedHash);
+  }
+
+  // remove a value, maximal overlap
+  modify(0, 1, false);
+  ASSERT_EQ(count(), 0);
+
+  // check that it sets everything it should, and nothing it shouldn't
+  for (std::size_t index = 0; index < nodeCountUpToDepth(meta().maxDepth); ++index) {
+    InternalMerkleTreeTest::Node& node = this->node(index);
+    ASSERT_EQ(node.count, 0);
+    ASSERT_EQ(node.hash, 0);
+  }
+}
+
+TEST_F(InternalMerkleTreeTest, test_grow) {
+  ASSERT_EQ(nodeCountAtDepth(1), 8);
+  ASSERT_EQ(nodeCountAtDepth(2), 64);
+
+  std::pair<std::size_t, std::size_t> range = this->range();
+  ASSERT_EQ(range.first, 0);
+  ASSERT_EQ(range.second, 64);
+
+  // fill the tree, but not enough that it grows
+  for (std::size_t i = 0; i < 64; ++i) {
+    insert(i, i + 1024);
+  }
+  range = this->range();
+  ASSERT_EQ(range.first, 0);
+  ASSERT_EQ(range.second, 64);
+
+  // check that tree state is as expected prior to growing
+  {
+    std::size_t hash0 = 0;
+    std::size_t hash1[8] = {0};
+    std::size_t hash2[64] = {0};
+    for (std::size_t i = 0; i < 64; ++i) {
+      hash0 ^= i + 1024;
+      hash1[i / 8] ^= i + 1024;
+      hash2[i] ^= i + 1024;
+    }
+    for (std::size_t i = 0; i < 64; ++i) {
+      Node& node = this->node(this->index(i, 2));
+      ASSERT_EQ(node.count, 1);
+      ASSERT_EQ(node.hash, hash2[i]);
+    }
+    for (std::size_t i = 0; i < 8; ++i) {
+      Node& node = this->node(i + 1);
+      ASSERT_EQ(node.count, 8);
+      ASSERT_EQ(node.hash, hash1[i]);
+    }
+    {
+      Node& node = this->node(0);
+      ASSERT_EQ(node.count, 64);
+      ASSERT_EQ(node.hash, hash0);
+    }
+  }
+
+  // insert some more and cause it to grow
+  for (std::size_t i = 64; i < 128; ++i) {
+    insert(i, i + 1024);
+  }
+  range = this->range();
+  ASSERT_EQ(range.first, 0);
+  ASSERT_EQ(range.second, 128);
+
+  // check that tree state is as expected after growing
+  {
+    std::size_t hash0 = 0;
+    std::size_t hash1[8] = {0};
+    std::size_t hash2[64] = {0};
+    for (std::size_t i = 0; i < 128; ++i) {
+      hash0 ^= i + 1024;
+      hash1[i / 8] ^= i + 1024;
+      hash2[i / 2] ^= i + 1024;
+    }
+    for (std::size_t i = 0; i < 64; ++i) {
+      Node& node = this->node(this->index(i, 2));
+      ASSERT_EQ(node.count, 2);
+      ASSERT_EQ(node.hash, hash2[i]);
+    }
+    for (std::size_t i = 0; i < 8; ++i) {
+      Node& node = this->node(i + 1);
+      ASSERT_EQ(node.count, 8);
+      ASSERT_EQ(node.hash, hash1[i]);
+    }
+    {
+      Node& node = this->node(0);
+      ASSERT_EQ(node.count, 64);
+      ASSERT_EQ(node.hash, hash0);
+    }
+  }
+}
