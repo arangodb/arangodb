e42a7a240573e510296ae81d25e5d1946939922f
jsteemann <jan@arangodb.com>
2019-10-16 11:27:08 +0200

    reactivate all other tests again

--- tests/js/client/shell/shell-transaction.js
+++ tests/js/client/shell/shell-transaction.js
@@ -4207,10 +4207,9 @@ function transactionTTLStreamSuite () {
 // / @brief executes the test suites
 // //////////////////////////////////////////////////////////////////////////////
 
-//jsunity.run(transactionRevisionsSuite);
-//jsunity.run(transactionRollbackSuite);
+jsunity.run(transactionRevisionsSuite);
+jsunity.run(transactionRollbackSuite);
 jsunity.run(transactionInvocationSuite);
-/*
 jsunity.run(transactionCollectionsSuite);
 jsunity.run(transactionOperationsSuite);
 jsunity.run(transactionBarriersSuite);
@@ -4219,5 +4218,5 @@ jsunity.run(transactionCrossCollectionSuite);
 jsunity.run(transactionTraversalSuite);
 jsunity.run(transactionAQLStreamSuite);
 jsunity.run(transactionTTLStreamSuite);
-*/
+
 return jsunity.done();
