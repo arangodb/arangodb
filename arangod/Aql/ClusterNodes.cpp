02760bff6788d02b727b8e15838a21e9242ef845
Simon Grätzer <simon@graetzer.org>
2019-10-22 12:48:18 +0200

    ffffff

--- arangod/Aql/ClusterNodes.cpp
+++ arangod/Aql/ClusterNodes.cpp
@@ -450,7 +450,7 @@ void GatherNode::toVelocyPackHelper(VPackBuilder& nodes, unsigned flags,
     }
   }
   
-  nodes.add("isParallizable", VPackValue(isParallelizable()));
+  nodes.add("isParallelizable", VPackValue(isParallelizable()));
 
   // And close it:
   nodes.close();
