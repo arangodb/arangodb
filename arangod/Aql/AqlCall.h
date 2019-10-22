35fe0e5d9a98afa13a2db58af4a038cdc8f30dff
Michael Hackstein <michael@arangodb.com>
2019-10-22 16:43:24 +0200

    Added first draft of AqlCallObject

---
+++ arangod/Aql/AqlCall.h
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_AQL_AQL_CALL_H
+#define ARANGOD_AQL_AQL_CALL_H 1
+
+#include "Aql/ExecutionBlock.h"
+
+#include <cstddef>
+#include <variant>
+
+namespace arangodb {
+namespace aql {
+struct AqlCall {
+  class Infinity {};
+  using Limit = std::variant<size_t, Infinity>;
+
+  std::size_t offset{0};
+  // TODO: The defaultBatchSize function could move into this file instead
+  Limit batchSize{ExecutionBlock::DefaultBatchSize()};
+  Limit limit{Infinity{}};
+  bool fullCount{false};
+};
+
+}  // namespace aql
+}  // namespace arangodb
+
+#endif
