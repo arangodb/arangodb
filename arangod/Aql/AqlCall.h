a46941ed34d7284a8f2063e682ef1213e11feb82
Michael Hackstein <michael@arangodb.com>
2019-10-25 09:12:38 +0200

    Added heplper functions to the AqlCall struct to avoid mangeling around with variants everywhere.

--- arangod/Aql/AqlCall.h
+++ arangod/Aql/AqlCall.h
@@ -36,9 +36,42 @@ struct AqlCall {
 
   std::size_t offset{0};
   // TODO: The defaultBatchSize function could move into this file instead
-  Limit batchSize{ExecutionBlock::DefaultBatchSize()};
-  Limit limit{Infinity{}};
+  Limit softLimit{Infinity{}};
+  Limit hardLimit{Infinity{}};
   bool fullCount{false};
+
+  std::size_t getOffset() const { return offset; }
+
+  std::size_t getLimit() const {
+    // By default we use batchsize
+    std::size_t limit = ExecutionBlock::DefaultBatchSize();
+    // We are not allowed to go above softLimit
+    if (std::holds_alternative<std::size_t>(softLimit)) {
+      limit = (std::min)(std::get<std::size_t>(softLimit), limit);
+    }
+    // We are not allowed to go above hardLimit
+    if (std::holds_alternative<std::size_t>(hardLimit)) {
+      limit = (std::min)(std::get<std::size_t>(hardLimit), limit);
+    }
+    return limit;
+  }
+
+  void didProduce(std::size_t n) {
+    if (std::holds_alternative<std::size_t>(softLimit)) {
+      TRI_ASSERT(n <= std::get<std::size_t>(softLimit));
+      softLimit = std::get<std::size_t>(softLimit) - n;
+    }
+    if (std::holds_alternative<std::size_t>(hardLimit)) {
+      TRI_ASSERT(n <= std::get<std::size_t>(hardLimit));
+      hardLimit = std::get<std::size_t>(hardLimit) - n;
+    }
+  }
+
+  bool hasHardLimit() const {
+    return !std::holds_alternative<AqlCall::Infinity>(hardLimit);
+  }
+
+  bool needsFullCount() const { return fullCount; }
 };
 
 }  // namespace aql
