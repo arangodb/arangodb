6c2d1f02a7823c80f15228bbd98308c0ebe7a14d
Kaveh Vahedipour <kaveh@arangodb.com>
2019-11-04 12:47:25 +0100

    assertion on compatibility, when timestamp missing

--- arangod/Agency/State.cpp
+++ arangod/Agency/State.cpp
@@ -423,7 +423,7 @@ index_t State::logFollower(query_t const& transactions) {
         tstamp = slice.get("timestamp").getUInt();
       }
       if(tstamp == 0) {
-        duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        tstamp = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
       }
 
       bool reconfiguration = query.keyAt(0).isEqualString(RECONFIGURE);
