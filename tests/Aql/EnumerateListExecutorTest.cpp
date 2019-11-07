feb350d481c2a7641799afa47186357c5f465bb3
hkernbach <heiko.kernbach@gmail.com>
2019-11-07 13:14:40 +0100

    produce inputRange test

--- tests/Aql/EnumerateListExecutorTest.cpp
+++ tests/Aql/EnumerateListExecutorTest.cpp
@@ -28,15 +28,18 @@
 #include "RowFetcherHelper.h"
 #include "fakeit.hpp"
 
+#include "Aql/AqlCall.h"
 #include "Aql/AqlItemBlock.h"
 #include "Aql/EnumerateListExecutor.h"
 #include "Aql/ExecutionBlockImpl.h"
 #include "Aql/OutputAqlItemRow.h"
 #include "Aql/ResourceUsage.h"
 #include "Aql/Stats.h"
+#include "AqlItemBlockHelper.h"
 #include "Transaction/Context.h"
 #include "Transaction/Methods.h"
 
+#include <Logger/LogMacros.h>
 #include <velocypack/Builder.h>
 #include <velocypack/velocypack-aliases.h>
 
@@ -62,7 +65,8 @@ TEST_F(EnumerateListExecutorTest, there_are_no_rows_upstream_the_producer_does_n
   SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 2)};
   VPackBuilder input;
 
-  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(itemBlockManager, input.steal(), false);
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, input.steal(), false);
   EnumerateListExecutor testee(fetcher, infos);
   // Use this instead of std::ignore, so the tests will be noticed and
   // updated when someone changes the stats type in the return value of
@@ -75,12 +79,14 @@ TEST_F(EnumerateListExecutorTest, there_are_no_rows_upstream_the_producer_does_n
   ASSERT_EQ(state, ExecutionState::DONE);
   ASSERT_FALSE(result.produced());
 }
+
 TEST_F(EnumerateListExecutorTest, there_are_no_rows_upstream_the_producer_waits) {
   EnumerateListExecutorInfos infos(0, 1, 1, 2, {}, {0});
   SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 2)};
   VPackBuilder input;
 
-  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(itemBlockManager, input.steal(), true);
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, input.steal(), true);
   EnumerateListExecutor testee(fetcher, infos);
   // Use this instead of std::ignore, so the tests will be noticed and
   // updated when someone changes the stats type in the return value of
@@ -103,7 +109,8 @@ TEST_F(EnumerateListExecutorTest, there_is_one_row_in_the_upstream_the_producer_
   SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 5)};
   auto input = VPackParser::fromJson("[ [1, 2, 3, [true, true, true]] ]");
 
-  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(itemBlockManager, input->steal(), true);
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, input->steal(), true);
   EnumerateListExecutor testee(fetcher, infos);
   // Use this instead of std::ignore, so the tests will be noticed and
   // updated when someone changes the stats type in the return value of
@@ -188,7 +195,8 @@ TEST_F(EnumerateListExecutorTest, there_is_one_empty_array_row_in_the_upstream_t
   SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 5)};
   auto input = VPackParser::fromJson("[ [1, 2, 3, [] ] ]");
 
-  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(itemBlockManager, input->steal(), true);
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, input->steal(), true);
   EnumerateListExecutor testee(fetcher, infos);
   // Use this instead of std::ignore, so the tests will be noticed and
   // updated when someone changes the stats type in the return value of
@@ -216,7 +224,8 @@ TEST_F(EnumerateListExecutorTest, there_are_rows_in_the_upstream_the_producer_wa
   auto input = VPackParser::fromJson(
       "[ [1, 2, 3, [true, true, true]], [1, 2, 3, [true, true, true]] ]");
 
-  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(itemBlockManager, input->steal(), true);
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, input->steal(), true);
   EnumerateListExecutor testee(fetcher, infos);
   // Use this instead of std::ignore, so the tests will be noticed and
   // updated when someone changes the stats type in the return value of
@@ -337,6 +346,70 @@ TEST_F(EnumerateListExecutorTest, there_are_rows_in_the_upstream_the_producer_wa
   ASSERT_TRUE(v.isBoolean());
   ASSERT_TRUE(v.toBoolean());
 }
+
+TEST_F(EnumerateListExecutorTest, test_produce_datarange) {
+  // This fetcher will not be called!
+  // After Execute is done this fetcher shall be removed, the Executor does not need it anymore!
+  auto fakeUnusedBlock = VPackParser::fromJson("[  ]");
+  SingleRowFetcherHelper<::arangodb::aql::BlockPassthrough::Disable> fetcher(
+      itemBlockManager, fakeUnusedBlock->steal(), false);
+
+  // SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 5)};
+  // This is the relevant part of the test
+  SharedAqlItemBlockPtr block{new AqlItemBlock(itemBlockManager, 1000, 5)};
+  EnumerateListExecutorInfos infos(3, 4, 4, 5, {}, {0, 1, 2, 3});
+  EnumerateListExecutor testee(fetcher, infos);
+  SharedAqlItemBlockPtr inBlock =
+      buildBlock<4>(itemBlockManager, {{{{1}, {2}, {3}, {R"([true, 1, 2])"}}}});
+
+  AqlItemBlockInputRange input{ExecutorState::DONE, inBlock, 0, inBlock->size()};
+  OutputAqlItemRow output(std::move(block), infos.getOutputRegisters(),
+                          infos.registersToKeep(), infos.registersToClear());
+
+  EXPECT_EQ(output.numRowsWritten(), 0);
+  auto const [state, stats, call] = testee.produceRows(1000, input, output);
+  EXPECT_EQ(state, ExecutorState::DONE);
+  EXPECT_EQ(output.numRowsWritten(), 3);
+  EXPECT_FALSE(input.hasMore());
+
+  block = output.stealBlock();
+  // check registers that should be kept
+  AqlValue v = block->getValue(0, 0);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 1);
+
+  v = block->getValue(1, 0);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 1);
+
+  v = block->getValue(1, 1);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 2);
+
+  v = block->getValue(1, 2);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 3);
+
+  bool mustDestroy = false;
+  v = block->getValue(1, 3);
+  ASSERT_TRUE(v.isArray());
+  ASSERT_TRUE(v.at(0, mustDestroy, false).toBoolean());
+  ASSERT_TRUE(v.at(1, mustDestroy, false).toBoolean());
+  ASSERT_TRUE(v.at(2, mustDestroy, false).toBoolean());
+
+  // check registers that should be filled
+  v = block->getValue(0, 4);
+  ASSERT_TRUE(v.isBoolean());
+  ASSERT_TRUE(v.toBoolean());
+
+  v = block->getValue(1, 4);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 1);
+
+  v = block->getValue(2, 4);
+  ASSERT_TRUE(v.isNumber());
+  ASSERT_EQ(v.toInt64(), 2);
+}  // namespace aql
 
 }  // namespace aql
 }  // namespace tests
