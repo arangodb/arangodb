90a4db9440751a90311393e144865161a3c288a3
Jan <jsteemann@users.noreply.github.com>
2019-11-08 19:54:02 +0100

    do not materialize huge ranges (#10376)

--- arangod/Aql/AqlValue.cpp
+++ arangod/Aql/AqlValue.cpp
@@ -910,6 +910,7 @@ v8::Handle<v8::Value> AqlValue::toV8(v8::Isolate* isolate, transaction::Methods*
     }
     case RANGE: {
       size_t const n = _data.range->size();
+      Range::throwIfTooBigForMaterialization(n);
       v8::Handle<v8::Array> result = v8::Array::New(isolate, static_cast<int>(n));
 
       for (uint32_t i = 0; i < n; ++i) {
@@ -967,8 +968,9 @@ void AqlValue::toVelocyPack(transaction::Methods* trx, arangodb::velocypack::Bui
       break;
     }
     case RANGE: {
-      builder.openArray();
+      builder.openArray(true);
       size_t const n = _data.range->size();
+      Range::throwIfTooBigForMaterialization(n);
       for (size_t i = 0; i < n; ++i) {
         builder.add(VPackValue(_data.range->at(i)));
       }
