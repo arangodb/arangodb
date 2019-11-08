fd33b7f4204693702a33fc3bdb63d549eb7215b3
hkernbach <heiko.kernbach@gmail.com>
2019-11-08 19:25:36 +0100

    Revert "working - not done yet"
    This reverts commit aa275b48ffc2c8fc9a3c364e2241321ec9c9293f.

--- arangod/Aql/TraversalExecutor.h
+++ arangod/Aql/TraversalExecutor.h
@@ -38,8 +38,6 @@ class Traverser;
 
 namespace aql {
 
-struct AqlCall;
-class AqlItemBlockInputRange;
 class Query;
 class OutputAqlItemRow;
 class ExecutorInfos;
@@ -140,15 +138,6 @@ class TraversalExecutor {
    */
   std::pair<ExecutionState, Stats> produceRows(OutputAqlItemRow& output);
 
-  /**
-   * @brief produce the next Row of Aql Values.
-   *
-   * @return ExecutorState, the stats, and a new Call that needs to be send to upstream
-   */
-  std::tuple<ExecutorState, Stats, AqlCall> produceRows(size_t limit,
-                                                        AqlItemBlockInputRange& inputRange,
-                                                        OutputAqlItemRow& output);
-
  private:
   /**
    * @brief compute the return state
@@ -157,7 +146,6 @@ class TraversalExecutor {
   ExecutionState computeState() const;
 
   bool resetTraverser();
-  bool resetTraverser(InputAqlItemRow const& input);
 
  private:
   Infos& _infos;
