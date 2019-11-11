b853a7b5d391662a1e5c70db005302e466f99364
Kaveh Vahedipour <kaveh@arangodb.com>
2019-11-11 08:53:31 +0100

    rest handler

---
+++ arangod/RestHandler/RestMetricsHandler.cpp
@@ -0,0 +1,73 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2016 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Dr. Frank Celler
+////////////////////////////////////////////////////////////////////////////////
+
+#include "RestMetricsHandler.h"
+
+#include "Agency/AgencyComm.h"
+#include "Agency/AgencyFeature.h"
+#include "Agency/Agent.h"
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "Cluster/ServerState.h"
+#include "GeneralServer/ServerSecurityFeature.h"
+#include "Rest/Version.h"
+#include "RestServer/ServerFeature.h"
+
+#if defined(TRI_HAVE_POSIX_THREADS)
+#include <unistd.h>
+#endif
+
+#include <velocypack/Builder.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::basics;
+using namespace arangodb::rest;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief ArangoDB server
+////////////////////////////////////////////////////////////////////////////////
+
+RestMetricsHandler::RestMetricsHandler(
+  application_features::ApplicationServer& server,
+  GeneralRequest* request, GeneralResponse* response)
+  : RestBaseHandler(server, request, response) {}
+
+RestStatus RestMetricsHandler::execute() {
+  auto& server = application_features::ApplicationServer::server();
+  ServerSecurityFeature& security = server.getFeature<ServerSecurityFeature>();
+
+  if (!security.canAccessHardenedApi()) {
+    // dont leak information about server internals here
+    generateError(rest::ResponseCode::FORBIDDEN, TRI_ERROR_FORBIDDEN); 
+    return RestStatus::DONE;
+  }
+
+  MetricsFeature& metrics = server.getFeature<MetricsFeature>();
+
+  VPackBuilder result; {
+    VPackObjectBuilder o(&result); 
+    metrics.toBuilder(result); }
+  
+  generateResult(rest::ResponseCode::OK, result.slice());
+  return RestStatus::DONE;
+}
