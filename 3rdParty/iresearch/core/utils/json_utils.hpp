82b78c49e9397425a5c04730d6c53a0c455f5279
Andrey Abramov <andrey@arangodb.com>
2019-11-20 19:02:25 +0300

    add missing files

---
+++ 3rdParty/iresearch/core/utils/json_utils.hpp
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2019 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Andrey Abramov
+/// @author Vasiliy Nabatchikov
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef IRESEARCH_JSON_UTILS_H
+#define IRESEARCH_JSON_UTILS_H
+
+#include <rapidjson/rapidjson/document.h>
+#include "string.hpp"
+
+NS_ROOT
+
+inline bool get_uint64(
+    rapidjson::Document const& json,
+    const irs::string_ref& name,
+    uint64_t& value
+) {
+  if (!json.HasMember(name.c_str())) {
+    return false;
+  }
+
+  const auto& attr = json[name.c_str()];
+
+  if (!attr.IsNumber()) {
+    return false;
+  }
+
+  value = attr.GetUint64();
+  return true;
+}
+
+inline bool get_bool(
+    rapidjson::Document const& json,
+    const irs::string_ref& name,
+    bool& value
+) {
+  if (!json.HasMember(name.c_str())) {
+    return false;
+  }
+
+  const auto& attr = json[name.c_str()];
+
+  if (!attr.IsBool()) {
+    return false;
+  }
+
+  value = attr.GetBool();
+  return true;
+}
+
+NS_END
+
+#endif // IRESEARCH_JSON_UTILS_H
