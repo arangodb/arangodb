c4a4f6154d3b068bb45d7fb1386983c25262e644
Max Neunhöffer <max@arangodb.com>
2019-11-20 18:06:40 +0100

    Fix vst network traffic accounting. (#10324)

--- arangod/GeneralServer/VstCommTask.cpp
+++ arangod/GeneralServer/VstCommTask.cpp
@@ -328,8 +328,6 @@ bool VstCommTask::processRead(double startTime) {
     RequestStatistics::SET_READ_START(stat, startTime);
   }
 
-  RequestStatistics::SET_READ_END(statistics(chunkHeader._messageID));
-
   if (chunkHeader._isFirst && chunkHeader._chunk == 1) {
     // CASE 1: message is in one chunk
     if (!getMessageFromSingleChunk(chunkHeader, message, doExecute, vpackBegin, chunkEnd)) {
@@ -352,6 +350,12 @@ bool VstCommTask::processRead(double startTime) {
   }
 
   if (doExecute) {
+    // A message is complete! Now we need to do the accounting, first let's
+    // set the "read-end time":
+    RequestStatistics* stat = statistics(message.id());
+    RequestStatistics::SET_READ_END(stat);
+    RequestStatistics::ADD_RECEIVED_BYTES(stat, message.totalSize());
+
     VPackSlice header = message.header();
 
     if (Logger::logRequestParameters()) {
