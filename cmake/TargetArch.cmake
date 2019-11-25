3cc0704c1a348429a48e095f02468fdf616e5be6
maoxiaochuan <maoxiaochuan-gz@loongson.cn>
2019-11-25 14:42:29 +0800

    add mips64 support, test on loongson 3A3000

--- cmake/TargetArch.cmake
+++ cmake/TargetArch.cmake
@@ -30,6 +30,8 @@ set(archdetect_c_code "
     #endif
 #elif defined(__aarch64__)
     #error cmake_ARCH aarch64
+#elif defined(__mips64)
+    #error cmake_ARCH mips64
 #elif defined(__i386) || defined(__i386__) || defined(_M_IX86)
     #error cmake_ARCH i386
 #elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(_M_X64)
