2c6c121ac0c89ce66ab18e91cb4f4e01ee2a9a87
Tobias Gödderz <tobias@arangodb.com>
2019-11-25 12:50:00 +0100

    Bugfix

--- arangod/Aql/ExecutionPlan.cpp
+++ arangod/Aql/ExecutionPlan.cpp
@@ -1190,7 +1190,7 @@ ExecutionNode* ExecutionPlan::fromNodeLet(ExecutionNode* previous, AstNode const
       // the LET. and don't create the LET
 
       subquery->replaceOutVariable(v);
-      return subquery;
+      return previous;
     }
     // otherwise fall-through to normal behavior
 
