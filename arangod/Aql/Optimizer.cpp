97c6a747f88747522d5e6be90e0e9f1044786b97
Tobias Gödderz <tobias@arangodb.com>
2019-11-26 14:51:15 +0100

    Fixed assert

--- arangod/Aql/Optimizer.cpp
+++ arangod/Aql/Optimizer.cpp
@@ -87,6 +87,7 @@ class CheckPlan : public WalkerWorker<ExecutionNode> {
       case ExecutionNode::REPLACE:
       case ExecutionNode::UPSERT:
       case ExecutionNode::REMOVE:
+      case ExecutionNode::GATHER:
         if (node->getParents().size() > 1) {
           emplaceError() << "#parents == " << node->getParents().size() << " at [" << node->id() << "] " << node->getTypeString();
           ok = false;
