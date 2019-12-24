a254eac4e4ed7193cf68bacfb7e9de323c5393aa
Andrei Lobov <andrei.lobov@arangodb.com>
2019-12-24 15:46:35 +0300

    Updated fix for 3.4

--- 3rdParty/iresearch/core/index/index_writer.cpp
+++ 3rdParty/iresearch/core/index/index_writer.cpp
@@ -1867,7 +1867,7 @@ index_writer::pending_context_t index_writer::flush_all() {
         // if this is pending consolidation, 
         // this segment is already in the mask (see assert below)
         // new version will be created. Remove old version from cache!
-        ctx->segment_mask_.emplace(pending_segment.segment.meta);
+        ctx->segment_mask_.emplace(pending_segment.segment.meta.name);
       }
       assert(ctx->segment_mask_.find(pending_segment.segment.meta) != ctx->segment_mask_.end()); // this segment should be deleted from cache!
       write_document_mask(dir, pending_segment.segment.meta, docs_mask, !pending_consolidation);
