5fcbb3e2e3ebc1ff86ad65e41a2de2f25c7ab168
jsteemann <jan@arangodb.com>
2020-01-17 14:50:35 +0100

    add include

--- arangod/Indexes/Index.cpp
+++ arangod/Indexes/Index.cpp
@@ -33,6 +33,7 @@
 #include "Aql/Ast.h"
 #include "Aql/AstNode.h"
 #include "Aql/Variable.h"
+#include "Basics/AttributeNameParser.h"
 #include "Basics/Exceptions.h"
 #include "Basics/StaticStrings.h"
 #include "Basics/StringUtils.h"
