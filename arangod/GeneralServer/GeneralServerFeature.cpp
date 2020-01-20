7e1fdcf6c70f5027317f488af7541cfe5e845977
Kaveh Vahedipour <kaveh@arangodb.com>
2020-01-20 16:16:55 +0100

    3.6 merge

--- arangod/GeneralServer/GeneralServerFeature.cpp
+++ arangod/GeneralServer/GeneralServerFeature.cpp
@@ -83,6 +83,7 @@
 #include "RestHandler/RestShutdownHandler.h"
 #include "RestHandler/RestSimpleHandler.h"
 #include "RestHandler/RestSimpleQueryHandler.h"
+#include "RestHandler/RestSystemReportHandler.h"
 #include "RestHandler/RestStatusHandler.h"
 #include "RestHandler/RestTasksHandler.h"
 #include "RestHandler/RestTestHandler.h"
@@ -494,6 +495,9 @@ void GeneralServerFeature::defineHandlers() {
 
   _handlerFactory->addHandler("/_admin/status",
                               RestHandlerCreator<RestStatusHandler>::createNoData);
+
+  _handlerFactory->addHandler("/_admin/system-report",
+                              RestHandlerCreator<RestSystemReportHandler>::createNoData);
 
   _handlerFactory->addPrefixHandler("/_admin/job",
                                     RestHandlerCreator<arangodb::RestJobHandler>::createData<AsyncJobManager*>,
