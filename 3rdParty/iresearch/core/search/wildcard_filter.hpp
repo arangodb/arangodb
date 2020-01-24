2c1e404c65b53ba318e18124d2e7afe4225dff25
Andrey Abramov <andrey@arangodb.com>
2020-01-24 23:29:32 +0300

    hopefully last attempt

--- 3rdParty/iresearch/core/search/wildcard_filter.hpp
+++ 3rdParty/iresearch/core/search/wildcard_filter.hpp
@@ -64,16 +64,6 @@ class IRESEARCH_API by_wildcard final : public by_prefix {
     boost_t boost,
     const attribute_view& ctx
   ) const override;
-
-  //////////////////////////////////////////////////////////////////////////////
-  /// @brief the maximum number of most frequent terms to consider for scoring
-  //////////////////////////////////////////////////////////////////////////////
-  by_wildcard& scored_terms_limit(size_t limit) noexcept {
-    by_prefix::scored_terms_limit(limit);
-    return *this;
-  }
-
-  using by_prefix::scored_terms_limit;
 }; // by_wildcard
 
 #endif // IRESEARCH_WILDCARD_FILTER_H
