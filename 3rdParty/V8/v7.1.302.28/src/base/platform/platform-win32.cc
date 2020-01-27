4a5c313d644a1c5cb16e9e5df6794d28de213641
Wilfried Goesgens <willi@arangodb.com>
2020-01-27 16:46:20 +0100

    not yet defined in older versions

--- 3rdParty/V8/v7.1.302.28/src/base/platform/platform-win32.cc
+++ 3rdParty/V8/v7.1.302.28/src/base/platform/platform-win32.cc
@@ -759,12 +759,16 @@ DWORD GetProtectionFromMemoryPermission(OS::MemoryPermission access) {
     case OS::MemoryPermission::kReadWrite:
       return PAGE_READWRITE;
     case OS::MemoryPermission::kReadWriteExecute:
+#if 0
       if (IsWindows10OrGreater())
         return PAGE_EXECUTE_READWRITE | PAGE_TARGETS_INVALID;
+#endif // TODO
       return PAGE_EXECUTE_READWRITE;
     case OS::MemoryPermission::kReadExecute:
+#if 0
       if (IsWindows10OrGreater())
         return PAGE_EXECUTE_READ | PAGE_TARGETS_INVALID;
+#endif // TODO
       return PAGE_EXECUTE_READ;
   }
   UNREACHABLE();
