d4295c5ec2d8e285c87b8596d223355022408def
hkernbach <heiko.kernbach@gmail.com>
2020-02-14 17:51:20 +0100

    added EnumerateCollection as new style executor

--- arangod/Aql/ExecutionBlockImpl.cpp
+++ arangod/Aql/ExecutionBlockImpl.cpp
@@ -132,7 +132,7 @@ constexpr bool is_one_of_v = (std::is_same_v<T, Es> || ...);
 template <typename Executor>
 constexpr bool isNewStyleExecutor =
     is_one_of_v<Executor, FilterExecutor, SortedCollectExecutor, IdExecutor<ConstFetcher>,
-                IdExecutor<SingleRowFetcher<BlockPassthrough::Enable>>, ReturnExecutor, IndexExecutor,
+                IdExecutor<SingleRowFetcher<BlockPassthrough::Enable>>, ReturnExecutor, IndexExecutor, EnumerateCollectionExeutor,
 #ifdef ARANGODB_USE_GOOGLE_TESTS
                 TestLambdaExecutor,
                 TestLambdaSkipExecutor,  // we need one after these to avoid compile errors in non-test mode
@@ -1045,7 +1045,7 @@ static SkipRowsRangeVariant constexpr skipRowsType() {
                 "Fetcher is chosen for skipping, but has not skipRows method!");
 
   static_assert(useExecutor ==
-                    (is_one_of_v<Executor, FilterExecutor, ShortestPathExecutor, ReturnExecutor, IndexExecutor,
+                    (is_one_of_v<Executor, FilterExecutor, ShortestPathExecutor, ReturnExecutor, IndexExecutor, EnumerateCollectionExecutor,
 #ifdef ARANGODB_USE_GOOGLE_TESTS
                                  TestLambdaSkipExecutor,
 #endif
