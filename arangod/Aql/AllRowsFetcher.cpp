ffc7770302461fdf9ca7ae1768c67aab17ce6573
hkernbach <heiko.kernbach@gmail.com>
2020-02-25 15:12:57 +0100

    added first implementation of execute - AllRowsFetcher

--- arangod/Aql/AllRowsFetcher.cpp
+++ arangod/Aql/AllRowsFetcher.cpp
@@ -59,6 +59,48 @@ std::pair<ExecutionState, AqlItemMatrix const*> AllRowsFetcher::fetchAllRows() {
   return {ExecutionState::DONE, nullptr};
 }
 
+std::tuple<ExecutionState, size_t, AqlItemBlockInputMatrix> AllRowsFetcher::execute(AqlCallStack& stack) {
+  if (!stack.isRelevant()) {
+    auto [state, skipped, block] = _dependencyProxy->execute(stack);
+    return {state, skipped, AqlItemBlockInputMatrix{block}};
+  }
+  TRI_ASSERT(stack.peek().getOffset() == 0);
+  TRI_ASSERT(!stack.peek().needsFullCount());
+  TRI_ASSERT(!stack.peek().hasHardLimit());
+  TRI_ASSERT(!stack.peek().hasSoftLimit());
+
+  auto [state, skipped, block] = _dependencyProxy->execute(stack);
+  if (state == ExecutionState::WAITING) {
+    // On waiting we have nothing to return
+    return {state, 0, AqlItemBlockInputMatrix{ExecutorState::HASMORE}};
+  }
+  if (block == nullptr) {
+    if (state == ExecutionState::HASMORE) {
+      return {state, skipped, AqlItemBlockInputMatrix{ExecutorState::HASMORE, skipped}};
+    }
+    return {state, skipped, AqlItemBlockInputMatrix{ExecutorState::DONE, skipped}};
+  }
+
+  if (_aqlItemMatrix == nullptr) {
+    _aqlItemMatrix = std::make_unique<AqlItemMatrix>(getNrInputRegisters());
+  }
+
+  // we will either build a complete fetched AqlItemBlockInputMatrix or return an empty one
+  // If we find a ShadowRow or ExecutionState == Done, we're done fetching.
+  if (state == ExecutionState::HASMORE && !_aqlItemMatrix->stoppedOnShadowRow()) {
+    TRI_ASSERT(block != nullptr);
+
+    // we need to store the block for later creation of AqlItemBlockInputMatrix
+    TRI_ASSERT(!_aqlItemMatrix->stoppedOnShadowRow());
+    _aqlItemMatrix->addBlock(std::move(block));
+    return {state, skipped, AqlItemBlockInputMatrix{ExecutorState::HASMORE, skipped}};
+  }
+
+  TRI_ASSERT(state == ExecutionState::DONE || _aqlItemMatrix->stoppedOnShadowRow());
+  return {state, skipped,
+          AqlItemBlockInputMatrix{ExecutorState::DONE, skipped, _aqlItemMatrix.get()}};
+}
+
 std::pair<ExecutionState, InputAqlItemRow> AllRowsFetcher::fetchRow(size_t atMost) {
   switch (_dataFetchedState) {
     case ALL_DATA_FETCHED:
