e6a611737603d5780fa7ccbcc8007e13cfe5bfcd
Markus Pfeiffer <markus@arangodb.com>
2020-02-27 09:23:38 +0000

    Add MultiAqlItemBlockInputRange

---
+++ arangod/Aql/MultiAqlItemBlockInputRange.h
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_AQL_MULTIAQLITEMBLOCKINPUTRANGE_H
+#define ARANGOD_AQL_MULTIAQLITEMBLOCKINPUTRANGE_H
+
+#include "Aql/AqlItemBlockInputRange.h"
+#include "Aql/ExecutionState.h"
+#include "Aql/InputAqlItemRow.h"
+#include "Aql/SharedAqlItemBlockPtr.h"
+
+namespace arangodb::aql {
+
+class MultiAqlItemBlockInputRange {
+ public:
+  explicit MultiAqlItemBlockInputRange(ExecutorState state, std::size_t skipped = 0,
+                                       std::size_t nrInputRanges = 1);
+
+  MultiAqlItemBlockInputRange(ExecutorState, std::size_t skipped,
+                              arangodb::aql::SharedAqlItemBlockPtr const&,
+                              std::size_t startIndex);
+
+  MultiAqlItemBlockInputRange(ExecutorState, std::size_t skipped,
+                              arangodb::aql::SharedAqlItemBlockPtr&&,
+                              std::size_t startIndex) noexcept;
+
+  ExecutorState upstreamState() const noexcept;
+  bool upstreamHasMore() const noexcept;
+
+  bool hasDataRow() const noexcept;
+
+  std::pair<ExecutorState, arangodb::aql::InputAqlItemRow> peekDataRow() const;
+  std::pair<ExecutorState, arangodb::aql::InputAqlItemRow> nextDataRow();
+
+  std::size_t getRowIndex() const noexcept;
+
+  std::pair<ExecutorState, arangodb::aql::ShadowAqlItemRow> peekShadowRow() const;
+
+  std::pair<ExecutorState, arangodb::aql::ShadowAqlItemRow> nextShadowRow();
+
+  // Subtract up to this many rows from the local `_skipped` state; return
+  // the number actually skipped. Does not skip data rows.
+  [[nodiscard]] auto skip(std::size_t) noexcept -> std::size_t;
+
+  [[nodiscard]] auto skipAll() noexcept -> std::size_t;
+
+  [[nodiscard]] auto skippedInFlight() const noexcept -> std::size_t;
+
+  auto resize(ExecutorState state, size_t skipped, size_t nrInputRanges) -> void;
+
+ private:
+  bool isIndexValid(std::size_t index) const noexcept;
+
+  bool isShadowRowAtIndex(std::size_t index) const noexcept;
+
+  enum LookAhead { NOW, NEXT };
+  enum RowType { DATA, SHADOW };
+  template <LookAhead doPeek, RowType type>
+  ExecutorState nextState() const noexcept;
+
+ private:
+  ExecutorState _finalState{ExecutorState::HASMORE};
+
+  std::vector<AqlItemBlockInputRange> _inputs;
+};
+
+}  // namespace arangodb::aql
+
+#endif
