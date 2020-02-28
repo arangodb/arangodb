fa2b610d45b50a60a73a6a06cdd44da7537031be
Tobias Gödderz <tobias@arangodb.com>
2020-02-28 18:25:17 +0100

    Revert "Next try to fix MSVC"
    This reverts commit 528c4c795d10ee0aa6686d143be1f6faa6e9b553.

--- 3rdParty/date/include/date/date.h
+++ 3rdParty/date/include/date/date.h
@@ -30,11 +30,6 @@
 // been invented (that would involve another several millennia of evolution).
 // We did not mean to shout.
 
-#ifdef _MSC_VER
-#define real_cplusplus __cplusplus
-#define __cplusplus 199711L
-#endif
-
 #ifndef HAS_STRING_VIEW
 #  if __cplusplus >= 201703
 #    define HAS_STRING_VIEW 1
@@ -8011,9 +8006,5 @@ operator<<(std::basic_ostream<CharT, Traits>& os,
 # pragma GCC diagnostic pop
 #endif
 
-#ifdef _MSC_VER
-#define __cplusplus real_cplusplus
-#undef real_cplusplus
-#endif
 
 #endif  // DATE_H
