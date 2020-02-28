a4ad9cd7be46cdef41b2979704b424ebf15b142b
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-02-28 11:53:07 +0100

    Delete unnecessary files.

--- 3rdParty/json-schema-validation/cmake/cpp20.cmake
+++
@@ -1,8 +0,0 @@
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL GNU)
-    set(CMAKE_CXX_FLAGS "${CAMAKE_CXX_FLAGS} -std=c++2a -fconcepts")
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL lang)
-    set(CMAKE_CXX_FLAGS "${CAMAKE_CXX_FLAGS} -std=c++2a -fconcepts")
-else()
-    message(FATAL_ERROR "compiler not supported")
-endif()
