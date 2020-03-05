afd100243452c94c134224d58d01a7c4979c02b0
Markus Pfeiffer <markus@arangodb.com>
2020-03-05 10:48:43 +0000

    Remove useless comments

--- arangod/Aql/UnsortedGatherExecutor.cpp
+++ arangod/Aql/UnsortedGatherExecutor.cpp
@@ -102,15 +102,11 @@ auto UnsortedGatherExecutor::skipRowsRange(typename Fetcher::DataRange& input, A
     if (done()) {
       return {ExecutorState::DONE, Stats{}, skipped, AqlCallSet{}};
     } else {
-      // TODO: CHECK ME AGAIN!
-      // Ask for more skips
-      //      if (call.needSkipMore()) {
+      // If we're not done, we can just request the current clientcall from
+      // upstream
       auto callSet = AqlCallSet{};
       callSet.calls.emplace_back(AqlCallSet::DepCallPair{currentDependency(), call});
       return {ExecutorState::HASMORE, Stats{}, skipped, callSet};
-      // } else {
-      //   return {ExecutorState::HASMORE, Stats{}, skipped, AqlCallSet{}};
-      // }
     }
   }
   TRI_ASSERT(false);
