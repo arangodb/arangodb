17caa567c34afe11d132a44ef09c7cf903c7d7ef
Kaveh Vahedipour <kaveh@arangodb.com>
2020-03-10 17:04:29 +0100

    Update State.cpp

--- arangod/Agency/State.cpp
+++ arangod/Agency/State.cpp
@@ -79,11 +79,7 @@ inline static std::string timestamp(uint64_t m) {
 
   using namespace std::chrono;
 
-<<<<<<< HEAD
-  std::time_t t =
-=======
   std::time_t t = (m == 0) ? std::time(nullptr) :
->>>>>>> f07ac211d6d8eee69ae0afebd11558b6d3b3a506
     system_clock::to_time_t(system_clock::time_point(milliseconds(m)));
   char mbstr[100];
   return std::strftime(mbstr, sizeof(mbstr), "%Y-%m-%d %H:%M:%S %Z", std::gmtime(&t))
