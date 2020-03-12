5ed670e22de69c2193acebf141fda689d8c69c72
Simon Grätzer <simon@graetzer.org>
2020-03-12 21:26:51 +0100

    fix warning on gcc

--- arangod/RocksDBEngine/RocksDBKeyBounds.cpp
+++ arangod/RocksDBEngine/RocksDBKeyBounds.cpp
@@ -148,7 +148,7 @@ RocksDBKeyBounds RocksDBKeyBounds::FulltextIndexPrefix(uint64_t objectId,
 
   uint64ToPersistent(internals.buffer(), objectId);
   internals.buffer().append(word.data(), word.length());
-  internals.push_back(static_cast<const char>(0xFFU));
+  internals.push_back(static_cast<char>(0xFFU));
   // 0xFF is higher than any valud utf-8 character
   return b;
 }
@@ -284,7 +284,7 @@ RocksDBKeyBounds::RocksDBKeyBounds(RocksDBEntryType type) : _type(type) {
 
       _internals.separate();
       _internals.push_back(static_cast<char>(_type));
-      _internals.push_back(static_cast<const char>(0xFFU));
+      _internals.push_back(static_cast<char>(0xFFU));
       break;
     }
     case RocksDBEntryType::CounterValue:
