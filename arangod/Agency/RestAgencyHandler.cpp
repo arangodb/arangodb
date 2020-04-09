cc98ca644bc94142a85809e92c65bc5f5a9f43b9
Kaveh Vahedipour <kaveh@arangodb.com>
2020-04-09 17:59:38 +0200

    removed all "accepted" from poll API

--- arangod/Agency/RestAgencyHandler.cpp
+++ arangod/Agency/RestAgencyHandler.cpp
@@ -162,8 +162,8 @@ RestStatus RestAgencyHandler::pollIndex(
     _agent->poll(start, timeout)
     .thenValue([this, start](std::shared_ptr<VPackBuilder> rb) {
       VPackSlice res = rb->slice();
-      if (res.hasKey("accepted")) {
-        TRI_ASSERT(res.hasKey("result"));
+      TRI_ASSERT(res.hasKey("result"));
+      if (res.hasKey("result")) {
         VPackBuffer<uint8_t> payload;
         VPackBuilder builder(payload);
         {
