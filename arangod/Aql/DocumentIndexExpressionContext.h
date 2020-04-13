50efd36803a07c70878fa22381c5ea80e10eb643
Yuriy Popov <yuriy@arango.com>
2020-04-13 14:42:10 +0300

    forgotten files

---
+++ arangod/Aql/DocumentIndexExpressionContext.h
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Yuriy Popov
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_AQL_DOCUMENT_INDEX_EXPRESSION_CONTEXT_H
+#define ARANGOD_AQL_DOCUMENT_INDEX_EXPRESSION_CONTEXT_H 1
+
+#include "Aql/QueryExpressionContext.h"
+
+#include <velocypack/Slice.h>
+
+namespace arangodb {
+namespace aql {
+class Query;
+
+class DocumentIndexExpressionContext final : public QueryExpressionContext {
+ public:
+  DocumentIndexExpressionContext(Query* query, AqlValue (*getValue)(void const* ctx, Variable const* var, bool doCopy),
+                                 void const* ctx);
+
+  ~DocumentIndexExpressionContext() = default;
+
+  virtual size_t numRegisters() const override;
+
+  virtual AqlValue getVariableValue(Variable const* variable, bool doCopy,
+                                    bool& mustDestroy) const override;
+
+ private:
+  /// @brief returns var's aql value
+  AqlValue (*_getValue)(void const* ctx, Variable const* var, bool doCopy);
+
+  /// @brief context for returning var's aql value
+  void const* _ctx;
+};
+
+}  // namespace aql
+}  // namespace arangodb
+
+#endif  // ARANGOD_AQL_DOCUMENT_INDEX_EXPRESSION_CONTEXT_H
