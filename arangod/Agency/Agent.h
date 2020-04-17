ce9e1b803c8a4a37ad0bb404ce1841f959f41446
Kaveh Vahedipour <kaveh@arangodb.com>
2020-04-17 13:45:34 +0200

    neunhoef review

--- arangod/Agency/Agent.h
+++ arangod/Agency/Agent.h
@@ -44,8 +44,7 @@ struct TRI_vocbase_t;
 namespace arangodb {
 namespace consensus {
 
-class Agent final : public arangodb::Thread, public AgentInterface,
-                    public std::enable_shared_from_this<Agent> {
+class Agent final : public arangodb::Thread, public AgentInterface {
  public:
   /// @brief Construct with program options
   explicit Agent(application_features::ApplicationServer& server, config_t const&);
