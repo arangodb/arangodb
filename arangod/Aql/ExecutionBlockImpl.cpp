f7f3cdce0105014496ed320f380750998f007498
Michael Hackstein <michael@arangodb.com>
2020-04-20 13:48:48 +0200

    Bug fix/do not inherit of shadowrows on invalid row (#11456)
    * Added a regression test for heavily nested subqueries with limit and count.
    
    * Do not inherit calls from a ShadowRow in blocks, where input is allowed to be absent
    
    * Simplified and fixed counting of ShadowRows for SubqueryEndExecutor. Also added an assertion that a passthrough does noever abort early.
    
    * Removed assertion again, this seems to not hold yet. Will be readded if it is correct in a different PR.
    
    * Fixed behaviour of SubqueryEnd if limit on top level is reached
    
    * Fixed test assertions in ExecutionBlock impl. We now returnd data.
    
    * Update arangod/Aql/ExecutionBlockImpl.cpp
    
    Co-Authored-By: Markus Pfeiffer <markuspf@users.noreply.github.com>
    
    Co-authored-by: Markus Pfeiffer <markuspf@users.noreply.github.com>

--- arangod/Aql/ExecutionBlockImpl.cpp
+++ arangod/Aql/ExecutionBlockImpl.cpp
@@ -894,11 +894,9 @@ auto ExecutionBlockImpl<SubqueryEndExecutor>::shadowRowForwarding(AqlCallStack& 
   }
   auto const& [state, shadowRow] = _lastRange.nextShadowRow();
   TRI_ASSERT(shadowRow.isInitialized());
-  bool didConsume = false;
   if (shadowRow.isRelevant()) {
     // We need to consume the row, and write the Aggregate to it.
     _executor.consumeShadowRow(shadowRow, *_outputItemRow);
-    didConsume = true;
     // we need to reset the ExecutorHasReturnedDone, it will
     // return done after every subquery is fully collected.
     _executorReturnedDone = false;
@@ -908,9 +906,9 @@ auto ExecutionBlockImpl<SubqueryEndExecutor>::shadowRowForwarding(AqlCallStack& 
 
   TRI_ASSERT(_outputItemRow->produced());
   _outputItemRow->advanceRow();
-  // we need to update the Top of the stack now
-  auto& topCall = stack.modifyTopCall();
-  topCall = _outputItemRow->getClientCall();
+  // The stack in used here contains all calls for within the subquery.
+  // Hence any inbound subquery needs to be counted on its level
+  countShadowRowProduced(stack, shadowRow.getDepth());
 
   if (state == ExecutorState::DONE) {
     // We have consumed everything, we are
@@ -928,11 +926,6 @@ auto ExecutionBlockImpl<SubqueryEndExecutor>::shadowRowForwarding(AqlCallStack& 
     // Need to return!
     return ExecState::DONE;
   } else {
-    if (didConsume) {
-      // We did only consume the input
-      // ask upstream
-      return ExecState::CHECKCALL;
-    }
     // End of input, we are done for now
     // Need to call again
     return ExecState::NEXTSUBQUERY;
@@ -1595,12 +1588,8 @@ ExecutionBlockImpl<Executor>::executeWithoutTrace(AqlCallStack stack) {
         }
         if constexpr (std::is_same_v<Executor, SubqueryEndExecutor>) {
           TRI_ASSERT(!stack.empty());
-          AqlCall const& subqueryCall = stack.peek();
-          AqlCall copyCall = subqueryCall;
-          ensureOutputBlock(std::move(copyCall), _lastRange);
-        } else {
-          ensureOutputBlock(std::move(clientCall), _lastRange);
         }
+        ensureOutputBlock(std::move(clientCall), _lastRange);
 
         TRI_ASSERT(!_outputItemRow->allRowsUsed());
         if constexpr (executorHasSideEffects<Executor>) {
@@ -1657,6 +1646,7 @@ ExecutionBlockImpl<Executor>::executeWithoutTrace(AqlCallStack stack) {
   }
   LOG_QUERY("80c24", DEBUG) << printBlockInfo() << " local statemachine done. Return now.";
   // If we do not have an output, we simply return a nullptr here.
+
   auto outputBlock = _outputItemRow != nullptr ? _outputItemRow->stealBlock()
                                                : SharedAqlItemBlockPtr{nullptr};
   // We are locally done with our output.
