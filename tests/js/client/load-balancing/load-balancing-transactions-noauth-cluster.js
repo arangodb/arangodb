7c795ee3a6110c19c6504f469c7b914d07435cdb
jsteemann <jan@arangodb.com>
2020-05-14 01:59:16 +0200

    extend tests

--- tests/js/client/load-balancing/load-balancing-transactions-noauth-cluster.js
+++ tests/js/client/load-balancing/load-balancing-transactions-noauth-cluster.js
@@ -1,5 +1,5 @@
 /* jshint globalstrict:true, strict:true, maxlen: 5000 */
-/* global assertTrue, assertFalse, assertEqual, require*/
+/* global assertTrue, assertFalse, assertEqual, assertNotUndefined, require*/
 
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
@@ -60,14 +60,15 @@ function TransactionsSuite () {
   const cn = 'UnitTestsCollection';
   let coordinators = [];
 
-  function sendRequest(method, endpoint, body, usePrimary) {
+  function sendRequest(method, endpoint, body, headers, usePrimary) {
     let res;
     const i = usePrimary ? 0 : 1;
     try {
       const envelope = {
         json: true,
         method,
-        url: `${coordinators[i]}${endpoint}`
+        url: `${coordinators[i]}${endpoint}`,
+        headers,
       };
       if (method !== 'GET') {
         envelope.body = body;
@@ -124,25 +125,25 @@ function TransactionsSuite () {
       const obj = { collections: { read: cn } };
 
       let url = "/_api/transaction";
-      let result = sendRequest('POST', url + "/begin", obj, true);
+      let result = sendRequest('POST', url + "/begin", obj, {}, true);
 
       assertEqual(result.status, 201);
-      assertFalse(result.body.result.id === undefined);
+      assertNotUndefined(result.body.result.id);
 
       let trx1 = result.body.result;
 
       try {
-        result = sendRequest('GET', url, {}, true);
+        result = sendRequest('GET', url, {}, {}, true);
 
         assertEqual(result.status, 200);
         assertInList(result.body.transactions, trx1);
 
-        result = sendRequest('GET', url, {}, false);
+        result = sendRequest('GET', url, {}, {}, false);
         
         assertEqual(result.status, 200);
         assertInList(result.body.transactions, trx1);
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, {}, true);
       }
     },
     
@@ -153,52 +154,52 @@ function TransactionsSuite () {
       let trx1, trx2;
       
       try {
-        let result = sendRequest('POST', url + "/begin", obj, true);
+        let result = sendRequest('POST', url + "/begin", obj, {}, true);
         assertEqual(result.status, 201);
-        assertFalse(result.body.result.id === undefined);
+        assertNotUndefined(result.body.result.id);
         trx1 = result.body.result;
         
-        result = sendRequest('POST', url + "/begin", obj, false);
+        result = sendRequest('POST', url + "/begin", obj, {}, false);
         assertEqual(result.status, 201);
-        assertFalse(result.body.result.id === undefined);
+        assertNotUndefined(result.body.result.id);
         trx2 = result.body.result;
       
-        result = sendRequest('GET', url, {}, true);
+        result = sendRequest('GET', url, {}, {}, true);
 
         assertEqual(result.status, 200);
         assertInList(result.body.transactions, trx1);
         assertInList(result.body.transactions, trx2);
 
-        result = sendRequest('GET', url, {}, false);
+        result = sendRequest('GET', url, {}, {}, false);
         
         assertEqual(result.status, 200);
         assertInList(result.body.transactions, trx1);
         assertInList(result.body.transactions, trx2);
 
         // commit trx1 on different coord
-        result = sendRequest('PUT', url + "/" + encodeURIComponent(trx1.id), {}, false);
+        result = sendRequest('PUT', url + "/" + encodeURIComponent(trx1.id), {}, {}, false);
         assertEqual(trx1.id, result.body.result.id);
         assertEqual("committed", result.body.result.status);
         
-        result = sendRequest('GET', url, {}, false);
+        result = sendRequest('GET', url, {}, {}, false);
         
         assertEqual(result.status, 200);
         assertNotInList(result.body.transactions, trx1);
         assertInList(result.body.transactions, trx2);
 
         // abort trx2 on different coord
-        result = sendRequest('DELETE', url + "/" + encodeURIComponent(trx2.id), {}, true);
+        result = sendRequest('DELETE', url + "/" + encodeURIComponent(trx2.id), {}, {}, true);
         assertEqual(trx2.id, result.body.result.id);
         assertEqual("aborted", result.body.result.status);
         
-        result = sendRequest('GET', url, {}, false);
+        result = sendRequest('GET', url, {}, {}, false);
         
         assertEqual(result.status, 200);
         assertNotInList(result.body.transactions, trx1);
         assertNotInList(result.body.transactions, trx2);
 
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, {}, true);
       }
     },
     
@@ -206,25 +207,25 @@ function TransactionsSuite () {
       const obj = { collections: { read: cn } };
 
       let url = "/_api/transaction";
-      let result = sendRequest('POST', url + "/begin", obj, true);
+      let result = sendRequest('POST', url + "/begin", obj, {}, true);
 
       assertEqual(result.status, 201);
-      assertFalse(result.body.result.id === undefined);
+      assertNotUndefined(result.body.result.id);
 
       let trx1 = result.body.result;
 
       try {
         // commit on different coord
-        result = sendRequest('PUT', url + "/" + encodeURIComponent(trx1.id), {}, false);
+        result = sendRequest('PUT', url + "/" + encodeURIComponent(trx1.id), {}, {}, false);
 
         assertEqual(result.status, 200);
         assertEqual(trx1.id, result.body.result.id);
         assertEqual("committed", result.body.result.status);
         
-        result = sendRequest('GET', url, {}, true);
+        result = sendRequest('GET', url, {}, {}, true);
         assertNotInList(result.body.transactions, trx1);
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, {}, true);
       }
     },
     
@@ -232,44 +233,105 @@ function TransactionsSuite () {
       const obj = { collections: { read: cn } };
 
       let url = "/_api/transaction";
-      let result = sendRequest('POST', url + "/begin", obj, true);
+      let result = sendRequest('POST', url + "/begin", obj, {}, true);
 
       assertEqual(result.status, 201);
-      assertFalse(result.body.result.id === undefined);
+      assertNotUndefined(result.body.result.id);
 
       let trx1 = result.body.result;
 
       try {
         // abort on different coord
-        result = sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, true);
+        result = sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, {}, true);
 
         assertEqual(result.status, 200);
         assertEqual(trx1.id, result.body.result.id);
         assertEqual("aborted", result.body.result.status);
         
-        result = sendRequest('GET', url, {}, true);
+        result = sendRequest('GET', url, {}, {}, true);
         assertNotInList(result.body.transactions, trx1);
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx1.id), {}, {}, true);
       }
     },
 
-    testUseTransaction: function () {
+    testUseTransactionCursorOther: function () {
       let trx;
       try {
         const obj = { collections: { read: cn } };
-        let result = sendRequest('POST', "/_api/transaction/begin", obj, true);
+        let result = sendRequest('POST', "/_api/transaction/begin", obj, {}, true);
         assertEqual(result.status, 201);
-        assertFalse(result.body.result.id === undefined);
+        assertNotUndefined(result.body.result.id);
         trx = result.body.result.id;
         
         // use trx on different coord to run a query
         const query = `FOR doc IN ${cn} RETURN doc`;
-        result = sendRequest('POST', "/_api/cursor", { query }, false);
+        result = sendRequest('POST', "/_api/cursor", { query }, { "x-arango-trx-id": trx }, false);
+        // request must have been forwarded
+        assertTrue(result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
         assertFalse(result.body.error);
         assertEqual(10, result.body.result.length);
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, {}, true);
+      }
+    },
+    
+    testUseTransactionCursorSame: function () {
+      let trx;
+      try {
+        const obj = { collections: { read: cn } };
+        let result = sendRequest('POST', "/_api/transaction/begin", obj, {}, true);
+        assertEqual(result.status, 201);
+        assertNotUndefined(result.body.result.id);
+        trx = result.body.result.id;
+        
+        // use trx on same coord to run a query
+        const query = `FOR doc IN ${cn} RETURN doc`;
+        result = sendRequest('POST', "/_api/cursor", { query }, { "x-arango-trx-id": trx }, true);
+        // request must not have been forwarded
+        assertFalse(result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
+        assertFalse(result.body.error);
+        assertEqual(10, result.body.result.length);
+      } finally {
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, {}, true);
+      }
+    },
+    
+    testUseTransactionCursorIncremental: function () {
+      let trx;
+      try {
+        const obj = { collections: { read: cn } };
+        let result = sendRequest('POST', "/_api/transaction/begin", obj, {}, true);
+        assertEqual(result.status, 201);
+        assertNotUndefined(result.body.result.id);
+        trx = result.body.result.id;
+        
+        // use trx on different coord to run a query
+        const query = `FOR doc IN ${cn} RETURN doc`;
+        let cursor;
+        result = sendRequest('POST', "/_api/cursor", { query, batchSize: 1 }, { "x-arango-trx-id": trx }, false);
+        try {
+          cursor = result.body.id;
+          assertTrue(cursor);
+          // request must have been forwarded
+          assertTrue(result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
+          assertFalse(result.body.error);
+          assertEqual(1, result.body.result.length);
+          assertTrue(result.body.hasMore);
+
+          for (let i = 0; i < 9; ++i) {
+            let same = i % 2 === 0;
+            result = sendRequest('PUT', "/_api/cursor/" + cursor, {}, { "x-arango-trx-id": trx }, same);
+            assertEqual(same, !result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
+            assertFalse(result.body.error);
+            assertEqual(1, result.body.result.length);
+            assertEqual(i < 8, result.body.hasMore);
+          }
+        } finally {
+          sendRequest('DELETE', '/_api/cursor/' + encodeURIComponent(cursor), {}, { "x-arango-trx-id": trx }, true);
+        }
+      } finally {
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, {}, true);
       }
     },
     
@@ -290,30 +352,32 @@ function TransactionsSuite () {
             }
           ]
         };
-        let result = sendRequest('POST', "/_api/gharial", graphDef, true);
+        let result = sendRequest('POST', "/_api/gharial", graphDef, {}, true);
         assertEqual(result.status, 202);
 
-        const obj = { collections: { read: "startVertices" } };
-        result = sendRequest('POST', "/_api/transaction/begin", obj, true);
+        const obj = { collections: { write: "startVertices" } };
+        result = sendRequest('POST', "/_api/transaction/begin", obj, {}, true);
         assertEqual(result.status, 201);
-        assertFalse(result.body.result.id === undefined);
+        assertNotUndefined(result.body.result.id);
         trx = result.body.result.id;
 
         const doc = { value: true };
-        result = sendRequest('POST', "/_api/gharial/myGraph/vertex/startVertices", doc, true);
+        result = sendRequest('POST', "/_api/gharial/myGraph/vertex/startVertices", doc, { "x-arango-trx-id": trx }, true);
+        assertFalse(result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
         assertEqual(result.status, 202);
-        assertFalse(result.body.vertex._key === undefined);
+        assertNotUndefined(result.body.vertex._key);
         const key = result.body.vertex._key;
 
-
         // get it via a different coordinator
-        result = sendRequest('GET', `/_api/gharial/myGraph/vertex/startVertices/${key}`, {}, false);
+        result = sendRequest('GET', `/_api/gharial/myGraph/vertex/startVertices/${key}`, {}, { "x-arango-trx-id": trx }, false);
+        // request must have been forwarded
+        assertTrue(result.headers.hasOwnProperty("x-arango-request-forwarded-to"));
         assertEqual(result.status, 200);
         assertEqual(result.body.vertex._key, key);
         assertTrue(result.body.vertex.value);
       } finally {
-        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, true);
-        sendRequest('DELETE', '/_api/gharial/myGraph?dropCollections=true', {}, true);
+        sendRequest('DELETE', '/_api/transaction/' + encodeURIComponent(trx), {}, {}, true);
+        sendRequest('DELETE', '/_api/gharial/myGraph?dropCollections=true', {}, {}, true);
       }
     }
 
