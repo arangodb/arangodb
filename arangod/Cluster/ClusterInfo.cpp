9fe449166dde3892e24637cd4af36bd35ac66f1e
Max Neunhöffer <max@arangodb.com>
2020-05-15 11:43:10 +0200

    Added a forgotten Plan/Version increment. (#11609)
    * Add a forgotten Plan/Version increment.
    
    * CHANGELOG.
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -2184,6 +2184,8 @@ Result ClusterInfo::createCollectionsCoordinator(
           AgencyPrecondition::Type::EMPTY, false);
       opers.emplace_back(collectionPlanPath, AgencySimpleOperationType::DELETE_OP);
     }
+    opers.emplace_back("Plan/Version", AgencySimpleOperationType::INCREMENT_OP);
+
     auto trx = AgencyWriteTransaction{opers, precs};
 
     using namespace std::chrono;
