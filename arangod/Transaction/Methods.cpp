2ebcd740fb19cac7f72633637a7a387741334cb6
Frank Celler <frank@arangodb.com>
2020-05-23 07:47:33 +0200

    statistics

--- arangod/Transaction/Methods.cpp
+++ arangod/Transaction/Methods.cpp
@@ -2532,7 +2532,7 @@ Future<OperationResult> transaction::Methods::truncateLocal(std::string const& c
           }
         }
       }
-      if (findRefusal(responses) && collectionName != "_statisticsRaw" && collectionName != "_statistics15" && collectionName != "_statistics") {
+      if (findRefusal(responses)) {
         return futures::makeFuture(OperationResult(TRI_ERROR_CLUSTER_SHARD_LEADER_RESIGNED));
       }
     }
