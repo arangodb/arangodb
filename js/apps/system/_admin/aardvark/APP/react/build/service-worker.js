1eb9d765f35da7d1a53f560b7ff9e497242a74ed
Simran Brucherseifer <simran@arangodb.com>
2020-05-29 14:16:01 +0200

    Revert "Rebuild UI"
    This reverts commit 4ffdc5524361e023c1575a1f2a169272a156f3ad.

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9a3323e2120a56c494fd2ac0c808e310.js"
+  "precache-manifest.6c7fa63ebdb70195a92c255c3ec4c488.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
