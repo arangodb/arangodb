a9028d85149e4d5843f585664462e2d38689163a
Tobias Gödderz <tobias@arangodb.com>
2020-06-02 12:28:40 +0200

    Removed noexcept specifier on defaulted constructors etc.

--- arangod/Aql/BlocksWithClients.h
+++ arangod/Aql/BlocksWithClients.h
@@ -55,7 +55,7 @@ class ClientsExecutorInfos {
  public:
   explicit ClientsExecutorInfos(std::vector<std::string> clientIds);
 
-  ClientsExecutorInfos(ClientsExecutorInfos&&) noexcept = default;
+  ClientsExecutorInfos(ClientsExecutorInfos&&) = default;
   ClientsExecutorInfos(ClientsExecutorInfos const&) = delete;
   ~ClientsExecutorInfos() = default;
 
