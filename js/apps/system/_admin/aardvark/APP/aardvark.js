4031aa08a9d6de9a88cd9eeb24f6ebaf834e826e
jsteemann <jan@arangodb.com>
2020-06-09 20:37:34 +0200

    fixed "Create Debug Package" in web UI in cluster mode

--- js/apps/system/_admin/aardvark/APP/aardvark.js
+++ js/apps/system/_admin/aardvark/APP/aardvark.js
@@ -212,6 +212,7 @@ authRouter.post('/query/debugDump', function (req, res) {
   try {
     explainer.debugDump(tmpDebugFileName, query, bindVars, options);
   } catch (e) {
+    print(String(e.stack));
     res.throw('bad request', e.message, {cause: e});
   }
   try {
