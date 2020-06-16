f6dd2b6aafd52e211770197d60bb85af6abd480a
jsteemann <jan@arangodb.com>
2020-06-16 15:52:50 +0200

    allow queries on DB-Servers

--- arangod/Aql/QueryCursor.cpp
+++ arangod/Aql/QueryCursor.cpp
@@ -202,7 +202,13 @@ QueryStreamCursor::~QueryStreamCursor() {
   _queryResults.clear();
 
   // now remove the continue handler we may have registered in the query
-  _query->sharedState()->invalidate();
+  {
+    auto ss = _query->sharedState();
+    if (ss) {
+      ss->invalidate();
+    }
+  }
+
   // Query destructor will cleanup plan and abort transaction
   _query.reset();
 }
@@ -264,6 +270,10 @@ Result QueryStreamCursor::dumpSync(VPackBuilder& builder) {
       << _query->queryString().extract(1024) << "'";
 
   std::shared_ptr<SharedQueryState> ss = _query->sharedState();
+  TRI_ASSERT(ss != nullptr);
+  if (ss == nullptr) {
+    return Result(TRI_ERROR_INTERNAL, "invalid query state");
+  }
   ss->resetWakeupHandler();
 
   try {
@@ -306,13 +316,23 @@ Result QueryStreamCursor::dumpSync(VPackBuilder& builder) {
 /// Set wakeup callback on streaming cursor
 void QueryStreamCursor::setWakeupHandler(std::function<bool()> const& cb) {
   if (_query) {
-    _query->sharedState()->setWakeupHandler(cb);
+    auto ss = _query->sharedState();
+    TRI_ASSERT(ss != nullptr);
+    if (ss == nullptr) {
+      THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_INTERNAL, "invalid query state");
+    }
+    ss->setWakeupHandler(cb);
   }
 }
 
 void QueryStreamCursor::resetWakeupHandler() {
   if (_query) {
-    _query->sharedState()->resetWakeupHandler();
+    auto ss = _query->sharedState();
+    TRI_ASSERT(ss != nullptr);
+    if (ss == nullptr) {
+      THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_INTERNAL, "invalid query state");
+    }
+    ss->resetWakeupHandler();
   }
 }
 
@@ -376,6 +396,7 @@ Result QueryStreamCursor::writeResult(VPackBuilder& builder) {
 
     if (!hasMore) {
       std::shared_ptr<SharedQueryState> ss = _query->sharedState();
+      TRI_ASSERT(ss != nullptr);
       ss->resetWakeupHandler();
 
       // cleanup before transaction is committed
