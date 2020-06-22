c43fa0801cc37b8289add3656a04ddc7519307dd
Markus Pfeiffer <markus@arangodb.com>
2020-06-22 16:59:23 +0100

    Remove fetchRow, skipRows from SingleRowFetcher

--- arangod/Aql/SingleRowFetcher.cpp
+++ arangod/Aql/SingleRowFetcher.cpp
@@ -116,43 +116,6 @@ bool SingleRowFetcher<passBlocksThrough>::fetchBlockIfNecessary(size_t atMost) {
 }
 
 template <BlockPassthrough passBlocksThrough>
-std::pair<ExecutionState, InputAqlItemRow> SingleRowFetcher<passBlocksThrough>::fetchRow(size_t atMost) {
-  if (!fetchBlockIfNecessary(atMost)) {
-    return {ExecutionState::WAITING, InputAqlItemRow{CreateInvalidInputRowHint{}}};
-  }
-  if (_currentShadowRow.isInitialized()) {
-    // Reset shadow rows as soon as we ask for data.
-    _currentShadowRow = ShadowAqlItemRow{CreateInvalidShadowRowHint{}};
-  }
-
-  if (_currentBlock == nullptr) {
-    TRI_ASSERT(_upstreamState == ExecutionState::DONE);
-    _currentRow = InputAqlItemRow{CreateInvalidInputRowHint{}};
-  } else {
-    TRI_ASSERT(_currentBlock != nullptr);
-    TRI_ASSERT(_upstreamState != ExecutionState::WAITING);
-    if (_currentBlock->isShadowRow(_rowIndex)) {
-      _currentRow = InputAqlItemRow{CreateInvalidInputRowHint{}};
-    } else {
-      _currentRow = InputAqlItemRow{_currentBlock, _rowIndex};
-      _rowIndex++;
-    }
-  }
-  return {returnState(false), _currentRow};
-}
-
-template <BlockPassthrough blockPassthrough>
-auto SingleRowFetcher<blockPassthrough>::fetchRowWithGlobalState(size_t atMost)
-    -> RowWithStates {
-  auto [state, row] = fetchRow(atMost);
-  if (state == ExecutionState::WAITING) {
-    return {ExecutionState::WAITING, ExecutionState::WAITING, row};
-  }
-
-  return {state, returnState(true), row};
-}
-
-template <BlockPassthrough passBlocksThrough>
 bool SingleRowFetcher<passBlocksThrough>::indexIsValid() const {
   return _currentBlock != nullptr && _rowIndex < _currentBlock->size();
 }
@@ -173,41 +136,6 @@ ExecutionState SingleRowFetcher<passBlocksThrough>::returnState(bool isShadowRow
 template <BlockPassthrough blockPassthrough>
 RegisterCount SingleRowFetcher<blockPassthrough>::getNrInputRegisters() const {
   return _dependencyProxy->getNrInputRegisters();
-}
-
-template <BlockPassthrough blockPassthrough>
-std::pair<ExecutionState, size_t> SingleRowFetcher<blockPassthrough>::preFetchNumberOfRows(size_t atMost) {
-  if (_upstreamState != ExecutionState::DONE && !indexIsValid()) {
-    // We have exhausted the current block and need to fetch a new one
-   auto [state, newBlock] = fetchBlock(atMost);
-    // we de not need result as local members are modified
-    if (state == ExecutionState::WAITING) {
-      return {state, 0};
-    }
-    // The internal state should be in-line with the returned state.
-    TRI_ASSERT(_upstreamState == state);
-    _currentBlock = std::move(newBlock);
-    _rowIndex = 0;
-  }
-
-  // The block before can put upstreamState to DONE.
-  // So we cannot swap the blocks
-  if (_upstreamState == ExecutionState::DONE) {
-    if (!indexIsValid()) {
-      // There is nothing more from upstream
-      return {_upstreamState, 0};
-    }
-    // we only have the block in hand, so we can only return that
-    // many additional rows
-    TRI_ASSERT(_rowIndex < _currentBlock->size());
-    return {_upstreamState, (std::min)(_currentBlock->size() - _rowIndex, atMost)};
-  }
-
-  TRI_ASSERT(_upstreamState == ExecutionState::HASMORE);
-  TRI_ASSERT(_currentBlock != nullptr);
-  // Here we can only assume that we have enough from upstream
-  // We do not want to pull additional block
-  return {_upstreamState, atMost};
 }
 
 template <BlockPassthrough blockPassthrough>
