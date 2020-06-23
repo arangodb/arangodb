5aa3d2d019e689dd4d190b3ba1395348d9edf544
KVS85 <vadim@arangodb.com>
2020-06-23 23:55:52 +0200

    Extend user properties changes

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/userView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/userView.js
@@ -237,14 +237,26 @@
         if (error) {
           arangoHelper.arangoError('User', 'Could not edit user settings');
         } else {
-          console.log(data);
-          if (data && data.extra && data.extra.img) {
-            var extra = self.currentUser.get('extra');
-            extra.img = data.extra.img;
+          var extra = self.currentUser.get('extra');
+          if (data) {
+            if (data.extra && data.extra.name) {
+              extra.name = data.extra.name;
+            } else {
+              extra.name = null;
+            }
+            self.currentUser.set('name.extra', extra);
+
+            if (data.extra && data.extra.img) {
+              extra.img = data.extra.img;
+            } else {
+              extra.img = null;
+            }
             self.currentUser.set('img.extra', extra);
+
             // rerender navigation containing userBarView
             window.App.naviView.render();
           }
+
           arangoHelper.arangoNotification('User', 'Changes confirmed.');
         }
       };
