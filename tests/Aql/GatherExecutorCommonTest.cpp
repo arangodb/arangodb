b9d39bbe43cfe5ea01fb123626e810a02d8f207e
Michael Hackstein <michael@arangodb.com>
2020-06-24 09:12:54 +0200

    Update tests/Aql/GatherExecutorCommonTest.cpp
    Co-authored-by: Markus Pfeiffer <markuspf@users.noreply.github.com>

--- tests/Aql/GatherExecutorCommonTest.cpp
+++ tests/Aql/GatherExecutorCommonTest.cpp
@@ -110,7 +110,7 @@ auto generateValue(std::vector<size_t> subqueryRuns, size_t branch) -> int64_t {
 // This is the inverse function for generateValue above.
 // It's not used anywhere yet, but maybe it is handy at one point
 // so i decided to keep it in.
-auto reverseValue(int64_t v) -> std::pair<std::vector<size_t>, size_t> {
+auto inverseGenerateValue(int64_t v) -> std::pair<std::vector<size_t>, size_t> {
   std::vector<size_t> subqueryValues{};
   size_t branch = (v / 100000) % 10;
   if (v == 0) {
