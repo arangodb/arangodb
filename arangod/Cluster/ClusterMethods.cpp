1ca661c805eeeab2c29a2c047ecb17b90b86b638
Max Neunhöffer <max@arangodb.com>
2020-06-29 17:06:55 +0200

    Update arangod/Cluster/ClusterMethods.cpp
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/Cluster/ClusterMethods.cpp
+++ arangod/Cluster/ClusterMethods.cpp
@@ -3889,8 +3889,8 @@ arangodb::Result hotBackupCoordinator(ClusterFeature& feature, VPackSlice const 
                                 /* force */ !gotLocks, meta);
     if (!result.ok()) {
       unlockDBServerTransactions(pool, backupId, dbServers);
-      ci.agencyHotBackupUnlock(backupId, timeout, supervisionOff);
-      releaseAgencyLock.cancel();
+      // release the lock
+      releaseAgencyLock.fire();
       result.reset(TRI_ERROR_HOT_BACKUP_INTERNAL,
                    std::string("failed to hot backup on all db servers: ") +
                        result.errorMessage());
