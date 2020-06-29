379c18be9c07b22d474f765b539d9ea9c9b58873
hkernbach <heiko@arangodb.com>
2020-06-29 12:34:02 +0200

    properly render 404 error view if a collection is not found

--- js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/arango/arango.js
@@ -880,7 +880,6 @@
       if (refresh || this.CollectionTypes[identifier] === undefined) {
         var callback = function (error, data, toRun) {
           if (error) {
-            arangoHelper.arangoError('Error', 'Could not detect collection type');
             if (toRun) {
               toRun(error);
             }
