ed4ba7c6bb4998e42b553ac7ba37e6b3b35bba6d
KVS85 <vadim@arangodb.com>
2020-07-01 00:56:32 +0200

    Rebuild UI

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b37dd52c5fe0ba8db8f99674b545445d.js"
+  "precache-manifest.0e332821cf352ed387985f573144ad6c.js"
 );
 
 workbox.clientsClaim();
