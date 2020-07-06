09af44777ca49fefec77e352d24622e95a197d3d
Max Neunhoeffer <max@arangodb.com>
2020-07-06 17:19:19 +0200

    Open connection lazily.

--- 3rdParty/fuerte/src/ConnectionBuilder.cpp
+++ 3rdParty/fuerte/src/ConnectionBuilder.cpp
@@ -66,7 +66,8 @@ std::shared_ptr<Connection> ConnectionBuilder::connect(EventLoopService& loop) {
   }
   
   // Start the connection implementation
-  result->startConnection();
+  //result->startConnection();
+  // lazy
 
   return result;
 }
