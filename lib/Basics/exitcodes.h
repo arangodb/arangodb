48360d8c79399c54a8709033417692b16867c3b2
jsteemann <jan@arangodb.com>
2020-07-07 13:17:32 +0200

    fix a typo in message

--- lib/Basics/exitcodes.h
+++ lib/Basics/exitcodes.h
@@ -80,7 +80,7 @@ constexpr int TRI_EXIT_RECOVERY                                                 
 constexpr int TRI_EXIT_DB_NOT_EMPTY                                             = 24;
 
 /// 25: EXIT_UNSUPPORTED_STORAGE_ENGINE
-/// unsupported storage egnine
+/// unsupported storage engine
 /// Will be returned when trying to start with an unsupported storage engine
 constexpr int TRI_EXIT_UNSUPPORTED_STORAGE_ENGINE                               = 25;
 
