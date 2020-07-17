e1e1ed6d9dfc44052d337173792187eb0f3f7556
jsteemann <jan@arangodb.com>
2020-07-17 22:28:49 +0200

    add some more files that were excluded by our .gitignore

---
+++ 3rdParty/rocksdb/6.11/monitoring/perf_context.cc.d
@@ -0,0 +1,18 @@
+monitoring/perf_context.cc.d monitoring/perf_context.o \
+ monitoring/perf_context.o: monitoring/perf_context.cc \
+ monitoring/perf_context_imp.h monitoring/perf_step_timer.h \
+ monitoring/perf_level_imp.h include/rocksdb/perf_level.h \
+ include/rocksdb/rocksdb_namespace.h port/port.h port/port_posix.h \
+ include/rocksdb/options.h include/rocksdb/advanced_options.h \
+ include/rocksdb/memtablerep.h include/rocksdb/slice.h \
+ include/rocksdb/cleanable.h include/rocksdb/universal_compaction.h \
+ include/rocksdb/comparator.h include/rocksdb/env.h \
+ include/rocksdb/status.h include/rocksdb/thread_status.h \
+ include/rocksdb/file_checksum.h include/rocksdb/listener.h \
+ include/rocksdb/compaction_job_stats.h \
+ include/rocksdb/table_properties.h include/rocksdb/types.h \
+ include/rocksdb/version.h include/rocksdb/write_buffer_manager.h \
+ include/rocksdb/cache.h include/rocksdb/memory_allocator.h \
+ include/rocksdb/statistics.h util/stop_watch.h monitoring/statistics.h \
+ monitoring/histogram.h port/likely.h util/core_local.h util/random.h \
+ util/mutexlock.h include/rocksdb/perf_context.h
---
+++ 3rdParty/rocksdb/6.11/monitoring/perf_level.cc.d
@@ -0,0 +1,15 @@
+monitoring/perf_level.cc.d monitoring/perf_level.o \
+ monitoring/perf_level.o: monitoring/perf_level.cc \
+ monitoring/perf_level_imp.h include/rocksdb/perf_level.h \
+ include/rocksdb/rocksdb_namespace.h port/port.h port/port_posix.h \
+ include/rocksdb/options.h include/rocksdb/advanced_options.h \
+ include/rocksdb/memtablerep.h include/rocksdb/slice.h \
+ include/rocksdb/cleanable.h include/rocksdb/universal_compaction.h \
+ include/rocksdb/comparator.h include/rocksdb/env.h \
+ include/rocksdb/status.h include/rocksdb/thread_status.h \
+ include/rocksdb/file_checksum.h include/rocksdb/listener.h \
+ include/rocksdb/compaction_job_stats.h \
+ include/rocksdb/table_properties.h include/rocksdb/types.h \
+ include/rocksdb/version.h include/rocksdb/write_buffer_manager.h \
+ include/rocksdb/cache.h include/rocksdb/memory_allocator.h \
+ include/rocksdb/statistics.h
