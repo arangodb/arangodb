1f069bcb4959e1d204545a2856d4ea951cdb1245
jsteemann <jan@arangodb.com>
2020-07-20 16:37:04 +0200

    further usages of improved VelocyPack serialization

--- arangod/Agency/Agent.cpp
+++ arangod/Agency/Agent.cpp
@@ -912,7 +912,7 @@ futures::Future<query_t> Agent::poll(
       builder->add("commitIndex", VPackValue(_commitIndex));
       builder->add("firstIndex", VPackValue(0));
       builder->add(VPackValue("readDB"));
-      _readDB.get().toBuilder(*builder, true);
+      _readDB.get("", *builder, true);
     } else if (index <= _commitIndex) {   // deliver immediately all logs since index
       builder = std::make_shared<VPackBuilder>();
       VPackObjectBuilder r(builder.get());
@@ -1911,7 +1911,8 @@ arangodb::consensus::index_t Agent::readDB(VPackBuilder& builder) const {
 
   uint64_t commitIndex = 0;
 
-  { READ_LOCKER(oLocker, _outputLock);
+  { 
+    READ_LOCKER(oLocker, _outputLock);
 
     commitIndex = _commitIndex;
     // commit index
@@ -1920,7 +1921,8 @@ arangodb::consensus::index_t Agent::readDB(VPackBuilder& builder) const {
 
     // key-value store {}
     builder.add(VPackValue("agency"));
-    _readDB.get().toBuilder(builder, true); }
+    _readDB.get("", builder, true); 
+  }
 
   // replicated log []
   _state.toVelocyPack(commitIndex, builder);
--- arangod/Agency/Store.h
+++ arangod/Agency/Store.h
@@ -134,7 +134,7 @@ class Store {
   Node const* nodePtr(std::string const& path = std::string("/")) const;
 
   /// @brief Get node at path under mutex and store it in velocypack
-  void get(std::string const& path, arangodb::velocypack::Builder& b) const;
+  void get(std::string const& path, arangodb::velocypack::Builder& b, bool showHidden) const;
 
   /// @brief Copy out a node
   Node get(std::string const& path = std::string("/")) const;
