204fbd3598088c2662583694c58c319bd1d6ac3c
Vadim <vadim@arangodb.com>
2020-07-20 23:17:11 +0300

    Revert "avoid throwing exception in fuerte (#12259)" (#12262)
    This reverts commit 8a0da87b46b96a0cb24587c2d93379d4ffd0103c.

--- 3rdParty/fuerte/src/AsioSockets.h
+++ 3rdParty/fuerte/src/AsioSockets.h
@@ -125,22 +125,18 @@ struct Socket<fuerte::SocketType::Ssl> {
             return;
           }
 
-          try {
-            // Perform SSL handshake and verify the remote host's certificate.
-            socket.next_layer().set_option(asio_ns::ip::tcp::no_delay(true));
-            if (verify) {
-              socket.set_verify_mode(asio_ns::ssl::verify_peer);
-              socket.set_verify_callback(
-                  asio_ns::ssl::rfc2818_verification(config._host));
-            } else {
-              socket.set_verify_mode(asio_ns::ssl::verify_none);
-            }
-            socket.async_handshake(asio_ns::ssl::stream_base::client,
-                                   std::move(done));
-            
-          } catch(boost::system::system_error const& exc) {
-            done(exc.code());
+          // Perform SSL handshake and verify the remote host's certificate.
+          socket.next_layer().set_option(asio_ns::ip::tcp::no_delay(true));
+          if (verify) {
+            socket.set_verify_mode(asio_ns::ssl::verify_peer);
+            socket.set_verify_callback(
+                asio_ns::ssl::rfc2818_verification(config._host));
+          } else {
+            socket.set_verify_mode(asio_ns::ssl::verify_none);
           }
+
+          socket.async_handshake(asio_ns::ssl::stream_base::client,
+                                 std::move(done));
         });
   }
 
