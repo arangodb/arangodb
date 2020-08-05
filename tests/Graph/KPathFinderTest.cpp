59db248801bdc65bb14e4f2e546f4344ceddc807
Michael Hackstein <michael@arangodb.com>
2020-08-05 18:27:00 +0200

    Added more test-cases

--- tests/Graph/KPathFinderTest.cpp
+++ tests/Graph/KPathFinderTest.cpp
@@ -52,14 +52,12 @@ class KPathFinderTest : public ::testing::Test {
     gdb.addVertexCollection("v", 100);
     gdb.addEdgeCollection(
         "e", "v",
-        {{1, 2},   {2, 3},   {3, 4},   {5, 4},   {6, 5},   {7, 6},   {8, 7},
-         {1, 10},  {10, 11}, {11, 12}, {12, 4},  {12, 5},  {21, 22}, {22, 23},
-         {23, 24}, {24, 25}, {21, 26}, {26, 27}, {27, 28}, {28, 25}, {30, 31},
-         {31, 32}, {32, 33}, {33, 34}, {34, 35}, {32, 30}, {33, 35}, {40, 41},
-         {41, 42}, {41, 43}, {42, 44}, {43, 44}, {44, 45}, {45, 46}, {46, 47},
-         {48, 47}, {49, 47}, {50, 47}, {48, 46}, {50, 46}, {50, 47}, {48, 46},
-         {50, 46}, {40, 60}, {60, 61}, {61, 62}, {62, 63}, {63, 64}, {64, 47},
-         {70, 71}, {70, 71}, {70, 71}});
+        {
+          /* a chain 1->2->3->4 */
+          {1, 2},   {2, 3},   {3, 4},
+          /* a diamond 5->6|7|8->9 */
+          {5, 6}, {5,7}, {5,8}, {6,9}, {7,9}, {8,9}
+          });
 
     query = gdb.getQuery("RETURN 1", std::vector<std::string>{"v", "e"});
     spo = gdb.getShortestPathOptions(query.get());
@@ -196,11 +194,121 @@ TEST_F(KPathFinderTest, path_depth_1) {
     result.clear();
     auto hasPath = finder->getNextPath(result);
     EXPECT_TRUE(hasPath);
-    pathStructureValid(result.slice(), 0);
+    pathStructureValid(result.slice(), 1);
     LOG_DEVEL << result.toString();
 
     EXPECT_TRUE(finder->hasMore());
   }
+
+  {
+    result.clear();
+    // Try again to make sure we stay at non-existing
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_FALSE(hasPath);
+    EXPECT_TRUE(result.isEmpty());
+    EXPECT_FALSE(finder->hasMore());
+  }
+}
+
+TEST_F(KPathFinderTest, path_depth_2) {
+  VPackBuilder result;
+  // Search 0 depth
+  spo->minDepth = 2;
+  spo->maxDepth = 2;
+
+  // Source and target are direkt neighbors, there is only one path between them
+  auto source = vId(1);
+  auto target = vId(3);
+
+  finder->reset(StringRef(source), StringRef(target));
+
+  EXPECT_TRUE(finder->hasMore());
+  {
+    result.clear();
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_TRUE(hasPath);
+    pathStructureValid(result.slice(), 2);
+    LOG_DEVEL << result.toString();
+
+    EXPECT_TRUE(finder->hasMore());
+  }
+
+  {
+    result.clear();
+    // Try again to make sure we stay at non-existing
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_FALSE(hasPath);
+    EXPECT_TRUE(result.isEmpty());
+    EXPECT_FALSE(finder->hasMore());
+  }
+}
+
+TEST_F(KPathFinderTest, path_depth_3) {
+  VPackBuilder result;
+  // Search 0 depth
+  spo->minDepth = 3;
+  spo->maxDepth = 3;
+
+  // Source and target are direkt neighbors, there is only one path between them
+  auto source = vId(1);
+  auto target = vId(3);
+
+  finder->reset(StringRef(source), StringRef(target));
+
+  EXPECT_TRUE(finder->hasMore());
+  {
+    result.clear();
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_TRUE(hasPath);
+    pathStructureValid(result.slice(), 3);
+    LOG_DEVEL << result.toString();
+
+    EXPECT_TRUE(finder->hasMore());
+  }
+
+  {
+    result.clear();
+    // Try again to make sure we stay at non-existing
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_FALSE(hasPath);
+    EXPECT_TRUE(result.isEmpty());
+    EXPECT_FALSE(finder->hasMore());
+  }
+}
+
+TEST_F(KPathFinderTest, path_depth_1_to_2) {
+  VPackBuilder result;
+  // Search 0 depth
+  spo->minDepth = 1;
+  spo->maxDepth = 2;
+
+  // Source and target are direkt neighbors, there is only one path between them
+  auto source = vId(1);
+  auto target = vId(3);
+
+  finder->reset(StringRef(source), StringRef(target));
+
+  EXPECT_TRUE(finder->hasMore());
+  {
+    result.clear();
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_TRUE(hasPath);
+    pathStructureValid(result.slice(), 1);
+    LOG_DEVEL << result.toString();
+
+    EXPECT_TRUE(finder->hasMore());
+  }
+
+  {
+    result.clear();
+    auto hasPath = finder->getNextPath(result);
+    EXPECT_TRUE(hasPath);
+    pathStructureValid(result.slice(), 2);
+    LOG_DEVEL << result.toString();
+
+    EXPECT_TRUE(finder->hasMore());
+  }
+
 
   {
     result.clear();
