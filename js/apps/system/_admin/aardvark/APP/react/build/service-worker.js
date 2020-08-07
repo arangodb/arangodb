dd98295dccfe7ca60de421e44abf147f0816ee0b
KVS85 <vadim@arangodb.com>
2020-08-07 13:35:31 +0200

    arangodb release 3.7.1

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.266a0aaf1299f6edc5965e13d9a93c74.js"
+  "precache-manifest.ca29147487313eafa7a6c68ba26ec812.js"
 );
 
 workbox.clientsClaim();
