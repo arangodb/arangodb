63ae0ffa70245739ec4537f099c1bf3cad47a785
jsteemann <jan@arangodb.com>
2020-08-18 13:46:45 +0200

    Show optimizer rules with highest execution times in explain output

--- tests/js/server/aql/aql-explain-cluster.js
+++ tests/js/server/aql/aql-explain-cluster.js
@@ -27,171 +27,25 @@
 /// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
 ////////////////////////////////////////////////////////////////////////////////
 
-var jsunity = require("jsunity");
-var errors = require("internal").errors;
-var db = require("@arangodb").db;
+let jsunity = require("jsunity");
+let db = require("@arangodb").db;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief test suite
 ////////////////////////////////////////////////////////////////////////////////
 
 function explainSuite () {
-  var cn = "UnitTestsAhuacatlExplain";
-  var c;
+  const cn = "UnitTestsAhuacatlExplain";
 
   return {
 
-////////////////////////////////////////////////////////////////////////////////
-/// @brief set up
-////////////////////////////////////////////////////////////////////////////////
-
     setUpAll : function () {
       db._drop(cn);
-      c = db._create(cn);
+      db._create(cn);
     },
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief tear down
-////////////////////////////////////////////////////////////////////////////////
 
     tearDownAll : function () {
       db._drop(cn);
-    },
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief test bind parameters
-////////////////////////////////////////////////////////////////////////////////
-
-    testExplainBindMissing : function () {
-      var actual;
-      var query = "RETURN @foo";
-      
-      try {
-        actual = AQL_EXPLAIN(query);
-        fail();
-      } catch (err) {
-        assertEqual(err.errorNum, errors.ERROR_QUERY_BIND_PARAMETER_MISSING.code);
-      }
-    },
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief test bind parameters
-////////////////////////////////////////////////////////////////////////////////
-
-    testExplainBindPresent : function () {
-      var actual;
-      var query = "RETURN @foo";
-      
-      actual = AQL_EXPLAIN(query, { foo: "bar" });
-      assertEqual(3, actual.plan.nodes.length);
-      assertEqual("SingletonNode", actual.plan.nodes[0].type);
-      assertEqual("CalculationNode", actual.plan.nodes[1].type);
-      assertEqual("ReturnNode", actual.plan.nodes[2].type);
-    },
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief test verbosity w/ single plan
-////////////////////////////////////////////////////////////////////////////////
-
-    testExplainVerbosity : function () {
-      var actual;
-      var query = "FOR i IN " + cn + " FOR j IN " + cn + " RETURN i";
-      
-      // single plan, no options
-      actual = AQL_EXPLAIN(query);
-      assertTrue(actual.hasOwnProperty("plan"));
-      assertFalse(Array.isArray(actual.plan));
-      assertTrue(actual.plan.hasOwnProperty("nodes"));
-      assertTrue(Array.isArray(actual.plan.nodes));
-      assertTrue(actual.plan.hasOwnProperty("rules"));
-      assertTrue(Array.isArray(actual.plan.rules));
-      assertTrue(actual.plan.hasOwnProperty("estimatedCost"));
-      
-      actual.plan.nodes.forEach(function(node) {
-        assertTrue(node.hasOwnProperty("type"));
-        assertFalse(node.hasOwnProperty("typeID")); // deactivated if not verbose
-        assertTrue(node.hasOwnProperty("dependencies"));
-        assertTrue(Array.isArray(node.dependencies));
-        assertFalse(node.hasOwnProperty("parents")); // deactivated if not verbose
-        assertTrue(node.hasOwnProperty("id"));
-        assertTrue(node.hasOwnProperty("estimatedCost"));
-      });
-
-      // single plan, verbose options
-      actual = AQL_EXPLAIN(query, { }, { verbosePlans: true });
-      assertTrue(actual.hasOwnProperty("plan"));
-      assertFalse(Array.isArray(actual.plan));
-      assertTrue(actual.plan.hasOwnProperty("nodes"));
-      assertTrue(Array.isArray(actual.plan.nodes));
-      assertTrue(actual.plan.hasOwnProperty("rules"));
-      assertTrue(Array.isArray(actual.plan.rules));
-
-      actual.plan.nodes.forEach(function(node) {
-        assertTrue(node.hasOwnProperty("type"));
-        assertTrue(node.hasOwnProperty("typeID"));
-        assertTrue(node.hasOwnProperty("dependencies"));
-        assertTrue(Array.isArray(node.dependencies));
-        assertTrue(node.hasOwnProperty("parents"));
-        assertTrue(Array.isArray(node.parents));
-        assertTrue(node.hasOwnProperty("id"));
-        assertTrue(node.hasOwnProperty("estimatedCost"));
-      });
-    },
-
-////////////////////////////////////////////////////////////////////////////////
-/// @brief test explain w/ a signle plan vs. all plans
-////////////////////////////////////////////////////////////////////////////////
-
-    testExplainAllPlansVsSingle : function () {
-      var actual;
-      var query = "FOR i IN " + cn + " FOR j IN " + cn + " RETURN i";
-      
-      // single plan
-      actual = AQL_EXPLAIN(query, { }, { verbosePlans: true });
-      assertTrue(actual.hasOwnProperty("plan"));
-      assertFalse(actual.hasOwnProperty("plans"));
-      assertFalse(Array.isArray(actual.plan));
-
-      assertTrue(actual.plan.hasOwnProperty("nodes"));
-      assertTrue(Array.isArray(actual.plan.nodes));
-
-      actual.plan.nodes.forEach(function(node) {
-        assertTrue(node.hasOwnProperty("type"));
-        assertTrue(node.hasOwnProperty("typeID")); 
-        assertTrue(node.hasOwnProperty("dependencies"));
-        assertTrue(Array.isArray(node.dependencies));
-        assertTrue(node.hasOwnProperty("parents"));
-        assertTrue(node.hasOwnProperty("id"));
-        assertTrue(node.hasOwnProperty("estimatedCost"));
-      });
-      
-      assertTrue(actual.plan.hasOwnProperty("rules"));
-      assertTrue(Array.isArray(actual.plan.rules));
-      
-
-      // multiple plans
-      actual = AQL_EXPLAIN(query, { }, { allPlans: true, verbosePlans: true });
-      assertFalse(actual.hasOwnProperty("plan"));
-      assertTrue(actual.hasOwnProperty("plans"));
-      assertTrue(Array.isArray(actual.plans));
-
-      actual.plans.forEach(function (plan) {
-        assertTrue(plan.hasOwnProperty("nodes"));
-        assertTrue(Array.isArray(plan.nodes));
-      
-        plan.nodes.forEach(function(node) {
-          assertTrue(node.hasOwnProperty("type"));
-          assertTrue(node.hasOwnProperty("typeID")); 
-          assertTrue(node.hasOwnProperty("dependencies"));
-          assertTrue(Array.isArray(node.dependencies));
-          assertTrue(node.hasOwnProperty("parents"));
-          assertTrue(node.hasOwnProperty("id"));
-          assertTrue(node.hasOwnProperty("estimatedCost"));
-        });
-
-        assertTrue(plan.hasOwnProperty("rules"));
-        assertTrue(Array.isArray(plan.rules));
-      });
     },
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -199,19 +53,18 @@ function explainSuite () {
 ////////////////////////////////////////////////////////////////////////////////
 
     testNodes : function () {
-      var actual;
-      var query = "FOR i IN " + cn + " FILTER i.value > 1 LET a = i.value / 2 SORT a DESC COLLECT x = a INTO g RETURN x";
+      const query = "FOR i IN " + cn + " FILTER i.value > 1 LET a = i.value / 2 SORT a DESC COLLECT x = a INTO g RETURN x";
       
-      actual = AQL_EXPLAIN(query, null, { optimizer: { rules: [ "-all" ] } });
-      var nodes = actual.plan.nodes, node;
+      let actual = AQL_EXPLAIN(query, null, { optimizer: { rules: [ "-all" ] } });
+      let nodes = actual.plan.nodes, node;
 
-      var n = 0;
+      let n = 0;
       node = nodes[n++];
       assertEqual("SingletonNode", node.type);
       assertEqual([ ], node.dependencies);
       assertEqual(1, node.id);
       assertEqual(1, node.estimatedCost);
-      var prev = node.id;
+      let prev = node.id;
       
       node = nodes[n++];
       assertEqual("ScatterNode", node.type);
@@ -247,7 +100,7 @@ function explainSuite () {
       assertEqual([ prev ], node.dependencies);
       assertTrue(node.hasOwnProperty("expression"));
       assertFalse(node.canThrow);
-      var out = node.outVariable.name;
+      let out = node.outVariable.name;
       prev = node.id;
       
       node = nodes[n++];
@@ -298,11 +151,6 @@ function explainSuite () {
   };
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// @brief executes the test suite
-////////////////////////////////////////////////////////////////////////////////
-
 jsunity.run(explainSuite);
 
 return jsunity.done();
-
