1332b5dbe14958ba83192f6dd597f098794917da
Andrey Abramov <andrey@arangodb.com>
2020-08-18 16:40:25 +0300

    bug-fix/internal-issue-#731 (#12457) (#12458)
    * fallback to LEVENSHTEIN_DISTANCE for Damerau Levenshtein case
    
    * make damerau distance default for LEVENSHTEIN_MATCH

--- arangod/Aql/Functions.cpp
+++ arangod/Aql/Functions.cpp
@@ -1779,7 +1779,7 @@ AqlValue Functions::LevenshteinMatch(ExpressionContext* ctx, transaction::Method
     return arangodb::aql::AqlValue{arangodb::aql::AqlValueHintNull{}};
   }
 
-  bool withTranspositionsValue = false;
+  bool withTranspositionsValue = true;
   int64_t maxDistanceValue = maxDistance.toInt64();
 
   if (args.size() > 3) {
@@ -1794,13 +1794,13 @@ AqlValue Functions::LevenshteinMatch(ExpressionContext* ctx, transaction::Method
   }
 
   if (maxDistanceValue < 0 ||
-      (withTranspositionsValue &&
-       maxDistanceValue > arangodb::iresearch::MAX_DAMERAU_LEVENSHTEIN_DISTANCE)) {
+      (!withTranspositionsValue &&
+       maxDistanceValue > arangodb::iresearch::MAX_LEVENSHTEIN_DISTANCE)) {
     registerInvalidArgumentWarning(ctx, AFN);
     return AqlValue{AqlValueHintNull{}};
   }
 
-  if (!withTranspositionsValue && maxDistanceValue > arangodb::iresearch::MAX_LEVENSHTEIN_DISTANCE) {
+  if (withTranspositionsValue && maxDistanceValue > arangodb::iresearch::MAX_DAMERAU_LEVENSHTEIN_DISTANCE) {
     // fallback to LEVENSHTEIN_DISTANCE
     auto const dist = Functions::LevenshteinDistance(ctx, trx, args);
     TRI_ASSERT(dist.isNumber());
