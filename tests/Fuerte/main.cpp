643c01454313429284b0cefe9dc3561bac7db49c
Simon Grätzer <simon@graetzer.org>
2020-08-31 16:53:54 +0200

    fix win32 build

--- tests/Fuerte/main.cpp
+++ tests/Fuerte/main.cpp
@@ -1,6 +1,3 @@
-
-#include <execinfo.h>
-#include <signal.h>
 #include <stdlib.h>
 
 #include <fuerte/VpackInit.h>
@@ -8,32 +5,17 @@
 #include "gtest/gtest.h"
 #include "common.h"
 
-using namespace std;
-
 // as it is not const this var has external linkage
 // it is assigned a value in main
 std::string myEndpoint = "tcp://localhost:8529";
 std::string myAuthentication = "basic:root:";
 
-vector<string> parse_args(int const& argc, char const* const* argv) {
-  vector<string> rv;
+std::vector<std::string> parse_args(int const& argc, char const* const* argv) {
+  std::vector<std::string> rv;
   for (int i = 0; i < argc; i++) {
     rv.emplace_back(argv[i]);
   }
   return rv;
-}
-
-void handler(int sig) {
-  void* array[20];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 20);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
 }
 
 int main(int argc, char** argv) {
