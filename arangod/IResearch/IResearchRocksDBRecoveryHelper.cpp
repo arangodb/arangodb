cee37b824e6737b4ca1fbb09904adddea33d85e6
Andrei Lobov <andrei.lobov@arangodb.com>
2020-08-31 15:36:43 +0300

    Fix internal issue BTS-183 (#12568)
    * Issue fixed
    
    * address review comments
    
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>
    
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/IResearch/IResearchRocksDBRecoveryHelper.cpp
+++ arangod/IResearch/IResearchRocksDBRecoveryHelper.cpp
@@ -365,7 +365,7 @@ void IResearchRocksDBRecoveryHelper::LogData(
       if (coll != nullptr) {
         auto const links = lookupLinks(*coll);
         for (auto link : links) {
-          link->afterTruncate(tick);
+          link->afterTruncate(tick, nullptr);
         }
       }
 
