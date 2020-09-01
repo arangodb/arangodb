3a5392fcf545a5eabdeb83f32be48ca530839017
Andrey Abramov <andrey@arangodb.com>
2020-09-01 16:04:21 +0300

    bug-fix-3.6/internal-issue-#744 (#12460) (#12483)
    * bug-fix/internal-issue-#744 (#12460)
    
    * refine IResearchViewNode cost estimation
    
    * adjust comment
    
    * fix test
    
    * fix broken tests
    
    * add tests to check estimation
    
    * add integration tests
    
    * moar tests
    
    * address review comments
    
    * address jslint issues
    
    * fix tests
    
    * fix tests
    # Conflicts:
    #	tests/IResearch/IResearchViewNode-test.cpp
    
    * adjust code to be 3.6 compliant
    
    * fix compilation
    
    * fix compilation
    
    * fix compilation
    
    * fix compilation
    
    * address vapck differences between 3.6 and 3.7
    
    * Update CHANGELOG
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- tests/IResearch/IResearchQueryOptions-test.cpp
+++ tests/IResearch/IResearchQueryOptions-test.cpp
@@ -32,6 +32,7 @@
 #include "VocBase/ManagedDocumentResult.h"
 
 #include <velocypack/Iterator.h>
+#include <velocypack/velocypack-aliases.h>
 
 extern const char* ARGV0;  // defined in main.cpp
 
@@ -303,6 +304,35 @@ TEST_F(IResearchQueryOptionsTest, Collections) {
     std::map<irs::string_ref, arangodb::ManagedDocumentResult const*> expectedDocs{
         {"A", &insertedDocs[1]}};
 
+    // check node estimation
+    {
+      auto explanationResult =
+          arangodb::tests::explainQuery(vocbase, query);
+      ASSERT_TRUE(explanationResult.result.ok());
+      auto const explanationSlice = explanationResult.data->slice();
+      ASSERT_TRUE(explanationSlice.isObject());
+      auto const nodesSlice = explanationSlice.get("nodes");
+      ASSERT_TRUE(nodesSlice.isArray());
+      VPackSlice viewNode;
+      for (auto node : VPackArrayIterator(nodesSlice)) {
+        auto const nodeType = arangodb::iresearch::getStringRef(node.get("type"));
+
+        if ("EnumerateViewNode" != nodeType) {
+          continue;
+        }
+
+        auto const viewName = arangodb::iresearch::getStringRef(node.get("view"));
+
+        if ("testView" == viewName) {
+          viewNode = node;
+          break;
+        }
+      }
+      ASSERT_TRUE(viewNode.isObject());
+      ASSERT_EQ(insertedDocs.size()/2 + 1., viewNode.get("estimatedCost").getDouble());
+      ASSERT_EQ(insertedDocs.size()/2, viewNode.get("estimatedNrItems").getNumber<size_t>());
+    }
+
     auto queryResult = arangodb::tests::executeQuery(vocbase, query);
     ASSERT_TRUE(queryResult.result.ok());
 
@@ -439,6 +469,35 @@ TEST_F(IResearchQueryOptionsTest, Collections) {
 
     EXPECT_TRUE(arangodb::tests::assertRules(
         vocbase, query, {arangodb::aql::OptimizerRule::handleArangoSearchViewsRule}));
+
+    // check node estimation
+    {
+      auto explanationResult =
+          arangodb::tests::explainQuery(vocbase, query);
+      ASSERT_TRUE(explanationResult.result.ok());
+      auto const explanationSlice = explanationResult.data->slice();
+      ASSERT_TRUE(explanationSlice.isObject());
+      auto const nodesSlice = explanationSlice.get("nodes");
+      ASSERT_TRUE(nodesSlice.isArray());
+      VPackSlice viewNode;
+      for (auto node : VPackArrayIterator(nodesSlice)) {
+        auto const nodeType = arangodb::iresearch::getStringRef(node.get("type"));
+
+        if ("EnumerateViewNode" != nodeType) {
+          continue;
+        }
+
+        auto const viewName = arangodb::iresearch::getStringRef(node.get("view"));
+
+        if ("testView" == viewName) {
+          viewNode = node;
+          break;
+        }
+      }
+      ASSERT_TRUE(viewNode.isObject());
+      ASSERT_EQ(insertedDocs.size() + 1., viewNode.get("estimatedCost").getDouble());
+      ASSERT_EQ(insertedDocs.size(), viewNode.get("estimatedNrItems").getNumber<size_t>());
+    }
 
     std::map<irs::string_ref, std::vector<arangodb::ManagedDocumentResult const*>> expectedDocs{
         {"A", {&insertedDocs[0], &insertedDocs[1]}}};
