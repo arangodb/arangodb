8bd54804b3208b86ee65da8c629280d1ce849089
Dan Larkin-York <dan@arangodb.com>
2020-09-10 15:28:44 -0400

    Fix truncate context for audit logging

--- arangod/Transaction/Methods.cpp
+++ arangod/Transaction/Methods.cpp
@@ -2339,7 +2339,8 @@ Future<OperationResult> transaction::Methods::truncateAsync(std::string const& c
   TRI_ASSERT(_state->status() == transaction::Status::RUNNING);
 
   OperationOptions optionsCopy = options;
-  auto cb = [this, collectionName](OperationResult res) {
+  auto cb = [this, collectionName, handler = rest::RestHandler::CURRENT_HANDLER](OperationResult res) {
+    rest::RestHandler::CURRENT_HANDLER = handler;
     events::TruncateCollection(vocbase().name(), collectionName, res.errorNumber());
     return res;
   };
