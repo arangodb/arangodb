d4944837bcbfcb74017c26632d2667af7f728ebe
Simon Grätzer <simon@graetzer.org>
2020-09-17 15:58:31 +0200

    Add tests, fix compilation

---
+++ tests/RocksDBEngine/HwEncryptionProviderTest.cpp
@@ -0,0 +1,107 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Simon Grätzer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Basics/Common.h"
+#include "Basics/files.h"
+#include "Basics/system-functions.h"
+#include "Random/RandomGenerator.h"
+
+#include "gtest/gtest.h"
+
+#include <array>
+#include <memory>
+#include <string>
+
+#include <velocypack/velocypack-aliases.h>
+
+#ifdef USE_ENTERPRISE
+#include "Enterprise/RocksDBEngine/HwEncryptionProvider.h"
+#endif
+
+using namespace arangodb;
+using namespace arangodb::basics;
+
+#ifdef USE_ENTERPRISE
+
+namespace {
+static const char* SAMPLE_KEY = "01234567890123456789012345678901";
+}
+
+TEST(EncryptionProviderTest, simple) {
+  
+  // hand rolled AES-256-CTR mode
+  arangodb::enterprise::HwEncryptionProvider hwprovider(rocksdb::Slice(SAMPLE_KEY, 32));
+
+  // this is the hand-rolled CTR mode with the openssl software-only AES_encrypt
+  arangodb::enterprise::AES256BlockCipher cipher(rocksdb::Slice(SAMPLE_KEY, 32));
+  rocksdb::CTREncryptionProvider softprovider(cipher);
+  
+  ASSERT_EQ(hwprovider.GetPrefixLength(), softprovider.GetPrefixLength());
+  
+  std::string prefix;
+  prefix.resize(softprovider.GetPrefixLength());
+  
+  rocksdb::EnvOptions opts;
+  
+  constexpr size_t kBuffSize = 1<<14;
+    
+  std::array<char, kBuffSize> buffer1;
+  std::array<char, kBuffSize> buffer2;
+  std::array<char, kBuffSize> buffer3;
+
+  for (int x = 0; x < 64; x++) {
+    for (size_t i = 0; i < kBuffSize; i++) {
+      buffer1[i] = buffer2[i] = buffer3[i] = RandomGenerator::interval(0, 255);
+    }
+    
+    size_t offset = RandomGenerator::interval(0, kBuffSize / 2);
+    size_t len = RandomGenerator::interval(0, kBuffSize - offset);
+    
+    softprovider.CreateNewPrefix("", prefix.data(), prefix.size());
+    rocksdb::Slice prefixSlice(prefix);
+    
+    std::unique_ptr<rocksdb::BlockAccessCipherStream> stream1, stream2;
+    ASSERT_TRUE(hwprovider.CreateCipherStream("", opts, prefixSlice, &stream1).ok());
+    ASSERT_TRUE(softprovider.CreateCipherStream("", opts, prefixSlice, &stream2).ok());
+
+    ASSERT_NE(stream1, nullptr);
+    ASSERT_NE(stream2, nullptr);
+
+    // encrypt data at offset
+    ASSERT_TRUE(stream1->Encrypt(offset, buffer1.data() + offset, len).ok());
+    ASSERT_TRUE(stream1->Encrypt(offset, buffer2.data() + offset, len).ok());
+
+    // check encrypted data
+    ASSERT_EQ(memcmp(buffer1.data() + offset, buffer2.data() + offset, len), 0);
+    
+    // decrypt data at offset
+    ASSERT_TRUE(stream1->Encrypt(offset, buffer1.data() + offset, len).ok());
+    ASSERT_TRUE(stream1->Encrypt(offset, buffer2.data() + offset, len).ok());
+
+    ASSERT_EQ(memcmp(buffer1.data(), buffer2.data(), kBuffSize), 0);
+
+    // check is it equal to original
+    ASSERT_EQ(memcmp(buffer1.data(), buffer3.data(), kBuffSize), 0);
+  }
+}
+
+#endif
