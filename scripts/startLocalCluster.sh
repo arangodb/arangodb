1a2e4b254c4a535e65a93f431f9131d3ad1f221d
Kaveh Vahedipour <kaveh@arangodb.com>
2020-09-17 13:52:54 +0200

    lower verbosity of clusters

--- scripts/startLocalCluster.sh
+++ scripts/startLocalCluster.sh
@@ -266,9 +266,6 @@ start() {
         --log.force-direct false \
         --log.thread true \
         --log.level $LOG_LEVEL_CLUSTER \
-        --log.level=maintenance=trace \
-        --log.level=cluster=trace \
-        --log.level=replication=debug \
         --javascript.allow-admin-execute true \
         $SYSTEM_REPLICATION_FACTOR \
         $STORAGE_ENGINE \
