048caff992fd8fc30a2195ed61d8203dabfe3d5e
Dan Larkin-York <danielhlarkin@users.noreply.github.com>
2020-10-07 15:03:28 -0400

    Update arangod/RocksDBEngine/RocksDBWalAccess.h
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/RocksDBEngine/RocksDBWalAccess.h
+++ arangod/RocksDBEngine/RocksDBWalAccess.h
@@ -32,7 +32,7 @@ namespace arangodb {
 /// TODO: add methods for _admin/wal/ and get rid of engine specific handlers
 class RocksDBWalAccess final : public WalAccess {
  public:
-  RocksDBWalAccess(RocksDBEngine&);
+  explicit RocksDBWalAccess(RocksDBEngine&);
   virtual ~RocksDBWalAccess() = default;
 
   /// {"tickMin":"123", "tickMax":"456", "version":"3.2", "serverId":"abc"}
