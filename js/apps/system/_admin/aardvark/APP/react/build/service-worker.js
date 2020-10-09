c19efd058e641323851b2c304e0dd4ab9761c0b8
Jan <jsteemann@users.noreply.github.com>
2020-10-09 14:33:30 +0200

    always show documents for a collection in web interface (#12812)
    * always show documents for a collection in web interface
    
    regardless of what `collection.count()` says. this fixes a potential
    race between fetching the count and fetching the documents.
    
    * Rebuild UI
    
    Co-authored-by: KVS85 <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.bfafb7f3e859beb1c85b13619082bde9.js"
+  "precache-manifest.e6ccd5309717595477b16d3b080980cf.js"
 );
 
 workbox.clientsClaim();
