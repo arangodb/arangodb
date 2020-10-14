df8500f5dfc741ef7b74b95c26fc6f2c22e27d14
jsteemann <jan@arangodb.com>
2020-10-14 16:38:08 +0200

    Revert "Update IResearchAnalyzerFeature.cpp"
    This reverts commit cd74c3326fe377bea4272b3d2e1f941a5a85e720.

--- arangod/IResearch/IResearchAnalyzerFeature.cpp
+++ arangod/IResearch/IResearchAnalyzerFeature.cpp
@@ -819,7 +819,7 @@ arangodb::Result visitAnalyzers(
         continue;
       }
 
-      if (response.response->statusCode() == arangodb::fuerte::StatusNotFound) {
+      if (response.statusCode() == arangodb::fuerte::StatusNotFound) {
         return { TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND };
       }
 
@@ -827,7 +827,7 @@ arangodb::Result visitAnalyzers(
         return { arangodb::network::fuerteToArangoErrorCode(response) };
       }
     
-      VPackSlice answer = response.response->slice();
+      VPackSlice answer = response.slice();
       if (!answer.isObject()) {
         return {
           TRI_ERROR_INTERNAL,
