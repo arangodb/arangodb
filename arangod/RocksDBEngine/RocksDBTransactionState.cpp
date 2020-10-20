46564dc29ecd52ddde2db5bb637a0703b28f8fcc
Dan Larkin-York <dan@arangodb.com>
2020-10-20 17:33:05 -0400

    Fix read-only counter adjustments

--- arangod/RocksDBEngine/RocksDBTransactionState.cpp
+++ arangod/RocksDBEngine/RocksDBTransactionState.cpp
@@ -277,14 +277,6 @@ arangodb::Result RocksDBTransactionState::internalCommit() {
       // we need this in case of an intermediate commit. The number of
       // initial documents is adjusted and numInserts / removes is set to 0
       // index estimator updates are buffered
-      TRI_IF_FAILURE("RocksDBCommitCounts") {
-        continue;
-      }
-      TRI_IF_FAILURE("RocksDBCommitCountsRandom") {
-        if (RandomGenerator::interval(uint16_t(100)) >= 50) {
-          continue;
-        }
-      }
       coll->commitCounts(id(), _lastWrittenOperationTick);
     }
   };
