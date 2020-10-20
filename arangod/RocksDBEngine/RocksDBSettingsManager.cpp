35697b1c2dd0a052d3552fe45c8299413ac72659
Dan Larkin-York <dan@arangodb.com>
2020-10-20 16:53:31 -0400

    Persist count changes from recovery to avoid lost updates

--- arangod/RocksDBEngine/RocksDBSettingsManager.cpp
+++ arangod/RocksDBEngine/RocksDBSettingsManager.cpp
@@ -228,6 +228,8 @@ Result RocksDBSettingsManager::sync(bool force) {
   }
   TRI_ASSERT(lastSync <= minSeqNr);
   if (!didWork) {
+    LOG_TOPIC("1039e", TRACE, Logger::ENGINES)
+        << "no collection data to serialize, updating lastSync to " << minSeqNr;
     _lastSync.store(minSeqNr);
     return Result();  // nothing was written
   }
@@ -244,6 +246,7 @@ Result RocksDBSettingsManager::sync(bool force) {
   // we have to commit all counters in one batch
   auto s = _db->Write(wo, &batch);
   if (s.ok()) {
+    LOG_TOPIC("103ae", TRACE, Logger::ENGINES) << "updating lastSync to " << minSeqNr;
     _lastSync.store(std::max(_lastSync.load(), minSeqNr));
   }
 
@@ -296,6 +299,8 @@ void RocksDBSettingsManager::loadSettings() {
         LOG_TOPIC("1b3de", WARN, Logger::ENGINES)
             << "unable to read initial settings: invalid data";
       }
+    } else {
+      LOG_TOPIC("7558b", TRACE, Logger::ENGINES) << "no initial settings found";
     }
   }
 }
