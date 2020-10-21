148038950bb6daf3d7cc09bb16ff246d89162d8a
Max Neunhoeffer <max@arangodb.com>
2020-10-21 15:29:37 +0200

    Port performance improvement from 3.7 to devel

--- arangod/Cluster/Maintenance.cpp
+++ arangod/Cluster/Maintenance.cpp
@@ -702,7 +702,7 @@ arangodb::Result arangodb::maintenance::diffPlanLocal(
             std::string const& id = p.first;
             bool found = false;
             for (auto const& ind : VPackArrayIterator(indexes)) {
-              if (ind.get(ID).copyString() == id) {
+              if (ind.get(ID).stringView() == id) {
                 found = true;
                 break;
               }
