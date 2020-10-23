5451917ba4ff3bb439a0c92986aeefea9367d205
Jan <jsteemann@users.noreply.github.com>
2020-10-23 09:16:41 +0200

    make sure the x-arango-source header is set for all internal requests (#12890)
    There is currently no consequence from setting or not setting this
    header except that there will be additional request logging in log level
    DEBUG if the header is set. It is not used for anything else, but it may
    help with debugging. Now it is set consistently for internal requests.

--- arangod/Network/Utils.cpp
+++ arangod/Network/Utils.cpp
@@ -30,6 +30,7 @@
 #include "Basics/NumberUtils.h"
 #include "Cluster/ClusterFeature.h"
 #include "Cluster/ClusterInfo.h"
+#include "Cluster/ServerState.h"
 #include "Logger/LogMacros.h"
 #include "Network/Methods.h"
 #include "Network/NetworkFeature.h"
@@ -315,6 +316,18 @@ int fuerteStatusToArangoErrorCode(fuerte::Response const& res) {
 
 std::string fuerteStatusToArangoErrorMessage(fuerte::Response const& res) {
   return fuerte::status_code_to_string(res.statusCode());
+}
+
+void addSourceHeader(fuerte::Request& req) {
+  auto state = ServerState::instance();
+  if (state->isCoordinator() || state->isDBServer()) {
+    req.header.addMeta(StaticStrings::ClusterCommSource, state->getId());
+  } else if (state->isAgent()) {
+    auto agent = AgencyFeature::AGENT;
+    if (agent != nullptr) {
+      req.header.addMeta(StaticStrings::ClusterCommSource, "AGENT-" + agent->id());
+    }
+  }
 }
 
 }  // namespace network
