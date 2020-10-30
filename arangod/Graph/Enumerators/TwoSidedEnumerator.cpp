9b87b792bfb5b820b8f0df50a04168f0176efc91
Michael Hackstein <michael@arangodb.com>
2020-10-30 13:05:30 +0100

    Added dummy test file and implementation for TwoSidedGraph enumerator.

---
+++ arangod/Graph/Enumerators/TwoSidedEnumerator.cpp
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2020-2020 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+/// @author Heiko Kernbach
+////////////////////////////////////////////////////////////////////////////////
+
+#include "TwoSidedEnumerator.h"
+
+#include <velocypack/Builder.h>
+#include <velocypack/HashedStringRef.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::graph;
+
+template <class QueueType, class PathStoreType, class ProviderType>
+TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::TwoSidedEnumerator() {}
+
+template <class QueueType, class PathStoreType, class ProviderType>
+TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::~TwoSidedEnumerator() {}
+
+template <class QueueType, class PathStoreType, class ProviderType>
+void TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::clear() {}
+
+/**
+ * @brief Quick test if the finder can prove there is no more data available.
+ *        It can respond with false, even though there is no path left.
+ * @return true There will be no further path.
+ * @return false There is a chance that there is more data available.
+ */
+template <class QueueType, class PathStoreType, class ProviderType>
+bool TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::isDone() const {
+  return true;
+}
+
+/**
+ * @brief Reset to new source and target vertices.
+ * This API uses string references, this class will not take responsibility
+ * for the referenced data. It is caller's responsibility to retain the
+ * underlying data and make sure the StringRefs stay valid until next
+ * call of reset.
+ *
+ * @param source The source vertex to start the paths
+ * @param target The target vertex to end the paths
+ */
+template <class QueueType, class PathStoreType, class ProviderType>
+void TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::reset(VertexRef source, VertexRef target) {}
+
+/**
+ * @brief Get the next path, if available written into the result build.
+ * The given builder will be not be cleared, this function requires a
+ * prepared builder to write into.
+ *
+ * @param result Input and output, this needs to be an open builder,
+ * where the path can be placed into.
+ * Can be empty, or an openArray, or the value of an object.
+ * Guarantee: Every returned path matches the conditions handed in via
+ * options. No path is returned twice, it is intended that paths overlap.
+ * @return true Found and written a path, result is modified.
+ * @return false No path found, result has not been changed.
+ */
+template <class QueueType, class PathStoreType, class ProviderType>
+bool TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::getNextPath(arangodb::velocypack::Builder& result) {
+  return false;
+}
+
+/**
+ * @brief Skip the next Path, like getNextPath, but does not return the path.
+ *
+ * @return true Found and skipped a path.
+ * @return false No path found.
+ */
+
+template <class QueueType, class PathStoreType, class ProviderType>
+bool TwoSidedEnumerator<QueueType, PathStoreType, ProviderType>::skipPath() {
+  return false;
+}
