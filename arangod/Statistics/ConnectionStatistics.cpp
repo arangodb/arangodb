e9edcf792550a7eff558a5d3c244eb28f4f72213
Jan <jsteemann@users.noreply.github.com>
2020-11-09 11:10:27 +0100

    decouple metrics provisioning from server.statistics-history option (#12996)

--- arangod/Statistics/ConnectionStatistics.cpp
+++ arangod/Statistics/ConnectionStatistics.cpp
@@ -64,7 +64,7 @@ void ConnectionStatistics::initialize() {
 ConnectionStatistics::Item ConnectionStatistics::acquire() {
   ConnectionStatistics* statistics = nullptr;
 
-  if (StatisticsFeature::enabled() && _freeList.pop(statistics)) {
+  if (_freeList.pop(statistics)) {
     return Item{ statistics };
   }
 
@@ -72,11 +72,6 @@ ConnectionStatistics::Item ConnectionStatistics::acquire() {
 }
 
 void ConnectionStatistics::getSnapshot(Snapshot& snapshot) {
-  if (!StatisticsFeature::enabled()) {
-    // all the below objects may be deleted if we don't have statistics enabled
-    return;
-  }
-
   snapshot.httpConnections = statistics::HttpConnections;
   snapshot.totalRequests = statistics::TotalRequests;
   snapshot.totalRequestsSuperuser = statistics::TotalRequestsSuperuser;
