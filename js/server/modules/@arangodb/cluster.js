88d3257308c1f71012eda943c953ea0b40e51c09
Max Neunhoeffer <max@arangodb.com>
2020-11-10 12:46:00 +0100

    Add test for concurrent write trx and hotbackups.

--- js/server/modules/@arangodb/cluster.js
+++ js/server/modules/@arangodb/cluster.js
@@ -33,7 +33,9 @@ var arangodb = require('@arangodb');
 var ArangoError = arangodb.ArangoError;
 var errors = require("internal").errors;
 var wait = require('internal').wait;
+var time = require('internal').time;
 var isEnterprise = require('internal').isEnterprise();
+var request = require('@arangodb/request');
 var _ = require('lodash');
 
 var endpointToURL = function (endpoint) {
@@ -435,6 +437,18 @@ function queryAgencyJob(id) {
   return {error: true, errorMsg: "Did not find job.", id, job: null};
 }
 
+function createManyBackups(t) {
+  let start = time();
+  while (time() - start < t) {
+    try {
+      require("internal").createHotbackup({});
+      console.log("Created a hotbackup!");
+    } catch(e) {
+      console.error("Caught exception when creating a hotbackup!", e);
+    }
+  }
+}
+
 exports.coordinatorId = coordinatorId;
 exports.isCluster = isCluster;
 exports.isCoordinator = isCoordinator;
@@ -450,3 +464,4 @@ exports.supervisionState = supervisionState;
 exports.waitForSyncRepl = waitForSyncRepl;
 exports.endpoints = endpoints;
 exports.queryAgencyJob = queryAgencyJob;
+exports.createManyBackups = createManyBackups;
---
+++ tests/js/client/shell/shell-deadlock-trx-hotbackup-cluster.js
@@ -0,0 +1,99 @@
+/* jshint globalstrict:false, strict:false, maxlen: 200 */
+/* global fail, assertTrue, assertFalse, assertEqual, assertNotUndefined, arango */
+
+// //////////////////////////////////////////////////////////////////////////////
+// / @brief ArangoDeadLockTests
+// /
+// /
+// / DISCLAIMER
+// /
+// / Copyright 2020 ArangoDB GmbH, Cologne, Germany
+// /
+// / Licensed under the Apache License, Version 2.0 (the "License")
+// / you may not use this file except in compliance with the License.
+// / You may obtain a copy of the License at
+// /
+// /     http://www.apache.org/licenses/LICENSE-2.0
+// /
+// / Unless required by applicable law or agreed to in writing, software
+// / distributed under the License is distributed on an "AS IS" BASIS,
+// / WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// / See the License for the specific language governing permissions and
+// / limitations under the License.
+// /
+// / @author Max Neunhoeffer
+// //////////////////////////////////////////////////////////////////////////////
+
+// tests for deadlocks in the cluster with writing trxs and hotbackup
+
+var jsunity = require('jsunity');
+var internal = require('internal');
+var arangodb = require('@arangodb');
+var db = arangodb.db;
+const isCluster = internal.isCluster();
+const time = internal.time;
+const wait = internal.wait;
+
+function trxWriteHotbackupDeadlock () {
+  'use strict';
+  var cn = 'UnitTestsDeadlockHotbackup';
+  var c = null;
+
+  return {
+
+    setUp: function () {
+      db._drop(cn);
+      c = db._create(cn, {numberOfShards: 1, replicationFactor: 2});
+    },
+
+    tearDown: function () {
+      if (c !== null) {
+        c.drop();
+      }
+      c = null;
+      internal.wait(0);
+    },
+
+    testRunAQLWritingTransactionsDuringHotbackup: function () {
+      if (!isCluster) {
+        return true;
+      }
+
+      wait(3);   // give the cluster some time to settle before we start
+                 // the hotbackups
+
+      let start = time();
+      // This will create lots of hotbackups for approximately 60 seconds
+      db._connection.POST("_admin/execute",`require("@arangodb/cluster.js").createManyBackups(60); return true;`,{"x-arango-async":"store"});
+      // Now we try to write something:
+      for (let i = 0; i < 1000; ++i) {
+        c.insert({Hallo:i});
+      }
+      // And now to execute exclusive write locking transactions:
+      for (let i = 1; i <= 750; ++i) {
+        db._query(`LET m = (FOR d IN ${cn} 
+                              SORT d.Hallo DESC
+                              LIMIT 1
+                              RETURN d)
+                   INSERT {Hallo: m[0].Hallo+1} INTO ${cn}
+                   OPTIONS {exclusive: true}
+                   RETURN NEW`).toArray();
+        if (i % 50 === 0) {
+          print("Done", i, "write transactions.");
+        }
+        wait(0.01);
+      }
+      let diff = time() - start;
+      print("Done 750 exclusive transactions in", diff, "seconds!");
+      assertTrue(diff < 60, "750 transactions took too long, probably some deadlock with hotbackups");
+      while (time() - start < 65) {
+        wait(1.0);
+        print("Waiting for hotbackups to finish...");
+      }
+    }
+  };
+
+}
+
+jsunity.run(trxWriteHotbackupDeadlock);
+return jsunity.done();
