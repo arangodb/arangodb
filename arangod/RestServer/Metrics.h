64c2c8b7889677b2c3d28ba496c8b1389d385ae8
Kaveh Vahedipour <kaveh@arangodb.com>
2020-11-16 14:04:02 +0100

    fixed gauge assignment operatorsd

--- arangod/RestServer/Metrics.h
+++ arangod/RestServer/Metrics.h
@@ -100,20 +100,45 @@ template<typename T> class Gauge : public Metric {
   ~Gauge() = default;
   std::ostream& print (std::ostream&) const;
   Gauge<T>& operator+=(T const& t) {
-    _g.store(_g + t);
+    if constexpr(std::is_integral_v<T>) {
+      _g.fetch_add(t, std::memory_order_relaxed);
+    } else {
+      T tmp;
+      do {
+        tmp = _g.load(std::memory_order_relaxed);
+      } while (!_g.compare_exchange_weak(
+                 tmp, tmp + t, std::memory_order_relaxed, std::memory_order_relaxed));
+
+    }
     return *this;
   }
   Gauge<T>& operator-=(T const& t) {
-    _g.store(_g - t);
+    if constexpr(std::is_integral_v<T>) {
+      _g.fetch_sub(t, std::memory_order_relaxed);
+    } else {
+      T tmp;
+      do {
+        tmp = _g.load(std::memory_order_relaxed);
+      } while (!_g.compare_exchange_weak(
+                 tmp, tmp - t, std::memory_order_relaxed, std::memory_order_relaxed));
+    }
     return *this;
   }
   Gauge<T>& operator*=(T const& t) {
-    _g.store(_g * t);
+    T tmp;
+    do {
+      tmp = _g.load(std::memory_order_relaxed);
+    } while (!_g.compare_exchange_weak(tmp, tmp * t, std::memory_order_relaxed,
+        std::memory_order_relaxed));
     return *this;
   }
   Gauge<T>& operator/=(T const& t) {
     TRI_ASSERT(t != T(0));
-    _g.store(_g / t);
+    T tmp;
+    do {
+      tmp = _g.load(std::memory_order_relaxed);
+    } while (!_g.compare_exchange_weak(tmp, tmp / t, std::memory_order_relaxed,
+        std::memory_order_relaxed));
     return *this;
   }
   Gauge<T>& operator=(T const& t) {
