54b4663b247c873f154404676715236d02594922
Wilfried Goesgens <willi@arangodb.com>
2020-11-19 15:39:02 +0100

    more places to fix sequence

--- arangod/IResearch/IResearchLinkHelper.cpp
+++ arangod/IResearch/IResearchLinkHelper.cpp
@@ -461,11 +461,10 @@ arangodb::Result modifyLinks(                              // modify links
                   collectionsToRemove.end()  // also has a removal request
                                              // (duplicate removal request)
               || collectionsToUpdate.find(cid) != collectionsToUpdate.end())) {  // also has a reindex request
-        itr = linkModifications.erase(itr);
         LOG_TOPIC("5c99e", TRACE, arangodb::iresearch::TOPIC)
             << "modification unnecessary, came from stale list, for link '"
             << state._link->id() << "'";
-
+        itr = linkModifications.erase(itr);
         continue;
       }
 
@@ -483,11 +482,10 @@ arangodb::Result modifyLinks(                              // modify links
           && state._linkDefinitionsOffset >= linkDefinitions.size()  // link removal request
           && collectionsToUpdate.find(state._collection->id()) !=
                  collectionsToUpdate.end()) {  // also has a reindex request
-        itr = linkModifications.erase(itr);
         LOG_TOPIC("1d095", TRACE, arangodb::iresearch::TOPIC)
             << "modification unnecessary, remove+update, for link '"
             << state._link->id() << "'";
-
+        itr = linkModifications.erase(itr);
         continue;
       }
 
