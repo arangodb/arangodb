426feb2ce6309ad9bf806121a4d658ba140a062a
kvahed <kaveh@arangodb.com>
2020-11-20 12:15:21 +0100

    fixes tests

--- arangod/Aql/Query.cpp
+++ arangod/Aql/Query.cpp
@@ -295,6 +295,8 @@ void Query::kill() {
 
 void Query::setExecutionTime() {
   if (_engine != nullptr) {
+    LOG_TOPIC("7ef18", DEBUG, Logger::QUERIES)
+      << TRI_microtime() - _startTime << " peak memory usage:" << _resourceMonitor.currentResources.peakMemoryUsage;
     _engine->_stats.setPeakMemoryUsage(_resourceMonitor.currentResources.peakMemoryUsage);
     _engine->_stats.setExecutionTime(TRI_microtime() - _startTime);
   }
