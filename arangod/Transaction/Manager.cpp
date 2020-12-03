474d92b6d94800f65d656d505dfeadf35072bd83
jsteemann <jan@arangodb.com>
2020-12-03 17:06:16 +0100

    Less dropped followers

--- arangod/Transaction/Manager.cpp
+++ arangod/Transaction/Manager.cpp
@@ -318,7 +318,7 @@ Result Manager::createManagedTrx(TRI_vocbase_t& vocbase, TRI_voc_tid_t tid,
     if (it != buck._managed.end()) {
       return res.reset(TRI_ERROR_TRANSACTION_INTERNAL,
                        std::string("transaction ID '") + std::to_string(tid) +
-                       "' already used, (before creating).");
+                       "' already used (before creating).");
     }
   }
 
@@ -429,9 +429,13 @@ Result Manager::createManagedTrx(TRI_vocbase_t& vocbase, TRI_voc_tid_t tid,
     TRI_ASSERT(state->id() == tid);
     auto it = _transactions[bucket]._managed.try_emplace(tid, MetaType::Managed, ttl, std::move(state));
     if (!it.second) {
+      if (options.isFollowerTransaction || transaction::isFollowerTransactionId(tid)) {
+        TRI_ASSERT(res.ok());
+        return res;
+      }
       return res.reset(TRI_ERROR_TRANSACTION_INTERNAL,
                        std::string("transaction ID '") + std::to_string(tid) +
-                           "' already used, (while creating)");
+                           "' already used (while creating)");
     }
   }
 
