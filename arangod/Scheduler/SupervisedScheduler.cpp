fb354cf352ec1c91571d43301a80c83d53b2b40a
Dan Larkin-York <danielhlarkin@users.noreply.github.com>
2020-12-04 12:08:59 -0500

    Update arangod/Scheduler/SupervisedScheduler.cpp
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/Scheduler/SupervisedScheduler.cpp
+++ arangod/Scheduler/SupervisedScheduler.cpp
@@ -204,8 +204,8 @@ SupervisedScheduler::SupervisedScheduler(application_features::ApplicationServer
                                                                                 "the scheduler"),
                            _server.getFeature<arangodb::MetricsFeature>().gauge("arangodb_scheduler_high_prio_queue_length",
                                                                                 uint64_t(0),
-                                                                                "This is current queue length of the high priority queue in "
-                                                                                "the scheduler."),
+                                                                                "Current queue length of the high priority queue in "
+                                                                                "the scheduler"),
                            _server.getFeature<arangodb::MetricsFeature>().gauge("arangodb_scheduler_medium_prio_queue_length",
                                                                                 uint64_t(0),
                                                                                 "This is current queue length of the medium priority queue in "
