d1675dc62bd459aeff43237dd05b0245c93b193b
Michael Hackstein <michael@arangodb.com>
2020-12-04 10:34:18 +0100

    Added more tests for Multi Client AQL. For now we enforce the situation we are about to fix. Those tests are red, we want to get them green.

--- tests/js/client/communication/test-communication.js
+++ tests/js/client/communication/test-communication.js
@@ -31,49 +31,72 @@ let pu = require('@arangodb/testutils/process-utils');
 let db = arangodb.db;
 const request = require('@arangodb/request');
 
+const endpointToURL = (endpoint) => {
+  if (endpoint.substr(0, 6) === 'ssl://') {
+    return 'https://' + endpoint.substr(6);
+  }
+  let pos = endpoint.indexOf('://');
+  if (pos === -1) {
+    return 'http://' + endpoint;
+  }
+  return 'http' + endpoint.substr(pos);
+};
+
+// detect the path of arangosh. quite hacky, but works
+const arangosh = fs.join(global.ARANGOSH_PATH, 'arangosh' + pu.executableExt);
+
+
+const debug = function (text) {
+  console.warn(text);
+};
+
+function getEndpointById(id) {
+  const toEndpoint = (d) => (d.endpoint);
+  const instanceInfo = JSON.parse(internal.env.INSTANCEINFO);
+  return instanceInfo.arangods.filter((d) => (d.id === id))
+    .map(toEndpoint)
+    .map(endpointToURL)[0];
+}
+
+const runShell = function (args, prefix) {
+  let options = internal.options();
+  args.push('--javascript.startup-directory');
+  args.push(options['javascript.startup-directory']);
+  for (let o in options['javascript.module-directory']) {
+    args.push('--javascript.module-directory');
+    args.push(options['javascript.module-directory'][o]);
+  }
+
+  let endpoint = arango.getEndpoint().replace(/\+vpp/, '').replace(/^http:/, 'tcp:').replace(/^https:/, 'ssl:').replace(/^vst:/, 'tcp:').replace(/^h2:/, 'tcp:');
+  args.push('--server.endpoint');
+  args.push(endpoint);
+  args.push('--server.database');
+  args.push(arango.getDatabaseName());
+  args.push('--server.username');
+  args.push(arango.connectedUser());
+  args.push('--server.password');
+  args.push('');
+  args.push('--log.foreground-tty');
+  args.push('false');
+  args.push('--log.output');
+  args.push('file://' + prefix + '.log');
+
+  let result = internal.executeExternal(arangosh, args, false /*usePipes*/);
+  assertTrue(result.hasOwnProperty('pid'));
+  let status = internal.statusExternal(result.pid);
+  assertEqual(status.status, "RUNNING");
+  return result.pid;
+};
+
+
 function CommunicationSuite() {
   'use strict';
   // generate a random collection name
   const cn = "UnitTests" + require("@arangodb/crypto").md5(internal.genRandomAlphaNumbers(32));
 
-  // detect the path of arangosh. quite hacky, but works
-  const arangosh = fs.join(global.ARANGOSH_PATH, 'arangosh' + pu.executableExt);
-
   assertTrue(fs.isFile(arangosh), "arangosh executable not found!");
 
-  let debug = function (text) {
-    console.warn(text);
-  };
 
-  let runShell = function (args, prefix) {
-    let options = internal.options();
-    args.push('--javascript.startup-directory');
-    args.push(options['javascript.startup-directory']);
-    for (let o in options['javascript.module-directory']) {
-      args.push('--javascript.module-directory');
-      args.push(options['javascript.module-directory'][o]);
-    }
-
-    let endpoint = arango.getEndpoint().replace(/\+vpp/, '').replace(/^http:/, 'tcp:').replace(/^https:/, 'ssl:').replace(/^vst:/, 'tcp:').replace(/^h2:/, 'tcp:');
-    args.push('--server.endpoint');
-    args.push(endpoint);
-    args.push('--server.database');
-    args.push(arango.getDatabaseName());
-    args.push('--server.username');
-    args.push(arango.connectedUser());
-    args.push('--server.password');
-    args.push('');
-    args.push('--log.foreground-tty');
-    args.push('false');
-    args.push('--log.output');
-    args.push('file://' + prefix + '.log');
-
-    let result = internal.executeExternal(arangosh, args, false /*usePipes*/);
-    assertTrue(result.hasOwnProperty('pid'));
-    let status = internal.statusExternal(result.pid);
-    assertEqual(status.status, "RUNNING");
-    return result.pid;
-  };
 
   let buildCode = function (key, command) {
     let file = fs.getTempFile() + "-" + key;
@@ -99,6 +122,77 @@ function CommunicationSuite() {
   };
 
 
+
+
+  // TODO externalize
+
+  function getEndpointsByType(type) {
+    const isType = (d) => (d.role.toLowerCase() === type);
+    const toEndpoint = (d) => (d.endpoint);
+    const endpointToURL = (endpoint) => {
+      if (endpoint.substr(0, 6) === 'ssl://') {
+        return 'https://' + endpoint.substr(6);
+      }
+      let pos = endpoint.indexOf('://');
+      if (pos === -1) {
+        return 'http://' + endpoint;
+      }
+      return 'http' + endpoint.substr(pos);
+    };
+
+    const instanceInfo = JSON.parse(internal.env.INSTANCEINFO);
+    return instanceInfo.arangods.filter(isType)
+      .map(toEndpoint)
+      .map(endpointToURL);
+  }
+
+  return {
+
+    setUp: function () {
+      db._drop(cn);
+      db._create(cn);
+
+      db._drop("UnitTestsTemp");
+      let c = db._create("UnitTestsTemp");
+      let docs = [];
+      for (let i = 0; i < 50000; ++i) {
+        docs.push({ value: i });
+        if (docs.length === 5000) {
+          c.insert(docs);
+          docs = [];
+        }
+      }
+    },
+
+    tearDown: function () {
+      db._drop(cn);
+      db._drop("UnitTestsTemp");
+    },
+
+    testWorkInParallel: function () {
+      let tests = [
+        ['simple-1', 'db._query("FOR doc IN _users RETURN doc");'],
+        ['simple-2', 'db._query("FOR doc IN _users RETURN doc");'],
+        ['insert-remove', 'db._executeTransaction({ collections: { write: "UnitTestsTemp" }, action: function() { let db = require("internal").db; let docs = []; for (let i = 0; i < 1000; ++i) docs.push({ _key: "test" + i }); let c = db.UnitTestsTemp; c.insert(docs); c.remove(docs); } });'],
+        ['aql', 'db._query("FOR doc IN UnitTestsTemp RETURN doc._key");'],
+      ];
+
+      // add some cluster stuff
+      if (internal.isCluster()) {
+        tests.push(['cluster-health', 'if (arango.GET("/_admin/cluster/health").code !== 200) { throw "nono cluster"; }']);
+      };
+
+      // run the suite for 5 minutes
+      runTests(tests, 5 * 60);
+    },
+  };
+}
+
+function AqlSetupPathSuite() {
+  'use strict';
+  // generate a random collection name
+  const cn = "UnitTests" + require("@arangodb/crypto").md5(internal.genRandomAlphaNumbers(32));
+  const twoShardColName = "UnitTestsTwoShard";
 
   let runTests = function (tests, duration) {
     assertFalse(db[cn].exists("stop"));
@@ -192,6 +286,83 @@ function CommunicationSuite() {
       });
     }
   };
+
+  /// @brief set failure point
+  function debugCanUseFailAt(endpoint) {
+    let res = request.get({
+      url: endpoint + '/_admin/debug/failat',
+    });
+    return res.status === 200;
+  }
+
+  /// @brief set failure point
+  function debugSetFailAt(endpoint, failAt) {
+    let res = request.put({
+      url: endpoint + '/_admin/debug/failat/' + failAt,
+      body: ""
+    });
+    if (res.status !== 200) {
+      throw "Error setting failure point";
+    }
+  }
+
+  /// @brief remove failure point
+  function debugRemoveFailAt(endpoint, failAt) {
+    let res = request.delete({
+      url: endpoint + '/_admin/debug/failat/' + failAt,
+      body: ""
+    });
+    if (res.status !== 200) {
+      throw "Error removing failure point";
+    }
+  }
+
+  function debugClearFailAt(endpoint) {
+    let res = request.delete({
+      url: endpoint + '/_admin/debug/failat',
+      body: ""
+    });
+    if (res.status !== 200) {
+      throw "Error removing failure points";
+    }
+  }
+
+  const activateShardLockingFailure = () => {
+    const shardList = db[twoShardColName].shards(true);
+    for (const [shard, servers] of Object.entries(shardList)) {
+      const endpoint = getEndpointById(servers[0]);
+      debugSetFailAt(endpoint, `WaitOnLock::${shard}`);
+    }
+  };
+
+  const activateTriggersAQLSetupSlowPathTest = () => {
+    let endpoint = endpointToURL(arango.getEndpoint());
+    debugSetFailAt(endpoint, `AqlSetupSlowPath::${twoShardColName}`);
+  };
+
+  const activateTriggersAQLSetupNoSlowPathTest = () => {
+    let endpoint = endpointToURL(arango.getEndpoint());
+    debugSetFailAt(endpoint, `AqlSetupNoSlowPath::${twoShardColName}`);
+  };
+
+  const deactivateShardLockingFailure = () => {
+    const shardList = db[twoShardColName].shards(true);
+    for (const [shard, servers] of Object.entries(shardList)) {
+      const endpoint = getEndpointById(servers[0]);
+      debugClearFailAt(endpoint);
+    }
+  };
+
+  const deactivateTriggersAQLSetupPathTest = () => {
+    let endpoint = endpointToURL(arango.getEndpoint());
+    debugClearFailAt(endpoint);
+  };
+
+  const docsPerWrite = 10;
+  const exclusiveQuery = `db._query("FOR x IN 1..${docsPerWrite} INSERT {} INTO ${twoShardColName} OPTIONS {exclusive: true}")`;
+  const writeQuery = `db._query("FOR x IN 1..${docsPerWrite} INSERT {} INTO ${twoShardColName} OPTIONS {exclusive: false}")`;
+  const readQuery = `db._query("FOR x IN ${twoShardColName} RETURN x")`;
+
 
   const buildSingleRunCode = (key, command) => {
     const cmd = `
@@ -287,107 +458,6 @@ function CommunicationSuite() {
     }
   };
 
-  const twoShardColName = "UnitTestsTwoShard";
-
-  // TODO externalize
-
-
-
-  function getEndpointById(id) {
-    const toEndpoint = (d) => (d.endpoint);
-    const endpointToURL = (endpoint) => {
-      if (endpoint.substr(0, 6) === 'ssl://') {
-        return 'https://' + endpoint.substr(6);
-      }
-      let pos = endpoint.indexOf('://');
-      if (pos === -1) {
-        return 'http://' + endpoint;
-      }
-      return 'http' + endpoint.substr(pos);
-    };
-
-    const instanceInfo = JSON.parse(internal.env.INSTANCEINFO);
-    return instanceInfo.arangods.filter((d) => (d.id === id))
-      .map(toEndpoint)
-      .map(endpointToURL)[0];
-  }
-
-  function getEndpointsByType(type) {
-    const isType = (d) => (d.role.toLowerCase() === type);
-    const toEndpoint = (d) => (d.endpoint);
-    const endpointToURL = (endpoint) => {
-      if (endpoint.substr(0, 6) === 'ssl://') {
-        return 'https://' + endpoint.substr(6);
-      }
-      let pos = endpoint.indexOf('://');
-      if (pos === -1) {
-        return 'http://' + endpoint;
-      }
-      return 'http' + endpoint.substr(pos);
-    };
-
-    const instanceInfo = JSON.parse(internal.env.INSTANCEINFO);
-    return instanceInfo.arangods.filter(isType)
-      .map(toEndpoint)
-      .map(endpointToURL);
-  }
-
-  /// @brief set failure point
-  function debugCanUseFailAt(endpoint) {
-    let res = request.get({
-      url: endpoint + '/_admin/debug/failat',
-    });
-    return res.status === 200;
-  }
-
-  /// @brief set failure point
-  function debugSetFailAt(endpoint, failAt) {
-    let res = request.put({
-      url: endpoint + '/_admin/debug/failat/' + failAt,
-      body: ""
-    });
-    if (res.status !== 200) {
-      throw "Error setting failure point";
-    }
-  }
-
-  /// @brief remove failure point
-  function debugRemoveFailAt(endpoint, failAt) {
-    let res = request.delete({
-      url: endpoint + '/_admin/debug/failat/' + failAt,
-      body: ""
-    });
-    if (res.status !== 200) {
-      throw "Error removing failure point";
-    }
-  }
-
-  function debugClearFailAt(endpoint) {
-    let res = request.delete({
-      url: endpoint + '/_admin/debug/failat',
-      body: ""
-    });
-    if (res.status !== 200) {
-      throw "Error removing failure points";
-    }
-  }
-
-  const activateShardLockingFailure = () => {
-    const shardList = db[twoShardColName].shards(true);
-    for (const [shard, servers] of Object.entries(shardList)) {
-      const endpoint = getEndpointById(servers[0]);
-      debugSetFailAt(endpoint, `WaitOnLock::${shard}`);
-    }
-  };
-
-  const deactivateShardLockingFailure = () => {
-    const shardList = db[twoShardColName].shards(true);
-    for (const [shard, servers] of Object.entries(shardList)) {
-      const endpoint = getEndpointById(servers[0]);
-      debugClearFailAt(endpoint);
-    }
-  };
-
   return {
 
     setUp: function () {
@@ -396,58 +466,110 @@ function CommunicationSuite() {
 
       db._drop(twoShardColName);
       db._create(twoShardColName, { numberOfShards: 2 });
-
-      db._drop("UnitTestsTemp");
-      let c = db._create("UnitTestsTemp");
-      let docs = [];
-      for (let i = 0; i < 50000; ++i) {
-        docs.push({ value: i });
-        if (docs.length === 5000) {
-          c.insert(docs);
-          docs = [];
-        }
-      }
     },
 
     tearDown: function () {
       db._drop(cn);
-      db._drop("UnitTestsTemp");
       deactivateShardLockingFailure();
+      deactivateTriggersAQLSetupPathTest();
       db._drop(twoShardColName);
     },
 
-    testWorkInParallel: function () {
+    testAqlSetupPathDeadLockExclusiveExclusive: function () {
+      assertEqual(db[twoShardColName].count(), 0);
       let tests = [
-        ['simple-1', 'db._query("FOR doc IN _users RETURN doc");'],
-        ['simple-2', 'db._query("FOR doc IN _users RETURN doc");'],
-        ['insert-remove', 'db._executeTransaction({ collections: { write: "UnitTestsTemp" }, action: function() { let db = require("internal").db; let docs = []; for (let i = 0; i < 1000; ++i) docs.push({ _key: "test" + i }); let c = db.UnitTestsTemp; c.insert(docs); c.remove(docs); } });'],
-        ['aql', 'db._query("FOR doc IN UnitTestsTemp RETURN doc._key");'],
+        ["exclusive-1", exclusiveQuery],
+        ["exclusive-2", exclusiveQuery]
       ];
+      activateShardLockingFailure();
+      // We enforce a deadlock -> go slowpath
+      activateTriggersAQLSetupSlowPathTest();
 
-      // add some cluster stuff
-      if (internal.isCluster()) {
-        tests.push(['cluster-health', 'if (arango.GET("/_admin/cluster/health").code !== 200) { throw "nono cluster"; }']);
-      };
-
-      // run the suite for 5 minutes
-      runTests(tests, 5 * 60);
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), 2 * docsPerWrite);
     },
 
-    testAqlSlowPathDeadLockExclusiveExclusive: function () {
-      if (internal.isCluster()) {
-        let tests = [
-          ["exclusive-1", `db._query("FOR x IN 1..10 INSERT {} INTO ${twoShardColName} OPTIONS {exclusive: true}")`],
-          ["exclusive-2", `db._query("FOR x IN 1..10 INSERT {} INTO ${twoShardColName} OPTIONS {exclusive: true}")`]
-        ];
-        activateShardLockingFailure();
+    testAqlSetupPathDeadLockExclusiveWrite: function () {
+      assertEqual(db[twoShardColName].count(), 0);
+      let tests = [
+        ["exclusive-1", exclusiveQuery],
+        ["write-2", writeQuery]
+      ];
+      activateShardLockingFailure();
+      // We enforce a deadlock -> go slowpath
+      activateTriggersAQLSetupSlowPathTest();
 
-        // run both queries in parallel
-        singleRun(tests);
-      }
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), 2 * docsPerWrite);
+    },
+
+    testAqlSetupPathDeadLockExclusiveRead: function () {
+      assertEqual(db[twoShardColName].count(), 0);
+      let tests = [
+        ["exclusive-1", exclusiveQuery],
+        ["read-2", readQuery]
+      ];
+      activateShardLockingFailure();
+      // Read cannot deadlock -> do not go slow path
+      activateTriggersAQLSetupNoSlowPathTest();
+
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), docsPerWrite);
+    },
+
+    testAqlSetupPathDeadLockWriteWrite: function () {
+      assertEqual(db[twoShardColName].count(), 0);
+      let tests = [
+        ["write-1", writeQuery],
+        ["write-2", writeQuery]
+      ];
+      activateShardLockingFailure();
+      // Write / Write cannot dead lock -> do not go slow path
+      activateTriggersAQLSetupNoSlowPathTest();
+
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), 2 * docsPerWrite);
+    },
+
+    testAqlSetupPathDeadLockWriteRead: function () {
+      assertEqual(db[twoShardColName].count(), 0);
+      let tests = [
+        ["write-1", writeQuery],
+        ["read-2", readQuery]
+      ];
+      activateShardLockingFailure();
+      // Read cannot deadlock -> do not go slow path
+      activateTriggersAQLSetupNoSlowPathTest();
+
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), docsPerWrite);
+    },
+
+    testAqlSetupPathDeadLockReadRead: function () {
+      assertEqual(db[twoShardColName].count(), 0);
+      let tests = [
+        ["read-1", readQuery],
+        ["read-2", readQuery]
+      ];
+      activateShardLockingFailure();
+      // Read cannot deadlock -> do not go slow path
+      activateTriggersAQLSetupNoSlowPathTest();
+
+      // run both queries in parallel
+      singleRun(tests);
+      assertEqual(db[twoShardColName].count(), 0);
     },
   };
 }
 
 jsunity.run(CommunicationSuite);
+if (internal.isCluster()) {
+  jsunity.run(AqlSetupPathSuite);
+}
 
 return jsunity.done();
