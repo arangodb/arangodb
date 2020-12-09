c9112e77b090513ae4c79f57f0952246bf9fa4b9
Dan Larkin-York <dan@arangodb.com>
2020-12-09 12:17:14 -0500

    Add some clarification

--- arangod/RocksDBEngine/Listeners/RocksDBMetricsListener.h
+++ arangod/RocksDBEngine/Listeners/RocksDBMetricsListener.h
@@ -31,6 +31,7 @@
 
 namespace arangodb {
 
+/// @brief Gathers better metrics from RocksDB than we can get by scraping alone.
 class RocksDBMetricsListener : public rocksdb::EventListener {
  public:
   explicit RocksDBMetricsListener(application_features::ApplicationServer&);
