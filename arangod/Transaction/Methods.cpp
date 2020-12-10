9bd385d1e5f3fb0570304acc334d2bd0a5a5e245
jsteemann <jan@arangodb.com>
2020-12-10 23:45:03 +0100

    Fix cluster-internal replication of documents with special keys
    (percent character, which has a special meaning when used inside URLs).

--- arangod/Transaction/Methods.cpp
+++ arangod/Transaction/Methods.cpp
@@ -2269,7 +2269,7 @@ Future<Result> Methods::replicateOperations(
     url.push_back('/');
     VPackValueLength len;
     const char* ptr = value.get(StaticStrings::KeyString).getString(len);
-    url.append(ptr, len);
+    basics::StringUtils::encodeURIComponent(url, ptr, len);
   }
 
   arangodb::fuerte::RestVerb requestType = arangodb::fuerte::RestVerb::Illegal;
