52acbe063003ca54f6d0ca2cac82653d1a603f99
hkernbach <heiko@arangodb.com>
2020-12-21 14:39:41 +0100

    template class definitions for mockprovider tracing

--- tests/Graph/GraphMockProviderInstances.cpp
+++ tests/Graph/GraphMockProviderInstances.cpp
@@ -23,31 +23,37 @@
 
 #include "./MockGraphProvider.h"
 #include "Graph/Enumerators/TwoSidedEnumerator.cpp"
-#include "Graph/PathManagement/PathStore.h"
-#include "Graph/PathManagement/PathStore.cpp"
-#include "Graph/PathManagement/PathResult.h"
 #include "Graph/PathManagement/PathResult.cpp"
+#include "Graph/PathManagement/PathResult.h"
+#include "Graph/PathManagement/PathStore.cpp"
+#include "Graph/PathManagement/PathStore.h"
 #include "Graph/Queues/FifoQueue.h"
 
-#include "Graph/Queues/QueueTracer.h"
+#include "Graph/PathManagement/PathStoreTracer.cpp"
+#include "Graph/PathManagement/PathStoreTracer.h"
 #include "Graph/Queues/QueueTracer.cpp"
+#include "Graph/Queues/QueueTracer.h"
 
+template class ::arangodb::graph::PathResult<::arangodb::tests::graph::MockGraphProvider,
+                                             ::arangodb::tests::graph::MockGraphProvider::Step>;
 
-template class
-::arangodb::graph::PathResult<::arangodb::tests::graph::MockGraphProvider, ::arangodb::tests::graph::MockGraphProvider::Step>;
-
-template class
-::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>;
+template class ::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>;
 
 template class ::arangodb::graph::TwoSidedEnumerator<
     ::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>,
     ::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>,
     ::arangodb::tests::graph::MockGraphProvider>;
 
-template class
-::arangodb::graph::QueueTracer<::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>>;
+template class ::arangodb::graph::QueueTracer<::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>>;
 
 template class ::arangodb::graph::TwoSidedEnumerator<
-::arangodb::graph::QueueTracer<::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>>,
-::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>,
-::arangodb::tests::graph::MockGraphProvider>;
+    ::arangodb::graph::QueueTracer<::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>>,
+    ::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>,
+    ::arangodb::tests::graph::MockGraphProvider>;
+
+template class ::arangodb::graph::PathStoreTracer<::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>>;
+
+template class ::arangodb::graph::TwoSidedEnumerator<
+    ::arangodb::graph::QueueTracer<::arangodb::graph::FifoQueue<::arangodb::tests::graph::MockGraphProvider::Step>>,
+    ::arangodb::graph::PathStoreTracer<::arangodb::graph::PathStore<::arangodb::tests::graph::MockGraphProvider::Step>>,
+    ::arangodb::tests::graph::MockGraphProvider>;
