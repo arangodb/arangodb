a817fcfd526528308c281772e2c29415db086e10
Michael Hackstein <michael@arangodb.com>
2020-12-22 09:49:07 +0100

    Update arangod/Aql/Query.cpp
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/Aql/Query.cpp
+++ arangod/Aql/Query.cpp
@@ -199,13 +199,13 @@ std::string const& Query::user() const {
   return _user;
 }
 
-double Query::getLockTimeout() const noexcept{
+double Query::getLockTimeout() const noexcept {
   return _queryOptions.transactionOptions.lockTimeout;
-};
+}
 
 void Query::setLockTimeout(double timeout) noexcept {
   _queryOptions.transactionOptions.lockTimeout = timeout;
-};
+}
 
 bool Query::killed() const {
   if (_queryOptions.maxRuntime > std::numeric_limits<double>::epsilon() &&
@@ -1322,4 +1322,3 @@ aql::ExecutionState Query::cleanupTrxAndEngines(int errorCode) {
   
   return ExecutionState::WAITING;
 }
-
