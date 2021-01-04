b08d659bd0908d0e166432d4a53cc8be9467b0d5
Andreas D Jung <7018968+coronabytes@users.noreply.github.com>
2021-01-04 14:33:42 +0100

    Update tests/js/server/aql/aql-functions-date.js
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- tests/js/server/aql/aql-functions-date.js
+++ tests/js/server/aql/aql-functions-date.js
@@ -2484,9 +2484,9 @@ function ahuacatlDateFunctionsTestSuite () {
 	
 	testDateTimeZones: function () {
 	  var res = getQueryResults("RETURN DATE_TIMEZONES()", {})[0];
-	  assertTrue(res.indexOf("America/New_York") !== -1);
-	  assertTrue(res.indexOf("Europe/Berlin") !== -1);
-	  assertTrue(res.indexOf("Asia/Shanghai") !== -1);
+	  assertNotEqual(-1, res.indexOf("America/New_York"));
+	  assertNotEqual(-1, res.indexOf("Europe/Berlin"));
+	  assertNotEqual(-1, res.indexOf("Asia/Shanghai"));
     },
 
     // //////////////////////////////////////////////////////////////////////////////
