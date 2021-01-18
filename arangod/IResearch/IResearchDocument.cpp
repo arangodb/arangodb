3cd79a4ed955381f39e803d23bda7826344e32a6
Andrei Lobov <andrei.lobov@arangodb.com>
2021-01-18 12:56:45 +0300

    Update arangod/IResearch/IResearchDocument.cpp
    Co-authored-by: Andrey Abramov <andrey@arangodb.com>

--- arangod/IResearch/IResearchDocument.cpp
+++ arangod/IResearch/IResearchDocument.cpp
@@ -618,7 +618,7 @@ setAnalyzers:
   return true;
 }
 
- StoredValue::StoredValue(transaction::Methods const& t, irs::string_ref cn, VPackSlice const& doc)
+  StoredValue::StoredValue(transaction::Methods const& t, irs::string_ref cn, VPackSlice const doc)
     : trx(t), document(doc), collection(cn),
       isDBServer(ServerState::instance()->isDBServer()) {}
 
