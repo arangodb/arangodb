868ceee5e4b3462efb1bc1775036d1794d4df827
hkernbach <heiko@arangodb.com>
2021-01-19 14:47:53 +0100

    no need to pass all opts here, passing now only the needed param

--- arangod/Cluster/ClusterMethods.cpp
+++ arangod/Cluster/ClusterMethods.cpp
@@ -1952,7 +1952,7 @@ Future<OperationResult> getDocumentOnCoordinator(transaction::Methods& trx,
 
 Result fetchEdgesFromEngines(transaction::Methods& trx,
                              graph::ClusterTraverserCache& travCache,
-                             traverser::TraverserOptions const* opts,
+                             arangodb::aql::FixedVarExpressionContext const& expressionContext,
                              arangodb::velocypack::StringRef vertexId,
                              size_t depth,
                              std::vector<VPackSlice>& result) {
@@ -1970,7 +1970,7 @@ Result fetchEdgesFromEngines(transaction::Methods& trx,
   leased->add(VPackValue("variables"));
   {
     leased->openArray();
-    opts->serializeVariables(*(leased.get()));
+    expressionContext.serializeAllVariables(trx.vpackOptions(), *(leased.get()));
     leased->close();
   }
   leased->close();
