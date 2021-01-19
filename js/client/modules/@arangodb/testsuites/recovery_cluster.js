b051643921c06c0e1c1931b4b5a09d383c45ad1b
Andrei Lobov <andrei.lobov@arangodb.com>
2021-01-19 17:38:53 +0300

    made recovery cluster as separate testsuite

--- js/client/modules/@arangodb/testsuites/recovery_cluster.js
+++ js/client/modules/@arangodb/testsuites/recovery_cluster.js
@@ -2,36 +2,37 @@
 /* global print */
 'use strict';
 
-// //////////////////////////////////////////////////////////////////////////////
-// / DISCLAIMER
-// /
-// / Copyright 2016 ArangoDB GmbH, Cologne, Germany
-// / Copyright 2014 triagens GmbH, Cologne, Germany
-// /
-// / Licensed under the Apache License, Version 2.0 (the "License")
-// / you may not use this file except in compliance with the License.
-// / You may obtain a copy of the License at
-// /
-// /     http://www.apache.org/licenses/LICENSE-2.0
-// /
-// / Unless required by applicable law or agreed to in writing, software
-// / distributed under the License is distributed on an "AS IS" BASIS,
-// / WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// / See the License for the specific language governing permissions and
-// / limitations under the License.
-// /
-// / Copyright holder is ArangoDB GmbH, Cologne, Germany
-// /
-// / @author Max Neunhoeffer
-// //////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Andrei Lobov
+////////////////////////////////////////////////////////////////////////////////
 
 const functionsDocumentation = {
-  'recovery': 'run recovery tests'
+  'recovery_cluster': 'run recovery tests for cluster'
 };
 const optionsDocumentation = [
 ];
 
 const fs = require('fs');
+const internal = require('internal');
 const pu = require('@arangodb/testutils/process-utils');
 const tu = require('@arangodb/testutils/test-utils');
 const _ = require('lodash');
@@ -42,17 +43,52 @@ const RED = require('internal').COLORS.COLOR_RED;
 const RESET = require('internal').COLORS.COLOR_RESET;
 const BLUE = require('internal').COLORS.COLOR_BLUE;
 
+const termSignal = 15;
+
 const testPaths = {
-  'recovery': [tu.pathForTesting('server/recovery')]
+  'recovery_cluster': [tu.pathForTesting('server/recovery')]
 };
 
+/// ensure that we have enough db servers in cluster tests
+function ensureServers(options, numServers) {
+  if (options.cluster && options.dbServers < numServers) {
+    let localOptions = _.clone(options);
+    localOptions.dbServers = numServers;
+    return localOptions;
+  }
+  return options;
+}
+
+function checkServersGOOD(instanceInfo) {
+  try {
+    let health = require('internal').clusterHealth();
+    let rc = instanceInfo.arangods.reduce((previous, arangod) => {
+        if (arangod.role === "agent") return true;
+        if (health.hasOwnProperty(arangod.id)) {
+          if (health[arangod.id].Status === "GOOD") {
+            return previous;
+          } else {
+            print(RED + "ClusterHealthCheck failed " + arangod.id + " has status " + health[arangod.id].Status + " (which is not equal to GOOD)");
+            return false;
+          }
+        } else {
+          print(RED + "ClusterHealthCheck failed " + arangod.id + " does not have health property");
+          return false;
+        }
+      }, true);
+    return rc;
+  } catch(e) {
+    print("Error checking cluster health " + e);
+    return false;
+  }
+}
+
 // //////////////////////////////////////////////////////////////////////////////
-// / @brief TEST: recovery
+// / @brief TEST: recovery_cluster
 // //////////////////////////////////////////////////////////////////////////////
 
 function runArangodRecovery (params) {
   let useEncryption = false;
-
   if (global.ARANGODB_CLIENT_VERSION) {
     let version = global.ARANGODB_CLIENT_VERSION(true);
     if (version.hasOwnProperty('enterprise-version')) {
@@ -64,6 +100,9 @@ function runArangodRecovery (params) {
     'log.foreground-tty': 'true',
     'database.ignore-datafile-errors': 'false', // intentionally false!
   };
+  
+  // for cluster runs we have separate parameter set for servers and for testagent(arangosh)
+  let additionalTestParams  = {};
 
   if (useEncryption) {
     // randomly turn on or off hardware-acceleration for encryption for both
@@ -73,23 +112,14 @@ function runArangodRecovery (params) {
     // incompatibility between the two modes, this will likely find it
     additionalParams['rocksdb.encryption-hardware-acceleration'] = (Math.random() * 100 >= 50) ? "true" : "false";
   }
-
   let argv = [];
-
   let binary = pu.ARANGOD_BIN;
-  let crashLogDir = fs.join(fs.getTempPath(), 'crash');
-  fs.makeDirectoryRecursive(crashLogDir);
-  pu.cleanupDBDirectoriesAppend(crashLogDir);
-
-  let crashLog = fs.join(crashLogDir, 'crash.log');
+  // arangosh has different name for parameter :(
+  additionalTestParams['javascript.execute'] =  params.script;
+  additionalTestParams['javascript.run-main'] = true;
 
   if (params.setup) {
-    additionalParams['javascript.script-parameter'] = 'setup';
-    try {
-      // clean up crash log before next test
-      fs.remove(crashLog);
-    } catch (err) {}
-
+    additionalTestParams['javascript.script-parameter'] = 'setup';
 
     // special handling for crash-handler recovery tests
     if (params.script.match(/crash-handler/)) {
@@ -99,17 +129,10 @@ function runArangodRecovery (params) {
     }
 
     params.options.disableMonitor = true;
-    params.testDir = fs.join(params.tempDir, `${params.count}`);
-    pu.cleanupDBDirectoriesAppend(params.testDir);
-    let dataDir = fs.join(params.testDir, 'data');
-    let appDir = fs.join(params.testDir, 'app');
-    let tmpDir = fs.join(params.testDir, 'tmp');
-    fs.makeDirectoryRecursive(params.testDir);
-    fs.makeDirectoryRecursive(dataDir);
-    fs.makeDirectoryRecursive(tmpDir);
-    fs.makeDirectoryRecursive(appDir);
-
-    let args = pu.makeArgs.arangod(params.options, appDir, '', tmpDir);
+    params.options =  ensureServers(params.options);
+    let args = {};
+    args['temp.path'] = params.instanceInfo.rootDir;
+    
     // enable development debugging if extremeVerbosity is set
     if (params.options.extremeVerbosity === true) {
       args['log.level'] = 'development=info';
@@ -117,13 +140,8 @@ function runArangodRecovery (params) {
     args = Object.assign(args, params.options.extraArgs);
     args = Object.assign(args, {
       'rocksdb.wal-file-timeout-initial': 10,
-      'database.directory': fs.join(dataDir + 'db'),
-      'server.rest-server': 'false',
-      'replication.auto-start': 'true',
-      'javascript.script': params.script
+      'replication.auto-start': 'true'
     });
-      
-    args['log.output'] = 'file://' + crashLog;
 
     if (useEncryption) {
       let keyDir = fs.join(fs.getTempPath(), 'arango_encryption');
@@ -148,26 +166,54 @@ function runArangodRecovery (params) {
     }
 
     params.args = args;
-
-    argv = toArgv(Object.assign(params.args, additionalParams));
+      
   } else {
-    additionalParams['javascript.script-parameter'] = 'recory';
-    argv = toArgv(Object.assign(params.args, additionalParams));
-    
-    if (params.options.rr) {
-      binary = 'rr';
-      argv.unshift(pu.ARANGOD_BIN);
+    additionalTestParams['javascript.script-parameter'] = 'recory';
+  }
+
+  if (params.setup) {
+    params.args = Object.assign(params.args, additionalParams);
+    params.instanceInfo = pu.startInstance(params.options.protocol,
+                                           params.options,
+                                           params.args,
+                                           fs.join('recovery_cluster', params.count.toString()));
+  } else {
+    print(BLUE + "Restarting cluster " + RESET);
+    pu.reStartInstance(params.options, params.instanceInfo, {});
+    let tryCount = 10;
+    while(tryCount > 0 && !checkServersGOOD(params.instanceInfo)) {
+      print(RESET + "Waiting for all servers to go GOOD");
+      internal.sleep(3); // give agency time to bootstrap DBServers
+      --tryCount;
     }
   }
-    
-  process.env["crash-log"] = crashLog;
-  params.instanceInfo.pid = pu.executeAndWait(
-    binary,
-    argv,
-    params.options,
-    'recovery',
-    params.instanceInfo.rootDir,
-    !params.setup && params.options.coreCheck);
+  let agentArgs = pu.makeArgs.arangosh(params.options);
+  agentArgs['server.endpoint'] = tu.findEndpoint(params.options, params.instanceInfo);
+  if (params.args['log.level']) {
+    agentArgs['log.level'] = params.args['log.level'];
+  }
+  agentArgs['temp.path'] = params.instanceInfo.rootDir;
+  Object.assign(agentArgs, additionalTestParams);
+  require('internal').env.INSTANCEINFO = JSON.stringify(params.instanceInfo);
+  try {
+    pu.executeAndWait(pu.ARANGOSH_BIN, toArgv(agentArgs), params.options, 'arangosh', params.instanceInfo.rootDir, false);
+  } catch(err) {
+    print('Error while launching test:' + err);
+    pu.shutdownInstance(params.instanceInfo, params.options, false);
+    return; // without test server test will for sure fail
+  }
+  if (params.setup) {
+    let dbServers = params.instanceInfo.arangods.slice().filter((a) => {return a.role === 'dbserver' || a.role === 'coordinator';});
+    print(BLUE + "killing " + dbServers.length + " DBServers/Coordinators " + RESET);
+    dbServers.forEach((arangod) => {
+      internal.debugTerminateInstance(arangod.endpoint);
+      // need this to properly mark spawned process as killed in internal test data
+      arangod.exitStatus = internal.killExternal(arangod.pid, termSignal); 
+      arangod.pid = 0;
+    });
+  } else {
+    pu.shutdownInstance(params.instanceInfo, params.options, false);
+  }
 }
 
 function recovery (options) {
@@ -192,7 +238,7 @@ function recovery (options) {
 
   let count = 0;
   let orgTmp = process.env.TMPDIR;
-  let tempDir = fs.join(fs.getTempPath(), 'recovery');
+  let tempDir = fs.join(fs.getTempPath(), 'recovery_cluster');
   fs.makeDirectoryRecursive(tempDir);
   process.env.TMPDIR = tempDir;
   pu.cleanupDBDirectoriesAppend(tempDir);
@@ -208,7 +254,7 @@ function recovery (options) {
       let params = {
         tempDir: tempDir,
         instanceInfo: {
-          rootDir: fs.join(fs.getTempPath(), 'recovery', count.toString())
+          rootDir: fs.join(fs.getTempPath(), 'recovery_cluster', count.toString())
         },
         options: _.cloneDeep(options),
         script: test,
@@ -269,7 +315,7 @@ function recovery (options) {
 
 exports.setup = function (testFns, defaultFns, opts, fnDocs, optionsDoc, allTestPaths) {
   Object.assign(allTestPaths, testPaths);
-  testFns['recovery'] = recovery;
+  testFns['recovery_cluster'] = recovery;
   for (var attrname in functionsDocumentation) { fnDocs[attrname] = functionsDocumentation[attrname]; }
   for (var i = 0; i < optionsDocumentation.length; i++) { optionsDoc.push(optionsDocumentation[i]); }
 };
