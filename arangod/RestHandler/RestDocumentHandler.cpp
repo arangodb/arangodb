7876c52a715a41eee90761eaef84586c012c9515
Max Neunhöffer <max@arangodb.com>
2021-01-22 18:34:04 +0100

    Bug in behavior of ignoreRevs=false (#13420)

--- arangod/RestHandler/RestDocumentHandler.cpp
+++ arangod/RestHandler/RestDocumentHandler.cpp
@@ -476,11 +476,15 @@ RestStatus RestDocumentHandler::modifyDocument(bool isPatch) {
         if (headerRev.isSet()) {
           builder.add(StaticStrings::RevString, VPackValue(headerRev.toString()));
         } else if (!opOptions.ignoreRevs && revInBody.isSet()) {
-          builder.add(StaticStrings::RevString, VPackValue(headerRev.toString()));
+          builder.add(StaticStrings::RevString, VPackValue(revInBody.toString()));
+          headerRev = revInBody;   // make sure that we report 412 and not 409
         }
       }
 
       body = builder.slice();
+    } else if (!headerRev.isSet() && revInBody.isSet() &&
+               opOptions.ignoreRevs == false) {
+      headerRev = revInBody;   // make sure that we report 412 and not 409
     }
   }
 
