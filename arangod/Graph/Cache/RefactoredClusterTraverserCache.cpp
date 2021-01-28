e4060832ff414144fc41e6645c267dd599d8eea5
hkernbach <heiko@arangodb.com>
2021-01-28 16:29:09 +0100

    source files for RefactoredClusterTraverserCache

---
+++ arangod/Graph/Cache/RefactoredClusterTraverserCache.cpp
@@ -0,0 +1,131 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+////////////////////////////////////////////////////////////////////////////////
+
+#include "RefactoredClusterTraverserCache.h"
+
+#include "Aql/AqlValue.h"
+#include "Aql/Query.h"
+#include "Basics/ResourceUsage.h"
+#include "Basics/VelocyPackHelper.h"
+#include "Cluster/ServerState.h"
+#include "Graph/BaseOptions.h"
+#include "Graph/EdgeDocumentToken.h"
+#include "Transaction/Methods.h"
+
+#include <velocypack/Builder.h>
+#include <velocypack/HashedStringRef.h>
+#include <velocypack/Slice.h>
+#include <velocypack/StringRef.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::basics;
+using namespace arangodb::graph;
+
+RefactoredClusterTraverserCache::RefactoredClusterTraverserCache(
+    aql::QueryContext& query,
+    std::unordered_map<ServerID, aql::EngineId> const* engines, BaseOptions* options)
+    : TraverserCache(query, options),
+      _datalake(options->resourceMonitor()),
+      _engines(engines) {}
+
+VPackSlice RefactoredClusterTraverserCache::lookupToken(EdgeDocumentToken const& token) {
+  return VPackSlice(token.vpack());
+}
+
+aql::AqlValue RefactoredClusterTraverserCache::fetchEdgeAqlResult(EdgeDocumentToken const& token) {
+  // FIXME: the RefactoredClusterTraverserCache lifetime is shorter than the
+  // query lifetime therefore we cannot get away here without copying the result
+  return aql::AqlValue(VPackSlice(token.vpack()));  // will copy slice
+}
+
+void RefactoredClusterTraverserCache::insertEdgeIntoResult(EdgeDocumentToken const& token,
+                                                           VPackBuilder& result) {
+  result.add(VPackSlice(token.vpack()));
+}
+
+bool RefactoredClusterTraverserCache::appendVertex(arangodb::velocypack::StringRef id,
+                                                   VPackBuilder& result) {
+  // There will be no idString of length above uint32_t
+  auto it = _cache.find(
+      arangodb::velocypack::HashedStringRef(id.data(), static_cast<uint32_t>(id.length())));
+
+  if (it != _cache.end()) {
+    // FIXME: fix TraverserCache lifetime and use addExternal
+    result.add(it->second);
+    return true;
+  }
+  // Register a warning. It is okay though but helps the user
+  std::string msg = "vertex '" + id.toString() + "' not found";
+  _query.warnings().registerWarning(TRI_ERROR_ARANGO_DOCUMENT_NOT_FOUND, msg.c_str());
+
+  // Document not found append NULL
+  result.add(arangodb::velocypack::Slice::nullSlice());
+  return false;
+}
+
+bool RefactoredClusterTraverserCache::appendVertex(arangodb::velocypack::StringRef id,
+                                                   arangodb::aql::AqlValue& result) {
+  // There will be no idString of length above uint32_t
+  auto it = _cache.find(
+      arangodb::velocypack::HashedStringRef(id.data(), static_cast<uint32_t>(id.length())));
+
+  if (it != _cache.end()) {
+    // FIXME: fix TraverserCache lifetime and use addExternal
+    result = arangodb::aql::AqlValue(it->second);
+    return true;
+  }
+  // Register a warning. It is okay though but helps the user
+  std::string msg = "vertex '" + id.toString() + "' not found";
+  _query.warnings().registerWarning(TRI_ERROR_ARANGO_DOCUMENT_NOT_FOUND, msg.c_str());
+
+  // Document not found append NULL
+  result = arangodb::aql::AqlValue(arangodb::aql::AqlValueHintNull());
+  return false;
+}
+
+auto RefactoredClusterTraverserCache::cacheVertex(VertexType const& vertexId,
+                                                  velocypack::Slice vertexSlice) -> void {
+  _vertexData.try_emplace(vertexId, vertexSlice);
+  std::vector<std::pair<EdgeType, VertexType>> initVector{};
+  _vertexConnectedEdges.try_emplace(vertexId, std::move(initVector));
+}
+
+auto RefactoredClusterTraverserCache::cacheEdge(VertexType origin, EdgeType edgeId,
+                                                velocypack::Slice edgeSlice) -> void {
+  TRI_ASSERT(isVertexCached(origin));
+
+  _edgeData.try_emplace(edgeId, edgeSlice);
+  auto& edgeVector = _vertexConnectedEdges.at(origin);
+
+  auto edgeToEmplace = std::pair{edgeId, VertexType{edgeSlice.get(StaticStrings::ToString)}};
+  edgeVector.emplace_back(std::move(edgeToEmplace));
+}
+
+auto RefactoredClusterTraverserCache::isVertexCached(VertexType vertexKey) -> bool {
+  return (_vertexData.find(vertexKey) == _vertexData.end()) ? false : true;
+}
+
+auto RefactoredClusterTraverserCache::isEdgeCached(EdgeType edgeKey) -> bool {
+  return (_edgeData.find(edgeKey) == _edgeData.end()) ? false : true;
+}
---
+++ arangod/Graph/Cache/RefactoredClusterTraverserCache.h
@@ -0,0 +1,137 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARANGOD_GRAPH_REFACTORED_CLUSTER_TRAVERSER_CACHE_H
+#define ARANGOD_GRAPH_REFACTORED_CLUSTER_TRAVERSER_CACHE_H 1
+
+#include "Aql/types.h"
+#include "Cluster/ClusterInfo.h"
+#include "Graph/ClusterGraphDatalake.h"
+#include "Graph/Providers/TypeAliases.h"
+#include "Graph/TraverserCache.h"
+
+#include <velocypack/Buffer.h>
+#include <velocypack/StringRef.h>
+
+#include <map>
+
+namespace arangodb {
+struct ResourceMonitor;
+
+namespace aql {
+struct AqlValue;
+class QueryContext;
+}  // namespace aql
+
+namespace transaction {
+class Methods;
+}
+
+namespace velocypack {
+class Builder;
+class Slice;
+class HashedStringRef;
+class StringRef;
+}  // namespace velocypack
+
+namespace graph {
+struct BaseOptions;
+
+class RefactoredClusterTraverserCache final : public TraverserCache {
+ public:
+  RefactoredClusterTraverserCache(aql::QueryContext& query,
+                                  std::unordered_map<ServerID, aql::EngineId> const* engines,
+                                  BaseOptions*);
+
+  ~RefactoredClusterTraverserCache() = default;
+
+  using Cache =
+      std::unordered_map<arangodb::velocypack::HashedStringRef, arangodb::velocypack::Slice>;
+
+  /// @brief will convert the EdgeDocumentToken to a slice
+  arangodb::velocypack::Slice lookupToken(EdgeDocumentToken const& token) override;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// @brief Inserts the real document stored within the token
+  ///        into the given builder. No need for actual lookup
+  //////////////////////////////////////////////////////////////////////////////
+  void insertEdgeIntoResult(graph::EdgeDocumentToken const& idToken,
+                            arangodb::velocypack::Builder& builder) override;
+
+  /// Lookup document in cache and add it into the builder
+  bool appendVertex(arangodb::velocypack::StringRef idString,
+                    velocypack::Builder& result) override;
+  bool appendVertex(arangodb::velocypack::StringRef idString,
+                    arangodb::aql::AqlValue& result) override;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// @brief Return AQL value containing the result
+  ///        The document will either be fetched from storage or looked up in
+  ///        the datalake (on the coordinator)
+  //////////////////////////////////////////////////////////////////////////////
+  aql::AqlValue fetchEdgeAqlResult(graph::EdgeDocumentToken const& idToken) override;
+
+  std::unordered_map<ServerID, aql::EngineId> const* engines() const {
+    return _engines;
+  }
+
+  /// Map of already fetched vertices and edges (raw _id attribute)
+  Cache& cache() noexcept { return _cache; }
+
+  arangodb::graph::ClusterGraphDatalake& datalake() noexcept {
+    return _datalake;
+  }
+
+  size_t& insertedDocuments() { return _insertedDocuments; }
+
+  size_t& filteredDocuments() { return _filteredDocuments; }
+
+  auto cacheVertex(VertexType const& vertex, velocypack::Slice vertexSlice) -> void;
+  auto cacheEdge(VertexType origin, EdgeType edgeId, velocypack::Slice edgeSlice) -> void;
+
+  auto isVertexCached(VertexType vertex) -> bool;
+  auto isEdgeCached(EdgeType edge) -> bool;
+
+ private:
+  /// @brief link by _id into our data dump
+  Cache _cache;
+
+  /// @brief dump for our edge and vertex documents
+  arangodb::graph::ClusterGraphDatalake _datalake;
+
+  /// @brief maps to organize data access
+  /// @brief vertex reference to vertex data slice
+  std::unordered_map<VertexType, velocypack::Slice> _vertexData;
+
+  /// @brief vertex reference to all connected edges including the edges target
+  std::unordered_map<VertexType, std::vector<std::pair<EdgeType, VertexType>>> _vertexConnectedEdges;
+
+  /// @brief edge reference to edge data slice
+  std::unordered_map<VertexType, velocypack::Slice> _edgeData;
+
+  std::unordered_map<ServerID, aql::EngineId> const* _engines;
+};
+
+}  // namespace graph
+}  // namespace arangodb
+#endif
