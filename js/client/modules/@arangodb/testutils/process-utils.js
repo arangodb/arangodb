5c31daacbb107b589c45fde28f3b1fa6792da694
Wilfried Goesgens <willi@arangodb.com>
2021-02-04 17:33:25 +0100

    work on dump & restore tests (#13409)
    Co-authored-by: Heiko <hkernbach@users.noreply.github.com>
    Co-authored-by: jsteemann <jan@arangodb.com>

--- js/client/modules/@arangodb/testutils/process-utils.js
+++ js/client/modules/@arangodb/testutils/process-utils.js
@@ -1039,6 +1039,7 @@ function runArangoBackup (options, instanceInfo, which, cmds, rootDir, coreCheck
   };
   if (options.username) {
     args['server.username'] = options.username;
+    args['server.password'] = "";
   }
   if (options.password) {
     args['server.password'] = options.password;
@@ -1046,8 +1047,12 @@ function runArangoBackup (options, instanceInfo, which, cmds, rootDir, coreCheck
 
   args = Object.assign(args, cmds);
 
+  args['log.level'] = 'info';
   if (!args.hasOwnProperty('verbose')) {
     args['log.level'] = 'warning';
+  }
+  if (options.extremeVerbosity) {
+    args['log.level'] = 'trace';
   }
 
   args['flatCommands'] = [which];
