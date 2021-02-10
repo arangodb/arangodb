57f192675fbcd2f11789b5ef37cf56259f15fc1f
jsteemann <jan@arangodb.com>
2021-02-10 21:30:08 +0100

    use correct variable

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -4199,7 +4199,7 @@ Result ClusterInfo::dropIndexCoordinator(
 
   Result res(TRI_ERROR_CLUSTER_TIMEOUT);
   do {
-    Result res = dropIndexCoordinatorInner(databaseName, collectionID, iid, endTime);
+    res = dropIndexCoordinatorInner(databaseName, collectionID, iid, endTime);
 
     if (res.ok()) {
       // success!
