2306fb14ef864b206342a9e89ad5e70d7a2fa27c
Wilfried Goesgens <willi@arangodb.com>
2021-02-26 17:21:49 +0100

    lint

--- js/client/modules/@arangodb/testsuites/dump.js
+++ js/client/modules/@arangodb/testsuites/dump.js
@@ -63,7 +63,7 @@ const RESET = require('internal').COLORS.COLOR_RESET;
 const encryptionKey = '01234567890123456789012345678901';
 const encryptionKeySha256 = "861009ec4d599fab1f40abc76e6f89880cff5833c79c548c99f9045f191cd90b";
 
-let timeoutFactor = ;
+let timeoutFactor = 1;
 if (global.ARANGODB_CLIENT_VERSION(true).asan  ||
     global.ARANGODB_CLIENT_VERSION(true).tsan  ||
     process.env.hasOwnProperty('GCOV_PREFIX')) {
