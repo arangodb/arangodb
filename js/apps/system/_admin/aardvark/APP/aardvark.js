aa8b873b53d15bc7e0fde30ecc32a60ea56a27cd
Jan <jsteemann@users.noreply.github.com>
2021-03-07 17:06:51 +0100

    Fixed web UI "not authorized to execute this request" error (#13651)
    * Fixed a bug in the web interface that displayed the error "Not authorized to
      execute this request" when trying to create an index in the web interface in a
      database other than `_system` with a user that does not have any access
      permissions for the `_system` database.
      The error message previously displayed error actually came from an internal
      request made by the web interface, but it did not affect the actual index
      creation.
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/aardvark.js
+++ js/apps/system/_admin/aardvark/APP/aardvark.js
@@ -399,7 +399,7 @@ authRouter.delete('/job/:id', function (req, res) {
   if (frontend) {
     // get the job result and return before deletion
     let resp = request.put({
-      url: '/_api/job/' + encodeURIComponent(req.pathParams.id),
+      url: '/_db/' + encodeURIComponent(db._name()) + '/_api/job/' + encodeURIComponent(req.pathParams.id),
       json: true,
       headers: {
         'Authorization': req.headers.authorization
