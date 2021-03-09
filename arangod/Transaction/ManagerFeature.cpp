c9c2dccf94cfbb80d0765a252328de915374cd20
Max Neunhoeffer <max@arangodb.com>
2021-03-09 09:09:56 +0100

    ManagerFeature.cpp done.

--- arangod/Transaction/ManagerFeature.cpp
+++ arangod/Transaction/ManagerFeature.cpp
@@ -72,7 +72,7 @@ void queueGarbageCollection(std::mutex& mutex, arangodb::Scheduler::WorkHandle& 
 namespace arangodb {
 namespace transaction {
 
-DECLARE_METRIC(arangodb_transactions_expired);
+DECLARE_COUNTER(arangodb_transactions_expired, "Total number of expired transactions");
 
 std::unique_ptr<transaction::Manager> ManagerFeature::MANAGER;
 
@@ -82,8 +82,7 @@ ManagerFeature::ManagerFeature(application_features::ApplicationServer& server)
       _gcfunc(),
       _streamingLockTimeout(8.0),
       _numExpiredTransactions(
-        server.getFeature<arangodb::MetricsFeature>().counter<arangodb_transactions_expired>(
-          0, "Total number of expired transactions")) {
+        server.getFeature<arangodb::MetricsFeature>().add(arangodb_transactions_expired{})) {
   setOptional(false);
   startsAfter<BasicFeaturePhaseServer>();
   startsAfter<EngineSelectorFeature>();
