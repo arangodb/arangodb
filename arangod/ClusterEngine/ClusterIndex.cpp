6d4dd34525eca2872a62d7b1b1a6bc929dc311ef
Jan <jsteemann@users.noreply.github.com>
2021-03-15 18:01:09 +0100

    Update arangod/ClusterEngine/ClusterIndex.cpp
    Co-authored-by: Manuel Pöter <manuel.poeter@leanfive.com>

--- arangod/ClusterEngine/ClusterIndex.cpp
+++ arangod/ClusterEngine/ClusterIndex.cpp
@@ -153,7 +153,7 @@ bool ClusterIndex::hasSelectivityEstimate() const {
            (_estimates && 
             (_indexType == Index::TRI_IDX_TYPE_HASH_INDEX ||
              _indexType == Index::TRI_IDX_TYPE_SKIPLIST_INDEX ||
-              _indexType == Index::TRI_IDX_TYPE_PERSISTENT_INDEX));
+             _indexType == Index::TRI_IDX_TYPE_PERSISTENT_INDEX));
 #ifdef ARANGODB_USE_GOOGLE_TESTS
   } else if (_engineType == ClusterEngineType::MockEngine) {
     return false;
