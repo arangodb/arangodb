f37b9d9b1c1c48a86ed488745ec816ce6af28b01
hkernbach <heiko@arangodb.com>
2021-03-15 19:12:44 +0100

    added Graph/TraverserCacheTest.cpp

---
+++ tests/Graph/TraverserCacheTest.cpp
@@ -0,0 +1,99 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Heiko Kernbach
+////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include "fakeit.hpp"
+
+#include "Aql/AqlValue.h"
+#include "Aql/Query.h"
+#include "Aql/QueryWarnings.h"
+#include "Cluster/ServerState.h"
+#include "Graph/Cache/RefactoredTraverserCache.h"
+//#include "Graph/ClusterTraverserCache.h"
+#include "Graph/GraphTestTools.h"
+#include "Graph/TraverserOptions.h"
+#include "Transaction/Methods.h"
+
+#include <Aql/TraversalStats.h>
+#include <velocypack/Builder.h>
+#include <velocypack/Slice.h>
+#include <velocypack/velocypack-aliases.h>
+
+using namespace arangodb;
+using namespace arangodb::aql;
+using namespace arangodb::graph;
+
+namespace arangodb {
+namespace tests {
+namespace traverser_cache_test {
+
+class TraverserCacheTest : public ::testing::Test {
+ protected:
+  ServerState::RoleEnum oldRole;
+
+  graph::GraphTestSetup s{};
+  graph::MockGraphDatabase gdb;
+  arangodb::aql::TraversalStats stats{};
+  std::unique_ptr<arangodb::aql::Query> query{nullptr};
+  std::unique_ptr<RefactoredTraverserCache> traverserCache{nullptr};
+  std::shared_ptr<transaction::Context> queryContext{nullptr};
+  std::unique_ptr<arangodb::transaction::Methods> trx{nullptr};
+
+  TraverserCacheTest() : gdb(s.server, "testVocbase") {
+    query = gdb.getQuery("RETURN 1", std::vector<std::string>{});
+    queryContext = query.get()->newTrxContext();
+    trx = std::make_unique<arangodb::transaction::Methods>(queryContext);
+    traverserCache =
+        std::make_unique<RefactoredTraverserCache>(trx.get(), query.get(),
+                                                   query->resourceMonitor(), stats);
+  }
+
+  ~TraverserCacheTest() = default;
+};
+
+TEST_F(TraverserCacheTest, it_should_return_a_null_aqlvalue_if_vertex_not_cached) {
+  // prepare graph data - in this case, no data (no vertices and no edges, but collections v and e)
+  graph::MockGraph graph{};
+  gdb.addGraph(graph);
+
+  std::string vertexId = "v/Vertex";
+  std::string expectedMessage = "vertex '" + vertexId + "' not found";
+
+  auto data = VPackParser::fromJson(R"({"_key":"Vertex", "_id": "v/Vertex"})");
+  VPackSlice doc = data->slice();
+  HashedStringRef id{doc.get("_id")};
+  VPackBuilder builder;
+
+  // NOTE: we do not have the data, so we get null for any vertex
+  traverserCache->insertVertexIntoResult(arangodb::velocypack::HashedStringRef(id), builder);
+  ASSERT_TRUE(builder.slice().isNull());
+  auto all = query->warnings().all();
+  ASSERT_TRUE(all.size() == 1);
+  ASSERT_TRUE(all[0].first == TRI_ERROR_ARANGO_DOCUMENT_NOT_FOUND);
+  ASSERT_TRUE(all[0].second == expectedMessage);
+}
+
+}  // namespace traverser_cache_test
+}  // namespace tests
+}  // namespace arangodb
