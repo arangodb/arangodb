04610e8b78af5e1ff2402200f7e039606d32fdc4
jsteemann <jan@arangodb.com>
2021-03-19 00:32:21 +0100

    yet another attempt

--- arangod/Cluster/RebootTracker.cpp
+++ arangod/Cluster/RebootTracker.cpp
@@ -65,8 +65,8 @@ RebootTracker::RebootTracker(RebootTracker::SchedulerPointer scheduler)
 void RebootTracker::updateServerState(std::unordered_map<ServerID, RebootId> const& state) {
   MUTEX_LOCKER(guard, _mutex);
         
-  LOG_TOPIC("77a6e", DEBUG, Logger::CLUSTER)
-      << "updating reboot server state from " << _rebootIds << " to " << state;
+  //LOG_TOPIC("77a6e", DEBUG, Logger::CLUSTER)
+  //    << "updating reboot server state from " << _rebootIds << " to " << state;
 
   // Call cb for each iterator.
   auto for_each_iter = [](auto begin, auto end, auto cb) {
