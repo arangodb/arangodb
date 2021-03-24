6ef6f5244919d1e1826ab0995fecb71270946cf4
jsteemann <jan@arangodb.com>
2021-03-24 12:13:58 +0100

    more test coverage for HTTP return codes

--- tests/js/client/shell/shell-document-api-http-return-codes.js
+++ tests/js/client/shell/shell-document-api-http-return-codes.js
@@ -42,6 +42,87 @@ function testSuite () {
       db._drop(cn);
     },
     
+    testGetDocument: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test",
+        value: 1
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test");
+      assertEqual(200, result.code);
+    },
+    
+    testGetNonExistingDocument: function () {
+      let result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test");
+      assertEqual(404, result.code);
+    },
+    
+    testGetNonExistingCollection: function () {
+      let result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn + "moeter") + "/test");
+      assertEqual(404, result.code);
+    },
+    
+    testGetIfDifferentUnchanged: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", { "If-None-Match" : rev });
+      assertEqual(304, result.code);
+    },
+    
+    testGetIfDifferentChanged: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", {
+        _key: "test",
+        value: 2
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", { "If-None-Match" : rev });
+      assertEqual(200, result.code);
+    },
+    
+    testGetWithPrecondition: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", { "If-Match" : rev });
+      assertEqual(200, result.code);
+    },
+    
+    testGetWithPreconditionFailed: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", {
+        _key: "test",
+        value: 2
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.GET_RAW("/_api/document/" + encodeURIComponent(cn) + "/test", { "If-Match" : rev });
+      assertEqual(412, result.code);
+    },
+    
     testInsertDocument: function () {
       let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
         _key: "test",
@@ -121,14 +202,81 @@ function testSuite () {
       assertEqual(409, result.code);
     },
     
-    testUpdateNonExistingDocument: function () {
+    testUpdateWithPrecondition: function () {
       let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
         _key: "test1",
         value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 2
+      }, { "If-Match" : rev });
+      assertEqual(202, result.code);
+    },
+    
+    testUpdateWithPreconditionFailedHeader: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
       });
       assertEqual(202, result.code);
-
-      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn + "moeter") + "/test2", {
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      }, { "If-Match" : rev });
+      assertEqual(412, result.code);
+    },
+    
+    testUpdateWithPreconditionFailedBody: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1?ignoreRevs=false", {
+        _rev: rev,
+        value: 3
+      });
+      assertEqual(412, result.code);
+    },
+    
+    testUpdateWithPreconditionFailedButIgnored: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1?ignoreRevs=true", {
+        _rev: rev,
+        value: 3
+      });
+      assertEqual(202, result.code);
+    },
+    
+    testUpdateNonExistingDocument: function () {
+      let result = arango.PATCH_RAW("/_api/document/" + encodeURIComponent(cn + "moeter") + "/test2", {
         value: 1
       });
       assertEqual(404, result.code);
@@ -181,15 +329,82 @@ function testSuite () {
       });
       assertEqual(409, result.code);
     },
-    
-    testReplaceNonExistingDocument: function () {
+
+    testReplaceWithPrecondition: function () {
       let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
         _key: "test1",
         value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 2
+      }, { "If-Match" : rev });
+      assertEqual(202, result.code);
+    },
+    
+    testReplaceWithPreconditionFailedHeader: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
       });
       assertEqual(202, result.code);
-
-      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn + "moeter") + "/test2", {
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      }, { "If-Match" : rev });
+      assertEqual(412, result.code);
+    },
+    
+    testReplaceWithPreconditionFailedBody: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1?ignoreRevs=false", {
+        _rev: rev,
+        value: 3
+      });
+      assertEqual(412, result.code);
+    },
+    
+    testReplaceWithPreconditionFailedButIgnored: function () {
+      let result = arango.POST_RAW("/_api/document/" + encodeURIComponent(cn), {
+        _key: "test1",
+        value: 1
+      }, { Accept: "application/json" });
+      assertEqual(202, result.code);
+      let rev = JSON.parse(result.body)._rev;
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1", {
+        value: 3
+      });
+      assertEqual(202, result.code);
+      
+      result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn) + "/test1?ignoreRevs=true", {
+        _rev: rev,
+        value: 3
+      });
+      assertEqual(202, result.code);
+    },
+    
+    testReplaceNonExistingDocument: function () {
+      let result = arango.PUT_RAW("/_api/document/" + encodeURIComponent(cn + "moeter") + "/test2", {
         value: 1
       });
       assertEqual(404, result.code);
