eb44c3f92d73723fb3671358e45405ef1329bc36
Jan <jsteemann@users.noreply.github.com>
2021-04-08 12:44:17 +0200

    upgrade velocypack to latest (#13920)

--- 3rdParty/velocypack/scripts/travis-build.sh
+++ 3rdParty/velocypack/scripts/travis-build.sh
@@ -6,9 +6,10 @@ ferr(){
 }
 
 
-CXX_STANDARD=${CXX_STANDARD:-14}
+CXX_STANDARD=${CXX_STANDARD:-17}
 echo "requesting c++ standard ${CXX_STANDARD}"
 BUILD_TYPE=${BUILD_TYPE:-Release}
+HASH_TYPE=${HASH_TYPE:-xxhash}
 
 threads=2
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
@@ -19,7 +20,7 @@ echo "Building with C++ Standard $CXX_STANDARD"
 
 mkdir -p build && cd build || ferr "could not create build dir"
 
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DHashType=xxhash \
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DHashType=${HASH_TYPE} \
       -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON \
       -DBuildTools=ON -DEnableSSE=OFF \
       -DCMAKE_CXX_STANDARD=${CXX_STANDARD} \
