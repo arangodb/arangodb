f3e20e642d52f4e410dc2977bec4977f62fd109d
kvahed <kaveh@arangodb.com>
2021-04-22 12:36:19 +0200

    fixed unit tests

--- tests/Maintenance/MaintenanceFeatureTest.cpp
+++ tests/Maintenance/MaintenanceFeatureTest.cpp
@@ -93,12 +93,12 @@ class TestActionBasic : public ActionBase {
       createPreAction(_preDesc);
     } else if (0 == _iteration) {
       // time to set result?
-      _result.reset(_resultCode);
+      result(_resultCode);
     }  // if
 
     // verify first() called once
     if (0 != getProgress()) {
-      _result.reset(TRI_ERROR_INTERNAL);
+      result(TRI_ERROR_INTERNAL);
     }  // if
 
     return (iteratorEndTest());
@@ -107,12 +107,12 @@ class TestActionBasic : public ActionBase {
   bool next() override {
     // time to set result?
     if (0 == _iteration) {
-      _result.reset(_resultCode);
+      result(_resultCode);
     }  // if
 
     // verify next() called properly
     if (0 == getProgress()) {
-      _result.reset(TRI_ERROR_INTERNAL);
+      result(TRI_ERROR_INTERNAL);
     }  // if
 
     return (iteratorEndTest());
@@ -123,7 +123,7 @@ class TestActionBasic : public ActionBase {
     bool more;
 
     //
-    if (_result.ok()) {
+    if (result().ok()) {
       more = 0 < _iteration--;
 
       // if going to stop, see if a postAction is needed
