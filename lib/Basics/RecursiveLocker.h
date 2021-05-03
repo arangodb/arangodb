3f458207485d45406cfc4b44d5e16e99bdbfce88
jsteemann <jan@arangodb.com>
2021-05-03 12:37:26 +0200

    port bugfixes suggested by @mpoeter

--- lib/Basics/RecursiveLocker.h
+++ lib/Basics/RecursiveLocker.h
@@ -62,17 +62,10 @@ class RecursiveMutexLocker {
 
     if (std::this_thread::get_id() != _owner.load()) {  // not recursive
       _locker.lock();
-      // it is possible that we cannot acquire the mutex, e.g.
-      // if we are a TRY locker
-      if (_locker.isLocked()) {
-        _owner.store(std::this_thread::get_id());
-        _update = owned;
-        _locked = true;
-      }
-    } else {
-      // we are the owning thread
-      _locked = true;
+      _owner.store(std::this_thread::get_id());
+      _update = owned;
     }
+    _locked = true;
   }
 
   void unlock() { _update(*this); }
@@ -149,17 +142,10 @@ class RecursiveWriteLocker {
 
     if (std::this_thread::get_id() != _owner.load()) {  // not recursive
       _locker.lock();
-      // it is possible that we cannot acquire the lock, e.g.
-      // if we are a TRY locker
-      if (_locker.isLocked()) {
-        _owner.store(std::this_thread::get_id());
-        _update = owned;
-        _locked = true;
-      }
-    } else {
-      // we are the owning thread
-      _locked = true;
+      _owner.store(std::this_thread::get_id());
+      _update = owned;
     }
+    _locked = true;
   }
 
   void unlock() {
