1e2fc9d7f73b625663305e6f610ac9441f37f0a8
mpoeter <manuel@manuel-poeter.at>
2021-05-18 11:40:33 +0200

    Fix jslint issue.

--- tests/js/server/aql/aql-index-choice.js
+++ tests/js/server/aql/aql-index-choice.js
@@ -610,7 +610,7 @@ function BaseTestConfig () {
       // generate a list of all permutations of the given array
       const perm = a => a.length
         ? a.reduce((r, v, i) => [...r, ...perm([...a.slice(0, i), ...a.slice(i + 1)]).map(x => [v, ...x])], [])
-        : [[]]
+        : [[]];
         
       // lowerBound = 10
       // upperBound = 100
