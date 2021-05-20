bfb414dad83c038961f2f1a27629efaf1ca2ed20
Max Neunhöffer <max@arangodb.com>
2021-05-20 16:23:10 +0200

    Update js/client/modules/@arangodb/arango-collection.js
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- js/client/modules/@arangodb/arango-collection.js
+++ js/client/modules/@arangodb/arango-collection.js
@@ -1551,5 +1551,5 @@ ArangoCollection.prototype._revisionTreeVerification = function() {
 ArangoCollection.prototype._revisionTreeRebuild = function() {
   // For some reason we need a batch ID here, which is not used!
   var requestResult = this._database._connection.POST(this._prefixurl(
-    `/_api/replication/revisions/tree?collection=${this._name}&batchId=42`), {});
+    `/_api/replication/revisions/tree?collection=${encodeURIComponent(this._name)}&batchId=42`), {});
 };
