7ce5f9a5c502665137232843d5e27ba7ee5b76df
Heiko Kernbach <heiko@arangodb.com>
2021-05-20 14:47:25 +0200

    forbid invalid edge weights and negative default weight in K_SHORTEST_PATHS

--- arangod/Graph/ShortestPathOptions.h
+++ arangod/Graph/ShortestPathOptions.h
@@ -47,8 +47,6 @@ struct ShortestPathOptions : public BaseOptions {
   uint64_t maxDepth;
   std::string start;
   std::string end;
-  std::string weightAttribute;
-  double defaultWeight;
   bool bidirectional;
   bool multiThreaded;
 
@@ -101,9 +99,16 @@ struct ShortestPathOptions : public BaseOptions {
 
   auto estimateDepth() const noexcept -> uint64_t override;
 
+  auto setWeightAttribute(std::string attribute) -> void;
+  auto getWeightAttribute() const -> std::string;
+  auto setDefaultWeight(double weight) -> void;
+  auto getDefaultWeight() const -> double;
+
  private:
   /// @brief Lookup info to find all reverse edges.
   std::vector<LookupInfo> _reverseLookupInfos;
+  std::string _weightAttribute;
+  double _defaultWeight;
 };
 
 }  // namespace graph
