022ca58cfe9051a3a3b1e3f360e0931c385dfccc
jsteemann <jan@arangodb.com>
2021-05-25 09:48:03 +0200

    add tests to PR

---
+++ tests/js/client/server_parameters/fill-block-cache-off-noncluster.js
@@ -0,0 +1,87 @@
+/*jshint globalstrict:false, strict:false */
+/* global getOptions, runSetup, assertTrue, assertEqual */
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test for server startup options
+///
+/// DISCLAIMER
+///
+/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB Inc, Cologne, Germany
+///
+/// @author Jan Steemann
+/// @author Copyright 2019, ArangoDB Inc, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+const db = require('@arangodb').db;
+const cn = 'UnitTestsCollection';
+
+if (getOptions === true) {
+  return {
+    'foxx.queues' : 'false',
+    'server.statistics' : 'false',
+    runSetup: true,
+  };
+}
+if (runSetup === true) {
+  let payload = Array(1024).join("x"); 
+  let c = db._create(cn);
+  let docs = [];
+  for (let i = 0; i < 250 * 1000; ++i) {
+    docs.push({ _key: "test" + i, value1: i, value2: "test" + i, payload });
+    if (docs.length === 1000) {
+      c.insert(docs);
+      docs = [];
+    }
+  }
+  return true;
+}
+
+const jsunity = require('jsunity');
+
+function FillBlockCacheSuite() {
+  'use strict';
+
+  return {
+    
+    testQuery: function() {
+      const oldValue = db._engineStats()["rocksdb.block-cache-usage"];
+      let result = db._query("FOR doc IN @@collection RETURN doc.value1", { "@collection": cn }, { fillBlockCache: false }).toArray();
+      assertEqual(250 * 1000, result.length);
+      
+      const newValue = db._engineStats()["rocksdb.block-cache-usage"];
+      // allow for some other background things to be run, thus add 1MB of leeway
+      assertTrue(newValue <= oldValue + 1000 * 1000, { oldValue, newValue });
+      
+      result = db._query("FOR doc IN @@collection RETURN doc.value1", { "@collection": cn }, { fillBlockCache: false }).toArray();
+      assertEqual(250 * 1000, result.length);
+      
+      const newValue2 = db._engineStats()["rocksdb.block-cache-usage"];
+      // allow for some other background things to be run, thus add 1MB of leeway
+      assertTrue(newValue2 <= newValue + 1000 * 1000, { newValue, newValue2 });
+      
+      result = db._query("FOR doc IN @@collection RETURN doc.value1", { "@collection": cn }, { fillBlockCache: true }).toArray();
+      assertEqual(250 * 1000, result.length);
+      
+      const newValue3 = db._engineStats()["rocksdb.block-cache-usage"];
+      assertTrue(newValue3 >= newValue2 + 100 * 1000 * 1000, { newValue2, newValue3 });
+    },
+
+  };
+}
+
+jsunity.run(FillBlockCacheSuite);
+return jsunity.done();
