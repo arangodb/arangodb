f29b80c27ffab7b7aecba32fe9262b030ca0c7e2
Vadim Kondratyev <vadim@arangodb.com>
2021-06-07 19:13:35 +0200

    arangodb release 3.8.0-rc.2

--- js/apps/system/_admin/aardvark/APP/react/build/precache-manifest.53329e84a589ad0636358a33adfdc507.js
+++ js/apps/system/_admin/aardvark/APP/react/build/precache-manifest.53329e84a589ad0636358a33adfdc507.js
@@ -52,12 +52,12 @@ self.__precacheManifest = [
     "url": "static/media/OpenSansLightItalic.05f81795.woff"
   },
   {
-    "revision": "403dee4359ca42b1d096976fda3ef95c",
-    "url": "static/media/OpenSansItalic.403dee43.woff"
-  },
-  {
     "revision": "84b1d9bb65b585d4b4a72ee9cefb2ac2",
     "url": "static/media/OpenSansBoldItalic.84b1d9bb.woff"
+  },
+  {
+    "revision": "403dee4359ca42b1d096976fda3ef95c",
+    "url": "static/media/OpenSansItalic.403dee43.woff"
   },
   {
     "revision": "25a32416abee198dd821b0b17a198a8f",
@@ -72,36 +72,40 @@ self.__precacheManifest = [
     "url": "static/media/fontawesome-webfont.c8ddf1e5.woff"
   },
   {
-    "revision": "c3547b2ec6f5eb324b44d8a0c6b2dd31",
-    "url": "static/media/Roboto-300.c3547b2e.eot"
-  },
-  {
     "revision": "16a9467557c38b4e1b19e981bd17fe3f",
     "url": "static/media/Roboto-300.16a94675.woff2"
   },
   {
-    "revision": "7e2d32e7141050d758a38b4ec96390c0",
-    "url": "static/media/Roboto-300.7e2d32e7.woff"
+    "revision": "c3547b2ec6f5eb324b44d8a0c6b2dd31",
+    "url": "static/media/Roboto-300.c3547b2e.eot"
   },
   {
     "revision": "1dc35d25e61d819a9c357074014867ab",
     "url": "static/media/fontawesome-webfont.1dc35d25.ttf"
   },
   {
-    "revision": "634f53eb79efa455a9e9d85d608b3447",
-    "url": "static/media/Roboto-300.634f53eb.ttf"
+    "revision": "7e2d32e7141050d758a38b4ec96390c0",
+    "url": "static/media/Roboto-300.7e2d32e7.woff"
   },
   {
-    "revision": "9f916e330c478bbfa2a0dd6614042046",
-    "url": "static/media/Roboto-regular.9f916e33.eot"
+    "revision": "634f53eb79efa455a9e9d85d608b3447",
+    "url": "static/media/Roboto-300.634f53eb.ttf"
   },
   {
     "revision": "1edaa6e50c2302bf0221d252e1caebb4",
     "url": "static/media/Roboto-300.1edaa6e5.svg"
   },
   {
+    "revision": "9f916e330c478bbfa2a0dd6614042046",
+    "url": "static/media/Roboto-regular.9f916e33.eot"
+  },
+  {
     "revision": "f84cb1bf9be983133497000554605b4d",
     "url": "static/media/Roboto-regular.f84cb1bf.woff2"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "static/media/fontawesome-webfont.d7c63908.svg"
   },
   {
     "revision": "f94d5e5102359961c44a1da1b58d37c9",
@@ -112,20 +116,16 @@ self.__precacheManifest = [
     "url": "static/media/Roboto-regular.38861cba.ttf"
   },
   {
-    "revision": "d7c639084f684d66a1bc66855d193ed8",
-    "url": "static/media/fontawesome-webfont.d7c63908.svg"
-  },
-  {
     "revision": "3d3a53586bd78d1069ae4b89a3b9aa98",
     "url": "static/media/Roboto-regular.3d3a5358.svg"
   },
   {
-    "revision": "2a52a20f9a56010ec5d985abe9bebcc9",
-    "url": "static/media/Roboto-500.2a52a20f.eot"
-  },
-  {
     "revision": "1afbee5a09a022fe0287f16e9a48da1f",
     "url": "static/media/Roboto-500.1afbee5a.woff2"
+  },
+  {
+    "revision": "2a52a20f9a56010ec5d985abe9bebcc9",
+    "url": "static/media/Roboto-500.2a52a20f.eot"
   },
   {
     "revision": "0f3b7101a8adc1afe1fbe89775553c32",
