98a298cbda38aa5bcb69652925329d91bf1c8318
Simran <Simran-B@users.noreply.github.com>
2021-06-08 15:41:35 +0200

    Describe logging format

--- lib/Logger/LoggerFeature.cpp
+++ lib/Logger/LoggerFeature.cpp
@@ -195,7 +195,9 @@ void LoggerFeature::collectOptions(std::shared_ptr<ProgramOptions> options) {
                      new StringParameter(&_file),
                      arangodb::options::makeDefaultFlags(arangodb::options::Flags::Hidden));
 
-  options->addOption("--log.line-number", "append source file name, line number and function name",
+  options->addOption("--log.line-number",
+                     "log function name, file name and line number of the source file "
+                     "in the format `[func@FileName.cpp:123]`",
                      new BooleanParameter(&_lineNumber),
                      arangodb::options::makeDefaultFlags(arangodb::options::Flags::Hidden));
 
