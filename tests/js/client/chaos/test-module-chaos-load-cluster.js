8efe2dbf7c11e0b96e1d02ae2900863beefa9eea
mpoeter <manuel@arangodb.com>
2021-06-29 11:32:48 +0200

    Add new chaos test suite.

---
+++ tests/js/client/chaos/test-module-chaos-load-cluster.js
@@ -0,0 +1,30 @@
+/* jshint globalstrict:false, strict:false, maxlen: 200 */
+/* global fail, print */
+
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License")
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Manuel Pöter
+////////////////////////////////////////////////////////////////////////////////
+
+const { run, makeConfig, parameters } = require("./test-chaos-load-common.inc");
+
+module.exports.getConfigs = () => [makeConfig(Array(parameters.length).fill(true))];
+module.exports.run = run;
