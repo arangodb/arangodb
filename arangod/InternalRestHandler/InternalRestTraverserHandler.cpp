a213bd243e6df47aebb40077b6c04089badda90c
Heiko Kernbach <heiko@arangodb.com>
2021-07-07 14:16:15 +0200

    smartSearchNew does both, bfs and dfs

--- arangod/InternalRestHandler/InternalRestTraverserHandler.cpp
+++ arangod/InternalRestHandler/InternalRestTraverserHandler.cpp
@@ -246,7 +246,7 @@ void InternalRestTraverserHandler::queryEngine() {
     // Safe cast BaseTraverserEngines are all of type TRAVERSER
     auto eng = static_cast<BaseTraverserEngine*>(engine);
     TRI_ASSERT(eng != nullptr);
-    eng->smartSearchBFS(body, result);
+    eng->smartSearchNew(body, result); // TODO: Rename/Refactor. smartSearchNew does both (DFS & BFS)
   } else if (option == "smartSearchWeighted") {
     if (engine->getType() != BaseEngine::EngineType::TRAVERSER) {
       generateError(ResponseCode::BAD, TRI_ERROR_HTTP_BAD_PARAMETER,
