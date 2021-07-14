408b3e4a059e3a8ba1a9d35c4efb960595de20d7
Vadim Kondratyev <vadim@arangodb.com>
2021-07-14 17:13:22 +0200

    arangodb release 3.8.0

--- js/apps/system/_admin/aardvark/APP/react/build/precache-manifest.f0f721161a12fe39b5b6c27b5f856cc2.js
+++ js/apps/system/_admin/aardvark/APP/react/build/precache-manifest.f0f721161a12fe39b5b6c27b5f856cc2.js
@@ -40,16 +40,16 @@ self.__precacheManifest = [
     "url": "static/media/OpenSansLight.11bbcff6.woff"
   },
   {
+    "revision": "05f81795ee61b18a60d5be1b40714e01",
+    "url": "static/media/OpenSansLightItalic.05f81795.woff"
+  },
+  {
     "revision": "f71094988de2d6567eb9caba78f61041",
     "url": "static/media/OpenSans.f7109498.woff"
   },
   {
     "revision": "2af7a71056d6f2fe2cb2e35c358b59de",
     "url": "static/media/OpenSansBold.2af7a710.woff"
-  },
-  {
-    "revision": "05f81795ee61b18a60d5be1b40714e01",
-    "url": "static/media/OpenSansLightItalic.05f81795.woff"
   },
   {
     "revision": "84b1d9bb65b585d4b4a72ee9cefb2ac2",
@@ -64,68 +64,72 @@ self.__precacheManifest = [
     "url": "static/media/fontawesome-webfont.25a32416.eot"
   },
   {
-    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
-    "url": "static/media/fontawesome-webfont.e6cf7c6e.woff2"
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "static/media/fontawesome-webfont.1dc35d25.ttf"
   },
   {
     "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
     "url": "static/media/fontawesome-webfont.c8ddf1e5.woff"
   },
   {
-    "revision": "16a9467557c38b4e1b19e981bd17fe3f",
-    "url": "static/media/Roboto-300.16a94675.woff2"
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "static/media/fontawesome-webfont.e6cf7c6e.woff2"
   },
   {
     "revision": "c3547b2ec6f5eb324b44d8a0c6b2dd31",
     "url": "static/media/Roboto-300.c3547b2e.eot"
   },
   {
-    "revision": "1dc35d25e61d819a9c357074014867ab",
-    "url": "static/media/fontawesome-webfont.1dc35d25.ttf"
-  },
-  {
-    "revision": "7e2d32e7141050d758a38b4ec96390c0",
-    "url": "static/media/Roboto-300.7e2d32e7.woff"
-  },
-  {
-    "revision": "634f53eb79efa455a9e9d85d608b3447",
-    "url": "static/media/Roboto-300.634f53eb.ttf"
+    "revision": "16a9467557c38b4e1b19e981bd17fe3f",
+    "url": "static/media/Roboto-300.16a94675.woff2"
   },
   {
     "revision": "1edaa6e50c2302bf0221d252e1caebb4",
     "url": "static/media/Roboto-300.1edaa6e5.svg"
   },
   {
-    "revision": "9f916e330c478bbfa2a0dd6614042046",
-    "url": "static/media/Roboto-regular.9f916e33.eot"
-  },
-  {
-    "revision": "f84cb1bf9be983133497000554605b4d",
-    "url": "static/media/Roboto-regular.f84cb1bf.woff2"
+    "revision": "634f53eb79efa455a9e9d85d608b3447",
+    "url": "static/media/Roboto-300.634f53eb.ttf"
   },
   {
     "revision": "d7c639084f684d66a1bc66855d193ed8",
     "url": "static/media/fontawesome-webfont.d7c63908.svg"
   },
   {
-    "revision": "f94d5e5102359961c44a1da1b58d37c9",
-    "url": "static/media/Roboto-regular.f94d5e51.woff"
+    "revision": "9f916e330c478bbfa2a0dd6614042046",
+    "url": "static/media/Roboto-regular.9f916e33.eot"
   },
   {
     "revision": "38861cba61c66739c1452c3a71e39852",
     "url": "static/media/Roboto-regular.38861cba.ttf"
   },
   {
+    "revision": "f84cb1bf9be983133497000554605b4d",
+    "url": "static/media/Roboto-regular.f84cb1bf.woff2"
+  },
+  {
     "revision": "3d3a53586bd78d1069ae4b89a3b9aa98",
     "url": "static/media/Roboto-regular.3d3a5358.svg"
+  },
+  {
+    "revision": "f94d5e5102359961c44a1da1b58d37c9",
+    "url": "static/media/Roboto-regular.f94d5e51.woff"
   },
   {
     "revision": "1afbee5a09a022fe0287f16e9a48da1f",
     "url": "static/media/Roboto-500.1afbee5a.woff2"
   },
   {
+    "revision": "7e2d32e7141050d758a38b4ec96390c0",
+    "url": "static/media/Roboto-300.7e2d32e7.woff"
+  },
+  {
     "revision": "2a52a20f9a56010ec5d985abe9bebcc9",
     "url": "static/media/Roboto-500.2a52a20f.eot"
+  },
+  {
+    "revision": "f1d811cdfaea49c969500d4bbe52251b",
+    "url": "static/media/Roboto-500.f1d811cd.svg"
   },
   {
     "revision": "0f3b7101a8adc1afe1fbe89775553c32",
@@ -134,10 +138,6 @@ self.__precacheManifest = [
   {
     "revision": "88f29ea5a372d06f521395134f62ab91",
     "url": "static/media/Roboto-500.88f29ea5.ttf"
-  },
-  {
-    "revision": "f1d811cdfaea49c969500d4bbe52251b",
-    "url": "static/media/Roboto-500.f1d811cd.svg"
   },
   {
     "revision": "128879da78c6c8eb4e2c07fa3732cea7",
--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.53329e84a589ad0636358a33adfdc507.js"
+  "precache-manifest.f0f721161a12fe39b5b6c27b5f856cc2.js"
 );
 
 workbox.clientsClaim();
