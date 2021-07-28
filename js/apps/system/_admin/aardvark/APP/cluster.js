38c1b60f4754377dde95587f714d0419a74181c8
jsteemann <jsteemann@users.noreply.github.com>
2021-07-28 12:16:31 +0200

    reduce diff size

--- js/apps/system/_admin/aardvark/APP/cluster.js
+++ js/apps/system/_admin/aardvark/APP/cluster.js
@@ -31,6 +31,7 @@ const createRouter = require('@arangodb/foxx/router');
 const router = createRouter();
 module.exports = router;
 
+
 router.use((req, res, next) => {
   if (internal.authenticationEnabled()) {
     if (!req.arangoUser) {
