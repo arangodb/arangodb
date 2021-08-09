2ee83e138beb5331c6cc8b2003207decfcc63d49
jsteemann <jsteemann@users.noreply.github.com>
2021-08-09 13:11:17 +0200

    fixed it

--- arangod/RestServer/ServerFeature.cpp
+++ arangod/RestServer/ServerFeature.cpp
@@ -147,7 +147,7 @@ void ServerFeature::validateOptions(std::shared_ptr<ProgramOptions> options) {
   
   DatabaseFeature& db = server().getFeature<DatabaseFeature>();
 
-  if (_operationMode == OperationMode::MODE_SERVER && !_restServer && db.upgrade()) {
+  if (_operationMode == OperationMode::MODE_SERVER && !_restServer && !db.upgrade()) {
     LOG_TOPIC("8daab", FATAL, arangodb::Logger::FIXME)
         << "need at least '--console', '--javascript.unit-tests' or"
         << "'--javascript.script if rest-server is disabled";
