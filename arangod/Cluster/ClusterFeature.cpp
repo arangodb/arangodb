44172e7eca9c211fff7d806b0bd5056fa270df80
jsteemann <jsteemann@users.noreply.github.com>
2021-08-17 18:58:41 +0200

    index creation improvements

--- arangod/Cluster/ClusterFeature.cpp
+++ arangod/Cluster/ClusterFeature.cpp
@@ -105,6 +105,10 @@ void ClusterFeature::collectOptions(std::shared_ptr<ProgramOptions> options) {
   options->addObsoleteOption("--cluster.my-id", "this server's id", false);
 
   options->addObsoleteOption("--cluster.agency-prefix", "agency prefix", false);
+  
+  options->addObsoleteOption("--cluster.index-create-timeout",
+      "amount of time (in seconds) the coordinator will wait for an index to "
+      "be created before giving up", true);
 
 
   options->addOption(
@@ -208,15 +212,6 @@ void ClusterFeature::collectOptions(std::shared_ptr<ProgramOptions> options) {
       arangodb::options::makeFlags(arangodb::options::Flags::DefaultNoComponents,
                                    arangodb::options::Flags::OnCoordinator,
                                    arangodb::options::Flags::OnDBServer,
-                                   arangodb::options::Flags::Hidden));
-
-  options->addOption(
-      "--cluster.index-create-timeout",
-      "amount of time (in seconds) the coordinator will wait for an index to "
-      "be created before giving up",
-      new DoubleParameter(&_indexCreationTimeout),
-      arangodb::options::makeFlags(arangodb::options::Flags::DefaultNoComponents,
-                                   arangodb::options::Flags::OnCoordinator,
                                    arangodb::options::Flags::Hidden));
 }
 
