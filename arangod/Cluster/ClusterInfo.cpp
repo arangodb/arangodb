94d5ca1f27ac5bc7b98449bc76151975916aaeda
jsteemann <jsteemann@users.noreply.github.com>
2021-08-24 18:51:37 +0200

    fix forward port

--- arangod/Cluster/ClusterInfo.cpp
+++ arangod/Cluster/ClusterInfo.cpp
@@ -6087,7 +6087,7 @@ CollectionWatcher::CollectionWatcher(AgencyCallbackRegistry* agencyCallbackRegis
       [self = weak_from_this()](VPackSlice const& result) {
         auto watcher = self.lock();
         if (result.isNone() && watcher) {
-          _present.store(false);
+          watcher->_present.store(false);
         }
         return true;
       },
