83714c7556c504f8e60a3b2881682db223ecf502
Heiko <hkernbach@users.noreply.github.com>
2021-09-07 13:07:06 +0200

    Update tests/Graph/DFSFinderTest.cpp
    Co-authored-by: Tobias Gödderz <tobias@arangodb.com>

--- tests/Graph/DFSFinderTest.cpp
+++ tests/Graph/DFSFinderTest.cpp
@@ -593,7 +593,6 @@ TEST_P(DFSFinderTest, path_depth_1_to_2_skip) {
     // Try again to make sure we stay at non-existing
     auto hasPath = finder.getNextPath();
     EXPECT_FALSE(hasPath);
-    EXPECT_TRUE(result.isEmpty());
     EXPECT_TRUE(finder.isDone());
   }
 }
