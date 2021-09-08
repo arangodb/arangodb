06c359cde63834f7efaf4798ba8e65ec6bfeb42f
Frank Celler <392005+fceller@users.noreply.github.com>
2021-09-08 12:21:55 +0200

    Preselect "create index in background" option when creating indexes i… (#14746)
    * Preselect "create index in background" option when creating indexes in the web UI
    
    * Rebuild UI
    
    Co-authored-by: Jenkins <jenkins@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.bcd14b85debed42e7ea3f9b02e70b8a5.js"
+  "precache-manifest.e718518bf70d763e10d521d6a74ee8d2.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
