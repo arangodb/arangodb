52c00c127398ea3ef8731df85d2263a3c172e440
roman <roman@arangodb.com>
2021-09-24 14:06:28 +0200

    compilation bug fix

--- arangod/VocBase/Methods/CollectionValidator.cpp
+++ arangod/VocBase/Methods/CollectionValidator.cpp
@@ -1,13 +1,16 @@
 #include "CollectionValidator.h"
 #include <Basics/StaticStrings.h>
 #include <Basics/VelocyPackHelper.h>
+#include <Utilities/NameValidator.h>
 #include <Utils/Events.h>
 #include <VocBase/vocbase.h>
 
 using namespace arangodb;
 
 Result CollectionValidator::verifyCollectionName(const arangodb::velocypack::Slice parameters, arangodb::CreateDatabaseInfo const& info) {
-  if (!TRI_vocbase_t::IsAllowedName(parameters)) {
+  std::string pars = arangodb::basics::VelocyPackHelper::getStringValue(parameters,
+                                                                                    StaticStrings::DataSourceName, "");
+  if (!CollectionNameValidator::isAllowedName(false, false, pars)) { // todo check the second false
     std::string name;
     std::string const& dbName = info.getName();
     if (parameters.isObject()) {
