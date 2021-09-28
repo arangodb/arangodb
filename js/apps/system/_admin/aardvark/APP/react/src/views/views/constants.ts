046da90c0b24d94dc516946519902e9689e33e2e
Aditya Mukhopadhyay <aditya@arangodb.com>
2021-09-28 08:49:29 +0530

    Schema complete.
    Signed-off-by: Aditya Mukhopadhyay <aditya@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/constants.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/constants.ts
@@ -1,6 +1,5 @@
 import { JSONSchemaType } from 'ajv';
 import { Dispatch } from 'react';
-import { Int } from "../../utils/constants";
 
 type Compression = 'lz4' | 'none';
 
@@ -23,10 +22,10 @@ type BytesAccumConsolidationPolicy = {
 
 type TierConsolidationPolicy = {
   type: 'tier';
-  segmentsMin?: Int;
-  segmentsMax?: Int;
-  segmentsBytesMax?: Int;
-  segmentsBytesFloor?: Int;
+  segmentsMin?: number;
+  segmentsMax?: number;
+  segmentsBytesMax?: number;
+  segmentsBytesFloor?: number;
 };
 
 type ConsolidationPolicy = BytesAccumConsolidationPolicy | TierConsolidationPolicy;
@@ -35,12 +34,12 @@ type ViewProperties = {
   primarySort?: PrimarySortOrder[];
   primarySortCompression?: Compression;
   storedValues?: StoredValue[];
-  cleanupIntervalStep?: Int;
-  commitIntervalMsec?: Int;
-  consolidationIntervalMsec?: Int;
-  writebufferIdle?: Int;
-  writebufferActive?: Int;
-  writebufferSizeMax?: Int;
+  cleanupIntervalStep?: number;
+  commitIntervalMsec?: number;
+  consolidationIntervalMsec?: number;
+  writebufferIdle?: number;
+  writebufferActive?: number;
+  writebufferSizeMax?: number;
   consolidationPolicy?: ConsolidationPolicy;
 };
 
@@ -65,32 +64,248 @@ type BaseFormState = {
 
 export type FormState = BaseFormState & ViewProperties;
 
-const linksSchema = {
-
-};
-
-const baseSchema = {
+export const linksSchema = {
+  $id: 'https://arangodb.com/schemas/views/linkProperties.json',
+  $recursiveAnchor: true,
   properties: {
-    name: {
+    analyzers: {
+      type: 'array',
       nullable: false,
-      type: 'string',
-      pattern: '^[a-zA-Z][a-zA-Z0-9-_]*$'
+      items: {
+        type: 'string',
+        pattern: '^([a-zA-Z0-9-_]+::)?[a-zA-Z][a-zA-Z0-9-_]*$'
+      }
     },
-    type: {
-      const: 'arangosearch'
-    },
-    links: {
-      nullable: false,
+    fields: {
       type: 'object',
-      properties: linksSchema
+      nullable: false,
+      patternProperties: {
+        '.+': {
+          $recursiveRef: '#'
+        }
+      }
+    },
+    includeAllFields: {
+      type: 'boolean',
+      nullable: false
+    },
+    trackListPositions: {
+      type: 'boolean',
+      nullable: false
+    },
+    storeValues: {
+      type: 'string',
+      nullable: false,
+      enum: ['none', 'id']
+    },
+    inBackground: {
+      type: 'boolean',
+      nullable: false
     }
   },
   additionalProperties: false
 };
 
 export const formSchema: JSONSchemaType<FormState> = {
+  $id: 'https://arangodb.com/schemas/views/views.json',
   type: 'object',
-  required: ['name', 'features']
+  properties: {
+    name: {
+      nullable: false,
+      type: 'string',
+      pattern: '^[a-zA-Z][a-zA-Z0-9-_]*$',
+      default: ''
+    },
+    type: {
+      type: 'string',
+      const: 'arangosearch'
+    },
+    links: {
+      nullable: true,
+      type: 'object',
+      patternProperties: {
+        '^[a-zA-Z0-9-_]+$': {
+          type: 'object',
+          $ref: 'linkProperties.json'
+        }
+      },
+      required: []
+    },
+    primarySort: {
+      type: 'array',
+      nullable: true,
+      items: {
+        type: 'object',
+        nullable: false,
+        properties: {
+          field: {
+            type: 'string',
+            nullable: false
+          },
+          direction: {
+            type: 'string',
+            enum: ['asc', 'desc'],
+            default: 'asc'
+          }
+        },
+        default: {
+          direction: 'asc'
+        },
+        required: ['field', 'direction'],
+        additionalProperties: false
+      }
+    },
+    primarySortCompression: {
+      type: 'string',
+      nullable: true,
+      enum: ['lz4', 'none'],
+      default: 'lz4'
+    },
+    storedValues: {
+      type: 'array',
+      nullable: true,
+      items: {
+        type: 'object',
+        nullable: false,
+        properties: {
+          fields: {
+            type: 'array',
+            nullable: false,
+            items: {
+              type: 'string'
+            }
+          },
+          compression: {
+            type: 'string',
+            enum: ['lz4', 'none'],
+            default: 'lz4'
+          }
+        },
+        required: ['fields', 'compression'],
+        default: {
+          compression: 'lz4'
+        },
+        additionalProperties: false
+      }
+    },
+    cleanupIntervalStep: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 2
+    },
+    commitIntervalMsec: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 1000
+    },
+    consolidationIntervalMsec: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 1000
+    },
+    writebufferIdle: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 64
+    },
+    writebufferActive: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 0
+    },
+    writebufferSizeMax: {
+      type: 'integer',
+      nullable: true,
+      minimum: 0,
+      default: 33554432
+    },
+    consolidationPolicy: {
+      type: 'object',
+      nullable: true,
+      discriminator: {
+        propertyName: "type"
+      },
+      oneOf: [
+        {
+          properties: {
+            type: {
+              const: 'bytes_accum'
+            },
+            threshold: {
+              type: 'number',
+              nullable: false,
+              minimum: 0,
+              maximum: 1,
+              default: 0.1
+            }
+          }
+        },
+        {
+          properties: {
+            type: {
+              const: 'tier'
+            },
+            segmentsMin: {
+              type: 'integer',
+              nullable: false,
+              minimum: 0,
+              maximum: {
+                $data: '1/segmentsMax'
+              },
+              default: 1
+            },
+            segmentsMax: {
+              type: 'integer',
+              nullable: false,
+              minimum: {
+                $data: '1/segmentsMin'
+              },
+              default: 10
+            },
+            segmentsBytesMax: {
+              type: 'integer',
+              nullable: false,
+              minimum: 0,
+              default: 5368709120
+            },
+            segmentsBytesFloor: {
+              type: 'integer',
+              nullable: false,
+              minimum: 0,
+              default: 2097152
+            }
+          }
+        }
+      ],
+      additionalProperties: false,
+      required: ['type']
+    }
+  },
+  required: ['name', 'type'],
+  default: {
+    name: '',
+    type: 'arangosearch',
+    primarySortCompression: 'lz4',
+    cleanupIntervalStep: 2,
+    commitIntervalMsec: 1000,
+    consolidationIntervalMsec: 1000,
+    writebufferIdle: 64,
+    writebufferActive: 0,
+    writebufferSizeMax: 33554432,
+    consolidationPolicy: {
+      type: 'tier',
+      segmentsMin: 1,
+      segmentsMax: 10,
+      segmentsBytesMax: 5368709120,
+      segmentsBytesFloor: 2097152
+    }
+  },
+  additionalProperties: false
 };
 
 export type State = {
