f8349fb02e0c8cede0a9e16c9c0aa6d21b3caeff
Heiko Kernbach <heiko@arangodb.com>
2021-10-06 13:18:40 +0200

    typo

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -270,7 +270,7 @@ LogicalCollection::LogicalCollection(TRI_vocbase_t& vocbase, VPackSlice info, bo
   }
 
   TRI_ASSERT(_physical != nullptr);
-  // This has to be called AFTER _phyiscal and _logical are properly linked
+  // This has to be called AFTER _physical and _logical are properly linked
   // together.
 
   prepareIndexes(info.get("indexes"));
