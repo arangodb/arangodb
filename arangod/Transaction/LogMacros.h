ae4f98617e8e8fac04b3c27c232ba653b339693c
Tobias Gödderz <tobias@arangodb.com>
2021-10-13 14:27:42 +0200

    Added Transaction/LogMacros.h

---
+++ arangod/Transaction/LogMacros.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2021-2021 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Tobias Gödderz
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Logger/LogMacros.h"
+
+#ifdef ARANGODB_ENABLE_MAINTAINER_MODE
+
+#define LOG_TRX(logid, llevel, trx)                        \
+  LOG_TOPIC(logid, llevel, arangodb::Logger::TRANSACTIONS) \
+      << "#" << trx->id().id() << " ("                     \
+      << transaction::statusString(trx->status()) << "): "
+
+#else
+
+#define LOG_TRX(logid, llevel, ...) \
+  while (0) LOG_TOPIC(logid, llevel, arangodb::Logger::TRANSACTIONS)
+#endif
