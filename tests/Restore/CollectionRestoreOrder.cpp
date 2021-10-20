07a5f593cac26ae06ab73ac2ca0af39261ee67e6
Tobias Gödderz <github@tobias.goedderz.info>
2021-10-20 14:46:26 +0200

    Added a random test

--- tests/Restore/CollectionRestoreOrder.cpp
+++ tests/Restore/CollectionRestoreOrder.cpp
@@ -22,12 +22,15 @@
 
 #include <gtest/gtest.h>
 
+#include "Containers/Enumerate.h"
+#include "Random/RandomGenerator.h"
 #include "Restore/RestoreFeature.h"
+#include "Utils/QuickGen.h"
 
-#include <Containers/Enumerate.h>
 #include <velocypack/Builder.h>
 
 using namespace arangodb;
+using namespace arangodb::tests;
 
 // @brief a regression test of a dump which was not restored in the right order
 //        regarding distributeShardsLike, due to a bug in the compare function.
@@ -100,4 +103,160 @@ TEST(CollectionRestoreOrder, regression1) {
         collections.at(idx).slice().get("parameters").get("name").stringView();
     EXPECT_EQ(name, actual);
   }
+}
+
+struct CollectionName {
+  std::string value;
+};
+static auto operator<<(std::ostream& ostream, CollectionName const& colName) -> std::ostream& {
+  return ostream << "\"" << colName.value << "\"";
+}
+struct Collection {
+  CollectionName name;
+  TRI_col_type_e type = TRI_COL_TYPE_DOCUMENT;
+  Collection* distributeShardsLike = nullptr;
+
+  void toVelocyPack(velocypack::Builder& builder) {
+    using namespace velocypack;
+    auto collection = ObjectBuilder(&builder);
+    builder.add(Value("parameters"));
+    {
+      auto parameters = ObjectBuilder(&builder);
+      builder.add("name", Value(name.value));
+      builder.add("type", Value(type));
+      if (distributeShardsLike != nullptr) {
+        builder.add("distributeShardsLike", Value(distributeShardsLike->name.value));
+      }
+    }
+  }
+};
+static auto operator<<(std::ostream& ostream, Collection const& col) -> std::ostream& {
+  ostream << "Collection{ name=" << col.name << ", type=" << col.type;
+  if (col.distributeShardsLike != nullptr) {
+    ostream << ", distributeShardsLike=" << col.distributeShardsLike->name;
+  }
+  ostream << " }";
+  return ostream;
+}
+struct CollectionSet {
+  // use pointers so we can shuffle collections around, but keep references
+  // between them intact
+  std::vector<std::unique_ptr<Collection>> collections;
+};
+
+namespace arangodb::tests::quick {
+static auto operator<<(std::ostream& ostream, CollectionSet const& colSet) -> std::ostream& {
+  ostream << "[";
+  bool first = true;
+  for (auto const& col : colSet.collections) {
+    if (first) {
+      first = false;
+      ostream << " ";
+    } else {
+      ostream << ", ";
+    }
+    ostream << *col;
+  }
+  ostream << "]";
+  return ostream;
+}
+
+template <>
+auto generate<CollectionName>() -> CollectionName {
+  auto name = std::string();
+  // make system collection with p=1/4
+  if (RandomGenerator::interval(0, 3) < 1) {
+    name += "_";
+  }
+  // short names suffice for these tests, so stay in SSO
+  auto const length = RandomGenerator::interval(1, 14);
+  for (auto i = 0; i < length; ++i) {
+    name += generate<AlphaNumeric>().c;
+  }
+
+  return {name};
+}
+
+template <>
+auto generate<CollectionSet>(std::int32_t max) -> CollectionSet {
+  std::size_t const n = RandomGenerator::interval(0, max);
+
+  auto collections = std::vector<std::unique_ptr<Collection>>();
+  collections.reserve(n);
+
+  // generate names until we get n different ones
+  auto collectionNames = std::unordered_set<std::string>();
+  collectionNames.reserve(n);
+  while (collectionNames.size() < n) {
+    // generate a name
+    auto const& [it, inserted] =
+        collectionNames.emplace(generate<CollectionName>().value);
+    if (inserted) {
+      // if unique, insert into vector
+      auto collection = Collection{};
+      collection.name.value = *it;
+      // generate the type
+      collection.type = generate<TRI_col_type_e>();
+      collections.emplace_back(std::make_unique<Collection>(collection));
+    }
+  }
+
+  // every collection but the first gets a distributeShardsLike with p=1/4
+  for (std::size_t i = 1; i < n; ++i) {
+    if (RandomGenerator::interval(0, 3) < 1) {
+      // choose a prototype with a smaller index, so we don't get cycles
+      std::size_t protoIdx =
+          RandomGenerator::interval(0, static_cast<std::int32_t>(i) - 1);
+      collections[i]->distributeShardsLike = collections[protoIdx].get();
+    }
+  }
+
+  // shuffle again to remove the pattern that distributeShardsLike always points
+  // "backwards".
+  auto urbg = RandomGenerator::UniformRandomGenerator<std::uint64_t>();
+  std::shuffle(collections.begin(), collections.end(), urbg);
+
+  return {std::move(collections)};
+}
+}  // namespace arangodb::tests::quick
+
+// Only checks order regarding distributeShardsLike for now
+void testValidity(CollectionSet const& collectionSet) {
+  auto const n = collectionSet.collections.size();
+
+  auto collections = std::vector<velocypack::Builder>();
+  std::transform(collectionSet.collections.begin(), collectionSet.collections.end(),
+                 std::back_inserter(collections), [](auto const& col) {
+                   auto builder = velocypack::Builder();
+                   col->toVelocyPack(builder);
+                   return builder;
+                 });
+  ASSERT_EQ(n, collections.size());
+
+  RestoreFeature::sortCollectionsForCreation(collections);
+
+  auto colToIdx = std::unordered_map<std::string, std::size_t>();
+  colToIdx.reserve(collections.size());
+  auto enumCols = enumerate(collections);
+  std::transform(enumCols.begin(), enumCols.end(),
+                 std::inserter(colToIdx, colToIdx.end()),
+                 [](auto const& it) -> std::pair<std::string, std::size_t> {
+                   auto const& [idx, builder] = it;
+                   return {builder.slice().get("parameters").get("name").copyString(), idx};
+                 });
+  ASSERT_EQ(n, colToIdx.size());
+
+  for (auto const& [idx, builder] : enumerate(collections)) {
+    auto const parametersSlice = builder.slice().get("parameters");
+    if (auto const distributeShardsLikeSlice = parametersSlice.get("distributeShardsLike"); !distributeShardsLikeSlice.isNone()) {
+      auto const distributeShardsLike = distributeShardsLikeSlice.copyString();
+      auto dslIdx = colToIdx.at(distributeShardsLike);
+      EXPECT_LT(dslIdx, idx);
+    }
+  }
+}
+
+TEST(CollectionRestoreOrder, random10) {
+  auto const collectionSet = quick::generate<CollectionSet>(10);
+  testValidity(collectionSet);
 }
