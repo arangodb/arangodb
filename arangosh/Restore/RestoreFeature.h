02de281e8949cd0fd65ae129cca917880b0ea927
Tobias Gödderz <tobias@arangodb.com>
2021-10-21 14:10:51 +0200

    Fixed compile error

--- arangosh/Restore/RestoreFeature.h
+++ arangosh/Restore/RestoreFeature.h
@@ -242,11 +242,6 @@ class RestoreFeature final : public application_features::ApplicationFeature {
   
   ClientTaskQueue<RestoreJob>& taskQueue();
 
-#ifndef ARANGODB_USE_GOOGLE_TESTS
- private:
-#else
- public:
-#endif
   static void sortCollectionsForCreation(std::vector<VPackBuilder>& collections);
 
  private:
