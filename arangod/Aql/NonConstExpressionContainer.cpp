c0c1b4611c5c36a78ef49bbe19b06eb57c86819c
Michael Hackstein <michael@arangodb.com>
2021-10-21 10:47:25 +0200

    Fixed copy right comment

--- arangod/Aql/NonConstExpressionContainer.cpp
+++ arangod/Aql/NonConstExpressionContainer.cpp
@@ -1,8 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
 ///
-/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
-/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+/// Copyright 2021-2021 ArangoDB GmbH, Cologne, Germany
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
