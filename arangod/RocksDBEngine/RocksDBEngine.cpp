6d7a250ed3ad7860dbd1403c8f154504329742ca
jsteemann <jsteemann@users.noreply.github.com>
2021-10-26 14:51:03 +0200

    TransactionDB does not support DeleteRange()
    call it on rocksdb::DB instead

--- arangod/RocksDBEngine/RocksDBEngine.cpp
+++ arangod/RocksDBEngine/RocksDBEngine.cpp
@@ -925,7 +925,7 @@ void RocksDBEngine::start() {
 
   _logPersistor = std::make_shared<RocksDBLogPersistor>(
       RocksDBColumnFamilyManager::get(RocksDBColumnFamilyManager::Family::ReplicatedLogs),
-      _db, std::make_shared<SchedulerExecutor>(server()));
+      _db->GetRootDB(), std::make_shared<SchedulerExecutor>(server()));
 
   _settingsManager->retrieveInitialValues();
 
