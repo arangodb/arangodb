c820017f20eb0f8f5f8f850b7e8bb137a638d95f
Kaveh Vahedipour <kaveh@arangodb.com>
2021-10-26 13:49:16 +0200

    code license mix up

--- arangod/RestHandler/RestLicenseHandler.h
+++ arangod/RestHandler/RestLicenseHandler.h
@@ -1,27 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
 ///
-/// Copyright 2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2014-2021 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
 ///
-/// The Programs (which include both the software and documentation) contain
-/// proprietary information of ArangoDB GmbH; they are provided under a license
-/// agreement containing restrictions on use and disclosure and are also
-/// protected by copyright, patent and other intellectual and industrial
-/// property laws. Reverse engineering, disassembly or decompilation of the
-/// Programs, except to the extent required to obtain interoperability with
-/// other independently created software or as specified by law, is prohibited.
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
 ///
-/// It shall be the licensee's responsibility to take all appropriate fail-safe,
-/// backup, redundancy, and other measures to ensure the safe use of
-/// applications if the Programs are used for purposes such as nuclear,
-/// aviation, mass transit, medical, or other inherently dangerous applications,
-/// and ArangoDB GmbH disclaims liability for any damages caused by such use of
-/// the Programs.
+///     http://www.apache.org/licenses/LICENSE-2.0
 ///
-/// This software is the confidential and proprietary information of ArangoDB
-/// GmbH. You shall not disclose such confidential and proprietary information
-/// and shall use it only in accordance with the terms of the license agreement
-/// you entered into with ArangoDB GmbH.
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
 ///
 /// Copyright holder is ArangoDB GmbH, Cologne, Germany
 ///
