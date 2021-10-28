2da0fc14a1fa60f6d6871a8fc17f23b1cd904bdf
cpjulia <julia@arangodb.com>
2021-10-28 16:43:26 -0300

    Fixed syntax

--- arangod/Aql/IndexNode.h
+++ arangod/Aql/IndexNode.h
@@ -122,11 +122,7 @@ class IndexNode : public ExecutionNode, public DocumentProducingNode, public Col
     return isProduceResult() && !_projections.supportsCoveringIndex();
   }
 
-<<<<<<< HEAD
   bool isDeterministic() override final { return canReadOwnWrites() == ReadOwnWrites::no; }
-=======
-  bool isDeterministic() override final { return canReadOwnWrites() == ReadOwnWrites::no); }
->>>>>>> 74b242dbe7 (Addressed suggestions)
 
   struct IndexVariable {
     size_t indexFieldNum;
