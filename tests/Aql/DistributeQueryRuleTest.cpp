27297e9ef31e15c045510c3c9384e7349fc2a55a
Michael Hackstein <michael@arangodb.com>
2021-10-29 16:06:56 +0200

    Added first test about DistributeQueryRule

---
+++ tests/Aql/DistributeQueryRuleTest.cpp
@@ -0,0 +1,93 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2021-2021 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Michael Hackstein
+////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include "Aql/Query.h"
+#include "Transaction/StandaloneContext.h"
+
+#include "../Mocks/Servers.h"
+
+
+using namespace arangodb;
+using namespace arangodb::aql;
+using namespace arangodb::transaction;
+
+namespace arangodb {
+namespace tests {
+namespace aql {
+
+class DistributeQueryRuleTest : public ::testing::Test {
+ protected:
+  mocks::MockCoordinator server;
+
+  std::shared_ptr<VPackBuilder> prepareQuery(std::string const& queryString) const {
+    auto ctx = std::make_shared<StandaloneContext>(server.getSystemDatabase());
+    auto const bindParamVPack = VPackParser::fromJson("{}");
+    auto const optionsVPack = VPackParser::fromJson(R"json({"optimizer": {"rules": ["distribute-query"]}})json");
+    auto query = Query::create(ctx, QueryString(queryString), bindParamVPack,
+                                                   QueryOptions(optionsVPack->slice()));
+
+    // NOTE: We can only get a VPacked Variant of the Plan, we cannot inject deep enough into the query.
+    auto res = query->explain();
+    if (!res.ok()) {
+      THROW_ARANGO_EXCEPTION_MESSAGE(res.errorNumber(), res.errorMessage());
+    }
+    return res.data;
+  }
+
+  void assertNodesMatch(VPackSlice actualNodes, std::vector<std::string> expectedNodes) {
+    ASSERT_TRUE(actualNodes.isArray());
+    for (size_t i = 0; i < expectedNodes.size(); ++i) {
+      if (actualNodes.length() > i) {
+        auto node = actualNodes.at(i);
+        EXPECT_TRUE(node.get("type").isEqualString(expectedNodes.at(i))) << node.get("type").toString() << " != " << expectedNodes.at(i);
+      } else {
+        EXPECT_TRUE(false) << "Too few nodes. expected: " << expectedNodes.size() << ", got: " << actualNodes.length();
+      }
+    }
+    EXPECT_EQ(actualNodes.length(), expectedNodes.size()) << "Non matching amount of nodes.";
+  }
+
+  DistributeQueryRuleTest() {
+    // We can register them, but then the API will call count, and the servers do not respond.
+    // Now we just get "no endpoint found" but this seems to be okay :shrug:
+    // server.registerFakedDBServer("DB1");
+    // server.registerFakedDBServer("DB2");
+    server.createCollection(server.getSystemDatabase().name(), "collection", {{"s123", "DB1"}, {"s234", "DB2"}}, TRI_col_type_e::TRI_COL_TYPE_DOCUMENT);
+  }
+};
+
+TEST_F(DistributeQueryRuleTest, single_enumerate_collection) {
+  auto queryString = "FOR x IN collection RETURN x";
+  auto plan = prepareQuery(queryString);
+
+  LOG_DEVEL << plan->toJson();
+
+  auto planSlice = plan->slice();
+  ASSERT_TRUE(planSlice.hasKey("nodes"));
+  planSlice = planSlice.get("nodes");
+  assertNodesMatch(planSlice, {"SingletonNode", "EnumerateCollectionNode", "RemoteNode", "ReturnNode"} );
+}
+}  // namespace aql
+}  // namespace tests
+}  // namespace arangodb
