e75e8278d1c86919435eec839e0bfc46c06283be
cpjulia <julia@arangodb.com>
2021-11-04 07:19:55 -0300

    Revert "Intermediate commit for testing"
    This reverts commit 3486bf231b86d3a50d92403ab716afc7d7ce3cca.

--- tests/IResearch/IResearchQueryScorer-test.cpp
+++ tests/IResearch/IResearchQueryScorer-test.cpp
@@ -43,8 +43,6 @@
 
 #include "utils/string_utils.hpp"
 
-#include "Logger/LogMacros.h"
-
 extern const char* ARGV0;  // defined in main.cpp
 
 namespace {
@@ -460,9 +458,7 @@ TEST_P(IResearchQueryScorerTest, test) {
         "RETURN { d, 'score' : customscorer(d, (FOR j IN testView SEARCH "
         "j.name == 'A' SORT BM25(j) RETURN j)[0].seq) }";
 
- //   auto queryResult = arangodb::tests::executeQuery(vocbase, query);
-    auto queryResult = arangodb::tests::executeQuery(vocbase, query, nullptr, "{ \"optimizer\": { \"rules\": [\"-move-calculations-up\", \"-move-calculations-up-2\"]}}");
-
+    auto queryResult = arangodb::tests::executeQuery(vocbase, query);
     ASSERT_TRUE(queryResult.result.is(TRI_ERROR_INTERNAL));
   }
 
