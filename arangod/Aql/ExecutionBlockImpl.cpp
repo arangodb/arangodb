b83278b74a9d9b5f67e05731bff192b4fa439f1d
Michael Hackstein <michael@arangodb.com>
2021-11-05 12:43:18 +0100

    Moved the fix a couple of lines upwards, auto-merg failure

--- arangod/Aql/ExecutionBlockImpl.cpp
+++ arangod/Aql/ExecutionBlockImpl.cpp
@@ -805,14 +805,16 @@ auto ExecutionBlockImpl<SubqueryStartExecutor>::shadowRowForwarding(AqlCallStack
   TRI_ASSERT(_outputItemRow);
   TRI_ASSERT(_outputItemRow->isInitialized());
   TRI_ASSERT(!_outputItemRow->allRowsUsed());
+
+  // The Subquery Start returns DONE after every row.
+  // This needs to be resetted as soon as a shadowRow has been produced
+  _executorReturnedDone = false;
+
   if (_lastRange.hasDataRow()) {
     // If we have a dataRow, the executor needs to write it's output.
     // If we get woken up by a dataRow during forwarding of ShadowRows
     // This will return false, and if so we need to call produce instead.
     auto didWrite = _executor.produceShadowRow(_lastRange, *_outputItemRow);
-    // The Subquery Start returns DONE after every row.
-    // This needs to be resetted as soon as a shadowRow has been produced
-    _executorReturnedDone = false;
     // Need to report that we have written a row in the call
 
     if (didWrite) {
@@ -852,8 +854,6 @@ auto ExecutionBlockImpl<SubqueryStartExecutor>::shadowRowForwarding(AqlCallStack
     if (subqueryCall.getLimit() == 0 && !subqueryCall.needSkipMore()) {
       return ExecState::DONE;
     }
-
-    _executorReturnedDone = false;
 
     return ExecState::NEXTSUBQUERY;
   }
