0c14cb4dcb1da87163d81868b3a9b551de5970c0
Heiko Kernbach <heiko@arangodb.com>
2021-11-08 12:22:42 +0100

    using isTrue is enough

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -3270,11 +3270,8 @@ ErrorCode RestReplicationHandler::createCollection(VPackSlice slice) {
   bool enforceReplicationFactor = false;
 
 #ifdef USE_ENTERPRISE
-  if ((slice.hasKey(StaticStrings::IsSmart) && slice.get(StaticStrings::IsSmart).isBoolean() &&
-       slice.get(StaticStrings::IsSmart).isTrue()) ||
-      (slice.hasKey(StaticStrings::GraphIsSatellite) &&
-       slice.get(StaticStrings::GraphIsSatellite).isBoolean() &&
-       slice.get(StaticStrings::GraphIsSatellite).isTrue())) {
+  if (slice.get(StaticStrings::IsSmart).isTrue() ||
+      slice.get(StaticStrings::GraphIsSatellite).isTrue()) {
     isSingleServerEnterpriseCollection = true;
     enforceReplicationFactor = true;
   }
