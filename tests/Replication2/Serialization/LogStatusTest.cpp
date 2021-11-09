923b03b3167d16b59cef528142df452050cad4d3
apetenchea <alexandru.petenchea@arangodb.com>
2021-11-09 12:55:21 +0200

    Added tests for CommitFailReason, FollowerStatistics, LeaderStatus and FollowerStatus.

--- tests/Replication2/Serialization/LogStatusTest.cpp
+++ tests/Replication2/Serialization/LogStatusTest.cpp
@@ -22,8 +22,6 @@
 
 #include "Basics/VelocyPackHelper.h"
 #include "velocypack/Builder.h"
-#include "velocypack/Options.h"
-#include "velocypack/Parser.h"
 #include "velocypack/velocypack-aliases.h"
 
 #include <gtest/gtest.h>
@@ -35,6 +33,7 @@ using namespace arangodb;
 using namespace arangodb::replication2;
 using namespace arangodb::replication2::replicated_log;
 using namespace arangodb::basics;
+using namespace std::literals::chrono_literals;
 
 static inline auto vpackFromJsonString(char const* c) -> VPackBuffer<uint8_t> {
   VPackOptions options;
@@ -53,15 +52,48 @@ static inline auto operator"" _vpack(const char* json, size_t) {
 }
 
 namespace arangodb::replication2::replicated_log {
-
-[[maybe_unused]] static inline auto operator==(LogStatistics const& left, LogStatistics const& right) {
-  return left.spearHead == right.spearHead && left.commitIndex == right.commitIndex &&
+[[maybe_unused]] static inline auto operator==(LogStatistics const& left,
+                                               LogStatistics const& right) {
+  return left.spearHead == right.spearHead &&
+         left.commitIndex == right.commitIndex &&
          left.firstIndex == right.firstIndex;
 }
 
+[[maybe_unused]] static inline auto operator==(CommitFailReason const& left,
+                                               CommitFailReason const& right) {
+  return left.value.index() == right.value.index();
 }
 
-TEST(LogStatusTest, term_index_pair_vpack) {
+[[maybe_unused]] static inline auto operator==(FollowerStatistics const& left,
+                                               FollowerStatistics const& right) {
+  return left.lastErrorReason == right.lastErrorReason &&
+         left.lastRequestLatencyMS == right.lastRequestLatencyMS &&
+         left.internalState.value.index() == right.internalState.value.index();
+}
+
+[[maybe_unused]] static inline auto operator==(LeaderStatus const& left,
+                                               LeaderStatus const& right) {
+  bool result = left.local == right.local &&
+                left.term == right.term &&
+                left.largestCommonIndex == right.largestCommonIndex &&
+                left.commitLagMS == right.commitLagMS &&
+                left.lastCommitStatus == right.lastCommitStatus
+                && left.follower.size() == right.follower.size();
+  if (!result) {
+    return false;
+  }
+  for (auto const& [participantId, followerStatistics] : left.follower) {
+    auto search = right.follower.find(participantId);
+    if (search == right.follower.end() || !(search->second == followerStatistics)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+}
+
+TEST(LogStatusTest, term_index_pair) {
   auto spearHead = TermIndexPair{LogTerm{2}, LogIndex{1}};
   VPackBuilder builder;
   spearHead.toVelocyPack(builder);
@@ -78,7 +110,7 @@ TEST(LogStatusTest, term_index_pair_vpack) {
             << "expected " << jsonSlice.toJson() << " found " << slice.toJson();
 }
 
-TEST(LogStatusTest, log_statistics_vpack) {
+TEST(LogStatusTest, log_statistics) {
   LogStatistics statistics;
   statistics.spearHead = TermIndexPair{LogTerm{2}, LogIndex{1}};
   statistics.commitIndex = LogIndex{1};
@@ -100,4 +132,166 @@ TEST(LogStatusTest, log_statistics_vpack) {
   auto jsonSlice = velocypack::Slice(jsonBuffer.data());
   EXPECT_TRUE(VelocyPackHelper::equal(jsonSlice, slice, true))
       << "expected " << jsonSlice.toJson() << " found " << slice.toJson();
+}
+
+TEST(LogStatusTest, commit_fail_reason) {
+  VPackBuilder builder;
+  auto reason = CommitFailReason::withNothingToCommit();
+  reason.toVelocyPack(builder);
+  auto slice = builder.slice();
+  auto fromVPack = CommitFailReason::fromVelocyPack(slice);
+  EXPECT_EQ(reason, fromVPack);
+
+  auto jsonBuffer = R"({
+    "reason": "NothingToCommit"
+  })"_vpack;
+  auto jsonSlice = velocypack::Slice(jsonBuffer.data());
+  EXPECT_TRUE(VelocyPackHelper::equal(jsonSlice, slice, true))
+            << "expected " << jsonSlice.toJson() << " found " << slice.toJson();
+
+  builder.clear();
+  reason = CommitFailReason::withQuorumSizeNotReached();
+  reason.toVelocyPack(builder);
+  slice = builder.slice();
+  fromVPack = CommitFailReason::fromVelocyPack(slice);
+  EXPECT_EQ(reason, fromVPack);
+
+  jsonBuffer = R"({"xyz": "NothingToCommit", "reason": "xyz"})"_vpack;
+  jsonSlice = velocypack::Slice(jsonBuffer.data());
+  EXPECT_ANY_THROW({CommitFailReason::fromVelocyPack(jsonSlice);});
+}
+
+TEST(LogStatusTest, follower_statistics_exceptions) {
+  // Missing commitIndex
+  EXPECT_ANY_THROW({
+    FollowerStatistics::fromVelocyPack(velocypack::Slice(R"({
+      "missing_commitIndex": 4,
+      "spearhead": {
+        "term": 2,
+        "index": 4
+      },
+      "lastErrorReason": 0,
+      "lastRequestLatencyMS": 0.012983,
+      "state": {
+        "state": "up-to-date"
+      }
+      })"_vpack.data()));
+  });
+
+  // Wrong type for commitIndex
+  EXPECT_ANY_THROW({
+    FollowerStatistics::fromVelocyPack(velocypack::Slice(R"({
+      "commitIndex": "4",
+      "spearhead": {
+        "term": 2,
+        "index": 4
+      },
+      "lastErrorReason": 0,
+      "lastRequestLatencyMS": 0.012983,
+      "state": {
+        "state": "up-to-date"
+      }
+      })"_vpack.data()));
+  });
+}
+
+TEST(LogStatusTest, leader_status) {
+  LeaderStatus leaderStatus;
+  LogStatistics statistics;
+  statistics.spearHead = TermIndexPair{LogTerm{2}, LogIndex{1}};
+  statistics.commitIndex = LogIndex{1};
+  statistics.firstIndex = LogIndex{1};
+  leaderStatus.local = statistics;
+  leaderStatus.term = LogTerm{2};
+  leaderStatus.largestCommonIndex = LogIndex{1};
+  std::unordered_map<ParticipantId, FollowerStatistics> follower({
+      {
+        "PRMR-45c56239-6a83-4ab0-961e-9adea5078286",
+        FollowerStatistics::fromVelocyPack(velocypack::Slice(R"({
+        "commitIndex": 4,
+        "spearhead": {
+          "term": 2,
+          "index": 4
+        },
+        "lastErrorReason": 0,
+        "lastRequestLatencyMS": 0.012983,
+        "state": {
+          "state": "up-to-date"
+        }
+        })"_vpack.data()))
+      },
+      {
+        "PRMR-13608015-4a2c-46aa-985f-73b6b8a73568",
+        FollowerStatistics::fromVelocyPack(velocypack::Slice(R"({
+          "commitIndex": 3,
+          "spearhead": {
+            "term": 2,
+            "index": 3
+          },
+          "lastErrorReason": 7,
+          "lastRequestLatencyMS": 11159.799272,
+          "state": {
+            "state": "request-in-flight",
+            "durationMS": 4143.651874
+          }
+        })"_vpack.data()))
+      }
+  });
+  leaderStatus.follower = std::move(follower);
+  leaderStatus.lastCommitStatus = CommitFailReason::withNothingToCommit();
+  leaderStatus.commitLagMS = 0.014453ms;
+  VPackBuilder builder;
+  leaderStatus.toVelocyPack(builder);
+  auto slice = builder.slice();
+  auto fromVPack = LeaderStatus::fromVelocyPack(slice);
+  EXPECT_EQ(leaderStatus, fromVPack);
+}
+
+TEST(LogStatusTest, follower_status) {
+  auto jsonBuffer = R"({
+    "role": "follower",
+    "leader": "PRMR-d2a1b29e-ff75-412e-8b97-f3bfbf464fab",
+    "term": 2,
+    "largestCommonIndex": 3,
+    "local": {
+      "commitIndex": 4,
+      "firstIndex": 1,
+      "spearhead": {
+        "term": 2,
+        "index": 4
+      }
+    }
+  })"_vpack;
+  auto followerSlice = velocypack::Slice(jsonBuffer.data());
+  auto followerStatus = FollowerStatus::fromVelocyPack(followerSlice);
+  EXPECT_TRUE(followerStatus.leader.has_value());
+
+  VPackBuilder builder;
+  followerStatus.toVelocyPack(builder);
+  auto builderSlice = builder.slice();
+  EXPECT_TRUE(VelocyPackHelper::equal(followerSlice, builderSlice, true))
+            << "expected " << followerSlice.toJson() << " found " << builderSlice.toJson();
+
+  builder.clear();
+  followerStatus.leader = std::nullopt;
+  followerStatus.toVelocyPack(builder);
+  builderSlice = builder.slice();
+  jsonBuffer = R"({
+    "role": "follower",
+    "term": 2,
+    "largestCommonIndex": 3,
+    "local": {
+      "commitIndex": 4,
+      "firstIndex": 1,
+      "spearhead": {
+        "term": 2,
+        "index": 4
+      }
+    }
+  })"_vpack;
+  followerSlice = velocypack::Slice(jsonBuffer.data());
+  auto followerStatusNoLeader = FollowerStatus::fromVelocyPack(followerSlice);
+  EXPECT_FALSE(followerStatusNoLeader.leader.has_value());
+  EXPECT_TRUE(VelocyPackHelper::equal(followerSlice, builderSlice, true))
+            << "expected " << followerSlice.toJson() << " found " << builderSlice.toJson();
 }
