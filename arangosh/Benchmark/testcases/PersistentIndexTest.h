ee98f5c3f6dc41f3dce172612ef607e5d8ace985
Jan <jsteemann@users.noreply.github.com>
2021-11-17 21:21:21 +0100

    remove deprecated arangobench testcases (#15147)

--- arangosh/Benchmark/testcases/PersistentIndexTest.h
+++ arangosh/Benchmark/testcases/PersistentIndexTest.h
@@ -24,57 +24,53 @@
 #pragma once
 
 #include "Benchmark.h"
+#include "helpers.h"
 #include <velocypack/Builder.h>
 #include <velocypack/Value.h>
 #include <string>
 
 namespace arangodb::arangobench {
 
-  struct ShapesAppendTest : public Benchmark<ShapesAppendTest> {
-    static std::string name() { return "shapes-append"; }
+  struct PersistentIndexTest : public Benchmark<PersistentIndexTest> {
+    static std::string name() { return "persistent-index"; }
 
-    ShapesAppendTest(BenchFeature& arangobench) : Benchmark<ShapesAppendTest>(arangobench) {}
+    PersistentIndexTest(BenchFeature& arangobench) : Benchmark<PersistentIndexTest>(arangobench) {}
 
     bool setUp(arangodb::httpclient::SimpleHttpClient* client) override {
       return DeleteCollection(client, _arangobench.collection()) &&
-        CreateCollection(client, _arangobench.collection(), 2, _arangobench);
+        CreateCollection(client, _arangobench.collection(), 2, _arangobench) &&
+        CreateIndex(client, _arangobench.collection(), "hash", "[\"value\"]");
     }
 
     void tearDown() override {}
 
     void buildRequest(size_t threadNumber, size_t threadCounter,
                       size_t globalCounter, BenchmarkOperation::RequestData& requestData) const override {
-      size_t keyId = static_cast<size_t>(globalCounter / 2);
+      size_t keyId = static_cast<size_t>(globalCounter / 4);
       std::string const key = "testkey" + StringUtils::itoa(keyId);
-      size_t const mod = globalCounter % 2;
+      size_t const mod = globalCounter % 4;
       if (mod == 0) {
         requestData.url = std::string("/_api/document?collection=" + _arangobench.collection()) + "&silent=true";
         requestData.type = rest::RequestType::POST;
-        
+      } else {
+        requestData.url = std::string("/_api/document/" + _arangobench.collection() + "/" + key);
+        requestData.type = (mod == 2) ? rest::RequestType::PATCH : rest::RequestType::GET;
+      }
+      if (mod == 0 || mod == 2) {
         using namespace arangodb::velocypack;
         requestData.payload.openObject();
         requestData.payload.add(StaticStrings::KeyString, Value(key));
-        uint64_t const n = _arangobench.complexity();
-        for (uint64_t i = 1; i <= n; ++i) {
-          uint64_t mod = _arangobench.operations() / 10;
-          if (mod < 100) {
-            mod = 100;
-          }
-          requestData.payload.add(std::string("value") + std::to_string(static_cast<uint64_t>((globalCounter + i) % mod)), Value("some bogus string value to fill up the datafile..."));
-        }
-        requestData.payload.close();;
-      } else {
-        requestData.url = std::string("/_api/document/" + _arangobench.collection() + "/" + key);
-        requestData.type = rest::RequestType::GET;
+        requestData.payload.add("value", Value(static_cast<uint32_t>(threadCounter)));
+        requestData.payload.close();
       }
     }
 
     char const* getDescription() const noexcept override {
-      return "will perform a mix of insert and get operations for documents with randomized attribute names. 50% of the operations will be single-document inserts, and 50% of the operations will be single-document reads. There will be a total of --requests operations. The --complexity parameter can be used to control the number of attributes for the inserted documents.";
+      return "will perform a mix of insert, update and get operations for documents. The collection created by this test does have an extra, non-unique, non-sparse persistent index on one attribute. 25% of the operations will be single-document inserts, 25% of the operations will be single-document updates, and 50% of the operations are single-document read requests. There will be a total of --requests operations. The --complexity parameter can be used to control the number of attributes for the inserted and updated documents. This test case can be used to determine the effects on write throughput caused by adding a secondary index to a collection. It originally tested a hash index, but both the in-memory hash and skiplist index types were removed in favor of the RocksDB-based persistent index type.";
     }
 
     bool isDeprecated() const noexcept override {
-      return true;
+      return false;
     }
 
   };
