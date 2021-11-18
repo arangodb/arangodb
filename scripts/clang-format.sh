05a343481d1caeba5253692e687c59e04ae1f2c4
aMahanna <anthony.mahanna@gmail.com>
2021-11-18 17:46:31 -0500

    Update clang-format.sh

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-[[ -z "$1" ]] && adb_dir="${PWD}" || adb_dir="$1" # If no argument is provided, set adb_dir to pwd
-docker run -it --rm -v "$adb_dir":/usr/src/arangodb arangodb/clang-format:latest
+[[ -z "$1" ]] && adb_path="${PWD}" || adb_path=$(echo "$1" | sed 's:/*$::') # Default path to arangodb as pwd
+[[ -z "$2" ]] && ent_dir="enterprise" || ent_dir=$(echo "$2" | sed 's:/*$::') # Default enterprise dir to enterprise
+
+echo "ArangoDB directory: $adb_path"
+echo "Enterprise directory: $adb_path/$ent_dir"
+
+cd "$adb_path"
+community_diff=$(git diff --diff-filter=d --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h')
+
+if [ -d "$adb_path/$ent_dir" ] # assume enterprise directory is within arangodb directory
+then
+   cd "$adb_path/$ent_dir"
+   enterprise_diff=$(git diff --diff-filter=d --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h' | sed "s,^,$ent_dir/,")
+fi
+
+diff="$community_diff $enterprise_diff"
+docker run -it --rm -v "$adb_path":/usr/src/arangodb clang-format:latest "$diff"
