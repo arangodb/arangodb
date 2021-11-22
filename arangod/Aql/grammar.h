91b4c7d32f514ec7c060ec6e2ebbec709624002f
jsteemann <jsteemann@users.noreply.github.com>
2021-11-22 18:16:25 +0100

    added generated files

--- arangod/Aql/grammar.h
+++ arangod/Aql/grammar.h
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.7.5.  */
+/* A Bison parser, made by GNU Bison 3.7.6.  */
 
 /* Bison interface for Yacc-like parsers in C
 
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -136,7 +136,7 @@ extern int Aqldebug;
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 union YYSTYPE
 {
-#line 37 "Aql/grammar.y"
+#line 38 "Aql/grammar.y"
 
   arangodb::aql::AstNode*  node;
   struct {
