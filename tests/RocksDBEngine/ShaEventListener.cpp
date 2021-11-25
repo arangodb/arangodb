2751cbde552c9a064dbe1aeb4e856e4aef54400d
cpjulia <julia@arangodb.com>
2021-11-25 13:16:42 -0300

    Updated test

--- tests/RocksDBEngine/ShaEventListener.cpp
+++ tests/RocksDBEngine/ShaEventListener.cpp
@@ -21,17 +21,16 @@
 /// @author Lars Maier
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <rocksdb/listener.h>
 #include "ApplicationFeatures/ApplicationServer.h"
 #include "Basics/StringBuffer.h"
 #include "Basics/files.h"
 #include "Basics/system-functions.h"
 #include "Random/RandomGenerator.h"
 #include "RocksDBEngine/RocksDBSha256Checksum.h"
-#include <rocksdb/listener.h>
 
-
-#include "gtest/gtest.h"
 #include <string>
+#include "gtest/gtest.h"
 
 using namespace arangodb::basics;
 
@@ -46,7 +45,7 @@ static uint64_t counter = 0;
 /// CFilesSetup class copied from tests/Basic/files-test.cpp
 ////////////////////////////////////////////////////////////////////////////////
 struct CFilesSetup {
-  CFilesSetup () : _directory(true) {
+  CFilesSetup() {
     long systemError;
     std::string errorMessage;
 
@@ -55,16 +54,16 @@ struct CFilesSetup {
       arangodb::RandomGenerator::initialize(arangodb::RandomGenerator::RandomType::MERSENNE);
     }
 
-    _directory.appendText(TRI_GetTempPath());
-    _directory.appendChar(TRI_DIR_SEPARATOR_CHAR);
-    _directory.appendText("arangotest-");
-    _directory.appendInteger(static_cast<uint64_t>(TRI_microtime()));
-    _directory.appendInteger(arangodb::RandomGenerator::interval(UINT32_MAX));
+    _directory = TRI_GetTempPath();
+    _directory += TRI_DIR_SEPARATOR_CHAR;
+    _directory += "arangotest-";
+    _directory += std::to_string(static_cast<uint64_t>(TRI_microtime()));
+    _directory += std::to_string(arangodb::RandomGenerator::interval(UINT32_MAX));
 
     TRI_CreateDirectory(_directory.c_str(), systemError, errorMessage);
   }
 
-  ~CFilesSetup () {
+  ~CFilesSetup() {
     // let's be sure we delete the right stuff
     TRI_ASSERT(_directory.length() > 10);
 
@@ -83,7 +82,7 @@ struct CFilesSetup {
 
     if (fd) {
       size_t numWritten = fwrite(blob, strlen(blob), 1, fd);
-      (void) numWritten;
+      (void)numWritten;
       fclose(fd);
     } else {
       EXPECT_TRUE(false);
@@ -100,16 +99,15 @@ struct CFilesSetup {
 
     if (fd) {
       size_t numWritten = fwrite(blob, strlen(blob), 1, fd);
-      (void) numWritten;
+      (void)numWritten;
       fclose(fd);
     } else {
       EXPECT_TRUE(false);
     }
   }
 
-  StringBuffer _directory;
+  std::string _directory;
 };
-
 
 // -----------------------------------------------------------------------------
 // --SECTION--                                                        action tests
@@ -122,12 +120,12 @@ struct CFilesSetup {
 TEST(RocksDBShaFileHandlerTest, sha_a_new_file) {
   CFilesSetup s;
 
-  auto shaFileManager = std::make_shared<arangodb::RocksDBShaFileManager>(s._directory.c_str());
+  auto shaFileManager = std::make_shared<arangodb::RocksDBShaFileManager>(s._directory);
 
-  std::string new_sst = s._directory.c_str();
-  new_sst += TRI_DIR_SEPARATOR_CHAR;
-  new_sst += "000042.sst";
-  
+  std::string filePath = s._directory;
+  filePath += TRI_DIR_SEPARATOR_CHAR;
+  std::string new_sst = filePath + "000042.sst";
+
   auto ret_val = TRI_WriteFile(new_sst.c_str(), "the quick brown fox", 19);
   EXPECT_EQ(ret_val, TRI_ERROR_NO_ERROR);
 
@@ -139,21 +137,35 @@ TEST(RocksDBShaFileHandlerTest, sha_a_new_file) {
     checksumGenerator.Finalize();
   }
 
-  EXPECT_EQ(checksumGenerator.GetChecksum(), "9ecb36561341d18eb65484e833efea61edc74b84cf5e6ae1b81c63533e25fc8f");
+  EXPECT_EQ(checksumGenerator.GetChecksum(),
+            "9ecb36561341d18eb65484e833efea61edc74b84cf5e6ae1b81c63533e25fc8f");
+  EXPECT_TRUE(TRI_ExistsFile(
+      (filePath +
+       "000042.sha."
+       "9ecb36561341d18eb65484e833efea61edc74b84cf5e6ae1b81c63533e25fc8f.hash")
+          .c_str()));
 }
 
 TEST(RocksDBShaFileHandlerTest, write_file_with_checksum) {
   CFilesSetup s;
 
-  std::string new_sst = s._directory.c_str();
-  new_sst += TRI_DIR_SEPARATOR_CHAR;
-  new_sst += "000042.sst";
+  std::string filePath = s._directory;
+  filePath += TRI_DIR_SEPARATOR_CHAR;
+  std::string new_sst = filePath + "000042.sst";
+
   auto ret_val = TRI_WriteFile(new_sst.c_str(), "12345 67890 12345 67890", 23);
   EXPECT_EQ(ret_val, TRI_ERROR_NO_ERROR);
 
-  arangodb::RocksDBShaFileManager shaFileManager(s._directory.c_str());
-  EXPECT_TRUE(shaFileManager.writeShaFile(new_sst, "e7f5561536b5891e35d6021015d67d5798b3731088b44dcebf6bad03785ac8c2"));
+  arangodb::RocksDBShaFileManager shaFileManager(s._directory);
+  EXPECT_TRUE(shaFileManager.writeShaFile(
+      new_sst,
+      "e7f5561536b5891e35d6021015d67d5798b3731088b44dcebf6bad03785ac8c2"));
 
+  EXPECT_TRUE(TRI_ExistsFile(
+      (filePath +
+       "000042.sha."
+       "e7f5561536b5891e35d6021015d67d5798b3731088b44dcebf6bad03785ac8c2.hash")
+          .c_str()));
 }
 
 /*
@@ -198,70 +210,107 @@ class RocksDBShaFileHandlerEnvGenerator {
   CFilesSetup setup;
 
   std::string getServerPath() {
-    return setup._directory.c_str();
+    return setup._directory;
   }
 
-  std::string getFilePath(const char * name) {
+  std::string getFilePath(const char* name) {
     std::string retpath;
-    retpath = setup._directory.c_str();
+    retpath = setup._directory;
     retpath += TRI_DIR_SEPARATOR_CHAR;
     retpath += name;
     return retpath;
   };
 };
 
-
 TEST(CheckMissingShaFilesTest, verify_common_situations) {
   arangodb::application_features::ApplicationServer server{nullptr, nullptr};
 
   RocksDBShaFileHandlerEnvGenerator envGenerator;
-  auto shaFileManager = std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
+  auto shaFileManager =
+      std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
 
   shaFileManager->checkMissingShaFiles();
 
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("MANIFEST-000004").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("CURRENT").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("IDENTITY").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("037793.sst").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("037793.sha.e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.hash").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("037684.sst").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("037684.sha.2db3c4a7da801356e4efda0d65229d0baadf6950b366418e96abb7ece9c56c12.hash").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("086219.sst").c_str()));
-  EXPECT_TRUE( TRI_ExistsFile(envGenerator.getFilePath("086219.sha.5d3cfa346c3852c0c108d720d580cf99910749f17c8429c07c1c2d714be2b7ff.hash").c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("MANIFEST-000004").c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("CURRENT").c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("IDENTITY").c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("037793.sst").c_str()));
+  EXPECT_TRUE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("037793.sha."
+                                      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4"
+                                      "649b934ca495991b7852b855.hash")
+                         .c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("037684.sst").c_str()));
+  EXPECT_TRUE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("037684.sha."
+                                      "2db3c4a7da801356e4efda0d65229d0baadf6950"
+                                      "b366418e96abb7ece9c56c12.hash")
+                         .c_str()));
+  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("086219.sst").c_str()));
+  EXPECT_TRUE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086219.sha."
+                                      "5d3cfa346c3852c0c108d720d580cf99910749f1"
+                                      "7c8429c07c1c2d714be2b7ff.hash")
+                         .c_str()));
 
-  EXPECT_TRUE( !TRI_ExistsFile(envGenerator.getFilePath("086218.sha.e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.hash").c_str()));
+  EXPECT_FALSE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086218.sha."
+                                      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4"
+                                      "649b934ca495991b7852b855.hash")
+                         .c_str()));
 }
-
 
 TEST(RocksDBShaFileHandlerTest, delete_sha_file_direct) {
   arangodb::application_features::ApplicationServer server{nullptr, nullptr};
 
   RocksDBShaFileHandlerEnvGenerator envGenerator;
-  auto shaFileManager = std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
+  auto shaFileManager =
+      std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
 
   shaFileManager->checkMissingShaFiles();
 
-  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("086219.sha.5d3cfa346c3852c0c108d720d580cf99910749f17c8429c07c1c2d714be2b7ff.hash").c_str()));
+  EXPECT_TRUE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086219.sha."
+                                      "5d3cfa346c3852c0c108d720d580cf99910749f1"
+                                      "7c8429c07c1c2d714be2b7ff.hash")
+                         .c_str()));
 
   shaFileManager->deleteFile(envGenerator.getFilePath("086219.sst"));
-  EXPECT_TRUE(!TRI_ExistsFile(envGenerator.getFilePath("086219.sha.5d3cfa346c3852c0c108d720d580cf99910749f17c8429c07c1c2d714be2b7ff.hash").c_str()));
-
+  EXPECT_FALSE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086219.sha."
+                                      "5d3cfa346c3852c0c108d720d580cf99910749f1"
+                                      "7c8429c07c1c2d714be2b7ff.hash")
+                         .c_str()));
 }
-
 
 TEST(RocksDBShaFileHandlerTest, delete_sha_file_indirect) {
   arangodb::application_features::ApplicationServer server{nullptr, nullptr};
 
   RocksDBShaFileHandlerEnvGenerator envGenerator;
 
-  auto shaFileManager = std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
+  auto shaFileManager =
+      std::make_shared<arangodb::RocksDBShaFileManager>(envGenerator.getServerPath());
   shaFileManager->checkMissingShaFiles();
 
   rocksdb::TableFileDeletionInfo info;
   info.file_path = envGenerator.getFilePath("086219.sst");
-  EXPECT_TRUE(TRI_ExistsFile(envGenerator.getFilePath("086219.sha.5d3cfa346c3852c0c108d720d580cf99910749f17c8429c07c1c2d714be2b7ff.hash").c_str()));
+  EXPECT_TRUE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086219.sha."
+                                      "5d3cfa346c3852c0c108d720d580cf99910749f1"
+                                      "7c8429c07c1c2d714be2b7ff.hash")
+                         .c_str()));
   shaFileManager->OnTableFileDeleted(info);
-  EXPECT_TRUE(!TRI_ExistsFile(envGenerator.getFilePath("086219.sha.5d3cfa346c3852c0c108d720d580cf99910749f17c8429c07c1c2d714be2b7ff.hash").c_str()));
+  EXPECT_FALSE(
+      TRI_ExistsFile(envGenerator
+                         .getFilePath("086219.sha."
+                                      "5d3cfa346c3852c0c108d720d580cf99910749f1"
+                                      "7c8429c07c1c2d714be2b7ff.hash")
+                         .c_str()));
 }
-
-
