50520ec7e9706ca5311745d50c036d09ca942c64
Andreas Jurdak <andreas@jurdak.at>
2021-12-06 15:00:00 +0100

    [FE-8] Feature: Add layout selector

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/MenuGraph.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/MenuGraph.js
@@ -2,20 +2,203 @@
 
 import React, { useEffect, useState, useCallback } from 'react';
 import Graphin, { Utils, GraphinContext } from '@antv/graphin';
-import { message, Card } from 'antd';
+import { message, Card, Select } from 'antd';
 import { ContextMenu, FishEye } from '@antv/graphin-components';
 import { useFetch } from './useFetch';
 import AqlEditor from './AqlEditor';
 import {
   TagFilled,
   DeleteFilled,
-  ExpandAltOutlined
+  ExpandAltOutlined,
+  TrademarkCircleFilled,
+  ChromeFilled,
+  BranchesOutlined,
+  ApartmentOutlined,
+  AppstoreFilled,
+  CopyrightCircleFilled,
+  CustomerServiceFilled,
+  ShareAltOutlined
+
 } from '@ant-design/icons';
 
 const MenuGraph = () => {
   let [queryString, setQueryString] = useState("/_admin/aardvark/g6graph/routeplanner");
   let [graphData, setGraphData] = useState(null);
   let [queryMethod, setQueryMethod] = useState("GET");
+  
+  const iconMap = {
+    'graphin-force': <ShareAltOutlined />,
+    random: <TrademarkCircleFilled />,
+    concentric: <ChromeFilled />,
+    circle: <BranchesOutlined />,
+    force: <AppstoreFilled />,
+    dagre: <ApartmentOutlined />,
+    grid: <CopyrightCircleFilled />,
+    radial: <ShareAltOutlined />,
+  };
+
+  const SelectOption = Select.Option;
+  const LayoutSelector = props => {
+    const { value, onChange, options } = props;
+
+    return (
+      <div>
+        <Select style={{ width: '120px' }} value={value} onChange={onChange}>
+          {options.map(item => {
+            const { type } = item;
+            const iconComponent = iconMap[type] || <CustomerServiceFilled />;
+            return (
+              <SelectOption key={type} value={type}>
+                {iconComponent} &nbsp;
+                {type}
+              </SelectOption>
+            );
+          })}
+        </Select>
+      </div>
+    );
+  };
+
+  const layouts = [
+    { type: 'graphin-force' },
+    {
+        type: 'grid',
+        // begin: [0, 0], // optional，
+        // preventOverlap: true, // optional
+        // preventOverlapPdding: 20, // optional
+        // nodeSize: 30, // optional
+        // condense: false, // optional
+        // rows: 5, // optional
+        // cols: 5, // optional
+        // sortBy: 'degree', // optional
+        // workerEnabled: false, // optional
+    },
+    {
+        type: 'circular',
+        // center: [200, 200], // optional
+        // radius: null, // optional
+        // startRadius: 10, // optional
+        // endRadius: 100, // optional
+        // clockwise: false, // optional
+        // divisions: 5, // optional
+        // ordering: 'degree', // optional
+        // angleRatio: 1, // optional
+    },
+    {
+        type: 'radial',
+        // center: [200, 200], // optional
+        // linkDistance: 50, // optional，
+        // maxIteration: 1000, // optional
+        // focusNode: 'node11', // optional
+        // unitRadius: 100, // optional
+        // preventOverlap: true, // optional
+        // nodeSize: 30, // optional
+        // strictRadial: false, // optional
+        // workerEnabled: false, // optional
+    },
+    {
+        type: 'force',
+        preventOverlap: true,
+        // center: [200, 200], // optional
+        linkDistance: 50, // optional
+        nodeStrength: 30, // optional
+        edgeStrength: 0.8, // optional
+        collideStrength: 0.8, // optional
+        nodeSize: 30, // optional
+        alpha: 0.9, // optional
+        alphaDecay: 0.3, // optional
+        alphaMin: 0.01, // optional
+        forceSimulation: null, // optional
+        onTick: () => {
+            // optional
+            console.log('ticking');
+        },
+        onLayoutEnd: () => {
+            // optional
+            console.log('force layout done');
+        },
+    },
+    {
+        type: 'gForce',
+        linkDistance: 150, // optional
+        nodeStrength: 30, // optional
+        edgeStrength: 0.1, // optional
+        nodeSize: 30, // optional
+        onTick: () => {
+            // optional
+            console.log('ticking');
+        },
+        onLayoutEnd: () => {
+            // optional
+            console.log('force layout done');
+        },
+        workerEnabled: false, // optional
+        gpuEnabled: false, // optional
+    },
+    {
+        type: 'concentric',
+        maxLevelDiff: 0.5,
+        sortBy: 'degree',
+        // center: [200, 200], // optional
+
+        // linkDistance: 50, // optional
+        // preventOverlap: true, // optional
+        // nodeSize: 30, // optional
+        // sweep: 10, // optional
+        // equidistant: false, // optional
+        // startAngle: 0, // optional
+        // clockwise: false, // optional
+        // maxLevelDiff: 10, // optional
+        // sortBy: 'degree', // optional
+        // workerEnabled: false, // optional
+    },
+    {
+        type: 'dagre',
+        rankdir: 'LR', // optional
+        // align: 'DL', // optional
+        // nodesep: 20, // optional
+        // ranksep: 50, // optional
+        // controlPoints: true, // optional
+    },
+    {
+        type: 'fruchterman',
+        // center: [200, 200], // optional
+        // gravity: 20, // optional
+        // speed: 2, // optional
+        // clustering: true, // optional
+        // clusterGravity: 30, // optional
+        // maxIteration: 2000, // optional
+        // workerEnabled: false, // optional
+        // gpuEnabled: false, // optional
+    },
+    {
+        type: 'mds',
+        workerEnabled: false, // optional
+    },
+    {
+        type: 'comboForce',
+        // // center: [200, 200], // optional
+        // linkDistance: 50, // optional
+        // nodeStrength: 30, // optional
+        // edgeStrength: 0.1, // optional
+        // onTick: () => {
+        //   // optional
+        //   console.log('ticking');
+        // },
+        // onLayoutEnd: () => {
+        //   // optional
+        //   console.log('combo force layout done');
+        // },
+    },
+  ];
+
+  const [type, setLayout] = React.useState('graphin-force');
+  const handleChange = value => {
+    console.log('value', value);
+    setLayout(value);
+  };
+
+  const layout = layouts.find(item => item.type === type);
 
   // fetchData component - start
   const fetchData = useCallback(() => {
@@ -108,8 +291,11 @@ const MenuGraph = () => {
           queryString={queryString}
           onNewSearch={(myString) => {setQueryString(myString)}}
           onQueryChange={(myString) => {setQueryString(myString)}} />
-        <Card title="Routeplanner">
-          <Graphin data={graphData}>
+        <Card
+          title="G6 Graph Viewer"
+          extra={<LayoutSelector options={layouts} value={type} onChange={handleChange} />}
+        >
+          <Graphin data={graphData} layout={layout}>
             <ContextMenu style={{ width: '80px' }}>
               <Menu options={options} onChange={HandleChange} bindType="node" />
             </ContextMenu>
