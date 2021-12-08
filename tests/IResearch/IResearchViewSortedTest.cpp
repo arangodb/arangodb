e87df2e08a8be108d3f50ce755c586b68a38cf8c
Tobias Gödderz <tobias@arangodb.com>
2021-12-08 10:36:28 +0100

    Remove global include directives (#15151)

--- tests/IResearch/IResearchViewSortedTest.cpp
+++ tests/IResearch/IResearchViewSortedTest.cpp
@@ -24,7 +24,7 @@
 
 #include "gtest/gtest.h"
 
-#include "3rdParty/iresearch/tests/tests_config.hpp"
+#include "../3rdParty/iresearch/tests/tests_config.hpp"
 #include "analysis/analyzers.hpp"
 #include "analysis/token_attributes.hpp"
 #include "utils/utf8_path.hpp"
