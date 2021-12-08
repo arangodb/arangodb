b66072f1f86e7a839e9d3c5708c4f859eecda91b
Andrei Lobov <andrei.lobov@arangodb.com>
2021-12-08 17:05:49 +0300

    wip

--- tests/IResearch/IResearchDocumentTest.cpp
+++ tests/IResearch/IResearchDocumentTest.cpp
@@ -3132,3 +3132,32 @@ TEST_F(IResearchDocumentTest, InvertedFieldIterator_traverse_complex_with_geo) {
 
   ASSERT_FALSE(it.valid());
 }
+
+TEST_F(IResearchDocumentTest, InvertedFieldIterator_invalid_array_expansion) {
+  auto const indexMetaJson = arangodb::velocypack::Parser::fromJson(
+      R"({"fields" : [{"name": "boost"},
+                      {"name": "keys[*]"}]})");
+
+  auto& sysDatabase = server.getFeature<arangodb::SystemDatabaseFeature>();
+  auto sysVocbase = sysDatabase.use();
+  arangodb::iresearch::InvertedIndexFieldMeta indexMeta;
+  std::string error;
+  ASSERT_TRUE(indexMeta.init(server.server(), indexMetaJson->slice(), false,
+                             error, sysVocbase.get()->name()));
+
+  std::vector<std::string> EMPTY;
+  arangodb::transaction::Methods trx(arangodb::transaction::StandaloneContext::Create(*sysVocbase),
+                                     EMPTY, EMPTY, EMPTY,
+                                     arangodb::transaction::Options());
+
+  auto json = arangodb::velocypack::Parser::fromJson(
+      R"({"keys": "not_an_array", "boost": 10)");
+
+    arangodb::iresearch::InvertedIndexFieldIterator it(trx, irs::string_ref::EMPTY,
+                                                     arangodb::IndexId(0));
+  it.reset(json->slice(), indexMeta);
+  size_t fieldIdx{};
+  ASSERT_TRUE(it.valid());
+  ASSERT_THROW(++it, arangodb::basics::Exception);
+  ASSERT_FALSE(it.valid());
+}
