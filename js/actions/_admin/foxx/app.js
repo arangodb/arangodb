d2c6de3b811260d750af1ec3b4d5d75d125338bd
Jan <jsteemann@users.noreply.github.com>
2021-12-10 15:18:46 +0100

    Update js/actions/_admin/foxx/app.js

--- js/actions/_admin/foxx/app.js
+++ js/actions/_admin/foxx/app.js
@@ -80,7 +80,6 @@ function resolveAppInfo (appInfo, refresh) {
   if (/^git:/i.test(appInfo)) {
     const splitted = appInfo.split(':');
     const baseUrl = process.env.FOXX_BASE_URL || 'https://github.com/';
-    FoxxManager.validateInstallUrl(baseUrl);
     return {source: `${baseUrl}${splitted[1]}/archive/${splitted[2] || 'master'}.zip`};
   }
   if (/^https?:/i.test(appInfo)) {
