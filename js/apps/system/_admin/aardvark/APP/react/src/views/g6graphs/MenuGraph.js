955974c4953bc62fded5b5252c17f6418a77ca7a
Andreas Jurdak <andreas@jurdak.at>
2021-12-10 11:50:14 +0100

    [FE-8] Feature: Add help modal

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/MenuGraph.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/MenuGraph.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState, useCallback } from 'react';
 import Graphin, { Utils, GraphinContext } from '@antv/graphin';
+import Modal, { ModalBody, ModalFooter, ModalHeader } from '../../components/modal/Modal';
 import { message, Card, Select } from 'antd';
 import { ContextMenu, MiniMap, Toolbar } from '@antv/graphin-components';
 import { useFetch } from './useFetch';
@@ -24,6 +25,43 @@ import {
 
 import { NodeList } from './components/node-list/node-list.component';
 import { EdgeList } from './components/edge-list/edge-list.component';
+
+const G6GraphHelpModal = () => {
+  const [show, setShow] = useState(false);
+
+  const showG6GraphHelp = (event) => {
+    event.preventDefault();
+    setShow(true);
+  };
+
+  return <>
+    <a href={'#g6'} onClick={showG6GraphHelp}>
+      <QuestionCircleOutlined />
+    </a>
+    <Modal show={show} setShow={setShow}>
+      <ModalHeader title={'Graph Viewer Help'}/>
+      <ModalBody>
+        <dl>
+          <dt>Graphs</dt>
+          <dd>
+            If you are new to graphs and displaying graph data... have a look at our <strong><a target={'_blank'} rel={'noreferrer'} href={'https://www.arangodb.com/docs/stable/graphs.html'}>graph documentation</a></strong>.
+          </dd>
+          <dt>Graph Course</dt>
+          <dd>
+            We also offer a course with the title <strong><a target={'_blank'} rel={'noreferrer'} href={'https://www.arangodb.com/learn/graphs/graph-course/'}>Get Started with Graphs in ArangoDB</a></strong> for free.
+          </dd>
+          <dt>Quick Start</dt>
+          <dd>
+            To start immediately, the explanationn of our <strong><a target={'_blank'} rel={'noreferrer'} href={'https://www.arangodb.com/docs/stable/graphs.html#example-graphs'}>example graphs</a></strong> might help very well.
+          </dd>
+        </dl>
+      </ModalBody>
+      <ModalFooter>
+        <button className="button-close" onClick={() => setShow(false)}>Close</button>
+      </ModalFooter>
+    </Modal>
+  </>;
+};
 
 const MenuGraph = () => {
   let [queryString, setQueryString] = useState("/_admin/aardvark/g6graph/routeplanner");
@@ -59,6 +97,7 @@ const MenuGraph = () => {
             );
           })}
         </Select>
+        <G6GraphHelpModal />
       </div>
     );
   };
