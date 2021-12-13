a9d4e4d58b3693d0265ebf1a365217b9c8a49a42
Olivier Bonnaure <olivier@solisoft.net>
2021-12-13 15:17:35 +0100

    Add Precision to ROUND AQL method

--- arangod/Aql/Functions.cpp
+++ arangod/Aql/Functions.cpp
@@ -6770,11 +6770,13 @@ AqlValue Functions::Matches(ExpressionContext* expressionContext, AstNode const&
 AqlValue Functions::Round(ExpressionContext*, AstNode const&,
                           VPackFunctionParameters const& parameters) {
   AqlValue const& value = extractFunctionParameterValue(parameters, 0);
+  AqlValue const& precision = extractFunctionParameterValue(parameters, 1);
 
   double input = value.toDouble();
+  double exp = std::pow(10, precision.toDouble() >= 0 ? precision.toDouble() : 0);
 
   // Rounds down for < x.4999 and up for > x.50000
-  return ::numberValue(std::floor(input + 0.5), true);
+  return ::numberValue(std::floor((input * exp) + 0.5) / exp, true);
 }
 
 /// @brief function ABS
