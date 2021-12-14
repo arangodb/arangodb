054690f286bd38586c3da874d3da7f5601ac5c9d
Wilfried Goesgens <willi@arangodb.com>
2021-12-14 18:31:32 +0100

    add integration for another long existing example

--- js/common/modules/@arangodb/examples/examples.js
+++ js/common/modules/@arangodb/examples/examples.js
@@ -27,6 +27,7 @@
 
 let db = require("internal").db;
 let examples = require("@arangodb/graph-examples/example-graph.js");
+let user_examples = require("@arangodb/examples/example-users.js");
 
 exports.Examples = {
   'traversalGraph': {
@@ -112,6 +113,41 @@ exports.Examples = {
       } catch (e) {}
       try {
         db._drop("relations");
+      } catch (e) {}
+    }
+  },
+  'observationsSampleDataset': {
+    createDS: function() {
+      db._create("observations");
+      db.observations.save([
+        { "time": "2021-05-25 07:00:00", "subject": "st113", "val": 10 },
+        { "time": "2021-05-25 07:15:00", "subject": "st113", "val": 9 },
+        { "time": "2021-05-25 07:30:00", "subject": "st113", "val": 25 },
+        { "time": "2021-05-25 07:45:00", "subject": "st113", "val": 20 },
+        { "time": "2021-05-25 07:00:00", "subject": "xh458", "val": 0 },
+        { "time": "2021-05-25 07:15:00", "subject": "xh458", "val": 10 },
+        { "time": "2021-05-25 07:30:00", "subject": "xh458", "val": 5 },
+        { "time": "2021-05-25 07:45:00", "subject": "xh458", "val": 30 },
+        { "time": "2021-05-25 08:00:00", "subject": "xh458", "val": 25 },
+      ]);
+    },
+    removeDS: function() {
+      try {
+        db._drop("observations");
+      } catch (e) {}
+    }
+  },
+  'usersDataset': {
+    createDS: function() {
+      let u = user_examples.createUsers('users');
+      let r = user_examples.createRegions('regions');
+      user_examples.createLocations('locations', u);
+    },
+    removeDS: function() {
+      try {
+        db._drop("users");
+        db._drop("regions");
+        db._drop("locations");
       } catch (e) {}
     }
   }
