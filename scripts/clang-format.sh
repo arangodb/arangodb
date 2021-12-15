94539509dc75a63846e2751b4b210217500947d9
Anthony Mahanna <43019056+aMahanna@users.noreply.github.com>
2021-12-15 15:29:32 -0500

    fix: arangodb/clang-format tag

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -16,7 +16,7 @@ fi
 
 diff="$community_diff $enterprise_diff"
 if ! [[ -z "${diff// }" ]]; then
-  docker run --rm -v "$adb_path":/usr/src/arangodb arangodb/clang-format:latest "$diff"
+  docker run --rm -v "$adb_path":/usr/src/arangodb arangodb/clang-format:1.0 "$diff"
 fi
 
 echo "(done)"
