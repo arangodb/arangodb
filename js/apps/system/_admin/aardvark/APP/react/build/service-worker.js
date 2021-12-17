8569e74d90c934f298b2e5b860ff25ee31f1f839
Kaveh Vahedipour <kaveh@arangodb.com>
2021-12-17 11:15:23 +0100

    pm updated license behaviour (#15261)
    * pm updated license behaviour
    
    * stringify the input
    
    * fixed ui
    
    * Update CHANGELOG
    
    * Rebuild UI
    
    * warnings fix
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a9ce0ec48dbce3604008a03316dc6717.js"
+  "precache-manifest.03972aba9635b048c378be01fd361e1d.js"
 );
 
 workbox.clientsClaim();
