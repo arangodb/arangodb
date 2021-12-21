609d322f84ad28c585841533de8f9c6a6da1a2c8
cpjulia <julia@arangodb.com>
2021-12-21 15:55:09 -0300

    Added backport for PRESUPP-439

--- arangosh/Import/ImportHelper.h
+++ arangosh/Import/ImportHelper.h
@@ -314,7 +314,7 @@ class ImportHelper {
   bool checkCreateCollection();
   bool truncateCollection();
 
-  void sendCsvBuffer();
+  void handleCsvBuffer(uint64_t bufferSizeThreshold);
   void sendJsonBuffer(char const* str, size_t len, bool isObject);
   SenderThread* findIdleSender();
   void waitForSenders();
