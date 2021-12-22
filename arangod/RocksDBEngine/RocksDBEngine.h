3b16d3c07f418445c0a9786ee5a5c0bad48db823
Max Neunhoeffer <max@arangodb.com>
2021-12-22 15:58:42 +0100

    Adjust RocksDB defaults.

--- arangod/RocksDBEngine/RocksDBEngine.h
+++ arangod/RocksDBEngine/RocksDBEngine.h
@@ -578,11 +578,11 @@ class RocksDBEngine final : public StorageEngine {
   /// @brief number of currently running compaction jobs
   size_t _runningCompactions;
 
-  // frequency for throttle in milliseconds
-  uint64_t _throttleFrequency = 60 * 1000; 
+  // frequency for throttle in milliseconds between iterations
+  uint64_t _throttleFrequency = 1000; 
 
   // number of historic data slots to keep around for throttle
-  uint64_t _throttleSlots = 63;
+  uint64_t _throttleSlots = 120;
   // adaptiveness factor for throttle
   // following is a heuristic value, determined by trial and error.
   // its job is slow down the rate of change in the current throttle.
