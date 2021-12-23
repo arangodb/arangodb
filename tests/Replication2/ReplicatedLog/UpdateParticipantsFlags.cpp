ca28b3eed84cfcfcdfefce1de01224e26035498c
Tobias Gödderz <tobias@arangodb.com>
2021-12-23 08:54:17 +0100

    Added multiple tests

--- tests/Replication2/ReplicatedLog/UpdateParticipantsFlags.cpp
+++ tests/Replication2/ReplicatedLog/UpdateParticipantsFlags.cpp
@@ -171,6 +171,52 @@ TEST_F(UpdateParticipantsFlagsTest, wc2_but_server_excluded) {
   }
 }
 
+TEST_F(UpdateParticipantsFlagsTest, wc2_but_server_excluded_leadership_is_established) {
+  {
+    auto [accepted, committed] = leader->getParticipantConfigGenerations();
+    EXPECT_EQ(accepted, 0);
+    EXPECT_EQ(committed, std::nullopt);
+  }
+
+  {
+    auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
+    auto newConfig = std::make_shared<ParticipantsConfig>();
+    newConfig->generation = 1;
+    // make follower1 excluded
+    newConfig->participants["follower1"] = replication2::ParticipantFlags{.excluded = true};
+    leader->updateParticipantsConfig(newConfig, oldConfig.generation, {}, {});
+  }
+
+  {
+    auto [accepted, committed] = leader->getParticipantConfigGenerations();
+    EXPECT_EQ(accepted, 1);
+    EXPECT_EQ(committed, std::nullopt);
+  }
+
+  auto idx2 = leader->insert(LogPayload::createFromString("entry #2"));
+  // let only commit Follower1
+  follower1->runAllAsyncAppendEntries();
+  // entry should not be committed because follower1 is excluded
+  // although wc is 2
+  EXPECT_NE(leader->getCommitIndex(), idx2);
+  {
+    auto [accepted, committed] = leader->getParticipantConfigGenerations();
+    EXPECT_EQ(accepted, 1);
+    EXPECT_EQ(committed, std::nullopt);
+  }
+
+  // now run both followers
+  runAllAsyncAppendEntries();
+  // the entry should now be committed
+  EXPECT_GE(leader->getCommitIndex(), idx2);
+
+  {
+    auto [accepted, committed] = leader->getParticipantConfigGenerations();
+    EXPECT_EQ(accepted, 1);
+    EXPECT_EQ(committed, 1);
+  }
+}
+
 TEST_F(UpdateParticipantsFlagsTest, multiple_updates_check) {
   // Here first update the config such that one follower is forced.
   // This config is never committed. We then change it back, such that
@@ -265,21 +311,108 @@ TEST_F(UpdateParticipantsFlagsTest, update_without_additional_entry) {
   }
 }
 
-TEST_F(UpdateParticipantsFlagsTest, add_and_remove_follower_like_moveshard) {
-  // Check the configuration is eventually committed even if the user
-  // does not write additional entries.
+TEST_F(UpdateParticipantsFlagsTest, wc2_add_new_follower) {
   leader->triggerAsyncReplication();
   runAllAsyncAppendEntries();
   ASSERT_TRUE(leader->isLeadershipEstablished());
 
-  // Add a new follower
-  {
-    // create follower3
-    std::shared_ptr<TestReplicatedLog> followerLog3 = makeReplicatedLog(logId);
-    std::shared_ptr<DelayedFollowerLog> follower3 =
-        followerLog1->becomeFollower("follower3", startTerm, "leader");
-    followers.emplace_back(follower3);
+  // create follower3
+  auto followerLog3 = makeReplicatedLog(logId);
+  auto follower3 = std::shared_ptr<DelayedFollowerLog>{};
+  follower3 = followerLog3->becomeFollower("follower3", startTerm, "leader");
+  followers.emplace_back(follower3);
 
+  {  // First add the new follower3
+    auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
+    auto newConfig = std::make_shared<ParticipantsConfig>();
+    newConfig->generation = 1;
+    newConfig->participants["follower3"] = replication2::ParticipantFlags{};
+
+    // note that this adds a new log entry
+    leader->updateParticipantsConfig(newConfig, oldConfig.generation,
+                                     {{"follower3", follower3}}, {});
+  }
+
+  {  // checks
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 0)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+
+    follower3->runAllAsyncAppendEntries();
+
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
+
+    auto fut = leader->waitFor(LogIndex{2});
+    ASSERT_TRUE(fut.isReady());
+    auto const& quorumData = *fut.get().quorum;
+    EXPECT_EQ(quorumData.index, LogIndex{2});
+    EXPECT_EQ(quorumData.term, startTerm);
+    // follower 3 should now be part of the quorum
+    EXPECT_NE(std::ranges::find(quorumData.quorum, "follower3"),
+              quorumData.quorum.end());
+
+    // settle remaining followers
+    runAllAsyncAppendEntries();
+  }
+}
+
+TEST_F(UpdateParticipantsFlagsTest, wc2_add_new_follower_before_leadership_is_established) {
+  // create follower3
+  auto followerLog3 = makeReplicatedLog(logId);
+  auto follower3 = std::shared_ptr<DelayedFollowerLog>{};
+  follower3 = followerLog3->becomeFollower("follower3", startTerm, "leader");
+  followers.emplace_back(follower3);
+
+  {  // First add the new follower3
+    auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
+    auto newConfig = std::make_shared<ParticipantsConfig>();
+    newConfig->generation = 1;
+    newConfig->participants["follower3"] = replication2::ParticipantFlags{};
+
+    // note that this adds a new log entry
+    leader->updateParticipantsConfig(newConfig, oldConfig.generation,
+                                     {{"follower3", follower3}}, {});
+  }
+
+  {  // checks
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, std::nullopt)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{0});
+
+    follower3->runAllAsyncAppendEntries();
+
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
+
+    auto fut = leader->waitFor(LogIndex{2});
+    ASSERT_TRUE(fut.isReady());
+    auto const& quorumData = *fut.get().quorum;
+    EXPECT_EQ(quorumData.index, LogIndex{2});
+    EXPECT_EQ(quorumData.term, startTerm);
+    // follower 3 should now be part of the quorum
+    EXPECT_NE(std::ranges::find(quorumData.quorum, "follower3"),
+              quorumData.quorum.end());
+
+    // settle remaining followers
+    runAllAsyncAppendEntries();
+  }
+}
+
+TEST_F(UpdateParticipantsFlagsTest, wc2_remove_exclude_flag) {
+  leader->triggerAsyncReplication();
+  runAllAsyncAppendEntries();
+  ASSERT_TRUE(leader->isLeadershipEstablished());
+
+  // create follower3
+  auto followerLog3 = makeReplicatedLog(logId);
+  auto follower3 = std::shared_ptr<DelayedFollowerLog>{};
+  follower3 = followerLog3->becomeFollower("follower3", startTerm, "leader");
+  followers.emplace_back(follower3);
+
+  {  // First add the new follower3, but excluded
     auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
     auto newConfig = std::make_shared<ParticipantsConfig>();
     newConfig->generation = 1;
@@ -287,32 +420,40 @@ TEST_F(UpdateParticipantsFlagsTest, add_and_remove_follower_like_moveshard) {
     newConfig->participants["follower3"] =
         replication2::ParticipantFlags{.excluded = true};
 
+    // note that this adds a new log entry
     leader->updateParticipantsConfig(newConfig, oldConfig.generation,
                                      {{"follower3", follower3}}, {});
   }
 
   {  // checks
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(1, 0)));
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 0)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+
+    follower3->runAllAsyncAppendEntries();
+
+    // must not be committed yet, as follower3 is excluded
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 0)));
     EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
 
     runAllAsyncAppendEntries();
 
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(1, 1)));
-    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
 
-    auto fut = leader->waitFor(LogIndex{1});
+    auto fut = leader->waitFor(LogIndex{2});
     ASSERT_TRUE(fut.isReady());
     auto const& quorumData = *fut.get().quorum;
-    EXPECT_EQ(quorumData.index, LogIndex{1});
+    EXPECT_EQ(quorumData.index, LogIndex{2});
     EXPECT_EQ(quorumData.term, startTerm);
-    // follower 3 may not be part of the quorum yet
+    // follower 3 must not be part of the quorum yet
     EXPECT_EQ(std::ranges::find(quorumData.quorum, "follower3"),
               quorumData.quorum.end());
   }
 
-  {  // set follower3.excluded = false
+  {  // set follower3.excluded = false; this is the central point of this test!
     auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
     auto newConfig = std::make_shared<ParticipantsConfig>();
     newConfig->generation = 2;
@@ -326,48 +467,129 @@ TEST_F(UpdateParticipantsFlagsTest, add_and_remove_follower_like_moveshard) {
 
   {  // checks
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(2, 1)));
-    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+              (std::pair<std::size_t, std::optional<std::size_t>>(2, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
 
-    runAllAsyncAppendEntries();
+    follower3->runAllAsyncAppendEntries();
 
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(2, 2)));
-    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+              (std::pair<std::size_t, std::optional<std::size_t>>(2, 2)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{3});
 
-    auto fut = leader->waitFor(LogIndex{1});
+    auto fut = leader->waitFor(LogIndex{3});
     ASSERT_TRUE(fut.isReady());
     auto const& quorumData = *fut.get().quorum;
-    EXPECT_EQ(quorumData.index, LogIndex{1});
+    EXPECT_EQ(quorumData.index, LogIndex{3});
     EXPECT_EQ(quorumData.term, startTerm);
+    // follower 3 should now be part of the quorum
+    EXPECT_NE(std::ranges::find(quorumData.quorum, "follower3"),
+              quorumData.quorum.end());
+
+    // settle remaining followers
+    runAllAsyncAppendEntries();
   }
+}
+
+TEST_F(UpdateParticipantsFlagsTest, wc2_remove_follower) {
+  leader->triggerAsyncReplication();
+  runAllAsyncAppendEntries();
+  ASSERT_TRUE(leader->isLeadershipEstablished());
 
   {  // remove follower1
     auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
     auto newConfig = std::make_shared<ParticipantsConfig>();
-    newConfig->generation = 3;
+    newConfig->generation = 1;
+
     // remove follower1
     leader->updateParticipantsConfig(newConfig, oldConfig.generation, {}, {"follower1"});
   }
 
   {  // checks
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(3, 2)));
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 0)));
     EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
 
+    // run follower1 first
+    follower1->runAllAsyncAppendEntries();
+
+    // nothing should have changed, as it was removed from the participants
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 0)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+
+    // now run everyone else
     runAllAsyncAppendEntries();
 
     EXPECT_EQ(leader->getParticipantConfigGenerations(),
-              (std::pair<std::size_t, std::size_t>(3, 3)));
-    EXPECT_EQ(leader->getCommitIndex(), LogIndex{1});
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
 
-    auto fut = leader->waitFor(LogIndex{1});
+    auto fut = leader->waitFor(LogIndex{2});
     ASSERT_TRUE(fut.isReady());
     auto const& quorumData = *fut.get().quorum;
-    EXPECT_EQ(quorumData.index, LogIndex{1});
+    EXPECT_EQ(quorumData.index, LogIndex{2});
     EXPECT_EQ(quorumData.term, startTerm);
-    auto quorum = quorumData.quorum;
-    std::ranges::sort(quorum);
-    EXPECT_EQ(quorum, (decltype(quorum){"follower1", "follower2", "follower3"}));
+    // follower 1 must not be part of the quorum any more
+    EXPECT_EQ(std::ranges::find(quorumData.quorum, "follower1"),
+              quorumData.quorum.end());
+
+    auto const status = leader->getStatus();
+    auto const leaderStatus = status.asLeaderStatus();
+    auto keys = std::vector<decltype(leaderStatus->follower)::key_type>{};
+    // std::ranges::keys does not work in clang/libc++ yet
+    std::ranges::transform(leaderStatus->follower, std::back_inserter(keys),
+                           [&](auto const& it) { return it.first; });
+    std::ranges::sort(keys);
+    EXPECT_EQ(keys, (decltype(keys){"follower2", "leader"}));
+  }
+}
+
+TEST_F(UpdateParticipantsFlagsTest, wc2_remove_follower_before_leadership_is_established) {
+  {  // remove follower1
+    auto oldConfig = leader->getStatus().asLeaderStatus()->activeParticipantsConfig;
+    auto newConfig = std::make_shared<ParticipantsConfig>();
+    newConfig->generation = 1;
+
+    // remove follower1
+    leader->updateParticipantsConfig(newConfig, oldConfig.generation, {}, {"follower1"});
+  }
+
+  {  // checks
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, std::nullopt)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{0});
+
+    // run follower1 first
+    follower1->runAllAsyncAppendEntries();
+
+    // nothing should have changed, as it was removed from the participants
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, std::nullopt)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{0});
+
+    // now run everyone else
+    runAllAsyncAppendEntries();
+
+    EXPECT_EQ(leader->getParticipantConfigGenerations(),
+              (std::pair<std::size_t, std::optional<std::size_t>>(1, 1)));
+    EXPECT_EQ(leader->getCommitIndex(), LogIndex{2});
+
+    auto fut = leader->waitFor(LogIndex{2});
+    ASSERT_TRUE(fut.isReady());
+    auto const& quorumData = *fut.get().quorum;
+    EXPECT_EQ(quorumData.index, LogIndex{2});
+    EXPECT_EQ(quorumData.term, startTerm);
+    // follower 1 must not be part of the quorum any more
+    EXPECT_EQ(std::ranges::find(quorumData.quorum, "follower1"),
+              quorumData.quorum.end());
+
+    auto const status = leader->getStatus();
+    auto const leaderStatus = status.asLeaderStatus();
+    auto keys = std::vector<decltype(leaderStatus->follower)::key_type>{};
+    // std::ranges::keys does not work in clang/libc++ yet
+    std::ranges::transform(leaderStatus->follower, std::back_inserter(keys),
+                           [&](auto const& it) { return it.first; });
+    std::ranges::sort(keys);
+    EXPECT_EQ(keys, (decltype(keys){"follower2", "leader"}));
   }
 }
