27c6a156478ef8644049988a85250b5db7ca704a
aMahanna <anthony.mahanna@gmail.com>
2021-12-27 18:43:14 -0500

    new clang-format hooks & scripts, one per OS

--- scripts/clang-format-windows.sh
+++ scripts/clang-format-windows.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# pre-requisite: MacOS-based system
 # pre-requisite: Docker
 
-[[ -z "$1" ]] && adb_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )" || adb_path=$(echo "$1" | sed 's:/*$::') # Get root directory of repo (can be passed as argument)
-[[ -z "$2" ]] && ent_dir="enterprise" || ent_dir=$(echo "$2" | sed 's:/*$::') # Get enterprise directory (can be passed as argument)
+# Get root directory of repo (can be passed as argument)
+[[ -z "$1" ]] && adb_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )" || adb_path=$(echo "$1" | sed 's:/*$::')
+ent_dir="enterprise"
 
 echo "ArangoDB directory: $adb_path"
 echo "Enterprise directory: $adb_path/$ent_dir"
@@ -10,7 +12,7 @@ echo "Enterprise directory: $adb_path/$ent_dir"
 cd "$adb_path"
 community_diff=$(git diff HEAD --diff-filter=ACMR --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h')
 
-if [ -d "$adb_path/$ent_dir" ] # assume enterprise directory is within arangodb directory
+if [ -d "$adb_path/$ent_dir" ]
 then
    cd "$adb_path/$ent_dir"
    enterprise_diff=$(git diff HEAD --diff-filter=ACMR --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h' | sed "s,^,$ent_dir/,")
