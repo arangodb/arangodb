17ccaf5f375879f5bdb76e6e7cd1096fb2339e79
Andrei Lobov <andrei.lobov@arangodb.com>
2021-12-30 18:23:29 +0300

    another format appliance

--- arangod/Aql/OptimizerRules.cpp
+++ arangod/Aql/OptimizerRules.cpp
@@ -7384,8 +7384,7 @@ void arangodb::aql::sortLimitRule(Optimizer* opt,
             auto* sortParent = sortNode->getFirstParent();
             TRI_ASSERT(sortParent);
             if (sortParent != auxLimitNode) {
-              sortParent->replaceDependency(sortNode,
-                                          auxLimitNode);
+              sortParent->replaceDependency(sortNode, auxLimitNode);
               sortNode->addParent(auxLimitNode);
             }
           }
