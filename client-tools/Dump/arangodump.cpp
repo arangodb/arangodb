084d6a014051e77f6e0bf55c43708a5c40d67ffc
Wilfried Goesgens <willi@arangodb.com>
2022-01-03 10:13:42 +0100

    fix undefined behaviour by initializing the exit code variable

--- client-tools/Dump/arangodump.cpp
+++ client-tools/Dump/arangodump.cpp
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
             argv[0], "Usage: arangodump [<options>]",
             "For more information use:", BIN_DIRECTORY));
     ApplicationServer server(options, BIN_DIRECTORY);
-    int ret;
+    int ret = EXIT_SUCCESS;
 
     server.addFeature<CommunicationFeaturePhase>();
     server.addFeature<BasicFeaturePhaseClient>();
