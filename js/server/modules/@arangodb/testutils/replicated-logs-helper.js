bb7221cd632432af74c16e9ca17fdf2d0a80d2d0
Tobias Gödderz <tobias@arangodb.com>
2022-01-05 18:17:40 +0100

    Fixed plan layout in test suite replication2_server

--- js/server/modules/@arangodb/testutils/replicated-logs-helper.js
+++ js/server/modules/@arangodb/testutils/replicated-logs-helper.js
@@ -23,115 +23,123 @@
 ////////////////////////////////////////////////////////////////////////////////
 const {wait} = require("internal");
 const _ = require("lodash");
+const jsunity = require("../../../../common/modules/jsunity");
+const {assertTrue} = jsunity.jsUnity.assertions;
 
 const waitFor = function (checkFn, maxTries = 100) {
-    let count = 0;
-    let result = null;
-    while (count < maxTries) {
-        result = checkFn();
-        if (result === true) {
-            return result;
-        }
-        if (!(result instanceof Error)) {
-            throw Error("expected error");
-        }
-        console.log(result);
-        count += 1;
-        wait(0.5);
+  let count = 0;
+  let result = null;
+  while (count < maxTries) {
+    result = checkFn();
+    if (result === true) {
+      return result;
     }
-    throw result;
+    if (!(result instanceof Error)) {
+      throw Error("expected error");
+    }
+    console.log(result);
+    count += 1;
+    wait(0.5);
+  }
+  throw result;
 };
 
 const readAgencyValueAt = function (key) {
-    const response = global.ArangoAgency.get(key);
-    const path = ["arango", ...key.split('/')];
-    let result = response;
-    for (const p of path) {
-        if (result === undefined) {
-            return undefined;
-        }
-        result = result[p];
+  const response = global.ArangoAgency.get(key);
+  const path = ["arango", ...key.split('/')];
+  let result = response;
+  for (const p of path) {
+    if (result === undefined) {
+      return undefined;
     }
-    return result;
+    result = result[p];
+  }
+  return result;
 };
 
 const getServerRebootId = function (serverId) {
-    return readAgencyValueAt(`Current/ServersKnown/${serverId}/rebootId`);
+  return readAgencyValueAt(`Current/ServersKnown/${serverId}/rebootId`);
 };
 
 
 const getParticipantsObjectForServers = function (servers) {
-    return _.reduce(servers, (a, v) => {
-        a[v] = {};
-        return a;
-    }, {});
+  return _.reduce(servers, (a, v) => {
+    a[v] = {};
+    return a;
+  }, {});
+};
+
+const createParticipantsConfig = function (generation, servers) {
+  return {
+    generation,
+    participants: getParticipantsObjectForServers(servers),
+  };
 };
 
 const createTermSpecification = function (term, servers, config, leader) {
-    let spec = {term, config};
-    //assertTrue(config.replicationFactor === servers.length);
-    spec.participants = getParticipantsObjectForServers(servers);
-    if (leader !== undefined) {
-        if(!_.includes(servers, leader)) {
-            throw Error("leader is not part of the participants");
-        }
-        spec.leader = {serverId: leader, rebootId: getServerRebootId(leader)};
+  let spec = {term, config};
+  if (leader !== undefined) {
+    if (!_.includes(servers, leader)) {
+      throw Error("leader is not part of the participants");
     }
-    return spec;
+    spec.leader = {serverId: leader, rebootId: getServerRebootId(leader)};
+  }
+  return spec;
 };
 
 
 const dbservers = (function () {
-    return global.ArangoClusterInfo.getDBServers().map((x) => x.serverId);
+  return global.ArangoClusterInfo.getDBServers().map((x) => x.serverId);
 }());
 
 
 const readReplicatedLogAgency = function (database, logId) {
-    let target = readAgencyValueAt(`Target/ReplicatedLogs/${database}/${logId}`);
-    let plan = readAgencyValueAt(`Plan/ReplicatedLogs/${database}/${logId}`);
-    let current = readAgencyValueAt(`Current/ReplicatedLogs/${database}/${logId}`);
-    return {target, plan, current};
+  let target = readAgencyValueAt(`Target/ReplicatedLogs/${database}/${logId}`);
+  let plan = readAgencyValueAt(`Plan/ReplicatedLogs/${database}/${logId}`);
+  let current = readAgencyValueAt(`Current/ReplicatedLogs/${database}/${logId}`);
+  return {target, plan, current};
 };
 
 const replicatedLogSetPlanParticipantsFlags = function (database, logId, participants) {
-    global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}/participantsConfig`, participants);
-    global.ArangoAgency.increaseVersion(`Plan/Version`);
+  global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}/participantsConfig`, participants);
+  global.ArangoAgency.increaseVersion(`Plan/Version`);
 };
 
 const replicatedLogUpdatePlanParticipantsFlags = function (database, logId, participants) {
-    let oldValue = readAgencyValueAt(`Plan/ReplicatedLogs/${database}/${logId}/participantsConfig`);
-    oldValue = oldValue || {generation: 0, participants: {}};
-    for (const [p, v] of Object.entries(participants)) {
-        if (v === null) {
-            delete oldValue.participants[p];
-        } else {
-            oldValue.participants[p] = v;
-        }
+  let oldValue = readAgencyValueAt(`Plan/ReplicatedLogs/${database}/${logId}/participantsConfig`);
+  oldValue = oldValue || {generation: 0, participants: {}};
+  for (const [p, v] of Object.entries(participants)) {
+    if (v === null) {
+      delete oldValue.participants[p];
+    } else {
+      oldValue.participants[p] = v;
     }
-    let gen = oldValue.generation += 1;
-    replicatedLogSetPlanParticipantsFlags(database, logId, oldValue);
-    return gen;
+  }
+  let gen = oldValue.generation += 1;
+  replicatedLogSetPlanParticipantsFlags(database, logId, oldValue);
+  return gen;
 };
 
 const replicatedLogSetPlanTerm = function (database, logId, term) {
-    global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}/currentTerm`, term);
-    global.ArangoAgency.increaseVersion(`Plan/Version`);
+  global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}/currentTerm`, term);
+  global.ArangoAgency.increaseVersion(`Plan/Version`);
 };
 
 const replicatedLogSetPlan = function (database, logId, spec) {
-    global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}`, spec);
-    global.ArangoAgency.increaseVersion(`Plan/Version`);
+  assertTrue(spec.targetConfig.replicationFactor <= Object.keys(spec.participantsConfig.participants).length);
+  global.ArangoAgency.set(`Plan/ReplicatedLogs/${database}/${logId}`, spec);
+  global.ArangoAgency.increaseVersion(`Plan/Version`);
 };
 
 const replicatedLogDeletePlan = function (database, logId) {
-    global.ArangoAgency.remove(`Plan/ReplicatedLogs/${database}/${logId}`);
-    global.ArangoAgency.increaseVersion(`Plan/Version`);
+  global.ArangoAgency.remove(`Plan/ReplicatedLogs/${database}/${logId}`);
+  global.ArangoAgency.increaseVersion(`Plan/Version`);
 };
-
 
 
 exports.waitFor = waitFor;
 exports.readAgencyValueAt = readAgencyValueAt;
+exports.createParticipantsConfig = createParticipantsConfig;
 exports.createTermSpecification = createTermSpecification;
 exports.dbservers = dbservers;
 exports.readReplicatedLogAgency = readReplicatedLogAgency;
