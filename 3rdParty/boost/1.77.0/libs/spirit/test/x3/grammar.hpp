4c2a60df8053d7bc5eeea22e4fd697c109b7c847
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-01-14 07:54:18 +0000

    Upgrade boost to 1.78.0

--- 3rdParty/boost/1.77.0/libs/spirit/test/x3/grammar.hpp
+++
@@ -1,7 +0,0 @@
-#include <boost/spirit/home/x3.hpp>
-
-namespace x3 = boost::spirit::x3;
-
-x3::rule<struct grammar_r, int> const grammar;
-using grammar_type = decltype(grammar);
-BOOST_SPIRIT_DECLARE(grammar_type)
