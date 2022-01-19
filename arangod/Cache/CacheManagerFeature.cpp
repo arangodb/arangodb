d9444a32514bc054f7266f8719831824d3cd9831
mpoeter <manuel@arangodb.com>
2022-01-19 12:36:31 +0100

    Fix potential thread leak.

--- arangod/Cache/CacheManagerFeature.cpp
+++ arangod/Cache/CacheManagerFeature.cpp
@@ -128,13 +128,14 @@ void CacheManagerFeature::start() {
 
 void CacheManagerFeature::beginShutdown() {
   if (_manager != nullptr) {
-    _manager->beginShutdown();
     _rebalancer->beginShutdown();
+    _manager->beginShutdown();
   }
 }
 
 void CacheManagerFeature::stop() {
   if (_manager != nullptr) {
+    _rebalancer->shutdown();
     _manager->shutdown();
   }
 }
