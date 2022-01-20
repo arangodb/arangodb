f94db12e6bb03ce1e5a5ec66721222f834075576
MBkkt <valery.mironow@gmail.com>
2022-01-20 15:30:26 +0300

    New async value

--- arangod/IResearch/Containers.h
+++ arangod/IResearch/Containers.h
@@ -24,8 +24,10 @@
 
 #pragma once
 
+#include <atomic>
+#include <condition_variable>
 #include <memory>
-#include <shared_mutex>
+#include <mutex>
 #include <unordered_map>
 
 #include "Basics/Common.h"
@@ -49,70 +51,96 @@ struct typelist;
 namespace arangodb {
 namespace iresearch {
 
-////////////////////////////////////////////////////////////////////////////////
-/// @brief a read-mutex for a resource
-////////////////////////////////////////////////////////////////////////////////
 template<typename T>
 class AsyncValue {
  public:
   class Value {
    public:
-    Value() = default;
-    Value(Value&&) = default;
-    Value& operator=(Value&&) = default;
+    constexpr Value() noexcept : _self{nullptr} {}
+    constexpr Value(Value&& other) noexcept
+        : _self{std::exchange(other._self, nullptr)} {}
+    constexpr Value& operator=(Value&& other) noexcept {
+      std::swap(_self, other._self);
+      return *this;
+    }
 
-    T* get() noexcept { return _resource; }
-    const T* get() const noexcept { return _resource; }
+    T* get() noexcept { return _self ? _self->_resource : nullptr; }
+    T const* get() const noexcept { return _self ? _self->_resource : nullptr; }
     T* operator->() noexcept { return get(); }
-    const T* operator->() const noexcept { return get(); }
+    T const* operator->() const noexcept { return get(); }
 
-    explicit operator bool() const noexcept { return nullptr != get(); }
+    explicit operator bool() const noexcept { return get() != nullptr; }
 
-    bool ownsLock() const noexcept { return _lock.owns_lock(); }
+    ~Value() {
+      if (_self) {
+        _self->destroy();
+      }
+    }
 
    private:
-    friend class AsyncValue<T>;
+    friend class AsyncValue;
+    constexpr Value(AsyncValue& self) noexcept : _self{&self} {}
 
-    Value(std::shared_lock<std::shared_mutex>&& lock, T* resource)
-        : _lock{std::move(lock)}, _resource{resource} {}
-
-    std::shared_lock<std::shared_mutex> _lock;
-    T* _resource{};
+    AsyncValue* _self;
   };
 
-  explicit AsyncValue(T* resource) noexcept : _resource{resource} {}
+  explicit AsyncValue(T* resource) noexcept
+      : _resource{resource}, _count{[&] {
+          if (!resource) {
+            return kReset | kDestroy;
+          } else {
+            return kRef;
+          }
+        }()} {}
 
   ~AsyncValue() { reset(); }
 
-  auto lock() const {
-    auto lock = irs::make_shared_lock(_mutex);
-    return Value{std::move(lock), _resource};
+  [[nodiscard]] bool empty() const noexcept {
+    return _count.load(std::memory_order_acquire) & kReset;
   }
 
-  auto try_lock() const {
-    auto lock = irs::make_shared_lock(_mutex, std::try_to_lock);
-
-    if (lock.owns_lock()) {
-      return Value{std::move(lock), _resource};
+  [[nodiscard]] Value lock() noexcept {
+    if (empty()) {
+      return {};
     }
-
-    return Value{};
+    if (_count.fetch_add(kRef, std::memory_order_acquire) & kDestroy) {
+      return {};
+    }
+    return {*this};
   }
 
-  // will block until a write lock can be acquired on the _mutex
   void reset() {
-    auto lock = irs::make_unique_lock(_mutex);
-    _resource = nullptr;
-  }
-
-  bool empty() const {
-    auto lock = irs::make_shared_lock(_mutex);
-    return nullptr == _resource;
+    auto count = _count.fetch_or(kReset, std::memory_order_acq_rel);
+    if (!(count & kReset)) {
+      destroy();
+    }
+    std::unique_lock lock{_m};
+    while (_resource != nullptr) {
+      _cv.wait(lock);
+    }
   }
 
  private:
-  mutable std::shared_mutex _mutex;  // read-lock to prevent '_resource' reset()
+  static constexpr uint64_t kReset = 1;
+  static constexpr uint64_t kDestroy = 2;
+  static constexpr uint64_t kRef = 4;
+
+  void destroy() {
+    auto count = _count.fetch_sub(kRef, std::memory_order_acq_rel) - kRef;
+    if (count == kReset &&
+        _count.compare_exchange_strong(count, kReset | kDestroy,
+                                       std::memory_order_release,
+                                       std::memory_order_relaxed)) {
+      std::lock_guard lock{_m};
+      _resource = nullptr;
+      _cv.notify_all();  // should be under lock
+    }
+  }
+
   T* _resource;
+  mutable std::atomic_uint64_t _count;
+  std::mutex _m;
+  std::condition_variable _cv;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
