a4e197a3d02cbe0f00e97a5ad332f9f7228c375f
Jan <jsteemann@users.noreply.github.com>
2022-01-21 09:26:34 +0100

    Fixed issue #15501 (#15565)

--- arangod/GeneralServer/RequestLane.h
+++ arangod/GeneralServer/RequestLane.h
@@ -72,6 +72,19 @@ enum class RequestLane {
   // from being worked on, having the same effect.
   CLUSTER_AQL_INTERNAL_COORDINATOR,
 
+  // Shutdown request for AQL queries, i.e. /_api/aql/finish/<id> on the
+  // DBserver. This calls have slightly higher priority than normal AQL requests
+  // because the query shutdown can release resources and unblock other
+  // operations.
+  CLUSTER_AQL_SHUTDOWN,
+
+  // DOCUMENT() requests inside cluster AQL queries, executed on DBservers.
+  // These requests will read a locally available document and do not depend
+  // on other requests. They can always make progress. They will be initiated
+  // on coordinators and handling them quickly may unblock the coordinator
+  // part of an AQL query.
+  CLUSTER_AQL_DOCUMENT,
+
   // For requests from the from the Coordinator to the
   // DBserver using V8.
   CLUSTER_V8,
@@ -120,7 +133,7 @@ enum class RequestLane {
 
 enum class RequestPriority { MAINTENANCE = 0, HIGH = 1, MED = 2, LOW = 3 };
 
-inline RequestPriority PriorityRequestLane(RequestLane lane) {
+constexpr inline RequestPriority PriorityRequestLane(RequestLane lane) {
   switch (lane) {
     case RequestLane::CLIENT_FAST:
       return RequestPriority::MAINTENANCE;
@@ -139,6 +152,10 @@ inline RequestPriority PriorityRequestLane(RequestLane lane) {
     case RequestLane::CLUSTER_AQL:
       return RequestPriority::LOW;
     case RequestLane::CLUSTER_AQL_INTERNAL_COORDINATOR:
+      return RequestPriority::MED;
+    case RequestLane::CLUSTER_AQL_SHUTDOWN:
+      return RequestPriority::MED;
+    case RequestLane::CLUSTER_AQL_DOCUMENT:
       return RequestPriority::MED;
     case RequestLane::CLUSTER_V8:
       return RequestPriority::LOW;
@@ -160,9 +177,11 @@ inline RequestPriority PriorityRequestLane(RequestLane lane) {
       return RequestPriority::HIGH;
     case RequestLane::CONTINUATION:
       return RequestPriority::MED;
-    case RequestLane::UNDEFINED: {
-      TRI_ASSERT(false);
-    }
+    case RequestLane::UNDEFINED:
+      // assume low priority for UNDEFINED. we should never get
+      // here under normal circumstances. if we do, returning the
+      // default shouldn't do any harm.
+      return RequestPriority::LOW;
   }
   return RequestPriority::LOW;
 }
