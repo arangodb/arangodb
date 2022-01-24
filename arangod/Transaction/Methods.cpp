54604a06e3faaf40a3e52156e3178f7ccf9dd10d
Kaveh Vahedipour <kaveh@arangodb.com>
2022-01-24 15:48:49 +0100

    fix race

--- arangod/Transaction/Methods.cpp
+++ arangod/Transaction/Methods.cpp
@@ -988,6 +988,7 @@ Future<OperationResult> transaction::Methods::insertLocal(std::string const& cna
         case FollowerInfo::WriteState::FORBIDDEN:
           // We cannot fulfill minimum replication Factor. Reject write.
           return OperationResult(TRI_ERROR_ARANGO_READ_ONLY, options);
+        case FollowerInfo::WriteState::UNAVAILABLE:
         case FollowerInfo::WriteState::STARTUP:
           return OperationResult(TRI_ERROR_CLUSTER_BACKEND_UNAVAILABLE, options);
         default:
