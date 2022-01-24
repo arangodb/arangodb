427580ef1264f1839216218ac8912f0f34297066
Wilfried Goesgens <willi@arangodb.com>
2022-01-24 15:29:10 +0100

    c

--- js/client/modules/@arangodb/testsuites/rta_makedata.js
+++ js/client/modules/@arangodb/testsuites/rta_makedata.js
@@ -5,7 +5,7 @@
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
 // /
-// / Copyright 2016 ArangoDB GmbH, Cologne, Germany
+// / Copyright 2022 ArangoDB GmbH, Cologne, Germany
 // / Copyright 2014 triagens GmbH, Cologne, Germany
 // /
 // / Licensed under the Apache License, Version 2.0 (the "License")
