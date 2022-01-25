arangodb/arangodb

commit e2bcc26f62fce3b20a8e7c4a2772959ed3e0ebf1

[BTS-714] Fix various TSan issues.  (#15515)
* Fix race in Cursor.

* Fix race when closing connections during shutdown.

* Fix some data races during shutdown.

* Fix race in query shutdown.

* Refactor QueryProfile/List and store weak_ptr instead of raw ptrs.

* Fix a race during shutdown.

* Fix race on ClusterCollectionCreationInfo::state

* Add some suppressions.

* Fix server shutdown.

* Clarify some memory orderings.

* Remove suppressions.

Co-authored-by: Vadim Kondratyev <vadim@arangodb.com>

arangod/Aql/QueryList.cpp
@@ -116,11 +116,9 @@ QueryList::QueryList(QueryRegistryFeature& feature)
 }
 
 /// @brief insert a query
-bool QueryList::insert(Query* query) {
-  TRI_ASSERT(query != nullptr);
-
+bool QueryList::insert(Query& query) {
   // not enabled or no query string
-  if (!enabled() || query->queryString().empty()) {
+  if (!enabled() || query.queryString().empty()) {
     return false;
   }
 
@@ -132,7 +130,8 @@ bool QueryList::insert(Query* query) {
     }
 
     // return whether or not insertion worked
-    bool inserted = _current.insert({query->id(), query}).second;
+    bool inserted =
+        _current.insert({query.id(), query.shared_from_this()}).second;
     _queryRegistryFeature.trackQueryStart();
     return inserted;
   } catch (...) {
@@ -141,9 +140,7 @@ bool QueryList::insert(Query* query) {
 }
 
 /// @brief remove a query
-void QueryList::remove(Query* query) {
-  TRI_ASSERT(query != nullptr);
-
+void QueryList::remove(Query& query) {
   // we're intentionally not checking _enabled here...
 
   // note: there is the possibility that a query got inserted when the
@@ -151,7 +148,7 @@ void QueryList::remove(Query* query) {
   // is turned off. in this case, removal is forced so the contents of
   // the list are correct
 
-  TRI_ASSERT(!query->queryString().empty());
+  TRI_ASSERT(!query.queryString().empty());
 
   {
     // acquire the query list's write lock only for a short amount of
@@ -160,14 +157,14 @@ void QueryList::remove(Query* query) {
     // not required
     WRITE_LOCKER(writeLocker, _lock);
 
-    if (_current.erase(query->id()) == 0) {
+    if (_current.erase(query.id()) == 0) {
       // not found
       return;
     }
   }
 
   // elapsed time since query start
-  double const elapsed = elapsedSince(query->startTime());
+  double const elapsed = elapsedSince(query.startTime());
 
   _queryRegistryFeature.trackQueryEnd(elapsed);
 
@@ -175,7 +172,7 @@ void QueryList::remove(Query* query) {
     return;
   }
 
-  bool const isStreaming = query->queryOptions().stream;
+  bool const isStreaming = query.queryOptions().stream;
   double threshold =
       isStreaming ? _slowStreamingQueryThreshold.load(std::memory_order_relaxed)
                   : _slowQueryThreshold.load(std::memory_order_relaxed);
@@ -198,11 +195,11 @@ void QueryList::remove(Query* query) {
       size_t const maxQueryStringLength =
           _maxQueryStringLength.load(std::memory_order_relaxed);
 
-      std::string q = extractQueryString(*query, maxQueryStringLength);
+      std::string q = extractQueryString(query, maxQueryStringLength);
       std::string bindParameters;
       if (_trackBindVars) {
         // also log bind variables
-        auto bp = query->bindParameters();
+        auto bp = query.bindParameters();
         if (bp != nullptr && !bp->slice().isNone()) {
           bindParameters.append(", bind vars: ");
           bp->slice().toJson(bindParameters);
@@ -215,7 +212,7 @@ void QueryList::remove(Query* query) {
 
       std::string dataSources;
       if (_trackDataSources) {
-        auto const d = query->collectionNames();
+        auto const d = query.collectionNames();
         if (!d.empty()) {
           size_t i = 0;
           dataSources = ", data sources: [";
@@ -232,28 +229,28 @@ void QueryList::remove(Query* query) {
         }
       }
 
-      auto resultCode = query->resultCode();
+      auto resultCode = query.resultCode();
 
       LOG_TOPIC("8bcee", WARN, Logger::QUERIES)
           << "slow " << (isStreaming ? "streaming " : "") << "query: '" << q
           << "'" << bindParameters << dataSources
-          << ", database: " << query->vocbase().name()
-          << ", user: " << query->user() << ", id: " << query->id()
-          << ", token: QRY" << query->id() << ", exit code: " << resultCode
+          << ", database: " << query.vocbase().name()
+          << ", user: " << query.user() << ", id: " << query.id()
+          << ", token: QRY" << query.id() << ", exit code: " << resultCode
           << ", took: " << Logger::FIXED(elapsed) << " s";
 
       // acquire the query list lock again
       WRITE_LOCKER(writeLocker, _lock);
 
       _slow.emplace_back(
-          query->id(), query->vocbase().name(), query->user(), std::move(q),
-          _trackBindVars ? query->bindParameters() : nullptr,
-          _trackDataSources ? query->collectionNames()
+          query.id(), query.vocbase().name(), query.user(), std::move(q),
+          _trackBindVars ? query.bindParameters() : nullptr,
+          _trackDataSources ? query.collectionNames()
                             : std::vector<std::string>(),
           now - elapsed, /* start timestamp */
           elapsed /* run time */,
-          query->killed() ? QueryExecutionState::ValueType::KILLED
-                          : QueryExecutionState::ValueType::FINISHED,
+          query.killed() ? QueryExecutionState::ValueType::KILLED
+                         : QueryExecutionState::ValueType::FINISHED,
           isStreaming, resultCode);
 
       // _slow is an std::list, but since c++11 the size() method of all
@@ -280,8 +277,11 @@ Result QueryList::kill(TRI_voc_tick_t id) {
     return {TRI_ERROR_QUERY_NOT_FOUND, "query ID not found in query list"};
   }
 
-  Query* query = (*it).second;
-  killQuery(*query, maxLength, false);
+  auto query = it->second.lock();
+  if (query) {
+    // if we cannot lock the query, it is already beeing destroyed
+    killQuery(*query, maxLength, false);
+  }
 
   return Result();
 }
@@ -297,13 +297,13 @@ uint64_t QueryList::kill(std::function<bool(Query&)> const& filter,
   READ_LOCKER(readLocker, _lock);
 
   for (auto& it : _current) {
-    Query& query = *(it.second);
+    auto query = it.second.lock();
 
-    if (!filter(query)) {
+    if (query == nullptr || !filter(*query)) {
       continue;
     }
 
-    killQuery(query, maxLength, silent);
+    killQuery(*query, maxLength, silent);
     ++killed;
   }
 
@@ -328,9 +328,10 @@ std::vector<QueryEntryCopy> QueryList::listCurrent() {
     result.reserve(_current.size());
 
     for (auto const& it : _current) {
-      Query const* query = it.second;
-
-      TRI_ASSERT(query != nullptr);
+      auto query = it.second.lock();
+      if (query == nullptr) {
+        continue;
+      }
 
       // elapsed time since query start
       double const elapsed = elapsedSince(query->startTime());

arangod/Aql/QueryList.h
@@ -25,6 +25,7 @@
 
 #include <cmath>
 #include <list>
+#include <memory>
 #include <optional>
 
 #include "Aql/QueryExecutionState.h"
@@ -199,10 +200,10 @@ class QueryList {
   }
 
   /// @brief enter a query
-  bool insert(Query*);
+  bool insert(Query&);
 
   /// @brief remove a query
-  void remove(Query*);
+  void remove(Query&);
 
   /// @brief kills a query
   Result kill(TRI_voc_tick_t id);
@@ -241,7 +242,7 @@ class QueryList {
   arangodb::basics::ReadWriteLock _lock;
 
   /// @brief list of current queries, protected by _lock
-  std::unordered_map<TRI_voc_tick_t, Query*> _current;
+  std::unordered_map<TRI_voc_tick_t, std::weak_ptr<Query>> _current;
 
   /// @brief list of slow queries, protected by _lock
   std::list<QueryEntryCopy> _slow;

arangod/Aql/Query.cpp
@@ -1135,7 +1135,7 @@ void Query::init(bool createProfile) {
   TRI_ASSERT(_queryProfile == nullptr);
   // adds query to QueryList which is needed for /_api/query/current
   if (createProfile && !ServerState::instance()->isDBServer()) {
-    _queryProfile = std::make_unique<QueryProfile>(this);
+    _queryProfile = std::make_unique<QueryProfile>(*this);
   }
   enterState(QueryExecutionState::ValueType::INITIALIZATION);
 
@@ -1433,9 +1433,6 @@ futures::Future<Result> finishDBServerParts(Query& query, ErrorCode errorCode) {
 }  // namespace
 
 aql::ExecutionState Query::cleanupTrxAndEngines(ErrorCode errorCode) {
-  ScopeGuard endQueryGuard(
-      [this]() noexcept { unregisterQueryInTransactionState(); });
-
   ShutdownState exp = _shutdownState.load(std::memory_order_relaxed);
   if (exp == ShutdownState::Done) {
     return ExecutionState::DONE;
@@ -1448,6 +1445,9 @@ aql::ExecutionState Query::cleanupTrxAndEngines(ErrorCode errorCode) {
                                               std::memory_order_relaxed)) {
     return ExecutionState::WAITING;  // someone else got here
   }
+
+  ScopeGuard endQueryGuard(
+      [this]() noexcept { unregisterQueryInTransactionState(); });
 
   enterState(QueryExecutionState::ValueType::FINALIZATION);
 
@@ -1630,7 +1630,9 @@ void Query::debugKillQuery() {
     isInRegistry = registry->queryIsRegistered(vocbase().name(), _queryId);
   }
   TRI_ASSERT(isInList || isStreaming || isInRegistry ||
-             _execState == QueryExecutionState::ValueType::FINALIZATION);
+             _execState == QueryExecutionState::ValueType::FINALIZATION)
+      << "_execState " << (int)_execState.load() << " queryList->enabled() "
+      << queryList->enabled();
   kill();
 #endif
 }

arangod/Aql/QueryProfile.cpp
@@ -37,12 +37,11 @@ using namespace arangodb;
 using namespace arangodb::aql;
 
 /// @brief create a profile
-QueryProfile::QueryProfile(Query* query)
-    : _query(query), _lastStamp(query->startTime()), _tracked(false) {
+QueryProfile::QueryProfile(Query& query)
+    : _query(query), _lastStamp(query.startTime()), _tracked(false) {
   for (auto& it : _timers) {
     it = 0.0;  // reset timers
   }
-  TRI_ASSERT(_query != nullptr);
 }
 
 /// @brief destroy a profile
@@ -50,23 +49,20 @@ QueryProfile::~QueryProfile() { unregisterFromQueryList(); }
 
 void QueryProfile::registerInQueryList() {
   TRI_ASSERT(!_tracked);
-  TRI_ASSERT(_query != nullptr);
-  auto queryList = _query->vocbase().queryList();
+  auto queryList = _query.vocbase().queryList();
   if (queryList) {
     _tracked = queryList->insert(_query);
 
     TRI_IF_FAILURE("QueryProfile::directKillAfterQueryGotRegistered") {
-      _query->debugKillQuery();
+      _query.debugKillQuery();
     }
   }
 }
 
 void QueryProfile::unregisterFromQueryList() noexcept {
-  TRI_ASSERT(_query != nullptr);
-
   // only remove from list when the query was inserted into it...
   if (_tracked) {
-    auto queryList = _query->vocbase().queryList();
+    auto queryList = _query.vocbase().queryList();
 
     try {
       queryList->remove(_query);

arangod/Aql/QueryProfile.h
@@ -41,7 +41,7 @@ struct QueryProfile {
   QueryProfile(QueryProfile const&) = delete;
   QueryProfile& operator=(QueryProfile const&) = delete;
 
-  explicit QueryProfile(Query* query);
+  explicit QueryProfile(Query& query);
 
   ~QueryProfile();
 
@@ -65,7 +65,7 @@ struct QueryProfile {
   void toVelocyPack(arangodb::velocypack::Builder&) const;
 
  private:
-  Query* _query;
+  Query& _query;
   std::array<double,
              static_cast<size_t>(QueryExecutionState::ValueType::INVALID_STATE)>
       _timers;

arangod/Cluster/ClusterCollectionCreationInfo.cpp
@@ -74,6 +74,19 @@ ClusterCollectionCreationInfo::ClusterCollectionCreationInfo(
   }
 }
 
+ClusterCollectionCreationInfo::ClusterCollectionCreationInfo(
+    ClusterCollectionCreationInfo const& r)
+    : collectionID(r.collectionID),
+      numberOfShards(r.numberOfShards),
+      replicationFactor(r.replicationFactor),
+      writeConcern(r.writeConcern),
+      waitForReplication(r.waitForReplication),
+      json(r.json),
+      name(r.name),
+      state(r.state.load(std::memory_order_relaxed)),
+      creator(r.creator),
+      _isBuildingJson(r._isBuildingJson) {}
+
 VPackSlice ClusterCollectionCreationInfo::isBuildingSlice() const {
   if (needsBuildingFlag()) {
     return _isBuildingJson.slice();

arangod/Cluster/ClusterCollectionCreationInfo.h
@@ -29,6 +29,8 @@
 #include <velocypack/Builder.h>
 #include <velocypack/Serializable.h>
 #include <velocypack/Slice.h>
+
+#include <atomic>
 #include <optional>
 
 namespace arangodb {
@@ -42,6 +44,10 @@ struct ClusterCollectionCreationInfo {
                                 velocypack::Slice const& slice,
                                 std::string coordinatorId, RebootId rebootId);
 
+  // TODO - remove this copy constructor and use atomic_ref instead of making
+  // `state` an atomic once we have fixed the Mac build
+  ClusterCollectionCreationInfo(ClusterCollectionCreationInfo const& r);
+
   std::string const collectionID;
   uint64_t numberOfShards;
   uint64_t replicationFactor;
@@ -49,7 +55,7 @@ struct ClusterCollectionCreationInfo {
   bool waitForReplication;
   velocypack::Slice const json;
   std::string name;
-  ClusterCollectionCreationState state;
+  std::atomic<ClusterCollectionCreationState> state;
 
   class CreatorInfo : public velocypack::Serializable {
    public:

arangod/Cluster/ClusterFeature.cpp
@@ -82,6 +82,9 @@ ClusterFeature::~ClusterFeature() {
 
     AgencyCommHelper::shutdown();
   }
+  // must make sure that the HeartbeatThread is fully stopped before
+  // we destroy the AgencyCallbackRegistry.
+  _heartbeatThread.reset();
 }
 
 void ClusterFeature::collectOptions(std::shared_ptr<ProgramOptions> options) {

commit 83d284156ec816acdc2fef42e35e2056e30900bc

Fix a potential deadlock in QueryList introduced in #15515

arangod/Aql/QueryList.cpp
@@ -130,8 +130,7 @@ bool QueryList::insert(Query& query) {
     }
 
     // return whether or not insertion worked
-    bool inserted =
-        _current.insert({query.id(), query.shared_from_this()}).second;
+    bool inserted = _current.insert({query.id(), &query}).second;
     _queryRegistryFeature.trackQueryStart();
     return inserted;
   } catch (...) {
@@ -277,11 +276,7 @@ Result QueryList::kill(TRI_voc_tick_t id) {
     return {TRI_ERROR_QUERY_NOT_FOUND, "query ID not found in query list"};
   }
 
-  auto query = it->second.lock();
-  if (query) {
-    // if we cannot lock the query, it is already beeing destroyed
-    killQuery(*query, maxLength, false);
-  }
+  killQuery(*it->second, maxLength, false);
 
   return Result();
 }
@@ -297,13 +292,13 @@ uint64_t QueryList::kill(std::function<bool(Query&)> const& filter,
   READ_LOCKER(readLocker, _lock);
 
   for (auto& it : _current) {
-    auto query = it.second.lock();
+    Query& query = *(it.second);
 
-    if (query == nullptr || !filter(*query)) {
+    if (!filter(query)) {
       continue;
     }
 
-    killQuery(*query, maxLength, silent);
+    killQuery(query, maxLength, silent);
     ++killed;
   }
 
@@ -328,10 +323,9 @@ std::vector<QueryEntryCopy> QueryList::listCurrent() {
     result.reserve(_current.size());
 
     for (auto const& it : _current) {
-      auto query = it.second.lock();
-      if (query == nullptr) {
-        continue;
-      }
+      Query const* query = it.second;
+
+      TRI_ASSERT(query != nullptr);
 
       // elapsed time since query start
       double const elapsed = elapsedSince(query->startTime());

arangod/Aql/QueryList.h
@@ -242,7 +242,7 @@ class QueryList {
   arangodb::basics::ReadWriteLock _lock;
 
   /// @brief list of current queries, protected by _lock
-  std::unordered_map<TRI_voc_tick_t, std::weak_ptr<Query>> _current;
+  std::unordered_map<TRI_voc_tick_t, Query*> _current;
 
   /// @brief list of slow queries, protected by _lock
   std::list<QueryEntryCopy> _slow;