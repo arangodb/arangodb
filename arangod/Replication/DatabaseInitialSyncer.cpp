65b42182d3aa1fe7c39ed9e8c43638b45b86252b
Kaveh Vahedipour <kaveh@arangodb.com>
2022-02-02 21:57:56 +0100

    fix potentially dangling reference

--- arangod/Replication/DatabaseInitialSyncer.cpp
+++ arangod/Replication/DatabaseInitialSyncer.cpp
@@ -389,7 +389,7 @@ DatabaseInitialSyncer::DatabaseInitialSyncer(
       _config{_state.applier, _batch,        _state.connection,
               false,          _state.leader, _progress,
               _state,         vocbase},
-      _lastAbortionCheck(std::chrono::steady_clock::now()),
+      _lastCancellationCheck(std::chrono::steady_clock::now()),
       _isClusterRole(ServerState::instance()->isClusterRole()),
       _quickKeysNumDocsLimit(
           vocbase.server().getFeature<ReplicationFeature>().quickKeysLimit()) {
@@ -571,13 +571,13 @@ bool DatabaseInitialSyncer::isAborted() const {
     }
   }
 
-  if (_checkAbortion) {
+  if (_checkCancellation) {
     // execute custom check for abortion only every few seconds, in case
     // it is expensive
     auto now = std::chrono::steady_clock::now();
-    if (now - _lastAbortionCheck >= std::chrono::seconds(5)) {
-      _lastAbortionCheck = now;
-      if (_checkAbortion()) {
+    if (now - _lastCancellationCheck >= std::chrono::seconds(5)) {
+      _lastCancellationCheck = now;
+      if (_checkCancellation()) {
         return true;
       }
     }
