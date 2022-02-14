950ee45447af9d7bf379529e139454d36808566e
jsteemann <jsteemann@users.noreply.github.com>
2022-02-14 13:26:24 +0100

    update formatting script

--- scripts/clang-format-linux.sh
+++ scripts/clang-format-linux.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 # pre-requisite: Linux-based system
 # pre-requisite: Docker
+# pre-requisite: Bash 4
+# pre-requisite: Git 2.9+
+# pre-requisite: sed
 
-# Get root directory of repo (can be passed as argument)
-[[ -z "$1" ]] && adb_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )" || adb_path=$(echo "$1" | sed 's:/*$::')
+adb_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+
 ent_dir="enterprise"
 
 echo "ArangoDB directory: $adb_path"
-echo "Enterprise directory: $adb_path/$ent_dir"
+pushd "$adb_path" > /dev/null
 
-cd "$adb_path"
-community_diff=$(git diff HEAD --diff-filter=ACMRT --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h')
+changed_files_filename=".clang-format-$$.changed.tmp"
+# clean up after ourselves
+trap "rm -f $changed_files_filename" EXIT SIGINT SIGTERM SIGHUP
 
-if [ -d "$adb_path/$ent_dir" ]
+git diff --diff-filter=ACMRT --name-only -- arangod/ lib/ client-tools/ tests/ | grep -e '\.ipp$' -e '\.tpp$' -e '\.cpp$' -e '\.hpp$' -e '\.cc$' -e '\.c$' -e '\.h$' > "$changed_files_filename"
+
+if [ -d "$ent_dir" ]
 then
-   cd "$adb_path/$ent_dir"
-   enterprise_diff=$(git diff HEAD --diff-filter=ACMRT --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h' | sed "s,^,$ent_dir/,")
+  # collect changes from enterprise repository as well
+  echo "Enterprise directory: $adb_path/$ent_dir"
+  pushd "$ent_dir" > /dev/null
+  git diff --diff-filter=ACMRT --name-only -- Enterprise tests/ | grep -e '\.ipp$' -e '\.tpp$' -e '\.cpp$' -e '\.hpp$' -e '\.cc$' -e '\.c$' -e '\.h$' | sed -e "s/^/$ent_dir\//g" >> "../$changed_files_filename"
+  popd > /dev/null
 fi
 
-cd "$adb_path"
-diff="$community_diff $enterprise_diff"
-if ! [[ -z "${diff// }" ]]; then
-  docker run --rm -u "$(id -u):$(id -g)" --mount type=bind,source="$adb_path",target=/usr/src/arangodb arangodb/clang-format:1.0 "$diff"
+if [ -s "$changed_files_filename" ]; then
+  docker run --rm -u "$(id -u):$(id -g)" --mount type=bind,source="$adb_path",target=/usr/src/arangodb jsteemann/clang-format-docker:0.2 "format" "$changed_files_filename" 
 fi
+status=$?
 
-echo "(done)"
+popd > /dev/null
+
+exit $status
