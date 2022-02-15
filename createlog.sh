87e34b8f46b5b18dfbfbac18e91a1cdfa96e7023
apetenchea <alexandru.petenchea@arangodb.com>
2022-02-15 16:53:57 +0200

    deleted unused files

--- createlog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s -XPOST http://localhost:8530/_api/log -d '{"id":12, "config":{"waitForSync":false, "writeConcern": 2, "softWriteConcern": 3, "replicationFactor": 3}}' | jq
--- x.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./scripts/startLocalCluster.sh -d 3
