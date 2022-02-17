4736b4189702c1d5f80c961934d6ff1d67c9f102
Emmanuel Etukudo <emzzyyoung@gmail.com>
2022-02-17 09:29:34 +0100

    Added table to display collection IDs

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewLinksReactView.jsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewLinksReactView.jsx
@@ -1,17 +1,24 @@
-import { cloneDeep } from 'lodash';
-import React, { useEffect, useReducer, useRef, useState } from 'react';
-import { getReducer, isAdminUser as userIsAdmin, usePermissions } from '../../utils/helpers';
-import { SaveButton } from './Actions';
-import LinkPropertiesForm from './forms/LinkPropertiesForm';
-import { buildSubNav, postProcessor, useView } from './helpers';
+import { cloneDeep } from "lodash";
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import {
+  getReducer,
+  isAdminUser as userIsAdmin,
+  usePermissions
+} from "../../utils/helpers";
+import { SaveButton } from "./Actions";
+import LinkPropertiesForm from "./forms/LinkPropertiesForm";
+import { buildSubNav, postProcessor, useView } from "./helpers";
+import { ArangoTable, ArangoTH, ArangoTD } from "../../components/arango/table";
 
 const ViewLinksReactView = ({ name }) => {
   const initialState = useRef({
     formState: { name },
     formCache: { name }
   });
-  const [state, dispatch] = useReducer(getReducer(initialState.current, postProcessor),
-    initialState.current);
+  const [state, dispatch] = useReducer(
+    getReducer(initialState.current, postProcessor),
+    initialState.current
+  );
   const view = useView(name);
   const permissions = usePermissions();
   const [isAdminUser, setIsAdminUser] = useState(false);
@@ -20,39 +27,92 @@ const ViewLinksReactView = ({ name }) => {
     initialState.current.formCache = cloneDeep(view);
 
     dispatch({
-      type: 'setFormState',
+      type: "setFormState",
       formState: view
     });
   }, [view, name]);
 
   useEffect(() => {
-    const observer = buildSubNav(isAdminUser, name, 'Links');
+    const observer = buildSubNav(isAdminUser, name, "Links");
 
     return () => observer.disconnect();
   }, [isAdminUser, name]);
 
   const tempIsAdminUser = userIsAdmin(permissions);
-  if (tempIsAdminUser !== isAdminUser) { // Prevents an infinite render loop.
+  if (tempIsAdminUser !== isAdminUser) {
+    // Prevents an infinite render loop.
     setIsAdminUser(tempIsAdminUser);
   }
 
   const formState = state.formState;
 
-  return <div className={'centralContent'} id={'content'}>
-    <div id={'modal-dialog'} className={'createModalDialog'} tabIndex={-1} role={'dialog'}
-         aria-labelledby={'myModalLabel'} aria-hidden={'true'}>
-      <div className="modal-body" style={{ overflowY: 'visible' }}>
-        <div className={'tab-content'}>
-          <div className="tab-pane tab-pane-modal active" id="Links">
-            <LinkPropertiesForm formState={formState} dispatch={dispatch} disabled={!isAdminUser}/>
+  const mockData = [
+    {
+      id: 0,
+      name: "fooBar",
+      desc: "This is fooBar",
+      action: "This is an action"
+    },
+    {
+      id: 1,
+      name: "airPort",
+      desc: "This is fooBar",
+      action: "This is an action"
+    },
+    {
+      id: 2,
+      name: "route",
+      desc: "This is fooBar",
+      action: "This is an action"
+    }
+  ];
+
+  return (
+    <div className={"centralContent"} id={"content"}>
+      <ArangoTable className={"arango-table"}>
+        <thead>
+          <tr>
+            <ArangoTH>ID</ArangoTH>
+            <ArangoTH>Name</ArangoTH>
+            <ArangoTH>Description</ArangoTH>
+            <ArangoTH>Actions</ArangoTH>
+          </tr>
+        </thead>
+
+        {mockData.map(c => (
+          <tbody>
+            <ArangoTD>{c.id}</ArangoTD>
+            <ArangoTD>{c.name}</ArangoTD>
+            <ArangoTD>{c.desc}</ArangoTD>
+            <ArangoTD>{c.action}</ArangoTD>
+          </tbody>
+        ))}
+      </ArangoTable>
+      <div
+        id={"modal-dialog"}
+        className={"createModalDialog"}
+        tabIndex={-1}
+        role={"dialog"}
+        aria-labelledby={"myModalLabel"}
+        aria-hidden={"true"}
+      >
+        <div className="modal-body" style={{ overflowY: "visible" }}>
+          <div className={"tab-content"}>
+            <div className="tab-pane tab-pane-modal active" id="Links">
+              <LinkPropertiesForm
+                formState={formState}
+                dispatch={dispatch}
+                disabled={!isAdminUser}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="modal-footer">
-        <SaveButton view={formState} oldName={name}/>
+        <div className="modal-footer">
+          <SaveButton view={formState} oldName={name} />
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 window.ViewLinksReactView = ViewLinksReactView;
