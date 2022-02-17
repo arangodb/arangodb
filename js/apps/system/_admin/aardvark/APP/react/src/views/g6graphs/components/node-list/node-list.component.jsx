43e40d0b01f9f4181b4deab60a86462754335a97
Andreas Jurdak <andreas@jurdak.at>
2022-02-17 14:53:45 +0100

    [FE-8] Refactor list components to fit search into UI

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/components/node-list/node-list.component.jsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/components/node-list/node-list.component.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 
 import { Node } from '../node/node.component';
 
-export const NodeList = ({ nodes, graphData, onNodeInfo, data, onDeleteNode }) => {
-  console.log('nodes (NodeList): ', nodes);
-  console.log('graphData (NodeList): ', graphData);
-  console.log('onNodeInfo (NodeList): ', onNodeInfo);
-
-  /*
-  const onDeleteNode = (node) => {
-    console.log('key in onDeleteNode in nodelist: ', node);
-  };
-  */
-
-  const onNodeEdit = ( node ) => {
-    console.log("Edit in NODELIST-Componet (node): ", node);
-    console.log("Edit in NODELIST-Componet (graphData): ", graphData);
-  }
+export const NodeList = ({ nodes, graphData, onNodeInfo, data, onDeleteNode, onNodeSelect }) => {
 
   return (
     <div>
       <div className="node-list">
-        <input list="nodelist" id="graphnodes" name="graphnodes" placeholder="Search nodes..." style={{ 'width': '90%' }} />
+        <input
+          list="nodelist"
+          id="graphnodes"
+          name="graphnodes"
+          placeholder="Search nodes..."
+          onChange={
+            (e) => {
+              onNodeSelect(e.target.value);
+            }
+          }
+          style={{ 'width': '100%' }} />
         <datalist id="nodelist">
           {nodes.map(node => (
             <Node
