3464d6705de7cc62a2cab1f260d445b960f8631d
Andrey Abramov <andrey@arangodb.com>
2022-02-18 15:34:14 +0300

    cleanup

--- cmake/UpdateModule.cmake
+++ cmake/UpdateModule.cmake
@@ -1,22 +1,22 @@
-macro(RefreshModule _SUBMODULE _CWD)
+macro(UpdateModule _GIT _SUBMODULE _CWD)
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse @:./${_SUBMODULE}
+    COMMAND ${_GIT} rev-parse @:./${_SUBMODULE}
     WORKING_DIRECTORY ${_CWD}
     OUTPUT_VARIABLE _EXPECTED_SUBMODULE_HASH)
 
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+    COMMAND ${_GIT} rev-parse HEAD
     WORKING_DIRECTORY ${_CWD}/${_SUBMODULE}
     OUTPUT_VARIABLE _ACTUAL_SUBMODULE_HASH)
 
   if (NOT ${_EXPECTED_SUBMODULE_HASH} EQUAL ${_ACTUAL_SUBMODULE_HASH})
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${_SUBMODULE}
+      COMMAND ${_GIT} submodule update --init -- ${_SUBMODULE}
       RESULT_VARIABLE _INIT_RESULT
       WORKING_DIRECTORY ${_CWD})
 
     if (NOT ${_INIT_RESULT} EQUAL "0")
-      message(WARNING "FAILED: ${GIT_EXECUTABLE} submodule update --init -- ${_SUBMODULE}")
+      message(WARNING "FAILED: ${_GIT} submodule update --init -- ${_SUBMODULE}")
     endif()
   endif()
-endmacro(RefreshModule)
+endmacro(UpdateModule)
