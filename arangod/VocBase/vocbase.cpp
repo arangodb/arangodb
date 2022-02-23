95cc30e7915e3e115cf7527ce22b7416bb856c1a
jsteemann <jsteemann@users.noreply.github.com>
2022-02-23 12:52:04 +0100

    intentionally break formatting

--- arangod/VocBase/vocbase.cpp
+++ arangod/VocBase/vocbase.cpp
@@ -89,7 +89,7 @@ using namespace arangodb;
 using namespace arangodb::basics;
 
 /// @brief increase the reference counter for a database
-bool TRI_vocbase_t::use() {
+bool   TRI_vocbase_t::use() {
   auto expected = _refCount.load(std::memory_order_relaxed);
   while (true) {
     if ((expected & 1) != 0) {
