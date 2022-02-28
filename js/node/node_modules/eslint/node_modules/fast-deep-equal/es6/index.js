37a8f189d1e03c3b2269a8f57e17bed562094ed9
Alan Plum <me@pluma.io>
2022-02-28 15:51:17 +0100

    Correctly remove top-level chalk

--- js/node/node_modules/eslint/node_modules/fast-deep-equal/es6/index.js
+++ js/node/node_modules/eslint/node_modules/fast-deep-equal/es6/index.js
@@ -3,6 +3,8 @@
 // do not edit .js files directly - edit src/index.jst
 
 
+  var envHasBigInt64Array = typeof BigInt64Array !== 'undefined';
+
 
 module.exports = function equal(a, b) {
   if (a === b) return true;
@@ -19,6 +21,30 @@ module.exports = function equal(a, b) {
       return true;
     }
 
+
+    if ((a instanceof Map) && (b instanceof Map)) {
+      if (a.size !== b.size) return false;
+      for (i of a.entries())
+        if (!b.has(i[0])) return false;
+      for (i of a.entries())
+        if (!equal(i[1], b.get(i[0]))) return false;
+      return true;
+    }
+
+    if ((a instanceof Set) && (b instanceof Set)) {
+      if (a.size !== b.size) return false;
+      for (i of a.entries())
+        if (!b.has(i[0])) return false;
+      return true;
+    }
+
+    if (ArrayBuffer.isView(a) && ArrayBuffer.isView(b)) {
+      length = a.length;
+      if (length != b.length) return false;
+      for (i = length; i-- !== 0;)
+        if (a[i] !== b[i]) return false;
+      return true;
+    }
 
 
     if (a.constructor === RegExp) return a.source === b.source && a.flags === b.flags;
