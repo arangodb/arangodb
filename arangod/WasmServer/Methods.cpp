c4c89a588fda494204b66fa00c211d79aeb0af91
Julia Volmer <julia.volmer@arangodb.de>
2022-03-02 13:05:30 +0100

    Add basic wasm vm method for single server

---
+++ arangod/WasmServer/Methods.cpp
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Julia Volmer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Methods.h"
+#include "Futures/Future.h"
+#include "Cluster/ServerState.h"
+
+using namespace arangodb;
+using namespace arangodb::wasm;
+
+struct WasmVmMethodsSingleServer final
+    : WasmVmMethods,
+      std::enable_shared_from_this<WasmVmMethodsSingleServer> {
+  explicit WasmVmMethodsSingleServer(TRI_vocbase_t& vocbase)
+      : vocbase(vocbase){};
+  auto createWasmUdf() const -> futures::Future<Result> override {
+    return Result{};
+  }
+  TRI_vocbase_t& vocbase;
+};
+
+auto WasmVmMethods::createInstance(TRI_vocbase_t& vocbase)
+    -> std::shared_ptr<WasmVmMethods> {
+  switch (ServerState::instance()->getRole()) {
+    case ServerState::ROLE_SINGLE:
+      return std::make_shared<WasmVmMethodsSingleServer>(vocbase);
+    default:
+      THROW_ARANGO_EXCEPTION_MESSAGE(
+          TRI_ERROR_NOT_IMPLEMENTED,
+          "This API is only available on single server.");
+  }
+}
