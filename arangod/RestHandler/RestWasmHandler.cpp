ecc3712d3a05f62e20bc39e631c5644971f5dc95
Julia Volmer <julia.volmer@arangodb.de>
2022-03-02 14:48:55 +0100

    Add basic rest wasm handler

---
+++ arangod/RestHandler/RestWasmHandler.cpp
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Julia Volmer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "RestWasmHandler.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+
+#include <velocypack/Builder.h>
+#include <velocypack/velocypack-aliases.h>
+
+#include "GeneralServer/RestHandler.h"
+#include "Logger/LogMacros.h"
+#include "Rest/CommonDefines.h"
+
+#include "WasmServer/Methods.h"
+#include "RestServer/arangod.h"
+
+using namespace arangodb;
+using namespace arangodb::wasm;
+
+RestWasmHandler::RestWasmHandler(ArangodServer& server, GeneralRequest* request,
+                                 GeneralResponse* response)
+    : RestVocbaseBaseHandler(server, request, response) {}
+
+RestWasmHandler::~RestWasmHandler() = default;
+
+auto RestWasmHandler::execute() -> RestStatus {
+  // depends on ServerState::RoleEnum
+  auto methods = WasmVmMethods::createInstance(_vocbase);
+  return executeByMethod(*methods);
+}
+
+auto RestWasmHandler::executeByMethod(WasmVmMethods const& methods)
+    -> RestStatus {
+  switch (_request->requestType()) {
+    case rest::RequestType::GET:
+      return handleGetRequest(methods);
+    case rest::RequestType::POST:
+      return handlePostRequest(methods);
+    default:
+      generateError(rest::ResponseCode::METHOD_NOT_ALLOWED,
+                    TRI_ERROR_HTTP_METHOD_NOT_ALLOWED);
+  }
+  return RestStatus::DONE;
+}
+
+auto RestWasmHandler::handleGetRequest(WasmVmMethods const& methods)
+    -> RestStatus {
+  LOG_DEVEL << "Hello world";
+  return RestStatus::DONE;
+}
+
+auto RestWasmHandler::handlePostRequest(WasmVmMethods const& methods)
+    -> RestStatus {
+  return RestStatus::DONE;
+}
---
+++ arangod/RestHandler/RestWasmHandler.h
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Julia Volmer
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "RestHandler/RestVocbaseBaseHandler.h"
+#include "WasmServer/Methods.h"
+
+namespace arangodb {
+using namespace wasm;
+
+class RestWasmHandler : public arangodb::RestVocbaseBaseHandler {
+ public:
+  explicit RestWasmHandler(ArangodServer&, GeneralRequest*, GeneralResponse*);
+
+  ~RestWasmHandler() override;
+
+ private:
+  auto executeByMethod(wasm::WasmVmMethods const& methods) -> RestStatus;
+  auto handleGetRequest(wasm::WasmVmMethods const& methods) -> RestStatus;
+  auto handlePostRequest(wasm::WasmVmMethods const& methods) -> RestStatus;
+
+ public:
+  RestStatus execute() override;
+  char const* name() const override { return "Wasm Rest Handler"; }
+  RequestLane lane() const final { return RequestLane::CLIENT_SLOW; }
+};
+}  // namespace arangodb
