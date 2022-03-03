f3bdb49596e4c1982c71a43d61594f765d16f85e
Michael Hackstein <michael@arangodb.com>
2022-03-03 15:09:54 +0100

    Added a new endpoint for smartSearchUnified

--- arangod/InternalRestHandler/InternalRestTraverserHandler.cpp
+++ arangod/InternalRestHandler/InternalRestTraverserHandler.cpp
@@ -238,7 +238,8 @@ void InternalRestTraverserHandler::queryEngine() {
     }
     engine->getVertexData(keysSlice, result, !depthSlice.isNone());
   } else if (option == "smartSearch" || option == "smartSearchBFS" ||
-             option == "smartSearchWeighted") {
+             option == "smartSearchWeighted" ||
+             option == "smartSearchUnified") {
     if (engine->getType() != BaseEngine::EngineType::TRAVERSER) {
       generateError(ResponseCode::BAD, TRI_ERROR_HTTP_BAD_PARAMETER,
                     "this engine does not support the requested operation.");
@@ -247,7 +248,12 @@ void InternalRestTraverserHandler::queryEngine() {
     // Safe cast BaseTraverserEngines are all of type TRAVERSER
     auto eng = static_cast<BaseTraverserEngine*>(engine);
     TRI_ASSERT(eng != nullptr);
-    eng->smartSearch(body, result);
+    if (option == "smartSearchUnified") {
+      eng->smartSearchUnified(body, result);
+    } else {
+      // TODO: Take deprecation path!
+      eng->smartSearch(body, result);
+    }
   } else {
     // PATH Info wrong other error
     generateError(ResponseCode::NOT_FOUND, TRI_ERROR_HTTP_NOT_FOUND, "");
