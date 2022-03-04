4b52766f3aba103590c3d1a4b384c5ab763f1e8a
apetenchea <alexandru.petenchea@arangodb.com>
2022-03-04 16:46:21 +0200

    more coordinator methods and small fixes

--- arangod/Replication2/StateMachines/Prototype/PrototypeStateMethods.cpp
+++ arangod/Replication2/StateMachines/Prototype/PrototypeStateMethods.cpp
@@ -29,9 +29,11 @@
 #include "Cluster/ClusterFeature.h"
 #include "Cluster/ClusterInfo.h"
 #include "Cluster/ServerState.h"
+#include "Replication2/Exceptions/ParticipantResignedException.h"
 #include "Replication2/ReplicatedLog/LogCommon.h"
 #include "Replication2/ReplicatedState/ReplicatedState.h"
 #include "Replication2/StateMachines/Prototype/PrototypeStateMachine.h"
+#include "Network/Methods.h"
 #include "VocBase/vocbase.h"
 
 #include "PrototypeStateMethods.h"
@@ -116,24 +118,114 @@ struct PrototypeStateMethodsCoordinator final
       std::enable_shared_from_this<PrototypeStateMethodsCoordinator> {
   auto insert(LogId id, std::string key, std::string value) const
       -> futures::Future<Result> override {
-    THROW_ARANGO_EXCEPTION(TRI_ERROR_NOT_IMPLEMENTED);
+    auto path =
+        basics::StringUtils::joinT("/", "_api/prototype-state", id, "insert");
+    network::RequestOptions opts;
+    opts.database = _vocbase.name();
+    VPackBuilder builder{};
+    {
+      VPackObjectBuilder ob{&builder};
+      builder.add(key, VPackValue(value));
+    }
+    return network::sendRequest(_pool, "server:" + getLogLeader(id),
+                                fuerte::RestVerb::Post, path,
+                                builder.bufferRef(), opts)
+        .thenValue([](network::Response&& resp) {
+          if (resp.fail() || !fuerte::statusIsSuccess(resp.statusCode())) {
+            THROW_ARANGO_EXCEPTION(resp.combinedResult());
+          }
+          return resp.combinedResult();
+        });
   }
 
   auto insert(LogId id,
               std::vector<std::pair<std::string, std::string>> const& entries)
       const -> futures::Future<Result> override {
-    THROW_ARANGO_EXCEPTION(TRI_ERROR_NOT_IMPLEMENTED);
+    auto path = basics::StringUtils::joinT("/", "_api/prototype-state", id,
+                                           "multi-insert");
+    network::RequestOptions opts;
+    opts.database = _vocbase.name();
+    VPackBuilder builder{};
+    {
+      VPackArrayBuilder ab{&builder};
+      for (auto const& [key, value] : entries) {
+        VPackObjectBuilder ob{&builder};
+        builder.add(key, VPackValue(value));
+      }
+    }
+    return network::sendRequest(_pool, "server:" + getLogLeader(id),
+                                fuerte::RestVerb::Post, path,
+                                builder.bufferRef(), opts)
+        .thenValue([](network::Response&& resp) {
+          if (resp.fail() || !fuerte::statusIsSuccess(resp.statusCode())) {
+            THROW_ARANGO_EXCEPTION(resp.combinedResult());
+          }
+          return resp.combinedResult();
+        });
   }
 
   auto get(LogId id, std::string key) const
       -> futures::Future<std::optional<std::string>> override {
-    THROW_ARANGO_EXCEPTION(TRI_ERROR_NOT_IMPLEMENTED);
+    auto path = basics::StringUtils::joinT("/", "_api/prototype-state", id,
+                                           "entry", key);
+    network::RequestOptions opts;
+    opts.database = _vocbase.name();
+    return network::sendRequest(_pool, "server:" + getLogLeader(id),
+                                fuerte::RestVerb::Get, path, {}, opts)
+        .thenValue([](network::Response&& resp) -> std::optional<std::string> {
+          if (resp.statusCode() == fuerte::StatusNotFound) {
+            return std::nullopt;
+          } else if (resp.fail() ||
+                     !fuerte::statusIsSuccess(resp.statusCode())) {
+            THROW_ARANGO_EXCEPTION(resp.combinedResult());
+          } else {
+            auto slice = resp.slice();
+            if (auto result = slice.get("result");
+                result.isObject() && result.length() == 1) {
+              return result.valueAt(0).copyString();
+            }
+            THROW_ARANGO_EXCEPTION_MESSAGE(
+                TRI_ERROR_INTERNAL, basics::StringUtils::concatT(
+                                        "expected result containing key-value "
+                                        "pair in leader response: ",
+                                        slice.toJson()));
+          }
+        });
   }
 
   auto get(LogId id, std::vector<std::string> const& keys) const
       -> futures::Future<
           std::unordered_map<std::string, std::string>> override {
-    THROW_ARANGO_EXCEPTION(TRI_ERROR_NOT_IMPLEMENTED);
+    auto path = basics::StringUtils::joinT("/", "_api/prototype-state", id,
+                                           "multi-get");
+
+    // TODO this is broken
+    network::RequestOptions opts;
+    opts.database = _vocbase.name();
+    for (auto const& it : keys) {
+      opts.parameters["key[]"] = it;
+    }
+    return network::sendRequest(_pool, "server:" + getLogLeader(id),
+                                fuerte::RestVerb::Get, path, {}, opts)
+        .thenValue([](network::Response&& resp) {
+          if (resp.fail() || !fuerte::statusIsSuccess(resp.statusCode())) {
+            THROW_ARANGO_EXCEPTION(resp.combinedResult());
+          } else {
+            auto slice = resp.slice();
+            if (auto result = slice.get("result"); result.isObject()) {
+              std::unordered_map<std::string, std::string> map;
+              for (auto it : VPackObjectIterator{result}) {
+                map.emplace(it.key.copyString(), it.value.copyString());
+              }
+              return map;
+            }
+            THROW_ARANGO_EXCEPTION_MESSAGE(
+                TRI_ERROR_INTERNAL,
+                basics::StringUtils::concatT("expected result containing map "
+                                             "in leader response: ",
+                                             slice.toJson()));
+          }
+        });
   }
 
   auto remove(LogId id, std::string key) const
@@ -151,6 +243,19 @@ struct PrototypeStateMethodsCoordinator final
         _clusterInfo(
             vocbase.server().getFeature<ClusterFeature>().clusterInfo()),
         _pool(vocbase.server().getFeature<NetworkFeature>().pool()) {}
+
+ private:
+  auto getLogLeader(LogId id) const -> ServerID {
+    auto leader = _clusterInfo.getReplicatedLogLeader(_vocbase.name(), id);
+    if (leader.fail()) {
+      if (leader.is(TRI_ERROR_REPLICATION_REPLICATED_LOG_LEADER_RESIGNED)) {
+        throw ParticipantResignedException(leader.result(), ADB_HERE);
+      } else {
+        THROW_ARANGO_EXCEPTION(leader.result());
+      }
+    }
+    return *leader;
+  }
 
  public:
   TRI_vocbase_t& _vocbase;
