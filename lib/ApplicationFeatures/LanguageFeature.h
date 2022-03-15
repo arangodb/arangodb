096b3b260d4d5117b2ee0a6f2737593715d78a6d
Alexey Bakharew <alex.bakharew@gmail.com>
2022-03-15 12:06:51 +0300

    intial commit for backport 3.9

--- lib/ApplicationFeatures/LanguageFeature.h
+++ lib/ApplicationFeatures/LanguageFeature.h
@@ -23,15 +23,17 @@
 
 #pragma once
 
-#include <unicode/locid.h>
 #include <memory>
 #include <string>
+#include <unicode/locid.h>
 
 #include "ApplicationFeatures/ApplicationFeature.h"
+#include "Basics/Utf8Helper.h"
 
 namespace arangodb {
 namespace application_features {
 class ApplicationServer;
+class GreetingsFeaturePhase;
 }
 namespace options {
 class ProgramOptions;
@@ -39,7 +41,20 @@ class ProgramOptions;
 
 class LanguageFeature final : public application_features::ApplicationFeature {
  public:
-  explicit LanguageFeature(application_features::ApplicationServer& server);
+  static constexpr std::string_view name() noexcept { return "Language"; }
+
+  template<typename Server>
+  explicit LanguageFeature(Server& server)
+      : application_features::ApplicationFeature{server, "Language"},
+        _locale(),
+        _langType(basics::LanguageType::INVALID),
+        _binaryPath(server.getBinaryPath()),
+        _icuDataPtr(nullptr),
+        _forceLanguageCheck(true) {
+    setOptional(false);
+    startsAfter<application_features::GreetingsFeaturePhase>();
+  }
+
   ~LanguageFeature();
 
   void collectOptions(std::shared_ptr<options::ProgramOptions>) override final;
@@ -49,14 +64,18 @@ class LanguageFeature final : public application_features::ApplicationFeature {
                           std::string const& binaryExecutionPath,
                           std::string& path, std::string const& binaryName);
   icu::Locale& getLocale();
-  std::string const& getDefaultLanguage() const;
+  std::tuple<std::string_view, arangodb::basics::LanguageType> getLanguage()
+      const;
   bool forceLanguageCheck() const;
   std::string getCollatorLanguage() const;
-  void resetDefaultLanguage(std::string const& language);
+  void resetLanguage(std::string_view language,
+                     arangodb::basics::LanguageType type);
 
  private:
   icu::Locale _locale;
-  std::string _language;
+  std::string _defaultLanguage;
+  std::string _icuLanguage;
+  arangodb::basics::LanguageType _langType;
   char const* _binaryPath;
   void* _icuDataPtr;
   bool _forceLanguageCheck;
