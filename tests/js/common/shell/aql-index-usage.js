80b2d3fe86a438267582f311440ea2a81352fd09
Wilfried Goesgens <willi@arangodb.com>
2022-03-16 14:01:26 +0100

    only set failurepoints, if the environment supports that (#15918)

--- tests/js/common/shell/aql-index-usage.js
+++ tests/js/common/shell/aql-index-usage.js
@@ -33,7 +33,7 @@ let jsunity = require("jsunity");
 let arangodb = require("@arangodb");
 let db = arangodb.db;
 let tasks = require("@arangodb/tasks");
-const {debugClearFailAt, debugSetFailAt} = require('internal');
+const {debugClearFailAt, debugSetFailAt, debugCanUseFailAt} = require('internal');
 
 function IndexUsageSuite () {
   const cnData = "UnitTestsCollection"; // used for test data
@@ -57,7 +57,9 @@ function IndexUsageSuite () {
     tearDown : function () {
       db._drop(cnData);
       db._drop(cnComm);
-      debugClearFailAt();
+      if (debugCanUseFailAt()) {
+        debugClearFailAt();
+      }
     },
 
     testIndexUsage : function () {
@@ -68,7 +70,7 @@ function IndexUsageSuite () {
           let comm = db[params.cnComm];
           let errors = require("@arangodb").errors;
           comm.insert({ _key: "runner1", value: 0 });
-              
+
           while (!comm.exists("runner2")) {
             require("internal").sleep(0.02);
           }
@@ -124,51 +126,52 @@ function IndexUsageSuite () {
       assertTrue(success > 0, success);
     },
 
-    
     testPrimaryIndexLookupConsistency : function () {
-      debugSetFailAt("RocksDBCollection::read-delay");
-      
-      const task = tasks.register({
-        command: function(params) {
-          require('jsunity').jsUnity.attachAssertions();
-          const {db, time} = require("internal");
-          const comm = db[params.cnComm];
-          comm.insert({ _key: "runner1", value: 0 });
-              
-          while (!comm.exists("runner2")) {
-            require("internal").sleep(0.02);
+      if (debugCanUseFailAt()) {
+        debugSetFailAt("RocksDBCollection::read-delay");
+
+        const task = tasks.register({
+          command: function(params) {
+            require('jsunity').jsUnity.attachAssertions();
+            const {db, time} = require("internal");
+            const comm = db[params.cnComm];
+            comm.insert({ _key: "runner1", value: 0 });
+
+            while (!comm.exists("runner2")) {
+              require("internal").sleep(0.02);
+            }
+
+            const start = time();
+            // this tasks keeps updating document "runner1"
+            let cnt = 0;
+            do {
+              comm.update("runner1", { value: ++cnt });
+            } while (time() - start < 10.0);
+          },
+          params: { cnComm, cnData }
+        });
+
+        const comm = db[cnComm];
+        comm.insert({ _key: "runner2" });
+        while (!comm.exists("runner1")) {
+          require("internal").sleep(0.02);
+        }
+
+        let time = require("internal").time;
+        let start = time();
+        // this task keeps reading document "runner1"
+        do {
+          assertTrue(comm.exists("runner1"));
+        } while (time() - start < 10.0);
+
+        while (true) {
+          try {
+            tasks.get(task);
+            require("internal").wait(0.25, false);
+          } catch (err) {
+            // "task not found" means the task is finished
+            break;
           }
-
-          const start = time();
-          // this tasks keeps updating document "runner1"
-          let cnt = 0;
-          do {
-            comm.update("runner1", { value: ++cnt });
-          } while (time() - start < 10.0);
-        },
-        params: { cnComm, cnData }
-      });
-
-      const comm = db[cnComm];
-      comm.insert({ _key: "runner2" });
-      while (!comm.exists("runner1")) {
-        require("internal").sleep(0.02);
-      }
-
-      let time = require("internal").time;
-      let start = time();
-      // this task keeps reading document "runner1"
-      do {
-        assertTrue(comm.exists("runner1"));
-      } while (time() - start < 10.0);
-
-      while (true) {
-        try {
-          tasks.get(task);
-          require("internal").wait(0.25, false);
-        } catch (err) {
-          // "task not found" means the task is finished
-          break;
         }
       }
     },
