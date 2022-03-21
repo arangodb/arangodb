0690cff652f77fb6e27d7e8e109369a2640f0fd8
Wilfried Goesgens <willi@arangodb.com>
2022-03-21 15:53:21 +0100

    Update js/client/modules/@arangodb/testutils/testing.js
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- js/client/modules/@arangodb/testing.js
+++ js/client/modules/@arangodb/testing.js
@@ -74,7 +74,7 @@ let optionsDocumentation = [
   '   - `loopEternal`: to loop one test over and over.',
   '   - `loopSleepWhen`: sleep every nth iteration',
   '   - `loopSleepSec`: sleep seconds between iterations',
-  '   - `setInterrupteable`: register special break handler ',
+  '   - `setInterruptable`: register special break handler',
   '   - `sleepBeforeStart` : sleep at tcpdump info - use this dump traffic or attach debugger',
   '',
   '   - `storageEngine`: set to `rocksdb` - defaults to `rocksdb`',
