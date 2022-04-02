35307516a234ad35494c4ad2672c4419087e6202
Wilfried Goesgens <willi@arangodb.com>
2022-04-02 15:24:12 +0200

    Update js/client/modules/@arangodb/testutils/test-utils.js
    Co-authored-by: Aditya Mukhopadhyay <aditya@arangodb.com>

--- js/client/modules/@arangodb/testutils/test-utils.js
+++ js/client/modules/@arangodb/testutils/test-utils.js
@@ -476,8 +476,8 @@ class runInArangoshRunner extends testRunnerBase{
 // //////////////////////////////////////////////////////////////////////////////
 
 class runLocalInArangoshRunner extends testRunnerBase{
-  constructor(options, testname, serverOptions, checkUsers=true, checkCollections=true) {
-    super(options, testname, serverOptions, checkUsers, checkCollections);
+  constructor(options, testname, ...optionalArgs) {
+    super(options, testname, ...optionalArgs);
     this.info = "localArangosh";
   }
   runOneTest(file) {
