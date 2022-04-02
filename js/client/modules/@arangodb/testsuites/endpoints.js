ad604720e2f294308d5d88cb231fc00f35ead20a
Wilfried Goesgens <willi@arangodb.com>
2022-04-02 15:31:35 +0200

    Update js/client/modules/@arangodb/testsuites/endpoints.js
    Co-authored-by: Aditya Mukhopadhyay <aditya@arangodb.com>

--- js/client/modules/@arangodb/testsuites/endpoints.js
+++ js/client/modules/@arangodb/testsuites/endpoints.js
@@ -52,8 +52,8 @@ const testPaths = {
 };
 
 class endpointRunner extends tu.runInArangoshRunner {
-  constructor(options, testname, serverOptions, checkUsers=true, checkCollections=true) {
-    super(options, testname, serverOptions, checkUsers, checkCollections);
+  constructor(options, testname, ...optionalArgs) {
+    super(options, testname, ...optionalArgs);
     this.info = "runImport";
   }
   
