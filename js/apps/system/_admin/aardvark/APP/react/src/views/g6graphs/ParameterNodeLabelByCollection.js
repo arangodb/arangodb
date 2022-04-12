ce407d163f780db34c487a52d2af7a2cf7509947
Andreas Jurdak <andreas@jurdak.at>
2022-04-12 15:59:17 +0200

    [FE-8] Refactor node label by collection form field

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeLabelByCollection.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeLabelByCollection.js
@@ -1,23 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Switch, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
 const ParameterNodeLabelByCollection = () => {
-  const urlParameters = useContext(UrlParametersContext);
+  const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
+  const [nodeLabelByCollection, setNodeLabelByCollection] = useState(urlParameters.nodeLabelByCollection);
 
-  const onNodeLabelByCollectionChange = (checked) => {
-    urlParameters.nodeLabelByCollection = checked;
-  }
-  
+  const NEWURLPARAMETERS = { ...urlParameters };
+
   return (
     <>
-      <Switch
-        checkedChildren="Show collection name"
-        unCheckedChildren="Hide collection name"
-        onChange={onNodeLabelByCollectionChange}
-        style={{ marginTop: '24px' }}
-      />
+      <label>
+        <input
+          type="checkbox"
+          checked={nodeLabelByCollection}
+          onChange={() => {
+            const newNodeLabelByCollection = !nodeLabelByCollection;
+            setNodeLabelByCollection(newNodeLabelByCollection);
+            NEWURLPARAMETERS.nodeLabelByCollection = newNodeLabelByCollection;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}
+        />
+        Show collection name
+      </label>
+      <p>Do we show the collection name? {nodeLabelByCollection.toString()}</p>
       <Tooltip title="Append collection name to the label?">
         <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)', marginTop: '24px' }} />
       </Tooltip>
