b373b127bdb737c7ef46f091acb2561f5a2fe0b6
Tobias Gödderz <tobias@arangodb.com>
2022-04-13 16:12:02 +0200

    Removed explicit construction of fu2

--- arangod/RocksDBEngine/RocksDBPersistedLog.cpp
+++ arangod/RocksDBEngine/RocksDBPersistedLog.cpp
@@ -239,10 +239,10 @@ void RocksDBLogPersistor::runPersistorWorker(Lane& lane) noexcept {
 
         // resolve all promises in [nextReqToResolve, nextReqToWrite)
         for (; nextReqToResolve != nextReqToWrite; ++nextReqToResolve) {
-          _executor->operator()(fu2::unique_function<void() noexcept>{
+          _executor->operator()(
               [reqToResolve = std::move(*nextReqToResolve)]() mutable noexcept {
                 reqToResolve.promise.setValue(TRI_ERROR_NO_ERROR);
-              }});
+              });
         }
       }
 
@@ -257,12 +257,11 @@ void RocksDBLogPersistor::runPersistorWorker(Lane& lane) noexcept {
         // should always be increased as well; meaning we only exactly iterate
         // over the unfulfilled promises here.
         TRI_ASSERT(!nextReqToResolve->promise.isFulfilled());
-        _executor->operator()(fu2::unique_function<void() noexcept>{
-            [reqToResolve = std::move(*nextReqToResolve),
-             result = result]() mutable noexcept {
-              TRI_ASSERT(!reqToResolve.promise.isFulfilled());
-              reqToResolve.promise.setValue(std::move(result));
-            }});
+        _executor->operator()([reqToResolve = std::move(*nextReqToResolve),
+                               result = result]() mutable noexcept {
+          TRI_ASSERT(!reqToResolve.promise.isFulfilled());
+          reqToResolve.promise.setValue(std::move(result));
+        });
       }
     }
   }
