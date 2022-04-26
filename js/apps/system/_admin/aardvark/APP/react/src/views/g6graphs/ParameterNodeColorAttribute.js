0b9b3202ee9bcc5e50923499b763bc77f91b8d55
Andreas Jurdak <andreas@jurdak.at>
2022-04-26 19:45:40 +0200

    [FE-8] Make node color attribute input conditional to color by collection parameter

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
@@ -1,28 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
 const ParameterNodeColorAttribute = () => {
-  const urlParameters = useContext(UrlParametersContext);
+  const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
+  const [nodeColorAttribute, setNodeColorAttribute] = useState(urlParameters.nodeColorAttribute);
 
-  const onNodeColorAttributeChange = (e) => {
-    urlParameters.nodeColorAttribute = e.target.value;
-  }
+  const NEWURLPARAMETERS = { ...urlParameters };
   
   return (
     <>
-      <Input
-        addonBefore="Color attribute"
-        placeholder={urlParameters.nodeColorAttribute || ''}
-        onChange={onNodeColorAttributeChange}
-        suffix={
-          <Tooltip title="If an attribute is given, nodes will then be colorized by the attribute. This setting ignores default node color if set.">
-            <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-          </Tooltip>
-        }
-        style={{ width: 240, marginTop: '24px' }}
-      />
+      <form>
+        <label>
+          Node color attribute:
+          <input
+            type="text"
+            value={nodeColorAttribute}
+            onChange={(e) => {
+              setNodeColorAttribute(e.target.value);
+              NEWURLPARAMETERS.nodeColorAttribute = e.target.value;
+              setUrlParameters(NEWURLPARAMETERS);
+            }}
+            disabled={urlParameters.nodeColorByCollection}
+          />
+        </label>
+      </form>
     </>
   );
 };
