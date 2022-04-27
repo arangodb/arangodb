f975a1c5d1a4be22bfce8d6ea024dc61c22dd52e
Tobias Gödderz <tobias@arangodb.com>
2022-04-27 16:49:12 +0200

    Moved code into .cpp file

--- tests/Basics/ThreadTestHelper.cpp
+++ tests/Basics/ThreadTestHelper.cpp
@@ -22,3 +22,52 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "ThreadTestHelper.h"
+
+#include <gtest/gtest.h>
+
+using namespace arangodb::tests;
+
+void WorkerThread::run() {
+  // This can't be done in the constructor (shared_from_this)
+  _thread = std::thread([self = this->shared_from_this()] {
+    auto guard = std::unique_lock(self->_mutex);
+
+    auto wait = [&] {
+      self->_cv.wait(guard, [&] {
+        return self->_callback != nullptr || self->_stopped;
+      });
+    };
+
+    for (wait(); !self->_stopped; wait()) {
+      self->_callback.operator()();
+      self->_callback = {};
+    }
+  });
+}
+
+void WorkerThread::execute(std::function<void()> callback) {
+  ASSERT_TRUE(_thread.joinable());
+  ASSERT_TRUE(callback);
+  {
+    auto guard = std::unique_lock(_mutex);
+    ASSERT_TRUE(!_stopped);
+    ASSERT_FALSE(_callback);
+    std::swap(_callback, callback);
+  }
+
+  _cv.notify_one();
+}
+
+void WorkerThread::stop() {
+  {
+    auto guard = std::unique_lock(_mutex);
+    _stopped = true;
+  }
+
+  _cv.notify_one();
+}
+
+void arangodb::tests::operator<<(WorkerThread& workerThread,
+                                 std::function<void()> callback) {
+  return workerThread.execute(std::move(callback));
+}
--- tests/Basics/ThreadTestHelper.h
+++ tests/Basics/ThreadTestHelper.h
@@ -23,8 +23,6 @@
 
 #pragma once
 
-#include <gtest/gtest.h>
-
 #include <condition_variable>
 #include <functional>
 #include <mutex>
@@ -41,45 +39,11 @@ namespace arangodb::tests {
 struct WorkerThread : std::enable_shared_from_this<WorkerThread> {
   WorkerThread() = default;
 
-  void run() {
-    // This can't be done in the constructor (shared_from_this)
-    _thread = std::thread([self = this->shared_from_this()] {
-      auto guard = std::unique_lock(self->_mutex);
+  void run();
 
-      auto wait = [&] {
-        self->_cv.wait(guard, [&] {
-          return self->_callback != nullptr || self->_stopped;
-        });
-      };
+  void execute(std::function<void()> callback);
 
-      for (wait(); !self->_stopped; wait()) {
-        self->_callback.operator()();
-        self->_callback = {};
-      }
-    });
-  }
-
-  void execute(std::function<void()> callback) {
-    ASSERT_TRUE(_thread.joinable());
-    ASSERT_TRUE(callback);
-    {
-      auto guard = std::unique_lock(_mutex);
-      ASSERT_TRUE(!_stopped);
-      ASSERT_FALSE(_callback);
-      std::swap(_callback, callback);
-    }
-
-    _cv.notify_one();
-  }
-
-  void stop() {
-    {
-      auto guard = std::unique_lock(_mutex);
-      _stopped = true;
-    }
-
-    _cv.notify_one();
-  }
+  void stop();
 
  private:
   std::thread _thread;
@@ -89,8 +53,6 @@ struct WorkerThread : std::enable_shared_from_this<WorkerThread> {
   bool _stopped = false;
 };
 
-auto operator<<(WorkerThread& workerThread, std::function<void()> callback) {
-  return workerThread.execute(std::move(callback));
-}
+void operator<<(WorkerThread& workerThread, std::function<void()> callback);
 
 }  // namespace arangodb::tests
