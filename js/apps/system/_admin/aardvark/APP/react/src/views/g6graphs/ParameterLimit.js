5a1614ef7720e33885cd20c55c4962dcffbdd464
Andreas Jurdak <andreas@jurdak.at>
2022-04-29 16:05:56 +0200

    [FE-8] Refactor limit parameter to textinput template

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterLimit.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterLimit.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import Textinput from "../../components/pure-css/form/Textinput";
 
 const ParameterLimit = () => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
@@ -12,10 +13,8 @@ const ParameterLimit = () => {
   /*
   <h5>urlParameters (in ParameterLimit): {JSON.stringify(urlParameters)}</h5>
   */
-  
-  return (
-    <>
-      <form>
+
+  /*
         <label>
           Limit:
           <input
@@ -28,6 +27,20 @@ const ParameterLimit = () => {
             }}
           />
         </label>
+        */
+  
+  return (
+    <>
+      <form>
+        <Textinput
+          label={'Limit'}
+          value={limit}
+          onChange={(e) => {
+            setLimit(+e.target.value);
+            NEWURLPARAMETERS.limit = +e.target.value;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}>
+        </Textinput>
       </form>
     </>
   );
