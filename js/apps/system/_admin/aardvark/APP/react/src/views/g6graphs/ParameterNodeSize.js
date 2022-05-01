e3a6642d0b5b5f5924d4205015ba3b7c48e07280
Andreas Jurdak <andreas@jurdak.at>
2022-05-02 01:44:02 +0200

    [FE-8] Refactor node sizing attribute parameter to textinput template

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeSize.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeSize.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import Textinput from "../../components/pure-css/form/Textinput";
 
 const ParameterNodeSize = () => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
@@ -9,10 +10,8 @@ const ParameterNodeSize = () => {
 
   const NEWURLPARAMETERS = { ...urlParameters };
 
-  return (
-    <>
-      <form>
-        <label>
+  /*
+  <label>
           Sizing attribute:
           <input
             type="text"
@@ -22,9 +21,23 @@ const ParameterNodeSize = () => {
               NEWURLPARAMETERS.nodeSize = e.target.value;
               setUrlParameters(NEWURLPARAMETERS);
             }}
-            disabled={urlParameters.nodeSizeByEdges}
           />
         </label>
+  */
+
+  return (
+    <>
+      <form>
+        <Textinput
+          label={'Sizing attribute'}
+          value={nodeSize}
+          onChange={(e) => {
+            setNodeSize(e.target.value);
+            NEWURLPARAMETERS.nodeSize = e.target.value;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}
+          disabled={urlParameters.nodeSizeByEdges}>
+        </Textinput>
       </form>
     </>
   );
