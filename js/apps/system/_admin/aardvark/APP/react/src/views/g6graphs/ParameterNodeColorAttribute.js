c94b13367f155d585779da775278957d64c1c6fd
Andreas Jurdak <andreas@jurdak.at>
2022-05-02 01:39:19 +0200

    [FE-8] Refactor node color attribute parameter to textinput template

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
@@ -2,17 +2,16 @@ import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import Textinput from "../../components/pure-css/form/Textinput";
 
 const ParameterNodeColorAttribute = () => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
   const [nodeColorAttribute, setNodeColorAttribute] = useState(urlParameters.nodeColorAttribute);
 
   const NEWURLPARAMETERS = { ...urlParameters };
-  
-  return (
-    <>
-      <form>
-        <label>
+
+  /*
+  <label>
           Node color attribute:
           <input
             type="text"
@@ -25,6 +24,20 @@ const ParameterNodeColorAttribute = () => {
             disabled={urlParameters.nodeColorByCollection}
           />
         </label>
+  */
+  
+  return (
+    <>
+      <form>
+        <Textinput
+          label={'Node color attribute'}
+          value={nodeColorAttribute}
+          onChange={(e) => {
+            setNodeColorAttribute(e.target.value);
+            NEWURLPARAMETERS.nodeColorAttribute = e.target.value;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}>
+        </Textinput>
       </form>
     </>
   );
