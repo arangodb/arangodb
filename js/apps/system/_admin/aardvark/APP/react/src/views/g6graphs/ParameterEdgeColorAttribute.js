6ae4c0f97395cde3d08ba1dd4eb9ea212bf4c186
Andreas Jurdak <andreas@jurdak.at>
2022-05-02 01:47:01 +0200

    [FE-8] Refactor edge color attribute parameter to textinput template

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterEdgeColorAttribute.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterEdgeColorAttribute.js
@@ -2,17 +2,16 @@ import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import Textinput from "../../components/pure-css/form/Textinput";
 
 const ParameterEdgeColorAttribute = () => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
   const [edgeColorAttribute, setEdgeColorAttribute] = useState(urlParameters.edgeColorAttribute);
 
   const NEWURLPARAMETERS = { ...urlParameters };
-  
-  return (
-    <>
-      <form>
-        <label>
+
+  /*
+  <label>
           Edge color attribute:
           <input
             type="text"
@@ -25,6 +24,21 @@ const ParameterEdgeColorAttribute = () => {
             disabled={urlParameters.edgeColorByCollection}
           />
         </label>
+  */
+  
+  return (
+    <>
+      <form>
+        <Textinput
+          label={'Edge color attribute'}
+          value={edgeColorAttribute}
+          onChange={(e) => {
+            setEdgeColorAttribute(e.target.value);
+            NEWURLPARAMETERS.edgeColorAttribute = e.target.value;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}
+          disabled={urlParameters.edgeColorByCollection}>
+        </Textinput>
       </form>
     </>
   );
