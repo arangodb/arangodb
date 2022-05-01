8c43e7691ea8d8428f3747b473b9b06ff27e7ded
Andreas Jurdak <andreas@jurdak.at>
2022-05-02 01:14:25 +0200

    [FE-8] Refactor depth parameter to textinput template

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterDepth.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterDepth.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
 import { Input, Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import Textinput from "../../components/pure-css/form/Textinput";
 
 const ParameterDepth = () => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
@@ -11,12 +12,7 @@ const ParameterDepth = () => {
 
   /*
   <h5>urlParameters (context value): {JSON.stringify(urlParameters)}</h5>
-  */
-  
-  return (
-    <>
-      <form>
-        <label>
+  <label>
           Depth:
           <input
             type="text"
@@ -28,6 +24,20 @@ const ParameterDepth = () => {
             }}
           />
         </label>
+  */
+  
+  return (
+    <>
+      <form>
+        <Textinput
+          label={'Depth'}
+          value={depth}
+          onChange={(e) => {
+            setDepth(+e.target.value);
+            NEWURLPARAMETERS.depth = +e.target.value;
+            setUrlParameters(NEWURLPARAMETERS);
+          }}>
+        </Textinput>
       </form>
     </>
   );
