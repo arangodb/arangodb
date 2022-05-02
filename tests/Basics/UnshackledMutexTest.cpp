300fc2062077091f3453361b6938a2b1e89f2eaa
Tobias Gödderz <tobias@arangodb.com>
2022-05-02 10:20:36 +0200

    Reverted UnshackledConditionVariable

--- tests/Basics/UnshackledMutexTest.cpp
+++ tests/Basics/UnshackledMutexTest.cpp
@@ -33,8 +33,6 @@
 #include <thread>
 #include <vector>
 
-namespace {
-
 /**
  * @brief The constructor of WorkerThread starts a thread, which immediately
  * starts waiting on a condition variable. The execute() method takes a
@@ -44,11 +42,45 @@ namespace {
 struct WorkerThread : std::enable_shared_from_this<WorkerThread> {
   WorkerThread() = default;
 
-  void run();
+  void run() {
+    // This can't be done in the constructor (shared_from_this)
+    _thread = std::thread([self = this->shared_from_this()] {
+      auto guard = std::unique_lock(self->_mutex);
 
-  void execute(std::function<void()> callback);
+      auto wait = [&] {
+        self->_cv.wait(guard, [&] {
+          return self->_callback != nullptr || self->_stopped;
+        });
+      };
 
-  void stop();
+      for (wait(); !self->_stopped; wait()) {
+        self->_callback.operator()();
+        self->_callback = {};
+      }
+    });
+  }
+
+  void execute(std::function<void()> callback) {
+    ASSERT_TRUE(_thread.joinable());
+    ASSERT_TRUE(callback);
+    {
+      auto guard = std::unique_lock(_mutex);
+      ASSERT_TRUE(!_stopped);
+      ASSERT_FALSE(_callback);
+      std::swap(_callback, callback);
+    }
+
+    _cv.notify_one();
+  }
+
+  void stop() {
+    {
+      auto guard = std::unique_lock(_mutex);
+      _stopped = true;
+    }
+
+    _cv.notify_one();
+  }
 
  private:
   std::thread _thread;
@@ -58,48 +90,7 @@ struct WorkerThread : std::enable_shared_from_this<WorkerThread> {
   bool _stopped = false;
 };
 
-void operator<<(WorkerThread& workerThread, std::function<void()> callback);
-
-void WorkerThread::run() {
-  // This can't be done in the constructor (shared_from_this)
-  _thread = std::thread([self = this->shared_from_this()] {
-    auto guard = std::unique_lock(self->_mutex);
-
-    auto wait = [&] {
-      self->_cv.wait(
-          guard, [&] { return self->_callback != nullptr || self->_stopped; });
-    };
-
-    for (wait(); !self->_stopped; wait()) {
-      self->_callback.operator()();
-      self->_callback = {};
-    }
-  });
-}
-
-void WorkerThread::execute(std::function<void()> callback) {
-  ASSERT_TRUE(_thread.joinable());
-  ASSERT_TRUE(callback);
-  {
-    auto guard = std::unique_lock(_mutex);
-    ASSERT_TRUE(!_stopped);
-    ASSERT_FALSE(_callback);
-    std::swap(_callback, callback);
-  }
-
-  _cv.notify_one();
-}
-
-void WorkerThread::stop() {
-  {
-    auto guard = std::unique_lock(_mutex);
-    _stopped = true;
-  }
-
-  _cv.notify_one();
-}
-
-void operator<<(WorkerThread& workerThread, std::function<void()> callback) {
+auto operator<<(WorkerThread& workerThread, std::function<void()> callback) {
   return workerThread.execute(std::move(callback));
 }
 
@@ -130,8 +121,6 @@ enum Thread {
   PSI = 22,
   OMEGA = 23,
 };
-
-}  // namespace
 
 // Note that this test will probably succeed even for std::mutex or others,
 // unless you run it with TSan.
