645f0134e5b9311003e05a57875a52092b957792
Andrei Lobov <andrei.lobov@arangodb.com>
2022-05-02 11:39:10 +0300

    force tick sync

--- arangod/RocksDBEngine/RocksDBCollection.cpp
+++ arangod/RocksDBEngine/RocksDBCollection.cpp
@@ -572,11 +572,22 @@ std::shared_ptr<Index> RocksDBCollection::createIndex(VPackSlice const& info,
     arangodb::aql::PlanCache::instance()->invalidate(vocbase);
 #endif
 
-    // force index creation commit
-    newIdx->waitForSync();
+    bool alreadySynced{false};
+    // FIXME: Better a virtual method. As Inverted index should have the same.
+    if (newIdx->type() == Index::TRI_IDX_TYPE_IRESEARCH_LINK) {
+      // force index creation commit
+      newIdx->waitForSync();
+      // force tick release, so we will not double the inserts on recovery!
+      res = engine.settingsManager()->sync(true);
+      if (res.fail()) {
+        return res;
+      }
+      alreadySynced = true;
+    }
 
     // inBackground index might not recover selectivity estimate w/o sync
-    if (inBackground && !newIdx->unique() && newIdx->hasSelectivityEstimate()) {
+    if (!alreadySynced && inBackground && !newIdx->unique() &&
+        newIdx->hasSelectivityEstimate()) {
       engine.settingsManager()->sync(false);
     }
 
