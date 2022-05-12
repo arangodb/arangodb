f0b3497e753b58f3df86111eaab681dce88e194e
Andrei Lobov <andrei.lobov@arangodb.com>
2022-05-12 15:59:24 +0300

    moar fixes

--- arangod/IResearch/IResearchLink.cpp
+++ arangod/IResearch/IResearchLink.cpp
@@ -846,7 +846,7 @@ Result IResearchLink::commitUnsafe(bool wait, CommitResult* code) {
 
       // no changes, can release the latest tick before commit
       impl.tick(lastTickBeforeCommit);
-
+      _lastCommittedTick = lastTickBeforeCommit;
       return {};
     }
 
