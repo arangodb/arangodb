9d2e41fa453dfa715df5d1866c61309e301e3830
roman <roman@arangodb.com>
2022-05-12 19:12:07 +0200

    Corrected the last merge

--- arangod/Graph/Providers/ProviderTracer.h
+++ arangod/Graph/Providers/ProviderTracer.h
@@ -95,7 +95,7 @@ class ProviderTracer {
   void prepareContext(aql::InputAqlItemRow input);
   void unPrepareContext();
   bool isResponsible(Step const& step) const;
-[nodiscard]] bool hasDepthSpecificLookup(uint64_t depth) const noexcept;
+  [[nodiscard]] bool hasDepthSpecificLookup(uint64_t depth) const noexcept;
 
  private:
   ProviderImpl _impl;
