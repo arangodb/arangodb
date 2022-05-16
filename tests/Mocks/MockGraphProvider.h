0c7698d1f1869123aad07e47ae7ff99c87a64241
Michael Hackstein <michael@arangodb.com>
2022-05-16 15:52:36 +0200

    CLangFormat

--- tests/Mocks/MockGraphProvider.h
+++ tests/Mocks/MockGraphProvider.h
@@ -293,7 +293,7 @@ class MockGraphProvider {
                           arangodb::velocypack::Builder& builder);
   void addEdgeToLookupMap(typename Step::Edge const& edge,
                           arangodb::velocypack::Builder& builder);
-  
+
   std::string getEdgeId(Step::Edge const& edge);
   velocypack::HashedStringRef getEdgeIdRef(Step::Edge const& edge);
 
