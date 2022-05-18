687ecbd7cfa1067cdd986b97fb921106522d2dee
Wilfried Goesgens <willi@arangodb.com>
2022-05-18 09:51:25 +0200

    rework drivers

--- js/client/modules/@arangodb/testsuites/permissions_client.js
+++ js/client/modules/@arangodb/testsuites/permissions_client.js
@@ -68,8 +68,19 @@ class permissionsRunner extends tu.runInArangoshRunner {
     let filtered = {};
     let rootDir = fs.join(fs.getTempPath(), 'permissions');
     const tests = tu.scanTestPaths(testPaths.permissions, this.options);
-    this.instanceInfo = {
+    this.instanceManager = {
+      rootDir: rootDir,
       endpoint: 'tcp://127.0.0.1:8888',
+      findEndpoint: function() {
+        return 'tcp://127.0.0.1:8888';
+      },
+      getStructure: function() {
+        print(rootDir)
+        return {
+          endpoint: 'tcp://127.0.0.1:8888',
+          rootDir: rootDir
+        };
+      }
     };
 
     fs.makeDirectoryRecursive(rootDir);
@@ -78,10 +89,10 @@ class permissionsRunner extends tu.runInArangoshRunner {
         let t = f.split(fs.pathSeparator);
         let testName = t[t.length - 1].replace(/\.js/, '');
         let instanceRoot = fs.join(rootDir, testName);
-        obj.instanceInfo['rootDir'] = instanceRoot;
+        fs.makeDirectoryRecursive(instanceRoot);
+        obj.instanceManager['rootDir'] = instanceRoot;
         let testResultJson = fs.join(instanceRoot, 'testresult.json');;
         process.env['RESULT'] = testResultJson;
-        fs.makeDirectoryRecursive(instanceRoot);
         pu.cleanupDBDirectoriesAppend(instanceRoot);
 
         let content = fs.read(f);
