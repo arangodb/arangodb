bc1d439e4a077fa85748e1eab3b925f84f726c71
Frank Celler <frank@arangodb.com>
2022-05-18 21:42:49 +0200

    added gtest_replication2

--- tests/Definition/rlog/pr.fish
+++ tests/Definition/rlog/pr.fish
@@ -1,5 +1,6 @@
 set TESTS ""
 
+set TESTS "$TESTS""250,runSingleTest1 gtest_replication2 -\n"
 set TESTS "$TESTS""250,runClusterTest1 replication2_server -\n"
 set TESTS "$TESTS""250,runClusterTest1 replication2_client -\n"
 set TESTS "$TESTS""250,runClusterTest1 auto - --test tests/js/common/shell/shell-replicated-logs-cluster.js\n"
