f84eefd5f57d889f41a2adc1e53cf6816c5eaff7
Frank Celler <frank@arangodb.com>
2022-05-25 13:55:49 +0200

    added cluster

--- tests/Definition/rlog/pr.ps1
+++ tests/Definition/rlog/pr.ps1
@@ -7,8 +7,8 @@ Function global:registerTests()
     $global:TESTSUITE_TIMEOUT = 3900
 
     registerTest -testname "gtest_replication2"
-    registerTest -testname "replication2_server"
-    registerTest -testname "replication2_client" -filter "tests/js/common/shell/shell-replicated-logs-cluster.js"
-    registerTest -testname "replication2_client" -filter "tests/js/common/shell/shell-prototype-state-cluster.js"
+    registerTest -testname "replication2_server" -cluster $true
+    registerTest -testname "replication2_client" -cluster $true -filter "tests/js/common/shell/shell-replicated-logs-cluster.js"
+    registerTest -testname "replication2_client" -cluster $true -filter "tests/js/common/shell/shell-prototype-state-cluster.js"
     comm
 }
