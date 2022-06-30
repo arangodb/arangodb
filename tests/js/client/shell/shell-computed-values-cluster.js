4b0b54ba6ef7dc5ee4ce32572702835859d1aa6c
cpjulia <julia@arangodb.com>
2022-06-30 10:49:41 -0300

    Fixed test for RAND() on cluster

--- tests/js/client/shell/shell-computed-values-cluster.js
+++ tests/js/client/shell/shell-computed-values-cluster.js
@@ -41,166 +41,60 @@ const {
   getDBServers
 } = require('@arangodb/test-helper');
 
-let clearFailurePoints = function(failurePointsToKeep) {
-  getDBServers().forEach((server) => {
-    // clear all failure points
-    request({method: "DELETE", url: server.url + "/_admin/debug/failat"});
-    failurePointsToKeep.forEach((fp) => {
-      getDBServers().forEach((server) => {
-        let result = request({
-          method: "PUT",
-          url: server.url + "/_admin/debug/failat/" + encodeURIComponent(fp),
-          body: {}
-        });
-        assertEqual(200, result.status);
-      });
-    });
-  });
-};
-
-function BaseTestConfig() {
+function collectionComputedValuesClusterSuite() {
   'use strict';
 
   return {
-    testFailureOnLeaderNoManagedTrx: function() {
+
+    tearDown: function() {
+      db._drop(cn);
+    },
+
+    testComputedValuesWithRand: function() {
       let c = db._create(cn, {
-        numberOfShards: 1, computedValues: [{
-          name: "value2",
-          expression: "RETURN CONCAT(@doc.value1, '+', RAND())",
+        numberOfShards: 1, replicationFactor: 2, computedValues: [{
+          name: "randValue",
+          expression: "RETURN TO_STRING(RAND())",
           override: false
         }]
       });
+      let docs = [];
       for (let i = 0; i < 100; ++i) {
-        c.insert({value1: "test" + i});
+        docs.push({});
       }
-      assertEqual(100, c.count());
-      assertEqual(100, c.toArray().length);
-
-      let compuetdValuesBefore = [];
-      let result = db._query(`FOR doc IN ${cn} RETURN doc`).toArray();
-      result.forEach(el => {
-        assertTrue(el.hasOwnProperty("value2"));
-        compuetdValuesBefore.push(el.value2);
-      });
-
+      c.insert(docs);
 
       let servers = getDBServers();
       let shardInfo = c.shards(true);
       let shard = Object.keys(shardInfo)[0];
-      let leader = shardInfo[shard][0];
-      let leaderUrl = servers.filter((server) => server.id === leader)[0].url;
 
-      // break leaseManagedTrx on the leader, so it will return a nullptr
-      result = request({method: "PUT", url: leaderUrl + "/_admin/debug/failat/leaseManagedTrxFail", body: {}});
-      assertEqual(200, result.status);
+      let randValues = {};
 
-      // add a follower. this will kick off the getting-in-sync protocol,
-      // which will eventually call the holdReadLockCollection API, which then
-      // will call leaseManagedTrx and get the nullptr back
-      c.properties({numberOfShards: 1, replicationFactor: 2});
-
-      waitForShardsInSync(cn, 60);
-
-      // wait until we have an in-sync follower
-      let tries = 0;
-      while (tries++ < 120) {
-        shardInfo = c.shards(true);
-        servers = shardInfo[shard];
-        if (tries > 20) {
-          if (servers.length === 2 && c.count() === 100) {
-            break;
-          }
-        } else if (tries === 20) {
-          // wait several seconds so we can be sure the
-          clearFailurePoints([]);
+      getDBServers().forEach(server => {
+        if (servers.indexOf(server.id) === -1) {
+          return;
         }
-        require("internal").sleep(0.5);
-      }
-      assertEqual(2, servers.length);
-      assertEqual(100, c.count());
-      assertEqual(100, c.toArray().length);
+        randValues[server.id] = {};
 
-      tries = 0;
-      let total;
-      while (tries++ < 120) {
-        total = 0;
-        getDBServers().forEach((server) => {
-          if (servers.indexOf(server.id) === -1) {
-            return;
-          }
-          let result = request({method: "GET", url: server.url + "/_api/collection/" + shard + "/count"});
-          assertEqual(200, result.status);
-          total += result.json.count;
+        let result = request({method: "GET", url: server.url + "/_api/collection/" + shard + "/properties"});
+        let jsonRes = result.json;
+        assertTrue(jsonRes.hasOwnProperty("computedValues"));
+        assertEqual(jsonRes.computedValues.length, 1);
+        assertTrue(jsonRes.computedValues[0].hasOwnProperty("name"));
+        assertEqual(jsonRes.computedValues[0].name, "value2");
 
-          result = request({method: "GET", url: server.url + "/_api/collection/" + shard + "/properties"});
-          let jsonRes = result.json;
-          assertTrue(jsonRes.hasOwnProperty("computedValues"));
-          assertEqual(jsonRes.computedValues.length, 1);
-          assertTrue(jsonRes.computedValues[0].hasOwnProperty("name"));
-          assertEqual(jsonRes.computedValues[0].name, "value2");
-
+        c.toArray().forEach(el => {
+          result = request({method: "GET", url: server.url + "/_api/document/" + el._id});
+          jsonRes = result.json;
+          assertTrue(jsonRes.hasOwnProperty("randValue"));
+          randValues[server.id][el._key] = el.randValue;
         });
 
-        if (total === 2 * 100) {
-          break;
-        }
-
-        require("internal").sleep(0.5);
-      }
-
-         assertEqual(2 * 100, total);
-      result = db._query(`FOR doc IN ${cn} RETURN doc`).toArray();
-      result.forEach((el, index) => {
-        assertTrue(el.hasOwnProperty("value2"));
-        assertEqual(compuetdValuesBefore[index], el.value2);
       });
-
+      assertEqual(randValues[servers[0].id], randValues[servers[1].id]);
     },
   };
 }
 
-function collectionCountsSuiteOldFormat() {
-  'use strict';
-
-  let suite = {
-    setUp: function() {
-      // this disables usage of the new collection format
-      clearFailurePoints(["disableRevisionsAsDocumentIds"]);
-      db._drop(cn);
-    },
-
-    tearDown: function() {
-      clearFailurePoints([]);
-      db._drop(cn);
-    }
-  };
-
-  deriveTestSuite(BaseTestConfig(), suite, '_OldFormat');
-  return suite;
-}
-
-function collectionCountsSuiteNewFormat() {
-  'use strict';
-
-  let suite = {
-    setUp: function() {
-      clearFailurePoints([]);
-      db._drop(cn);
-    },
-
-    tearDown: function() {
-      clearFailurePoints([]);
-      db._drop(cn);
-    }
-  };
-
-  deriveTestSuite(BaseTestConfig(), suite, '_NewFormat');
-  return suite;
-}
-
-let res = request({method: "GET", url: getCoordinators()[0].url + "/_admin/debug/failat"});
-if (res.body === "true") {
-  jsunity.run(collectionCountsSuiteOldFormat);
-  jsunity.run(collectionCountsSuiteNewFormat);
-}
+jsunity.run(collectionComputedValuesClusterSuite);
 return jsunity.done();
