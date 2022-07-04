dcc391d35ade46c4ebf9920214d2edfb7e944ce6
Andrey Abramov <andrey@arangodb.com>
2022-07-05 01:38:17 +0200

    clang-format

--- lib/Basics/StaticStrings.h
+++ lib/Basics/StaticStrings.h
@@ -128,10 +128,10 @@ class StaticStrings {
   // Index definition fields
   static std::string const
       IndexDeduplicate;  // index deduplicate flag (for array indexes)
-  static std::string const IndexExpireAfter;     // ttl index expire value
-  static std::string const IndexFields;          // index fields
-  static std::string const IndexId;              // index id
-  static std::string const IndexInBackground;    // index in background
+  static std::string const IndexExpireAfter;   // ttl index expire value
+  static std::string const IndexFields;        // index fields
+  static std::string const IndexId;            // index id
+  static std::string const IndexInBackground;  // index in background
   static std::string_view constexpr IndexParallelism = "parallelism";
   static std::string const IndexIsBuilding;      // index build in-process
   static std::string const IndexName;            // index name
