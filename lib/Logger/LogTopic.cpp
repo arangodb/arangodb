3ead3332c4b6c9b8a18b7a84b6fc012019e9f921
maierlars <lars@arangodb.com>
2022-07-12 13:46:32 +0200

    Disallow same term again.

--- lib/Logger/LogTopic.cpp
+++ lib/Logger/LogTopic.cpp
@@ -143,7 +143,7 @@ LogTopic Logger::PERFORMANCE("performance", LogLevel::WARN);
 LogTopic Logger::PREGEL("pregel", LogLevel::INFO);
 LogTopic Logger::QUERIES("queries", LogLevel::INFO);
 LogTopic Logger::REPLICATION("replication", LogLevel::INFO);
-LogTopic Logger::REPLICATION2("replication2", LogLevel::TRACE);
+LogTopic Logger::REPLICATION2("replication2", LogLevel::INFO);
 LogTopic Logger::REPLICATED_STATE("rep-state", LogLevel::DEBUG);
 LogTopic Logger::REQUESTS("requests", LogLevel::FATAL);  // suppress
 LogTopic Logger::RESTORE("restore", LogLevel::INFO);
