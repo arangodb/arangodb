7dc1d602164db2084573d2804479a1614b28c582
jsteemann <jsteemann@users.noreply.github.com>
2022-07-12 19:08:56 +0200

    fix test

--- tests/js/server/aql/aql-temp-rocksdb-storage.js
+++ tests/js/server/aql/aql-temp-rocksdb-storage.js
@@ -36,12 +36,17 @@ const errors = require('@arangodb').errors;
 function StorageForQueryWithCollectionSortSuite() {
   const cn = 'UnitTestCollection';
 
-  let assertOrder = (order, values) => {
-    let firstVal = values[0].value1;
-    values.forEach(el => {
+  let assertResult = (order, res, expectedCount) => {
+    assertTrue(res.hasNext());
+    let firstVal = res.next().value1;
+    let count = 1;
+    while (res.hasNext()) {
+      ++count;
+      let el = res.next();
       assertTrue(order === "asc" ? el.value1 >= firstVal : el.value1 <= firstVal, { order, el, firstVal });
       firstVal = el.value1;
-    });
+    }
+    assertEqual(count, expectedCount);
   };
 
   return {
@@ -52,7 +57,7 @@ function StorageForQueryWithCollectionSortSuite() {
 
       let docs = [];
       for (let i = 0; i < 500000; ++i) {
-        docs.push({value1: i, value2: "a" + i});
+        docs.push({value1: i});
         if (docs.length === 10000) {
           c.insert(docs);
           docs = [];
@@ -66,12 +71,14 @@ function StorageForQueryWithCollectionSortSuite() {
 
     testSortComparePeakMemoryUsageAscCollection: function() {
       let query = `FOR doc IN ${cn} SORT doc.value1 ASC RETURN doc`;
-      let res = db._query(query, null, {thresholdNumRows: 100000000, silent: true, options: { silent: true } });
+      let res = db._query(query, null, {thresholdNumRows: 100000000, stream: true });
+      assertResult("asc", res, 500000);
       let queryStats = res.getExtra().stats;
       assertTrue(queryStats.hasOwnProperty("peakMemoryUsage"));
       const memoryUsage1 = queryStats.peakMemoryUsage;
       
-      res = db._query(query, null, {thresholdNumRows: 10000, silent: true, options: { silent: true } });
+      res = db._query(query, null, {thresholdNumRows: 10000, stream: true });
+      assertResult("asc", res, 500000);
       queryStats = res.getExtra().stats;
       assertTrue(queryStats.hasOwnProperty("peakMemoryUsage"));
       assertTrue(queryStats.peakMemoryUsage * 2 < memoryUsage1, { current: queryStats.peakMemoryUsage, previous: memoryUsage1 });
@@ -79,43 +86,19 @@ function StorageForQueryWithCollectionSortSuite() {
 
     testSortComparePeakMemoryUsageDescCollection: function() {
       let query = `FOR doc IN ${cn} SORT doc.value1 DESC RETURN doc`;
-      let res = db._query(query, null, {thresholdNumRows: 100000000, silent: true, options: { silent: true } });
+      let res = db._query(query, null, {thresholdNumRows: 100000000, stream: true });
+      assertResult("desc", res, 500000);
       let queryStats = res.getExtra().stats;
       assertTrue(queryStats.hasOwnProperty("peakMemoryUsage"));
       const memoryUsage1 = queryStats.peakMemoryUsage;
       
-      res = db._query(query, null, {thresholdNumRows: 5000, silent: true, options: { silent: true } });
+      res = db._query(query, null, {thresholdNumRows: 5000, stream: true });
+      assertResult("desc", res, 500000);
       queryStats = res.getExtra().stats;
       assertTrue(queryStats.hasOwnProperty("peakMemoryUsage"));
       assertTrue(queryStats.peakMemoryUsage * 2 < memoryUsage1, { current: queryStats.peakMemoryUsage, previous: memoryUsage1 });
     },
     
-    testSortAscCollectionResults: function() {
-      let query = `FOR doc IN ${cn} SORT doc.value1 ASC RETURN doc`;
-      let res = db._query(query, null, {thresholdNumRows: 100000000});
-      let allDocs = res.toArray();
-      assertEqual(allDocs.length, 500000);
-      assertOrder("asc", allDocs);
-      
-      res = db._query(query, null, {thresholdNumRows: 10000});
-      allDocs = res.toArray();
-      assertEqual(allDocs.length, 500000);
-      assertOrder("asc", allDocs);
-    },
-
-    testSortDescCollectionResults: function() {
-      let query = `FOR doc IN ${cn} SORT doc.value1 DESC RETURN doc`;
-      let res = db._query(query, null, {thresholdNumRows: 100000000});
-      let allDocs = res.toArray();
-      assertEqual(allDocs.length, 500000);
-      assertOrder("desc", allDocs);
-      
-      res = db._query(query, null, {thresholdNumRows: 5000});
-      allDocs = res.toArray();
-      assertEqual(allDocs.length, 500000);
-      assertOrder("desc", allDocs);
-    },
-
   };
 }
 
