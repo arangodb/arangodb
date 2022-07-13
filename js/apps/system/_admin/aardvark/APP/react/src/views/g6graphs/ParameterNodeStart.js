852b5b7bc5b540d69d9636c9b7609b9d8321c05d
Andreas Jurdak <andreas@jurdak.at>
2022-07-13 13:03:05 +0200

    [FE-128] Switch datalist to text input and make it reactive

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeStart.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeStart.js
@@ -1,9 +1,8 @@
 import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
-import { Node } from './components/node/Node';
 import { Tooltip } from 'antd';
+import Textinput from "./components/pure-css/form/Textinput.tsx";
 import { InfoCircleFilled } from '@ant-design/icons';
-import PlainLabel from "./components/pure-css/form/PlainLabel";
 
 const ParameterNodeStart = ({ nodes, onNodeSelect }) => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
@@ -13,46 +12,19 @@ const ParameterNodeStart = ({ nodes, onNodeSelect }) => {
 
   return (
     <div style={{'marginTop': '24px'}}>
-      <PlainLabel htmlFor={'graphnodes'}>Start node</PlainLabel>
-      <div className="node-list">
-        <input
-          list="nodelist"
-          id="graphnodes"
-          name="graphnodes"
-          autocomplete="off"
-          value={nodeStart}
-          onChange={
-            (e) => {
-              setNodeStart(e.target.value);
-              newUrlParameters.nodeStart = e.target.value;
-              setUrlParameters(newUrlParameters);
-            }
-          }
-          style={{
-            'width': '500px',
-            'height': 'auto',
-            'margin-right': '8px',
-            'color': '#555555',
-            'border': '2px solid rgba(140, 138, 137, 0.25)',
-            'border-radius': '4px',
-            'background-color': '#fff !important',
-            'box-shadow': 'none',
-            'outline': 'none',
-            'outline-color': 'transparent',
-            'outline-style': 'none'
-          }}
-        />
-        <datalist id="nodelist">
-          {nodes.map(node => (
-            <Node
-              key={node.id}
-              node={node} />
-          ))}
-        </datalist>
-        <Tooltip placement="bottom" title={"A valid node ID or a space-separated list of IDs. If empty, a random node will be chosen."}>
-          <InfoCircleFilled style={{ fontSize: '12px', color: '#555555' }} />
-        </Tooltip>
-      </div>
+      <Textinput
+        label={'Start node'}
+        value={urlParameters.nodeStart}
+        width={'500px'}
+        onChange={(e) => {
+          setNodeStart(e.target.value);
+          newUrlParameters.nodeStart = e.target.value;
+          setUrlParameters(newUrlParameters);
+        }}>
+      </Textinput>
+      <Tooltip placement="bottom" title={"A valid node ID or a space-separated list of IDs. If empty, a random node will be chosen."}>
+        <InfoCircleFilled style={{ fontSize: '12px', color: '#555555' }} />
+      </Tooltip>
     </div>
   );
 };
