ed1fc6fecf2895bfabe6f03ef9e6b9521381a059
Vadim Kondratyev <vadim@arangodb.com>
2022-07-15 16:58:34 +0200

    Rebuild UI

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.03ba2f4830ffbb204e4373336c494d15.js"
+  "precache-manifest.8c2c0b9ec1853bfa61e11b4cd2aaca6f.js"
 );
 
 workbox.clientsClaim();
