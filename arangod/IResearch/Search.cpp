23319fb88ea609a25d1ae761114638bd3ddbcf62
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2022-07-21 12:11:40 +0300

    [Search-324] Fix slow search view creation (#16578)
    * Remove unnecessary sleep
    
    * Fix getCollection name for Cluster case

--- arangod/IResearch/Search.cpp
+++ arangod/IResearch/Search.cpp
@@ -53,7 +53,6 @@ std::shared_ptr<LogicalCollection> getCollection(
   if (cidOrName.isString()) {
     return resolver.getCollection(cidOrName.stringView());
   } else if (cidOrName.isNumber<DataSourceId::BaseType>()) {
-    std::this_thread::sleep_for(std::chrono::seconds{10});
     auto const cid = cidOrName.getNumber<DataSourceId::BaseType>();
     absl::AlphaNum const cidStr{cid};  // TODO make resolve by id?
     return resolver.getCollection(cidStr.Piece());
@@ -378,7 +377,7 @@ Result Search::appendVPackImpl(velocypack::Builder& build, Serialization ctx,
       for (auto& handle : handles) {
         if (auto index = handle->lock(); index) {
           if (ctx == Serialization::Properties) {
-            auto collectionName = resolver.getCollectionName(cid);
+            auto collectionName = resolver.getCollectionNameCluster(cid);
             // TODO(MBkkt) remove dynamic_cast
             auto* rawIndex = dynamic_cast<Index*>(index.get());
             if (collectionName.empty() || !rawIndex) {
