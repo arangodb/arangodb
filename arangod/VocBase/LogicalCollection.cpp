8dc314e7c984a31eaa8842056e52b2781ba5a245
Alex Petenchea <alex.petenchea@gmail.com>
2022-07-22 12:05:37 -0400

    Fixing formatting.

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -1301,15 +1301,13 @@ auto LogicalCollection::getDocumentStateLeader() -> std::shared_ptr<
     replication2::replicated_state::document::DocumentLeaderState> {
   auto stateMachine = getDocumentState();
 
-  static constexpr auto throwUnavailable =
-      []<typename... Args>(basics::SourceLocation location,
-                           fmt::format_string<Args...> formatString,
-                           Args&&... args) {
-        throw basics::Exception(
-            TRI_ERROR_REPLICATION_REPLICATED_STATE_NOT_AVAILABLE,
-            fmt::vformat(formatString, fmt::make_format_args(args...)),
-            location);
-      };
+  static constexpr auto throwUnavailable = []<typename... Args>(
+      basics::SourceLocation location, fmt::format_string<Args...> formatString,
+      Args && ... args) {
+    throw basics::Exception(
+        TRI_ERROR_REPLICATION_REPLICATED_STATE_NOT_AVAILABLE,
+        fmt::vformat(formatString, fmt::make_format_args(args...)), location);
+  };
 
   auto const status = stateMachine->getStatus();
   if (status == std::nullopt) {
