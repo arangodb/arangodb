83c7b4ee3e54fde60636e57a1b0bdf40b1582690
Valery Mironov <valera.mironow@gmail.com>
2022-08-02 16:27:17 +0300

    Try to fix probably bug in AsyncValue. It can be store buffering caused by acq_rel on atomic with mutex

--- arangod/IResearch/Containers.h
+++ arangod/IResearch/Containers.h
@@ -131,14 +131,14 @@ class AsyncValue {
   /// @brief Denies access to resource granted by a lock,
   ///        and waits until all locks on resource have been released.
   //////////////////////////////////////////////////////////////////////////////
-  void reset() {
+  void reset() noexcept {
     auto count = _count.fetch_or(kReset, std::memory_order_release);
     if (!(count & kReset)) {
       destroy();
     }
-    std::unique_lock lock{_m};
-    while (_resource != nullptr) {
-      _cv.wait(lock);
+    while ((count = _count.load(std::memory_order_acquire)) !=
+           (kReset | kDestroy)) {
+      _count.wait(count, std::memory_order_relaxed);
     }
   }
 
@@ -149,20 +149,16 @@ class AsyncValue {
 
   void destroy() {
     auto count = _count.fetch_sub(kRef, std::memory_order_release) - kRef;
-    if (count == kReset  // acquire for fetch_sub, release for fetch_add
-        && _count.compare_exchange_strong(count, kReset | kDestroy,
-                                          std::memory_order_acq_rel,
-                                          std::memory_order_relaxed)) {
-      std::lock_guard lock{_m};
-      _resource = nullptr;
-      _cv.notify_all();  // should be under lock
+    if (count == kReset &&
+        _count.compare_exchange_strong(count, kReset | kDestroy,
+                                       std::memory_order_acq_rel,
+                                       std::memory_order_relaxed)) {
+      _count.notify_all();
     }
   }
 
   T* _resource;
   mutable std::atomic_size_t _count;
-  std::mutex _m;
-  std::condition_variable _cv;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
