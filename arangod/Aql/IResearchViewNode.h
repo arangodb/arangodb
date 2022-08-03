a0c3406c72d93faa7dcba922ba6de5ff37a4a878
Andrey Abramov <andrey@arangodb.com>
2022-08-03 19:34:45 +0200

    Update arangod/Aql/IResearchViewNode.h
    Co-authored-by: Valery Mironov <32071355+MBkkt@users.noreply.github.com>

--- arangod/Aql/IResearchViewNode.h
+++ arangod/Aql/IResearchViewNode.h
@@ -308,7 +308,7 @@ class IResearchViewNode final : public aql::ExecutionNode {
     bool canVariablesBeReplaced(aql::CalculationNode* calculationNode) const;
 
     ViewVarsInfo replaceViewVariables(
-        std::span<aql::CalculationNode*> calcNodes,
+        std::span<aql::CalculationNode* const> calcNodes,
         containers::HashSet<ExecutionNode*>& toUnlink);
 
     ViewVarsInfo replaceAllViewVariables(
