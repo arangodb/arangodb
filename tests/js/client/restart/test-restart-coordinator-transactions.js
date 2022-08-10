d582385db8d554d36a27ad3621ecb4806016956a
jsteemann <jsteemann@users.noreply.github.com>
2022-08-10 11:17:14 +0200

    add restart test

---
+++ tests/js/client/restart/test-restart-coordinator-transactions.js
@@ -0,0 +1,163 @@
+/*jshint globalstrict:false, strict:false */
+/* global assertTrue, assertEqual, arango, fail */
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test for security-related server options
+///
+/// @file
+///
+/// DISCLAIMER
+///
+/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB Inc, Cologne, Germany
+///
+/// @author Jan Steemann
+/// @author Copyright 2019, ArangoDB Inc, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+let jsunity = require('jsunity');
+const _ = require('lodash');
+const pu = require('@arangodb/testutils/process-utils');
+const crypto = require('@arangodb/crypto');
+const request = require("@arangodb/request");
+const time = require("internal").time;
+const db = require("internal").db;
+const errors = require("internal").errors;
+
+const {
+  getCtrlCoordinators,
+  getCtrlDBServers
+} = require('@arangodb/test-helper');
+
+const cn = "UnitTestsCollection";
+
+function testSuite() {
+  let waitForAlive = function (timeout, baseurl, data) {
+    let tries = 0, res;
+    let all = Object.assign(data || {}, { method: "get", timeout: 1, url: baseurl + "/_api/version" }); 
+    const end = time() + timeout;
+    while (time() < end) {
+      res = request(all);
+      if (res.status === 200 || res.status === 401 || res.status === 403) {
+        break;
+      }
+      console.warn("waiting for server response from url " + baseurl);
+      require('internal').sleep(0.5);
+    }
+    return res.status;
+  };
+
+  let checkAvailability = function (servers, expectedCode) {
+    require("console").warn("checking (un)availability of " + servers.map((s) => s.url).join(", "));
+    servers.forEach(function(server) {
+      let res = request({ method: "get", url: server.url + "/_api/version", timeout: 3 });
+      assertEqual(expectedCode, res.status);
+    });
+  };
+
+  let suspend = function (servers) {
+    servers.forEach(function(server) {
+      server.suspend();
+    });
+  };
+  
+  let resume = function (servers) {
+    servers.forEach(function(server) {
+      server.resume();
+    });
+  };
+
+  return {
+    setUp : function() {
+      db._create(cn);
+    },
+
+    tearDown: function () {
+      db._drop(cn);
+    },
+
+    tearDownAll : function() {
+      /*
+      // Need to restart without authentication for other tests to succeed:
+      let coordinators = getCtrlCoordinators();
+      let coordinator = coordinators[0];
+      coordinator.exitStatus = null;
+      coordinator.shutdownArangod(false);
+      coordinator.waitForInstanceShutdown(30);
+      coordinator.pid = null;
+      console.warn("Cleaning up and restarting coordinator without authentication...", coordinator.getStructure());
+      coordinator.exitStatus = null;
+
+      coordinator.restartOneInstance({
+        "server.authentication": "false"
+      });
+      let aliveStatus = waitForAlive(30, coordinator.url, {});
+      assertEqual(200, aliveStatus);
+      */
+    },
+
+    testRestartCoordinatorNormal : function() {
+      let trx = db._createTransaction({ 
+        collections: { write: cn }
+      });
+       
+      let tc = trx.collection(cn);
+      tc.insert({ _key: "test1" });
+
+      let coordinators = getCtrlCoordinators();
+      assertTrue(coordinators.length > 0);
+      let coordinator = coordinators[0];
+      coordinator.shutdownArangod(false);
+      coordinator.waitForInstanceShutdown(30);
+      coordinator.exitStatus = null;
+      coordinator.pid = null;
+      console.warn("Restarting coordinator...", coordinator.getStructure());
+
+      coordinator.restartOneInstance({
+        "server.authentication": "false"
+      });
+        
+      waitForAlive(30, coordinator.url, {});
+       
+      // will fail, because the coordinator owning the
+      // transaction got restarted
+      try {
+        tc.insert({ _key: "test2" });
+        fail();
+      } catch (err) {
+        assertEqual(errors.ERROR_TRANSACTION_NOT_FOUND.code, err.errorNum);
+      }
+      
+
+      // we should be able to start a new transaction, however
+      trx = db._createTransaction({ 
+        collections: { write: cn }
+      });
+      
+      try {
+        tc = trx.collection(cn);
+        tc.insert({ _key: "test1" });
+
+        assertEqual(1, tc.count()); 
+      } finally {
+        trx.abort();
+      }
+    },
+    
+  };
+}
+jsunity.run(testSuite);
+return jsunity.done();
