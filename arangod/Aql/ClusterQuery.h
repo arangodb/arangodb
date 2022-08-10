af11ab08c566932a852abc8321e25085964912da
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2022-08-10 12:59:35 +0300

    Fix few tsan issues (#16710)

--- arangod/Aql/ClusterQuery.h
+++ arangod/Aql/ClusterQuery.h
@@ -27,44 +27,41 @@
 #include "Cluster/ClusterTypes.h"
 #include "Futures/Future.h"
 
-namespace arangodb {
-namespace aql {
+namespace arangodb::aql {
 
-// additonally contains TraversalEngines
-class ClusterQuery : public arangodb::aql::Query {
+// additionally contains TraversalEngines
+class ClusterQuery : public Query {
  protected:
   /// Used to construct a cluster query. the constructor is protected to ensure
   /// that call sites only create ClusterQuery objects using the `create`
   /// factory method
-  ClusterQuery(QueryId id, std::shared_ptr<arangodb::transaction::Context> ctx,
+  ClusterQuery(QueryId id, std::shared_ptr<transaction::Context> ctx,
                QueryOptions options);
 
- public:
-  ~ClusterQuery();
+  ~ClusterQuery() override;
 
+ public:
   /// @brief factory method for creating a cluster query. this must be used to
   /// ensure that ClusterQuery objects are always created using shared_ptrs.
   static std::shared_ptr<ClusterQuery> create(
       QueryId id, std::shared_ptr<transaction::Context> ctx,
       QueryOptions options);
 
-  traverser::GraphEngineList const& traversers() const { return _traversers; }
+  auto const& traversers() const { return _traversers; }
 
-  void prepareClusterQuery(
-      arangodb::velocypack::Slice querySlice,
-      arangodb::velocypack::Slice collections,
-      arangodb::velocypack::Slice variables,
-      arangodb::velocypack::Slice snippets,
-      arangodb::velocypack::Slice traversals,
-      arangodb::velocypack::Builder& answer,
-      arangodb::QueryAnalyzerRevisions const& analyzersRevision);
+  void prepareClusterQuery(velocypack::Slice querySlice,
+                           velocypack::Slice collections,
+                           velocypack::Slice variables,
+                           velocypack::Slice snippets,
+                           velocypack::Slice traversals,
+                           velocypack::Builder& answer,
+                           QueryAnalyzerRevisions const& analyzersRevision);
 
-  arangodb::futures::Future<Result> finalizeClusterQuery(ErrorCode errorCode);
+  futures::Future<Result> finalizeClusterQuery(ErrorCode errorCode);
 
  private:
   /// @brief first one should be the local one
-  arangodb::traverser::GraphEngineList _traversers;
+  traverser::GraphEngineList _traversers;
 };
 
-}  // namespace aql
-}  // namespace arangodb
+}  // namespace arangodb::aql
