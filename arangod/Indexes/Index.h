e358cc7f1e219718c808e23334ce91fdb43b5267
cpjulia <juliamagoo@gmail.com>
2022-08-16 02:27:50 -0300

    Update arangod/Indexes/Index.h
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- arangod/Indexes/Index.h
+++ arangod/Indexes/Index.h
@@ -258,8 +258,8 @@ class Index {
   /// @brief whether or not the index is unique
   inline bool unique() const { return _unique; }
 
-  /// @brief validate fields that start or end with ":"
-  static void validateFieldsWithSpecialCase(velocypack::Slice const& fields);
+  /// @brief validates that field names don't start or end with ":"
+  static void validateFieldsWithSpecialCase(velocypack::Slice fields);
 
   /// @brief validate fields from slice
   static void validateFields(velocypack::Slice slice);
