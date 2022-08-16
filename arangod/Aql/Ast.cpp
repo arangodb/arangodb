b724d2cdba5bb39ec94bd3d1b0ddc6646f45955a
Jan <jsteemann@users.noreply.github.com>
2022-08-16 15:37:33 +0200

    fix an invalid attribute access (#16749)
    * fix an invalid attribute access
    
    * Update CHANGELOG
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- arangod/Aql/Ast.cpp
+++ arangod/Aql/Ast.cpp
@@ -2432,7 +2432,7 @@ void Ast::validateAndOptimize(transaction::Methods& trx) {
 
     // indexed access, e.g. a[0] or a['foo']
     if (node->type == NODE_TYPE_INDEXED_ACCESS) {
-      return this->optimizeIndexedAccess(node);
+      return this->optimizeIndexedAccess(node, ctx->variableDefinitions);
     }
 
     // LET
@@ -3717,7 +3717,9 @@ AstNode* Ast::optimizeReference(AstNode* node) {
 }
 
 /// @brief optimizes indexed access, e.g. a[0] or a['foo']
-AstNode* Ast::optimizeIndexedAccess(AstNode* node) {
+AstNode* Ast::optimizeIndexedAccess(
+    AstNode* node, std::unordered_map<Variable const*, AstNode const*> const&
+                       variableDefinitions) {
   TRI_ASSERT(node != nullptr);
   TRI_ASSERT(node->type == NODE_TYPE_INDEXED_ACCESS);
   TRI_ASSERT(node->numMembers() == 2);
@@ -3736,9 +3738,10 @@ AstNode* Ast::optimizeIndexedAccess(AstNode* node) {
       // we have to be careful with numeric values here...
       // e.g. array['0'] is not the same as array.0 but must remain a['0'] or
       // (a[0])
-      return createNodeAttributeAccess(node->getMember(0),
-                                       index->getStringValue(),
-                                       index->getStringLength());
+      return this->optimizeAttributeAccess(
+          createNodeAttributeAccess(node->getMember(0), index->getStringValue(),
+                                    index->getStringLength()),
+          variableDefinitions);
     }
   }
 
