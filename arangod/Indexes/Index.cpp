0e353306bd1a8ab8f862aabaa5172895d32474e0
cpjulia <julia@arangodb.com>
2022-08-16 09:07:48 -0300

    Fixed clang format

--- arangod/Indexes/Index.cpp
+++ arangod/Indexes/Index.cpp
@@ -267,9 +267,11 @@ void Index::validateFieldsWithSpecialCase(VPackSlice fields) {
     return;
   }
   for (VPackSlice name : VPackArrayIterator(fields)) {
-    if (name.stringView().starts_with(":") || name.stringView().ends_with(":")) {
-      THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_ARANGO_ATTRIBUTE_PARSER_FAILED,
-                                     "field names starting or ending with \":\"are disallowed in indexes");
+    if (name.stringView().starts_with(":") ||
+        name.stringView().ends_with(":")) {
+      THROW_ARANGO_EXCEPTION_MESSAGE(
+          TRI_ERROR_ARANGO_ATTRIBUTE_PARSER_FAILED,
+          "field names starting or ending with \":\"are disallowed in indexes");
     }
   }
 }
