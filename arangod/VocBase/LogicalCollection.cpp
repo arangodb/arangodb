89bd833e55f0bd09218d345018e6367202541e53
Heiko Kernbach <heiko@arangodb.com>
2022-08-18 11:11:44 +0200

    ce required methods

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -250,7 +250,10 @@ LogicalCollection::LogicalCollection(TRI_vocbase_t& vocbase, VPackSlice info,
 LogicalCollection::~LogicalCollection() = default;
 
 #ifndef USE_ENTERPRISE
-void LogicalCollection::initializeSmartAttributes(velocypack::Slice info) {
+void LogicalCollection::initializeSmartAttributesBefore(velocypack::Slice info) {
+  // nothing to do in community edition
+}
+void LogicalCollection::initializeSmartAttributesAfter(velocypack::Slice info) {
   // nothing to do in community edition
 }
 #endif
