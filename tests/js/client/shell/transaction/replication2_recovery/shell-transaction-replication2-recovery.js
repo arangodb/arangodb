6885c6d732909f900a3a6018c07f010323d89453
Alex Petenchea <alexandru.petenchea@arangodb.com>
2022-08-19 16:36:08 +0300

    Update tests/js/client/shell/transaction/replication2_recovery/shell-transaction-replication2-recovery.js
    Co-authored-by: Michael Hackstein <michael@arangodb.com>

--- tests/js/client/shell/transaction/replication2_recovery/shell-transaction-replication2-recovery.js
+++ tests/js/client/shell/transaction/replication2_recovery/shell-transaction-replication2-recovery.js
@@ -21,7 +21,7 @@
 // / See the License for the specific language governing permissions and
 // / limitations under the License.
 // /
-// / Copyright holder is triAGENS GmbH, Cologne, Germany
+// / Copyright holder is ArangoDB GmbH, Cologne, Germany
 // /
 // / @author Alexandru Petenchea
 // //////////////////////////////////////////////////////////////////////////////
