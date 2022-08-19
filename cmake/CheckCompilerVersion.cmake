e01ade0faf42348ab53cc42ed62b54e9556ee7f9
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-08-19 23:13:00 +0100

    Improve compiler version check

--- cmake/CheckCompilerVersion.cmake
+++ cmake/CheckCompilerVersion.cmake
@@ -1,25 +1,47 @@
-macro(CheckCompilerVersion MIN_GCC_VERSION MIN_CLANG_VERSION MIN_MSVC_VERSION)
 
-if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
-  set(MIN_VERSION ${MIN_GCC_VERSION})
-elseif (CMAKE_C_COMPILER_ID STREQUAL "Clang")
-  set(MIN_VERSION ${MIN_CLANG_VERSION})
-elseif (CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-  set(MIN_VERSION ${MIN_MSVC_VERSION})
-endif()
+option(ARANGODB_SKIP_COMPILER_CHECK "Skip compiler support check.")
 
-if(NOT WIN32)
-  string(ASCII 27 ESC)
-  set(COLOR_RESET "${ESC}[m")
-  set(RED        "${ESC}[31m")
-endif()
+macro(CheckMinVersionsSet)
+  if(NOT DEFINED ARANGODB_MIN_GCC_VERSION)
+    message(FATAL_ERROR "Please define ARANGODB_MIN_GCC_VERSION in CMakeLists.txt.")
+  endif()
+  if(NOT DEFINED ARANGODB_MIN_CLANG_VERSION)
+    message(FATAL_ERROR "Please define ARANGODB_MIN_CLANG_VERSION in CMakeLists.txt.")
+  endif()
+  if(NOT DEFINED ARANGODB_MIN_MSVC_VERSION)
+    message(FATAL_ERROR "Please define ARANGODB_MIN_MSVC_VERSION in CMakeLists.txt.")
+  endif()
+endmacro()
 
-if (NOT DEFINED MIN_VERSION)
-  message(WARNING "${RED}${CMAKE_C_COMPILER_ID}/${CMAKE_CXX_COMPILER_ID} compiler is not supported.${COLOR_RESET}")
-endif()
+macro(CheckMinCompilerVersionInner MIN_VERSION)
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${MIN_VERSION})
+    message(
+      FATAL_ERROR
+        "ArangoDB requires at least ${CMAKE_CXX_COMPILER_ID} version ${MIN_VERSION},"
+        " detected ${CMAKE_CXX_COMPILER_VERSION}."
+        "\nYou can use ARANGODB_SKIP_COMPILER_CHECK to skip this check.")
+  endif()
+endmacro()
 
-if ((CMAKE_C_COMPILER_VERSION VERSION_LESS ${MIN_VERSION}) OR (CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${MIN_VERSION}))
-  message(WARNING "${RED}${CMAKE_C_COMPILER_ID} compiler version C(${CMAKE_C_COMPILER_VERSION})/C++(${CMAKE_C_COMPILER_VERSION}) is too old. Need at least ${MIN_VERSION}.${COLOR_RESET}")
-endif()
+macro(CheckCompilerVersion)
+
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    CheckMinCompilerVersionInner(${ARANGODB_MIN_GCC_VERSION})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    CheckMinCompilerVersionInner(${ARANGODB_MIN_CLANG_VERSION})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    CheckMinCompilerVersionInner(${ARANGODB_MIN_MSVC_VERSION})
+  else()
+    message(
+      FATAL_ERROR
+        "ArangoDB does not support CMAKE_CXX_COMPILER_ID ${CMAKE_CXX_COMPILER_ID}"
+    )
+  endif()
 
 endmacro()
+
+CheckMinVersionsSet()
+
+if (NOT ${ARANGODB_SKIP_COMPILER_CHECK})
+  CheckCompilerVersion()
+endif()
