9aa66e358a432c3889993a6802951d04f2ed41ea
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-08-24 15:31:01 +0100

    Add subdirectory

---
+++ arangod/Pregel/tests/AlgorithmTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <Algorithm/Algorithm.h>
+#include <Algorithm/PageRank.h>
+
+auto main(int argc, char** argv) -> int {
+  auto conductor = makeConductor<PageRank>();
+
+  return 0;
+}
