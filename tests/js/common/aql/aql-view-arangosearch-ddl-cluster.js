92ed267bdce53290fe5439081552cba64ef64d00
Valery Mironov <valera.mironow@gmail.com>
2022-09-02 14:24:13 +0300

    add test

--- tests/js/common/aql/aql-view-arangosearch-ddl-cluster.js
+++ tests/js/common/aql/aql-view-arangosearch-ddl-cluster.js
@@ -998,16 +998,15 @@ function IResearchFeatureDDLTestSuite() {
       }
 
       let syncIndex = function (expected) {
-        const syncQuery = aql`
-        FOR d IN TestCollection OPTIONS {indexHint: "TestIndex"}
-          FILTER d.foo != "unknown" COLLECT WITH COUNT INTO c RETURN c`;
+        const syncQuery = `FOR d IN TestCollection OPTIONS {indexHint: "TestIndex"}
+                            FILTER d.foo != "unknown" COLLECT WITH COUNT INTO c RETURN c`;
         let syncWait = 100;
         let count = db._query(syncQuery).toArray()[0];
         while (count < expected && (--syncWait) > 0) {
           require("internal").sleep(1);
           count = db._query(syncQuery).toArray()[0];
         }
-      }
+      };
       // ensure data is synchronized
       var res = db._query("FOR d IN TestView OPTIONS {waitForSync:true} SORT d.foo RETURN d").toArray();
       assertEqual(2, res.length);
