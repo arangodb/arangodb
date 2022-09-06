53c667a5a711ba459177a38a96ba1e5c951c5f96
Andrey Abramov <andrey@arangodb.com>
2022-09-06 12:37:45 +0200

    adjust test

--- tests/js/server/recovery/inverted-index-search-alias-populate.js
+++ tests/js/server/recovery/inverted-index-search-alias-populate.js
@@ -60,7 +60,7 @@ function runSetup () {
   db._view('UnitTestsRecoveryView4').properties(meta1);
   db._view('UnitTestsRecoveryView5').properties(meta2);
 
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 50000; i++) {
     c.save({ a: "foo_" + i, b: "bar_" + i, c: i });
   }
 
