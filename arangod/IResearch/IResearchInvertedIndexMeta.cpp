61d87a6ba6e4c55c19dba85b622d4bf933b9b68c
Andrey Abramov <andrey@arangodb.com>
2022-09-14 00:56:19 +0200

    [SEARCH-362] Handle nested fields in CE (#17070)
    * Handle nested fields in CE
    
    * honor nested fields in meta comparison
    
    * fix test

--- arangod/IResearch/IResearchInvertedIndexMeta.cpp
+++ arangod/IResearch/IResearchInvertedIndexMeta.cpp
@@ -750,13 +750,21 @@ bool InvertedIndexField::init(
     _path += tmp;
   }
 
-  auto fieldsAttributeName =
-      rootMode ? kFieldsFieldName : kNestedFieldsFieldName;
 #ifndef USE_ENTERPRISE
   if (!rootMode) {
+    if (slice.isObject() && slice.hasKey(kNestedFieldsFieldName)) {
+      errorField =
+          absl::StrCat(kNestedFieldsFieldName,
+                       " is supported in ArangoDB Enterprise Edition only.");
+      return false;
+    }
+
     return true;
   }
 #endif
+  auto const fieldsAttributeName =
+      rootMode ? kFieldsFieldName : kNestedFieldsFieldName;
+
   if (slice.isObject() && slice.hasKey(fieldsAttributeName)) {
     auto nestedSlice = slice.get(fieldsAttributeName);
     if (!nestedSlice.isArray()) {
