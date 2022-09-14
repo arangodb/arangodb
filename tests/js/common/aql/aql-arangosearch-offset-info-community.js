73d8f22a7e2fb35d2cd0f5d2874d4ed230192ac3
Vitaly Pryakhin <vitaly@arangodb.com>
2022-09-14 15:00:07 +0300

    add test cases for OFFSET_INFO on community edition

--- tests/js/common/aql/aql-arangosearch-offset-info-community.js
+++ tests/js/common/aql/aql-arangosearch-offset-info-community.js
@@ -44,28 +44,67 @@ function IResearchOffsetInfoAqlTestSuiteCommunity() {
     try { db._dropDatabase(dbName); } catch (err) {}
   };
 
+  var checkThatQueryReturnsExpectedError = function(query, expected_error_code){
+    try {
+      db._query(query);
+      assertTrue(false);
+    } catch (e) {
+      assertEqual(expected_error_code, e.errorNum);
+    }
+  };
+
   return {
     setUpAll : function () { 
       cleanup();
 
       db._createDatabase(dbName);
       db._useDatabase(dbName);
-      db._create("docs");
+      col = db._create("docs");
+      analyzers.save("offsets", "delimiter", { delimiter: " " }, [ "frequency", "position", "offset"]);
+      col.insert({ _key: "a"});
+      col.insert({ body: "the quick brown fox jumps over the lazy dog", sub: { body: "the quick brown fox jumps over the lazy dog" } });
+      col.insert({ body: "the foxx jumps over the fox", sub: { body: "the foxx jumps over the fox" } });
+      col.insert({ body: "the fox jumps over the lazy dog", sub: { body: "the fox jumps over the lazy dog" } });
+      col.insert({ body: "the dog jumps over the lazy fox", sub: { body: "the dog jumps over the lazy fox" } });
+      col.insert({ body: "roses are red, violets are blue, foxes are quick", sub: { body: "roses are red, violets are blue, foxes are quick" } });
       db._createView("docsView", "arangosearch",
-          { links: { docs: { includeAllFields : true } } });
+          { links: { docs: { includeAllFields : true, analyzers: ["offsets"] } } });
+      db._query("FOR d IN docsView OPTIONS {waitForSync:true} LIMIT 1 RETURN 1");
     },
     tearDownAll : function () { 
       cleanup();
     },
 
     testSimpleOffset: function() {
-      try {
-        db._query("FOR d IN docsView SEARCH d._key == 'a' RETURN OFFSET_INFO(d, '_key')");
-        assertTrue(false);
-      } catch (e) {
-        assertEqual(errors.ERROR_NOT_IMPLEMENTED.code, e.errorNum);
-      }
-    }
+      checkThatQueryReturnsExpectedError(
+        "FOR d IN docsView SEARCH d._key == 'a' RETURN OFFSET_INFO(d, '_key')",
+        errors.ERROR_NOT_IMPLEMENTED.code);
+    },
+
+    testOffsetWithAnalyzer: function() {
+      checkThatQueryReturnsExpectedError(
+        "FOR doc IN docsView SEARCH ANALYZER(doc.body == 'brown', 'offsets') RETURN OFFSET_INFO(doc, 'body')",
+        errors.ERROR_NOT_IMPLEMENTED.code);
+    },
+
+    testOffsetWithSubField: function() {
+      checkThatQueryReturnsExpectedError(
+        "FOR doc IN docsView SEARCH ANALYZER(doc.sub.body == 'brown', 'offsets') RETURN OFFSET_INFO(doc, 'sub.body')",
+        errors.ERROR_NOT_IMPLEMENTED.code);
+    },
+
+    testOffsetWithNonExistentField: function() {
+      checkThatQueryReturnsExpectedError(
+        "FOR doc IN docsView SEARCH ANALYZER(doc.doesNotExist == 'brown', 'offsets') RETURN OFFSET_INFO(doc, 'doesNotExist')",
+        errors.ERROR_NOT_IMPLEMENTED.code);
+    },
+
+    testOffsetWithNonExistentSubField: function() {
+      checkThatQueryReturnsExpectedError(
+        "FOR doc IN docsView SEARCH ANALYZER(doc.sub.doesNotExist == 'brown', 'offsets') RETURN OFFSET_INFO(doc, 'sub.body.doesNotExist')",
+        errors.ERROR_NOT_IMPLEMENTED.code);
+    },
+
   }
 }
 
