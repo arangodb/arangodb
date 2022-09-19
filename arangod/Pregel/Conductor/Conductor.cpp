2f379d6841a6cc72b0441c30d85bd40afe9df0f7
Julia Volmer <julia.volmer@arangodb.com>
2022-09-19 16:42:32 +0200

    Add explicit deduction guide for clangd

--- arangod/Pregel/Conductor/Conductor.cpp
+++ arangod/Pregel/Conductor/Conductor.cpp
@@ -86,6 +86,8 @@ template<class... Ts>
 struct overloaded : Ts... {
   using Ts::operator()...;
 };
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 }  // namespace
 
 #define LOG_PREGEL(logId, level)          \
