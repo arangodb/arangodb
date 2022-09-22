34e418a17ec836635448fc1267bf8c1d2af57509
Andrey Abramov <andrey@arangodb.com>
2022-09-22 16:26:14 +0200

    [SEARCH-396] Use warnings instead of errors for search-alias views (#17160)
    * [SEARCH-396] Use warnings instead of errors for search-alias views (#17147)
    
    * Use warnings instead of errors for search-alias views
    
    * refactor `FilterContext::fieldAnalyzer` function and add tests
    
    * address review comments
    # Conflicts:
    #	tests/js/common/aql/aql-view-arangosearch-cluster.inc
    #	tests/js/common/aql/aql-view-arangosearch-noncluster.js
    
    * wip
    
    * Merge branch '3.10' of https://github.com/arangodb/arangodb into bug-fix-3.10/SEARCH-396
    
    # Conflicts:
    #	tests/js/common/aql/aql-view-arangosearch-cluster.inc
    #	tests/js/common/aql/aql-view-arangosearch-noncluster.js
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- arangod/IResearch/IResearchInvertedIndex.cpp
+++ arangod/IResearch/IResearchInvertedIndex.cpp
@@ -53,15 +53,14 @@ using namespace arangodb;
 using namespace arangodb::iresearch;
 
 AnalyzerProvider makeAnalyzerProvider(IResearchInvertedIndexMeta const& meta) {
-  static FieldMeta::Analyzer const defaultAnalyzer{
-      IResearchAnalyzerFeature::identity()};
-  return [&meta](std::string_view fieldPath) -> FieldMeta::Analyzer const& {
+  return [&meta](std::string_view fieldPath, aql::ExpressionContext*,
+                 FieldMeta::Analyzer const&) -> FieldMeta::Analyzer const& {
     for (auto const& field : meta._fields) {
       if (field.path() == fieldPath) {
         return field.analyzer();
       }
     }
-    return defaultAnalyzer;
+    return FieldMeta::identity();
   };
 }
 
@@ -89,8 +88,9 @@ bool supportsFilterNode(
 
   // The analyzer is referenced in the FilterContext and used during the
   // following ::makeFilter() call, so may not be a temporary.
+  auto emptyAnalyzer = makeEmptyAnalyzer();
   FilterContext const filterCtx{.fieldAnalyzerProvider = provider,
-                                .contextAnalyzer = emptyAnalyzer(),
+                                .contextAnalyzer = emptyAnalyzer,
                                 .fields = metaFields};
 
   auto rv = FilterFactory::filter(nullptr, queryCtx, filterCtx, *node);
@@ -307,9 +307,10 @@ class IResearchInvertedIndexIteratorBase : public IndexIterator {
            condition->type != aql::NODE_TYPE_OPERATOR_NARY_OR)) {
         // The analyzer is referenced in the FilterContext and used during the
         // following FilterFactory::::filter() call, so may not be a temporary.
+        auto emptyAnalyzer = makeEmptyAnalyzer();
         FilterContext const filterCtx{
             .fieldAnalyzerProvider = &analyzerProvider,
-            .contextAnalyzer = emptyAnalyzer(),
+            .contextAnalyzer = emptyAnalyzer,
             .fields = _indexMeta->_fields};
         auto rv = FilterFactory::filter(&root, queryCtx, filterCtx, *condition);
 
@@ -344,9 +345,10 @@ class IResearchInvertedIndexIteratorBase : public IndexIterator {
           conditionJoiner = &root.add<irs::Or>();
         }
 
+        auto emptyAnalyzer = makeEmptyAnalyzer();
         FilterContext const filterCtx{
             .fieldAnalyzerProvider = &analyzerProvider,
-            .contextAnalyzer = emptyAnalyzer(),
+            .contextAnalyzer = emptyAnalyzer,
             .fields = _indexMeta->_fields};
 
         auto& mutable_root = conditionJoiner->add<irs::Or>();
@@ -386,9 +388,10 @@ class IResearchInvertedIndexIteratorBase : public IndexIterator {
 
           // The analyzer is referenced in the FilterContext and used during the
           // following ::filter() call, so may not be a temporary.
+          auto emptyAnalyzer = makeEmptyAnalyzer();
           FilterContext const filterCtx{
               .fieldAnalyzerProvider = &analyzerProvider,
-              .contextAnalyzer = emptyAnalyzer()};
+              .contextAnalyzer = emptyAnalyzer};
 
           for (int64_t i = 0; i < conditionSize; ++i) {
             if (i != _mutableConditionIdx) {
