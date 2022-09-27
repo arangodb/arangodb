2c11d9889efc2ae0f600bd555c69545a31caefb3
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-09-27 08:59:36 +0100

    clang-format

--- tests/Zkd/main.cpp
+++ tests/Zkd/main.cpp
@@ -27,5 +27,5 @@ int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   GTEST_FLAG_SET(death_test_style, "threadsafe");
 
-  return RUN_ALL_TESTS(); 
+  return RUN_ALL_TESTS();
 }
