f81d090cf829c77e77a267cfe8db507bc0881449
Michael Hackstein <michael@arangodb.com>
2022-09-29 07:57:39 +0200

    Added an AgencyPreconidtio with handed in builder. This one takes memory ownership of the data.

--- arangod/Agency/AgencyComm.h
+++ arangod/Agency/AgencyComm.h
@@ -214,6 +214,8 @@ class AgencyPrecondition {
   AgencyPrecondition();
   AgencyPrecondition(std::string const& key, Type, bool e);
   AgencyPrecondition(std::string const& key, Type, velocypack::Slice const&);
+  AgencyPrecondition(std::string const& key, Type,
+                     std::shared_ptr<velocypack::Builder>);
   template<typename T>
   AgencyPrecondition(std::string const& key, Type t, T const& v)
       : key(AgencyCommHelper::path(key)),
@@ -228,6 +230,8 @@ class AgencyPrecondition {
                      Type, bool e);
   AgencyPrecondition(std::shared_ptr<cluster::paths::Path const> const& path,
                      Type, velocypack::Slice const&);
+  AgencyPrecondition(std::shared_ptr<cluster::paths::Path const> const& path,
+                     Type, std::shared_ptr<velocypack::Builder>);
   template<typename T>
   AgencyPrecondition(std::shared_ptr<cluster::paths::Path const> const& path,
                      Type t, T const& v)
@@ -247,8 +251,8 @@ class AgencyPrecondition {
   std::string key;
   Type type;
   bool empty;
-  velocypack::Slice value;
   std::shared_ptr<VPackBuilder> builder;
+  velocypack::Slice value;
 };
 
 // -----------------------------------------------------------------------------
