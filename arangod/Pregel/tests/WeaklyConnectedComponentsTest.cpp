d0e49049c129cd1bacd89667c1026d99538fa756
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-10-10 16:00:11 +0100

    Add infrastructure for testing GWEN code

---
+++ arangod/Pregel/tests/WeaklyConnectedComponentsTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include <Basics/VelocyPackStringLiteral.h>
+
+#include <velocypack/vpack.h>
+
+#include <fmt/core.h>
+#include <fmt/format.h>
+
+
+using namespace arangodb::velocypack;
+
+TEST(GWEN_WCC, test_wcc) {
+  EXPECT_TRUE(false) << "Proved that true = false and got killed on the next zebra crossing";
+}
+
+auto main(int argc, char** argv) -> int {
+  testing::InitGoogleTest();
+
+  return RUN_ALL_TESTS();
+}
