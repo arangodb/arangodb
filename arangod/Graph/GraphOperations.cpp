7b806c1d42a4d5e4719eee21ba517a976790bfaf
Heiko <hkernbach@users.noreply.github.com>
2022-10-11 11:02:16 +0200

    cherry picking

--- arangod/Graph/GraphOperations.cpp
+++ arangod/Graph/GraphOperations.cpp
@@ -404,12 +404,18 @@ OperationResult GraphOperations::eraseOrphanCollection(bool waitForSync,
   }
 
   // check if collection exists in the database
+  bool collectionExists = true;
   Result res = checkVertexCollectionAvailability(collectionName);
   if (res.fail()) {
-    return OperationResult(res, options);
+    if (res.errorNumber() == TRI_ERROR_GRAPH_VERTEX_COL_DOES_NOT_EXIST) {
+      // in this case, we are allowed just to drop it out of the definition
+      collectionExists = false;
+    } else {
+      return OperationResult(res, options);
+    }
   }
 
-  if (!hasRWPermissionsFor(collectionName)) {
+  if (collectionExists && !hasRWPermissionsFor(collectionName)) {
     return OperationResult{TRI_ERROR_FORBIDDEN, options};
   }
 
@@ -441,7 +447,7 @@ OperationResult GraphOperations::eraseOrphanCollection(bool waitForSync,
     res = trx.finish(result.result);
   }
 
-  if (dropCollection) {
+  if (dropCollection && collectionExists) {
     std::unordered_set<std::string> collectionsToBeRemoved;
     GraphManager gmngr{_vocbase};
     res = gmngr.pushCollectionIfMayBeDropped(collectionName, "", collectionsToBeRemoved);
