302875a12a1d48f08f20bca4696302d841420271
Heiko <hkernbach@users.noreply.github.com>
2022-10-12 18:12:22 +0200

    [BTS-908] UI Graph Viewer: add + remove edge broken  (#17314)
    * Always render a Select element, even if we only have one option available. In this specific case the element will be rendered anyway but will be disabled instead. Additionally, there is now a title set which indicates that there is only one entry element to choose - therefore disabled.
    
    * CHANGELOG
    
    * render readonly without line breaks.
    
    * Rebuild UI
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.79484843cc011ee30504b8850ec34f09.js"
+  "precache-manifest.ff3b4e2eff04441d21132aed69d220e9.js"
 );
 
 workbox.clientsClaim();
