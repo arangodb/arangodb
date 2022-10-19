b6c343634514cc779d6a41b1f821780f8216bc8a
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2022-10-19 12:43:56 +0100

    Draft of ErrorT

---
+++ tests/Errors/ErrorTTest.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2020 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+/// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include <Basics/ErrorT.h>
+
+using namespace ::arangodb::errors;
+
+struct Error {
+  std::string msg;
+
+  Error(std::string msg) : msg(msg) {}
+};
+
+TEST(ErrorTTest, simple_test) {
+  auto f = ErrorT<Error, int>::ok(5);
+
+  EXPECT_EQ(f.get(), 5);
+
+  auto g = ErrorT<Error, int>::error(Error{"foo"});
+
+  f = g;
+}
