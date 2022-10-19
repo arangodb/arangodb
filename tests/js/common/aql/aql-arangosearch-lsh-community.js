bc760ed7caf3e8cb505db6e124836c685cd0d684
Alexey Bakharew <alex.bakharew@gmail.com>
2022-10-19 16:43:26 +0200

    fix js lint

--- tests/js/common/aql/aql-arangosearch-lsh-community.js
+++ tests/js/common/aql/aql-arangosearch-lsh-community.js
@@ -39,12 +39,12 @@ let dbName = "LshTestSuiteCommunity";
 let isEnterprise = internal.isEnterprise();
 
 function IResearchLshAqlTestSuiteCommunity() {
-  let cleanup = function() {
+  let cleanup = function () {
     db._useDatabase("_system");
-    try { db._dropDatabase(dbName); } catch (err) {}
+    try { db._dropDatabase(dbName); } catch (err) { }
   };
 
-  var checkThatQueryReturnsExpectedError = function(query, expected_error_code){
+  var checkThatQueryReturnsExpectedError = function (query, expected_error_code) {
     try {
       db._query(query);
       assertTrue(false);
@@ -54,20 +54,22 @@ function IResearchLshAqlTestSuiteCommunity() {
   };
 
   return {
-    setUpAll : function () { 
+    setUpAll: function () {
       cleanup();
 
       db._createDatabase(dbName);
       db._useDatabase(dbName);
-      let collection_0 = db._create("collection_0", {computedValues:[{"name":"mh10","expression":"Return 'a'","computeOn":["insert","update","replace"],"overwrite":true,"failOnWarning":false,"keepNull":true},{"name":"mh50","expression":"Return 'b'","computeOn":["insert","update","replace"],"overwrite":true,"failOnWarning":false,"keepNull":true},{"name":"mh100","expression":"Return 'c'","computeOn":["insert","update","replace"],"overwrite":true,"failOnWarning":false,"keepNull":true}],
-      replicationFactor:3, 
-      writeConcern:1, 
-      numberOfShards : 3});
+      let collection_0 = db._create("collection_0", {
+        computedValues: [{ "name": "mh10", "expression": "Return 'a'", "computeOn": ["insert", "update", "replace"], "overwrite": true, "failOnWarning": false, "keepNull": true }, { "name": "mh50", "expression": "Return 'b'", "computeOn": ["insert", "update", "replace"], "overwrite": true, "failOnWarning": false, "keepNull": true }, { "name": "mh100", "expression": "Return 'c'", "computeOn": ["insert", "update", "replace"], "overwrite": true, "failOnWarning": false, "keepNull": true }],
+        replicationFactor: 3,
+        writeConcern: 1,
+        numberOfShards: 3
+      });
 
       var analyzers = require("@arangodb/analyzers");
-      analyzers.save("delimiter", "delimiter", {"delimiter": "#", "features": []})
+      analyzers.save("delimiter", "delimiter", { "delimiter": "#", "features": [] });
 
-      collection_0.insert({ _key: "a"});
+      collection_0.insert({ _key: "a" });
       collection_0.insert({ body: "the quick brown fox jumps over the lazy dog", sub: { body: "the quick brown fox jumps over the lazy dog" } });
       collection_0.insert({ body: "the foxx jumps over the fox", sub: { body: "the foxx jumps over the fox" } });
       collection_0.insert({ body: "the fox jumps over the lazy dog", sub: { body: "the fox jumps over the lazy dog" } });
@@ -76,62 +78,62 @@ function IResearchLshAqlTestSuiteCommunity() {
 
       db._createView("view", "arangosearch");
       db.view.properties({
-          "links": {
-              "collection_0": {
-                  "fields": {
-                      "mh50": {
-                          "analyzers": [
-                              "identity"
-                          ]
-                      },
-                      "mh100": {
-                          "analyzers": [
-                              "identity"
-                          ]
-                      },
-                      "mh10": {
-                          "analyzers": [
-                              "identity"
-                          ]
-                      }
-                  }
+        "links": {
+          "collection_0": {
+            "fields": {
+              "mh50": {
+                "analyzers": [
+                  "identity"
+                ]
+              },
+              "mh100": {
+                "analyzers": [
+                  "identity"
+                ]
+              },
+              "mh10": {
+                "analyzers": [
+                  "identity"
+                ]
               }
-           }
-        });
+            }
+          }
+        }
+      });
     },
-    tearDownAll : function () { 
+    tearDownAll: function () {
       cleanup();
     },
 
-    testMinhashAnalyzer: function() {
+    testMinhashAnalyzer: function () {
       // assert that minhash analyzers are unavailable
       try {
-        analyzers.save("myMinHash10", "minhash", {"numHashes": 10, "analyzer": {"type": "delimiter", "properties": {"delimiter": "#", "features": []}}})
+        analyzers.save("myMinHash10", "minhash", { "numHashes": 10, "analyzer": { "type": "delimiter", "properties": { "delimiter": "#", "features": [] } } });
         assertTrue(false);
       } catch (e) {
         assertEqual(errors.ERROR_NOT_IMPLEMENTED.code, e.errorNum);
       }
     },
 
-    testMinhash: function() {
+    testMinhash: function () {
       checkThatQueryReturnsExpectedError(
         "for d in collection_0 filter minhash(Tokens(d.dataStr, 'delimiter'), 10) == d.mh10 collect with count into c return c",
         errors.ERROR_NOT_IMPLEMENTED.code);
     },
 
-    testMinhashMatch: function() {
+    testMinhashMatch: function () {
       checkThatQueryReturnsExpectedError(
         "for doc in view search minhash_match(doc.dataStr, 'abc', 0.3, 'identity') return doc",
         errors.ERROR_NOT_IMPLEMENTED.code);
     },
 
-    testMinhashCount: function() {
+    testMinhashCount: function () {
       checkThatQueryReturnsExpectedError(
         "RETURN MINHASH_COUNT(5)",
         errors.ERROR_NOT_IMPLEMENTED.code);
     },
 
-    testMinhashError: function() {
+    testMinhashError: function () {
       checkThatQueryReturnsExpectedError(
         "RETURN MINHASH_ERROR(0.05)",
         errors.ERROR_NOT_IMPLEMENTED.code);
