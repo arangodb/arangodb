88598b82e1f05982e37dcc207753a0d8401f471f
roman <roman@arangodb.com>
2022-10-22 11:30:23 +0200

    updated class Graph, added a function to write an equivalence relation from a disjoint set into vertices

--- arangod/Pregel/tests/Graph.cpp
+++ arangod/Pregel/tests/Graph.cpp
@@ -1,8 +1,11 @@
+#include <unordered_set>
 #include "Graph.h"
+#include "SCCGraph.h"
+#include "WCCGraph.h"
 
 template<typename EdgeProperties, typename VertexProperties>
 void Graph<EdgeProperties, VertexProperties>::_checkEdgeEnds(
-    GraphEdge edge, std::map<VertexKey, size_t> const& vertexPosition) {
+    Edge edge, std::map<VertexKey, size_t> const& vertexPosition) {
   if (not vertexPosition.contains(edge._from)) {
     throw std::runtime_error("Edge " + edge._key + " has a _from vertex " +
                              edge._from +
@@ -19,15 +22,15 @@ void Graph<EdgeProperties, VertexProperties>::_checkEdgeEnds(
 template<typename EdgeProperties, typename VertexProperties>
 size_t Graph<EdgeProperties, VertexProperties>::readEdges(
     const SharedSlice& graphJson, bool checkEdgeEnds,
-    std::function<void(GraphEdge edge)> onReadEdge) {
+    std::function<void(Edge edge)> onReadEdge) {
   auto es = graphJson["edges"];
   if (not es.isArray()) {
     throw std::runtime_error("The input graph slice " + es.toString() +
                              " has not field \"edges\"");
   }
   for (EdgeIndex i = 0; i < es.length(); ++i) {
-    GraphEdge edge;
-    auto result = deserializeWithStatus<GraphEdge>(es[i].slice(), edge);
+    Edge edge;
+    auto result = deserializeWithStatus<Edge>(es[i].slice(), edge);
     if (not result.ok()) {
       throw std::runtime_error("Could not read edge: " + result.error());
     }
@@ -37,6 +40,7 @@ size_t Graph<EdgeProperties, VertexProperties>::readEdges(
     }
     onReadEdge(edge);
   }
+  return es.length();
 }
 
 template<typename EdgeProperties, typename VertexProperties>
@@ -44,8 +48,8 @@ auto Graph<EdgeProperties, VertexProperties>::readVertices(
     const SharedSlice& graphJson, bool checkDuplicateVertices) -> void {
   auto vs = graphJson["vertices"];
   for (VertexIndex i = 0; i < vs.length(); ++i) {
-    GraphVertex vertex;
-    auto result = deserializeWithStatus<GraphVertex>(vs[i].slice(), vertex);
+    Vertex vertex;
+    auto result = deserializeWithStatus<Vertex>(vs[i].slice(), vertex);
     if (not result.ok()) {
       throw std::runtime_error("Could not red vertex: " + result.error());
     }
@@ -61,7 +65,27 @@ auto Graph<EdgeProperties, VertexProperties>::readVertices(
     vertices.push_back(vertex);
   }
 }
-
-#include "WCCGraph.h"
+template<typename EdgeProperties, typename VertexProperties>
+auto Graph<EdgeProperties, VertexProperties>::
+    writeEquivalenceRelationIntoVertices(DisjointSet& eqRel) -> size_t {
+  // todo: if a lot of (small) connected components, it may be better to use a
+  //  bitset
+  std::unordered_set<size_t> markedRepresentatives;
+  size_t counter = 0;
+  for (size_t i = 0; i < this->vertices.size(); ++i) {
+    size_t const representative = eqRel.representative(i);
+    if (markedRepresentatives.contains(representative)) {
+      this->vertices[i].properties.value =
+          this->vertices[representative].properties.value;
+    } else {
+      size_t const newId = counter++;
+      this->vertices[i].properties.value = newId;
+      this->vertices[representative].properties.value = newId;
+      markedRepresentatives.insert(representative);
+    }
+  }
+  return counter;
+}
 
 template class Graph<EmptyEdgeProperties, WCCVertexProperties>;
+template class Graph<EmptyEdgeProperties, SCCVertexProperties>;
--- arangod/Pregel/tests/Graph.h
+++ arangod/Pregel/tests/Graph.h
@@ -1,13 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Roman Rabinovich
+////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
-#include <string>
-#include <unordered_set>
 #include <map>
 
 #include <Basics/VelocyPackStringLiteral.h>
-#include <Inspection/VPackPure.h>
-#include <fmt/core.h>
-#include <fmt/format.h>
 
 #include "DisjointSet.h"
 
@@ -19,11 +38,11 @@ using VertexKey = std::string;
 using EdgeKey = std::string;
 
 template<typename VertexProperties>
-struct Vertex {
+struct BaseVertex {
   VertexKey _key;
   VertexProperties properties;
 
-  auto operator==(Vertex const& other) { return _key == other._key; }
+  auto operator==(BaseVertex const& other) { return _key == other._key; }
 };
 
 template<typename VertexProperties>
@@ -34,7 +53,7 @@ concept VertexPropertiesWithValue = requires(VertexProperties t) {
                                     };
 
 template<typename EdgeProperties = EmptyEdgeProperties>
-struct Edge {
+struct BaseEdge {
   EdgeKey _key;
   VertexKey _from;
   VertexKey _to;
@@ -47,8 +66,8 @@ using EdgeIndex = size_t;
 template<typename EdgeProperties, typename VertexProperties>
 class Graph {
  public:
-  using GraphVertex = Vertex<VertexProperties>;
-  using GraphEdge = Edge<EdgeProperties>;
+  using Vertex = BaseVertex<VertexProperties>;
+  using Edge = BaseEdge<EdgeProperties>;
 
   Graph() = default;
 
@@ -56,26 +75,39 @@ class Graph {
       -> void;
 
   size_t readEdges(const SharedSlice& graphJson, bool checkEdgeEnds,
-                   std::function<void(GraphEdge edge)> onReadEdge);
+                   std::function<void(Edge edge)> onReadEdge);
 
   auto getVertexPosition(VertexKey const& key) -> VertexIndex {
     return _vertexPosition[key];
   }
   auto clearVertexPositions() -> void { _vertexPosition.clear(); }
 
-  std::vector<GraphVertex> vertices;
+  std::vector<Vertex> vertices;
+
+  /**
+   * Write
+   * Compute the weakly connected components. Each component has an id of type
+   * size_t and is written into the value property of each of its vertices.
+   * @return the number of weakly connected components.
+   */
+
+  auto writeEquivalenceRelationIntoVertices(DisjointSet& eqRel) -> size_t;
 
  private:
   std::map<VertexKey, VertexIndex> _vertexPosition;
 
  protected:
-  auto _checkEdgeEnds(GraphEdge,
-                      std::map<VertexKey, size_t> const& vertexPosition)
+  auto _checkEdgeEnds(Edge, std::map<VertexKey, size_t> const& vertexPosition)
       -> void;
 };
 
+template<typename EdgeProperties, VertexPropertiesWithValue VertexProperties>
+class ValuedGraph : public Graph<EdgeProperties, VertexProperties> {
+
+};
+
 template<typename Inspector, typename VertexProperties>
-auto inspect(Inspector& f, Vertex<VertexProperties>& p) {
+auto inspect(Inspector& f, BaseVertex<VertexProperties>& p) {
   return f.object(p).fields(f.field("_key", p._key),
                             f.embedFields(p.properties));
 }
@@ -86,7 +118,7 @@ auto inspect(Inspector& f, EmptyEdgeProperties& x) {
 }
 
 template<typename Inspector, typename EdgeProperties = EmptyEdgeProperties>
-auto inspect(Inspector& f, Edge<EdgeProperties>& p) {
+auto inspect(Inspector& f, BaseEdge<EdgeProperties>& p) {
   return f.object(p).fields(f.field("_key", p._key), f.field("_from", p._from),
                             f.field("_to", p._to), f.embedFields(p.properties));
 }
