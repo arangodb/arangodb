296928f29093968ba7c60b63b6912c0c484e4bd0
roman <roman@arangodb.com>
2022-10-22 11:31:58 +0200

    updated WCCGraph

--- arangod/Pregel/tests/WCCGraph.cpp
+++ arangod/Pregel/tests/WCCGraph.cpp
@@ -1,9 +1,8 @@
 #include "WCCGraph.h"
-#include <Inspection/VPackPure.h>
 
 template<typename EdgeProperties, typename VertexProperties>
 void Graph<EdgeProperties, VertexProperties>::_checkEdgeEnds(
-    GraphEdge edge, std::map<VertexKey, size_t> const& vertexPosition) {
+    Edge edge, std::map<VertexKey, size_t> const& vertexPosition) {
   if (not vertexPosition.contains(edge._from)) {
     throw std::runtime_error("Edge " + edge._key + " has a _from vertex " +
                              edge._from +
@@ -19,41 +18,17 @@ void Graph<EdgeProperties, VertexProperties>::_checkEdgeEnds(
 
 template<typename EdgeProperties, VertexPropertiesWithValue VertexProperties>
 WCCGraph<EdgeProperties, VertexProperties>::WCCGraph(
-    const SharedSlice& graphJson, bool checkDuplicateVertices) : _wccs() {
+    const SharedSlice& graphJson, bool checkDuplicateVertices) : wccs() {
   this->readVertices(graphJson, checkDuplicateVertices);
   for (EdgeIndex idx = 0; idx < this->vertices.size(); ++idx) {
-    _wccs.addSingleton(idx);
+    wccs.addSingleton(idx);
   }
 
-  this->readEdges(graphJson, true,   [&](GraphEdge edge) {
-    _wccs.merge(this->getVertexPosition(edge._from),
+  this->readEdges(graphJson, true,   [&](WCCEdge edge) {
+    wccs.merge(this->getVertexPosition(edge._from),
                 this->getVertexPosition(edge._to));
   });
   this->clearVertexPositions();
-}
-
-template<typename EdgeProperties, VertexPropertiesWithValue VertexProperties>
-auto WCCGraph<EdgeProperties, VertexProperties>::computeWCC() -> size_t {
-  std::unordered_set<size_t> markedRepresentatives;
-  size_t counter = 0;
-  for (size_t i = 0; i < this->vertices.size(); ++i) {
-    size_t const representative = _wccs.representative(i);
-    if (markedRepresentatives.contains(representative)) {
-      this->vertices[i].properties.value =
-          this->vertices[representative].properties.value;
-    } else {
-      size_t const newId = counter++;
-      this->vertices[i].properties.value = newId;
-      this->vertices[representative].properties.value = newId;
-      markedRepresentatives.insert(representative);
-    }
-  }
-  return counter;
-}
-
-template<typename EdgeProperties, VertexPropertiesWithValue VertexProperties>
-auto WCCGraph<EdgeProperties, VertexProperties>::printWccs() -> void {
-  _wccs.print();
 }
 
 template class WCCGraph<EmptyEdgeProperties, WCCVertexProperties>;
--- arangod/Pregel/tests/WCCGraph.h
+++ arangod/Pregel/tests/WCCGraph.h
@@ -1,13 +1,31 @@
-#pragma once
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
 
-#include <string>
-#include <unordered_set>
-#include <map>
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Roman Rabinovich
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
 
 #include <Basics/VelocyPackStringLiteral.h>
 #include <Inspection/VPackPure.h>
-#include <fmt/core.h>
-#include <fmt/format.h>
 
 #include "Graph.h"
 #include "DisjointSet.h"
@@ -29,22 +47,12 @@ auto inspect(Inspector& f, WCCVertexProperties& x) {
 }
 
 template<typename EdgeProperties, VertexPropertiesWithValue VertexProperties>
-class WCCGraph : public Graph<EdgeProperties, VertexProperties> {
+class WCCGraph : public ValuedGraph<EdgeProperties, VertexProperties> {
  public:
-  using GraphVertex = Vertex<VertexProperties>;
-  using GraphEdge = Edge<EdgeProperties>;
+  using WCCEdge = BaseEdge<EdgeProperties>;
 
   WCCGraph(SharedSlice const& graphJson, bool checkDuplicateVertices);
-  /**
-   * Compute the weakly connected components. Each component has an id of type
-   * size_t and is written into the value property of each of its vertices.
-   * @return the number of weakly connected components.
-   */
-  auto computeWCC() -> size_t;
 
-  auto printWccs() -> void;
-
- private:
-  DisjointSet _wccs;
+  DisjointSet wccs;
 };
 
