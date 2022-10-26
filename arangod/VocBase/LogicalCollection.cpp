e686ab1c88a43b67076459f1820fc371dd30693b
jsteemann <jsteemann@users.noreply.github.com>
2022-10-26 19:11:12 +0200

    fix hanger

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -949,8 +949,9 @@ Result LogicalCollection::properties(velocypack::Slice slice) {
       auto& cf = vocbase().server().getFeature<ClusterFeature>();
       replicationFactor = replicationFactorSlice.getNumber<size_t>();
       if ((!isSatellite() && replicationFactor == 0) ||
-          replicationFactor < cf.minReplicationFactor() ||
-          replicationFactor > cf.maxReplicationFactor()) {
+          (ServerState::instance()->isCoordinator() &&
+           (replicationFactor < cf.minReplicationFactor() ||
+            replicationFactor > cf.maxReplicationFactor()))) {
         return Result(TRI_ERROR_BAD_PARAMETER,
                       "bad value for replicationFactor");
       }
