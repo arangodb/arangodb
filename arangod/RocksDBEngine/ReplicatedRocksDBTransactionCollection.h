0669a2f93c19bca30252f60992c33df67da5d69f
Manuel Pöter <manuel@arangodb.com>
2022-10-27 13:33:41 +0200

    Refactor and fix race when committing trx in replication2 (#17427)

--- arangod/RocksDBEngine/ReplicatedRocksDBTransactionCollection.h
+++ arangod/RocksDBEngine/ReplicatedRocksDBTransactionCollection.h
@@ -23,6 +23,7 @@
 
 #pragma once
 
+#include "RocksDBEngine/Methods/RocksDBTrxBaseMethods.h"
 #include "RocksDBEngine/RocksDBTransactionCollection.h"
 
 namespace arangodb {
@@ -37,7 +38,8 @@ class RocksDBTransactionMethods;
 class ReplicatedRocksDBTransactionState;
 
 class ReplicatedRocksDBTransactionCollection final
-    : public RocksDBTransactionCollection {
+    : public RocksDBTransactionCollection,
+      public IRocksDBTransactionCallback {
  public:
   ReplicatedRocksDBTransactionCollection(ReplicatedRocksDBTransactionState* trx,
                                          DataSourceId cid,
@@ -77,6 +79,11 @@ class ReplicatedRocksDBTransactionCollection final
 
  protected:
   auto ensureCollection() -> Result override;
+
+  // IRocksDBTransactionCallback methods
+  rocksdb::SequenceNumber prepare() override;
+  void cleanup() override;
+  void commit(rocksdb::SequenceNumber lastWritten) override;
 
  private:
   void maybeDisableIndexing();
