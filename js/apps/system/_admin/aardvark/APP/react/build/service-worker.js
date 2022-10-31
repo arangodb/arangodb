25b5d277e3b570e166f22074f3f8b27995274696
Heiko <hkernbach@users.noreply.github.com>
2022-10-31 20:05:16 +0100

    UI: read & write visual graph properties bugfix [ES-1078] (#16886)
    * Fixed reading and writing of graph visibility properties into and from database.
    
    * [ES-1078] Fix check if visual properties are already available
    
    * Rebuild UI
    
    Co-authored-by: Andreas Jurdak <andreas.jurdak@arangodb.com>
    Co-authored-by: Vadim Kondratyev <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d7e4b6ff13369727fbf20d8fff583d46.js"
+  "precache-manifest.0650c2c921a9123f462a9c97d8c9e76e.js"
 );
 
 workbox.clientsClaim();
