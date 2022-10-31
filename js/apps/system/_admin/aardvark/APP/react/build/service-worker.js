1ef892ef3f9b8f41f37d10cd2bdf11efef0cd468
Andreas Jurdak <56896586+ajurdak@users.noreply.github.com>
2022-10-31 18:06:38 +0100

    [FE-159] Make-db-settings-visible (#17296)
    * [FE-159] Receive db settings and display placeholder content
    
    * [FE-159] Display returned db settings in modal
    
    * Update CHANGELOG
    
    * Rebuild UI
    
    Co-authored-by: Andreas Jurdak <andreasjurdak@MacBook-Pro-von-Andreas.local>
    Co-authored-by: Vadim <vadim@arangodb.com>

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6785141c0c64333523b9e68b42cc36fd.js"
+  "precache-manifest.d7e4b6ff13369727fbf20d8fff583d46.js"
 );
 
 workbox.clientsClaim();
