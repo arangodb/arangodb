98c3cc3042c80e91a15abf58f9ac8acfe2118b14
Alexey Bakharew <alex.bakharew@gmail.com>
2022-11-06 22:04:55 +0100

    fix

--- tests/js/common/aql/aql-arangosearch-inverted-index-search-alias-community.js
+++ tests/js/common/aql/aql-arangosearch-inverted-index-search-alias-community.js
@@ -740,7 +740,6 @@ function IResearchInvertedIndexSearchAliasAqlTestSuiteCommunity() {
                 assertInvertedIndexCreation("testColl", { name: "i1", type: "inverted", fields: ["a"] });
 
                 assertInvertedIndexCreation("testColl", { name: "i2", type: "inverted", fields: ["b"] });
-                assertInvertedIndexCreation("testColl", { name: "i6", type: "inverted", fields: ["b"] });
                 assertEqual(testColl.indexes().length, 3);
             }
 
