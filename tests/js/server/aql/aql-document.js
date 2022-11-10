6f970d065097256b074b5ceac24510b397de9b13
jsteemann <jsteemann@users.noreply.github.com>
2022-11-10 23:31:54 +0100

    fix jslint errors

--- tests/js/server/aql/aql-document.js
+++ tests/js/server/aql/aql-document.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertEqual, AQL_EXECUTE */
+/*global assertEqual, assertNull, AQL_EXECUTE */
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief tests for DOCUMENT function
