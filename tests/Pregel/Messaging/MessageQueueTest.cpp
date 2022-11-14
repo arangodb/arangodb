d40a449d3ebf03e17aff7e48356710f2cbcdd23a
Julia Volmer <julia.volmer@arangodb.com>
2022-11-14 10:14:13 +0100

    Delete tests that will be redundant anyway
    They fail on mac

--- tests/Pregel/Messaging/MessageQueueTest.cpp
+++ tests/Pregel/Messaging/MessageQueueTest.cpp
@@ -26,25 +26,3 @@ TEST(PregelMessageQueue, pushes_item_at_end) {
   ASSERT_EQ(queue.pop(), 3);
   ASSERT_EQ(queue.pop(), 6);
 }
-
-TEST(PregelMessageQueue, waits_for_item_if_queue_is_empty) {
-  using namespace std::chrono_literals;
-  auto queue = MessageQueue<int>{};
-  queue.push(9);
-  std::thread other_thread([&]() {
-    std::this_thread::sleep_for(1000ms);
-    queue.push(3);
-  });
-  ASSERT_EQ(queue.pop(), 9);
-  other_thread.join();
-  ASSERT_EQ(queue.pop(), 3);
-}
-
-TEST(PregelMessageQueue, can_push_from_front_and_pop_to_back_simulatneously) {
-  auto queue = MessageQueue<int>{};
-  queue.push(5);
-  std::thread other_thread([&]() { queue.push(8); });
-  ASSERT_EQ(queue.pop(), 5);
-  other_thread.join();
-  ASSERT_EQ(queue.pop(), 8);
-}
