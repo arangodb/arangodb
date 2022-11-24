fc8cf4cc2139f6dedc6b4c50089b968206f1c8e4
Andrei Lobov <andrei.lobov@arangodb.com>
2022-11-24 21:55:19 +0200

    fix warning

--- arangod/IResearch/IResearchCommon.h
+++ arangod/IResearch/IResearchCommon.h
@@ -92,7 +92,7 @@ constexpr std::string_view getFormat(LinkVersion version) noexcept {
   return IRESEARCH_FORMATS[static_cast<uint32_t>(version)];
 }
 
-inline constexpr std::string_view PK_COLUMN{"@_PK"};
+inline constexpr ::iresearch::string_ref PK_COLUMN{"@_PK"};
 
 struct StaticStrings {
   static constexpr std::string_view ViewArangoSearchType = "arangosearch";
