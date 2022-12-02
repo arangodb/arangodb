268796db83f9e960a82f5cce621860fc8e90dc32
Valery Mironov <valera.mironow@gmail.com>
2022-12-02 15:37:14 +0100

    Fix

--- arangod/VocBase/Methods/Databases.cpp
+++ arangod/VocBase/Methods/Databases.cpp
@@ -454,6 +454,7 @@ arangodb::Result Databases::drop(ExecContext const& exec,
   if (server.hasFeature<V8DealerFeature>() &&
       server.isEnabled<V8DealerFeature>()) {
     V8DealerFeature& dealer = server.getFeature<V8DealerFeature>();
+    bool needReload = false;
     try {
       JavaScriptSecurityContext securityContext =
           JavaScriptSecurityContext::createInternalContext();
@@ -488,7 +489,7 @@ arangodb::Result Databases::drop(ExecContext const& exec,
         // run the garbage collection in case the database held some objects
         // which can now be freed
         TRI_RunGarbageCollectionV8(isolate, 0.25);
-        dealer.addGlobalContextMethod("reloadRouting");
+        needReload = true;
       }
     } catch (arangodb::basics::Exception const& ex) {
       events::DropDatabase(dbName, TRI_ERROR_INTERNAL, exec);
@@ -499,6 +500,9 @@ arangodb::Result Databases::drop(ExecContext const& exec,
     } catch (...) {
       events::DropDatabase(dbName, Result(TRI_ERROR_INTERNAL), exec);
       return Result(TRI_ERROR_INTERNAL, dropError);
+    }
+    if (needReload) {
+      dealer.addGlobalContextMethod("reloadRouting");
     }
   } else {
     if (ServerState::instance()->isCoordinator()) {
