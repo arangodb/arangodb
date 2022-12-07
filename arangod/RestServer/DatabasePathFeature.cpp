3e16bc4f9d3cb3743d3f328d1ac1f10ccfbd0035
Jan <jsteemann@users.noreply.github.com>
2022-12-07 12:32:10 +0100

    remove transitive dependencies for ProgramOptions (#17750)

--- arangod/RestServer/DatabasePathFeature.cpp
+++ arangod/RestServer/DatabasePathFeature.cpp
@@ -34,6 +34,7 @@
 #include "Logger/LogMacros.h"
 #include "Logger/Logger.h"
 #include "Logger/LoggerStream.h"
+#include "ProgramOptions/Parameters.h"
 #include "ProgramOptions/ProgramOptions.h"
 #include "ProgramOptions/Section.h"
 
