83e19bd8c572c6dcfba14c859306968ec5f0d0c3
Max Neunhöffer <max@arangodb.com>
2022-12-07 11:28:52 +0100

    [BTS-1104] Undo ResignLeadership (#17726)
    * Backport from devel.
    
    * Make test stable.
    
    * Stabilize test.

--- arangod/RestHandler/RestAdminClusterHandler.cpp
+++ arangod/RestHandler/RestAdminClusterHandler.cpp
@@ -660,6 +660,7 @@ RestAdminClusterHandler::MoveShardContext::fromVelocyPack(VPackSlice slice) {
     auto fromServer = slice.get("fromServer");
     auto toServer = slice.get("toServer");
     auto remainsFollower = slice.get("remainsFollower");
+    auto tryUndo = slice.get("tryUndo");
 
     bool valid = collection.isString() && shard.isString() &&
                  fromServer.isString() && toServer.isString();
@@ -670,7 +671,8 @@ RestAdminClusterHandler::MoveShardContext::fromVelocyPack(VPackSlice slice) {
       return std::make_unique<MoveShardContext>(
           std::move(databaseStr), collection.copyString(), shard.copyString(),
           fromServer.copyString(), toServer.copyString(), std::string{},
-          remainsFollower.isNone() || remainsFollower.isTrue());
+          remainsFollower.isNone() || remainsFollower.isTrue(),
+          tryUndo.isTrue());
     }
   }
 
@@ -801,6 +803,7 @@ RestAdminClusterHandler::FutureVoid RestAdminClusterHandler::createMoveShard(
                    builder.add("remainsFollower",
                                isLeader ? VPackValue(ctx->remainsFollower)
                                         : VPackValue(false));
+                   builder.add("tryUndo", VPackValue(ctx->tryUndo));
                    builder.add("creator",
                                VPackValue(ServerState::instance()->getId()));
                    builder.add("timeCreated",
@@ -1174,7 +1177,7 @@ RestStatus RestAdminClusterHandler::handleSingleServerJob(
     VPackSlice server = body.get("server");
     if (server.isString()) {
       std::string serverId = resolveServerNameID(server.copyString());
-      return handleCreateSingleServerJob(job, serverId);
+      return handleCreateSingleServerJob(job, serverId, body);
     }
   }
 
@@ -1184,7 +1187,7 @@ RestStatus RestAdminClusterHandler::handleSingleServerJob(
 }
 
 RestStatus RestAdminClusterHandler::handleCreateSingleServerJob(
-    std::string const& job, std::string const& serverId) {
+    std::string const& job, std::string const& serverId, VPackSlice body) {
   std::string jobId = std::to_string(
       server().getFeature<ClusterFeature>().clusterInfo().uniqid());
   auto jobToDoPath =
@@ -1197,6 +1200,11 @@ RestStatus RestAdminClusterHandler::handleCreateSingleServerJob(
     builder.add("server", VPackValue(serverId));
     builder.add("jobId", VPackValue(jobId));
     builder.add("creator", VPackValue(ServerState::instance()->getId()));
+    if (job == "resignLeadership") {
+      if (body.isObject() && body.hasKey("undoMoves")) {
+        builder.add("undoMoves", VPackValue(body.get("undoMoves").isTrue()));
+      }
+    }
     builder.add(
         "timeCreated",
         VPackValue(timepointToString(std::chrono::system_clock::now())));
