16fb89c2b97d5e9a6b812ccc3144f4d618445a6b
Andrey Abramov <andrey@arangodb.com>
2022-12-08 18:24:06 +0100

    wip

--- arangod/StorageEngine/WalAccess.cpp
+++ arangod/StorageEngine/WalAccess.cpp
@@ -73,7 +73,7 @@ bool WalAccessContext::shouldHandleCollection(TRI_voc_tick_t dbid,
 TRI_vocbase_t* WalAccessContext::loadVocbase(TRI_voc_tick_t dbid) {
   TRI_ASSERT(dbid != 0);
 
-  if (auto const& it = _vocbases.find(dbid); it == _vocbases.end()) {
+  if (auto const it = _vocbases.find(dbid); it == _vocbases.end()) {
     if (auto vocbase = _server.getFeature<DatabaseFeature>().useDatabase(dbid);
         vocbase != nullptr) {
       auto& [_, db] = *_vocbases.try_emplace(dbid, std::move(vocbase)).first;
