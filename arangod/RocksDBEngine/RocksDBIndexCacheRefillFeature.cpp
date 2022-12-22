02d3ca3ba817309b5f33ce7e717cbef759a8a346
jsteemann <jsteemann@users.noreply.github.com>
2022-12-22 13:45:19 +0100

    apply review suggestion

--- arangod/RocksDBEngine/RocksDBIndexCacheRefillFeature.cpp
+++ arangod/RocksDBEngine/RocksDBIndexCacheRefillFeature.cpp
@@ -378,10 +378,7 @@ void RocksDBIndexCacheRefillFeature::waitForCatchup() {
 
 void RocksDBIndexCacheRefillFeature::stopThreads() {
   for (auto& t : _backgroundThreads) {
-    t->beginShutdown();
-    while (t->isRunning()) {
-      std::this_thread::yield();
-    }
+    t->shutdown();
   }
 }
 
