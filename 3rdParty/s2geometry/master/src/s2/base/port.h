b1bc74334697f687c9ea4ce6b099a5714464ed51
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-01-10 00:51:59 +0100

    Avoid msvc warnings

--- 3rdParty/s2geometry/master/src/s2/base/port.h
+++ 3rdParty/s2geometry/master/src/s2/base/port.h
@@ -59,6 +59,15 @@
 #undef ERROR
 #undef DELETE
 #undef DIFFERENCE
+#undef S_IRUSR
+#undef S_IWUSR
+#undef S_IXUSR
+#undef S_IRGRP
+#undef S_IWGRP
+#undef S_IXGRP
+#undef S_IROTH
+#undef S_IWOTH
+#undef S_IXOTH
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
