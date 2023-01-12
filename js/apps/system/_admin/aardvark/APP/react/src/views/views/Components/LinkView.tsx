e5134ff2998d6cd0a1e16387ae11b2df2ab4aece
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-01-12 18:42:12 +0100

    [FE-174] Remove save button in LinkView component

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/Components/LinkView.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/Components/LinkView.tsx
@@ -3,7 +3,6 @@ import ViewLinkLayout from "./ViewLinkLayout";
 import { ArangoTable, ArangoTD } from "../../../components/arango/table";
 import LinkPropertiesInput from "../forms/inputs/LinkPropertiesInput";
 import { FormState, ViewContext } from "../constants";
-import { SaveButton } from "../Actions";
 
 interface LinkViewProps {
   links: FormState['links'];
@@ -37,13 +36,6 @@ const LinkView = ({
         </tr>
         </tbody>
       </ArangoTable>
-      {
-        isAdminUser && changed
-          ? <div className="tab-pane tab-pane-modal active" id="Save">
-            <SaveButton view={formState as FormState} setChanged={setChanged} oldName={name}/>
-          </div>
-          : null
-      }
     </ViewLinkLayout>
     : null;
 };
