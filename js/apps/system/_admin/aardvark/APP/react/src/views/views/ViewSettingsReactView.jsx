ef851fb7ab66efa31fafda210c1a7ac2f4c68eb2
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-01-17 21:04:33 +0100

    [FE-174] Remove unwanted space on top and bottom of the JSON editor component

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewSettingsReactView.jsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewSettingsReactView.jsx
@@ -396,7 +396,7 @@ const ViewSettingsReactView = ({ name }) => {
               marginLeft: 'auto',
               marginRight: 'auto'
             }}>
-              <div className="modal-body" style={{ display: 'unset' }} id={'view-json'}>
+              <div className="modal-body" id={'view-json'}>
                 <div className={'tab-content'} style={{ display: 'unset' }}>
                   <div className="tab-pane tab-pane-modal active" id="JSON">
                     <Grid>
