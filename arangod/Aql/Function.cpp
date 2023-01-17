2f27673b9985ed2d00fffe30da8a2614fa963a28
jsteemann <jsteemann@users.noreply.github.com>
2023-01-17 11:56:54 +0100

    fix yet another assertion failure

--- arangod/Aql/Function.cpp
+++ arangod/Aql/Function.cpp
@@ -73,7 +73,9 @@ Function::Function(std::string const& name, char const* arguments,
 
   // only the V8 function does not have a C++ implementation.
   // don't ever change this!
-  TRI_ASSERT(hasCxxImplementation() || name == "V8" || name == "CUSTOMSCORER")
+  // note: CUSTOMSCORER and INVALID are only used by unit tests
+  TRI_ASSERT(hasCxxImplementation() || name == "V8" || name == "CUSTOMSCORER" ||
+             name == "INVALID")
       << "unexpected AQL function without C++ implementation: " << name;
 }
 
