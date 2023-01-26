1954b4ce31901a6ebade3c849c254a445bc795f0
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-01-26 15:27:22 +0000

    Change header

--- arangod/Pregel/Worker/Messages.h
+++ arangod/Pregel/Worker/Messages.h
@@ -25,7 +25,7 @@
 #include "Cluster/ClusterTypes.h"
 #include "Inspection/Format.h"
 #include "Pregel/ExecutionNumber.h"
-#include "Pregel/Graph.h"
+#include "Pregel/Graph/Graph.h"
 #include "Pregel/Statistics.h"
 #include "Pregel/Status/Status.h"
 
