9a2ef6396d58a01c768500cf1d2ee6b3ad94c4ab
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-01-26 17:20:02 +0000

    Format tpp files too

--- utils/clang-format.sh
+++ utils/clang-format.sh
@@ -8,17 +8,21 @@ HEADERNAMES=`mktemp`
 
 clang-format --version
 
-find client-tools lib arangod tests -iname \*.h >> $HEADERNAMES
+# clang-format incorrectly deems some .h files in the ArangoDB repository
+# to be ObjectiveC files; we sidestep this problem by renaming them all
+# to .hpp, then running clang-format, and moving them back to their
+# original name.
+find client-tools lib arangod tests -iname \*.h > $HEADERNAMES
 
 for file in $(cat $HEADERNAMES) ;
 do
   mv $file $(dirname $file)/$(basename $file .h).hpp
 done
 
-clang-format -i arangod/**/*.{cpp,hpp}
-clang-format -i lib/**/*.{cpp,hpp}
-clang-format -i client-tools/**/*.{cpp,hpp}
-clang-format -i tests/**/*.{cpp,hpp}
+clang-format -i arangod/**/*.{cpp,hpp,tpp}
+clang-format -i lib/**/*.{cpp,hpp,tpp}
+clang-format -i client-tools/**/*.{cpp,hpp,tpp}
+clang-format -i tests/**/*.{cpp,hpp,tpp}
 
 for file in $(cat $HEADERNAMES) ;
 do
