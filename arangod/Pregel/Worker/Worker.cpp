03ee368baa5ff116522a18ac5db53553ab623296
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-02-06 14:14:47 +0000

    Fix some complaints of the MSVC compiler

--- arangod/Pregel/Worker/Worker.cpp
+++ arangod/Pregel/Worker/Worker.cpp
@@ -566,7 +566,7 @@ auto Worker<V, E, M>::aqlResult(bool withId) const -> PregelResults {
   for (; it.hasMore(); ++it) {
     Vertex<V, E> const* vertexEntry = *it;
 
-    TRI_ASSERT(vertexEntry->shard() < _config.globalShardIDs().size());
+    TRI_ASSERT(vertexEntry->shard().value < _config.globalShardIDs().size());
     ShardID const& shardId = _config.globalShardID(vertexEntry->shard());
 
     results.openObject(/*unindexed*/ true);
