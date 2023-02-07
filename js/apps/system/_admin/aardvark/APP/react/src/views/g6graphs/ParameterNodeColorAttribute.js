9b36da4478cbe2dac1fe077e9cb7d8c088855438
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-02-07 15:11:28 +0100

    [FE-179] Remove antd from ParameterNodeColorAttribute component

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/ParameterNodeColorAttribute.js
@@ -1,9 +1,7 @@
 import React, { useContext, useState } from "react";
 import { UrlParametersContext } from "./url-parameters-context";
-import { Tooltip } from 'antd';
-import { InfoCircleFilled } from '@ant-design/icons';
+import ToolTip from '../../components/arango/tootip';
 import Textinput from "./components/pure-css/form/Textinput.tsx";
-
 
 const ParameterNodeColorAttribute = ({ nodesColorAttributes }) => {
   const [urlParameters, setUrlParameters] = useContext(UrlParametersContext);
@@ -25,9 +23,12 @@ const ParameterNodeColorAttribute = ({ nodesColorAttributes }) => {
           }}
           disabled={urlParameters.nodeColorByCollection}>
         </Textinput>
-        <Tooltip placement="bottom" title={"If an attribute is given, nodes will be colorized by the attribute. This setting ignores default node color if set."}>
-          <InfoCircleFilled style={{ fontSize: '12px', color: '#555555' }} />
-        </Tooltip>
+        <ToolTip
+          title={"If an attribute is given, nodes will be colorized by the attribute. This setting ignores default node color if set."}
+          setArrow={true}
+        >
+          <span className="arangoicon icon_arangodb_info" style={{ fontSize: '16px' }}></span>
+        </ToolTip>
       </div>
     </>
   );
