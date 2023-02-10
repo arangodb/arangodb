b25a041dfa4189790d33577bd84125835bb53840
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-02-10 14:49:50 +0000

    Introduce a simple store with inspectors and tests

--- arangod/Pregel/GraphStore/Store.h
+++ arangod/Pregel/GraphStore/Store.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
 ///
-/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2014-2022 ArangoDB GmbH, Cologne, Germany
 /// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,46 +18,30 @@
 ///
 /// Copyright holder is ArangoDB GmbH, Cologne, Germany
 ///
-/// @author Julia Volmer
 /// @author Markus Pfeiffer
 ////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
-#include <cstdint>
-#include <string>
-
-#include "VertexID.h"
+#include "Pregel/GraphStore/Edge.h"
+#include "Pregel/GraphStore/Vertex.h"
 
 namespace arangodb::pregel {
 
 template<typename V, typename E>
-class GraphStore;
+struct Store {
+  using VertexType = Vertex<V,E>;
+  using EdgeType = Edge<E>;
 
-// header entry for the edge file
-template<typename E>
-// cppcheck-suppress noConstructor
-struct Edge {
-  template<typename V, typename E2>
-  friend class GraphStore;
+  auto emplace(VertexType&& v) -> void { vertices.emplace(v); }
+  auto numberOfVertices() -> size_t { return vertices.size(); }
 
-  Edge() = delete;
-  Edge(VertexID id, E&& data) : _to{id}, _data(std::move(data)) {}
-  Edge(Edge const&) = delete;
-  Edge(Edge&&) = default;
-  auto operator=(Edge const& other) -> Edge& = delete;
-  auto operator=(Edge&& other) -> Edge& = default;
-
-  [[nodiscard]] std::string_view toKey() const { return _to.key; }
-  E& data() noexcept { return _data; }
-  [[nodiscard]] PregelShard targetShard() const noexcept { return _to.shard; }
-
-  VertexID _to;
-  E _data;
+  std::vector<VertexType> vertices;
 };
 
-template<typename E, typename Inspector>
-auto inspect(Inspector& f, Edge<E>& e) {
-  return f.object(e).fields(f.field("to", e._to), f.field("data", e._data));
+template<typename V, typename E, typename Inspector>
+auto inspect(Inspector& f, Store<V, E>& s) {
+  return f.object(s).fields(f.field("vertices", s.vertices));
 }
 
 }  // namespace arangodb::pregel
