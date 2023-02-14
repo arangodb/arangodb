6abe70c06727de5d985a106f9ccacf079abcefdc
Max Neunhoeffer <max@arangodb.com>
2023-02-14 15:13:03 +0100

    Take out include.

--- arangod/Transaction/SmartContext.h
+++ arangod/Transaction/SmartContext.h
@@ -24,7 +24,6 @@
 #pragma once
 
 #include "Basics/Common.h"
-#include "StorageEngine/TransactionState.h"
 #include "Transaction/Context.h"
 #include "VocBase/vocbase.h"
 #include "VocBase/AccessMode.h"
