e7861e7ecbe812a6e40fcd7805a922d3562710d7
Jan <jsteemann@users.noreply.github.com>
2023-02-15 17:30:12 +0100

    Update tests/RocksDBEngine/CachedCollectionNameTest.cpp
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>

--- tests/RocksDBEngine/CachedCollectionNameTest.cpp
+++ tests/RocksDBEngine/CachedCollectionNameTest.cpp
@@ -127,7 +127,8 @@ TEST(CachedCollectionNameTest, test_set_multiple_times_different_collection) {
   ASSERT_NE(nullptr, previous);
   EXPECT_EQ("foobar", *previous);
   ASSERT_EQ("/abc", result);
-  ASSERT_EQ("foobar", *testee.get());
+  ASSERT_NE(nullptr, testee.get());
+  EXPECT_EQ("foobar", *testee.get());
 
   result = testee.buildCompressedValue(previous, "barbaz/123456");
   ASSERT_EQ("foobar", *previous);
