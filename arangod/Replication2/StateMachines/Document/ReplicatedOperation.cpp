fe248da438d7dfcb15b88e1a11770263ea34c038
Alex Petenchea <alex.petenchea@gmail.com>
2023-02-21 12:00:13 +0200

    Introducing ReplicatedOperation

---
+++ arangod/Replication2/StateMachines/Document/ReplicatedOperation.cpp
@@ -0,0 +1,106 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Alexandru Petenchea
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Replication2/StateMachines/Document/ReplicatedOperation.h"
+
+#include "Assertions/AssertionLogger.h"
+#include "Assertions/ProdAssert.h"
+
+namespace arangodb::replication2::replicated_state::document {
+
+ReplicatedOperation::DocumentOperation::DocumentOperation(
+    TransactionId tid, ShardID shard, velocypack::SharedSlice payload)
+    : tid{tid}, shard{std::move(shard)}, payload{std::move(payload)} {}
+
+template<typename... Args>
+ReplicatedOperation::ReplicatedOperation(std::in_place_t,
+                                         Args&&... args) noexcept
+    : operation(std::forward<Args>(args)...) {}
+
+auto ReplicatedOperation::buildAbortAllOngoingTrxOperation() noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{std::in_place, AbortAllOngoingTrx{}};
+}
+
+auto ReplicatedOperation::buildCommitOperation(TransactionId tid) noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{std::in_place, Commit{tid}};
+}
+
+auto ReplicatedOperation::buildIntermediateCommitOperation(
+    TransactionId tid) noexcept -> ReplicatedOperation {
+  return ReplicatedOperation{std::in_place, IntermediateCommit{tid}};
+}
+
+auto ReplicatedOperation::buildAbortOperation(TransactionId tid) noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{std::in_place, Abort{tid}};
+}
+
+auto ReplicatedOperation::buildTruncateOperation(TransactionId tid,
+                                                 ShardID shard) noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{std::in_place, Truncate{tid, std::move(shard)}};
+}
+
+auto ReplicatedOperation::buildCreateShardOperation(CollectionID collection,
+                                                    ShardID shard) noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{
+      std::in_place, CreateShard{std::move(collection), std::move(shard)}};
+}
+
+auto ReplicatedOperation::buildDropShardOperation(CollectionID collection,
+                                                  ShardID shard) noexcept
+    -> ReplicatedOperation {
+  return ReplicatedOperation{
+      std::in_place, DropShard{std::move(collection), std::move(shard)}};
+}
+
+auto ReplicatedOperation::buildDocumentOperation(
+    TRI_voc_document_operation_e& op, TransactionId tid, ShardID shard,
+    velocypack::SharedSlice payload) noexcept -> ReplicatedOperation {
+  switch (op) {
+    case TRI_VOC_DOCUMENT_OPERATION_INSERT:
+      return ReplicatedOperation{
+          std::in_place,
+          Insert{DocumentOperation(tid, std::move(shard), std::move(payload))}};
+    case TRI_VOC_DOCUMENT_OPERATION_UPDATE:
+      return ReplicatedOperation{
+          std::in_place,
+          Update{DocumentOperation(tid, std::move(shard), std::move(payload))}};
+    case TRI_VOC_DOCUMENT_OPERATION_REPLACE:
+      return ReplicatedOperation{
+          std::in_place, Replace{DocumentOperation(tid, std::move(shard),
+                                                   std::move(payload))}};
+    case TRI_VOC_DOCUMENT_OPERATION_REMOVE:
+      return ReplicatedOperation{
+          std::in_place,
+          Remove{DocumentOperation(tid, std::move(shard), std::move(payload))}};
+    default:
+      ADB_PROD_ASSERT(false) << "Unexpected document operation " << op;
+  }
+  return {};  // should never be reached, but compiler complains about
+              // missing return
+}
+}  // namespace arangodb::replication2::replicated_state::document
---
+++ arangod/Replication2/StateMachines/Document/ReplicatedOperation.h
@@ -0,0 +1,206 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Alexandru Petenchea
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Cluster/ClusterTypes.h"
+#include "Inspection/Format.h"
+#include "Inspection/Status.h"
+#include "Inspection/Types.h"
+#include "VocBase/Identifiers/TransactionId.h"
+
+#include <velocypack/SharedSlice.h>
+
+#include <variant>
+
+namespace arangodb::replication2::replicated_state::document {
+
+/*
+ * When a log entry is received, the ReplicatedOperation tells the state machine
+ * what it has to do (i.e. start a transaction, drop a shard, ...).
+ */
+struct ReplicatedOperation {
+  ReplicatedOperation() = default;
+
+ private:
+  struct DocumentOperation {
+    TransactionId tid;
+    ShardID shard;
+    velocypack::SharedSlice payload;
+
+    explicit DocumentOperation(TransactionId tid, ShardID shard,
+                               velocypack::SharedSlice payload);
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, DocumentOperation& x) {
+      return f.object(x).fields(f.field("tid", x.tid),
+                                f.field("shard", x.shard),
+                                f.field("payload", x.payload));
+    }
+  };
+
+ public:
+  struct AbortAllOngoingTrx {
+    template<class Inspector>
+    friend auto inspect(Inspector& f, AbortAllOngoingTrx& x) {
+      return f.object(x).fields();
+    }
+  };
+
+  struct Commit {
+    TransactionId tid;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Commit& x) {
+      return f.object(x).fields(f.field("tid", x.tid));
+    }
+  };
+
+  struct IntermediateCommit {
+    TransactionId tid;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, IntermediateCommit& x) {
+      return f.object(x).fields(f.field("tid", x.tid));
+    }
+  };
+
+  struct Abort {
+    TransactionId tid;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Abort& x) {
+      return f.object(x).fields(f.field("tid", x.tid));
+    }
+  };
+
+  struct Truncate {
+    TransactionId tid;
+    ShardID shard;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Truncate& x) {
+      return f.object(x).fields(f.field("tid", x.tid),
+                                f.field("shard", x.shard));
+    }
+  };
+
+  struct CreateShard {
+    ShardID shard;
+    CollectionID collection;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, CreateShard& x) {
+      return f.object(x).fields(f.field("shard", x.shard),
+                                f.field("collection", x.collection));
+    }
+  };
+
+  struct DropShard {
+    ShardID shard;
+    CollectionID collection;
+
+    template<class Inspector>
+    friend auto inspect(Inspector& f, DropShard& x) {
+      return f.object(x).fields(f.field("shard", x.shard),
+                                f.field("collection", x.collection));
+    }
+  };
+
+  struct Insert : DocumentOperation {
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Insert& x) {
+      return f.object(x).fields(f.template embedFields<DocumentOperation>(x));
+    }
+  };
+
+  struct Update : DocumentOperation {
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Update& x) {
+      return f.object(x).fields(f.template embedFields<DocumentOperation>(x));
+    }
+  };
+
+  struct Replace : DocumentOperation {
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Replace& x) {
+      return f.object(x).fields(f.template embedFields<DocumentOperation>(x));
+    }
+  };
+
+  struct Remove : DocumentOperation {
+    template<class Inspector>
+    friend auto inspect(Inspector& f, Remove& x) {
+      return f.object(x).fields(f.template embedFields<DocumentOperation>(x));
+    }
+  };
+
+ public:
+  std::variant<AbortAllOngoingTrx, Commit, IntermediateCommit, Abort, Truncate,
+               CreateShard, DropShard, Insert, Update, Replace, Remove>
+      operation;
+
+  static auto buildAbortAllOngoingTrxOperation() noexcept
+      -> ReplicatedOperation;
+  static auto buildCommitOperation(TransactionId tid) noexcept
+      -> ReplicatedOperation;
+  static auto buildIntermediateCommitOperation(TransactionId tid) noexcept
+      -> ReplicatedOperation;
+  static auto buildAbortOperation(TransactionId tid) noexcept
+      -> ReplicatedOperation;
+  static auto buildTruncateOperation(TransactionId tid, ShardID shard) noexcept
+      -> ReplicatedOperation;
+  static auto buildCreateShardOperation(CollectionID collection,
+                                        ShardID shard) noexcept
+      -> ReplicatedOperation;
+  static auto buildDropShardOperation(CollectionID collection,
+                                      ShardID shard) noexcept
+      -> ReplicatedOperation;
+  static auto buildDocumentOperation(TRI_voc_document_operation_e& op,
+                                     TransactionId tid, ShardID shard,
+                                     velocypack::SharedSlice payload) noexcept
+      -> ReplicatedOperation;
+
+  template<typename Inspector>
+  friend auto inspect(Inspector& f, ReplicatedOperation& x) {
+    return f.variant(x.operation)
+        .embedded("type")
+        .alternatives(
+            inspection::type<AbortAllOngoingTrx>("AbortAllOngoingTrx"),
+            inspection::type<Commit>("Commit"),
+            inspection::type<IntermediateCommit>("IntermediateCommit"),
+            inspection::type<Abort>("Abort"),
+            inspection::type<Truncate>("Truncate"),
+            inspection::type<CreateShard>("CreateShard"),
+            inspection::type<DropShard>("DropShard"),
+            inspection::type<Insert>("Insert"),
+            inspection::type<Update>("Update"),
+            inspection::type<Replace>("Replace"),
+            inspection::type<Remove>("Remove"));
+  }
+
+ private:
+  template<typename... Args>
+  explicit ReplicatedOperation(std::in_place_t, Args&&... args) noexcept;
+};
+}  // namespace arangodb::replication2::replicated_state::document
