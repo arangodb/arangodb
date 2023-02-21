451319e57cea56c150e246c6c4e7bb3cc72f1750
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-02-21 11:21:54 +0100

    [FE-135] Cleanup VisNetwork

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/VisNetwork.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/VisNetwork.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Network } from "vis-network";
 
-const VisNetwork = ({graphData, graphName, layout, options}) => {
-	const [layoutOptions, setLayoutOptions] = useState(layout);
+const VisNetwork = ({graphData, graphName, options}) => {
+	const [layoutOptions, setLayoutOptions] = useState(options);
 
 	const nodes = [
 		...graphData.nodes
@@ -15,12 +15,13 @@ const VisNetwork = ({graphData, graphName, layout, options}) => {
 	const visJsRef = useRef(null);
 
 	useEffect(() => {
+		if(graphData.settings !== undefined) {
+			setLayoutOptions(graphData.settings.layout);
+		}
 		const network =
 			visJsRef.current &&
-			new Network(visJsRef.current, { nodes, edges }, layout );
-			//new Network(visJsRef.current, { nodes, edges }, options );
+			new Network(visJsRef.current, { nodes, edges }, layoutOptions );
 		// Use `network` here to configure events, etc
-		//console.log("network.getViewPosition(): ", network.getViewPosition());
 		network.on("selectNode", (event, params) => {
 			if (event.nodes.length === 1) {
 				console.log("selectNode (event): ", event);
@@ -46,22 +47,6 @@ const VisNetwork = ({graphData, graphName, layout, options}) => {
 				console.log("Object.values(nodes): ", Object.values(nodes));
 				const nodesArr = Object.values(nodes);
 				console.log("typeof nodesArr: ", typeof nodesArr);
-				/*
-				const clickedNode = nodes.get(nodeID);
-				console.log("clickedNode: ", clickedNode);
-				*/
-
-			  /*
-			  clickedNode.color = {
-				border: '#000000',
-				background: '#000000',
-				highlight: {
-				  border: '#2B7CE9',
-				  background: '#D2E5FF'
-				}
-			  }
-			  nodes.update(clickedNode);
-			  */
 			}
 			//network.redraw();
 			//console.log("Is network redrawn?");
@@ -71,24 +56,6 @@ const VisNetwork = ({graphData, graphName, layout, options}) => {
 			console.log("ONCONTEXT: ", event);
 		});
 	}, [visJsRef, nodes, edges, layoutOptions]);
-
-	const changeNodeStyle = () => {
-		const nodesTemp = nodes;
-		console.log("nodesTemp: ", nodesTemp);
-		nodesTemp.forEach((node) => {
-			console.log("node id: ", node.id);
-			console.log("node shape: ", node.shape);
-			node.shape = "circle";
-		});
-		console.log("nodes: ", nodes);
-		console.log("New nodes after chaning dot to circle: ", nodes);
-	}
-
-
-
-
-
-
 	return <>
 		<div id="visnetworkdiv" ref={visJsRef} style={{ height: '90vh', width: '100%', background: '#fff' }} />
 	</>;
