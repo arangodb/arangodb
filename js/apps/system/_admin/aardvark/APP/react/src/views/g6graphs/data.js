6cb3da91cc29c5fe4b43156cf28cdf2ae952272f
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-02-25 01:07:19 +0100

    [FE-196] Add loading node

--- js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/data.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/g6graphs/data.js
@@ -1,6 +1,16 @@
 export const data = {
   nodes: [
     {
+      "id": "loadingnode",
+      "label": "loading",
+      "size": 20,
+      "value": 2,
+      "shape": "dot",
+      "font": {
+          "strokeWidth": 2,
+          "strokeColor": "#ffffff",
+          "vadjust": -7
+      },
     },
   ],
   edges: [
