bef639eced9c44199210ec88418ed48036323f26
Palash Karia <palash.karia@gmail.com>
2023-03-01 03:23:33 +0530

    remove unused

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewSettingsReactView.jsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewSettingsReactView.jsx
@@ -1,18 +1,12 @@
 /* global frontendConfig */
-
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import "./split-pane-styles.css";
 import { ViewSettings } from "./ViewSettings";
-import "./viewsheader.css";
 
 const ViewSettingsReactView = ({ name }) => {
   return (
     <ChakraProvider>
-      <ViewSettings
-        isCluster={frontendConfig.isCluster}
-        name={name}
-      />
+      <ViewSettings isCluster={frontendConfig.isCluster} name={name} />
     </ChakraProvider>
   );
 };
