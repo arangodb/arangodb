805398c6ebbe042eaf2ba722ee8caf5b11fe2b6c
jsteemann <jsteemann@users.noreply.github.com>
2023-02-28 21:51:19 +0100

    fixes

--- lib/V8/v8-utils.cpp
+++ lib/V8/v8-utils.cpp
@@ -2335,7 +2335,7 @@ static void JS_LogLevel(v8::FunctionCallbackInfo<v8::Value> const& args) {
 
       uint32_t pos = 0;
 
-      for (auto level : levels) {
+      for (auto const& level : levels) {
         std::string output =
             level.first + "=" + Logger::translateLogLevel(level.second);
         v8::Handle<v8::String> val = TRI_V8_STD_STRING(isolate, output);
