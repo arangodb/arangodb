81970d719f83bd2c00cd4c8939f29bd224a4cfde
Palash Karia <palash.karia@gmail.com>
2023-02-28 23:19:50 +0530

    format CollectionsDropdown

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/Components/CollectionsDropdown.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/Components/CollectionsDropdown.tsx
@@ -1,4 +1,4 @@
-import { chain, isNull, map, without } from "lodash";
+import { chain, map, without } from "lodash";
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 import { components, MultiValueGenericProps } from "react-select";
@@ -6,7 +6,6 @@ import useSWR from "swr";
 import MultiSelect from "../../../components/pure-css/form/MultiSelect";
 import { getApiRouteForCurrentDB } from "../../../utils/arangoClient";
 import { FormState, ViewContext } from "../constants";
-
 
 const MultiValueLabel = (props: MultiValueGenericProps) => {
   const match = useRouteMatch();
@@ -30,24 +29,21 @@ const CollectionsDropdown = () => {
   const viewContext = useContext(ViewContext);
   const { dispatch, formState: fs, isAdminUser } = viewContext;
   const formState = fs as FormState;
-  const { data } = useSWR(['/collection', 'excludeSystem=true'], (path, qs) =>
+  const { data } = useSWR(["/collection", "excludeSystem=true"], (path, qs) =>
     getApiRouteForCurrentDB().get(path, qs)
   );
-  const [options, setOptions] = useState<{label: string, value: string}[]>([]);
+  const [options, setOptions] = useState<{ label: string; value: string }[]>(
+    []
+  );
   useEffect(() => {
     if (data) {
-      const linkKeys = chain(formState.links)
-        .omitBy(isNull)
-        .keys()
-        .value();
-      const collNames = map(data.body.result, 'name');
+      const collNames = map(data.body.result, "name");
       const tempOptions = collNames.sort().map(name => {
-          return {
-            label: name,
-            value: name
-          }
-      })
-      console.log({tempOptions, collNames, results: data.body.result, linkKeys})
+        return {
+          label: name,
+          value: name
+        };
+      });
       setOptions(tempOptions);
     }
   }, [data, formState.links]);
@@ -60,10 +56,12 @@ const CollectionsDropdown = () => {
         value: {}
       }
     });
-    setOptions(without(options, {
-      value: link,
-      label: link
-    }));
+    setOptions(
+      without(options, {
+        value: link,
+        label: link
+      })
+    );
   };
 
   const removeLink = async (link: string) => {
@@ -74,32 +72,45 @@ const CollectionsDropdown = () => {
         value: null
       }
     });
-    setOptions(options.concat([{
-      value: link,
-      label: link
-    }]).sort());
+    setOptions(
+      options
+        .concat([
+          {
+            value: link,
+            label: link
+          }
+        ])
+        .sort()
+    );
   };
 
-  const validLinks = chain(formState.links).toPairs().filter(pair => pair[1] !== null).map(pair => ({
-    label: pair[0],
-    value: pair[0]
-  })).value();
+  const validLinks = chain(formState.links)
+    .toPairs()
+    .filter(pair => pair[1] !== null)
+    .map(pair => ({
+      label: pair[0],
+      value: pair[0]
+    }))
+    .value();
 
-  console.log({links: formState.links, validLinks, data, addLink, removeLink, isAdminUser, options});
-  return <MultiSelect value={validLinks} options={options} components={{
-    MultiValueLabel
-  }}
-  onChange={(_, action) => {
-    if (action.action === "remove-value") {
-      removeLink((action.removedValue as any).value as string);
-      return;
-    }
-    if (action.action === "create-option") {
-      addLink((action.option as any).value as string);
-    }
-  }}
-  
-  />
+  return (
+    <MultiSelect
+      value={validLinks}
+      options={options}
+      components={{
+        MultiValueLabel
+      }}
+      onChange={(_, action) => {
+        if (action.action === "remove-value") {
+          removeLink((action.removedValue as any).value as string);
+          return;
+        }
+        if (action.action === "create-option") {
+          addLink((action.option as any).value as string);
+        }
+      }}
+    />
+  );
 };
 
 export default CollectionsDropdown;
