d7a6399b2f8e78a4460c8691e1083a56068bfeb9
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-03-02 16:07:14 +0100

    Bugfix BTS-1243 (#18194)
    * Fix BTS-1243
    
    * Improve startup
    
    * Other fix and review suggestion

--- arangod/IResearch/IResearchDataStore.cpp
+++ arangod/IResearch/IResearchDataStore.cpp
@@ -194,7 +194,8 @@ uint64_t computeAvg(std::atomic<uint64_t>& timeNum, uint64_t newTime) {
   auto const oldTime = oldTimeNum >> 32U;
   auto const oldNum = oldTimeNum & std::numeric_limits<uint32_t>::max();
   if (oldNum >= kWindowSize) {
-    timeNum.fetch_sub((oldTime / oldNum) + 1, std::memory_order_relaxed);
+    timeNum.fetch_sub(((oldTime / oldNum) << 32U) + 1,
+                      std::memory_order_relaxed);
   }
   return (oldTime + newTime) / (oldNum + 1);
 }
