916210634582e35f6298532fc3a0395e3369802a
jsteemann <jsteemann@users.noreply.github.com>
2023-03-04 13:49:47 +0100

    Make `simplify-conditions` correctly report its invocation
    * make AQL optimizer rule `simplify-conditions` correctly report that it was
      triggered. Previously that rule never reported that it was triggered
      although even though it actually was.

--- arangod/Aql/OptimizerRules.cpp
+++ arangod/Aql/OptimizerRules.cpp
@@ -2608,9 +2608,6 @@ void arangodb::aql::simplifyConditionsRule(Optimizer* opt,
       AstNode* simplified = plan->getAst()->traverseAndModify(root, visitor);
       if (simplified != root) {
         nn->expression()->replaceNode(simplified);
-      }
-      // cppcheck-suppress knownConditionTrueFalse
-      if (modifiedNode) {
         nn->expression()->invalidateAfterReplacements();
         modified = true;
       }
