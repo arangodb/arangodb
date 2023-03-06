1f1de9c4b82bf1e080d8eb48bd2d243fda8eedfa
Alan Plum <pluma4345@users.noreply.github.com>
2023-03-06 11:59:34 +0100

    [FE-19] Rebuild web UI

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c79f81fff113fd6334afd1d3bc85ac2f.js"
+  "precache-manifest.acbc9100ce58845b44d2f6a411f8ef4e.js"
 );
 
 workbox.clientsClaim();
