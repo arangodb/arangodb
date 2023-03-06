4e1533fb9e374be00012f36922950c97fd457b19
Palash Karia <palash.karia@gmail.com>
2023-03-06 21:47:35 +0530

    feat: search-alias form

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/viewsList/IndexesForm.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/viewsList/IndexesForm.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { SelectControl } from "../../../components/form/SelectControl";
 import { AddNewViewFormValues } from "./AddNewViewForm.types";
 import { useCollectionsList } from "./useCollectionsList";
+import { useInvertedIndexList } from "./useInvertedIndexList";
 
 export const IndexesForm = () => {
   const { collectionsList } = useCollectionsList();
@@ -20,55 +21,13 @@ export const IndexesForm = () => {
         {({ remove, push }) => (
           <Box display={"grid"} rowGap="4">
             {values.indexes.map((value, index) => {
-              const { collection } = value;
-              const indexOptions = [{
-                value: '',
-                label: ''
-              }];
-              console.log({ collection });
-
               return (
-                <Box
-                  display={"grid"}
-                  gridColumnGap="4"
-                  gridTemplateColumns={"1fr 1fr 40px"}
-                  rowGap="5"
-                  alignItems={"end"}
-                  key={index}
-                >
-                  <Box>
-                    <FormLabel htmlFor={`indexes.${index}.collection`}>
-                      Collection
-                    </FormLabel>
-                    <SelectControl
-                      name={`indexes.${index}.collection`}
-                      selectProps={{
-                        options: collectionsOptions
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <FormLabel htmlFor={`indexes.${index}.collection`}>
-                      Index
-                    </FormLabel>
-                    <SelectControl
-                      name={`indexes.${index}.collection`}
-                      selectProps={{
-                        options: indexOptions
-                      }}
-                    />
-                  </Box>
-                  {index > 0 ? (
-                    <IconButton
-                      size="sm"
-                      variant="ghost"
-                      colorScheme="red"
-                      aria-label="Remove field"
-                      icon={<CloseIcon />}
-                      onClick={() => remove(index)}
-                    />
-                  ) : null}
-                </Box>
+                <CollectionIndexRow
+                  value={value}
+                  index={index}
+                  collectionsOptions={collectionsOptions}
+                  remove={remove}
+                />
               );
             })}
             <Button
@@ -84,6 +43,65 @@ export const IndexesForm = () => {
           </Box>
         )}
       </FieldArray>
+    </Box>
+  );
+};
+const CollectionIndexRow = ({
+  value,
+  index,
+  collectionsOptions,
+  remove
+}: {
+  value: { collection: string; index: string };
+  index: number;
+  collectionsOptions: { value: string; label: string }[] | undefined;
+  remove: <T>(index: number) => T | undefined;
+}) => {
+  const { collection } = value;
+  const { indexesList } = useInvertedIndexList(collection);
+
+  const indexesOptions = indexesList?.map(indexItem => {
+    return { value: indexItem.name, label: indexItem.name };
+  });
+  return (
+    <Box
+      display={"grid"}
+      gridColumnGap="4"
+      gridTemplateColumns={"1fr 1fr 40px"}
+      rowGap="5"
+      alignItems={"end"}
+      key={index}
+    >
+      <Box>
+        <FormLabel htmlFor={`indexes.${index}.collection`}>
+          Collection
+        </FormLabel>
+        <SelectControl
+          name={`indexes.${index}.collection`}
+          selectProps={{
+            options: collectionsOptions
+          }}
+        />
+      </Box>
+      <Box>
+        <FormLabel htmlFor={`indexes.${index}.index`}>Index</FormLabel>
+        <SelectControl
+          name={`indexes.${index}.index`}
+          selectProps={{
+            options: indexesOptions
+          }}
+        />
+      </Box>
+      {index > 0 ? (
+        <IconButton
+          size="sm"
+          variant="ghost"
+          colorScheme="red"
+          aria-label="Remove field"
+          icon={<CloseIcon />}
+          onClick={() => remove(index)}
+        />
+      ) : null}
     </Box>
   );
 };
