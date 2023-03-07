9ad18ad84696dde89a96d925d6048440d101e0cd
Palash Karia <palash.karia@gmail.com>
2023-03-07 11:40:41 +0530

    fix: but with SWR caching causes fetch to not work

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/forms/inputs/CollectionsDropdown.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/forms/inputs/CollectionsDropdown.tsx
@@ -32,8 +32,8 @@ const CollectionsDropdown = () => {
   const viewContext = useContext(ViewContext);
   const { dispatch, formState: fs, isAdminUser } = viewContext;
   const formState = fs as FormState;
-  const { data } = useSWR(["/collection", "excludeSystem=true"], ([path, qs]) =>
-    getApiRouteForCurrentDB().get(path, qs)
+  const { data } = useSWR("/collection?excludeSystetm=true", () =>
+    getApiRouteForCurrentDB().get("/collection", "excludeSystetm=true")
   );
   const [options, setOptions] = useState<{ label: string; value: string }[]>(
     []
