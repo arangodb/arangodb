1c01c81b102c773ebb0cc6bbf1104e4ff1c01ebf
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-03-10 17:27:03 +0530

    chore: allow creation of view without indexes specificed

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/viewsList/addNewViewForm/AddNewViewModal.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/viewsList/addNewViewForm/AddNewViewModal.tsx
@@ -11,6 +11,33 @@ import {
 import { AddNewViewForm } from "./AddNewViewForm";
 import { useAddView } from "./useAddView";
 
+const initialValues = {
+  name: "",
+  type: "arangosearch",
+  primarySortCompression: "lz4",
+  primarySort: [
+    {
+      field: "",
+      direction: "asc"
+    }
+  ],
+  storedValues: [
+    {
+      fields: [],
+      compression: "lz4"
+    }
+  ],
+  writebufferIdle: 64,
+  writebufferActive: 0,
+  writebufferSizeMax: 33554432,
+  indexes: [
+    {
+      collection: "",
+      index: ""
+    }
+  ]
+};
+
 export const AddNewViewModal = ({
   onClose,
   isOpen
@@ -18,49 +45,30 @@ export const AddNewViewModal = ({
   onClose: () => void;
   isOpen: boolean;
 }) => {
+  const getFinalValues = (values: typeof initialValues) => {
+    if (values.type === "arangosearch") {
+      return { ...values, indexes: undefined };
+    }
+    const finalIndexes = values.indexes
+      .filter(indexItem => indexItem.collection && indexItem.index)
+      .filter(Boolean);
+    return {
+      type: values.type,
+      name: values.name,
+      indexes: finalIndexes
+    };
+  };
   const { onAdd } = useAddView();
   return (
     <Modal size="6xl" onClose={onClose} isOpen={isOpen}>
       <ModalHeader>Create New View</ModalHeader>
       <Formik
-        initialValues={{
-          name: "",
-          type: "arangosearch",
-          primarySortCompression: "lz4",
-          primarySort: [
-            {
-              field: "",
-              direction: "asc"
-            }
-          ],
-          storedValues: [
-            {
-              fields: [],
-              compression: "lz4"
-            }
-          ],
-          writebufferIdle: 64,
-          writebufferActive: 0,
-          writebufferSizeMax: 33554432,
-          indexes: [
-            {
-              collection: "",
-              index: ""
-            }
-          ]
-        }}
+        initialValues={initialValues}
         validationSchema={Yup.object({
           name: Yup.string().required("Name is required")
         })}
         onSubmit={(values, { setSubmitting }) => {
-          const finalValues =
-            values.type === "arangosearch"
-              ? { ...values, indexes: undefined }
-              : {
-                  type: values.type,
-                  name: values.name,
-                  indexes: values.indexes
-                };
+          const finalValues = getFinalValues(values);
           onAdd(finalValues).then(() => {
             setSubmitting(false);
             onClose();
