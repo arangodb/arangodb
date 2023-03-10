2f8e44b011ef07fb9dbde82163c324cb1db27c98
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-03-10 10:13:48 +0000

    Introduce GraphLoader and GraphStorer

---
+++ arangod/Pregel/GraphStore/GraphLoader.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "GraphLoader.h"
+
+namespace arangodb::pregel {
+
+template<typename V, typename E>
+auto GraphLoader<V, E>::load() -> std::unique_ptr<Quiver<V, E>> {
+  return std::make_unique<Quiver<V, E>>();
+};
+
+}  // namespace arangodb::pregel
