de66df065fed7c05db7aa1473f40d392b0079bf2
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-03-13 14:24:13 +0100

    [FE-196] Add ChakraUI provider

--- js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/VisGraphReactView.jsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/VisGraphReactView.jsx
@@ -5,13 +5,17 @@ import './graph.css';
 import VisJsGraph from './VisJsGraph';
 import { useDisableNavBar } from "../../utils/useDisableNavBar";
 import { useGlobalStyleReset } from "../../utils/useGlobalStyleReset";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../../theme/theme";
 
 const VisGraphReactView = () => {
   useDisableNavBar();
   useGlobalStyleReset();
   
   return <div className="graphReactViewContainer">
-    <VisJsGraph />
+    <ChakraProvider theme={theme}>
+      <VisJsGraph />
+    </ChakraProvider>
   </div>;
 };
 
