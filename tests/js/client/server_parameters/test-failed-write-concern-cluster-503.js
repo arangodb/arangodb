3b91d563111d0d7d569f8b7355f887c10ade3cf1
Max Neunhoeffer <max@arangodb.com>
2023-03-13 15:39:19 +0100

    Add more tests.

--- tests/js/client/server_parameters/test-failed-write-concern-cluster-503.js
+++ tests/js/client/server_parameters/test-failed-write-concern-cluster-503.js
@@ -60,12 +60,18 @@ function testSuite() {
       let follower1 = getEndpointById(servers[1]);
 
       try {
+        // Insert two documents:
+        let d1 = c.insert({Hallo:1});
+        let d2 = c.insert({Hallo:2});
+
         reconnectRetry(follower1, "_system", "root", "");
         arango.PUT_RAW("/_admin/debug/failat/LogicalCollection::insert", {});
         arango.PUT_RAW("/_admin/debug/failat/SynchronizeShard::disable", {});
         reconnectRetry(coordinator, "_system", "root", "");
 
-        c.insert({Hallo:1});  // This drops the followers, but works
+        let d = c.insert({Hallo:1});  // This drops the followers, but works
+
+        // INSERT test, single document:
         let startTime = new Date();
         let res = arango.POST_RAW(`/_api/document/${cn}`, {Hallo:2});
         assertEqual(503, res.code);
@@ -75,6 +81,94 @@ function testSuite() {
         // 5 seconds are enough to keep the test stable (usually, the
         // call returns after 3 milliseconds).
         assertTrue(timeSpentMs < 5000);
+
+        // INSERT test, batch:
+        startTime = new Date();
+        res = arango.POST_RAW(`/_api/document/${cn}`, [{Hallo:2},{Hallo:3}]);
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // REPLACE test, single document:
+        startTime = new Date();
+        res = arango.PUT_RAW(`/_api/document/${cn}/d._key`, {Hallo:2});
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // REPLACE test, batch:
+        startTime = new Date();
+        res = arango.PUT_RAW(`/_api/document/${cn}`,
+             [{_key:d1._key, Hallo:2},{_key:d2._key, Hallo:3}]);
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // UPDATE test, single document:
+        startTime = new Date();
+        res = arango.PATCH_RAW(`/_api/document/${cn}/d._key`, {Hallo:2});
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // UPDATE test, batch:
+        startTime = new Date();
+        res = arango.PATCH_RAW(`/_api/document/${cn}`,
+             [{_key:d1._key, Hallo:2},{_key:d2._key, Hallo:3}]);
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // DELETE test, single document:
+        startTime = new Date();
+        res = arango.DELETE_RAW(`/_api/document/${cn}/d._key`, {});
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+        // DELETE test, batch:
+        startTime = new Date();
+        res = arango.DELETE_RAW(`/_api/document/${cn}`, [d1._key, d2._key]);
+        console.error(JSON.stringify(res), new Date() - startTime);
+        assertEqual(503, res.code);
+        timeSpentMs = new Date() - startTime;
+        // With this error code the coordinator does not do a retry. Therefore,
+        // we should not get a significant delay. The 5 seconds are enough to
+        // keep the test stable (usually, the call returns after
+        // 3 milliseconds).
+        assertTrue(timeSpentMs < 5000);
+
+
       } finally {
         reconnectRetry(follower1, "_system", "root", "");
         arango.DELETE_RAW("/_admin/debug/failat/LogicalCollection::insert");
