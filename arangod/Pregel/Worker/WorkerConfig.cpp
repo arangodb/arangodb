034307e93d1a46c20bbdeded6dd6a97bbdb55b82
Julia Volmer <julia.volmer@arangodb.com>
2023-03-13 16:36:36 +0100

    [GORDO-1548] Add loading conductor state (#18262)
    * Add loading conductor state
    
    * Get rid of ClusterInfo include in WorkerConfig.h
    
    ConductorStateTest should not be forced to link against more arango stuff
    
    * Fix dependency of ConductorStateTest on AggregatorHandler

--- arangod/Pregel/Worker/WorkerConfig.cpp
+++ arangod/Pregel/Worker/WorkerConfig.cpp
@@ -21,12 +21,13 @@
 /// @author Simon Grätzer
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "Cluster/ClusterFeature.h"
 #include "Pregel/Conductor/Messages.h"
 #include "ApplicationFeatures/ApplicationServer.h"
 #include "Pregel/Worker/WorkerConfig.h"
 #include "Pregel/Algorithm.h"
 #include "Pregel/PregelFeature.h"
-#include "Pregel/Utils.h"
+#include "Pregel/ResolveShard.h"
 #include "VocBase/vocbase.h"
 
 using namespace arangodb;
@@ -109,8 +110,8 @@ VertexID WorkerConfig::documentIdToPregel(std::string_view documentID) const {
   ShardID responsibleShard;
 
   auto& ci = _vocbase->server().getFeature<ClusterFeature>().clusterInfo();
-  Utils::resolveShard(ci, this, std::string(collPart), StaticStrings::KeyString,
-                      keyPart, responsibleShard);
+  ResolveShard::resolve(ci, this, std::string(collPart),
+                        StaticStrings::KeyString, keyPart, responsibleShard);
 
   PregelShard source = this->shardId(responsibleShard);
   return VertexID(source, std::string(keyPart));
