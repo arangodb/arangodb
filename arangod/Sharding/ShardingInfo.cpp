54613ba7534a340c69846c5e523c453c558c4ffc
mchacki <michael@arangodb.com>
2023-03-13 11:21:02 +0000

    Reactivate Mock sharding strategy again

--- arangod/Sharding/ShardingInfo.cpp
+++ arangod/Sharding/ShardingInfo.cpp
@@ -33,6 +33,7 @@
 #include "Logger/LogMacros.h"
 #include "Sharding/ShardingFeature.h"
 #include "Sharding/ShardingStrategyDefault.h"
+#include "StorageEngine/EngineSelectorFeature.h"
 #include "Utils/CollectionNameResolver.h"
 #include "VocBase/LogicalCollection.h"
 #include "VocBase/vocbase.h"
@@ -165,6 +166,17 @@ ShardingInfo::ShardingInfo(arangodb::velocypack::Slice info,
   // set the sharding strategy
 
   auto& server = _collection->vocbase().server();
+#ifdef ARANGODB_USE_GOOGLE_TESTS
+  auto const& engineSelection =
+      server.getFeature<arangodb::EngineSelectorFeature>();
+  if (!ServerState::instance()->isRunningInCluster() &&
+      engineSelection.engineName() == "Mock") {
+    // shortcut, so we do not need to set up the whole application
+    // server for testing
+    _shardingStrategy = std::make_unique<ShardingStrategyNone>();
+    return;
+  }
+#endif
   _shardingStrategy =
       server.getFeature<ShardingFeature>().fromVelocyPack(info, this);
   TRI_ASSERT(_shardingStrategy != nullptr);
