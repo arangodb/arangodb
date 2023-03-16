6b982ba1d9bdb7f16c439454f3f271d6256de79e
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-03-16 15:42:31 +0100

    [FE-196] Resolve review comments (move StyledContextComponent out of VisNetwork function)

--- js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/VisNetwork.js
+++ js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/VisNetwork.js
@@ -5,6 +5,20 @@ import { isEqual } from "lodash";
 import { ProgressBar } from "./ProgressBar";
 import { Box, Center, Progress } from '@chakra-ui/react';
 
+const StyledContextComponent = styled.div`
+	position: absolute;
+	left: ${(props) => props.left};
+	top: ${(props) => props.top};
+	display: flex;
+	flex-direction: column;
+	z-index: 99999;
+	background-color: rgb(85, 85, 85);
+	color: #ffffff;
+	border-radius: 10px;
+	box-shadow: 0 5px 15px rgb(85 85 85 / 50%);
+	padding: 10px 0;
+`;
+
 const VisNetwork = ({graphData, graphName, options, selectedNode, onSelectNode, onSelectEdge, onDeleteNode, onDeleteEdge, onEditNode, onEditEdge, onSetStartnode, onExpandNode, onAddNodeToDb, onAddEdge}) => {
 	const [layoutOptions, setLayoutOptions] = useState(options);
 	const [contextMenu, toggleContextMenu] = useState("");
@@ -29,20 +43,6 @@ const VisNetwork = ({graphData, graphName, options, selectedNode, onSelectNode, 
 	];
 
 	const visJsRef = useRef(null);
-
-	const StyledContextComponent = styled.div`
-		position: absolute;
-		left: ${(props) => props.left};
-		top: ${(props) => props.top};
-		display: flex;
-		flex-direction: column;
-		z-index: 99999;
-		background-color: rgb(85, 85, 85);
-		color: #ffffff;
-		border-radius: 10px;
-		box-shadow: 0 5px 15px rgb(85 85 85 / 50%);
-		padding: 10px 0;
-	`;
 
 	useEffect(() => {
 		if(graphData.settings !== undefined) {
