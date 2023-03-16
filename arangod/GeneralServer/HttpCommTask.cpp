60e0249316d4e58b69fdf498b750f1be6800f224
Max Neunhoeffer <max@arangodb.com>
2023-03-16 11:31:35 +0100

    More logging for HTTP/1.

--- arangod/GeneralServer/HttpCommTask.cpp
+++ arangod/GeneralServer/HttpCommTask.cpp
@@ -543,27 +543,42 @@ void HttpCommTask<T>::doProcessRequest() {
 
     std::string_view body = _request->rawPayload();
     this->_generalServerFeature.countHttp1Request(body.size());
-    if (!body.empty() && Logger::isEnabled(LogLevel::TRACE, Logger::REQUESTS) &&
+    if (Logger::isEnabled(LogLevel::TRACE, Logger::REQUESTS) &&
         Logger::logRequestParameters()) {
-      std::string bodyForLogging;
-      try {
-        velocypack::Slice s = _request->payload(false);
-        if (!s.isNone()) {
-          // "none" can happen if the content-type is neither JSON nor vpack
-          bodyForLogging = StringUtils::escapeUnicode(s.toJson());
+      // Log HTTP headers:
+      std::string headersForLogging =
+          StringUtils::headersToString(_request->headers());
+      LOG_TOPIC("b9e77", TRACE, Logger::REQUESTS)
+          << "\"http-request-headers\",\"" << (void*)this << "\",\""
+          << headersForLogging << "\"";
+      if (!body.empty()) {
+        std::string bodyForLogging;
+        if (_request->contentType() == ContentType::JSON ||
+            _request->contentType() == ContentType::HTML ||
+            _request->contentType() == ContentType::TEXT) {
+          bodyForLogging = StringUtils::escapeUnicode(
+              std::string_view(_response->data(), _response->size()));
+        } else {
+          try {
+            velocypack::Slice s = _request->payload(false);
+            if (!s.isNone()) {
+              // "none" can happen if the content-type is neither JSON nor vpack
+              bodyForLogging = StringUtils::escapeUnicode(s.toJson());
+            }
+          } catch (...) {
+            // cannot stringify request body
+          }
+
+          if (bodyForLogging.empty() && !body.empty()) {
+            bodyForLogging = "potential binary data";
+          }
         }
-      } catch (...) {
-        // cannot stringify request body
-      }
 
-      if (bodyForLogging.empty() && !body.empty()) {
-        bodyForLogging = "potential binary data";
+        LOG_TOPIC("b9e76", TRACE, Logger::REQUESTS)
+            << "\"http-request-body\",\"" << (void*)this << "\",\""
+            << rest::contentTypeToString(_request->contentType()) << "\",\""
+            << _request->contentLength() << "\",\"" << bodyForLogging << "\"";
       }
-
-      LOG_TOPIC("b9e76", TRACE, Logger::REQUESTS)
-          << "\"http-request-body\",\"" << (void*)this << "\",\""
-          << rest::contentTypeToString(_request->contentType()) << "\",\""
-          << _request->contentLength() << "\",\"" << bodyForLogging << "\"";
     }
   }
 
@@ -742,6 +757,45 @@ void HttpCommTask<T>::sendResponse(std::unique_ptr<GeneralResponse> baseRes,
   TRI_ASSERT(_response == nullptr);
   _response = response.stealBody();
   // append write buffer and statistics
+
+  if (Logger::isEnabled(LogLevel::TRACE, Logger::REQUESTS) &&
+      Logger::logRequestParameters()) {
+    // Log HTTP headers:
+    std::string headersForLogging =
+        StringUtils::headersToString(response.headers());
+    LOG_TOPIC("8f553", TRACE, Logger::REQUESTS)
+        << "\"http-response-headers\",\"" << (void*)this << "\",\""
+        << headersForLogging << "\"";
+    if (!_response->empty()) {
+      std::string bodyForLogging;
+      if (response.contentType() == ContentType::JSON ||
+          response.contentType() == ContentType::HTML ||
+          response.contentType() == ContentType::TEXT) {
+        bodyForLogging = StringUtils::escapeUnicode(
+            std::string_view(_response->data(), _response->size()));
+      } else {
+        try {
+          velocypack::Slice s(
+              reinterpret_cast<uint8_t const*>(_response->data()));
+          if (!s.isNone()) {
+            // "none" can happen if the content-type is neither JSON nor vpack
+            bodyForLogging = StringUtils::escapeUnicode(s.toJson());
+          }
+        } catch (...) {
+          // cannot stringify request body
+        }
+
+        if (bodyForLogging.empty() && !_response->empty()) {
+          bodyForLogging = "potential binary data";
+        }
+      }
+
+      LOG_TOPIC("8f554", TRACE, Logger::REQUESTS)
+          << "\"http-response-body\",\"" << (void*)this << "\",\""
+          << rest::contentTypeToString(response.contentType()) << "\",\""
+          << bodyForLogging.size() << "\",\"" << bodyForLogging << "\"";
+    }
+  }
 
   // and give some request information
   LOG_TOPIC("8f555", DEBUG, Logger::REQUESTS)
