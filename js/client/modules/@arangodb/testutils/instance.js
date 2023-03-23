56b2cf1ec21bd7e5a572c60e941cad91c364a3aa
Wilfried Goesgens <willi@arangodb.com>
2023-03-23 22:10:46 +0100

    retry faster

--- js/client/modules/@arangodb/testutils/instance.js
+++ js/client/modules/@arangodb/testutils/instance.js
@@ -796,7 +796,7 @@ class instance {
     let res = statusExternal(this.pid, false);
     if (res.status === 'NOT-FOUND') {
       print(`${Date()} ${this.name}: PID ${this.pid} missing on our list, retry?`);
-      time.sleep(1);
+      time.sleep(0.2);
       res = statusExternal(this.pid, false);
     }
     const running = res.status === 'RUNNING';
