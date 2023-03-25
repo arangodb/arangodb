5423874339c8fb9e814e02600593af787edd552f
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-03-25 14:54:22 +0530

    fix: disable hierarchical layout after setting to allowing moving nodes

--- js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/GraphNetwork.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/GraphNetwork.tsx
@@ -4,39 +4,59 @@ import { Network } from "vis-network";
 import { useGraph } from "./GraphContext";
 import { GraphRightClickMenu } from "./GraphRightClickMenu";
 
+let timer: number;
+let hasDrawnOnce = false;
+function registerNetwork(newNetwork: Network) {
+  newNetwork.on("stabilizationIterationsDone", function() {
+    newNetwork.fit();
+    newNetwork.setOptions({
+      physics: false
+    });
+  });
+  newNetwork.on("startStabilizing", function() {
+    if (hasDrawnOnce) {
+      newNetwork.stopSimulation();
+    }
+    hasDrawnOnce = true;
+  });
+  newNetwork.on("stabilized", () => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      newNetwork.stopSimulation();
+      newNetwork.setOptions({
+        physics: false,
+        layout: {
+          hierarchical: false
+        }
+      });
+    }, 1000);
+  });
+}
+
 export const GraphNetwork = () => {
   const visJsRef = useRef<HTMLDivElement>(null);
-  const { graphData, network, setNetwork } = useGraph();
+  const { graphData, setNetwork } = useGraph();
   const { edges, nodes, settings } = graphData || {};
-  const { layout } = settings || {};
+  const { layout: options } = settings || {};
   useEffect(() => {
-    if (!nodes || !edges || network) {
+    if (!nodes || !edges || !visJsRef.current) {
       return;
     }
     const nodesDataSet = new DataSet(nodes);
     const edgesDataSet = new DataSet(edges);
-    const newNetwork =
-      visJsRef.current &&
-      new Network(
-        visJsRef.current,
-        {
-          nodes: nodesDataSet,
-          edges: edgesDataSet
-        },
-        layout
-      );
-    newNetwork && setNetwork(newNetwork);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [edges, nodes, network]);
-  useEffect(() => {
-    if (!nodes || !edges) {
-      return;
-    }
-    const nodesDataSet = new DataSet(nodes);
-    const edgesDataSet = new DataSet(edges);
-    network && network.setData({ nodes: nodesDataSet, edges: edgesDataSet });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [nodes, edges]);
+    const newNetwork = new Network(
+      visJsRef.current,
+      {
+        nodes: nodesDataSet,
+        edges: edgesDataSet
+      },
+      options
+    );
+    setNetwork(newNetwork);
+    registerNetwork(newNetwork);
+  }, [edges, nodes, options, setNetwork]);
+
   return (
     <>
       <GraphRightClickMenu visJsRef={visJsRef} />
