2916e836f9db06e5a9788070c1093e0864d78e2f
mpoeter <manuel@arangodb.com>
2023-03-25 17:23:44 +0100

    Simplify script and include more files for linting

--- utils/eslint.sh
+++ utils/eslint.sh
@@ -16,11 +16,7 @@ if [ -z "$*" ] ; then
     $(find "${WD}/tests/js/common" -name "*.js" -o -name "*.inc" | grep -v "test-data") \
     $(find "${WD}/tests/js/client" -name "*.js" -o -name "*.inc") \
     \
-    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/models" -name "*.js") \
-    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/views" -name "*.js") \
-    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/collections" -name "*.js") \
-    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/routers" -name "*.js") \
-    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/arango" -name "*.js") \
+    $(find "${WD}/js/apps/system/_admin/aardvark/APP/frontend/js/" -name "*.js") \
     \
     $(find "${WD}/scripts" -name "*.js") \
     \
