c03bc4fae9cc66492bc8d2044f0aa7e39e2dd1bb
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-03-30 16:35:02 +0200

    Fix community build

--- arangod/IResearch/IResearchViewCoordinator.cpp
+++ arangod/IResearch/IResearchViewCoordinator.cpp
@@ -188,10 +188,12 @@ Result IResearchViewCoordinator::appendVPackImpl(VPackBuilder& build,
     build.add(StaticStrings::LinksField, VPackValue(VPackValueType::Object));
     auto const accept = [](std::string_view key) {
       return key != iresearch::StaticStrings::AnalyzerDefinitionsField &&
+#ifdef USE_ENTERPRISE
+             key != iresearch::StaticStrings::kOptimizeTopKField &&
+#endif
              key != iresearch::StaticStrings::PrimarySortField &&
              key != iresearch::StaticStrings::PrimarySortCompressionField &&
              key != iresearch::StaticStrings::StoredValuesField &&
-             key != iresearch::StaticStrings::kOptimizeTopKField &&
              key != iresearch::StaticStrings::VersionField &&
              key != iresearch::StaticStrings::CollectionNameField;
     };
