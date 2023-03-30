63933ee1e492a9481815a90797092a89eeb2256b
Alan Plum <alan.plum-extern@arangodb.com>
2023-03-30 23:38:19 +0200

    Fix production build

--- js/apps/system/_admin/aardvark/APP/react/config-overrides.js
+++ js/apps/system/_admin/aardvark/APP/react/config-overrides.js
@@ -1,8 +1,9 @@
 const fs = require("node:fs");
 const path = require("node:path");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  webpack: (config, _env) => {
+  webpack: (config, env) => {
     Object.assign(config.resolve.alias, {
       "old-frontend": path.resolve(__dirname, "../frontend"),
       "/img": path.resolve(__dirname, "../frontend/img"),
@@ -15,40 +16,49 @@ module.exports = {
       querystring: require.resolve("querystring-es3"),
       url: require.resolve("url/"),
     };
-    config.module.rules.unshift(
-      {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader",
-          options: {
-            preprocessor: (content, loaderContext) =>
-              content.replace(
-                /<include src="(.+)"\/?>(?:<\/include>)?/gi,
-                (_, src) =>
-                  fs.readFileSync(
-                    path.resolve(loaderContext.context, src),
-                    "utf-8"
-                  )
-              ),
-          },
+    config.module.rules.unshift({
+      test: /\.html$/,
+      use: {
+        loader: "html-loader",
+        options: {
+          preprocessor: (content, loaderContext) =>
+            content.replace(
+              /<include src="(.+)"\/?>(?:<\/include>)?/gi,
+              (_, src) =>
+                fs.readFileSync(
+                  path.resolve(loaderContext.context, src),
+                  "utf-8"
+                )
+            ),
         },
       },
-      {
-        test: /\.ejs$/i,
-        use: "raw-loader",
-      }
-    );
-    const catchAllRule = config.module.rules.at(-1);
-    catchAllRule.oneOf.splice(-1, 0, {
+    });
+    const loaders = config.module.rules.at(-1).oneOf;
+    loaders.splice(-1, 0, {
       test: /\.css$/,
       use: ["style-loader", "css-loader"],
     });
-    catchAllRule.oneOf.at(-1).exclude = [
+    loaders.at(-1).exclude = [
       /^$/,
       /\.(js|mjs|jsx|ts|tsx|ejs)$/,
       /\.html$/,
       /\.json$/,
     ];
+    if (env === "production") {
+      config.module.rules.unshift({
+        test: /\.ejs$/,
+        loader: "underscore-template-loader",
+        options: {
+          attributes: [],
+        },
+      });
+      config.plugins.push(new CompressionPlugin());
+    } else {
+      config.module.rules.unshift({
+        test: /\.ejs$/i,
+        use: "raw-loader",
+      });
+    }
     return config;
   },
 };
