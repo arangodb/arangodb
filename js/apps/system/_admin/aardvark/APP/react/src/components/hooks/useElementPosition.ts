7ec0599de0faf9e39bfcaf108c6d178878e09971
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-04-01 03:30:22 +0530

    chore: update useElementPosition

--- js/apps/system/_admin/aardvark/APP/react/src/components/hooks/useElementPosition.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/components/hooks/useElementPosition.ts
@@ -4,23 +4,16 @@ import useResizeObserver from "@react-hook/resize-observer";
 export const useElementPosition = (
   target: MutableRefObject<HTMLElement | null>
 ) => {
-  const [position, setPosition] = useState<{ top: number; left: number }>();
+  const [position, setPosition] = useState<DOMRect>();
   useEffect(() => {
     const rect = target.current?.getBoundingClientRect();
-    const { top, left } = rect || {};
-    setPosition({
-      top: top || 0,
-      left: left || 0
-    });
+    setPosition(rect);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useResizeObserver(target, () => {
-    const { top, left } = target.current?.getBoundingClientRect() || {};
-    setPosition({
-      top: top || 0,
-      left: left || 0
-    });
+    const rect = target.current?.getBoundingClientRect();
+    setPosition(rect);
   });
 
   return position;
