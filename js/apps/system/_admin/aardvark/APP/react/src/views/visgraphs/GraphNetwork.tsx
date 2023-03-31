3d74a7a46b7557d2e217c423c85c0b74ed05d0a8
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-03-31 18:31:36 +0530

    chore: add progress bar

--- js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/GraphNetwork.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/visgraphs/GraphNetwork.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@chakra-ui/react";
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Box, Progress } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
 import { DataSet } from "vis-data";
 import { Network } from "vis-network";
 import { useElementPosition } from "../../components/hooks/useElementPosition";
@@ -8,53 +8,6 @@ import { GraphInfo } from "./GraphInfo";
 import { GraphRightClickMenu } from "./GraphRightClickMenu";
 
 let timer: number;
-function registerNetwork({
-  newNetwork,
-  onSelectEntity,
-  hasDrawnOnce
-}: {
-  newNetwork: Network;
-  onSelectEntity: (data: { entityId: string; type: "node" | "edge" }) => void;
-  hasDrawnOnce: MutableRefObject<boolean>;
-}) {
-  newNetwork.on("stabilizationIterationsDone", function() {
-    newNetwork.fit();
-    newNetwork.setOptions({
-      physics: false
-    });
-  });
-  newNetwork.on("startStabilizing", function() {
-    if (hasDrawnOnce.current) {
-      newNetwork.stopSimulation();
-    }
-    hasDrawnOnce.current = true;
-  });
-  newNetwork.on("stabilized", () => {
-    clearTimeout(timer);
-
-    timer = setTimeout(() => {
-      newNetwork.stopSimulation();
-      newNetwork.setOptions({
-        physics: false,
-        layout: {
-          hierarchical: false
-        }
-      });
-    }, 1000);
-  });
-
-  newNetwork.on("selectNode", event => {
-    if (event.nodes.length === 1) {
-      onSelectEntity({ entityId: event.nodes[0], type: "node" });
-    }
-  });
-
-  newNetwork.on("selectEdge", event => {
-    if (event.edges.length === 1) {
-      onSelectEntity({ entityId: event.edges[0], type: "edge" });
-    }
-  });
-}
 
 export const GraphNetwork = () => {
   const visJsRef = useRef<HTMLDivElement>(null);
@@ -69,6 +22,7 @@ export const GraphNetwork = () => {
   } = useGraph();
   const { edges, nodes, settings } = graphData || {};
   const { layout: options } = settings || {};
+  const [progressValue, setProgressValue] = useState(0);
   useEffect(() => {
     if (!nodes || !edges || !visJsRef.current) {
       return;
@@ -94,11 +48,56 @@ export const GraphNetwork = () => {
       newOptions
     );
     setNetwork(newNetwork);
-    registerNetwork({
-      newNetwork,
-      onSelectEntity,
-      hasDrawnOnce
-    });
+
+    function registerNetwork() {
+      newNetwork.on("stabilizationProgress", function(params) {
+        if (nodes?.length && nodes.length > 1) {
+          const widthFactor = params.iterations / params.total;
+          const calculatedProgressValue = Math.round(widthFactor * 100);
+          setProgressValue(calculatedProgressValue);
+        }
+      });
+      newNetwork.on("stabilizationIterationsDone", function() {
+        newNetwork.fit();
+        newNetwork.setOptions({
+          physics: false
+        });
+      });
+      newNetwork.on("startStabilizing", function() {
+        if (hasDrawnOnce.current) {
+          newNetwork.stopSimulation();
+        }
+        hasDrawnOnce.current = true;
+      });
+      newNetwork.on("stabilized", () => {
+        clearTimeout(timer);
+        setProgressValue(100);
+
+        timer = setTimeout(() => {
+          newNetwork.stopSimulation();
+          newNetwork.setOptions({
+            physics: false,
+            layout: {
+              hierarchical: false
+            }
+          });
+        }, 1000);
+      });
+
+      newNetwork.on("selectNode", event => {
+        if (event.nodes.length === 1) {
+          onSelectEntity({ entityId: event.nodes[0], type: "node" });
+        }
+      });
+
+      newNetwork.on("selectEdge", event => {
+        if (event.edges.length === 1) {
+          onSelectEntity({ entityId: event.edges[0], type: "edge" });
+        }
+      });
+    }
+
+    registerNetwork();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [edges, nodes, options, setNetwork]);
   const position = useElementPosition(visJsRef);
@@ -116,6 +115,7 @@ export const GraphNetwork = () => {
         }}
       />
       <Box id="graphNetworkWrap" height="full" backgroundColor="white">
+        {progressValue < 100 ? <Progress value={progressValue} /> : null}
         <Box ref={visJsRef} height="calc(100% - 40px)" width="full" />
         <GraphInfo />
       </Box>
