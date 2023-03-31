79f2f30d994a1756857e47f40c8ede51bab94eff
Wilfried Goesgens <willi@arangodb.com>
2023-03-31 18:23:22 +0200

    rename git directory

--- js/client/modules/@arangodb/testsuites/rta_makedata.js
+++ js/client/modules/@arangodb/testsuites/rta_makedata.js
@@ -180,7 +180,7 @@ function makeDataWrapper (options) {
 exports.setup = function (testFns, opts, fnDocs, optionsDoc, allTestPaths) {
   Object.assign(allTestPaths, testPaths);
   testFns['rta_makedata'] = makeDataWrapper;
-  opts['rtasource'] = fs.makeAbsolute(fs.join('.', '3rdParty', 'rta_makedata'));
+  opts['rtasource'] = fs.makeAbsolute(fs.join('.', '3rdParty', 'rta-makedata'));
   for (var attrname in functionsDocumentation) { fnDocs[attrname] = functionsDocumentation[attrname]; }
   for (var i = 0; i < optionsDocumentation.length; i++) { optionsDoc.push(optionsDocumentation[i]); }
 };
