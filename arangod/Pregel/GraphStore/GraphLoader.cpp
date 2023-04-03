583649083fdae4f40424e2669e85af984f493b6e
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-04-03 20:37:08 +0100

    Renovate GraphLoader a tiny bit

--- arangod/Pregel/GraphStore/GraphLoader.cpp
+++ arangod/Pregel/GraphStore/GraphLoader.cpp
@@ -84,8 +84,8 @@ auto GraphLoader<V, E>::requestVertexIds(uint64_t numVertices) -> void {
 }
 
 template<typename V, typename E>
-auto GraphLoader<V, E>::load() -> Magazine<V, E> {
-  auto result = Magazine<V, E>{};
+auto GraphLoader<V, E>::load() -> std::shared_ptr<Quiver<V, E>> {
+  auto result = std::shared_ptr<Quiver<V, E>>{};
   // Contains the shards located on this db server in the right order
   // assuming edges are sharded after _from, vertices after _key
   // then every ith vertex shard has the corresponding edges in
@@ -129,7 +129,7 @@ auto GraphLoader<V, E>::load() -> Magazine<V, E> {
       }
 
       try {
-        result.emplace(loadVertices(vertexShard, edges));
+        result = loadVertices(vertexShard, edges);
       } catch (basics::Exception const& ex) {
         LOG_PREGEL("8682a", WARN)
             << "caught exception while loading pregel graph: " << ex.what();
@@ -145,8 +145,8 @@ auto GraphLoader<V, E>::load() -> Magazine<V, E> {
 
   std::visit(overload{[&](ActorLoadingUpdate const& update) {
                         update.fn(message::GraphLoadingUpdate{
-                            .verticesLoaded = result.numberOfVertices(),
-                            .edgesLoaded = result.numberOfEdges(),
+                            .verticesLoaded = result->numberOfVertices(),
+                            .edgesLoaded = result->numberOfEdges(),
                             .memoryBytesUsed = 0  // TODO
                         });
                       },
