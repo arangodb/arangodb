10ab527e5ed8900f6d83d2d52d5490c31b419e19
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-04-07 14:56:11 +0530

    chore: add encodeHelper, remove unused
    # Conflicts:
    #	js/apps/system/_admin/aardvark/APP/react/src/views/views/searchAliasView/useUpdateAliasViewProperties.ts
    #	js/apps/system/_admin/aardvark/APP/react/src/views/views/viewsList/ViewTile.tsx

---
+++ js/apps/system/_admin/aardvark/APP/react/src/utils/encodeHelper.ts
@@ -0,0 +1,8 @@
+export const encodeHelper = (url: string) => {
+  const normalized = url;
+  const encoded = normalized ? encodeURIComponent(normalized) : "";
+  const advanced = normalized
+    ? encodeURIComponent(normalized.replace(/%/g, "%25"))
+    : "";
+  return { encoded, normalized, advanced };
+};
