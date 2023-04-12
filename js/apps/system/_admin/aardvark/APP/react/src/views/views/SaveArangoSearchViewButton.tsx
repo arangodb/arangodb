c512bc9ba07c1be0bc17a4c93f6a81fecf2f3a2d
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-04-12 23:08:19 +0530

    wip save

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/SaveArangoSearchViewButton.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/SaveArangoSearchViewButton.tsx
@@ -1,8 +1,8 @@
 import { CheckIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/react";
 import { pick } from "lodash";
-import React from "react";
-import { mutate } from "swr";
+import React, { useEffect } from "react";
+import { useSWRConfig } from "swr";
 import { getApiRouteForCurrentDB } from "../../utils/arangoClient";
 import { FormState } from "./constants";
 
@@ -40,68 +40,18 @@ function useHandleSave(
   oldName: string | undefined,
   setChanged: (changed: boolean) => void
 ) {
+  useSyncPatchViewJob({ view });
   const handleSave = async () => {
-    const route = getApiRouteForCurrentDB();
-    let result;
-    let error = false;
-    const path = `/view/${view.name}/properties`;
-
+    let hasError = false;
+    const isNameChanged = (oldName && view.name !== oldName) || false;
     try {
-      if (oldName && view.name !== oldName) {
-        result = await route.put(`/view/${oldName}/rename`, {
-          name: view.name
-        });
-
-        if (result.body.error) {
-          window.arangoHelper.arangoError(
-            "Failure",
-            `Got unexpected server response: ${result.body.errorMessage}`
-          );
-          error = true;
-        }
+      if (isNameChanged && oldName) {
+        hasError = await putRenameView({ oldName, view });
       }
 
-      if (!error) {
-        window.arangoHelper.arangoMessage(
-          "Saving",
-          `Please wait while the view is being saved. This could take some time for large views.`
-        );
-        const properties = pick(
-          view,
-          "consolidationIntervalMsec",
-          "commitIntervalMsec",
-          "cleanupIntervalStep",
-          "links",
-          "consolidationPolicy"
-        );
-        result = await route.patch(path, properties);
-        window.arangoHelper.hideArangoNotifications();
-
-        if (result.body.error) {
-          window.arangoHelper.arangoError(
-            "Failure",
-            `Got unexpected server response: ${result.body.errorMessage}`
-          );
-        } else {
-          window.sessionStorage.removeItem(oldName);
-          window.sessionStorage.removeItem(`${oldName}-changed`);
-          setChanged(false);
-
-          if (view.name === oldName) {
-            await mutate(path);
-          } else {
-            let newRoute = `#view/${view.name}`;
-            window.App.navigate(newRoute, {
-              trigger: true,
-              replace: true
-            });
-          }
-
-          window.arangoHelper.arangoNotification(
-            "Success",
-            `Updated View: ${view.name}`
-          );
-        }
+      if (!hasError) {
+        console.log("patch!");
+        await patchViewProperties({ view, oldName, setChanged });
       }
     } catch (e: any) {
       window.arangoHelper.arangoError(
@@ -112,3 +62,120 @@ function useHandleSave(
   };
   return handleSave;
 }
+
+async function putRenameView({
+  oldName,
+  view
+}: {
+  oldName: string;
+  view: FormState;
+}) {
+  const route = getApiRouteForCurrentDB();
+  let error = false;
+  const result = await route.put(`/view/${oldName}/rename`, {
+    name: view.name
+  });
+
+  if (result.body.error) {
+    window.arangoHelper.arangoError(
+      "Failure",
+      `Got unexpected server response: ${result.body.errorMessage}`
+    );
+    error = true;
+  }
+  return error;
+}
+
+async function patchViewProperties({
+  view,
+  oldName,
+  setChanged
+}: {
+  view: FormState;
+  oldName: string | undefined;
+  setChanged: (changed: boolean) => void;
+}) {
+  const path = `/view/${view.name}/properties`;
+
+  const route = getApiRouteForCurrentDB();
+  window.arangoHelper.arangoMessage(
+    "Saving",
+    `Please wait while the view is being saved. This could take some time for large views.`
+  );
+  const properties = pick(
+    view,
+    "consolidationIntervalMsec",
+    "commitIntervalMsec",
+    "cleanupIntervalStep",
+    "links",
+    "consolidationPolicy"
+  );
+  const result = await route.patch(
+    path,
+    properties,
+    {},
+    {
+      "x-arango-async": "store"
+    }
+  );
+  const asyncId = result.headers["x-arango-async-id"];
+  window.arangoHelper.addAardvarkJob({
+    id: asyncId,
+    type: "view",
+    desc: "Patching View",
+    collection: view.name
+  });
+  if (result.body.error) {
+    window.arangoHelper.arangoError(
+      "Failure",
+      `Got unexpected server response: ${result.body.errorMessage}`
+    );
+  } else {
+    oldName && window.sessionStorage.removeItem(oldName);
+    oldName && window.sessionStorage.removeItem(`${oldName}-changed`);
+    setChanged(false);
+
+    if (view.name !== oldName) {
+      let newRoute = `#view/${view.name}`;
+      window.App.navigate(newRoute, {
+        trigger: true,
+        replace: true
+      });
+    }
+  }
+  return result;
+}
+const useSyncPatchViewJob = ({ view }: { view: FormState }) => {
+  const { mutate } = useSWRConfig();
+
+  const checkState = function (
+    error: boolean,
+    jobs: { id: string; collection: string }[]
+  ) {
+    if (error) {
+      window.arangoHelper.arangoError("Jobs", "Could not read pending jobs.");
+      
+    } else {
+      const foundJob = jobs.find(locked => locked.collection === view.name);
+      if (foundJob) {
+        const path = `/view/${view.name}/properties`;
+        mutate(path);
+        window.arangoHelper.arangoNotification(
+          "Success",
+          `Updated View: ${view.name}`
+        );
+        window.arangoHelper.deleteAardvarkJob(foundJob.id);
+      }
+    }
+  };
+  useEffect(() => {
+    window.arangoHelper.syncAndReturnUnfinishedAardvarkJobs("view", checkState);
+
+    let interval = window.setInterval(() => {
+      window.arangoHelper.getAardvarkJobs(checkState);
+    }, 10000);
+    return () => window.clearInterval(interval);
+    // disabled because function creation can cause re-render, and this only needs to run on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
