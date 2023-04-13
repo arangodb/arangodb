cac53cb76c76b4837e3c38192505eab1ff65f028
Alex Petenchea <alexandru.petenchea@arangodb.com>
2023-04-13 16:10:24 +0300

    Update arangod/RocksDBEngine/Methods/RocksDBTrxMethods.cpp
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>

--- arangod/RocksDBEngine/Methods/RocksDBTrxMethods.cpp
+++ arangod/RocksDBEngine/Methods/RocksDBTrxMethods.cpp
@@ -49,8 +49,7 @@ RocksDBTrxMethods::~RocksDBTrxMethods() {
   // and call it ourselves here, because the call in the base class would only
   // execute the base class implementation, therefore leaking the
   // iteratorReadSnapshot.
-  // cppcheck-suppress *
-  cleanupTransaction();
+  RocksDBTrxMethods::cleanupTransaction();
 }
 
 Result RocksDBTrxMethods::beginTransaction() {
