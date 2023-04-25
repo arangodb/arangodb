7f6fd56cced39305912035ee49f99bdf37f274b0
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-04-25 16:16:41 +0100

    Add configuration for Graph Serde in Pregel

---
+++ arangod/Pregel/GraphStore/GraphSerdeConfigBuilderCluster.cpp
@@ -0,0 +1,204 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Pregel/GraphStore/GraphSerdeConfigBuilderCluster.h"
+
+#include "Containers/Enumerate.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "Cluster/ClusterInfo.h"
+#include "Cluster/ClusterFeature.h"
+#include "VocBase/LogicalCollection.h"
+
+namespace arangodb::pregel {
+GraphSerdeConfigBuilderCluster::GraphSerdeConfigBuilderCluster(
+    TRI_vocbase_t& vocbase, GraphByCollections const& graphByCollections)
+    : vocbase(vocbase),
+      clusterInfo(vocbase.server().getFeature<ClusterFeature>().clusterInfo()),
+      graphByCollections(graphByCollections) {}
+
+[[nodiscard]] auto
+GraphSerdeConfigBuilderCluster::edgeCollectionRestrictionsByShard() const
+    -> ShardMap {
+  std::unordered_map<std::string, std::vector<std::string>>
+      edgeCollectionRestrictionsPerShard;
+
+  for (auto const& [vertexCollection, edgeCollections] :
+       graphByCollections.edgeCollectionRestrictions) {
+    for (auto const& shardId : getShardIds(vertexCollection)) {
+      // intentionally create key in map
+      auto& restrictions = edgeCollectionRestrictionsPerShard[shardId];
+      for (auto const& edgeCollection : edgeCollections) {
+        for (auto const& edgeShardId : getShardIds(edgeCollection)) {
+          restrictions.push_back(edgeShardId);
+        }
+      }
+    }
+  }
+  return edgeCollectionRestrictionsPerShard;
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderCluster::checkVertexCollections()
+    const -> Result {
+  for (std::string const& name : graphByCollections.vertexCollections) {
+    try {
+      auto coll = clusterInfo.getCollection(vocbase.name(), name);
+
+      if (coll->system()) {
+        return Result{TRI_ERROR_BAD_PARAMETER,
+                      "Cannot use pregel on system collection"};
+      }
+
+      if (coll->deleted()) {
+        return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name};
+      }
+    } catch (...) {
+      return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name};
+    }
+  }
+  return Result{};
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderCluster::checkEdgeCollections() const
+    -> errors::ErrorT<Result, std::vector<CollectionID>> {
+  std::vector<CollectionID> result;
+
+  for (std::string const& name : graphByCollections.edgeCollections) {
+    try {
+      auto coll = clusterInfo.getCollection(vocbase.name(), name);
+
+      if (coll->system()) {
+        return errors::ErrorT<Result, std::vector<CollectionID>>::error(Result{
+            TRI_ERROR_BAD_PARAMETER, "Cannot use pregel on system collection"});
+      }
+
+      if (!coll->isSmart()) {
+        std::vector<std::string> eKeys = coll->shardKeys();
+
+        if (eKeys.size() != 1 ||
+            eKeys[0] != graphByCollections.shardKeyAttribute) {
+          return errors::ErrorT<Result, std::vector<CollectionID>>::error(
+              Result{TRI_ERROR_BAD_PARAMETER,
+                     "Edge collection needs to be sharded "
+                     "by shardKeyAttribute parameter ('" +
+                         graphByCollections.shardKeyAttribute +
+                         "'), or use SmartGraphs. The current shardKey is: " +
+                         (eKeys.empty() ? "undefined" : "'" + eKeys[0] + "'")
+
+              });
+        }
+      }
+
+      if (coll->deleted()) {
+        return errors::ErrorT<Result, std::vector<CollectionID>>::error(
+            Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name});
+      }
+
+      // smart edge collections contain multiple actual collections
+      std::vector<std::string> actual = coll->realNamesForRead();
+
+      result.insert(result.end(), actual.begin(), actual.end());
+    } catch (...) {
+      return errors::ErrorT<Result, std::vector<CollectionID>>::error(
+          Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name});
+    }
+  }
+  return errors::ErrorT<Result, std::vector<CollectionID>>::ok(result);
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderCluster::loadableVertexShards() const
+    -> LoadableVertexShards {
+  auto result = LoadableVertexShards{};
+
+  std::vector<std::vector<ShardID>> vertexShardTable;
+  std::vector<std::vector<ShardID>> edgeShardTable;
+
+  for (auto const& vertexCollection : graphByCollections.vertexCollections) {
+    vertexShardTable.emplace_back(getShardIds(vertexCollection));
+  }
+  for (auto const& edgeCollection : graphByCollections.edgeCollections) {
+    edgeShardTable.emplace_back(getShardIds(edgeCollection));
+  }
+
+  for (auto collIdx = size_t{0}; collIdx < vertexShardTable.size(); ++collIdx) {
+    for (auto shardIdx = size_t{0}; shardIdx < vertexShardTable[collIdx].size();
+         ++shardIdx) {
+      auto loadableVertexShard = LoadableVertexShard{
+          .pregelShard = PregelShard(result.loadableVertexShards.size()),
+          .vertexShard = vertexShardTable[collIdx][shardIdx],
+          .collectionName = graphByCollections.vertexCollections[collIdx],
+          .edgeShards = {}};
+
+      for (auto edgeCollIdx = size_t{0}; edgeCollIdx < edgeShardTable.size();
+           ++edgeCollIdx) {
+        if (not graphByCollections.isRestricted(
+                graphByCollections.vertexCollections.at(collIdx),
+                graphByCollections.edgeCollections.at(edgeCollIdx))) {
+          loadableVertexShard.edgeShards.emplace_back(
+              edgeShardTable[edgeCollIdx][shardIdx]);
+        }
+      }
+      result.loadableVertexShards.emplace_back(loadableVertexShard);
+    }
+  }
+  return result;
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderCluster::responsibleServerMap(
+    LoadableVertexShards const& loadableVertexShards) const
+    -> ResponsibleServerMap {
+  auto result = ResponsibleServerMap{};
+
+  for (auto const& [idx, shard] :
+       enumerate(loadableVertexShards.loadableVertexShards)) {
+    std::shared_ptr<std::vector<ServerID> const> servers =
+        clusterInfo.getResponsibleServer(shard.vertexShard);
+    ADB_PROD_ASSERT(servers->size() > 0);
+    if (servers->size() > 0) {
+      result.responsibleServerMap.push_back(servers->at(0));
+    }
+  }
+  return result;
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderCluster::getShardIds(
+    ShardID collection) const -> std::vector<ShardID> {
+  std::vector<ShardID> result;
+  try {
+    std::shared_ptr<LogicalCollection> lc =
+        clusterInfo.getCollection(vocbase.name(), collection);
+    std::shared_ptr<std::vector<ShardID>> shardIDs =
+        clusterInfo.getShardList(std::to_string(lc->id().id()));
+    result.reserve(shardIDs->size());
+    for (auto const& it : *shardIDs) {
+      result.emplace_back(it);
+    }
+  } catch (...) {
+    // TODO: Lol.
+    result.clear();
+  }
+
+  return result;
+}
+
+}  // namespace arangodb::pregel
---
+++ arangod/Pregel/GraphStore/GraphSerdeConfigBuilderSingleServer.cpp
@@ -0,0 +1,102 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Markus Pfeiffer
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Pregel/GraphStore/GraphSerdeConfigBuilderSingleServer.h"
+
+#include "VocBase/LogicalCollection.h"
+
+namespace arangodb::pregel {
+
+GraphSerdeConfigBuilderSingleServer::GraphLoadingSpecCalculatorSingleServer(
+    TRI_vocbase_t& vocbase, GraphByCollections const& graphByCollections)
+    : vocbase(vocbase), graphByCollections(graphByCollections) {}
+
+[[nodiscard]] auto
+GraphSerdeConfigBuilderSingleServer::edgeCollectionRestrictionsByShard() const
+    -> ShardMap {
+  return graphByCollections.edgeCollectionRestrictions;
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderSingleServer::checkVertexCollections()
+    const -> Result {
+  for (std::string const& name : graphByCollections.vertexCollections) {
+    auto coll = vocbase.lookupCollection(name);
+
+    if (coll == nullptr || coll->deleted()) {
+      return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name};
+    }
+  }
+  return Result{};
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderSingleServer::checkEdgeCollections()
+    const -> errors::ErrorT<Result, std::vector<CollectionID>> {
+  std::vector<CollectionID> result;
+
+  for (std::string const& name : graphByCollections.edgeCollections) {
+    auto coll = vocbase.lookupCollection(name);
+
+    if (coll == nullptr || coll->deleted()) {
+      return errors::ErrorT<Result, std::vector<CollectionID>>::error(
+          Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND, name});
+    }
+    std::vector<std::string> actual = coll->realNamesForRead();
+    result.insert(result.end(), actual.begin(), actual.end());
+  }
+
+  return errors::ErrorT<Result, std::vector<CollectionID>>::ok(result);
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderSingleServer::loadableVertexShards()
+    const -> LoadableVertexShards {
+  auto result = LoadableVertexShards{};
+
+  for (auto const [idx, vertexCollection] :
+       enumerate(graphByCollections.vertexCollections)) {
+    auto loadableVertexShard =
+        LoadableVertexShard{.pregelShard = PregelShard(idx),
+                            .vertexShard = vertexCollection,
+                            .collectionName = vertexCollection,
+                            .edgeShards = {}};
+    for (auto const& edgeCollection : graphByCollections.edgeCollections) {
+      if (not graphByCollections.isRestricted(vertexCollection,
+                                              edgeCollection)) {
+        loadableVertexShard.edgeShards.emplace_back(edgeCollection);
+      }
+    }
+    result.loadableVertexShards.emplace_back(loadableVertexShard);
+  }
+  return result;
+}
+
+[[nodiscard]] auto GraphSerdeConfigBuilderSingleServer::responsibleServerMap(
+    LoadableVertexShards const& loadableVertexShards) const
+    -> ResponsibleServerMap {
+  auto result = ResponsibleServerMap{};
+
+  result.responsibleServerMap.resize(
+      loadableVertexShards.loadableVertexShards.size(), "");
+  return result;
+}
+
+}  // namespace arangodb::pregel
