4358e130502c5a35aa21563a0d67bf93e8ced8ae
Wilfried Goesgens <willi@arangodb.com>
2023-04-28 16:19:09 +0200

    as discussed with @MBKKt use just one try/catch and set 10s timout to not risk test stability

--- tests/js/server/recovery/view-arangosearch-link-populate-no-callbacks.js
+++ tests/js/server/recovery/view-arangosearch-link-populate-no-callbacks.js
@@ -50,21 +50,12 @@ function runSetup () {
   if (global.hasOwnProperty('arango')) {
     // we intend to crash, so we should get to know quickly:
     print('setting short timeout');
-    arango.timeout(1);
+    arango.timeout(10);
   }
   try {
     for (let i = 0; i < 100000; i++) {
       c.save({ a: "foo_" + i, b: "bar_" + i, c: i });
     }
-  } catch (ex) {
-    if ((ex.errorNum !== internal.errors.ERROR_SIMPLE_CLIENT_UNKNOWN_ERROR.code) &&
-        (ex.errorNum !== internal.errors.ERROR_CLUSTER_CONNECTION_LOST.code)) {
-      print(ex);
-      throw ex;
-    }
-  }
-
-  try {
     c.save({ name: 'crashme' }, { waitForSync: true });
   } catch (ex) {
     if ((ex.errorNum !== internal.errors.ERROR_SIMPLE_CLIENT_UNKNOWN_ERROR.code) &&
