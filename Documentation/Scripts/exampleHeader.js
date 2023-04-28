898cb21f07fa50278ab73858d3b4f25dec8070fa
Wilfried Goesgens <willi@arangodb.com>
2023-04-28 15:17:38 +0200

    print the function source so the user can better allocate the assert

--- Documentation/Scripts/exampleHeader.js
+++ Documentation/Scripts/exampleHeader.js
@@ -160,9 +160,9 @@ var logPlainResponse = internal.appendPlainResponse(plainAppender, plainAppender
 var logErrorResponse = function (response) {
     allErrors += "Server reply was: " + JSON.stringify(response) + "\n";
 };
-var globalAssert = function(condition, testname, sourceFile) {
+var globalAssert = function(condition, testname, sourceFile, fnsource) {
   if (! condition) {
-    internal.output(hashes + '\nASSERTION FAILED: ' + testname + ' in file ' + sourceFile + '\n' + hashes + '\n');
+    internal.output(`${hashes}\nASSERTION FAILED: \n${testname} in file ${sourceFile}\nIn this Function:\n${fnsource}\n${hashes}\n`);
     throw new Error('assertion ' + testname + ' in file ' + sourceFile + ' failed');
   }
 };
