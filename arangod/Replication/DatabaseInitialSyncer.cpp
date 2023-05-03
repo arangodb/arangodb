2701f68b17f078601efba100f7a815e592c5f1b0
jsteemann <jsteemann@users.noreply.github.com>
2023-05-03 18:43:31 +0200

    don't clear vectors twice

--- arangod/Replication/DatabaseInitialSyncer.cpp
+++ arangod/Replication/DatabaseInitialSyncer.cpp
@@ -1941,7 +1941,7 @@ Result DatabaseInitialSyncer::fetchCollectionSyncByRevisions(
       if (toFetch.size() >= 250000) {
         res = ::fetchRevisions(nf, *trx, _config, _state, *coll, leaderColl,
                                encodeAsHLC, toFetch, stats);
-        toFetch.clear();
+        TRI_ASSERT(res.fail() || toFetch.empty());
       }
       return res;
     };
@@ -1959,7 +1959,7 @@ Result DatabaseInitialSyncer::fetchCollectionSyncByRevisions(
       // much in memory
       if (toRemove.size() >= options.intermediateCommitCount) {
         res = ::removeRevisions(*trx, *coll, toRemove, stats);
-        toRemove.clear();
+        TRI_ASSERT(res.fail() || toRemove.empty());
       }
       return res;
     };
@@ -2198,17 +2198,17 @@ Result DatabaseInitialSyncer::fetchCollectionSyncByRevisions(
       }
 
       res = ::removeRevisions(*trx, *coll, toRemove, stats);
+      TRI_ASSERT(res.fail() || toRemove.empty());
       if (res.fail()) {
         return res;
       }
-      TRI_ASSERT(toRemove.empty());
 
       res = ::fetchRevisions(nf, *trx, _config, _state, *coll, leaderColl,
                              encodeAsHLC, toFetch, stats);
+      TRI_ASSERT(res.fail() || toFetch.empty());
       if (res.fail()) {
         return res;
       }
-      TRI_ASSERT(toFetch.empty());
     }
 
     // adjust counts
