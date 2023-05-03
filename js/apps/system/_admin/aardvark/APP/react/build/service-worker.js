2135d23a4330606af5cb21901d8d13325e700aa7
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-03 20:38:25 +0530

    chore: Rebuild UI

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.1013cab1f1e50b76ce5b43343d23f599.js"
+  "precache-manifest.f2dc8b9555e6aa26d421441b8e3a8333.js"
 );
 
 workbox.clientsClaim();
