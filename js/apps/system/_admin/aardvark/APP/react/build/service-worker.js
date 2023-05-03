4ea23b9be52a332a55bcc62eb3d07cfa1eca91ed
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-03 17:25:06 +0530

    Feature-3.10/FE-238: auto-login backport (#18766)
    * chore: set jwtUser from /whoAmI API call
    
    * feat: don't show logout when SSO is enabled
    
    * fix: ssoEnabled check
    
    * chore: add a message about logout when SSO is enabled
    
    * chore: update logout message
    
    * chore: rename to autoLogin
    
    * chore: Rebuild UI
    
    * chore: update changelog
    
    * Update CHANGELOG

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4d160f8a6a1ec399b6ea89d47832b006.js"
+  "precache-manifest.1013cab1f1e50b76ce5b43343d23f599.js"
 );
 
 workbox.clientsClaim();
