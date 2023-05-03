14455292d9aca1d703afa38b9db47ee995f3dd91
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-03 23:42:41 +0530

    fix: remove unused import

--- js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/forms/GeopointConfig.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/forms/GeopointConfig.tsx
@@ -1,8 +1,7 @@
 import { Grid } from "@chakra-ui/react";
 import React from "react";
-import { GeoTypeInput } from "./inputs/GeoTypeInput";
-import { GeoOptionsInputs } from "./inputs/GeoOptionsInputs";
 import { CreatableMultiSelectControl } from "../../../components/form/CreatableMultiSelectControl";
+import { GeoOptionsInputs } from "./inputs/GeoOptionsInputs";
 
 export const GeopointConfig = () => {
   return (
