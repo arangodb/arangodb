2abd6958988ce9a42319ea30e443e7a6c8bd2f36
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-05 15:15:46 +0530

    chore: add modalContentProps to Modal

--- js/apps/system/_admin/aardvark/APP/react/src/components/modal/Modal.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/components/modal/Modal.tsx
@@ -1,17 +1,22 @@
 import {
   Modal as ChakraModal,
   ModalContent,
+  ModalContentProps,
   ModalOverlay,
   ModalProps
 } from "@chakra-ui/react";
 import React from "react";
 
-export const Modal = (modalProps: ModalProps) => {
-  const { children, ...rest } = modalProps;
+export const Modal = (
+  modalProps: ModalProps & { modalContentProps?: ModalContentProps }
+) => {
+  const { children, modalContentProps, ...rest } = modalProps;
   return (
     <ChakraModal {...rest}>
       <ModalOverlay />
-      <ModalContent marginX="4">{children}</ModalContent>
+      <ModalContent marginX="4" {...modalContentProps}>
+        {children}
+      </ModalContent>
     </ChakraModal>
   );
 };
