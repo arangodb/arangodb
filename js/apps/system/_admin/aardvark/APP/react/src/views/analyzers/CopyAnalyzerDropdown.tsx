ff100bd903f73881d10ead6908ba93f8f8b4a807
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-05 17:10:44 +0530

    fix: don't copy name

--- js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/CopyAnalyzerDropdown.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/CopyAnalyzerDropdown.tsx
@@ -4,6 +4,7 @@ import { AnalyzerDescription } from "arangojs/analyzer";
 import { useFormikContext } from "formik";
 import React from "react";
 import SingleSelect from "../../components/select/SingleSelect";
+import { AnalyzerState } from "./Analyzer.types";
 import { useAnalyzersContext } from "./AnalyzersContext";
 
 export const CopyAnalyzerDropdown = () => {
@@ -15,7 +16,7 @@ export const CopyAnalyzerDropdown = () => {
   const [selectedAnalyzer, setSelectedAnalyzer] = React.useState<
     AnalyzerDescription | undefined
   >(analyzers?.[0]);
-  const { setValues } = useFormikContext();
+  const { setValues, values } = useFormikContext<AnalyzerState>();
   return (
     <Stack direction="row" alignItems="center" flexWrap="wrap">
       <Stack direction="row" alignItems="center">
@@ -35,7 +36,13 @@ export const CopyAnalyzerDropdown = () => {
           size="xs"
           leftIcon={<ArrowBackIcon />}
           onClick={() => {
-            setValues(selectedAnalyzer);
+            if (!selectedAnalyzer) {
+              return;
+            }
+            setValues({
+              ...selectedAnalyzer,
+              name: values.name
+            } as AnalyzerState);
           }}
         >
           Copy from
