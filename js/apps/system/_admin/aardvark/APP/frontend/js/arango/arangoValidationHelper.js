23d7f5a672c6631293056f05e5ecdd77ad4d3f49
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-10 13:13:06 +0530

    chore: move database validations

--- js/apps/system/_admin/aardvark/APP/frontend/js/arango/arangoValidationHelper.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/arango/arangoValidationHelper.js
@@ -13,6 +13,56 @@
       };
       return keySpecialCharactersValidation;
     },
+    getDatabaseNameValidations: () => {
+      var extendedNames = window.frontendConfig.extendedNames;
+      var traditionalNameValidation = [
+        {
+          rule: Joi.string().regex(/^[a-zA-Z]/),
+          msg: "Database name must always start with a letter.",
+        },
+        {
+          rule: Joi.string().regex(/^[a-zA-Z0-9\-_]*$/),
+          msg: 'Only symbols, "_" and "-" are allowed.',
+        },
+        {
+          rule: Joi.string().max(64, "utf8"),
+          msg: "Database name max length is 64.",
+        },
+      ];
+
+      var extendedValidation = [
+        {
+          rule: Joi.string().regex(/^(?![0-9._])/),
+          msg: "Database name cannot start with a number, a dot (.), or an underscore (_).",
+        },
+        {
+          rule: Joi.string().regex(
+            window.arangoValidationHelper.getControlCharactersRegex()
+          ),
+          msg: "Database name cannot contain control characters (0-31).",
+        },
+        {
+          rule: Joi.string().regex(/^(?!.*[/:])/),
+          msg: "Database name cannot contain a forward slash (/) or a colon (:)",
+        },
+        {
+          rule: Joi.string().regex(/^\S(.*\S)?$/),
+          msg: "Database name cannot contain leading/trailing spaces.",
+        },
+        {
+          rule: Joi.string().normalize().max(128, "utf8"),
+          msg: "Database name max length is 128 bytes.",
+        },
+      ];
+      var databaseNameValidations = extendedNames
+        ? extendedValidation
+        : traditionalNameValidation;
+      databaseNameValidations = databaseNameValidations.concat({
+        rule: Joi.string().required(),
+        msg: "No database name given.",
+      });
+      return databaseNameValidations;
+    },
     getDocumentNameValidations: () => {
       var keySpecialCharactersValidation =
         window.arangoValidationHelper.getDocumentKeySpecialCharactersValidation();
