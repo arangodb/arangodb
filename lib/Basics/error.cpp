73253b01e8773e5cfe13b8cf7966c9359fdee5d1
Alex Petenchea <alex.petenchea@gmail.com>
2023-05-11 16:08:55 +0300

    Proper error reporting for Windows

--- lib/Basics/error.cpp
+++ lib/Basics/error.cpp
@@ -32,10 +32,19 @@
 #include "Basics/exitcodes.h"
 #include "Basics/voc-errors.h"
 
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 /// @brief error number and system error
 struct ErrorContainer {
   ErrorCode _number = TRI_ERROR_NO_ERROR;
+
+#if defined(_WIN32)
+  DWORD _sys = 0;
+#else
   int _sys = 0;
+#endif
 };
 
 /// @brief holds the last error that occurred in the current thread
@@ -45,14 +54,32 @@ thread_local ErrorContainer LastError;
 ErrorCode TRI_errno() { return LastError._number; }
 
 /// @brief returns the last error as string
-std::string_view TRI_last_error() {
+std::string TRI_last_error() {
   ErrorCode err = LastError._number;
 
   if (err == TRI_ERROR_SYS_ERROR) {
+#if defined(_WIN32)
+    if (LastError._sys == 0) {
+      return {};
+    }
+    LPSTR messageBuffer = nullptr;
+    std::size_t size = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr, LastError._sys, 0, (LPSTR)&messageBuffer, 0, nullptr);
+    if (size == 0) {
+      return "failed to get error message, the error code is " +
+             std::to_string(LastError._sys);
+    }
+    std::string message(messageBuffer, size);
+    LocalFree(messageBuffer);
+    return message;
+#else
     return strerror(LastError._sys);
+#endif
   }
 
-  return TRI_errno_string(err);
+  return std::string{TRI_errno_string(err)};
 }
 
 /// @brief sets the last error
@@ -60,7 +87,11 @@ ErrorCode TRI_set_errno(ErrorCode error) {
   LastError._number = error;
 
   if (error == TRI_ERROR_SYS_ERROR) {
+#if defined(_WIN32)
+    LastError._sys = GetLastError();
+#else
     LastError._sys = errno;
+#endif
   } else {
     LastError._sys = 0;
   }
