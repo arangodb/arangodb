37aac7ca6d45455b59bd63c40ca361a051625948
mpoeter <manuel@arangodb.com>
2023-05-15 15:19:48 +0200

    Add explanation to regression test.

--- tests/js/client/shell/shell-aql-regression-cluster.js
+++ tests/js/client/shell/shell-aql-regression-cluster.js
@@ -120,9 +120,39 @@ function ShellAqlParallelGatherSuite() {
 
     testLongRunningUpstream: function () {
       // Regression test for https://arangodb.atlassian.net/browse/BTS-1400
-      // Previously an upstream operation that took longer than 30s would cause
-      // the request to abort with an error "query ID xxx not found"
+      // Previously we would only wait for up to 30s when trying to open an engine.
+      // If this failed, we would return with this "query ID xxx not found" error.
+      // However, there is no good reason to give up after 30s. In fact, there are
+      // enough cases where the upstream operation can take longer than 30s, so we
+      // have to wait for as long as it takes.
+      assertEqual(1, db[colName].count);
       db._query("FOR d in @@col  LET x = SLEEP(35) INSERT {} INTO @@col", { "@col": colName });
+      assertEqual(2, db[colName].count);
+
+      // The explain output for this query looks as follows:
+      // Execution plan:
+      //  Id   NodeType          Site  Est.   Comment
+      //   1   SingletonNode     DBS      1   * ROOT
+      //   4   CalculationNode   DBS      1     - LET #3 = {  }   /* json expression */   /* const assignment */
+      //  14   IndexNode         DBS      1     - FOR d IN UnitTestsCollection   /* primary index scan, scan only, 2 shard(s) */    
+      //   3   CalculationNode   DBS      1       - LET x = SLEEP(35)   /* simple expression */
+      //  12   RemoteNode        COOR     1       - REMOTE
+      //  13   GatherNode        COOR     1       - GATHER   /* parallel, unsorted */
+      //  15   CalculationNode   COOR     1       - LET #5 = MAKE_DISTRIBUTE_INPUT_WITH_KEY_CREATION(#3, null, { "allowSpecifiedKeys" : false, "ignoreErrors" : false, "collection" : "UnitTestsCollection" })   /* simple expression */
+      //   6   DistributeNode    COOR     1       - DISTRIBUTE #5
+      //   7   RemoteNode        DBS      1       - REMOTE
+      //   5   InsertNode        DBS      0       - INSERT #5 IN UnitTestsCollection 
+      //   8   RemoteNode        COOR     0       - REMOTE
+      //   9   GatherNode        COOR     0       - GATHER   /* parallel, unsorted */
+
+      // In the first parallel gather we fan out to the db servers, which then go
+      // back to the coordinator. These requests to the coordinator may come in
+      // concurrently. However, only the first one will be able to open the engine
+      // and then perform the next request to the db server. The engine will not
+      // be released while we wait for this response. Since the db server sleeps
+      // for 35s this can take a while during which the other requests have to
+      // wait and must not abort the query.
+      // This test basically just checks that the query does not fail.
     },
   };
 }
