87e4787287f2d66d8c482e6305649ced235955aa
Markus Pfeiffer <markus.pfeiffer@arangodb.com>
2023-05-16 14:21:24 +0100

    PyLint

--- utils/checkLogIds.py
+++ utils/checkLogIds.py
@@ -3,23 +3,27 @@
 import sys
 import os
 
-from enum import Enum
-
 import re
-g_log_topic_pattern = re.compile(r'(?P<macro>LOG_(TOPIC(_IF)?|TRX|CTX(_IF)?|QUERY|PREGEL))\((?P<param>[^),]*),(?P<rest>.*)')
+g_log_topic_pattern = re.compile( \
+    r'(?P<macro>LOG_(TOPIC(_IF)?|TRX|CTX(_IF)?|QUERY|PREGEL))\((?P<param>[^),]*),(?P<rest>.*)')
 
 # a LogId is exactly 5 hexadecimal digits
 g_log_id_pattern = re.compile(r'"[0-9a-f]{5}"')
 
-def check_file(fullpath, project_path, id_database, invalid_logid_database):
-    with open(fullpath) as source_file_handle:
+def check_file(fullpath, id_database, invalid_logid_database):
+    """
+    Collects LogIDs and invalid LogID attempts from file given in fullpath
+
+        :param fullpath: full path to the file to open
+        :param id_database: dict containing all occurrences of LogIDs
+        :param invalid_logid_database: locations in which invalid LogID attempts have been found
+    """
+    with open(fullpath, encoding="UTF-8") as source_file_handle:
         for cnt, line in enumerate(source_file_handle):
             match = g_log_topic_pattern.search(line)
 
             if match:
-                location_string = "{}:{}".format(fullpath, cnt + 1)
-                macro = match.group('macro')
-                rest = match.group('rest')
+                location_string = f"{fullpath}:{cnt+1}"
                 param = match.group('param').strip()
 
                 if g_log_id_pattern.match(param):
@@ -32,7 +36,8 @@ def check_file(fullpath, project_path, id_database, invalid_logid_database):
                         id_database[uid]=[location_string]
 
                 elif param == "logId":
-                    # TODO: warn?
+                    # This is made to pass over macro definitions that
+                    # look like invocations to our primitive regex
                     pass
                 else:
                     invalid_logid_database[location_string] = param
@@ -49,8 +54,8 @@ def check_log_ids_main(root_directory, directories_to_include, directories_or_fi
         :rtype: dict
     """
 
-    id_database = dict()
-    invalid_logid_database = dict()
+    id_database = {}
+    invalid_logid_database = {}
 
     include = [ root_directory + os.path.sep + d for d in directories_to_include ]
     exclude = [ root_directory + os.path.sep + d for d in directories_or_files_to_exclude ]
@@ -65,22 +70,15 @@ def check_log_ids_main(root_directory, directories_to_include, directories_or_fi
             if fullpath in exclude:
                 continue
             if filename.endswith((".cpp", ".h", ".tpp")):
-                check_file(fullpath, root_directory, id_database, invalid_logid_database)
+                check_file(fullpath, id_database, invalid_logid_database)
 
     return [id_database, invalid_logid_database]
 
 if __name__ == "__main__":
-    """
-    Returns non zero status if there is a duplicate id!
+    my_root = os.path.dirname(__file__)
+    my_root = os.path.abspath(os.path.join(my_root, os.pardir))
 
-    Note:
-    `check_only` must be set to true for normal operation mode.
-    """
-
-    root = os.path.dirname(__file__)
-    root = os.path.abspath(root + os.path.sep + os.pardir)
-
-    id_database, invalid_logid_database = check_log_ids_main(root,
+    my_id_database, my_invalid_logid_database = check_log_ids_main(my_root,
                        ["arangod", "client-tools", "enterprise", "lib"],
                        [".", "lib/Logger/LogMacros.h",
                         "arangod/StorageEngine/TransactionState.h",
@@ -88,17 +86,17 @@ if __name__ == "__main__":
 
     return_value = 0
 
-    for k, v in id_database.items():
+    for k, v in my_id_database.items():
         if len(v) > 1:
             return_value = 1
-            print("LogId {} is used more than once:".format(k))
+            print(f"LogId {k} is used more than once:")
             for loc in v:
-                print("  {}".format(loc))
+                print(f"  {loc}")
 
-    if invalid_logid_database:
+    if my_invalid_logid_database:
         return_value = 1
         print("Found the following invalid LogIds: ")
-        for loc, logId in invalid_logid_database.items():
-            print(" {} at {}".format(logId, loc))
+        for loc, logId in my_invalid_logid_database.items():
+            print(f" {logId} at {loc}")
 
     sys.exit(return_value)
