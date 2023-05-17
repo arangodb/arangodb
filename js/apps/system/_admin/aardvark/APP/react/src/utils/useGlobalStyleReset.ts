cffff1244bf31f55a658bdde5090d14cf6d2c5ac
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-17 14:49:08 +0530

    TODO

--- js/apps/system/_admin/aardvark/APP/react/src/utils/useGlobalStyleReset.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/utils/useGlobalStyleReset.ts
@@ -10,8 +10,10 @@ export const useGlobalStyleReset = () => {
   useEffect(() => {
     const contentWrapper = document.querySelector(".contentWrapper");
     const contentDiv = document.querySelector("#content");
-    contentWrapper && contentWrapper.setAttribute("style", "padding: 0;")
-    contentDiv && contentDiv.setAttribute("style", "margin: 0; padding: 0; display: none;")
+    contentWrapper && contentWrapper.setAttribute("style", "padding: 0;");
+    // todo - fix this display none
+    contentDiv &&
+      contentDiv.setAttribute("style", "margin: 0; padding: 0; display: none;");
     return () => {
       contentDiv && contentDiv.removeAttribute("style");
       contentWrapper && contentWrapper.removeAttribute("style");
