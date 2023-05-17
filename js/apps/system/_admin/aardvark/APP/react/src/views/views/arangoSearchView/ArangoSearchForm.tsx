d2f0f6c6d2581d692f6717ac8a882f612704d10b
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-17 14:51:06 +0530

    chore: init arangosearch refactor

---
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/arangoSearchView/ArangoSearchForm.tsx
@@ -0,0 +1,72 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box
+} from "@chakra-ui/react";
+import { useFormikContext } from "formik";
+import React from "react";
+import { FormField } from "../../../components/form/FormField";
+
+import { useArangoSearchFieldsData } from "./useArangoSearchFieldsData";
+
+export const ArangoSearchForm = () => {
+  const { values } = useFormikContext();
+  console.log("values", values);
+  return (
+    <Accordion
+      borderColor={"gray.200"}
+      defaultIndex={[0, 1]}
+      borderRightWidth="1px solid"
+      borderLeftWidth="1px solid"
+      marginTop="4"
+      allowMultiple
+      allowToggle
+      padding="4"
+    >
+      <GeneralAccordionItem />
+    </Accordion>
+  );
+};
+
+const FieldsGrid = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box
+      display={"grid"}
+      gridTemplateColumns={"200px 1fr 40px"}
+      rowGap="5"
+      columnGap="3"
+      maxWidth="800px"
+      paddingRight="8"
+      paddingLeft="4"
+      alignItems="center"
+      marginTop="4"
+    >
+      {children}
+    </Box>
+  );
+};
+
+const GeneralAccordionItem = () => {
+  const { fields } = useArangoSearchFieldsData();
+  const generalFields = fields.filter(field => field.group === "general");
+  return (
+    <AccordionItem>
+      <AccordionButton>
+        <Box flex="1" textAlign="left">
+          General
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+      <AccordionPanel pb={4}>
+        <FieldsGrid>
+          {generalFields.map(field => {
+            return <FormField field={field} key={field.name} />;
+          })}
+        </FieldsGrid>
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
