c6d3e3a0701ba0cc2d03a66f9ef5d5bd4f7e929f
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-05-19 19:29:43 +0200

    Rename variable and disable navigation buttons

--- js/apps/system/_admin/aardvark/APP/aardvark.js
+++ js/apps/system/_admin/aardvark/APP/aardvark.js
@@ -1308,11 +1308,10 @@ authRouter.get('/graphs-v2/:name', function (req, res) {
     const generateNodeObject = (node) => {
       nodeNames[node._id] = true;
       var label = "";
-      var fullLabel = "";
+      var tooltipText = "";
 
       if (config.nodeLabel) {
           var nodeLabelArr = config.nodeLabel.split(" ");
-          print(nodeLabelArr.length);
           if(nodeLabelArr.length > 1) {
             _.each(nodeLabelArr, function (attr) {
 
@@ -1320,24 +1319,26 @@ authRouter.get('/graphs-v2/:name', function (req, res) {
               if(attrVal !== undefined) {
                 if (typeof attrVal === 'string') {
                   label += "<b>" + attr + ":</b> " + truncate(getAttributeByKey(node, attr), 16) + "\n";
-                  fullLabel += attr + ": " + getAttributeByKey(node, attr) + "\n";
+                  tooltipText += attr + ": " + getAttributeByKey(node, attr) + "\n";
                 } else {
                   // in case we do not have a string here, we need to stringify it
                   // otherwise we might end up sending not displayable values.
                   label += "<b>" + attr + ":</b> " + JSON.stringify(getAttributeByKey(node, attr)) + "\n";
-                  fullLabel += attr + ": " + JSON.stringify(getAttributeByKey(node, attr)) + "\n";
+                  tooltipText += attr + ": " + JSON.stringify(getAttributeByKey(node, attr)) + "\n";
                 }
               } else {
                 label += "<b>" + attr + ":</b> " + notFoundString + "\n";
-                fullLabel += attr + ": " + notFoundString + "\n";
+                tooltipText += attr + ": " + notFoundString + "\n";
               }
               
             });
           } else {
             label = getAttributeByKey(node, config.nodeLabel);
+            tooltipText = getAttributeByKey(node, config.nodeLabel);
           }
       } else {
         label = node._key || node._id;
+        tooltipText = node._key || node._id;
       }
 
       if (config.nodeLabelByCollection === 'true') {
@@ -1382,7 +1383,7 @@ authRouter.get('/graphs-v2/:name', function (req, res) {
           strokeColor: '#ffffff',
           vadjust: -7
         },
-        title: fullLabel,
+        title: tooltipText,
         sizeAttributeFound
       };
 
@@ -1656,7 +1657,7 @@ authRouter.get('/graphs-v2/:name', function (req, res) {
         bindToWindow: false
       },
       multiselect: false,
-      navigationButtons: true,
+      navigationButtons: false,
       selectable: true,
       selectConnectedEdges: false,
       tooltipDelay: 300,
