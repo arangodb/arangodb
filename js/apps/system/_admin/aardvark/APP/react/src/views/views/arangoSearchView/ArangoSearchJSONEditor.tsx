132933308b20805e302dd45ca0aa18edc7cf63ed
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-29 17:53:47 +0530

    fix: add schema reset

--- js/apps/system/_admin/aardvark/APP/react/src/views/views/arangoSearchView/ArangoSearchJSONEditor.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/arangoSearchView/ArangoSearchJSONEditor.tsx
@@ -1,11 +1,12 @@
 import { Box } from "@chakra-ui/react";
-import Ajv from "ajv";
+import Ajv, { JSONSchemaType } from "ajv";
 import { useFormikContext } from "formik";
-import React from "react";
+import React, { useEffect } from "react";
 import { ControlledJSONEditor } from "../../../components/jsonEditor/ControlledJSONEditor";
 import { JSONErrors } from "../../../components/jsonEditor/JSONErrors";
 import { useEditViewContext } from "../editView/EditViewContext";
 import { useArangoSearchJSONSchema } from "../SearchJSONSchema";
+import { ArangoSearchViewPropertiesType } from "../searchView.types";
 
 const ajv = new Ajv({
   allErrors: true,
@@ -14,10 +15,23 @@ const ajv = new Ajv({
   $data: true
 });
 
+/**
+ * used to remove the schema on unmount, to avoid issues in next usage
+ */
+const useResetSchema = (
+  schema: JSONSchemaType<ArangoSearchViewPropertiesType>
+) => {
+  useEffect(() => {
+    return () => {
+      ajv.removeSchema(schema.$id);
+    };
+  }, [schema]);
+};
 export const ArangoSearchJSONEditor = () => {
   const { values, setValues } = useFormikContext();
   const { initialView, setErrors, errors } = useEditViewContext();
   const { schema } = useArangoSearchJSONSchema({ view: initialView });
+  useResetSchema(schema);
   return (
     <Box height="100%" backgroundColor="white" position="relative" minWidth={0}>
       <ControlledJSONEditor
