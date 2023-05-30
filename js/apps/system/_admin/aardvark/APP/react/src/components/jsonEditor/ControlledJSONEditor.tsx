681cfdc5f0584a8b815c2395fa0e5c40fbe79ea8
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-30 12:40:24 +0530

    chore: move resetSchema

--- js/apps/system/_admin/aardvark/APP/react/src/components/jsonEditor/ControlledJSONEditor.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/components/jsonEditor/ControlledJSONEditor.tsx
@@ -1,12 +1,24 @@
 import { JsonEditor, JsonEditorProps } from "jsoneditor-react";
 import React, { useEffect, useRef } from "react";
 
+/**
+ * used to remove the schema on unmount, to avoid issues in next usage
+ */
+const useResetSchema = ({ schema, ajv }: { schema: any; ajv?: any }) => {
+  useEffect(() => {
+    return () => {
+      ajv?.removeSchema(schema.$id);
+    };
+  }, [schema, ajv]);
+};
+
 export const ControlledJSONEditor = ({
   value,
   onChange,
   schema,
   ...rest
 }: JsonEditorProps) => {
+  useResetSchema({ schema, ajv: rest.ajv });
   const jsonEditorRef = useRef(null);
   useEffect(() => {
     const editor = (jsonEditorRef.current as any)?.jsonEditor;
