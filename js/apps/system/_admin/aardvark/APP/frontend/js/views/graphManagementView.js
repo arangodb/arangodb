f890305762789fd956b57bfbfe0a235075a84e78
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-05-30 17:17:03 +0530

    fix: number validation for replicationFactor and writeConcern

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/graphManagementView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/graphManagementView.js
@@ -1301,7 +1301,7 @@
             false,
             [
               {
-                rule: Joi.string().allow('').optional().regex(/^[1-9]*$/),
+                rule: Joi.string().allow('').optional().regex(/^[1-9][0-9]*$/),
                 msg: 'Must be a number.'
               }
             ]
@@ -1318,7 +1318,7 @@
             false,
             [
               {
-                rule: Joi.string().allow('').optional().regex(/^[1-9]*$/),
+                rule: Joi.string().allow('').optional().regex(/^[1-9][0-9]*$/),
                 msg: rowDescription.writeConcern.description
               }
             ]
@@ -1380,7 +1380,7 @@
             false,
             [
               {
-                rule: Joi.string().allow('').optional().regex(/^[1-9]*$/),
+                rule: Joi.string().allow('').optional().regex(/^[1-9][0-9]*$/),
                 msg: 'Must be a number.'
               }
             ]
@@ -1396,7 +1396,7 @@
             false,
             [
               {
-                rule: Joi.string().allow('').optional().regex(/^[1-9]*$/),
+                rule: Joi.string().allow('').optional().regex(/^[1-9][0-9]*$/),
                 msg: 'Must be a number. Must be at least 1 and has to be smaller or equal compared to the replicationFactor.'
               }
             ]
