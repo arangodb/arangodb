14ac7a9d8cfe63a551fb09521f91482a7418a8e6
jsteemann <jsteemann@users.noreply.github.com>
2023-06-02 01:48:35 +0200

    dump collection data

--- lib/Basics/BoundedChannel.h
+++ lib/Basics/BoundedChannel.h
@@ -1,10 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Lars Maier
+////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
+
+#include <condition_variable>
 #include <cstdint>
 #include <memory>
-#include <vector>
 #include <mutex>
-#include <condition_variable>
+#include <vector>
 
 namespace arangodb {
 
@@ -25,7 +48,7 @@ struct BoundedChannel {
     std::unique_lock guard(_mutex);
     _numProducer -= 1;
     if (_numProducer == 0) {
-      // wake up all waiting worker
+      // wake up all waiting workers
       _stopped = true;
       _writeCv.notify_all();
     }
