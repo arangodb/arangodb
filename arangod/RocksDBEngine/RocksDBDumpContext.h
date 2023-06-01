ee2ba4bd42135fa3fb988f1306812450d67e3db7
jsteemann <jsteemann@users.noreply.github.com>
2023-06-01 21:06:17 +0200

    add guard objects for database and collection

--- arangod/RocksDBEngine/RocksDBDumpContext.h
+++ arangod/RocksDBEngine/RocksDBDumpContext.h
@@ -27,6 +27,7 @@
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace rocksdb {
@@ -34,6 +35,9 @@ class ManagedSnapshot;
 }  // namespace rocksdb
 
 namespace arangodb {
+class CollectionGuard;
+class DatabaseFeature;
+class DatabaseGuard;
 class RocksDBEngine;
 
 class RocksDBDumpContext {
@@ -41,42 +45,78 @@ class RocksDBDumpContext {
   RocksDBDumpContext(RocksDBDumpContext const&) = delete;
   RocksDBDumpContext& operator=(RocksDBDumpContext const&) = delete;
 
-  RocksDBDumpContext(RocksDBEngine& engine, std::string id, uint64_t batchSize,
-                     uint64_t prefetchCount, uint64_t parallelism,
-                     std::vector<std::string> shards, double ttl,
+  RocksDBDumpContext(RocksDBEngine& engine, DatabaseFeature& databaseFeature,
+                     std::string id, uint64_t batchSize, uint64_t prefetchCount,
+                     uint64_t parallelism,
+                     std::vector<std::string> const& shards, double ttl,
                      std::string const& user, std::string const& database);
 
   ~RocksDBDumpContext();
 
+  // return id of the context. will not change during the lifetime of the
+  // context.
   std::string const& id() const noexcept;
 
+  // return database name used by the context. will not change during the
+  // lifetime of the context.
   std::string const& database() const noexcept;
+  // return name of the user that created the context. can be used for access
+  // permissions checking. will not change during the lifetime of the context.
   std::string const& user() const noexcept;
 
+  // return TTL value of this context. will not change during the lifetime of
+  // the context.
   double ttl() const noexcept;
 
+  // return expire date, as a timestamp in seconds since 1970/1/1
   double expires() const noexcept;
 
+  // check whether the context is for database <database> and was created by
+  // <user>.
   bool canAccess(std::string const& database,
                  std::string const& user) const noexcept;
 
+  // extend the contexts lifetime, by adding TTL to the current time and storing
+  // it in _expires.
   void extendLifetime() noexcept;
 
  private:
   RocksDBEngine& _engine;
 
-  // these parameters will not change during the lifetime of the object
+  // these parameters will not change during the lifetime of the object.
+
+  // context id
   std::string const _id;
+
   [[maybe_unused]] uint64_t const _batchSize;
   [[maybe_unused]] uint64_t const _prefetchCount;
   [[maybe_unused]] uint64_t const _parallelism;
-  [[maybe_unused]] std::vector<std::string> const _shards;
-  [[maybe_unused]] double const _ttl;
+
+  // time-to-live value for this context. will be extended automatically
+  // whenever the context is accessed.
+  double const _ttl;
   std::string const _user;
   std::string const _database;
 
+  // timestamp when this context expires and will be removed by the manager.
+  // will be extended whenever the context is leased from the manager and
+  // when it is returned to the manager.
+  // timestamp is in seconds since 1970/1/1
   std::atomic<double> _expires;
 
+  // a guard object that protects the underlying database from being deleted
+  // while the dump is ongoing. will be populated in the constructor and then
+  // be static.
+  std::unique_ptr<DatabaseGuard> _databaseGuard;
+
+  // guard objects that project the underlying collections/shards from being
+  // deleted while the dump is ongoing. will be populated in the constructor and
+  // then be static
+  std::unordered_map<std::string, std::unique_ptr<CollectionGuard>>
+      _collectionGuards;
+
+  // the RocksDB snapshot that can be used concurrently by all operations that
+  // use this context.
   std::shared_ptr<rocksdb::ManagedSnapshot> _snapshot;
 };
 
