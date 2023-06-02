be0b467354db602f511c119fd73daf0c74d3fefe
hkernbach <heiko@arangodb.com>
2023-06-02 13:32:40 +0200

    simplify feed import

--- scripts/toolbox/tests/RocksDBCacheSizeTest.py
+++ scripts/toolbox/tests/RocksDBCacheSizeTest.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 
-import importlib.util
-
-
-def module_from_file(module_name, file_path):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
-
-
-Feed = module_from_file("Feed", "modules/Feed.py")
+from modules import Feed
 
 
 def start(args, cfg):
