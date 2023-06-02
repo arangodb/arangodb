4ded1a75604541b3036ccb235daa7ae5db944962
hkernbach <heiko@arangodb.com>
2023-06-02 13:16:45 +0200

    use camelCase everywhere

--- scripts/toolbox/modules/Feed.py
+++ scripts/toolbox/modules/Feed.py
@@ -6,8 +6,8 @@ import subprocess
 def start(cfg, operations):
     # Feed needs to have a local file as input
     fileName = "work/feedInput.txt"
-    with open(fileName, "w") as text_file:
-        text_file.write(operations)
+    with open(fileName, "w") as textFile:
+        textFile.write(operations)
 
     parameters = ["go", "run", "main.go", "--execute", "../../" + fileName, "--jsonOutputFile",
                   "../../" + cfg.feed["jsonOutputFile"]]
