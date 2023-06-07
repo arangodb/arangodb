03d3f482db7fc8eb5c27f9531e2e105109909ca5
Lars Maier <lars@arangodb.com>
2023-06-07 16:57:00 +0200

    Fix logids.

--- client-tools/Dump/DumpFeature.cpp
+++ client-tools/Dump/DumpFeature.cpp
@@ -1522,7 +1522,7 @@ void DumpFeature::ParallelDumpServer::finishDumpContext(
   auto check = ::arangodb::HttpResponseChecker::check(client.getErrorMessage(),
                                                       response.get());
   if (check.fail()) {
-    LOG_TOPIC("bdecf", ERR, Logger::DUMP)
+    LOG_TOPIC("bdedf", ERR, Logger::DUMP)
         << "failed to finish dump context on server " << server << ": "
         << check.errorMessage();
   }
@@ -1725,7 +1725,7 @@ DumpFeature::ParallelDumpServer::receiveNextBatch(
     auto check = ::arangodb::HttpResponseChecker::check(
         client.getErrorMessage(), response.get());
     if (check.fail()) {
-      LOG_TOPIC("ac972", ERR, arangodb::Logger::DUMP)
+      LOG_TOPIC("ad972", ERR, arangodb::Logger::DUMP)
           << "An error occurred while dumping from server '" << server
           << "': " << check.errorMessage();
 
@@ -1790,7 +1790,7 @@ std::shared_ptr<ManagedDirectory::File> DumpFeature::DumpFileProvider::getFile(
     auto file = _directory.writableFile(filename, true /*overwrite*/, 0,
                                         true /*gzipOk*/);
     if (file == nullptr || file->status().fail()) {
-      LOG_TOPIC("40543", FATAL, Logger::DUMP)
+      LOG_TOPIC("43543", FATAL, Logger::DUMP)
           << "Failed to open file " << filename
           << " for writing: " << file->status().errorMessage();
       FATAL_ERROR_EXIT();
