ed1a90527701ff26f4dc6c5005bd8b6f92d5b567
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-06-13 00:13:55 +0530

    chore: refactor table header, remove toolbar

--- js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/AnalyzersView.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/AnalyzersView.tsx
@@ -3,28 +3,32 @@ import { Box, Button, Heading, Stack, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import { AddAnalyzerModal } from "./addAnalyzer/AddAnalyzerModal";
 import { AnalyzersTable } from "./listAnalyzers/AnalyzersTable";
-import { AnalyzersToolbar } from "./AnalyzersToolbar";
 
 export const AnalyzersView = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box padding="4" width="100%">
-      <Stack direction="row" marginBottom="4" alignItems="center">
-        <Heading size="lg">Analyzers</Heading>
-        <Button
-          size="sm"
-          leftIcon={<AddIcon />}
-          colorScheme="blue"
-          onClick={() => {
-            onOpen();
-          }}
-        >
-          Add analyzer
-        </Button>
-      </Stack>
-      <AnalyzersToolbar />
+      <AnalyzerViewHeader onOpen={onOpen} />
       <AnalyzersTable />
       <AddAnalyzerModal isOpen={isOpen} onClose={onClose} />
     </Box>
+  );
+};
+
+const AnalyzerViewHeader = ({ onOpen }: { onOpen: () => void }) => {
+  return (
+    <Stack direction="row" marginBottom="4" alignItems="center">
+      <Heading size="lg">Analyzers</Heading>
+      <Button
+        size="sm"
+        leftIcon={<AddIcon />}
+        colorScheme="blue"
+        onClick={() => {
+          onOpen();
+        }}
+      >
+        Add analyzer
+      </Button>
+    </Stack>
   );
 };
