9e9dcb53845c69a1d8386cf2a0b2180864a32fa9
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-06-12 20:19:05 +0200

    Bugfix (#19222)

--- arangod/IResearch/IResearchDataStore.cpp
+++ arangod/IResearch/IResearchDataStore.cpp
@@ -1385,17 +1385,20 @@ Result IResearchDataStore::initDataStore(
         const auto recoveryTick = linkLock->_engine->recoveryTick();
 
         bool isOutOfSync = linkLock->isOutOfSync();
+        auto const dataStoreTick =
+            dataStore._recoveryTickHigh != std::numeric_limits<uint64_t>::max()
+                ? dataStore._recoveryTickHigh
+                : dataStore._recoveryTickLow;
         if (isOutOfSync) {
           LOG_TOPIC("2721a", WARN, iresearch::TOPIC)
               << "marking ArangoSearch index '" << id.id()
               << "' as out of sync, consider to drop and re-create the index "
                  "in order to synchronize it";
 
-        } else if (dataStore._recoveryTickHigh > recoveryTick) {
+        } else if (dataStoreTick > recoveryTick) {
           LOG_TOPIC("5b59f", WARN, iresearch::TOPIC)
               << "ArangoSearch index '" << id.id() << "' is recovered at tick '"
-              << dataStore._recoveryTickHigh
-              << "' greater than storage engine tick '"
+              << dataStoreTick << "' greater than storage engine tick '"
               << linkLock->_engine->recoveryTick()
               << "', it seems WAL tail was lost and index is out of sync with "
                  "the underlying collection: "
