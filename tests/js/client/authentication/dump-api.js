36218f4ca4e2a0e6197c6539990eb9d69897bdb7
Lars Maier <lars@arangodb.com>
2023-06-12 23:05:41 +0200

    Updating important comments.

--- tests/js/client/authentication/dump-api.js
+++ tests/js/client/authentication/dump-api.js
@@ -6,7 +6,7 @@
 ///
 /// DISCLAIMER
 ///
-/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+/// Copyright 2023 ArangoDB GmbH, Cologne, Germany
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 /// Copyright holder is triAGENS GmbH, Cologne, Germany
 ///
 /// @author Lars Maier
-/// @author Copyright 2013, triAGENS GmbH, Cologne, Germany
 ////////////////////////////////////////////////////////////////////////////////
 
 const jsunity = require("jsunity");
