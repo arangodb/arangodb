e09fa8ed0d84f01ba10dc7585ee2b543e14e80dc
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-06-13 17:34:38 +0530

    Revert "chore: add JSON validation"
    This reverts commit f3dcabea66ac0d32733a8d77f40e4dd98959ea6c.

--- js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/addAnalyzer/AnalyzerJSONForm.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/analyzers/addAnalyzer/AnalyzerJSONForm.tsx
@@ -1,39 +1,19 @@
-import Ajv from "ajv";
-import ajvErrors from "ajv-errors";
 import { useFormikContext } from "formik";
-import React, { useState } from "react";
+import React from "react";
 import { ControlledJSONEditor } from "../../../components/jsonEditor/ControlledJSONEditor";
-import { JSONErrors } from "../../../components/jsonEditor/JSONErrors";
-import { AnalyzerJSONFormSchema } from "../AnalyzerJSONSchemaHelper";
 
-const ajv = new Ajv({
-  allErrors: true,
-  verbose: false,
-  discriminator: true,
-  $data: true
-});
-ajvErrors(ajv);
 export const AnalyzerJSONForm = () => {
   const { values } = useFormikContext();
-  const [errors, setErrors] = useState<any[]>([]);
   return (
-    <>
-      <ControlledJSONEditor
-        value={values}
-        mode={"code"}
-        ajv={ajv}
-        onValidationError={errors => {
-          setErrors(errors);
-        }}
-        schema={AnalyzerJSONFormSchema}
-        htmlElementProps={{
-          style: {
-            height: "calc(100vh - 300px)",
-            width: "100%"
-          }
-        }}
-      />
-      <JSONErrors errors={errors} />
-    </>
+    <ControlledJSONEditor
+      value={values}
+      mode={"code"}
+      htmlElementProps={{
+        style: {
+          height: "calc(100vh - 300px)",
+          width: "100%"
+        }
+      }}
+    />
   );
 };
