ebf6f1db6936337034d48057a916d0915efb61eb
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-06-13 14:36:24 +0530

    chore: add reset button

--- js/apps/system/_admin/aardvark/APP/react/src/components/table/FiltersList.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/components/table/FiltersList.tsx
@@ -1,4 +1,4 @@
-import { Badge, Button, Grid } from "@chakra-ui/react";
+import { Badge, Button, Grid, Stack } from "@chakra-ui/react";
 import { Table as TableType } from "@tanstack/react-table";
 import * as React from "react";
 import { FilterBar } from "./FilterBar";
@@ -19,8 +19,9 @@ export const FiltersList = <Data extends object>({
   const [showFilters, setShowFilters] = React.useState(false);
   const columnFilters = table.getState().columnFilters;
   return (
-    <Grid gridTemplateColumns="100px 1fr" gap="4">
-      <FilterButton
+    <Grid gridTemplateColumns="200px 1fr" gap="4">
+      <FilterButton<Data>
+        table={table}
         appliedFiltersCount={columnFilters.length}
         onClick={() => setShowFilters(!showFilters)}
         isSelected={showFilters}
@@ -39,41 +40,58 @@ export const FiltersList = <Data extends object>({
   );
 };
 
-const FilterButton = ({
+const FilterButton = <Data extends object>({
+  table,
   onClick,
   isSelected,
   appliedFiltersCount
 }: {
+  table: TableType<Data>;
   onClick: () => void;
   isSelected: boolean;
   appliedFiltersCount: number;
 }) => {
   return (
-    <Button
-      onClick={onClick}
-      backgroundColor={isSelected ? "gray.200" : "white"}
-      borderColor="gray.900"
-      variant={"outline"}
-    >
-      Filters
+    <Stack direction="row" alignItems="center">
+      <Button
+        onClick={onClick}
+        backgroundColor={isSelected ? "gray.200" : "white"}
+        borderColor="gray.900"
+        variant={"outline"}
+      >
+        Filters
+        {appliedFiltersCount > 0 && (
+          <Badge
+            fontSize="sm"
+            width="5"
+            height="5"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            position="absolute"
+            right="-10px"
+            top="-10px"
+            backgroundColor="gray.900"
+            color="white"
+            borderRadius="20px"
+          >
+            {appliedFiltersCount}
+          </Badge>
+        )}
+      </Button>
       {appliedFiltersCount > 0 && (
-        <Badge
-          fontSize="sm"
-          width="5"
-          height="5"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          position="absolute"
-          right="-10px"
-          top="-10px"
-          backgroundColor="gray.900"
-          color="white"
-          borderRadius="20px"
+        <Button
+          textTransform="uppercase"
+          onClick={() => {
+            table.resetColumnFilters();
+          }}
+          variant="ghost"
+          size="xs"
+          colorScheme="blue"
         >
-          {appliedFiltersCount}
-        </Badge>
+          Reset
+        </Button>
       )}
-    </Button>
+    </Stack>
   );
 };
