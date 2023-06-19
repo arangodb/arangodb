18cac835c0211d535227f6212a6131529dff7290
Markus Pfeiffer <markuspf@users.noreply.github.com>
2023-06-19 16:36:57 +0100

    Update lib/Basics/TimeString.h
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- lib/Basics/TimeString.h
+++ lib/Basics/TimeString.h
@@ -28,7 +28,7 @@
 
 inline std::string timepointToString(
     std::chrono::system_clock::time_point const& t) {
-  return date::format("%Y-%m-%dT%H:%M:%SZ", floor<std::chrono::seconds>(t));
+  return date::format("%Y-%m-%dT%H:%M:%SZ", std::floor<std::chrono::seconds>(t));
 }
 
 inline std::string timepointToString(
