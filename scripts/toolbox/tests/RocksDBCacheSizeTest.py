741352443b8ad4a54422b545ac69a4ba2598352d
hkernbach <heiko@arangodb.com>
2023-06-23 13:38:26 +0200

    added additional bigger test case

--- scripts/toolbox/tests/RocksDBCacheSizeTest.py
+++ scripts/toolbox/tests/RocksDBCacheSizeTest.py
@@ -48,6 +48,38 @@ def start(args, cfg):
       }
     ]'''
 
+    bigAdvancedOperations = '''[
+          # Create a collection
+          normal create database=xyz collection=c numberOfShards=3 replicationFactor=2
+          normal insert database=xyz collection=c size=10G documentSize=300 withGeo=false withWords=0 parallelism=64 numberFields=5
+
+          # Drop stuff again:
+          normal drop database=xyz collection=c
+          normal dropDatabase database=xyz
+
+          # Now two collections, half the data each:
+          normal create database=xyz collection=d numberOfShards=3 replicationFactor=2 drop=true
+          normal create database=xyz collection=e numberOfShards=3 replicationFactor=2 drop=true
+
+          # Insert data concurrently:
+          {
+            normal insert database=xyz collection=d size=5G documentSize=300 withGeo=false withWords=0 parallelism=64 numberFields=5
+            normal insert database=xyz collection=e size=5G documentSize=300 withGeo=false withWords=0 parallelism=64 numberFields=5
+          }
+
+          # Replace with concurrent random reads:
+          {
+            normal randomReplace database=xyz collection=d parallelism=64 batchSize=1000 loadPerThread=20
+            normal randomRead database=xyz collection=e parallelism=64 loadPerThread=100
+          }
+
+          # Update with concurrent random reads:
+          {
+            normal randomUpdate database=xyz collection=d parallelism=64 batchSize=1000 loadPerThread=20
+            normal randomRead database=xyz collection=e parallelism=64 loadPerThread=100
+          }
+        ]'''
+
     process = Feed.start(cfg, smallAdvancedOperations)
     print('--> Started test: "RocksDBCacheSizeTest". Output file is: "' + cfg["feed"]["jsonOutputFile"] + '"')
     return process
