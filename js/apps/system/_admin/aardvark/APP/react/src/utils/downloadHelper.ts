bfcb97650a49518618c0c5b324b35528fae1f48a
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-06-23 13:39:13 +0530

    chore: refactor download

--- js/apps/system/_admin/aardvark/APP/react/src/utils/downloadHelper.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/utils/downloadHelper.ts
@@ -13,19 +13,12 @@ export const downloadPost = async ({
     body
   });
   const blobUrl = window.URL.createObjectURL(response.body);
-  const a = document.createElement("a");
-  a.href = blobUrl;
-  a.download =
+  const filename =
     response.headers?.["content-disposition"]?.replace(
       /.* filename="([^")]*)"/,
       "$1"
     ) || "";
-  document.body.appendChild(a);
-  a.click();
-  window.setTimeout(() => {
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  }, 500);
+  downloadBlob(blobUrl, filename);
 };
 
 export const download = async (url: string) => {
@@ -35,17 +28,22 @@ export const download = async (url: string) => {
     method: "GET"
   });
   const blobUrl = window.URL.createObjectURL(response.body);
-  const a = document.createElement("a");
-  a.href = blobUrl;
-  a.download =
+  const filename =
     response.headers?.["content-disposition"]?.replace(
       /.* filename="([^")]*)"/,
       "$1"
-    ) || "";
+    ) || "download";
+  downloadBlob(blobUrl, filename);
+};
+
+const downloadBlob = (blobUrl: string, filename: string) => {
+  const a = document.createElement("a");
+  a.href = blobUrl;
+  a.download = filename;
   document.body.appendChild(a);
   a.click();
   window.setTimeout(() => {
-    window.URL.revokeObjectURL(url);
+    window.URL.revokeObjectURL(blobUrl);
     document.body.removeChild(a);
   }, 500);
 };
