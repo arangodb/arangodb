967944491fcca6545a179897b0886fd424335f8c
hkernbach <heiko@arangodb.com>
2023-06-27 16:22:21 +0200

    added MemoryTypes.h

---
+++ lib/Basics/Memory/MemoryTypes.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2023 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Heiko Kernbach
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <boost/container/pmr/memory_resource.hpp>
+#include <boost/container/vector.hpp>
+#include <boost/container/pmr/polymorphic_allocator.hpp>
+#include <boost/container/pmr/global_resource.hpp>
+
+namespace arangodb {
+
+typedef boost::container::pmr::memory_resource memory_resource_t;
+
+struct new_delete_resource_t {
+  static memory_resource_t* new_delete_resource() {
+    return boost::container::pmr::new_delete_resource();
+  }
+};
+
+template<class T>
+using pmr_vector_t =
+    boost::container::vector<T,
+                             boost::container::pmr::polymorphic_allocator<T>>;
+
+}  // namespace arangodb
