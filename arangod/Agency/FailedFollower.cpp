72aafc3d76edadc0e635cfd3e80f3c6023e6a7be
Lars Maier <lars@arangodb.com>
2023-06-28 14:04:09 +0200

    Revert change.

--- arangod/Agency/FailedFollower.cpp
+++ arangod/Agency/FailedFollower.cpp
@@ -370,7 +370,7 @@ bool FailedFollower::start(bool& aborts) {
 
   slice = result.get(std::vector<std::string>(
       {agencyPrefix, "Supervision", "Health", _to, "Status"}));
-  if (slice.isString() &&
+  if (!slice.isString() ||
       slice.stringView() != Supervision::HEALTH_STATUS_GOOD) {
     LOG_TOPIC("4785b", INFO, Logger::SUPERVISION)
         << "Destination server " << _to << " is no longer in good condition";
