d886be00d54d144ae67e3ea83fdacff4e2f38906
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-06-29 18:27:28 +0200

    Add sorting to select options

--- js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/addGraph/useEdgeCollectionOptions.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/addGraph/useEdgeCollectionOptions.tsx
@@ -6,7 +6,18 @@ const useFetchCollections = () => {
   const currentDB = getCurrentDB();
   const fetchCollections = async () => {
     const collections = await currentDB.listCollections();
-    return collections;
+    const sortedCollection = collections.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    return sortedCollection;
   };
 
   const { data } = useSWR("/collections", fetchCollections);
