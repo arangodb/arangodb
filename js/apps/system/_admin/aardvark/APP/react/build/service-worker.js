98bcc82cedb05292fcb14cbeee973d5fca5975a2
Vitaly Pryakhin <vitaly@arangodb.com>
2023-06-30 17:27:39 +0300

    arangodb release 3.10.9

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.ccb13381841cfb384836b7de46e82dd9.js"
+  "precache-manifest.cc74c59bfa386ac1ca70f659db346ed7.js"
 );
 
 workbox.clientsClaim();
