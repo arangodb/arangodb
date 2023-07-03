7c1880b09773fe8a79281426e4c2bba459e702e3
Jan <jsteemann@users.noreply.github.com>
2023-07-03 16:01:45 +0200

    APM-666: dummy implementation for memory tracking inside RocksDB transactions (#19323)

--- arangod/RocksDBEngine/RocksDBKey.h
+++ arangod/RocksDBEngine/RocksDBKey.h
@@ -63,7 +63,6 @@ class RocksDBKey {
   RocksDBKey& operator=(RocksDBKey const& other) = delete;
   RocksDBKey& operator=(RocksDBKey&& other) = delete;
 
- public:
   /// @brief verify that a key actually contains the given local document id
   bool containsLocalDocumentId(LocalDocumentId const& id) const;
 
@@ -191,7 +190,6 @@ class RocksDBKey {
   //////////////////////////////////////////////////////////////////////////////
   void constructLogEntry(uint64_t objectId, replication2::LogIndex idx);
 
- public:
   //////////////////////////////////////////////////////////////////////////////
   /// @brief Extracts the type from a key
   ///
@@ -309,7 +307,6 @@ class RocksDBKey {
   /// size of internal objectID
   static constexpr size_t objectIdSize() { return sizeof(uint64_t); }
 
- public:
   //////////////////////////////////////////////////////////////////////////////
   /// @brief Returns a reference to the full, constructed key
   //////////////////////////////////////////////////////////////////////////////
