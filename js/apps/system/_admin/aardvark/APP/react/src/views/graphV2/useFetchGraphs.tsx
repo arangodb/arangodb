8e34f0d579e3bc37798c064ed4d282df76192eeb
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-07-03 14:51:04 +0200

    Rename SWR key

--- js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/useFetchGraphs.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/useFetchGraphs.tsx
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import { getCurrentDB } from "../../utils/arangoClient";
 
 export const useFetchGraphs = () => {
-  const { data } = useSWR("/graph", async () => {
+  const { data } = useSWR("/graphs", async () => {
     const db = getCurrentDB();
     const data = await db.listGraphs();
     return data;
