54ef06d010bdcc961394cf8bb3b65a055b7a66ce
Alan Plum <alan.plum-extern@arangodb.com>
2023-07-06 16:01:22 +0200

    Add redirect on error

--- js/apps/system/_admin/aardvark/APP/react/src/views/databases/useFetchInitialValues.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/views/databases/useFetchInitialValues.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import { getAdminRouteForCurrentDB } from "../../utils/arangoClient";
 
 export const useFetchInitialValues = () => {
-  const { data } = useSWR(`/databaseDefaults`, async () => {
+  const { data, error } = useSWR(`/databaseDefaults`, async () => {
     const route = getAdminRouteForCurrentDB();
     const res = await route.get(`server/databaseDefaults`);
     const { replicationFactor, sharding, writeConcern } = res.body;
@@ -16,5 +16,14 @@ export const useFetchInitialValues = () => {
       writeConcern
     };
   });
+
+  if (error) {
+    window.App.navigate("#databases", { trigger: true });
+    window.arangoHelper.arangoError(
+      "Failure",
+      `Got unexpected server response: ${error.errorNum}: ${error.message}`
+    );
+  }
+
   return { defaults: data };
 };
