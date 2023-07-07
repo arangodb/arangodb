e18ba31f3abeb26e6573b6d9a9c008530ddd04f9
Heiko Kernbach <heiko@arangodb.com>
2023-07-07 20:46:30 +0200

    this should not be required

--- lib/Basics/MemoryTypes/MemoryTypes.h
+++ lib/Basics/MemoryTypes/MemoryTypes.h
@@ -45,9 +45,9 @@ struct hash_monitored_string {
     return std::hash<std::string>{}(txt);
   }
 
-  [[nodiscard]] size_t operator()(MonitoredString const& value) const noexcept {
-    return std::hash<std::string_view>{}(std::string_view{value});
-  }
+  /*[[nodiscard]] size_t operator()(MonitoredString const& value) const noexcept
+  { return std::hash<std::string_view>{}(std::string_view{value});
+  }*/
 };
 
 struct compare_monitored_string {
