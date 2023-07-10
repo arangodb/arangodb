40f6222d1dc3ec8a268d88c21671ac172cb54c6a
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-07-10 15:40:27 +0200

    Add reset display settings function (base)

--- js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/listGraphs/EditGraphButtons.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/listGraphs/EditGraphButtons.tsx
@@ -2,20 +2,29 @@ import { Button, useDisclosure } from "@chakra-ui/react";
 import { GraphInfo } from "arangojs/graph";
 import React from "react";
 import { DeleteGraphModal } from "./DeleteGraphModal";
+import { fetchUserConfig } from "../useFetchAndSetupGraphParams";
+import { DEFAULT_URL_PARAMETERS } from "../UrlParametersContext";
+import { putUserConfig } from "../useGraphSettingsHandlers";
 
 export const EditGraphButtons = ({ graph }: { graph?: GraphInfo }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const resetDisplaySettings = async () => {
+    const fullConfig = await fetchUserConfig();
+    const info = await putUserConfig({
+      params: DEFAULT_URL_PARAMETERS,
+      fullConfig,
+      graphName: "routeplanner" // currently hardcoded: To be changed
+      //graphName: graph?.name
+    });
+    return info;
+  };
   return (
     <>
       <DeleteGraphModal isOpen={isOpen} onClose={onClose} graph={graph} />
       <Button colorScheme="red" type="button" onClick={onOpen}>
         Delete
       </Button>
-      <Button
-        colorScheme="orange"
-        type="button"
-        onClick={() => console.log("Reset display settings")}
-      >
+      <Button colorScheme="orange" type="button" onClick={resetDisplaySettings}>
         Reset display settings
       </Button>
     </>
