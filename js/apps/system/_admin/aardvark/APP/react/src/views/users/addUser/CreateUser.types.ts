91a9f1f0aa501eb0b1d0232c82842f242a16d21b
Andreas Jurdak <andreas.jurdak@arangodb.com>
2023-07-13 14:13:24 +0200

    Adjust types

--- js/apps/system/_admin/aardvark/APP/react/src/views/users/addUser/CreateUser.types.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/views/users/addUser/CreateUser.types.ts
@@ -1,7 +1,19 @@
 export type CreateDatabaseUserValues = {
+  extra: {
+    img: string
+  };
   active: boolean;
   username: string;
   name: string;
   gravatar: string;
   passwd: string;
+};
+
+export type DatabaseUserValues = {
+  extra: {
+    img: string;
+    name: string;
+  };
+  user: string;
+  active: boolean;
 };
