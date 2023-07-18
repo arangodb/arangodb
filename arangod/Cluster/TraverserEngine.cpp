9734b57526c551cc0fe071bbc0d89eddae8cf6e6
Heiko Kernbach <heiko@arangodb.com>
2023-07-18 12:47:05 +0200

    prevent string copy. Implementation now support std::string_view

--- arangod/Cluster/TraverserEngine.cpp
+++ arangod/Cluster/TraverserEngine.cpp
@@ -191,8 +191,7 @@ void BaseEngine::getVertexData(VPackSlice vertex, VPackBuilder& builder,
     std::string_view vertex = id.substr(pos + 1);
     for (auto const& shard : shards->second) {
       Result res = _trx->documentFastPathLocal(
-          std::string{shard}, vertex,
-          [&](LocalDocumentId const&, VPackSlice doc) {
+          shard, vertex, [&](LocalDocumentId const&, VPackSlice doc) {
             // FOUND. short circuit.
             read++;
             builder.add(v);
