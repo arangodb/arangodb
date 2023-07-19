363f4986a027240e8faa16d39fa8a774475d9ac0
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-07-19 15:47:24 +0530

    chore: use localcompare to sort edges

--- js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/addGraph/useEdgeCollectionOptions.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/graphV2/addGraph/useEdgeCollectionOptions.tsx
@@ -7,15 +7,7 @@ const useFetchCollections = () => {
   const fetchCollections = async () => {
     const collections = await currentDB.listCollections();
     const sortedCollection = collections.sort((a, b) => {
-      const nameA = a.name.toUpperCase();
-      const nameB = b.name.toUpperCase();
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
+      return a.name.localeCompare(b.name, undefined, { sensitivity: "base" });
     });
     return sortedCollection;
   };
