ba2a396cabbf3afa42dd46e510a8b46c9ee6d3b7
jsteemann <jsteemann@users.noreply.github.com>
2023-07-20 15:48:59 +0200

    reformat

--- lib/Basics/ReadWriteLock.cpp
+++ lib/Basics/ReadWriteLock.cpp
@@ -89,15 +89,17 @@ bool ReadWriteLock::tryLockWriteFor(std::chrono::microseconds timeout) {
   }
 
   // Undo the counting of us as queued writer:
-  auto state = _state.fetch_sub(QUEUED_WRITER_INC, std::memory_order_relaxed) - QUEUED_WRITER_INC;
- 
+  auto state = _state.fetch_sub(QUEUED_WRITER_INC, std::memory_order_relaxed) -
+               QUEUED_WRITER_INC;
+
   if (state == 0) {
     // no queued writers and no locks acquired
     // no more writers -> wake up any waiting readings
     { std::lock_guard<std::mutex> guard(_reader_mutex); }
     _readers_bell.notify_all();
   } else if ((state & QUEUED_WRITER_MASK) != 0 && (state & WRITE_LOCK) == 0) {
-    // there are other writers waiting and the lock is not acquired -> wake up one of them
+    // there are other writers waiting and the lock is not acquired -> wake up
+    // one of them
     { std::lock_guard<std::mutex> guard(_writer_mutex); }
     _writers_bell.notify_one();
   }
