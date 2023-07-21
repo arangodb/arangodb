e6ea01ac76f86a8feb223e64bfdea3576baf879f
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-07-21 15:34:01 +0200

    Backports to 3.11 (#19444)
    * Fix unstable test (#19281)
    
    * Try to make cluster tests tsan run more useful (#19344)
    
    * Remove already fixed issues
    
    * Suppress very common known and easily reproducable issues
    
    * Fix data race Supervision
    
    * Fix data race ClusterMetrics
    
    * Fix data race Pregel
    
    * Fix SEARCH-465 (#19360)
    
    * Make post recovery callbacks parallel (#19428)
    
    * Fix
    
    * Fix
    
    * fix data race
    
    * simplify
    
    * redundant header
    
    ---------
    
    Co-authored-by: Andrei Lobov <andrei.lobov@arangodb.com>
    
    * wip
    
    * wip
    
    * wip
    
    * Update CHANGELOG
    
    ---------
    
    Co-authored-by: Andrei Lobov <andrei.lobov@arangodb.com>
    Co-authored-by: Vadim Kondratev <vadim@arangodb.com>

--- arangod/RocksDBEngine/RocksDBBuilderIndex.h
+++ arangod/RocksDBEngine/RocksDBBuilderIndex.h
@@ -106,8 +106,9 @@ class RocksDBBuilderIndex final : public RocksDBIndex {
 
   Result drop() override { return _wrapped->drop(); }
 
-  void afterTruncate(TRI_voc_tick_t tick, transaction::Methods* trx) override {
-    _wrapped->afterTruncate(tick, trx);
+  void truncateCommit(TruncateGuard&& guard, TRI_voc_tick_t tick,
+                      transaction::Methods* trx) final {
+    _wrapped->truncateCommit(std::move(guard), tick, trx);
   }
 
   void load() override { _wrapped->load(); }
