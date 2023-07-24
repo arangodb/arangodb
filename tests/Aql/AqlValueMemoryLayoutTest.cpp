60cdd75e6638eb86ffbe6878ee928aabcc7ff8cc
Tobias Gödderz <tobias@arangodb.com>
2023-07-24 15:16:22 +0200

    Remove dependency to VelocyPackStringLiteral.h

--- tests/Aql/AqlValueMemoryLayoutTest.cpp
+++ tests/Aql/AqlValueMemoryLayoutTest.cpp
@@ -25,9 +25,9 @@
 
 #include "Aql/AqlValue.h"
 #include "Basics/Endian.h"
-#include "VelocypackUtils/VelocyPackStringLiteral.h"
 
 #include <velocypack/Builder.h>
+#include <velocypack/Parser.h>
 #include <velocypack/Slice.h>
 #include <velocypack/SharedSlice.h>
 
@@ -35,7 +35,6 @@
 
 using namespace arangodb;
 using namespace arangodb::aql;
-using arangodb::velocypack::operator""_vpack;
 
 namespace {
 
@@ -144,6 +143,18 @@ void runChecksForSlice(velocypack::Slice value, std::uint8_t const* expected) {
   } else {
     TRI_ASSERT(false) << "Unexpected type " << value.typeName();
   }
+}
+
+auto vpackFromJsonString(char const* c) -> std::shared_ptr<VPackBuilder> {
+  velocypack::Options options;
+  options.checkAttributeUniqueness = true;
+  velocypack::Parser parser(&options);
+  parser.parse(c);
+  return parser.steal();
+}
+
+auto operator"" _vpack(const char* json, size_t) -> velocypack::SharedSlice {
+  return vpackFromJsonString(json)->sharedSlice();
 }
 
 // note: in all following tests, the value UNINITIALIZED (0xa5) has a special
