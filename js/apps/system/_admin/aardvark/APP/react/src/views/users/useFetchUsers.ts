c40d1317fe395c7993ab2aaf7e5c69979c4a027a
Andreas Jurdak <56896586+ajurdak@users.noreply.github.com>
2023-07-24 10:03:46 +0200

    FE-301: Unify users view on web UI (#19432)
    Co-authored-by: Palash Karia <2976363+palashkaria@users.noreply.github.com>

--- js/apps/system/_admin/aardvark/APP/react/src/views/users/useFetchUsers.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/views/users/useFetchUsers.ts
@@ -1,12 +1,12 @@
 import useSWR from "swr";
 import { getApiRouteForCurrentDB } from "../../utils/arangoClient";
-import { ArangoUser } from "arangojs/database";
+import { DatabaseUserValues } from "./addUser/CreateUser.types";
 
 export const useFetchUsers = () => {
   const { data } = useSWR("/users", async () => {
     const route = getApiRouteForCurrentDB();
     const res = await route.get("user");
-    return res.body.result as ArangoUser[];
+    return res.body.result as DatabaseUserValues[];
   });
   return { users: data };
 };
