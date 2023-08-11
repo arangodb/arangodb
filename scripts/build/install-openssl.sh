955dc3f0fbd5d9c66ff4ffc6e5243565f2d2f9d8
mpoeter <manuel@arangodb.com>
2023-08-11 13:33:36 +0200

    Update build container to OpenSSL 3.0.10

--- scripts/build/install-openssl.sh
+++ scripts/build/install-openssl.sh
@@ -15,7 +15,7 @@ fi;
 echo "https://www.openssl.org/source/${OLD}openssl-$OPENSSLVERSION.tar.gz"
 
 test -n "$OPENSSLVERSION"
-export OPENSSLPATH=`echo $OPENSSLVERSION | sed 's/\([a-zA-Z]$\|\.[0-9]$\)//g'`
+export OPENSSLPATH=`echo $OPENSSLVERSION | sed 's/\.[0-9]*$//g'`
 cd /tmp
 curl -O https://www.openssl.org/source/${OLD}openssl-$OPENSSLVERSION.tar.gz
 tar xzvf openssl-$OPENSSLVERSION.tar.gz
