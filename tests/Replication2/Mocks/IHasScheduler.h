d2782b2c9146fd3e9bb4d1f2651c5870e769f76a
Tobias Gödderz <tobias@arangodb.com>
2023-08-15 11:03:24 +0200

    Workaround for gcc

--- tests/Replication2/Mocks/IHasScheduler.h
+++ tests/Replication2/Mocks/IHasScheduler.h
@@ -34,7 +34,7 @@ namespace arangodb::replication2::test {
 template<class T>
 concept PointerLike = requires {
   // gcc-11's STL has ::pointer defined even for non-pointers (e.g.,
-  // std::shared_ptr<int>::pointer is int).
+  // std::pointer_traits<int>::pointer is int).
   typename std::pointer_traits<T>::element_type;
 };
 
