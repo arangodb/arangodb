acf3faa4f7d9cd8a1d8e476f49e93def3fe06357
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-08-16 16:44:13 +0530

    fix: don't reset search on pressing meta key

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/collectionsView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/collectionsView.js
@@ -262,18 +262,16 @@
 
     restrictToSearchPhraseKey: function (event) {
       if (
-        event && event.originalEvent && (
-          (
-            event.originalEvent.key &&
-            (
-              event.originalEvent.key === 'Control' ||
-              event.originalEvent.key === 'Alt' ||
-              event.originalEvent.key === 'Shift'
-            )
-          ) ||
+        event &&
+        event.originalEvent &&
+        ((event.originalEvent.key &&
+          (event.originalEvent.key === "Control" ||
+            event.originalEvent.key === "Alt" ||
+            event.originalEvent.key === "Shift" ||
+            event.originalEvent.key === "Meta")) ||
           event.originalEvent.ctrlKey ||
-          event.originalEvent.altKey
-        )
+          event.originalEvent.altKey ||
+          event.originalEvent.metaKey)
       ) {
         return;
       }
