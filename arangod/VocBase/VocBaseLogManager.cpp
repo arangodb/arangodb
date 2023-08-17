c5c2dc13b7719d9938e203f22a29fa412728dc0f
Jan <jsteemann@users.noreply.github.com>
2023-08-17 11:28:42 +0200

    fix multiple issues in scheduler (#19612)
    * fix multiple issues in scheduler
    
    - sendRequestRetry in NetworkFeature could add a request to the
      scheduler even when the server was already stopping. the request was
      canceled correctly, but still posted to the scheduler unnecessarily.
      this is now fixed so that the request is not posted to the scheduler
      anymore if the server is shutting down
    - this also fixes an issue with retry requests being posted to the
      scheduler after the shutdown was started. such retry requests led to
      assertion failures (`item->isDisabled()`) in the shutdown of the
      scheduler.
    - in case the requested delay for `Scheduler::queueDelayed()` was less
      than 1ms, the task was queued directly and a nullptr was returned.
      this potential nullptr return value was not handled correctly in the
      NetworkFeature and could have led to nullptr dereferences. this is now
      fixed by removing the special case in `queueDelayed()` for tasks with a
      delay < 1ms.
    - `Scheduler::queueDelayed()` was, probably intentionally, marked
      noexcept, although the method could throw in at least 2 places in OOM
      situations. now, the method is really noexcept and returns a nullptr
      on failure. this is not a problem because most call sites of the
      function handle nullptr return values correctly. the only problematic
      call site in `sendRequestRetry()` is now also adjusted.
    
    * Update arangod/Network/NetworkFeature.cpp
    
    * fix use-after-move
    
    * fix invocation of recursive lock
    
    * 🦃

--- arangod/VocBase/VocBaseLogManager.cpp
+++ arangod/VocBase/VocBaseLogManager.cpp
@@ -378,12 +378,12 @@ auto VocBaseLogManager::GuardedData::buildReplicatedStateWithMethods(
     }
   };
 
-  auto sched = std::make_shared<MyScheduler>();
   auto maybeMetadata = storage->readMetadata();
   if (!maybeMetadata) {
     throw basics::Exception(std::move(maybeMetadata).result(), ADB_HERE);
   }
 
+  auto sched = std::make_shared<MyScheduler>();
   auto& logFeature = server.getFeature<ReplicatedLogFeature>();
   auto& ci = server.getFeature<ClusterFeature>().clusterInfo();
   auto& log = stateAndLog.log = std::invoke([&]() {
