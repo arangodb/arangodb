2a6af7b609e6e35c62dbb30cd4cd37f02e9478b0
Alex Petenchea <alex.petenchea@gmail.com>
2023-08-24 20:19:25 +0300

    Suppressing cppcheck warning

--- arangod/Auth/TokenCache.cpp
+++ arangod/Auth/TokenCache.cpp
@@ -396,11 +396,10 @@ bool auth::TokenCache::validateJwtHMAC256Signature(
   absl::WebSafeBase64Unescape(signatureWebBase64, &signature);
 
   READ_LOCKER(guard, _jwtSecretLock);
-  // cppcheck-suppress invalidLifetime
-  return verifyHMAC(_jwtActiveSecret.data(), _jwtActiveSecret.size(),
-                    message.data(), message.size(), signature.data(),
-                    signature.size(),
-                    SslInterface::Algorithm::ALGORITHM_SHA256);
+  return verifyHMAC(
+      _jwtActiveSecret.data(), _jwtActiveSecret.size(), message.data(),
+      message.size(), signature.data(),  // cppcheck-suppress invalidLifetime
+      signature.size(), SslInterface::Algorithm::ALGORITHM_SHA256);
 }
 #endif
 
