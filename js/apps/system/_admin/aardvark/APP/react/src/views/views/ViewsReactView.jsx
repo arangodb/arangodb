ef6128c9112560362ed9120330417175a543fe85
Alan Plum @ArangoDB <109235316+pluma4345@users.noreply.github.com>
2023-08-31 20:57:48 +0200

    [FE-300] Update views list & modal (#19419)
    Co-authored-by: Palash Karia <2976363+palashkaria@users.noreply.github.com>

---
+++ js/apps/system/_admin/aardvark/APP/react/src/views/views/ViewsReactView.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ViewsListWrap } from "./ViewsListWrap";
+
+const ViewsReactView = () => {
+  return <ViewsListWrap />;
+};
+window.ViewsReactView = ViewsReactView;
