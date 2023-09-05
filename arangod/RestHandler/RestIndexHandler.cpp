b12311f920b98cb2bade1a704189e04d1d410071
Jan <jsteemann@users.noreply.github.com>
2023-09-05 08:16:14 +0200

    BTS-1531 (#19716)
    * Fixed BTS-1531: Async cancel Job returns different error messages.
      Unify error messages for canceled jobs (error code 21) to "request canceled".
      Previously, the server could return different messages, either "request
      canceled", "request has been canceled by user" or "handler canceled".

--- arangod/RestHandler/RestIndexHandler.cpp
+++ arangod/RestHandler/RestIndexHandler.cpp
@@ -40,6 +40,8 @@
 #include "VocBase/LogicalCollection.h"
 #include "VocBase/Methods/Indexes.h"
 
+#include <absl/strings/str_cat.h>
+
 #include <velocypack/Builder.h>
 #include <velocypack/Collection.h>
 #include <velocypack/Iterator.h>
@@ -292,11 +294,10 @@ RestStatus RestIndexHandler::getSelectivityEstimates() {
     if (idx->inProgress() || idx->isHidden()) {
       continue;
     }
-    std::string name = coll->name();
-    name.push_back(TRI_INDEX_HANDLE_SEPARATOR_CHR);
-    name.append(std::to_string(idx->id().id()));
     if (idx->hasSelectivityEstimate() || idx->unique()) {
-      builder.add(name, VPackValue(idx->selectivityEstimate()));
+      builder.add(absl::StrCat(coll->name(), TRI_INDEX_HANDLE_SEPARATOR_STR,
+                               idx->id().id()),
+                  VPackValue(idx->selectivityEstimate()));
     }
   }
   builder.close();
@@ -317,8 +318,8 @@ RestStatus RestIndexHandler::createIndex() {
     events::CreateIndexEnd(_vocbase.name(), "(unknown)", body,
                            TRI_ERROR_BAD_PARAMETER);
     generateError(rest::ResponseCode::BAD, TRI_ERROR_HTTP_BAD_PARAMETER,
-                  "expecting POST " + _request->requestPath() +
-                      "?collection=<collection-name>");
+                  absl::StrCat("expecting POST ", _request->requestPath(),
+                               "?collection=<collection-name>"));
     return RestStatus::DONE;
   }
 
@@ -433,7 +434,8 @@ RestStatus RestIndexHandler::dropIndex() {
   if (iid.starts_with(cName + TRI_INDEX_HANDLE_SEPARATOR_CHR)) {
     idBuilder.add(VPackValue(iid));
   } else {
-    idBuilder.add(VPackValue(cName + TRI_INDEX_HANDLE_SEPARATOR_CHR + iid));
+    idBuilder.add(
+        VPackValue(absl::StrCat(cName, TRI_INDEX_HANDLE_SEPARATOR_STR, iid)));
   }
 
   Result res = methods::Indexes::drop(*coll, idBuilder.slice());
