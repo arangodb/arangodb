a60631cf1667865a3cbb8c1eab3a712a7f054e95
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-09-07 16:18:25 +0530

    fix: user & database search

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/userManagementView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/userManagementView.js
@@ -7,7 +7,6 @@
   window.UserManagementView = Backbone.View.extend({
     el: '#content',
     el2: '#userManagementThumbnailsIn',
-
     template: templateEngine.createTemplate('userManagementView.ejs'),
 
     remove: function () {
@@ -24,7 +23,7 @@
       'click #userManagementThumbnailsIn .tile': 'editUser',
       'click #submitEditUser': 'submitEditUser',
       'click #userManagementToggle': 'toggleView',
-      'keyup #userManagementSearchInput': 'search',
+      'keydown #userManagementSearchInput': 'searchKeyDown',
       'click #userManagementSearchSubmit': 'search',
       'click #callEditUserPassword': 'editUserPassword',
       'click #submitEditUserPassword': 'submitEditUserPassword',
@@ -117,8 +116,38 @@
 
       return this;
     },
-
+    searchKeyDown: function (event) {
+      if (
+        event && event.originalEvent && (
+          (
+            event.originalEvent.key &&
+            (
+              event.originalEvent.key === 'Control' || 
+              event.originalEvent.key === 'Alt' || 
+              event.originalEvent.key === 'Shift'
+            )
+          ) || 
+          event.originalEvent.ctrlKey || 
+          event.originalEvent.altKey ||
+          event.originalEvent.metaKey
+        )
+      ) {
+        return;
+      }
+      this.resetSearch();
+      var self = this;
+      self.searchTimeout = setTimeout(function () {
+        self.search();
+      }, 200);
+    },
+    resetSearch: function () {
+      if (this.searchTimeout) {
+        clearTimeout(this.searchTimeout);
+        this.searchTimeout = null;
+      }
+    },
     search: function () {
+      this.resetSearch();
       var searchInput,
         searchString,
         strLength,
@@ -126,15 +155,22 @@
 
       searchInput = $('#userManagementSearchInput');
       searchString = arangoHelper.escapeHtml($('#userManagementSearchInput').val());
-
       reducedCollection = this.collection.filter(
         function (u) {
-          if (typeof u.attributes.extra.name !== 'undefined') {
-            return (u.attributes.extra.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) || (u.attributes.user.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+          if (searchString === '') {
+            return true;
+          }
+          var name = u.attributes.extra.name || '';
+          var user = u.attributes.user || '';
+          if (typeof name !== 'undefined') {
+            var foundName = name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
+            var foundUser = user.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
+            return foundName || foundUser;
           }
           return false;
         }
       );
+      console.log(reducedCollection, this.collection, {searchString});
 
       $(this.el).html(this.template.render({
         collection: reducedCollection,
