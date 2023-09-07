4d429aa705f58679d3663ba78af38d5ac165d739
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-09-07 17:48:04 +0200

    [BTS-1583] Don't request search-alias view from cluster info second time (#19732)
    * Update RestViewHandler.cpp
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix windows
    
    * Update arangod/RestHandler/RestViewHandler.cpp
    
    Co-authored-by: Andrei Lobov <andrei.lobov@arangodb.com>
    
    * Update arangod/RestHandler/RestViewHandler.cpp
    
    Co-authored-by: Andrei Lobov <andrei.lobov@arangodb.com>
    
    * Fix
    
    * Fix
    
    ---------
    
    Co-authored-by: Andrei Lobov <andrei.lobov@arangodb.com>

--- arangod/RestHandler/RestViewHandler.cpp
+++ arangod/RestHandler/RestViewHandler.cpp
@@ -33,6 +33,7 @@
 #include "Utils/CollectionNameResolver.h"
 #include "Utils/Events.h"
 #include "VocBase/LogicalView.h"
+#include "Logger/LogMacros.h"
 
 namespace {
 
@@ -258,165 +259,92 @@ void RestViewHandler::createView() {
   }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// @brief was docuBlock JSF_post_api_cursor_identifier
-////////////////////////////////////////////////////////////////////////////////
-
 void RestViewHandler::modifyView(bool partialUpdate) {
-  std::vector<std::string> const& suffixes = _request->suffixes();
-
-  if ((suffixes.size() != 2) ||
+  auto const& suffixes = _request->suffixes();
+  if (suffixes.size() != 2 ||
       (suffixes[1] != "properties" && suffixes[1] != "rename")) {
-    generateError(rest::ResponseCode::BAD, TRI_ERROR_BAD_PARAMETER,
-                  "expecting [PUT, PATCH] /_api/view/<view-name>/properties or "
-                  "PUT /_api/view/<view-name>/rename");
-
-    return;
+    return generateError(
+        rest::ResponseCode::BAD, TRI_ERROR_BAD_PARAMETER,
+        "expecting [PUT, PATCH] /_api/view/<view-name>/properties or "
+        "PUT /_api/view/<view-name>/rename");
   }
 
-  auto name = arangodb::basics::StringUtils::urlDecode(suffixes[0]);
-  CollectionNameResolver resolver(_vocbase);
+  auto name = basics::StringUtils::urlDecode(suffixes[0]);
+
+  CollectionNameResolver resolver{_vocbase};
   auto view = resolver.getView(name);
-
-  if (view == nullptr) {
-    generateError(rest::ResponseCode::NOT_FOUND,
-                  TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND);
-
-    return;
+  if (!view) {
+    return generateError(rest::ResponseCode::NOT_FOUND,
+                         TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND);
   }
 
   bool parseSuccess = false;
-  VPackSlice const body = this->parseVPackBody(parseSuccess);
-
+  auto body = this->parseVPackBody(parseSuccess);
   if (!parseSuccess) {
     return;
   }
 
   // First refresh our analyzers cache to see all latest changes in analyzers
-  auto const analyzersRes =
-      server()
-          .getFeature<arangodb::iresearch::IResearchAnalyzerFeature>()
-          .loadAvailableAnalyzers(_vocbase.name());
-  if (analyzersRes.fail()) {
-    generateError(analyzersRes);
-    return;
+  auto& analyzers = server().getFeature<iresearch::IResearchAnalyzerFeature>();
+  r = analyzers.loadAvailableAnalyzers(_vocbase.name());
+  if (!r.ok()) {
+    return generateError(r);
   }
 
-  // handle rename functionality
-  if (suffixes[1] == "rename") {
-    VPackSlice newName = body.get("name");
-
-    if (!newName.isString()) {
-      generateError(rest::ResponseCode::BAD, TRI_ERROR_BAD_PARAMETER,
-                    "expecting \"name\" parameter to be a string");
-
-      return;
-    }
-
-    // .......................................................................
-    // end of parameter parsing
-    // .......................................................................
-
-    if (!view->canUse(auth::Level::RW)) {  // check auth after ensuring that the
-                                           // view exists
-      generateError(
-          Result(TRI_ERROR_FORBIDDEN, "insufficient rights to rename view"));
-
-      return;
-    }
-
-    // skip views for which the full view definition cannot be generated, as
-    // per https://github.com/arangodb/backlog/issues/459
-    try {
-      arangodb::velocypack::Builder viewBuilder;
-
-      viewBuilder.openObject();
-
-      auto res = view->properties(viewBuilder,
-                                  LogicalDataSource::Serialization::Properties);
-
-      if (!res.ok()) {
-        generateError(res);
-
-        return;  // skip view
-      }
-    } catch (...) {
-      generateError(arangodb::Result(TRI_ERROR_INTERNAL));
-
-      return;  // skip view
-    }
-
-    auto res = view->rename(newName.copyString());
-
-    if (res.ok()) {
-      getView(view->name(), false);
-    } else {
-      generateError(res);
-    }
-
-    return;
-  }
-
-  // .........................................................................
-  // end of parameter parsing
-  // .........................................................................
-
-  if (!view->canUse(
-          auth::Level::RW)) {  // check auth after ensuring that the view exists
-    generateError(
-        Result(TRI_ERROR_FORBIDDEN, "insufficient rights to modify view"));
-
-    return;
-  }
-
-  // check ability to read current properties
-  {
-    arangodb::velocypack::Builder builderCurrent;
-
-    builderCurrent.openObject();
-
-    auto resCurrent = view->properties(
-        builderCurrent, LogicalDataSource::Serialization::Properties);
-
-    if (!resCurrent.ok()) {
-      generateError(resCurrent);
-
-      return;
+  bool const isRename = suffixes[1] == "rename";
+  if (isRename) {
+    body = body.get("name");
+    if (!body.isString()) {
+      return generateError(rest::ResponseCode::BAD, TRI_ERROR_BAD_PARAMETER,
+                           "expecting \"name\" parameter to be a string");
     }
   }
 
-  auto result = view->properties(body, true, partialUpdate);
-
-  if (!result.ok()) {
-    generateError(result);
-
-    return;
+  // check auth after ensuring that the view exists
+  if (!view->canUse(auth::Level::RW)) {
+    return generateError(rest::ResponseCode::FORBIDDEN, TRI_ERROR_FORBIDDEN,
+                         "insufficient rights to modify view");
+  }
+  velocypack::Builder builder;
+  // skip views for which the full view definition cannot be generated, as
+  // per https://github.com/arangodb/backlog/issues/459
+  builder.openObject();
+  r = view->properties(builder, LogicalDataSource::Serialization::Properties);
+  if (!r.ok()) {
+    return generateError(r);
   }
 
-  view = resolver.getView(view->id());  // ensure have the latest definition
-
-  if (!view) {
-    generateError(arangodb::Result(TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND));
-
-    return;
+  if (isRename) {
+    // handle rename functionality
+    r = view->rename(body.copyString());
+  } else {
+    // handle update functionality
+    r = view->properties(body, true, partialUpdate);
+  }
+  if (!r.ok()) {
+    return generateError(r);
   }
 
-  arangodb::velocypack::Builder updated;
-
-  updated.openObject();
-
-  auto res =
-      view->properties(updated, LogicalDataSource::Serialization::Properties);
-
-  updated.close();
-
-  if (!res.ok()) {
-    generateError(res);
-
-    return;
+  // ensure have the latest definition, by id because name is cached
+  // search-alias view and name are modified in place so we can avoid re-reading
+  if (!isRename && view->type() != ViewType::kSearchAlias) {
+    auto newView = resolver.getView(view->id());
+    if (!newView) {
+      LOG_TOPIC("f58dc", WARN, Logger::CLUSTER)
+          << "Failed to refresh view definition from the Agency";
+      return generateResult(rest::ResponseCode::OK, builder.close().slice());
+    }
+    view = std::move(newView);
   }
 
-  generateResult(rest::ResponseCode::OK, updated.slice());
+  // return updated definition
+  builder.clear();
+  builder.openObject();
+  r = view->properties(builder, LogicalDataSource::Serialization::Properties);
+  if (!r.ok()) {
+    return generateError(r);
+  }
+  generateResult(rest::ResponseCode::OK, builder.close().slice());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
