3796d2a10274c1b482db17713bbdb8b699b579be
jsteemann <jsteemann@users.noreply.github.com>
2023-09-26 18:15:42 +0200

    make {Plain,Transactional}Cache::freeMemoryWhile() cheaper
    the previous implementation of the function called `getBucket()` for
    each slot it looked at. `getBucket()` then acquired a shared pointer on
    the underlying table, which is not lock-free on x86_64.
    instead, the calls to `Cache::table()` are guarded via a
    pthread_mutex_lock and _unlock, which has prohibitive costs when called
    too often.
    the code is now changed so that there is a `getBucket()` overload which
    gets the Table pointer from the outside, avoiding repeated calls to
    acquire the Table pointer from `freeMemoryWhile()`.

--- arangod/Cache/PlainCache.cpp
+++ arangod/Cache/PlainCache.cpp
@@ -247,6 +247,12 @@ bool PlainCache<Hasher>::freeMemoryWhile(
   for (std::size_t i = 0; i < n; ++i) {
     std::uint64_t index = (offset + i) % n;
 
+    // we can do a lot of iterations from here. don't check for
+    // shutdown in every iteration, but only in every 1000th.
+    if (index % 1024 == 0 && ADB_UNLIKELY(isShutdown())) {
+      break;
+    }
+
     auto [status, guard] = getBucket(Table::BucketId{index}, Cache::triesFast,
                                      /*singleOperation*/ false);
 
@@ -346,14 +352,26 @@ std::pair<::ErrorCode, Table::BucketLocker> PlainCache<Hasher>::getBucket(
   if (ADB_UNLIKELY(isShutdown() || table == nullptr)) {
     status = TRI_ERROR_SHUTTING_DOWN;
   } else {
-    if (singleOperation) {
-      _manager->reportAccess(_id);
-    }
+    std::tie(status, guard) =
+        getBucket(table.get(), bucket, maxTries, singleOperation);
+  }
 
-    guard = table->fetchAndLockBucket(bucket, maxTries);
-    if (!guard.isLocked()) {
-      status = TRI_ERROR_LOCK_TIMEOUT;
-    }
+  return std::make_pair(status, std::move(guard));
+}
+
+template<typename Hasher>
+std::pair<::ErrorCode, Table::BucketLocker> PlainCache<Hasher>::getBucket(
+    Table* table, Table::HashOrId bucket, std::uint64_t maxTries,
+    bool singleOperation) {
+  ::ErrorCode status = TRI_ERROR_NO_ERROR;
+
+  if (singleOperation) {
+    _manager->reportAccess(_id);
+  }
+
+  Table::BucketLocker guard = table->fetchAndLockBucket(bucket, maxTries);
+  if (!guard.isLocked()) {
+    status = TRI_ERROR_LOCK_TIMEOUT;
   }
 
   return std::make_pair(status, std::move(guard));
