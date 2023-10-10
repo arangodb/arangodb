1823f4915b204055a596e9417c2eec5d76ffe9da
Michael Hackstein <michael@arangodb.com>
2023-10-10 19:39:00 +0200

    Added a generic way and test to test different levels of subquery nesting

--- tests/Aql/ShadowRowForwardingTest.cpp
+++ tests/Aql/ShadowRowForwardingTest.cpp
@@ -23,6 +23,7 @@
 #include <gtest/gtest.h>
 
 #include "Aql/AqlExecutorTestCase.h"
+#include "Aql/TestLambdaExecutor.h"
 
 #include "Aql/FilterExecutor.h"
 #include "Aql/SubqueryStartExecutor.h"
@@ -33,6 +34,12 @@ using namespace arangodb::tests;
 using namespace arangodb::tests::aql;
 
 namespace {
+
+// Static strings to use as datainput for test blocks.
+static std::vector<std::string> const rowContent{
+    R"("data row")", R"("shadow row depth 1")", R"("shadow row depth 2")",
+    R"("shadow row depth 3")", R"("shadow row depth 4")"};
+
 RegisterInfos MakeBaseInfos(RegisterCount numRegs, size_t subqueryDepth = 2) {
   RegIdSet prototype{};
   for (RegisterId::value_t r = 0; r < numRegs; ++r) {
@@ -45,7 +52,35 @@ RegisterInfos MakeBaseInfos(RegisterCount numRegs, size_t subqueryDepth = 2) {
   return RegisterInfos({}, {}, numRegs, numRegs, {}, regsToKeep);
 }
 
+auto createProduceCall() -> ProduceCall {
+  return [](AqlItemBlockInputRange& input, OutputAqlItemRow& output)
+             -> std::tuple<ExecutorState, NoStats, AqlCall> {
+    while (input.hasDataRow() && !output.isFull()) {
+      auto const [state, row] = input.nextDataRow();
+      output.cloneValueInto(1, row, AqlValue("foo"));
+      output.advanceRow();
+    }
+    NoStats stats{};
+    AqlCall call{};
+
+    return {input.upstreamState(), stats, call};
+  };
+};
+
+auto createSkipCall() -> SkipCall {
+  return [](AqlItemBlockInputRange& input, AqlCall& call)
+             -> std::tuple<ExecutorState, NoStats, size_t, AqlCall> {
+    TRI_ASSERT(false);
+    THROW_ARANGO_EXCEPTION(TRI_ERROR_NOT_IMPLEMENTED);
+  };
+};
+
+LambdaSkipExecutorInfos MakeNonPassThroughInfos() {
+  return LambdaSkipExecutorInfos{createProduceCall(), createSkipCall()};
+}
+
 auto generateCallStack(size_t nestedSubqueryLevels, size_t indexWithoutContinueCall) -> AqlCallStack {
+  TRI_ASSERT(indexWithoutContinueCall < nestedSubqueryLevels);
   // MainQuery never has continue call
   AqlCallStack stack{AqlCallList{AqlCall{}}};
   for (size_t i = 0; i < nestedSubqueryLevels; ++i) {
@@ -56,6 +91,69 @@ auto generateCallStack(size_t nestedSubqueryLevels, size_t indexWithoutContinueC
     }
   }
   return stack;
+}
+
+struct DataBlockInput {
+  MatrixBuilder<1> data;
+  std::vector<std::pair<size_t, uint64_t>> shadowRows;
+  SkipResult skip;
+};
+
+/*
+ * This will generate a Matrix of data rows with a single column.
+ * The matrix will always follow the pattern:
+ * DataRow
+ * D1 Subquery
+ * D2 Subquery
+ * ...
+ * Dn Subquery
+ * DataRow
+ * D1 Subquery
+ * D2 Subquery
+ * ...
+ * Dn Subquery
+ *
+ * So we have a DataRow followed by all higher level Subqueries, repeated 2 times.
+ * with skipFrontRows we can skip rows from the beginning of the matrix.
+ * (At most nestedSubqueryLevels many rows.)
+ * We can limit the amount of returned rows by setting the rowLimit.
+ * rowLimit == 0 means no limit, otherwise we write exactly that many rows
+ */
+auto generateOutputRowData(size_t nestedSubqueryLevels, size_t skipFrontRows, size_t rowLimit)
+    -> DataBlockInput {
+  // Add the "data row"
+  size_t numRows = nestedSubqueryLevels + 1;
+  TRI_ASSERT(numRows <= rowContent.size())
+      << "If this ASSERT kicks in just add value for more shadow rows to the "
+         "static list of values.";
+  TRI_ASSERT(skipFrontRows < numRows);
+  MatrixBuilder<1> data{};
+  std::vector<std::pair<size_t, uint64_t>> shadowRows{};
+  // We start at skipFrontRows to skip the first ones
+  for (size_t i = skipFrontRows; i < numRows * 2; ++i) {
+    auto index = i % numRows;
+    data.emplace_back(RowBuilder<1>{rowContent[index].data()});
+    if (index > 0) {
+      shadowRows.emplace_back(std::make_pair(i - skipFrontRows, index - 1));
+    }
+    if (rowLimit > 0) {
+      rowLimit--;
+      if (rowLimit == 0) {
+        break;
+      }
+    }
+  }
+  SkipResult skipResult;
+  for (size_t i = 0; i < nestedSubqueryLevels; ++i) {
+    skipResult.incrementSubquery();
+  }
+  return DataBlockInput{std::move(data), std::move(shadowRows),
+                        std::move(skipResult)};
+}
+
+auto generateInputRowData(size_t nestedSubqueryLevels, size_t skipFrontRows)
+    -> DataBlockInput {
+  return generateOutputRowData(nestedSubqueryLevels, skipFrontRows, 0);
 }
 
 }  // namespace
@@ -77,24 +175,15 @@ TEST_F(ShadowRowForwardingTest, subqueryStart1) {
   auto const test = [&](SplitType splitType) {
     SCOPED_TRACE("with split type " + to_string(splitType));
 
+    auto inputVal = generateInputRowData(2, 2);
     makeExecutorTestHelper<1, 1>()
         .addConsumer<SubqueryStartExecutor>(MakeBaseInfos(1, 3),
                                             MakeBaseInfos(1, 3),
                                             ExecutionNode::SUBQUERY_START)
         .setInputSubqueryDepth(2)
         .setInputValue(
-            {
-                {R"("outer shadow row")"},
-                {R"("data row")"},
-                {R"("inner shadow row")"},
-                {R"("outer shadow row")"},
-            },
-            {
-                {0, 1},
-                // {1, data row}
-                {2, 0},
-                {3, 1},
-            })
+            std::move(inputVal.data),
+            std::move(inputVal.shadowRows))
         .setInputSplitType(splitType)
         .setCallStack(generateCallStack(3, 1))
         .expectedStats(ExecutionStats{})
@@ -102,7 +191,7 @@ TEST_F(ShadowRowForwardingTest, subqueryStart1) {
         .expectOutput(
             {0},
             {
-                {R"("outer shadow row")"},
+                {rowContent[2].data()},
                 // {R"("data row")"},
                 // {R"("data row")"},  // this is now a relevant shadow row
                 // {R"("inner shadow row")"},
@@ -171,4 +260,41 @@ TEST_F(ShadowRowForwardingTest, subqueryEnd1) {
   test(std::monostate{});
   test(std::size_t{1});
   test(std::vector<std::size_t>{1, 3});
+};
+
+TEST_F(ShadowRowForwardingTest, nonForwardingExecutor) {
+  auto const test = [&](SplitType splitType, size_t nestedSubqueryLevels, size_t skippedRowsAtFront, size_t indexWithoutContinueCall) {
+    SCOPED_TRACE("with split type " + to_string(splitType) +
+                 " subqueryLevels: " + std::to_string(nestedSubqueryLevels) +
+                 " skippedRowsAtFront: " + std::to_string(skippedRowsAtFront) +
+                 " indexWithoutContinueCall: " +
+                 std::to_string(indexWithoutContinueCall));
+
+    auto inputVal = generateInputRowData(nestedSubqueryLevels, skippedRowsAtFront);
+    auto outputVal = generateOutputRowData(nestedSubqueryLevels, skippedRowsAtFront, 1);
+    makeExecutorTestHelper<1, 1>()
+        .addConsumer<TestLambdaSkipExecutor>(MakeBaseInfos(1, nestedSubqueryLevels),
+                                             MakeNonPassThroughInfos(),
+                                          ExecutionNode::CALCULATION)
+        .setInputSubqueryDepth(nestedSubqueryLevels)
+        .setInputValue(std::move(inputVal.data), std::move(inputVal.shadowRows))
+        .setInputSplitType(splitType)
+        .setCallStack(generateCallStack(nestedSubqueryLevels, indexWithoutContinueCall))
+        .expectedStats(ExecutionStats{})
+        .expectedState(ExecutionState::HASMORE)
+        .expectOutput(
+            {0},
+            std::move(outputVal.data),
+            std::move(outputVal.shadowRows))
+        .expectSkipped(std::move(outputVal.skip))
+        .run();
+  };
+
+  for (size_t nestedSubqueries = 2; nestedSubqueries < 5; ++nestedSubqueries) {
+    size_t skippedRowsAtFront = nestedSubqueries - 0;
+    size_t callWithoutContinue = nestedSubqueries - 1;
+    test(std::monostate{}, nestedSubqueries, skippedRowsAtFront, callWithoutContinue);
+    test(std::size_t{1}, nestedSubqueries, skippedRowsAtFront, callWithoutContinue);
+    test(std::vector<std::size_t>{1, 3}, nestedSubqueries, skippedRowsAtFront, callWithoutContinue);
+  }
 };
