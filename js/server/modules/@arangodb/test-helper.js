2b411b3d1b503db40dba68653ac7218f10240989
Alexey Bakharew <alex.bakharew@gmail.com>
2023-10-11 16:57:20 +0200

    Trying to fix CI

--- js/server/modules/@arangodb/test-helper.js
+++ js/server/modules/@arangodb/test-helper.js
@@ -28,47 +28,194 @@
 // / @author Copyright 2011-2012, triAGENS GmbH, Cologne, Germany
 // //////////////////////////////////////////////////////////////////////////////
 
-// const internal = require('internal'); // OK: processCsvFile
-// const request = require('@arangodb/request');
-// const {
-//   runWithRetry,
-//   getServerById,
-//   getServersByType,
-//   getEndpointById,
-//   getEndpointsByType,
-//   helper,
-//   deriveTestSuite,
-//   deriveTestSuiteWithnamespace,
-//   typeName,
-//   isEqual,
-//   compareStringIds,
-//   endpointToURL,
-//   versionHas,
-//   isEnterprise,
-// } = require('@arangodb/test-helper-common');
-// const clusterInfo = global.ArangoClusterInfo;
+const internal = require('internal'); // OK: processCsvFile
+const request = require('@arangodb/request');
+const {
+  runWithRetry,
+  getServerById,
+  getServersByType,
+  getEndpointById,
+  getEndpointsByType,
+  helper,
+  deriveTestSuite,
+  deriveTestSuiteWithnamespace,
+  typeName,
+  isEqual,
+  compareStringIds,
+  endpointToURL,
+  versionHas,
+  isEnterprise,
+} = require('@arangodb/test-helper-common');
+const clusterInfo = global.ArangoClusterInfo;
 
-// exports.runWithRetry = runWithRetry;
-// exports.isEnterprise = isEnterprise;
-// exports.versionHas = versionHas;
-// exports.getServerById = getServerById;
-// exports.getServersByType = getServersByType;
-// exports.getEndpointById = getEndpointById;
-// exports.getEndpointsByType = getEndpointsByType;
-// exports.helper = helper;
-// exports.deriveTestSuite = deriveTestSuite;
-// exports.deriveTestSuiteWithnamespace = deriveTestSuiteWithnamespace;
-// exports.typeName = typeName;
-// exports.isEqual = isEqual;
-// exports.compareStringIds = compareStringIds;
+exports.runWithRetry = runWithRetry;
+exports.isEnterprise = isEnterprise;
+exports.versionHas = versionHas;
+exports.getServerById = getServerById;
+exports.getServersByType = getServersByType;
+exports.getEndpointById = getEndpointById;
+exports.getEndpointsByType = getEndpointsByType;
+exports.helper = helper;
+exports.deriveTestSuite = deriveTestSuite;
+exports.deriveTestSuiteWithnamespace = deriveTestSuiteWithnamespace;
+exports.typeName = typeName;
+exports.isEqual = isEqual;
+exports.compareStringIds = compareStringIds;
 
+/// @brief set failure point
+exports.debugCanUseFailAt = function (endpoint) {
+  let res = request.get({
+    url: endpoint + '/_admin/debug/failat',
+  });
+  return res.status === 200;
+};
 
+/// @brief set failure point
+exports.debugSetFailAt = function (endpoint, failAt) {
+  let res = request.put({
+    url: endpoint + '/_admin/debug/failat/' + failAt,
+    body: ""
+  });
+  if (res.status !== 200) {
+    throw "Error setting failure point";
+  }
+};
 
+exports.debugResetRaceControl = function (endpoint) {
+  let res = request.delete({
+    url: endpoint + '/_admin/debug/raceControl',
+    body: ""
+  });
+  if (res.status !== 200) {
+    throw "Error resetting race control.";
+  }
+};
 
+exports.debugRemoveFailAt = function (endpoint, failAt) {
+  let res = request.delete({
+    url: endpoint + '/_admin/debug/failat/' + failAt,
+    body: ""
+  });
+  if (res.status !== 200) {
+    throw "Error removing failure point";
+  }
+};
 
+exports.debugClearFailAt = function (endpoint) {
+  let res = request.delete({
+    url: endpoint + '/_admin/debug/failat',
+    body: ""
+  });
+  if (res.status !== 200) {
+    throw "Error removing failure points";
+  }
+};
 
+exports.getChecksum = function (endpoint, name) {
+  let res = request.get({
+    url: endpoint + '/_api/collection/' + name + '/checksum',
+  });
+  return JSON.parse(res.body).checksum;
+};
 
+function getMetricName(text, name) {
+  let re = new RegExp("^" + name);
+  let matches = text.split("\n").filter((line) => !line.match(/^#/)).filter((line) => line.match(re));
+  if (!matches.length) {
+    throw "Metric " + name + " not found";
+  }
+  return Number(matches[0].replace(/^.*{.*}\s*([0-9.]+)$/, "$1"));
+}
 
+exports.getMetric = function (endpoint, name) {
+  let res = request.get({
+    url: endpoint + "/_admin/metrics",
+  });
+  return getMetricName(res.body, name);
+};
 
-// exports.uniqid = global.ArangoClusterInfo.uniqid;
-// exports.agency = global.ArangoAgency;
+exports.getMetricSingle = function (name) {
+  let res = request.get("/_admin/metrics");
+  if (res.status !== 200) {
+    throw "error fetching metric";
+  }
+  return getMetricName(res.body, name);
+};
+
+exports.waitForShardsInSync = function(cn, timeout) {
+  if (!timeout) {
+    timeout = 300;
+  }
+  // The client variant has the database name inside it's scope.
+  // TODO: Need to figure out how to get that done on server api. As soon as
+  // this is necessary. We can always hand it in as parameter, but then we have
+  // differences for client / server on this helper methods api ;(
+  let dbName = "_system";
+  let start = internal.time();
+  const setsAreEqual = (left, right) => {
+    // Sets have to be equal in size
+    if (left.size !== right.size) {
+      return false;
+    }
+    // Every entry in left, needs to be in right.
+    // THere can be no duplicates in a set, so this
+    // is equivalent to left and right having exactly the
+    // same entries.
+    for (const entry of left) {
+      if (!right.has(entry)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  while (internal.time() - start < timeout) {
+    let insync = 0;
+    const { shards } = clusterInfo.getCollectionInfo(dbName, cn);
+    for (const [shard, plannedServers] of Object.entries(shards)) {
+      const { servers } = clusterInfo.getCollectionInfoCurrent(dbName, cn, shard);
+      if (setsAreEqual(new Set(servers), new Set(plannedServers))) {
+        ++insync;
+      }
+    }
+    if (insync === Object.keys(shards).length) {
+      return;
+    }
+    if ((internal.time() - start) * 2 > timeout) {
+      console.warn(`Only ${insync} in-sync out of ${Object.keys(shards).length} after ${internal.time() - start}, waiting until ${timeout}`);
+    }
+    internal.wait(1);
+  }
+  assertTrue(false, "Shards were not getting in sync in time, giving up!");
+};
+
+exports.getCoordinators = function () {
+  // Note that the client implementation has more information, not all of which
+  // we have available.
+  return global.ArangoClusterInfo.getCoordinators().map(id => ({id}));
+};
+
+exports.getDBServers = function() {
+  // Note that the client implementation has more information, not all of which
+  // we have available.
+  return global.ArangoClusterInfo.getDBServers().map(x => ({id: x.serverId}));
+};
+
+exports.triggerMetrics = function () {
+  request({
+    method: "get",
+    url: "/_db/_system/_admin/metrics?mode=write_global",
+    headers: {accept: "application/json"},
+    body: {}
+  });
+  request({
+    method: "get",
+    url: "/_db/_system/_admin/metrics?mode=trigger_global",
+    headers: {accept: "application/json"},
+    body: {}
+  });
+  require("internal").sleep(2);
+};
+
+exports.uniqid = global.ArangoClusterInfo.uniqid;
+exports.agency = global.ArangoAgency;
