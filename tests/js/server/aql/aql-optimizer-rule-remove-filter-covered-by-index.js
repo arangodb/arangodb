4fc74b1978bed13a19638fff61cddcc52040cca0
Lars Maier <lars@arangodb.com>
2023-10-16 16:50:57 +0200

    Fixing more tests.

--- tests/js/server/aql/aql-optimizer-rule-remove-filter-covered-by-index.js
+++ tests/js/server/aql/aql-optimizer-rule-remove-filter-covered-by-index.js
@@ -227,7 +227,7 @@ function optimizerRuleTestSuite() {
       query = "FOR v IN " + colName + " FILTER 'foo' IN v.z && 'bar' IN v.z RETURN v";
       result = AQL_EXPLAIN(query);
       // should optimize away one part of the filter
-      assertEqual([ "remove-filter-covered-by-index", "use-indexes", "move-filters-into-enumerate" ].sort(),  
+      assertEqual([ "remove-filter-covered-by-index", "use-indexes", "move-filters-into-enumerate", "remove-unnecessary-projections" ].sort(),
         removeAlwaysOnClusterRules(result.plan.rules.sort()), query);
       hasIndexNodeWithRanges(result);
       
@@ -241,7 +241,7 @@ function optimizerRuleTestSuite() {
       query = "FOR v IN " + colName + " FILTER 'foo' IN v.z[*] && 'bar' IN v.z[*] RETURN v";
       result = AQL_EXPLAIN(query);
       // should optimize away one part of the filter
-      assertEqual([ "remove-filter-covered-by-index", "use-indexes", "move-filters-into-enumerate" ].sort(),  
+      assertEqual([ "remove-filter-covered-by-index", "use-indexes", "move-filters-into-enumerate", "remove-unnecessary-projections" ].sort(),
         removeAlwaysOnClusterRules(result.plan.rules.sort()), query);
       hasIndexNodeWithRanges(result);
       
