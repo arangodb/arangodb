6e8cd55787688ec25c972c233d8e4809a23351c8
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-10-17 15:20:03 +0700

    chore: fix prop

--- js/apps/system/_admin/aardvark/APP/react/src/views/collections/indices/listIndices/CollectionIndicesTable.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/collections/indices/listIndices/CollectionIndicesTable.tsx
@@ -58,7 +58,7 @@ const TABLE_COLUMNS = [
     header: "Actions",
     id: "actions",
     cell: info => {
-      return <CollectionIndexActionButtons index={info.row.original} />;
+      return <CollectionIndexActionButtons collectionIndex={info.row.original} />;
     }
   })
 ];
