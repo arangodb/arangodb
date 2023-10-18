0e75bd43476372add9d3142f47e1a69e50a7ec01
jsteemann <jsteemann@users.noreply.github.com>
2023-10-18 16:55:38 +0200

    remove unreachable test code

--- tests/js/client/aql/aql-arangosearch-inverted-index-optimization.js
+++ tests/js/client/aql/aql-arangosearch-inverted-index-optimization.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false */
-/*global assertEqual, assertNotEqual, assertTrue, print, AQL_EXPLAIN */
+/*global assertEqual, assertNotEqual, assertTrue */
 
 ////////////////////////////////////////////////////////////////////////////////
 ///
@@ -25,7 +25,6 @@
 const jsunity = require("jsunity");
 const arangodb = require("@arangodb");
 const db = arangodb.db;
-const isServer = arangodb.isServer;
 
 function testOptimizeFilterCondition() {
   const dbName = "OptimizeFilterCondition";
@@ -65,24 +64,6 @@ function testOptimizeFilterCondition() {
           RETURN [contribution, d]`;
       let docs = db._query(query).toArray();
       assertEqual(docs.length, 5);
-
-      if (isServer) {
-        let plan = AQL_EXPLAIN(query);
-        plan = require("@arangodb/aql-helper").getCompactPlan(plan);
-        let hasInRangeCalculationNode = false;
-        let hasFilter = false;
-        for (let node of plan) {
-          if (!hasInRangeCalculationNode) {
-            if (node.type === "CalculationNode" && node.expression.includes("IN_RANGE")) {
-              hasInRangeCalculationNode = true;
-            }
-          } else if (node.type === "FilterNode") {
-            hasFilter = true;
-          }
-        }
-        assertTrue(hasInRangeCalculationNode);
-        assertTrue(hasFilter);  
-      }
     },
 
     testBTS1495_earlyPrunning: function() {
@@ -104,19 +85,6 @@ function testOptimizeFilterCondition() {
           RETURN [productLocationId, maxCumulative]`;
       let docs = db._query(query).toArray();
       assertEqual(docs.length, 1);
-
-      if (isServer) {
-        let plan = AQL_EXPLAIN(query);
-        plan = require("@arangodb/aql-helper").getLinearizedPlan(plan);
-
-        let hasFilter = false;
-        for (let node of plan) {
-          if (node.type === "IndexNode" && node.filter !== undefined && node.filter.name === "IN_RANGE") {
-            hasFilter = true;
-          }
-        }
-        assertTrue(hasFilter);
-      }
     }
   };
 }
