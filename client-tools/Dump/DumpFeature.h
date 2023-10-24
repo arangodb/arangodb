f16273395a38ec019e099a1fa337be877b7550b0
jsteemann <jsteemann@users.noreply.github.com>
2023-10-24 16:42:45 +0200

    enable parallel dump by default

--- client-tools/Dump/DumpFeature.h
+++ client-tools/Dump/DumpFeature.h
@@ -83,7 +83,7 @@ class DumpFeature final : public ArangoDumpFeature {
     bool useGzipForStorage{true};
     bool useGzipForTransport{false};
     bool useVPack{false};
-    bool useParalleDump{false};
+    bool useParalleDump{true};
     bool splitFiles{false};
     std::uint64_t dbserverWorkerThreads{5};
     std::uint64_t dbserverPrefetchBatches{5};
