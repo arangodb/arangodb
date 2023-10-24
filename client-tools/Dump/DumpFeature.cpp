c5089412ef42d0f8148f9703fc9a3264c5edbf50
Heiko <hkernbach@users.noreply.github.com>
2023-10-24 17:29:04 +0200

    Update client-tools/Dump/DumpFeature.cpp
    Co-authored-by: Jan <jsteemann@users.noreply.github.com>

--- client-tools/Dump/DumpFeature.cpp
+++ client-tools/Dump/DumpFeature.cpp
@@ -810,7 +810,8 @@ void DumpFeature::collectOptions(
       "--ignore-collection",
       "Ignore and exclude this collection during the dump process (can be "
       "specified multiple times).",
-      new VectorParameter<StringParameter>(&_options.collectionsToBeIgnored));
+      new VectorParameter<StringParameter>(&_options.collectionsToBeIgnored))
+      .setIntroducedIn(31200);
 
   options
       ->addOption(
