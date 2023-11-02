5ad84242db0e91fe9c1bdd8a72b59e4e20d037a8
Max Neunhöffer <max@arangodb.com>
2023-11-02 07:59:25 +0100

    Deal with race in leader change [BTS-1647][BTS-1658] (#19996)
    * Cleanup _theLeader in case of error during SynchronizeShard.
    * CHANGELOG.
    * Add files for global serialization.
    * Fix second bug of unnecessarily dropped follower.
    * Fix changelog.
    * Suggestions by reviewers.
    * Add a dire warning.
    * Revert header guard.
    * Allow O_APPEND in TRI_OPEN_WIN32.
    * CONTRIBUTING.

--- arangod/Cluster/SynchronizeShard.cpp
+++ arangod/Cluster/SynchronizeShard.cpp
@@ -26,6 +26,7 @@
 
 #include "Agency/AgencyStrings.h"
 #include "ApplicationFeatures/ApplicationServer.h"
+#include "Basics/GlobalSerialization.h"
 #include "Basics/ScopeGuard.h"
 #include "Basics/StringUtils.h"
 #include "Basics/TimeString.h"
@@ -39,6 +40,7 @@
 #include "Cluster/FollowerInfo.h"
 #include "Cluster/Maintenance.h"
 #include "Cluster/MaintenanceFeature.h"
+#include "Cluster/ResignShardLeadership.h"
 #include "Cluster/ReplicationTimeoutFeature.h"
 #include "Cluster/ServerState.h"
 #include "GeneralServer/AuthenticationFeature.h"
@@ -907,6 +909,14 @@ bool SynchronizeShard::first() {
       << "SynchronizeShard: synchronizing shard '" << database << "/" << shard
       << "' for central '" << database << "/" << planId << "'";
 
+  TRI_IF_FAILURE("SynchronizeShard::beginning") {
+    std::string shortName = ServerState::instance()->getShortName();
+    waitForGlobalEvent("SynchronizeShard::beginning",
+                       absl::StrCat(shortName, ":", shard));
+    waitForGlobalEvent("SynchronizeShard::beginning2",
+                       absl::StrCat(shortName, ":", shard));
+  }
+
   auto& clusterInfo =
       _feature.server().getFeature<ClusterFeature>().clusterInfo();
   auto const ourselves = arangodb::ServerState::instance()->getId();
@@ -1149,9 +1159,31 @@ bool SynchronizeShard::first() {
         config.add("verbose", VPackValue(false));
       }
 
+      TRI_IF_FAILURE("SynchronizeShard::beforeSetTheLeader") {
+        std::string shortName = ServerState::instance()->getShortName();
+        waitForGlobalEvent("SynchronizeShard::beforeSetTheLeader",
+                           absl::StrCat(shortName, ":", shard));
+      }
       // Configure the shard to follow the leader without any following
-      // term id:
+      // term id, this is necessary, such that no replication requests
+      // from synchronous replication can interfere with the initial
+      // synchronization, regardless of whether they come from an old
+      // leader or from the right leader with whom we want to synchronize.
+      // Note that it is possible that the proper leader thinks that we
+      // are in sync, but we still run this SynchronizeShard job, simply
+      // because it was scheduled earlier:
       collection->followers()->setTheLeader(leader);
+
+      // If we fail to get in sync with this task, then we want to make
+      // sure that the leader is reset to a known value, such that the
+      // Maintenance will trigger another SynchronizeShard job eventually:
+      ScopeGuard rollbackTheLeader([&]() noexcept {
+        collection->followers()->setTheLeader(
+            ResignShardLeadership::LeaderNotYetKnownString);
+        LOG_TOPIC("ca777", INFO, Logger::MAINTENANCE)
+            << "SynchronizeShard failed for shard " << collection->name()
+            << ", resetting shard leader to trigger new run.";
+      });
 
       startTime = std::chrono::system_clock::now();
 
@@ -1163,12 +1195,24 @@ bool SynchronizeShard::first() {
       auto const endTime = std::chrono::system_clock::now();
 
       // Long shard sync initialization
-      if (endTime - startTime > seconds(5)) {
+      if (endTime - startTime > seconds(15)) {
         LOG_TOPIC("ca7e3", INFO, Logger::MAINTENANCE)
-            << "synchronizeOneShard: long call to syncCollection for shard"
+            << "synchronizeOneShard: call to syncCollection for shard"
             << database << "/" << shard << " " << syncRes.errorMessage()
             << " start time: " << timepointToString(startTime)
-            << ", end time: " << timepointToString(endTime);
+            << ", end time: " << timepointToString(endTime) << ", duration: "
+            << std::chrono::duration_cast<std::chrono::seconds>(endTime -
+                                                                startTime)
+                   .count()
+            << " seconds.";
+      }
+
+      TRI_IF_FAILURE("SynchronizeShard::fail") {
+        LOG_TOPIC("ca778", INFO, Logger::MAINTENANCE)
+            << "SynchronizeShard failed for shard " << collection->name()
+            << " because of a failure point.";
+        result(TRI_ERROR_FAILED, "Failure point");
+        return false;
       }
 
       // If this did not work, then we cannot go on:
@@ -1185,7 +1229,7 @@ bool SynchronizeShard::first() {
         std::stringstream error;
         error << "could not initially synchronize shard " << database << "/"
               << shard << ": " << syncRes.errorMessage();
-        LOG_TOPIC("c1b31", DEBUG, Logger::MAINTENANCE)
+        LOG_TOPIC("c1b31", INFO, Logger::MAINTENANCE)
             << "SynchronizeOneShard: " << error.str();
 
         result(syncRes.errorNumber(), error.str());
@@ -1204,7 +1248,7 @@ bool SynchronizeShard::first() {
             "can happen if a collection was dropped during synchronization!");
         LOG_TOPIC("664ae", WARN, Logger::MAINTENANCE)
             << "SynchronizeOneShard: " << error;
-        result(TRI_ERROR_INTERNAL);
+        result(TRI_ERROR_INTERNAL, error);
         return false;
       }
 
@@ -1254,6 +1298,12 @@ bool SynchronizeShard::first() {
         result(res);
         return false;
       }
+
+      // Now that we have set the correct leader with the correct
+      // FollowerTermInfo, we must stop the rollbackTheLeader scope
+      // guard from wasting everything:
+      rollbackTheLeader.cancel();
+
     } catch (basics::Exception const& e) {
       // don't log errors for already dropped databases/collections
       if (e.code() != TRI_ERROR_ARANGO_DATABASE_NOT_FOUND &&
