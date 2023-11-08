d3b669f6e737189bbc2c69bca9c1300cdb15a850
Michael Hackstein <michael@arangodb.com>
2023-11-08 09:25:39 +0100

    Improve locking in Async Logger (#19989)

--- lib/Logger/LoggerFeature.cpp
+++ lib/Logger/LoggerFeature.cpp
@@ -556,6 +556,21 @@ printed in the thread that triggered the log message. This is non-optimal for
 performance but can aid debugging. If set to `false`, log messages are handed
 off to an extra logging thread, which asynchronously writes the log messages.)");
 
+  options
+      ->addOption(
+          "--log.max-queued-entries",
+          "Upper limit of log entries that are queued in background thread.",
+          new UInt32Parameter(&_maxQueuedLogMessages),
+          arangodb::options::makeDefaultFlags(
+              arangodb::options::Flags::Uncommon))
+      .setIntroducedIn(31200)
+      .setLongDescription(
+          R"(If you are using the option `--log.force-direct` (default)
+log entries are pushed on a queue for asynchronous writing. In case you
+are using a slow log output (e.g. syslog) the queue might grow and eventually overflow.
+This option allows to configure the upper bound. If the queue is full, log entries
+will be written synchronously, until the queue has space again.)");
+
   options->addOption(
       "--log.request-parameters",
       "include full URLs and HTTP request parameters in trace logs",
@@ -630,7 +645,7 @@ void LoggerFeature::validateOptions(std::shared_ptr<ProgramOptions> options) {
   if (!_fileMode.empty()) {
     try {
       int result = std::stoi(_fileMode, nullptr, 8);
-      LogAppenderFile::setFileMode(result);
+      LogAppenderFileFactory::setFileMode(result);
     } catch (...) {
       LOG_TOPIC("797c2", FATAL, arangodb::Logger::FIXME)
           << "expecting an octal number for log.file-mode, got '" << _fileMode
@@ -674,7 +689,7 @@ void LoggerFeature::validateOptions(std::shared_ptr<ProgramOptions> options) {
 #endif
     }
 
-    LogAppenderFile::setFileGroup(gidNumber);
+    LogAppenderFileFactory::setFileGroup(gidNumber);
   }
 #endif
 
@@ -731,9 +746,9 @@ void LoggerFeature::prepare() {
   }
 
   if (_forceDirect || _supervisor) {
-    Logger::initialize(server(), false);
+    Logger::initialize(server(), false, _maxQueuedLogMessages);
   } else {
-    Logger::initialize(server(), _threaded);
+    Logger::initialize(server(), _threaded, _maxQueuedLogMessages);
   }
 }
 
