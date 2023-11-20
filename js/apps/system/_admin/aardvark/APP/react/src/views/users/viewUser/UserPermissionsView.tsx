d46763739124bae2008b72ae20b709d3db9ac18d
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-11-20 18:42:47 +0530

    fix: top nav

--- js/apps/system/_admin/aardvark/APP/react/src/views/users/viewUser/UserPermissionsView.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/users/viewUser/UserPermissionsView.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ChakraCustomProvider } from "../../../theme/ChakraCustomProvider";
 import { useGlobalStyleReset } from "../../../utils/useGlobalStyleReset";
+import { useUsername } from "./useFetchDatabasePermissions";
 import { UserPermissionsTable } from "./UserPermissionsTable";
+
+const useSetupUserPermissionsNav = () => {
+  const { username } = useUsername();
+
+  useEffect(() => {
+    const breadcrumbDiv = document.querySelector(
+      "#subNavigationBar .breadcrumb"
+    );
+    breadcrumbDiv?.append(`User: ${username}`);
+    window.arangoHelper.buildUserSubNav(username, "Permissions");
+  }, [username]);
+};
 
 export const UserPermissionsView = () => {
   useGlobalStyleReset();
+  useSetupUserPermissionsNav();
   return (
     <ChakraCustomProvider>
       <UserPermissionsTable />
