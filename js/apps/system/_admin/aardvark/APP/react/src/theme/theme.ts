8bd916b0c21d71e2b1fa1082bf477e3c3dea9137
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-11-20 16:25:19 +0530

    feat: change more colors, adjust navbar styles

--- js/apps/system/_admin/aardvark/APP/react/src/theme/theme.ts
+++ js/apps/system/_admin/aardvark/APP/react/src/theme/theme.ts
@@ -12,16 +12,17 @@ export const theme = extendTheme({
   },
   colors: {
     gray: {
-      50: "#fafafa",
-      100: "#f5f5f5",
-      200: "#eeeeee",
-      300: "#e0e0e0",
-      400: "#bdbdbd",
-      500: "#9e9e9e",
-      600: "#757575",
-      700: "#616161",
-      800: "#424242",
-      900: "#212121"
+      50: "#f8f8f8",
+      100: "#f0f0f0",
+      200: "#e5e5e5",
+      300: "#d1d1d1",
+      400: "#b4b4b4",
+      500: "#9a9a9a",
+      600: "#818181",
+      700: "#6a6a6a",
+      800: "#5a5a5a",
+      900: "#4e4e4e",
+      950: "#282828"
     }
   },
   components: {
@@ -49,6 +50,13 @@ export const theme = extendTheme({
             my: 0
           }
         }
+      }
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        color: "gray.950"
       }
     }
   }
