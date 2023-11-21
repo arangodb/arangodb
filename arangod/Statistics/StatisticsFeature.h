3eaefabce1116ae926850de7b8483cb96aa7ae3c
jsteemann <jsteemann@users.noreply.github.com>
2023-11-21 17:41:53 +0100

    fix initialization order

--- arangod/Statistics/StatisticsFeature.h
+++ arangod/Statistics/StatisticsFeature.h
@@ -94,7 +94,6 @@ class StatisticsFeature final : public ArangodFeature {
 
   void collectOptions(std::shared_ptr<options::ProgramOptions>) override final;
   void validateOptions(std::shared_ptr<options::ProgramOptions>) override final;
-  void prepare() override final;
   void start() override final;
   void stop() override final;
   void toPrometheus(std::string& result, double now, bool ensureWhitespace);
