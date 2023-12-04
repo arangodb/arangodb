adb344678bd40dc0606461d54a4a26feaf8c5b3d
jsteemann <jsteemann@users.noreply.github.com>
2023-12-04 11:33:37 +0100

    clone traversal conditions instead of referencing them
    otherwise this could lead to races upon value initialization inside the
    conditions when executing them in a parallel traversal

--- arangod/Aql/TraversalNode.cpp
+++ arangod/Aql/TraversalNode.cpp
@@ -1130,12 +1130,12 @@ void TraversalNode::traversalCloneHelper(ExecutionPlan& plan, TraversalNode& c,
   // Filter Condition Parts
   c._fromCondition = _fromCondition->clone(_plan->getAst());
   c._toCondition = _toCondition->clone(_plan->getAst());
-  c._globalEdgeConditions.insert(c._globalEdgeConditions.end(),
-                                 _globalEdgeConditions.begin(),
-                                 _globalEdgeConditions.end());
-  c._globalVertexConditions.insert(c._globalVertexConditions.end(),
-                                   _globalVertexConditions.begin(),
-                                   _globalVertexConditions.end());
+  for (auto const& it : _globalEdgeConditions) {
+    c._globalEdgeConditions.emplace_back(it->clone(_plan->getAst()));
+  }
+  for (auto const& it : _globalVertexConditions) {
+    c._globalVertexConditions.emplace_back(it->clone(_plan->getAst()));
+  }
 
   for (auto const& it : _edgeConditions) {
     // Copy the builder
