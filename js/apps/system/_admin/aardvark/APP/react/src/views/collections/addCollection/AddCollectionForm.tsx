58c862ec9dc2e98a7566412b32715377fde71af7
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2023-12-06 12:18:21 +0530

    chore: split component
    fix switch

--- js/apps/system/_admin/aardvark/APP/react/src/views/collections/addCollection/AddCollectionForm.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/collections/addCollection/AddCollectionForm.tsx
@@ -1,19 +1,17 @@
 import { Divider, Grid, Stack, Text } from "@chakra-ui/react";
-import React from "react";
-import { InputControl } from "../../../components/form/InputControl";
-import { useCollectionsContext } from "../CollectionsContext";
 import { useField } from "formik";
-import { SwitchControl } from "../../../components/form/SwitchControl";
-import { useFetchDatabase } from "../../databases/useFetchDatabase";
-import { getCurrentDB } from "../../../utils/arangoClient";
-import { SelectControl } from "../../../components/form/SelectControl";
+import React from "react";
 import { CreatableMultiSelectControl } from "../../../components/form/CreatableMultiSelectControl";
+import { InputControl } from "../../../components/form/InputControl";
+import { SelectControl } from "../../../components/form/SelectControl";
+import { SwitchControl } from "../../../components/form/SwitchControl";
+import { getCurrentDB } from "../../../utils/arangoClient";
+import { useFetchDatabase } from "../../databases/useFetchDatabase";
+import { useCollectionsContext } from "../CollectionsContext";
 
 export const AddCollectionForm = () => {
   const { database, error } = useFetchDatabase(getCurrentDB().name);
-  const { collections, isFormDisabled: isDisabled } = useCollectionsContext();
-  const [distributeShardsLike] = useField<string>("distributeShardsLike");
-  const [isSatellite] = useField<boolean>("isSatellite");
+  const { isFormDisabled: isDisabled } = useCollectionsContext();
   if (error) {
     window.App.navigate("#collections", { trigger: true });
     window.arangoHelper.arangoError(
@@ -21,7 +19,9 @@ export const AddCollectionForm = () => {
       `Got unexpected server response: ${error.errorNum}: ${error.message}`
     );
   }
-  if (!database) return null;
+  if (!database) {
+    return null;
+  }
   const isOneShard = database.isOneShard || window.frontendConfig.forceOneShard;
   return (
     <Grid templateColumns={"1fr"} gap="6">
@@ -54,100 +54,7 @@ export const AddCollectionForm = () => {
           marginY="4"
           maxWidth="full"
         >
-          {window.App.isCluster && (
-            <>
-              <InputControl
-                isDisabled={
-                  isDisabled ||
-                  isOneShard ||
-                  isSatellite.value ||
-                  distributeShardsLike.value !== ""
-                }
-                name={"numberOfShards"}
-                label="Number of shards"
-                inputProps={{ type: "number" }}
-                tooltip={
-                  isOneShard
-                    ? undefined
-                    : `The number of shards to create. The maximum value is ${window.frontendConfig.maxNumberOfShards}. You cannot change this afterwards.`
-                }
-              />
-              <CreatableMultiSelectControl
-                isDisabled={isDisabled || isSatellite.value}
-                name={"shardKeys"}
-                label="Shard keys"
-                selectProps={{
-                  noOptionsMessage: () => "Start typing to add a key"
-                }}
-                tooltip={"The keys used to distribute documents on shards."}
-              />
-              <InputControl
-                isDisabled={
-                  isDisabled ||
-                  isSatellite.value ||
-                  distributeShardsLike.value !== ""
-                }
-                name={"replicationFactor"}
-                label="Replication Factor"
-                inputProps={{ type: "number" }}
-                tooltip={"Total number of copies of the data in the cluster."}
-              />
-              <InputControl
-                isDisabled={
-                  isDisabled ||
-                  isSatellite.value ||
-                  distributeShardsLike.value !== ""
-                }
-                name={"writeConcern"}
-                label="Write Concern"
-                inputProps={{ type: "number" }}
-                tooltip={
-                  "Total number of copies of the data in the cluster that are required for each write operation. If we get below this value the collection will be read-only until enough copies are created. Must be smaller or equal compared to the replication factor."
-                }
-              />
-              {window.frontendConfig.isEnterprise && (
-                <>
-                  {!isOneShard && (
-                    <>
-                      <SelectControl
-                        isDisabled={isDisabled}
-                        name={"distributeShardsLike"}
-                        label="Distribute shards like"
-                        selectProps={{
-                          options: [
-                            { label: "N/A", value: "" },
-                            ...(collections?.map(collection => ({
-                              label: collection.name,
-                              value: collection.name
-                            })) ?? [])
-                          ]
-                        }}
-                        tooltip={
-                          "Name of another collection that should be used as a prototype for sharding this collection."
-                        }
-                      />
-                      <InputControl
-                        isDisabled={isDisabled || isSatellite.value}
-                        name={"smartJoinAttribute"}
-                        label="SmartJoin attribute"
-                        tooltip={
-                          "String attribute name. Can be left empty if SmartJoins are not used."
-                        }
-                      />
-                    </>
-                  )}
-                  <SwitchControl
-                    isDisabled={isDisabled || distributeShardsLike.value !== ""}
-                    name={"isSatellite"}
-                    label="SatelliteCollection"
-                    tooltip={
-                      "If enabled, every collection in this collection will be replicated to every DB-Server."
-                    }
-                  />
-                </>
-              )}
-            </>
-          )}
+          <ClusterFields isOneShard={isOneShard} />
           <SwitchControl
             isDisabled={isDisabled}
             name={"waitForSync"}
@@ -159,5 +66,105 @@ export const AddCollectionForm = () => {
         </Grid>
       </Stack>
     </Grid>
+  );
+};
+
+const ClusterFields = ({ isOneShard }: { isOneShard: boolean }) => {
+  const { collections, isFormDisabled: isDisabled } = useCollectionsContext();
+  const [distributeShardsLike] = useField<string>("distributeShardsLike");
+  const [isSatellite] = useField<boolean>("isSatellite");
+
+  if (!window.App.isCluster) {
+    return null;
+  }
+  return (
+    <>
+      <InputControl
+        isDisabled={
+          isDisabled ||
+          isOneShard ||
+          isSatellite.value ||
+          distributeShardsLike.value !== ""
+        }
+        name={"numberOfShards"}
+        label="Number of shards"
+        inputProps={{ type: "number" }}
+        tooltip={
+          isOneShard
+            ? undefined
+            : `The number of shards to create. The maximum value is ${window.frontendConfig.maxNumberOfShards}. You cannot change this afterwards.`
+        }
+      />
+      <CreatableMultiSelectControl
+        isDisabled={isDisabled || isSatellite.value}
+        name={"shardKeys"}
+        label="Shard keys"
+        selectProps={{
+          noOptionsMessage: () => "Start typing to add a key"
+        }}
+        tooltip={"The keys used to distribute documents on shards."}
+      />
+      <InputControl
+        isDisabled={
+          isDisabled || isSatellite.value || distributeShardsLike.value !== ""
+        }
+        name={"replicationFactor"}
+        label="Replication Factor"
+        inputProps={{ type: "number" }}
+        tooltip={"Total number of copies of the data in the cluster."}
+      />
+      <InputControl
+        isDisabled={
+          isDisabled || isSatellite.value || distributeShardsLike.value !== ""
+        }
+        name={"writeConcern"}
+        label="Write Concern"
+        inputProps={{ type: "number" }}
+        tooltip={
+          "Total number of copies of the data in the cluster that are required for each write operation. If we get below this value the collection will be read-only until enough copies are created. Must be smaller or equal compared to the replication factor."
+        }
+      />
+      {window.frontendConfig.isEnterprise && (
+        <>
+          {!isOneShard && (
+            <>
+              <SelectControl
+                isDisabled={isDisabled}
+                name={"distributeShardsLike"}
+                label="Distribute shards like"
+                selectProps={{
+                  options: [
+                    { label: "N/A", value: "" },
+                    ...(collections?.map(collection => ({
+                      label: collection.name,
+                      value: collection.name
+                    })) ?? [])
+                  ]
+                }}
+                tooltip={
+                  "Name of another collection that should be used as a prototype for sharding this collection."
+                }
+              />
+              <InputControl
+                isDisabled={isDisabled || isSatellite.value}
+                name={"smartJoinAttribute"}
+                label="SmartJoin attribute"
+                tooltip={
+                  "String attribute name. Can be left empty if SmartJoins are not used."
+                }
+              />
+            </>
+          )}
+          <SwitchControl
+            isDisabled={isDisabled || distributeShardsLike.value !== ""}
+            name={"isSatellite"}
+            label="SatelliteCollection"
+            tooltip={
+              "If enabled, every collection in this collection will be replicated to every DB-Server."
+            }
+          />
+        </>
+      )}
+    </>
   );
 };
