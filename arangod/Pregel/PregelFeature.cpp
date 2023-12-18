260bdd85210d8b46e581c1a32d31a7cd6b1c2245
Michael Hackstein <michael@arangodb.com>
2023-12-18 11:06:26 +0100

    Applied clang-format

--- arangod/Pregel/PregelFeature.cpp
+++ arangod/Pregel/PregelFeature.cpp
@@ -523,7 +523,6 @@ void PregelFeature::start() {
 }
 
 void PregelFeature::beginShutdown() {
-
   // Copy the _conductors and _workers maps here, because in the case of a
   // single server there is a lock order inversion.  This is because the
   // conductor code directly calls back into the feature while holding the
