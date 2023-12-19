bba548f548d11f7488baaf787d8cd29db0c2ac07
Jan <jsteemann@users.noreply.github.com>
2023-12-19 12:10:59 +0100

    apply sanitizer options earlier (#20333)
    * execute shell_client test in forked arangosh
    
    if the shell_client test is executed in the same arangosh instance as
    the basic test executor, then all suppressions that are set by the basic
    test executor for the sanitizers (TSan, ASan) will not be used by the
    arangosh instance itself. the suppressions will only be picked up
    correctly by newly started processes. thus we need to execute the
    shell_client test in a new arangosh instance.
    
    * remove garbage
    
    * move setting of options to scripts/unittest
    
    * de-dry code

--- js/client/modules/@arangodb/testutils/process-utils.js
+++ js/client/modules/@arangodb/testutils/process-utils.js
@@ -436,6 +436,22 @@ function setupBinaries (builddir, buildType, configDir) {
     isEnterpriseClient = true;
     checkFiles.push(ARANGOBACKUP_BIN);
   }
+
+  checkFiles.forEach((file) => {
+    if (!fs.isFile(file)) {
+      throw new Error('unable to locate ' + file);
+    }
+  });
+
+  // fiddle in suppressions for sanitizers if not already set from an
+  // outside script. this can populate the environment variables
+  // - ASAN_OPTIONS
+  // - UBSAN_OPTIONS
+  // - LSAN_OPTIONS
+  // - TSAN_OPTIONS
+  // note: this code repeats existing logic that can be found in
+  // scripts/unittest as well. according to @dothebart it must be
+  // present in both code locations.
   ["asan", "ubsan", "lsan", "tsan"].forEach((san) => {
     let envName = san.toUpperCase() + "_OPTIONS";
     let fileName = san + "_arangodb_suppressions.txt";
@@ -446,11 +462,6 @@ function setupBinaries (builddir, buildType, configDir) {
     }
   });
 
-  for (let b = 0; b < checkFiles.length; ++b) {
-    if (!fs.isFile(checkFiles[b])) {
-      throw new Error('unable to locate ' + checkFiles[b]);
-    }
-  }
   global.ARANGOSH_BIN = ARANGOSH_BIN;
 }
 
