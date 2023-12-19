e85a99daccbab42ba967542b55de33e98f9d01ad
Lars Maier <lars@arangodb.com>
2023-12-19 17:29:03 +0100

    Prefix Values for ZKD (#20329)
    * Introduce sortedPrefixValues attribute.
    
    * Use vpack comparator for zkd
    
    * Added support for sortedPrefixFields.
    
    * Add randomized test for zkd.
    
    * Add more debug output.
    
    * Fixing cluster case.
    
    * Sort out the unique key case for zkd index.
    
    * Finalize usage of two column families.
    
    * Introduce StaticString.
    
    * Added more tests.
    
    * Fix costs.
    
    * Rename functions.
    
    * Remove unnecessary changes.
    
    * Applied review comments.

--- arangod/ClusterEngine/ClusterIndex.cpp
+++ arangod/ClusterEngine/ClusterIndex.cpp
@@ -90,6 +90,9 @@ ClusterIndex::ClusterIndex(IndexId id, LogicalCollection& collection,
           Index::parseFields(info.get(StaticStrings::IndexStoredValues),
                              /*allowEmpty*/ true, /*allowExpansion*/ false));
     } else if (_indexType == TRI_IDX_TYPE_ZKD_INDEX) {
+      _prefixFields =
+          Index::parseFields(info.get(StaticStrings::IndexPrefixFields),
+                             /*allowEmpty*/ true, /*allowExpansion*/ false);
       _coveredFields =
           Index::parseFields(info.get(StaticStrings::IndexStoredValues),
                              /*allowEmpty*/ true, /*allowExpansion*/ false);
@@ -134,6 +137,19 @@ void ClusterIndex::toVelocyPack(
   } else if (_indexType == Index::TRI_IDX_TYPE_TTL_INDEX) {
     // no estimates for the ttl index
     builder.add(StaticStrings::IndexEstimates, VPackValue(false));
+  }
+
+  if (_indexType == Index::TRI_IDX_TYPE_ZKD_INDEX) {
+    builder.add(arangodb::velocypack::Value(StaticStrings::IndexPrefixFields));
+    builder.openArray();
+
+    for (auto const& field : _prefixFields) {
+      std::string fieldString;
+      TRI_AttributeNamesToString(field, fieldString);
+      builder.add(VPackValue(fieldString));
+    }
+
+    builder.close();
   }
 
   if (inProgress()) {
