7d46d0e1fbcfb4f34f52e2e2ba67b571b7d5a8f7
jsteemann <jsteemann@users.noreply.github.com>
2023-12-19 22:01:23 +0100

    lint

--- tests/js/client/aql/aql-optimizer-rule-fuse-filters.js
+++ tests/js/client/aql/aql-optimizer-rule-fuse-filters.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertEqual, assertNotEqual */
+/*global assertEqual, assertNotEqual, assertTrue */
 
 ////////////////////////////////////////////////////////////////////////////////
 /// DISCLAIMER
