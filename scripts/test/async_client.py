f6fe73b8206a9dba110e2cf5af00271123fed3ff
Wilfried Goesgens <willi@arangodb.com>
2023-12-20 14:31:52 +0100

    more processes that seem to be spawned

--- scripts/test/async_client.py
+++ scripts/test/async_client.py
@@ -200,11 +200,12 @@ def kill_children(identifier, params, children):
         if one_child.pid in killed:
             continue
         try:
-            if one_child.name() != "svchost.exe":
+            pname = one_child.name()
+            if pname not in ["svchost.exe", "conhost.exe". "mscorsvw.exe"]:
                 killed.append(one_child.pid)
                 err += add_message_to_report(
                     params,
-                    f"{identifier}: killing {one_child.name()} - {str(one_child.pid)}",
+                    f"{identifier}: killing {pname} - {str(one_child.pid)}",
                 )
                 one_child.resume()
         except FileNotFoundError:
