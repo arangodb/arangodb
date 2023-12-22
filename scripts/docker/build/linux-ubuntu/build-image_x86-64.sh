3a803118518e622782e718694c3b40c5fa61944f
Max Neunhoeffer <max@arangodb.com>
2023-12-22 15:28:34 +0100

    Fix build-image scripts.

--- scripts/docker/build/linux-ubuntu/build-image_x86-64.sh
+++ scripts/docker/build/linux-ubuntu/build-image_x86-64.sh
@@ -5,13 +5,13 @@ arch=amd64
 image=arangodb/build-ubuntu
 docker build --platform linux/$arch -t $image:latest-$arch --file Dockerfile .
 
-GCC_VERSION=$(docker run --rm -it $image:latest-$arch g++ "--version" | grep -Po " \K\d+\.\d+(?=\.\d+ )")
+GCC_VERSION=$(docker run --rm -it $image:latest-$arch g++ "--version" | head -1 | cut -d " " -f 4 | tr -d "\n\r")
 
 OPENSSL_VERSION=$(docker run --rm -it $image:latest-$arch sh -c "cat /opt/lib64/pkgconfig/openssl.pc" | grep -Po "Version: \K\d+\.\d+\.\d+")
 
 OS_VERSION=23.10
 
-IMAGE_TAG=${OS_VERSION}-gcc${GCC_VERSION}-openssl${OPENSSL_VERSION}-$(git rev-parse --short HEAD)-$arch
+IMAGE_TAG="${OS_VERSION}-gcc${GCC_VERSION}-openssl${OPENSSL_VERSION}-$(git rev-parse --short HEAD)-$arch"
 
 echo "Tagging image as \"${IMAGE_TAG}\""
 docker tag $image:latest-$arch $image:${IMAGE_TAG}
