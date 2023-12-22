ddf3c8e48b603e71c6d0aeb0b958192204776b7c
Max Neunhoeffer <max@arangodb.com>
2023-12-22 15:30:48 +0100

    Add clang-version to tag.

--- scripts/docker/build/linux-ubuntu/build-image_arm64.sh
+++ scripts/docker/build/linux-ubuntu/build-image_arm64.sh
@@ -7,11 +7,13 @@ docker build --platform linux/$arch -t $image:latest-$arch --file Dockerfile.arm
 
 GCC_VERSION=$(docker run --rm -it $image:latest-$arch g++ "--version" | head -1 | cut -d " " -f 4 | tr -d "\n\r")
 
+CLANG_VERSION=$(docker run --rm -it $image:latest-$arch clang++ "--version" | head -1 | cut -d " " -f 4 | tr -d "\n\r")
+
 OPENSSL_VERSION=$(docker run --rm -it $image:latest-$arch sh -c "cat /opt/lib/pkgconfig/openssl.pc" | grep -Po "Version: \K\d+\.\d+\.\d+")
 
 OS_VERSION=23.10
 
-IMAGE_TAG="${OS_VERSION}-gcc${GCC_VERSION}-openssl${OPENSSL_VERSION}-$(git rev-parse --short HEAD)-$arch"
+IMAGE_TAG="${OS_VERSION}-clang${CLANG_VERSION}-gcc${GCC_VERSION}-openssl${OPENSSL_VERSION}-$(git rev-parse --short HEAD)-$arch"
 
 echo "Tagging image as \"${IMAGE_TAG}\""
 docker tag $image:latest-$arch $image:${IMAGE_TAG}
