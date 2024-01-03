92321791b308c93b65a068e65b328e6f976bcbc7
Vadim Kondratev <vadim@arangodb.com>
2024-01-03 21:23:34 +0100

    Ubuntu single build image (devel) (#20385)
    * Add CLANG_LINUX to VERSIONS
    
    * Only version
    
    * Unify oskar and CircleCI build images
    
    * Update readmes and makefile to be in order
    
    * Use numeric tag for Linux build image
    
    * Fix typos
    
    * Try to use sccache as a compiler launcher
    
    * Update scripts/docker/build/README.md
    
    Co-authored-by: Max Neunhöffer <max@arangodb.com>
    
    * Switch on larger build ids.
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Max Neunhoeffer <max@arangodb.com>

---
+++ scripts/docker/build/linux/static/rebuild_glibc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /root
+apt-get source glibc
+cd glibc-*
+patch -u debian/rules.d/build.mk /static/enable_static_nss.patch
+export EMAIL="Max Neunhoeffer <max@arangodb.com>"
+debchange -n "Switched on --enable-static-nss=yes"
+debchange -r ignored
+export DEB_BUILD_OPTIONS=nocheck
+debian/rules -j 64 binary
+cd /root
+mkdir debs
+mv libc*.deb debs
+mv locales*.deb debs
+mv nscd*.deb debs
