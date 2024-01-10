53c792a297d9b66d52d38f46dca638832568d4fb
mpoeter <manuel@arangodb.com>
2024-01-10 14:15:09 +0100

    Simplify and fix cmake frontend build
    The output dependency did not work as intended, because the existence of the build folder alone was already enough for make to conclude that there was nothing to do.

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -5,14 +5,8 @@
 find_package(Nodejs 16 REQUIRED)
 find_package(Yarn REQUIRED)
 
-set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react/build)
-
 add_custom_target(frontend ALL
-  DEPENDS ${FRONTEND_DESTINATION}
-)
-add_custom_command(# frontend
   COMMENT "create frontend build"
-  OUTPUT ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react/build
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react
   COMMAND yarn install
   COMMAND yarn build
