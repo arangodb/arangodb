69215df1d0c2439a14dd257d356cfe32ecf9e269
mpoeter <manuel@arangodb.com>
2024-01-10 14:20:56 +0100

    Fix

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -5,14 +5,16 @@
 find_package(Nodejs 16 REQUIRED)
 find_package(Yarn REQUIRED)
 
+set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
+
 add_custom_target(frontend ALL
   COMMENT "create frontend build"
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react
+  WORKING_DIRECTORY ${FRONTEND_DESTINATION}
   COMMAND yarn install
   COMMAND yarn build
 )
 
 add_custom_target(frontend_clean
-  COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}
+  COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
   COMMENT "Removing frontend artefacts"
   )
