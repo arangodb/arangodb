41a64fdee25967f72c0c977722604e49041d959c
Rustmilian <110098609+Rustmilian@users.noreply.github.com>
2024-01-11 17:13:58 -0500

    fgrep: warning: fgrep is obsolescent; using grep -F

--- utils/generateREADME.sh
+++ utils/generateREADME.sh
@@ -4,9 +4,9 @@ set -e
 IN=${1:-README.md}
 OUT=${2:-README}
 
-fgrep -v "picture>" $IN \
-  | fgrep -v "<source " \
-  | fgrep -v "<img " \
+grep -Fv "picture>" $IN \
+  | grep -Fv "<source " \
+  | grep -Fv "<img " \
   | sed -e 's:&gt;:>:g' \
   | awk 'BEGIN { s = 0; } /^\*\*\*\*/ {if (s > 0) print ""} {if (length($0) == 0) {if (s != 0) print $0;} else {s = 1; print $0; }} /^\*\*\*\*/ {print ""}' \
   > $OUT
