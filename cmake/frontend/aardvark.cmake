e3044c40fd7c6443d06a744cc347ad1f17546121
Heiko Kernbach <heiko@arangodb.com>
2024-01-12 13:28:40 +0100

    first approach broke the linux build, now seperating both

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -7,13 +7,22 @@ find_package(Yarn REQUIRED)
 
 set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
 
-add_custom_target(frontend ALL
-  COMMENT "create frontend build"
-  WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-  COMMAND cmd /c "yarn install && yarn build"
-)
+if (WIN32)
+    add_custom_target(frontend ALL
+            COMMENT "create frontend build"
+            WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+            COMMAND cmd /c "yarn install && yarn build"
+    )
+else ()
+    add_custom_target(frontend ALL
+            COMMENT "create frontend build"
+            WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+            COMMAND yarn install
+            COMMAND yarn build
+    )
+endif ()
 
 add_custom_target(frontend_clean
-  COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
-  COMMENT "Removing frontend artefacts"
-  )
+        COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
+        COMMENT "Removing frontend artefacts"
+)
