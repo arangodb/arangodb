bcb6da492de0240803b0153c45b4f4178804f840
Heiko Kernbach <heiko@arangodb.com>
2024-01-12 13:20:34 +0100

    attempt to fix windows frontend build

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -10,8 +10,7 @@ set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/AP
 add_custom_target(frontend ALL
   COMMENT "create frontend build"
   WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-  COMMAND yarn install
-  COMMAND yarn build
+  COMMAND cmd /c "yarn install && yarn build"
 )
 
 add_custom_target(frontend_clean
