fbcec26b2d5bd0d34f127e88b4ffe32b854dfd59
Heiko Kernbach <heiko@arangodb.com>
2024-01-12 13:30:24 +0100

    indent 2 spaces

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -8,21 +8,21 @@ find_package(Yarn REQUIRED)
 set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
 
 if (WIN32)
-    add_custom_target(frontend ALL
-            COMMENT "create frontend build"
-            WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-            COMMAND cmd /c "yarn install && yarn build"
-    )
+  add_custom_target(frontend ALL
+    COMMENT "create frontend build"
+    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+    COMMAND cmd /c "yarn install && yarn build"
+  )
 else ()
-    add_custom_target(frontend ALL
-            COMMENT "create frontend build"
-            WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-            COMMAND yarn install
-            COMMAND yarn build
-    )
+  add_custom_target(frontend ALL
+    COMMENT "create frontend build"
+    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+    COMMAND yarn install
+    COMMAND yarn build
+  )
 endif ()
 
 add_custom_target(frontend_clean
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
-        COMMENT "Removing frontend artefacts"
+  COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
+  COMMENT "Removing frontend artefacts"
 )
