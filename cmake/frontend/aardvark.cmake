de2ebcc2fd0956a2ae76b43ac8cab77e905cff69
mpoeter <manuel@arangodb.com>
2024-01-12 14:42:57 +0100

    Use CALL to run yarn on windows

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -8,21 +8,22 @@ find_package(Yarn REQUIRED)
 set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
 
 if (WIN32)
-  add_custom_target(frontend ALL
-    COMMENT "create frontend build"
-    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-    COMMAND cmd /c "yarn install && yarn build"
-  )
+  # On Windows cmake creates a cmd script for the custom command. However, yarn itself is
+  # also a cmd script. We have to use the CALL command to run a nested batch script, because
+  # otherwise the nested batch script will terminate the _parent_ script.
+  set(YARN_PREFIX "CALL")
 else ()
-  add_custom_target(frontend ALL
-    COMMENT "create frontend build"
-    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-    COMMAND yarn install
-    COMMAND yarn build
-  )
+  set(YARN_PREFIX "")
 endif ()
+
+add_custom_target(frontend ALL
+  COMMENT "create frontend build"
+  WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+  COMMAND ${YARN_PREFIX} yarn install
+  COMMAND ${YARN_PREFIX} yarn build
+)
 
 add_custom_target(frontend_clean
   COMMAND ${CMAKE_COMMAND} -E remove_directory ${FRONTEND_DESTINATION}/build
-  COMMENT "Removing frontend artefacts"
+  COMMENT "Removing frontend artifacts"
 )
