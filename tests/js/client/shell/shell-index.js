cb04c817b5b30ad4f7bb42046da93b411d637a2a
Kaveh Vahedipour <kaveh@arangodb.com>
2024-01-20 23:13:14 +0100

    added client tests

---
+++ tests/js/client/shell/shell-index.js
@@ -0,0 +1,116 @@
+/*jshint globalstrict:false, strict:false */
+/*global fail, assertEqual, assertNotEqual, assertTrue, assertFalse, assertNull */
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test index methods
+///
+/// DISCLAIMER
+///
+/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is triAGENS GmbH, Cologne, Germany
+///
+/// @author Dr. Frank Celler, Lucas Dohmen
+/// @author Copyright 2012, triAGENS GmbH, Cologne, Germany
+////////////////////////////////////////////////////////////////////////////////
+
+const jsunity = require("jsunity");
+const internal = require("internal");
+const errors = internal.errors;
+const { helper, versionHas } = require("@arangodb/test-helper");
+const platform = require('internal').platform;
+
+
+const cn = "UnitTestsCollection";
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test suite: basics
+////////////////////////////////////////////////////////////////////////////////
+
+function IndexSuite() {
+  'use strict';
+
+  let collection = null;
+
+  return {
+
+    setUp : function () {
+      internal.db._drop(cn);
+      collection = internal.db._create(cn);
+    },
+
+    tearDown : function () {
+      internal.db._drop(cn);
+      collection = null;
+    },
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test: indexes
+////////////////////////////////////////////////////////////////////////////////
+
+    testIndexProgress : function () {
+      var c = internal.db._create("c0l", {"numberOfShards": 3});
+      var docs = [];
+      const sleep = require('internal').sleep;
+      for (var i = 0; i < 30720; ++i) {
+        docs.push({name : "name" + i});
+      }
+      c.insert(docs);
+      internal.debugSetFailAt("fillIndex::pause");
+      var idx = { name:"progress", type: "persistent", fields:["name"], inBackground: true};
+      arango.GET(`/_api/index?collection=c0l`);
+      arango.POST_RAW(`/_api/index?collection=c0l`, idx,
+                      {"x-arango-async": "store"});
+      var progress = 0.0;
+      var idxs = [];
+      while (true) {
+        idxs = arango.GET(`/_api/index?collection=c0l&withHidden=true`).indexes;
+        if (idxs.length > 1) {
+          break;
+        }
+        sleep(0.1);
+      }
+      while (arango.GET(`/_api/index?collection=c0l&withHidden=true`) &&
+             arango.GET(`/_api/index?collection=c0l&withHidden=true`).indexes[1].hasOwnProperty("progress")) {
+        while (true) {
+          var idxs = arango.GET(`/_api/index?collection=c0l&withHidden=true`).indexes;
+          if (idxs[1].hasOwnProperty("progress") && idxs[1].progress > progress) {
+            progress = idxs[1].progress;
+            console.warn(Math.round(progress) + "%");
+            internal.debugSetFailAt("fillIndex::unpause");
+            break;
+          }
+          sleep (0.1);
+        }
+        sleep(0.1);
+        internal.debugRemoveFailAt("fillIndex::unpause");
+        internal.debugSetFailAt("fillIndex::next");
+        sleep(0.1);
+        internal.debugRemoveFailAt("fillIndex::next");
+      }
+      
+      internal.debugSetFailAt("fillIndex::unpause");
+      internal.debugSetFailAt("fillIndex::next");
+      internal.debugRemoveFailAt("fillIndex::pause");
+      internal.debugRemoveFailAt("fillIndex::unpause");
+      internal.debugRemoveFailAt("fillIndex::next");
+      c.drop();
+    }
+  };
+}
+
+jsunity.run(IndexSuite);
+
+return jsunity.done();
