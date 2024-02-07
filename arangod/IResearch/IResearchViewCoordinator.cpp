601f499bf80834a1e4dc990b61a581ce334e68f0
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2024-02-07 21:08:15 +0300

    [BTS-1782] Fix view restore (#20605)
    * Restore view is user action and user cannot specify id or planId of view object
    
    * Adjust tests
    
    * Fix view upgrade task

--- arangod/IResearch/IResearchViewCoordinator.cpp
+++ arangod/IResearch/IResearchViewCoordinator.cpp
@@ -151,11 +151,11 @@ struct IResearchViewCoordinator::ViewFactory final : arangodb::ViewFactory {
 
   Result instantiate(LogicalView::ptr& view, TRI_vocbase_t& vocbase,
                      velocypack::Slice definition,
-                     bool /*isUserRequest*/) const final {
+                     bool isUserRequest) const final {
     std::string error;
     // TODO make_shared instead of new
     auto impl = std::shared_ptr<IResearchViewCoordinator>(
-        new IResearchViewCoordinator(vocbase, definition));
+        new IResearchViewCoordinator(vocbase, definition, isUserRequest));
     if (!impl->_meta.init(definition, error)) {
       return {TRI_ERROR_BAD_PARAMETER,
               absl::StrCat(
@@ -310,8 +310,9 @@ Result IResearchViewCoordinator::unlink(DataSourceId) noexcept {
 }
 
 IResearchViewCoordinator::IResearchViewCoordinator(TRI_vocbase_t& vocbase,
-                                                   velocypack::Slice info)
-    : LogicalView(*this, vocbase, info) {
+                                                   velocypack::Slice info,
+                                                   bool isUserRequest)
+    : LogicalView(*this, vocbase, info, isUserRequest) {
   TRI_ASSERT(ServerState::instance()->isCoordinator());
 }
 
