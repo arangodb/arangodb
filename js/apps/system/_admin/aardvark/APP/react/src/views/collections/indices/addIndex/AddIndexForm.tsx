af3c457fc52d483d13eb233c1f9ed02afd3b30b2
Palash Karia <2976363+palashkaria@users.noreply.github.com>
2024-02-20 16:11:47 +0700

    fix: type issues

--- js/apps/system/_admin/aardvark/APP/react/src/views/collections/indices/addIndex/AddIndexForm.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/collections/indices/addIndex/AddIndexForm.tsx
@@ -1,20 +1,20 @@
 import { Box, FormLabel } from "@chakra-ui/react";
-import { Index } from "arangojs/indexes";
 import React, { useState } from "react";
 import SingleSelect from "../../../../components/select/SingleSelect";
+import { CollectionIndex } from "../CollectionIndex.types";
 import { useCollectionIndicesContext } from "../CollectionIndicesContext";
 import { FulltextIndexForm } from "./FulltextIndexForm";
 import { GeoIndexForm } from "./GeoIndexForm";
 import { IndexInfoTooltip } from "./IndexInfoTooltip";
 import { InvertedIndexFormWrap } from "./invertedIndex/InvertedIndexFormWrap";
-import { PersistentIndexForm } from "./PersistentIndexForm";
-import { TTLIndexForm } from "./TTLIndexForm";
 import { MDIIndexForm } from "./MDIIndexForm";
 import { MDIPrefixedIndexForm } from "./MDIPrefixedIndexForm";
+import { PersistentIndexForm } from "./PersistentIndexForm";
+import { TTLIndexForm } from "./TTLIndexForm";
 
 export const AddIndexForm = ({ onClose }: { onClose: () => void }) => {
   const { indexTypeOptions } = useCollectionIndicesContext();
-  const [indexType, setIndexType] = useState<Index["type"]>(
+  const [indexType, setIndexType] = useState<CollectionIndex["type"]>(
     indexTypeOptions[0].value
   );
   let tooltipText = "Type of index to create.";
@@ -39,7 +39,7 @@ export const AddIndexForm = ({ onClose }: { onClose: () => void }) => {
           defaultValue={indexTypeOptions[0]}
           options={indexTypeOptions}
           onChange={value => {
-            setIndexType(value?.value as Index["type"]);
+            setIndexType(value?.value as CollectionIndex["type"]);
           }}
         />
         <IndexInfoTooltip label={tooltipText} />
@@ -55,7 +55,7 @@ const IndexTypeForm = ({
   type,
   onClose
 }: {
-  type: Index["type"];
+  type: CollectionIndex["type"];
   onClose: () => void;
 }) => {
   if (type === "inverted") {
