09abe435b2b5510e1245bb5674065ea72a8aeffe
Alan Plum <alan.plum-extern@arangodb.com>
2024-02-28 18:59:02 +0100

    Fix FE-316

--- js/apps/system/_admin/aardvark/APP/frontend/js/views/documentsView.js
+++ js/apps/system/_admin/aardvark/APP/frontend/js/views/documentsView.js
@@ -1125,13 +1125,13 @@
     render: function () {
       this.collectionName = window.location.hash.split('/')[1];
 
-      $(this.el).html(this.template.render({}));
-
       if (this.type === 2) {
         this.type = 'document';
       } else if (this.type === 3) {
         this.type = 'edge';
       }
+
+      $(this.el).html(this.template.render({type: this.type}));
 
       this.tableView.setElement($(this.table)).drawLoading();
 
