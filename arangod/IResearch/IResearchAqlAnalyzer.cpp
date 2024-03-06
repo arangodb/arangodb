040591463c55d62a73abcdebd0e067f38f8fb843
Jan <jsteemann@users.noreply.github.com>
2024-03-06 19:06:56 +0100

    Allow limiting async prefetching (#20648)
    * Allow limiting async prefetching
    
    * Allow limiting the amount of async prefetching operations for AQL queries.
      Since version 3.12, AQL queries can make use of async prefetching. While this
      is normally beneficial for query performance, the async prefetch operations
      may cause congestion in the ArangoDB scheduler and interfere with other
      operations.
      In order to control the maximum amount of prefetch operations, the following
      startup options have been added:
    
      - `--query.max-total-async-prefetch-slots`: the maximum total number of slots
        available for asynchronous prefetching, across all AQL queries.
      - `--query.max-query-async-prefetch-slots`: the maximum per-query number of
        slots available for asynchronous prefetching inside any AQL query.
    
      That means the total number of concurrent prefetch operations across all AQL
      queries can be limited using the first option, and the maximum number of
      prefetch operations in every single AQL query can be capped with the second
      option. These options prevent that running a lot of AQL queries with async
      prefetching fully congests the scheduler queue, and also they prevent large
      AQL queries to use up all async prefetching capacity on their own.
    
    * fix disabling optimizer rules
    
    * Update arangod/Aql/ExecutionEngine.cpp
    
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>
    
    * Update arangod/RestServer/QueryRegistryFeature.cpp
    
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>
    
    * refactored
    
    * fix test
    
    * Update arangod/Aql/AsyncPrefetchSlotsManager.cpp
    
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>
    
    * address review comments
    
    ---------
    
    Co-authored-by: Manuel Pöter <manuel@arangodb.com>

--- arangod/IResearch/IResearchAqlAnalyzer.cpp
+++ arangod/IResearch/IResearchAqlAnalyzer.cpp
@@ -489,12 +489,15 @@ bool AqlAnalyzer::reset(std::string_view field) noexcept {
       // SubqueryNodes with SubqueryStartNodes and SubqueryEndNodes.
       Optimizer optimizer(_query->resourceMonitor(), 1);
       // disable all rules which are not necessary
+      optimizer.initializeRules(plan.get(), _query->queryOptions());
       optimizer.disableRules(plan.get(), [](OptimizerRule const& rule) -> bool {
         return rule.canBeDisabled() || rule.isClusterOnly();
       });
       optimizer.createPlans(std::move(plan), _query->queryOptions(), false);
 
       _plan = optimizer.stealBest();
+      TRI_ASSERT(
+          !_plan->hasAppliedRule(OptimizerRule::RuleLevel::asyncPrefetchRule));
 
       // try to optimize
       if (tryOptimize(this)) {
