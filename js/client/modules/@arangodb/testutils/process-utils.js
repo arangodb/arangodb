92955b2f8f4c45cf0734d5973c2ba2d4647f3317
Wilfried Goesgens <willi@arangodb.com>
2024-03-26 17:17:13 +0100

    signal single shard to rta-makedata

--- js/client/modules/@arangodb/testutils/process-utils.js
+++ js/client/modules/@arangodb/testutils/process-utils.js
@@ -573,6 +573,9 @@ function rtaMakedata(options, instanceManager, writeReadClean, msg, logFile, mor
   if (options.rtaNegFilter !== '') {
     argv = argv.concat(['--skip', options.rtaNegFilter]);
   }
+  if (options.extraArgs.hasOwnProperty('cluster.force-one-shard')) {
+    argv = argv.concat(['--singleShard', 'true']);
+  }
   if (options.hasOwnProperty('makedata_args')) {
     argv = argv.concat(toArgv(options['makedata_args']));
   }
