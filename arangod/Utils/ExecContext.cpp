b8d30174d6e6a900cb0bc1ac86bed60fe83b07c4
jsteemann <jsteemann@users.noreply.github.com>
2024-04-22 15:06:00 +0200

    remove special handling for _queries collection

--- arangod/Utils/ExecContext.cpp
+++ arangod/Utils/ExecContext.cpp
@@ -29,8 +29,6 @@
 #include "GeneralServer/AuthenticationFeature.h"
 #include "VocBase/vocbase.h"
 
-#include "Logger/LogMacros.h"
-
 using namespace arangodb;
 
 thread_local std::shared_ptr<ExecContext const> ExecContext::CURRENT = nullptr;
@@ -154,14 +152,15 @@ auth::Level ExecContext::collectionAuthLevel(std::string const& dbname,
     // TODO: move this block above, such that it takes effect
     //       when authentication is disabled
     if (dbname == StaticStrings::SystemDatabase &&
-        (coll == StaticStrings::UsersCollection ||
-         coll == StaticStrings::QueriesCollection)) {
-      // _users or _queries (only present in _system database)
+        coll == StaticStrings::UsersCollection) {
+      // _users (only present in _system database)
       return auth::Level::NONE;
-    } else if (coll == StaticStrings::QueuesCollection) {
+    }
+    if (coll == StaticStrings::QueuesCollection) {
       // _queues
       return auth::Level::RO;
-    } else if (coll == StaticStrings::FrontendCollection) {
+    }
+    if (coll == StaticStrings::FrontendCollection) {
       // _frontend
       return auth::Level::RW;
     }  // intentional fall through
