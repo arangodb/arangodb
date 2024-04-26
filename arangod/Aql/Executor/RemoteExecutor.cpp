064f59e64072ec2c4a9ff25bc524c90aee9a25ce
Lars Maier <lars@arangodb.com>
2024-04-26 11:48:11 +0200

    Disable PIPEWAIT.

--- arangod/Aql/Executor/RemoteExecutor.cpp
+++ arangod/Aql/Executor/RemoteExecutor.cpp
@@ -379,14 +379,11 @@ Result ExecutionBlockImpl<RemoteExecutor>::sendAsyncRequest(
 
   _engine->getQuery().incHttpRequests(unsigned(1));
 
-  LOG_DEVEL << "ASYNC REQ " << ticket << " BEGIN";
-
   network::sendRequest(pool, _server, type,
                        absl::StrCat(urlPart, "/", _queryId), std::move(body),
                        options, header)
       .thenFinal([this, ticket, sqs = _engine->sharedState()](
                      futures::Try<network::Response> resp) {
-        LOG_DEVEL << "ASYNC REQ " << ticket << " COMPLETED";
         // `this` is only valid as long as sharedState is valid.
         // So we must execute this under sharedState's mutex.
         sqs->executeAndWakeup([&]() {
@@ -411,12 +408,8 @@ Result ExecutionBlockImpl<RemoteExecutor>::sendAsyncRequest(
                 _lastResponse = std::move(res);
               }
             }
-
-            LOG_DEVEL << "ASYNC REQ " << ticket << " WAKE UP QUERY";
             return true;
           }
-
-          LOG_DEVEL << "ASYNC REQ " << ticket << " UNUSED";
           return false;
         });
       });
--- arangod/Network/curl-stuff.cpp
+++ arangod/Network/curl-stuff.cpp
@@ -336,8 +336,9 @@ curl_easy_handle::curl_easy_handle() : _easy_handle(curl_easy_init()) {
   }
 
   // curl_easy_setopt(_easy_handle, CURLOPT_HTTP_VERSION,
-  // CURL_HTTP_VERSION_2_0);
-  curl_easy_setopt(_easy_handle, CURLOPT_PIPEWAIT, 1l);
+  // CURL_HTTP_VERSION_2_0); curl_easy_setopt(_easy_handle, CURLOPT_PIPEWAIT,
+  // 1l);
+
   curl_easy_setopt(_easy_handle, CURLOPT_SSL_ENABLE_ALPN, 1l);
   curl_easy_setopt(_easy_handle, CURLOPT_SSL_VERIFYPEER, 0l);
   curl_easy_setopt(_easy_handle, CURLOPT_SSL_VERIFYHOST, 0l);
