4b506ffb0493b2d5667aab1dc623b04329f00867
jsteemann <jsteemann@users.noreply.github.com>
2024-05-03 11:29:48 +0200

    simplify PR

--- 3rdParty/fuerte/src/AsioSockets.h
+++ 3rdParty/fuerte/src/AsioSockets.h
@@ -170,7 +170,7 @@ struct Socket<SocketType::Tcp> {
 template <>
 struct Socket<fuerte::SocketType::Ssl> {
   Socket(EventLoopService& loop, asio_ns::io_context& ctx)
-    : resolver(ctx), socket(ctx, loop.sslContext()), timer(ctx), ctx(ctx), sslContext(loop.sslContext()), cleanupDone(false) {}
+    : resolver(ctx), socket(ctx, loop.sslContext()), timer(ctx), cleanupDone(false) {}
 
   ~Socket() { 
     try {
@@ -234,7 +234,6 @@ struct Socket<fuerte::SocketType::Ssl> {
   }
   
   void rearm() {
-    socket = asio_ns::ssl::stream<asio_ns::ip::tcp::socket>(this->ctx, this->sslContext);
     canceled = false;
   }
 
@@ -300,8 +299,6 @@ struct Socket<fuerte::SocketType::Ssl> {
   asio_ns::ip::tcp::resolver resolver;
   asio_ns::ssl::stream<asio_ns::ip::tcp::socket> socket;
   asio_ns::steady_timer timer;
-  asio_ns::io_context& ctx;
-  asio_ns::ssl::context& sslContext;
   std::atomic<bool> cleanupDone;
   bool canceled = false;
 };
