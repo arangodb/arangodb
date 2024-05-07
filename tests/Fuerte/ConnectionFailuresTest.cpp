b4b20a00a8078eeda461231b62b1e02c3fc78d6a
jsteemann <jsteemann@users.noreply.github.com>
2024-05-07 15:40:02 +0200

    remove retry logic for fuerte connections

--- tests/Fuerte/ConnectionFailuresTest.cpp
+++ tests/Fuerte/ConnectionFailuresTest.cpp
@@ -27,21 +27,37 @@
 
 #include "gtest/gtest.h"
 
+#include <string_view>
+
 namespace f = ::arangodb::fuerte;
+
+// for testing connection failures, we need a free port that is not used by
+// another service. because we can't be sure about high port numbers, we are
+// using some from the very range. according to
+// https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=2
+// port 60 is unassigned and thus likely not taken by any service on the test
+// host. previously we used port 8629 for testing connection failures, but this
+// was flawed because it was possible that some service was running on port
+// 8629, which then made the connection failure tests fail.
+constexpr std::string_view urls[] = {
+    "http://localhost:60",
+    "h2://localhost:60",
+    "ssl://localhost:60",
+    "h2s://localhost:60",
+};
 
 // tryToConnectExpectFailure tries to make a connection to a host with given
 // url. This is expected to fail.
 static void tryToConnectExpectFailure(f::EventLoopService& eventLoopService,
-                                      const std::string& url) {
+                                      std::string_view url) {
   f::WaitGroup wg;
 
   wg.add();
   f::ConnectionBuilder cbuilder;
   cbuilder.connectTimeout(std::chrono::milliseconds(250));
-  cbuilder.connectRetryPause(std::chrono::milliseconds(100));
-  cbuilder.endpoint(url);
+  cbuilder.endpoint(std::string{url});
 
-  cbuilder.onFailure([&](f::Error errorCode, const std::string& errorMessage) {
+  cbuilder.onFailure([&](f::Error errorCode, std::string const& errorMessage) {
     ASSERT_EQ(errorCode, f::Error::CouldNotConnect);
     wg.done();
   });
@@ -66,12 +82,9 @@ TEST(ConnectionFailureTest, CannotResolveHttp) {
 
 // CannotConnect tests try to make a connection to a host with a valid name
 // but a wrong port.
-TEST(ConnectionFailureTest, CannotConnectHttp) {
-  f::EventLoopService loop;
-  tryToConnectExpectFailure(loop, "http://localhost:8629");
-}
-
-TEST(ConnectionFailureTest, CannotConnectHttp2) {
-  f::EventLoopService loop;
-  tryToConnectExpectFailure(loop, "h2://localhost:8629");
+TEST(ConnectionFailureTest, CannotConnect) {
+  for (auto const& url : urls) {
+    f::EventLoopService loop;
+    tryToConnectExpectFailure(loop, url);
+  }
 }
