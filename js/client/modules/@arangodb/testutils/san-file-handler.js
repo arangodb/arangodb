8ad10273624ae2441f0247333043a0259e2bb2c9
Wilfried Goesgens <willi@arangodb.com>
2024-05-13 10:30:41 +0200

    debug

--- js/client/modules/@arangodb/testutils/san-file-handler.js
+++ js/client/modules/@arangodb/testutils/san-file-handler.js
@@ -69,6 +69,7 @@ class sanHandler {
           if (oneSet.length > 0) {
             oneSet += ":";
           }
+          print(valueOne);
           let val = valueOne.replaceAll(',', '_');
           oneSet += `${keyOne}=${val}`;
         }
