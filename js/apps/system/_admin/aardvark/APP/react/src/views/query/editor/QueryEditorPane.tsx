6498e827a3c0dd93f66619bcc13343b3b8d8daad
Alan Plum <alan.plum-extern@arangodb.com>
2024-05-31 17:58:00 +0200

    FE-310: Make query UI resizable

--- js/apps/system/_admin/aardvark/APP/react/src/views/query/editor/QueryEditorPane.tsx
+++ js/apps/system/_admin/aardvark/APP/react/src/views/query/editor/QueryEditorPane.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Grid } from "@chakra-ui/react";
+import { Resizable } from "re-resizable";
+import { Box, Divider, Flex, Grid } from "@chakra-ui/react";
 import React from "react";
 import { useQueryContext } from "../QueryContextProvider";
 import { AQLEditor } from "./AQLEditor";
@@ -20,24 +21,38 @@ export const QueryEditorPane = () => {
     );
   }
   return (
-    <Box background="white" height="full">
-      <QueryEditorTopBar />
-      <Grid gridTemplateColumns="1.4fr 4px 0.6fr" height="calc(100% - 300px)">
-        <AQLEditor
-          autoFocus
-          resetEditor={resetEditor}
-          value={queryValue}
-          onChange={onQueryValueChange}
-        />
-        <Divider
-          borderLeftWidth="4px"
-          borderColor="gray.400"
-          orientation="vertical"
-        />
-        <QueryOptionsPane />
-      </Grid>
-      <Divider borderColor="gray.400" borderBottomWidth="4px" />
-      <QueryEditorBottomBar />
+    <Box background="white" height="calc(100vh - 300px)">
+      <Resizable
+        style={{ overflow: "hidden" }}
+        defaultSize={{ width: "100%", height: "90%" }}
+        minHeight={270}
+        maxHeight="100%"
+        enable={{ bottom: true }}
+      >
+        <Flex height="100%" direction="column">
+          <QueryEditorTopBar />
+          <Grid
+            gridTemplateColumns="1.4fr 4px 0.6fr"
+            height="full"
+            flexShrink={1}
+          >
+            <AQLEditor
+              autoFocus
+              resetEditor={resetEditor}
+              value={queryValue}
+              onChange={onQueryValueChange}
+            />
+            <Divider
+              borderLeftWidth="4px"
+              borderColor="gray.400"
+              orientation="vertical"
+            />
+            <QueryOptionsPane />
+          </Grid>
+          <Divider borderColor="gray.400" borderBottomWidth="4px" />
+          <QueryEditorBottomBar />
+        </Flex>
+      </Resizable>
       <QueryResults />
     </Box>
   );
