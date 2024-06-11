f1d0f58eb38b6d0b36714c6abfe889732900f004
Tobias Gödderz <tobias@arangodb.com>
2024-06-11 13:41:19 +0200

    Fix several lifetime issues in transaction::Methods

--- arangod/Transaction/Methods.h
+++ arangod/Transaction/Methods.h
@@ -445,7 +445,7 @@ class Methods {
 
   Future<OperationResult> documentLocal(std::string const& collectionName,
                                         VPackSlice value,
-                                        OperationOptions const& options);
+                                        OperationOptions const options);
 
   Future<OperationResult> insertCoordinator(std::string const& collectionName,
                                             VPackSlice value,
@@ -454,7 +454,7 @@ class Methods {
 
   Future<OperationResult> insertLocal(std::string const& collectionName,
                                       VPackSlice value,
-                                      OperationOptions& options);
+                                      OperationOptions options);
 
   Result determineReplication1TypeAndFollowers(
       LogicalCollection& collection, std::string_view operationName,
@@ -475,7 +475,7 @@ class Methods {
 
   Future<OperationResult> modifyLocal(std::string const& collectionName,
                                       VPackSlice newValue,
-                                      OperationOptions& options, bool isUpdate);
+                                      OperationOptions options, bool isUpdate);
 
   Future<OperationResult> removeCoordinator(std::string const& collectionName,
                                             VPackSlice value,
@@ -484,7 +484,7 @@ class Methods {
 
   Future<OperationResult> removeLocal(std::string const& collectionName,
                                       VPackSlice value,
-                                      OperationOptions& options);
+                                      OperationOptions options);
 
   futures::Future<OperationResult> allCoordinator(
       std::string const& collectionName, uint64_t skip, uint64_t limit,
@@ -492,13 +492,13 @@ class Methods {
 
   futures::Future<OperationResult> allLocal(std::string const& collectionName,
                                             uint64_t skip, uint64_t limit,
-                                            OperationOptions& options);
+                                            OperationOptions options);
 
   OperationResult anyCoordinator(std::string const& collectionName,
                                  OperationOptions const& options);
 
   futures::Future<OperationResult> anyLocal(std::string const& collectionName,
-                                            OperationOptions const& options);
+                                            OperationOptions options);
 
   Future<OperationResult> truncateCoordinator(std::string const& collectionName,
                                               OperationOptions& options,
