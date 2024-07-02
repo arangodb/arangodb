4dce8c502769e98e58bd85246c1a5dec7c41ef04
jsteemann <jsteemann@users.noreply.github.com>
2024-07-02 12:49:04 +0200

    fix the fix

--- arangod/Aql/IndexHint.cpp
+++ arangod/Aql/IndexHint.cpp
@@ -362,6 +362,7 @@ void IndexHint::toVelocyPack(VPackBuilder& builder) const {
           } else {
             builder.add(VPackValue(std::to_string(level.first)));
           }
+          VPackArrayBuilder hintGuard(&builder);
           indexesToVelocyPack(builder, level.second);
         }
         builder.close();
@@ -429,11 +430,9 @@ IndexHint IndexHint::getFromNested(std::string_view direction,
 void IndexHint::indexesToVelocyPack(
     velocypack::Builder& builder,
     IndexHint::PossibleIndexes const& indexes) const {
-  builder.openArray();
   for (auto const& index : indexes) {
     builder.add(VPackValue(index));
   }
-  builder.close();
 }
 
 bool IndexHint::parseNestedHint(AstNode const* node,
