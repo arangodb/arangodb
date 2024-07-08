3676d3df2a184374f8c45ca93691c43d2f24a77d
Wilfried Goesgens <willi@arangodb.com>
2024-07-08 13:25:37 +0200

    add binary name to coverage directory

--- js/client/modules/@arangodb/testutils/san-file-handler.js
+++ js/client/modules/@arangodb/testutils/san-file-handler.js
@@ -93,7 +93,7 @@ class sanHandler {
         path.pop();
       }
       path.push(crypto.md5(String(internal.time() + Math.random())));
-      subProcesEnv.push(`${coverage_name}=${fs.pathSeparator + fs.join(...path)}`);
+      subProcesEnv.push(`${coverage_name}=${fs.pathSeparator + fs.join(...path)}_${this.binaryName}`);
     }
     print(subProcesEnv)
     return subProcesEnv;
