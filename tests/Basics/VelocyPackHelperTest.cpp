0dcc76697083ac2149896b3311b24021d4c6bd34
Max Neunhoeffer <max@arangodb.com>
2024-07-17 17:11:21 +0200

    Add first unittests for new vpack comparator for numbers.

--- tests/Basics/VelocyPackHelperTest.cpp
+++ tests/Basics/VelocyPackHelperTest.cpp
@@ -39,6 +39,36 @@
   r = VPackParser::fromJson(rValue);                      \
   EXPECT_EQ(expected, func(l->slice(), r->slice(), true));
 
+struct DoubleValue {
+  double d;
+  uint8_t sign;
+  uint16_t E;
+  uint64_t M;
+
+  std::string to_string() {
+    return std::to_string(sign) + " " + std::to_string(E) + " " +
+           std::to_string(M);
+  }
+};
+
+DoubleValue makeDoubleValue(uint8_t sign, uint16_t E, uint64_t M) {
+  EXPECT_LT(sign, 2);
+  EXPECT_LT(E, 2048);
+  EXPECT_LT(M, uint64_t{1} << 52);
+  uint64_t x = (static_cast<uint64_t>(sign) << 63) |
+               (static_cast<uint64_t>(E) << 52) | static_cast<uint64_t>(M);
+  double y;
+  memcpy(&y, &x, 8);
+  return DoubleValue{.d = y, .sign = sign, .E = E, .M = M};
+}
+
+template<typename T>
+VPackBuilder makeVPack(T x) {
+  VPackBuilder b;
+  b.add(VPackValue(x));
+  return b;
+}
+
 // -----------------------------------------------------------------------------
 // --SECTION--                                                        test suite
 // -----------------------------------------------------------------------------
@@ -254,5 +284,67 @@ TEST(VPackHelperTest, velocypack_string_literals) {
     })"_vpack;
     ASSERT_TRUE(obj.slice()["vertices"].isArray());
     ASSERT_TRUE(obj.slice()["edges"].isArray());
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// @brief test comparison of numerical values
+//////////////////////////////////////////////////////////////////////////////
+
+constexpr uint64_t mantmax = (uint64_t(1) << 52) - 1;
+
+using VelocyPackHelper = arangodb::basics::VelocyPackHelper;
+
+TEST(VPackHelperTest, test_comparison_numerical) {
+  // We create a vector of numerical velocypack values which is supposed
+  // to be sorted stricly ascending. We also check transitivity by comparing
+  // all pairs:
+  std::vector<VPackBuilder> v;
+  v.push_back(makeVPack(makeDoubleValue(1, 2047, 0).d));  // -Inf
+  for (uint16_t i = 2046; i >= 2; --i) {
+    v.push_back(makeVPack(makeDoubleValue(1, i, mantmax).d));
+    v.push_back(makeVPack(makeDoubleValue(1, i, 0).d));
+  }
+  v.push_back(makeVPack(makeDoubleValue(1, 1, mantmax).d));  // -2.0+2^-52
+  v.push_back(makeVPack(makeDoubleValue(1, 1, 0).d));        // - 1.0
+  v.push_back(makeVPack(makeDoubleValue(1, 0, mantmax).d));  // - denormalized
+  v.push_back(makeVPack(makeDoubleValue(1, 0, 1).d));        // - denormalized
+  // v.push_back(makeVPack(makeDoubleValue(1, 0, 0).d));        // - 0
+
+  v.push_back(makeVPack(makeDoubleValue(0, 0, 0).d));        // + 0
+  v.push_back(makeVPack(makeDoubleValue(0, 0, 1).d));        // + denormalized
+  v.push_back(makeVPack(makeDoubleValue(0, 0, mantmax).d));  // + denormalized
+  v.push_back(makeVPack(makeDoubleValue(0, 1, 0).d));        // + 1.0
+  v.push_back(makeVPack(makeDoubleValue(0, 1, mantmax).d));  // + 2.0-2^-52
+  for (uint16_t i = 2; i <= 2046; ++i) {
+    v.push_back(makeVPack(makeDoubleValue(0, i, 0).d));
+    v.push_back(makeVPack(makeDoubleValue(0, i, mantmax).d));
+  }
+  v.push_back(makeVPack(makeDoubleValue(0, 2047, 0).d));  // infinity
+
+  // Now check if our comparator agrees that this is strictly ascending:
+  for (std::size_t i = 0; i < v.size() - 1; ++i) {
+    EXPECT_EQ(-1, VelocyPackHelper::compareNumberValuesCorrectly(
+                      v[i].slice().type(), v[i].slice(), v[i + 1].slice()));
+  }
+}
+
+TEST(VPackHelperTest, test_equality_zeros) {
+  std::vector<VPackBuilder> v;
+  // +0.0:
+  v.push_back(makeVPack(makeDoubleValue(0, 0, 0).d));
+  // -0.0:
+  v.push_back(makeVPack(makeDoubleValue(1, 0, 0).d));
+  // uint64_t{0}:
+  v.push_back(makeVPack(VPackValue(uint64_t{0})));
+  // int64_t{0}:
+  v.push_back(makeVPack(VPackValue(int64_t{0})));
+  // smallint{0}:
+  v.push_back(makeVPack(VPackValue(0, VPackValueType::SmallInt)));
+  for (std::size_t i = 0; i < v.size(); ++i) {
+    for (std::size_t j = 0; j < v.size(); ++j) {
+      EXPECT_EQ(0, VelocyPackHelper::compareNumberValuesCorrectly(
+                       v[i].slice().type(), v[i].slice(), v[j].slice()));
+    }
   }
 }
