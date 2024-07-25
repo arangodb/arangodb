cb52de79f2b65328ffaeabcc2f32e8a760a7190b
Julia Volmer <julia.volmer@arangodb.com>
2024-07-25 22:43:12 +0200

    Start writing tests

---
+++ tests/Basics/CoroutineRegistryTest.cpp
@@ -0,0 +1,60 @@
+#include "Basics/async/async.h"
+#include "Basics/async/promise.hpp"
+#include "Basics/async/thread_registry.hpp"
+#include <gtest/gtest.h>
+#include <thread>
+
+using namespace arangodb;
+
+std::vector<uint> promise_lines;
+auto collect_promise_line(coroutine::PromiseInList* promise) -> void {
+  promise_lines.push_back(promise->_where.line());
+}
+
+namespace {
+
+struct CoroutineRegistryTest : ::testing::Test {
+  coroutine::ThreadRegistryForPromises thread_registry;
+  void SetUp() override { promise_lines = std::vector<uint>(); }
+};
+
+auto foo() -> async<int> { co_return 1; }
+auto bar() -> async<int> { co_return 4; }
+auto baz() -> async<int> { co_return 2; }
+
+}  // namespace
+
+TEST_F(CoroutineRegistryTest, includes_current_coroutine) {
+  thread_registry.create();
+  // auto coro = []() -> async<int> { co_return 1; }();
+  auto coro = foo();
+  thread_registry.for_promise(collect_promise_line);
+  EXPECT_EQ(promise_lines, std::vector<uint>{21});
+}
+
+TEST_F(CoroutineRegistryTest, includes_several_coroutines) {
+  thread_registry.create();
+  // auto coro = []() -> async<int> { co_return 1; }();
+  auto f = foo();
+  auto b = bar();
+  thread_registry.for_promise(collect_promise_line);
+  EXPECT_EQ(promise_lines, std::vector<uint>({22, 21}));
+}
+
+TEST_F(CoroutineRegistryTest, includes_coroutines_running_on_differen_threads) {
+  thread_registry.create();
+
+  auto f = foo();
+  auto b = bar();
+
+  auto thread = std::jthread([this]() {
+    thread_registry.create();
+
+    auto z = baz();
+
+    // we are sure that all threads still exist
+    thread_registry.for_promise(collect_promise_line);
+
+    EXPECT_EQ(promise_lines, std::vector<uint>({23, 22, 21}));
+  });
+}
