34cb5ecae062eabeb6a804f5e8c254c062e93c2c
Julia Volmer <julia.volmer@arangodb.com>
2024-07-29 13:41:16 +0200

    Add promise registry tests

--- lib/Basics/async/promise_registry.hpp
+++ lib/Basics/async/promise_registry.hpp
@@ -8,13 +8,14 @@
 #include <mutex>
 #include <source_location>
 #include <unordered_set>
+#include <thread>
 
 namespace arangodb::coroutine {
 
 /**
    List of coroutine promises on one thread.
 
-   add and remove are called on this thread, therefore they are not supposed
+   Add and remove are called on this thread, therefore they are not supposed
    to run concurrently. for_promise is called from a different thread and can
    run concurrently to add. But remove and for_promise are mutually exclusive
    via a mutex to make sure that for_promise always iterates over a valid list.
@@ -23,8 +24,16 @@ namespace arangodb::coroutine {
 struct PromiseRegistryOnThread {
   /**
      Add a promise on the current thread to the list.
+
+     Returns an error if this function is called on a differnt thread than the
+     thread the registry lives on.
    */
-  auto add(PromiseInList* promise) -> void {
+  auto add(PromiseInList* promise) -> Result {
+    if (std::this_thread::get_id() != thread_id) {
+      return Result{TRI_ERROR_INTERNAL,
+                    "You cannot add a promise of another thread to this "
+                    "promise registry."};
+    }
     auto current_head = head.load(std::memory_order_relaxed);
     promise->next.store(current_head, std::memory_order_relaxed);
     if (current_head != nullptr) {
@@ -32,14 +41,22 @@ struct PromiseRegistryOnThread {
     }
     // (1) sets value read by (2)
     head.store(promise, std::memory_order_release);
+    return Result{};
   }
 
   /**
      Erase a promise on the current thread.
 
      This removes the promise from the list and destroys the promise.
+     Returns an error if this function is called on a differnt thread than the
+     thread the registry lives on.
    */
-  auto erase(PromiseInList* promise) -> void {
+  auto erase(PromiseInList* promise) -> Result {
+    if (std::this_thread::get_id() != thread_id) {
+      return Result{TRI_ERROR_INTERNAL,
+                    "You cannot erase a promise of another thread from this "
+                    "promise registry."};
+    }
     auto guard = std::lock_guard(mutex);
     auto next = promise->next.load(std::memory_order_relaxed);
     auto previous = promise->previous.load(std::memory_order_relaxed);
@@ -54,6 +71,7 @@ struct PromiseRegistryOnThread {
       next->previous.store(previous, std::memory_order_relaxed);
     }
     // TODO delete promise
+    return Result{};
   }
 
   /**
@@ -72,6 +90,7 @@ struct PromiseRegistryOnThread {
     }
   }
 
+  const std::thread::id thread_id = std::this_thread::get_id();
   std::atomic<PromiseInList*> head = nullptr;
   std::atomic<std::shared_ptr<PromiseRegistryOnThread>> next;
   std::mutex mutex;
--- tests/Basics/async/PromiseRegistryTest.cpp
+++ tests/Basics/async/PromiseRegistryTest.cpp
@@ -1,18 +1,156 @@
-#include <gtest/gtest.h>
-#include <source_location>
 #include "Basics/async/promise.hpp"
 #include "Basics/async/promise_registry.hpp"
+#include "Basics/Result.h"
+
+#include <gtest/gtest.h>
+#include <source_location>
+#include <thread>
 
 using namespace arangodb::coroutine;
 
-TEST(PromiseRegistryTest, adds_a_promise_to_an_empty_list) {
-  auto promise = PromiseInList(std::source_location::current());
+TEST(PromiseRegistryTest, adds_a_promise) {
   auto promise_registry = PromiseRegistryOnThread{};
 
+  auto promise = PromiseInList(std::source_location::current());
+  auto line_nr = promise._where.line();
   promise_registry.add(&promise);
 
-  // thread_registry.create();
-  // auto coro = foo();
-  // thread_registry.for_promise(collect_promise_line);
-  // EXPECT_EQ(promise_lines, std::vector<uint>{21});
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+  EXPECT_EQ(promise_lines, std::vector<uint>{line_nr});
 }
+
+TEST(PromiseRegistryTest, another_thread_cannot_add_a_promise) {
+  auto promise_registry = PromiseRegistryOnThread{};
+
+  std::jthread([&]() {
+    auto promise = PromiseInList(std::source_location::current());
+    EXPECT_TRUE(promise_registry.add(&promise).fail());
+  });
+}
+
+TEST(PromiseRegistryTest, iterates_over_all_promises) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_1 = PromiseInList(std::source_location::current());
+  auto line_nr_1 = promise_1._where.line();
+  promise_registry.add(&promise_1);
+  auto promise_2 = PromiseInList(std::source_location::current());
+  auto line_nr_2 = promise_2._where.line();
+  promise_registry.add(&promise_2);
+  auto promise_3 = PromiseInList(std::source_location::current());
+  auto line_nr_3 = promise_3._where.line();
+  promise_registry.add(&promise_3);
+
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+
+  EXPECT_EQ(promise_lines,
+            (std::vector<uint>{line_nr_3, line_nr_2, line_nr_1}));
+}
+
+TEST(PromiseRegistryTest, iterates_in_another_thread_over_all_promises) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_1 = PromiseInList(std::source_location::current());
+  auto line_nr_1 = promise_1._where.line();
+  promise_registry.add(&promise_1);
+  auto promise_2 = PromiseInList(std::source_location::current());
+  auto line_nr_2 = promise_2._where.line();
+  promise_registry.add(&promise_2);
+  auto promise_3 = PromiseInList(std::source_location::current());
+  auto line_nr_3 = promise_3._where.line();
+  promise_registry.add(&promise_3);
+
+  std::jthread([&]() {
+    std::vector<uint> promise_lines;
+    promise_registry.for_promise([&](PromiseInList* promise) {
+      promise_lines.push_back(promise->_where.line());
+    });
+    EXPECT_EQ(promise_lines,
+              (std::vector<uint>{line_nr_3, line_nr_2, line_nr_1}));
+  });
+}
+
+TEST(PromiseRegistryTest, erases_a_promise_at_the_start_of_the_list) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_1 = PromiseInList(std::source_location::current());
+  promise_registry.add(&promise_1);
+  auto promise_2 = PromiseInList(std::source_location::current());
+  auto line_nr_2 = promise_2._where.line();
+  promise_registry.add(&promise_2);
+
+  promise_registry.erase(&promise_1);
+
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+  EXPECT_EQ(promise_lines, (std::vector<uint>{line_nr_2}));
+}
+
+TEST(PromiseRegistryTest, erases_a_promise_in_the_middle_of_the_list) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_1 = PromiseInList(std::source_location::current());
+  auto line_nr_1 = promise_1._where.line();
+  promise_registry.add(&promise_1);
+  auto promise_2 = PromiseInList(std::source_location::current());
+  promise_registry.add(&promise_2);
+  auto promise_3 = PromiseInList(std::source_location::current());
+  auto line_nr_3 = promise_3._where.line();
+  promise_registry.add(&promise_3);
+
+  promise_registry.erase(&promise_2);
+
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+  EXPECT_EQ(promise_lines, (std::vector<uint>{line_nr_3, line_nr_1}));
+}
+
+TEST(PromiseRegistryTest, erases_a_promise_at_the_end_of_the_list) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_1 = PromiseInList(std::source_location::current());
+  auto line_nr_1 = promise_1._where.line();
+  promise_registry.add(&promise_1);
+  auto promise_2 = PromiseInList(std::source_location::current());
+  promise_registry.add(&promise_2);
+
+  promise_registry.erase(&promise_2);
+
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+  EXPECT_EQ(promise_lines, (std::vector<uint>{line_nr_1}));
+}
+
+TEST(PromiseRegistryTest, another_thread_cannot_erase_a_promise) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise = PromiseInList(std::source_location::current());
+
+  std::jthread([&]() { EXPECT_TRUE(promise_registry.erase(&promise).fail()); });
+}
+
+// TODO
+TEST(PromiseRegistryTest, erases_a_existing_promise_although_not_in_list) {
+  auto promise_registry = PromiseRegistryOnThread{};
+  auto promise_in_list = PromiseInList(std::source_location::current());
+  auto line_nr = promise_in_list._where.line();
+  promise_registry.add(&promise_in_list);
+
+  auto free_promise = PromiseInList(std::source_location::current());
+  EXPECT_TRUE(promise_registry.erase(&free_promise).ok());
+
+  std::vector<uint> promise_lines;
+  promise_registry.for_promise([&](PromiseInList* promise) {
+    promise_lines.push_back(promise->_where.line());
+  });
+  EXPECT_EQ(promise_lines, (std::vector<uint>{line_nr}));
+}
+
+// TODO
+TEST(PromiseRegistryTest, does_not_delete_while_iterating) {}
