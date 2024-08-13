2d6b5aba9ebd0b3314a49ab12359d420628625b0
Julia Volmer <julia.volmer@arangodb.com>
2024-08-13 17:53:39 +0200

    Add basic feature

---
+++ arangod/AsyncRegistryServer/Feature.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ApplicationFeatures/ApplicationFeature.h"
+
+namespace arangodb::async_registry {
+
+class Feature final : public application_features::ApplicationFeature {
+ public:
+  static constexpr std::string_view name() { return "Coroutines"; }
+
+  template<typename Server>
+  Feature(Server& server)
+      : application_features::ApplicationFeature{
+            server, Server::template id<Feature>(), name()} {
+    // startsAfter<Bla, Server>();
+  }
+};
+
+}  // namespace arangodb::async_registry
