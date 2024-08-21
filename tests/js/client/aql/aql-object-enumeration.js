99514421921082716ec49b60c5b4dfbe72cac532
Alexey Bakharew <alex.bakharew@gmail.com>
2024-08-21 13:16:54 +0200

    Added more detailed tests

--- tests/js/client/aql/aql-object-enumeration.js
+++ tests/js/client/aql/aql-object-enumeration.js
@@ -54,30 +54,34 @@ function enumerateObjectTestSuite() {
     }
   };
 
-  function queryDocuments(coll, query, isOptimized = true) {
-    let options = {fullCount: true, optimizer: {rules: ["+replace-entries-with-object-iteration"]}};
+  function prepareStatement(query, bindVars, options) {
+    return db._createStatement({query: query, bindVars: bindVars, options: options});
+  }
+
+  function queryDocuments(coll, query, fullCount, isOptimized = true) {
+    let options = {fullCount: fullCount, optimizer: {rules: ["+replace-entries-with-object-iteration"]}};
     if (!isOptimized) {
       options["optimizer"]["rules"] = ["-replace-entries-with-object-iteration"];
     }
-    const stmt = db._createStatement({query: query, bindVars: null, options: options});
+    const stmt = prepareStatement(query, null, options);
     checkPlan(stmt.explain().plan, isOptimized);
 
-    return stmt.execute().toArray();
+    return stmt.execute();
   }
 
-  function queryDocumentsWithLimit(coll, isOptimized = true) {
+  function queryDocumentsWithLimit(coll, offset, count, fullCount, isOptimized = true) {
     const query = `
     FOR doc IN ${coll}
       FOR [key, value] IN ENTRIES(doc)
       SORT doc._key, key
-      LIMIT 12, 40
+      LIMIT ${offset}, ${count}
       RETURN [key, value]
     `;
 
-    return queryDocuments(coll, query, isOptimized);
+    return queryDocuments(coll, query, fullCount, isOptimized);
   }
 
-  function queryDocumentsNoLimit(coll, isOptimized = true) {
+  function queryDocumentsNoLimit(coll, fullCount, isOptimized = true) {
     const query = `
     FOR doc IN ${coll}
       FOR [key, value] IN ENTRIES(doc)
@@ -85,7 +89,7 @@ function enumerateObjectTestSuite() {
       RETURN [key, value]
     `;
 
-    return queryDocuments(coll, query, isOptimized);
+    return queryDocuments(coll, query, fullCount, isOptimized);
   }
 
   return {
@@ -101,6 +105,8 @@ function enumerateObjectTestSuite() {
       db[docCollection].insert({"key4": "e"});
       db[docCollection].insert({"key5": "f"});
       db[docCollection].insert({"key6": [1,2,3]});
+      db[docCollection].insert({"key7": 42});
+      db[docCollection].insert({"key8": {"subkey": {"a": 1}}});
 
       db[edgeColl].insert({_from: docCollection + "/key1", _to: docCollection + "/key4", "field": {"a": 1}});
       db[edgeColl].insert({_from: docCollection + "/key3", _to: docCollection + "/key4", "field": {"b": 2}});
@@ -112,23 +118,28 @@ function enumerateObjectTestSuite() {
       db._dropDatabase(database);
     },
 
-    testSimpleObjectIteration : function() {
+    testSimpleObjectIterationPlan : function() {
       const query = `
         FOR doc IN ${docCollection}
           FOR [key, value] IN ENTRIES(doc)
           RETURN [key, value]
       `;
 
-      const stmt = db._createStatement({query: query, bindVars: null, options: null});
-      checkPlan(stmt.explain().plan);
+      // with optimization
+      var stmt = prepareStatement(query, null, null);
+      checkPlan(stmt.explain().plan, true);
+
+      // without optimization
+      stmt = prepareStatement(query, null, {optimizer: {rules: ["-replace-entries-with-object-iteration"]}})
+      checkPlan(stmt.explain().plan, false);
     },
 
     testOutputDocumentCollection : function() {
-      let actual = queryDocumentsNoLimit(docCollection);
+      let actual = queryDocumentsNoLimit(docCollection).toArray();
 
       // Check that system fields are present
       let systemFields = actual.filter((e) => ["_key", "_rev", "_id"].includes(e[0]));
-      assertEqual(systemFields.length, 3 * 6); // 3 system fields * 6 documents in collection
+      assertEqual(systemFields.length, 3 * 8); // 3 system fields * 8 documents in collection
       actual = actual.filter((e) => !["_key", "_rev", "_id"].includes(e[0])).flat();
       let expected = [ 
         "key1", 
@@ -146,13 +157,17 @@ function enumerateObjectTestSuite() {
           1, 
           2, 
           3
-        ] 
+        ],
+        "key7",
+        42,
+        "key8",
+        {"subkey": {"a": 1}}
       ];
       assertEqual(actual, expected);
     },
 
     testOutputEdgeCollection : function() {
-      let actual = queryDocumentsNoLimit(edgeColl);
+      let actual = queryDocumentsNoLimit(edgeColl).toArray();
       // Check that system fields are present
       let systemFields = actual.filter((e) => ["_key", "_rev", "_id", "_from", "_to"].includes(e[0]));
       assertEqual(systemFields.length, 5 * 3); // 5 system fields * 3 documents in collection
@@ -169,12 +184,19 @@ function enumerateObjectTestSuite() {
       assertEqual(actual, expected);
     },
 
-    testOutputDocumentCollectionWithLimit : function() {
-      let actual = queryDocumentsWithLimit(docCollection);
+    testOutputDocumentCollectionWithLimit1 : function() {
+      // skip 12 first values (therefore 3 complete documents will be skipped), return 40.
+      // In our case, the count will be more than left documents in a collection
+      let actual = queryDocumentsWithLimit(docCollection, 12, 40, true);
       
+      // Check fullCount value
+      assertEqual(actual.getExtra()["stats"]["fullCount"], db[docCollection].count() * 4);
+
+      actual = actual.toArray();
       // Check that system fields are present
       let systemFields = actual.filter((e) => ["_key", "_rev", "_id"].includes(e[0]));
-      assertEqual(systemFields.length, 3 * 3); // 3 system fields * 3 non-skipped documents in collection
+      // First 3 documents were skipped (offset == 12)
+      assertEqual(systemFields.length, 3 * 5); // 3 system fields * 5 non-skipped documents in collection
       actual = actual.filter((e) => !["_key", "_rev", "_id"].includes(e[0])).flat();
       let expected = [
         "key4", 
@@ -186,11 +208,65 @@ function enumerateObjectTestSuite() {
           1, 
           2, 
           3
-        ] 
+        ],
+        "key7",
+        42,
+        "key8",
+        {"subkey": {"a": 1}}
       ];
       assertEqual(actual, expected);
-    }
+    },
 
+    testOutputDocumentCollectionWithLimit2 : function() {
+      // skip 4 first values (therefore 1 complete document will be skipped), return 4.
+      let actual = queryDocumentsWithLimit(docCollection, 4, 4, true);
+      
+      // Check fullCount value
+      assertEqual(actual.getExtra()["stats"]["fullCount"], db[docCollection].count() * 4);
+
+      actual = actual.toArray();
+      // Check that system fields are present
+      let systemFields = actual.filter((e) => ["_key", "_rev", "_id"].includes(e[0]));
+      // First 3 documents were skipped (offset == 12)
+      assertEqual(systemFields.length, 3 * 1); // 3 system fields * 1 non-skipped documents in collection
+      actual = actual.filter((e) => !["_key", "_rev", "_id"].includes(e[0])).flat();
+      let expected = [
+        "key2", 
+        "b"
+      ];
+      assertEqual(actual, expected);
+    },
+
+    testOutputDocumentCollectionWithLimit3 : function() {
+      // skip 5 (odd number) first values (therefore 1 complete document will be skipped. 
+      // The first system field of the second document is also skipped), return 4.
+      let actual = queryDocumentsWithLimit(docCollection, 5, 4, true);
+      
+      // Check fullCount value
+      assertEqual(actual.getExtra()["stats"]["fullCount"], db[docCollection].count() * 4);
+
+      actual = actual.toArray();
+      // Check that system fields are present
+      let systemFields = actual.filter((e) => ["_key", "_rev", "_id"].includes(e[0]));
+      assertEqual(systemFields.length, 3); // 3 system fields 
+      actual = actual.filter((e) => !["_key", "_rev", "_id"].includes(e[0])).flat();
+      let expected = [
+        "key2", 
+        "b"
+      ];
+      assertEqual(actual, expected);
+    },
+
+    testOutputDocumentCollectionWithLimit4 : function() {
+      // skip 400 first values (therefore all documents will be skipped), return 4.
+      let actual = queryDocumentsWithLimit(docCollection, 400, 4, true);
+      
+      // Check fullCount value
+      assertEqual(actual.getExtra()["stats"]["fullCount"], db[docCollection].count() * 4);
+
+      actual = actual.toArray();
+      assertTrue(actual.length == 0);
+    }
   };
 }
 
