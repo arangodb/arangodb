ad6c673475037215ed107b3c8b362231f15de84e
Wilfried Goesgens <willi@arangodb.com>
2024-09-27 13:51:44 +0200

    demand 2 coordinators, switch between them while running a transaction

--- tests/js/client/server_parameters/test-streaming-max-transaction-size.js
+++ tests/js/client/server_parameters/test-streaming-max-transaction-size.js
@@ -28,6 +28,9 @@
 if (getOptions === true) {
   return {
     'transaction.streaming-max-transaction-size': "16MB",
+    'opts': {
+      coordinators: 2
+    }
   };
 }
 const jsunity = require('jsunity');
@@ -164,6 +167,24 @@ function testSuite() {
       trx.commit();
       let doc = c.byExample({_key: '500'}).toArray()[0];
       assertTrue(doc.hasOwnProperty('aaa'), JSON.stringify(doc));
+    },
+    testStreamTransactionCluster: function () {
+      let IM = global.instanceManager;
+      print(IM.options)
+      if (IM.options.cluster) {
+        let coordinators = IM.arangods.filter(arangod => { return arangod.isFrontend();});
+        let count = 0;
+        let trx = db._createTransaction({ collections: { write: [cn], exclusive: [cn] } });
+        let c = trx.collection(cn);
+        while (count < 50){
+          coordinators[count %2].connect();
+          c.insert({count: count});
+          count ++;
+        }
+        trx.abort();
+        coordinators[0].connect();
+        assertEqual(db[cn].count(), 0);
+      }
     }
   };
 }
