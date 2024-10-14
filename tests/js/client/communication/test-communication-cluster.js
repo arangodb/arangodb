91d7fbf9eb71e65a3ac76ca7f72d9770a105cb11
Wilfried Goesgens <willi@arangodb.com>
2024-10-14 13:06:21 +0200

    prolongue timeout

--- tests/js/client/communication/test-communication-cluster.js
+++ tests/js/client/communication/test-communication-cluster.js
@@ -126,8 +126,8 @@ function CommunicationSuite() {
       db[cn].insert({ _key: "stop" }, { overwriteMode: "ignore" });
       let tries = 0;
       let done = 0;
-      const waitFor = isCov ? 60 * 4 : 60;
-      joinBGShells(IM.options, clients, waitFor);
+      const waitFor = isCov ? 80 * 4 : 80;
+      joinBGShells(IM.options, clients, waitFor, cn);
 
       assertEqual(1 + clients.length, db[cn].count());
       let stats = {};
