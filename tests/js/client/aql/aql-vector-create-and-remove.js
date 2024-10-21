5072b701575b122cdf36933198d5864786898705
Jure Bajic <jure.bajic94@gmail.com>
2024-10-21 14:54:58 +0200

    Fix eslint

--- tests/js/client/aql/aql-vector-create-and-remove.js
+++ tests/js/client/aql/aql-vector-create-and-remove.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertEqual, assertTrue */
+/*global assertEqual, assertTrue, assertNotEqual */
 
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
