26455d7000bf59c3c6abec34434d79546ccc49bc
Wilfried Goesgens <willi@arangodb.com>
2024-11-07 15:05:40 +0100

    QA-575: unwrap line breaks when printing out errors in testfailures.txt for better readability (#21412)
    * unwrap line breaks when printing out errors in testfailures.txt for better readability
    
    * make cause more visible
    
    * scan for interesting log lines so we may se asserts or fatal errors
    
    * dedebug
    
    * fix possible undefined access
    
    * fix possible undefined access
    
    * also find the INFO crash stacktrace
    
    * use deadline
    
    * missing include
    
    * fix variable reference

--- js/client/modules/@arangodb/testsuites/rta_makedata.js
+++ js/client/modules/@arangodb/testsuites/rta_makedata.js
@@ -43,6 +43,7 @@ const ct = require('@arangodb/testutils/client-tools');
 const tu = require('@arangodb/testutils/test-utils');
 const im = require('@arangodb/testutils/instance-manager');
 const inst = require('@arangodb/testutils/instance');
+const SetGlobalExecutionDeadlineTo = require('internal').SetGlobalExecutionDeadlineTo;
 const testRunnerBase = require('@arangodb/testutils/testrunner').testRunner;
 const yaml = require('js-yaml');
 const platform = require('internal').platform;
@@ -173,7 +174,17 @@ function makeDataWrapper (options) {
     localOptions.dbServers = 3;
   }
 
+  SetGlobalExecutionDeadlineTo(localOptions.oneTestTimeout * 1000);
   let rc = new rtaMakedataRunner(localOptions, 'rta_makedata_test').run(['rta']);
+  let timeout = SetGlobalExecutionDeadlineTo(0.0);
+  if (timeout) {
+    return {
+      timeout: true,
+      forceTerminate: true,
+      status: false,
+      message: `test aborted due to >>${require('internal').getDeadlineReasonString()}<<. Original test status: ${JSON.stringify(rc)}`,
+    };
+  }
   options.cleanup = options.cleanup && localOptions.cleanup;
   return rc;
 }
