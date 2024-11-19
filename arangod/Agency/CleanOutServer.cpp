beef5fe59fce51bb15c4c80377d571910584b9f7
Lars Maier <lars@arangodb.com>
2024-11-19 11:21:54 +0100

    Backport fix for BTS-2017.

--- arangod/Agency/CleanOutServer.cpp
+++ arangod/Agency/CleanOutServer.cpp
@@ -439,7 +439,7 @@ bool CleanOutServer::scheduleMoveShards(std::shared_ptr<Builder>& trx) {
 
             MoveShard(_snapshot, _agent, _jobId + "-" + std::to_string(sub++),
                       _jobId, database.first, collptr.first, shard.first,
-                      _server, toServer, isLeader, false)
+                      _server, toServer, isLeader, true)
                 .withParent(_jobId)
                 .create(trx);
 
