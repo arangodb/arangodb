c109c5a09d39b1e554d9be24f22099ac809eac76
Max Neunhoeffer <max@arangodb.com>
2024-11-25 14:35:54 +0100

    eslint.

--- tests/js/client/aql/aql-query-plan-cache.js
+++ tests/js/client/aql/aql-query-plan-cache.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertEqual, assertTrue, assertFalse, assertNotEqual, assertUndefined, assertNotUndefined, assertMatch, assertNotMatch  */
+/*global assertEqual, assertTrue, assertFalse, assertNotEqual, assertUndefined, assertNotUndefined, assertMatch, assertNotMatch, fail  */
 
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
