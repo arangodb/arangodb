14beeaef1332c02a5c7d1da9a0daecbcc0ac253e
Max Neunhoeffer <max@arangodb.com>
2024-12-04 14:50:43 +0100

    Backport from devel.

--- arangod/Agency/Supervision.cpp
+++ arangod/Agency/Supervision.cpp
@@ -1814,12 +1814,18 @@ void Supervision::cleanupFinishedAndFailedJobs() {
   // /Target/Failed. We can be rather generous here since old
   // snapshots and log entries are kept for much longer.
   // We only keep up to 500 finished jobs and 1000 failed jobs.
+  // Note that we must not throw away failed jobs which are subjobs
+  // of a larger job (e.g. MoveShard jobs in the context of a
+  // CleanOutServer job), or else the larger job can no longer
+  // detect any failures!
 
   constexpr size_t maximalFinishedJobs = 500;
   constexpr size_t maximalFailedJobs = 1000;
 
   auto cleanup = [&](std::string const& prefix, size_t limit) {
     auto const& jobs = snapshot().hasAsChildren(prefix).value().get();
+    auto const& pendingJobs =
+        snapshot().hasAsChildren(pendingPrefix).value().get();
     if (jobs.size() <= 2 * limit) {
       return;
     }
@@ -1827,6 +1833,18 @@ void Supervision::cleanupFinishedAndFailedJobs() {
     std::vector<keyDate> v;
     v.reserve(jobs.size());
     for (auto const& p : jobs) {
+      // Let's first see if this is a subjob of a larger job:
+      auto pos = p.first.find('-');
+      if (pos != std::string::npos) {
+        auto const& parent = p.first.substr(0, pos);
+        if (pendingJobs.find(parent) != nullptr) {
+          LOG_TOPIC("99887", TRACE, Logger::SUPERVISION)
+              << "Skipping removal of subjob " << p.first << " of parent "
+              << parent << " since the parent is still pending.";
+          continue;  // this is a subjob, and its parent is still pending, let's
+                     // keep it
+        }
+      }
       auto created = p.second->hasAsString("timeCreated");
       if (created) {
         v.emplace_back(p.first, *created);
@@ -2654,8 +2672,8 @@ auto parseSomethingFromNode(Node const& n) -> T {
 }
 
 template<typename T>
-auto parseIfExists(Node const& root, std::string const& url)
-    -> std::optional<T> {
+auto parseIfExists(Node const& root,
+                   std::string const& url) -> std::optional<T> {
   if (auto node = root.get(url); node.has_value()) {
     return parseSomethingFromNode<T>(node->get());
   }
@@ -2713,11 +2731,10 @@ auto replicatedLogOwnerGone(Node const& snapshot, Node const& node,
   return true;
 }
 
-auto handleReplicatedLog(Node const& snapshot, Node const& targetNode,
-                         std::string const& dbName, std::string const& idString,
-                         ParticipantsHealth const& health,
-                         arangodb::agency::envelope envelope)
-    -> arangodb::agency::envelope try {
+auto handleReplicatedLog(
+    Node const& snapshot, Node const& targetNode, std::string const& dbName,
+    std::string const& idString, ParticipantsHealth const& health,
+    arangodb::agency::envelope envelope) -> arangodb::agency::envelope try {
   if (replicatedLogOwnerGone(snapshot, targetNode, dbName, idString)) {
     auto logId = replication2::LogId{basics::StringUtils::uint64(idString)};
     return methods::deleteReplicatedLogTrx(std::move(envelope), dbName, logId);
