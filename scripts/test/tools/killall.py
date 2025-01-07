804cc5e26d48be9e9ecd291e8fc067351fd9e475
Wilfried Goesgens <willi@arangodb.com>
2025-01-07 16:55:24 +0100

    QA-585: analyse process metrics recorded during test runs (#21442)
    * even longer timeouts
    
    * even longer timeouts
    
    * add script to analyze process log
    
    * add more fields to remember
    
    * write file to arangosh pid lookup jsonl-file
    
    * add option to show only one PID
    
    * write system information as well
    
    * fix write mode
    
    * lookup of arangoshs testing.js names
    
    * start UI
    
    * try pyqt
    
    * modularize, try to work further displaying it
    
    * first line chart
    
    * display gantt chart and load graph at once
    
    * start adding a range selector, cleanup parameters
    
    * add range filtering
    
    * pandas -> polars
    
    * put stuff into a database, make more analysis
    
    * use arangodb as backend storage
    
    * add line chart of resources used
    
    * add possibility to choose the gauge
    
    * dig into one testrun
    
    * add option to switch away from stacked mode.
    
    * work on evaluation
    
    * sync to oskar
    
    * lint
    
    * lint
    
    * lint, but we don't use that here
    
    * cleanup
    
    ---------
    
    Co-authored-by: Vadim Kondratev <vadim@arangodb.com>

--- scripts/test/tools/killall.py
+++ scripts/test/tools/killall.py
@@ -6,12 +6,13 @@ import sys
 import logging
 import psutil
 
-
 def list_all_processes():
     """list all processes for later reference"""
     pseaf = "PID  Process"
     # pylint: disable=catching-non-exception
     for process in psutil.process_iter(["pid", "ppid", "name"]):
+        if  process.pid in [1, 2] or process.ppid() == 2:
+            continue
         cmdline = process.name
         try:
             cmdline = str(process.cmdline())
@@ -27,10 +28,8 @@ def list_all_processes():
     logging.info(pseaf)
     sys.stdout.flush()
 
-
 def kill_all_arango_processes():
     """list all processes for later reference"""
-    pseaf = "PID  Process"
     # pylint: disable=catching-non-exception
     for process in psutil.process_iter(["pid", "name"]):
         if (
@@ -55,6 +54,8 @@ def gather_process_thread_statistics(p):
     ret['process'] = [{
         'time': time.ctime(),
         'pid': p.pid,
+        'ppid': p.ppid(),
+        'cmdline': p.cmdline(),
         'name': p.name(),
         'percent': p.cpu_percent(),
         'iocounters': p.io_counters(),
@@ -77,25 +78,27 @@ def add_delta(p1, p2):
             p1[tid]['d_sys'] = p2[tid]['sys'] - p1[tid]['sys']
     p1['process'].append(p2['process'][0])
 
-def get_all_processes_stats_json():
+def get_all_processes_stats_json(load):
     """ aggregate a structure of all processes and their threads plus delta """
+    # pylint: disable=broad-exception-caught
     process_full_list = {}
-    for n in [True, False]:
-        processes = psutil.process_iter()
-        for process in processes:
-            name = ""
-            try:
-                name = process.name()
-                if  process.ppid() != 2 and process.pid not in [1, 2]:
-                    procstat = gather_process_thread_statistics(process)
-                    if n:
-                        process_full_list[f"p{process.pid}"] = procstat
-                    else:
-                        add_delta(process_full_list[f"p{process.pid}"], procstat)
-            except psutil.AccessDenied:
-                pass
-            except Exception as ex:
-                print(f"while inspecting {name}: {ex} ")
-        if n:
-            time.sleep(1)
+    process_full_list['sys'] = {
+        'load': load,
+        'vmem': psutil.virtual_memory(),
+        'mem': psutil.swap_memory(),
+        'diskio': psutil.disk_io_counters(perdisk=True, nowrap=True),
+        'netio': psutil.net_io_counters(pernic=True, nowrap=True),
+    }
+    processes = psutil.process_iter()
+    for process in processes:
+        name = ""
+        try:
+            name = process.name()
+            if  process.pid not in [1, 2] and process.ppid() != 2:
+                procstat = gather_process_thread_statistics(process)
+                process_full_list[f"p{process.pid}"] = procstat
+        except psutil.AccessDenied:
+            pass
+        except Exception as ex:
+            print(f"while inspecting {name}: {ex} ")
     return json.dumps(process_full_list)
