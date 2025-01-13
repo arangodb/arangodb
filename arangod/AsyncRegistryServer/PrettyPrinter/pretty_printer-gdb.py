7f0b4541d8c255e3b1557ecf012ac8b66ed75fa6
Julia Volmer <julia.volmer@arangodb.com>
2025-01-13 13:54:36 +0100

    Finish promise structure

--- arangod/AsyncRegistryServer/PrettyPrinter/pretty_printer-gdb.py
+++ arangod/AsyncRegistryServer/PrettyPrinter/pretty_printer-gdb.py
@@ -4,29 +4,78 @@ Parsing
 
 """
 
+class State:
+    def __init__(self, value):
+        self.name = value['_M_i']
+
+    def is_deleted(self):
+        return str(self.name) == "arangodb::async_registry::State::Deleted"
+
+    def __str__(self):
+        return str(self.name)
+    
+class Thread:
+    def __init__(self, value):
+        # TODO is there a way to get the thread name?
+        self.posix_id = value['posix_id']
+        self.lwpid = value['kernel_id']
+
+    def __str__(self):
+        return "thread LWPID: " + str(self.lwpid) + " & posix_id: " + str(self.posix_id)
+
+class PromiseId:
+    def __init__(self, value):
+       self.id = value 
+
+    def __str__(self):
+        return str(self.id)
+
 class Requester:
     def __init__(self, value):
-        self.alternative = value['_M_index'] # this is currently 0
-        self.value = value['_M_u']['_M_rest']['_M_first']['_M_storage'] # this is currently type void* so probably as PromiseId
+        self.value = value
+        self.alternative = value['_M_index']
+        if self.alternative == 0:
+            self.content = Thread(value['_M_u']['_M_first']['_M_storage'])
+        elif self.alternative == 1:
+            self.content = PromiseId(value['_M_u']['_M_rest']['_M_first']['_M_storage'])
+        else:
+            self.content = None
+
+    def is_valid(self):
+        return self.content != None
 
     def __str__(self):
-        return str(self.alternative) + ": " + str(self.value)
+        return str(self.content)
+
+class SourceLocation:
+    def __init__(self, value):
+        self.file_name = value['file_name']
+        self.function_name = value['function_name']
+        self.line = value['line']['_M_i']
+
+    def __str__(self):
+        return str(self.file_name) + ":" + str(self.line) + " " + str(self.function_name)
 
 class Promise:
+    # raw print in gdb: print /r *(registry.registries[0]._M_ptr).promise_head._M_b._M_p
+    
     def __init__(self, value_ptr):
-        self.id = value_ptr.address # TODO seems to be incorrect id
+        self.id = value_ptr
         value = value_ptr.dereference()
-        self.thread = value["thread"] # TODO add type
-        self.source_location = value["source_location"] # TODO add type
-        self.requester = Requester(value["requester"]['_M_i']) # TODO add type
-        self.state = value["state"] # TODO add type
+        self.thread = Thread(value["thread"])
+        self.source_location = SourceLocation(value["source_location"])
+        self.requester = Requester(value["requester"]['_M_i'])
+        self.state = State(value["state"])
+
+    def is_valid(self):
+        return not self.state.is_deleted() and self.requester.is_valid()
 
     def __str__(self):
-        return str(self.id) + ": " + str(self.state) + " - " + str(self.requester)
+        return str(self.id) + ": waiter " + str(self.requester) + " " + str(self.source_location) + ", " + str(self.thread) + ", " + str(self.state) 
 
 class PromiseList:
-    def __init__(self, value_ptr):
-        self.head_ptr = value_ptr
+    def __init__(self, begin):
+        self.head_ptr = begin
 
     def __iter__(self):
         current = self.head_ptr
@@ -41,8 +90,7 @@ class ThreadRegistry:
        self.promises = PromiseList(value["promise_head"]["_M_b"]["_M_p"])
 
     def __str__(self):
-        for promise in self.promises:
-            return str(promise)
+        return "\n".join(str(promise) for promise in filter(lambda x: x.is_valid(), self.promises))
     
 class ThreadRegistryList:
     def __init__(self, value):
@@ -50,14 +98,11 @@ class ThreadRegistryList:
         self.end = value["_M_finish"]
         
     def __iter__(self):
-        counter = 0
         current = self.begin
         while current != self.end:
-            thread_registry = ThreadRegistry(current.dereference()["_M_ptr"].dereference())
-            output = ("[%d]" % counter, str(thread_registry))
+            registry = ThreadRegistry(current.dereference()["_M_ptr"].dereference())
             current += 1
-            counter += 1
-            yield output
+            yield registry
 
 class AsyncRegistry:
     def __init__(self, value):
@@ -69,15 +114,17 @@ class AsyncRegistry:
         for registry in ThreadRegistryList(self.thread_registries["_M_impl"]):
             # TODO collect all promises in a forest
             count += 1
-            if count % 2 == 0:
-                self.stacktraces.append(registry);
-        return "bla_vector " + str(count)
+            self.stacktraces.append(registry);
+        return "async_registry on " + str(count) + " threads"
 
     def children(self):
         # TODO create stacktraces from the forest and yield each one
+        counter = 0
         for trace in self.stacktraces:
+            output = ("[%d]" % counter, str(trace))
+            counter += 1
             # keep in mind: this needs to yield a tuple (not sure if this is what we want for the stacktrace)
-            yield trace
+            yield output
 
 def lookup_type (val):
     if str(val.type) == 'arangodb::async_registry::Registry':
