1663351d464961e401ec55d533436adbb82e8b7c
Julia Volmer <julia.volmer@arangodb.com>
2025-01-17 14:49:10 +0100

    Make pretty_printer a python package

--- arangod/AsyncRegistryServer/PrettyPrinter/asyncregistry/src/asyncregistry/promise_data.py
+++ arangod/AsyncRegistryServer/PrettyPrinter/asyncregistry/src/asyncregistry/promise_data.py
@@ -1,41 +1,37 @@
-"""Async Registry Pretty Printer for gdb
-
-Parsing 
-
-"""
+from __future__ import annotations
 from typing import Iterable
 
 class State:
     def __init__(self, name: str):
-        self._name = name
+        self.name = name
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
         return cls(str(value['_M_i']))
 
     def is_deleted(self):
-        return self._name == "arangodb::async_registry::State::Deleted"
+        return self.name == "arangodb::async_registry::State::Deleted"
 
     def __str__(self):
-        return self._name
+        return self.name
     
 class Thread:
     def __init__(self, posix_id, lwpid):
         # TODO is there a way to get the thread name?
-        self._posix_id = posix_id
-        self._lwpid = lwpid
+        self.posix_id = posix_id
+        self.lwpid = lwpid
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
         return cls(value['posix_id'], value['kernel_id'])
 
     def __str__(self):
-        return "thread LWPID: " + str(self._lwpid) + " & posix_id: " + str(self._posix_id)
+        return "thread LWPID: " + str(self.lwpid) + " & posix_id: " + str(self.posix_id)
 
 class Requester:
     def __init__(self, alternative, content: Thread | PromiseId):
-        self._alternative = alternative
-        self._content = content
+        self.alternative = alternative
+        self.content = content
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
@@ -49,37 +45,37 @@ class Requester:
        return cls(alternative, content)
 
     def __str__(self):
-        return str(self._content)
+        return str(self.content)
 
 class SourceLocation:
     def __init__(self, file_name, function_name, line):
-        self._file_name = file_name
-        self._function_name = function_name
-        self._line = line
+        self.file_name = file_name
+        self.function_name = function_name
+        self.line = line
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
         return cls(value['file_name'], value['function_name'], value['line']['_M_i'])
 
     def __str__(self):
-        return str(self._file_name) + ":" + str(self._line) + " " + str(self._function_name)
+        return str(self.file_name) + ":" + str(self.line) + " " + str(self.function_name)
 
 class PromiseId:
     def __init__(self, id):
-        self._id = id
+        self.id = id
 
     def __str__(self):
-        return str(self._id)
-    
+        return str(self.id)
+
 class Promise:
     # raw print in gdb: print /r *(registry.registries[0]._M_ptr).promise_head._M_b._M_p
     
     def __init__(self, id: PromiseId, thread: Thread, source_location: SourceLocation, requester: Requester, state: State):
-        self._id = id
-        self._thread = thread
-        self._source_location = source_location
-        self._requester = requester
-        self._state = state
+        self.id = id
+        self.thread = thread
+        self.source_location = source_location
+        self.requester = requester
+        self.state = state
 
     @classmethod
     def from_gdb(cls, value_ptr: gdb.Value):
@@ -93,10 +89,10 @@ class Promise:
         )
 
     def is_valid(self):
-        return not self._state.is_deleted()
+        return not self.state.is_deleted()
 
     def __str__(self):
-        return str(self._id) + ": waiter " + str(self._requester) + " " + str(self._source_location) + ", " + str(self._thread) + ", " + str(self._state) 
+        return str(self.id) + ": waiter " + str(self.requester) + " " + str(self.source_location) + ", " + str(self.thread) + ", " + str(self.state) 
 
 class GdbAtomicList:
     def __init__(self, begin):
@@ -112,17 +108,14 @@ class GdbAtomicList:
 
 class ThreadRegistry:
     def __init__(self, promises: Iterable[MyTest]):
-       self._promises = promises
+       self.promises = promises
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
         return cls(Promise.from_gdb(promise) for promise in GdbAtomicList(value["promise_head"]["_M_b"]["_M_p"]))
 
-    def promises(self):
-        yield from self._promises
-    
     def __str__(self):
-        return "\n".join(str(promise) for promise in filter(lambda x: x.is_valid(), self._promises))
+        return "\n".join(str(promise) for promise in filter(lambda x: x.is_valid(), self.promises))
     
 # special gdb vector
 class GdbVector:
@@ -139,37 +132,13 @@ class GdbVector:
 
 class AsyncRegistry:
     def __init__(self, registries: Iterable[ThreadRegistry]):
-        self._thread_registries = registries
+        self.thread_registries = registries
 
     @classmethod
     def from_gdb(cls, value: gdb.Value):
         return cls(ThreadRegistry.from_gdb(registry) for registry in GdbVector(value['registries']['_M_impl']))
 
     def promises(self) -> Iterable[MyTest]:
-        for registry in self._thread_registries:
-            for promise in registry.promises():
+        for registry in self.thread_registries:
+            for promise in registry.promises:
                 yield promise
-
-
-class AsyncRegistryPrinter:
-    def __init__(self, value: gdb.Value):
-        self._registry = AsyncRegistry.from_gdb(value)
-
-    def to_string (self):
-        return "async registry bla"
-        # TODO forest = Forest(all_promises(ThreadRegistryList(self.thread_registries["_M_impl"])))
-
-    def children(self):
-        # TODO create stacktraces from the forest and yield each one
-        counter = 0
-        for promise in self._registry.promises():
-            output = ("[%d]" % counter, str(promise))
-            counter += 1
-            yield output
-
-def lookup_type (val):
-    if str(val.type) == 'arangodb::async_registry::Registry':
-        return AsyncRegistryPrinter(val)
-    return None
-
-gdb.pretty_printers.append (lookup_type)
