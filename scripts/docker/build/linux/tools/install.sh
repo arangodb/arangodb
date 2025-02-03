e08f7a27612afd4a2dce581937b56e43d998bd1c
Vadim Kondratev <vadim@arangodb.com>
2025-02-03 16:36:08 +0300

    make it build

--- scripts/docker/build/linux/tools/install.sh
+++ scripts/docker/build/linux/tools/install.sh
@@ -38,7 +38,7 @@ cd /tmp
 rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
 
 # Compile openldap library:
-export OPENLDAPVERSION=2.6.8
+export OPENLDAPVERSION=2.6.9
 cd /tmp
 curl -O ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
 tar xzf openldap-$OPENLDAPVERSION.tgz
