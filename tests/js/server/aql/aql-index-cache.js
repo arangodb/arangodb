606f0f3aec4fd6eb3d70d177322f907673635dc4
Jure Bajic <jure.bajic94@gmail.com>
2025-02-06 15:43:25 +0100

    Change number of buckets

--- tests/js/server/aql/aql-index-cache.js
+++ tests/js/server/aql/aql-index-cache.js
@@ -205,7 +205,7 @@ function FiguresSuite () {
 function VPackIndexCacheModifySuite (unique) {
   const n = 2000;
 
-  const maxTries = 10;
+  const maxTries = 5;
   
   let setFailurePointForPointLookup = () => {
     if (canUseFailAt) {
