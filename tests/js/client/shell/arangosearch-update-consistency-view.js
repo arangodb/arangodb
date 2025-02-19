a63c1e6dc7159bc6450d9047bfc9fddf0b70b7ab
Wilfried Goesgens <willi@arangodb.com>
2025-02-19 14:58:36 +0100

    split cases, reduce test actions

--- tests/js/client/shell/arangosearch-update-consistency-view.js
+++ tests/js/client/shell/arangosearch-update-consistency-view.js
@@ -34,22 +34,27 @@ function UpdateConsistencyArangoSearchView() {
   const collection = "testUpdate";
   const view = "testUpdateView";
   return {
-    setUp: function () {
+    setUpAll: function () {
       let collectionProperties = {};
       if (isCluster) {
         collectionProperties = {numberOfShards: 9, replicationFactor: 3};
       }
       let c = db._create(collection, collectionProperties);
+      c.save({_key: "279974", value: 0, flag: true});
     },
 
-    tearDown: function () {
+    tearDownAll: function () {
       db._dropView(view);
       db._drop(collection);
+    },
+    tearDown: function () {
+      let c = db._collection(collection);
+      c.update("279974", {value: 0, flag: true});
+      db._dropView(view);
     },
 
     testSingleUpdateIncludeAllFields: function () {
       let c = db._collection(collection);
-      c.save({_key: "279974", value: 0, flag: true});
       db._createView(view, "arangosearch", {links: {testUpdate: {includeAllFields: true}}});
 
       // Run waitForSync query for view index initialization
@@ -69,9 +74,9 @@ function UpdateConsistencyArangoSearchView() {
 
     testSingleUpdateOneLinkedField: function () {
       let c = db._collection(collection);
-      c.save({_key: "279974", value: 345, flag: true});
       db._createView(view, "arangosearch", {links: {testUpdate: {"fields": {"value": {"analyzers": ["identity"]}}}}});
 
+      c.update("279974", {value: 345, flag: true});
       // Run waitForSync query for view index initialization
       db._query("FOR d IN " + view + " SEARCH d.value >= 0 OPTIONS { waitForSync: true } RETURN d");      
 
@@ -87,7 +92,8 @@ function UpdateConsistencyArangoSearchView() {
 
     testSingleleUpdateUnlinkedField: function () {
       let c = db._collection(collection);
-      c.save({_key: "279974", value: 345, unlinkedField: 456, flag: true});
+      c.update("279974", {value: 345, unlinkedField: 456, flag: true});
+
       db._createView(view, "arangosearch", {links: {testUpdate: {"fields": {"value": {"analyzers": ["identity"]}}}}});
 
       // Run waitForSync query for view index initialization
@@ -152,114 +158,6 @@ function UpdateConsistencyArangoSearchView() {
   };
 }
 
-function UpdateConsistencySearchAliasView() {
-  const collection = "testUpdate";
-  const view = "testUpdateView";
-  return {
-    setUp: function () {
-      let collectionProperties = {};
-      if (isCluster) {
-        collectionProperties = {numberOfShards: 9, replicationFactor: 3};
-      }
-      let c = db._create(collection, collectionProperties);
-      c.save({_key: "279974", value: 345, flag: true});
-      c.ensureIndex({name: "inverted_index", type: "inverted", fields: ["value"]});
-      v = db._createView(view, "search-alias", {});
-    },
-
-    tearDown: function () {
-      db._dropView(view);
-      db._drop(collection);
-    },
-
-    testInvertedSingleUpdateLinkedField: function () {
-      let c = db._collection(collection);
-      v = db._view(view);
-      v.properties({indexes: [{collection: "testUpdate", index: "inverted_index"}]});
-
-      // Run waitForSync query for view index initialization
-      db._query("FOR d IN " + view + " SEARCH d.value >= 0 OPTIONS { waitForSync: true } RETURN d");
-      
-      for (let j = 0; j < 100; j++) {
-        let oldLen = db._query("FOR d IN " + view + " SEARCH d.value == 345 RETURN d").toArray().length;
-        assertEqual(oldLen, 1);
-        c.update("279974", {value: 345, flag: true});
-        let newLen = db._query("FOR d IN " + view + " SEARCH d.value == 345 RETURN d").toArray().length;
-        assertEqual(newLen, 1);
-        c.update("279974", {value: 345, flag: false});
-      }
-    },
-
-    testInvertedSingleleUpdateUnlinkedField: function () {
-      let c = db._collection(collection);
-      v = db._view(view);
-      v.properties({indexes: [{collection: "testUpdate", index: "inverted_index"}]});
-
-      // Run waitForSync query for view index initialization
-      db._query("FOR d IN " + view + " SEARCH d.value >= 0 OPTIONS { waitForSync: true } RETURN d");
-            
-      for (let j = 0; j < 100; j++) {
-        let oldLen = db._query("FOR d IN " + view + " SEARCH d.value == 345 RETURN d").toArray().length;
-        assertEqual(oldLen, 1);
-        c.update("279974", {unlinkedField: j, flag: true});
-        let newLen = db._query("FOR d IN " + view + " SEARCH d.value == 345 RETURN d").toArray().length;
-        assertEqual(newLen, 1);
-        c.update("279974", {unlinkedField: j, flag: false});
-      }
-    },
-
-    testMultiUpdates: function () {
-      let collectionProperties = {};
-      if (isCluster) {
-        collectionProperties = {numberOfShards: 9, replicationFactor: 3};
-      }
-
-      const amountOfCollections = 3;
-      const amountOfDocuments = 10;
-      const documents = [];
-
-      for (let i = 0; i < amountOfDocuments; i++) {
-        documents.push({_key: `doc${i}`, value: i});
-      }
-
-      v = db._view(view);
-      const insertedDocs = [];
-      for (let i = 0; i < amountOfCollections; i++) {
-        c = db._create(`testUpdate${i}`, collectionProperties);
-        c.ensureIndex({name: "inverted_index", type: "inverted", fields: ["value"]});
-        insertedDocs.push(db[`testUpdate${i}`].insert(documents));
-        v.properties({indexes: [{collection: `testUpdate${i}`, index: "inverted_index"}]});
-      }
-
-      // Run waitForSync query for view index initialization
-      db._query("FOR d IN " + view + " SEARCH d.value >= 0 OPTIONS { waitForSync: true } RETURN d");
-
-      for (let j = 1; j < 100; j++) {
-        let oldLen = db._query("FOR d IN " + view + " SEARCH d.value >= 0 RETURN d").toArray().length;
-        // print(db._query("FOR d IN " + view + " SEARCH d.value >= 0 RETURN d").toArray());
-        assertEqual(oldLen, amountOfCollections * amountOfDocuments);
-        for (let i = 0; i < amountOfCollections; i++) {
-          for (let j = 0; j < amountOfDocuments; j++) {
-            db[`testUpdate${i}`].update(`doc${j}`, {value: j, flag: true});
-          }
-        }
-        let newLen = db._query("FOR d IN " + view + " SEARCH d.value >= 0 RETURN d").toArray().length;
-        assertEqual(newLen, amountOfCollections * amountOfDocuments);
-        for (let i = 0; i < amountOfCollections; i++) {
-          for (let j = 0; j < amountOfDocuments; j++) {
-            db[`testUpdate${i}`].update(`doc${j}`, {value: j, flag: false});
-          }
-        }
-      }
-
-      for (let i = 0; i < amountOfCollections; i++) {
-        db._drop(`testUpdate${i}`, collectionProperties);
-      }
-    }
-  };
-}
-
 jsunity.run(UpdateConsistencyArangoSearchView);
-jsunity.run(UpdateConsistencySearchAliasView);
 
 return jsunity.done();
