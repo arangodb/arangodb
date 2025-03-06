00be08e9c8961ffab911256e5e55c1a1648cbf47
Frank Celler <frank@arangodb.com>
2025-03-06 10:23:38 +0100

    added '/_api/token' as exception for authorization

--- arangod/GeneralServer/CommTask.cpp
+++ arangod/GeneralServer/CommTask.cpp
@@ -64,6 +64,7 @@ namespace {
 // some static URL path prefixes
 constexpr std::string_view pathPrefixApi("/_api/");
 constexpr std::string_view pathPrefixApiUser("/_api/user/");
+constexpr std::string_view pathPrefixApiToken("/_api/token/");
 constexpr std::string_view pathPrefixAdmin("/_admin/");
 constexpr std::string_view pathPrefixAdminAardvark("/_admin/aardvark/");
 constexpr std::string_view pathPrefixOpen("/_open/");
@@ -840,6 +841,8 @@ CommTask::Flow CommTask::canAccessPath(auth::TokenCache::Entry const& token,
         result = Flow::Continue;
         vc->forceReadOnly();
       } else if (userAuthenticated && path.starts_with(::pathPrefixApiUser)) {
+        result = Flow::Continue;
+      } else if (userAuthenticated && path.starts_with(::pathPrefixApiToken)) {
         result = Flow::Continue;
       }
     }
