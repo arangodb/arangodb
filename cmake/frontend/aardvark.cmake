83ba331470da19314d187c6f61995ce73a814b0e
Wilfried Goesgens <willi@arangodb.com>
2025-04-23 10:45:54 +0200

    optionally choose pre-packaged node modules

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -7,10 +7,27 @@ find_package(Yarn REQUIRED)
 
 set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
 
+set(NODE_MODULES_BUNDLE /node_modules.tar.xz)
+
+
+if (EXISTS "${NODE_MODULES_BUNDLE}")
+  add_custom_target(frontend ALL
+    COMMENT "extracting node modules"
+    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+    COMMAND ${CMAKE_COMMAND} -E tar xvJf "${NODE_MODULES_BUNDLE}"
+    )
+else()
+  add_custom_target(frontend ALL
+    COMMENT "install node modules"
+    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+    COMMAND yarn install
+    )
+endif()
+
+
 add_custom_target(frontend ALL
   COMMENT "create frontend build"
   WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-  COMMAND yarn install
   COMMAND yarn build
 )
 
