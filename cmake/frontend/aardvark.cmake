8cf892e6a78fc677c8f5da9e7379ddffa4607170
Vadim Kondratev <vadim@arangodb.com>
2025-04-23 15:27:58 +0300

    Update aardvark.cmake

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -2,7 +2,7 @@
 ## web interface build
 ################################################################################
 
-find_package(Nodejs 18 REQUIRED)
+find_package(Nodejs 20 REQUIRED)
 find_package(Yarn REQUIRED)
 
 set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/APP/react)
@@ -10,17 +10,11 @@ set(FRONTEND_DESTINATION ${PROJECT_SOURCE_DIR}/js/apps/system/_admin/aardvark/AP
 option(NODE_MODULES_BUNDLE "specify pre-downloaded node modules for aardvark" "")
 
 
-if (EXISTS "${NODE_MODULES_BUNDLE}")
+if (NODE_MODULES_BUNDLE AND EXISTS "${NODE_MODULES_BUNDLE}")
   add_custom_target(frontend ALL
     COMMENT "extracting node modules"
     WORKING_DIRECTORY ${FRONTEND_DESTINATION}
     COMMAND ${CMAKE_COMMAND} -E tar xvJf "${NODE_MODULES_BUNDLE}"
-    )
-else()
-  add_custom_target(frontend ALL
-    COMMENT "install node modules"
-    WORKING_DIRECTORY ${FRONTEND_DESTINATION}
-    COMMAND yarn install
     )
 endif()
 
@@ -28,6 +22,7 @@ endif()
 add_custom_target(frontend ALL
   COMMENT "create frontend build"
   WORKING_DIRECTORY ${FRONTEND_DESTINATION}
+  COMMAND yarn install
   COMMAND yarn build
 )
 
