6f166d58bf80a8f9d184bf9732d76e84dde04907
Wilfried Goesgens <willi@arangodb.com>
2025-04-24 16:23:45 +0200

    set dependencies

--- cmake/frontend/aardvark.cmake
+++ cmake/frontend/aardvark.cmake
@@ -11,15 +11,20 @@ option(NODE_MODULES_BUNDLE "specify pre-downloaded node modules for aardvark" ""
 
 
 if (NODE_MODULES_BUNDLE AND EXISTS "${NODE_MODULES_BUNDLE}")
-  add_custom_target(frontend ALL
+  add_custom_target(node_cache ALL
     COMMENT "extracting node modules"
     WORKING_DIRECTORY ${FRONTEND_DESTINATION}
     COMMAND ${CMAKE_COMMAND} -E tar xvJf "${NODE_MODULES_BUNDLE}"
+    )
+else()
+  add_custom_target(node_cache ALL
+    COMMENT "not extracting node modules"
     )
 endif()
 
 
 add_custom_target(frontend ALL
+  DEPENDS node_cache
   COMMENT "create frontend build"
   WORKING_DIRECTORY ${FRONTEND_DESTINATION}
   COMMAND yarn install
