fd19ac5264e60b360008c5f42260ca4aa7a1b557
Kaveh Vahedipour <kaveh@arangodb.com>
2025-05-13 14:41:15 +0200

    rework

--- arangod/Aql/ModificationExecutorHelpers.cpp
+++ arangod/Aql/ModificationExecutorHelpers.cpp
@@ -250,36 +250,27 @@ void ModificationExecutorHelpers::waitAndDetach(
   if (!future.isReady()) {
     {
       using namespace std::literals::chrono_literals;
-      future.wait(std::chrono::steady_clock::now() + 100ms);
+      future.wait(std::chrono::steady_clock::now() + 1100ms);
     }
     if (!future.isReady()) {
-      auto const detachTime = std::chrono::milliseconds(
-          1000 + RandomGenerator::interval(uint32_t(100)) * 100);
-      auto start = std::chrono::steady_clock::now();
-      while (!future.isReady() &&
-             std::chrono::steady_clock::now() - start < detachTime) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      LOG_TOPIC("afe32", INFO, Logger::THREADS)
+        << "Did not get replication response within " << detachTime.count()
+        << " milliseconds, detaching scheduler thread.";
+      uint64_t currentNumberDetached = 0;
+      uint64_t maximumNumberDetached = 0;
+      auto res = SchedulerFeature::SCHEDULER->detachThread(
+        &currentNumberDetached, &maximumNumberDetached);
+      if (res.is(TRI_ERROR_TOO_MANY_DETACHED_THREADS)) {
+        LOG_TOPIC("afe33", WARN, Logger::THREADS)
+          << "Could not detach scheduler thread (currently detached "
+          "threads: "
+          << currentNumberDetached
+          << ", maximal number of detached threads: "
+          << maximumNumberDetached
+          << "), will continue to wait for replication in scheduler "
+          "thread, this can potentially lead to blockages!";
       }
-      if (!future.isReady()) {
-        LOG_TOPIC("afe32", INFO, Logger::THREADS)
-            << "Did not get replication response within " << detachTime.count()
-            << " milliseconds, detaching scheduler thread.";
-        uint64_t currentNumberDetached = 0;
-        uint64_t maximumNumberDetached = 0;
-        auto res = SchedulerFeature::SCHEDULER->detachThread(
-            &currentNumberDetached, &maximumNumberDetached);
-        if (res.is(TRI_ERROR_TOO_MANY_DETACHED_THREADS)) {
-          LOG_TOPIC("afe33", WARN, Logger::THREADS)
-              << "Could not detach scheduler thread (currently detached "
-                 "threads: "
-              << currentNumberDetached
-              << ", maximal number of detached threads: "
-              << maximumNumberDetached
-              << "), will continue to wait for replication in scheduler "
-                 "thread, this can potentially lead to blockages!";
-        }
-        future.wait();
-      }
+      future.wait();
     }
   }
 }
