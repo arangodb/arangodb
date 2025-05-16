af166b8f54972c2f87b69ec1869700d1e1ddd1f3
Heiko Kernbach <heiko@arangodb.com>
2025-05-16 11:27:54 +0200

    clang format

--- tests/Graph/DFSFinderTest.cpp
+++ tests/Graph/DFSFinderTest.cpp
@@ -174,7 +174,8 @@ class DFSFinderTest
   }
 
   auto pathFinder(size_t minDepth, size_t maxDepth) -> DFSFinder {
-    arangodb::graph::OneSidedEnumeratorOptions options{minDepth, maxDepth, *_query.get()};
+    arangodb::graph::OneSidedEnumeratorOptions options{minDepth, maxDepth,
+                                                       *_query.get()};
     PathValidatorOptions validatorOpts{&_tmpVar, _expressionContext};
     return DFSFinder(
         {*_query.get(),
