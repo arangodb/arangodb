a65e9dbdcd167426dec1245f533035890617ac3c
Wilfried Goesgens <willi@arangodb.com>
2025-05-30 11:35:02 +0200

    set default if container isn't there upsteam

--- scripts/docker/go-test/determine-tag.sh
+++ scripts/docker/go-test/determine-tag.sh
@@ -3,7 +3,7 @@
 image=$1
 arch=$2
 
-OS_VERSION=$(docker run --rm -it $image:latest-$arch cat /etc/os-release | grep -Po "VERSION_ID=\"\K\d+\.\d+")
+OS_VERSION=$((docker run --rm -it $image:latest-$arch cat /etc/os-release||echo 'VERSION_ID="24.04"') | grep -Po "VERSION_ID=\"\K\d+\.\d+")
 
 echo ${OS_VERSION}-$(git rev-parse --short HEAD)
 
