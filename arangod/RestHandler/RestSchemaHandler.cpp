529e205f6119497589518f5deb76c2b5a9ea2b2c
knakatasf <knakata@dons.usfca.edu>
2025-06-17 18:51:36 +0200

    feature(api): Added an endpoint _api/schema/<collection-name>

---
+++ arangod/RestHandler/RestSchemaHandler.cpp
@@ -0,0 +1,3 @@
+//
+// Created by koichi on 6/17/25.
+//
---
+++ arangod/RestHandler/RestSchemaHandler.h
@@ -0,0 +1,8 @@
+//
+// Created by koichi on 6/16/25.
+//
+
+#ifndef RESTSCHEMAHANDLER_H
+#define RESTSCHEMAHANDLER_H
+
+#endif //RESTSCHEMAHANDLER_H
