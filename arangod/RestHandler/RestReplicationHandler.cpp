fa32e9584e683ca45dd9a97b4da3661119ed0a11
Jure Bajic <jure.bajic94@gmail.com>
2025-07-07 18:42:50 +0200

    Make cancelBlockingTransaction async

--- arangod/RestHandler/RestReplicationHandler.cpp
+++ arangod/RestHandler/RestReplicationHandler.cpp
@@ -2858,7 +2858,7 @@ RestReplicationHandler::handleCommandHoldReadLockCollection() {
   // synchronize shard job
   bool isLeader = col->followers()->getLeader().empty();
   if (!isLeader) {
-    auto res = cancelBlockingTransaction(id);
+    auto res = co_await cancelBlockingTransaction(id);
     if (!res.ok()) {
       // this is potentially bad!
       LOG_TOPIC("957fa", WARN, Logger::REPLICATION)
@@ -2941,7 +2941,7 @@ void RestReplicationHandler::handleCommandCancelHoldReadLockCollection() {
   LOG_TOPIC("9a5e3", DEBUG, Logger::REPLICATION)
       << "Attempt to cancel Lock: " << id;
 
-  auto res = cancelBlockingTransaction(id);
+  auto res = cancelBlockingTransaction(id).waitAndGet();
   if (!res.ok()) {
     LOG_TOPIC("9caf7", DEBUG, Logger::REPLICATION)
         << "Lock " << id << " not canceled because of: " << res.errorMessage();
@@ -3630,24 +3630,24 @@ Result RestReplicationHandler::isLockHeld(TransactionId id) const {
   return {};
 }
 
-ResultT<bool> RestReplicationHandler::cancelBlockingTransaction(
-    TransactionId id) const {
+futures::Future<ResultT<bool>>
+RestReplicationHandler::cancelBlockingTransaction(TransactionId id) const {
   // This lookup is only required for API compatibility,
   // otherwise an unconditional destroy() would do.
   auto res = isLockHeld(id);
   if (res.ok()) {
     transaction::Manager* mgr = transaction::ManagerFeature::manager();
     if (mgr) {
-      auto isAborted = mgr->abortManagedTrx(id, _vocbase.name()).waitAndGet();
+      auto isAborted = co_await mgr->abortManagedTrx(id, _vocbase.name());
       if (isAborted.ok()) {  // lock was held
-        return ResultT<bool>::success(true);
+        co_return ResultT<bool>::success(true);
       }
-      return ResultT<bool>::error(isAborted);
+      co_return ResultT<bool>::error(isAborted);
     }
   } else {
     registerTombstone(id);
   }
-  return res;
+  co_return res;
 }
 
 futures::Future<ResultT<std::string>>
