4fa11c1a66044b68d9c896debf13fb1d9398027d
Jure Bajic <jure.bajic94@gmail.com>
2025-07-28 12:46:04 +0200

    Fix eslint

--- tests/js/client/server_parameters/restore-vector-index.js
+++ tests/js/client/server_parameters/restore-vector-index.js
@@ -1,5 +1,5 @@
 /* jshint globalstrict:false, strict:false, maxlen: 200 */
-/* global fail, arango */
+/* global fail, arango, getOptions */
 
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
