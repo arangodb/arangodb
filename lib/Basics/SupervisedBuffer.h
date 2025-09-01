1ddfd85a2f5de5fbf244994d81c2d75c6a8f8803
knakatasf <knakata@dons.usfca.edu>
2025-09-01 20:51:50 +0200

    Change usageScope of SupervisedBuffer to unique_ptr

--- lib/Basics/SupervisedBuffer.h
+++ lib/Basics/SupervisedBuffer.h
@@ -11,11 +11,11 @@ class SupervisedBuffer : public Buffer<uint8_t> {
   SupervisedBuffer() = delete;
 
   explicit SupervisedBuffer(arangodb::ResourceMonitor& monitor)
-      : _usageScope(monitor) {}
+      : _usageScope(std::make_unique<ResourceUsageScope>(monitor, 0)) {}
 
   uint8_t* steal() noexcept override {
     uint8_t* ptr = Buffer<uint8_t>::steal();
-    _usageScope.revert();
+    _usageScope->revert();
     return ptr;
   }
 
@@ -25,7 +25,7 @@ class SupervisedBuffer : public Buffer<uint8_t> {
     Buffer<uint8_t>::grow(length);
     auto newCapacity = this->capacity();
     if (newCapacity > currentCapacity) {
-      _usageScope.increase(newCapacity - currentCapacity);
+      _usageScope->increase(newCapacity - currentCapacity);
     }
   }
 
@@ -35,11 +35,11 @@ class SupervisedBuffer : public Buffer<uint8_t> {
     auto after = this->capacity();
     // if before > after, means that it has released usage from the heap
     if (before > after) {
-      _usageScope.decrease(before - after);
+      _usageScope->decrease(before - after);
     }
   }
 
-  arangodb::ResourceUsageScope _usageScope;
+  std::unique_ptr<ResourceUsageScope> _usageScope;
 };
 
 }  // namespace arangodb::velocypack
