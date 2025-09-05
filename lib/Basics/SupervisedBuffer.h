e69ac28212619c577f69c93f15fe1363a70b7c23
Julia CP <julia@arangodb.com>
2025-09-05 13:51:17 -0300

    Put supervised buffer header in the right place

--- lib/Basics/SupervisedBuffer.h
+++ lib/Basics/SupervisedBuffer.h
@@ -1,45 +1,68 @@
 #pragma once
 
-#include <memory>
-#include <velocypack/Buffer.h>
-#include "ResourceUsage.h"
 
-namespace arangodb::velocypack {
+#include "Aql/ResourceMonitor.h"
+#include "Aql/ResourceUsageScope.h"
 
-class SupervisedBuffer : public Buffer<uint8_t> {
+#include <vector>
+
+
+
+namespace arangodb {
+
+template<typename Byte = uint8_t>
+class SupervisedBuffer {
  public:
-  SupervisedBuffer() = delete;
 
-  explicit SupervisedBuffer(arangodb::ResourceMonitor& monitor)
-      : _usageScope(std::make_unique<ResourceUsageScope>(monitor, 0)) {}
+  explicit SupervisedBuffer(ResourceUsageScope& scope)
+      : _usageScope(&scope), _resourceMonitor(nullptr) {}
 
-  uint8_t* steal() noexcept override {
-    uint8_t* ptr = Buffer<uint8_t>::steal();
-    _usageScope->revert();
-    return ptr;
+
+  explicit SupervisedBuffer(ResourceMonitor& monitor)
+      : _usageScope(nullptr), _resourceMonitor(&monitor) {}
+
+
+  SupervisedBuffer() : _usageScope(nullptr), _resourceMonitor(nullptr) {}
+
+  void appendByte(Byte byte) {
+    reserve(_data.size() + 1);
+    _data.push_back(byte);
   }
+
+  void append(Byte const* data, size_t length) {
+    reserve(_data.size() + length);
+    _data.insert(_data.end(), data, data + length);
+  }
+
+
+  void reserve(size_t amountBytes) {
+    size_t currentCapacity = _data.capacity();
+    if (amountBytes > currentCapacity) {
+      if (_usageScope) {
+        _usageScope->increase(n - oldCap);
+      } else if (_resourceMonitor) {
+        _resourceMonitor->increaseMemoryUsage(n - oldCap);
+      }
+      _data.reserve(amountBytes);
+    }
+  }
+
+
+
+
+  void steal() {
+    if (_usageScope) {
+      _usageScope->steal();
+    }
+  }
+
+  const Byte* data() const noexcept { return _data.data(); }
+  size_t size() const noexcept { return _data.size(); }
+  size_t capacity() const noexcept { return _data.capacity(); }
 
  private:
-  void grow(ValueLength length) override {
-    auto currentCapacity = this->capacity();
-    Buffer<uint8_t>::grow(length);
-    auto newCapacity = this->capacity();
-    if (newCapacity > currentCapacity) {
-      _usageScope->increase(newCapacity - currentCapacity);
-    }
-  }
-
-  void clear() noexcept override {
-    auto before = this->capacity();
-    Buffer<uint8_t>::clear();
-    auto after = this->capacity();
-    // if before > after, means that it has released usage from the heap
-    if (before > after) {
-      _usageScope->decrease(before - after);
-    }
-  }
-
-  std::unique_ptr<ResourceUsageScope> _usageScope;
+  std::vector<Byte> _data;
+  ResourceUsageScope* _usageScope;
+  ResourceMonitor* _resourceMonitor;
 };
-
-}  // namespace arangodb::velocypack
+}  // namespace arangodb
