ca5277e67c75f551d369aa8288b5d6b542a3b07a
Wilfried Goesgens <willi@arangodb.com>
2025-09-24 12:03:37 +0200

    also need to be able to skip SUT-checkers that require server js

--- js/client/modules/@arangodb/testutils/testrunner.js
+++ js/client/modules/@arangodb/testutils/testrunner.js
@@ -81,11 +81,17 @@ class testRunner {
     this.shellTimeout = arango.timeout();
   }
   loadSutChecks(disableCheckFilter) {
+    const skipServerJS = this.options.skipServerJS;
     let sutCheckers = _.filter(fs.list(fs.join(__dirname, 'sutcheckers')),
                               function (p) {
                                 let extension = p.substr(-3);
                                 let basename = p.slice(0, -3);
+                                let filterByJs = true;
+                                if (skipServerJS) {
+                                  filterByJs = basename.indexOf('-sjs') === -1;
+                                }
                                 return (
+                                  filterByJs &&
                                   (extension === '.js') &&
                                   (disableCheckFilter.filter(f => f === basename).length === 0)
                                 );
