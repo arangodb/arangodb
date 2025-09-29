24205ab93d95248d6adc4ec5eff73e87335bd1db
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2025-09-29 12:48:32 +0100

    Try removing in/out parameter for good measure

--- arangod/Aql/Ast.cpp
+++ arangod/Aql/Ast.cpp
@@ -243,18 +243,18 @@ bool translateNodeStackToAttributePath(
  * @return The Category of this datasource (Collection or View), and a reference
  * to the translated name (cid => name if required).
  */
-LogicalDataSource::Category injectDataSourceInQuery(
-    Ast& ast, CollectionNameResolver const& resolver,
-    AccessMode::Type accessType, bool failIfDoesNotExist,
-    std::string_view& nameRef) {
-  // NOTE nameRef may be modified later if a numeric collection ID is given
-  // instead of a collection Name. Afterwards it will contain the name.
-  if (nameRef.empty()) {
+std::pair<LogicalDataSource::Category, std::string_view>
+injectDataSourceInQuery(Ast& ast, CollectionNameResolver const& resolver,
+                        AccessMode::Type accessType, bool failIfDoesNotExist,
+                        std::string_view name) {
+  // NOTE name may contain a numeric collection ID; in this case the string_view
+  // returned by this function will contain the resolved datasource name
+  if (name.empty()) {
     THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_ARANGO_ILLEGAL_NAME,
                                    "empty name collections are not supported");
   }
 
-  std::string const name = std::string(nameRef);
+  std::string_view resolved_name{name};
   auto const dataSource = resolver.getDataSource(name);
 
   if (dataSource == nullptr) {
@@ -270,10 +270,10 @@ LogicalDataSource::Category injectDataSourceInQuery(
     // for queries that are parsed-only (e.g. via `db._parse(query);`. In this
     // case it is ok that the datasource does not exist, but we need to track
     // the names of datasources used in the query
-    ast.query().collections().add(name, accessType,
+    ast.query().collections().add(std::string{name}, accessType,
                                   aql::Collection::Hint::None);
 
-    return LogicalDataSource::Category::kCollection;
+    return {LogicalDataSource::Category::kCollection, resolved_name};
   }
 
   // query actual name from datasource... this may be different to the
@@ -287,7 +287,7 @@ LogicalDataSource::Category injectDataSourceInQuery(
     // name on the heap and update our std::string_view to point to it
     char* p = ast.resources().registerString(dataSourceName.data(),
                                              dataSourceName.size());
-    nameRef = std::string_view(p, dataSourceName.size());
+    resolved_name = std::string_view(p, dataSourceName.size());
   }
 
   if (dataSource->category() == LogicalDataSource::Category::kCollection) {
@@ -297,16 +297,16 @@ LogicalDataSource::Category injectDataSourceInQuery(
     if (ServerState::instance()->isDBServer()) {
       hint = aql::Collection::Hint::Shard;
     }
-    ast.query().collections().add(std::string(nameRef), accessType, hint);
-    if (nameRef != name) {
-      ast.query().collections().add(name, accessType,
+    ast.query().collections().add(std::string(resolved_name), accessType, hint);
+    if (resolved_name != name) {
+      ast.query().collections().add(std::string{name}, accessType,
                                     hint);  // Add collection by ID as well
     }
   } else if (dataSource->category() == LogicalDataSource::Category::kView) {
     // it's a view!
     // add views to the collection list
     // to register them with transaction as well
-    ast.query().collections().add(std::string(nameRef), accessType,
+    ast.query().collections().add(std::string(name), accessType,
                                   aql::Collection::Hint::None);
 
     ast.query().addDataSource(*dataSource);
@@ -324,16 +324,14 @@ LogicalDataSource::Category injectDataSourceInQuery(
                                    "unexpected datasource type");
   }
 
-  return dataSource->category();
+  return {dataSource->category(), resolved_name};
 }
 
 LogicalDataSource::Category addDataSource(
     Ast& ast, CollectionNameResolver const& resolver,
     std::string_view& nameRef) {
-  //  std::string_view nameRef(name);
-
-  auto category = injectDataSourceInQuery(ast, resolver, AccessMode::Type::READ,
-                                          false, nameRef);
+  auto [category, resolved_name] = injectDataSourceInQuery(
+      ast, resolver, AccessMode::Type::READ, false, nameRef);
 
   if (category == LogicalDataSource::Category::kCollection) {
     if (ServerState::instance()->isCoordinator()) {
@@ -342,14 +340,14 @@ LogicalDataSource::Category addDataSource(
                      .server()
                      .getFeature<ClusterFeature>()
                      .clusterInfo();
-      auto c = ci.getCollectionNT(ast.query().vocbase().name(), nameRef);
+      auto c = ci.getCollectionNT(ast.query().vocbase().name(), resolved_name);
       if (c != nullptr) {
         auto const& names = c->realNames();
 
         for (auto const& n : names) {
           std::string_view shardsNameRef(n);
 
-          LogicalDataSource::Category shardsCategory = injectDataSourceInQuery(
+          auto [shardsCategory, _] = injectDataSourceInQuery(
               ast, resolver, AccessMode::Type::READ, false, shardsNameRef);
 
           TRI_ASSERT(shardsCategory ==
@@ -962,7 +960,7 @@ AstNode* Ast::createNodeDataSource(CollectionNameResolver const& resolver,
   // will throw if validation fails
   validateDataSourceName(name, validateName);
   // this call may update name
-  LogicalCollection::Category category = injectDataSourceInQuery(
+  auto [category, resolved_name] = injectDataSourceInQuery(
       *this, resolver, accessType, failIfDoesNotExist, name);
 
   if (category == LogicalDataSource::Category::kCollection) {
@@ -970,7 +968,7 @@ AstNode* Ast::createNodeDataSource(CollectionNameResolver const& resolver,
   }
   if (category == LogicalDataSource::Category::kView) {
     AstNode* node = createNode(NODE_TYPE_VIEW);
-    node->setStringValue(name.data(), name.size());
+    node->setStringValue(resolved_name.data(), resolved_name.size());
     return node;
   }
   // injectDataSourceInQuery is supposed to throw in this case.
@@ -987,16 +985,16 @@ AstNode* Ast::createNodeCollection(CollectionNameResolver const& resolver,
   // will throw if validation fails
   validateDataSourceName(name, true);
   // this call may update name
-  LogicalCollection::Category category =
+  auto [category, resolved_name] =
       injectDataSourceInQuery(*this, resolver, accessType, false, name);
 
   if (category == LogicalDataSource::Category::kCollection) {
     // add collection to query
-    _query.collections().add(std::string(name), accessType,
+    _query.collections().add(std::string(resolved_name), accessType,
                              Collection::Hint::Collection);
 
     // call private function after validation
-    return createNodeCollectionNoValidation(name, accessType);
+    return createNodeCollectionNoValidation(resolved_name, accessType);
   }
   THROW_ARANGO_EXCEPTION_MESSAGE(
       TRI_ERROR_ARANGO_COLLECTION_TYPE_MISMATCH,
