02dc09a3d866a754a6752d08ec33f68245e6d49b
Wilfried Goesgens <willi@arangodb.com>
2025-11-13 11:05:36 +0100

    fix test code, specify experimental vector index on our own

--- js/client/modules/@arangodb/testsuites/dump.js
+++ js/client/modules/@arangodb/testsuites/dump.js
@@ -424,7 +424,7 @@ function dumpEncrypted (options) {
   };
 
   let dumpOptions = _.clone(options);
-  dumpAuthOpts.extraArgs['experimental-vector-index'] = true;
+  dumpOptions.extraArgs['experimental-vector-index'] = true;
   dumpOptions.encrypted = true;
   dumpOptions.compressed = true; // Should be overruled by 'encrypted'
   dumpOptions.dbServers = 3;
@@ -449,7 +449,7 @@ function dumpNonParallel (options) {
   dumpOptions.useParallelDump = false;
   dumpOptions.splitFiles = false;
   dumpOptions.dbServers = 3;
-  dumpAuthOpts.extraArgs['experimental-vector-index'] = true;
+  dumpOptions.extraArgs['experimental-vector-index'] = true;
 
   let tstFiles = {
     dumpSetup: 'dump-setup' + c.cluster + '.js',
