93e908cbb6266cd99e5649a685c37bb18ccac000
Wilfried Goesgens <willi@arangodb.com>
2025-11-24 17:52:54 +0100

    fix search of items

--- js/client/modules/@arangodb/testsuites/hotbackup.js
+++ js/client/modules/@arangodb/testsuites/hotbackup.js
@@ -328,13 +328,13 @@ while(true) {
             `).toArray().sort();
       let notFoundQ = [];
       p.forEach(oneP => {
-        if (!(oneP in q)) {
+        if (q.find(n => n === oneP) !== oneP) {
           notFoundQ.push(oneP);
         }
       });
       let notFoundP = [];
       q.forEach(oneQ => {
-        if (!(oneQ in p)) {
+        if (p.find(n => n === oneQ) !== oneQ) {
           notFoundP.push(oneQ);
         }
       });
