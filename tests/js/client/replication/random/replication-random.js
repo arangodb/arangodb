7824947eec1214e03cf96d0fb62cd4217147d851
Wilfried Goesgens <willi@arangodb.com>
2025-12-11 10:16:18 +0100

    lint

--- tests/js/client/replication/random/replication-random.js
+++ tests/js/client/replication/random/replication-random.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, unused: false */
-/*global assertEqual, assertTrue, arango, ARGUMENTS */
+/*global assertEqual, assertTrue, arango, ARGUMENTS, SYS_IS_V8_BUILD, print */
 
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
