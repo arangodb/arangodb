BlackRoad-OS/arangodb-1

Fix smart traversals
Smart traversals on coordinator need to to use the non-batched queue
at the moment. With the batched queue, the SmartGraphProvider needs at
least to implement addExpansionIterator and expandToNextBatch
properly, possibly more is needed.

arangod/Graph/algorithm-aliases.h
@@ -41,7 +41,12 @@
 #include "Graph/PathManagement/PathValidatorTabooWrapper.h"
 #include "Graph/PathManagement/PathValidatorTracer.h"
 #include "Graph/Providers/ProviderTracer.h"
+#include "Graph/Steps/ClusterProviderStep.h"
 #include "Graph/Types/UniquenessLevel.h"
+
+#ifdef USE_ENTERPRISE
+#include "Enterprise/Graph/Providers/SmartGraphProvider.h"
+#endif
 
 namespace arangodb::graph {
 
@@ -226,9 +231,17 @@ struct DFSConfiguration {
       typename std::conditional<useTracing, ProviderTracer<ProviderType>,
                                 ProviderType>::type;
   using Step = typename Provider::Step;
+
+  // smart traversal queue on coordinator needs to be non-batched
+  // for that, SmartGraphProvider fns addExpansionIterator and expandToNextBatch
+  // need to be implemented
+  using batched = typename std::conditional<
+      std::is_same_v<ProviderType,
+                     enterprise::SmartGraphProvider<ClusterProviderStep>>,
+      LifoQueue<Step>, BatchedLifoQueue<Step>>::type;
   using Queue =
       typename std::conditional<useTracing, QueueTracer<LifoQueue<Step>>,
-                                BatchedLifoQueue<Step>>::type;
+                                batched>::type;
   using Store =
       typename std::conditional<useTracing, PathStoreTracer<PathStore<Step>>,
                                 PathStore<Step>>::type;

arangodb/arangodb

arangod/Graph/algorithm-aliases.h
@@ -37,7 +37,12 @@
 #include "Graph/PathManagement/PathStore.h"
 #include "Graph/PathManagement/PathValidator.h"
 #include "Graph/PathManagement/PathValidatorTabooWrapper.h"
+#include "Graph/Steps/ClusterProviderStep.h"
 #include "Graph/Types/UniquenessLevel.h"
+
+#ifdef USE_ENTERPRISE
+#include "Enterprise/Graph/Providers/SmartGraphProvider.h"
+#endif
 
 namespace arangodb::graph {
 
@@ -114,7 +119,13 @@ template<class ProviderType, VertexUniquenessLevel vertexUniqueness,
 struct DFSConfiguration {
   using Provider = ProviderType;
   using Step = typename Provider::Step;
-  using Queue = BatchedLifoQueue<Step>;
+  // smart traversal queue on coordinator needs to be non-batched
+  // for that, SmartGraphProvider fns addExpansionIterator and
+  // expandToNextBatch need to be implemented
+  using Queue = typename std::conditional<
+      std::is_same_v<ProviderType,
+                     enterprise::SmartGraphProvider<ClusterProviderStep>>,
+      LifoQueue<Step>, BatchedLifoQueue<Step>>::type;
   using Store = PathStore<Step>;
   using Validator =
       PathValidator<Provider, Store, vertexUniqueness, edgeUniqueness>;