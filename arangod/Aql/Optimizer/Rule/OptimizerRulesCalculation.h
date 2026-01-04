2bff30fa662fa2eb02b7d9f0c6f01cbeabeeb45e
knakatasf <knakata@dons.usfca.edu>
2026-01-04 18:03:49 +0100

    Add OptimizerRulesCalculation.h and cpp files

---
+++ arangod/Aql/Optimizer/Rule/OptimizerRulesCalculation.h
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2025 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Max Neunhoeffer
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Aql/ExecutionPlan.h"
+
+namespace arangodb::aql {
+class Optimizer;
+
+/// @brief move calculations up in the plan
+/// this rule modifies the plan in place
+/// it aims to move up calculations as far up in the plan as possible, to
+/// avoid redundant calculations in inner loops
+void moveCalculationsUpRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
+                            OptimizerRule const&);
+
+/// @brief move calculations down in the plan
+/// this rule modifies the plan in place
+/// it aims to move down calculations as far down in the plan as possible,
+/// beyond FILTER and LIMIT statements
+void moveCalculationsDownRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
+                              OptimizerRule const&);
+
+/// @brief remove redundant CalculationNodes
+void removeRedundantCalculationsRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
+                                     OptimizerRule const&);
+
+/// @brief remove CalculationNodes and SubqueryNodes that are never needed
+void removeUnnecessaryCalculationsRule(Optimizer*,
+                                       std::unique_ptr<ExecutionPlan>,
+                                       OptimizerRule const&);
+
+}  // namespace arangodb::aql
