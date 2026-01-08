1299b43c4bd65dedc6441db6474f770fa848dd96
knakatasf <knakata@dons.usfca.edu>
2026-01-09 00:40:15 +0100

    Revert time limit

--- tests/js/client/shell/shell-index-link-parallel-drop-create.js
+++ tests/js/client/shell/shell-index-link-parallel-drop-create.js
@@ -35,7 +35,7 @@ const {
   cleanupBGShells
 } = require('@arangodb/testutils/client-tools').run;
 let IM = global.instanceManager;
-const waitFor = IM.options.isInstrumented ? 80 * 10 : 200;
+const waitFor = IM.options.isInstrumented ? 80 * 10 : 80;
 
 function ParallelIndexLinkCreateDropSuite() {
   'use strict';
