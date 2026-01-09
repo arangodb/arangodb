7ce6a8e0cfebc82100e087a5e266c52233386b2c
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-01-09 17:18:14 +0000

    Slightly more tests

--- tests/js/client/aql/aql-nested-subquery-modification-regression.js
+++ tests/js/client/aql/aql-nested-subquery-modification-regression.js
@@ -30,7 +30,7 @@ const db = require("@arangodb").db;
 
 const colName = "UnitTestCollection";
 
-const tearDownAll = () => {
+const tearDown = () => {
   db._drop(colName);
 };
 const createCollection = () => {
@@ -43,12 +43,11 @@ const createCollection = () => {
 
 function subqueryModificationRegressionSuite() {
   return {
-    setUpAll: function() {
-      tearDownAll();
+    setUp: function() {
+      tearDown();
       createCollection();
     },
-    tearDownAll,
-
+    tearDown,
     testNestedSubqueryInsert: function () {
       // We only got this query to crash on cluster setups, as
       // the distribute/gather combination deletes defaultCalls
@@ -76,6 +75,146 @@ function subqueryModificationRegressionSuite() {
           LIMIT 1,10
           RETURN {})
         RETURN {}`;
+
+      const r = db._query(query);
+    },
+    testNestedSubqueryRemoveWithLimitAndFilter: function() {
+      const prepQuery = `INSERT {_key: "testDocument"} IN ${colName} RETURN {}`;
+      db._query(prepQuery);
+      const query = `
+        LET sq0 = (
+          LET sq1 = (
+            LET sq2 = (REMOVE {_key: "testDocument"} IN ${colName} RETURN {} )
+            RETURN {})
+          FILTER NOOPT(true)
+          LIMIT 1,10
+          RETURN {})
+        RETURN {}`;
+      const r = db._query(query);
+
+      const check = db._query(`FOR x IN ${colName} RETURN x`).toArray();
+      assertEqual(check.length, 0);
+    },
+    testNestedSubqueryUpdateWithLimitAndFilter: function() {
+      // This query even crashes on single server as the
+      // LIMIT/FILTER combination induces the same problem as with the
+      // query above
+      const prepQuery = `INSERT {_key: "testDocument", testAttr: 1} IN ${colName} RETURN {}`;
+      db._query(prepQuery);
+      const query = `
+        LET sq0 = (
+          LET sq1 = (
+            LET sq2 = (UPDATE {_key: "testDocument", testAttr: 0} IN ${colName} RETURN {} )
+            RETURN {})
+          FILTER NOOPT(true)
+          LIMIT 1,10
+          RETURN {})
+        RETURN {}`;
+
+      const r = db._query(query);
+
+      const check = db._query(`FOR x IN ${colName} RETURN x`).toArray();
+      assertEqual(check.length, 1);
+
+      assertEqual(check[0].testAttr, 0);
+    },
+    testNestedSubqueryUpdateWithLimitAndFilter: function() {
+     const prepQuery = `INSERT {_key: "testDocument", testAttr: 1} IN ${colName} RETURN {}`;
+      db._query(prepQuery);
+      const query = `
+        LET sq0 = (
+          LET sq1 = (
+            LET sq2 = (UPDATE {_key: "testDocument", testAttr: 0} IN ${colName} RETURN {} )
+            RETURN {})
+          FILTER NOOPT(true)
+          LIMIT 1,10
+          RETURN {})
+        RETURN {}`;
+
+      const r = db._query(query);
+
+      const check = db._query(`FOR x IN ${colName} RETURN x`).toArray();
+      assertEqual(check.length, 1);
+
+      assertEqual(check[0].testAttr, 0);
+    },
+    testNestedSubqueryReplaceWithLimitAndFilter: function() {
+      const prepQuery = `INSERT {_key: "testDocument", testAttr: 1} IN ${colName} RETURN {}`;
+      db._query(prepQuery);
+      const query = `
+        LET sq0 = (
+          LET sq1 = (
+            LET sq2 = (UPDATE {_key: "testDocument", testAttr: 0} IN ${colName} RETURN {} )
+            RETURN {})
+          FILTER NOOPT(true)
+          LIMIT 1,10
+          RETURN {})
+        RETURN {}`;
+
+      const r = db._query(query);
+
+      const check = db._query(`FOR x IN ${colName} RETURN x`).toArray();
+      assertEqual(check.length, 1);
+
+      assertEqual(check[0].testAttr, 0);
+    },
+    testNestedSubqueryUpsertWithLimitAndFilter: function() {
+     const query = `
+        LET sq0 = (
+          FOR x IN 1..1001
+            LET sq1 = (
+              LET sq2 = (UPSERT { _key: 'test' }
+                         INSERT { _key: 'test', value: [x] }
+                         UPDATE { value: APPEND(OLD.value, [x]) } IN ${colName} RETURN NEW)
+              RETURN sq2)
+            FILTER NOOPT(true)
+            LIMIT 1,10
+            RETURN sq1)
+        RETURN sq0`;
+
+      const r = db._query(query);
+
+      const r2 = db._query( `FOR x IN ${colName} FILTER x._key == 'test' RETURN x`);
+      const arr = r2.toArray();
+      assertEqual(arr.length, 1);
+      assertEqual(arr[0].value.length, 1001);
+    },
+    testNestedSubqueryInsertWithLimitAndFilter2: function() {
+      const query = `
+        LET sq0 = (
+          FOR y IN 1..20
+          LET sq1 = (
+            FOR x IN 1..1001
+              LET sq2 = (INSERT {v: TO_STRING(x) } INTO ${colName} RETURN NEW)
+              LIMIT 1000,1
+              FILTER NOOPT(true)
+              RETURN sq2)
+          FILTER NOOPT(true)
+          LIMIT 1,10
+          RETURN sq1)
+        LIMIT 5,100
+        RETURN sq0`;
+
+      const r = db._query(query);
+    },
+    testDeeplyNestedSubquery: function() {
+     const query = `
+        LET sq0 = (
+          LET sq1 = (
+            LET sq2 = (
+              LET sq3 = (
+                LET sq4 = (
+                  FOR x IN 1..1001
+                    LET sq5 = (INSERT {v: TO_STRING(x) } INTO ${colName} RETURN NEW)
+                    LIMIT 900,120
+                    RETURN sq5)
+                FILTER NOOPT(true)
+                LIMIT 1,10
+                RETURN sq4)
+              RETURN sq3)
+            RETURN sq2)
+          RETURN sq1)
+        RETURN sq0`;
 
       const r = db._query(query);
     },
