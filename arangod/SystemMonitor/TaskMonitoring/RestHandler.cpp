df5de602c3fc6c78f4c9e0ee9b2ecaf4550065fd
Julia Volmer <julia.volmer@arangodb.com>
2026-01-12 16:58:21 +0100

    Extract forwarding to RestBaseHandler

--- arangod/SystemMonitor/TaskMonitoring/RestHandler.cpp
+++ arangod/SystemMonitor/TaskMonitoring/RestHandler.cpp
@@ -29,15 +29,7 @@
 #include "ApplicationFeatures/ApplicationServer.h"
 #include "TaskMonitoring/task.h"
 #include "TaskMonitoring/task_registry_variable.h"
-#include "Cluster/ClusterFeature.h"
-#include "Cluster/ClusterInfo.h"
-#include "Cluster/ServerState.h"
 #include "Inspection/VPack.h"
-#include "Network/ConnectionPool.h"
-#include "Network/Methods.h"
-#include "Network/NetworkFeature.h"
-#include "Network/RequestOptions.h"
-#include "Rest/CommonDefines.h"
 
 using namespace arangodb;
 using namespace arangodb::task_monitoring;
@@ -135,56 +127,9 @@ auto RestHandler::executeAsync() -> futures::Future<futures::Unit> {
     co_return;
   }
 
-  // forwarding
-  bool foundServerIdParameter;
-  std::string const& serverId =
-      _request->value("serverId", foundServerIdParameter);
-
-  if (ServerState::instance()->isCoordinator() && foundServerIdParameter) {
-    if (serverId != ServerState::instance()->getId()) {
-      // not ourselves! - need to pass through the request
-      auto& ci = server().getFeature<ClusterFeature>().clusterInfo();
-
-      bool found = false;
-      for (auto const& srv : ci.getServers()) {
-        // validate if server id exists
-        if (srv.first == serverId) {
-          found = true;
-          break;
-        }
-      }
-
-      if (!found) {
-        generateError(rest::ResponseCode::NOT_FOUND,
-                      TRI_ERROR_HTTP_BAD_PARAMETER,
-                      "unknown serverId supplied.");
-        co_return;
-      }
-
-      NetworkFeature const& nf = server().getFeature<NetworkFeature>();
-      network::ConnectionPool* pool = nf.pool();
-      if (pool == nullptr) {
-        THROW_ARANGO_EXCEPTION(TRI_ERROR_SHUTTING_DOWN);
-      }
-      network::RequestOptions options;
-      options.timeout = network::Timeout(30.0);
-      options.database = _request->databaseName();
-      options.parameters = _request->parameters();
-
-      auto f = network::sendRequestRetry(
-          pool, "server:" + serverId, fuerte::RestVerb::Get,
-          _request->requestPath(), VPackBuffer<uint8_t>{}, options);
-      co_await std::move(f).thenValue(
-          [self = std::dynamic_pointer_cast<RestHandler>(shared_from_this())](
-              network::Response const& r) {
-            if (r.fail()) {
-              self->generateError(r.combinedResult());
-            } else {
-              self->generateResult(rest::ResponseCode::OK, r.slice());
-            }
-          });
-      co_return;
-    }
+  auto isForwarded = co_await tryForwarding();
+  if (isForwarded) {
+    co_return;
   }
 
   auto lock_guard = co_await _feature.asyncLock();
