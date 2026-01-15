c653ecc193f96dc8f650dfa60543941d519bd3cf
Jure Bajic <jure.bajic94@gmail.com>
2026-01-15 15:15:32 +0100

    Fix async registry feature name

--- arangod/SystemMonitor/AsyncRegistry/Feature.h
+++ arangod/SystemMonitor/AsyncRegistry/Feature.h
@@ -44,7 +44,7 @@ class Feature final : public ArangodFeature,
       -> std::shared_ptr<RegistryMetrics>;
 
  public:
-  static constexpr std::string_view name() { return "Coroutines"; }
+  static constexpr std::string_view name() { return "AsyncRegistry"; }
   auto asyncLock() -> futures::Future<AsyncLockWithScheduler::Lock> {
     return _asyncLock.lock();
   };
