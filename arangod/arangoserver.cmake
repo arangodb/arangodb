bfd494ed65e03b37f3e030fff3de29c75dccf91b
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-01-29 15:03:03 +0000

    Remove stray cmake code

--- arangod/arangoserver.cmake
+++ arangod/arangoserver.cmake
@@ -238,10 +238,6 @@ if(USE_V8)
   target_link_libraries(arangoserver arango_v8server)
 endif()
 
-if(USE_JEMALLOC)
-  target_link_libraries(arangoserver jemalloc)
-endif()
-
 target_include_directories(arangoserver PRIVATE
   "${PROJECT_SOURCE_DIR}/arangod"
   "${PROJECT_SOURCE_DIR}/${ENTERPRISE_INCLUDE_DIR}")
