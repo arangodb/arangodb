1082325d62e1e146795c468127c44c17c891b018
Jure Bajic <jure.bajic94@gmail.com>
2026-01-30 16:02:13 +0100

    Adapt test

--- tests/js/client/aql/aql-index-cache-noinstr_or_noncluster.js
+++ tests/js/client/aql/aql-index-cache-noinstr_or_noncluster.js
@@ -1018,12 +1018,7 @@ function OtherIndexesSuite () {
       });
     },
     
-    testFulltextIndex : function () {
-      // fulltext index should not show the "cacheEnabled" property
-      [ false, true, undefined ].forEach((value) => {
-        checkIndex({ type: "fulltext", fields: ["value1"], minLength: 3 }, value);
-      });
-    },
+    // Note: testFulltextIndex removed - fulltext indexes are no longer supported (removed in 3.12)
   };
 }
 
