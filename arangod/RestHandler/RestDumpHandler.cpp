4185d910419a297c4d3d6720ce5a1d1cb901a3b7
Julia Volmer <julia.volmer@arangodb.com>
2026-02-02 10:59:25 +0100

    [COR-174] Add a wrapper for the dump endpoint in arangosh (#22261)
    * Add a wrapper for dump endpoint
    
    * Fix dump tests
    
    * Get rid of forgotten sleep
    
    * Fix condition

--- arangod/RestHandler/RestDumpHandler.cpp
+++ arangod/RestHandler/RestDumpHandler.cpp
@@ -218,7 +218,6 @@ void RestDumpHandler::handleCommandDumpNext() {
     } else {
       firstFetch.store(false);
     }
-    std::this_thread::sleep_for(std::chrono::seconds(5));
   }
 
   if (batch == nullptr) {
