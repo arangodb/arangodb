62edbd7546b3034c0e677a7ad5bddeaec6360a17
Jure Bajic <jure.bajic94@gmail.com>
2026-02-05 11:04:30 +0100

    Fix format

--- arangod/Utils/SupportInfoBuilder.cpp
+++ arangod/Utils/SupportInfoBuilder.cpp
@@ -669,9 +669,9 @@ void SupportInfoBuilder::buildDbServerDataStoredInfo(
             auto flags = Index::makeFlags(Index::Serialize::Estimates,
                                           Index::Serialize::Figures);
             static constexpr std::array<std::string_view, 14> idxTypes = {
-                "edge",       "geo",      "inverted",
-                "persistent", "ttl",     "mdi",      "mdi-prefixed",
-                "iresearch",  "primary", "vector",   "unknown"};
+                "edge",    "geo",    "inverted",     "persistent",
+                "ttl",     "mdi",    "mdi-prefixed", "iresearch",
+                "primary", "vector", "unknown"};
             for (auto const& type : idxTypes) {
               idxTypesToAmounts.try_emplace(type, 0);
             }
