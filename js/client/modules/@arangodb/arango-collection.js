f68cdff569f85cbd5d22f93934b73784e91ca3ca
Max Neunhoeffer <max@arangodb.com>
2026-02-10 16:40:06 +0100

    Fix tests.

--- js/client/modules/@arangodb/arango-collection.js
+++ js/client/modules/@arangodb/arango-collection.js
@@ -324,6 +324,7 @@ ArangoCollection.prototype.properties = function (properties) {
     'schema' : true,
     'isDisjoint': false,
     'groupId': false,
+    'useRBAC': true,
   };
 
   let requestResult;
