390c0e96ffec2e0c432b52992247ff8fdc874160
Jure Bajic <jure.bajic94@gmail.com>
2026-02-13 14:12:06 +0100

    Fix lint

--- arangod/RocksDBEngine/RocksDBVectorIndex.cpp
+++ arangod/RocksDBEngine/RocksDBVectorIndex.cpp
@@ -235,7 +235,7 @@ Result RocksDBVectorIndex::insert(transaction::Methods& trx,
                                   OperationOptions const& /*options*/,
                                   bool /*performChecks*/) {
   if (_faissIndex == nullptr) {
-    LOG_TOPIC("v1d0a", WARN, Logger::ENGINES)
+    LOG_TOPIC("d1e0a", WARN, Logger::ENGINES)
         << "vector index " << _iid.id() << " not yet trained, skipping insert";
     return {};
   }
@@ -320,7 +320,7 @@ Result RocksDBVectorIndex::remove(transaction::Methods& /*trx*/,
                                   velocypack::Slice doc,
                                   OperationOptions const& /*options*/) {
   if (_faissIndex == nullptr) {
-    LOG_TOPIC("v1d0b", WARN, Logger::ENGINES)
+    LOG_TOPIC("d1e0b", WARN, Logger::ENGINES)
         << "vector index " << _iid.id() << " not yet trained, skipping remove";
     return {};
   }
