d16c589bcd4fca9ba4252bf0b9805d498d91fb27
Max Neunhoeffer <max@arangodb.com>
2026-02-14 00:03:43 +0100

    Fix compilation.

--- lib/Activities/src/registry.cpp
+++ lib/Activities/src/registry.cpp
@@ -23,20 +23,12 @@
 #include "Activities/registry.h"
 #include "Activities/activity.h"
 
-< < < < < < < <
-    HEAD : lib / Activities / src / registry.cpp namespace arangodb::activities{
-      == == == ==
-#include "ApplicationFeatures/ApplicationFeaturePhase.h"
-#include "ApplicationFeatures/ApplicationServer.h"
-      >>>>>>>> origin / devel : arangod / FeaturePhases /
-                   FoxxFeaturePhase.h
+namespace arangodb::activities {
 
-                       Registry::ScopedCurrentlyExecutingActivity::
-                           ScopedCurrentlyExecutingActivity(
-                               ActivityId activity) noexcept {
-                               _oldExecutingActivity =
-                                   Registry::currentlyExecutingActivity();
-Registry::setCurrentlyExecutingActivity(activity);
+Registry::ScopedCurrentlyExecutingActivity::ScopedCurrentlyExecutingActivity(
+    ActivityId activity) noexcept {
+  _oldExecutingActivity = Registry::currentlyExecutingActivity();
+  Registry::setCurrentlyExecutingActivity(activity);
 }
 
 Registry::ScopedCurrentlyExecutingActivity::
@@ -44,13 +36,4 @@ Registry::ScopedCurrentlyExecutingActivity::
   Registry::setCurrentlyExecutingActivity(_oldExecutingActivity);
 }
 
-< < < < < < < < HEAD : lib / Activities / src / registry.cpp
 }  // namespace arangodb::activities
-== == == ==
-    explicit FoxxFeaturePhase(application_features::ApplicationServer& server);
-}
-;
-
-}  // namespace application_features
-}  // namespace arangodb
->>>>>>>> origin / devel : arangod / FeaturePhases / FoxxFeaturePhase.h
