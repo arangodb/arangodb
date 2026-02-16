fd63914099f55cdfec80d9afe489cb27670fbce4
Jure Bajic <jure.bajic94@gmail.com>
2026-02-16 18:12:34 +0100

    Remove print

--- tests/js/client/aql/vector/aql-vector-replication-failover-cluster.js
+++ tests/js/client/aql/vector/aql-vector-replication-failover-cluster.js
@@ -1,5 +1,5 @@
 /*jshint globalstrict:false, strict:false, maxlen: 500 */
-/*global assertEqual, assertTrue, assertFalse, print */
+/*global assertEqual, assertTrue, assertFalse */
 
 // //////////////////////////////////////////////////////////////////////////////
 // / DISCLAIMER
