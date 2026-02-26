b8beaacc0b081cc1ec3ad65240dd6159aa3fff24
bluepal-krishna-bendalam <krishna.bendalam-external@arangodb.com>
2026-02-26 14:40:04 +0000

    attempt to resolve circle-ci test failure.

--- tests/Mocks/plan_colls_db.h
+++ tests/Mocks/plan_colls_db.h
@@ -3,7 +3,17 @@
 // The JSON has been reformatted with jq, then par and then spaces have
 // been removed. The idea is to have relatively few lines and bytes.
 R"=(
-{"10069":{"waitForSync":false,"type":2,"status":3,"shards":
+{"10071":{"waitForSync":false,"type":2,"status":3,
+"shards":{"s10078":["PRMR-ab4cdcec-bae6-4998-af25-a93c0b4a3ada",
+"PRMR-8cb161bd-aa92-4d02-a0c3-9e48096e18a0"]},"statusString":
+"loaded","shardingStrategy":"hash","shardKeys":["_key"],
+"replicationFactor":2,"numberOfShards":1,"keyOptions":{"type":
+"traditional","allowUserKeys":true},"isSystem":true,"name":
+"_analyzers","indexes":[{"id":"0","type":"primary","name":
+"primary","fields":["_key"],"unique":true,"sparse":false
+}],"isSmart":false,"id":"10071","distributeShardsLike":
+"10069","deleted":false,"minReplicationFactor":1,"cacheEnabled":
+false},"10069":{"waitForSync":false,"type":2,"status":3,"shards":
 {"s10070":["PRMR-ab4cdcec-bae6-4998-af25-a93c0b4a3ada",
 "PRMR-8cb161bd-aa92-4d02-a0c3-9e48096e18a0"]},"statusString":
 "loaded","shardingStrategy":"hash","shardKeys":["_key"
