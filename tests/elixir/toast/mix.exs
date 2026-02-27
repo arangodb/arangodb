857487f991131fa6b3e12030f3b733ee41e9140a
Manuel Pöter <manuel@arangodb.com>
2026-02-27 14:50:33 +0100

    Add credo

--- tests/elixir/toast/mix.exs
+++ tests/elixir/toast/mix.exs
@@ -6,10 +6,14 @@ defmodule Toast.MixProject do
       app: :toast,
       version: "0.1.0",
       elixir: "~> 1.19",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   def application do
     [
@@ -23,7 +27,8 @@ defmodule Toast.MixProject do
       {:req, "~> 0.5"},
       {:erlexec, "~> 2.0"},
       {:joken, "~> 2.6"},
-      {:plug, "~> 1.0", only: :test}
+      {:plug, "~> 1.0", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
