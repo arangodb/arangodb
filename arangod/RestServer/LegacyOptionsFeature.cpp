28b175ccf285b90145698000d2fb017755441cef
Manuel Pöter <manuel@arangodb.com>
2026-03-11 11:47:25 +0100

    Remove irrelevant options and fix some descriptions for diffing

--- arangod/RestServer/LegacyOptionsFeature.cpp
+++ arangod/RestServer/LegacyOptionsFeature.cpp
@@ -36,117 +36,109 @@ LegacyOptionsFeature::LegacyOptionsFeature(
 
 void LegacyOptionsFeature::collectOptions(
     std::shared_ptr<ProgramOptions> options) {
-  options->addSection("javascript", "JavaScript engine");
-  options->addSection("foxx", "Foxx services");
-
   // V8DealerFeature options
   options->addObsoleteOption("--javascript.gc-frequency",
-                             "JavaScript GC frequency (each x-th request)",
+                             "Time-based garbage collection frequency for "
+                             "JavaScript objects (each x seconds).",
                              true);
-  options->addObsoleteOption("--javascript.gc-interval",
-                             "JavaScript GC interval (each x seconds)", true);
+  options->addObsoleteOption(
+      "--javascript.gc-interval",
+      "Request-based garbage collection interval for JavaScript objects "
+      "(each x requests).",
+      true);
   options->addObsoleteOption("--javascript.app-path",
-                             "directory for Foxx applications", true);
-  options->addObsoleteOption("--javascript.startup-directory",
-                             "path to the directory containing JavaScript "
-                             "startup scripts",
-                             true);
+                             "The directory for Foxx applications.", true);
+  options->addObsoleteOption(
+      "--javascript.startup-directory",
+      "A path to the directory containing the JavaScript startup scripts.",
+      true);
   options->addObsoleteOption("--javascript.module-directory",
-                             "additional paths containing JavaScript modules",
+                             "Additional paths containing JavaScript modules.",
                              true);
-  options->addObsoleteOption("--javascript.copy-installation",
-                             "copy contents of 'javascript.startup-directory'",
-                             false);
-  options->addObsoleteOption("--javascript.v8-contexts",
-                             "maximum number of V8 contexts", true);
-  options->addObsoleteOption("--javascript.v8-contexts-minimum",
-                             "minimum number of V8 contexts to keep", true);
-  options->addObsoleteOption("--javascript.v8-contexts-max-invocations",
-                             "maximum number of invocations for each V8 "
-                             "context before it is disposed",
-                             true);
-  options->addObsoleteOption("--javascript.v8-contexts-max-age",
-                             "maximum lifetime for each V8 context (in "
-                             "seconds) before it is disposed",
-                             true);
-  options->addObsoleteOption("--javascript.allow-admin-execute",
-                             "for testing purposes allow /_admin/execute",
-                             false);
+  options->addObsoleteOption(
+      "--javascript.copy-installation",
+      "Copy the contents of `javascript.startup-directory` on first start.",
+      false);
+  options->addObsoleteOption(
+      "--javascript.v8-contexts",
+      "The maximum number of V8 contexts that are created for "
+      "executing JavaScript actions.",
+      true);
+  options->addObsoleteOption(
+      "--javascript.v8-contexts-minimum",
+      "The minimum number of V8 contexts to keep available for "
+      "executing JavaScript actions.",
+      true);
+  options->addObsoleteOption(
+      "--javascript.v8-contexts-max-invocations",
+      "The maximum number of invocations for each V8 context before it is "
+      "disposed (0 = unlimited).",
+      true);
+  options->addObsoleteOption(
+      "--javascript.v8-contexts-max-age",
+      "The maximum age for each V8 context (in seconds) before it "
+      "is disposed.",
+      true);
+  options->addObsoleteOption(
+      "--javascript.allow-admin-execute",
+      "For testing purposes, allow `/_admin/execute`. Never enable "
+      "this option in production!",
+      false);
   options->addObsoleteOption("--javascript.transactions",
-                             "enable JavaScript transactions", false);
-  options->addObsoleteOption("--javascript.user-defined-functions",
-                             "enable JavaScript user-defined functions", false);
-  options->addObsoleteOption("--javascript.tasks", "enable JavaScript tasks",
+                             "Enable JavaScript transactions.", false);
+  options->addObsoleteOption(
+      "--javascript.user-defined-functions",
+      "Enable JavaScript user-defined functions (UDFs) in AQL queries.", false);
+  options->addObsoleteOption("--javascript.tasks", "Enable JavaScript tasks.",
                              false);
   options->addObsoleteOption("--javascript.enabled",
-                             "enable the V8 JavaScript engine", false);
+                             "Enable the V8 JavaScript engine.", false);
 
   // V8SecurityFeature options
-  options->addObsoleteOption("--javascript.allow-port-testing",
-                             "allow testing of ports from within JavaScript "
-                             "actions",
-                             false);
-  options->addObsoleteOption("--javascript.allow-external-process-control",
-                             "allow controlling external processes from within "
-                             "JavaScript actions",
-                             false);
-  options->addObsoleteOption("--javascript.harden", "harden JavaScript engine",
-                             false);
-  options->addObsoleteOption("--javascript.startup-options-allowlist",
-                             "startup options whose names match this regular "
-                             "expression are allowed",
-                             true);
-  options->addObsoleteOption("--javascript.startup-options-denylist",
-                             "startup options whose names match this regular "
-                             "expression are denied",
-                             true);
-  options->addObsoleteOption("--javascript.environment-variables-allowlist",
-                             "environment variables that are accessible from "
-                             "within JavaScript",
-                             true);
+  options->addObsoleteOption(
+      "--javascript.allow-port-testing",
+      "Allow the testing of ports from within JavaScript actions.", false);
+  options->addObsoleteOption(
+      "--javascript.allow-external-process-control",
+      "Allow the execution and control of external processes from "
+      "within JavaScript actions.",
+      false);
+  options->addObsoleteOption(
+      "--javascript.harden",
+      "Disable access to JavaScript functions in the internal "
+      "module: getPid() and logLevel().",
+      false);
+  options->addObsoleteOption(
+      "--javascript.startup-options-allowlist",
+      "Startup options whose names match this regular "
+      "expression are allowed and exposed to JavaScript.",
+      true);
+  options->addObsoleteOption(
+      "--javascript.startup-options-denylist",
+      "Startup options whose names match this regular "
+      "expression are not exposed (if not in the allowlist) to "
+      "JavaScript actions.",
+      true);
+  options->addObsoleteOption(
+      "--javascript.environment-variables-allowlist",
+      "Environment variables that are accessible in JavaScript.", true);
   options->addObsoleteOption("--javascript.environment-variables-denylist",
-                             "environment variables that are not accessible "
-                             "from within JavaScript",
+                             "Environment variables that are inaccessible in "
+                             "JavaScript (if not in the allowlist).",
                              true);
-  options->addObsoleteOption("--javascript.endpoints-allowlist",
-                             "endpoints that can be connected to from within "
-                             "JavaScript",
-                             true);
+  options->addObsoleteOption(
+      "--javascript.endpoints-allowlist",
+      "Endpoints that can be connected to via the "
+      "`@arangodb/request` module in JavaScript actions.",
+      true);
   options->addObsoleteOption("--javascript.endpoints-denylist",
-                             "endpoints that cannot be connected to from "
-                             "within JavaScript",
+                             "Endpoints that cannot be connected to via the "
+                             "`@arangodb/request` module in JavaScript actions "
+                             "(if not in the allowlist).",
                              true);
   options->addObsoleteOption("--javascript.files-allowlist",
-                             "filesystem paths accessible from within "
-                             "JavaScript",
-                             true);
-  options->addObsoleteOption("--javascript.startup-options-whitelist",
-                             "startup options whose names match this regular "
-                             "expression are allowed",
-                             true);
-  options->addObsoleteOption("--javascript.startup-options-blacklist",
-                             "startup options whose names match this regular "
-                             "expression are denied",
-                             true);
-  options->addObsoleteOption("--javascript.environment-variables-whitelist",
-                             "environment variables that are accessible from "
-                             "within JavaScript",
-                             true);
-  options->addObsoleteOption("--javascript.environment-variables-blacklist",
-                             "environment variables that are not accessible "
-                             "from within JavaScript",
-                             true);
-  options->addObsoleteOption("--javascript.endpoints-whitelist",
-                             "endpoints that can be connected to from within "
-                             "JavaScript",
-                             true);
-  options->addObsoleteOption("--javascript.endpoints-blacklist",
-                             "endpoints that cannot be connected to from "
-                             "within JavaScript",
-                             true);
-  options->addObsoleteOption("--javascript.files-whitelist",
-                             "filesystem paths accessible from within "
-                             "JavaScript",
+                             "Filesystem paths that are accessible from within "
+                             "JavaScript actions.",
                              true);
 
   // V8PlatformFeature options
@@ -198,11 +190,6 @@ void LegacyOptionsFeature::collectOptions(
   options->addObsoleteOption("--javascript.script-parameter",
                              "Script parameter.", true);
 
-  // ServerFeature options
-  options->addObsoleteOption("--console",
-                             "Start the server with a JavaScript emergency "
-                             "console.",
-                             false);
   options->addObsoleteOption("--javascript.script", "Run the script and exit.",
                              true);
 
@@ -210,8 +197,4 @@ void LegacyOptionsFeature::collectOptions(
   options->addObsoleteOption("--web-interface.version-check",
                              "Alert the user if new versions are available.",
                              false);
-
-  // NonceFeature options
-  options->addObsoleteOption("--nonce.size",
-                             "the size of the hash array for nonces", true);
 }
