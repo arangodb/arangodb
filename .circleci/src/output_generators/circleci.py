c5c8cdfe206ffd81c35ffd1be13c5def009d7b5a
Wilfried Goesgens <willi@arangodb.com>
2026-03-16 14:48:03 +0100

    remove dead code

--- .circleci/src/output_generators/circleci.py
+++ .circleci/src/output_generators/circleci.py
@@ -356,8 +356,6 @@ class CircleCIGenerator(OutputGenerator):
         """
         # Handle RTA UI tests specially - they generate multiple jobs
         if job.job_type == "run-rta-tests":
-            # return []
-            # TODO RTA UI
             return self._create_rta_test_jobs(job, build_config, build_jobs)
 
         result = []
