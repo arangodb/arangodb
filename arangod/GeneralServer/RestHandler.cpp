7454213082f594bf309514324796ccfca29617f2
knakatasf <knakata@dons.usfca.edu>
2026-03-17 22:15:46 +0100

    Format code

--- arangod/GeneralServer/RestHandler.cpp
+++ arangod/GeneralServer/RestHandler.cpp
@@ -702,7 +702,7 @@ bool RestHandler::isAllowedHttpMethod(
 // and generates an error if so
 bool RestHandler::rejectNumericCollectionId(std::string_view cname) {
   if (cname.empty()) return false;
-  if (cname.front() < '0' || cname.front() > '9') return false; // early exit
+  if (cname.front() < '0' || cname.front() > '9') return false;  // early exit
   if (std::all_of(cname.begin(), cname.end(),
                   [](unsigned char c) { return c >= '0' && c <= '9'; })) {
     generateError(rest::ResponseCode::BAD, TRI_ERROR_HTTP_BAD_PARAMETER,
