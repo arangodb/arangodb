35a8b83f908fcc166257794f98b507ec03059be3
Johann Listunov <johann.listunov@arangodb.com>
2026-03-30 16:54:06 +0200

    Reenable statistics

--- arangod/Statistics/StatisticsFeatureOptions.h
+++ arangod/Statistics/StatisticsFeatureOptions.h
@@ -26,7 +26,7 @@ namespace arangodb {
 
 struct StatisticsFeatureOptions {
   bool statisticsHistory = true;
-  bool statisticsAllDatabases = false;
+  bool statisticsAllDatabases = true;
 };
 
 }  // namespace arangodb
