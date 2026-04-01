fa739a91a797931a91053e4cb5bc5386b4795c21
Lars Maier <lars@arangodb.com>
2026-04-01 11:11:45 +0000

    Fixing RestVersionHandler.

--- arangod/RestHandler/RestVersionHandler.h
+++ arangod/RestHandler/RestVersionHandler.h
@@ -31,9 +31,6 @@ class RestVersionHandler : public arangodb::RestBaseHandler {
   RestVersionHandler(application_features::ApplicationServer&, GeneralRequest*,
                      GeneralResponse*);
 
-protected:
-  async<Result> checkUserCanAccess() const override;
-
  public:
   static void getVersion(application_features::ApplicationServer& server,
                          bool allowInfo, bool includeDetails,
