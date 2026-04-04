945ee73c5c1196f5b474abe0a9a5094441a06494
Simran Spiller <simran.spiller@arango.ai>
2026-04-04 18:29:52 +0200

    Improve some long startup option descriptions
    Fix typos, broken Markdown, minor rewording

--- arangod/GeneralServer/AuthenticationFeature.cpp
+++ arangod/GeneralServer/AuthenticationFeature.cpp
@@ -211,10 +211,10 @@ endpoint. Requests with expiry times above this value will be rejected.)");
                       arangodb::options::Flags::Experimental))
       .setLongDescription(
           R"(When set to a non-empty string, this must be the HTTP or HTTPS
-endpoint of an external RBAC authorization service for use by coordinators and single
-servers. In this case, all requests with use role-based-access-control (RBAC) via the
-specified service for authorization decisions. When set to an empty string, RBAC is
-disabled and instead the old permission system is used.)");
+endpoint of an external RBAC authorization service for use by Coordinators and
+single servers. In this case, all requests with use role-based-access-control
+(RBAC) via the specified service for authorization decisions. When set to an
+empty string, RBAC is disabled and instead the old permission system is used.)");
 
   options->addObsoleteOption(
       "--server.local-authentication",
