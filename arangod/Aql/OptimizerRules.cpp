1a97fa2538526f7435c581bfc5a7115d4b729d0e
knakatasf <knakata@dons.usfca.edu>
2026-04-08 04:15:12 +0200

    Refactor condition rewrite related optimizer rules to a newly created file: OptimizerRulesConditionRewrite.h/cpp

--- arangod/Aql/OptimizerRules.cpp
+++ arangod/Aql/OptimizerRules.cpp
@@ -1149,7 +1149,7 @@ void arangodb::aql::removeCollectVariablesRule(
           modified = true;
         }
       }  // end - if doOptimize
-    }    // end - if collectNode has outVariable
+    }  // end - if collectNode has outVariable
 
     size_t numGroupVariables = collectNode->groupVariables().size();
     size_t numAggregateVariables = collectNode->aggregateVariables().size();
@@ -2355,7 +2355,7 @@ void arangodb::aql::distributeInClusterRule(Optimizer* opt,
         node = node->getFirstDependency();
       }
     }  // for node in subquery
-  }    // for end subquery in plan
+  }  // for end subquery in plan
   opt->addPlan(std::move(plan), rule, wasModified);
 }
 
@@ -3826,7 +3826,7 @@ static bool distanceFuncArgCheck(ExecutionPlan* plan, AstNode const* latArg,
         return true;
       }
     }  // if isGeo 1 or 2
-  }    // for index in collection
+  }  // for index in collection
   return false;
 }
 
