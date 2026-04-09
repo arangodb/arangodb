cad35ef5e038a9b3b3af2fd699f3d63035ad9d48
Koichi Nakata <126118326+knakatasf@users.noreply.github.com>
2026-04-09 00:35:49 -0700

    [COR-390] Refactor sort and gather related optimizer rules (#22582)
    * Refactor sort and gather related optimizer rule functions to a newly created file: OptimizerRulesSortAndGather.h/cpp
    
    * Revert rule registration
    
    * Separate OptimizerRulesSortAndGather into 4 files

--- arangod/Aql/OptimizerRules.h
+++ arangod/Aql/OptimizerRules.h
@@ -53,12 +53,6 @@ void activateCallstackSplit(ExecutionPlan& plan);
 void sortInValuesRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
                       OptimizerRule const&);
 
-/// @brief remove redundant sorts
-/// this rule modifies the plan in place:
-/// - sorts that are covered by earlier sorts will be removed
-void removeRedundantSortsRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
-                              OptimizerRule const&);
-
 /// @brief remove all unnecessary filters
 /// this rule modifies the plan in place:
 /// - filters that are always true are removed completely
@@ -291,10 +285,6 @@ void inlineSubqueriesRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
 void geoIndexRule(Optimizer*, std::unique_ptr<aql::ExecutionPlan>,
                   OptimizerRule const&);
 
-/// @brief make sort node aware of limit to enable internal optimizations
-void sortLimitRule(Optimizer*, std::unique_ptr<aql::ExecutionPlan>,
-                   OptimizerRule const&);
-
 /// @brief push LIMIT into subqueries, and simplify them
 void optimizeSubqueriesRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
                             OptimizerRule const&);
@@ -321,10 +311,6 @@ void moveFiltersIntoEnumerateRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
 void optimizeCountRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
                        OptimizerRule const&);
 
-/// @brief parallelize Gather nodes (cluster-only)
-void parallelizeGatherRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
-                           OptimizerRule const&);
-
 /// @brief allows execution nodes to asynchronously prefetch the next batch from
 /// their upstream node.
 void asyncPrefetchRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
@@ -333,11 +319,6 @@ void asyncPrefetchRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
 //// @brief splice in subqueries
 void spliceSubqueriesRule(Optimizer*, std::unique_ptr<ExecutionPlan>,
                           OptimizerRule const&);
-
-//// @brief reduces a sorted gather to an unsorted gather if only one shard is
-/// involved
-void decayUnnecessarySortedGather(Optimizer*, std::unique_ptr<ExecutionPlan>,
-                                  OptimizerRule const&);
 
 void createScatterGatherSnippet(
     ExecutionPlan& plan, TRI_vocbase_t* vocbase, ExecutionNode* node,
