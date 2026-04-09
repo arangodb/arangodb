a0b8ac70c826bb8d0a93833d2631a51ce483d85d
Koichi Nakata <126118326+knakatasf@users.noreply.github.com>
2026-04-09 00:31:46 -0700

    [COR-389] Refactor calculation related optimizer rules (#22581)
    * Move calculation related optimizer rule functions to newly created file: OptimizerRulesCalculation
    
    * Change to strongly typed ast node
    
    * Separate calculation into 4 files
    
    * Create Utils folder under Optimzier; Add helper function and struct to it
    
    * Change namespace
    
    * Format code

---
+++ arangod/Aql/Optimizer/Rule/MoveCalculationsUp.cpp
@@ -0,0 +1,125 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Max Neunhoeffer
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#include "MoveCalculationsUp.h"
+
+#include "Aql/ExecutionNode/CalculationNode.h"
+#include "Aql/ExecutionNode/ExecutionNode.h"
+#include "Aql/ExecutionNode/SubqueryNode.h"
+#include "Aql/ExecutionPlan.h"
+#include "Aql/Optimizer.h"
+#include "Aql/Optimizer/Utils/AccessesCollectionVariable.h"
+#include "Cluster/ServerState.h"
+#include "Containers/SmallUnorderedMap.h"
+#include "Containers/SmallVector.h"
+
+namespace arangodb::aql {
+using EN = ExecutionNode;
+
+void moveCalculationsUpRule(Optimizer* opt, std::unique_ptr<ExecutionPlan> plan,
+                            OptimizerRule const& rule) {
+  containers::SmallVector<ExecutionNode*, 8> nodes;
+  plan->findNodesOfType(nodes, EN::CALCULATION, true);
+  plan->findNodesOfType(nodes, EN::SUBQUERY, true);
+
+  containers::SmallUnorderedMap<ExecutionNode*,
+                                ExecutionNode*>::allocator_type::arena_type
+      subqueriesArena;
+  containers::SmallUnorderedMap<ExecutionNode*, ExecutionNode*> subqueries{
+      subqueriesArena};
+  {
+    containers::SmallVector<ExecutionNode*, 8> subs;
+    plan->findNodesOfType(subs, ExecutionNode::SUBQUERY, true);
+
+    for (auto& it : subs) {
+      auto sub = ExecutionNode::castTo<SubqueryNode const*>(it)->getSubquery();
+      while (sub->hasDependency()) {
+        sub = sub->getFirstDependency();
+      }
+      subqueries.emplace(sub, it);
+    }
+  }
+
+  bool modified = false;
+  VarSet neededVars;
+  VarSet vars;
+
+  for (auto const& n : nodes) {
+    bool isAccessCollection = false;
+    if (!n->isDeterministic()) {
+      continue;
+    }
+    if (n->getType() == EN::CALCULATION) {
+      auto nn = ExecutionNode::castTo<CalculationNode*>(n);
+      if (accessesCollectionVariable(plan.get(), nn, vars)) {
+        isAccessCollection = true;
+      }
+    }
+
+    neededVars.clear();
+    n->getVariablesUsedHere(neededVars);
+
+    auto current = n->getFirstDependency();
+
+    while (current != nullptr) {
+      if (current->setsVariable(neededVars)) {
+        break;
+      }
+
+      auto dep = current->getFirstDependency();
+      if (dep == nullptr) {
+        auto it = subqueries.find(current);
+        if (it != subqueries.end()) {
+          plan->unlinkNode(n);
+          plan->insertDependency(it->second, n);
+
+          modified = true;
+          current = n->getFirstDependency();
+          continue;
+        }
+
+        break;
+      }
+
+      if (current->getType() == EN::LIMIT) {
+        if (!arangodb::ServerState::instance()->isCoordinator()) {
+          break;
+        }
+
+        if (!isAccessCollection) {
+          break;
+        }
+      }
+
+      plan->unlinkNode(n);
+      plan->insertDependency(current, n);
+
+      modified = true;
+      current = dep;
+    }
+  }
+
+  opt->addPlan(std::move(plan), rule, modified);
+}
+}  // namespace arangodb::aql
