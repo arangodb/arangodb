e108f60924041c0e41951eeaf6a16603e619824a
knakatasf <knakata@dons.usfca.edu>
2026-04-11 02:17:08 +0200

    Add include paths

--- arangod/Aql/Optimizer/Rule/OptimizePaths.cpp
+++ arangod/Aql/Optimizer/Rule/OptimizePaths.cpp
@@ -37,7 +37,7 @@
 #include "Basics/StaticStrings.h"
 #include "Containers/FlatHashSet.h"
 #include "Containers/SmallVector.h"
-#include "Graph/PathType.h"
+#include "Graph/ShortestPathOptions.h"
 
 namespace arangodb::aql {
 using EN = ExecutionNode;
