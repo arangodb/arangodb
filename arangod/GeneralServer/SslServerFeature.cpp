5fd2db9bed8b9c3cc140ae7f14abae1c53dea4f0
Julia CP <julia@arangodb.com>
2026-04-13 12:01:42 -0300

    Removed SSL 2 and 3 related code, left TLS 1 onwards

--- arangod/GeneralServer/SslServerFeature.cpp
+++ arangod/GeneralServer/SslServerFeature.cpp
@@ -174,11 +174,7 @@ Mac=SHA1
       .setLongDescription(R"(Use this option to specify the default encryption
 protocol to be used. The default value is 9 (generic TLS), which allows the
 negotiation of the TLS version between the client and the server, dynamically
-choosing the highest mutually supported version of TLS.
-
-Note that SSLv2 is unsupported as of version 3.4, because of the inherent
-security vulnerabilities in this protocol. Selecting SSLv2 as protocol aborts
-the startup.)");
+choosing the highest mutually supported version of TLS.)");
 
   options
       ->addOption("--ssl.options",
@@ -220,15 +216,7 @@ http://www.openssl.org/docs/ssl/SSL_CTX_set_options.html))");
 }
 
 void SslServerFeature::validateOptions(
-    std::shared_ptr<ProgramOptions> options) {
-  // check for SSLv2
-  if (_options.sslProtocol == SslProtocol::SSL_V2) {
-    LOG_TOPIC("b7890", FATAL, arangodb::Logger::SSL)
-        << "SSLv2 is not supported any longer because of security "
-           "vulnerabilities in this protocol";
-    FATAL_ERROR_EXIT();
-  }
-}
+    std::shared_ptr<ProgramOptions> options) {}
 
 void SslServerFeature::prepare() {
   LOG_TOPIC("afcd3", INFO, arangodb::Logger::SSL)
