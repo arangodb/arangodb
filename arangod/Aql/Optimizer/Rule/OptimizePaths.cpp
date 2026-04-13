06df54c8e6baf948fb3da7d25296a2feb9c391dc
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-04-13 13:44:30 +0100

    Add ShortestPathOptions

--- arangod/Aql/Optimizer/Rule/OptimizePaths.cpp
+++ arangod/Aql/Optimizer/Rule/OptimizePaths.cpp
@@ -37,6 +37,7 @@
 #include "Basics/StaticStrings.h"
 #include "Containers/FlatHashSet.h"
 #include "Containers/SmallVector.h"
+#include "Graph/ShortestPathOptions.h"
 
 namespace arangodb::aql {
 using EN = ExecutionNode;
