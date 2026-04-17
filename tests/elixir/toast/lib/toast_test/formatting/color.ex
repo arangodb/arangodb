e20781ef0ce472ee0ca0f300e924efbae9841697
Manuel Pöter <manuel@arangodb.com>
2026-04-17 18:46:17 +0200

    Update colors

--- tests/elixir/toast/lib/toast_test/formatting/color.ex
+++ tests/elixir/toast/lib/toast_test/formatting/color.ex
@@ -4,8 +4,8 @@ defmodule ToastTest.Formatting.Color do
   @doc "256-color codes for section header backgrounds."
   def failure, do: 1
   def timeout, do: 1
-  def crash, do: 196
-  def sanitizer, do: 220
+  def crash, do: 160
+  def sanitizer, do: 214
   def warning, do: 3
   def summary, do: 4
   def info, do: 195
