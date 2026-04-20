29d3e99433061761e63af70e1dcfbcda0a6910d4
Julia Volmer <julia.volmer@arangodb.com>
2026-04-20 21:30:57 +0200

    Be able to set apiVersion in network headers

--- arangod/Network/RequestOptions.h
+++ arangod/Network/RequestOptions.h
@@ -25,6 +25,7 @@
 #include "Assertions/Assert.h"
 #include "GeneralServer/RequestLane.h"
 #include "Network/types.h"
+#include "fuerte/ApiVersion.h"
 
 #include <fuerte/types.h>
 
@@ -61,6 +62,8 @@ struct RequestOptions {
   // the leader, but rather to the server given here. This is read for
   // the "allowDirtyReads" options when we want to read from followers.
   std::string overrideDestination;
+
+  std::optional<fuerte::api_version::ApiVersion> apiVersion;
 
   template<typename K, typename V>
   RequestOptions& param(K&& key, V&& val) {
