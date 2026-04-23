efcbf6e48cbdb25e8a41fbe5891a60fe41a1b302
Johann Listunov <johann.listunov@arangodb.com>
2026-04-23 13:38:10 +0200

    First clean-up

--- arangod/Aql/Optimizer/Rule/OptimizerRuleUpgradeScatterToDistribute.cpp
+++ arangod/Aql/Optimizer/Rule/OptimizerRuleUpgradeScatterToDistribute.cpp
@@ -1,6 +1,5 @@
 // TODO(listunov): disclaimer
 
-#include "absl/strings/str_format.h"
 #include "ApplicationFeatures/ApplicationServer.h"
 
 #include "Aql/Ast.h"
@@ -11,99 +10,82 @@
 #include "Aql/ExecutionNode/CalculationNode.h"
 #include "Aql/ExecutionNode/DistributeNode.h"
 #include "Aql/ExecutionNode/EnumerateCollectionNode.h"
-#include "Aql/ExecutionNode/FilterNode.h"
 #include "Aql/ExecutionNode/IndexNode.h"
-#include "Aql/ExecutionNode/SubqueryNode.h"
 #include "Aql/Expression.h"
 #include "Aql/Optimizer.h"
 #include "Aql/OptimizerRules.h"
 #include "Aql/QueryContext.h"
-
-#include "Basics/voc-errors.h"
 
 #include "Cluster/ServerState.h"
 #include "Containers/SmallVector.h"
 
 #include "Logger/LogMacros.h"
 
-#define LOG_RULE LOG_DEVEL_IF(true) << "UpgradeScatterToDistribute: "
+#define LOG_RULE \
+  LOG_DEVEL_IF(false) << "UpgradeScatterToDistribute: "
 
-using EN = arangodb::aql::ExecutionNode;
+struct DistributeNodeDependency {
+  std::unordered_map<std::string_view, arangodb::aql::AstNode const*>
+      shardKeyAccessMap;
+};
 
 arangodb::aql::Collection const* getCollection(
     arangodb::aql::ExecutionNode const* node) {
-  using arangodb::aql::ExecutionNode;
+  using namespace arangodb::aql;
 
   switch (node->getType()) {
-    case EN::ENUMERATE_COLLECTION:
-      return ExecutionNode::castTo<
-                 arangodb::aql::EnumerateCollectionNode const*>(node)
+    case ExecutionNode::NodeType::ENUMERATE_COLLECTION:
+      return ExecutionNode::castTo<EnumerateCollectionNode const*>(node)
           ->collection();
-    case EN::INDEX:
-      return ExecutionNode::castTo<arangodb::aql::IndexNode const*>(node)
-          ->collection();
-    default:
-      // note: modification nodes are not covered here yet
-      THROW_ARANGO_EXCEPTION_MESSAGE(TRI_ERROR_INTERNAL,
-                                     "node type does not have a collection");
-  }
-}
-
-arangodb::aql::Variable const* getVariableFromAttributeAccess(
-    arangodb::aql::AstNode const* node) {
-  if (node->type != arangodb::aql::NODE_TYPE_ATTRIBUTE_ACCESS) {
-    return nullptr;
-  }
-
-  node = node->getMember(0);
-
-  if (node->type != arangodb::aql::NODE_TYPE_REFERENCE) {
-    return nullptr;
-  }
-
-  return static_cast<arangodb::aql::Variable const*>(node->getData());
-}
-
-arangodb::aql::Variable const* getOutVariable(
-    arangodb::aql::ExecutionNode const* node) {
-  using arangodb::aql::ExecutionNode;
-
-  switch (node->getType()) {
-    case EN::INDEX:
-    case EN::ENUMERATE_COLLECTION: {
-      auto const* n =
-          dynamic_cast<arangodb::aql::DocumentProducingNode const*>(node);
-      if (n != nullptr) {
-        return n->outVariable();
-      }
-    }
+    case ExecutionNode::NodeType::INDEX:
+      return ExecutionNode::castTo<IndexNode const*>(node)->collection();
     default:
       break;
   }
   return nullptr;
 }
 
-struct DistributeNodeDep {
-  arangodb::aql::Variable const* distVar{nullptr};
-  std::set<std::string> members;
-};
+arangodb::aql::Variable const* getVariableFromAttributeAccess(
+    arangodb::aql::AstNode const* node) {
+  using namespace arangodb::aql;
+  if (node->numMembers() > 0) {
+    node = node->getMember(0);
+  }
+  if (node->type != AstNodeType::NODE_TYPE_REFERENCE) {
+    return nullptr;
+  }
+  return static_cast<Variable const*>(node->getData());
+}
+
+arangodb::aql::Variable const* getOutVariable(
+    arangodb::aql::ExecutionNode const* node) {
+  using namespace arangodb::aql;
+  const auto nodeType = node->getType();
+  if (nodeType == ExecutionNode::NodeType::INDEX ||
+      nodeType == ExecutionNode::NodeType::ENUMERATE_COLLECTION) {
+    auto const* n = dynamic_cast<DocumentProducingNode const*>(node);
+    if (n != nullptr) {
+      return n->outVariable();
+    }
+  }
+  return nullptr;
+}
+
 bool checkIfAllShardKeysAreUsed(arangodb::aql::AstNode const* root,
                                 arangodb::aql::ExecutionNode const* node,
-                                DistributeNodeDep& distDep) {
+                                DistributeNodeDependency& distDep) {
   using namespace arangodb::aql;
-
   if (root == nullptr) {
     return false;
   }
-
   if (root->type != AstNodeType::NODE_TYPE_OPERATOR_NARY_OR) {
     return false;
   }
 
   // number of ANDs
   size_t const numAnds = root->numMembers();
+  // TODO(listunov): Is this even solvable?
   if (numAnds != 1) {
-    // TODO(listunov): do we really only support one AND-branch ? or what is happening here ?
     LOG_RULE << "found more than one AND-branch, stop evaluation";
     return false;
   }
@@ -115,9 +97,9 @@ bool checkIfAllShardKeysAreUsed(arangodb::aql::AstNode const* root,
   }
   Collection const* collection = getCollection(node);
   LOG_RULE << std::format("checking node {}({}) for var({}) and collection({})",
-                         node->getTypeString(), node->id(), var->name,
-                         collection->name());
-  std::vector<std::string> shardKeys{ collection->shardKeys(true) };
+                          node->getTypeString(), node->id(), var->name,
+                          collection->name());
+  std::vector<std::string> shardKeys{collection->shardKeys(true)};
 
   uint32_t foundAllShardKeysCount{0};
   for (size_t i = 0; i < numAnds; ++i) {
@@ -136,93 +118,83 @@ bool checkIfAllShardKeysAreUsed(arangodb::aql::AstNode const* root,
         LOG_RULE << "condition not equal operator, skip.";
         continue;
       }
-      auto const* lhs{ condNode->getMember(0) };
-      auto const* rhs{ condNode->getMember(1) };
+      auto const* lhs{condNode->getMember(0)};
+      auto const* rhs{condNode->getMember(1)};
       if (lhs->type != AstNodeType::NODE_TYPE_ATTRIBUTE_ACCESS &&
           rhs->type != AstNodeType::NODE_TYPE_ATTRIBUTE_ACCESS) {
-        // No side has attribute access, something else, we cant check for shardKey access
+        // No side has attribute access, something else, we cant check for
+        // shardKey access
         LOG_RULE << "condition has no attribute access, skip";
         continue;
       }
 
-      // TODO(listunov): getVariableFromAttributeAccess can probably be removed but it does a check on NODE_TYPE_REFERENCE, maybe its easier to keep it in separate function
       Variable const* lhsVar = getVariableFromAttributeAccess(lhs);
       Variable const* rhsVar = getVariableFromAttributeAccess(rhs);
-
-      // TODO(listunov): we could work if one of them is nullptr, so is this a check only for smartJoinsRule ?
-      if (lhsVar == nullptr || rhsVar == nullptr) {
-        // TODO(listunov): when can this happen?
-        // TODO(listunov): when its not NODE_TYPE_REFERENCE, what is NODE_TYPE_REFERENCE?
-        LOG_RULE << "lhsVar or rhsVar are null, skip";
+      if (lhsVar == nullptr && rhsVar == nullptr) {
+        LOG_RULE << "lhsVar and rhsVar are null, skip";
         continue;
       }
 
-      AstNode const* attribute{ nullptr };
-      AstNode const* other{ nullptr };
+      AstNode const* shardKeyNode{nullptr};
+      AstNode const* expression{nullptr};
       if (lhsVar == var) {
-        distDep.distVar = rhsVar;
-        attribute = lhs;
-        other = rhs;
+        shardKeyNode = lhs;
+        expression = rhs;
       } else if (rhsVar == var) {
-        distDep.distVar = lhsVar;
-        attribute = rhs;
-        other = lhs;
+        shardKeyNode = rhs;
+        expression = lhs;
       }
 
-      if (attribute == nullptr) {
+      if (shardKeyNode == nullptr) {
         // Neither side is our var, ignore
         LOG_RULE << "var is neither on the left nor on the right side, skip";
         continue;
       }
-      std::string const shardField = attribute->getString();
-      LOG_RULE << "Found attribute, remove from set: " << shardField;
-      auto const numErased = shardKeySet.erase(shardField);
-      if (numErased > 0 && other != nullptr) {
-        distDep.members.insert(other->getString());
+      std::string const shardKeyField = shardKeyNode->getString();
+      LOG_RULE << "Found attribute, remove from set: " << shardKeyField;
+      auto const numErased = shardKeySet.erase(shardKeyField);
+      if (numErased > 0 && expression != nullptr) {
+        distDep.shardKeyAccessMap[shardKeyNode->getStringView()] = expression;
       }
     }
     if (shardKeySet.empty()) {
-      LOG_RULE << "found all shardKeys for " << var->name;
       foundAllShardKeysCount++;
     } else {
-      for (auto const& leftOver : shardKeySet) {
-        LOG_RULE << "LeftOver ShardKey: " << leftOver;
-      }
       // Some condition does not handle all shard-keys -> bail
+      LOG_RULE << "Found AND-branch without all ShardKeys, cancel.";
       return false;
     }
   }
+  LOG_RULE << std::format("Found all the ShardKeys {}-times in {} AND-branches",
+                          foundAllShardKeysCount, numAnds);
   // Found shard-keys in all and-branches
   return foundAllShardKeysCount == numAnds;
 }
 
 void replaceScatterWithDistribute(arangodb::aql::ExecutionPlan& plan,
-  arangodb::aql::ExecutionNode* scatter, arangodb::aql::Collection const* coll,
-  arangodb::aql::ExecutionNodeId targetNodeId, DistributeNodeDep const& distDep) {
-
-  // TODO(listunov): targetNodeId IndexNode is wrong here, isn't it ? EnumerateCollectionNode sounds more plausible
-
+                                  arangodb::aql::ExecutionNode* scatter,
+                                  arangodb::aql::Collection const* coll,
+                                  arangodb::aql::ExecutionNodeId targetNodeId,
+                                  DistributeNodeDependency const& distDep) {
   using namespace arangodb::aql;
 
   Ast* ast = plan.getAst();
-
   Variable* shardInputVar = ast->variables()->createTemporaryVariable();
   AstNode* obj = ast->createNodeObject();
 
-  for (auto const& member : distDep.members) {
-    LOG_RULE << "Add Member: " << distDep.distVar->name << "." << member;
-    obj->addMember(ast->createNodeObjectElement(
-      member,
-      ast->createNodeAttributeAccess(ast->createNodeReference(distDep.distVar), member)));
+  for (auto const& [shardKey, accessExpr] : distDep.shardKeyAccessMap) {
+    LOG_RULE << "Add expression for: " << shardKey;
+    obj->addMember(ast->createNodeObjectElement(shardKey, accessExpr));
   }
 
   auto expr = std::make_unique<Expression>(ast, obj);
-  auto* calc = plan.createNode<CalculationNode>(
-      &plan, plan.nextId(), std::move(expr), shardInputVar);
+  auto* calc = plan.createNode<CalculationNode>(&plan, plan.nextId(),
+                                                std::move(expr), shardInputVar);
 
+  LOG_RULE << "Create DistributeNode for collection: " << coll->name();
   auto* distribution = plan.createNode<DistributeNode>(
-    &plan, plan.nextId(), ScatterNode::ScatterType::SHARD, coll,
-    shardInputVar, targetNodeId);
+      &plan, plan.nextId(), ScatterNode::ScatterType::SHARD, coll,
+      shardInputVar, targetNodeId);
 
   plan.replaceNode(scatter, distribution);
   plan.insertBefore(distribution, calc);
@@ -233,13 +205,9 @@ void upgradeScatterToDistributeRule(Optimizer* opt,
                                     std::unique_ptr<ExecutionPlan> plan,
                                     OptimizerRule const& rule) {
   TRI_ASSERT(arangodb::ServerState::instance()->isCoordinator());
-
   bool wasModified = false;
-
   if (plan->isDisabledRule(static_cast<int>(
           OptimizerRule::removeUnnecessaryRemoteScatterRule))) {
-    // TODO(listunov): From the name, this should also be skipped when
-    // removeUnnecessaryRemoteScatterRule is off
     opt->addPlan(std::move(plan), rule, wasModified);
     return;
   }
@@ -251,10 +219,11 @@ void upgradeScatterToDistributeRule(Optimizer* opt,
                         },
                         true);
 
-  for (auto& node : nodes) {
+  for (auto const node : nodes) {
     ExecutionNode* current = node->getFirstParent();
     while (current != nullptr) {
       Condition condition(plan->getAst());
+      bool foundFirstIndexOrEnumerationNode = false;
       if (current->getType() == ExecutionNode::INDEX) {
         auto const indexNode = ExecutionNode::castTo<IndexNode const*>(current);
         auto const cond = indexNode->condition();
@@ -265,29 +234,33 @@ void upgradeScatterToDistributeRule(Optimizer* opt,
         if (filter != nullptr && filter->node() != nullptr) {
           condition.andCombine(filter->node());
         }
+        foundFirstIndexOrEnumerationNode = true;
       } else if (current->getType() == ExecutionNode::ENUMERATE_COLLECTION) {
-        auto const enumNode = ExecutionNode::castTo<EnumerateCollectionNode const*>(current);
+        auto const enumNode =
+            ExecutionNode::castTo<EnumerateCollectionNode const*>(current);
         auto const filter = enumNode->filter();
         if (filter != nullptr && filter->node() != nullptr) {
           condition.andCombine(filter->node());
         }
+        foundFirstIndexOrEnumerationNode = true;
       }
 
       if (condition.root() != nullptr) {
         condition.normalize(plan.get());
 
-        DistributeNodeDep distDep;
+        DistributeNodeDependency distDep;
         if (checkIfAllShardKeysAreUsed(condition.root(), current, distDep)) {
           auto const scatterNode = ExecutionNode::castTo<ScatterNode*>(node);
-          Collection const* coll{ getCollection(current) };
-          LOG_RULE << "--------------------------------------";
-          plan->show();
-          replaceScatterWithDistribute(*plan, scatterNode, coll, current->id(), distDep);
+          Collection const* coll{getCollection(current)};
+          replaceScatterWithDistribute(*plan, scatterNode, coll, current->id(),
+                                       distDep);
           wasModified = true;
-          LOG_RULE << "--------------------------------------";
-          plan->show();
         }
-        // Only the first Index / Enumeration Parent-Node is relevant for us, we can skip the rest
+      }
+
+      if (foundFirstIndexOrEnumerationNode) {
+        // Only the first Index / Enumeration Parent-Node is relevant for us, we
+        // can skip the rest
         break;
       }
       current = current->getFirstParent();
--- tests/js/client/aql/aql-optimizer-upgrade-scatter-to-distribute.js
+++ tests/js/client/aql/aql-optimizer-upgrade-scatter-to-distribute.js
@@ -7,126 +7,126 @@ const jsunity = require("jsunity");
 const db = require("@arangodb").db;
 const explainer = require("@arangodb/aql/explainer");
 
+const options = {
+  optimizer:{
+    rules:[
+      "-interchange-adjacent-enumerations"
+    ]}
+};
+
 function optimizerUpgradeScatterToDistributeSuite() {
 
   let col; // main collection, 3 shards
-  let dsl_col; // distrubute shards like the main collection
-  let rand_col; // sharded, but unrelated collection 
+  let col_dsl; // distribute shards like the main collection
+  let col_msk; // sharded, but unrelated collection with multiple shard keys
+  let col_msk_dsl; // distribute shards like the collection with multiple shard keys
 
   return {
     setUpAll: function () {
-      db._drop("UnitTestsCollection_rand_col");
-      db._drop("UnitTestsCollection_dsl_col");
+      db._drop("UnitTestsCollection_col_msk_dsl");
+      db._drop("UnitTestsCollection_col_msk");
+      db._drop("UnitTestsCollection_col_dsl");
       db._drop("UnitTestsCollection_col");
 
-      col = db._create("UnitTestsCollection_col", { numberOfShards: 3, shardKeys: ["_key", "mykey"] });
-      dsl_col = db._create("UnitTestsCollection_dsl_col", { numberOfShards: 3, shardKeys: ["_key", "mykey"], distributeShardsLike: "UnitTestsCollection_col" });
-      rand_col = db._create("UnitTestsCollection_rand_col", { numberOfShards: 3, shardKeys: ["_key", "mykey", "attr"] });
+      col = db._create("UnitTestsCollection_col", { numberOfShards: 3});
+      col_dsl = db._create("UnitTestsCollection_col_dsl", { numberOfShards: 3, distributeShardsLike: "UnitTestsCollection_col" });
+
+      col_msk = db._create("UnitTestsCollection_col_msk", { numberOfShards: 3, shardKeys: ["shardKey", "shardKey2"] });
+      col_msk_dsl = db._create("UnitTestsCollection_col_msk_dsl", { numberOfShards: 3, shardKeys: ["shardKey", "shardKey2"], distributeShardsLike: "UnitTestsCollection_col_msk" });
 
       let docs = [];
+      let docs_no_key = [];
       for (let i = 0; i < 1000; ++i) {
-        docs.push({ _key: "_key-" + i, mykey: "mykey-" + i, value: i });
+        docs.push({
+          _key: "key-" + i,
+          foreign_key : "key-" + i,
+          shardKey: "shardKey-" + i,
+          foreign_shardKey: "shardKey-" + i,
+          shardKey2: "shardKey2-" + i,
+          foreign_shardKey2: "shardKey2-" + i,
+          value: i
+        });
+        docs_no_key.push({
+          foreign_key : "key-" + i,
+          shardKey: "shardKey-" + i,
+          foreign_shardKey: "shardKey-" + i,
+          shardKey2: "shardKey2-" + i,
+          foreign_shardKey2: "shardKey2-" + i,
+          value: i
+        });
       }
       col.insert(docs);
-      dsl_col.insert(docs);
-      rand_col.insert(docs);
+      col_dsl.insert(docs);
+      col_msk.insert(docs_no_key);
+      col_msk_dsl.insert(docs_no_key);
     },
 
     tearDownAll: function () {
-      db._drop("UnitTestsCollection_rand_col");
-      db._drop("UnitTestsCollection_dsl_col");
+      db._drop("UnitTestsCollection_col_msk_dsl");
+      db._drop("UnitTestsCollection_col_msk");
+      db._drop("UnitTestsCollection_col_dsl");
       db._drop("UnitTestsCollection_col");
     },
 
-    test_SanityCheck_AllShardKeys_DistributedShardsLike_SmartJoins: function () {
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${dsl_col.name()}
-            FILTER (doc2._key == doc1._key
-              AND doc2.mykey == doc1.mykey)
-            RETURN [doc1, doc2]
-      `;
-      db._explain(query, {}, { optimizer: { rules: ["-remove-unnecessary-remote-scatter"] } });
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      assertTrue(plan.rules.includes("smart-joins"));
-    },
-
-    test_SanityCheck_PartialShardKeys_DistributedShardsLike_NoSmartJoins: function () {
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${dsl_col.name()}
-            FILTER doc2._key == doc1._key
-            RETURN [doc1, doc2]
-      `;
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      assertFalse(plan.rules.includes("smart-joins"));
-    },
-
-    test_ExplainDistribute: function() {
-      const query =
-        `FOR doc IN  ${col.name()} 
-          INSERT { _key: doc._key, mykey: doc.mykey, value: doc.value }
-          INTO ${dsl_col.name()}`;
-      db._explain(query);
-    },
-
-    test_AllShardKeys_NoDistributedShardsLike_NoSmartJoins_Upgrade: function () {
-      // TODO(listunov): Test multiple AND-branches
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${rand_col.name()}
-            FILTER (doc2._key == doc1._key
-              AND doc2.mykey == doc1.mykey
-              AND doc2.value == doc1.value)
-            RETURN [doc1, doc2]
-      `;
-      console.error('-------------------------------------------------------');
-      db._explain(query);
-      console.error('-------------------------------------------------------');
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      console.error(plan.rules);
-      console.error('-------------------------------------------------------');
-      assertFalse(plan.rules.includes("smart-joins"));
+    test_DefaultShardKey_Upgrade: function() {
+      let query =
+          `FOR doc1 IN ${col.name()}
+            FOR doc2 IN ${col.name()}
+             FILTER doc2._key == doc1.foreign_key
+             RETURN [doc1, doc2]`;
+      let plan = db._createStatement({query: query, options: options}).explain().plan;
       assertTrue(plan.rules.includes("upgrade-scatter-to-distribute"));
+      let result = db._query(query, {}, {}, options).toArray();
+      assertEqual(result.length, col.count());
     },
 
-    test_AllShardKeys_NoDistributedShardsLike_NotAllShardKeys_NoUpgrade: function () {
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${rand_col.name()}
-            FILTER doc2.mykey == doc1.mykey AND doc2.value == 5
-            RETURN [doc1, doc2]
-      `;
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      assertFalse(plan.rules.includes("smart-joins"));
-      assertFalse(plan.rules.includes("upgrade-scatter-to-distribute"));
+    test_MultipleShardKeys_Upgrade: function() {
+      let query =
+          `FOR doc1 IN ${col.name()}
+            FOR doc2 IN ${col_msk.name()}
+             FILTER doc2.shardKey == doc1.foreign_shardKey
+              AND doc2.shardKey2 == doc1.foreign_shardKey2
+             RETURN [doc1, doc2]`;
+      let plan = db._createStatement({query: query, options: options}).explain().plan;
+      assertTrue(plan.rules.includes("upgrade-scatter-to-distribute"));
+      let result = db._query(query, {}, {}, options).toArray();
+      assertEqual(result.length, col_msk.count());
     },
 
-    test_AllShardKeys_NoDistributedShardsLike_NotAllConditionsContainShardKeys_NoUpgrade: function () {
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${rand_col.name()}
-            FILTER (doc2._key == doc1._key
-              AND doc2.mykey == doc1.mykey)
-              OR doc2.value == 5
-            RETURN [doc1, doc2]
-      `;
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      assertFalse(plan.rules.includes("smart-joins"));
-      assertFalse(plan.rules.includes("upgrade-scatter-to-distribute"));
+
+    test_NoAttributeAccess_Upgrade: function() {
+      let query =
+          `FOR doc1 IN ${col.name()}
+            FOR doc2 IN ${col_msk.name()}
+              FILTER doc2.shardKey == "shardKey-12"
+               AND doc2.shardKey2 == "shardKey2-12"
+               AND doc2.value == doc1.value
+             RETURN [doc1, doc2]`;
+      db._explain(query, {}, options);
+      let plan = db._createStatement({query: query, options: options}).explain().plan;
+      assertTrue(plan.rules.includes("upgrade-scatter-to-distribute"));
+      let result = db._query(query, {}, {}, options).toArray();
+      assertEqual(result.length, 1);
     },
 
-    test_PartialShardKeys_NoDistributedShardsLike_NoSmartJoins: function () {
-      const query = `
-        FOR doc1 IN ${col.name()}
-          FOR doc2 IN ${rand_col.name()}
-            FILTER doc2._key == doc1._key
-            RETURN [doc1, doc2]
-      `;
-      let plan = db._createStatement({query: query, bindVars:  {}}).explain().plan;
-      assertFalse(plan.rules.includes("smart-joins"));
+    // TODO(listunov): Is this even possible ?
+    test_MultipleAndBranches_AllShards_Upgrade: function() {
+      let query =
+          `FOR doc1 IN ${col.name()}
+            FOR doc2 IN ${col_msk.name()}
+             FILTER (doc2.shardKey == doc1.foreign_shardKey
+              AND doc2.shardKey2 == doc1.foreign_shardKey2
+              AND doc2.value == 12) 
+              OR (doc2.shardKey == doc1.foreign_shardKey
+              AND doc2.shardKey2 == doc1.foreign_shardKey2
+              AND doc2.value == 15 AND doc2.foreign_key == "key-15")              
+             RETURN [doc1, doc2]`;
+      db._explain(query, {}, options);
+      let plan = db._createStatement({query: query, options: options}).explain().plan;
+      assertTrue(plan.rules.includes("upgrade-scatter-to-distribute"));
+      let result = db._query(query, {}, {}, options).toArray();
+      assertEqual(result.length, 2);
     },
-
   };
 }
 
