0aec12231c77dd654be89c8d1e5ac46633d556e0
knakatasf <knakata@dons.usfca.edu>
2026-05-12 04:26:12 +0200

    Add condition for injecting engine to EngineSelectorFeature and DatabaseFeature

--- tests/Mocks/Servers.cpp
+++ tests/Mocks/Servers.cpp
@@ -309,8 +309,12 @@ void MockServer::startFeatures() {
   _server.setupDependencies(false);
   auto orderedFeatures = _server.getOrderedFeatures();
 
-  _server.getFeature<EngineSelectorFeature>().setEngineTesting(_engine.get());
-  _server.getFeature<DatabaseFeature>().setEngineTesting(_engine.get());
+  if (_server.hasFeature<EngineSelectorFeature>()) {
+    _server.getFeature<EngineSelectorFeature>().setEngineTesting(_engine.get());
+  }
+  if (_server.hasFeature<DatabaseFeature>()) {
+    _server.getFeature<DatabaseFeature>().setEngineTesting(_engine.get());
+  }
 
   if (_server.hasFeature<SchedulerFeature>()) {
     auto& sched = _server.getFeature<SchedulerFeature>();
