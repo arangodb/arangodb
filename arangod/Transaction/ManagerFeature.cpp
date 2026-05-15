2f8f428a1f87b7fd143d75d91acbdd691f296544
knakatasf <knakata@dons.usfca.edu>
2026-05-15 20:30:59 +0200

    Remove EngineSelectorFeature from ManagerFeature and RocksDBRecorveryManager

--- arangod/Transaction/ManagerFeature.cpp
+++ arangod/Transaction/ManagerFeature.cpp
@@ -24,6 +24,8 @@
 #include "ManagerFeature.h"
 
 #include "ApplicationFeatures/ApplicationServer.h"
+#include "Cluster/ServerState.h"
+#include "ClusterEngine/ClusterEngine.h"
 #include "FeaturePhases/BasicFeaturePhaseServer.h"
 #include "Logger/LogMacros.h"
 #include "Logger/Logger.h"
@@ -32,7 +34,7 @@
 #include "Metrics/CounterBuilder.h"
 #include "Scheduler/SchedulerFeature.h"
 #include "RestServer/DatabaseFeature.h"
-#include "StorageEngine/EngineSelectorFeature.h"
+#include "RocksDBEngine/RocksDBEngine.h"
 #include "StorageEngine/StorageEngine.h"
 #include "Transaction/Manager.h"
 
@@ -54,7 +56,6 @@ ManagerFeature::ManagerFeature(application_features::ApplicationServer& server,
           metrics.add(arangodb_transactions_expired_total{})) {
   setOptional(false);
   startsAfter<BasicFeaturePhaseServer>();
-  startsAfter<EngineSelectorFeature>();
   startsAfter<metrics::MetricsFeature>();
   startsAfter<SchedulerFeature>();
   startsBefore<DatabaseFeature>();
@@ -122,11 +123,10 @@ timeout to the configured idle timeout.)");
 
 void ManagerFeature::prepare() {
   TRI_ASSERT(MANAGER.get() == nullptr);
-  TRI_ASSERT(server().getFeature<EngineSelectorFeature>().selected());
-  MANAGER = server()
-                .getFeature<EngineSelectorFeature>()
-                .engine()
-                .createTransactionManager(*this);
+  StorageEngine& engine = ServerState::instance()->isCoordinator()
+                     ? static_cast<StorageEngine&>(server().getFeature<ClusterEngine>())
+                     : static_cast<StorageEngine&>(server().getFeature<RocksDBEngine>());                    
+  MANAGER = engine.createTransactionManager(*this);
 }
 
 void ManagerFeature::start() {
