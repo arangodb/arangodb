c45ff752b345ee74ed6ba00df4b467398179d0e0
Julia Volmer <julia.volmer@arangodb.com>
2026-05-21 11:21:25 +0200

    Add more activity test cases

--- tests/Activities/ActivitiesSchedulerTest.cpp
+++ tests/Activities/ActivitiesSchedulerTest.cpp
@@ -65,7 +65,8 @@ struct ActivitiesSchedulerTest : ::testing::Test {
   activities::GenericActivityData activityData;
 };
 
-TEST_F(ActivitiesSchedulerTest, current_activity_persists) {
+TEST_F(ActivitiesSchedulerTest,
+       currently_executing_activity_persists_over_scheduled_executions) {
   auto outer_activity = arangodb::activities::make<activities::GenericActivity>(
       "TestActivity", this->activityData);
   auto guard = arangodb::activities::Registry::ScopedCurrentlyExecutingActivity(
@@ -125,7 +126,8 @@ TEST_F(ActivitiesSchedulerTest, multiple_queues) {
             outer_activity);
 }
 
-TEST_F(ActivitiesSchedulerTest, with_set_current_activity_works) {
+TEST_F(ActivitiesSchedulerTest,
+       scheduler_automatically_caputures_currently_executing_activiy) {
   auto outer_activity = arangodb::activities::make<activities::GenericActivity>(
       "TestActivity", this->activityData);
   auto guard = arangodb::activities::Registry::ScopedCurrentlyExecutingActivity(
@@ -137,13 +139,10 @@ TEST_F(ActivitiesSchedulerTest, with_set_current_activity_works) {
     auto new_guard =
         arangodb::activities::Registry::ScopedCurrentlyExecutingActivity(
             new_activity);
-    scheduler.queue(
-        arangodb::RequestLane::CLIENT_FAST,
-        arangodb::activities::withCurrentlyExecutingActivity([&new_activity]() {
-          EXPECT_EQ(
-              arangodb::activities::Registry::currentlyExecutingActivity(),
-              new_activity);
-        }));
+    scheduler.queue(arangodb::RequestLane::CLIENT_FAST, [&new_activity]() {
+      EXPECT_EQ(arangodb::activities::Registry::currentlyExecutingActivity(),
+                new_activity);
+    });
   }
 
   // TODO: Is there a way to know whether the queued thing ran?
