a77950f263e3084e019e3886b479f693628755ff
Julia Volmer <julia.volmer@arangodb.com>
2026-05-25 15:29:07 +0200

    Add docu to main entry-point

--- lib/Activities/docu/src/find_activity_subclasses.h
+++ lib/Activities/docu/src/find_activity_subclasses.h
@@ -17,6 +17,14 @@
  *   for (auto const& activity : activities) {
  *     std::cout << activity.data_type << " @ " << activity.owner_file << "\n";
  *   }
+ *
+ * This functions works the following:
+ * - runs over all the sources (module sources) we specify
+ * - user-defined matchers define which code we are interested in (module
+ * matcher)
+ * - for each match, it executes a user-defined callback that converts the match
+ * into an ActivityDeclaration and adds it to the output vector (module
+ * conversion)
  */
 auto find_all_activities(std::string const& path)
     -> std::vector<ActivityDeclaration>;
