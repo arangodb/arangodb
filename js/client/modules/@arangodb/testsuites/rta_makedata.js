0f4ef990a048bb94205fa9a1d773ae053c687f75
Wilfried Goesgens <willi@arangodb.com>
2026-06-10 10:12:21 +0200

    disable foxx tests on follower

--- js/client/modules/@arangodb/testsuites/rta_makedata.js
+++ js/client/modules/@arangodb/testsuites/rta_makedata.js
@@ -376,7 +376,7 @@ function makeDataWrapper (options) {
           // run checkdata for follower.
           if (count === 2 || count === 3) {
             let rtaNegFilter = this.options.rtaNegFilter;
-            this.options.rtaNegFilter = "070,071"
+            this.options.rtaNegFilter = "070,071";
             this.instanceManager.endpoint = this.instanceManager.arangods[1].endpoint;
             rc += this.runMakeData(moreargv, file, whichRTA, count, testCount, 1, res);
             res.total++;
