32a5063a2d7619890185027921cab61b67cedab0
knakatasf <knakata@dons.usfca.edu>
2026-06-19 11:43:17 +0200

    Format code; Add copyright

--- arangod/RocksDBEngine/Listeners/RocksDBActivitiesListener.cpp
+++ arangod/RocksDBEngine/Listeners/RocksDBActivitiesListener.cpp
@@ -1,3 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+////////////////////////////////////////////////////////////////////////////////
+
 #include "RocksDBActivitiesListener.h"
 
 #include "Activities/RegistryGlobalVariable.h"
@@ -7,29 +29,28 @@
 
 namespace arangodb {
 
-void RocksDBActivitiesListener::OnCompactionBegin(rocksdb::DB*, rocksdb::CompactionJobInfo const& info) {
-    auto handle = activities::make<activities::GenericActivity>(
-        "RocksDBCompaction",
-        activities::GenericActivityData{
-            {"job_id", std::to_string(info.job_id)},
-            {"column_family", info.cf_name},
-            {"base_input_level", std::to_string(info.base_input_level)},
-            {"output_level", std::to_string(info.output_level)},
-            {"input_files", std::to_string(info.input_files.size())},
-            {"reason", std::string(
-                rocksdb::GetCompactionReasonString(info.compaction_reason)
-            )},
-            {"phase", "running"}
-        }
-    );
+void RocksDBActivitiesListener::OnCompactionBegin(
+    rocksdb::DB*, rocksdb::CompactionJobInfo const& info) {
+  auto handle = activities::make<activities::GenericActivity>(
+      "RocksDBCompaction",
+      activities::GenericActivityData{
+          {"job_id", std::to_string(info.job_id)},
+          {"column_family", info.cf_name},
+          {"base_input_level", std::to_string(info.base_input_level)},
+          {"output_level", std::to_string(info.output_level)},
+          {"input_files", std::to_string(info.input_files.size())},
+          {"reason", std::string(rocksdb::GetCompactionReasonString(
+                         info.compaction_reason))},
+          {"phase", "running"}});
 
-    std::lock_guard guard(_mutex);
-    _activities.emplace(info.job_id, std::move(handle));
+  std::lock_guard guard(_mutex);
+  _activities.emplace(info.job_id, std::move(handle));
 }
 
-void RocksDBActivitiesListener::OnCompactionCompleted(rocksdb::DB*, rocksdb::CompactionJobInfo const& info) {
-    std::lock_guard guard(_mutex);
-    _activities.erase(info.job_id);
+void RocksDBActivitiesListener::OnCompactionCompleted(
+    rocksdb::DB*, rocksdb::CompactionJobInfo const& info) {
+  std::lock_guard guard(_mutex);
+  _activities.erase(info.job_id);
 }
 
 }  // namespace arangodb
--- arangod/RocksDBEngine/Listeners/RocksDBActivitiesListener.h
+++ arangod/RocksDBEngine/Listeners/RocksDBActivitiesListener.h
@@ -1,11 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
 #include "Activities/GenericActivity.h"
 
 #include <rocksdb/listener.h>
 
 #include <mutex>
 #include <unordered_map>
-
-#pragma once
 
 namespace rocksdb {
 struct CompactionJobInfo;
@@ -15,14 +37,16 @@ class DB;
 namespace arangodb {
 
 class RocksDBActivitiesListener : public rocksdb::EventListener {
-    public:
-     RocksDBActivitiesListener() = default;
+ public:
+  RocksDBActivitiesListener() = default;
 
-     void OnCompactionBegin(rocksdb::DB*, rocksdb::CompactionJobInfo const&) override;
-     void OnCompactionCompleted(rocksdb::DB*, rocksdb::CompactionJobInfo const&) override;
-    
-    private: 
-     std::mutex _mutex;
-     std::unordered_map<int, activities::GenericActivity::HandleType> _activities;
+  void OnCompactionBegin(rocksdb::DB*,
+                         rocksdb::CompactionJobInfo const&) override;
+  void OnCompactionCompleted(rocksdb::DB*,
+                             rocksdb::CompactionJobInfo const&) override;
+
+ private:
+  std::mutex _mutex;
+  std::unordered_map<int, activities::GenericActivity::HandleType> _activities;
 };
 }  // namespace arangodb
