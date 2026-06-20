940462e015fc4f94845cf5e9b1a4fb401ae42433
Vadim Kondratev <vadim@arangodb.com>
2026-06-20 14:18:31 +0200

    Update node modules to fix CVEs as of 19.06.2026 (#22810)

--- js/node/node_modules/@sinonjs/samsam/lib/is-element.js
+++ js/node/node_modules/@sinonjs/samsam/lib/is-element.js
@@ -8,7 +8,6 @@ var div = typeof document !== "undefined" && document.createElement("div");
  * Unlike Underscore.js/lodash, this function will return `false` if `object`
  * is an *element-like* object, i.e. a regular object with a `nodeType`
  * property that holds the value `1`.
- *
  * @alias module:samsam.isElement
  * @param {object} object The object to examine
  * @returns {boolean} Returns `true` for DOM element nodes
---
+++ js/node/node_modules/js-yaml/lib/type/str.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const Type = require('../type')
+
+module.exports = new Type('tag:yaml.org,2002:str', {
+  kind: 'scalar',
+  construct: function (data) { return data !== null ? data : '' }
+})
