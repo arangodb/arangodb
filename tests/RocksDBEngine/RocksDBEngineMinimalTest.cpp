7d3dc210fb34d7b525c3b7d4f8b7dc5a684207b7
Julia CP <julia@arangodb.com>
2026-06-22 11:22:10 -0300

    Improved test using mock

--- tests/RocksDBEngine/RocksDBEngineMinimalTest.cpp
+++ tests/RocksDBEngine/RocksDBEngineMinimalTest.cpp
@@ -21,21 +21,11 @@
 /// @author Julia Puget
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "gtest/gtest.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 #include "ApplicationFeatures/ApplicationServer.h"
-#include "Metrics/ICollector.h"
-
-// interface headers
-#include "RocksDBEngine/ISortingPolicy.h"
-#include "Cache/ICacheManagerProvider.h"
-#include "Replication2/ReplicatedLog/IReplicatedLogProvider.h"
-#include "RestServer/IDatabasePathProvider.h"
-#include "RestServer/IDatabaseProvider.h"
-#include "RestServer/IDumpLimitsProvider.h"
-#include "RestServer/IFlushControl.h"
-#include "RocksDBEngine/IIndexCacheRefill.h"
-#include "VectorIndex/IVectorIndexProvider.h"
+#include "Mocks.h"
 
 // concrete — both in arango_rocksdb
 #include "RocksDBEngine/RocksDBEngine.h"
@@ -43,8 +33,9 @@
 #include "RocksDBEngine/RocksDBRecoveryManager.h"
 
 using namespace arangodb;
-
-// --- stubs ---
+using namespace arangodb::tests;
+using ::testing::_;
+using ::testing::ReturnRef;
 
 struct TestRocksDBOptionsProvider final : RocksDBOptionsProvider {
   rocksdb::TransactionDBOptions getTransactionDBOptions() const override {
@@ -62,95 +53,32 @@ struct TestRocksDBOptionsProvider final : RocksDBOptionsProvider {
   }
 };
 
-struct TestDatabasePathProvider final : IDatabasePathProvider {
-  std::string const& directory() const override { return _dir; }
-  std::string subdirectoryName(std::string const& sub) const override {
-    return _dir + "/" + sub;
-  }
-  std::string _dir{"/tmp/test"};
-};
-
-struct TestVectorIndexProvider final : IVectorIndexProvider {
-  bool isVectorIndexEnabled() const noexcept override { return false; }
-};
-
-struct TestFlushControl final : IFlushControl {
-  bool isEnabled() const noexcept override { return false; }
-  std::tuple<std::size_t, std::size_t, TRI_voc_tick_t> releaseUnusedTicks()
-      override {
-    return {0, 0, 0};
-  }
-};
-
-struct TestDumpLimitsProvider final : IDumpLimitsProvider {
-  DumpLimitsFeatureOptions const& limits() const noexcept override {
-    return _limits;
-  }
-  DumpLimitsFeatureOptions _limits{};
-};
-
-struct TestDatabaseProvider final : IDatabaseProvider {
-  VocbasePtr useDatabase(std::string_view) const override { return {}; }
-  VocbasePtr useDatabase(TRI_voc_tick_t) const override { return {}; }
-  void enumerateDatabases(std::function<void(TRI_vocbase_t&)> const&) override {
-  }
-  void inventory(
-      velocypack::Builder&, TRI_voc_tick_t,
-      std::function<bool(LogicalCollection const*)> const&) override {}
-  replication::Version defaultReplicationVersion() const noexcept override {
-    return {};
-  }
-  bool extendedNames() const noexcept override { return false; }
-  void extendedNames(bool) noexcept override {}
-};
-
-struct TestCacheManagerProvider final : ICacheManagerProvider {
-  cache::Manager* manager() override { return nullptr; }
-};
-
-struct TestSortingPolicy final : ISortingPolicy {
-  basics::VelocyPackHelper::SortingMethod getSortingMethod()
-      const noexcept override {
-    return basics::VelocyPackHelper::SortingMethod::Correct;
-  }
-};
-
-struct TestIndexCacheRefill final : IIndexCacheRefill {
-  void scheduleFullIndexRefill(std::string const&, std::string const&,
-                               IndexId) override {}
-  bool autoRefill() const noexcept override { return false; }
-  bool autoRefillOnFollowers() const noexcept override { return false; }
-  void waitForCatchup() override {}
-};
-
-struct TestMetricsCollector final : metrics::ICollector {
- protected:
-  std::shared_ptr<metrics::Metric> doAdd(metrics::Builder& builder) override {
-    auto metric = builder.build();
-    _metrics.push_back(metric);
-    return metric;
-  }
-
- private:
-  std::vector<std::shared_ptr<metrics::Metric>> _metrics;
-};
-
-// --- test ---
-
 TEST(RocksDBEngineMinimal, CanConstruct) {
-  TestMetricsCollector metricsCollector;
+  MockMetricsCollector metricsCollector;
+  ON_CALL(metricsCollector, doAdd(_))
+      .WillByDefault([](metrics::Builder& builder) { return builder.build(); });
+
+  std::string dir{"/tmp/test"};
+  MockDatabasePathProvider dbPath;
+  ON_CALL(dbPath, directory()).WillByDefault(ReturnRef(dir));
+  ON_CALL(dbPath, subdirectoryName(_))
+      .WillByDefault(
+          [&dir](std::string const& sub) { return dir + "/" + sub; });
+
+  DumpLimitsFeatureOptions limitsOptions{};
+  MockDumpLimitsProvider dumpLimits;
+  ON_CALL(dumpLimits, limits()).WillByDefault(ReturnRef(limitsOptions));
+
   application_features::ApplicationServer server{nullptr, nullptr};
   auto& recovery = server.addFeature<RocksDBRecoveryManager>();
 
   TestRocksDBOptionsProvider optionsProvider;
-  TestDatabasePathProvider dbPath;
-  TestVectorIndexProvider vectorIdx;
-  TestFlushControl flush;
-  TestDumpLimitsProvider dumpLimits;
-  TestDatabaseProvider dbProvider;
-  TestCacheManagerProvider cacheManager;
-  TestSortingPolicy sortingPolicy;
-  TestIndexCacheRefill indexCacheRefill;
+  MockVectorIndexProvider vectorIdx;
+  MockFlushControl flush;
+  MockDatabaseProvider dbProvider;
+  MockCacheManagerProvider cacheManager;
+  MockSortingPolicy sortingPolicy;
+  MockIndexCacheRefill indexCacheRefill;
 
   auto& engine = server.addFeature<RocksDBEngine>(
       optionsProvider, metricsCollector, dbPath, vectorIdx, flush, dumpLimits,
