ee5f99d88842376fa2a1dd2c14cf344a2f96f676
knakatasf <knakata@dons.usfca.edu>
2026-06-29 21:16:14 +0200

    Add MockRegistry to tests

---
+++ tests/Mocks/MockRegistry.h
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Metrics/IRegistry.h"
+#include "Metrics/Builder.h"
+#include "Metrics/Metric.h"
+
+#include <memory>
+#include <vector>
+
+namespace arangodb::tests {
+
+struct MockRegistry : public metrics::IRegistry {
+ protected:
+  std::shared_ptr<metrics::Metric> doAdd(metrics::Builder& builder) override {
+    return _metrics.emplace_back(builder.build());
+  }
+
+ private:
+  std::vector<std::shared_ptr<metrics::Metric>> _metrics;
+};
+}  // namespace arangodb::tests
