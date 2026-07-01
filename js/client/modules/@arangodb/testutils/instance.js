f8cce7dbfe79a0edbbaf2353b1ac2c4197861cce
Wilfried Goesgens <willi@arangodb.com>
2026-07-01 14:33:55 +0200

    also clear on instance

--- js/client/modules/@arangodb/testutils/instance.js
+++ js/client/modules/@arangodb/testutils/instance.js
@@ -1581,38 +1581,37 @@ class instance {
     return true;
   }
   debugClearFailAt(failurePoint) {
-    if (!this.connect()) {
-      throw new Error(`${this.name}: failed to connect my instance {JSON.stringify(this.getStructure())}`);
-    }
-    if (failurePoint === "") {
-      failurePoint = undefined;
-    }
-    let deleteUrl = `/_admin/debug/failat/${(failurePoint=== undefined)?'': '/' + failurePoint}`;
-    let reply;
-    let count = 0;
-    while (count < 10) {
-      try {
-        reply = arango.DELETE_RAW(deleteUrl);
-        break;
-      } catch (ex) {
-        count += 1;
-        print(`${RED} ${this.name}: failed to delete failurepoint by ${ex}`);
-        this._disconnect();
-        this.connect();
+    return this.toThisInstance(() => {
+      if (failurePoint === "") {
+        failurePoint = undefined;
       }
-    }
-    if (reply.code !== 200) {
-      // we may no longer be able to work on a database as forced by fuerte
-      print(`${BLUE}${this.name}: fallback to internal.download to clear failurepoint${RESET}`);
-      let httpOptions = _.clone(this.authHeaders);
-      httpOptions.method = 'DELETE';
-      httpOptions.returnBodyOnError = true;
-      const reply = download(deleteUrl, '', httpOptions);
+      let deleteUrl = `/_admin/debug/failat/${(failurePoint=== undefined)?'': '/' + failurePoint}`;
+      let reply;
+      let count = 0;
+      while (count < 10) {
+        try {
+          reply = arango.DELETE_RAW(deleteUrl);
+          break;
+        } catch (ex) {
+          count += 1;
+          print(`${RED} ${this.name}: failed to delete failurepoint by ${ex}`);
+          this._disconnect();
+          this.connect();
+        }
+      }
       if (reply.code !== 200) {
-        throw new Error(`${this.name}: Failed to remove FP: '${failurePoint}' =>  ${JSON.stringify(reply.parsedBody)}`);
+        // we may no longer be able to work on a database as forced by fuerte
+        print(`${BLUE}${this.name}: fallback to internal.download to clear failurepoint${RESET}`);
+        let httpOptions = _.clone(this.authHeaders);
+        httpOptions.method = 'DELETE';
+        httpOptions.returnBodyOnError = true;
+        const reply = download(deleteUrl, '', httpOptions);
+        if (reply.code !== 200) {
+          throw new Error(`${this.name}: Failed to remove FP: '${failurePoint}' =>  ${JSON.stringify(reply.parsedBody)}`);
+        }
       }
-    }
-    return true;
+      return true;
+    });
   }
   debugCanUseFailAt() {
     let reply = arango.GET_RAW('/_admin/debug/failat/');
