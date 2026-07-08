88cde4fd9b57a881dcabc8a9f6aaa0e96a49869f
Wilfried Goesgens <willi@arangodb.com>
2026-07-08 17:27:23 +0200

    clip the uninteresting parts from the rta-makedata error output

--- js/client/modules/@arangodb/testsuites/rta_makedata.js
+++ js/client/modules/@arangodb/testsuites/rta_makedata.js
@@ -251,8 +251,7 @@ function makeDataWrapper (options) {
       res[whichRTA] = rc;
       if (!rc.status) {
         this.continueTesting = false;
-        let rx = new RegExp(/\\n/g);
-        res[whichRTA].message += file + ':\n' + fs.read(logFile).replace(rx, '\n');
+        res[whichRTA].message += file + ':\n' + ct.run.readRtaErrorLog(logFile),
         res.status = false;
         res.failed += 1;
       } else {
