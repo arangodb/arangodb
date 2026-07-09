7690fafe3ed4bc51a5b99381fdc8eec5a5df7bf2
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-07-09 13:53:55 +0100

    Add regression test

---
+++ tests/js/client/aql/aql-traversal-vars-used-regression.js
@@ -0,0 +1,77 @@
+/*jshint globalstrict:false, strict:false, maxlen: 500 */
+/*global assertEqual, assertTrue, fail */
+
+// //////////////////////////////////////////////////////////////////////////////
+// / DISCLAIMER
+// /
+// / Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+// / Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+// /
+// / Licensed under the Business Source License 1.1 (the "License");
+// / you may not use this file except in compliance with the License.
+// / You may obtain a copy of the License at
+// /
+// /     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+// /
+// / Unless required by applicable law or agreed to in writing, software
+// / distributed under the License is distributed on an "AS IS" BASIS,
+// / WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// / See the License for the specific language governing permissions and
+// / limitations under the License.
+// /
+// / Copyright holder is ArangoDB GmbH, Cologne, Germany
+// /
+// //////////////////////////////////////////////////////////////////////////////
+
+const jsunity = require("jsunity");
+const internal = require("internal");
+const errors = internal.errors;
+const explainer = require("@arangodb/aql/explainer");
+const db = require("@arangodb").db;
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief test suite
+////////////////////////////////////////////////////////////////////////////////
+
+function traversalVarsUsedRegressionSuite() {
+  const testDatabaseName = "UnitTestDatabase";
+  const testDocumentCollectionName = "UnitTestDocuments";
+  const testEdgeCollectionName = "UnitTestEdgeCollection";
+  
+  return {
+    setUp: function() {
+      let coll = db._createDocumentCollection(testDocumentCollectionName);
+      db._createEdgeCollection(testEdgeCollectionName);
+
+      var projects = [];
+
+      // need enough documents for a materialize node to be part of the plan!
+      for(var i = 0; i<101; i++) {
+	projects.push({name: `${i}`});
+      }
+      coll.insert(projects);
+      coll.ensureIndex({type: "persistent", fields: [ "name" ]});
+    },
+    tearDown: function() {
+      db._drop(testEdgeCollectionName);
+      db._drop(testDocumentCollectionName);
+    },
+    testTraversalVarsUsedRegression: function () {
+      let query = 
+        `FOR doc IN ${testDocumentCollectionName}
+              SORT doc.name
+                FOR v, e, p IN 1..1 OUTBOUND {} ${testEdgeCollectionName}
+	        FILTER v.tag == doc.tag
+                RETURN v
+        `;
+
+      let ex = explainer.explain(query);
+      require("internal").log(JSON.stringify(ex));
+    },
+
+  };
+}
+
+jsunity.run(traversalVarsUsedRegressionSuite);
+
+return jsunity.done();
