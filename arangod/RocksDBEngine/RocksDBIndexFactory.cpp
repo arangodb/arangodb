2234ad323269207bd7b8ae3a9743b31de43efbd8
Jure Bajic <jure.bajic94@gmail.com>
2026-07-16 09:35:09 +0200

    Remove aliases

--- arangod/RocksDBEngine/RocksDBIndexFactory.cpp
+++ arangod/RocksDBEngine/RocksDBIndexFactory.cpp
@@ -563,13 +563,11 @@ RocksDBIndexFactory::RocksDBIndexFactory(
           iresearchInvertedIndexFactory);
 }
 
-/// @brief index name aliases (e.g. "persistent" => "hash", "skiplist" =>
-/// "hash") used to display storage engine capabilities
+/// @brief index name aliases (e.g. "zkd" => "mdi") used to display storage
+/// engine capabilities
 std::vector<std::pair<std::string_view, std::string_view>>
 RocksDBIndexFactory::indexAliases() const {
   return {
-      {"hash", "persistent"},
-      {"skiplist", "persistent"},
       {"zkd", "mdi"},
   };
 }
