87858ece0788fd552498b0aa9dab3fe021992fa9
Max Neunhoeffer <max@arangodb.com>
2026-07-27 16:48:28 +0200

    First check existence, then permissions for drop collection

--- arangod/RestHandler/RestCollectionHandler.cpp
+++ arangod/RestHandler/RestCollectionHandler.cpp
@@ -722,14 +722,6 @@ async<void> RestCollectionHandler::handleCommandDelete() {
       _request->parsedValue(StaticStrings::DataSourceSystem, false);
   _builder.clear();
 
-  // Check if we are allowed to drop the collection:
-  if (auto r = ExecContext::current().canDropCollection(_vocbase.name(), name);
-      r.fail()) {
-    events::DropCollection(_vocbase.name(), name, TRI_ERROR_FORBIDDEN);
-    generateError(r);
-    co_return;
-  }
-
   std::shared_ptr<LogicalCollection> coll;
   Result res = methods::Collections::lookup(_vocbase, name, coll);
   if (res.fail()) {
@@ -738,6 +730,14 @@ async<void> RestCollectionHandler::handleCommandDelete() {
     co_return;
   }
   TRI_ASSERT(coll);
+
+  // Check if we are allowed to drop the collection:
+  if (auto r = ExecContext::current().canDropCollection(_vocbase.name(), name);
+      r.fail()) {
+    events::DropCollection(_vocbase.name(), name, TRI_ERROR_FORBIDDEN);
+    generateError(r);
+    co_return;
+  }
 
   {
     VPackObjectBuilder obj(&_builder, true);
