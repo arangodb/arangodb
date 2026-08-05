6488b31fd16888a80c7734bc900f2fd224eddb76
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-08-05 16:51:19 +0100

    Throw parse error directly
    This is equivalent to the previous code.

--- arangod/Aql/Parser/Parser.cpp
+++ arangod/Aql/Parser/Parser.cpp
@@ -167,7 +167,7 @@ void Parser::registerParseError(ErrorCode errorCode, std::string_view data,
       absl::StrCat(data, " near '", queryString().extractRegion(line, column),
                    "' at position ", line, ":", (column + 1));
 
-  _query.warnings().registerError(errorCode, errorMessage);
+  THROW_ARANGO_EXCEPTION_MESSAGE(errorCode, errorMessage);
 }
 
 /// @brief register a warning
