f39c9939e03093b9be6e0a1cf31e9fb4bb0e2802
knakatasf <knakata@dons.usfca.edu>
2026-08-12 01:16:56 +0200

    Add new typed data structure: CollectionDescriptor

---
+++ arangod/VocBase/Properties/CollectionDescriptor.h
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2026 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "VocBase/Properties/CollectionConstantProperties.h"
+#include "VocBase/Properties/CollectionMutableProperties.h"
+#include "VocBase/Properties/CollectionInternalProperties.h"
+#include "VocBase/Properties/ClusteringProperties.h"
+#include "VocBase/Properties/CollectionStorageProperties.h"
+
+namespace arangodb {
+
+struct CollectionDescriptor {
+  CollectionConstantProperties constant{};
+  CollectionMutableProperties mutableProps{};
+  CollectionInternalProperties internal{};
+  ClusteringProperties clustering{};
+  CollectionStorageProperties storage{};
+  // CollectionIndexesProperties indexes{};  // optional Phase-1 scope
+  bool operator==(CollectionDescriptor const&) const = default;
+};
+
+template<class Inspector>
+auto inspect(Inspector& f, CollectionDescriptor& d) {
+  return f.object(d).fields(
+      f.embedFields(d.constant),
+      f.embedFields(d.mutableProps),
+      f.embedFields(d.internal),
+      f.embedFields(d.clustering),
+      f.embedFields(d.storage));
+}
+
+}  // namespace arangodb
