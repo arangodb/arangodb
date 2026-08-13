7e66f7b0377488869f8f624c845bdbfe0188cb26
Max Neunhoeffer <max@arangodb.com>
2026-08-13 15:48:01 +0200

    Revert "Fix external endpoint for rbac service"
    This reverts commit 9584a41783b807e133f6090b1808fd1adab9b0db.

--- arangod/GeneralServer/AuthenticationOptionsProvider.cpp
+++ arangod/GeneralServer/AuthenticationOptionsProvider.cpp
@@ -27,7 +27,6 @@
 #include "Logger/Logger.h"
 #include "ProgramOptions/Parameters.h"
 #include "ProgramOptions/ProgramOptions.h"
-#include "absl/strings/str_cat.h"
 
 #include <limits>
 
@@ -265,13 +264,8 @@ void AuthenticationOptionsProvider::validateOptionsImpl(
   }
 
   if (!options.externalRbacService.empty()) {
-    if (options.externalRbacService.starts_with("http://")) {
-      options.externalRbacService =
-          absl::StrCat("tcp://", options.externalRbacService.substr(7));
-    } else if (options.externalRbacService.starts_with("https://")) {
-      options.externalRbacService =
-          absl::StrCat("ssl://", options.externalRbacService.substr(8));
-    } else {
+    if (!options.externalRbacService.starts_with("http://") &&
+        !options.externalRbacService.starts_with("https://")) {
       LOG_TOPIC("1aaaf", FATAL, arangodb::Logger::AUTHENTICATION)
           << "--server.external-rbac-service must start with http:// or "
              "https://";
