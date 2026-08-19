decffd8044002fb2d592d4a4c5a506ab6314b801
Wilfried Goesgens <willi@arangodb.com>
2026-08-19 11:49:32 +0200

    set the deadline for each individual makedata run, as we do for spawned arangosh tests as well.

--- js/client/modules/@arangodb/testutils/client-tools.js
+++ js/client/modules/@arangodb/testutils/client-tools.js
@@ -776,7 +776,13 @@ function rtaMakedata(options, instanceManager, writeReadClean, msg, logFile, mor
   }
   
   let timeout = (options.isInstrumented) ? 60 * 45 : 60 * 15;
-  return pu.executeAndWait(pu.ARANGOSH_BIN, argv, options, 'arangosh', instanceManager.rootDir, options.coreCheck, timeout);
+  SetGlobalExecutionDeadlineTo(timeout);
+  let ret = pu.executeAndWait(pu.ARANGOSH_BIN, argv, options, 'arangosh', instanceManager.rootDir, options.coreCheck, timeout);
+  let timeout = SetGlobalExecutionDeadlineTo(0.0);
+  if (timeout) {
+    ret.status = false;
+  }
+  return ret;
 }
 function rtaWaitShardsInSync(options, instanceManager) {
   let args = Object.assign(makeArgsArangosh(options), {
