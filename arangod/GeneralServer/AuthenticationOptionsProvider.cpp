bd4f02fd0d2143a1c109ea1579e72fc011d68305
Simran Spiller <simran.spiller@arango.ai>
2026-08-26 18:20:02 +0200

    Add details about secret selection, PEM-encoded keys, and HMAC fallback

--- arangod/GeneralServer/AuthenticationOptionsProvider.cpp
+++ arangod/GeneralServer/AuthenticationOptionsProvider.cpp
@@ -229,11 +229,30 @@ You can use this feature to roll out new JWT secrets throughout a cluster.)");
           "A folder containing one or more JWT secret files to use for JWT "
           "authentication.",
           new StringParameter(&options.jwtSecretFolderProgramOption))
-      .setLongDescription(R"(Hidden files (starting with a `.`) and files with
-the extension `.tmp` are ignored. The other files are sorted alphabetically, the
-first secret is used for signing + verifying JWT tokens (_active_ secret), and
-all other secrets are only used to validate incoming JWT tokens (_passive_
-secrets). Only one secret needs to verify a JWT token for it to be accepted.
+      .setLongDescription(R"(The files in the specified folder are processed
+as follows:
+
+1. The files are sorted alphabetically by their name.
+2. Hidden files (starting with a `.`) and files with the extension `.tmp` are
+   skipped.
+3. The content of the first non-skipped file is used for the first secret. The
+   first secret is used for signing and verifying JWT tokens (_active_ secret).
+   - If the file contains a PEM-encoded public/private key pair, the public key
+     is used for verification and the private key to sign JWT.
+   - If the file contains a PEM-encoded public key only, the server can't sign
+     JWT because it lacks the private key and communication may be defunct.
+   - If the file is PEM-encoded but contains no private or public key, an error
+     is raised.
+   - In all other cases, the binary content of the file is used as a symmetric
+     HMAC secret, used for both verifying and signing JWT.
+4. All other non-skipped files are read and used for additional secrets.
+   They are only used to validate incoming JWT tokens (_passive_ secrets).
+   Only one secret needs to verify a JWT token for it to be accepted.
+   - If the file contains a PEM-encoded public/private key pair or public key,
+     the public key is used for verification.
+   - If the file isn't PEM-encoded, the binary content of the file is used as a
+     symmetric HMAC secret, used for verifying JWT.
+   - If the file is PEM-encoded but contains no public key, an error is raised.
 
 You can reload JWT secrets from disk without restarting the server or the nodes
 of a cluster deployment via the `POST /_admin/server/jwt` HTTP API endpoint.
