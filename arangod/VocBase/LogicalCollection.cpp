76a537eb14baefb51e79af5b02dcdda824fcb138
knakatasf <knakata@dons.usfca.edu>
2026-08-31 20:07:36 +0200

    Remove duplicated groupId

--- arangod/VocBase/LogicalCollection.cpp
+++ arangod/VocBase/LogicalCollection.cpp
@@ -893,7 +893,8 @@ Result LogicalCollection::appendVPack(velocypack::Builder& build,
                                                 "smartGraphAttribute",
                                                 "smartJoinAttribute",
                                                 "schema",
-                                                "computedValues"};
+                                                "computedValues",
+                                                "groupId"};
 
   for (auto it : VPackObjectIterator(tmp.slice())) {
     auto key = it.key.stringView();
