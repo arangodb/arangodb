3c84f2b7d54ad7abf97df7a83d3daa56efba94b9
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2026-09-03 17:32:10 +0100

    Move getIndexForSortCondition

--- arangod/Aql/Condition.cpp
+++ arangod/Aql/Condition.cpp
@@ -1132,7 +1132,7 @@ void Condition::optimize(ExecutionPlan* plan, bool multivalued) {
           ++l;
         }
       }  // cross compare sub-and-nodes
-    }    // foreach sub-and-node
+    }  // foreach sub-and-node
 
   fastForwardToNextOrItem:
     if (!retry) {
