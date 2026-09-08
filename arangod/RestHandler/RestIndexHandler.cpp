5faffa41d81e0640e73f9fad92f83124e2e8c5e4
Julia Volmer <julia.volmer@arangodb.com>
2026-09-08 21:51:28 +0200

    And one more

--- arangod/RestHandler/RestIndexHandler.cpp
+++ arangod/RestHandler/RestIndexHandler.cpp
@@ -230,23 +230,25 @@ ResultT<std::shared_ptr<LogicalCollection>> RestIndexHandler::collection(
     return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND};
   }
 
-  if (auth::isNameAndNoId(cName).fail()) {
-    return Result{TRI_ERROR_FORBIDDEN};
+  if (not ServerState::instance()->isDBServer()) {
+    if (auth::isNameAndNoId(cName).fail()) {
+      return Result{TRI_ERROR_FORBIDDEN};
+    }
+    if (auto r = ExecContext::current().canUseCollection(_vocbase.name(), cName,
+                                                         AccessLevel::Read);
+        r.fail()) {
+      return Result{TRI_ERROR_FORBIDDEN};
+    }
   }
-  if (auto r = ExecContext::current().canUseCollection(_vocbase.name(), cName,
-                                                       AccessLevel::Read);
-      r.fail()) {
-    return Result{TRI_ERROR_FORBIDDEN};
-  }
-  if (not ServerState::instance()->isCoordinator()) {
-    auto coll = _vocbase.lookupCollection(cName);
+  if (ServerState::instance()->isCoordinator()) {
+    auto coll =
+        _clusterFeature.clusterInfo().getCollectionNT(_vocbase.name(), cName);
     if (coll == nullptr) {
       return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND};
     }
     return coll;
   }
-  auto coll =
-      _clusterFeature.clusterInfo().getCollectionNT(_vocbase.name(), cName);
+  auto coll = _vocbase.lookupCollection(cName);
   if (coll == nullptr) {
     return Result{TRI_ERROR_ARANGO_DATA_SOURCE_NOT_FOUND};
   }
