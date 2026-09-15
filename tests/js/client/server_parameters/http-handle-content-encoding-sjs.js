6ecd887280018720181d853f6c837dec3aad8f78
Wilfried Goesgens <willi@arangodb.com>
2026-09-15 09:30:46 +0200

    fix test

--- tests/js/client/server_parameters/http-handle-content-encoding-sjs.js
+++ tests/js/client/server_parameters/http-handle-content-encoding-sjs.js
@@ -86,14 +86,7 @@ function HandleContentEncodingSuite() {
         // this is still supposed to fail, because we are not sending any
         // valid content
         assertEqual(400, res.status, res);
-        let count = 0;
-        ['support for handling Content-Encoding headers is turned off for unauthenticated requests', 
-         'decoding error occurred while handling Content-Encoding'].forEach(msg => {
-           if (res.json.errorMessage.search(msg) >= 0) {
-             count += 1;
-           }
-         });
-        assertTrue(count > 0, `error message not one of the wanted: ${JSON.stringify(res)}`);
+        assertMatch(new RegExp(`a decoding error occurred while handling Content-Encoding: ${h["Content-Encoding"]}`), res.json.errorMessage);
       });
     },
     
