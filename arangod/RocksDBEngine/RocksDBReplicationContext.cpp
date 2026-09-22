52ce09b92bc56325504d8167e8ec3691cbbaec9b
Jure Bajic <jure.bajic94@gmail.com>
2026-09-22 15:41:52 +0200

    Fix build

--- arangod/RocksDBEngine/RocksDBReplicationContext.cpp
+++ arangod/RocksDBEngine/RocksDBReplicationContext.cpp
@@ -1120,7 +1120,7 @@ void RocksDBReplicationContext::extendLifetime(double ttl) {
 
   std::lock_guard locker{_contextLock};
 
-  ttl = std::min(std::max(_ttl, ttl), replutils::BatchInfo::MaxTimeout);
+  ttl = std::min(std::max(_ttl, ttl), replutils::BatchInfo::kMaxTimeout);
   TRI_ASSERT(ttl > 0.0);
   _expires = now + ttl;
 
